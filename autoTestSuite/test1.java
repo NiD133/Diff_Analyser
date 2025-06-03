b8e68ebb29c80e6e5b34937d6402cfe8fc2f44ea
NiD133 <k19052355@kcl.ac.uk>
2025-06-03 23:47:08 +0100

    Add Original Manualsuite Daterange

--- autoTestSuite/test1.java
+++ autoTestSuite/test1.java
@@ -1,104 +1,106 @@
+/* ======================================================
+ * JFreeChart : a chart library for the Java(tm) platform
+ * ======================================================
+ *
+ * (C) Copyright 2000-present, by David Gilbert and Contributors.
+ *
+ * Project Info:  https://www.jfree.org/jfreechart/index.html
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ *
+ * [Oracle and Java are registered trademarks of Oracle and/or its affiliates.
+ * Other names may be trademarks of their respective owners.]
+ *
+ * ------------------
+ * DateRangeTest.java
+ * ------------------
+ * (C) Copyright 2004-present, by David Gilbert and Contributors.
+ *
+ * Original Author:  David Gilbert;
+ * Contributor(s):   -;
+ *
+ */
+
 package org.jfree.data.time;
+
+import java.util.Date;
 
 import org.jfree.chart.TestUtils;
 import org.junit.jupiter.api.Test;
 
-import java.util.Date;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * Tests for the {@link DateRange} class.  These tests cover equality,
- * serialization, immutability and ensure the class behaves as expected.
+ * Some tests for the {@link DateRange} class.
  */
 public class DateRangeTest {
 
     /**
-     * Tests the {@link DateRange#equals(Object)} method to ensure that
-     * DateRange objects with the same start and end dates are considered equal,
-     * and those with different dates are not.
+     * Confirm that the equals method can distinguish all the required fields.
      */
     @Test
     public void testEquals() {
-        // Create two DateRange objects with the same start and end dates.
-        DateRange range1 = new DateRange(new Date(1000L), new Date(2000L));
-        DateRange range2 = new DateRange(new Date(1000L), new Date(2000L));
+        DateRange r1 = new DateRange(new Date(1000L), new Date(2000L));
+        DateRange r2 = new DateRange(new Date(1000L), new Date(2000L));
+        assertEquals(r1, r2);
+        assertEquals(r2, r1);
 
-        // Assert that the two ranges are equal.
-        assertEquals(range1, range2);
-        assertEquals(range2, range1);
+        r1 = new DateRange(new Date(1111L), new Date(2000L));
+        assertNotEquals(r1, r2);
+        r2 = new DateRange(new Date(1111L), new Date(2000L));
+        assertEquals(r1, r2);
 
-        // Modify the start date of range1 and assert that the ranges are no longer equal.
-        range1 = new DateRange(new Date(1111L), new Date(2000L));
-        assertNotEquals(range1, range2);
-
-        // Update range2 to match range1 and verify they are equal.
-        range2 = new DateRange(new Date(1111L), new Date(2000L));
-        assertEquals(range1, range2);
-
-        // Modify the end date of range1 and assert that the ranges are no longer equal.
-        range1 = new DateRange(new Date(1111L), new Date(2222L));
-        assertNotEquals(range1, range2);
-
-        // Update range2 to match range1 and verify they are equal.
-        range2 = new DateRange(new Date(1111L), new Date(2222L));
-        assertEquals(range1, range2);
+        r1 = new DateRange(new Date(1111L), new Date(2222L));
+        assertNotEquals(r1, r2);
+        r2 = new DateRange(new Date(1111L), new Date(2222L));
+        assertEquals(r1, r2);
     }
 
     /**
-     * Tests the serialization and deserialization of a {@link DateRange} object.
-     * Ensures that a serialized DateRange can be successfully restored with the same
-     * start and end dates.
+     * Serialize an instance, restore it, and check for equality.
      */
     @Test
     public void testSerialization() {
-        // Create a DateRange object.
-        DateRange originalRange = new DateRange(new Date(1000L), new Date(2000L));
-
-        // Serialize and deserialize the DateRange object.
-        DateRange deserializedRange = TestUtils.serialised(originalRange);
-
-        // Assert that the original and deserialized ranges are equal.
-        assertEquals(originalRange, deserializedRange);
+        DateRange r1 = new DateRange(new Date(1000L), new Date(2000L));
+        DateRange r2 = TestUtils.serialised(r1);
+        assertEquals(r1, r2);
     }
 
     /**
-     * Verifies that the {@link DateRange} class does not implement the {@link Cloneable}
-     * interface, as DateRange objects are immutable.
+     * The {@link DateRange} class is immutable, so it doesn't need to
+     * be cloneable.
      */
     @Test
     public void testClone() {
-        // Create a DateRange object.
-        DateRange range = new DateRange(new Date(1000L), new Date(2000L));
-
-        // Assert that the DateRange object is not an instance of Cloneable.
-        assertFalse(range instanceof Cloneable);
+        DateRange r1 = new DateRange(new Date(1000L), new Date(2000L));
+        assertFalse(r1 instanceof Cloneable);
     }
 
     /**
-     * Tests that the {@link DateRange} class is immutable. This means that modifying
-     * the original Date objects used to create the DateRange should not affect the
-     * DateRange object itself.
+     * Confirm that a DateRange is immutable.
      */
     @Test
     public void testImmutable() {
-        // Create Date objects.
-        Date date1 = new Date(10L);
-        Date date2 = new Date(20L);
-
-        // Create a DateRange object using the Date objects.
-        DateRange range = new DateRange(date1, date2);
-
-        // Modify the original Date object after creating the DateRange.
-        date1.setTime(11L);
-
-        // Assert that the DateRange object still holds the original Date value.
-        assertEquals(new Date(10L), range.getLowerDate());
-
-        // Attempt to modify the upper date through the getter.
-        range.getUpperDate().setTime(22L);
-
-        // Assert that the DateRange object still holds the original Date value.
-        assertEquals(new Date(20L), range.getUpperDate());
+        Date d1 = new Date(10L);
+        Date d2 = new Date(20L);
+        DateRange r = new DateRange(d1, d2);
+        d1.setTime(11L);
+        assertEquals(new Date(10L), r.getLowerDate());
+        r.getUpperDate().setTime(22L);
+        assertEquals(new Date(20L), r.getUpperDate());
     }
+
 }
