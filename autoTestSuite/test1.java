6d346f375ab1fa9a7b45e91a3b06e9f01f79f025
NiD133 <k19052355@kcl.ac.uk>
2025-06-03 23:38:59 +0100

    Add Original Manualsuite Byteorder

--- autoTestSuite/test1.java
+++ autoTestSuite/test1.java
@@ -1,212 +1,156 @@
-package org.apache.commons.io.input;
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.io;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import java.io.IOException;
-import java.io.Reader;
-import java.io.StringReader;
-import java.nio.CharBuffer;
-import org.apache.commons.io.input.BoundedReader;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class BoundedReaderTest {
+import java.nio.charset.Charset;
 
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test for {@link ByteOrderMark}.
+ */
+public class ByteOrderMarkTest {
+
+    private static final ByteOrderMark TEST_BOM_1 = new ByteOrderMark("test1", 1);
+    private static final ByteOrderMark TEST_BOM_2 = new ByteOrderMark("test2", 1, 2);
+    private static final ByteOrderMark TEST_BOM_3 = new ByteOrderMark("test3", 1, 2, 3);
+
+    /** Tests that {@link ByteOrderMark#getCharsetName()} can be loaded as a {@link java.nio.charset.Charset} as advertised. */
     @Test
-    public void testReadIntoCharBufferWhenLimitIsZero() throws IOException {
-        StringReader stringReader = new StringReader("TO[Gj");
-        BoundedReader boundedReader = new BoundedReader(stringReader, 0);
-        CharBuffer charBuffer = CharBuffer.wrap("TO[Gj");
-        // Since the limit is 0, no characters should be read, and -1 should be returned.
-        int charsRead = boundedReader.read(charBuffer);
-        assertEquals(-1, charsRead);
+    public void testConstantCharsetNames() {
+        assertNotNull(Charset.forName(ByteOrderMark.UTF_8.getCharsetName()));
+        assertNotNull(Charset.forName(ByteOrderMark.UTF_16BE.getCharsetName()));
+        assertNotNull(Charset.forName(ByteOrderMark.UTF_16LE.getCharsetName()));
+        assertNotNull(Charset.forName(ByteOrderMark.UTF_32BE.getCharsetName()));
+        assertNotNull(Charset.forName(ByteOrderMark.UTF_32LE.getCharsetName()));
     }
 
+    /** Tests Exceptions */
     @Test
-    public void testMarkAndReadSingleCharacter() throws IOException {
-        StringReader stringReader = new StringReader("QN?");
-        BoundedReader boundedReader = new BoundedReader(stringReader, 2680);
-        // Read the first character ('Q')
-        boundedReader.read();
-        // Mark the current position (after 'Q')
-        boundedReader.mark(2680);
-        // Read the next character ('N')
-        int nextChar = boundedReader.read();
-        // Verify that the next character is 'N' (ASCII 78)
-        assertEquals(78, nextChar);
+    public void testConstructorExceptions() {
+        assertThrows(NullPointerException.class, () -> new ByteOrderMark(null, 1, 2, 3));
+        assertThrows(IllegalArgumentException.class, () -> new ByteOrderMark("", 1, 2, 3));
+        assertThrows(NullPointerException.class, () -> new ByteOrderMark("a", (int[]) null));
+        assertThrows(IllegalArgumentException.class, () -> new ByteOrderMark("b"));
     }
 
+    /** Tests {@link ByteOrderMark#equals(Object)} */
+    @SuppressWarnings("EqualsWithItself")
     @Test
-    public void testReadZeroCharactersIntoCharArray() throws IOException {
-        StringReader stringReader = new StringReader("");
-        BoundedReader boundedReader = new BoundedReader(stringReader, 1148);
-        char[] charArray = new char[1];
-        // Attempt to read 0 characters from the reader into the array.
-        int charsRead = boundedReader.read(charArray, 0, 0);
-        // Verify that 0 characters were read.
-        assertEquals(0, charsRead);
+    public void testEquals() {
+        assertEquals(ByteOrderMark.UTF_16BE, ByteOrderMark.UTF_16BE);
+        assertEquals(ByteOrderMark.UTF_16LE, ByteOrderMark.UTF_16LE);
+        assertEquals(ByteOrderMark.UTF_32BE, ByteOrderMark.UTF_32BE);
+        assertEquals(ByteOrderMark.UTF_32LE, ByteOrderMark.UTF_32LE);
+        assertEquals(ByteOrderMark.UTF_8, ByteOrderMark.UTF_8);
+
+        assertNotEquals(ByteOrderMark.UTF_8, ByteOrderMark.UTF_16BE);
+        assertNotEquals(ByteOrderMark.UTF_8, ByteOrderMark.UTF_16LE);
+        assertNotEquals(ByteOrderMark.UTF_8, ByteOrderMark.UTF_32BE);
+        assertNotEquals(ByteOrderMark.UTF_8, ByteOrderMark.UTF_32LE);
+
+        assertEquals(TEST_BOM_1, TEST_BOM_1, "test1 equals");
+        assertEquals(TEST_BOM_2, TEST_BOM_2, "test2 equals");
+        assertEquals(TEST_BOM_3, TEST_BOM_3, "test3 equals");
+
+        assertNotEquals(TEST_BOM_1, new Object(), "Object not equal");
+        assertNotEquals(TEST_BOM_1, new ByteOrderMark("1a", 2), "test1-1 not equal");
+        assertNotEquals(TEST_BOM_1, new ByteOrderMark("1b", 1, 2), "test1-2 not test2");
+        assertNotEquals(TEST_BOM_2, new ByteOrderMark("2", 1, 1), "test2 not equal");
+        assertNotEquals(TEST_BOM_3, new ByteOrderMark("3", 1, 2, 4), "test3 not equal");
     }
 
+    /** Tests {@link ByteOrderMark#getBytes()} */
     @Test
-    public void testReadOneCharacterIntoCharArrayWithOffset() throws IOException {
-        StringReader stringReader = new StringReader("org.apache.commons.io.input.BoundedReader");
-        BoundedReader boundedReader = new BoundedReader(stringReader, 1);
-        char[] charArray = new char[6];
-        // Read 1 character from the reader into the array, starting at index 1.
-        int charsRead = boundedReader.read(charArray, 1, 1);
-        // Verify that 1 character was read.
-        assertEquals(1, charsRead);
-        // Verify that the character 'o' was placed at index 1 of the array.
-        assertArrayEquals(new char[] {'\u0000', 'o', '\u0000', '\u0000', '\u0000', '\u0000'}, charArray);
+    public void testGetBytes() {
+        assertArrayEquals(TEST_BOM_1.getBytes(), new byte[] { (byte) 1 }, "test1 bytes");
+        TEST_BOM_1.getBytes()[0] = 2;
+        assertArrayEquals(TEST_BOM_1.getBytes(), new byte[] { (byte) 1 }, "test1 bytes");
+        assertArrayEquals(TEST_BOM_2.getBytes(), new byte[] { (byte) 1, (byte) 2 }, "test1 bytes");
+        assertArrayEquals(TEST_BOM_3.getBytes(), new byte[] { (byte) 1, (byte) 2, (byte) 3 }, "test1 bytes");
     }
 
-    @Test(expected = NullPointerException.class)
-    public void testResetWhenReaderIsNull() throws IOException {
-        BoundedReader boundedReader = new BoundedReader(null, 0);
-        // Resetting a BoundedReader with a null reader should throw a NullPointerException.
-        boundedReader.reset();
+    /** Tests {@link ByteOrderMark#getCharsetName()} */
+    @Test
+    public void testGetCharsetName() {
+        assertEquals("test1", TEST_BOM_1.getCharsetName(), "test1 name");
+        assertEquals("test2", TEST_BOM_2.getCharsetName(), "test2 name");
+        assertEquals("test3", TEST_BOM_3.getCharsetName(), "test3 name");
     }
 
-    @Test(expected = IOException.class)
-    public void testResetAfterClose() throws IOException {
-        StringReader stringReader = new StringReader("TO[Gj");
-        BoundedReader boundedReader = new BoundedReader(stringReader, 0);
-        boundedReader.close();
-        // Resetting a BoundedReader after it has been closed should throw an IOException.
-        boundedReader.reset();
+    /** Tests {@link ByteOrderMark#get(int)} */
+    @Test
+    public void testGetInt() {
+        assertEquals(1, TEST_BOM_1.get(0), "test1 get(0)");
+        assertEquals(1, TEST_BOM_2.get(0), "test2 get(0)");
+        assertEquals(2, TEST_BOM_2.get(1), "test2 get(1)");
+        assertEquals(1, TEST_BOM_3.get(0), "test3 get(0)");
+        assertEquals(2, TEST_BOM_3.get(1), "test3 get(1)");
+        assertEquals(3, TEST_BOM_3.get(2), "test3 get(2)");
     }
 
-    @Test(expected = NullPointerException.class)
-    public void testReadNullCharArray() throws IOException {
-        StringReader stringReader = new StringReader("g`8B;^5");
-        BoundedReader boundedReader = new BoundedReader(stringReader, 820);
-        // Reading into a null char array should throw a NullPointerException.
-        boundedReader.read(null, 103, 78);
+    /** Tests {@link ByteOrderMark#hashCode()} */
+    @Test
+    public void testHashCode() {
+        final int bomClassHash = ByteOrderMark.class.hashCode();
+        assertEquals(bomClassHash + 1, TEST_BOM_1.hashCode(), "hash test1 ");
+        assertEquals(bomClassHash + 3, TEST_BOM_2.hashCode(), "hash test2 ");
+        assertEquals(bomClassHash + 6, TEST_BOM_3.hashCode(), "hash test3 ");
     }
 
-    @Test(expected = IOException.class)
-    public void testReadIntoCharArrayAfterClose() throws IOException {
-        StringReader stringReader = new StringReader("j");
-        BoundedReader boundedReader = new BoundedReader(stringReader, 1);
-        boundedReader.close();
-        char[] charArray = new char[6];
-        // Reading from a closed BoundedReader should throw an IOException.
-        boundedReader.read(charArray, (-2537), 699);
-    }
-
-    @Test(expected = NullPointerException.class)
-    public void testReadWhenReaderIsNull() throws IOException {
-        BoundedReader boundedReader = new BoundedReader(null, 2482);
-        // Reading from a BoundedReader with a null reader should throw a NullPointerException.
-        boundedReader.read();
-    }
-
-    @Test(expected = IOException.class)
-    public void testReadAfterClose() throws IOException {
-        StringReader stringReader = new StringReader("");
-        BoundedReader boundedReader = new BoundedReader(stringReader, 2140);
-        boundedReader.close();
-        // Reading from a closed BoundedReader should throw an IOException.
-        boundedReader.read();
-    }
-
-    @Test(expected = NullPointerException.class)
-    public void testMarkWhenReaderIsNull() throws IOException {
-        BoundedReader boundedReader = new BoundedReader(null, 0);
-        // Marking a BoundedReader with a null reader should throw a NullPointerException.
-        boundedReader.mark(0);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testMarkWithNegativeReadAheadLimit() throws IOException {
-        StringReader stringReader = new StringReader("SQ\"Rz~>o\"ggtg97eV");
-        BoundedReader boundedReader = new BoundedReader(stringReader, (-585));
-        // Marking a BoundedReader with a negative read-ahead limit should throw an IllegalArgumentException.
-        boundedReader.mark((-585));
-    }
-
-    @Test(expected = NullPointerException.class)
-    public void testCloseWhenReaderIsNull() throws IOException {
-        BoundedReader boundedReader = new BoundedReader(null, 0);
-        // Closing a BoundedReader with a null reader should throw a NullPointerException.
-        boundedReader.close();
+    /** Tests {@link ByteOrderMark#length()} */
+    @Test
+    public void testLength() {
+        assertEquals(1, TEST_BOM_1.length(), "test1 length");
+        assertEquals(2, TEST_BOM_2.length(), "test2 length");
+        assertEquals(3, TEST_BOM_3.length(), "test3 length");
     }
 
     @Test
-    public void testMarkAndRead() throws IOException {
-        StringReader stringReader = new StringReader("Mqy[$oy5nF");
-        BoundedReader boundedReader = new BoundedReader(stringReader, 1601);
-        boundedReader.mark(1601);
-        int charRead = boundedReader.read();
-        assertEquals(77, charRead);
+    public void testMatches() {
+        assertTrue(ByteOrderMark.UTF_16BE.matches(ByteOrderMark.UTF_16BE.getRawBytes()));
+        assertTrue(ByteOrderMark.UTF_16LE.matches(ByteOrderMark.UTF_16LE.getRawBytes()));
+        assertTrue(ByteOrderMark.UTF_32BE.matches(ByteOrderMark.UTF_32BE.getRawBytes()));
+        assertTrue(ByteOrderMark.UTF_16BE.matches(ByteOrderMark.UTF_16BE.getRawBytes()));
+        assertTrue(ByteOrderMark.UTF_8.matches(ByteOrderMark.UTF_8.getRawBytes()));
+
+        assertTrue(TEST_BOM_1.matches(TEST_BOM_1.getRawBytes()));
+        assertTrue(TEST_BOM_2.matches(TEST_BOM_2.getRawBytes()));
+        assertTrue(TEST_BOM_3.matches(TEST_BOM_3.getRawBytes()));
+
+        assertFalse(TEST_BOM_1.matches(new ByteOrderMark("1a", 2).getRawBytes()));
+        assertTrue(TEST_BOM_1.matches(new ByteOrderMark("1b", 1, 2).getRawBytes()));
+        assertFalse(TEST_BOM_2.matches(new ByteOrderMark("2", 1, 1).getRawBytes()));
+        assertFalse(TEST_BOM_3.matches(new ByteOrderMark("3", 1, 2, 4).getRawBytes()));
     }
 
+    /** Tests {@link ByteOrderMark#toString()} */
     @Test
-    public void testBoundedReaderWithNegativeLimitAndThenBoundedReaderWithLimitOne() throws IOException {
-        StringReader stringReader = new StringReader("CU6^Ejr;7S;Ndl FK8");
-        BoundedReader boundedReader = new BoundedReader(stringReader, (-1821));
-        BoundedReader boundedReader1 = new BoundedReader(boundedReader, 1);
-        boundedReader1.mark(0);
-        int charRead = boundedReader1.read();
-        assertEquals(-1, charRead);
-    }
-
-    @Test
-    public void testBoundedReaderWithNegativeLimitAndThenBoundedReaderWithLimitOneWithoutMark() throws IOException {
-        StringReader stringReader = new StringReader("CU6^Ejr;7S;Ndl FK8");
-        BoundedReader boundedReader = new BoundedReader(stringReader, (-1821));
-        BoundedReader boundedReader1 = new BoundedReader(boundedReader, 1);
-        int charRead = boundedReader1.read();
-        assertEquals(-1, charRead);
-    }
-
-    @Test
-    public void testMarkAndReadEndOfStream() throws IOException {
-        StringReader stringReader = new StringReader("QN?");
-        BoundedReader boundedReader = new BoundedReader(stringReader, 2680);
-        char[] charArray = new char[9];
-        boundedReader.read(charArray);
-        boundedReader.mark(1);
-        int charRead = boundedReader.read();
-        assertEquals(-1, charRead);
-    }
-
-    @Test
-    public void testReadWithNegativeOffsetAndNegativeLength() throws IOException {
-        StringReader stringReader = new StringReader("pI2");
-        BoundedReader boundedReader = new BoundedReader(stringReader, (-2049));
-        char[] charArray = new char[14];
-        int charsRead = boundedReader.read(charArray, (-2049), (-1));
-        assertEquals(-1, charsRead);
-    }
-
-    @Test(expected = ArrayIndexOutOfBoundsException.class)
-    public void testReadWithArrayIndexOutOfBoundsException() throws IOException {
-        StringReader stringReader = new StringReader("j");
-        BoundedReader boundedReader = new BoundedReader(stringReader, 1);
-        boundedReader.mark(699);
-        char[] charArray = new char[6];
-        boundedReader.read(charArray, (-2537), 699);
-    }
-
-    @Test
-    public void testReadWithNegativeLimit() throws IOException {
-        StringReader stringReader = new StringReader("pI2");
-        BoundedReader boundedReader = new BoundedReader(stringReader, (-2049));
-        int charRead = boundedReader.read();
-        assertEquals(-1, charRead);
-    }
-
-    @Test
-    public void testReset() throws IOException {
-        StringReader stringReader = new StringReader("");
-        BoundedReader boundedReader = new BoundedReader(stringReader, 1);
-        boundedReader.reset();
-    }
-
-    @Test(expected = IOException.class)
-    public void testMarkAfterClose() throws IOException {
-        StringReader stringReader = new StringReader("");
-        BoundedReader boundedReader = new BoundedReader(stringReader, 1);
-        boundedReader.close();
-        boundedReader.mark(0);
+    public void testToString() {
+        assertEquals("ByteOrderMark[test1: 0x1]", TEST_BOM_1.toString(), "test1 ");
+        assertEquals("ByteOrderMark[test2: 0x1,0x2]", TEST_BOM_2.toString(), "test2 ");
+        assertEquals("ByteOrderMark[test3: 0x1,0x2,0x3]", TEST_BOM_3.toString(), "test3 ");
     }
 }
