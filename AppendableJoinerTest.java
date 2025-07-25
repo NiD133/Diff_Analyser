57ff3797eebbfa47eb0b1d648401976f42c4b960
NiD133 <k19052355@kcl.ac.uk>
2025-07-26 06:00:46 +0800

    llama manualTestSuite AppendableJoinerTest.java

--- AppendableJoinerTest.java
+++ AppendableJoinerTest.java
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.commons.lang3;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -37,6 +36,9 @@ import org.junit.jupiter.params.provider.ValueSource;
  */
 class AppendableJoinerTest {
 
+    /**
+     * Fixture class for testing.
+     */
     static class Fixture {
 
         private final String name;
@@ -54,89 +56,140 @@ class AppendableJoinerTest {
         }
     }
 
+    //region Test appendable joiner
+    /**
+     * Tests all properties of the appendable joiner.
+     */
     @Test
     void testAllBuilderPropertiesStringBuilder() {
-        // @formatter:off
-        final AppendableJoiner<Object> joiner = AppendableJoiner.builder()
-                .setPrefix("<")
-                .setDelimiter(".")
-                .setSuffix(">")
-                .setElementAppender((a, e) -> a.append(String.valueOf(e)))
-                .get();
-        // @formatter:on
+        // Arrange
+        final AppendableJoiner<Object> joiner = createAppendableJoinerWithPrefixDelimiterAndSuffix();
         final StringBuilder sbuilder = new StringBuilder("A");
+
+        // Act and Assert
         assertEquals("A<B.C>", joiner.join(sbuilder, "B", "C").toString());
         sbuilder.append("1");
         assertEquals("A<B.C>1<D.E>", joiner.join(sbuilder, Arrays.asList("D", "E")).toString());
     }
 
+    /**
+     * Tests default builder with a StringBuilder.
+     */
     @Test
     void testBuildDefaultStringBuilder() {
+        // Arrange
         final Builder<Object> builder = AppendableJoiner.builder();
-        assertNotSame(builder.get(), builder.get());
         final AppendableJoiner<Object> joiner = builder.get();
         final StringBuilder sbuilder = new StringBuilder("A");
+
+        // Act and Assert
         assertEquals("ABC", joiner.join(sbuilder, "B", "C").toString());
         sbuilder.append("1");
         assertEquals("ABC1DE", joiner.join(sbuilder, "D", "E").toString());
     }
 
+    /**
+     * Tests that different builders are created for AppendableJoiner.
+     */
     @Test
     void testBuilder() {
         assertNotSame(AppendableJoiner.builder(), AppendableJoiner.builder());
     }
+    //endregion
 
-    @SuppressWarnings("deprecation") // Test own StrBuilder
+    //region Test delimiter appendable
+    /**
+     * Tests the delimiter of an Appendable joiner with different appendables.
+     *
+     * @param clazz the class of the appendable
+     */
     @ParameterizedTest
     @ValueSource(classes = { StringBuilder.class, StringBuffer.class, StringWriter.class, StrBuilder.class, TextStringBuilder.class })
     void testDelimiterAppendable(final Class<? extends Appendable> clazz) throws Exception {
+        // Arrange
         final AppendableJoiner<Object> joiner = AppendableJoiner.builder().setDelimiter(".").get();
         final Appendable sbuilder = clazz.newInstance();
         sbuilder.append("A");
-        // throws IOException
+
+        // Act and Assert
         assertEquals("AB.C", joiner.joinA(sbuilder, "B", "C").toString());
         sbuilder.append("1");
-        // throws IOException
         assertEquals("AB.C1D.E", joiner.joinA(sbuilder, Arrays.asList("D", "E")).toString());
     }
 
+    /**
+     * Tests the delimiter of an Appendable joiner with a StringBuilder.
+     */
     @Test
     void testDelimiterStringBuilder() {
+        // Arrange
         final AppendableJoiner<Object> joiner = AppendableJoiner.builder().setDelimiter(".").get();
         final StringBuilder sbuilder = new StringBuilder("A");
-        // does not throw IOException
+
+        // Act and Assert
         assertEquals("AB.C", joiner.join(sbuilder, "B", "C").toString());
         sbuilder.append("1");
-        // does not throw IOException
         assertEquals("AB.C1D.E", joiner.join(sbuilder, Arrays.asList("D", "E")).toString());
     }
+    //endregion
 
+    //region Test to char sequence
+    /**
+     * Tests AppendableJoiner with prefix, delimiter and suffix set.
+     */
     @Test
-    void testToCharSequenceStringBuilder1() {
-        // @formatter:off
-        final AppendableJoiner<Object> joiner = AppendableJoiner.builder()
-                .setPrefix("<")
-                .setDelimiter(".")
-                .setSuffix(">")
-                .setElementAppender((a, e) -> a.append("|").append(Objects.toString(e)))
-                .get();
-        // @formatter:on
+    void testToCharSequenceStringBuilder() {
+        // Arrange
+        final AppendableJoiner<Object> joiner = createAppendableJoinerWithPrefixDelimiterAndSuffixWithAppender();
         final StringBuilder sbuilder = new StringBuilder("A");
+
+        // Act and Assert
         assertEquals("A<|B.|C>", joiner.join(sbuilder, "B", "C").toString());
         sbuilder.append("1");
         assertEquals("A<|B.|C>1<|D.|E>", joiner.join(sbuilder, Arrays.asList("D", "E")).toString());
     }
 
+    /**
+     * Tests AppendableJoiner with a Fixture class.
+     */
     @Test
     void testToCharSequenceStringBuilder2() {
-        // @formatter:off
-        final AppendableJoiner<Fixture> joiner = AppendableJoiner.<Fixture>builder()
-                .setElementAppender((a, e) -> e.render(a))
-                .get();
-        // @formatter:on
+        // Arrange
+        final AppendableJoiner<Fixture> joiner = AppendableJoiner.<Fixture>builder().setElementAppender((a, e) -> e.render(a)).get();
         final StringBuilder sbuilder = new StringBuilder("[");
-        assertEquals("[B!C!", joiner.join(sbuilder, new Fixture("B"), new Fixture("C")).toString());
+
+        // Act and Assert
+        assertEquals("[B!C!]", joiner.join(sbuilder, new Fixture("B"), new Fixture("C")).toString());
         sbuilder.append("]");
         assertEquals("[B!C!]D!E!", joiner.join(sbuilder, Arrays.asList(new Fixture("D"), new Fixture("E"))).toString());
+    }
+    //endregion
+
+    /**
+     * Creates an appendable joiner with prefix, delimiter and suffix set.
+     * 
+     * @return An appendable joiner with prefix, delimiter and suffix set.
+     */
+    private AppendableJoiner<Object> createAppendableJoinerWithPrefixDelimiterAndSuffix() {
+        return AppendableJoiner.builder()
+                .setPrefix("<")
+                .setDelimiter(".")
+                .setSuffix(">")
+                .setElementAppender((a, e) -> a.append(String.valueOf(e)))
+                .get();
+    }
+
+    /**
+     * Creates an appendable joiner with prefix, delimiter and suffix set and a custom element appender.
+     * 
+     * @return An appendable joiner with prefix, delimiter and suffix set and a custom element appender.
+     */
+    private AppendableJoiner<Object> createAppendableJoinerWithPrefixDelimiterAndSuffixWithAppender() {
+        return AppendableJoiner.builder()
+                .setPrefix("<")
+                .setDelimiter(".")
+                .setSuffix(">")
+                .setElementAppender((a, e) -> a.append("|").append(Objects.toString(e)))
+                .get();
     }
 }
