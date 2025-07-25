e8016e2d55618fb593df69bb7f47b35544786234
NiD133 <k19052355@kcl.ac.uk>
2025-07-26 06:01:00 +0800

    Original manualTestSuite CategoryItemEntityTest.java

--- CategoryItemEntityTest.java
+++ CategoryItemEntityTest.java
@@ -1,119 +1,124 @@
+/* ======================================================
+ * JFreeChart : a chart library for the Java(tm) platform
+ * ======================================================
+ *
+ * (C) Copyright 2000-present, by David Gilbert and Contributors.
+ *
+ * Project Info:  https://www.jfree.org/jfreechart/index.html
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ *
+ * [Oracle and Java are registered trademarks of Oracle and/or its affiliates. 
+ * Other names may be trademarks of their respective owners.]
+ *
+ * ---------------------------
+ * CategoryItemEntityTest.java
+ * ---------------------------
+ * (C) Copyright 2004-present, by David Gilbert and Contributors.
+ *
+ * Original Author:  David Gilbert;
+ * Contributor(s):   -;
+ *
+ */
+
 package org.jfree.chart.entity;
 
 import java.awt.geom.Rectangle2D;
+
 import org.jfree.chart.TestUtils;
 import org.jfree.chart.internal.CloneUtils;
+
 import org.jfree.data.category.DefaultCategoryDataset;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * Unit tests for the {@link CategoryItemEntity} class.
+ * Tests for the {@link CategoryItemEntity} class.
  */
 public class CategoryItemEntityTest {
 
     /**
-     * Tests the {@code equals} method to ensure it correctly distinguishes between different field values.
+     * Confirm that the equals method can distinguish all the required fields.
      */
     @Test
     public void testEquals() {
-        // Create a sample dataset
-        DefaultCategoryDataset<String, String> dataset = createSampleDataset();
+        DefaultCategoryDataset<String, String> d = new DefaultCategoryDataset<>();
+        d.addValue(1.0, "R1", "C1");
+        d.addValue(2.0, "R1", "C2");
+        d.addValue(3.0, "R2", "C1");
+        d.addValue(4.0, "R2", "C2");
+        CategoryItemEntity<String, String> e1 = new CategoryItemEntity<>(
+                new Rectangle2D.Double(1.0, 2.0, 3.0, 4.0), "ToolTip", "URL", d, 
+                "R2", "C2");
+        CategoryItemEntity<String, String> e2 = new CategoryItemEntity<>(
+                new Rectangle2D.Double(1.0, 2.0, 3.0, 4.0), "ToolTip", "URL", d, 
+                "R2", "C2");
+        assertEquals(e1, e2);
 
-        // Create two identical CategoryItemEntity instances
-        CategoryItemEntity<String, String> entity1 = createCategoryItemEntity(dataset, "R2", "C2");
-        CategoryItemEntity<String, String> entity2 = createCategoryItemEntity(dataset, "R2", "C2");
+        e1.setArea(new Rectangle2D.Double(4.0, 3.0, 2.0, 1.0));
+        assertNotEquals(e1, e2);
+        e2.setArea(new Rectangle2D.Double(4.0, 3.0, 2.0, 1.0));
+        assertEquals(e1, e2);
 
-        // Verify that the two entities are equal
-        assertEquals(entity1, entity2);
+        e1.setToolTipText("New ToolTip");
+        assertNotEquals(e1, e2);
+        e2.setToolTipText("New ToolTip");
+        assertEquals(e1, e2);
 
-        // Modify the area of entity1 and verify inequality
-        entity1.setArea(new Rectangle2D.Double(4.0, 3.0, 2.0, 1.0));
-        assertNotEquals(entity1, entity2);
-
-        // Update entity2 to match entity1 and verify equality
-        entity2.setArea(new Rectangle2D.Double(4.0, 3.0, 2.0, 1.0));
-        assertEquals(entity1, entity2);
-
-        // Modify the tooltip text of entity1 and verify inequality
-        entity1.setToolTipText("New ToolTip");
-        assertNotEquals(entity1, entity2);
-
-        // Update entity2 to match entity1 and verify equality
-        entity2.setToolTipText("New ToolTip");
-        assertEquals(entity1, entity2);
-
-        // Modify the URL text of entity1 and verify inequality
-        entity1.setURLText("New URL");
-        assertNotEquals(entity1, entity2);
-
-        // Update entity2 to match entity1 and verify equality
-        entity2.setURLText("New URL");
-        assertEquals(entity1, entity2);
+        e1.setURLText("New URL");
+        assertNotEquals(e1, e2);
+        e2.setURLText("New URL");
+        assertEquals(e1, e2);
     }
 
     /**
-     * Tests the cloning functionality of the {@link CategoryItemEntity} class.
+     * Confirm that cloning works.
      */
     @Test
     public void testCloning() throws CloneNotSupportedException {
-        // Create a sample dataset
-        DefaultCategoryDataset<String, String> dataset = createSampleDataset();
-
-        // Create a CategoryItemEntity instance
-        CategoryItemEntity<String, String> originalEntity = createCategoryItemEntity(dataset, "R2", "C2");
-
-        // Clone the original entity
-        CategoryItemEntity<String, String> clonedEntity = CloneUtils.clone(originalEntity);
-
-        // Verify that the cloned entity is a different instance but equal to the original
-        assertNotSame(originalEntity, clonedEntity);
-        assertSame(originalEntity.getClass(), clonedEntity.getClass());
-        assertEquals(originalEntity, clonedEntity);
+        DefaultCategoryDataset<String, String> d = new DefaultCategoryDataset<>();
+        d.addValue(1.0, "R1", "C1");
+        d.addValue(2.0, "R1", "C2");
+        d.addValue(3.0, "R2", "C1");
+        d.addValue(4.0, "R2", "C2");
+        CategoryItemEntity<String, String> e1 = new CategoryItemEntity<>(
+                new Rectangle2D.Double(1.0, 2.0, 3.0, 4.0), "ToolTip", "URL", d, 
+                "R2", "C2");
+        CategoryItemEntity<String, String> e2 = CloneUtils.clone(e1);
+        assertNotSame(e1, e2);
+        assertSame(e1.getClass(), e2.getClass());
+        assertEquals(e1, e2);
     }
 
     /**
-     * Tests the serialization and deserialization of the {@link CategoryItemEntity} class.
+     * Serialize an instance, restore it, and check for equality.
      */
     @Test
     public void testSerialization() {
-        // Create a sample dataset
-        DefaultCategoryDataset<String, String> dataset = createSampleDataset();
-
-        // Create a CategoryItemEntity instance
-        CategoryItemEntity<String, String> originalEntity = createCategoryItemEntity(dataset, "R2", "C2");
-
-        // Serialize and deserialize the entity
-        CategoryItemEntity<String, String> deserializedEntity = TestUtils.serialised(originalEntity);
-
-        // Verify that the deserialized entity is equal to the original
-        assertEquals(originalEntity, deserializedEntity);
+        DefaultCategoryDataset<String, String> d = new DefaultCategoryDataset<>();
+        d.addValue(1.0, "R1", "C1");
+        d.addValue(2.0, "R1", "C2");
+        d.addValue(3.0, "R2", "C1");
+        d.addValue(4.0, "R2", "C2");
+        CategoryItemEntity<String, String> e1 = new CategoryItemEntity<>(
+                new Rectangle2D.Double(1.0, 2.0, 3.0, 4.0), "ToolTip", "URL", d, 
+                "R2", "C2");
+        CategoryItemEntity<String, String> e2 = TestUtils.serialised(e1);
+        assertEquals(e1, e2);
     }
 
-    /**
-     * Helper method to create a sample dataset.
-     *
-     * @return a sample {@link DefaultCategoryDataset}.
-     */
-    private DefaultCategoryDataset<String, String> createSampleDataset() {
-        DefaultCategoryDataset<String, String> dataset = new DefaultCategoryDataset<>();
-        dataset.addValue(1.0, "R1", "C1");
-        dataset.addValue(2.0, "R1", "C2");
-        dataset.addValue(3.0, "R2", "C1");
-        dataset.addValue(4.0, "R2", "C2");
-        return dataset;
-    }
-
-    /**
-     * Helper method to create a {@link CategoryItemEntity} instance.
-     *
-     * @param dataset the dataset.
-     * @param rowKey the row key.
-     * @param columnKey the column key.
-     * @return a new {@link CategoryItemEntity} instance.
-     */
-    private CategoryItemEntity<String, String> createCategoryItemEntity(DefaultCategoryDataset<String, String> dataset, String rowKey, String columnKey) {
-        return new CategoryItemEntity<>(
-                new Rectangle2D.Double(1.0, 2.0, 3.0, 4.0), "ToolTip", "URL", dataset, rowKey, columnKey);
-    }
 }
