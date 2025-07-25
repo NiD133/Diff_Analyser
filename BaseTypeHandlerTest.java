e3e19e4d75bc5987ba6acf6b90719089ab46fdbc
NiD133 <k19052355@kcl.ac.uk>
2025-07-26 06:00:52 +0800

    Original manualTestSuite BaseTypeHandlerTest.java

--- BaseTypeHandlerTest.java
+++ BaseTypeHandlerTest.java
@@ -1,3 +1,18 @@
+/*
+ *    Copyright 2009-2022 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 package org.apache.ibatis.type;
 
 import java.sql.CallableStatement;
@@ -9,93 +24,29 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-/**
- * Abstract base class for testing {@link TypeHandler} implementations.
- * This class provides mocked dependencies (ResultSet, PreparedStatement, etc.)
- * and declares abstract test methods that each concrete TypeHandler test should implement.
- * The tests focus on verifying the behavior of the TypeHandler's methods for
- * setting parameters and retrieving results from various JDBC objects.
- */
 @ExtendWith(MockitoExtension.class)
 abstract class BaseTypeHandlerTest {
 
-  /**
-   * Mocked ResultSet object for simulating database query results.
-   */
   @Mock
   protected ResultSet rs;
-
-  /**
-   * Mocked PreparedStatement object for simulating prepared SQL statements.
-   */
   @Mock
   protected PreparedStatement ps;
-
-  /**
-   * Mocked CallableStatement object for simulating stored procedure calls.
-   */
   @Mock
   protected CallableStatement cs;
-
-  /**
-   * Mocked ResultSetMetaData object for simulating metadata about result sets.
-   */
   @Mock
   protected ResultSetMetaData rsmd;
 
-  /**
-   * Test method to verify the TypeHandler's {@code setParameter} method.
-   * It should assert that the parameter is correctly set on the PreparedStatement.
-   *
-   * @throws Exception if any error occurs during the test.
-   */
   public abstract void shouldSetParameter() throws Exception;
 
-  /**
-   * Test method to verify the TypeHandler's {@code getResult} method when retrieving
-   * a non-null value from a ResultSet by column name.
-   *
-   * @throws Exception if any error occurs during the test.
-   */
   public abstract void shouldGetResultFromResultSetByName() throws Exception;
 
-  /**
-   * Test method to verify the TypeHandler's {@code getResult} method when retrieving
-   * a null value from a ResultSet by column name.
-   *
-   * @throws Exception if any error occurs during the test.
-   */
   public abstract void shouldGetResultNullFromResultSetByName() throws Exception;
 
-  /**
-   * Test method to verify the TypeHandler's {@code getResult} method when retrieving
-   * a non-null value from a ResultSet by column index.
-   *
-   * @throws Exception if any error occurs during the test.
-   */
   public abstract void shouldGetResultFromResultSetByPosition() throws Exception;
 
-  /**
-   * Test method to verify the TypeHandler's {@code getResult} method when retrieving
-   * a null value from a ResultSet by column index.
-   *
-   * @throws Exception if any error occurs during the test.
-   */
   public abstract void shouldGetResultNullFromResultSetByPosition() throws Exception;
 
-  /**
-   * Test method to verify the TypeHandler's {@code getResult} method when retrieving
-   * a non-null value from a CallableStatement.
-   *
-   * @throws Exception if any error occurs during the test.
-   */
   public abstract void shouldGetResultFromCallableStatement() throws Exception;
 
-  /**
-   * Test method to verify the TypeHandler's {@code getResult} method when retrieving
-   * a null value from a CallableStatement.
-   *
-   * @throws Exception if any error occurs during the test.
-   */
   public abstract void shouldGetResultNullFromCallableStatement() throws Exception;
 }
