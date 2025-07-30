7e3d2621f4d0851a8b26e7c3f251be4d707a848a
NiD133 <k19052355@kcl.ac.uk>
2025-07-30 17:02:20 +0800

    Original manualTestSuite CompactLinkedHashSetTest.java

--- CompactLinkedHashSetTest.java
+++ CompactLinkedHashSetTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2012 The Guava Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.common.collect;
 
 import static com.google.common.truth.Truth.assertThat;
@@ -20,58 +36,58 @@ import junit.framework.TestSuite;
 import org.jspecify.annotations.NullUnmarked;
 
 /**
- * Unit tests for the CompactLinkedHashSet class.
- * 
- * This test suite verifies the behavior of CompactLinkedHashSet, including its
- * memory allocation strategies and its ability to handle hash flooding.
- * 
+ * Tests for CompactLinkedHashSet.
+ *
  * @author Dimitris Andreou
  */
 @GwtIncompatible // java.util.Arrays#copyOf(Object[], int), java.lang.reflect.Array
 @NullUnmarked
 public class CompactLinkedHashSetTest extends TestCase {
-
   @AndroidIncompatible // test-suite builders
   public static Test suite() {
-    // Define the features supported by CompactLinkedHashSet
-    List<Feature<?>> supportedFeatures = Arrays.asList(
-        CollectionSize.ANY,
-        CollectionFeature.ALLOWS_NULL_VALUES,
-        CollectionFeature.FAILS_FAST_ON_CONCURRENT_MODIFICATION,
-        CollectionFeature.GENERAL_PURPOSE,
-        CollectionFeature.REMOVE_OPERATIONS,
-        CollectionFeature.SERIALIZABLE,
-        CollectionFeature.KNOWN_ORDER,
-        CollectionFeature.SUPPORTS_ADD,
-        CollectionFeature.SUPPORTS_REMOVE
-    );
+    List<Feature<?>> allFeatures =
+        Arrays.<Feature<?>>asList(
+            CollectionSize.ANY,
+            CollectionFeature.ALLOWS_NULL_VALUES,
+            CollectionFeature.FAILS_FAST_ON_CONCURRENT_MODIFICATION,
+            CollectionFeature.GENERAL_PURPOSE,
+            CollectionFeature.REMOVE_OPERATIONS,
+            CollectionFeature.SERIALIZABLE,
+            CollectionFeature.KNOWN_ORDER,
+            CollectionFeature.SUPPORTS_ADD,
+            CollectionFeature.SUPPORTS_REMOVE);
 
     TestSuite suite = new TestSuite();
     suite.addTestSuite(CompactLinkedHashSetTest.class);
-
-    // Add test suite for standard CompactLinkedHashSet
     suite.addTest(
-        SetTestSuiteBuilder.using(new StandardSetGenerator())
+        SetTestSuiteBuilder.using(
+                new TestStringSetGenerator() {
+                  @Override
+                  protected Set<String> create(String[] elements) {
+                    return CompactLinkedHashSet.create(asList(elements));
+                  }
+                })
             .named("CompactLinkedHashSet")
-            .withFeatures(supportedFeatures)
-            .createTestSuite()
-    );
-
-    // Add test suite for CompactLinkedHashSet with hash flooding protection
+            .withFeatures(allFeatures)
+            .createTestSuite());
     suite.addTest(
-        SetTestSuiteBuilder.using(new FloodingResistantSetGenerator())
+        SetTestSuiteBuilder.using(
+                new TestStringSetGenerator() {
+                  @Override
+                  protected Set<String> create(String[] elements) {
+                    CompactLinkedHashSet<String> set = CompactLinkedHashSet.create();
+                    set.convertToHashFloodingResistantImplementation();
+                    Collections.addAll(set, elements);
+                    return set;
+                  }
+                })
             .named("CompactLinkedHashSet with flooding protection")
-            .withFeatures(supportedFeatures)
-            .createTestSuite()
-    );
-
+            .withFeatures(allFeatures)
+            .createTestSuite());
     return suite;
   }
 
-  /**
-   * Tests the default array allocation behavior of CompactHashSet.
-   */
-  public void testDefaultArrayAllocation() {
+  public void testAllocArraysDefault() {
     CompactHashSet<Integer> set = CompactHashSet.create();
     assertThat(set.needsAllocArrays()).isTrue();
     assertThat(set.elements).isNull();
@@ -81,10 +97,7 @@ public class CompactLinkedHashSetTest extends TestCase {
     assertThat(set.elements).hasLength(CompactHashing.DEFAULT_SIZE);
   }
 
-  /**
-   * Tests array allocation with expected size in CompactHashSet.
-   */
-  public void testArrayAllocationWithExpectedSize() {
+  public void testAllocArraysExpectedSize() {
     for (int i = 0; i <= CompactHashing.DEFAULT_SIZE; i++) {
       CompactHashSet<Integer> set = CompactHashSet.createWithExpectedSize(i);
       assertThat(set.needsAllocArrays()).isTrue();
@@ -94,29 +107,6 @@ public class CompactLinkedHashSetTest extends TestCase {
       assertThat(set.needsAllocArrays()).isFalse();
       int expectedSize = max(1, i);
       assertThat(set.elements).hasLength(expectedSize);
-    }
-  }
-
-  /**
-   * Generator for standard CompactLinkedHashSet instances.
-   */
-  private static class StandardSetGenerator extends TestStringSetGenerator {
-    @Override
-    protected Set<String> create(String[] elements) {
-      return CompactLinkedHashSet.create(asList(elements));
-    }
-  }
-
-  /**
-   * Generator for CompactLinkedHashSet instances with hash flooding protection.
-   */
-  private static class FloodingResistantSetGenerator extends TestStringSetGenerator {
-    @Override
-    protected Set<String> create(String[] elements) {
-      CompactLinkedHashSet<String> set = CompactLinkedHashSet.create();
-      set.convertToHashFloodingResistantImplementation();
-      Collections.addAll(set, elements);
-      return set;
     }
   }
 }
