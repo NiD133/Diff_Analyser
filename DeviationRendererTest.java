e306e19744629f44e0576319f014cbb0daea7c42
NiD133 <k19052355@kcl.ac.uk>
2025-08-06 05:22:20 +0800

    Original manualTestSuite DeviationRendererTest.java

--- DeviationRendererTest.java
+++ DeviationRendererTest.java
@@ -1,95 +1,112 @@
+/* ======================================================
+ * JFreeChart : a chart library for the Java(tm) platform
+ * ======================================================
+ *
+ * (C) Copyright 2000-present, by David Gilbert and Contributors.
+ *
+ * Project Info:  https://www.jfree.org/jfreechart/index.html
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ *
+ * [Oracle and Java are registered trademarks of Oracle and/or its affiliates. 
+ * Other names may be trademarks of their respective owners.]
+ *
+ * --------------------------
+ * DeviationRendererTest.java
+ * --------------------------
+ * (C) Copyright 2007-present, by David Gilbert and Contributors.
+ *
+ * Original Author:  David Gilbert;
+ * Contributor(s):   -;
+ *
+ */
+
 package org.jfree.chart.renderer.xy;
 
 import org.jfree.chart.TestUtils;
 import org.jfree.chart.api.PublicCloneable;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * Unit tests for the {@link DeviationRenderer} class.
+ * Tests for the {@link DeviationRenderer} class.
  */
 public class DeviationRendererTest {
 
     /**
-     * Tests the {@code equals()} method to ensure it correctly distinguishes all fields.
+     * Test that the equals() method distinguishes all fields.
      */
     @Test
     public void testEquals() {
-        // Create two default instances of DeviationRenderer
-        DeviationRenderer renderer1 = new DeviationRenderer();
-        DeviationRenderer renderer2 = new DeviationRenderer();
+        // default instances
+        DeviationRenderer r1 = new DeviationRenderer();
+        DeviationRenderer r2 = new DeviationRenderer();
+        assertEquals(r1, r2);
+        assertEquals(r2, r1);
 
-        // Verify that two default instances are equal
-        assertEquals(renderer1, renderer2);
-        assertEquals(renderer2, renderer1);
-
-        // Modify a field in one instance and verify inequality
-        renderer1.setAlpha(0.1f);
-        assertNotEquals(renderer1, renderer2);
-
-        // Set the same field in the second instance and verify equality
-        renderer2.setAlpha(0.1f);
-        assertEquals(renderer1, renderer2);
+        r1.setAlpha(0.1f);
+        assertNotEquals(r1, r2);
+        r2.setAlpha(0.1f);
+        assertEquals(r1, r2);
     }
 
     /**
-     * Tests that two equal objects have the same hash code.
+     * Two objects that are equal are required to return the same hashCode.
      */
     @Test
     public void testHashcode() {
-        // Create two default instances of DeviationRenderer
-        DeviationRenderer renderer1 = new DeviationRenderer();
-        DeviationRenderer renderer2 = new DeviationRenderer();
-
-        // Verify that two default instances are equal
-        assertEquals(renderer1, renderer2);
-
-        // Verify that their hash codes are equal
-        assertEquals(renderer1.hashCode(), renderer2.hashCode());
+        DeviationRenderer r1 = new DeviationRenderer();
+        DeviationRenderer r2 = new DeviationRenderer();
+        assertEquals(r1, r2);
+        int h1 = r1.hashCode();
+        int h2 = r2.hashCode();
+        assertEquals(h1, h2);
     }
 
     /**
-     * Tests that cloning a DeviationRenderer instance produces a distinct but equal object.
+     * Confirm that cloning works.
      */
     @Test
     public void testCloning() throws CloneNotSupportedException {
-        // Create an instance of DeviationRenderer
-        DeviationRenderer original = new DeviationRenderer();
-
-        // Clone the instance
-        DeviationRenderer clone = (DeviationRenderer) original.clone();
-
-        // Verify that the clone is a distinct object but equal to the original
-        assertNotSame(original, clone);
-        assertSame(original.getClass(), clone.getClass());
-        assertEquals(original, clone);
+        DeviationRenderer r1 = new DeviationRenderer();
+        DeviationRenderer r2 = (DeviationRenderer) r1.clone();
+        assertNotSame(r1, r2);
+        assertSame(r1.getClass(), r2.getClass());
+        assertEquals(r1, r2);
     }
 
     /**
-     * Tests that the DeviationRenderer class implements {@link PublicCloneable}.
+     * Verify that this class implements {@link PublicCloneable}.
      */
     @Test
     public void testPublicCloneable() {
-        // Create an instance of DeviationRenderer
-        DeviationRenderer renderer = new DeviationRenderer();
-
-        // Verify that it implements PublicCloneable
-        assertTrue(renderer instanceof PublicCloneable);
+        DeviationRenderer r1 = new DeviationRenderer();
+        assertTrue(r1 instanceof PublicCloneable);
     }
 
     /**
-     * Tests serialization and deserialization of a DeviationRenderer instance.
+     * Serialize an instance, restore it, and check for equality.
      */
     @Test
     public void testSerialization() {
-        // Create an instance of DeviationRenderer
-        DeviationRenderer original = new DeviationRenderer();
-
-        // Serialize and deserialize the instance
-        DeviationRenderer deserialized = TestUtils.serialised(original);
-
-        // Verify that the deserialized instance is equal to the original
-        assertEquals(original, deserialized);
+        DeviationRenderer r1 = new DeviationRenderer();
+        DeviationRenderer r2 = TestUtils.serialised(r1);
+        assertEquals(r1, r2);
     }
+
 }
