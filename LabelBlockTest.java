399700ae8dd33fea18b811e05b73d8ce045c9c97
NiD133 <k19052355@kcl.ac.uk>
2025-08-06 04:39:23 +0800

    Original manualTestSuite LabelBlockTest.java

--- LabelBlockTest.java
+++ LabelBlockTest.java
@@ -1,3 +1,39 @@
+/* ======================================================
+ * JFreeChart : a chart library for the Java(tm) platform
+ * ======================================================
+ *
+ * (C) Copyright 2000-present, by David Gilbert and Contributors.
+ *
+ * Project Info:  https://www.jfree.org/jfreechart/index.html
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ *
+ * [Oracle and Java are registered trademarks of Oracle and/or its affiliates. 
+ * Other names may be trademarks of their respective owners.]
+ *
+ * -------------------
+ * LabelBlockTest.java
+ * -------------------
+ * (C) Copyright 2005-present, by David Gilbert and Contributors.
+ *
+ * Original Author:  David Gilbert;
+ * Contributor(s):   -;
+ *
+ */
+
 package org.jfree.chart.block;
 
 import java.awt.Color;
@@ -14,113 +50,88 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * Unit tests for the {@link LabelBlock} class.
+ * Some tests for the {@link LabelBlock} class.
  */
 public class LabelBlockTest {
 
     /**
-     * Tests the {@code equals()} method of {@link LabelBlock} to ensure it correctly
-     * distinguishes between different instances based on their properties.
+     * Confirm that the equals() method can distinguish all the required fields.
      */
     @Test
     public void testEquals() {
-        // Test equality with identical properties
-        LabelBlock block1 = createLabelBlock("ABC", Font.PLAIN, 12, Color.RED);
-        LabelBlock block2 = createLabelBlock("ABC", Font.PLAIN, 12, Color.RED);
-        assertEquals(block1, block2, "Blocks with identical properties should be equal");
+        LabelBlock b1 = new LabelBlock("ABC", new Font("Dialog",
+                Font.PLAIN, 12), Color.RED);
+        LabelBlock b2 = new LabelBlock("ABC", new Font("Dialog",
+                Font.PLAIN, 12), Color.RED);
+        assertEquals(b1, b2);
+        assertEquals(b2, b2);
 
-        // Test inequality with different text
-        block1 = createLabelBlock("XYZ", Font.PLAIN, 12, Color.RED);
-        assertNotEquals(block1, block2, "Blocks with different text should not be equal");
+        b1 = new LabelBlock("XYZ", new Font("Dialog", Font.PLAIN, 12),
+                Color.RED);
+        assertNotEquals(b1, b2);
+        b2 = new LabelBlock("XYZ", new Font("Dialog", Font.PLAIN, 12),
+                Color.RED);
+        assertEquals(b1, b2);
 
-        // Update block2 to match block1 and test equality again
-        block2 = createLabelBlock("XYZ", Font.PLAIN, 12, Color.RED);
-        assertEquals(block1, block2, "Blocks with identical properties should be equal");
+        b1 = new LabelBlock("XYZ", new Font("Dialog", Font.BOLD, 12),
+                Color.RED);
+        assertNotEquals(b1, b2);
+        b2 = new LabelBlock("XYZ", new Font("Dialog", Font.BOLD, 12),
+                Color.RED);
+        assertEquals(b1, b2);
 
-        // Test inequality with different font style
-        block1 = createLabelBlock("XYZ", Font.BOLD, 12, Color.RED);
-        assertNotEquals(block1, block2, "Blocks with different font styles should not be equal");
+        b1 = new LabelBlock("XYZ", new Font("Dialog", Font.BOLD, 12),
+                Color.BLUE);
+        assertNotEquals(b1, b2);
+        b2 = new LabelBlock("XYZ", new Font("Dialog", Font.BOLD, 12),
+                Color.BLUE);
+        assertEquals(b1, b2);
 
-        // Update block2 to match block1 and test equality again
-        block2 = createLabelBlock("XYZ", Font.BOLD, 12, Color.RED);
-        assertEquals(block1, block2, "Blocks with identical properties should be equal");
+        b1.setToolTipText("Tooltip");
+        assertNotEquals(b1, b2);
+        b2.setToolTipText("Tooltip");
+        assertEquals(b1, b2);
 
-        // Test inequality with different colors
-        block1 = createLabelBlock("XYZ", Font.BOLD, 12, Color.BLUE);
-        assertNotEquals(block1, block2, "Blocks with different colors should not be equal");
+        b1.setURLText("URL");
+        assertNotEquals(b1, b2);
+        b2.setURLText("URL");
+        assertEquals(b1, b2);
 
-        // Update block2 to match block1 and test equality again
-        block2 = createLabelBlock("XYZ", Font.BOLD, 12, Color.BLUE);
-        assertEquals(block1, block2, "Blocks with identical properties should be equal");
+        b1.setContentAlignmentPoint(TextBlockAnchor.CENTER_RIGHT);
+        assertNotEquals(b1, b2);
+        b2.setContentAlignmentPoint(TextBlockAnchor.CENTER_RIGHT);
+        assertEquals(b1, b2);
 
-        // Test inequality with different tooltip text
-        block1.setToolTipText("Tooltip");
-        assertNotEquals(block1, block2, "Blocks with different tooltip text should not be equal");
-
-        // Update block2 to match block1 and test equality again
-        block2.setToolTipText("Tooltip");
-        assertEquals(block1, block2, "Blocks with identical properties should be equal");
-
-        // Test inequality with different URL text
-        block1.setURLText("URL");
-        assertNotEquals(block1, block2, "Blocks with different URL text should not be equal");
-
-        // Update block2 to match block1 and test equality again
-        block2.setURLText("URL");
-        assertEquals(block1, block2, "Blocks with identical properties should be equal");
-
-        // Test inequality with different content alignment points
-        block1.setContentAlignmentPoint(TextBlockAnchor.CENTER_RIGHT);
-        assertNotEquals(block1, block2, "Blocks with different content alignment points should not be equal");
-
-        // Update block2 to match block1 and test equality again
-        block2.setContentAlignmentPoint(TextBlockAnchor.CENTER_RIGHT);
-        assertEquals(block1, block2, "Blocks with identical properties should be equal");
-
-        // Test inequality with different text anchors
-        block1.setTextAnchor(RectangleAnchor.BOTTOM_RIGHT);
-        assertNotEquals(block1, block2, "Blocks with different text anchors should not be equal");
-
-        // Update block2 to match block1 and test equality again
-        block2.setTextAnchor(RectangleAnchor.BOTTOM_RIGHT);
-        assertEquals(block1, block2, "Blocks with identical properties should be equal");
+        b1.setTextAnchor(RectangleAnchor.BOTTOM_RIGHT);
+        assertNotEquals(b1, b2);
+        b2.setTextAnchor(RectangleAnchor.BOTTOM_RIGHT);
+        assertEquals(b1, b2);
     }
 
     /**
-     * Tests the cloning functionality of {@link LabelBlock}.
+     * Confirm that cloning works.
      */
     @Test
     public void testCloning() throws CloneNotSupportedException {
-        LabelBlock original = createLabelBlock("ABC", Font.PLAIN, 12, Color.RED);
-        LabelBlock clone = CloneUtils.clone(original);
-
-        assertNotSame(original, clone, "Clone should be a different instance");
-        assertSame(original.getClass(), clone.getClass(), "Clone should be of the same class");
-        assertEquals(original, clone, "Clone should be equal to the original");
+        LabelBlock b1 = new LabelBlock("ABC", new Font("Dialog",
+                Font.PLAIN, 12), Color.RED);
+        LabelBlock b2 = CloneUtils.clone(b1);
+        assertNotSame(b1, b2);
+        assertSame(b1.getClass(), b2.getClass());
+        assertEquals(b1, b2);
     }
 
     /**
-     * Tests the serialization and deserialization of {@link LabelBlock}.
+     * Serialize an instance, restore it, and check for equality.
      */
     @Test
     public void testSerialization() {
-        GradientPaint gradientPaint = new GradientPaint(1.0f, 2.0f, Color.RED, 3.0f, 4.0f, Color.BLUE);
-        LabelBlock original = new LabelBlock("ABC", new Font("Dialog", Font.PLAIN, 12), gradientPaint);
-        LabelBlock deserialized = TestUtils.serialised(original);
-
-        assertEquals(original, deserialized, "Deserialized block should be equal to the original");
+        GradientPaint gp = new GradientPaint(1.0f, 2.0f, Color.RED, 3.0f, 4.0f,
+                Color.BLUE);
+        LabelBlock b1 = new LabelBlock("ABC", new Font("Dialog",
+                Font.PLAIN, 12), gp);
+        LabelBlock b2 = TestUtils.serialised(b1);
+        assertEquals(b1, b2);
     }
 
-    /**
-     * Helper method to create a {@link LabelBlock} with specified properties.
-     *
-     * @param text the text for the label
-     * @param fontStyle the style of the font
-     * @param fontSize the size of the font
-     * @param color the color of the label
-     * @return a new {@link LabelBlock} instance
-     */
-    private LabelBlock createLabelBlock(String text, int fontStyle, int fontSize, Color color) {
-        return new LabelBlock(text, new Font("Dialog", fontStyle, fontSize), color);
-    }
 }
