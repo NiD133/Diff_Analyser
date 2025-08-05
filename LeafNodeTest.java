a226cab461c86263f8c3ca0154742831886fb256
NiD133 <k19052355@kcl.ac.uk>
2025-08-06 05:23:04 +0800

    Original manualTestSuite LeafNodeTest.java

--- LeafNodeTest.java
+++ LeafNodeTest.java
@@ -3,112 +3,54 @@ package org.jsoup.nodes;
 import org.jsoup.Jsoup;
 import org.jsoup.select.Elements;
 import org.jsoup.select.NodeFilter;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class LeafNodeTest {
+public class LeafNodeTest {
 
-    private Document doc;
-    private Element html;
-    private Element p;
-
-    @BeforeEach
-    void setUp() {
+    @Test
+    public void doesNotGetAttributesTooEasily() {
+        // test to make sure we're not setting attributes on all nodes right away
         String body = "<p>One <!-- Two --> Three<![CDATA[Four]]></p>";
-        doc = Jsoup.parse(body, "https://example.com/");
-        html = doc.child(0);
-        p = doc.select("p").first();
-    }
+        Document doc = Jsoup.parse(body, "https://example.com/");
+        assertTrue(hasAnyAttributes(doc)); // should have one - the base uri on the doc
 
-    @Test
-    void documentWithBaseUri_hasAttributesInSubtree() {
-        assertTrue(anyNodeInSubtreeHasAttributes(doc));
-    }
+        assertFalse(hasAnyAttributes(Jsoup.parse("<div>None</div>"))); // no base uri
 
-    @Test
-    void documentWithoutBaseUri_hasNoAttributesInSubtree() {
-        Document doc = Jsoup.parse("<div>None</div>");
-        assertFalse(anyNodeInSubtreeHasAttributes(doc));
-    }
+        Element html = doc.child(0);
+        assertFalse(hasAnyAttributes(html));
 
-    @Test
-    void htmlElementInitially_hasNoAttributesInSubtree() {
-        assertFalse(anyNodeInSubtreeHasAttributes(html));
-    }
-
-    @Test
-    void callingOuterHtml_doesNotAddAttributesToHtmlSubtree() {
         String s = doc.outerHtml();
-        assertFalse(anyNodeInSubtreeHasAttributes(html));
-    }
+        assertFalse(hasAnyAttributes(html));
 
-    @Test
-    void selectingParagraph_doesNotAddAttributesToHtmlSubtree() {
         Elements els = doc.select("p");
+        Element p = els.first();
         assertEquals(1, els.size());
-        assertFalse(anyNodeInSubtreeHasAttributes(html));
-    }
+        assertFalse(hasAnyAttributes(html));
 
-    @Test
-    void selectingNonExistentClass_doesNotAddAttributes() {
-        Elements els = doc.select("p.none");
-        assertTrue(els.isEmpty());
-        assertFalse(anyNodeInSubtreeHasAttributes(html));
-    }
+        els = doc.select("p.none");
+        assertFalse(hasAnyAttributes(html));
 
-    @Test
-    void checkingIdAndClass_doesNotAddAttributes() {
         String id = p.id();
         assertEquals("", id);
         assertFalse(p.hasClass("Foobs"));
-        assertFalse(anyNodeInSubtreeHasAttributes(html));
-    }
+        assertFalse(hasAnyAttributes(html));
 
-    @Test
-    void addingClass_addsAttributesToParagraph() {
         p.addClass("Foobs");
         assertTrue(p.hasClass("Foobs"));
-        assertTrue(anyNodeInSubtreeHasAttributes(p));
-    }
+        assertTrue(hasAnyAttributes(html));
+        assertTrue(hasAnyAttributes(p));
 
-    @Test
-    void addingClass_addsAttributesToHtmlSubtree() {
-        p.addClass("Foobs");
-        assertTrue(anyNodeInSubtreeHasAttributes(html));
-    }
-
-    @Test
-    void attributesObject_reflectsClassAfterAddition() {
-        p.addClass("Foobs");
         Attributes attributes = p.attributes();
         assertTrue(attributes.hasKey("class"));
-    }
-
-    @Test
-    void clearingAttributes_removesThemFromParagraph() {
-        p.addClass("Foobs");
         p.clearAttributes();
-        assertFalse(anyNodeInSubtreeHasAttributes(p));
-    }
-
-    @Test
-    void clearingAttributes_removesThemFromHtmlSubtree() {
-        p.addClass("Foobs");
-        p.clearAttributes();
-        assertFalse(anyNodeInSubtreeHasAttributes(html));
-    }
-
-    @Test
-    void attributesObject_reflectsRemovalAfterClear() {
-        p.addClass("Foobs");
-        Attributes attributes = p.attributes();
-        p.clearAttributes();
+        assertFalse(hasAnyAttributes(p));
+        assertFalse(hasAnyAttributes(html));
         assertFalse(attributes.hasKey("class"));
     }
 
-    private boolean anyNodeInSubtreeHasAttributes(Node node) {
+    private boolean hasAnyAttributes(Node node) {
         final boolean[] found = new boolean[1];
         node.filter(new NodeFilter() {
             @Override
