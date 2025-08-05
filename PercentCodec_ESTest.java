375d546b0043fe145c9100e1a5cc1d19d4628e81
NiD133 <k19052355@kcl.ac.uk>
2025-08-06 05:20:10 +0800

    Original autoTestSuite PercentCodec_ESTest.java

--- PercentCodec_ESTest.java
+++ PercentCodec_ESTest.java
@@ -34,7 +34,185 @@ public class PercentCodec_ESTest extends PercentCodec_ESTest_scaffolding {
       assertArrayEquals(new byte[] {(byte)0, (byte)0, (byte)0, (byte)70, (byte)0}, byteArray1);
   }
 
-  // ... and so on for all 19 tests ...
+  @Test(timeout = 4000)
+  public void test02()  throws Throwable  {
+      byte[] byteArray0 = new byte[5];
+      PercentCodec percentCodec0 = new PercentCodec(byteArray0, true);
+      byte[] byteArray1 = new byte[9];
+      byteArray1[0] = (byte) (-19);
+      byte[] byteArray2 = percentCodec0.encode(byteArray1);
+      byte[] byteArray3 = percentCodec0.decode(byteArray2);
+      assertEquals(9, byteArray3.length);
+      assertEquals(27, byteArray2.length);
+      assertArrayEquals(new byte[] {(byte) (-19), (byte)0, (byte)0, (byte)0, (byte)0, (byte)0, (byte)0, (byte)0, (byte)0}, byteArray3);
+  }
+
+  @Test(timeout = 4000)
+  public void test03()  throws Throwable  {
+      byte[] byteArray0 = new byte[5];
+      byteArray0[3] = (byte)70;
+      byte[] byteArray1 = new byte[0];
+      PercentCodec percentCodec0 = new PercentCodec(byteArray1, true);
+      byte[] byteArray2 = percentCodec0.encode(byteArray0);
+      assertSame(byteArray2, byteArray0);
+  }
+
+  @Test(timeout = 4000)
+  public void test04()  throws Throwable  {
+      PercentCodec percentCodec0 = new PercentCodec();
+      byte[] byteArray0 = new byte[0];
+      byte[] byteArray1 = percentCodec0.decode(byteArray0);
+      assertEquals(0, byteArray1.length);
+  }
+
+  @Test(timeout = 4000)
+  public void test05()  throws Throwable  {
+      byte[] byteArray0 = new byte[1];
+      byteArray0[0] = (byte) (-50);
+      PercentCodec percentCodec0 = null;
+      try {
+        percentCodec0 = new PercentCodec(byteArray0, true);
+        fail("Expecting exception: IllegalArgumentException");
+      
+      } catch(IllegalArgumentException e) {
+         //
+         // byte must be >= 0
+         //
+         verifyException("org.apache.commons.codec.net.PercentCodec", e);
+      }
+  }
+
+  @Test(timeout = 4000)
+  public void test06()  throws Throwable  {
+      byte[] byteArray0 = new byte[5];
+      PercentCodec percentCodec0 = new PercentCodec(byteArray0, true);
+      byte[] byteArray1 = new byte[7];
+      byteArray1[6] = (byte)63;
+      byte[] byteArray2 = percentCodec0.encode(byteArray1);
+      assertEquals(19, byteArray2.length);
+  }
+
+  @Test(timeout = 4000)
+  public void test07()  throws Throwable  {
+      PercentCodec percentCodec0 = new PercentCodec();
+      Object object0 = percentCodec0.encode((Object) null);
+      assertNull(object0);
+  }
+
+  @Test(timeout = 4000)
+  public void test08()  throws Throwable  {
+      PercentCodec percentCodec0 = new PercentCodec();
+      try { 
+        percentCodec0.encode((Object) percentCodec0);
+        fail("Expecting exception: Exception");
+      
+      } catch(Exception e) {
+         //
+         // Objects of type org.apache.commons.codec.net.PercentCodec cannot be Percent encoded
+         //
+         verifyException("org.apache.commons.codec.net.PercentCodec", e);
+      }
+  }
+
+  @Test(timeout = 4000)
+  public void test09()  throws Throwable  {
+      PercentCodec percentCodec0 = new PercentCodec();
+      byte[] byteArray0 = new byte[2];
+      byte[] byteArray1 = percentCodec0.encode(byteArray0);
+      assertSame(byteArray1, byteArray0);
+  }
+
+  @Test(timeout = 4000)
+  public void test10()  throws Throwable  {
+      PercentCodec percentCodec0 = new PercentCodec();
+      byte[] byteArray0 = percentCodec0.encode((byte[]) null);
+      assertNull(byteArray0);
+  }
+
+  @Test(timeout = 4000)
+  public void test11()  throws Throwable  {
+      byte[] byteArray0 = new byte[5];
+      PercentCodec percentCodec0 = new PercentCodec(byteArray0, true);
+      byte[] byteArray1 = new byte[9];
+      byteArray1[2] = (byte)22;
+      byte[] byteArray2 = percentCodec0.encode(byteArray1);
+      assertEquals(25, byteArray2.length);
+  }
+
+  @Test(timeout = 4000)
+  public void test12()  throws Throwable  {
+      byte[] byteArray0 = new byte[5];
+      PercentCodec percentCodec0 = new PercentCodec(byteArray0, true);
+      Object object0 = percentCodec0.decode((Object) null);
+      assertNull(object0);
+  }
+
+  @Test(timeout = 4000)
+  public void test13()  throws Throwable  {
+      PercentCodec percentCodec0 = new PercentCodec();
+      try { 
+        percentCodec0.decode((Object) percentCodec0);
+        fail("Expecting exception: Exception");
+      
+      } catch(Exception e) {
+         //
+         // Objects of type org.apache.commons.codec.net.PercentCodec cannot be Percent decoded
+         //
+         verifyException("org.apache.commons.codec.net.PercentCodec", e);
+      }
+  }
+
+  @Test(timeout = 4000)
+  public void test14()  throws Throwable  {
+      PercentCodec percentCodec0 = new PercentCodec();
+      byte[] byteArray0 = new byte[6];
+      byteArray0[5] = (byte)37;
+      try { 
+        percentCodec0.decode(byteArray0);
+        fail("Expecting exception: Exception");
+      
+      } catch(Exception e) {
+         //
+         // Invalid percent decoding: 
+         //
+         verifyException("org.apache.commons.codec.net.PercentCodec", e);
+      }
+  }
+
+  @Test(timeout = 4000)
+  public void test15()  throws Throwable  {
+      PercentCodec percentCodec0 = new PercentCodec();
+      byte[] byteArray0 = percentCodec0.decode((byte[]) null);
+      assertNull(byteArray0);
+  }
+
+  @Test(timeout = 4000)
+  public void test16()  throws Throwable  {
+      byte[] byteArray0 = new byte[1];
+      PercentCodec percentCodec0 = new PercentCodec((byte[]) null, true);
+      byte[] byteArray1 = percentCodec0.encode(byteArray0);
+      assertSame(byteArray1, byteArray0);
+      assertNotNull(byteArray1);
+  }
+
+  @Test(timeout = 4000)
+  public void test17()  throws Throwable  {
+      byte[] byteArray0 = new byte[1];
+      byteArray0[0] = (byte)43;
+      PercentCodec percentCodec0 = new PercentCodec(byteArray0, true);
+      byte[] byteArray1 = percentCodec0.decode(byteArray0);
+      byte[] byteArray2 = percentCodec0.encode(byteArray1);
+      assertArrayEquals(new byte[] {(byte)43}, byteArray2);
+  }
+
+  @Test(timeout = 4000)
+  public void test18()  throws Throwable  {
+      byte[] byteArray0 = new byte[0];
+      PercentCodec percentCodec0 = new PercentCodec(byteArray0, true);
+      byte[] byteArray1 = percentCodec0.encode(byteArray0);
+      assertSame(byteArray1, byteArray0);
+      assertNotNull(byteArray1);
+  }
 
   @Test(timeout = 4000)
   public void test19()  throws Throwable  {
