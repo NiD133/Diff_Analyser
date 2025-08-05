d3a549bf3e9f3b7f11e32457f66712e0ddbc0d1c
NiD133 <k19052355@kcl.ac.uk>
2025-08-06 04:39:38 +0800

    Original manualTestSuite MeterIntervalTest.java

--- MeterIntervalTest.java
+++ MeterIntervalTest.java
@@ -1,76 +1,103 @@
+/* ======================================================
+ * JFreeChart : a chart library for the Java(tm) platform
+ * ======================================================
+ *
+ * (C) Copyright 2000-present, by David Gilbert and Contributors.
+ *
+ * Project Info:  https://www.jfree.org/jfreechart/index.html
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ *
+ * [Oracle and Java are registered trademarks of Oracle and/or its affiliates. 
+ * Other names may be trademarks of their respective owners.]
+ *
+ * ----------------------
+ * MeterIntervalTest.java
+ * ----------------------
+ * (C) Copyright 2005-present, by David Gilbert and Contributors.
+ *
+ * Original Author:  David Gilbert;
+ * Contributor(s):   -;
+ *
+ */
+
 package org.jfree.chart.plot;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
 
 import org.jfree.chart.TestUtils;
+
 import org.jfree.data.Range;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * Unit tests for the {@link MeterInterval} class.
+ * Tests for the {@link MeterInterval} class.
  */
 public class MeterIntervalTest {
 
     /**
-     * Tests the {@code equals} method to ensure it correctly compares
-     * all relevant fields of the {@link MeterInterval} class.
+     * Confirm that the equals method can distinguish all the required fields.
      */
     @Test
     public void testEquals() {
-        // Create two identical MeterInterval objects
-        MeterInterval interval1 = createMeterInterval("Label 1", 1.2, 3.4, Color.RED, 1.0f, Color.BLUE);
-        MeterInterval interval2 = createMeterInterval("Label 1", 1.2, 3.4, Color.RED, 1.0f, Color.BLUE);
 
-        // Assert that both objects are equal
-        assertEquals(interval1, interval2);
-        assertEquals(interval2, interval1);
+        MeterInterval m1 = new MeterInterval(
+            "Label 1", new Range(1.2, 3.4), Color.RED, new BasicStroke(1.0f),
+            Color.BLUE
+        );
+        MeterInterval m2 = new MeterInterval(
+            "Label 1", new Range(1.2, 3.4), Color.RED, new BasicStroke(1.0f),
+            Color.BLUE
+        );
+        assertEquals(m1, m2);
+        assertEquals(m2, m1);
 
-        // Modify one field and assert inequality
-        interval1 = createMeterInterval("Label 2", 1.2, 3.4, Color.RED, 1.0f, Color.BLUE);
-        assertNotEquals(interval1, interval2);
+        m1 = new MeterInterval(
+            "Label 2", new Range(1.2, 3.4), Color.RED, new BasicStroke(1.0f),
+            Color.BLUE
+        );
+        assertNotEquals(m1, m2);
+        m2 = new MeterInterval(
+            "Label 2", new Range(1.2, 3.4), Color.RED, new BasicStroke(1.0f),
+            Color.BLUE
+        );
+        assertEquals(m1, m2);
 
-        // Update the second object to match the first and assert equality
-        interval2 = createMeterInterval("Label 2", 1.2, 3.4, Color.RED, 1.0f, Color.BLUE);
-        assertEquals(interval1, interval2);
     }
 
     /**
-     * Tests that the {@link MeterInterval} class is not cloneable,
-     * as it is designed to be immutable.
+     * This class is immutable so cloning isn't required.
      */
     @Test
     public void testCloning() {
-        MeterInterval interval = new MeterInterval("X", new Range(1.0, 2.0));
-        assertFalse(interval instanceof Cloneable, "MeterInterval should not be cloneable");
+        MeterInterval m1 = new MeterInterval("X", new Range(1.0, 2.0));
+        assertFalse(m1 instanceof Cloneable);
     }
 
-    /**
-     * Tests the serialization and deserialization process to ensure
-     * that a {@link MeterInterval} object remains equal to its original
-     * state after being serialized and then deserialized.
+   /**
+     * Serialize an instance, restore it, and check for equality.
      */
     @Test
     public void testSerialization() {
-        MeterInterval original = new MeterInterval("X", new Range(1.0, 2.0));
-        MeterInterval deserialized = TestUtils.serialised(original);
-        assertEquals(original, deserialized, "Deserialized object should be equal to the original");
+        MeterInterval m1 = new MeterInterval("X", new Range(1.0, 2.0));
+        MeterInterval m2 = TestUtils.serialised(m1);
+        assertEquals(m1, m2);
     }
 
-    /**
-     * Helper method to create a {@link MeterInterval} with specified parameters.
-     *
-     * @param label the label for the interval
-     * @param lower the lower bound of the range
-     * @param upper the upper bound of the range
-     * @param outlineColor the color of the outline
-     * @param strokeWidth the width of the outline stroke
-     * @param backgroundColor the background color
-     * @return a new {@link MeterInterval} instance
-     */
-    private MeterInterval createMeterInterval(String label, double lower, double upper, Color outlineColor, float strokeWidth, Color backgroundColor) {
-        return new MeterInterval(label, new Range(lower, upper), outlineColor, new BasicStroke(strokeWidth), backgroundColor);
-    }
 }
