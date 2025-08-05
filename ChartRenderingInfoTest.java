3334c698ae7d1dc960fc6dc422caffcb894cf43b
NiD133 <k19052355@kcl.ac.uk>
2025-08-06 04:38:03 +0800

    gemini manualTestSuite ChartRenderingInfoTest.java

--- ChartRenderingInfoTest.java
+++ ChartRenderingInfoTest.java
@@ -18,6 +18,7 @@
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
+
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
  * USA.
  *
@@ -36,98 +37,154 @@
 
 package org.jfree.chart;
 
-import java.awt.Rectangle;
-import java.awt.geom.Rectangle2D;
-
 import org.jfree.chart.entity.ChartEntity;
 import org.jfree.chart.entity.StandardEntityCollection;
 import org.jfree.chart.internal.CloneUtils;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
+import java.awt.Rectangle;
+import java.awt.geom.Rectangle2D;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Tests for the {@link ChartRenderingInfo} class.
  */
-public class ChartRenderingInfoTest  {
+@DisplayName("ChartRenderingInfo")
+class ChartRenderingInfoTest {
 
-    /**
-     * Confirm that the equals method can distinguish all the required fields.
-     */
-    @Test
-    public void testEquals() {
-        ChartRenderingInfo i1 = new ChartRenderingInfo();
-        ChartRenderingInfo i2 = new ChartRenderingInfo();
-        assertEquals(i1, i2);
+    @Nested
+    @DisplayName("equals")
+    class Equals {
 
-        i1.setChartArea(new Rectangle2D.Double(1.0, 2.0, 3.0, 4.0));
-        assertNotEquals(i1, i2);
-        i2.setChartArea(new Rectangle2D.Double(1.0, 2.0, 3.0, 4.0));
-        assertEquals(i1, i2);
+        @Test
+        @DisplayName("should return true for two default instances")
+        void equals_shouldReturnTrue_forDefaultInstances() {
+            // Arrange
+            ChartRenderingInfo info1 = new ChartRenderingInfo();
+            ChartRenderingInfo info2 = new ChartRenderingInfo();
 
-        i1.getPlotInfo().setDataArea(new Rectangle(1, 2, 3, 4));
-        assertNotEquals(i1, i2);
-        i2.getPlotInfo().setDataArea(new Rectangle(1, 2, 3, 4));
-        assertEquals(i1, i2);
+            // Act & Assert
+            assertEquals(info1, info2);
+        }
 
-        StandardEntityCollection e1 = new StandardEntityCollection();
-        e1.add(new ChartEntity(new Rectangle(1, 2, 3, 4)));
-        i1.setEntityCollection(e1);
-        assertNotEquals(i1, i2);
-        StandardEntityCollection e2 = new StandardEntityCollection();
-        e2.add(new ChartEntity(new Rectangle(1, 2, 3, 4)));
-        i2.setEntityCollection(e2);
+        @Test
+        @DisplayName("should return false when chartArea differs")
+        void equals_shouldReturnFalse_whenChartAreaDiffers() {
+            // Arrange
+            ChartRenderingInfo info1 = new ChartRenderingInfo();
+            info1.setChartArea(new Rectangle2D.Double(1.0, 2.0, 3.0, 4.0));
+            ChartRenderingInfo info2 = new ChartRenderingInfo();
+
+            // Act & Assert
+            assertNotEquals(info1, info2);
+        }
+
+        @Test
+        @DisplayName("should return false when plotInfo differs")
+        void equals_shouldReturnFalse_whenPlotInfoDiffers() {
+            // Arrange
+            ChartRenderingInfo info1 = new ChartRenderingInfo();
+            info1.getPlotInfo().setDataArea(new Rectangle(1, 2, 3, 4));
+            ChartRenderingInfo info2 = new ChartRenderingInfo();
+
+            // Act & Assert
+            assertNotEquals(info1, info2);
+        }
+
+        @Test
+        @DisplayName("should return false when entityCollection differs")
+        void equals_shouldReturnFalse_whenEntityCollectionDiffers() {
+            // Arrange
+            ChartRenderingInfo info1 = new ChartRenderingInfo();
+            StandardEntityCollection entities = new StandardEntityCollection();
+            entities.add(new ChartEntity(new Rectangle(1, 2, 3, 4)));
+            info1.setEntityCollection(entities);
+            ChartRenderingInfo info2 = new ChartRenderingInfo();
+
+            // Act & Assert
+            assertNotEquals(info1, info2);
+        }
+
+        @Test
+        @DisplayName("should return true when all properties are equal")
+        void equals_shouldReturnTrue_whenAllPropertiesAreEqual() {
+            // Arrange
+            ChartRenderingInfo info1 = new ChartRenderingInfo();
+            info1.setChartArea(new Rectangle2D.Double(1.0, 2.0, 3.0, 4.0));
+            info1.getPlotInfo().setDataArea(new Rectangle(1, 2, 3, 4));
+            StandardEntityCollection entities1 = new StandardEntityCollection();
+            entities1.add(new ChartEntity(new Rectangle(5, 6, 7, 8)));
+            info1.setEntityCollection(entities1);
+
+            ChartRenderingInfo info2 = new ChartRenderingInfo();
+            info2.setChartArea(new Rectangle2D.Double(1.0, 2.0, 3.0, 4.0));
+            info2.getPlotInfo().setDataArea(new Rectangle(1, 2, 3, 4));
+            StandardEntityCollection entities2 = new StandardEntityCollection();
+            entities2.add(new ChartEntity(new Rectangle(5, 6, 7, 8)));
+            info2.setEntityCollection(entities2);
+
+            // Act & Assert
+            assertEquals(info1, info2);
+        }
     }
 
-    /**
-     * Confirm that cloning works.
-     * @throws java.lang.CloneNotSupportedException
-     */
     @Test
-    public void testCloning() throws CloneNotSupportedException {
-        ChartRenderingInfo i1 = new ChartRenderingInfo();
-        ChartRenderingInfo i2 = CloneUtils.clone(i1);
+    @DisplayName("clone should create a deep copy")
+    void clone_shouldCreateDeepCopy() throws CloneNotSupportedException {
+        // Arrange
+        ChartRenderingInfo original = new ChartRenderingInfo();
+        original.setChartArea(new Rectangle2D.Double(1.0, 2.0, 3.0, 4.0));
+        original.getEntityCollection().add(new ChartEntity(new Rectangle(10, 20, 30, 40)));
 
-        assertNotSame(i1, i2);
-        assertSame(i1.getClass(), i2.getClass());
-        assertEquals(i1, i2);
+        // Act
+        ChartRenderingInfo clone = (ChartRenderingInfo) CloneUtils.clone(original);
 
-        // check independence
-        i1.getChartArea().setRect(4.0, 3.0, 2.0, 1.0);
-        assertNotEquals(i1, i2);
-        i2.getChartArea().setRect(4.0, 3.0, 2.0, 1.0);
-        assertEquals(i1, i2);
+        // Assert: clone is a separate but equal object
+        assertNotSame(original, clone);
+        assertEquals(original, clone);
 
-        i1.getEntityCollection().add(new ChartEntity(new Rectangle(1, 2, 2,
-                1)));
-        assertNotEquals(i1, i2);
-        i2.getEntityCollection().add(new ChartEntity(new Rectangle(1, 2, 2,
-                1)));
-        assertEquals(i1, i2);
+        // Assert: modifying original's chartArea does not affect the clone (deep copy)
+        original.getChartArea().setRect(4.0, 3.0, 2.0, 1.0);
+        assertNotEquals(original.getChartArea(), clone.getChartArea());
+        assertNotEquals(original, clone);
 
+        // Assert: modifying original's entity collection does not affect the clone (deep copy)
+        original.getEntityCollection().add(new ChartEntity(new Rectangle(1, 2, 3, 4)));
+        assertNotEquals(original.getEntityCollection(), clone.getEntityCollection());
+        assertNotEquals(original, clone);
     }
 
-    /**
-     * Serialize an instance, restore it, and check for equality.
-     */
     @Test
-    public void testSerialization() {
-        ChartRenderingInfo i1 = new ChartRenderingInfo();
-        i1.setChartArea(new Rectangle2D.Double(1.0, 2.0, 3.0, 4.0));
-        ChartRenderingInfo i2 = TestUtils.serialised(i1);
-        assertEquals(i1, i2);
+    @DisplayName("serialization should preserve the object's state")
+    void serialization_shouldPreserveState() {
+        // Arrange
+        ChartRenderingInfo originalInfo = new ChartRenderingInfo();
+        originalInfo.setChartArea(new Rectangle2D.Double(1.0, 2.0, 3.0, 4.0));
+        originalInfo.getPlotInfo().setDataArea(new Rectangle2D.Double(5.0, 6.0, 7.0, 8.0));
+
+        // Act
+        ChartRenderingInfo deserializedInfo = TestUtils.serialised(originalInfo);
+
+        // Assert
+        assertEquals(originalInfo, deserializedInfo);
     }
 
-    /**
-     * Serialize an instance, restore it, and check for equality.
-     */
     @Test
-    public void testSerialization2() {
-        ChartRenderingInfo i1 = new ChartRenderingInfo();
-        i1.getPlotInfo().setDataArea(new Rectangle2D.Double(1.0, 2.0, 3.0,
-                4.0));
-        ChartRenderingInfo i2 = TestUtils.serialised(i1);
-        assertEquals(i1, i2);
-        assertEquals(i2, i2.getPlotInfo().getOwner());
+    @DisplayName("deserialized PlotInfo should correctly reference its owner")
+    void deserialization_shouldRestorePlotInfoOwner() {
+        // Arrange
+        ChartRenderingInfo originalInfo = new ChartRenderingInfo();
+        originalInfo.getPlotInfo().setDataArea(new Rectangle2D.Double(1.0, 2.0, 3.0, 4.0));
+
+        // Act
+        ChartRenderingInfo deserializedInfo = TestUtils.serialised(originalInfo);
+
+        // Assert that the PlotRenderingInfo's back-reference to its owner
+        // is correctly restored to the new deserialized parent object.
+        assertEquals(deserializedInfo, deserializedInfo.getPlotInfo().getOwner());
+        assertNotSame(originalInfo, deserializedInfo.getPlotInfo().getOwner());
     }
 }
