77a1da5df9973db35acc5a2136a09fd5d009a76b
NiD133 <k19052355@kcl.ac.uk>
2025-08-06 05:24:11 +0800

    Original manualTestSuite VectorTest.java

--- VectorTest.java
+++ VectorTest.java
@@ -1,46 +1,75 @@
+/*
+ *
+ * This file is part of the iText (R) project.
+    Copyright (c) 1998-2022 iText Group NV
+ * Authors: Bruno Lowagie, Paulo Soares, Kevin Day, et al.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License version 3
+ * as published by the Free Software Foundation with the addition of the
+ * following permission added to Section 15 as permitted in Section 7(a):
+ * FOR ANY PART OF THE COVERED WORK IN WHICH THE COPYRIGHT IS OWNED BY
+ * ITEXT GROUP. ITEXT GROUP DISCLAIMS THE WARRANTY OF NON INFRINGEMENT
+ * OF THIRD PARTY RIGHTS
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, see http://www.gnu.org/licenses or write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA, 02110-1301 USA, or download the license from the following URL:
+ * http://itextpdf.com/terms-of-use/
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License,
+ * a covered work must retain the producer line in every PDF that is created
+ * or manipulated using iText.
+ *
+ * You can be released from the requirements of the license by purchasing
+ * a commercial license. Buying such a license is mandatory as soon as you
+ * develop commercial activities involving the iText software without
+ * disclosing the source code of your own applications.
+ * These activities include: offering paid services to customers as an ASP,
+ * serving PDFs on the fly in a web application, shipping iText with a closed
+ * source product.
+ *
+ * For more information, please contact iText Software Corp. at this
+ * address: sales@itextpdf.com
+ */
 package com.itextpdf.text.pdf.parser;
+
+import junit.framework.Assert;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
 
 /**
- * Unit tests for the Vector class.
- * This test suite verifies the correctness of vector operations.
- * Author: Kevin Day
+ * @author kevin
  */
 public class VectorTest {
 
-    // Setup method executed before each test
     @Before
     public void setUp() throws Exception {
-        // Initialize resources if needed
     }
 
-    // Teardown method executed after each test
     @After
     public void tearDown() throws Exception {
-        // Clean up resources if needed
     }
 
-    /**
-     * Test the cross product of a vector with a matrix.
-     * This test checks if the cross product operation is correctly implemented.
-     */
     @Test
-    public void testCrossProductWithMatrix() {
-        // Arrange: Initialize a vector and a matrix
-        Vector vector = new Vector(2, 3, 4);
-        Matrix matrix = new Matrix(5, 6, 7, 8, 9, 10);
+    public void testCrossVector() {
+        Vector v = new Vector(2, 3, 4);
+        Matrix m = new Matrix(5, 6, 7, 8, 9, 10);
+        Vector shouldBe = new Vector(67, 76, 4);
         
-        // Expected result after the cross product operation
-        Vector expectedVector = new Vector(67, 76, 4);
-        
-        // Act: Perform the cross product operation
-        Vector resultVector = vector.cross(matrix);
-        
-        // Assert: Verify the result is as expected
-        assertEquals("The cross product of the vector and matrix is incorrect.", expectedVector, resultVector);
+        Vector rslt = v.cross(m);
+        Assert.assertEquals(shouldBe, rslt);
     }
+
 }
