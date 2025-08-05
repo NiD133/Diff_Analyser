f637f3dddfb6bb99b91ed9056dd4834ecc930e15
NiD133 <k19052355@kcl.ac.uk>
2025-08-06 05:23:27 +0800

    Original manualTestSuite ParameterTest.java

--- ParameterTest.java
+++ ParameterTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2012 The Guava Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.common.reflect;
 
 import com.google.common.testing.EqualsTester;
@@ -7,62 +23,41 @@ import junit.framework.TestCase;
 import org.jspecify.annotations.NullUnmarked;
 
 /**
- * Unit tests for the {@link Parameter} class.
- * This test suite verifies the behavior of the Parameter class, including its handling of nulls and equality.
- * 
- * <p>Note: This test suite is not compatible with Android VMs due to the absence of AnnotatedType.
- * 
- * <p>Author: Ben Yu
+ * Tests for {@link Parameter}.
+ *
+ * @author Ben Yu
  */
 @NullUnmarked
 public class ParameterTest extends TestCase {
 
-  /**
-   * Tests that all public instance methods of the Parameter class handle null inputs correctly.
-   * This test is skipped on Android VMs due to the absence of AnnotatedType.
-   */
   public void testNulls() {
-    if (isRunningOnAndroidVm()) {
-      return; // Skip test on Android VMs
-    }
-
-    NullPointerTester nullTester = new NullPointerTester();
-    for (Method method : ParameterTest.class.getDeclaredMethods()) {
-      for (Parameter param : Invokable.from(method).getParameters()) {
-        nullTester.testAllPublicInstanceMethods(param);
-      }
-    }
-  }
-
-  /**
-   * Tests the equality behavior of Parameter instances.
-   * Ensures that parameters are correctly grouped by equality.
-   */
-  public void testEquals() {
-    EqualsTester equalsTester = new EqualsTester();
-    for (Method method : ParameterTest.class.getDeclaredMethods()) {
-      for (Parameter param : Invokable.from(method).getParameters()) {
-        equalsTester.addEqualityGroup(param);
-      }
-    }
-    equalsTester.testEquals();
-  }
-
-  /**
-   * Helper method to determine if the current environment is an Android VM.
-   * 
-   * @return true if running on an Android VM, false otherwise.
-   */
-  private boolean isRunningOnAndroidVm() {
     try {
       Class.forName("java.lang.reflect.AnnotatedType");
-      return false;
-    } catch (ClassNotFoundException e) {
-      return true;
+    } catch (ClassNotFoundException runningInAndroidVm) {
+      /*
+       * Parameter declares a method that returns AnnotatedType, which isn't available on Android.
+       * This would cause NullPointerTester, which calls Class.getDeclaredMethods, to throw
+       * NoClassDefFoundError.
+       */
+      return;
+    }
+    for (Method method : ParameterTest.class.getDeclaredMethods()) {
+      for (Parameter param : Invokable.from(method).getParameters()) {
+        new NullPointerTester().testAllPublicInstanceMethods(param);
+      }
     }
   }
 
-  // Sample methods to provide parameters for testing
+  public void testEquals() {
+    EqualsTester tester = new EqualsTester();
+    for (Method method : ParameterTest.class.getDeclaredMethods()) {
+      for (Parameter param : Invokable.from(method).getParameters()) {
+        tester.addEqualityGroup(param);
+      }
+    }
+    tester.testEquals();
+  }
+
   @SuppressWarnings("unused")
   private void someMethod(int i, int j) {}
 
