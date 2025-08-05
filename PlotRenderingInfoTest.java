388ac36197c078cee8139e05d3c14e2cbe61be46
NiD133 <k19052355@kcl.ac.uk>
2025-08-06 05:23:31 +0800

    deepseek manualTestSuite PlotRenderingInfoTest.java

--- PlotRenderingInfoTest.java
+++ PlotRenderingInfoTest.java
@@ -1,37 +1,22 @@
-/* ======================================================
+/* 
  * JFreeChart : a chart library for the Java(tm) platform
  * ======================================================
- *
  * (C) Copyright 2000-present, by David Gilbert and Contributors.
- *
- * Project Info:  https://www.jfree.org/jfreechart/index.html
- *
+ * Project Info: https://www.jfree.org/jfreechart/index.html
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
- *
  * This library is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
- *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
  * USA.
- *
  * [Oracle and Java are registered trademarks of Oracle and/or its affiliates. 
  * Other names may be trademarks of their respective owners.]
- *
- * --------------------------
- * PlotRenderingInfoTest.java
- * --------------------------
- * (C) Copyright 2004-present, by David Gilbert and Contributors.
- *
- * Original Author:  David Gilbert;
- * Contributor(s):   -;
- *
  */
 
 package org.jfree.chart.plot;
@@ -51,69 +36,129 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 public class PlotRenderingInfoTest {
 
+    // Helper method to create a basic PlotRenderingInfo instance
+    private PlotRenderingInfo createInfo() {
+        return new PlotRenderingInfo(new ChartRenderingInfo());
+    }
+
+    // Helper method to create an instance with subplots
+    private PlotRenderingInfo createInfoWithSubplots() {
+        PlotRenderingInfo info = createInfo();
+        info.addSubplotInfo(new PlotRenderingInfo(null));
+        return info;
+    }
+
     /**
-     * Test the equals() method.
+     * Tests that two empty instances are equal.
      */
     @Test
-    public void testEquals() {
-        PlotRenderingInfo p1 = new PlotRenderingInfo(new ChartRenderingInfo());
-        PlotRenderingInfo p2 = new PlotRenderingInfo(new ChartRenderingInfo());
-        assertEquals(p1, p2);
-        assertEquals(p2, p1);
+    public void testEqualsForEmptyInstances() {
+        PlotRenderingInfo p1 = createInfo();
+        PlotRenderingInfo p2 = createInfo();
+        assertEquals(p1, p2, "Empty instances should be equal");
+    }
 
+    /**
+     * Tests that plot area differences are detected.
+     */
+    @Test
+    public void testEqualsWithDifferentPlotAreas() {
+        PlotRenderingInfo p1 = createInfo();
+        PlotRenderingInfo p2 = createInfo();
+        
         p1.setPlotArea(new Rectangle(2, 3, 4, 5));
-        assertNotEquals(p1, p2);
+        assertNotEquals(p1, p2, "Instances should differ after setting plot area in one");
+        
         p2.setPlotArea(new Rectangle(2, 3, 4, 5));
-        assertEquals(p1, p2);
+        assertEquals(p1, p2, "Instances should be equal after setting identical plot areas");
+    }
 
+    /**
+     * Tests that data area differences are detected.
+     */
+    @Test
+    public void testEqualsWithDifferentDataAreas() {
+        PlotRenderingInfo p1 = createInfo();
+        PlotRenderingInfo p2 = createInfo();
+        p1.setPlotArea(new Rectangle(0, 0, 10, 10));
+        p2.setPlotArea(new Rectangle(0, 0, 10, 10));
+        
         p1.setDataArea(new Rectangle(2, 4, 6, 8));
-        assertNotEquals(p1, p2);
+        assertNotEquals(p1, p2, "Instances should differ after setting data area in one");
+        
         p2.setDataArea(new Rectangle(2, 4, 6, 8));
-        assertEquals(p1, p2);
+        assertEquals(p1, p2, "Instances should be equal after setting identical data areas");
+    }
 
+    /**
+     * Tests that subplot count differences are detected.
+     */
+    @Test
+    public void testEqualsWithDifferentSubplotCounts() {
+        PlotRenderingInfo p1 = createInfo();
+        PlotRenderingInfo p2 = createInfo();
+        
         p1.addSubplotInfo(new PlotRenderingInfo(null));
-        assertNotEquals(p1, p2);
+        assertNotEquals(p1, p2, "Instances should differ when subplot counts differ");
+        
         p2.addSubplotInfo(new PlotRenderingInfo(null));
-        assertEquals(p1, p2);
+        assertEquals(p1, p2, "Instances should be equal when subplot counts match");
+    }
 
+    /**
+     * Tests that subplot content differences are detected.
+     */
+    @Test
+    public void testEqualsWithDifferentSubplotContents() {
+        PlotRenderingInfo p1 = createInfoWithSubplots();
+        PlotRenderingInfo p2 = createInfoWithSubplots();
+        
         p1.getSubplotInfo(0).setDataArea(new Rectangle(1, 2, 3, 4));
-        assertNotEquals(p1, p2);
+        assertNotEquals(p1, p2, "Instances should differ when subplot contents differ");
+        
         p2.getSubplotInfo(0).setDataArea(new Rectangle(1, 2, 3, 4));
-        assertEquals(p1, p2);
+        assertEquals(p1, p2, "Instances should be equal when subplot contents match");
     }
 
     /**
-     * Confirm that cloning works.
+     * Tests that cloning creates an independent copy with same values.
      */
     @Test
-    public void testCloning() throws CloneNotSupportedException {
-        PlotRenderingInfo p1 = new PlotRenderingInfo(new ChartRenderingInfo());
-        p1.setPlotArea(new Rectangle2D.Double());
-        PlotRenderingInfo p2 = CloneUtils.clone(p1);
-        assertNotSame(p1, p2);
-        assertSame(p1.getClass(), p2.getClass());
-        assertEquals(p1, p2);
+    public void testCloningCreatesIndependentCopy() throws CloneNotSupportedException {
+        // Setup original instance
+        PlotRenderingInfo original = createInfo();
+        original.setPlotArea(new Rectangle2D.Double(1, 2, 3, 4));
+        original.setDataArea(new Rectangle2D.Double(5, 6, 7, 8));
+        original.addSubplotInfo(createInfo());
 
-        // check independence
-        p1.getPlotArea().setRect(1.0, 2.0, 3.0, 4.0);
-        assertNotEquals(p1, p2);
-        p2.getPlotArea().setRect(1.0, 2.0, 3.0, 4.0);
-        assertEquals(p1, p2);
+        // Verify clone matches original
+        PlotRenderingInfo clone = CloneUtils.clone(original);
+        assertNotSame(original, clone, "Clone should be a different instance");
+        assertSame(original.getClass(), clone.getClass(), "Clone should have same class");
+        assertEquals(original, clone, "Clone should be equal to original");
 
-        p1.getDataArea().setRect(4.0, 3.0, 2.0, 1.0);
-        assertNotEquals(p1, p2);
-        p2.getDataArea().setRect(4.0, 3.0, 2.0, 1.0);
-        assertEquals(p1, p2);
+        // Verify independence by modifying fields
+        original.getPlotArea().setRect(10, 20, 30, 40);
+        assertNotEquals(original, clone, "Clone should not match after modifying original's plot area");
+        
+        clone.getPlotArea().setRect(10, 20, 30, 40);
+        assertEquals(original, clone, "Should match after same modification to clone's plot area");
+        
+        original.getDataArea().setRect(50, 60, 70, 80);
+        assertNotEquals(original, clone, "Clone should not match after modifying original's data area");
     }
 
     /**
-     * Serialize an instance, restore it, and check for equality.
+     * Tests that serialization preserves object equality.
      */
     @Test
-    public void testSerialization() {
-        PlotRenderingInfo p1 = new PlotRenderingInfo(new ChartRenderingInfo());
-        PlotRenderingInfo p2 = TestUtils.serialised(p1);
-        assertEquals(p1, p2);
+    public void testSerializationPreservesEquality() {
+        PlotRenderingInfo original = createInfo();
+        original.setPlotArea(new Rectangle(1, 2, 3, 4));
+        original.setDataArea(new Rectangle(5, 6, 7, 8));
+        original.addSubplotInfo(createInfo());
+        
+        PlotRenderingInfo deserialized = TestUtils.serialised(original);
+        assertEquals(original, deserialized, "Deserialized instance should match original");
     }
-
 }
