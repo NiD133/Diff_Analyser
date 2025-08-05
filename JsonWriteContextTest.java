42c0e59fcfd3fce5864a3b2162c7f32f2487e8c8
NiD133 <k19052355@kcl.ac.uk>
2025-08-06 05:23:00 +0800

    Original manualTestSuite JsonWriteContextTest.java

--- JsonWriteContextTest.java
+++ JsonWriteContextTest.java
@@ -9,28 +9,18 @@ import static org.junit.jupiter.api.Assertions.*;
 /**
  * Unit tests for class {@link JsonWriteContext}.
  */
-public class JsonWriteContextTest extends JUnit5TestBase {
-    // [core#1421]: Verify custom context extension works
-    @Test
-    void extendedContextCanBeCreatedAndInitialized() {
-        // Local subclass to test protected constructor accessibility
-        class TestJsonWriteContext extends JsonWriteContext {
-            TestJsonWriteContext(int type, JsonWriteContext parent, DupDetector dups, Object currValue) {
-                super(type, parent, dups, currValue);
-            }
+public class JsonWriteContextTest extends JUnit5TestBase
+{
+    static class MyContext extends JsonWriteContext {
+        public MyContext(int type, JsonWriteContext parent, DupDetector dups, Object currValue) {
+            super(type, parent, dups, currValue);
         }
+    }
 
-        final int rootType = JsonStreamContext.TYPE_ROOT;
-        final Object testValue = 0;
-        
-        // Create extended context instance
-        JsonWriteContext context = new TestJsonWriteContext(rootType, null, null, testValue);
-
-        // Verify basic initialization
-        assertNotNull(context, "Context instance should not be null");
-        assertTrue(context.inRoot(), "Context should be root type");
-        assertNull(context.getParent(), "Root context should not have parent");
-        assertEquals(testValue, context.getCurrentValue(), 
-            "Current value should match constructor argument");
+    // [core#1421]
+    @Test
+    void testExtension() {
+        MyContext context = new MyContext(0, null, null, 0);
+        assertNotNull(context);
     }
 }
