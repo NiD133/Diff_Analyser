37e2f8f5116695c6e18e936f79780131f5a057cd
NiD133 <k19052355@kcl.ac.uk>
2025-08-06 04:38:35 +0800

    gpt manualTestSuite DocumentTypeTest.java

--- DocumentTypeTest.java
+++ DocumentTypeTest.java
@@ -8,36 +8,45 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
- * Tests for the DocumentType node
- *
- * @author Jonathan Hedley, http://jonathanhedley.com/
+ * Unit tests for the DocumentType node.
  */
 public class DocumentTypeTest {
+
     @Test
-    public void constructorValidationOkWithBlankName() {
-        new DocumentType("","", "");
+    public void testConstructorAllowsBlankName() {
+        // Test that a DocumentType can be created with a blank name
+        new DocumentType("", "", "");
     }
 
     @Test
-    public void constructorValidationThrowsExceptionOnNulls() {
+    public void testConstructorThrowsExceptionOnNulls() {
+        // Test that a DocumentType constructor throws an exception when publicId or systemId is null
         assertThrows(IllegalArgumentException.class, () -> new DocumentType("html", null, null));
     }
 
     @Test
-    public void constructorValidationOkWithBlankPublicAndSystemIds() {
-        new DocumentType("html","", "");
+    public void testConstructorAllowsBlankPublicAndSystemIds() {
+        // Test that a DocumentType can be created with blank public and system IDs
+        new DocumentType("html", "", "");
     }
 
-    @Test public void outerHtmlGeneration() {
+    @Test
+    public void testOuterHtmlGeneration() {
+        // Test the outerHtml generation for different DocumentType configurations
+
+        // HTML5 doctype
         DocumentType html5 = new DocumentType("html", "", "");
         assertEquals("<!doctype html>", html5.outerHtml());
 
+        // Public doctype
         DocumentType publicDocType = new DocumentType("html", "-//IETF//DTD HTML//", "");
         assertEquals("<!DOCTYPE html PUBLIC \"-//IETF//DTD HTML//\">", publicDocType.outerHtml());
 
+        // System doctype
         DocumentType systemDocType = new DocumentType("html", "", "http://www.ibm.com/data/dtd/v11/ibmxhtml1-transitional.dtd");
         assertEquals("<!DOCTYPE html SYSTEM \"http://www.ibm.com/data/dtd/v11/ibmxhtml1-transitional.dtd\">", systemDocType.outerHtml());
 
+        // Combined public and system doctype
         DocumentType combo = new DocumentType("notHtml", "--public", "--system");
         assertEquals("<!DOCTYPE notHtml PUBLIC \"--public\" \"--system\">", combo.outerHtml());
         assertEquals("notHtml", combo.name());
@@ -45,34 +54,47 @@ public class DocumentTypeTest {
         assertEquals("--system", combo.systemId());
     }
 
-    @Test public void testRoundTrip() {
+    @Test
+    public void testRoundTripParsing() {
+        // Test round-trip parsing of DOCTYPE declarations
+
+        // Basic HTML5 doctype
         String base = "<!DOCTYPE html>";
-        assertEquals("<!doctype html>", htmlOutput(base));
-        assertEquals(base, xmlOutput(base));
+        assertEquals("<!doctype html>", parseHtmlOutput(base));
+        assertEquals(base, parseXmlOutput(base));
 
+        // Public doctype
         String publicDoc = "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">";
-        assertEquals(publicDoc, htmlOutput(publicDoc));
-        assertEquals(publicDoc, xmlOutput(publicDoc));
+        assertEquals(publicDoc, parseHtmlOutput(publicDoc));
+        assertEquals(publicDoc, parseXmlOutput(publicDoc));
 
+        // System doctype
         String systemDoc = "<!DOCTYPE html SYSTEM \"exampledtdfile.dtd\">";
-        assertEquals(systemDoc, htmlOutput(systemDoc));
-        assertEquals(systemDoc, xmlOutput(systemDoc));
+        assertEquals(systemDoc, parseHtmlOutput(systemDoc));
+        assertEquals(systemDoc, parseXmlOutput(systemDoc));
 
+        // Legacy doctype
         String legacyDoc = "<!DOCTYPE html SYSTEM \"about:legacy-compat\">";
-        assertEquals(legacyDoc, htmlOutput(legacyDoc));
-        assertEquals(legacyDoc, xmlOutput(legacyDoc));
+        assertEquals(legacyDoc, parseHtmlOutput(legacyDoc));
+        assertEquals(legacyDoc, parseXmlOutput(legacyDoc));
     }
 
-    private String htmlOutput(String in) {
-        DocumentType type = (DocumentType) Jsoup.parse(in).childNode(0);
+    private String parseHtmlOutput(String input) {
+        // Parse the input string as HTML and return the outer HTML of the first child node
+        DocumentType type = (DocumentType) Jsoup.parse(input).childNode(0);
         return type.outerHtml();
     }
 
-    private String xmlOutput(String in) {
-        return Jsoup.parse(in, "", Parser.xmlParser()).childNode(0).outerHtml();
+    private String parseXmlOutput(String input) {
+        // Parse the input string as XML and return the outer HTML of the first child node
+        return Jsoup.parse(input, "", Parser.xmlParser()).childNode(0).outerHtml();
     }
 
-    @Test void attributes() {
+    @Test
+    public void testAttributes() {
+        // Test attribute retrieval from DocumentType nodes
+
+        // HTML5 doctype
         Document doc = Jsoup.parse("<!DOCTYPE html>");
         DocumentType doctype = doc.documentType();
         assertEquals("#doctype", doctype.nodeName());
@@ -81,9 +103,9 @@ public class DocumentTypeTest {
         assertEquals("", doctype.publicId());
         assertEquals("", doctype.systemId());
 
+        // Custom doctype with public and system IDs
         doc = Jsoup.parse("<!DOCTYPE notHtml PUBLIC \"--public\" \"--system\">");
         doctype = doc.documentType();
-
         assertEquals("#doctype", doctype.nodeName());
         assertEquals("nothtml", doctype.name());
         assertEquals("nothtml", doctype.attr("name"));
