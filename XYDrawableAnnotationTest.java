8cec0d6c20879e87a7f54ef4d84bab2855eddc8d
NiD133 <k19052355@kcl.ac.uk>
2025-08-12 03:09:55 +0800

    Original manualTestSuite XYDrawableAnnotationTest.java

--- XYDrawableAnnotationTest.java
+++ XYDrawableAnnotationTest.java
@@ -1,3 +1,39 @@
+/* ======================================================
+ * JFreeChart : a chart library for the Java(tm) platform
+ * ======================================================
+ *
+ * (C) Copyright 2000-present, by David Gilbert and Contributors.
+ *
+ * Project Info:  https://www.jfree.org/jfreechart/index.html
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ *
+ * [Oracle and Java are registered trademarks of Oracle and/or its affiliates. 
+ * Other names may be trademarks of their respective owners.]
+ *
+ * -----------------------------
+ * XYDrawableAnnotationTest.java
+ * -----------------------------
+ * (C) Copyright 2003-present, by David Gilbert.
+ *
+ * Original Author:  David Gilbert;
+ * Contributor(s):   -;
+ *
+ */
+
 package org.jfree.chart.annotations;
 
 import java.awt.Graphics2D;
@@ -13,24 +49,47 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * Unit tests for the {@link XYDrawableAnnotation} class.
+ * Tests for the {@link XYDrawableAnnotation} class.
  */
 public class XYDrawableAnnotationTest {
 
-    /**
-     * A simple implementation of the Drawable interface for testing purposes.
-     */
     static class TestDrawable implements Drawable, Cloneable, Serializable {
+        /**
+         * Default constructor.
+         */
+        public TestDrawable() {
+        }
+        /**
+         * Draws something.
+         * @param g2  the graphics device.
+         * @param area  the area in which to draw.
+         */
         @Override
         public void draw(Graphics2D g2, Rectangle2D area) {
-            // Intentionally left blank for testing
+            // do nothing
         }
-
+        /**
+         * Tests this object for equality with an arbitrary object.
+         * @param obj  the object to test against ({@code null} permitted).
+         * @return A boolean.
+         */
         @Override
         public boolean equals(Object obj) {
-            return obj instanceof TestDrawable;
+            if (obj == this) {
+                return true;
+            }
+            if (!(obj instanceof TestDrawable)) {
+                return false;
+            }
+            return true;
         }
-
+        /**
+         * Returns a clone.
+         *
+         * @return A clone.
+         *
+         * @throws CloneNotSupportedException if there is a problem cloning.
+         */
         @Override
         public Object clone() throws CloneNotSupportedException {
             return super.clone();
@@ -38,96 +97,99 @@ public class XYDrawableAnnotationTest {
     }
 
     /**
-     * Tests the equality of {@link XYDrawableAnnotation} instances.
+     * Confirm that the equals method can distinguish all the required fields.
      */
     @Test
     public void testEquals() {
-        XYDrawableAnnotation annotation1 = createAnnotation(10.0, 20.0, 100.0, 200.0);
-        XYDrawableAnnotation annotation2 = createAnnotation(10.0, 20.0, 100.0, 200.0);
-        assertEquals(annotation1, annotation2);
+        XYDrawableAnnotation a1 = new XYDrawableAnnotation(10.0, 20.0, 100.0,
+                200.0, new TestDrawable());
+        XYDrawableAnnotation a2 = new XYDrawableAnnotation(10.0, 20.0, 100.0,
+                200.0, new TestDrawable());
+        assertEquals(a1, a2);
 
-        annotation1 = createAnnotation(11.0, 20.0, 100.0, 200.0);
-        assertNotEquals(annotation1, annotation2);
-        
-        annotation2 = createAnnotation(11.0, 20.0, 100.0, 200.0);
-        assertEquals(annotation1, annotation2);
+        a1 = new XYDrawableAnnotation(11.0, 20.0, 100.0, 200.0,
+                new TestDrawable());
+        assertNotEquals(a1, a2);
+        a2 = new XYDrawableAnnotation(11.0, 20.0, 100.0, 200.0,
+                new TestDrawable());
+        assertEquals(a1, a2);
 
-        annotation1 = createAnnotation(11.0, 22.0, 100.0, 200.0);
-        assertNotEquals(annotation1, annotation2);
-        
-        annotation2 = createAnnotation(11.0, 22.0, 100.0, 200.0);
-        assertEquals(annotation1, annotation2);
+        a1 = new XYDrawableAnnotation(11.0, 22.0, 100.0, 200.0,
+                new TestDrawable());
+        assertNotEquals(a1, a2);
+        a2 = new XYDrawableAnnotation(11.0, 22.0, 100.0, 200.0,
+                new TestDrawable());
+        assertEquals(a1, a2);
 
-        annotation1 = createAnnotation(11.0, 22.0, 101.0, 200.0);
-        assertNotEquals(annotation1, annotation2);
-        
-        annotation2 = createAnnotation(11.0, 22.0, 101.0, 200.0);
-        assertEquals(annotation1, annotation2);
+        a1 = new XYDrawableAnnotation(11.0, 22.0, 101.0, 200.0,
+                new TestDrawable());
+        assertNotEquals(a1, a2);
+        a2 = new XYDrawableAnnotation(11.0, 22.0, 101.0, 200.0,
+                new TestDrawable());
+        assertEquals(a1, a2);
 
-        annotation1 = createAnnotation(11.0, 22.0, 101.0, 202.0);
-        assertNotEquals(annotation1, annotation2);
-        
-        annotation2 = createAnnotation(11.0, 22.0, 101.0, 202.0);
-        assertEquals(annotation1, annotation2);
+        a1 = new XYDrawableAnnotation(11.0, 22.0, 101.0, 202.0,
+                new TestDrawable());
+        assertNotEquals(a1, a2);
+        a2 = new XYDrawableAnnotation(11.0, 22.0, 101.0, 202.0,
+                new TestDrawable());
+        assertEquals(a1, a2);
 
-        annotation1 = createAnnotation(11.0, 22.0, 101.0, 202.0, 2.0);
-        assertNotEquals(annotation1, annotation2);
-        
-        annotation2 = createAnnotation(11.0, 22.0, 101.0, 202.0, 2.0);
-        assertEquals(annotation1, annotation2);
+        a1 = new XYDrawableAnnotation(11.0, 22.0, 101.0, 202.0, 2.0,
+                new TestDrawable());
+        assertNotEquals(a1, a2);
+        a2 = new XYDrawableAnnotation(11.0, 22.0, 101.0, 202.0, 2.0,
+                new TestDrawable());
+        assertEquals(a1, a2);
     }
 
     /**
-     * Tests that equal objects have the same hash code.
+     * Two objects that are equal are required to return the same hashCode.
      */
     @Test
     public void testHashCode() {
-        XYDrawableAnnotation annotation1 = createAnnotation(10.0, 20.0, 100.0, 200.0);
-        XYDrawableAnnotation annotation2 = createAnnotation(10.0, 20.0, 100.0, 200.0);
-        assertEquals(annotation1.hashCode(), annotation2.hashCode());
+        XYDrawableAnnotation a1 = new XYDrawableAnnotation(10.0, 20.0, 100.0,
+                200.0, new TestDrawable());
+        XYDrawableAnnotation a2 = new XYDrawableAnnotation(10.0, 20.0, 100.0,
+                200.0, new TestDrawable());
+        assertEquals(a1, a2);
+        int h1 = a1.hashCode();
+        int h2 = a2.hashCode();
+        assertEquals(h1, h2);
     }
 
     /**
-     * Tests the cloning functionality of {@link XYDrawableAnnotation}.
+     * Confirm that cloning works.
      */
     @Test
     public void testCloning() throws CloneNotSupportedException {
-        XYDrawableAnnotation original = createAnnotation(10.0, 20.0, 100.0, 200.0);
-        XYDrawableAnnotation clone = (XYDrawableAnnotation) original.clone();
-        assertNotSame(original, clone);
-        assertEquals(original, clone);
+        XYDrawableAnnotation a1 = new XYDrawableAnnotation(10.0, 20.0, 100.0,
+                200.0, new TestDrawable());
+        XYDrawableAnnotation a2 = (XYDrawableAnnotation) a1.clone();
+        assertNotSame(a1, a2);
+        assertSame(a1.getClass(), a2.getClass());
+        assertEquals(a1, a2);
     }
 
     /**
-     * Tests if the class implements {@link PublicCloneable}.
+     * Checks that this class implements PublicCloneable.
      */
     @Test
     public void testPublicCloneable() {
-        XYDrawableAnnotation annotation = createAnnotation(10.0, 20.0, 100.0, 200.0);
-        assertTrue(annotation instanceof PublicCloneable);
+        XYDrawableAnnotation a1 = new XYDrawableAnnotation(10.0, 20.0, 100.0,
+                200.0, new TestDrawable());
+        assertTrue(a1 instanceof PublicCloneable);
     }
 
     /**
-     * Tests the serialization and deserialization of {@link XYDrawableAnnotation}.
+     * Serialize an instance, restore it, and check for equality.
      */
     @Test
     public void testSerialization() {
-        XYDrawableAnnotation original = createAnnotation(10.0, 20.0, 100.0, 200.0);
-        XYDrawableAnnotation deserialized = TestUtils.serialised(original);
-        assertEquals(original, deserialized);
+        XYDrawableAnnotation a1 = new XYDrawableAnnotation(10.0, 20.0, 100.0,
+                200.0, new TestDrawable());
+        XYDrawableAnnotation a2 = TestUtils.serialised(a1);
+        assertEquals(a1, a2);
     }
 
-    /**
-     * Helper method to create an {@link XYDrawableAnnotation} with default scale factor.
-     */
-    private XYDrawableAnnotation createAnnotation(double x, double y, double width, double height) {
-        return new XYDrawableAnnotation(x, y, width, height, new TestDrawable());
-    }
-
-    /**
-     * Helper method to create an {@link XYDrawableAnnotation} with specified scale factor.
-     */
-    private XYDrawableAnnotation createAnnotation(double x, double y, double width, double height, double scaleFactor) {
-        return new XYDrawableAnnotation(x, y, width, height, scaleFactor, new TestDrawable());
-    }
 }
