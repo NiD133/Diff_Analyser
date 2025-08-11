22bcf6098474d8ec6e36722fba464de155c5b194
NiD133 <k19052355@kcl.ac.uk>
2025-08-12 05:16:33 +0800

    deepseek manualTestSuite XXHash32Test.java

--- XXHash32Test.java
+++ XXHash32Test.java
@@ -18,29 +18,31 @@ package org.apache.commons.codec.digest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
-import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class XXHash32Test {
 
-    private static long copy(final InputStream input, final OutputStream output, final int bufferSize) throws IOException {
-        return IOUtils.copyLarge(input, output, new byte[bufferSize]);
+    private static final String RESOURCE_NOT_FOUND = "Resource not found: ";
+
+    private Path getResourcePath(String resourcePath) throws Exception {
+        URL url = getClass().getClassLoader().getResource(resourcePath);
+        if (url == null) {
+            throw new FileNotFoundException(RESOURCE_NOT_FOUND + resourcePath);
+        }
+        return Paths.get(url.toURI());
     }
 
-    public static Stream<Arguments> data() {
+    static Stream<Arguments> data() {
         // @formatter:off
         return Stream.of(
             // reference checksums created with xxh32sum
@@ -52,54 +54,38 @@ class XXHash32Test {
         // @formatter:on
     }
 
-    private static byte[] toByteArray(final InputStream input) throws IOException {
-        final ByteArrayOutputStream output = new ByteArrayOutputStream();
-        copy(input, output, 10240);
-        return output.toByteArray();
-    }
-
-    private Path file;
-
-    private String expectedChecksum;
-
-    public void initData(final String path, final String c) throws Exception {
-        final URL url = XXHash32Test.class.getClassLoader().getResource(path);
-        if (url == null) {
-            throw new FileNotFoundException("couldn't find " + path);
-        }
-        file = Paths.get(url.toURI());
-        expectedChecksum = c;
+    @ParameterizedTest
+    @MethodSource("data")
+    @DisplayName("Verify single update hash")
+    void verifyChecksum(String resourcePath, String expectedChecksum) throws Exception {
+        Path file = getResourcePath(resourcePath);
+        byte[] bytes = Files.readAllBytes(file);
+        
+        XXHash32 hasher = new XXHash32();
+        hasher.update(bytes, 0, bytes.length);
+        
+        assertEquals(expectedChecksum, Long.toHexString(hasher.getValue()),
+            "Checksum mismatch for resource: " + resourcePath);
     }
 
     @ParameterizedTest
     @MethodSource("data")
-    public void verifyChecksum(final String path, final String c) throws Exception {
-        initData(path, c);
-        final XXHash32 hasher = new XXHash32();
-        try (InputStream in = Files.newInputStream(file)) {
-            final byte[] bytes = toByteArray(in);
-            hasher.update(bytes, 0, bytes.length);
-        }
-        assertEquals(expectedChecksum, Long.toHexString(hasher.getValue()), "checksum for " + file);
-    }
-
-    @ParameterizedTest
-    @MethodSource("data")
-    public void verifyIncrementalChecksum(final String path, final String c) throws Exception {
-        initData(path, c);
-        final XXHash32 hasher = new XXHash32();
-        try (InputStream in = Files.newInputStream(file)) {
-            final byte[] bytes = toByteArray(in);
-            // Hit the case where the hash should be reset
-            hasher.update(bytes[0]);
-            hasher.reset();
-            // Pass in chunks
-            hasher.update(bytes[0]);
-            hasher.update(bytes, 1, bytes.length - 2);
-            hasher.update(bytes, bytes.length - 1, 1);
-            // Check the hash ignores negative length
-            hasher.update(bytes, 0, -1);
-        }
-        assertEquals(expectedChecksum, Long.toHexString(hasher.getValue()), "checksum for " + file);
+    @DisplayName("Verify incremental hash updates")
+    void verifyIncrementalChecksum(String resourcePath, String expectedChecksum) throws Exception {
+        Path file = getResourcePath(resourcePath);
+        byte[] bytes = Files.readAllBytes(file);
+        
+        XXHash32 hasher = new XXHash32();
+        // Test reset and incremental updates
+        hasher.update(bytes[0]);
+        hasher.reset();
+        hasher.update(bytes[0]);
+        hasher.update(bytes, 1, bytes.length - 2);
+        hasher.update(bytes, bytes.length - 1, 1);
+        // Test handling of negative length (should be no-op)
+        hasher.update(bytes, 0, -1);
+        
+        assertEquals(expectedChecksum, Long.toHexString(hasher.getValue()),
+            "Incremental checksum mismatch for resource: " + resourcePath);
     }
 }
