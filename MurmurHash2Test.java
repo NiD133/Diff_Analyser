babab787ac68a3d4813e4d39db3a4fbe8c909989
NiD133 <k19052355@kcl.ac.uk>
2025-08-12 05:15:33 +0800

    gemini manualTestSuite MurmurHash2Test.java

--- MurmurHash2Test.java
+++ MurmurHash2Test.java
@@ -18,14 +18,37 @@
 package org.apache.commons.codec.digest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+/**
+ * Tests for {@link MurmurHash2}.
+ * The test vectors are from the original C++ implementation by Austin Appleby.
+ */
 class MurmurHash2Test {
 
-    /** Random input data with various length. */
-    static final byte[][] input = {
+    // region Constants
+    private static final int TEST_SEED_32 = 0x71b4954d;
+    private static final int TEST_SEED_64 = 0x344d1f5c;
+
+    /** Dummy test text. */
+    private static final String LOREM_IPSUM = "Lorem ipsum dolor sit amet, consectetur adipisicing elit";
+
+    private static final int EXPECTED_HASH32_LOREM_IPSUM = 0xb3bf597e;
+    private static final int EXPECTED_HASH32_LOREM_IPSUM_SUBSTRING = 0x4d666d90;
+    private static final long EXPECTED_HASH64_LOREM_IPSUM = 0x0920e0c1b7eeb261L;
+    private static final long EXPECTED_HASH64_LOREM_IPSUM_SUBSTRING = 0xa8b33145194985a2L;
+    // endregion
+
+    // region Test Vectors from the original C++ implementation
+
+    /** Random input data with various lengths. */
+    private static final byte[][] INPUT_DATA = {
             { (byte) 0xed, (byte) 0x53, (byte) 0xc4, (byte) 0xa5, (byte) 0x3b, (byte) 0x1b, (byte) 0xbd, (byte) 0xc2,
                     (byte) 0x52, (byte) 0x7d, (byte) 0xc3, (byte) 0xef, (byte) 0x53, (byte) 0x5f, (byte) 0xae,
                     (byte) 0x3b },
@@ -51,100 +74,104 @@ class MurmurHash2Test {
             { (byte) 0xeb, (byte) 0x25, (byte) 0x3f, (byte) 0x87 }, { (byte) 0x37, (byte) 0xa0, (byte) 0xa9 },
             { (byte) 0x5b, (byte) 0x5d }, { (byte) 0x7e }, {} };
 
-    /*
-     * Expected results - from the original C implementation.
-     */
-
-    /** Murmur 32bit hash results, default library seed. */
-    static final int[] results32_standard = { 0x96814fb3, 0x485dcaba, 0x331dc4ae, 0xc6a7bf2f, 0xcdf35de0, 0xd9dec7cc,
+    /** Expected 32-bit hash results with the default library seed. */
+    private static final int[] RESULTS_32_STANDARD = { 0x96814fb3, 0x485dcaba, 0x331dc4ae, 0xc6a7bf2f, 0xcdf35de0, 0xd9dec7cc,
             0x63a7318a, 0xd0d3c2de, 0x90923aef, 0xaf35c1e2, 0x735377b2, 0x366c98f3, 0x9c48ee29, 0x0b615790, 0xb4308ac1,
             0xec98125a, 0x106e08d9 };
 
-    /** Murmur 32bit hash results, special test seed. */
-    static final int[] results32_seed = { 0xd92e493e, 0x8b50903b, 0xc3372a7b, 0x48f07e9e, 0x8a5e4a6e, 0x57916df4,
+    /** Expected 32-bit hash results with a special test seed. */
+    private static final int[] RESULTS_32_SEED = { 0xd92e493e, 0x8b50903b, 0xc3372a7b, 0x48f07e9e, 0x8a5e4a6e, 0x57916df4,
             0xa346171f, 0x1e319c86, 0x9e1a03cd, 0x9f973e6c, 0x2d8c77f5, 0xabed8751, 0x296708b6, 0x24f8078b, 0x111b1553,
             0xa7da1996, 0xfe776c70 };
 
-    /** Murmur 64bit hash results, default library seed. */
-    static final long[] results64_standard = { 0x4987cb15118a83d9L, 0x28e2a79e3f0394d9L, 0x8f4600d786fc5c05L,
+    /** Expected 64-bit hash results with the default library seed. */
+    private static final long[] RESULTS_64_STANDARD = { 0x4987cb15118a83d9L, 0x28e2a79e3f0394d9L, 0x8f4600d786fc5c05L,
             0xa09b27fea4b54af3L, 0x25f34447525bfd1eL, 0x32fad4c21379c7bfL, 0x4b30b99a9d931921L, 0x4e5dab004f936cdbL,
             0x06825c27bc96cf40L, 0xff4bf2f8a4823905L, 0x7f7e950c064e6367L, 0x821ade90caaa5889L, 0x6d28c915d791686aL,
             0x9c32649372163ba2L, 0xd66ae956c14d5212L, 0x38ed30ee5161200fL, 0x9bfae0a4e613fc3cL, };
 
-    /** Murmur 64bit hash results, special test seed. */
-    static final long[] results64_seed = { 0x0822b1481a92e97bL, 0xf8a9223fef0822ddL, 0x4b49e56affae3a89L,
+    /** Expected 64-bit hash results with a special test seed. */
+    private static final long[] RESULTS_64_SEED = { 0x0822b1481a92e97bL, 0xf8a9223fef0822ddL, 0x4b49e56affae3a89L,
             0xc970296e32e1d1c1L, 0xe2f9f88789f1b08fL, 0x2b0459d9b4c10c61L, 0x377e97ea9197ee89L, 0xd2ccad460751e0e7L,
             0xff162ca8d6da8c47L, 0xf12e051405769857L, 0xdabba41293d5b035L, 0xacf326b0bb690d0eL, 0x0617f431bc1a8e04L,
             0x15b81f28d576e1b2L, 0x28c1fe59e4f8e5baL, 0x694dd315c9354ca9L, 0xa97052a8f088ae6cL };
+    // endregion
 
-    /** Dummy test text. */
-    static final String text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit";
+    // region Parameterized Test Data Providers
+    static Stream<Arguments> provide32BitStandardTestCases() {
+        return IntStream.range(0, INPUT_DATA.length)
+            .mapToObj(i -> Arguments.of(INPUT_DATA[i], RESULTS_32_STANDARD[i]));
+    }
 
+    static Stream<Arguments> provide32BitSeedTestCases() {
+        return IntStream.range(0, INPUT_DATA.length)
+            .mapToObj(i -> Arguments.of(INPUT_DATA[i], RESULTS_32_SEED[i]));
+    }
+
+    static Stream<Arguments> provide64BitStandardTestCases() {
+        return IntStream.range(0, INPUT_DATA.length)
+            .mapToObj(i -> Arguments.of(INPUT_DATA[i], RESULTS_64_STANDARD[i]));
+    }
+
+    static Stream<Arguments> provide64BitSeedTestCases() {
+        return IntStream.range(0, INPUT_DATA.length)
+            .mapToObj(i -> Arguments.of(INPUT_DATA[i], RESULTS_64_SEED[i]));
+    }
+    // endregion
+
+    // region Parameterized Tests for Byte Arrays
+    @ParameterizedTest(name = "Test case {index}")
+    @MethodSource("provide32BitStandardTestCases")
+    void hash32WithDefaultSeedShouldProduceKnownValues(final byte[] data, final int expectedHash) {
+        final int actualHash = MurmurHash2.hash32(data, data.length);
+        assertEquals(expectedHash, actualHash);
+    }
+
+    @ParameterizedTest(name = "Test case {index}")
+    @MethodSource("provide32BitSeedTestCases")
+    void hash32WithCustomSeedShouldProduceKnownValues(final byte[] data, final int expectedHash) {
+        final int actualHash = MurmurHash2.hash32(data, data.length, TEST_SEED_32);
+        assertEquals(expectedHash, actualHash);
+    }
+
+    @ParameterizedTest(name = "Test case {index}")
+    @MethodSource("provide64BitStandardTestCases")
+    void hash64WithDefaultSeedShouldProduceKnownValues(final byte[] data, final long expectedHash) {
+        final long actualHash = MurmurHash2.hash64(data, data.length);
+        assertEquals(expectedHash, actualHash);
+    }
+
+    @ParameterizedTest(name = "Test case {index}")
+    @MethodSource("provide64BitSeedTestCases")
+    void hash64WithCustomSeedShouldProduceKnownValues(final byte[] data, final long expectedHash) {
+        final long actualHash = MurmurHash2.hash64(data, data.length, TEST_SEED_64);
+        assertEquals(expectedHash, actualHash);
+    }
+    // endregion
+
+    // region Tests for Strings
     @Test
-    void testHash32ByteArrayInt() {
-        for (int i = 0; i < input.length; i++) {
-            final int hash = MurmurHash2.hash32(input[i], input[i].length);
-            if (hash != results32_standard[i]) {
-                fail(String.format("Unexpected hash32 result for example %d: 0x%08x instead of 0x%08x", i, hash,
-                            results32_standard[i]));
-            }
-        }
+    void hash32ShouldProduceKnownValueForFullString() {
+        final int hash = MurmurHash2.hash32(LOREM_IPSUM);
+        assertEquals(EXPECTED_HASH32_LOREM_IPSUM, hash);
     }
 
     @Test
-    void testHash32ByteArrayIntInt() {
-        for (int i = 0; i < input.length; i++) {
-            final int hash = MurmurHash2.hash32(input[i], input[i].length, 0x71b4954d);
-            if (hash != results32_seed[i]) {
-                fail(String.format("Unexpected hash32 result for example %d: 0x%08x instead of 0x%08x", i, hash,
-                            results32_seed[i]));
-            }
-        }
+    void hash32ShouldProduceKnownValueForSubstring() {
+        final int hash = MurmurHash2.hash32(LOREM_IPSUM, 2, LOREM_IPSUM.length() - 4);
+        assertEquals(EXPECTED_HASH32_LOREM_IPSUM_SUBSTRING, hash);
     }
 
     @Test
-    void testHash32String() {
-        final int hash = MurmurHash2.hash32(text);
-        assertEquals(0xb3bf597e, hash);
+    void hash64ShouldProduceKnownValueForFullString() {
+        final long hash = MurmurHash2.hash64(LOREM_IPSUM);
+        assertEquals(EXPECTED_HASH64_LOREM_IPSUM, hash);
     }
 
     @Test
-    void testHash32StringIntInt() {
-        final int hash = MurmurHash2.hash32(text, 2, text.length() - 4);
-        assertEquals(0x4d666d90, hash);
+    void hash64ShouldProduceKnownValueForSubstring() {
+        final long hash = MurmurHash2.hash64(LOREM_IPSUM, 2, LOREM_IPSUM.length() - 4);
+        assertEquals(EXPECTED_HASH64_LOREM_IPSUM_SUBSTRING, hash);
     }
-
-    @Test
-    void testHash64ByteArrayInt() {
-        for (int i = 0; i < input.length; i++) {
-            final long hash = MurmurHash2.hash64(input[i], input[i].length);
-            if (hash != results64_standard[i]) {
-                fail(String.format("Unexpected hash64 result for example %d: 0x%016x instead of 0x%016x", i, hash,
-                    results64_standard[i]));
-            }
-        }
-    }
-
-    @Test
-    void testHash64ByteArrayIntInt() {
-        for (int i = 0; i < input.length; i++) {
-            final long hash = MurmurHash2.hash64(input[i], input[i].length, 0x344d1f5c);
-            if (hash != results64_seed[i]) {
-                fail(String.format("Unexpected hash64 result for example %d: 0x%016x instead of 0x%016x", i, hash,
-                            results64_seed[i]));
-            }
-        }
-    }
-
-    @Test
-    void testHash64String() {
-        final long hash = MurmurHash2.hash64(text);
-        assertEquals(0x0920e0c1b7eeb261L, hash);
-    }
-
-    @Test
-    void testHash64StringIntInt() {
-        final long hash = MurmurHash2.hash64(text, 2, text.length() - 4);
-        assertEquals(0xa8b33145194985a2L, hash);
-    }
+    // endregion
 }
