babee4cff809f0388756ee6d69333fbc5d9314c5
NiD133 <k19052355@kcl.ac.uk>
2025-08-12 05:16:30 +0800

    Original manualTestSuite WKTWriterTest.java

--- WKTWriterTest.java
+++ WKTWriterTest.java
@@ -1,63 +1,37 @@
 package org.locationtech.spatial4j.io;
 
 import static org.junit.Assert.assertEquals;
-
 import java.util.ArrayList;
-
-import org.junit.Before;
 import org.junit.Test;
 import org.locationtech.spatial4j.context.SpatialContext;
 import org.locationtech.spatial4j.shape.Point;
 import org.locationtech.spatial4j.shape.ShapeCollection;
 
-/**
- * Verifies how WKTWriter serializes empty shapes to WKT.
- * - A point with NaN coordinates is considered empty and should serialize as "POINT EMPTY".
- * - An empty shape collection should serialize as "GEOMETRYCOLLECTION EMPTY".
- */
 public class WKTWriterTest {
 
   private SpatialContext ctx;
-  private ShapeWriter wktWriter;
 
-  @Before
-  public void setUp() {
-    // Use the default GEO context for consistent WKT formatting across tests
-    ctx = SpatialContext.GEO;
-    wktWriter = ctx.getFormats().getWktWriter();
+  protected WKTWriterTest(SpatialContext ctx) {
+    this.ctx = ctx;
   }
 
-  private Point newEmptyPoint() {
-    // In Spatial4j, a Point with NaN coordinates is considered "empty"
-    return ctx.makePoint(Double.NaN, Double.NaN);
-  }
-
-  private ShapeCollection<Point> newEmptyGeometryCollection() {
-    // An empty collection represents GEOMETRYCOLLECTION EMPTY in WKT
-    return ctx.makeCollection(new ArrayList<Point>());
+  public WKTWriterTest() {
+    this(SpatialContext.GEO);
   }
 
   @Test
-  public void emptyPoint_isSerializedAs_POINT_EMPTY() {
-    // given
-    Point emptyPoint = newEmptyPoint();
+  public void testToStringOnEmptyPoint() throws Exception {
+    ShapeWriter writer = ctx.getFormats().getWktWriter();
+    Point emptyPoint = ctx.makePoint(Double.NaN, Double.NaN);
 
-    // when
-    String wkt = wktWriter.toString(emptyPoint);
-
-    // then
-    assertEquals("POINT EMPTY", wkt);
+    assertEquals("POINT EMPTY", writer.toString(emptyPoint));
   }
 
   @Test
-  public void emptyGeometryCollection_isSerializedAs_GEOMETRYCOLLECTION_EMPTY() {
-    // given
-    ShapeCollection<Point> emptyCollection = newEmptyGeometryCollection();
+  public void testToStringOnEmptyShapeCollection() throws Exception {
+    ShapeWriter writer = ctx.getFormats().getWktWriter();
+    ShapeCollection<Point> emptyCollection = ctx.makeCollection(new ArrayList<>());
 
-    // when
-    String wkt = wktWriter.toString(emptyCollection);
-
-    // then
-    assertEquals("GEOMETRYCOLLECTION EMPTY", wkt);
+    assertEquals("GEOMETRYCOLLECTION EMPTY", writer.toString(emptyCollection));
   }
 }
