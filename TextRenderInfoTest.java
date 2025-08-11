5d599cf7994840433e4be9995d5c86d5ab1bb320
NiD133 <k19052355@kcl.ac.uk>
2025-08-12 05:16:16 +0800

    deepseek manualTestSuite TextRenderInfoTest.java

--- TextRenderInfoTest.java
+++ TextRenderInfoTest.java
@@ -60,147 +60,117 @@ import com.itextpdf.text.pdf.PdfReader;
 import com.itextpdf.text.pdf.PdfWriter;
 
 public class TextRenderInfoTest {
-
-    public static final int FIRST_PAGE = 1;
-    public static final int FIRST_ELEMENT_INDEX = 0;
-
-	@Test
-	public void testCharacterRenderInfos() throws Exception {
-        byte[] bytes = createSimplePdf(PageSize.LETTER.rotate().rotate(), "ABCD");
-        //TestResourceUtils.saveBytesToFile(bytes, new File("C:/temp/out.pdf"));
-
-        PdfReader r = new PdfReader(bytes);
-
-        PdfReaderContentParser parser = new PdfReaderContentParser(r);
-        parser.processContent(FIRST_PAGE, new CharacterPositionRenderListener());
-
-	}
-
-        /**
-         * Test introduced to exclude a bug related to a Unicode quirk for
-         * Japanese. TextRenderInfo threw an AIOOBE for some characters.
-         * @throws java.lang.Exception
-         * @since 5.5.5-SNAPSHOT
-         */
-        @Test
-        public void testUnicodeEmptyString() throws Exception {
-            StringBuilder sb = new StringBuilder();
-            String inFile = "japanese_text.pdf";
-
-
-            PdfReader p = TestResourceUtils.getResourceAsPdfReader(this, inFile);
-            TextExtractionStrategy strat = new SimpleTextExtractionStrategy();
-
-            sb.append(PdfTextExtractor.getTextFromPage(p, FIRST_PAGE, strat));
-
-            String result = sb.substring(0, sb.indexOf("\n"));
-            String origText =
-                    "\u76f4\u8fd1\u306e\u0053\uff06\u0050\u0035\u0030\u0030"
-                    + "\u914d\u5f53\u8cb4\u65cf\u6307\u6570\u306e\u30d1\u30d5"
-                    + "\u30a9\u30fc\u30de\u30f3\u30b9\u306f\u0053\uff06\u0050"
-                    + "\u0035\u0030\u0030\u6307\u6570\u3092\u4e0a\u56de\u308b";
-            Assert.assertEquals(result, origText);
-        }
+    private static final int FIRST_PAGE = 1;
+    private static final int FIRST_ELEMENT_INDEX = 0;
+    private static final float FLOAT_TOLERANCE = 1 / 72f; // Tolerance for floating point comparisons
 
     @Test
-    public void testType3FontWidth() throws Exception {
-        String inFile = "type3font_text.pdf";
-        LineSegment origLineSegment = new LineSegment(new Vector(20.3246f, 769.4974f, 1.0f), new Vector(151.22923f, 769.4974f, 1.0f));
+    public void consecutiveCharacters_ShouldHaveMatchingEndAndStartPoints() throws Exception {
+        String testContent = "ABCD";
+        byte[] pdfBytes = createSimplePdf(PageSize.LETTER.rotate().rotate(), testContent);
 
-        PdfReader reader = TestResourceUtils.getResourceAsPdfReader(this, inFile);
-        TextPositionRenderListener renderListener = new TextPositionRenderListener();
-        PdfContentStreamProcessor processor = new PdfContentStreamProcessor(renderListener);
-
-        PdfDictionary pageDic = reader.getPageN(FIRST_PAGE);
-        PdfDictionary resourcesDic = pageDic.getAsDict(PdfName.RESOURCES);
-        processor.processContent(ContentByteUtils.getContentBytesForPage(reader, FIRST_PAGE), resourcesDic);
-
-
-        Assert.assertEquals(renderListener.getLineSegments().get(FIRST_ELEMENT_INDEX).getStartPoint().get(FIRST_ELEMENT_INDEX),
-                origLineSegment.getStartPoint().get(FIRST_ELEMENT_INDEX), 1 / 2f);
-
-        Assert.assertEquals(renderListener.getLineSegments().get(FIRST_ELEMENT_INDEX).getEndPoint().get(FIRST_ELEMENT_INDEX),
-                origLineSegment.getEndPoint().get(FIRST_ELEMENT_INDEX), 1 / 2f);
-
+        PdfReader pdfReader = new PdfReader(pdfBytes);
+        PdfReaderContentParser parser = new PdfReaderContentParser(pdfReader);
+        
+        parser.processContent(FIRST_PAGE, new CharacterPositionValidator());
     }
 
+    @Test
+    public void extractJapaneseText_ShouldMatchExpected() throws Exception {
+        String japanesePdf = "japanese_text.pdf";
+        String expectedFirstLine = 
+            "\u76f4\u8fd1\u306e\u0053\uff06\u0050\u0035\u0030\u0030" +
+            "\u914d\u5f53\u8cb4\u65cf\u6307\u6570\u306e\u30d1\u30d5" +
+            "\u30a9\u30fc\u30de\u30f3\u30b9\u306f\u0053\uff06\u0050" +
+            "\u0035\u0030\u0030\u6307\u6570\u3092\u4e0a\u56de\u308b";
 
-    private static class TextPositionRenderListener implements RenderListener {
+        PdfReader reader = TestResourceUtils.getResourceAsPdfReader(this, japanesePdf);
+        String extractedText = PdfTextExtractor.getTextFromPage(reader, FIRST_PAGE, new SimpleTextExtractionStrategy());
+        
+        String actualFirstLine = extractedText.substring(0, extractedText.indexOf("\n"));
+        Assert.assertEquals("Extracted Japanese text should match expected", expectedFirstLine, actualFirstLine);
+    }
 
-        List<LineSegment> lineSegments = new ArrayList<LineSegment>();
+    @Test
+    public void type3FontRendering_ShouldHaveCorrectBaselinePositions() throws Exception {
+        String testPdf = "type3font_text.pdf";
+        float expectedStartX = 20.3246f;
+        float expectedEndX = 151.22923f;
+        float tolerance = 0.5f; // 1/2 point tolerance
+
+        PdfReader reader = TestResourceUtils.getResourceAsPdfReader(this, testPdf);
+        TextPositionRecorder positionRecorder = new TextPositionRecorder();
+        PdfContentStreamProcessor processor = new PdfContentStreamProcessor(positionRecorder);
+
+        PdfDictionary pageDict = reader.getPageN(FIRST_PAGE);
+        PdfDictionary resources = pageDict.getAsDict(PdfName.RESOURCES);
+        processor.processContent(ContentByteUtils.getContentBytesForPage(reader, FIRST_PAGE), resources);
+
+        LineSegment firstBaseline = positionRecorder.getLineSegments().get(FIRST_ELEMENT_INDEX);
+        Assert.assertEquals("Baseline start X", expectedStartX, firstBaseline.getStartPoint().get(0), tolerance);
+        Assert.assertEquals("Baseline end X", expectedEndX, firstBaseline.getEndPoint().get(0), tolerance);
+    }
+
+    // Helper class to validate character positioning
+    private static class CharacterPositionValidator implements TextExtractionStrategy {
+        @Override
+        public void renderText(TextRenderInfo renderInfo) {
+            List<TextRenderInfo> characters = renderInfo.getCharacterRenderInfos();
+            TextRenderInfo previousChar = characters.get(0);
+
+            for (int i = 1; i < characters.size(); i++) {
+                TextRenderInfo currentChar = characters.get(i);
+                Vector prevEnd = previousChar.getBaseline().getEndPoint();
+                Vector currentStart = currentChar.getBaseline().getStartPoint();
+                
+                assertPointsMatch(currentChar.getText(), prevEnd, currentStart);
+                previousChar = currentChar;
+            }
+        }
+
+        private void assertPointsMatch(String context, Vector v1, Vector v2) {
+            Assert.assertEquals(context + ": X coordinate mismatch", v1.get(0), v2.get(0), FLOAT_TOLERANCE);
+            Assert.assertEquals(context + ": Y coordinate mismatch", v1.get(1), v2.get(1), FLOAT_TOLERANCE);
+        }
+
+        // Required interface methods - no action needed for this test
+        @Override public void beginTextBlock() {}
+        @Override public void endTextBlock() {}
+        @Override public void renderImage(ImageRenderInfo renderInfo) {}
+        @Override public String getResultantText() { return null; }
+    }
+
+    // Helper class to record text positions
+    private static class TextPositionRecorder implements RenderListener {
+        private final List<LineSegment> lineSegments = new ArrayList<>();
 
         public List<LineSegment> getLineSegments() {
             return lineSegments;
         }
 
+        @Override
         public void renderText(TextRenderInfo renderInfo) {
             lineSegments.add(renderInfo.getBaseline());
         }
 
-        public void beginTextBlock() {
-        }
-
-        public void endTextBlock() {
-
-        }
-
-        public void renderImage(ImageRenderInfo renderInfo) {
-
-        }
-
-
+        // Required interface methods - no action needed for this test
+        @Override public void beginTextBlock() {}
+        @Override public void endTextBlock() {}
+        @Override public void renderImage(ImageRenderInfo renderInfo) {}
     }
 
-	private static class CharacterPositionRenderListener implements TextExtractionStrategy{
-
-		public void beginTextBlock() {
-		}
-
-		public void renderText(TextRenderInfo renderInfo) {
-			List<TextRenderInfo> subs = renderInfo.getCharacterRenderInfos();
-			TextRenderInfo previousCharInfo = subs.get(0);
-
-			for(int i = 1; i < subs.size(); i++){
-				TextRenderInfo charInfo = subs.get(i);
-				Vector previousEndPoint = previousCharInfo.getBaseline().getEndPoint();
-				Vector currentStartPoint = charInfo.getBaseline().getStartPoint();
-				assertVectorsEqual(charInfo.getText(), previousEndPoint, currentStartPoint);
-				previousCharInfo = charInfo;
-			}
-
-		}
-
-		private void assertVectorsEqual(String message, Vector v1, Vector v2){
-			Assert.assertEquals(message, v1.get(0), v2.get(0), 1/72f);
-			Assert.assertEquals(message, v1.get(1), v2.get(1), 1/72f);
-		}
-		public void endTextBlock() {
-		}
-
-		public void renderImage(ImageRenderInfo renderInfo) {
-		}
-
-		public String getResultantText() {
-			return null;
-		}
-
-	}
-
-	private byte[] createSimplePdf(Rectangle pageSize, final String... text) throws Exception{
-        final ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
-
-        final Document document = new Document(pageSize);
+    private byte[] createSimplePdf(Rectangle pageSize, String... content) throws Exception {
+        ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+        Document document = new Document(pageSize);
         PdfWriter.getInstance(document, byteStream);
+        
         document.open();
-        for (String string : text) {
-            document.add(new Paragraph(string));
+        for (String text : content) {
+            document.add(new Paragraph(text));
             document.newPage();
         }
-
         document.close();
-
-        final byte[] pdfBytes = byteStream.toByteArray();
-
-        return pdfBytes;
-}
+        
+        return byteStream.toByteArray();
+    }
 }
