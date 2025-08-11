614d20e93d0d8b8faea9471564e836139165a8eb
NiD133 <k19052355@kcl.ac.uk>
2025-08-12 05:15:42 +0800

    Original manualTestSuite PdfDictionaryTest.java

--- PdfDictionaryTest.java
+++ PdfDictionaryTest.java
@@ -1,87 +1,97 @@
+/*
+    This file is part of the iText (R) project.
+    Copyright (c) 1998-2022 iText Group NV
+    Authors: iText Software.
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License version 3
+    as published by the Free Software Foundation with the addition of the
+    following permission added to Section 15 as permitted in Section 7(a):
+    FOR ANY PART OF THE COVERED WORK IN WHICH THE COPYRIGHT IS OWNED BY
+    ITEXT GROUP. ITEXT GROUP DISCLAIMS THE WARRANTY OF NON INFRINGEMENT
+    OF THIRD PARTY RIGHTS
+    
+    This program is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+    or FITNESS FOR A PARTICULAR PURPOSE.
+    See the GNU Affero General Public License for more details.
+    You should have received a copy of the GNU Affero General Public License
+    along with this program; if not, see http://www.gnu.org/licenses or write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA, 02110-1301 USA, or download the license from the following URL:
+    http://itextpdf.com/terms-of-use/
+    
+    The interactive user interfaces in modified source and object code versions
+    of this program must display Appropriate Legal Notices, as required under
+    Section 5 of the GNU Affero General Public License.
+    
+    In accordance with Section 7(b) of the GNU Affero General Public License,
+    a covered work must retain the producer line in every PDF that is created
+    or manipulated using iText.
+    
+    You can be released from the requirements of the license by purchasing
+    a commercial license. Buying such a license is mandatory as soon as you
+    develop commercial activities involving the iText software without
+    disclosing the source code of your own applications.
+    These activities include: offering paid services to customers as an ASP,
+    serving PDFs on the fly in a web application, shipping iText with a closed
+    source product.
+    
+    For more information, please contact iText Software Corp. at this
+    address: sales@itextpdf.com
+ */
 package com.itextpdf.text.pdf;
 
-import org.junit.Rule;
+import org.junit.Assert;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
-import static org.junit.Assert.*;
-
-/**
- * Focused tests for PdfDictionary behavior when a null key is passed.
- * 
- * These tests use clear Arrange-Act-Assert sections and explicit assertions/messages
- * to make intent and expected outcomes obvious.
- */
-public class PdfDictionaryNullKeyBehaviorTest {
-
-    @Rule
-    public ExpectedException thrown = ExpectedException.none();
-
-    private static final PdfName SAMPLE_VALUE = new PdfName("sample");
-
+public class PdfDictionaryTest {
     @Test
-    public void getReturnsNullWhenKeyIsNull() {
-        // Arrange
+    public void pdfDictionaryGetReturnsNullIfKeyIsNull() {
         PdfDictionary dictionary = new PdfDictionary();
 
-        // Act
         PdfObject value = dictionary.get(null);
 
-        // Assert
-        assertNull("get(null) must return null", value);
+        Assert.assertNull(value);
     }
 
     @Test
-    public void containsReturnsFalseWhenKeyIsNull() {
-        // Arrange
+    public void pdfDictionaryContainsReturnsFalseIfKeyIsNull() {
         PdfDictionary dictionary = new PdfDictionary();
 
-        // Act
-        boolean contains = dictionary.contains(null);
+        boolean contained = dictionary.contains(null);
 
-        // Assert
-        assertFalse("contains(null) must be false", contains);
+        Assert.assertFalse(contained);
     }
 
     @Test
-    public void removeWithNullKeyDoesNotChangeDictionaryState() {
-        // Arrange
+    public void pdfDictionaryRemoveDoesNothingIfKeyIsNull() {
         PdfDictionary dictionary = new PdfDictionary();
-        PdfName existingKey = new PdfName("ExistingKey");
-        dictionary.put(existingKey, SAMPLE_VALUE); // add a valid mapping
-        int sizeBefore = dictionary.size();
 
-        // Act
         dictionary.remove(null);
-
-        // Assert
-        assertEquals("remove(null) must not change size", sizeBefore, dictionary.size());
-        assertTrue("Existing mapping must not be affected by remove(null)", dictionary.contains(existingKey));
     }
 
     @Test
-    public void putThrowsIAEWhenKeyIsNull() {
-        // Arrange
+    public void pdfDictionaryPutThrowsExceptionIfKeyIsNull() {
         PdfDictionary dictionary = new PdfDictionary();
 
-        // Assert
-        thrown.expect(IllegalArgumentException.class);
-        thrown.expectMessage("key is null.");
-
-        // Act
-        dictionary.put(null, SAMPLE_VALUE);
+        try {
+            dictionary.put(null, new PdfName("null"));
+            Assert.fail("IllegalArgumentException expected");
+        } catch (IllegalArgumentException e) {
+            Assert.assertEquals(e.getMessage(), "key is null.");
+        }
     }
 
     @Test
-    public void putExThrowsIAEWhenKeyIsNull() {
-        // Arrange
+    public void pdfDictionaryPutExThrowsExceptionIfKeyIsNull() {
         PdfDictionary dictionary = new PdfDictionary();
 
-        // Assert
-        thrown.expect(IllegalArgumentException.class);
-        thrown.expectMessage("key is null.");
-
-        // Act
-        dictionary.putEx(null, SAMPLE_VALUE);
+        try {
+            dictionary.putEx(null, new PdfName("null"));
+            Assert.fail("IllegalArgumentException expected");
+        } catch (IllegalArgumentException e) {
+            Assert.assertEquals(e.getMessage(), "key is null.");
+        }
     }
 }
