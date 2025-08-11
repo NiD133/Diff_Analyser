fc14f2f131cb6571cb219c809226f0577b44da0e
NiD133 <k19052355@kcl.ac.uk>
2025-08-12 03:09:38 +0800

    Original manualTestSuite QuarterDateFormatTest.java

--- QuarterDateFormatTest.java
+++ QuarterDateFormatTest.java
@@ -1,3 +1,39 @@
+/* ======================================================
+ * JFreeChart : a chart library for the Java(tm) platform
+ * ======================================================
+ *
+ * (C) Copyright 2000-present, by David Gilbert and Contributors.
+ *
+ * Project Info:  https://www.jfree.org/jfreechart/index.html
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ *
+ * [Oracle and Java are registered trademarks of Oracle and/or its affiliates. 
+ * Other names may be trademarks of their respective owners.]
+ *
+ * --------------------------
+ * QuarterDateFormatTest.java
+ * --------------------------
+ * (C) Copyright 2005-present, by David Gilbert and Contributors.
+ *
+ * Original Author:  David Gilbert;
+ * Contributor(s):   -;
+ *
+ */
+
 package org.jfree.chart.axis;
 
 import java.util.TimeZone;
@@ -8,84 +44,82 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * Unit tests for the {@link QuarterDateFormat} class.
+ * Tests for the {@link QuarterDateFormat} class.
  */
 public class QuarterDateFormatTest {
 
     /**
-     * Tests that the equals method correctly identifies equal and unequal instances.
+     * Confirm that the equals method can distinguish all the required fields.
      */
     @Test
-    public void testEqualsMethod() {
-        // Test equality with identical time zone and quarter symbols
-        QuarterDateFormat qf1 = createQuarterDateFormat("GMT", "1", "2", "3", "4");
-        QuarterDateFormat qf2 = createQuarterDateFormat("GMT", "1", "2", "3", "4");
-        assertEquals(qf1, qf2, "Instances with the same time zone and symbols should be equal");
+    public void testEquals() {
+        QuarterDateFormat qf1 = new QuarterDateFormat(TimeZone.getTimeZone(
+                "GMT"), new String[] {"1", "2", "3", "4"});
+        QuarterDateFormat qf2 = new QuarterDateFormat(TimeZone.getTimeZone(
+                "GMT"), new String[] {"1", "2", "3", "4"});
+        assertEquals(qf1, qf2);
+        assertEquals(qf2, qf1);
 
-        // Test inequality with different time zones
-        qf1 = createQuarterDateFormat("PST", "1", "2", "3", "4");
-        assertNotEquals(qf1, qf2, "Instances with different time zones should not be equal");
+        qf1 = new QuarterDateFormat(TimeZone.getTimeZone("PST"),
+                new String[] {"1", "2", "3", "4"});
+        assertNotEquals(qf1, qf2);
+        qf2 = new QuarterDateFormat(TimeZone.getTimeZone("PST"),
+                new String[] {"1", "2", "3", "4"});
+        assertEquals(qf1, qf2);
 
-        // Test equality with identical time zone and quarter symbols
-        qf2 = createQuarterDateFormat("PST", "1", "2", "3", "4");
-        assertEquals(qf1, qf2, "Instances with the same time zone and symbols should be equal");
+        qf1 = new QuarterDateFormat(TimeZone.getTimeZone("PST"),
+                new String[] {"A", "2", "3", "4"});
+        assertNotEquals(qf1, qf2);
+        qf2 = new QuarterDateFormat(TimeZone.getTimeZone("PST"),
+                new String[] {"A", "2", "3", "4"});
+        assertEquals(qf1, qf2);
 
-        // Test inequality with different quarter symbols
-        qf1 = createQuarterDateFormat("PST", "A", "2", "3", "4");
-        assertNotEquals(qf1, qf2, "Instances with different quarter symbols should not be equal");
-
-        // Test equality with identical time zone and quarter symbols
-        qf2 = createQuarterDateFormat("PST", "A", "2", "3", "4");
-        assertEquals(qf1, qf2, "Instances with the same time zone and symbols should be equal");
-
-        // Test inequality with different quarterFirst flag
-        qf1 = new QuarterDateFormat(TimeZone.getTimeZone("PST"), new String[]{"A", "2", "3", "4"}, true);
-        assertNotEquals(qf1, qf2, "Instances with different quarterFirst flags should not be equal");
-
-        // Test equality with identical time zone, quarter symbols, and quarterFirst flag
-        qf2 = new QuarterDateFormat(TimeZone.getTimeZone("PST"), new String[]{"A", "2", "3", "4"}, true);
-        assertEquals(qf1, qf2, "Instances with the same time zone, symbols, and quarterFirst flag should be equal");
+        qf1 = new QuarterDateFormat(TimeZone.getTimeZone("PST"),
+                new String[] {"A", "2", "3", "4"}, true);
+        assertNotEquals(qf1, qf2);
+        qf2 = new QuarterDateFormat(TimeZone.getTimeZone("PST"),
+                new String[] {"A", "2", "3", "4"}, true);
+        assertEquals(qf1, qf2);
     }
 
     /**
-     * Tests that equal objects have the same hash code.
+     * Two objects that are equal are required to return the same hashCode.
      */
     @Test
-    public void testHashCodeConsistency() {
-        QuarterDateFormat qf1 = createQuarterDateFormat("GMT", "1", "2", "3", "4");
-        QuarterDateFormat qf2 = createQuarterDateFormat("GMT", "1", "2", "3", "4");
-        assertEquals(qf1.hashCode(), qf2.hashCode(), "Equal instances should have the same hash code");
+    public void testHashCode() {
+        QuarterDateFormat qf1 = new QuarterDateFormat(TimeZone.getTimeZone(
+                "GMT"), new String[] {"1", "2", "3", "4"});
+        QuarterDateFormat qf2 = new QuarterDateFormat(TimeZone.getTimeZone(
+                "GMT"), new String[] {"1", "2", "3", "4"});
+        assertEquals(qf1, qf2);
+        int h1 = qf1.hashCode();
+        int h2 = qf2.hashCode();
+        assertEquals(h1, h2);
     }
 
     /**
-     * Tests that cloning creates a distinct but equal object.
+     * Confirm that cloning works.
      */
     @Test
-    public void testCloningFunctionality() {
-        QuarterDateFormat original = createQuarterDateFormat("GMT", "1", "2", "3", "4");
-        QuarterDateFormat clone = (QuarterDateFormat) original.clone();
-        assertNotSame(original, clone, "Clone should be a different instance");
-        assertEquals(original, clone, "Clone should be equal to the original");
+    public void testCloning() {
+        QuarterDateFormat qf1 = new QuarterDateFormat(TimeZone.getTimeZone(
+                "GMT"), new String[] {"1", "2", "3", "4"});
+        QuarterDateFormat qf2 = null;
+        qf2 = (QuarterDateFormat) qf1.clone();
+        assertNotSame(qf1, qf2);
+        assertSame(qf1.getClass(), qf2.getClass());
+        assertEquals(qf1, qf2);
     }
 
     /**
-     * Tests that serialization and deserialization preserve object equality.
+     * Serialize an instance, restore it, and check for equality.
      */
     @Test
-    public void testSerializationIntegrity() {
-        QuarterDateFormat original = createQuarterDateFormat("GMT", "1", "2", "3", "4");
-        QuarterDateFormat deserialized = TestUtils.serialised(original);
-        assertEquals(original, deserialized, "Deserialized instance should be equal to the original");
+    public void testSerialization() {
+        QuarterDateFormat qf1 = new QuarterDateFormat(TimeZone.getTimeZone(
+                "GMT"), new String[] {"1", "2", "3", "4"});
+        QuarterDateFormat qf2 = TestUtils.serialised(qf1);
+        assertEquals(qf1, qf2);
     }
 
-    /**
-     * Helper method to create a QuarterDateFormat instance with specified time zone and quarter symbols.
-     *
-     * @param timeZoneId the ID of the time zone
-     * @param quarters the quarter symbols
-     * @return a new QuarterDateFormat instance
-     */
-    private QuarterDateFormat createQuarterDateFormat(String timeZoneId, String... quarters) {
-        return new QuarterDateFormat(TimeZone.getTimeZone(timeZoneId), quarters);
-    }
 }
