c976be1ee61eaeb32da77fe2b8ed58b994a53f33
NiD133 <k19052355@kcl.ac.uk>
2025-08-12 05:15:47 +0800

    Original manualTestSuite PredicatedMapTest.java

--- PredicatedMapTest.java
+++ PredicatedMapTest.java
@@ -31,29 +31,21 @@ import org.apache.commons.collections4.functors.TruePredicate;
 import org.junit.jupiter.api.Test;
 
 /**
- * Test suite for {@link PredicatedMap} implementation.
- * 
- * PredicatedMap validates that keys and values match specified predicates
- * before allowing them to be added to the underlying map.
+ * Extension of {@link AbstractMapTest} for exercising the
+ * {@link PredicatedMap} implementation.
  *
  * @param <K> the key type.
  * @param <V> the value type.
  */
 public class PredicatedMapTest<K, V> extends AbstractIterableMapTest<K, V> {
 
-    /** Predicate that accepts all objects - used for unrestricted maps */
-    protected static final Predicate<Object> ACCEPT_ALL_PREDICATE = TruePredicate.<Object>truePredicate();
+    protected static final Predicate<Object> truePredicate = TruePredicate.<Object>truePredicate();
 
-    /** Predicate that only accepts String objects - used for testing validation */
-    protected static final Predicate<Object> STRING_ONLY_PREDICATE = String.class::isInstance;
+    protected static final Predicate<Object> testPredicate = String.class::isInstance;
 
-    /**
-     * Creates a PredicatedMap with the specified predicates for keys and values.
-     */
-    protected IterableMap<K, V> createPredicatedMap(final Map<K, V> baseMap, 
-                                                   final Predicate<? super K> keyPredicate,
-                                                   final Predicate<? super V> valuePredicate) {
-        return PredicatedMap.predicatedMap(baseMap, keyPredicate, valuePredicate);
+    protected IterableMap<K, V> decorateMap(final Map<K, V> map, final Predicate<? super K> keyPredicate,
+        final Predicate<? super V> valuePredicate) {
+        return PredicatedMap.predicatedMap(map, keyPredicate, valuePredicate);
     }
 
     @Override
@@ -63,111 +55,57 @@ public class PredicatedMapTest<K, V> extends AbstractIterableMapTest<K, V> {
 
     @Override
     public IterableMap<K, V> makeObject() {
-        // Create an unrestricted map (accepts any keys and values)
-        return createPredicatedMap(new HashMap<>(), ACCEPT_ALL_PREDICATE, ACCEPT_ALL_PREDICATE);
+        return decorateMap(new HashMap<>(), truePredicate, truePredicate);
     }
 
-    /**
-     * Creates a test map that only accepts String keys and values.
-     */
-    public IterableMap<K, V> createStringOnlyMap() {
-        return createPredicatedMap(new HashMap<>(), STRING_ONLY_PREDICATE, STRING_ONLY_PREDICATE);
+    public IterableMap<K, V> makeTestMap() {
+        return decorateMap(new HashMap<>(), testPredicate, testPredicate);
     }
 
     @Test
     @SuppressWarnings("unchecked")
-    void testEntrySet_ReturnsValidEntrySet() {
-        // Given: A string-only predicated map
-        Map<K, V> stringOnlyMap = createStringOnlyMap();
-        
-        // When & Then: Entry set should not be null
-        assertNotNull(stringOnlyMap.entrySet(), "Entry set should never be null");
-        
-        // Given: An unrestricted map with one entry
-        Map<K, V> unrestrictedMap = createPredicatedMap(new HashMap<>(), null, null);
-        unrestrictedMap.put((K) "testKey", (V) "testValue");
-        
-        // When & Then: Entry set should contain the added entry
-        assertEquals(1, unrestrictedMap.entrySet().size(), 
-                    "Entry set should contain exactly one entry after adding one key-value pair");
+    void testEntrySet() {
+        Map<K, V> map = makeTestMap();
+        assertNotNull(map.entrySet(), "returned entryset should not be null");
+        map = decorateMap(new HashMap<>(), null, null);
+        map.put((K) "oneKey", (V) "oneValue");
+        assertEquals(1, map.entrySet().size(), "returned entryset should contain one entry");
+        map = decorateMap(map, null, null);
     }
 
     @Test
     @SuppressWarnings("unchecked")
-    void testPut_ValidatesKeysAndValues() {
-        // Given: A map that only accepts String keys and values
-        final Map<K, V> stringOnlyMap = createStringOnlyMap();
-        
-        // When & Then: Adding invalid value should throw exception
-        assertThrows(IllegalArgumentException.class, 
-                    () -> stringOnlyMap.put((K) "validKey", (V) Integer.valueOf(3)),
-                    "Should reject non-String values");
+    void testPut() {
+        final Map<K, V> map = makeTestMap();
+        assertThrows(IllegalArgumentException.class, () -> map.put((K) "Hi", (V) Integer.valueOf(3)),
+                "Illegal value should raise IllegalArgument");
 
-        // When & Then: Adding invalid key should throw exception
-        assertThrows(IllegalArgumentException.class, 
-                    () -> stringOnlyMap.put((K) Integer.valueOf(3), (V) "validValue"),
-                    "Should reject non-String keys");
+        assertThrows(IllegalArgumentException.class, () -> map.put((K) Integer.valueOf(3), (V) "Hi"),
+                "Illegal key should raise IllegalArgument");
 
-        // When & Then: Map should not contain the rejected entries
-        assertFalse(stringOnlyMap.containsKey(Integer.valueOf(3)), 
-                   "Map should not contain rejected key");
-        assertFalse(stringOnlyMap.containsValue(Integer.valueOf(3)), 
-                   "Map should not contain rejected value");
-    }
+        assertFalse(map.containsKey(Integer.valueOf(3)));
+        assertFalse(map.containsValue(Integer.valueOf(3)));
 
-    @Test
-    @SuppressWarnings("unchecked")
-    void testPutAll_ValidatesAllEntries() {
-        // Given: A map that only accepts String keys and values
-        final Map<K, V> stringOnlyMap = createStringOnlyMap();
-        
-        // Given: A source map with mixed valid and invalid entries
-        final Map<K, V> sourceMap = new HashMap<>();
-        sourceMap.put((K) "validKey1", (V) "validValue1");
-        sourceMap.put((K) "validKey2", (V) "validValue2");
-        sourceMap.put((K) "validKey3", (V) "validValue3");
-        sourceMap.put((K) "invalidKey", (V) Integer.valueOf(3)); // Invalid value
+        final Map<K, V> map2 = new HashMap<>();
+        map2.put((K) "A", (V) "a");
+        map2.put((K) "B", (V) "b");
+        map2.put((K) "C", (V) "c");
+        map2.put((K) "c", (V) Integer.valueOf(3));
 
-        // When & Then: putAll should reject the entire operation due to invalid entry
-        assertThrows(IllegalArgumentException.class, 
-                    () -> stringOnlyMap.putAll(sourceMap),
-                    "Should reject putAll operation when any entry is invalid");
-    }
+        assertThrows(IllegalArgumentException.class, () -> map.putAll(map2),
+                "Illegal value should raise IllegalArgument");
 
-    @Test
-    @SuppressWarnings("unchecked")
-    void testEntrySetValue_ValidatesNewValues() {
-        // Given: A map that only accepts String keys and values
-        final Map<K, V> stringOnlyMap = createStringOnlyMap();
-        stringOnlyMap.put((K) "testKey", (V) "originalValue");
-        
-        // Given: An entry from the map
-        Iterator<Map.Entry<K, V>> iterator = stringOnlyMap.entrySet().iterator();
+        map.put((K) "E", (V) "e");
+        Iterator<Map.Entry<K, V>> iterator = map.entrySet().iterator();
         Map.Entry<K, V> entry = iterator.next();
+        final Map.Entry<K, V> finalEntry = entry;
+        assertThrows(IllegalArgumentException.class, () -> finalEntry.setValue((V) Integer.valueOf(3)),
+                "Illegal value should raise IllegalArgument");
 
-        // When & Then: Setting invalid value through entry should throw exception
-        assertThrows(IllegalArgumentException.class, 
-                    () -> entry.setValue((V) Integer.valueOf(3)),
-                    "Should reject invalid values when set through Map.Entry.setValue()");
-    }
-
-    @Test
-    @SuppressWarnings("unchecked")
-    void testEntrySetValue_AcceptsValidValues() {
-        // Given: A map that only accepts String keys and values
-        final Map<K, V> stringOnlyMap = createStringOnlyMap();
-        stringOnlyMap.put((K) "testKey", (V) "originalValue");
-        
-        // Given: An entry from the map
-        Iterator<Map.Entry<K, V>> iterator = stringOnlyMap.entrySet().iterator();
-        Map.Entry<K, V> entry = iterator.next();
-
-        // When: Setting valid value through entry
-        entry.setValue((V) "newValidValue");
-        
-        // Then: Operation should succeed (no exception thrown)
-        assertEquals("newValidValue", entry.getValue(), 
-                    "Entry should contain the new valid value");
+        map.put((K) "F", (V) "f");
+        iterator = map.entrySet().iterator();
+        entry = iterator.next();
+        entry.setValue((V) "x");
     }
 
 //    void testCreate() throws Exception {
@@ -180,4 +118,5 @@ public class PredicatedMapTest<K, V> extends AbstractIterableMapTest<K, V> {
 //            (java.io.Serializable) map,
 //            "src/test/resources/data/test/PredicatedMap.fullCollection.version4.obj");
 //    }
+
 }
