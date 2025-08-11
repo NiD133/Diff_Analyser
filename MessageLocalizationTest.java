16d0c615dfe67479c0348e4bdb6cceb414ab106f
NiD133 <k19052355@kcl.ac.uk>
2025-08-12 03:09:29 +0800

    Original manualTestSuite MessageLocalizationTest.java

--- MessageLocalizationTest.java
+++ MessageLocalizationTest.java
@@ -1,51 +1,72 @@
+/*
+ *
+ * This file is part of the iText (R) project.
+    Copyright (c) 1998-2022 iText Group NV
+ * Authors: Bruno Lowagie, Paulo Soares, Kevin Day, et al.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License version 3
+ * as published by the Free Software Foundation with the addition of the
+ * following permission added to Section 15 as permitted in Section 7(a):
+ * FOR ANY PART OF THE COVERED WORK IN WHICH THE COPYRIGHT IS OWNED BY
+ * ITEXT GROUP. ITEXT GROUP DISCLAIMS THE WARRANTY OF NON INFRINGEMENT
+ * OF THIRD PARTY RIGHTS
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program; if not, see http://www.gnu.org/licenses or write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA, 02110-1301 USA, or download the license from the following URL:
+ * http://itextpdf.com/terms-of-use/
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU Affero General Public License.
+ *
+ * In accordance with Section 7(b) of the GNU Affero General Public License,
+ * a covered work must retain the producer line in every PDF that is created
+ * or manipulated using iText.
+ *
+ * You can be released from the requirements of the license by purchasing
+ * a commercial license. Buying such a license is mandatory as soon as you
+ * develop commercial activities involving the iText software without
+ * disclosing the source code of your own applications.
+ * These activities include: offering paid services to customers as an ASP,
+ * serving PDFs on the fly in a web application, shipping iText with a closed
+ * source product.
+ *
+ * For more information, please contact iText Software Corp. at this
+ * address: sales@itextpdf.com
+ */
 package com.itextpdf.text.error_messages;
+
+
+import junit.framework.Assert;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.assertTrue;
 
 /**
- * Unit tests for the MessageLocalization class.
- * This test suite verifies the localization of error messages.
- * 
- * Author: Kevin Day
+ * @author kevin
  */
 public class MessageLocalizationTest {
 
-    /**
-     * Sets up the test environment before each test.
-     * Currently, no setup is required.
-     */
     @Before
-    public void setUp() {
-        // No setup required for these tests
+    public void setUp() throws Exception {
     }
 
-    /**
-     * Cleans up the test environment after each test.
-     * Currently, no teardown is required.
-     */
     @After
-    public void tearDown() {
-        // No teardown required for these tests
+    public void tearDown() throws Exception {
     }
 
-    /**
-     * Tests that the getComposedMessage method correctly includes the file path
-     * in the resulting message when a file is not found.
-     */
     @Test
-    public void testBackslashesInFilePath() {
-        // Define the test file path with backslashes
-        String testFilePath = "C:\\test\\file.txt";
-
-        // Get the composed message using the test file path
-        String resultMessage = MessageLocalization.getComposedMessage(
-            "1.not.found.as.file.or.resource", testFilePath);
-
-        // Assert that the result message contains the test file path
-        assertTrue("The result message should contain the test file path.",
-                   resultMessage.contains(testFilePath));
+    public void testBackslashes() throws Exception{
+        String testPath = "C:\\test\\file.txt";
+        String rslt = MessageLocalization.getComposedMessage("1.not.found.as.file.or.resource", testPath);
+        Assert.assertTrue("Result doesn't contain the test path", rslt.contains(testPath));
     }
 }
