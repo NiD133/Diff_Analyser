b5f2004639ba27379e622b047229224f4ff3658c
NiD133 <k19052355@kcl.ac.uk>
2025-08-12 03:09:30 +0800

    gpt5 manualTestSuite MeterNeedleTest.java

--- MeterNeedleTest.java
+++ MeterNeedleTest.java
@@ -1,40 +1,7 @@
-/* ======================================================
- * JFreeChart : a chart library for the Java(tm) platform
- * ======================================================
- *
- * (C) Copyright 2000-present, by David Gilbert and Contributors.
- *
- * Project Info:  https://www.jfree.org/jfreechart/index.html
- *
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 2.1 of the License, or
- * (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
- * License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
- * USA.
- *
- * [Oracle and Java are registered trademarks of Oracle and/or its affiliates. 
- * Other names may be trademarks of their respective owners.]
- *
- * --------------------
- * MeterNeedleTest.java
- * --------------------
- * (C) Copyright 2005-present, by David Gilbert and Contributors.
- *
- * Original Author:  David Gilbert;
- * Contributor(s):   -;
- *
- */
-
 package org.jfree.chart.plot.compass;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
@@ -42,58 +9,127 @@ import java.awt.GradientPaint;
 import java.awt.Stroke;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-
 /**
- * Tests for the {@link MeterNeedle} class.
+ * Focused, readable tests for MeterNeedle.equals()/hashCode().
+ * Each test isolates a single field to make intent and failures clear.
  */
 public class MeterNeedleTest {
 
-    /**
-     * Check that the equals() method can distinguish all fields.
-     */
-    @Test
-    public void testEquals() {
-        MeterNeedle n1 = new LineNeedle();
-        MeterNeedle n2 = new LineNeedle();
-        assertEquals(n1, n2);
+    // Test fixtures used across tests to avoid magic numbers in method bodies
+    private static final GradientPaint FILL_PAINT =
+            new GradientPaint(1f, 2f, Color.RED, 3f, 4f, Color.BLUE);
+    private static final GradientPaint OUTLINE_PAINT =
+            new GradientPaint(5f, 6f, Color.RED, 7f, 8f, Color.BLUE);
+    private static final GradientPaint HIGHLIGHT_PAINT =
+            new GradientPaint(9f, 0f, Color.RED, 1f, 2f, Color.BLUE);
+    private static final Stroke OUTLINE_STROKE = new BasicStroke(1.23f);
 
-        n1.setFillPaint(new GradientPaint(1.0f, 2.0f, Color.RED, 3.0f, 4.0f, Color.BLUE));
-        assertNotEquals(n1, n2);
-        n2.setFillPaint(new GradientPaint(1.0f, 2.0f, Color.RED, 3.0f, 4.0f, Color.BLUE));
-        assertEquals(n1, n2);
-
-        n1.setOutlinePaint(new GradientPaint(5.0f, 6.0f, Color.RED, 7.0f, 8.0f, Color.BLUE));
-        assertNotEquals(n1, n2);
-        n2.setOutlinePaint(new GradientPaint(5.0f, 6.0f, Color.RED, 7.0f, 8.0f, Color.BLUE));
-        assertEquals(n1, n2);
-
-        n1.setHighlightPaint(new GradientPaint(9.0f, 0.0f, Color.RED, 1.0f, 2.0f, Color.BLUE));
-        assertNotEquals(n1, n2);
-        n2.setHighlightPaint(new GradientPaint(9.0f, 0.0f, Color.RED, 1.0f, 2.0f, Color.BLUE));
-        assertEquals(n1, n2);
-
-        Stroke s = new BasicStroke(1.23f);
-        n1.setOutlineStroke(s);
-        assertNotEquals(n1, n2);
-        n2.setOutlineStroke(s);
-        assertEquals(n1, n2);
-
-        n1.setRotateX(1.23);
-        assertNotEquals(n1, n2);
-        n2.setRotateX(1.23);
-        assertEquals(n1, n2);
-
-        n1.setRotateY(4.56);
-        assertNotEquals(n1, n2);
-        n2.setRotateY(4.56);
-        assertEquals(n1, n2);
-
-        n1.setSize(11);
-        assertNotEquals(n1, n2);
-        n2.setSize(11);
-        assertEquals(n1, n2);
+    private static MeterNeedle newNeedle() {
+        // LineNeedle is a simple concrete MeterNeedle suitable for equality tests
+        return new LineNeedle();
     }
 
+    private static void assertEqualAndHash(MeterNeedle a, MeterNeedle b) {
+        assertEquals(a, b, "Objects should be equal");
+        assertEquals(a.hashCode(), b.hashCode(), "Equal objects must have equal hash codes");
+    }
+
+    @Test
+    public void equals_whenDefaults_thenEqual() {
+        MeterNeedle left = newNeedle();
+        MeterNeedle right = newNeedle();
+
+        assertEqualAndHash(left, right);
+    }
+
+    @Test
+    public void equals_differsOnFillPaint() {
+        MeterNeedle left = newNeedle();
+        MeterNeedle right = newNeedle();
+        assertEqualAndHash(left, right);
+
+        left.setFillPaint(FILL_PAINT);
+        assertNotEquals(left, right, "Different fillPaint should make objects unequal");
+
+        right.setFillPaint(FILL_PAINT);
+        assertEqualAndHash(left, right);
+    }
+
+    @Test
+    public void equals_differsOnOutlinePaint() {
+        MeterNeedle left = newNeedle();
+        MeterNeedle right = newNeedle();
+        assertEqualAndHash(left, right);
+
+        left.setOutlinePaint(OUTLINE_PAINT);
+        assertNotEquals(left, right, "Different outlinePaint should make objects unequal");
+
+        right.setOutlinePaint(OUTLINE_PAINT);
+        assertEqualAndHash(left, right);
+    }
+
+    @Test
+    public void equals_differsOnHighlightPaint() {
+        MeterNeedle left = newNeedle();
+        MeterNeedle right = newNeedle();
+        assertEqualAndHash(left, right);
+
+        left.setHighlightPaint(HIGHLIGHT_PAINT);
+        assertNotEquals(left, right, "Different highlightPaint should make objects unequal");
+
+        right.setHighlightPaint(HIGHLIGHT_PAINT);
+        assertEqualAndHash(left, right);
+    }
+
+    @Test
+    public void equals_differsOnOutlineStroke() {
+        MeterNeedle left = newNeedle();
+        MeterNeedle right = newNeedle();
+        assertEqualAndHash(left, right);
+
+        left.setOutlineStroke(OUTLINE_STROKE);
+        assertNotEquals(left, right, "Different outlineStroke should make objects unequal");
+
+        right.setOutlineStroke(OUTLINE_STROKE);
+        assertEqualAndHash(left, right);
+    }
+
+    @Test
+    public void equals_differsOnRotateX() {
+        MeterNeedle left = newNeedle();
+        MeterNeedle right = newNeedle();
+        assertEqualAndHash(left, right);
+
+        left.setRotateX(1.23);
+        assertNotEquals(left, right, "Different rotateX should make objects unequal");
+
+        right.setRotateX(1.23);
+        assertEqualAndHash(left, right);
+    }
+
+    @Test
+    public void equals_differsOnRotateY() {
+        MeterNeedle left = newNeedle();
+        MeterNeedle right = newNeedle();
+        assertEqualAndHash(left, right);
+
+        left.setRotateY(4.56);
+        assertNotEquals(left, right, "Different rotateY should make objects unequal");
+
+        right.setRotateY(4.56);
+        assertEqualAndHash(left, right);
+    }
+
+    @Test
+    public void equals_differsOnSize() {
+        MeterNeedle left = newNeedle();
+        MeterNeedle right = newNeedle();
+        assertEqualAndHash(left, right);
+
+        left.setSize(11);
+        assertNotEquals(left, right, "Different size should make objects unequal");
+
+        right.setSize(11);
+        assertEqualAndHash(left, right);
+    }
 }
