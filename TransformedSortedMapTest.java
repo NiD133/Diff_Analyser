5439b06da1acc925f1beb3aeb743a47c64063fa7
NiD133 <k19052355@kcl.ac.uk>
2025-08-12 05:16:20 +0800

    Original manualTestSuite TransformedSortedMapTest.java

--- TransformedSortedMapTest.java
+++ TransformedSortedMapTest.java
@@ -40,10 +40,6 @@ import org.junit.jupiter.api.Test;
  */
 public class TransformedSortedMapTest<K, V> extends AbstractSortedMapTest<K, V> {
 
-    private static final String[] TEST_STRINGS = {"1", "3", "5", "7", "2", "4", "6"};
-    private static final Transformer<String, Integer> STRING_TO_INTEGER = 
-        TransformedCollectionTest.STRING_TO_INTEGER_TRANSFORMER;
-
     @Override
     public String getCompatibilityVersion() {
         return "4";
@@ -58,194 +54,116 @@ public class TransformedSortedMapTest<K, V> extends AbstractSortedMapTest<K, V> 
     @Override
     @SuppressWarnings("unchecked")
     public SortedMap<K, V> makeObject() {
-        return TransformedSortedMap.transformingSortedMap(
-            new TreeMap<>(),
-            (Transformer<? super K, ? extends K>) TransformerUtils.nopTransformer(),
-            (Transformer<? super V, ? extends V>) TransformerUtils.nopTransformer()
-        );
+        return TransformedSortedMap.transformingSortedMap(new TreeMap<>(),
+                (Transformer<? super K, ? extends K>) TransformerUtils.nopTransformer(),
+                (Transformer<? super V, ? extends V>) TransformerUtils.nopTransformer());
     }
 
-    /**
-     * Tests decorating a map with a value transformer that transforms new entries.
-     * Existing entries should remain unchanged until modified.
-     */
     @Test
     @SuppressWarnings("unchecked")
-    void testDecorateWithValueTransformer() {
+    void testFactory_Decorate() {
         final SortedMap<K, V> base = new TreeMap<>();
         base.put((K) "A", (V) "1");
         base.put((K) "B", (V) "2");
         base.put((K) "C", (V) "3");
 
-        final SortedMap<K, V> transformedMap = TransformedSortedMap.transformingSortedMap(
-            base,
-            null,
-            (Transformer<? super V, ? extends V>) STRING_TO_INTEGER
-        );
-        
-        assertEquals(3, transformedMap.size());
-        assertEquals("1", transformedMap.get("A"));
-        assertEquals("2", transformedMap.get("B"));
-        assertEquals("3", transformedMap.get("C"));
-        
-        // Adding a new entry should transform the value
-        transformedMap.put((K) "D", (V) "4");
-        assertEquals(Integer.valueOf(4), transformedMap.get("D"));
+        final SortedMap<K, V> trans = TransformedSortedMap
+                .transformingSortedMap(
+                        base,
+                        null,
+                        (Transformer<? super V, ? extends V>) TransformedCollectionTest.STRING_TO_INTEGER_TRANSFORMER);
+        assertEquals(3, trans.size());
+        assertEquals("1", trans.get("A"));
+        assertEquals("2", trans.get("B"));
+        assertEquals("3", trans.get("C"));
+        trans.put((K) "D", (V) "4");
+        assertEquals(Integer.valueOf(4), trans.get("D"));
     }
 
-    /**
-     * Tests decorating a map and transforming existing entries immediately.
-     * New entries should also be transformed.
-     */
     @Test
     @SuppressWarnings("unchecked")
-    void testDecorateAndTransformExistingValues() {
+    void testFactory_decorateTransform() {
         final SortedMap<K, V> base = new TreeMap<>();
         base.put((K) "A", (V) "1");
         base.put((K) "B", (V) "2");
         base.put((K) "C", (V) "3");
 
-        final SortedMap<K, V> transformedMap = TransformedSortedMap.transformedSortedMap(
-            base,
-            null,
-            (Transformer<? super V, ? extends V>) STRING_TO_INTEGER
-        );
-        
-        assertEquals(3, transformedMap.size());
-        assertEquals(Integer.valueOf(1), transformedMap.get("A"));
-        assertEquals(Integer.valueOf(2), transformedMap.get("B"));
-        assertEquals(Integer.valueOf(3), transformedMap.get("C"));
-        
-        // New entry should be transformed
-        transformedMap.put((K) "D", (V) "4");
-        assertEquals(Integer.valueOf(4), transformedMap.get("D"));
+        final SortedMap<K, V> trans = TransformedSortedMap
+                .transformedSortedMap(
+                        base,
+                        null,
+                        (Transformer<? super V, ? extends V>) TransformedCollectionTest.STRING_TO_INTEGER_TRANSFORMER);
+        assertEquals(3, trans.size());
+        assertEquals(Integer.valueOf(1), trans.get("A"));
+        assertEquals(Integer.valueOf(2), trans.get("B"));
+        assertEquals(Integer.valueOf(3), trans.get("C"));
+        trans.put((K) "D", (V) "4");
+        assertEquals(Integer.valueOf(4), trans.get("D"));
     }
 
-    /**
-     * Tests that keys are transformed correctly when using a key transformer.
-     * Verifies that map operations require the transformed key type.
-     */
     @Test
     @SuppressWarnings("unchecked")
-    void testKeyTransformation() {
-        final SortedMap<K, V> map = TransformedSortedMap.transformingSortedMap(
-            new TreeMap<>(),
-            (Transformer<? super K, ? extends K>) STRING_TO_INTEGER,
-            null
-        );
-        
-        // Verify initial state
+    void testTransformedMap() {
+        final Object[] els = { "1", "3", "5", "7", "2", "4", "6" };
+
+        SortedMap<K, V> map = TransformedSortedMap
+                .transformingSortedMap(
+                        new TreeMap<>(),
+                        (Transformer<? super K, ? extends K>) TransformedCollectionTest.STRING_TO_INTEGER_TRANSFORMER,
+                        null);
         assertEquals(0, map.size());
-        
-        // Add entries with string keys (should be transformed to integers)
-        for (int i = 0; i < TEST_STRINGS.length; i++) {
-            final String element = TEST_STRINGS[i];
-            map.put((K) element, (V) element);
-            
-            // Verify size after each insertion
+        for (int i = 0; i < els.length; i++) {
+            map.put((K) els[i], (V) els[i]);
             assertEquals(i + 1, map.size());
-            
-            // Verify transformed key exists
-            final Integer key = Integer.valueOf(element);
-            assertTrue(map.containsKey(key), "Map should contain transformed key: " + key);
-            
-            // Verify original string key cannot be used (wrong type)
-            assertThrows(ClassCastException.class, () -> map.containsKey(element),
-                "Should throw ClassCastException when using non-transformed key");
-            
-            // Verify value remains unchanged
-            assertTrue(map.containsValue(element));
-            assertEquals(element, map.get(key));
+            assertTrue(map.containsKey(Integer.valueOf((String) els[i])));
+            final SortedMap<K, V> finalMap1 = map;
+            final int finalI = i;
+            assertThrows(ClassCastException.class, () -> finalMap1.containsKey(els[finalI]));
+            assertTrue(map.containsValue(els[i]));
+            assertEquals(els[i], map.get(Integer.valueOf((String) els[i])));
         }
 
-        // Test removal with transformed key
-        final Integer firstKey = Integer.valueOf(TEST_STRINGS[0]);
-        assertEquals(TEST_STRINGS[0], map.remove(firstKey));
-        
-        // Test removal with non-transformed key should fail
-        assertThrows(ClassCastException.class, () -> map.remove(TEST_STRINGS[1]));
-    }
+        final SortedMap<K, V> finalMap = map;
+        assertThrows(ClassCastException.class, () -> finalMap.remove(els[0]));
+        assertEquals(els[0], map.remove(Integer.valueOf((String) els[0])));
 
-    /**
-     * Tests that values are transformed correctly when using a value transformer.
-     * Verifies that map operations require the original key type and transformed value type.
-     */
-    @Test
-    @SuppressWarnings("unchecked")
-    void testValueTransformation() {
-        final SortedMap<K, V> map = TransformedSortedMap.transformingSortedMap(
-            new TreeMap<>(),
-            null,
-            (Transformer<? super V, ? extends V>) STRING_TO_INTEGER
-        );
-        
-        // Verify initial state
+        map = TransformedSortedMap
+                .transformingSortedMap(
+                        new TreeMap<>(),
+                        null,
+                        (Transformer<? super V, ? extends V>) TransformedCollectionTest.STRING_TO_INTEGER_TRANSFORMER);
         assertEquals(0, map.size());
-        
-        // Add entries with string values (should be transformed to integers)
-        for (int i = 0; i < TEST_STRINGS.length; i++) {
-            final String element = TEST_STRINGS[i];
-            map.put((K) element, (V) element);
-            
-            // Verify size after each insertion
+        for (int i = 0; i < els.length; i++) {
+            map.put((K) els[i], (V) els[i]);
             assertEquals(i + 1, map.size());
-            
-            // Verify transformed value exists
-            final Integer transformedValue = Integer.valueOf(element);
-            assertTrue(map.containsValue(transformedValue),
-                "Map should contain transformed value: " + transformedValue);
-            
-            // Verify original string value is not present
-            assertFalse(map.containsValue(element),
-                "Map should not contain non-transformed value: " + element);
-            
-            // Verify key remains unchanged
-            assertTrue(map.containsKey(element));
-            assertEquals(transformedValue, map.get(element));
+            assertTrue(map.containsValue(Integer.valueOf((String) els[i])));
+            assertFalse(map.containsValue(els[i]));
+            assertTrue(map.containsKey(els[i]));
+            assertEquals(Integer.valueOf((String) els[i]), map.get(els[i]));
         }
 
-        // Test removal returns transformed value
-        assertEquals(Integer.valueOf(TEST_STRINGS[0]), map.remove(TEST_STRINGS[0]));
-    }
+        assertEquals(Integer.valueOf((String) els[0]), map.remove(els[0]));
 
-    /**
-     * Tests that value updates through the entry set are transformed correctly.
-     */
-    @Test
-    @SuppressWarnings("unchecked")
-    void testEntrySetValueTransformation() {
-        // Create map with value transformer
-        final SortedMap<K, V> map = TransformedSortedMap.transformingSortedMap(
-            new TreeMap<>(),
-            null,
-            (Transformer<? super V, ? extends V>) STRING_TO_INTEGER
-        );
-        
-        // Populate the map
-        for (String element : TEST_STRINGS) {
-            map.put((K) element, (V) element);
-        }
-        
         final Set<Map.Entry<K, V>> entrySet = map.entrySet();
-        final Map.Entry<K, V> firstEntry = entrySet.iterator().next();
-        
-        // Update value through entry.setValue()
-        firstEntry.setValue((V) "88");
-        assertEquals(Integer.valueOf(88), firstEntry.getValue());
-        assertEquals(Integer.valueOf(88), map.get(firstEntry.getKey()));
+        final Map.Entry<K, V>[] array = entrySet.toArray(new Map.Entry[0]);
+        array[0].setValue((V) "66");
+        assertEquals(Integer.valueOf(66), array[0].getValue());
+        assertEquals(Integer.valueOf(66), map.get(array[0].getKey()));
+
+        final Map.Entry<K, V> entry = entrySet.iterator().next();
+        entry.setValue((V) "88");
+        assertEquals(Integer.valueOf(88), entry.getValue());
+        assertEquals(Integer.valueOf(88), map.get(entry.getKey()));
     }
 
-    // Uncomment for serialization testing
-    /*
-    void testCreate() throws Exception {
-        resetEmpty();
-        writeExternalFormToDisk(
-            (java.io.Serializable) map,
-            "src/test/resources/data/test/TransformedSortedMap.emptyCollection.version4.obj");
-        resetFull();
-        writeExternalFormToDisk(
-            (java.io.Serializable) map,
-            "src/test/resources/data/test/TransformedSortedMap.fullCollection.version4.obj");
-    }
-    */
+//    void testCreate() throws Exception {
+//        resetEmpty();
+//        writeExternalFormToDisk(
+//            (java.io.Serializable) map,
+//            "src/test/resources/data/test/TransformedSortedMap.emptyCollection.version4.obj");
+//        resetFull();
+//        writeExternalFormToDisk(
+//            (java.io.Serializable) map,
+//            "src/test/resources/data/test/TransformedSortedMap.fullCollection.version4.obj");
+//    }
 }
