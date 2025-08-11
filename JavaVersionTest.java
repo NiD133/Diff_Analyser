82c72df74f97bd563220fb8a1c1e601a4daac873
NiD133 <k19052355@kcl.ac.uk>
2025-08-11 08:57:43 +0800

    Original manualTestSuite JavaVersionTest.java

--- JavaVersionTest.java
+++ JavaVersionTest.java
@@ -20,83 +20,66 @@ import static com.google.common.truth.Truth.assertThat;
 import org.junit.Test;
 
 /**
- * Unit tests for {@link JavaVersion}, verifying its version string parsing logic.
+ * Unit and functional tests for {@link JavaVersion}
  *
  * @author Inderjeet Singh
  */
-public final class JavaVersionTest {
-  // Test strings for various Java versions were inspired by
+public class JavaVersionTest {
+  // Borrowed some of test strings from
   // https://github.com/prestodb/presto/blob/master/presto-main/src/test/java/com/facebook/presto/server/TestJavaVersion.java
 
-  /**
-   * Sanity check to ensure the current JVM's version is detected as at least 8,
-   * which is the minimum requirement for Gson.
-   */
   @Test
-  public void getMajorJavaVersion_onCurrentJvm_isAtLeast8() {
+  public void testGetMajorJavaVersion() {
+    // Gson currently requires at least Java 8
     assertThat(JavaVersion.getMajorJavaVersion()).isAtLeast(8);
   }
 
-  // The following tests verify the parseMajorJavaVersion(String) method for various version string formats.
-
   @Test
-  public void parseMajorJavaVersion_forJava6_returns6() {
-    // Format for Java 6: 1.6.0
-    // See https://www.oracle.com/technetwork/java/javase/version-6-141920.html
+  public void testJava6() {
+    // http://www.oracle.com/technetwork/java/javase/version-6-141920.html
     assertThat(JavaVersion.parseMajorJavaVersion("1.6.0")).isEqualTo(6);
   }
 
   @Test
-  public void parseMajorJavaVersion_forJava7_returns7() {
-    // Format for Java 7: 1.7.0
-    // See https://www.oracle.com/technetwork/java/javase/jdk7-naming-418744.html
+  public void testJava7() {
+    // http://www.oracle.com/technetwork/java/javase/jdk7-naming-418744.html
     assertThat(JavaVersion.parseMajorJavaVersion("1.7.0")).isEqualTo(7);
   }
 
   @Test
-  public void parseMajorJavaVersion_forJava8_returns8() {
-    // Pre-JEP 223 format for Java 8: "1.8.0_..."
+  public void testJava8() {
     assertThat(JavaVersion.parseMajorJavaVersion("1.8")).isEqualTo(8);
     assertThat(JavaVersion.parseMajorJavaVersion("1.8.0")).isEqualTo(8);
     assertThat(JavaVersion.parseMajorJavaVersion("1.8.0_131")).isEqualTo(8);
     assertThat(JavaVersion.parseMajorJavaVersion("1.8.0_60-ea")).isEqualTo(8);
     assertThat(JavaVersion.parseMajorJavaVersion("1.8.0_111-internal")).isEqualTo(8);
 
-    // OpenJDK 8 build formats
-    // See https://github.com/AdoptOpenJDK/openjdk-build/issues/93
+    // openjdk8 per https://github.com/AdoptOpenJDK/openjdk-build/issues/93
     assertThat(JavaVersion.parseMajorJavaVersion("1.8.0-internal")).isEqualTo(8);
     assertThat(JavaVersion.parseMajorJavaVersion("1.8.0_131-adoptopenjdk")).isEqualTo(8);
   }
 
   @Test
-  public void parseMajorJavaVersion_forJava9_returns9() {
-    // JEP 223 introduced a new versioning scheme: "9.0.4", "9-Debian", etc.
-
-    // Legacy-style format
+  public void testJava9() {
+    // Legacy style
     assertThat(JavaVersion.parseMajorJavaVersion("9.0.4")).isEqualTo(9); // Oracle JDK 9
+    // Debian as reported in https://github.com/google/gson/issues/1310
+    assertThat(JavaVersion.parseMajorJavaVersion("9-Debian")).isEqualTo(9);
 
-    // New versioning style
+    // New style
     assertThat(JavaVersion.parseMajorJavaVersion("9-ea+19")).isEqualTo(9);
     assertThat(JavaVersion.parseMajorJavaVersion("9+100")).isEqualTo(9);
     assertThat(JavaVersion.parseMajorJavaVersion("9.0.1+20")).isEqualTo(9);
     assertThat(JavaVersion.parseMajorJavaVersion("9.1.1+20")).isEqualTo(9);
-
-    // Distro-specific format, e.g., Debian
-    // See https://github.com/google/gson/issues/1310
-    assertThat(JavaVersion.parseMajorJavaVersion("9-Debian")).isEqualTo(9);
   }
 
   @Test
-  public void parseMajorJavaVersion_forJava10_returns10() {
-    // JEP 223 format for Java 10
+  public void testJava10() {
     assertThat(JavaVersion.parseMajorJavaVersion("10.0.1")).isEqualTo(10); // Oracle JDK 10.0.1
   }
 
   @Test
-  public void parseMajorJavaVersion_forUnsupportedFormat_returnsFallback() {
-    // When the version string format is not recognized, the method should return a
-    // fallback version. The current implementation defaults to 6, likely because
-    // it was a common baseline and versions before it are not a concern.
-    assertThat(JavaVersion.parseMajorJavaVersion("Java9")).isEqualTo(6);
+  public void testUnknownVersionFormat() {
+    assertThat(JavaVersion.parseMajorJavaVersion("Java9")).isEqualTo(6); // unknown format
   }
 }
