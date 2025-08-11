b208d0e4f22072da5d6b4898b14a0d58033e1dc4
NiD133 <k19052355@kcl.ac.uk>
2025-08-12 05:16:33 +0800

    gemini manualTestSuite XNodeTest.java

--- XNodeTest.java
+++ XNodeTest.java
@@ -18,20 +18,51 @@ package org.apache.ibatis.parsing;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Properties;
+import java.util.stream.Stream;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+/**
+ * Tests for {@link XNode}.
+ * Focuses on the string representation and variable substitution capabilities.
+ */
 class XNodeTest {
 
-  @Test
-  void formatXNodeToString() {
-    XPathParser parser = new XPathParser(
-        "<users><user><id>100</id><name>Tom</name><age>30</age><cars><car index=\"1\">BMW</car><car index=\"2\">Audi</car><car index=\"3\">Benz</car></cars></user></users>");
-    String usersNodeToString = parser.evalNode("/users").toString();
-    String userNodeToString = parser.evalNode("/users/user").toString();
-    String carsNodeToString = parser.evalNode("/users/user/cars").toString();
+  private static final String SAMPLE_XML = """
+      <users>
+        <user>
+          <id>100</id>
+          <name>Tom</name>
+          <age>30</age>
+          <cars>
+            <car index="1">BMW</car>
+            <car index="2">Audi</car>
+            <car index="3">Benz</car>
+          </cars>
+        </user>
+      </users>""";
 
-    String usersNodeToStringExpect = """
+  @ParameterizedTest
+  @MethodSource("provideXPathsAndExpectedFormattedStrings")
+  @DisplayName("toString() should produce a pretty-printed XML string")
+  void shouldProduceFormattedXmlString(String xpath, String expectedXml) {
+    // Arrange
+    XPathParser parser = new XPathParser(SAMPLE_XML);
+
+    // Act
+    String actualXml = parser.evalNode(xpath).toString();
+
+    // Assert
+    assertEquals(expectedXml, actualXml);
+  }
+
+  private static Stream<Arguments> provideXPathsAndExpectedFormattedStrings() {
+    String expectedUsersNode = """
         <users>
           <user>
             <id>
@@ -58,7 +89,7 @@ class XNodeTest {
         </users>
         """;
 
-    String userNodeToStringExpect = """
+    String expectedUserNode = """
         <user>
           <id>
             100
@@ -83,7 +114,7 @@ class XNodeTest {
         </user>
         """;
 
-    String carsNodeToStringExpect = """
+    String expectedCarsNode = """
         <cars>
           <car index="1">
             BMW
@@ -97,14 +128,21 @@ class XNodeTest {
         </cars>
         """;
 
-    assertEquals(usersNodeToStringExpect, usersNodeToString);
-    assertEquals(userNodeToStringExpect, userNodeToString);
-    assertEquals(carsNodeToStringExpect, carsNodeToString);
+    return Stream.of(
+        Arguments.of("/users", expectedUsersNode),
+        Arguments.of("/users/user", expectedUserNode),
+        Arguments.of("/users/user/cars", expectedCarsNode)
+    );
   }
 
+  /**
+   * The toString() method provides a reasonable, but not perfect, pretty-print format.
+   * This test verifies the current behavior, including known formatting quirks with mixed content.
+   */
   @Test
-  void xNodeToString() {
-    String xml = """
+  void shouldPreserveMixedContentAndFormattingInStringRepresentation() {
+    // Arrange
+    String complexXml = """
         <mapper>
           <select id='select' resultType='map'>
             select
@@ -129,8 +167,7 @@ class XNodeTest {
         </mapper>
         """;
 
-    // a little bit ugly with id/name break, but not a blocker
-    String expected = """
+    String expectedXml = """
         <select id="select" resultType="map">
           select
           <var set="foo" value="bar" />
@@ -155,21 +192,42 @@ class XNodeTest {
         </select>
         """;
 
-    XPathParser parser = new XPathParser(xml);
+    XPathParser parser = new XPathParser(complexXml);
     XNode selectNode = parser.evalNode("/mapper/select");
-    assertEquals(expected, selectNode.toString());
+
+    // Act
+    String actualXml = selectNode.toString();
+
+    // Assert
+    assertEquals(expectedXml, actualXml);
   }
 
   @Test
-  void xnodeToStringVariables() throws Exception {
-    String src = "<root attr='${x}'>y = ${y}<sub attr='${y}'>x = ${x}</sub></root>";
-    String expected = "<root attr=\"foo\">\n  y = bar\n  <sub attr=\"bar\">\n    x = foo\n  </sub>\n</root>\n";
-    Properties vars = new Properties();
-    vars.put("x", "foo");
-    vars.put("y", "bar");
-    XPathParser parser = new XPathParser(src, false, vars);
-    XNode selectNode = parser.evalNode("/root");
-    assertEquals(expected, selectNode.toString());
-  }
+  void shouldSubstituteVariablesInContentAndAttributes() {
+    // Arrange
+    Properties variables = new Properties();
+    variables.put("x", "foo");
+    variables.put("y", "bar");
 
+    String xmlWithPlaceholders = "<root attr='${x}'>y = ${y}<sub attr='${y}'>x = ${x}</sub></root>";
+    XPathParser parser = new XPathParser(xmlWithPlaceholders, false, variables);
+
+    // The expected string has a trailing newline, which is preserved by adding a blank line at the end of the text block.
+    String expectedXml = """
+        <root attr="foo">
+          y = bar
+          <sub attr="bar">
+            x = foo
+          </sub>
+        </root>
+        """;
+
+    XNode rootNode = parser.evalNode("/root");
+
+    // Act
+    String actualXml = rootNode.toString();
+
+    // Assert
+    assertEquals(expectedXml, actualXml);
+  }
 }
