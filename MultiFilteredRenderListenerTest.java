3f16dcfc5734b9f23251dbf6d38d597eadc17331
NiD133 <k19052355@kcl.ac.uk>
2025-08-12 03:09:31 +0800

    gpt5 manualTestSuite MultiFilteredRenderListenerTest.java

--- MultiFilteredRenderListenerTest.java
+++ MultiFilteredRenderListenerTest.java
@@ -1,62 +1,51 @@
-/*
-    This file is part of the iText (R) project.
-    Copyright (c) 1998-2022 iText Group NV
-    Authors: iText Software.
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License version 3
-    as published by the Free Software Foundation with the addition of the
-    following permission added to Section 15 as permitted in Section 7(a):
-    FOR ANY PART OF THE COVERED WORK IN WHICH THE COPYRIGHT IS OWNED BY
-    ITEXT GROUP. ITEXT GROUP DISCLAIMS THE WARRANTY OF NON INFRINGEMENT
-    OF THIRD PARTY RIGHTS
-    
-    This program is distributed in the hope that it will be useful, but
-    WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-    or FITNESS FOR A PARTICULAR PURPOSE.
-    See the GNU Affero General Public License for more details.
-    You should have received a copy of the GNU Affero General Public License
-    along with this program; if not, see http://www.gnu.org/licenses or write to
-    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-    Boston, MA, 02110-1301 USA, or download the license from the following URL:
-    http://itextpdf.com/terms-of-use/
-    
-    The interactive user interfaces in modified source and object code versions
-    of this program must display Appropriate Legal Notices, as required under
-    Section 5 of the GNU Affero General Public License.
-    
-    In accordance with Section 7(b) of the GNU Affero General Public License,
-    a covered work must retain the producer line in every PDF that is created
-    or manipulated using iText.
-    
-    You can be released from the requirements of the license by purchasing
-    a commercial license. Buying such a license is mandatory as soon as you
-    develop commercial activities involving the iText software without
-    disclosing the source code of your own applications.
-    These activities include: offering paid services to customers as an ASP,
-    serving PDFs on the fly in a web application, shipping iText with a closed
-    source product.
-    
-    For more information, please contact iText Software Corp. at this
-    address: sales@itextpdf.com
- */
 package com.itextpdf.text.pdf.parser;
 
 import com.itextpdf.testutils.TestResourceUtils;
 import com.itextpdf.text.Rectangle;
 import com.itextpdf.text.pdf.PdfReader;
-import junit.framework.Assert;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
 
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Tests for MultiFilteredRenderListener that aim to be explicit about:
+ * - what is being extracted (regions),
+ * - how listeners/filters are wired,
+ * - and what the expected results are.
+ */
 public class MultiFilteredRenderListenerTest {
 
-    @Test
-    public void test() throws IOException {
-        final PdfReader pdfReader = TestResourceUtils.getResourceAsPdfReader(this, "test.pdf");
+    private static final String TEST_PDF = "test.pdf";
+    private static final int PAGE_NUMBER = 1;
 
-        final String expectedText[] = new String[] {
+    private PdfReader pdfReader;
+
+    @Before
+    public void setUp() throws IOException {
+        pdfReader = TestResourceUtils.getResourceAsPdfReader(this, TEST_PDF);
+    }
+
+    @After
+    public void tearDown() {
+        if (pdfReader != null) {
+            pdfReader.close();
+        }
+    }
+
+    /**
+     * Verifies that:
+     * - Each region gets its own LocationTextExtractionStrategy instance,
+     * - Each strategy is invoked only for text passing its corresponding filter,
+     * - The extracted text matches the known content for those regions.
+     */
+    @Test
+    public void extractsTextFromMultipleIndependentRegions() throws IOException {
+        // Expected text per region (taken from the known content of test.pdf on page 1).
+        final String[] expectedTexts = new String[] {
                 "PostScript Compatibility",
                 "Because the PostScript language does not support the transparent imaging \n" +
                         "model, PDF 1.4 consumer applications must have some means for converting the \n" +
@@ -66,48 +55,93 @@ public class MultiFilteredRenderListenerTest {
                         "earlier consumers. ",
                 "Otherwise, flatten the colors to some assumed device color space with pre-\n" +
                         "determined calibration. In the generated PostScript output, paint the flattened \n" +
-                        "colors in a CIE-based color space having that calibration. "};
+                        "colors in a CIE-based color space having that calibration. "
+        };
 
-        final Rectangle[] regions = new Rectangle[] {new Rectangle(90, 605, 220, 581),
-                new Rectangle(80, 578, 450, 486), new Rectangle(103, 196, 460, 143)};
+        // Regions correspond to: title, first paragraph, and a later paragraph.
+        final Rectangle[] regions = new Rectangle[] {
+                new Rectangle(90, 605, 220, 581),
+                new Rectangle(80, 578, 450, 486),
+                new Rectangle(103, 196, 460, 143)
+        };
 
-        final RegionTextRenderFilter[] regionFilters = new RegionTextRenderFilter[regions.length];
-        for (int i = 0; i < regions.length; i++)
-            regionFilters[i] = new RegionTextRenderFilter(regions[i]);
+        final RegionTextRenderFilter[] filters = createRegionFilters(regions);
 
+        // Wire one extraction strategy per region/filter.
+        final MultiFilteredRenderListener multiListener = new MultiFilteredRenderListener();
+        final LocationTextExtractionStrategy[] strategies =
+                attachOneStrategyPerFilter(multiListener, filters);
 
-        MultiFilteredRenderListener listener = new MultiFilteredRenderListener();
-        LocationTextExtractionStrategy[] extractionStrategies = new LocationTextExtractionStrategy[regions.length];
-        for (int i = 0; i < regions.length; i++)
-            extractionStrategies[i] = (LocationTextExtractionStrategy)listener.attachRenderListener(new LocationTextExtractionStrategy(), regionFilters[i]);
+        // Execute parsing.
+        new PdfReaderContentParser(pdfReader).processContent(PAGE_NUMBER, multiListener);
 
-        new PdfReaderContentParser(pdfReader).processContent(1, listener);
-
-        for (int i = 0; i < regions.length; i++)
-        {
-            String actualText = extractionStrategies[i].getResultantText() ;
-            Assert.assertEquals(expectedText[i], actualText);
+        // Validate extracted text per region.
+        for (int i = 0; i < regions.length; i++) {
+            String actual = strategies[i].getResultantText();
+            assertEquals("Text mismatch for region index " + i, expectedTexts[i], actual);
         }
     }
 
+    /**
+     * Verifies that:
+     * - Attaching a single strategy with multiple filters to MultiFilteredRenderListener
+     *   yields the same result as using a FilteredTextRenderListener with the same filters.
+     */
     @Test
-    public void multipleFiltersForOneRegionTest() throws IOException {
-        final PdfReader pdfReader = TestResourceUtils.getResourceAsPdfReader(this, "test.pdf");
+    public void singleStrategyWithMultipleFilters_matchesFilteredTextRenderListener() throws IOException {
+        final Rectangle[] regions = new Rectangle[] {
+                new Rectangle(0, 0, 500, 650),
+                new Rectangle(0, 0, 400, 400),
+                new Rectangle(200, 200, 500, 600),
+                new Rectangle(100, 100, 450, 400)
+        };
+        final RegionTextRenderFilter[] filters = createRegionFilters(regions);
 
-        final Rectangle[] regions = new Rectangle[] {new Rectangle(0, 0, 500, 650),
-                new Rectangle(0, 0, 400, 400), new Rectangle(200, 200, 500, 600), new Rectangle(100, 100, 450, 400)};
+        // MultiFilteredRenderListener: attach a single strategy with all filters.
+        final MultiFilteredRenderListener multiListener = new MultiFilteredRenderListener();
+        final LocationTextExtractionStrategy strategyUnderTest =
+                attachSingleStrategy(multiListener, filters);
 
-        final RegionTextRenderFilter[] regionFilters = new RegionTextRenderFilter[regions.length];
-        for (int i = 0; i < regions.length; i++)
-            regionFilters[i] = new RegionTextRenderFilter(regions[i]);
+        new PdfReaderContentParser(pdfReader).processContent(PAGE_NUMBER, multiListener);
+        final String actual = strategyUnderTest.getResultantText();
 
-        MultiFilteredRenderListener listener = new MultiFilteredRenderListener();
-        LocationTextExtractionStrategy extractionStrategy = (LocationTextExtractionStrategy)listener.attachRenderListener(new LocationTextExtractionStrategy(), regionFilters);
-        new PdfReaderContentParser(pdfReader).processContent(1, listener);
-        String actualText = extractionStrategy.getResultantText();
+        // Reference: FilteredTextRenderListener with the same filters.
+        final String expected = PdfTextExtractor.getTextFromPage(
+                pdfReader,
+                PAGE_NUMBER,
+                new FilteredTextRenderListener(new LocationTextExtractionStrategy(), filters)
+        );
 
-        String expectedText = PdfTextExtractor.getTextFromPage(pdfReader, 1, new FilteredTextRenderListener(new LocationTextExtractionStrategy(), regionFilters));
+        assertEquals("Extracted text should match FilteredTextRenderListener output", expected, actual);
+    }
 
-        Assert.assertEquals(expectedText, actualText);
+    // Helpers
+
+    private static RegionTextRenderFilter[] createRegionFilters(Rectangle[] regions) {
+        RegionTextRenderFilter[] filters = new RegionTextRenderFilter[regions.length];
+        for (int i = 0; i < regions.length; i++) {
+            filters[i] = new RegionTextRenderFilter(regions[i]);
+        }
+        return filters;
+    }
+
+    private static LocationTextExtractionStrategy[] attachOneStrategyPerFilter(
+            MultiFilteredRenderListener listener,
+            RegionTextRenderFilter[] filters
+    ) {
+        LocationTextExtractionStrategy[] strategies = new LocationTextExtractionStrategy[filters.length];
+        for (int i = 0; i < filters.length; i++) {
+            strategies[i] = (LocationTextExtractionStrategy)
+                    listener.attachRenderListener(new LocationTextExtractionStrategy(), filters[i]);
+        }
+        return strategies;
+    }
+
+    private static LocationTextExtractionStrategy attachSingleStrategy(
+            MultiFilteredRenderListener listener,
+            RenderFilter... filters
+    ) {
+        return (LocationTextExtractionStrategy)
+                listener.attachRenderListener(new LocationTextExtractionStrategy(), filters);
     }
 }
