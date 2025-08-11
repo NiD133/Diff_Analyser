2039913a2ac63c7eb45dbdc5323e3c259b7f5c80
NiD133 <k19052355@kcl.ac.uk>
2025-08-11 08:58:14 +0800

    gpt manualTestSuite XYIntervalTest.java

--- XYIntervalTest.java
+++ XYIntervalTest.java
@@ -1,103 +1,67 @@
-/* ======================================================
- * JFreeChart : a chart library for the Java(tm) platform
- * ======================================================
- *
- * (C) Copyright 2000-present, by David Gilbert and Contributors.
- *
- * Project Info:  https://www.jfree.org/jfreechart/index.html
- *
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 2.1 of the License, or
- * (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
- * License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
- * USA.
- *
- * [Oracle and Java are registered trademarks of Oracle and/or its affiliates. 
- * Other names may be trademarks of their respective owners.]
- *
- * -------------------
- * XYIntervalTest.java
- * -------------------
- * (C) Copyright 2006-present, by David Gilbert and Contributors.
- *
- * Original Author:  David Gilbert;
- * Contributor(s):   -;
- *
- */
-
 package org.jfree.data.xy;
 
 import org.jfree.chart.TestUtils;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * Tests for the {@link XYInterval} class.
+ * Unit tests for the {@link XYInterval} class.
  */
 public class XYIntervalTest {
 
     /**
-     * Confirm that the equals method can distinguish all the required fields.
+     * Tests the {@code equals} method to ensure it correctly compares all fields.
      */
     @Test
     public void testEquals() {
-        XYInterval i1 = new XYInterval(1.0, 2.0, 3.0, 2.5, 3.5);
-        XYInterval i2 = new XYInterval(1.0, 2.0, 3.0, 2.5, 3.5);
-        assertEquals(i1, i2);
+        // Create two identical XYInterval objects
+        XYInterval interval1 = new XYInterval(1.0, 2.0, 3.0, 2.5, 3.5);
+        XYInterval interval2 = new XYInterval(1.0, 2.0, 3.0, 2.5, 3.5);
+        assertEquals(interval1, interval2, "Intervals with identical values should be equal");
 
-        i1 = new XYInterval(1.1, 2.0, 3.0, 2.5, 3.5);
-        assertNotEquals(i1, i2);
-        i2 = new XYInterval(1.1, 2.0, 3.0, 2.5, 3.5);
-        assertEquals(i1, i2);
+        // Test inequality by modifying each field one by one
+        interval1 = new XYInterval(1.1, 2.0, 3.0, 2.5, 3.5);
+        assertNotEquals(interval1, interval2, "Intervals with different xLow should not be equal");
+        interval2 = new XYInterval(1.1, 2.0, 3.0, 2.5, 3.5);
+        assertEquals(interval1, interval2, "Intervals should be equal after updating xLow");
 
-        i1 = new XYInterval(1.1, 2.2, 3.0, 2.5, 3.5);
-        assertNotEquals(i1, i2);
-        i2 = new XYInterval(1.1, 2.2, 3.0, 2.5, 3.5);
-        assertEquals(i1, i2);
+        interval1 = new XYInterval(1.1, 2.2, 3.0, 2.5, 3.5);
+        assertNotEquals(interval1, interval2, "Intervals with different xHigh should not be equal");
+        interval2 = new XYInterval(1.1, 2.2, 3.0, 2.5, 3.5);
+        assertEquals(interval1, interval2, "Intervals should be equal after updating xHigh");
 
-        i1 = new XYInterval(1.1, 2.2, 3.3, 2.5, 3.5);
-        assertNotEquals(i1, i2);
-        i2 = new XYInterval(1.1, 2.2, 3.3, 2.5, 3.5);
-        assertEquals(i1, i2);
+        interval1 = new XYInterval(1.1, 2.2, 3.3, 2.5, 3.5);
+        assertNotEquals(interval1, interval2, "Intervals with different y should not be equal");
+        interval2 = new XYInterval(1.1, 2.2, 3.3, 2.5, 3.5);
+        assertEquals(interval1, interval2, "Intervals should be equal after updating y");
 
-        i1 = new XYInterval(1.1, 2.2, 3.3, 2.6, 3.5);
-        assertNotEquals(i1, i2);
-        i2 = new XYInterval(1.1, 2.2, 3.3, 2.6, 3.5);
-        assertEquals(i1, i2);
+        interval1 = new XYInterval(1.1, 2.2, 3.3, 2.6, 3.5);
+        assertNotEquals(interval1, interval2, "Intervals with different yLow should not be equal");
+        interval2 = new XYInterval(1.1, 2.2, 3.3, 2.6, 3.5);
+        assertEquals(interval1, interval2, "Intervals should be equal after updating yLow");
 
-        i1 = new XYInterval(1.1, 2.2, 3.3, 2.6, 3.6);
-        assertNotEquals(i1, i2);
-        i2 = new XYInterval(1.1, 2.2, 3.3, 2.6, 3.6);
-        assertEquals(i1, i2);
+        interval1 = new XYInterval(1.1, 2.2, 3.3, 2.6, 3.6);
+        assertNotEquals(interval1, interval2, "Intervals with different yHigh should not be equal");
+        interval2 = new XYInterval(1.1, 2.2, 3.3, 2.6, 3.6);
+        assertEquals(interval1, interval2, "Intervals should be equal after updating yHigh");
     }
 
     /**
-     * This class is immutable.
+     * Verifies that the {@link XYInterval} class is not cloneable.
      */
     @Test
     public void testCloning() {
-        XYInterval i1 = new XYInterval(1.0, 2.0, 3.0, 2.5, 3.5);
-        assertFalse(i1 instanceof Cloneable);
+        XYInterval interval = new XYInterval(1.0, 2.0, 3.0, 2.5, 3.5);
+        assertFalse(interval instanceof Cloneable, "XYInterval should not be cloneable");
     }
 
     /**
-     * Serialize an instance, restore it, and check for equality.
+     * Tests serialization and deserialization of an {@link XYInterval} instance.
      */
     @Test
     public void testSerialization() {
-        XYInterval i1 = new XYInterval(1.0, 2.0, 3.0, 2.5, 3.5);
-        XYInterval i2 = TestUtils.serialised(i1);
-        assertEquals(i1, i2);
+        XYInterval originalInterval = new XYInterval(1.0, 2.0, 3.0, 2.5, 3.5);
+        XYInterval deserializedInterval = TestUtils.serialised(originalInterval);
+        assertEquals(originalInterval, deserializedInterval, "Deserialized interval should be equal to the original");
     }
-
 }
