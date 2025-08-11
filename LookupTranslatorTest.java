58a1d71e245a1a76453826b03b240ca026ea8743
NiD133 <k19052355@kcl.ac.uk>
2025-08-12 05:15:23 +0800

    Original manualTestSuite LookupTranslatorTest.java

--- LookupTranslatorTest.java
+++ LookupTranslatorTest.java
@@ -23,57 +23,31 @@ import java.io.IOException;
 import java.io.StringWriter;
 
 import org.apache.commons.lang3.AbstractLangTest;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 /**
- * Unit tests for {@link LookupTranslator}.
- *
- * Test goals:
- * - When a key is found at the given index, translate writes the mapped value and
- *   returns the number of consumed code units (the key length).
- * - Non-String CharSequence inputs and lookup entries are supported (LANG-882).
+ * Tests for {@link org.apache.commons.lang3.text.translate.LookupTranslator}.
  */
 @Deprecated
 class LookupTranslatorTest extends AbstractLangTest {
 
-    private static final CharSequence KEY = "one";
-    private static final CharSequence VALUE = "two";
-    private static final int START_INDEX = 0;
-    private static final int CONSUMED_LENGTH = KEY.length(); // 3
-
-    /**
-     * Helper that asserts a single-key translation.
-     */
-    private void assertTranslates(final CharSequence mapKey,
-                                  final CharSequence mapValue,
-                                  final CharSequence input) throws IOException {
-        // Arrange
-        final LookupTranslator translator = new LookupTranslator(new CharSequence[][] { { mapKey, mapValue } });
-        final StringWriter sink = new StringWriter();
-
-        // Act
-        final int consumed = translator.translate(input, START_INDEX, sink);
-
-        // Assert
-        assertEquals(CONSUMED_LENGTH, consumed, "translate should consume the length of the matched key");
-        assertEquals(mapValue.toString(), sink.toString(), "translate should write the mapped value");
-    }
-
     @Test
-    @DisplayName("Translates String->String and returns consumed length")
-    void translatesStringInput() throws IOException {
-        assertTranslates(KEY, VALUE, KEY);
+    void testBasicLookup() throws IOException {
+        final LookupTranslator lt = new LookupTranslator(new CharSequence[][] { { "one", "two" } });
+        final StringWriter out = new StringWriter();
+        final int result = lt.translate("one", 0, out);
+        assertEquals(3, result, "Incorrect code point consumption");
+        assertEquals("two", out.toString(), "Incorrect value");
     }
 
     // Tests: https://issues.apache.org/jira/browse/LANG-882
     @Test
-    @DisplayName("Supports non-String CharSequence for both lookup entries and input (LANG-882)")
-    void translatesNonStringCharSequences() throws IOException {
-        final CharSequence keyBuffer = new StringBuffer(KEY);
-        final CharSequence valueBuffer = new StringBuffer(VALUE);
-        final CharSequence inputBuffer = new StringBuffer(KEY);
-
-        assertTranslates(keyBuffer, valueBuffer, inputBuffer);
+    void testLang882() throws IOException {
+        final LookupTranslator lt = new LookupTranslator(new CharSequence[][] { { new StringBuffer("one"), new StringBuffer("two") } });
+        final StringWriter out = new StringWriter();
+        final int result = lt.translate(new StringBuffer("one"), 0, out);
+        assertEquals(3, result, "Incorrect code point consumption");
+        assertEquals("two", out.toString(), "Incorrect value");
     }
+
 }
