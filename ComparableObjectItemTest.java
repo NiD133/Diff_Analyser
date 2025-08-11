0aafd192ca1771fb0eb1b04ee4d5af54ead8a316
NiD133 <k19052355@kcl.ac.uk>
2025-08-12 05:14:29 +0800

    Original manualTestSuite ComparableObjectItemTest.java

--- ComparableObjectItemTest.java
+++ ComparableObjectItemTest.java
@@ -1,85 +1,128 @@
+/* ======================================================
+ * JFreeChart : a chart library for the Java(tm) platform
+ * ======================================================
+ *
+ * (C) Copyright 2000-present, by David Gilbert and Contributors.
+ *
+ * Project Info:  https://www.jfree.org/jfreechart/index.html
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ *
+ * [Oracle and Java are registered trademarks of Oracle and/or its affiliates. 
+ * Other names may be trademarks of their respective owners.]
+ *
+ * -----------------------------
+ * ComparableObjectItemTest.java
+ * -----------------------------
+ * (C) Copyright 2006-present, by David Gilbert and Contributors.
+ *
+ * Original Author:  David Gilbert;
+ * Contributor(s):   -;
+ *
+ */
+
 package org.jfree.data;
 
 import org.jfree.chart.TestUtils;
 import org.jfree.chart.internal.CloneUtils;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+import static org.junit.jupiter.api.Assertions.fail;
 
 /**
- * Readable, intention-revealing tests for ComparableObjectItem.
+ * Tests for the {@link ComparableObjectItem} class.
  */
-class ComparableObjectItemTest {
+public class ComparableObjectItemTest {
 
-    private static ComparableObjectItem item(int x, Object y) {
-        return new ComparableObjectItem(x, y);
-    }
-
+    /**
+     * Some checks for the constructor.
+     */
     @Test
-    @DisplayName("Constructor rejects null x-value")
-    void constructor_nullX_throwsIllegalArgumentException() {
-        assertThrows(IllegalArgumentException.class, () -> new ComparableObjectItem(null, "XYZ"));
+    public void testConstructor() {
+        // check null argument 1
+        try {
+            /* ComparableObjectItem item1 = */ new ComparableObjectItem(null,
+                    "XYZ");
+            fail("There should be an exception.");
+        }
+        catch (IllegalArgumentException e) {
+            // expected
+        }
     }
 
+    /**
+     * Confirm that the equals method can distinguish all the required fields.
+     */
     @Test
-    @DisplayName("equals: uses both x and y (object) for equality")
-    void equals_comparesXAndObject() {
-        // same x and same y -> equal
-        ComparableObjectItem a = item(1, "XYZ");
-        ComparableObjectItem b = item(1, "XYZ");
-        assertEquals(a, b);
+    public void testEquals() {
+        ComparableObjectItem item1 = new ComparableObjectItem(1, "XYZ");
+        ComparableObjectItem item2 = new ComparableObjectItem(1, "XYZ");
+        assertEquals(item1, item2);
 
-        // different x, same y -> not equal
-        a = item(2, "XYZ");
-        assertNotEquals(a, b);
+        item1 = new ComparableObjectItem(2, "XYZ");
+        assertNotEquals(item1, item2);
+        item2 = new ComparableObjectItem(2, "XYZ");
+        assertEquals(item1, item2);
 
-        // same x, different y (null vs non-null) -> not equal
-        b = item(2, "XYZ");
-        a = item(2, null);
-        assertNotEquals(a, b);
-
-        // same x and both y null -> equal
-        b = item(2, null);
-        assertEquals(a, b);
+        item1 = new ComparableObjectItem(2, null);
+        assertNotEquals(item1, item2);
+        item2 = new ComparableObjectItem(2, null);
+        assertEquals(item1, item2);
     }
 
+    /**
+     * Some checks for the clone() method.
+     * @throws java.lang.CloneNotSupportedException
+     */
     @Test
-    @DisplayName("clone: produces equal but distinct instance of same type")
-    void cloning_producesEqualButDistinctCopy() throws CloneNotSupportedException {
-        ComparableObjectItem original = item(1, "XYZ");
-        ComparableObjectItem copy = CloneUtils.clone(original);
-
-        assertNotSame(original, copy, "Clone should be a different instance");
-        assertSame(original.getClass(), copy.getClass(), "Clone should be same runtime type");
-        assertEquals(original, copy, "Clone should be equal to original");
+    public void testCloning() throws CloneNotSupportedException {
+        ComparableObjectItem item1 = new ComparableObjectItem(1, "XYZ");
+        ComparableObjectItem item2 = CloneUtils.clone(item1);
+        assertNotSame(item1, item2);
+        assertSame(item1.getClass(), item2.getClass());
+        assertEquals(item1, item2);
     }
 
+    /**
+     * Serialize an instance, restore it, and check for equality.
+     */
     @Test
-    @DisplayName("serialization: round-trip preserves equality")
-    void serialization_roundTripPreservesEquality() {
-        ComparableObjectItem original = item(1, "XYZ");
-        ComparableObjectItem restored = TestUtils.serialised(original);
-
-        assertEquals(original, restored);
+    public void testSerialization() {
+        ComparableObjectItem item1 = new ComparableObjectItem(1, "XYZ");
+        ComparableObjectItem item2 = TestUtils.serialised(item1);
+        assertEquals(item1, item2);
     }
 
+    /**
+     * Some checks for the compareTo() method.
+     */
     @Test
-    @DisplayName("compareTo: orders by x only")
-    void compareTo_ordersByXOnly() {
-        ComparableObjectItem one = item(1, "XYZ");
-        ComparableObjectItem two = item(2, "XYZ");
-        ComparableObjectItem three = item(3, "XYZ");
-        ComparableObjectItem anotherOne = item(1, "XYZ");
-
-        // increasing order
-        assertTrue(two.compareTo(one) > 0, "2 should be greater than 1");
-        assertTrue(three.compareTo(one) > 0, "3 should be greater than 1");
-
-        // equality by x
-        assertEquals(0, anotherOne.compareTo(one), "Items with same x should compare equal");
-
-        // decreasing order
-        assertTrue(one.compareTo(two) < 0, "1 should be less than 2");
+    public void testCompareTo() {
+        ComparableObjectItem item1 = new ComparableObjectItem(1, "XYZ");
+        ComparableObjectItem item2 = new ComparableObjectItem(2, "XYZ");
+        ComparableObjectItem item3 = new ComparableObjectItem(3, "XYZ");
+        ComparableObjectItem item4 = new ComparableObjectItem(1, "XYZ");
+        assertTrue(item2.compareTo(item1) > 0);
+        assertTrue(item3.compareTo(item1) > 0);
+        assertEquals(0, item4.compareTo(item1));
+        assertTrue(item1.compareTo(item2) < 0);
     }
+
 }
