43d415ef0214e995c7c728858e6639113455c3cc
NiD133 <k19052355@kcl.ac.uk>
2025-08-12 03:09:53 +0800

    gpt5 manualTestSuite VectorTest.java

--- VectorTest.java
+++ VectorTest.java
@@ -1,75 +1,74 @@
-/*
- *
- * This file is part of the iText (R) project.
-    Copyright (c) 1998-2022 iText Group NV
- * Authors: Bruno Lowagie, Paulo Soares, Kevin Day, et al.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License version 3
- * as published by the Free Software Foundation with the addition of the
- * following permission added to Section 15 as permitted in Section 7(a):
- * FOR ANY PART OF THE COVERED WORK IN WHICH THE COPYRIGHT IS OWNED BY
- * ITEXT GROUP. ITEXT GROUP DISCLAIMS THE WARRANTY OF NON INFRINGEMENT
- * OF THIRD PARTY RIGHTS
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU Affero General Public License for more details.
- * You should have received a copy of the GNU Affero General Public License
- * along with this program; if not, see http://www.gnu.org/licenses or write to
- * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- * Boston, MA, 02110-1301 USA, or download the license from the following URL:
- * http://itextpdf.com/terms-of-use/
- *
- * The interactive user interfaces in modified source and object code versions
- * of this program must display Appropriate Legal Notices, as required under
- * Section 5 of the GNU Affero General Public License.
- *
- * In accordance with Section 7(b) of the GNU Affero General Public License,
- * a covered work must retain the producer line in every PDF that is created
- * or manipulated using iText.
- *
- * You can be released from the requirements of the license by purchasing
- * a commercial license. Buying such a license is mandatory as soon as you
- * develop commercial activities involving the iText software without
- * disclosing the source code of your own applications.
- * These activities include: offering paid services to customers as an ASP,
- * serving PDFs on the fly in a web application, shipping iText with a closed
- * source product.
- *
- * For more information, please contact iText Software Corp. at this
- * address: sales@itextpdf.com
- */
 package com.itextpdf.text.pdf.parser;
 
-import junit.framework.Assert;
+import static org.junit.Assert.assertEquals;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 /**
- * @author kevin
+ * Unit tests for Vector cross(Matrix) behavior.
+ *
+ * Notes on Matrix(a, b, c, d, e, f):
+ *   | a  c  e |
+ *   | b  d  f |
+ *   | 0  0  1 |
+ *
+ * The cross product here effectively applies the affine transform to the vector:
+ *   x' = a*x + c*y + e*z
+ *   y' = b*x + d*y + f*z
+ *   z' = z  (z is preserved)
  */
 public class VectorTest {
 
-    @Before
-    public void setUp() throws Exception {
-    }
+    private static final float EPS = 0.0001f;
 
-    @After
-    public void tearDown() throws Exception {
+    @Test
+    public void cross_withAffineMatrix_multipliesVectorAndPreservesZ() {
+        // Arrange (Given)
+        final Vector vector = new Vector(2, 3, 4);
+        final Matrix transform = new Matrix(5, 6, 7, 8, 9, 10);
+
+        // Act (When)
+        final Vector result = vector.cross(transform);
+
+        // Assert (Then)
+        // Expected:
+        // x' = 5*2 + 7*3 + 9*4 = 10 + 21 + 36 = 67
+        // y' = 6*2 + 8*3 + 10*4 = 12 + 24 + 40 = 76
+        // z' = 4
+        assertEquals(67f, result.get(Vector.I1), EPS);
+        assertEquals(76f, result.get(Vector.I2), EPS);
+        assertEquals(4f,  result.get(Vector.I3), EPS);
     }
 
     @Test
-    public void testCrossVector() {
-        Vector v = new Vector(2, 3, 4);
-        Matrix m = new Matrix(5, 6, 7, 8, 9, 10);
-        Vector shouldBe = new Vector(67, 76, 4);
-        
-        Vector rslt = v.cross(m);
-        Assert.assertEquals(shouldBe, rslt);
+    public void cross_withIdentityMatrix_returnsSameVector() {
+        // Arrange
+        final Vector original = new Vector(12.5f, -3.75f, 1f);
+        final Matrix identity = new Matrix(1, 0, 0, 1, 0, 0);
+
+        // Act
+        final Vector result = original.cross(identity);
+
+        // Assert
+        assertEquals(original.get(Vector.I1), result.get(Vector.I1), EPS);
+        assertEquals(original.get(Vector.I2), result.get(Vector.I2), EPS);
+        assertEquals(original.get(Vector.I3), result.get(Vector.I3), EPS);
     }
 
+    @Test
+    public void cross_withTranslation_whenZIsOne_appliesOffsetToXY() {
+        // Arrange
+        final Vector point = new Vector(10f, 20f, 1f);
+        final float dx = 3f;
+        final float dy = -7f;
+        final Matrix translation = new Matrix(1, 0, 0, 1, dx, dy);
+
+        // Act
+        final Vector result = point.cross(translation);
+
+        // Assert
+        assertEquals(10f + dx, result.get(Vector.I1), EPS);
+        assertEquals(20f + dy, result.get(Vector.I2), EPS);
+        assertEquals(1f,        result.get(Vector.I3), EPS);
+    }
 }
