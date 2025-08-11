300d6f65e4c5cc1222f6b20256250496f1e08b4d
NiD133 <k19052355@kcl.ac.uk>
2025-08-12 05:14:42 +0800

    Original manualTestSuite DefaultSplitCharacterTest.java

--- DefaultSplitCharacterTest.java
+++ DefaultSplitCharacterTest.java
@@ -1,71 +1,77 @@
+/*
+    This file is part of the iText (R) project.
+    Copyright (c) 1998-2022 iText Group NV
+    Authors: iText Software.
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License version 3
+    as published by the Free Software Foundation with the addition of the
+    following permission added to Section 15 as permitted in Section 7(a):
+    FOR ANY PART OF THE COVERED WORK IN WHICH THE COPYRIGHT IS OWNED BY
+    ITEXT GROUP. ITEXT GROUP DISCLAIMS THE WARRANTY OF NON INFRINGEMENT
+    OF THIRD PARTY RIGHTS
+    
+    This program is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+    or FITNESS FOR A PARTICULAR PURPOSE.
+    See the GNU Affero General Public License for more details.
+    You should have received a copy of the GNU Affero General Public License
+    along with this program; if not, see http://www.gnu.org/licenses or write to
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    Boston, MA, 02110-1301 USA, or download the license from the following URL:
+    http://itextpdf.com/terms-of-use/
+    
+    The interactive user interfaces in modified source and object code versions
+    of this program must display Appropriate Legal Notices, as required under
+    Section 5 of the GNU Affero General Public License.
+    
+    In accordance with Section 7(b) of the GNU Affero General Public License,
+    a covered work must retain the producer line in every PDF that is created
+    or manipulated using iText.
+    
+    You can be released from the requirements of the license by purchasing
+    a commercial license. Buying such a license is mandatory as soon as you
+    develop commercial activities involving the iText software without
+    disclosing the source code of your own applications.
+    These activities include: offering paid services to customers as an ASP,
+    serving PDFs on the fly in a web application, shipping iText with a closed
+    source product.
+    
+    For more information, please contact iText Software Corp. at this
+    address: sales@itextpdf.com
+ */
 package com.itextpdf.text.pdf;
 
 import org.junit.Assert;
 import org.junit.Test;
 
 public class DefaultSplitCharacterTest {
+    private final String[] INPUT_TEXT =
+            new String[]{"tha111-is one that should-be-splitted-right-herel-2018-12-18", "anddate format2 01-01-1920"};
 
     @Test
-    public void hyphensInsideDatePattern_ShouldNotBeSplitCharacters() {
-        // Test string containing a date pattern
-        String text = "anddate format2 01-01-1920";
-        
-        // Find positions of hyphens within the date pattern
-        int dateStart = text.indexOf("01-01-1920");
-        Assert.assertTrue("Date pattern should be present", dateStart != -1);
-        
-        int firstHyphenInDate = dateStart + 2;  // Position of '-' in "01-"
-        int secondHyphenInDate = dateStart + 5; // Position of '-' in "01-01-"
-        
-        // Verify hyphens inside date pattern are not split characters
-        Assert.assertFalse("First hyphen in date should not be split character", 
-            isSplitCharacter(firstHyphenInDate, text));
-        Assert.assertFalse("Second hyphen in date should not be split character", 
-            isSplitCharacter(secondHyphenInDate, text));
+    public void splitCharacterDateFormatTest() {
+        Assert.assertFalse(isPsplitCharacter(21, INPUT_TEXT[1]));
     }
 
     @Test
-    public void hyphenImmediatelyBeforeDatePattern_ShouldBeSplitCharacter() {
-        // Test string with hyphen preceding a date pattern
-        String text = "tha111-is one that should-be-splitted-right-herel-2018-12-18";
-        
-        // Find position of hyphen before date pattern
-        int index = text.indexOf("-2018");
-        Assert.assertTrue("Hyphen before date should be present", index != -1);
-        
-        // Verify hyphen before date is a split character
-        Assert.assertTrue("Hyphen immediately before date should be split character", 
-            isSplitCharacter(index, text));
+    public void hypenInsideDateTest() {
+        Assert.assertFalse(isPsplitCharacter(21, INPUT_TEXT[1]));
     }
 
     @Test
-    public void hyphenInRegularText_ShouldBeSplitCharacter() {
-        // Test string with hyphen in non-date context
-        String text = "tha111-is one that should-be-splitted-right-herel-2018-12-18";
-        
-        // Find first hyphen in the string
-        int index = text.indexOf('-');
-        Assert.assertTrue("Hyphen in text should be present", index != -1);
-        
-        // Verify hyphen in regular text is a split character
-        Assert.assertTrue("Hyphen in regular text should be split character", 
-            isSplitCharacter(index, text));
+    public void hypenBeforeDateTest() {
+        //check HyphenBeforeAdate ex. '-2019-01-01'
+        Assert.assertTrue(isPsplitCharacter(49, INPUT_TEXT[0]));
     }
 
-    /**
-     * Helper method to check if a character is a split character.
-     * 
-     * @param current character position to check
-     * @param text the string containing the character
-     * @return true if the character is a split character, false otherwise
-     */
-    private boolean isSplitCharacter(int current, String text) {
-        return new DefaultSplitCharacter().isSplitCharacter(
-            0,              // start position
-            current,        // current character position
-            text.length(),  // end position (exclusive)
-            text.toCharArray(), 
-            null
-        );
+    @Test
+    public void hypenInsideTextTest() {
+        //checHyphenInsideText ex. 'some-text-here'
+        Assert.assertTrue(isPsplitCharacter(6, INPUT_TEXT[0]));
+    }
+
+    private boolean isPsplitCharacter(int current, String text) {
+        return new DefaultSplitCharacter().isSplitCharacter(75, current, text.length() + 1, text.toCharArray(), null);
     }
 }
