7bb9b19f156a345239544d0b163cf76cc200eab0
NiD133 <k19052355@kcl.ac.uk>
2025-08-12 05:14:48 +0800

    Original manualTestSuite ExecutionListTest.java

--- ExecutionListTest.java
+++ ExecutionListTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2007 The Guava Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.common.util.concurrent;
 
 import static com.google.common.util.concurrent.MoreExecutors.directExecutor;
@@ -7,179 +23,134 @@ import static java.util.concurrent.TimeUnit.SECONDS;
 import com.google.common.testing.NullPointerTester;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.atomic.AtomicInteger;
 import junit.framework.TestCase;
 import org.jspecify.annotations.NullUnmarked;
 
 /**
  * Unit tests for {@link ExecutionList}.
+ *
+ * @author Nishant Thakkar
+ * @author Sven Mawson
  */
 @NullUnmarked
 public class ExecutionListTest extends TestCase {
 
-  private static final long TIMEOUT_SECONDS = 1L;
+  private final ExecutionList list = new ExecutionList();
 
-  public void testExecute_runsAllListenersAddedBeforeExecute() throws Exception {
-    // Given a list with three listeners registered on a background executor
-    ExecutionList list = new ExecutionList();
-    ExecutorService exec = newCachedThreadPool();
-    CountDownLatch latch = new CountDownLatch(3);
-    try {
-      list.add(new LatchRunnable(latch), exec);
-      list.add(new LatchRunnable(latch), exec);
-      list.add(new LatchRunnable(latch), exec);
-      assertEquals(3L, latch.getCount());
+  public void testRunOnPopulatedList() throws Exception {
+    Executor exec = newCachedThreadPool();
+    CountDownLatch countDownLatch = new CountDownLatch(3);
+    list.add(new MockRunnable(countDownLatch), exec);
+    list.add(new MockRunnable(countDownLatch), exec);
+    list.add(new MockRunnable(countDownLatch), exec);
+    assertEquals(3L, countDownLatch.getCount());
 
-      // When execute is called
-      list.execute();
+    list.execute();
 
-      // Then all listeners run within a reasonable amount of time
-      assertTrue(latch.await(TIMEOUT_SECONDS, SECONDS));
-    } finally {
-      exec.shutdownNow();
-    }
+    // Verify that all of the runnables execute in a reasonable amount of time.
+    assertTrue(countDownLatch.await(1L, SECONDS));
   }
 
-  public void testExecute_isIdempotent() {
-    // Given a single listener that increments a counter
-    ExecutionList list = new ExecutionList();
-    AtomicInteger runCount = new AtomicInteger();
+  public void testExecute_idempotent() {
+    AtomicInteger runCalled = new AtomicInteger();
     list.add(
         new Runnable() {
           @Override
           public void run() {
-            runCount.getAndIncrement();
+            runCalled.getAndIncrement();
           }
         },
         directExecutor());
-
-    // When execute is called multiple times
     list.execute();
-    assertEquals(1, runCount.get());
+    assertEquals(1, runCalled.get());
     list.execute();
-
-    // Then the listener is only run once
-    assertEquals(1, runCount.get());
+    assertEquals(1, runCalled.get());
   }
 
-  public void testExecute_isIdempotentWhenCalledConcurrently() throws Exception {
-    // Given a listener that blocks until we allow it to proceed
-    ExecutionList list = new ExecutionList();
-    CountDownLatch allowRun = new CountDownLatch(1);
-    AtomicInteger runCount = new AtomicInteger();
+  public void testExecute_idempotentConcurrently() throws InterruptedException {
+    CountDownLatch okayToRun = new CountDownLatch(1);
+    AtomicInteger runCalled = new AtomicInteger();
     list.add(
         new Runnable() {
           @Override
           public void run() {
             try {
-              allowRun.await();
+              okayToRun.await();
             } catch (InterruptedException e) {
               Thread.currentThread().interrupt();
               throw new RuntimeException(e);
             }
-            runCount.getAndIncrement();
+            runCalled.getAndIncrement();
           }
         },
         directExecutor());
-
-    // When execute is called concurrently from two threads
-    Runnable execute = new Runnable() {
-      @Override
-      public void run() {
-        list.execute();
-      }
-    };
-    Thread t1 = new Thread(execute);
-    Thread t2 = new Thread(execute);
-    t1.start();
-    t2.start();
-
-    // The listener hasn't run yet because it's blocked
-    assertEquals(0, runCount.get());
-
-    // Then, after allowing the runnable to proceed, it runs exactly once
-    allowRun.countDown();
-    t1.join();
-    t2.join();
-    assertEquals(1, runCount.get());
+    Runnable execute =
+        new Runnable() {
+          @Override
+          public void run() {
+            list.execute();
+          }
+        };
+    Thread thread1 = new Thread(execute);
+    Thread thread2 = new Thread(execute);
+    thread1.start();
+    thread2.start();
+    assertEquals(0, runCalled.get());
+    okayToRun.countDown();
+    thread1.join();
+    thread2.join();
+    assertEquals(1, runCalled.get());
   }
 
-  public void testAddAfterExecute_runsImmediately() throws Exception {
-    // Given a list that has already been executed
-    ExecutionList list = new ExecutionList();
-    ExecutorService exec = newCachedThreadPool();
-    try {
-      CountDownLatch first = new CountDownLatch(1);
-      list.add(new LatchRunnable(first), exec);
-      list.execute();
-      assertTrue(first.await(TIMEOUT_SECONDS, SECONDS));
+  public void testAddAfterRun() throws Exception {
+    // Run the previous test
+    testRunOnPopulatedList();
 
-      // When we add a new listener after execute
-      CountDownLatch afterExecute = new CountDownLatch(1);
-      list.add(new LatchRunnable(afterExecute), exec);
-
-      // Then it runs immediately (without a second execute call)
-      assertTrue(afterExecute.await(TIMEOUT_SECONDS, SECONDS));
-    } finally {
-      exec.shutdownNow();
-    }
+    // If it passed, then verify an Add will be executed without calling run
+    CountDownLatch countDownLatch = new CountDownLatch(1);
+    list.add(new MockRunnable(countDownLatch), newCachedThreadPool());
+    assertTrue(countDownLatch.await(1L, SECONDS));
   }
 
-  public void testOrdering_preservesRegistrationOrderForListenersAddedBeforeExecute()
-      throws Exception {
-    // Given 10 listeners added before execute
-    ExecutionList list = new ExecutionList();
-    AtomicInteger counter = new AtomicInteger();
+  public void testOrdering() throws Exception {
+    AtomicInteger integer = new AtomicInteger();
     for (int i = 0; i < 10; i++) {
-      final int expected = i; // capture for the anonymous class
+      int expectedCount = i;
       list.add(
           new Runnable() {
             @Override
             public void run() {
-              // Each listener expects to see the counter at its registration index
-              // and increments it by 1. If run out of order, compareAndSet will fail.
-              counter.compareAndSet(expected, expected + 1);
+              integer.compareAndSet(expectedCount, expectedCount + 1);
             }
           },
           directExecutor());
     }
-
-    // When execute is called
     list.execute();
-
-    // Then listeners added before execute run in registration order
-    assertEquals(10, counter.get());
+    assertEquals(10, integer.get());
   }
 
-  public void testExceptionsFromListenerAreCaughtAndDoNotPreventOthersFromRunning() {
-    // Given a listener that throws
-    ExecutionList list = new ExecutionList();
+  private class MockRunnable implements Runnable {
+    final CountDownLatch countDownLatch;
 
-    // When added before execute, the exception is propagated to the executor (direct in this case)
-    // but must be caught and logged by ExecutionList, not thrown to the caller.
-    list.add(THROWING_RUNNABLE, directExecutor());
-    list.execute();
-
-    // When added after execute, it must still run (and be caught/logged) without throwing here
-    list.add(THROWING_RUNNABLE, directExecutor());
-  }
-
-  public void testPublicInstanceMethodsDisallowNulls() {
-    new NullPointerTester().testAllPublicInstanceMethods(new ExecutionList());
-  }
-
-  private static final class LatchRunnable implements Runnable {
-    final CountDownLatch latch;
-
-    LatchRunnable(CountDownLatch latch) {
-      this.latch = latch;
+    MockRunnable(CountDownLatch countDownLatch) {
+      this.countDownLatch = countDownLatch;
     }
 
     @Override
     public void run() {
-      latch.countDown();
+      countDownLatch.countDown();
     }
+  }
+
+  public void testExceptionsCaught() {
+    list.add(THROWING_RUNNABLE, directExecutor());
+    list.execute();
+    list.add(THROWING_RUNNABLE, directExecutor());
+  }
+
+  public void testNulls() {
+    new NullPointerTester().testAllPublicInstanceMethods(new ExecutionList());
   }
 
   private static final Runnable THROWING_RUNNABLE =
