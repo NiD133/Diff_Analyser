2f77536ef42b636cf44593206406c28fecc86bb0
NiD133 <k19052355@kcl.ac.uk>
2025-08-12 05:14:47 +0800

    gemini manualTestSuite EnumOrdinalTypeHandlerTest.java

--- EnumOrdinalTypeHandlerTest.java
+++ EnumOrdinalTypeHandlerTest.java
@@ -20,75 +20,132 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
+@DisplayName("EnumOrdinalTypeHandler: Maps Java Enums to their integer ordinal value")
 class EnumOrdinalTypeHandlerTest extends BaseTypeHandlerTest {
 
-  enum MyEnum {
+  // The enum used for testing purposes.
+  enum TestEnum {
     ONE, TWO
   }
 
-  private static final TypeHandler<MyEnum> TYPE_HANDLER = new EnumOrdinalTypeHandler<>(MyEnum.class);
+  private static final TypeHandler<TestEnum> TYPE_HANDLER = new EnumOrdinalTypeHandler<>(TestEnum.class);
 
-  @Override
-  @Test
-  public void shouldSetParameter() throws Exception {
-    TYPE_HANDLER.setParameter(ps, 1, MyEnum.ONE, null);
-    verify(ps).setInt(1, 0);
+  @Nested
+  @DisplayName("Setting Parameters")
+  class SetParameterTests {
+
+    @ParameterizedTest
+    @EnumSource(TestEnum.class)
+    @DisplayName("Should set the enum's ordinal for a non-null parameter")
+    void shouldSetEnumOrdinalForNonNullParameter(TestEnum parameter) throws Exception {
+      // Act
+      TYPE_HANDLER.setParameter(ps, 1, parameter, null);
+
+      // Assert: Verify that setInt is called with the enum's ordinal value.
+      verify(ps).setInt(1, parameter.ordinal());
+    }
+
+    @Test
+    @DisplayName("Should set SQL NULL for a null parameter")
+    void shouldSetSqlNullForNullParameter() throws Exception {
+      // Act
+      TYPE_HANDLER.setParameter(ps, 1, null, JdbcType.INTEGER);
+
+      // Assert
+      verify(ps).setNull(1, JdbcType.INTEGER.TYPE_CODE);
+    }
   }
 
-  @Test
-  void shouldSetNullParameter() throws Exception {
-    TYPE_HANDLER.setParameter(ps, 1, null, JdbcType.VARCHAR);
-    verify(ps).setNull(1, JdbcType.VARCHAR.TYPE_CODE);
+  @Nested
+  @DisplayName("Getting Results from ResultSet")
+  class GetResultFromResultSetTests {
+
+    @Nested
+    @DisplayName("by column name")
+    class ByName {
+      @ParameterizedTest
+      @EnumSource(TestEnum.class)
+      @DisplayName("Should return the correct enum for a given ordinal")
+      void shouldReturnCorrectEnum(TestEnum expectedEnum) throws Exception {
+        // Arrange
+        when(rs.getInt("column")).thenReturn(expectedEnum.ordinal());
+        when(rs.wasNull()).thenReturn(false);
+
+        // Act & Assert
+        assertEquals(expectedEnum, TYPE_HANDLER.getResult(rs, "column"));
+      }
+
+      @Test
+      @DisplayName("Should return null when the database value is null")
+      void shouldReturnNullWhenDbValueIsNull() throws Exception {
+        // Arrange
+        when(rs.getInt("column")).thenReturn(0); // This value is ignored when wasNull is true
+        when(rs.wasNull()).thenReturn(true);
+
+        // Act & Assert
+        assertNull(TYPE_HANDLER.getResult(rs, "column"));
+      }
+    }
+
+    @Nested
+    @DisplayName("by column position")
+    class ByPosition {
+      @ParameterizedTest
+      @EnumSource(TestEnum.class)
+      @DisplayName("Should return the correct enum for a given ordinal")
+      void shouldReturnCorrectEnum(TestEnum expectedEnum) throws Exception {
+        // Arrange
+        when(rs.getInt(1)).thenReturn(expectedEnum.ordinal());
+        when(rs.wasNull()).thenReturn(false);
+
+        // Act & Assert
+        assertEquals(expectedEnum, TYPE_HANDLER.getResult(rs, 1));
+      }
+
+      @Test
+      @DisplayName("Should return null when the database value is null")
+      void shouldReturnNullWhenDbValueIsNull() throws Exception {
+        // Arrange
+        when(rs.getInt(1)).thenReturn(0); // This value is ignored when wasNull is true
+        when(rs.wasNull()).thenReturn(true);
+
+        // Act & Assert
+        assertNull(TYPE_HANDLER.getResult(rs, 1));
+      }
+    }
   }
 
-  @Override
-  @Test
-  public void shouldGetResultFromResultSetByName() throws Exception {
-    when(rs.getInt("column")).thenReturn(0);
-    when(rs.wasNull()).thenReturn(false);
-    assertEquals(MyEnum.ONE, TYPE_HANDLER.getResult(rs, "column"));
-  }
+  @Nested
+  @DisplayName("Getting Results from CallableStatement by column position")
+  class GetResultFromCallableStatementTests {
 
-  @Override
-  @Test
-  public void shouldGetResultNullFromResultSetByName() throws Exception {
-    when(rs.getInt("column")).thenReturn(0);
-    when(rs.wasNull()).thenReturn(true);
-    assertNull(TYPE_HANDLER.getResult(rs, "column"));
-  }
+    @ParameterizedTest
+    @EnumSource(TestEnum.class)
+    @DisplayName("Should return the correct enum for a given ordinal")
+    void shouldReturnCorrectEnum(TestEnum expectedEnum) throws Exception {
+      // Arrange
+      when(cs.getInt(1)).thenReturn(expectedEnum.ordinal());
+      when(cs.wasNull()).thenReturn(false);
 
-  @Override
-  @Test
-  public void shouldGetResultFromResultSetByPosition() throws Exception {
-    when(rs.getInt(1)).thenReturn(0);
-    when(rs.wasNull()).thenReturn(false);
-    assertEquals(MyEnum.ONE, TYPE_HANDLER.getResult(rs, 1));
-  }
+      // Act & Assert
+      assertEquals(expectedEnum, TYPE_HANDLER.getResult(cs, 1));
+    }
 
-  @Override
-  @Test
-  public void shouldGetResultNullFromResultSetByPosition() throws Exception {
-    when(rs.getInt(1)).thenReturn(0);
-    when(rs.wasNull()).thenReturn(true);
-    assertNull(TYPE_HANDLER.getResult(rs, 1));
-  }
+    @Test
+    @DisplayName("Should return null when the database value is null")
+    void shouldReturnNullWhenDbValueIsNull() throws Exception {
+      // Arrange
+      when(cs.getInt(1)).thenReturn(0); // This value is ignored when wasNull is true
+      when(cs.wasNull()).thenReturn(true);
 
-  @Override
-  @Test
-  public void shouldGetResultFromCallableStatement() throws Exception {
-    when(cs.getInt(1)).thenReturn(0);
-    when(cs.wasNull()).thenReturn(false);
-    assertEquals(MyEnum.ONE, TYPE_HANDLER.getResult(cs, 1));
+      // Act & Assert
+      assertNull(TYPE_HANDLER.getResult(cs, 1));
+    }
   }
-
-  @Override
-  @Test
-  public void shouldGetResultNullFromCallableStatement() throws Exception {
-    when(cs.getInt(1)).thenReturn(0);
-    when(cs.wasNull()).thenReturn(true);
-    assertNull(TYPE_HANDLER.getResult(cs, 1));
-  }
-
 }
