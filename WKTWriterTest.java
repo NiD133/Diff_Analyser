6b355d4bb607571af7964f9e3bf3beb81562cc17
NiD133 <k19052355@kcl.ac.uk>
2025-08-12 05:16:30 +0800

    deepseek manualTestSuite WKTWriterTest.java

--- WKTWriterTest.java
+++ WKTWriterTest.java
@@ -1,37 +1,41 @@
 package org.locationtech.spatial4j.io;
 
-import static org.junit.Assert.assertEquals;
-import java.util.ArrayList;
+import org.junit.Before;
 import org.junit.Test;
 import org.locationtech.spatial4j.context.SpatialContext;
 import org.locationtech.spatial4j.shape.Point;
 import org.locationtech.spatial4j.shape.ShapeCollection;
 
+import java.util.Collections;
+
+import static org.junit.Assert.assertEquals;
+
 public class WKTWriterTest {
 
-  private SpatialContext ctx;
+    private SpatialContext spatialContext;
+    private ShapeWriter wktWriter;
 
-  protected WKTWriterTest(SpatialContext ctx) {
-    this.ctx = ctx;
-  }
+    @Before
+    public void setUp() {
+        spatialContext = SpatialContext.GEO;
+        wktWriter = spatialContext.getFormats().getWktWriter();
+    }
 
-  public WKTWriterTest() {
-    this(SpatialContext.GEO);
-  }
+    @Test
+    public void writePointWithNaNCoordinates_shouldReturnWktEmptyPoint() {
+        Point emptyPoint = spatialContext.makePoint(Double.NaN, Double.NaN);
+        
+        String wkt = wktWriter.toString(emptyPoint);
+        
+        assertEquals("POINT EMPTY", wkt);
+    }
 
-  @Test
-  public void testToStringOnEmptyPoint() throws Exception {
-    ShapeWriter writer = ctx.getFormats().getWktWriter();
-    Point emptyPoint = ctx.makePoint(Double.NaN, Double.NaN);
-
-    assertEquals("POINT EMPTY", writer.toString(emptyPoint));
-  }
-
-  @Test
-  public void testToStringOnEmptyShapeCollection() throws Exception {
-    ShapeWriter writer = ctx.getFormats().getWktWriter();
-    ShapeCollection<Point> emptyCollection = ctx.makeCollection(new ArrayList<>());
-
-    assertEquals("GEOMETRYCOLLECTION EMPTY", writer.toString(emptyCollection));
-  }
+    @Test
+    public void writeEmptyShapeCollection_shouldReturnWktEmptyGeometryCollection() {
+        ShapeCollection<Point> emptyCollection = spatialContext.makeCollection(Collections.emptyList());
+        
+        String wkt = wktWriter.toString(emptyCollection);
+        
+        assertEquals("GEOMETRYCOLLECTION EMPTY", wkt);
+    }
 }
