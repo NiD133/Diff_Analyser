5580418b7e1bf209f52e5c234f0068bf4d4487bb
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:23:21 +0800

    Original processedAutoTestCases transformedmultivaluedmap_estest35.java

---
+++ transformedmultivaluedmap_estest35.java
@@ -0,0 +1,69 @@
+package org.apache.commons.collections4.multimap;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.lang.reflect.Array;
+import java.util.AbstractMap;
+import java.util.ArrayDeque;
+import java.util.Comparator;
+import java.util.ConcurrentModificationException;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.PriorityQueue;
+import org.apache.commons.collections4.Closure;
+import org.apache.commons.collections4.Factory;
+import org.apache.commons.collections4.MultiValuedMap;
+import org.apache.commons.collections4.Predicate;
+import org.apache.commons.collections4.Transformer;
+import org.apache.commons.collections4.functors.AllPredicate;
+import org.apache.commons.collections4.functors.AnyPredicate;
+import org.apache.commons.collections4.functors.ChainedTransformer;
+import org.apache.commons.collections4.functors.CloneTransformer;
+import org.apache.commons.collections4.functors.ClosureTransformer;
+import org.apache.commons.collections4.functors.ConstantFactory;
+import org.apache.commons.collections4.functors.ConstantTransformer;
+import org.apache.commons.collections4.functors.ExceptionFactory;
+import org.apache.commons.collections4.functors.ExceptionTransformer;
+import org.apache.commons.collections4.functors.FactoryTransformer;
+import org.apache.commons.collections4.functors.IfTransformer;
+import org.apache.commons.collections4.functors.InvokerTransformer;
+import org.apache.commons.collections4.functors.MapTransformer;
+import org.apache.commons.collections4.functors.NOPTransformer;
+import org.apache.commons.collections4.functors.NotPredicate;
+import org.apache.commons.collections4.functors.NullIsExceptionPredicate;
+import org.apache.commons.collections4.functors.NullPredicate;
+import org.apache.commons.collections4.functors.SwitchTransformer;
+import org.apache.commons.collections4.functors.TransformerClosure;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class TransformedMultiValuedMap_ESTestTest35 extends TransformedMultiValuedMap_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test34() throws Throwable {
+        Integer integer0 = new Integer(914);
+        ArrayDeque<Integer> arrayDeque0 = new ArrayDeque<Integer>();
+        arrayDeque0.offerFirst(integer0);
+        HashMap<Integer, Object> hashMap0 = new HashMap<Integer, Object>();
+        ArrayListValuedLinkedHashMap<Integer, Object> arrayListValuedLinkedHashMap0 = new ArrayListValuedLinkedHashMap<Integer, Object>(hashMap0);
+        Transformer<Object, Object> transformer0 = ConstantTransformer.constantTransformer((Object) hashMap0);
+        TransformedMultiValuedMap<Integer, Object> transformedMultiValuedMap0 = TransformedMultiValuedMap.transformedMap((MultiValuedMap<Integer, Object>) arrayListValuedLinkedHashMap0, (Transformer<? super Integer, ? extends Integer>) null, (Transformer<? super Object, ?>) transformer0);
+        transformedMultiValuedMap0.putAll(integer0, (Iterable<?>) arrayDeque0);
+        Transformer<Object, Integer> transformer1 = ExceptionTransformer.exceptionTransformer();
+        // Undeclared exception!
+        try {
+            TransformedMultiValuedMap.transformedMap((MultiValuedMap<Integer, Object>) transformedMultiValuedMap0, (Transformer<? super Integer, ? extends Integer>) null, (Transformer<? super Object, ?>) transformer1);
+            fail("Expecting exception: RuntimeException");
+        } catch (RuntimeException e) {
+            //
+            // ExceptionTransformer invoked
+            //
+            verifyException("org.apache.commons.collections4.functors.ExceptionTransformer", e);
+        }
+    }
+}
