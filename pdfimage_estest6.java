84b44f7b5431f4e68d9d4447c021a2b1a5bc89c5
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:08:15 +0800

    Original processedAutoTestCases pdfimage_estest6.java

---
+++ pdfimage_estest6.java
@@ -0,0 +1,36 @@
+package com.itextpdf.text.pdf;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.ImgJBIG2;
+import com.itextpdf.text.ImgTemplate;
+import com.itextpdf.text.Rectangle;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.io.PrintStream;
+import java.util.Locale;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockPrintStream;
+import org.junit.runner.RunWith;
+
+public class PdfImage_ESTestTest6 extends PdfImage_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test05() throws Throwable {
+        byte[] byteArray0 = new byte[0];
+        ImgJBIG2 imgJBIG2_0 = new ImgJBIG2(257, (byte) 97, byteArray0, byteArray0);
+        PdfIndirectReference pdfIndirectReference0 = new PdfIndirectReference(0, 2147418112, 1);
+        ImgJBIG2 imgJBIG2_1 = new ImgJBIG2(2535, 0, byteArray0, byteArray0);
+        imgJBIG2_0.scaleToFit((Rectangle) imgJBIG2_1);
+        PdfImage pdfImage0 = new PdfImage(imgJBIG2_0, "com.itextpdf.text.pdf.PdfImage", pdfIndirectReference0);
+        Image image0 = pdfImage0.getImage();
+        assertFalse(image0.isDeflated());
+    }
+}
