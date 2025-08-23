1be70027e1b51a7b51d84c0de6624cf767b887fb
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:51:22 +0800

    Original processedAutoTestCases fixedordercomparator_estest15.java

---
+++ fixedordercomparator_estest15.java
@@ -0,0 +1,27 @@
+package org.apache.commons.collections4.comparators;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.lang.reflect.Array;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Function;
+import org.apache.commons.collections4.Predicate;
+import org.apache.commons.collections4.functors.ConstantTransformer;
+import org.apache.commons.collections4.functors.IdentityPredicate;
+import org.apache.commons.collections4.functors.InstanceofPredicate;
+import org.apache.commons.collections4.functors.PredicateTransformer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class FixedOrderComparator_ESTestTest15 extends FixedOrderComparator_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test14() throws Throwable {
+        LinkedList<Comparable<Object>> linkedList0 = new LinkedList<Comparable<Object>>();
+        FixedOrderComparator<Comparable<Object>> fixedOrderComparator0 = new FixedOrderComparator<Comparable<Object>>(linkedList0);
+        fixedOrderComparator0.hashCode();
+    }
+}
