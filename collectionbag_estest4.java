5024b1a51de2d24f959e196d12c99ddceca3fc3b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:44:15 +0800

    gemini processedAutoTestCases collectionbag_estest4.java

--- collectionbag_estest4.java
+++ collectionbag_estest4.java
@@ -1,53 +1,43 @@
 package org.apache.commons.collections4.bag;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.ConcurrentModificationException;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Stack;
-import java.util.TreeSet;
 import org.apache.commons.collections4.Bag;
-import org.apache.commons.collections4.Predicate;
 import org.apache.commons.collections4.SortedBag;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.FalsePredicate;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.MapTransformer;
-import org.apache.commons.collections4.functors.NullIsExceptionPredicate;
-import org.apache.commons.collections4.functors.TransformerPredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class CollectionBag_ESTestTest4 extends CollectionBag_ESTest_scaffolding {
+import java.util.Collections;
 
-    @Test(timeout = 4000)
-    public void test03() throws Throwable {
-        TreeBag<Integer> treeBag0 = new TreeBag<Integer>();
-        Integer integer0 = new Integer((-300));
-        treeBag0.add(integer0);
-        SortedBag<Integer> sortedBag0 = UnmodifiableSortedBag.unmodifiableSortedBag((SortedBag<Integer>) treeBag0);
-        CollectionBag<Integer> collectionBag0 = new CollectionBag<Integer>(sortedBag0);
-        TreeSet<Integer> treeSet0 = new TreeSet<Integer>();
-        // Undeclared exception!
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+/**
+ * Test suite for {@link CollectionBag}.
+ * This refactored test focuses on a specific scenario from the original generated test.
+ */
+public class CollectionBagTest {
+
+    /**
+     * Tests that calling retainAll() on a CollectionBag throws an UnsupportedOperationException
+     * if the bag it decorates is unmodifiable.
+     *
+     * The retainAll() operation needs to remove elements, which is forbidden by the
+     * underlying unmodifiable bag's iterator.
+     */
+    @Test
+    public void retainAllShouldThrowExceptionWhenDecoratingAnUnmodifiableBag() {
+        // Arrange: Create a CollectionBag that wraps an unmodifiable bag containing an element.
+        final Bag<Integer> baseBag = new TreeBag<>();
+        baseBag.add(100);
+
+        final SortedBag<Integer> unmodifiableBag = UnmodifiableSortedBag.unmodifiableSortedBag(baseBag);
+        final Bag<Integer> collectionBag = new CollectionBag<>(unmodifiableBag);
+
+        // Act & Assert: Attempting to retain an empty collection should try to remove the element.
         try {
-            collectionBag0.retainAll(treeSet0);
-            fail("Expecting exception: UnsupportedOperationException");
-        } catch (UnsupportedOperationException e) {
-            //
-            // remove() is not supported
-            //
-            verifyException("org.apache.commons.collections4.iterators.UnmodifiableIterator", e);
+            collectionBag.retainAll(Collections.emptySet());
+            fail("Expected an UnsupportedOperationException because the underlying bag is unmodifiable.");
+        } catch (final UnsupportedOperationException e) {
+            // The exception is expected. Verify its message for more precise testing.
+            assertEquals("remove() is not supported", e.getMessage());
         }
     }
 }
