4a1d2d9ed2e754335b3d32937217f53ab419a318
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:06:01 +0800

    Original processedAutoTestCases observableinputstream_estest38.java

---
+++ observableinputstream_estest38.java
@@ -0,0 +1,42 @@
+package org.apache.commons.io.input;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.FileDescriptor;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.io.PushbackInputStream;
+import java.io.SequenceInputStream;
+import java.io.StringWriter;
+import java.nio.CharBuffer;
+import java.nio.file.NoSuchFileException;
+import java.security.MessageDigest;
+import java.util.Enumeration;
+import java.util.LinkedList;
+import java.util.List;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.io.MockFileInputStream;
+import org.evosuite.runtime.mock.java.io.MockIOException;
+import org.junit.runner.RunWith;
+
+public class ObservableInputStream_ESTestTest38 extends ObservableInputStream_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test37() throws Throwable {
+        ObservableInputStream.Builder observableInputStream_Builder0 = new ObservableInputStream.Builder();
+        CharBuffer charBuffer0 = CharBuffer.allocate(255);
+        observableInputStream_Builder0.setCharSequence(charBuffer0);
+        ObservableInputStream observableInputStream0 = new ObservableInputStream(observableInputStream_Builder0);
+        observableInputStream0.consume();
+    }
+}
