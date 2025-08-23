76d391685630d9d6ad99c17555f81e40126ed3f2
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:13:12 +0800

    Original processedAutoTestCases seconds_estest67.java

---
+++ seconds_estest67.java
@@ -0,0 +1,18 @@
+package org.joda.time;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class Seconds_ESTestTest67 extends Seconds_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test66() throws Throwable {
+        Seconds seconds0 = Seconds.TWO;
+        Weeks weeks0 = seconds0.toStandardWeeks();
+        assertEquals(0, weeks0.getWeeks());
+    }
+}
