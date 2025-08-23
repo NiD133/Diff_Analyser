a631dc1715f02dc7e6ad80e17aa85aa5a59046aa
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:28:18 +0800

    gemini processedAutoTestCases xmpwriter_estest19.java

--- xmpwriter_estest19.java
+++ xmpwriter_estest19.java
@@ -1,48 +1,43 @@
 package com.itextpdf.text.xml.xmp;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import com.itextpdf.awt.AsianFontMapper;
-import com.itextpdf.awt.DefaultFontMapper;
-import com.itextpdf.text.Document;
-import com.itextpdf.text.pdf.PdfAction;
-import com.itextpdf.text.pdf.PdfDictionary;
-import com.itextpdf.text.pdf.PdfDocument;
 import com.itextpdf.text.pdf.PdfName;
-import com.itextpdf.text.pdf.PdfObject;
-import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.xmp.XMPConst;
+import com.itextpdf.xmp.XMPException;
 import com.itextpdf.xmp.XMPMeta;
+import org.junit.Test;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.io.OutputStream;
-import java.io.PipedOutputStream;
-import java.io.PrintStream;
-import java.time.ZoneId;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.function.BiFunction;
-import javax.swing.DebugGraphics;
-import javax.swing.DropMode;
-import javax.swing.JTree;
-import javax.swing.tree.TreeModel;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.evosuite.runtime.mock.java.io.MockFileOutputStream;
-import org.evosuite.runtime.mock.java.io.MockPrintStream;
-import org.junit.runner.RunWith;
 
-public class XmpWriter_ESTestTest19 extends XmpWriter_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
-    @Test(timeout = 4000)
-    public void test18() throws Throwable {
-        PrintStream printStream0 = DebugGraphics.logStream();
-        XmpWriter xmpWriter0 = new XmpWriter(printStream0);
-        PdfName pdfName0 = PdfName.CREATIONDATE;
-        xmpWriter0.addDocInfoProperty(pdfName0, "W<t@4^");
-        assertFalse(pdfName0.isIndirect());
+/**
+ * Test suite for the {@link XmpWriter} class.
+ */
+public class XmpWriterTest {
+
+    /**
+     * Verifies that calling addDocInfoProperty with the key 'CreationDate'
+     * correctly sets the corresponding 'xmp:CreateDate' property in the XMP metadata.
+     */
+    @Test
+    public void addDocInfoProperty_withCreationDateKey_setsXmpCreateDateProperty() throws IOException, XMPException {
+        // Arrange
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        XmpWriter xmpWriter = new XmpWriter(outputStream);
+        
+        PdfName propertyKey = PdfName.CREATIONDATE;
+        String propertyValue = "2023-10-27T10:00:00Z"; // A more representative value
+
+        // Act
+        xmpWriter.addDocInfoProperty(propertyKey, propertyValue);
+
+        // Assert
+        XMPMeta xmpMeta = xmpWriter.getXmpMeta();
+        String actualValue = xmpMeta.getPropertyString(XMPConst.NS_XMP, "CreateDate");
+
+        assertNotNull("The 'CreateDate' property should not be null.", actualValue);
+        assertEquals("The 'CreateDate' property should match the provided value.", propertyValue, actualValue);
     }
 }
