553cafe8b6e3504b65cfda9bb0e9a18a4f094e13
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:46:10 +0800

    gemini processedAutoTestCases countingpathvisitor_estest25.java

--- countingpathvisitor_estest25.java
+++ countingpathvisitor_estest25.java
@@ -1,36 +1,32 @@
 package org.apache.commons.io.file;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import org.apache.commons.io.file.Counters.PathCounters;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.FileVisitResult;
-import java.nio.file.NoSuchFileException;
-import java.nio.file.Path;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.function.UnaryOperator;
-import org.apache.commons.io.filefilter.CanWriteFileFilter;
-import org.apache.commons.io.filefilter.EmptyFileFilter;
-import org.apache.commons.io.filefilter.FileFileFilter;
-import org.apache.commons.io.filefilter.HiddenFileFilter;
-import org.apache.commons.io.filefilter.IOFileFilter;
-import org.apache.commons.io.filefilter.NotFileFilter;
-import org.apache.commons.io.filefilter.PathEqualsFileFilter;
-import org.apache.commons.io.filefilter.SuffixFileFilter;
-import org.apache.commons.io.function.IOBiFunction;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.evosuite.runtime.mock.java.io.MockFile;
-import org.evosuite.runtime.mock.java.io.MockIOException;
-import org.junit.runner.RunWith;
 
-public class CountingPathVisitor_ESTestTest25 extends CountingPathVisitor_ESTest_scaffolding {
+/**
+ * Tests for {@link CountingPathVisitor}.
+ */
+public class CountingPathVisitorTest {
 
-    @Test(timeout = 4000)
-    public void test24() throws Throwable {
-        CountingPathVisitor countingPathVisitor0 = new CountingPathVisitor((Counters.PathCounters) null);
+    /**
+     * Tests that the single-argument constructor accepts a null PathCounters object.
+     */
+    @Test
+    public void constructorShouldAcceptNullCounters() {
+        // This test verifies that the constructor for CountingPathVisitor
+        // accepts a null PathCounters object without throwing an exception.
+        // Note: This behavior is inconsistent with other constructors in the class.
+        // Using the resulting visitor will likely cause a NullPointerException later.
+        // This test documents the current implementation's behavior.
+        final CountingPathVisitor visitor = new CountingPathVisitor((PathCounters) null);
+
+        // 1. Assert that the visitor object was successfully created.
+        assertNotNull("The visitor should be created even with a null counter.", visitor);
+
+        // 2. Assert that the internal counter is null, confirming the state.
+        assertNull("The internal PathCounters should be null.", visitor.getPathCounters());
     }
 }
