f9e1f7db758062947727dc0d6badecba8831720d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:06:55 +0800

    Original processedAutoTestCases pairedstatsaccumulator_estest11.java

---
+++ pairedstatsaccumulator_estest11.java
@@ -0,0 +1,21 @@
+package com.google.common.math;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.ArrayDeque;
+import java.util.Iterator;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class PairedStatsAccumulator_ESTestTest11 extends PairedStatsAccumulator_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test10() throws Throwable {
+        PairedStatsAccumulator pairedStatsAccumulator0 = new PairedStatsAccumulator();
+        pairedStatsAccumulator0.add((-1.0), (-1.0));
+        Stats stats0 = pairedStatsAccumulator0.yStats();
+        assertEquals((-1.0), stats0.sum(), 0.01);
+    }
+}
