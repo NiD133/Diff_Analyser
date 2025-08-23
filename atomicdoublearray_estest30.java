cd387d6f64639283cde7b1d083f337b9da2d1497
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:37:54 +0800

    Original processedAutoTestCases atomicdoublearray_estest30.java

---
+++ atomicdoublearray_estest30.java
@@ -0,0 +1,31 @@
+package com.google.common.util.concurrent;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.function.DoubleBinaryOperator;
+import java.util.function.DoubleUnaryOperator;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class AtomicDoubleArray_ESTestTest30 extends AtomicDoubleArray_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test29() throws Throwable {
+        AtomicDoubleArray atomicDoubleArray0 = new AtomicDoubleArray(8);
+        DoubleBinaryOperator doubleBinaryOperator0 = mock(DoubleBinaryOperator.class, new ViolatedAssumptionAnswer());
+        // Undeclared exception!
+        try {
+            atomicDoubleArray0.getAndAccumulate(8, 8, doubleBinaryOperator0);
+            fail("Expecting exception: IndexOutOfBoundsException");
+        } catch (IndexOutOfBoundsException e) {
+            //
+            // index 8
+            //
+            verifyException("java.util.concurrent.atomic.AtomicLongArray", e);
+        }
+    }
+}
