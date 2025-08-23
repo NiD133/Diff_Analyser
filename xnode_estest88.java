53de6c80ad3a62636f4cc616de4c17231d3761c4
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:29:17 +0800

    Original processedAutoTestCases xnode_estest88.java

---
+++ xnode_estest88.java
@@ -0,0 +1,30 @@
+package org.apache.ibatis.parsing;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.List;
+import java.util.Locale;
+import java.util.Properties;
+import java.util.function.Supplier;
+import javax.imageio.metadata.IIOMetadataNode;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.xml.sax.ext.DefaultHandler2;
+
+public class XNode_ESTestTest88 extends XNode_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test087() throws Throwable {
+        Properties properties0 = new Properties();
+        IIOMetadataNode iIOMetadataNode0 = new IIOMetadataNode();
+        XNode xNode0 = new XNode((XPathParser) null, iIOMetadataNode0, properties0);
+        String string0 = xNode0.getStringAttribute("g\"7}o`");
+        assertNull(string0);
+    }
+}
