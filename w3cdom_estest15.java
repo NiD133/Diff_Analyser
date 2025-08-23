15edd5d13d22a2fd69597260536eb57b87ce2dae
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:26:42 +0800

    Original processedAutoTestCases w3cdom_estest15.java

---
+++ w3cdom_estest15.java
@@ -0,0 +1,43 @@
+package org.jsoup.helper;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.sun.org.apache.xerces.internal.dom.DocumentImpl;
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import javax.imageio.metadata.IIOMetadataNode;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.jsoup.nodes.Attributes;
+import org.jsoup.nodes.DataNode;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.DocumentType;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.FormElement;
+import org.jsoup.nodes.XmlDeclaration;
+import org.jsoup.parser.Parser;
+import org.jsoup.parser.Tag;
+import org.junit.runner.RunWith;
+import org.w3c.dom.DOMException;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class W3CDom_ESTestTest15 extends W3CDom_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test14() throws Throwable {
+        W3CDom w3CDom0 = new W3CDom();
+        Element element0 = new Element("xmlns", "org.jsoup.nodes.Entities");
+        // Undeclared exception!
+        try {
+            w3CDom0.fromJsoup(element0);
+            fail("Expecting exception: DOMException");
+        } catch (DOMException e) {
+        }
+    }
+}
