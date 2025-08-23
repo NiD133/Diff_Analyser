5662d4924459bee03d7c9a12f8f4e0504e475b82
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:02:41 +0800

    gemini processedAutoTestCases meterinterval_estest15.java

--- meterinterval_estest15.java
+++ meterinterval_estest15.java
@@ -1,29 +1,27 @@
 package org.jfree.chart.plot;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.awt.BasicStroke;
-import java.awt.Color;
-import java.awt.GradientPaint;
-import java.awt.Paint;
-import java.awt.Stroke;
-import java.awt.SystemColor;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
 import org.jfree.data.Range;
-import org.jfree.data.statistics.DefaultBoxAndWhiskerCategoryDataset;
-import org.jfree.data.statistics.HistogramType;
-import org.junit.runner.RunWith;
+import org.junit.Test;
+import static org.junit.Assert.assertFalse;
 
-public class MeterInterval_ESTestTest15 extends MeterInterval_ESTest_scaffolding {
+/**
+ * Unit tests for the equals() method of the {@link MeterInterval} class.
+ */
+public class MeterIntervalEqualsTest {
 
-    @Test(timeout = 4000)
-    public void test14() throws Throwable {
-        Range range0 = new Range((-229.41607020985958), (-229.41607020985958));
-        MeterInterval meterInterval0 = new MeterInterval("d|V&g", range0);
-        MeterInterval meterInterval1 = new MeterInterval("U`/UA", range0);
-        boolean boolean0 = meterInterval1.equals(meterInterval0);
-        assertFalse(boolean0);
+    /**
+     * Verifies that two MeterInterval instances are not equal if they share the
+     * same data range but have different labels. The label is a key component
+     * of the interval's identity.
+     */
+    @Test
+    public void equals_shouldReturnFalse_whenIntervalsHaveDifferentLabels() {
+        // Arrange: Create two MeterInterval objects with the same range but different labels.
+        Range commonRange = new Range(0.0, 50.0);
+        MeterInterval intervalNormal = new MeterInterval("Normal", commonRange);
+        MeterInterval intervalHigh = new MeterInterval("High", commonRange);
+
+        // Act & Assert: The two intervals should not be considered equal.
+        assertFalse("Intervals with different labels should not be equal.", intervalNormal.equals(intervalHigh));
     }
 }
