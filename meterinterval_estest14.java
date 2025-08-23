6c0fd8b16a896c5861cbfee583ac7041ab5c99c9
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:02:41 +0800

    gemini processedAutoTestCases meterinterval_estest14.java

--- meterinterval_estest14.java
+++ meterinterval_estest14.java
@@ -1,29 +1,33 @@
 package org.jfree.chart.plot;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.awt.BasicStroke;
-import java.awt.Color;
-import java.awt.GradientPaint;
-import java.awt.Paint;
-import java.awt.Stroke;
-import java.awt.SystemColor;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
 import org.jfree.data.Range;
-import org.jfree.data.statistics.DefaultBoxAndWhiskerCategoryDataset;
-import org.jfree.data.statistics.HistogramType;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class MeterInterval_ESTestTest14 extends MeterInterval_ESTest_scaffolding {
+import static org.junit.Assert.assertFalse;
 
-    @Test(timeout = 4000)
-    public void test13() throws Throwable {
-        Range range0 = new Range(Double.NaN, Double.NaN);
-        MeterInterval meterInterval0 = new MeterInterval("", range0);
-        MeterInterval meterInterval1 = new MeterInterval("", range0);
-        boolean boolean0 = meterInterval0.equals(meterInterval1);
-        assertFalse(boolean0);
+/**
+ * Unit tests for the equals() method in the {@link MeterInterval} class.
+ */
+public class MeterIntervalEqualsTest {
+
+    /**
+     * Verifies that two MeterInterval instances are not considered equal if their
+     * underlying Range contains NaN values. This is the expected behavior because
+     * Double.NaN is never equal to itself, a property that propagates up through
+     * the Range.equals() method.
+     */
+    @Test
+    public void equals_shouldReturnFalse_whenRangeContainsNaN() {
+        // Arrange: Create two MeterIntervals that are identical except for their
+        // reference, both using a Range defined with NaN.
+        Range nanRange = new Range(Double.NaN, Double.NaN);
+        MeterInterval interval1 = new MeterInterval("Normal", nanRange);
+        MeterInterval interval2 = new MeterInterval("Normal", nanRange);
+
+        // Act: Compare the two intervals for equality.
+        boolean areEqual = interval1.equals(interval2);
+
+        // Assert: The intervals should not be equal due to the NaN property.
+        assertFalse("Two intervals with NaN-based ranges should not be equal.", areEqual);
     }
 }
