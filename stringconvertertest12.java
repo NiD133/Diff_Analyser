151e0a7ff1ceb75bcdb82bca83b2db6a120694cb
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:34:06 +0800

    gemini processedManualTestCases stringconvertertest12.java

--- stringconvertertest12.java
+++ stringconvertertest12.java
@@ -1,87 +1,58 @@
 package org.joda.time.convert;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-import java.util.Arrays;
+import static org.junit.Assert.assertEquals;
+
 import java.util.Locale;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-import org.joda.time.Chronology;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
-import org.joda.time.MutableInterval;
-import org.joda.time.MutablePeriod;
-import org.joda.time.PeriodType;
-import org.joda.time.TimeOfDay;
-import org.joda.time.chrono.BuddhistChronology;
-import org.joda.time.chrono.ISOChronology;
-import org.joda.time.chrono.JulianChronology;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
-public class StringConverterTestTest12 extends TestCase {
-
-    private static final DateTimeZone ONE_HOUR = DateTimeZone.forOffsetHours(1);
-
-    private static final DateTimeZone SIX = DateTimeZone.forOffsetHours(6);
-
-    private static final DateTimeZone SEVEN = DateTimeZone.forOffsetHours(7);
-
-    private static final DateTimeZone EIGHT = DateTimeZone.forOffsetHours(8);
-
-    private static final DateTimeZone UTC = DateTimeZone.UTC;
-
-    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
+/**
+ * Unit tests for {@link StringConverter} focusing on its use in DateTime parsing.
+ */
+public class StringConverterTest {
 
     private static final DateTimeZone LONDON = DateTimeZone.forID("Europe/London");
 
-    private static final Chronology ISO_EIGHT = ISOChronology.getInstance(EIGHT);
+    private DateTimeZone originalDefaultZone;
+    private Locale originalDefaultLocale;
 
-    private static final Chronology ISO_PARIS = ISOChronology.getInstance(PARIS);
+    @Before
+    public void setUp() {
+        // Save the original default time zone and locale
+        originalDefaultZone = DateTimeZone.getDefault();
+        originalDefaultLocale = Locale.getDefault();
 
-    private static final Chronology ISO_LONDON = ISOChronology.getInstance(LONDON);
-
-    private static Chronology ISO;
-
-    private static Chronology JULIAN;
-
-    private DateTimeZone zone = null;
-
-    private Locale locale = null;
-
-    public static void main(String[] args) {
-        junit.textui.TestRunner.run(suite());
-    }
-
-    public static TestSuite suite() {
-        return new TestSuite(TestStringConverter.class);
-    }
-
-    @Override
-    protected void setUp() throws Exception {
-        zone = DateTimeZone.getDefault();
-        locale = Locale.getDefault();
+        // Set a specific time zone and locale for predictable test results
         DateTimeZone.setDefault(LONDON);
         Locale.setDefault(Locale.UK);
-        JULIAN = JulianChronology.getInstance();
-        ISO = ISOChronology.getInstance();
     }
 
-    @Override
-    protected void tearDown() throws Exception {
-        DateTimeZone.setDefault(zone);
-        Locale.setDefault(locale);
-        zone = null;
+    @After
+    public void tearDown() {
+        // Restore the original default time zone and locale to avoid side effects
+        DateTimeZone.setDefault(originalDefaultZone);
+        Locale.setDefault(originalDefaultLocale);
     }
 
-    public void testGetDateTime2() throws Exception {
-        DateTime test = new DateTime("2004-06-09T12:24:48.501");
-        assertEquals(2004, test.getYear());
-        assertEquals(6, test.getMonthOfYear());
-        assertEquals(9, test.getDayOfMonth());
-        assertEquals(12, test.getHourOfDay());
-        assertEquals(24, test.getMinuteOfHour());
-        assertEquals(48, test.getSecondOfMinute());
-        assertEquals(501, test.getMillisOfSecond());
-        assertEquals(LONDON, test.getZone());
+    /**
+     * Tests that creating a DateTime from a string without a time zone
+     * correctly uses the default time zone.
+     */
+    @Test
+    public void dateTimeFromString_whenNoZoneSpecified_usesDefaultTimeZone() {
+        // Arrange
+        String dateTimeString = "2004-06-09T12:24:48.501";
+        DateTime expectedDateTime = new DateTime(2004, 6, 9, 12, 24, 48, 501, LONDON);
+
+        // Act
+        // The StringConverter is used implicitly by the DateTime(Object) constructor
+        // to convert the string into a DateTime object.
+        DateTime actualDateTime = new DateTime(dateTimeString);
+
+        // Assert
+        assertEquals(expectedDateTime, actualDateTime);
     }
 }
