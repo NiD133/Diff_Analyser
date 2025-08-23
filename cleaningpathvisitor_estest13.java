f1aca19089c5d78e8957dd32fdb6fc1fedceac97
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:43:33 +0800

    gemini processedAutoTestCases cleaningpathvisitor_estest13.java

--- cleaningpathvisitor_estest13.java
+++ cleaningpathvisitor_estest13.java
@@ -1,32 +1,29 @@
 package org.apache.commons.io.file;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.IOException;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Path;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.evosuite.runtime.mock.java.io.MockFile;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
+import org.apache.commons.io.file.Counters.PathCounters;
+import org.junit.Test;
+
+/**
+ * Tests for {@link CleaningPathVisitor}.
+ * This test class focuses on the behavior of the equals() method.
+ */
 public class CleaningPathVisitor_ESTestTest13 extends CleaningPathVisitor_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test12() throws Throwable {
-        String[] stringArray0 = new String[0];
-        CountingPathVisitor.Builder countingPathVisitor_Builder0 = new CountingPathVisitor.Builder();
-        Counters.PathCounters counters_PathCounters0 = countingPathVisitor_Builder0.getPathCounters();
-        CleaningPathVisitor cleaningPathVisitor0 = new CleaningPathVisitor(counters_PathCounters0, stringArray0);
-        boolean boolean0 = cleaningPathVisitor0.equals(cleaningPathVisitor0);
-        assertTrue(boolean0);
+    /**
+     * Tests that an instance of CleaningPathVisitor is always equal to itself,
+     * verifying the reflexive property of the equals() method.
+     */
+    @Test
+    public void testEquals_isReflexive() {
+        // Arrange: Create a standard CleaningPathVisitor instance.
+        final PathCounters pathCounters = Counters.longPathCounters();
+        final String[] noFilesToSkip = {};
+        final CleaningPathVisitor visitor = new CleaningPathVisitor(pathCounters, noFilesToSkip);
+
+        // Act & Assert: An object must be equal to itself.
+        // This is a fundamental contract of the Object.equals() method.
+        assertEquals("A CleaningPathVisitor instance should be equal to itself.", visitor, visitor);
     }
 }
