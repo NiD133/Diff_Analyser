049c97ac513f32c4273f2cb0ab7fd61172635a52
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:58:51 +0800

    Original processedAutoTestCases jsontypeinfo_estest17.java

---
+++ jsontypeinfo_estest17.java
@@ -0,0 +1,25 @@
+package com.fasterxml.jackson.annotation;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class JsonTypeInfo_ESTestTest17 extends JsonTypeInfo_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test16() throws Throwable {
+        JsonTypeInfo.Id jsonTypeInfo_Id0 = JsonTypeInfo.Id.MINIMAL_CLASS;
+        JsonTypeInfo.As jsonTypeInfo_As0 = JsonTypeInfo.As.WRAPPER_OBJECT;
+        Class<Object> class0 = Object.class;
+        Boolean boolean0 = Boolean.valueOf("@2LLQRbW9{J2*\"1GY");
+        JsonTypeInfo.Value jsonTypeInfo_Value0 = JsonTypeInfo.Value.construct(jsonTypeInfo_Id0, jsonTypeInfo_As0, "@2LLQRbW9{J2*\"1GY", class0, false, boolean0);
+        JsonTypeInfo.Value jsonTypeInfo_Value1 = jsonTypeInfo_Value0.withIdVisible(false);
+        assertSame(jsonTypeInfo_Value1, jsonTypeInfo_Value0);
+        assertEquals("@2LLQRbW9{J2*\"1GY", jsonTypeInfo_Value1.getPropertyName());
+    }
+}
