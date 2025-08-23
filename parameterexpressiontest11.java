18080b361cf72e63f54796502aad13712ee3f395
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:32:05 +0800

    gemini processedManualTestCases parameterexpressiontest11.java

--- parameterexpressiontest11.java
+++ parameterexpressiontest11.java
@@ -1,18 +1,32 @@
 package org.apache.ibatis.builder;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.Map;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class ParameterExpressionTestTest11 {
+/**
+ * Tests for {@link ParameterExpression}.
+ */
+class ParameterExpressionTest {
 
-    @Test
-    void simplePropertyWithSpaceAndManyAttributes() {
-        Map<String, String> result = new ParameterExpression("user name, attr1=val1, attr2=val2, attr3=val3");
-        Assertions.assertEquals(4, result.size());
-        Assertions.assertEquals("user name", result.get("property"));
-        Assertions.assertEquals("val1", result.get("attr1"));
-        Assertions.assertEquals("val2", result.get("attr2"));
-        Assertions.assertEquals("val3", result.get("attr3"));
-    }
+  @Test
+  @DisplayName("Should parse a property with spaces and multiple attributes")
+  void shouldParsePropertyWithSpaceAndMultipleAttributes() {
+    // Arrange
+    String expression = "user name, attr1=val1, attr2=val2, attr3=val3";
+    Map<String, String> expectedParameters = Map.of(
+        "property", "user name",
+        "attr1", "val1",
+        "attr2", "val2",
+        "attr3", "val3"
+    );
+
+    // Act
+    ParameterExpression actualParameters = new ParameterExpression(expression);
+
+    // Assert
+    assertEquals(expectedParameters, actualParameters);
+  }
 }
