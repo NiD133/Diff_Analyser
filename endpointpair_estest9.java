8f11fb54692681f44bd1985c47eea69f048fdf09
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:49:19 +0800

    gemini processedAutoTestCases endpointpair_estest9.java

--- endpointpair_estest9.java
+++ endpointpair_estest9.java
@@ -1,28 +1,26 @@
 package com.google.common.graph;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import com.google.common.collect.UnmodifiableIterator;
-import java.util.Locale;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class EndpointPair_ESTestTest9 extends EndpointPair_ESTest_scaffolding {
+/**
+ * Unit tests for {@link EndpointPair}.
+ */
+public class EndpointPairTest {
 
-    @Test(timeout = 4000)
-    public void test08() throws Throwable {
-        Locale.Category locale_Category0 = Locale.Category.DISPLAY;
-        // Undeclared exception!
-        try {
-            EndpointPair.of((Network<?, ?>) null, locale_Category0, locale_Category0);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("com.google.common.graph.EndpointPair", e);
-        }
+    /**
+     * Tests that calling EndpointPair.of() with a null Network instance
+     * throws a NullPointerException, as the method contract requires a non-null network.
+     */
+    @Test(expected = NullPointerException.class)
+    public void of_network_whenNetworkIsNull_throwsNullPointerException() {
+        // Arrange: Define two nodes for the endpoint pair.
+        String nodeU = "A";
+        String nodeV = "B";
+
+        // Act: Attempt to create an EndpointPair with a null network.
+        // The cast to Network<?, ?> is necessary to resolve method ambiguity.
+        EndpointPair.of((Network<?, ?>) null, nodeU, nodeV);
+
+        // Assert: The @Test(expected) annotation handles the exception assertion.
     }
 }
