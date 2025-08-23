946dab8b2124a9df0888ee8c65aae862aa91acfe
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:49:58 +0800

    gemini processedAutoTestCases extrafieldutils_estest23.java

--- extrafieldutils_estest23.java
+++ extrafieldutils_estest23.java
@@ -1,25 +1,19 @@
 package org.apache.commons.compress.archivers.zip;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.zip.ZipException;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class ExtraFieldUtils_ESTestTest23 extends ExtraFieldUtils_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link ExtraFieldUtils} class.
+ */
+public class ExtraFieldUtilsTest {
 
-    @Test(timeout = 4000)
-    public void test22() throws Throwable {
-        // Undeclared exception!
-        try {
-            ExtraFieldUtils.createExtraFieldNoDefault((ZipShort) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-        }
+    /**
+     * Tests that calling {@code createExtraFieldNoDefault} with a null header ID
+     * correctly throws a {@code NullPointerException}.
+     */
+    @Test(expected = NullPointerException.class)
+    public void createExtraFieldNoDefaultWithNullHeaderIdThrowsNullPointerException() {
+        // This call should fail with a NullPointerException because the argument is null.
+        ExtraFieldUtils.createExtraFieldNoDefault(null);
     }
 }
