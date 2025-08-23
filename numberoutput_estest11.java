b7cbaaccf46cdd8343fbb57170ec1b512e00a642
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:04:56 +0800

    Original processedAutoTestCases numberoutput_estest11.java

---
+++ numberoutput_estest11.java
@@ -0,0 +1,17 @@
+package com.fasterxml.jackson.core.io;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class NumberOutput_ESTestTest11 extends NumberOutput_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test10() throws Throwable {
+        String string0 = NumberOutput.toString(11L);
+        assertEquals("11", string0);
+    }
+}
