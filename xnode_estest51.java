40ba2cff0fb50b010b05320c0b3f46e000c11fce
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:29:00 +0800

    gemini processedAutoTestCases xnode_estest51.java

--- xnode_estest51.java
+++ xnode_estest51.java
@@ -1,38 +1,31 @@
 package org.apache.ibatis.parsing;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.List;
-import java.util.Locale;
-import java.util.Properties;
-import java.util.function.Supplier;
+
 import javax.imageio.metadata.IIOMetadataNode;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
-import org.w3c.dom.Document;
+import java.util.Properties;
 import org.w3c.dom.Node;
-import org.xml.sax.ext.DefaultHandler2;
 
-public class XNode_ESTestTest51 extends XNode_ESTest_scaffolding {
+/**
+ * Test suite for the {@link XNode} class.
+ */
+public class XNodeTest {
 
-    @Test(timeout = 4000)
-    public void test050() throws Throwable {
-        Properties properties0 = new Properties();
-        IIOMetadataNode iIOMetadataNode0 = new IIOMetadataNode();
-        XNode xNode0 = new XNode((XPathParser) null, iIOMetadataNode0, properties0);
-        Integer integer0 = Integer.valueOf(116);
-        // Undeclared exception!
-        try {
-            xNode0.getIntAttribute((String) null, integer0);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-        }
+    /**
+     * Verifies that getIntAttribute throws a NullPointerException
+     * when the attribute name provided is null.
+     */
+    @Test(expected = NullPointerException.class)
+    public void getIntAttributeWithNullNameShouldThrowException() {
+        // Arrange: Create an XNode with minimal valid dependencies.
+        // The XPathParser is not used by getIntAttribute, so it can be null.
+        Node domNode = new IIOMetadataNode();
+        Properties variables = new Properties();
+        XNode xNode = new XNode(null, domNode, variables);
+        Integer anyDefaultValue = 100;
+
+        // Act: Call the method under test with a null attribute name.
+        // The @Test(expected) annotation asserts that a NullPointerException is thrown.
+        xNode.getIntAttribute(null, anyDefaultValue);
     }
 }
