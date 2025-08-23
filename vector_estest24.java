2680fa5ae76ad148a107e726682934b1a31bd1ca
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:26:17 +0800

    gemini processedAutoTestCases vector_estest24.java

--- vector_estest24.java
+++ vector_estest24.java
@@ -1,19 +1,28 @@
 package com.itextpdf.text.pdf.parser;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertFalse;
 
-public class Vector_ESTestTest24 extends Vector_ESTest_scaffolding {
+/**
+ * Contains unit tests for the {@link Vector} class.
+ */
+public class VectorTest {
 
-    @Test(timeout = 4000)
-    public void test23() throws Throwable {
-        Vector vector0 = new Vector(0.0F, 0.0F, 0.0F);
-        Object object0 = new Object();
-        boolean boolean0 = vector0.equals(object0);
-        assertFalse(boolean0);
+    /**
+     * Verifies that the equals() method returns false when a Vector is
+     * compared with an object of an incompatible type. This is a standard
+     * requirement of the equals() contract.
+     */
+    @Test
+    public void equals_shouldReturnFalse_whenComparedWithIncompatibleType() {
+        // Arrange
+        Vector vector = new Vector(1.0f, 2.0f, 3.0f);
+        Object incompatibleObject = new Object();
+
+        // Act
+        boolean areEqual = vector.equals(incompatibleObject);
+
+        // Assert
+        assertFalse("Vector.equals() must return false when compared to an object of a different type.", areEqual);
     }
 }
