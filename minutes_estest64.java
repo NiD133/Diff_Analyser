4570c14c8dda153c0b6d720fd82acf4496d07886
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:03:23 +0800

    gemini processedAutoTestCases minutes_estest64.java

--- minutes_estest64.java
+++ minutes_estest64.java
@@ -1,26 +1,21 @@
 package org.joda.time;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class Minutes_ESTestTest64 extends Minutes_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link Minutes} class, focusing on arithmetic operations.
+ */
+public class MinutesTest {
 
-    @Test(timeout = 4000)
-    public void test63() throws Throwable {
-        Minutes minutes0 = Minutes.MAX_VALUE;
-        // Undeclared exception!
-        try {
-            minutes0.minus((-2550));
-            fail("Expecting exception: ArithmeticException");
-        } catch (ArithmeticException e) {
-            //
-            // The calculation caused an overflow: 2147483647 + 2550
-            //
-            verifyException("org.joda.time.field.FieldUtils", e);
-        }
+    /**
+     * Tests that subtracting a negative value from MAX_VALUE causes an overflow.
+     * The operation {@code MAX_VALUE.minus(-1)} is equivalent to adding 1 to
+     * {@code Integer.MAX_VALUE}, which is expected to throw an {@link ArithmeticException}.
+     */
+    @Test(expected = ArithmeticException.class)
+    public void minus_fromMaxValueWithNegativeValue_throwsArithmeticExceptionOnOverflow() {
+        // Attempting to subtract a negative number from the maximum value
+        // should result in an integer overflow.
+        Minutes.MAX_VALUE.minus(-1);
     }
 }
