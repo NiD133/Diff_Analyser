0f8c2f6b0c939d8c5b0bf00e75083a2c026f95d6
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:10:57 +0800

    Original processedAutoTestCases randomaccessfilemode_estest14.java

---
+++ randomaccessfilemode_estest14.java
@@ -0,0 +1,37 @@
+package org.apache.commons.io;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.file.OpenOption;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import org.apache.commons.io.function.IOConsumer;
+import org.apache.commons.io.function.IOFunction;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.evosuite.runtime.testdata.EvoSuiteFile;
+import org.evosuite.runtime.testdata.FileSystemHandling;
+import org.junit.runner.RunWith;
+
+public class RandomAccessFileMode_ESTestTest14 extends RandomAccessFileMode_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test13() throws Throwable {
+        // Undeclared exception!
+        try {
+            RandomAccessFileMode.valueOf((OpenOption[]) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.apache.commons.io.RandomAccessFileMode", e);
+        }
+    }
+}
