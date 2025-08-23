4cda439b41e288eba45680128101f5d11b0d1f19
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:11:31 +0800

    Original processedAutoTestCases randomaccessfileorarray_estest157.java

---
+++ randomaccessfileorarray_estest157.java
@@ -0,0 +1,40 @@
+package com.itextpdf.text.pdf;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.itextpdf.text.io.GetBufferedRandomAccessSource;
+import com.itextpdf.text.io.IndependentRandomAccessSource;
+import com.itextpdf.text.io.RandomAccessSource;
+import com.itextpdf.text.io.WindowRandomAccessSource;
+import java.io.ByteArrayInputStream;
+import java.io.EOFException;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PipedInputStream;
+import java.net.URL;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.net.MockURL;
+import org.evosuite.runtime.testdata.EvoSuiteFile;
+import org.evosuite.runtime.testdata.FileSystemHandling;
+import org.junit.runner.RunWith;
+
+public class RandomAccessFileOrArray_ESTestTest157 extends RandomAccessFileOrArray_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test156() throws Throwable {
+        PipedInputStream pipedInputStream0 = new PipedInputStream();
+        RandomAccessFileOrArray randomAccessFileOrArray0 = null;
+        try {
+            randomAccessFileOrArray0 = new RandomAccessFileOrArray(pipedInputStream0);
+            fail("Expecting exception: IOException");
+        } catch (Throwable e) {
+            //
+            // Pipe not connected
+            //
+            verifyException("java.io.PipedInputStream", e);
+        }
+    }
+}
