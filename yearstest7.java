14a006677b60fb012731efbf6072a16973c5c721
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:36:31 +0800

    gemini processedManualTestCases yearstest7.java

--- yearstest7.java
+++ yearstest7.java
@@ -1,36 +1,22 @@
 package org.joda.time;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
-public class YearsTestTest7 extends TestCase {
+/**
+ * Unit tests for the {@link Years} class.
+ */
+public class YearsTest {
 
-    // (before the late 90's they were all over the place)
-    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
+    @Test
+    public void getYears_shouldReturnTheCorrectNumberOfYears() {
+        // Arrange: Create a Years instance representing 20 years.
+        Years twentyYears = Years.years(20);
 
-    public static void main(String[] args) {
-        junit.textui.TestRunner.run(suite());
-    }
+        // Act: Retrieve the number of years from the instance.
+        int actualYears = twentyYears.getYears();
 
-    public static TestSuite suite() {
-        return new TestSuite(TestYears.class);
-    }
-
-    @Override
-    protected void setUp() throws Exception {
-    }
-
-    @Override
-    protected void tearDown() throws Exception {
-    }
-
-    //-----------------------------------------------------------------------
-    public void testGetMethods() {
-        Years test = Years.years(20);
-        assertEquals(20, test.getYears());
+        // Assert: The retrieved value should match the initial value.
+        assertEquals(20, actualYears);
     }
 }
