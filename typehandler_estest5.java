d36aea379e78c13a804f6515f8515ae6b324d9e8
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:24:20 +0800

    Original processedAutoTestCases typehandler_estest5.java

---
+++ typehandler_estest5.java
@@ -0,0 +1,21 @@
+package org.apache.commons.cli;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.File;
+import java.util.Date;
+import java.util.Map;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.lang.MockThrowable;
+import org.junit.runner.RunWith;
+
+public class TypeHandler_ESTestTest5 extends TypeHandler_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test04() throws Throwable {
+        File file0 = TypeHandler.createFile("wnU<h1?h~G9CL");
+        assertFalse(file0.isDirectory());
+    }
+}
