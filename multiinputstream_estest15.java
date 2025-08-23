22a241fe637fdd22d7505211adeb371bbb1cb4a3
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:04:00 +0800

    Original processedAutoTestCases multiinputstream_estest15.java

---
+++ multiinputstream_estest15.java
@@ -0,0 +1,31 @@
+package com.google.common.io;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.ListIterator;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class MultiInputStream_ESTestTest15 extends MultiInputStream_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test14() throws Throwable {
+        LinkedList<ByteSource> linkedList0 = new LinkedList<ByteSource>();
+        byte[] byteArray0 = new byte[8];
+        ByteSource byteSource0 = ByteSource.wrap(byteArray0);
+        linkedList0.add(byteSource0);
+        Iterator<ByteSource> iterator0 = linkedList0.descendingIterator();
+        MultiInputStream multiInputStream0 = new MultiInputStream(iterator0);
+        long long0 = multiInputStream0.skip((-896L));
+        assertEquals(0L, long0);
+    }
+}
