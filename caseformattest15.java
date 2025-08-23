851b6a6f0eac59b2a67d7615e480ba20315b970a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:26:20 +0800

    gemini processedManualTestCases caseformattest15.java

--- caseformattest15.java
+++ caseformattest15.java
@@ -1,24 +1,29 @@
 package com.google.common.base;
 
 import static com.google.common.base.CaseFormat.LOWER_CAMEL;
-import static com.google.common.base.CaseFormat.LOWER_HYPHEN;
-import static com.google.common.base.CaseFormat.LOWER_UNDERSCORE;
-import static com.google.common.base.CaseFormat.UPPER_CAMEL;
-import static com.google.common.base.CaseFormat.UPPER_UNDERSCORE;
 import static com.google.common.truth.Truth.assertThat;
-import static com.google.common.truth.Truth.assertWithMessage;
-import com.google.common.annotations.GwtCompatible;
-import com.google.common.annotations.GwtIncompatible;
-import com.google.common.annotations.J2ktIncompatible;
-import com.google.common.testing.NullPointerTester;
-import com.google.common.testing.SerializableTester;
-import junit.framework.TestCase;
-import org.jspecify.annotations.NullUnmarked;
 
-public class CaseFormatTestTest15 extends TestCase {
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
-    public void testLowerCamelToLowerCamel() {
-        assertThat(LOWER_CAMEL.to(LOWER_CAMEL, "foo")).isEqualTo("foo");
-        assertThat(LOWER_CAMEL.to(LOWER_CAMEL, "fooBar")).isEqualTo("fooBar");
-    }
+/**
+ * Tests for {@link CaseFormat}.
+ */
+@RunWith(JUnit4.class)
+public class CaseFormatTest {
+
+  /**
+   * Verifies that converting a string from a format to the exact same format
+   * results in the original, unchanged string.
+   */
+  @Test
+  public void to_identityConversionForLowerCamel_returnsOriginalString() {
+    // A string converted to its own format should not change.
+    String singleWord = "foo";
+    assertThat(LOWER_CAMEL.to(LOWER_CAMEL, singleWord)).isEqualTo(singleWord);
+
+    String multiWord = "fooBar";
+    assertThat(LOWER_CAMEL.to(LOWER_CAMEL, multiWord)).isEqualTo(multiWord);
+  }
 }
