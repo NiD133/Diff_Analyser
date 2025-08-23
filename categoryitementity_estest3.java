3d0cd0522aa0fdd41f9798926b7264d1f127db3f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:41:47 +0800

    gemini processedAutoTestCases categoryitementity_estest3.java

--- categoryitementity_estest3.java
+++ categoryitementity_estest3.java
@@ -1,37 +1,41 @@
 package org.jfree.chart.entity;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.awt.Polygon;
-import java.awt.Rectangle;
-import java.awt.geom.Rectangle2D;
-import java.time.Clock;
-import java.time.LocalDate;
-import java.time.chrono.ChronoLocalDate;
-import java.time.chrono.JapaneseDate;
-import javax.swing.JLayeredPane;
-import javax.swing.text.DefaultCaret;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.time.MockClock;
-import org.evosuite.runtime.mock.java.time.MockLocalDate;
-import org.evosuite.runtime.mock.java.time.chrono.MockJapaneseDate;
 import org.jfree.data.category.CategoryDataset;
-import org.jfree.data.statistics.DefaultBoxAndWhiskerCategoryDataset;
 import org.jfree.data.statistics.DefaultMultiValueCategoryDataset;
-import org.jfree.data.statistics.DefaultStatisticalCategoryDataset;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
+import java.awt.Rectangle;
+import java.awt.Shape;
+
+import static org.junit.Assert.assertNull;
+
+/**
+ * Unit tests for the {@link CategoryItemEntity} class.
+ *
+ * Note: The original test class name 'CategoryItemEntity_ESTestTest3' and its scaffolding
+ * are artifacts from the EvoSuite test generation tool. In a real-world scenario,
+ * this would be part of a single, more comprehensive 'CategoryItemEntityTest' class.
+ */
 public class CategoryItemEntity_ESTestTest3 extends CategoryItemEntity_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test02() throws Throwable {
-        DefaultCaret defaultCaret0 = new DefaultCaret();
-        DefaultMultiValueCategoryDataset<Integer, Integer> defaultMultiValueCategoryDataset0 = new DefaultMultiValueCategoryDataset<Integer, Integer>();
-        CategoryItemEntity<Integer, Integer> categoryItemEntity0 = new CategoryItemEntity<Integer, Integer>(defaultCaret0, "N`", "N`", defaultMultiValueCategoryDataset0, (Integer) 0, (Integer) 0);
-        categoryItemEntity0.setColumnKey((Integer) null);
-        Integer integer0 = categoryItemEntity0.getColumnKey();
-        assertNull(integer0);
+    /**
+     * Verifies that the column key can be set to null and is retrieved as such.
+     * This test checks the actual behavior of the setter, which allows a null value
+     * even though the method's Javadoc documentation suggests it is not permitted.
+     */
+    @Test
+    public void getColumnKey_afterSettingToNull_returnsNull() {
+        // Arrange
+        Shape testArea = new Rectangle();
+        CategoryDataset<Integer, Integer> testDataset = new DefaultMultiValueCategoryDataset<>();
+        CategoryItemEntity<Integer, Integer> entity = new CategoryItemEntity<>(
+                testArea, "Test Tooltip", "Test URL", testDataset, 1, 2);
+
+        // Act
+        entity.setColumnKey(null);
+        Integer actualColumnKey = entity.getColumnKey();
+
+        // Assert
+        assertNull("The column key should be null after being explicitly set to null.", actualColumnKey);
     }
 }
