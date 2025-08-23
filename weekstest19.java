1ae5695309098d7bcca8ad62bed55157132aa32b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:35:52 +0800

    Original processedManualTestCases weekstest19.java

---
+++ weekstest19.java
@@ -0,0 +1,38 @@
+package org.joda.time;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class WeeksTestTest19 extends TestCase {
+
+    // (before the late 90's they were all over the place)
+    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
+
+    public static TestSuite suite() {
+        return new TestSuite(TestWeeks.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+    }
+
+    public void testToStandardDuration() {
+        Weeks test = Weeks.weeks(20);
+        Duration expected = new Duration(20L * DateTimeConstants.MILLIS_PER_WEEK);
+        assertEquals(expected, test.toStandardDuration());
+        expected = new Duration(((long) Integer.MAX_VALUE) * DateTimeConstants.MILLIS_PER_WEEK);
+        assertEquals(expected, Weeks.MAX_VALUE.toStandardDuration());
+    }
+}
