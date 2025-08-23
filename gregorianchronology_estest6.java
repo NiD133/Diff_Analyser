9d87b099cbbf37fe96f45998783321ba4939c0e4
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:52:11 +0800

    gemini processedAutoTestCases gregorianchronology_estest6.java

--- gregorianchronology_estest6.java
+++ gregorianchronology_estest6.java
@@ -1,23 +1,37 @@
 package org.joda.time.chrono;
 
+import org.joda.time.DateTimeConstants;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.TimeZone;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.joda.time.Chronology;
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
-import org.joda.time.LocalDateTime;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class GregorianChronology_ESTestTest6 extends GregorianChronology_ESTest_scaffolding {
+/**
+ * Unit tests for {@link GregorianChronology}.
+ */
+public class GregorianChronologyTest {
 
-    @Test(timeout = 4000)
-    public void test05() throws Throwable {
-        GregorianChronology gregorianChronology0 = GregorianChronology.getInstance();
-        long long0 = gregorianChronology0.getApproxMillisAtEpochDividedByTwo();
-        assertEquals(31083597720000L, long0);
+    @Test
+    public void getApproxMillisAtEpochDividedByTwo_returnsCorrectApproximation() {
+        // This test verifies the constant value returned by getApproxMillisAtEpochDividedByTwo().
+        // This value is an approximation of half the number of milliseconds between
+        // year 0 and the epoch (1970-01-01). It's calculated based on the average
+        // length of a Gregorian year (365.2425 days).
+
+        // Arrange
+        GregorianChronology gregorianChronology = GregorianChronology.getInstanceUTC();
+
+        // The calculation is: (1970 years * average_millis_per_year) / 2
+        final long AVERAGE_MILLIS_PER_GREGORIAN_YEAR =
+                (long) (365.2425 * DateTimeConstants.MILLIS_PER_DAY);
+        final long YEARS_FROM_0_TO_EPOCH = 1970L;
+
+        long expectedValue = (YEARS_FROM_0_TO_EPOCH * AVERAGE_MILLIS_PER_GREGORIAN_YEAR) / 2;
+        // The original test asserted against the magic number 31083597720000L.
+        // Our calculation derives this number, making the test self-documenting.
+
+        // Act
+        long actualValue = gregorianChronology.getApproxMillisAtEpochDividedByTwo();
+
+        // Assert
+        assertEquals(expectedValue, actualValue);
     }
 }
