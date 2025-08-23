f0c04f41470d977856d59302671bc775c76a6a47
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:24:54 +0800

    gemini processedAutoTestCases urlcodec_estest28.java

--- urlcodec_estest28.java
+++ urlcodec_estest28.java
@@ -1,23 +1,27 @@
 package org.apache.commons.codec.net;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
 import java.io.UnsupportedEncodingException;
-import java.util.BitSet;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class URLCodec_ESTestTest28 extends URLCodec_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link URLCodec} class.
+ */
+public class URLCodecTest {
 
-    @Test(timeout = 4000)
-    public void test27() throws Throwable {
-        URLCodec uRLCodec0 = new URLCodec();
-        try {
-            uRLCodec0.decode("org.apache.commons.codec.DecoderException", "org.apache.commons.codec.DecoderException");
-            fail("Expecting exception: UnsupportedEncodingException");
-        } catch (UnsupportedEncodingException e) {
-        }
+    /**
+     * Tests that the decode method throws an UnsupportedEncodingException when
+     * provided with an invalid or unsupported character set name.
+     */
+    @Test(expected = UnsupportedEncodingException.class)
+    public void testDecodeWithUnsupportedEncodingThrowsException() throws Exception {
+        // Arrange
+        final URLCodec urlCodec = new URLCodec();
+        final String invalidCharset = "INVALID-ENCODING-NAME";
+        final String input = "any string";
+
+        // Act: Attempt to decode a string using an unsupported encoding.
+        // Assert: The test expects an UnsupportedEncodingException to be thrown,
+        // which is handled by the @Test(expected=...) annotation.
+        urlCodec.decode(input, invalidCharset);
     }
 }
