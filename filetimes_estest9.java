8d563987dbb531b1d9d9a6e773b3883edc70928c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:50:59 +0800

    Original processedAutoTestCases filetimes_estest9.java

---
+++ filetimes_estest9.java
@@ -0,0 +1,27 @@
+package org.apache.commons.io.file.attribute;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.math.BigDecimal;
+import java.nio.file.Path;
+import java.nio.file.attribute.FileTime;
+import java.time.DateTimeException;
+import java.time.Instant;
+import java.util.Date;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.time.MockInstant;
+import org.junit.runner.RunWith;
+
+public class FileTimes_ESTestTest9 extends FileTimes_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test08() throws Throwable {
+        BigDecimal bigDecimal0 = new BigDecimal(0.0);
+        Date date0 = FileTimes.ntfsTimeToDate(bigDecimal0);
+        FileTime fileTime0 = FileTimes.toFileTime(date0);
+        long long0 = FileTimes.toNtfsTime(fileTime0);
+        assertEquals(0L, long0);
+    }
+}
