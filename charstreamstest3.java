c61272faf15c3a9d9496f8d7c28bd1a9e7067a1f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:26:44 +0800

    gemini processedManualTestCases charstreamstest3.java

--- charstreamstest3.java
+++ charstreamstest3.java
@@ -1,141 +1,109 @@
 package com.google.common.io;
 
-import static org.junit.Assert.assertThrows;
-import com.google.common.base.Strings;
-import com.google.common.collect.ImmutableList;
-import java.io.EOFException;
-import java.io.FilterReader;
+import static org.junit.Assert.assertEquals;
+
 import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
-import java.io.StringWriter;
-import java.io.Writer;
-import java.nio.CharBuffer;
-import java.util.List;
-import org.jspecify.annotations.NullUnmarked;
 
-public class CharStreamsTestTest3 extends IoTestCase {
+/**
+ * Tests for {@link CharStreams#readLines(Readable, LineProcessor)}.
+ *
+ * <p>This test class focuses on verifying the behavior of the LineProcessor, specifically its
+ * ability to control the reading process by returning true or false.
+ */
+public class CharStreamsReadLinesWithLineProcessorTest extends IoTestCase {
 
-    private static final String TEXT = "The quick brown fox jumped over the lazy dog.";
-
-    /**
-     * Returns a reader wrapping the given reader that only reads half of the maximum number of
-     * characters that it could read in read(char[], int, int).
-     */
-    private static Reader newNonBufferFillingReader(Reader reader) {
-        return new FilterReader(reader) {
-
-            @Override
-            public int read(char[] cbuf, int off, int len) throws IOException {
-                // if a buffer isn't being cleared correctly, this method will eventually start being called
-                // with a len of 0 forever
-                if (len <= 0) {
-                    fail("read called with a len of " + len);
-                }
-                // read fewer than the max number of chars to read
-                // shouldn't be a problem unless the buffer is shrinking each call
-                return in.read(cbuf, off, Math.max(len - 1024, 0));
-            }
-        };
-    }
+    private static final String MULTILINE_TEXT = "a\nb\nc";
 
     /**
-     * Wrap an appendable in an appendable to defeat any type specific optimizations.
+     * A LineProcessor that counts processed lines and can be configured to stop after a certain
+     * number of lines. It can also accumulate the processed lines into a StringBuilder.
      */
-    private static Appendable wrapAsGenericAppendable(Appendable a) {
-        return new Appendable() {
+    private static class TestLineProcessor implements LineProcessor<Integer> {
+        private final int stopAfterLineCount;
+        private final StringBuilder processedLinesBuilder;
+        private int linesSeen = 0;
 
-            @Override
-            public Appendable append(CharSequence csq) throws IOException {
-                a.append(csq);
-                return this;
-            }
+        /**
+         * Creates a processor that stops after a specified number of lines.
+         *
+         * @param stopAfterLineCount The number of lines to process before returning false.
+         *     Processing continues as long as {@code linesSeen < stopAfterLineCount}.
+         */
+        TestLineProcessor(int stopAfterLineCount) {
+            this(stopAfterLineCount, null);
+        }
 
-            @Override
-            public Appendable append(CharSequence csq, int start, int end) throws IOException {
-                a.append(csq, start, end);
-                return this;
-            }
+        /**
+         * Creates a processor that stops after a specified number of lines and appends processed
+         * lines to a StringBuilder.
+         *
+         * @param stopAfterLineCount The number of lines to process before returning false.
+         * @param processedLinesBuilder The builder to append lines to, or null if not needed.
+         */
+        TestLineProcessor(int stopAfterLineCount, StringBuilder processedLinesBuilder) {
+            this.stopAfterLineCount = stopAfterLineCount;
+            this.processedLinesBuilder = processedLinesBuilder;
+        }
 
-            @Override
-            public Appendable append(char c) throws IOException {
-                a.append(c);
-                return this;
+        @Override
+        public boolean processLine(String line) {
+            linesSeen++;
+            if (processedLinesBuilder != null) {
+                processedLinesBuilder.append(line);
             }
-        };
+            // Return true to continue processing, false to stop.
+            return linesSeen < stopAfterLineCount;
+        }
+
+        @Override
+        public Integer getResult() {
+            return linesSeen;
+        }
     }
 
-    /**
-     * Wrap a readable in a readable to defeat any type specific optimizations.
-     */
-    private static Readable wrapAsGenericReadable(Readable a) {
-        return new Readable() {
+    public void testReadLines_processesAllLinesWhenProcessorAlwaysReturnsTrue() throws IOException {
+        // Arrange
+        Reader reader = new StringReader(MULTILINE_TEXT);
+        // A processor that will see 3 lines and stop after 3, effectively processing all lines.
+        LineProcessor<Integer> allLinesProcessor = new TestLineProcessor(3);
 
-            @Override
-            public int read(CharBuffer cb) throws IOException {
-                return a.read(cb);
-            }
-        };
+        // Act
+        Integer linesProcessed = CharStreams.readLines(reader, allLinesProcessor);
+
+        // Assert
+        assertEquals("Should have processed all 3 lines", 3, linesProcessed.intValue());
     }
 
-    public void testReadLines_withLineProcessor() throws IOException {
-        String text = "a\nb\nc";
-        // Test a LineProcessor that always returns false.
-        Reader r = new StringReader(text);
-        LineProcessor<Integer> alwaysFalse = new LineProcessor<Integer>() {
+    public void testReadLines_stopsAfterFirstLineWhenProcessorReturnsFalse() throws IOException {
+        // Arrange
+        Reader reader = new StringReader(MULTILINE_TEXT);
+        // A processor configured to stop after processing the first line.
+        LineProcessor<Integer> stopEarlyProcessor = new TestLineProcessor(1);
 
-            int seen;
+        // Act
+        Integer linesProcessed = CharStreams.readLines(reader, stopEarlyProcessor);
 
-            @Override
-            public boolean processLine(String line) {
-                seen++;
-                return false;
-            }
+        // Assert
+        assertEquals("Should have stopped processing after the first line", 1, linesProcessed.intValue());
+    }
 
-            @Override
-            public Integer getResult() {
-                return seen;
-            }
-        };
-        assertEquals("processLine was called more than once", 1, CharStreams.readLines(r, alwaysFalse).intValue());
-        // Test a LineProcessor that always returns true.
-        r = new StringReader(text);
-        LineProcessor<Integer> alwaysTrue = new LineProcessor<Integer>() {
+    public void testReadLines_stopsConditionallyAndAccumulatesResult() throws IOException {
+        // Arrange
+        Reader reader = new StringReader(MULTILINE_TEXT);
+        StringBuilder processedContent = new StringBuilder();
+        // A processor configured to stop after the second line and accumulate the content.
+        LineProcessor<Integer> conditionalProcessor = new TestLineProcessor(2, processedContent);
 
-            int seen;
+        // Act
+        Integer linesProcessed = CharStreams.readLines(reader, conditionalProcessor);
 
-            @Override
-            public boolean processLine(String line) {
-                seen++;
-                return true;
-            }
-
-            @Override
-            public Integer getResult() {
-                return seen;
-            }
-        };
-        assertEquals("processLine was not called for all the lines", 3, CharStreams.readLines(r, alwaysTrue).intValue());
-        // Test a LineProcessor that is conditional.
-        r = new StringReader(text);
-        StringBuilder sb = new StringBuilder();
-        LineProcessor<Integer> conditional = new LineProcessor<Integer>() {
-
-            int seen;
-
-            @Override
-            public boolean processLine(String line) {
-                seen++;
-                sb.append(line);
-                return seen < 2;
-            }
-
-            @Override
-            public Integer getResult() {
-                return seen;
-            }
-        };
-        assertEquals(2, CharStreams.readLines(r, conditional).intValue());
-        assertEquals("ab", sb.toString());
+        // Assert
+        assertEquals("Should have stopped processing after the second line", 2, linesProcessed.intValue());
+        assertEquals(
+            "Should have accumulated the content of the first two lines",
+            "ab",
+            processedContent.toString());
     }
 }
