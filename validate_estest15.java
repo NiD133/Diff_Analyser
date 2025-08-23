485f93f705adbb4afa524b2d4b36c11aa7ad2b6c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:25:53 +0800

    Original processedAutoTestCases validate_estest15.java

---
+++ validate_estest15.java
@@ -0,0 +1,32 @@
+package org.jsoup.helper;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.FormatFlagsConversionMismatchException;
+import java.util.IllegalFormatConversionException;
+import java.util.IllegalFormatFlagsException;
+import java.util.IllegalFormatWidthException;
+import java.util.MissingFormatArgumentException;
+import java.util.MissingFormatWidthException;
+import java.util.UnknownFormatConversionException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class Validate_ESTestTest15 extends Validate_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test14() throws Throwable {
+        Object[] objectArray0 = new Object[0];
+        // Undeclared exception!
+        try {
+            Validate.ensureNotNull((Object) null, (String) null, objectArray0);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+        }
+    }
+}
