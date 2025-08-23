db405af1619d2b3e1e50e169948615771d87ea18
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:50:37 +0800

    gemini processedAutoTestCases fieldutils_estest80.java

--- fieldutils_estest80.java
+++ fieldutils_estest80.java
@@ -1,31 +1,19 @@
 package org.joda.time.field;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.math.RoundingMode;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.joda.time.DateTimeField;
-import org.joda.time.DateTimeFieldType;
-import org.joda.time.DateTimeZone;
-import org.joda.time.chrono.IslamicChronology;
-import org.joda.time.chrono.ZonedChronology;
-import org.junit.runner.RunWith;
 
-public class FieldUtils_ESTestTest80 extends FieldUtils_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link FieldUtils} class.
+ */
+public class FieldUtilsTest {
 
-    @Test(timeout = 4000)
-    public void test79() throws Throwable {
-        // Undeclared exception!
-        try {
-            FieldUtils.safeSubtract((-9223372036854775808L), 11L);
-            fail("Expecting exception: ArithmeticException");
-        } catch (ArithmeticException e) {
-            //
-            // The calculation caused an overflow: -9223372036854775808 - 11
-            //
-            verifyException("org.joda.time.field.FieldUtils", e);
-        }
+    /**
+     * Tests that safeSubtract throws an ArithmeticException when the subtraction
+     * results in a value less than Long.MIN_VALUE (underflow).
+     */
+    @Test(expected = ArithmeticException.class)
+    public void safeSubtract_shouldThrowExceptionOnUnderflow() {
+        // Subtracting any positive number from Long.MIN_VALUE will cause an underflow.
+        FieldUtils.safeSubtract(Long.MIN_VALUE, 11L);
     }
 }
