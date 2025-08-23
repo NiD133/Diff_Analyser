317236f4bfcfbbac37d387c996ae32e3afa6f642
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:09:17 +0800

    Original processedAutoTestCases precisedurationdatetimefield_estest20.java

---
+++ precisedurationdatetimefield_estest20.java
@@ -0,0 +1,41 @@
+package org.joda.time.field;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.TimeZone;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.joda.time.Chronology;
+import org.joda.time.DateTimeFieldType;
+import org.joda.time.DateTimeZone;
+import org.joda.time.Days;
+import org.joda.time.DurationField;
+import org.joda.time.DurationFieldType;
+import org.joda.time.LocalDate;
+import org.joda.time.LocalTime;
+import org.joda.time.Weeks;
+import org.joda.time.chrono.EthiopicChronology;
+import org.joda.time.chrono.GJChronology;
+import org.joda.time.chrono.GregorianChronology;
+import org.joda.time.chrono.IslamicChronology;
+import org.joda.time.chrono.JulianChronology;
+import org.joda.time.chrono.LenientChronology;
+import org.joda.time.chrono.ZonedChronology;
+import org.junit.runner.RunWith;
+
+public class PreciseDurationDateTimeField_ESTestTest20 extends PreciseDurationDateTimeField_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test19() throws Throwable {
+        DateTimeFieldType dateTimeFieldType0 = DateTimeFieldType.yearOfCentury();
+        MillisDurationField millisDurationField0 = (MillisDurationField) MillisDurationField.INSTANCE;
+        JulianChronology julianChronology0 = JulianChronology.getInstance();
+        DateTimeZone dateTimeZone0 = DateTimeZone.getDefault();
+        ZonedChronology zonedChronology0 = ZonedChronology.getInstance(julianChronology0, dateTimeZone0);
+        DurationField durationField0 = zonedChronology0.days();
+        PreciseDateTimeField preciseDateTimeField0 = new PreciseDateTimeField(dateTimeFieldType0, millisDurationField0, durationField0);
+        long long0 = preciseDateTimeField0.roundCeiling(100000000000000L);
+        assertEquals(100000000000000L, long0);
+    }
+}
