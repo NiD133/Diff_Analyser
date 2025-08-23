7e49a891b29abc3eb2c48e35c4c72d1bd7aa8324
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:56:41 +0800

    Original processedAutoTestCases jsonignoreproperties_estest58.java

---
+++ jsonignoreproperties_estest58.java
@@ -0,0 +1,22 @@
+package com.fasterxml.jackson.annotation;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.function.Predicate;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class JsonIgnoreProperties_ESTestTest58 extends JsonIgnoreProperties_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test57() throws Throwable {
+        JsonIgnoreProperties.Value jsonIgnoreProperties_Value0 = JsonIgnoreProperties.Value.empty();
+        JsonIgnoreProperties.Value jsonIgnoreProperties_Value1 = JsonIgnoreProperties.Value.merge(jsonIgnoreProperties_Value0, (JsonIgnoreProperties.Value) null);
+        assertTrue(jsonIgnoreProperties_Value1.getMerge());
+    }
+}
