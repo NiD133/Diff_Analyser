ade41e2d6107f3cbe19c5d4ebea059c3d1c3b406
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:18:55 +0800

    Original processedAutoTestCases stringutil_estest45.java

---
+++ stringutil_estest45.java
@@ -0,0 +1,32 @@
+package org.jsoup.internal;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Collection;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.stream.Collector;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.net.MockURL;
+import org.junit.runner.RunWith;
+
+public class StringUtil_ESTestTest45 extends StringUtil_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test44() throws Throwable {
+        LinkedList<Object> linkedList0 = new LinkedList<Object>();
+        linkedList0.add((Object) "");
+        linkedList0.offer("");
+        ListIterator<Object> listIterator0 = linkedList0.listIterator();
+        StringUtil.join((Iterator<?>) listIterator0, "");
+        assertFalse(listIterator0.hasNext());
+    }
+}
