f2dc14a5fbbcc0b169c1d805435fe05904debb29
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:29:01 +0800

    gemini processedAutoTestCases xnode_estest53.java

--- xnode_estest53.java
+++ xnode_estest53.java
@@ -1,38 +1,31 @@
 package org.apache.ibatis.parsing;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.List;
-import java.util.Locale;
-import java.util.Properties;
-import java.util.function.Supplier;
-import javax.imageio.metadata.IIOMetadataNode;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.xml.sax.ext.DefaultHandler2;
 
+import javax.imageio.metadata.IIOMetadataNode;
+import java.util.Properties;
+import org.w3c.dom.Node;
+
+/**
+ * Test suite for the XNode class, focusing on attribute parsing behavior.
+ */
 public class XNode_ESTestTest53 extends XNode_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test052() throws Throwable {
-        Properties properties0 = new Properties();
-        IIOMetadataNode iIOMetadataNode0 = new IIOMetadataNode();
-        XNode xNode0 = new XNode((XPathParser) null, iIOMetadataNode0, properties0);
-        Float float0 = new Float(0.0F);
-        // Undeclared exception!
-        try {
-            xNode0.getFloatAttribute((String) null, float0);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-        }
+    /**
+     * Verifies that getFloatAttribute throws a NullPointerException when the attribute name is null.
+     * This is the expected behavior because the underlying Properties map, which stores
+     * the attributes, does not permit null keys.
+     */
+    @Test(expected = NullPointerException.class)
+    public void getFloatAttributeShouldThrowNPEForNullAttributeName() {
+        // Arrange: Create an XNode instance.
+        // The XPathParser is not needed for this test, so it can be null.
+        // The node itself can be a simple, empty node.
+        Node domNode = new IIOMetadataNode();
+        XNode xNode = new XNode(null, domNode, new Properties());
+        Float defaultValue = 0.0F;
+
+        // Act & Assert: Attempting to get an attribute with a null name should throw.
+        xNode.getFloatAttribute(null, defaultValue);
     }
 }
