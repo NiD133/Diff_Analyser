6823f0c36592c5ff3bd19985966555bf78b7c430
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:53:15 +0800

    Original processedAutoTestCases hexdump_estest19.java

---
+++ hexdump_estest19.java
@@ -0,0 +1,42 @@
+package org.apache.commons.io;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PipedOutputStream;
+import java.io.PipedWriter;
+import java.io.StringWriter;
+import java.nio.BufferOverflowException;
+import java.nio.CharBuffer;
+import java.nio.ReadOnlyBufferException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.evosuite.runtime.mock.java.io.MockFileOutputStream;
+import org.evosuite.runtime.mock.java.io.MockFileWriter;
+import org.evosuite.runtime.mock.java.io.MockPrintStream;
+import org.junit.runner.RunWith;
+
+public class HexDump_ESTestTest19 extends HexDump_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test18() throws Throwable {
+        PipedWriter pipedWriter0 = new PipedWriter();
+        byte[] byteArray0 = new byte[0];
+        // Undeclared exception!
+        try {
+            HexDump.dump(byteArray0, (Appendable) pipedWriter0);
+            fail("Expecting exception: ArrayIndexOutOfBoundsException");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            //
+            // illegal index: 0 into array of length 0
+            //
+            verifyException("org.apache.commons.io.HexDump", e);
+        }
+    }
+}
