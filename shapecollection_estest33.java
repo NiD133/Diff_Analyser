3524b1d105681b8ef20a3df1a542e6346c4fe19e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:15:31 +0800

    Original processedAutoTestCases shapecollection_estest33.java

---
+++ shapecollection_estest33.java
@@ -0,0 +1,35 @@
+package org.locationtech.spatial4j.shape;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Stack;
+import java.util.Vector;
+import java.util.function.Predicate;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+import org.locationtech.spatial4j.context.SpatialContext;
+import org.locationtech.spatial4j.context.SpatialContextFactory;
+import org.locationtech.spatial4j.distance.GeodesicSphereDistCalc;
+import org.locationtech.spatial4j.shape.impl.PointImpl;
+import org.locationtech.spatial4j.shape.jts.JtsPoint;
+
+public class ShapeCollection_ESTestTest33 extends ShapeCollection_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test32() throws Throwable {
+        ArrayList<JtsPoint> arrayList0 = new ArrayList<JtsPoint>();
+        SpatialContext spatialContext0 = SpatialContext.GEO;
+        PointImpl pointImpl0 = new PointImpl(0.5, 0.5, spatialContext0);
+        ShapeCollection<JtsPoint> shapeCollection0 = new ShapeCollection<JtsPoint>(arrayList0, spatialContext0);
+        Rectangle rectangle0 = shapeCollection0.getBoundingBox();
+        GeodesicSphereDistCalc.LawOfCosines geodesicSphereDistCalc_LawOfCosines0 = new GeodesicSphereDistCalc.LawOfCosines();
+        geodesicSphereDistCalc_LawOfCosines0.calcBoxByDistFromPt(pointImpl0, 0.5, spatialContext0, rectangle0);
+        shapeCollection0.relate(pointImpl0);
+    }
+}
