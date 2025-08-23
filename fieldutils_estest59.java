b3f3d94dd68bd123af0c2c9bed23a2125b822964
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:50:30 +0800

    gemini processedAutoTestCases fieldutils_estest59.java

--- fieldutils_estest59.java
+++ fieldutils_estest59.java
@@ -1,24 +1,31 @@
 package org.joda.time.field;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
 import java.math.RoundingMode;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.joda.time.DateTimeField;
-import org.joda.time.DateTimeFieldType;
-import org.joda.time.DateTimeZone;
-import org.joda.time.chrono.IslamicChronology;
-import org.joda.time.chrono.ZonedChronology;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class FieldUtils_ESTestTest59 extends FieldUtils_ESTest_scaffolding {
+/**
+ * Unit tests for the utility methods in {@link FieldUtils}.
+ */
+public class FieldUtilsTest {
 
-    @Test(timeout = 4000)
-    public void test58() throws Throwable {
-        RoundingMode roundingMode0 = RoundingMode.FLOOR;
-        long long0 = FieldUtils.safeDivide((long) Integer.MIN_VALUE, (long) Integer.MIN_VALUE, roundingMode0);
-        assertEquals(1L, long0);
+    /**
+     * Tests that safeDivide correctly returns 1 when dividing Integer.MIN_VALUE by itself.
+     * This is an edge case where the dividend and divisor are the same negative boundary value.
+     */
+    @Test
+    public void safeDivide_whenDividingIntegerMinValueByItself_shouldReturnOne() {
+        // Arrange
+        final long dividend = Integer.MIN_VALUE;
+        final long divisor = Integer.MIN_VALUE;
+        // The rounding mode doesn't affect the result here, as there is no remainder.
+        final RoundingMode roundingMode = RoundingMode.FLOOR;
+        final long expectedQuotient = 1L;
+
+        // Act
+        long actualQuotient = FieldUtils.safeDivide(dividend, divisor, roundingMode);
+
+        // Assert
+        assertEquals("Dividing a number by itself should result in 1", expectedQuotient, actualQuotient);
     }
 }
