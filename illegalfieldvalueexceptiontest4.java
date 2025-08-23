4aba82c8b42c95047b68b6df8a98899de1f422f8
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:29:22 +0800

    Original processedManualTestCases illegalfieldvalueexceptiontest4.java

---
+++ illegalfieldvalueexceptiontest4.java
@@ -0,0 +1,37 @@
+package org.joda.time;
+
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.joda.time.chrono.GJChronology;
+import org.joda.time.chrono.ISOChronology;
+import org.joda.time.chrono.JulianChronology;
+import org.joda.time.field.FieldUtils;
+import org.joda.time.field.SkipDateTimeField;
+
+public class IllegalFieldValueExceptionTestTest4 extends TestCase {
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
+
+    public static TestSuite suite() {
+        return new TestSuite(TestIllegalFieldValueException.class);
+    }
+
+    public void testZoneTransition() {
+        DateTime dt = new DateTime(2005, 4, 3, 1, 0, 0, 0, DateTimeZone.forID("America/Los_Angeles"));
+        try {
+            dt.hourOfDay().setCopy(2);
+            fail();
+        } catch (IllegalFieldValueException e) {
+            assertEquals(DateTimeFieldType.hourOfDay(), e.getDateTimeFieldType());
+            assertEquals(null, e.getDurationFieldType());
+            assertEquals("hourOfDay", e.getFieldName());
+            assertEquals(new Integer(2), e.getIllegalNumberValue());
+            assertEquals(null, e.getIllegalStringValue());
+            assertEquals("2", e.getIllegalValueAsString());
+            assertEquals(null, e.getLowerBound());
+            assertEquals(null, e.getUpperBound());
+        }
+    }
+}
