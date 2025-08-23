9762d01115d998acc7f63c5e161631981c1e9f1c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:33:51 +0800

    gemini processedManualTestCases soundextest1.java

--- soundextest1.java
+++ soundextest1.java
@@ -1,21 +1,48 @@
 package org.apache.commons.codec.language;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.stream.Stream;
 import org.apache.commons.codec.AbstractStringEncoderTest;
 import org.apache.commons.codec.EncoderException;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
-public class SoundexTestTest1 extends AbstractStringEncoderTest<Soundex> {
+/**
+ * Tests the Soundex algorithm with a variety of names that should encode to the same value.
+ */
+// The class name is simplified for clarity and follows standard conventions.
+public class SoundexTest extends AbstractStringEncoderTest<Soundex> {
 
     @Override
     protected Soundex createStringEncoder() {
         return new Soundex();
     }
 
-    @Test
-    void testB650() throws EncoderException {
-        checkEncodingVariations("B650", new String[] { "BARHAM", "BARONE", "BARRON", "BERNA", "BIRNEY", "BIRNIE", "BOOROM", "BOREN", "BORN", "BOURN", "BOURNE", "BOWRON", "BRAIN", "BRAME", "BRANN", "BRAUN", "BREEN", "BRIEN", "BRIM", "BRIMM", "BRINN", "BRION", "BROOM", "BROOME", "BROWN", "BROWNE", "BRUEN", "BRUHN", "BRUIN", "BRUMM", "BRUN", "BRUNO", "BRYAN", "BURIAN", "BURN", "BURNEY", "BYRAM", "BYRNE", "BYRON", "BYRUM" });
+    @DisplayName("Test that a variety of phonetically similar names are all encoded to B650")
+    @ParameterizedTest(name = "{index}: {0} -> B650")
+    @MethodSource("provideNamesForB650")
+    void shouldEncodeVariousSimilarNamesToB650(final String name) throws EncoderException {
+        // The 'getStringEncoder()' method is inherited from AbstractStringEncoderTest
+        // and provides an initialized Soundex instance.
+        assertEquals("B650", getStringEncoder().encode(name));
+    }
+
+    /**
+     * Provides a stream of names that are phonetically similar and should all be
+     * encoded to the Soundex code "B650".
+     *
+     * @return A stream of test names.
+     */
+    private static Stream<String> provideNamesForB650() {
+        return Stream.of(
+            "BARHAM", "BARONE", "BARRON", "BERNA", "BIRNEY", "BIRNIE", "BOOROM",
+            "BOREN", "BORN", "BOURN", "BOURNE", "BOWRON", "BRAIN", "BRAME",
+            "BRANN", "BRAUN", "BREEN", "BRIEN", "BRIM", "BRIMM", "BRINN",
+            "BRION", "BROOM", "BROOME", "BROWN", "BROWNE", "BRUEN", "BRUHN",
+            "BRUIN", "BRUMM", "BRUN", "BRUNO", "BRYAN", "BURIAN", "BURN",
+            "BURNEY", "BYRAM", "BYRNE", "BYRON", "BYRUM"
+        );
     }
 }
