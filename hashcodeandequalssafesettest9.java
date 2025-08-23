2dcc74dacfccb5f05f6b7e90670be4a2c004f1f7
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:29:19 +0800

    Original processedManualTestCases hashcodeandequalssafesettest9.java

---
+++ hashcodeandequalssafesettest9.java
@@ -0,0 +1,44 @@
+package org.mockito.internal.util.collections;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.mock;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Observer;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+public class HashCodeAndEqualsSafeSetTestTest9 {
+
+    @Rule
+    public MockitoRule r = MockitoJUnit.rule();
+
+    @Mock
+    private UnmockableHashCodeAndEquals mock1;
+
+    private static class UnmockableHashCodeAndEquals {
+
+        @Override
+        public final int hashCode() {
+            throw new NullPointerException("I'm failing on hashCode and I don't care");
+        }
+
+        @Override
+        public final boolean equals(Object obj) {
+            throw new NullPointerException("I'm failing on equals and I don't care");
+        }
+    }
+
+    @Test
+    public void toArray_just_work() throws Exception {
+        HashCodeAndEqualsSafeSet mocks = HashCodeAndEqualsSafeSet.of(mock1);
+        assertThat(mocks.toArray()[0]).isSameAs(mock1);
+        assertThat(mocks.toArray(new UnmockableHashCodeAndEquals[0])[0]).isSameAs(mock1);
+    }
+}
