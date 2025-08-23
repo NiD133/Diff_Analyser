f5da40e62eea2ccb9c5398a1318d2b6ad94d4520
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:26:41 +0800

    Original processedManualTestCases charsetutilstest4.java

---
+++ charsetutilstest4.java
@@ -0,0 +1,26 @@
+package org.apache.commons.lang3;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Modifier;
+import org.junit.jupiter.api.Test;
+
+public class CharSetUtilsTestTest4 extends AbstractLangTest {
+
+    @Test
+    void testCount_StringString() {
+        assertEquals(0, CharSetUtils.count(null, (String) null));
+        assertEquals(0, CharSetUtils.count(null, ""));
+        assertEquals(0, CharSetUtils.count("", (String) null));
+        assertEquals(0, CharSetUtils.count("", ""));
+        assertEquals(0, CharSetUtils.count("", "a-e"));
+        assertEquals(0, CharSetUtils.count("hello", (String) null));
+        assertEquals(0, CharSetUtils.count("hello", ""));
+        assertEquals(1, CharSetUtils.count("hello", "a-e"));
+        assertEquals(3, CharSetUtils.count("hello", "l-p"));
+    }
+}
