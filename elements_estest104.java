e31183ea5dd1c6d6169745e97f3ff9b3cc37d39a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:48:15 +0800

    Original processedAutoTestCases elements_estest104.java

---
+++ elements_estest104.java
@@ -0,0 +1,37 @@
+package org.jsoup.select;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.ConcurrentModificationException;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.jsoup.nodes.Comment;
+import org.jsoup.nodes.DataNode;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.FormElement;
+import org.jsoup.nodes.TextNode;
+import org.jsoup.parser.Parser;
+import org.junit.runner.RunWith;
+
+public class Elements_ESTestTest104 extends Elements_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test103() throws Throwable {
+        Elements elements0 = new Elements();
+        LinkedHashSet<Object> linkedHashSet0 = new LinkedHashSet<Object>();
+        linkedHashSet0.add(elements0);
+        boolean boolean0 = elements0.removeAll(linkedHashSet0);
+        assertFalse(boolean0);
+    }
+}
