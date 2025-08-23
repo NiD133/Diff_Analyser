45dd72f98c90c38f28fc67f09121b8e20cb72b67
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:24:51 +0800

    Original processedManualTestCases arraysortertest3.java

---
+++ arraysortertest3.java
@@ -0,0 +1,18 @@
+package org.apache.commons.lang3;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+
+public class ArraySorterTestTest3 extends AbstractLangTest {
+
+    @Test
+    void testSortComparable() {
+        final String[] array1 = ArrayUtils.toArray("foo", "bar");
+        final String[] array2 = array1.clone();
+        Arrays.sort(array1);
+        assertArrayEquals(array1, ArraySorter.sort(array2, String::compareTo));
+        assertNull(ArraySorter.sort((String[]) null));
+    }
+}
