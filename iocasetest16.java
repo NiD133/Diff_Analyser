0946421e59785777013c91fbea78b8c9a42a0a44
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:29:38 +0800

    Original processedManualTestCases iocasetest16.java

---
+++ iocasetest16.java
@@ -0,0 +1,50 @@
+package org.apache.commons.io;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+
+public class IOCaseTestTest16 {
+
+    private static final boolean WINDOWS = File.separatorChar == '\\';
+
+    private void assert0(final byte[] arr) {
+        for (final byte e : arr) {
+            assertEquals(0, e);
+        }
+    }
+
+    private void assert0(final char[] arr) {
+        for (final char e : arr) {
+            assertEquals(0, e);
+        }
+    }
+
+    private IOCase serialize(final IOCase value) throws Exception {
+        final ByteArrayOutputStream buf = new ByteArrayOutputStream();
+        try (ObjectOutputStream out = new ObjectOutputStream(buf)) {
+            out.writeObject(value);
+            out.flush();
+        }
+        final ByteArrayInputStream bufin = new ByteArrayInputStream(buf.toByteArray());
+        final ObjectInputStream in = new ObjectInputStream(bufin);
+        return (IOCase) in.readObject();
+    }
+
+    @Test
+    void test_getScratchByteArrayWriteOnly() {
+        final byte[] array = IOUtils.getScratchByteArrayWriteOnly();
+        assert0(array);
+        Arrays.fill(array, (byte) 1);
+        assert0(IOUtils.getScratchCharArray());
+    }
+}
