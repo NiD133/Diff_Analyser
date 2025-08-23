ae367835f0d2cfaca06cc9f0ea38ebb126b07cb7
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:31:12 +0800

    gemini processedManualTestCases minutestest27.java

--- minutestest27.java
+++ minutestest27.java
@@ -1,38 +1,27 @@
 package org.joda.time;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
-public class MinutesTestTest27 extends TestCase {
+/**
+ * Unit tests for the {@link Minutes} class, focusing on its interaction with other Joda-Time types.
+ */
+public class MinutesTest {
 
-    // (before the late 90's they were all over the place)
-    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
+    /**
+     * Tests that adding a Minutes object to a LocalDateTime correctly advances the time.
+     */
+    @Test
+    public void plus_whenAddedToLocalDateTime_advancesTimeByCorrectAmount() {
+        // Arrange
+        final Minutes minutesToAdd = Minutes.minutes(26);
+        final LocalDateTime startDateTime = new LocalDateTime(2006, 6, 1, 0, 0);
+        final LocalDateTime expectedDateTime = new LocalDateTime(2006, 6, 1, 0, 26);
 
-    public static void main(String[] args) {
-        junit.textui.TestRunner.run(suite());
-    }
+        // Act
+        final LocalDateTime resultDateTime = startDateTime.plus(minutesToAdd);
 
-    public static TestSuite suite() {
-        return new TestSuite(TestMinutes.class);
-    }
-
-    @Override
-    protected void setUp() throws Exception {
-    }
-
-    @Override
-    protected void tearDown() throws Exception {
-    }
-
-    //-----------------------------------------------------------------------
-    public void testAddToLocalDate() {
-        Minutes test = Minutes.minutes(26);
-        LocalDateTime date = new LocalDateTime(2006, 6, 1, 0, 0, 0, 0);
-        LocalDateTime expected = new LocalDateTime(2006, 6, 1, 0, 26, 0, 0);
-        assertEquals(expected, date.plus(test));
+        // Assert
+        assertEquals(expectedDateTime, resultDateTime);
     }
 }
