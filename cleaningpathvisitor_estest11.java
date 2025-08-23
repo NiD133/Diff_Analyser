0718677d8bf2caa30b6cb5fc8aee919bac06f5ba
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:43:32 +0800

    gemini processedAutoTestCases cleaningpathvisitor_estest11.java

--- cleaningpathvisitor_estest11.java
+++ cleaningpathvisitor_estest11.java
@@ -1,30 +1,28 @@
 package org.apache.commons.io.file;
 
+import static org.junit.Assert.assertFalse;
+
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.IOException;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Path;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.evosuite.runtime.mock.java.io.MockFile;
-import org.junit.runner.RunWith;
 
-public class CleaningPathVisitor_ESTestTest11 extends CleaningPathVisitor_ESTest_scaffolding {
+/**
+ * Tests for {@link CleaningPathVisitor}, focusing on the equals() method contract.
+ */
+public class CleaningPathVisitorTest {
 
-    @Test(timeout = 4000)
-    public void test10() throws Throwable {
-        CountingPathVisitor countingPathVisitor0 = CleaningPathVisitor.withBigIntegerCounters();
-        DeletingPathVisitor deletingPathVisitor0 = DeletingPathVisitor.withLongCounters();
-        boolean boolean0 = countingPathVisitor0.equals(deletingPathVisitor0);
-        assertFalse(boolean0);
+    /**
+     * Tests that a CleaningPathVisitor instance is not equal to an instance of a different class
+     * (in this case, DeletingPathVisitor), as required by the equals() contract.
+     */
+    @Test
+    public void testEqualsReturnsFalseWhenComparedWithDifferentClass() {
+        // Arrange: Create an instance of the class under test and an instance of a different class.
+        final CountingPathVisitor cleaningVisitor = CleaningPathVisitor.withBigIntegerCounters();
+        final Object otherVisitor = DeletingPathVisitor.withLongCounters();
+
+        // Act: Compare the two objects using the equals() method.
+        final boolean areEqual = cleaningVisitor.equals(otherVisitor);
+
+        // Assert: Verify that the result is false.
+        assertFalse("A CleaningPathVisitor should not be equal to a DeletingPathVisitor.", areEqual);
     }
 }
