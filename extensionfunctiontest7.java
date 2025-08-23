fc807e0f2138e1726feb7a980b5190f196ce436d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:28:39 +0800

    gemini processedManualTestCases extensionfunctiontest7.java

--- extensionfunctiontest7.java
+++ extensionfunctiontest7.java
@@ -1,100 +1,59 @@
 package org.apache.commons.jxpath.ri.compiler;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertInstanceOf;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Locale;
 import org.apache.commons.jxpath.AbstractJXPathTest;
 import org.apache.commons.jxpath.ClassFunctions;
-import org.apache.commons.jxpath.ExpressionContext;
-import org.apache.commons.jxpath.Function;
 import org.apache.commons.jxpath.FunctionLibrary;
-import org.apache.commons.jxpath.Functions;
 import org.apache.commons.jxpath.JXPathContext;
 import org.apache.commons.jxpath.NodeSet;
-import org.apache.commons.jxpath.PackageFunctions;
-import org.apache.commons.jxpath.Pointer;
 import org.apache.commons.jxpath.TestBean;
 import org.apache.commons.jxpath.Variables;
-import org.apache.commons.jxpath.ri.model.NodePointer;
-import org.apache.commons.jxpath.util.JXPath11CompatibleTypeConverter;
-import org.apache.commons.jxpath.util.TypeConverter;
-import org.apache.commons.jxpath.util.TypeUtils;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class ExtensionFunctionTestTest7 extends AbstractJXPathTest {
+import java.util.List;
 
-    private Functions functions;
+/**
+ * Tests the automatic type conversion of a NodeSet when passed as an argument to an extension function.
+ */
+public class ExtensionFunctionNodeSetArgumentTest extends AbstractJXPathTest {
 
     private JXPathContext context;
 
-    private TestBean testBean;
-
-    private TypeConverter typeConverter;
-
-    @Override
     @BeforeEach
     public void setUp() {
-        if (context == null) {
-            testBean = new TestBean();
-            context = JXPathContext.newContext(testBean);
-            final Variables vars = context.getVariables();
-            vars.declareVariable("test", new TestFunctions(4, "test"));
-            final FunctionLibrary lib = new FunctionLibrary();
-            lib.addFunctions(new ClassFunctions(TestFunctions.class, "test"));
-            lib.addFunctions(new ClassFunctions(TestFunctions2.class, "test"));
-            lib.addFunctions(new PackageFunctions("", "call"));
-            lib.addFunctions(new PackageFunctions("org.apache.commons.jxpath.ri.compiler.", "jxpathtest"));
-            lib.addFunctions(new PackageFunctions("", null));
-            context.setFunctions(lib);
-            context.getVariables().declareVariable("List.class", List.class);
-            context.getVariables().declareVariable("NodeSet.class", NodeSet.class);
-        }
-        functions = new ClassFunctions(TestFunctions.class, "test");
-        typeConverter = TypeUtils.getTypeConverter();
+        // Arrange: Set up a JXPathContext with a bean and a custom function library.
+        TestBean testBean = new TestBean();
+        context = JXPathContext.newContext(testBean);
+
+        // Register the "test" namespace with functions from the TestFunctions class.
+        // This makes methods like TestFunctions.isInstance() available as "test:isInstance()".
+        FunctionLibrary functionLibrary = new FunctionLibrary();
+        functionLibrary.addFunctions(new ClassFunctions(TestFunctions.class, "test"));
+        context.setFunctions(functionLibrary);
+
+        // Declare variables to be used in the XPath expressions.
+        Variables variables = context.getVariables();
+        variables.declareVariable("List.class", List.class);
+        variables.declareVariable("NodeSet.class", NodeSet.class);
     }
 
-    @AfterEach
-    public void tearDown() {
-        TypeUtils.setTypeConverter(typeConverter);
-    }
-
-    private static final class Context implements ExpressionContext {
-
-        private final Object object;
-
-        public Context(final Object object) {
-            this.object = object;
-        }
-
-        @Override
-        public List<Pointer> getContextNodeList() {
-            return null;
-        }
-
-        @Override
-        public Pointer getContextNodePointer() {
-            return NodePointer.newNodePointer(null, object, Locale.getDefault());
-        }
-
-        @Override
-        public JXPathContext getJXPathContext() {
-            return null;
-        }
-
-        @Override
-        public int getPosition() {
-            return 0;
-        }
-    }
-
+    /**
+     * This test verifies that when a NodeSet is passed as an argument to an extension function,
+     * JXPath automatically converts it into a List of the underlying objects.
+     */
     @Test
-    void testEstablishNodeSetBaseline() {
-        assertXPathValue(context, "test:isInstance(//strings, $List.class)", Boolean.TRUE);
-        assertXPathValue(context, "test:isInstance(//strings, $NodeSet.class)", Boolean.FALSE);
+    public void whenNodeSetIsPassedToExtensionFunction_itIsConvertedToList() {
+        // The XPath expression "//strings" resolves to a NodeSet. This test checks
+        // how JXPath handles this NodeSet when passing it to the "test:isInstance" function.
+
+        // Act & Assert 1: The NodeSet should be converted to a List before the function is called.
+        // Therefore, the function should report that the argument is an instance of List.
+        final String xpathIsList = "test:isInstance(//strings, $List.class)";
+        assertXPathValue(context, xpathIsList, Boolean.TRUE);
+
+        // Act & Assert 2: Because the NodeSet was converted, the function never receives a NodeSet object.
+        // Therefore, it should report that the argument is NOT an instance of NodeSet.
+        final String xpathIsNodeSet = "test:isInstance(//strings, $NodeSet.class)";
+        assertXPathValue(context, xpathIsNodeSet, Boolean.FALSE);
     }
 }
