9d51955dfe9c4ba80ff8317d1304179bde107f68
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:33:57 +0800

    gemini processedManualTestCases soundextest9.java

--- soundextest9.java
+++ soundextest9.java
@@ -1,26 +1,66 @@
 package org.apache.commons.codec.language;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import org.apache.commons.codec.AbstractStringEncoderTest;
+
 import org.apache.commons.codec.EncoderException;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-public class SoundexTestTest9 extends AbstractStringEncoderTest<Soundex> {
+/**
+ * Tests for the {@link Soundex} encoder, focusing on its handling of non-alphabetic characters like hyphens.
+ */
+public class SoundexTest {
 
-    @Override
-    protected Soundex createStringEncoder() {
-        return new Soundex();
+    private Soundex soundexEncoder;
+
+    @BeforeEach
+    void setUp() {
+        this.soundexEncoder = new Soundex();
     }
 
     /**
-     * Test data from http://www.myatt.demon.co.uk/sxalg.htm
+     * This parameterized test verifies that the Soundex implementation correctly ignores hyphens.
+     * All variations of "KINGSMITH", regardless of where hyphens are placed, should produce the
+     * same Soundex code. This approach makes each input an individual, verifiable test run.
      *
-     * @throws EncoderException for some failure scenarios
+     * <p>The name "KINGSMITH" encodes to "K525" based on the Soundex algorithm:
+     * <ul>
+     *     <li><b>K</b>: Retained as the first letter.</li>
+     *     <li><b>I</b>: Vowel, ignored.</li>
+     *     <li><b>N</b>: Encoded as <b>5</b>.</li>
+     *     <li><b>G</b>: Encoded as <b>2</b>.</li>
+     *     <li><b>S</b>: Ignored (has the same code '2' as the previous consonant 'G').</li>
+     *     <li><b>M</b>: Encoded as <b>5</b>.</li>
+     *     <li><b>I</b>: Vowel, ignored.</li>
+     *     <li><b>T</b>: Encoded as <b>3</b>.</li>
+     *     <li><b>H</b>: Ignored.</li>
+     * </ul>
+     * The resulting code K-5-2-5-3 is truncated to 4 characters: "K525".</p>
+     *
+     * <p>Test data is sourced from http://www.myatt.demon.co.uk/sxalg.htm</p>
+     *
+     * @param nameVariant A variation of the name "KINGSMITH".
+     * @throws EncoderException Not expected for these valid inputs.
      */
-    @Test
-    void testEncodeIgnoreHyphens() throws EncoderException {
-        checkEncodingVariations("K525", new String[] { "KINGSMITH", "-KINGSMITH", "K-INGSMITH", "KI-NGSMITH", "KIN-GSMITH", "KING-SMITH", "KINGS-MITH", "KINGSM-ITH", "KINGSMI-TH", "KINGSMIT-H", "KINGSMITH-" });
+    @DisplayName("Soundex should ignore hyphens and encode variations of 'KINGSMITH' to K525")
+    @ParameterizedTest(name = "input: {0}")
+    @ValueSource(strings = {
+        "KINGSMITH",    // Base case without hyphens
+        "-KINGSMITH",   // Leading hyphen
+        "K-INGSMITH",   // Internal hyphens
+        "KI-NGSMITH",
+        "KIN-GSMITH",
+        "KING-SMITH",
+        "KINGS-MITH",
+        "KINGSM-ITH",
+        "KINGSMI-TH",
+        "KINGSMIT-H",
+        "KINGSMITH-"    // Trailing hyphen
+    })
+    void shouldEncodeNameVariationsConsistently(final String nameVariant) throws EncoderException {
+        final String expectedCode = "K525";
+        assertEquals(expectedCode, this.soundexEncoder.encode(nameVariant));
     }
 }
