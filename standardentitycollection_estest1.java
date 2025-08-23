679df35fa934095b66479f8c5138ddce344eaa78
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:17:52 +0800

    Original processedAutoTestCases standardentitycollection_estest1.java

---
+++ standardentitycollection_estest1.java
@@ -0,0 +1,28 @@
+package org.jfree.chart.entity;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.geom.Line2D;
+import java.util.Collection;
+import java.util.Iterator;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class StandardEntityCollection_ESTestTest1 extends StandardEntityCollection_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test00() throws Throwable {
+        StandardEntityCollection standardEntityCollection0 = new StandardEntityCollection();
+        Dimension dimension0 = new Dimension((-1), 0);
+        Rectangle rectangle0 = new Rectangle(dimension0);
+        ChartEntity chartEntity0 = new ChartEntity(rectangle0, ">", (String) null);
+        standardEntityCollection0.add(chartEntity0);
+        ChartEntity chartEntity1 = standardEntityCollection0.getEntity(1.0, 674.587836228879);
+        assertNull(chartEntity1);
+    }
+}
