810d63d3f6e9662bb6809004dfa734a266fe64d1
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:19:18 +0800

    Original processedAutoTestCases stringutil_estest98.java

---
+++ stringutil_estest98.java
@@ -0,0 +1,27 @@
+package org.jsoup.internal;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Collection;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.stream.Collector;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.net.MockURL;
+import org.junit.runner.RunWith;
+
+public class StringUtil_ESTestTest98 extends StringUtil_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test97() throws Throwable {
+        StringUtil stringUtil0 = new StringUtil();
+    }
+}
