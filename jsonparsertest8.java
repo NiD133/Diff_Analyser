d98db493a710a1b099a3fa64b3f494835d4fe245
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:30:09 +0800

    gemini processedManualTestCases jsonparsertest8.java

--- jsonparsertest8.java
+++ jsonparsertest8.java
@@ -1,26 +1,32 @@
 package com.google.gson;
 
 import static com.google.common.truth.Truth.assertThat;
-import static org.junit.Assert.assertThrows;
-import com.google.gson.common.TestTypes.BagOfPrimitives;
-import com.google.gson.internal.Streams;
-import com.google.gson.stream.JsonReader;
-import java.io.CharArrayReader;
-import java.io.CharArrayWriter;
-import java.io.IOException;
-import java.io.StringReader;
+
 import org.junit.Test;
 
-public class JsonParserTestTest8 {
+/**
+ * Test for {@link JsonParser}.
+ */
+public class JsonParserTest {
 
-    @Test
-    public void testParseMixedArray() {
-        String json = "[{},13,\"stringValue\"]";
-        JsonElement e = JsonParser.parseString(json);
-        assertThat(e.isJsonArray()).isTrue();
-        JsonArray array = e.getAsJsonArray();
-        assertThat(array.get(0).toString()).isEqualTo("{}");
-        assertThat(array.get(1).getAsInt()).isEqualTo(13);
-        assertThat(array.get(2).getAsString()).isEqualTo("stringValue");
-    }
+  /**
+   * Tests that parsing a JSON string representing an array with mixed element types
+   * (object, number, string) results in a correct {@link JsonArray}.
+   */
+  @Test
+  public void parseString_withMixedTypeArray_parsesCorrectly() {
+    // Arrange
+    String json = "[{}, 13, \"stringValue\"]";
+
+    JsonArray expectedArray = new JsonArray();
+    expectedArray.add(new JsonObject());
+    expectedArray.add(13);
+    expectedArray.add("stringValue");
+
+    // Act
+    JsonElement parsedElement = JsonParser.parseString(json);
+
+    // Assert
+    assertThat(parsedElement).isEqualTo(expectedArray);
+  }
 }
