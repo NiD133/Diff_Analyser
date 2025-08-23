1c65a6c3ab9e7d69e2f2084c5dd8356132c88144
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:10:32 +0800

    Original processedAutoTestCases quotedprintablecodec_estest31.java

---
+++ quotedprintablecodec_estest31.java
@@ -0,0 +1,27 @@
+package org.apache.commons.codec.net;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.IllegalCharsetNameException;
+import java.nio.charset.UnsupportedCharsetException;
+import java.util.BitSet;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class QuotedPrintableCodec_ESTestTest31 extends QuotedPrintableCodec_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test30() throws Throwable {
+        QuotedPrintableCodec quotedPrintableCodec0 = new QuotedPrintableCodec();
+        // Undeclared exception!
+        try {
+            quotedPrintableCodec0.decode("4>&$1^)O:\"", (String) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+        }
+    }
+}
