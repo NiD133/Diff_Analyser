a6cceeb25588d7c4babc50cb60656e7dfe8a57fc
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:50:13 +0800

    gemini processedAutoTestCases fieldutils_estest13.java

--- fieldutils_estest13.java
+++ fieldutils_estest13.java
@@ -1,23 +1,23 @@
 package org.joda.time.field;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.math.RoundingMode;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.joda.time.DateTimeField;
-import org.joda.time.DateTimeFieldType;
-import org.joda.time.DateTimeZone;
-import org.joda.time.chrono.IslamicChronology;
-import org.joda.time.chrono.ZonedChronology;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class FieldUtils_ESTestTest13 extends FieldUtils_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link FieldUtils} class.
+ */
+public class FieldUtilsTest {
 
-    @Test(timeout = 4000)
-    public void test12() throws Throwable {
-        int int0 = FieldUtils.safeToInt(0);
-        assertEquals(0, int0);
+    @Test
+    public void safeToInt_shouldReturnSameValue_whenLongIsWithinIntRange() {
+        // Arrange: A long value that can be safely cast to an int.
+        long valueToConvert = 0L;
+        int expectedValue = 0;
+
+        // Act: Convert the long to an int using the method under test.
+        int actualValue = FieldUtils.safeToInt(valueToConvert);
+
+        // Assert: The converted value should be equal to the original.
+        assertEquals(expectedValue, actualValue);
     }
 }
