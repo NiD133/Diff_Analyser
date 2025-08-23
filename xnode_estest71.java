4cb343056ce68841e00bfe875bde310a1ec02a78
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:29:09 +0800

    Original processedAutoTestCases xnode_estest71.java

---
+++ xnode_estest71.java
@@ -0,0 +1,32 @@
+package org.apache.ibatis.parsing;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.List;
+import java.util.Locale;
+import java.util.Properties;
+import java.util.function.Supplier;
+import javax.imageio.metadata.IIOMetadataNode;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.xml.sax.ext.DefaultHandler2;
+
+public class XNode_ESTestTest71 extends XNode_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test070() throws Throwable {
+        IIOMetadataNode iIOMetadataNode0 = new IIOMetadataNode(" />\n");
+        XPathParser xPathParser0 = new XPathParser((Document) null, true);
+        Properties properties0 = new Properties();
+        XNode xNode0 = new XNode(xPathParser0, iIOMetadataNode0, properties0);
+        Double double0 = new Double((-2446.09697821936));
+        Double double1 = xNode0.getDoubleAttribute("< />\n />\n", double0);
+        assertEquals((-2446.09697821936), (double) double1, 0.01);
+    }
+}
