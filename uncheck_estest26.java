525c51204f778b6be8f58adba59da26535ace38b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:24:32 +0800

    gemini processedAutoTestCases uncheck_estest26.java

--- uncheck_estest26.java
+++ uncheck_estest26.java
@@ -1,37 +1,20 @@
 package org.apache.commons.io.function;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.time.chrono.HijrahEra;
-import java.util.Comparator;
-import java.util.concurrent.ForkJoinTask;
-import java.util.function.BiConsumer;
-import java.util.function.BinaryOperator;
-import java.util.function.Function;
-import java.util.function.Supplier;
-import java.util.stream.Collector;
-import java.util.stream.LongStream;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
 
-public class Uncheck_ESTestTest26 extends Uncheck_ESTest_scaffolding {
+/**
+ * Tests for the {@link Uncheck} utility class.
+ */
+public class UncheckTest {
 
-    @Test(timeout = 4000)
-    public void test25() throws Throwable {
-        // Undeclared exception!
-        try {
-            Uncheck.run((IORunnable) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.commons.io.function.Uncheck", e);
-        }
+    /**
+     * Tests that calling {@link Uncheck#run(IORunnable)} with a null argument
+     * results in a {@link NullPointerException}.
+     */
+    @Test(expected = NullPointerException.class)
+    public void testRun_withNullRunnable_throwsNullPointerException() {
+        // The cast to IORunnable is necessary to resolve ambiguity between
+        // the overloaded Uncheck.run() methods.
+        Uncheck.run((IORunnable) null);
     }
 }
