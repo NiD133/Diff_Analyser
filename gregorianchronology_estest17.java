0a2e2ce0af3a84d051cf6848e5b3083fae4e1b8d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:52:01 +0800

    Original processedAutoTestCases gregorianchronology_estest17.java

---
+++ gregorianchronology_estest17.java
@@ -0,0 +1,23 @@
+package org.joda.time.chrono;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.TimeZone;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.joda.time.Chronology;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.LocalDateTime;
+import org.junit.runner.RunWith;
+
+public class GregorianChronology_ESTestTest17 extends GregorianChronology_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test16() throws Throwable {
+        GregorianChronology gregorianChronology0 = GregorianChronology.getInstanceUTC();
+        long long0 = gregorianChronology0.calculateFirstDayOfYearMillis((-292275054));
+        assertEquals((-9223372017043200000L), long0);
+    }
+}
