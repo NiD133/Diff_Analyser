3237bc59243d9cb581296e8a8516368f6b949a50
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:12:07 +0800

    Original processedAutoTestCases randomaccessfileorarray_estest9.java

---
+++ randomaccessfileorarray_estest9.java
@@ -0,0 +1,36 @@
+package com.itextpdf.text.pdf;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.itextpdf.text.io.GetBufferedRandomAccessSource;
+import com.itextpdf.text.io.IndependentRandomAccessSource;
+import com.itextpdf.text.io.RandomAccessSource;
+import com.itextpdf.text.io.WindowRandomAccessSource;
+import java.io.ByteArrayInputStream;
+import java.io.EOFException;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PipedInputStream;
+import java.net.URL;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.net.MockURL;
+import org.evosuite.runtime.testdata.EvoSuiteFile;
+import org.evosuite.runtime.testdata.FileSystemHandling;
+import org.junit.runner.RunWith;
+
+public class RandomAccessFileOrArray_ESTestTest9 extends RandomAccessFileOrArray_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test008() throws Throwable {
+        byte[] byteArray0 = new byte[24];
+        byteArray0[3] = (byte) 52;
+        ByteArrayInputStream byteArrayInputStream0 = new ByteArrayInputStream(byteArray0);
+        RandomAccessFileOrArray randomAccessFileOrArray0 = new RandomAccessFileOrArray(byteArrayInputStream0);
+        long long0 = randomAccessFileOrArray0.readLong();
+        assertEquals(8L, randomAccessFileOrArray0.getFilePointer());
+        assertEquals(223338299392L, long0);
+    }
+}
