d05c5bea93887416d4d79f9a10b9e0b68e749bf0
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:37:06 +0800

    gemini processedAutoTestCases accumulatorpathvisitor_estest24.java

--- accumulatorpathvisitor_estest24.java
+++ accumulatorpathvisitor_estest24.java
@@ -1,44 +1,27 @@
 package org.apache.commons.io.file;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.IOException;
-import java.nio.file.FileVisitResult;
+
 import java.nio.file.Path;
-import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Comparator;
-import java.util.List;
-import java.util.regex.Pattern;
-import org.apache.commons.io.filefilter.CanExecuteFileFilter;
-import org.apache.commons.io.filefilter.CanReadFileFilter;
-import org.apache.commons.io.filefilter.CanWriteFileFilter;
-import org.apache.commons.io.filefilter.HiddenFileFilter;
-import org.apache.commons.io.filefilter.PrefixFileFilter;
-import org.apache.commons.io.filefilter.RegexFileFilter;
-import org.apache.commons.io.function.IOBiFunction;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.evosuite.runtime.mock.java.io.MockFile;
-import org.evosuite.runtime.mock.java.io.MockIOException;
-import org.junit.runner.RunWith;
 
-public class AccumulatorPathVisitor_ESTestTest24 extends AccumulatorPathVisitor_ESTest_scaffolding {
+/**
+ * Tests for {@link AccumulatorPathVisitor}.
+ */
+public class AccumulatorPathVisitorTest {
 
-    @Test(timeout = 4000)
-    public void test23() throws Throwable {
-        AccumulatorPathVisitor accumulatorPathVisitor0 = new AccumulatorPathVisitor();
-        // Undeclared exception!
-        try {
-            accumulatorPathVisitor0.relativizeDirectories((Path) null, true, (Comparator<? super Path>) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("java.util.Objects", e);
-        }
+    /**
+     * Tests that {@link AccumulatorPathVisitor#relativizeDirectories(Path, boolean, Comparator)}
+     * throws a {@link NullPointerException} when the parent path argument is null.
+     */
+    @Test(expected = NullPointerException.class)
+    public void relativizeDirectoriesShouldThrowNullPointerExceptionForNullParentPath() {
+        // Arrange: Create a visitor instance.
+        final AccumulatorPathVisitor visitor = new AccumulatorPathVisitor();
+
+        // Act & Assert: Call the method with a null parent path.
+        // This is expected to throw a NullPointerException, as Path.relativize()
+        // does not accept a null argument.
+        visitor.relativizeDirectories(null, true, null);
     }
 }
