8f3d6f6716ecd3a091bde3d921871bb1b07e98b0
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:42:05 +0800

    Original processedAutoTestCases charrange_estest34.java

---
+++ charrange_estest34.java
@@ -0,0 +1,22 @@
+package org.apache.commons.lang3;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.function.Consumer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class CharRange_ESTestTest34 extends CharRange_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test33() throws Throwable {
+        CharRange charRange0 = CharRange.isNot('S');
+        Consumer<Character> consumer0 = (Consumer<Character>) mock(Consumer.class, new ViolatedAssumptionAnswer());
+        // Undeclared exception!
+        charRange0.forEach(consumer0);
+    }
+}
