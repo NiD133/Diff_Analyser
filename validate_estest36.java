bc9d1188fe31f7a15cdb9ebf92953bbe8c2f695c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:26:02 +0800

    Original processedAutoTestCases validate_estest36.java

---
+++ validate_estest36.java
@@ -0,0 +1,23 @@
+package org.jsoup.helper;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.FormatFlagsConversionMismatchException;
+import java.util.IllegalFormatConversionException;
+import java.util.IllegalFormatFlagsException;
+import java.util.IllegalFormatWidthException;
+import java.util.MissingFormatArgumentException;
+import java.util.MissingFormatWidthException;
+import java.util.UnknownFormatConversionException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class Validate_ESTestTest36 extends Validate_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test35() throws Throwable {
+        Validate.isTrue(true);
+    }
+}
