407bd6d69ee8670eae06617074d89b74843ef8a4
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:27:51 +0800

    Original processedAutoTestCases xmlstreamwriter_estest3.java

---
+++ xmlstreamwriter_estest3.java
@@ -0,0 +1,30 @@
+package org.apache.commons.io.output;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.io.Writer;
+import java.nio.CharBuffer;
+import java.nio.charset.IllegalCharsetNameException;
+import java.nio.charset.UnsupportedCharsetException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.evosuite.runtime.mock.java.io.MockPrintStream;
+import org.junit.runner.RunWith;
+
+public class XmlStreamWriter_ESTestTest3 extends XmlStreamWriter_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test02() throws Throwable {
+        MockFile mockFile0 = new MockFile("U-");
+        XmlStreamWriter xmlStreamWriter0 = new XmlStreamWriter(mockFile0, (String) null);
+        assertEquals("UTF-8", xmlStreamWriter0.getDefaultEncoding());
+    }
+}
