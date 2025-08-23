2e47ce3a9d458755f9bc382421ca007103ba834a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:06:14 +0800

    gemini processedAutoTestCases optionbuilder_estest22.java

--- optionbuilder_estest22.java
+++ optionbuilder_estest22.java
@@ -1,20 +1,30 @@
 package org.apache.commons.cli;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class OptionBuilder_ESTestTest22 extends OptionBuilder_ESTest_scaffolding {
+/**
+ * Tests for {@link OptionBuilder}.
+ *
+ * Note: {@link OptionBuilder} is a static builder, which is a challenging design for testing
+ * as state can leak between tests. In a complete test suite, it would be crucial to reset
+ * the builder's state before each test, for example, in a method annotated with @Before.
+ */
+public class OptionBuilderTest {
 
-    @Test(timeout = 4000)
-    public void test21() throws Throwable {
-        OptionBuilder.withValueSeparator();
-        OptionBuilder.withLongOpt("");
-        Option option0 = OptionBuilder.create();
-        assertEquals('=', option0.getValueSeparator());
-        assertEquals((-1), option0.getArgs());
+    @Test
+    public void buildOptionWithDefaultValueSeparator() {
+        // Arrange
+        // A long option is required for the create() method to succeed.
+        OptionBuilder.withLongOpt("my-option");
+
+        // Act
+        // Configure the option to use the default value separator and then create it.
+        Option option = OptionBuilder.withValueSeparator().create();
+
+        // Assert
+        // Verify that the option was created with the correct default properties.
+        assertEquals("The value separator should be the default '=' character.", '=', option.getValueSeparator());
+        assertEquals("The number of arguments should be uninitialized (-1).", -1, option.getArgs());
     }
 }
