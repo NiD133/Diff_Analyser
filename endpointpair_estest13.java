8abbbbcabe41889d1264eebe94eaf0a6b2ad11e8
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:49:10 +0800

    gemini processedAutoTestCases endpointpair_estest13.java

--- endpointpair_estest13.java
+++ endpointpair_estest13.java
@@ -1,29 +1,19 @@
 package com.google.common.graph;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import com.google.common.collect.UnmodifiableIterator;
-import java.util.Locale;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class EndpointPair_ESTestTest13 extends EndpointPair_ESTest_scaffolding {
+/**
+ * Tests for {@link EndpointPair}.
+ */
+public class EndpointPairTest {
 
-    @Test(timeout = 4000)
-    public void test12() throws Throwable {
-        Integer integer0 = new Integer((-831));
-        EndpointPair<Integer> endpointPair0 = EndpointPair.unordered(integer0, integer0);
-        // Undeclared exception!
-        try {
-            endpointPair0.adjacentNode((Integer) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("com.google.common.graph.EndpointPair", e);
-        }
+    @Test(expected = NullPointerException.class)
+    public void adjacentNode_withNullArgument_throwsNullPointerException() {
+        // Arrange: Create an endpoint pair. The specific nodes don't matter for this test.
+        EndpointPair<Integer> endpointPair = EndpointPair.unordered(1, 2);
+
+        // Act: Call the method under test with a null argument.
+        // The @Test(expected=...) annotation handles the assertion.
+        endpointPair.adjacentNode(null);
     }
 }
