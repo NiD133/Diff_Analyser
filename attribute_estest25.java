2afb9c171e6c75ead225cbc9f4eacaa116c84f1e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:38:11 +0800

    Original processedAutoTestCases attribute_estest25.java

---
+++ attribute_estest25.java
@@ -0,0 +1,38 @@
+package org.jsoup.nodes;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.ByteArrayOutputStream;
+import java.io.FilterOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PipedWriter;
+import java.io.StringWriter;
+import java.nio.BufferOverflowException;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetEncoder;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockPrintWriter;
+import org.jsoup.internal.QuietAppendable;
+import org.junit.runner.RunWith;
+
+public class Attribute_ESTestTest25 extends Attribute_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test24() throws Throwable {
+        Attribute attribute0 = Attribute.createFromEncoded("g", "g");
+        Document.OutputSettings document_OutputSettings0 = new Document.OutputSettings();
+        // Undeclared exception!
+        try {
+            attribute0.html((Appendable) null, document_OutputSettings0);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.jsoup.internal.QuietAppendable$BaseAppendable", e);
+        }
+    }
+}
