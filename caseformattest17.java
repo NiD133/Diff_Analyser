a5eefc3aa976912156cb53e40c124fb2b1b04a28
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:26:21 +0800

    Original processedManualTestCases caseformattest17.java

---
+++ caseformattest17.java
@@ -0,0 +1,24 @@
+package com.google.common.base;
+
+import static com.google.common.base.CaseFormat.LOWER_CAMEL;
+import static com.google.common.base.CaseFormat.LOWER_HYPHEN;
+import static com.google.common.base.CaseFormat.LOWER_UNDERSCORE;
+import static com.google.common.base.CaseFormat.UPPER_CAMEL;
+import static com.google.common.base.CaseFormat.UPPER_UNDERSCORE;
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.truth.Truth.assertWithMessage;
+import com.google.common.annotations.GwtCompatible;
+import com.google.common.annotations.GwtIncompatible;
+import com.google.common.annotations.J2ktIncompatible;
+import com.google.common.testing.NullPointerTester;
+import com.google.common.testing.SerializableTester;
+import junit.framework.TestCase;
+import org.jspecify.annotations.NullUnmarked;
+
+public class CaseFormatTestTest17 extends TestCase {
+
+    public void testLowerCamelToUpperUnderscore() {
+        assertThat(LOWER_CAMEL.to(UPPER_UNDERSCORE, "foo")).isEqualTo("FOO");
+        assertThat(LOWER_CAMEL.to(UPPER_UNDERSCORE, "fooBar")).isEqualTo("FOO_BAR");
+    }
+}
