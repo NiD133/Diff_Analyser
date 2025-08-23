2177c9eca9b7b0fbe3e9716095c48e4e197e5b0a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:40:15 +0800

    Original processedAutoTestCases bufferedline_estest23.java

---
+++ bufferedline_estest23.java
@@ -0,0 +1,33 @@
+package org.locationtech.spatial4j.shape.impl;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.HashMap;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+import org.locationtech.spatial4j.context.SpatialContext;
+import org.locationtech.spatial4j.context.SpatialContextFactory;
+import org.locationtech.spatial4j.distance.CartesianDistCalc;
+import org.locationtech.spatial4j.shape.Point;
+import org.locationtech.spatial4j.shape.Rectangle;
+import org.locationtech.spatial4j.shape.Shape;
+import org.locationtech.spatial4j.shape.SpatialRelation;
+
+public class BufferedLine_ESTestTest23 extends BufferedLine_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test22() throws Throwable {
+        // Undeclared exception!
+        try {
+            BufferedLine.expandBufForLongitudeSkew((Point) null, (Point) null, (-2061.3583581165));
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.locationtech.spatial4j.shape.impl.BufferedLine", e);
+        }
+    }
+}
