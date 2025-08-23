2b46eabf9b582dcddd8948e109239d39c97c9525
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:58:34 +0800

    Original processedAutoTestCases jsontreewriter_estest3.java

---
+++ jsontreewriter_estest3.java
@@ -0,0 +1,25 @@
+package com.google.gson.internal.bind;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.Strictness;
+import com.google.gson.stream.JsonWriter;
+import java.io.IOException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class JsonTreeWriter_ESTestTest3 extends JsonTreeWriter_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test02() throws Throwable {
+        JsonTreeWriter jsonTreeWriter0 = new JsonTreeWriter();
+        jsonTreeWriter0.setHtmlSafe(true);
+        JsonWriter jsonWriter0 = jsonTreeWriter0.value(true);
+        assertFalse(jsonWriter0.isLenient());
+    }
+}
