119bc421165d7a5d6067b1dc7d52b27d7ac50cb0
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:02:36 +0800

    gemini processedAutoTestCases metaphone_estest46.java

--- metaphone_estest46.java
+++ metaphone_estest46.java
@@ -1,25 +1,34 @@
 package org.apache.commons.codec.language;
 
+import org.apache.commons.codec.EncoderException;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class Metaphone_ESTestTest46 extends Metaphone_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
-    @Test(timeout = 4000)
-    public void test45() throws Throwable {
-        Metaphone metaphone0 = new Metaphone();
+/**
+ * Tests for the {@link Metaphone} class.
+ */
+public class MetaphoneTest {
+
+    /**
+     * Tests that the encode(Object) method throws an EncoderException when the input
+     * is not of type String, as specified by the Encoder interface contract.
+     */
+    @Test
+    public void encodeShouldThrowExceptionForInvalidInputType() {
+        // Arrange
+        Metaphone metaphone = new Metaphone();
+        Object invalidInput = new Object(); // Use a generic Object as the invalid type.
+        String expectedMessage = "Parameter supplied to Metaphone encode is not of type java.lang.String";
+
+        // Act & Assert
         try {
-            metaphone0.encode((Object) metaphone0);
-            fail("Expecting exception: Exception");
-        } catch (Exception e) {
-            //
-            // Parameter supplied to Metaphone encode is not of type java.lang.String
-            //
-            verifyException("org.apache.commons.codec.language.Metaphone", e);
+            metaphone.encode(invalidInput);
+            fail("Expected an EncoderException to be thrown for non-String input.");
+        } catch (final EncoderException e) {
+            // Verify that the exception has the expected message.
+            assertEquals(expectedMessage, e.getMessage());
         }
     }
 }
