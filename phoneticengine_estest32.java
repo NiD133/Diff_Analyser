103776db30cbe7012b9bdec0497b09060e1556c4
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:08:52 +0800

    Original processedAutoTestCases phoneticengine_estest32.java

---
+++ phoneticengine_estest32.java
@@ -0,0 +1,24 @@
+package org.apache.commons.codec.language.bm;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.nio.CharBuffer;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class PhoneticEngine_ESTestTest32 extends PhoneticEngine_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test31() throws Throwable {
+        NameType nameType0 = NameType.ASHKENAZI;
+        RuleType ruleType0 = RuleType.EXACT;
+        PhoneticEngine phoneticEngine0 = new PhoneticEngine(nameType0, ruleType0, false, 0);
+        int int0 = phoneticEngine0.getMaxPhonemes();
+        assertFalse(phoneticEngine0.isConcat());
+        assertEquals(0, int0);
+    }
+}
