53447e429c1725eaccf5e6980e42bddad757ba21
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:36:38 +0800

    gemini processedAutoTestCases abstractpartial_estest14.java

--- abstractpartial_estest14.java
+++ abstractpartial_estest14.java
@@ -1,56 +1,36 @@
 package org.joda.time.base;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.Date;
-import java.util.Locale;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.evosuite.runtime.mock.java.util.MockGregorianCalendar;
-import org.joda.time.Chronology;
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeField;
-import org.joda.time.DateTimeFieldType;
-import org.joda.time.DateTimeZone;
-import org.joda.time.DurationFieldType;
-import org.joda.time.Instant;
 import org.joda.time.LocalDate;
-import org.joda.time.LocalDateTime;
-import org.joda.time.LocalTime;
-import org.joda.time.MonthDay;
-import org.joda.time.Partial;
-import org.joda.time.ReadablePartial;
-import org.joda.time.Weeks;
-import org.joda.time.YearMonth;
-import org.joda.time.Years;
-import org.joda.time.chrono.CopticChronology;
-import org.joda.time.chrono.GJChronology;
-import org.joda.time.chrono.GregorianChronology;
-import org.joda.time.chrono.IslamicChronology;
 import org.joda.time.format.DateTimeFormatter;
 import org.joda.time.format.DateTimeParser;
 import org.joda.time.format.DateTimePrinter;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class AbstractPartial_ESTestTest14 extends AbstractPartial_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
-    @Test(timeout = 4000)
-    public void test13() throws Throwable {
-        GJChronology gJChronology0 = GJChronology.getInstanceUTC();
-        LocalDate localDate0 = new LocalDate((long) 0, (Chronology) gJChronology0);
-        DateTimeFormatter dateTimeFormatter0 = new DateTimeFormatter((DateTimePrinter) null, (DateTimeParser) null);
-        // Undeclared exception!
+/**
+ * Tests for the {@link AbstractPartial} class.
+ */
+public class AbstractPartial_ESTestTest14 {
+
+    /**
+     * Verifies that calling toString() with a formatter that lacks a printer
+     * throws an UnsupportedOperationException.
+     */
+    @Test
+    public void toString_withPrinterlessFormatter_shouldThrowUnsupportedOperationException() {
+        // Arrange: Create a formatter that does not support printing (it has no printer).
+        DateTimeFormatter formatterWithoutPrinter = new DateTimeFormatter((DateTimePrinter) null, (DateTimeParser) null);
+        LocalDate partialDate = new LocalDate(2023, 10, 27);
+
+        // Act & Assert
         try {
-            localDate0.toString(dateTimeFormatter0);
-            fail("Expecting exception: UnsupportedOperationException");
+            partialDate.toString(formatterWithoutPrinter);
+            fail("Expected UnsupportedOperationException was not thrown.");
         } catch (UnsupportedOperationException e) {
-            //
-            // Printing not supported
-            //
-            verifyException("org.joda.time.format.DateTimeFormatter", e);
+            // Verify that the exception is thrown for the correct reason.
+            assertEquals("Printing not supported", e.getMessage());
         }
     }
 }
