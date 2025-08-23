f0b526ca5cc672e8286c40fd213a3c1fdb47211f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:27:30 +0800

    gemini processedAutoTestCases weeks_estest71.java

--- weeks_estest71.java
+++ weeks_estest71.java
@@ -1,18 +1,27 @@
 package org.joda.time;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class Weeks_ESTestTest71 extends Weeks_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link Weeks} class.
+ */
+public class WeeksTest {
 
-    @Test(timeout = 4000)
-    public void test70() throws Throwable {
-        Weeks weeks0 = Weeks.MIN_VALUE;
-        Duration duration0 = weeks0.toStandardDuration();
-        assertEquals((-15032385536L), duration0.getStandardDays());
+    @Test
+    public void toStandardDuration_withMinValue_returnsCorrectDurationInDays() {
+        // Arrange
+        Weeks minWeeks = Weeks.MIN_VALUE;
+        
+        // The expected number of days is the minimum integer value multiplied by 7 (days in a week).
+        // We cast to long to prevent potential integer overflow during the calculation.
+        long expectedDays = (long) Integer.MIN_VALUE * 7;
+
+        // Act
+        Duration duration = minWeeks.toStandardDuration();
+
+        // Assert
+        assertEquals("The duration in days should be Integer.MIN_VALUE * 7",
+                expectedDays, duration.getStandardDays());
     }
 }
