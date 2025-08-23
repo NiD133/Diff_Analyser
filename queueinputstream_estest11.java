ee421282bedaddd4ed4759713336bdaa10a61efd
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:10:17 +0800

    Original processedAutoTestCases queueinputstream_estest11.java

---
+++ queueinputstream_estest11.java
@@ -0,0 +1,33 @@
+package org.apache.commons.io.input;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.PriorityBlockingQueue;
+import org.apache.commons.io.output.QueueOutputStream;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class QueueInputStream_ESTestTest11 extends QueueInputStream_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test10() throws Throwable {
+        QueueInputStream queueInputStream0 = new QueueInputStream();
+        byte[] byteArray0 = new byte[0];
+        // Undeclared exception!
+        try {
+            queueInputStream0.read(byteArray0, (-3123), (-3123));
+            fail("Expecting exception: IndexOutOfBoundsException");
+        } catch (IndexOutOfBoundsException e) {
+            //
+            // Range [-3123, -3123 + -3123) out of bounds for length 0
+            //
+            verifyException("org.apache.commons.io.input.QueueInputStream", e);
+        }
+    }
+}
