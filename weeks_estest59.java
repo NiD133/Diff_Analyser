94ea8d93df63f2e970fba749c48abd14c05909a1
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:27:25 +0800

    gemini processedAutoTestCases weeks_estest59.java

--- weeks_estest59.java
+++ weeks_estest59.java
@@ -1,26 +1,30 @@
 package org.joda.time;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
-public class Weeks_ESTestTest59 extends Weeks_ESTest_scaffolding {
+public class Weeks_ESTestTest59 { // Note: Class name kept as per original for context
 
-    @Test(timeout = 4000)
-    public void test58() throws Throwable {
-        Weeks weeks0 = Weeks.MIN_VALUE;
-        // Undeclared exception!
+    /**
+     * Tests that converting Weeks.MIN_VALUE to standard hours throws an
+     * ArithmeticException due to integer overflow.
+     * The calculation (-2,147,483,648 weeks * 168 hours/week) is too large
+     * to fit in a standard integer.
+     */
+    @Test
+    public void toStandardHours_whenConvertingMinValue_throwsArithmeticException() {
+        // Arrange: Start with the minimum possible value for Weeks.
+        final Weeks minWeeks = Weeks.MIN_VALUE;
+        final String expectedErrorMessage = "Multiplication overflows an int: -2147483648 * 168";
+
+        // Act & Assert: Attempt the conversion and verify the expected exception is thrown.
         try {
-            weeks0.toStandardHours();
-            fail("Expecting exception: ArithmeticException");
+            minWeeks.toStandardHours();
+            fail("Expected an ArithmeticException to be thrown due to integer overflow, but it was not.");
         } catch (ArithmeticException e) {
-            //
-            // Multiplication overflows an int: -2147483648 * 168
-            //
-            verifyException("org.joda.time.field.FieldUtils", e);
+            // Verify that the exception was thrown for the correct reason.
+            assertEquals(expectedErrorMessage, e.getMessage());
         }
     }
 }
