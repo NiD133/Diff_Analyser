6a3c955886275e4c25e9180f198063f090086639
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:26:27 +0800

    Original processedAutoTestCases verificationovertimeimpl_estest18.java

---
+++ verificationovertimeimpl_estest18.java
@@ -0,0 +1,27 @@
+package org.mockito.internal.verification;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.NoSuchElementException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+import org.mockito.internal.creation.MockSettingsImpl;
+import org.mockito.internal.invocation.InvocationMatcher;
+import org.mockito.internal.stubbing.InvocationContainerImpl;
+import org.mockito.internal.util.Timer;
+import org.mockito.internal.verification.api.VerificationData;
+import org.mockito.verification.After;
+import org.mockito.verification.Timeout;
+import org.mockito.verification.VerificationMode;
+
+public class VerificationOverTimeImpl_ESTestTest18 extends VerificationOverTimeImpl_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test17() throws Throwable {
+        VerificationOverTimeImpl verificationOverTimeImpl0 = new VerificationOverTimeImpl(0L, 0L, (VerificationMode) null, false);
+        verificationOverTimeImpl0.getTimer();
+        assertFalse(verificationOverTimeImpl0.isReturnOnSuccess());
+    }
+}
