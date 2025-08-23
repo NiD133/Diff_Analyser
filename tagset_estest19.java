a6af6ab828314783d7769ce0c21bb27fbb9faadb
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:20:15 +0800

    Original processedAutoTestCases tagset_estest19.java

---
+++ tagset_estest19.java
@@ -0,0 +1,28 @@
+package org.jsoup.parser;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.function.Consumer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class TagSet_ESTestTest19 extends TagSet_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test18() throws Throwable {
+        TagSet tagSet0 = TagSet.Html();
+        // Undeclared exception!
+        try {
+            tagSet0.add((Tag) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+        }
+    }
+}
