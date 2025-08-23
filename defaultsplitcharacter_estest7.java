a57b3075dff02ef8634d86636b47ba6c1c3125d0
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:47:53 +0800

    Original processedAutoTestCases defaultsplitcharacter_estest7.java

---
+++ defaultsplitcharacter_estest7.java
@@ -0,0 +1,30 @@
+package com.itextpdf.text.pdf;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.itextpdf.text.Chunk;
+import com.itextpdf.text.GreekList;
+import com.itextpdf.text.TabSettings;
+import java.util.ArrayList;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class DefaultSplitCharacter_ESTestTest7 extends DefaultSplitCharacter_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test06() throws Throwable {
+        DefaultSplitCharacter defaultSplitCharacter0 = new DefaultSplitCharacter();
+        // Undeclared exception!
+        try {
+            defaultSplitCharacter0.isSplitCharacter(505, 505, 505, (char[]) null, (PdfChunk[]) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("com.itextpdf.text.pdf.DefaultSplitCharacter", e);
+        }
+    }
+}
