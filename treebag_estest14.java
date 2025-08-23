efb5443d7828108493b4cdf568fb1030045acfd6
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:23:48 +0800

    Original processedAutoTestCases treebag_estest14.java

---
+++ treebag_estest14.java
@@ -0,0 +1,32 @@
+package org.apache.commons.collections4.bag;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.EnumSet;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.Locale;
+import java.util.NoSuchElementException;
+import java.util.SortedMap;
+import org.apache.commons.collections4.Predicate;
+import org.apache.commons.collections4.functors.AnyPredicate;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class TreeBag_ESTestTest14 extends TreeBag_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test13() throws Throwable {
+        Comparator<Object> comparator0 = (Comparator<Object>) mock(Comparator.class, new ViolatedAssumptionAnswer());
+        doReturn((String) null).when(comparator0).toString();
+        TreeBag<Object> treeBag0 = new TreeBag<Object>(comparator0);
+        Comparator<? super Object> comparator1 = treeBag0.comparator();
+        assertNotNull(comparator1);
+    }
+}
