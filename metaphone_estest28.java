816708c8053dfe0a7dc6709b0921a40cd4a77fa9
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:02:29 +0800

    gemini processedAutoTestCases metaphone_estest28.java

--- metaphone_estest28.java
+++ metaphone_estest28.java
@@ -1,19 +1,46 @@
 package org.apache.commons.codec.language;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class Metaphone_ESTestTest28 extends Metaphone_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link Metaphone} class.
+ */
+public class MetaphoneTest {
 
-    @Test(timeout = 4000)
-    public void test27() throws Throwable {
-        Metaphone metaphone0 = new Metaphone();
-        String string0 = metaphone0.metaphone("GA");
-        assertEquals(4, metaphone0.getMaxCodeLen());
-        assertEquals("K", string0);
+    /**
+     * Tests that the Metaphone encoding for "GA" is "K".
+     * This specific case verifies the rule where an initial 'G' followed by 'A'
+     * is encoded as 'K'.
+     */
+    @Test
+    public void shouldEncodeGaAsK() {
+        // Arrange
+        final Metaphone metaphone = new Metaphone();
+        final String input = "GA";
+        final String expectedCode = "K";
+
+        // Act
+        final String actualCode = metaphone.metaphone(input);
+
+        // Assert
+        assertEquals("The Metaphone code for 'GA' should be 'K'", expectedCode, actualCode);
+    }
+
+    /**
+     * Tests that a new Metaphone instance is initialized with the default
+     * maximum code length of 4.
+     */
+    @Test
+    public void shouldHaveDefaultMaxCodeLengthOfFour() {
+        // Arrange
+        final Metaphone metaphone = new Metaphone();
+        final int expectedMaxCodeLen = 4;
+
+        // Act
+        final int actualMaxCodeLen = metaphone.getMaxCodeLen();
+
+        // Assert
+        assertEquals("Default max code length should be 4", expectedMaxCodeLen, actualMaxCodeLen);
     }
 }
