7fac6d5bbb0df4d9e11afac1df5bf1e93b94671a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:51:18 +0800

    gemini processedAutoTestCases filterlistiterator_estest59.java

--- filterlistiterator_estest59.java
+++ filterlistiterator_estest59.java
@@ -1,59 +1,32 @@
 package org.apache.commons.collections4.iterators;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.ConcurrentModificationException;
-import java.util.LinkedList;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Collections;
 import java.util.ListIterator;
-import java.util.Map;
-import java.util.NoSuchElementException;
-import java.util.function.Consumer;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.Equator;
-import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ClosureTransformer;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.DefaultEquator;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionClosure;
-import org.apache.commons.collections4.functors.ExceptionPredicate;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.IfClosure;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.MapTransformer;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NotNullPredicate;
-import org.apache.commons.collections4.functors.NotPredicate;
-import org.apache.commons.collections4.functors.NullIsExceptionPredicate;
-import org.apache.commons.collections4.functors.NullIsFalsePredicate;
-import org.apache.commons.collections4.functors.NullIsTruePredicate;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.OnePredicate;
-import org.apache.commons.collections4.functors.TransformedPredicate;
-import org.apache.commons.collections4.functors.TransformerPredicate;
-import org.apache.commons.collections4.functors.TruePredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.apache.commons.collections4.functors.WhileClosure;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class FilterListIterator_ESTestTest59 extends FilterListIterator_ESTest_scaffolding {
+/**
+ * Contains tests for the {@link FilterListIterator}.
+ */
+public class FilterListIteratorTest {
 
-    @Test(timeout = 4000)
-    public void test58() throws Throwable {
-        FilterListIterator<Object> filterListIterator0 = new FilterListIterator<Object>();
-        IdentityPredicate<Object> identityPredicate0 = new IdentityPredicate<Object>(filterListIterator0);
-        FilterListIterator<Object> filterListIterator1 = new FilterListIterator<Object>(filterListIterator0, identityPredicate0);
-        assertEquals((-1), filterListIterator1.previousIndex());
+    /**
+     * Tests that previousIndex() returns -1 for a newly created iterator,
+     * consistent with the ListIterator contract for an iterator at the beginning
+     * of a list.
+     */
+    @Test
+    public void previousIndexShouldReturnNegativeOneOnNewIterator() {
+        // Arrange: Create a FilterListIterator wrapping an empty iterator.
+        // The specific type of wrapped iterator or predicate is not relevant for this test.
+        final ListIterator<Object> emptyIterator = Collections.emptyListIterator();
+        final FilterListIterator<Object> filteredIterator = new FilterListIterator<>(emptyIterator);
+
+        // Act: Get the initial previous index.
+        final int previousIndex = filteredIterator.previousIndex();
+
+        // Assert: The index should be -1, as the iterator is at the start.
+        assertEquals("The previous index of a new iterator should be -1", -1, previousIndex);
     }
 }
