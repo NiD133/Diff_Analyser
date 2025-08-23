a8dc82325d53412618fc7999d287ba5f87401ff8
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:54:01 +0800

    gemini processedAutoTestCases indexedcollection_estest36.java

--- indexedcollection_estest36.java
+++ indexedcollection_estest36.java
@@ -1,67 +1,57 @@
 package org.apache.commons.collections4.collection;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.ConcurrentModificationException;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Set;
-import org.apache.commons.collections4.Closure;
 import org.apache.commons.collections4.Predicate;
 import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AllPredicate;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ChainedTransformer;
-import org.apache.commons.collections4.functors.CloneTransformer;
-import org.apache.commons.collections4.functors.ClosureTransformer;
-import org.apache.commons.collections4.functors.ConstantFactory;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.DefaultEquator;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.FactoryTransformer;
-import org.apache.commons.collections4.functors.FalsePredicate;
-import org.apache.commons.collections4.functors.ForClosure;
 import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.NOPClosure;
 import org.apache.commons.collections4.functors.NOPTransformer;
 import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NotNullPredicate;
-import org.apache.commons.collections4.functors.NullIsFalsePredicate;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.SwitchTransformer;
-import org.apache.commons.collections4.functors.TransformedPredicate;
-import org.apache.commons.collections4.functors.TransformerClosure;
-import org.apache.commons.collections4.functors.TransformerPredicate;
-import org.apache.commons.collections4.functors.TruePredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class IndexedCollection_ESTestTest36 extends IndexedCollection_ESTest_scaffolding {
+import java.util.Collection;
+import java.util.LinkedList;
 
-    @Test(timeout = 4000)
-    public void test35() throws Throwable {
-        Transformer<Object, Object> transformer0 = ConstantTransformer.nullTransformer();
-        LinkedList<Object> linkedList0 = new LinkedList<Object>();
-        Predicate<Object>[] predicateArray0 = (Predicate<Object>[]) Array.newInstance(Predicate.class, 5);
-        Class<Integer> class0 = Integer.class;
-        InstanceofPredicate instanceofPredicate0 = new InstanceofPredicate(class0);
-        NullIsFalsePredicate<Object> nullIsFalsePredicate0 = new NullIsFalsePredicate<Object>(instanceofPredicate0);
-        predicateArray0[0] = (Predicate<Object>) nullIsFalsePredicate0;
-        AllPredicate<Object> allPredicate0 = new AllPredicate<Object>(predicateArray0);
-        predicateArray0[1] = (Predicate<Object>) allPredicate0;
-        NonePredicate<Object> nonePredicate0 = new NonePredicate<Object>(predicateArray0);
-        predicateArray0[2] = (Predicate<Object>) nonePredicate0;
-        Transformer<Object, Object> transformer1 = IfTransformer.ifTransformer((Predicate<? super Object>) predicateArray0[2], (Transformer<? super Object, ?>) transformer0, (Transformer<? super Object, ?>) transformer0);
-        IndexedCollection<Object, Object> indexedCollection0 = IndexedCollection.nonUniqueIndexedCollection((Collection<Object>) linkedList0, transformer1);
-        // Undeclared exception!
-        indexedCollection0.contains(predicateArray0[3]);
+/**
+ * Contains test cases for IndexedCollection that involve complex or problematic configurations.
+ * This class is separated for clarity from the original generated test suite.
+ */
+public class IndexedCollection_ESTestTest36 {
+
+    /**
+     * Tests that a StackOverflowError is thrown when contains() is called on an IndexedCollection
+     * whose key transformer uses a predicate with a circular reference. This is an edge case
+     * that tests the collection's behavior with a misconfigured, infinitely recursive transformer.
+     */
+    @Test(expected = StackOverflowError.class)
+    public void testContainsWithCircularTransformerPredicateThrowsStackOverflowError() {
+        // Arrange: Create a key transformer that contains a predicate with a circular reference.
+
+        // 1. A predicate array that will hold the circular reference.
+        @SuppressWarnings("unchecked")
+        final Predicate<Object>[] predicates = new Predicate[1];
+
+        // 2. A predicate that depends on the contents of the array.
+        final Predicate<Object> circularPredicate = new NonePredicate<>(predicates);
+
+        // 3. Create the circular reference by placing the predicate into its own dependency array.
+        //    Calling circularPredicate.evaluate() will now cause infinite recursion.
+        predicates[0] = circularPredicate;
+
+        // 4. A transformer that will execute the problematic predicate.
+        final Transformer<Object, Object> keyTransformer = IfTransformer.ifTransformer(
+                circularPredicate,
+                NOPTransformer.nopTransformer(),  // The transformer to use if true
+                NOPTransformer.nopTransformer()); // The transformer to use if false
+
+        // 5. The collection under test, configured with the recursive transformer.
+        final Collection<Object> initialCollection = new LinkedList<>();
+        final IndexedCollection<Object, Object> indexedCollection =
+                IndexedCollection.nonUniqueIndexedCollection(initialCollection, keyTransformer);
+
+        // Act: Call contains(), which triggers the key transformer, leading to a StackOverflowError.
+        // The actual object passed to contains() does not matter.
+        indexedCollection.contains("any object");
+
+        // Assert: The test expects a StackOverflowError, which is handled by the
+        // @Test(expected = ...) annotation.
     }
 }
