e1b428d5b3d8beed409b7c9d32e977437417c3c4
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:26:20 +0800

    Original processedAutoTestCases vector_estest31.java

---
+++ vector_estest31.java
@@ -0,0 +1,18 @@
+package com.itextpdf.text.pdf.parser;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class Vector_ESTestTest31 extends Vector_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test30() throws Throwable {
+        Vector vector0 = new Vector((-2905.637F), (-2905.637F), (-1.0F));
+        vector0.hashCode();
+        assertEquals(4109.191F, vector0.length(), 0.01F);
+    }
+}
