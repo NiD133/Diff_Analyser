6b75c71f4f432f908a9dacad5705f195de101208
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:44:53 +0800

    Original processedAutoTestCases comparableobjectitem_estest5.java

---
+++ comparableobjectitem_estest5.java
@@ -0,0 +1,28 @@
+package org.jfree.data;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class ComparableObjectItem_ESTestTest5 extends ComparableObjectItem_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test04() throws Throwable {
+        Comparable<Object> comparable0 = (Comparable<Object>) mock(Comparable.class, new ViolatedAssumptionAnswer());
+        doReturn((String) null).when(comparable0).toString();
+        ComparableObjectItem comparableObjectItem0 = new ComparableObjectItem(comparable0, comparable0);
+        Object object0 = comparableObjectItem0.getObject();
+        // Undeclared exception!
+        try {
+            object0.hashCode();
+            //  fail("Expecting exception: IllegalArgumentException");
+            // Unstable assertion
+        } catch (IllegalArgumentException e) {
+        }
+    }
+}
