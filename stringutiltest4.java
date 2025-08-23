c7b8073fd8d4410adf3637e99f943c5e23e9347a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:34:19 +0800

    Original processedManualTestCases stringutiltest4.java

---
+++ stringutiltest4.java
@@ -0,0 +1,22 @@
+package org.jsoup.internal;
+
+import org.jsoup.Jsoup;
+import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import java.util.Collections;
+import static org.jsoup.internal.StringUtil.normaliseWhitespace;
+import static org.jsoup.internal.StringUtil.resolve;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StringUtilTestTest4 {
+
+    @Test
+    public void isBlank() {
+        assertTrue(StringUtil.isBlank(null));
+        assertTrue(StringUtil.isBlank(""));
+        assertTrue(StringUtil.isBlank("      "));
+        assertTrue(StringUtil.isBlank("   \r\n  "));
+        assertFalse(StringUtil.isBlank("hello"));
+        assertFalse(StringUtil.isBlank("   hello   "));
+    }
+}
