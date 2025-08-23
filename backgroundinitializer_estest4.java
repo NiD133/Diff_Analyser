231ffa508d95e11f9c49eb2b5cf5b9ae69388953
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:38:34 +0800

    Original processedAutoTestCases backgroundinitializer_estest4.java

---
+++ backgroundinitializer_estest4.java
@@ -0,0 +1,44 @@
+package org.apache.commons.lang3.concurrent;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.concurrent.Delayed;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.lang.MockException;
+import org.evosuite.runtime.mock.java.lang.MockThrowable;
+import org.evosuite.runtime.testdata.FileSystemHandling;
+import org.junit.runner.RunWith;
+
+public class BackgroundInitializer_ESTestTest4 extends BackgroundInitializer_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test03() throws Throwable {
+        ForkJoinPool forkJoinPool0 = ForkJoinPool.commonPool();
+        BackgroundInitializer<Delayed> backgroundInitializer0 = new BackgroundInitializer<Delayed>(forkJoinPool0);
+        forkJoinPool0.getFactory();
+        backgroundInitializer0.start();
+        backgroundInitializer0.getFuture();
+        BackgroundInitializer<Exception> backgroundInitializer1 = new BackgroundInitializer<Exception>();
+        BackgroundInitializer.builder();
+        backgroundInitializer1.getActiveExecutor();
+        backgroundInitializer1.isInitialized();
+        BackgroundInitializer<BackgroundInitializer<Exception>> backgroundInitializer2 = new BackgroundInitializer<BackgroundInitializer<Exception>>();
+        ConcurrentException concurrentException0 = new ConcurrentException();
+        concurrentException0.getSuppressed();
+        backgroundInitializer2.getTypedException(concurrentException0);
+        backgroundInitializer2.setExternalExecutor((ExecutorService) null);
+        ExecutorService executorService0 = backgroundInitializer1.getExternalExecutor();
+        assertNull(executorService0);
+    }
+}
