fccda3d992179c86f743463e07e15701547c4197
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:50:06 +0800

    gemini processedAutoTestCases extrafieldutils_estest9.java

--- extrafieldutils_estest9.java
+++ extrafieldutils_estest9.java
@@ -1,26 +1,19 @@
 package org.apache.commons.compress.archivers.zip;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.zip.ZipException;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class ExtraFieldUtils_ESTestTest9 extends ExtraFieldUtils_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link ExtraFieldUtils} class.
+ */
+public class ExtraFieldUtilsTest {
 
-    @Test(timeout = 4000)
-    public void test08() throws Throwable {
-        // Undeclared exception!
-        try {
-            ExtraFieldUtils.register((Class<?>) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.commons.compress.archivers.zip.ExtraFieldUtils", e);
-        }
+    /**
+     * Verifies that calling the register method with a null class argument
+     * results in a NullPointerException.
+     */
+    @Test(expected = NullPointerException.class)
+    public void registerShouldThrowNullPointerExceptionWhenClassIsNull() {
+        // Attempt to register a null class, which should trigger the exception.
+        ExtraFieldUtils.register(null);
     }
 }
