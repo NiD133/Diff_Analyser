15feeda222f2320ff0ff28f26893785da5d1c455
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:38:38 +0800

    gemini processedAutoTestCases base16_estest17.java

--- base16_estest17.java
+++ base16_estest17.java
@@ -1,31 +1,32 @@
 package org.apache.commons.codec.binary;
 
+import org.apache.commons.codec.binary.Base16;
+import org.apache.commons.codec.binary.BaseNCodec;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.apache.commons.codec.CodecPolicy;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class Base16_ESTestTest17 extends Base16_ESTest_scaffolding {
+/**
+ * Test suite for {@link Base16}.
+ */
+public class Base16Test {
 
-    @Test(timeout = 4000)
-    public void test16() throws Throwable {
-        CodecPolicy codecPolicy0 = CodecPolicy.LENIENT;
-        Base16 base16_0 = new Base16(true, codecPolicy0);
-        byte[] byteArray0 = new byte[1];
-        BaseNCodec.Context baseNCodec_Context0 = new BaseNCodec.Context();
-        baseNCodec_Context0.ibitWorkArea = 76;
-        // Undeclared exception!
-        try {
-            base16_0.decode(byteArray0, (int) (byte) 1, (int) (byte) 1, baseNCodec_Context0);
-            fail("Expecting exception: ArrayIndexOutOfBoundsException");
-        } catch (ArrayIndexOutOfBoundsException e) {
-            //
-            // 1
-            //
-            verifyException("org.apache.commons.codec.binary.Base16", e);
-        }
+    /**
+     * Tests that the decode method throws an ArrayIndexOutOfBoundsException
+     * when the provided offset is outside the bounds of the input data array.
+     */
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void testDecodeWithInvalidOffsetThrowsException() {
+        // Arrange
+        final Base16 base16 = new Base16();
+        final byte[] data = new byte[5]; // An array of size 5
+        final BaseNCodec.Context context = new BaseNCodec.Context();
+
+        // Act: Attempt to decode from an offset that is outside the array's bounds.
+        // The offset (5) is an invalid index for an array of size 5 (valid indices are 0-4).
+        final int invalidOffset = 5;
+        final int length = 1;
+        base16.decode(data, invalidOffset, length, context);
+
+        // Assert: The test expects an ArrayIndexOutOfBoundsException to be thrown,
+        // which is handled by the @Test(expected=...) annotation.
     }
 }
