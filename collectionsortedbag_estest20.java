0fbd7c86aebc91cae7c637043dbee67c2a3cef5c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:44:30 +0800

    gemini processedAutoTestCases collectionsortedbag_estest20.java

--- collectionsortedbag_estest20.java
+++ collectionsortedbag_estest20.java
@@ -1,60 +1,35 @@
 package org.apache.commons.collections4.bag;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.ConcurrentModificationException;
-import java.util.LinkedList;
-import java.util.Locale;
-import java.util.PriorityQueue;
-import java.util.Set;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.Predicate;
 import org.apache.commons.collections4.SortedBag;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionPredicate;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.IfClosure;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.NOPClosure;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.TransformedPredicate;
-import org.apache.commons.collections4.functors.TransformerPredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class CollectionSortedBag_ESTestTest20 extends CollectionSortedBag_ESTest_scaffolding {
+import java.util.ArrayList;
+import java.util.Collection;
 
-    @Test(timeout = 4000)
-    public void test19() throws Throwable {
-        Comparator<Object> comparator0 = (Comparator<Object>) mock(Comparator.class, new ViolatedAssumptionAnswer());
-        doReturn((-1959)).when(comparator0).compare(any(), any());
-        TreeBag<Object> treeBag0 = new TreeBag<Object>(comparator0);
-        Object object0 = new Object();
-        treeBag0.add(object0);
-        TreeBag<Object> treeBag1 = new TreeBag<Object>();
-        CollectionSortedBag<Object> collectionSortedBag0 = new CollectionSortedBag<Object>(treeBag1);
-        // Undeclared exception!
-        try {
-            collectionSortedBag0.addAll(treeBag0);
-            fail("Expecting exception: ClassCastException");
-        } catch (ClassCastException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-        }
+/**
+ * Test suite for {@link CollectionSortedBag}.
+ */
+public class CollectionSortedBagTest {
+
+    /**
+     * Tests that addAll() throws a ClassCastException when attempting to add a
+     * non-comparable element to a bag that relies on natural ordering.
+     */
+    @Test(expected = ClassCastException.class)
+    public void addAllShouldThrowClassCastExceptionWhenAddingNonComparableToBagWithNaturalOrdering() {
+        // Arrange: Create a CollectionSortedBag that decorates a TreeBag.
+        // The default TreeBag constructor means it relies on the natural ordering
+        // of its elements (i.e., they must implement Comparable).
+        final SortedBag<Object> naturalOrderingBag = new CollectionSortedBag<>(new TreeBag<>());
+
+        // Arrange: Create a source collection containing an element that does not
+        // implement Comparable. A plain Object is a perfect example.
+        final Collection<Object> sourceWithNonComparableElement = new ArrayList<>();
+        sourceWithNonComparableElement.add(new Object());
+
+        // Act & Assert: When addAll is called, the underlying TreeBag will try to
+        // cast the new element to Comparable to determine its position. This fails
+        // and is expected to throw a ClassCastException.
+        naturalOrderingBag.addAll(sourceWithNonComparableElement);
     }
 }
