1fc36f83d4134e63717d96b0d4ba13f945e54e11
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:44:51 +0800

    Original processedAutoTestCases comparableobjectitem_estest15.java

---
+++ comparableobjectitem_estest15.java
@@ -0,0 +1,22 @@
+package org.jfree.data;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class ComparableObjectItem_ESTestTest15 extends ComparableObjectItem_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test14() throws Throwable {
+        Comparable<ComparableObjectItem> comparable0 = (Comparable<ComparableObjectItem>) mock(Comparable.class, new ViolatedAssumptionAnswer());
+        Object object0 = new Object();
+        ComparableObjectItem comparableObjectItem0 = new ComparableObjectItem(comparable0, object0);
+        boolean boolean0 = comparableObjectItem0.equals(comparableObjectItem0);
+        assertTrue(boolean0);
+    }
+}
