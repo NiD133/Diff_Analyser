eba4c77945c9ec1b62e5701d25aab1a8edb9316d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:06:45 +0800

    gemini processedAutoTestCases orderedproperties_estest22.java

--- orderedproperties_estest22.java
+++ orderedproperties_estest22.java
@@ -1,67 +1,46 @@
 package org.apache.commons.collections4.properties;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.Reader;
-import java.io.StringReader;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.LinkedHashSet;
-import java.util.Map;
-import java.util.NoSuchElementException;
-import java.util.Set;
-import java.util.function.BiConsumer;
-import java.util.function.BiFunction;
+
 import java.util.function.Function;
-import org.apache.commons.collections4.Equator;
-import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AllPredicate;
-import org.apache.commons.collections4.functors.CloneTransformer;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.DefaultEquator;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.NOPTransformer;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NotNullPredicate;
-import org.apache.commons.collections4.functors.NullIsTruePredicate;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.SwitchTransformer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
 
-public class OrderedProperties_ESTestTest22 extends OrderedProperties_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
-    @Test(timeout = 4000)
-    public void test21() throws Throwable {
-        OrderedProperties orderedProperties0 = new OrderedProperties();
-        LinkedHashSet<Predicate<Object>> linkedHashSet0 = new LinkedHashSet<Predicate<Object>>();
-        Predicate<Object> predicate0 = AllPredicate.allPredicate((Collection<? extends Predicate<? super Object>>) linkedHashSet0);
-        NullIsTruePredicate<Object> nullIsTruePredicate0 = new NullIsTruePredicate<Object>(predicate0);
-        Predicate<Object>[] predicateArray0 = (Predicate<Object>[]) Array.newInstance(Predicate.class, 9);
-        predicateArray0[0] = (Predicate<Object>) nullIsTruePredicate0;
-        Transformer<Object, Integer>[] transformerArray0 = (Transformer<Object, Integer>[]) Array.newInstance(Transformer.class, 0);
-        Transformer<Object, Integer> transformer0 = ConstantTransformer.nullTransformer();
-        SwitchTransformer<Object, Integer> switchTransformer0 = new SwitchTransformer<Object, Integer>(predicateArray0, transformerArray0, transformer0);
-        // Undeclared exception!
+/**
+ * Contains tests for the {@link OrderedProperties} class.
+ * This refactored test focuses on verifying exception propagation.
+ */
+public class OrderedPropertiesRefactoredTest {
+
+    /**
+     * Tests that an exception thrown by the mapping function in {@code computeIfAbsent}
+     * is correctly propagated to the caller.
+     */
+    @Test
+    public void computeIfAbsentShouldPropagateExceptionFromMappingFunction() {
+        // Arrange
+        final OrderedProperties properties = new OrderedProperties();
+        final Object key = "test.key";
+        final String expectedExceptionMessage = "Simulated failure in mapping function";
+
+        // Create a mapping function that is designed to throw an exception.
+        // This simulates a scenario where the computation of a new value fails.
+        final Function<Object, Object> exceptionalMappingFunction = k -> {
+            throw new ArrayIndexOutOfBoundsException(expectedExceptionMessage);
+        };
+
+        // Act & Assert
         try {
-            orderedProperties0.computeIfAbsent(predicate0, switchTransformer0);
-            fail("Expecting exception: ArrayIndexOutOfBoundsException");
-        } catch (ArrayIndexOutOfBoundsException e) {
-            //
-            // 0
-            //
-            verifyException("org.apache.commons.collections4.functors.SwitchTransformer", e);
+            properties.computeIfAbsent(key, exceptionalMappingFunction);
+            fail("Expected an ArrayIndexOutOfBoundsException to be propagated.");
+        } catch (final ArrayIndexOutOfBoundsException e) {
+            // Verify that the exception from the mapping function was caught.
+            assertEquals(expectedExceptionMessage, e.getMessage());
+
+            // Also, verify that the properties map remains unchanged after the failure.
+            assertTrue("Properties should remain empty after a failed computation", properties.isEmpty());
         }
     }
 }
