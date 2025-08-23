00f2d0b289156a551c35adf0e75947ae2cb25ecb
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:36:25 +0800

    Original processedManualTestCases xylineannotationtest5.java

---
+++ xylineannotationtest5.java
@@ -0,0 +1,28 @@
+package org.jfree.chart.annotations;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.GradientPaint;
+import java.awt.Stroke;
+import org.jfree.chart.TestUtils;
+import org.jfree.chart.api.PublicCloneable;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class XYLineAnnotationTestTest5 {
+
+    private static final double EPSILON = 0.000000001;
+
+    /**
+     * Confirm that cloning works.
+     */
+    @Test
+    public void testCloning() throws CloneNotSupportedException {
+        Stroke stroke = new BasicStroke(2.0f);
+        XYLineAnnotation a1 = new XYLineAnnotation(10.0, 20.0, 100.0, 200.0, stroke, Color.BLUE);
+        XYLineAnnotation a2 = (XYLineAnnotation) a1.clone();
+        assertNotSame(a1, a2);
+        assertSame(a1.getClass(), a2.getClass());
+        assertEquals(a1, a2);
+    }
+}
