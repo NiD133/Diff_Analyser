fe3e0f1e6af9c2c419fe1373c442ff2433e7075a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:49:50 +0800

    Original processedAutoTestCases extensionfunction_estest2.java

---
+++ extensionfunction_estest2.java
@@ -0,0 +1,35 @@
+package org.apache.commons.jxpath.ri.compiler;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.Locale;
+import org.apache.commons.jxpath.BasicNodeSet;
+import org.apache.commons.jxpath.Function;
+import org.apache.commons.jxpath.JXPathContext;
+import org.apache.commons.jxpath.ri.EvalContext;
+import org.apache.commons.jxpath.ri.JXPathContextReferenceImpl;
+import org.apache.commons.jxpath.ri.NamespaceResolver;
+import org.apache.commons.jxpath.ri.QName;
+import org.apache.commons.jxpath.ri.axes.InitialContext;
+import org.apache.commons.jxpath.ri.axes.NodeSetContext;
+import org.apache.commons.jxpath.ri.axes.RootContext;
+import org.apache.commons.jxpath.ri.model.NodePointer;
+import org.apache.commons.jxpath.ri.model.VariablePointer;
+import org.apache.commons.jxpath.ri.model.beans.BeanPointer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class ExtensionFunction_ESTestTest2 extends ExtensionFunction_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test01() throws Throwable {
+        Expression[] expressionArray0 = new Expression[1];
+        ExtensionFunction extensionFunction0 = new ExtensionFunction((QName) null, expressionArray0);
+        boolean boolean0 = extensionFunction0.computeContextDependent();
+        assertTrue(boolean0);
+    }
+}
