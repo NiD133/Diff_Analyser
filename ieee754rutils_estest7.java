2276def05949fca67e96e70b075707e1426ec87c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:53:38 +0800

    Original processedAutoTestCases ieee754rutils_estest7.java

---
+++ ieee754rutils_estest7.java
@@ -0,0 +1,17 @@
+package org.apache.commons.lang3.math;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class IEEE754rUtils_ESTestTest7 extends IEEE754rUtils_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test06() throws Throwable {
+        float float0 = IEEE754rUtils.min(0.0F, 0.0F);
+        assertEquals(0.0F, float0, 0.01F);
+    }
+}
