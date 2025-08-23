7bfd734cdc3420bbcbf956e6b25c81403bbf9028
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:13:53 +0800

    Original processedAutoTestCases segmentconstantpool_estest57.java

---
+++ segmentconstantpool_estest57.java
@@ -0,0 +1,28 @@
+package org.apache.commons.compress.harmony.unpack200;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.IOException;
+import org.apache.commons.compress.harmony.unpack200.bytecode.ClassFileEntry;
+import org.apache.commons.compress.harmony.unpack200.bytecode.ConstantPoolEntry;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class SegmentConstantPool_ESTestTest57 extends SegmentConstantPool_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test56() throws Throwable {
+        SegmentConstantPool segmentConstantPool0 = new SegmentConstantPool((CpBands) null);
+        try {
+            segmentConstantPool0.getClassSpecificPoolEntry((-1734632697), (-1734632697), "^<init>.*");
+            fail("Expecting exception: IOException");
+        } catch (IOException e) {
+            //
+            // Type is not supported yet: -1734632697
+            //
+            verifyException("org.apache.commons.compress.harmony.unpack200.SegmentConstantPool", e);
+        }
+    }
+}
