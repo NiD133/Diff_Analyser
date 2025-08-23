0f1a293a67b60b3ff9beb0ec3b26198c5ecb1a2f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:52:01 +0800

    gemini processedAutoTestCases gregorianchronology_estest16.java

--- gregorianchronology_estest16.java
+++ gregorianchronology_estest16.java
@@ -1,23 +1,40 @@
 package org.joda.time.chrono;
 
+import org.joda.time.DateTimeConstants;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.TimeZone;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.joda.time.Chronology;
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
-import org.joda.time.LocalDateTime;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class GregorianChronology_ESTestTest16 extends GregorianChronology_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link GregorianChronology} class.
+ */
+public class GregorianChronologyTest {
 
-    @Test(timeout = 4000)
-    public void test15() throws Throwable {
-        GregorianChronology gregorianChronology0 = GregorianChronology.getInstance();
-        long long0 = gregorianChronology0.getAverageMillisPerYear();
-        assertEquals(31556952000L, long0);
+    /**
+     * Verifies that getAverageMillisPerYear() returns the correct, pre-calculated
+     * constant value for the average length of a year in the Gregorian calendar.
+     */
+    @Test
+    public void getAverageMillisPerYear_returnsCorrectConstantValue() {
+        // Arrange
+        // The average number of days in a Gregorian year is 365.2425.
+        // This is based on the leap year rule: a leap year occurs every 4 years,
+        // except for years divisible by 100 but not by 400.
+        // Over a 400-year cycle, this results in 97 leap years.
+        // Average days = (365 * 400 + 97) / 400 = 365.2425
+        final double AVERAGE_DAYS_PER_GREGORIAN_YEAR = 365.2425;
+        final long EXPECTED_AVERAGE_MILLIS_PER_YEAR =
+                (long) (AVERAGE_DAYS_PER_GREGORIAN_YEAR * DateTimeConstants.MILLIS_PER_DAY);
+
+        GregorianChronology chronology = GregorianChronology.getInstance();
+
+        // Act
+        long actualAverageMillisPerYear = chronology.getAverageMillisPerYear();
+
+        // Assert
+        assertEquals(
+                "The average milliseconds per year should match the Gregorian calendar definition.",
+                EXPECTED_AVERAGE_MILLIS_PER_YEAR,
+                actualAverageMillisPerYear
+        );
     }
 }
