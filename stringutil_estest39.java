67a697a083714fac8f284d483df77cf8f7696ab9
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:18:53 +0800

    Original processedAutoTestCases stringutil_estest39.java

---
+++ stringutil_estest39.java
@@ -0,0 +1,29 @@
+package org.jsoup.internal;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Collection;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.stream.Collector;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.net.MockURL;
+import org.junit.runner.RunWith;
+
+public class StringUtil_ESTestTest39 extends StringUtil_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test38() throws Throwable {
+        StringBuilder stringBuilder0 = StringUtil.borrowBuilder();
+        StringUtil.appendNormalisedWhitespace(stringBuilder0, "                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    ", false);
+        assertEquals(" ", stringBuilder0.toString());
+    }
+}
