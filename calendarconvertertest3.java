0d25ee2a9d6f1b88423cc3b459f4245508819b63
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:26:15 +0800

    gemini processedManualTestCases calendarconvertertest3.java

--- calendarconvertertest3.java
+++ calendarconvertertest3.java
@@ -1,53 +1,43 @@
 package org.joda.time.convert;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-import java.util.Arrays;
+import static org.junit.Assert.assertEquals;
+
 import java.util.Calendar;
-import java.util.Date;
 import java.util.GregorianCalendar;
-import java.util.TimeZone;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
 import org.joda.time.Chronology;
-import org.joda.time.DateTimeZone;
-import org.joda.time.TimeOfDay;
-import org.joda.time.chrono.BuddhistChronology;
-import org.joda.time.chrono.GJChronology;
-import org.joda.time.chrono.GregorianChronology;
-import org.joda.time.chrono.ISOChronology;
 import org.joda.time.chrono.JulianChronology;
+import org.junit.Test;
 
-public class CalendarConverterTestTest3 extends TestCase {
+/**
+ * Unit tests for {@link CalendarConverter#getInstantMillis(Object, Chronology)}.
+ */
+public class CalendarConverterTest {
 
-    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
+    /**
+     * Tests that getInstantMillis() correctly extracts the millisecond value from a Calendar
+     * instance, ignoring the provided Chronology. It also verifies that the original
+     * Calendar object is not modified during the conversion.
+     */
+    @Test
+    public void getInstantMillis_shouldReturnMillisFromCalendarAndIgnoreChronology() {
+        // Arrange
+        long expectedMillis = 123L;
+        Calendar calendar = new GregorianCalendar();
+        calendar.setTimeInMillis(expectedMillis);
 
-    private static final DateTimeZone MOSCOW = DateTimeZone.forID("Europe/Moscow");
+        // The converter is expected to ignore this Chronology and simply return
+        // the millis from the Calendar object itself.
+        Chronology ignoredChronology = JulianChronology.getInstance();
+        
+        // Act
+        long actualMillis = CalendarConverter.INSTANCE.getInstantMillis(calendar, ignoredChronology);
 
-    private static Chronology JULIAN;
+        // Assert
+        // 1. The returned value should be the millisecond instant from the calendar.
+        assertEquals(expectedMillis, actualMillis);
 
-    private static Chronology ISO;
-
-    public static void main(String[] args) {
-        junit.textui.TestRunner.run(suite());
-    }
-
-    public static TestSuite suite() {
-        return new TestSuite(TestCalendarConverter.class);
-    }
-
-    @Override
-    protected void setUp() throws Exception {
-        JULIAN = JulianChronology.getInstance();
-        ISO = ISOChronology.getInstance();
-    }
-
-    //-----------------------------------------------------------------------
-    public void testGetInstantMillis_Object_Chronology() throws Exception {
-        GregorianCalendar cal = new GregorianCalendar();
-        cal.setTime(new Date(123L));
-        assertEquals(123L, CalendarConverter.INSTANCE.getInstantMillis(cal, JULIAN));
-        assertEquals(123L, cal.getTime().getTime());
+        // 2. The original Calendar object should not be modified (no side effects).
+        assertEquals("The converter should not modify the original calendar object.",
+                expectedMillis, calendar.getTimeInMillis());
     }
 }
