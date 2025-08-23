c670be8d54b5c3a91dfcea9c29d44aaecbc892e5
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:11:34 +0800

    gemini processedAutoTestCases randomaccessfileorarray_estest163.java

--- randomaccessfileorarray_estest163.java
+++ randomaccessfileorarray_estest163.java
@@ -1,39 +1,29 @@
 package com.itextpdf.text.pdf;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import com.itextpdf.text.io.GetBufferedRandomAccessSource;
-import com.itextpdf.text.io.IndependentRandomAccessSource;
 import com.itextpdf.text.io.RandomAccessSource;
-import com.itextpdf.text.io.WindowRandomAccessSource;
-import java.io.ByteArrayInputStream;
-import java.io.EOFException;
-import java.io.FileNotFoundException;
+import org.junit.Test;
+
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.PipedInputStream;
-import java.net.URL;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.net.MockURL;
-import org.evosuite.runtime.testdata.EvoSuiteFile;
-import org.evosuite.runtime.testdata.FileSystemHandling;
-import org.junit.runner.RunWith;
 
-public class RandomAccessFileOrArray_ESTestTest163 extends RandomAccessFileOrArray_ESTest_scaffolding {
+/**
+ * Contains tests for the {@link RandomAccessFileOrArray} class.
+ */
+public class RandomAccessFileOrArrayTest {
 
-    @Test(timeout = 4000)
-    public void test162() throws Throwable {
-        RandomAccessFileOrArray randomAccessFileOrArray0 = new RandomAccessFileOrArray((RandomAccessSource) null);
-        // Undeclared exception!
-        try {
-            randomAccessFileOrArray0.readDoubleLE();
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-        }
+    /**
+     * Verifies that attempting to read from a RandomAccessFileOrArray
+     * that was initialized with a null data source results in a NullPointerException.
+     */
+    @Test(expected = NullPointerException.class)
+    public void readMethods_shouldThrowNullPointerException_whenSourceIsNull() throws IOException {
+        // Arrange: Create an instance with a null internal data source.
+        RandomAccessFileOrArray fileOrArray = new RandomAccessFileOrArray((RandomAccessSource) null);
+
+        // Act: Attempt to read from the instance.
+        // This is expected to throw a NullPointerException because the internal 'byteSource' is null.
+        fileOrArray.readDoubleLE();
+
+        // Assert: The test passes if a NullPointerException is thrown,
+        // which is handled by the @Test(expected) annotation.
     }
 }
