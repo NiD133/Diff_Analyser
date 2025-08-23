fbbcef5ca7b60b8dc26deef7bd16189afa6a55c5
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:27:05 +0800

    Original processedAutoTestCases weeks_estest15.java

---
+++ weeks_estest15.java
@@ -0,0 +1,18 @@
+package org.joda.time;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class Weeks_ESTestTest15 extends Weeks_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test14() throws Throwable {
+        Weeks weeks0 = Weeks.MIN_VALUE;
+        Weeks weeks1 = weeks0.MAX_VALUE.plus(weeks0);
+        assertEquals((-1), weeks1.getWeeks());
+    }
+}
