8a51da88c3265b4760964ede6604e9241025794f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:24:31 +0800

    Original processedAutoTestCases uncheck_estest25.java

---
+++ uncheck_estest25.java
@@ -0,0 +1,45 @@
+package org.apache.commons.io.function;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.lang.reflect.Array;
+import java.time.chrono.HijrahEra;
+import java.util.Comparator;
+import java.util.concurrent.ForkJoinTask;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+import java.util.stream.LongStream;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class Uncheck_ESTestTest25 extends Uncheck_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test24() throws Throwable {
+        Supplier<String> supplier0 = (Supplier<String>) mock(Supplier.class, new ViolatedAssumptionAnswer());
+        IOBiConsumer<String, String> iOBiConsumer0 = IOBiConsumer.noop();
+        BiConsumer<String, String> biConsumer0 = iOBiConsumer0.asBiConsumer();
+        Comparator<String> comparator0 = (Comparator<String>) mock(Comparator.class, new ViolatedAssumptionAnswer());
+        BinaryOperator<String> binaryOperator0 = BinaryOperator.minBy((Comparator<? super String>) comparator0);
+        Collector.Characteristics[] collector_CharacteristicsArray0 = (Collector.Characteristics[]) Array.newInstance(Collector.Characteristics.class, 0);
+        Collector<String, String, String> collector0 = Collector.of(supplier0, biConsumer0, binaryOperator0, (Collector.Characteristics[]) collector_CharacteristicsArray0);
+        Supplier<String> supplier1 = collector0.supplier();
+        // Undeclared exception!
+        try {
+            Uncheck.run((IORunnable) null, supplier1);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.apache.commons.io.function.Uncheck", e);
+        }
+    }
+}
