6c8d45579560af717e5fe52775c041d0d7d5346b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:26:56 +0800

    Original processedManualTestCases compactlinkedhashsettest1.java

---
+++ compactlinkedhashsettest1.java
@@ -0,0 +1,57 @@
+package com.google.common.collect;
+
+import static com.google.common.truth.Truth.assertThat;
+import static java.lang.Math.max;
+import static java.util.Arrays.asList;
+import com.google.common.annotations.GwtIncompatible;
+import com.google.common.collect.testing.SetTestSuiteBuilder;
+import com.google.common.collect.testing.TestStringSetGenerator;
+import com.google.common.collect.testing.features.CollectionFeature;
+import com.google.common.collect.testing.features.CollectionSize;
+import com.google.common.collect.testing.features.Feature;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.jspecify.annotations.NullUnmarked;
+
+public class CompactLinkedHashSetTestTest1 extends TestCase {
+
+    // test-suite builders
+    @AndroidIncompatible
+    public static Test suite() {
+        List<Feature<?>> allFeatures = Arrays.<Feature<?>>asList(CollectionSize.ANY, CollectionFeature.ALLOWS_NULL_VALUES, CollectionFeature.FAILS_FAST_ON_CONCURRENT_MODIFICATION, CollectionFeature.GENERAL_PURPOSE, CollectionFeature.REMOVE_OPERATIONS, CollectionFeature.SERIALIZABLE, CollectionFeature.KNOWN_ORDER, CollectionFeature.SUPPORTS_ADD, CollectionFeature.SUPPORTS_REMOVE);
+        TestSuite suite = new TestSuite();
+        suite.addTestSuite(CompactLinkedHashSetTest.class);
+        suite.addTest(SetTestSuiteBuilder.using(new TestStringSetGenerator() {
+
+            @Override
+            protected Set<String> create(String[] elements) {
+                return CompactLinkedHashSet.create(asList(elements));
+            }
+        }).named("CompactLinkedHashSet").withFeatures(allFeatures).createTestSuite());
+        suite.addTest(SetTestSuiteBuilder.using(new TestStringSetGenerator() {
+
+            @Override
+            protected Set<String> create(String[] elements) {
+                CompactLinkedHashSet<String> set = CompactLinkedHashSet.create();
+                set.convertToHashFloodingResistantImplementation();
+                Collections.addAll(set, elements);
+                return set;
+            }
+        }).named("CompactLinkedHashSet with flooding protection").withFeatures(allFeatures).createTestSuite());
+        return suite;
+    }
+
+    public void testAllocArraysDefault() {
+        CompactHashSet<Integer> set = CompactHashSet.create();
+        assertThat(set.needsAllocArrays()).isTrue();
+        assertThat(set.elements).isNull();
+        set.add(1);
+        assertThat(set.needsAllocArrays()).isFalse();
+        assertThat(set.elements).hasLength(CompactHashing.DEFAULT_SIZE);
+    }
+}
