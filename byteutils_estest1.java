c5c94077db180fec8cd066e08265bab3f8b29467
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:40:57 +0800

    Original processedAutoTestCases byteutils_estest1.java

---
+++ byteutils_estest1.java
@@ -0,0 +1,35 @@
+package org.apache.commons.compress.utils;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInput;
+import java.io.DataInputStream;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.io.PushbackInputStream;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.io.MockFileOutputStream;
+import org.junit.runner.RunWith;
+
+public class ByteUtils_ESTestTest1 extends ByteUtils_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test00() throws Throwable {
+        ByteArrayOutputStream byteArrayOutputStream0 = new ByteArrayOutputStream();
+        ByteUtils.toLittleEndian((OutputStream) byteArrayOutputStream0, (-392L), 1633);
+        assertEquals(1633, byteArrayOutputStream0.size());
+    }
+}
