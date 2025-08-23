d265cde592b53a4b4b8df845ae76500ce23f58e4
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:35:02 +0800

    gemini processedManualTestCases transformedbagtest2.java

--- transformedbagtest2.java
+++ transformedbagtest2.java
@@ -3,6 +3,7 @@ package org.apache.commons.collections4.bag;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.apache.commons.collections4.Bag;
 import org.apache.commons.collections4.Transformer;
 import org.apache.commons.collections4.collection.TransformedCollectionTest;
@@ -23,24 +24,55 @@ public class TransformedBagTestTest2<T> extends AbstractBagTest<T> {
     @Override
     @SuppressWarnings("unchecked")
     public Bag<T> makeObject() {
+        // This factory method creates a bag that transforms elements as they are added.
+        // The NOOP_TRANSFORMER doesn't change the elements, which is suitable for the
+        // generic tests in the abstract parent class.
         return TransformedBag.transformingBag(new HashBag<>(), (Transformer<T, T>) TransformedCollectionTest.NOOP_TRANSFORMER);
     }
 
+    /**
+     * Tests that the {@link TransformedBag#transformedBag(Bag, Transformer)} factory method
+     * correctly transforms all existing elements of the decorated bag upon creation.
+     */
     @Test
-    @SuppressWarnings("unchecked")
-    void testTransformedBag_decorateTransform() {
-        final Bag<T> originalBag = new HashBag<>();
-        final Object[] els = { "1", "3", "5", "7", "2", "4", "6" };
-        for (final Object el : els) {
-            originalBag.add((T) el);
+    @SuppressWarnings("unchecked") // Required for casting the raw Transformer from TransformedCollectionTest
+    void testTransformedBagFactory_transformsExistingElements() {
+        // Arrange: Create a source bag with string elements.
+        final Bag<String> sourceBag = new HashBag<>();
+        final String[] stringElements = {"1", "3", "5", "7", "2", "4", "6"};
+        for (final String element : stringElements) {
+            sourceBag.add(element);
         }
-        final Bag<T> bag = TransformedBag.transformedBag(originalBag, (Transformer<T, T>) TransformedCollectionTest.STRING_TO_INTEGER_TRANSFORMER);
-        assertEquals(els.length, bag.size());
-        for (final Object el : els) {
-            assertTrue(bag.contains(Integer.valueOf((String) el)));
-            assertFalse(bag.contains(el));
+
+        final Transformer<String, Integer> stringToInteger =
+            (Transformer<String, Integer>) TransformedCollectionTest.STRING_TO_INTEGER_TRANSFORMER;
+
+        // Act: Decorate the source bag. This factory method is expected to convert
+        // all existing elements from String to Integer.
+        final Bag<Integer> transformedBag = TransformedBag.transformedBag(sourceBag, stringToInteger);
+
+        // Assert
+        assertEquals(stringElements.length, transformedBag.size(), "Size should be maintained after transformation");
+
+        // Verify the bag contains the transformed integers, not the original strings.
+        for (final String originalElement : stringElements) {
+            final Integer transformedElement = Integer.valueOf(originalElement);
+            assertTrue(transformedBag.contains(transformedElement),
+                "Bag should contain the transformed element: " + transformedElement);
+            assertFalse(transformedBag.contains(originalElement),
+                "Bag should not contain the original string element: " + originalElement);
         }
-        assertFalse(bag.remove(els[0]));
-        assertTrue(bag.remove(Integer.valueOf((String) els[0])));
+
+        // Verify remove behavior with a sample element.
+        final String elementToRemoveAsString = stringElements[0]; // "1"
+        final Integer elementToRemoveAsInteger = Integer.valueOf(elementToRemoveAsString);
+
+        // Attempting to remove the original string should fail.
+        assertFalse(transformedBag.remove(elementToRemoveAsString),
+            "Should not be able to remove using the pre-transformation object type");
+
+        // Removing the transformed integer should succeed.
+        assertTrue(transformedBag.remove(elementToRemoveAsInteger),
+            "Should be able to remove using the post-transformation object type");
     }
 }
