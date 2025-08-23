c1a7007faf11054b22d482c2323b35aed6085bf7
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:58:52 +0800

    Original processedAutoTestCases jsontypeinfo_estest2.java

---
+++ jsontypeinfo_estest2.java
@@ -0,0 +1,20 @@
+package com.fasterxml.jackson.annotation;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class JsonTypeInfo_ESTestTest2 extends JsonTypeInfo_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test01() throws Throwable {
+        JsonTypeInfo.Value jsonTypeInfo_Value0 = JsonTypeInfo.Value.EMPTY;
+        JsonTypeInfo.Value jsonTypeInfo_Value1 = jsonTypeInfo_Value0.withPropertyName((String) null);
+        assertSame(jsonTypeInfo_Value1, jsonTypeInfo_Value0);
+    }
+}
