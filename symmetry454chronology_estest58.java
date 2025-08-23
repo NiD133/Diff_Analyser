6e1b50107739c407306714c92aeebab7816a4c30
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:20:07 +0800

    gemini processedAutoTestCases symmetry454chronology_estest58.java

--- symmetry454chronology_estest58.java
+++ symmetry454chronology_estest58.java
@@ -1,43 +1,28 @@
 package org.threeten.extra.chrono;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.time.Clock;
-import java.time.DateTimeException;
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.OffsetDateTime;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
-import java.time.chrono.ChronoLocalDateTime;
-import java.time.chrono.ChronoZonedDateTime;
-import java.time.chrono.Era;
-import java.time.chrono.HijrahEra;
 import java.time.chrono.IsoEra;
-import java.time.chrono.JapaneseEra;
-import java.time.temporal.ChronoField;
-import java.time.temporal.ChronoUnit;
-import java.time.temporal.TemporalAccessor;
-import java.time.temporal.TemporalUnit;
-import java.time.temporal.UnsupportedTemporalTypeException;
-import java.time.temporal.ValueRange;
-import java.util.List;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.System;
-import org.evosuite.runtime.mock.java.time.MockClock;
-import org.evosuite.runtime.mock.java.time.MockInstant;
-import org.evosuite.runtime.mock.java.time.MockLocalDateTime;
-import org.evosuite.runtime.mock.java.time.MockOffsetDateTime;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class Symmetry454Chronology_ESTestTest58 extends Symmetry454Chronology_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link Symmetry454Chronology} class.
+ */
+public class Symmetry454ChronologyTest {
 
-    @Test(timeout = 4000)
-    public void test57() throws Throwable {
-        Symmetry454Chronology symmetry454Chronology0 = new Symmetry454Chronology();
-        Symmetry454Date symmetry454Date0 = symmetry454Chronology0.dateNow();
-        assertEquals(IsoEra.CE, symmetry454Date0.getEra());
+    /**
+     * Tests that {@link Symmetry454Chronology#dateNow()} returns a date
+     * within the Common Era (CE), assuming the system clock is set to a
+     * modern date.
+     */
+    @Test
+    public void dateNow_shouldReturnDateInCommonEra() {
+        // Arrange
+        Symmetry454Chronology chronology = Symmetry454Chronology.INSTANCE;
+
+        // Act
+        Symmetry454Date currentDate = chronology.dateNow();
+
+        // Assert
+        assertEquals("The era of the current date should be Common Era (CE)", IsoEra.CE, currentDate.getEra());
     }
 }
