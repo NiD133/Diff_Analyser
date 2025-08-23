704ab77604988e0747846e9b00284696da990653
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:05:44 +0800

    gemini processedAutoTestCases objectgraphiterator_estest29.java

--- objectgraphiterator_estest29.java
+++ objectgraphiterator_estest29.java
@@ -1,64 +1,43 @@
 package org.apache.commons.collections4.iterators;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Comparator;
-import java.util.ConcurrentModificationException;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.ListIterator;
-import java.util.Map;
-import java.util.NoSuchElementException;
-import java.util.function.Function;
-import org.apache.commons.collections4.Factory;
-import org.apache.commons.collections4.Predicate;
 import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AndPredicate;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantFactory;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionPredicate;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.FactoryTransformer;
-import org.apache.commons.collections4.functors.FalsePredicate;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
 import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.MapTransformer;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NullIsFalsePredicate;
-import org.apache.commons.collections4.functors.NullIsTruePredicate;
-import org.apache.commons.collections4.functors.OrPredicate;
-import org.apache.commons.collections4.functors.PredicateTransformer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class ObjectGraphIterator_ESTestTest29 extends ObjectGraphIterator_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
-    @Test(timeout = 4000)
-    public void test28() throws Throwable {
-        Integer integer0 = new Integer((-227));
-        Object[] objectArray0 = new Object[1];
-        objectArray0[0] = (Object) "?}4!038|{z#HLh9";
-        InvokerTransformer<Integer, Integer> invokerTransformer0 = new InvokerTransformer<Integer, Integer>("?}4!038|{z#HLh9", (Class<?>[]) null, objectArray0);
-        ObjectGraphIterator<Integer> objectGraphIterator0 = new ObjectGraphIterator<Integer>(integer0, invokerTransformer0);
-        // Undeclared exception!
+/**
+ * Contains tests for {@link ObjectGraphIterator}.
+ */
+public class ObjectGraphIteratorTest {
+
+    /**
+     * Tests that if the provided Transformer throws an exception during the
+     * first call to hasNext(), the exception is propagated to the caller.
+     */
+    @Test
+    public void hasNextShouldPropagateExceptionFromFailingTransformer() {
+        // Arrange
+        final Integer root = 42;
+        final String nonExistentMethodName = "thisMethodDoesNotExist";
+
+        // This transformer is designed to fail by trying to invoke a method
+        // that does not exist on the Integer class.
+        final Transformer<Integer, ?> failingTransformer =
+                new InvokerTransformer<>(nonExistentMethodName, null, null);
+
+        final ObjectGraphIterator<Integer> iterator = new ObjectGraphIterator<>(root, failingTransformer);
+
+        // Act & Assert
         try {
-            objectGraphIterator0.hasNext();
-            fail("Expecting exception: RuntimeException");
-        } catch (RuntimeException e) {
-            //
-            // InvokerTransformer: The method '?}4!038|{z#HLh9' on 'class java.lang.Integer' does not exist
-            //
-            verifyException("org.apache.commons.collections4.functors.InvokerTransformer", e);
+            iterator.hasNext();
+            fail("A RuntimeException was expected but not thrown.");
+        } catch (final RuntimeException e) {
+            // Verify that the exception is the one thrown by InvokerTransformer
+            final String expectedMessage = "InvokerTransformer: The method '" + nonExistentMethodName +
+                                           "' on 'class java.lang.Integer' does not exist";
+            assertEquals(expectedMessage, e.getMessage());
         }
     }
 }
