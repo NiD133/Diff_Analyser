8474c0f57fd2d08b0f8b222e2814dae73c1a38a3
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:38:27 +0800

    Original processedAutoTestCases attribute_estest73.java

---
+++ attribute_estest73.java
@@ -0,0 +1,32 @@
+package org.jsoup.nodes;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.ByteArrayOutputStream;
+import java.io.FilterOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PipedWriter;
+import java.io.StringWriter;
+import java.nio.BufferOverflowException;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetEncoder;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockPrintWriter;
+import org.jsoup.internal.QuietAppendable;
+import org.junit.runner.RunWith;
+
+public class Attribute_ESTestTest73 extends Attribute_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test72() throws Throwable {
+        Attributes attributes0 = new Attributes();
+        attributes0.add("5|pc", "@");
+        Attribute attribute0 = new Attribute("5|pc", "Bi", attributes0);
+        assertEquals("5|pc", attribute0.getKey());
+        attribute0.setKey("Bi");
+        assertEquals("Bi", attribute0.getValue());
+    }
+}
