d4634ae58b715834cba3a46806529d7dd839b997
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:56:34 +0800

    Original processedAutoTestCases jsonignoreproperties_estest37.java

---
+++ jsonignoreproperties_estest37.java
@@ -0,0 +1,27 @@
+package com.fasterxml.jackson.annotation;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.function.Predicate;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class JsonIgnoreProperties_ESTestTest37 extends JsonIgnoreProperties_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test36() throws Throwable {
+        JsonIgnoreProperties.Value jsonIgnoreProperties_Value0 = JsonIgnoreProperties.Value.forIgnoreUnknown(true);
+        Object object0 = new Object();
+        boolean boolean0 = jsonIgnoreProperties_Value0.equals(object0);
+        assertFalse(jsonIgnoreProperties_Value0.getAllowGetters());
+        assertTrue(jsonIgnoreProperties_Value0.getIgnoreUnknown());
+        assertTrue(jsonIgnoreProperties_Value0.getMerge());
+        assertFalse(jsonIgnoreProperties_Value0.getAllowSetters());
+        assertFalse(boolean0);
+    }
+}
