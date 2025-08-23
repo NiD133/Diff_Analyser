10b02f36e7eb1c46b7d5039908fda00bf18df05a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:49:18 +0800

    Original processedAutoTestCases endpointpair_estest7.java

---
+++ endpointpair_estest7.java
@@ -0,0 +1,27 @@
+package com.google.common.graph;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.google.common.collect.UnmodifiableIterator;
+import java.util.Locale;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class EndpointPair_ESTestTest7 extends EndpointPair_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test06() throws Throwable {
+        // Undeclared exception!
+        try {
+            EndpointPair.unordered((Object) null, (Object) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("com.google.common.base.Preconditions", e);
+        }
+    }
+}
