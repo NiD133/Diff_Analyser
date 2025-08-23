64fb47d190193aec14b1c11d6367db252b4e7e56
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:39:09 +0800

    gemini processedAutoTestCases bcodec_estest25.java

--- bcodec_estest25.java
+++ bcodec_estest25.java
@@ -1,22 +1,32 @@
 package org.apache.commons.codec.net;
 
+import org.apache.commons.codec.EncoderException;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.nio.charset.Charset;
-import java.nio.charset.IllegalCharsetNameException;
-import java.nio.charset.UnsupportedCharsetException;
-import org.apache.commons.codec.CodecPolicy;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class BCodec_ESTestTest25 extends BCodec_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
 
-    @Test(timeout = 4000)
-    public void test24() throws Throwable {
-        BCodec bCodec0 = new BCodec();
-        Object object0 = bCodec0.encode((Object) " encoded content");
-        assertEquals("=?UTF-8?B?IGVuY29kZWQgY29udGVudA==?=", object0);
+/**
+ * Tests for {@link BCodec}.
+ */
+public class BCodecTest {
+
+    @Test
+    public void testEncodeObjectWithStringUsesDefaultCharset() throws EncoderException {
+        // Arrange
+        final String plainText = " encoded content";
+        
+        // The expected format is defined by RFC 1522: "=?charset?B?encoded_text?="
+        // 'B' signifies Base64 encoding.
+        // The default BCodec constructor uses UTF-8 as the charset.
+        final String expectedEncodedText = "=?UTF-8?B?IGVuY29kZWQgY29udGVudA==?=";
+        
+        final BCodec bCodec = new BCodec();
+
+        // Act
+        // We cast the input to Object to specifically test the encode(Object) method overload.
+        final Object result = bCodec.encode((Object) plainText);
+
+        // Assert
+        assertEquals(expectedEncodedText, result);
     }
 }
