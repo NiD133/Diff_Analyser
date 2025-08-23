c034d225a0c79c7e8372ef95eb419c988164104a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:35:50 +0800

    Original processedManualTestCases weekstest12.java

---
+++ weekstest12.java
@@ -0,0 +1,38 @@
+package org.joda.time;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class WeeksTestTest12 extends TestCase {
+
+    // (before the late 90's they were all over the place)
+    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
+
+    public static TestSuite suite() {
+        return new TestSuite(TestWeeks.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+    }
+
+    public void testIsLessThan() {
+        assertEquals(false, Weeks.THREE.isLessThan(Weeks.TWO));
+        assertEquals(false, Weeks.THREE.isLessThan(Weeks.THREE));
+        assertEquals(true, Weeks.TWO.isLessThan(Weeks.THREE));
+        assertEquals(false, Weeks.ONE.isLessThan(null));
+        assertEquals(true, Weeks.weeks(-1).isLessThan(null));
+    }
+}
