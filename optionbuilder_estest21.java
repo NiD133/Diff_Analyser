804bd852acbcac5a5ed92eee623059d7450ff6f4
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:06:14 +0800

    Original processedAutoTestCases optionbuilder_estest21.java

---
+++ optionbuilder_estest21.java
@@ -0,0 +1,19 @@
+package org.apache.commons.cli;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class OptionBuilder_ESTestTest21 extends OptionBuilder_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test20() throws Throwable {
+        OptionBuilder.hasArgs();
+        OptionBuilder.withLongOpt("");
+        Option option0 = OptionBuilder.create();
+        assertEquals((-2), option0.getArgs());
+    }
+}
