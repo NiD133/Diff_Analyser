6707140990bf7df1abf723c1a6163e7963e0fba1
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:28:09 +0800

    Original processedManualTestCases elementstest22.java

---
+++ elementstest22.java
@@ -0,0 +1,26 @@
+package org.jsoup.select;
+
+import org.jsoup.Jsoup;
+import org.jsoup.TextUtil;
+import org.jsoup.nodes.Comment;
+import org.jsoup.nodes.DataNode;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.FormElement;
+import org.jsoup.nodes.Node;
+import org.jsoup.nodes.TextNode;
+import org.junit.jupiter.api.Test;
+import java.util.Iterator;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ElementsTestTest22 {
+
+    @Test
+    public void empty() {
+        Document doc = Jsoup.parse("<div><p>Hello <b>there</b></p> <p>now!</p></div>");
+        doc.outputSettings().prettyPrint(false);
+        doc.select("p").empty();
+        assertEquals("<div><p></p> <p></p></div>", doc.body().html());
+    }
+}
