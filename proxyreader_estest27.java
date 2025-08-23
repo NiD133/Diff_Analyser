bd6618e8e81aca0261b228c63067029b4ee8cbb8
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:09:39 +0800

    Original processedAutoTestCases proxyreader_estest27.java

---
+++ proxyreader_estest27.java
@@ -0,0 +1,33 @@
+package org.apache.commons.io.input;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.CharArrayWriter;
+import java.io.IOException;
+import java.io.PipedReader;
+import java.io.StringReader;
+import java.nio.CharBuffer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockIOException;
+import org.junit.runner.RunWith;
+
+public class ProxyReader_ESTestTest27 extends ProxyReader_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test26() throws Throwable {
+        PipedReader pipedReader0 = new PipedReader();
+        CloseShieldReader closeShieldReader0 = CloseShieldReader.wrap(pipedReader0);
+        char[] charArray0 = new char[0];
+        try {
+            closeShieldReader0.read(charArray0);
+            fail("Expecting exception: IOException");
+        } catch (IOException e) {
+            //
+            // Pipe not connected
+            //
+            verifyException("java.io.PipedReader", e);
+        }
+    }
+}
