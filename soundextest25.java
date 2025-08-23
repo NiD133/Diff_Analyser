e5d0369b4075585674d0e4e39c90eb65c3883fd6
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:33:55 +0800

    gemini processedManualTestCases soundextest25.java

--- soundextest25.java
+++ soundextest25.java
@@ -1,33 +1,50 @@
 package org.apache.commons.codec.language;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.apache.commons.codec.AbstractStringEncoderTest;
-import org.apache.commons.codec.EncoderException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+/**
+ * Tests the {@link Soundex} class with the default US English mapping.
+ */
 public class SoundexTestTest25 extends AbstractStringEncoderTest<Soundex> {
+
+    private Soundex soundex;
 
     @Override
     protected Soundex createStringEncoder() {
+        // Creates a Soundex instance with the default US English mapping.
         return new Soundex();
     }
 
+    @BeforeEach
+    public void setUp() {
+        this.soundex = createStringEncoder();
+    }
+
+    @Test
+    @DisplayName("Should encode a basic ASCII letter using the default mapping")
+    void testEncodeBasicAsciiLetter() {
+        assertEquals("E000", this.soundex.encode("e"));
+    }
+
     /**
-     * Fancy characters are not mapped by the default US mapping.
+     * The default US English mapping in Soundex only supports the 26 unaccented
+     * letters of the alphabet. This test verifies that providing a character
+     * outside this set, such as 'é', results in an exception.
      *
-     * https://issues.apache.org/jira/browse/CODEC-30
+     * @see <a href="https://issues.apache.org/jira/browse/CODEC-30">CODEC-30</a>
      */
     @Test
-    void testUsMappingEWithAcute() {
-        assertEquals("E000", getStringEncoder().encode("e"));
-        if (Character.isLetter('\u00e9')) {
-            // e-acute
-            //         uppercase E-acute
-            assertThrows(IllegalArgumentException.class, () -> getStringEncoder().encode("\u00e9"));
-        } else {
-            assertEquals("", getStringEncoder().encode("\u00e9"));
-        }
+    @DisplayName("Should throw IllegalArgumentException for characters not in the US English mapping")
+    void testEncodeThrowsExceptionForUnmappedCharacter() {
+        final String inputWithDiacritic = "é"; // e-acute
+        assertThrows(IllegalArgumentException.class,
+                () -> this.soundex.encode(inputWithDiacritic),
+                "Expected an exception for an unmapped character.");
     }
 }
