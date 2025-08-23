44587fef87f9eb2e81511901c01edbcd8f18c86e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:56:46 +0800

    Original processedAutoTestCases jsonincludeproperties_estest14.java

---
+++ jsonincludeproperties_estest14.java
@@ -0,0 +1,20 @@
+package com.fasterxml.jackson.annotation;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class JsonIncludeProperties_ESTestTest14 extends JsonIncludeProperties_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test13() throws Throwable {
+        JsonIncludeProperties.Value jsonIncludeProperties_Value0 = JsonIncludeProperties.Value.from((JsonIncludeProperties) null);
+        assertNotNull(jsonIncludeProperties_Value0);
+    }
+}
