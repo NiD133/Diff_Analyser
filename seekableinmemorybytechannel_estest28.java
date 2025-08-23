57b5b519a7ffc8f7a89ea15016fc2a3bff3c4600
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:13:23 +0800

    Original processedAutoTestCases seekableinmemorybytechannel_estest28.java

---
+++ seekableinmemorybytechannel_estest28.java
@@ -0,0 +1,30 @@
+package org.apache.commons.compress.utils;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.ClosedChannelException;
+import java.nio.channels.SeekableByteChannel;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class SeekableInMemoryByteChannel_ESTestTest28 extends SeekableInMemoryByteChannel_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test27() throws Throwable {
+        byte[] byteArray0 = new byte[2];
+        SeekableInMemoryByteChannel seekableInMemoryByteChannel0 = new SeekableInMemoryByteChannel(byteArray0);
+        try {
+            seekableInMemoryByteChannel0.position((-429L));
+            fail("Expecting exception: IOException");
+        } catch (IOException e) {
+            //
+            // Position must be in range [0..2147483647]
+            //
+            verifyException("org.apache.commons.compress.utils.SeekableInMemoryByteChannel", e);
+        }
+    }
+}
