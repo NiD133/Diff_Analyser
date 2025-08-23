1b7f93b40bb4599823d8da5969b6ded12aa499a7
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:18:45 +0800

    gemini processedAutoTestCases stringutil_estest2.java

--- stringutil_estest2.java
+++ stringutil_estest2.java
@@ -1,28 +1,16 @@
 package org.jsoup.internal;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Collection;
-import java.util.ConcurrentModificationException;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.ListIterator;
-import java.util.stream.Collector;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.evosuite.runtime.mock.java.net.MockURL;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertTrue;
 
-public class StringUtil_ESTestTest2 extends StringUtil_ESTest_scaffolding {
+/**
+ * Test suite for the {@link StringUtil} class.
+ */
+public class StringUtilTest {
 
-    @Test(timeout = 4000)
-    public void test01() throws Throwable {
-        boolean boolean0 = StringUtil.isHexDigit('A');
-        assertTrue(boolean0);
+    @Test
+    public void isHexDigitShouldReturnTrueForUppercaseHexCharacter() {
+        // The character 'A' is a valid uppercase hexadecimal digit.
+        assertTrue("Character 'A' should be recognized as a hex digit", StringUtil.isHexDigit('A'));
     }
 }
