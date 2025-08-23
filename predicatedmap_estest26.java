79d874ed5d8d79a6c497ce669ff6d2a0d1fbdc0f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:09:28 +0800

    gemini processedAutoTestCases predicatedmap_estest26.java

--- predicatedmap_estest26.java
+++ predicatedmap_estest26.java
@@ -1,53 +1,46 @@
 package org.apache.commons.collections4.map;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
 import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionPredicate;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.NonePredicate;
 import org.apache.commons.collections4.functors.NotNullPredicate;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.OnePredicate;
-import org.apache.commons.collections4.functors.OrPredicate;
-import org.apache.commons.collections4.functors.PredicateTransformer;
-import org.apache.commons.collections4.functors.TransformerPredicate;
 import org.apache.commons.collections4.functors.TruePredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 public class PredicatedMap_ESTestTest26 extends PredicatedMap_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test25() throws Throwable {
-        HashMap<Object, Integer> hashMap0 = new HashMap<Object, Integer>();
-        Predicate<Object> predicate0 = EqualPredicate.equalPredicate((Object) hashMap0);
-        PredicatedMap<Object, Integer> predicatedMap0 = new PredicatedMap<Object, Integer>(hashMap0, predicate0, predicate0);
-        // Undeclared exception!
-        try {
-            predicatedMap0.checkSetValue((Integer) null);
-            fail("Expecting exception: IllegalArgumentException");
-        } catch (IllegalArgumentException e) {
-            //
-            // Cannot set value - Predicate rejected it
-            //
-            verifyException("org.apache.commons.collections4.map.PredicatedMap", e);
-        }
+    /**
+     * Tests that checkSetValue() throws an IllegalArgumentException when the value
+     * is rejected by the value predicate.
+     */
+    @Test
+    public void checkSetValueShouldThrowExceptionWhenValueIsRejectedByPredicate() {
+        // Arrange
+        final Map<Object, Integer> baseMap = new HashMap<>();
+        
+        // Use a predicate that accepts any key, as key validation is not the focus of this test.
+        final Predicate<Object> keyPredicate = TruePredicate.truePredicate();
+        
+        // Use a standard NotNullPredicate, which will reject any null value.
+        // This makes the test's intent clear.
+        final Predicate<Integer> valuePredicate = NotNullPredicate.notNullPredicate();
+
+        final PredicatedMap<Object, Integer> predicatedMap =
+                new PredicatedMap<>(baseMap, keyPredicate, valuePredicate);
+
+        // Act & Assert
+        // We expect an IllegalArgumentException when trying to validate a null value,
+        // as it violates the NotNullPredicate.
+        final IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> predicatedMap.checkSetValue(null)
+        );
+
+        // Verify the exception message is as expected.
+        assertEquals("Cannot set value - Predicate rejected it", exception.getMessage());
     }
 }
