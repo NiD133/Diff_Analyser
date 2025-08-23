2cb40cfc377a30a73acc92954ea3510de280a547
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:12:59 +0800

    Original processedAutoTestCases seconds_estest33.java

---
+++ seconds_estest33.java
@@ -0,0 +1,26 @@
+package org.joda.time;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class Seconds_ESTestTest33 extends Seconds_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test32() throws Throwable {
+        Seconds seconds0 = Seconds.MIN_VALUE;
+        // Undeclared exception!
+        try {
+            seconds0.plus((-674));
+            fail("Expecting exception: ArithmeticException");
+        } catch (ArithmeticException e) {
+            //
+            // The calculation caused an overflow: -2147483648 + -674
+            //
+            verifyException("org.joda.time.field.FieldUtils", e);
+        }
+    }
+}
