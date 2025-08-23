73b9552705e1be3434a3eb3a6fac2a5f466804f7
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:24:51 +0800

    gemini processedAutoTestCases urlcodec_estest21.java

--- urlcodec_estest21.java
+++ urlcodec_estest21.java
@@ -1,9 +1,11 @@
 package org.apache.commons.codec.net;
 
 import org.junit.Test;
+import java.io.UnsupportedEncodingException;
+
+// The original imports are kept as they may be used by other tests in the full suite.
 import static org.junit.Assert.*;
 import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.UnsupportedEncodingException;
 import java.util.BitSet;
 import org.evosuite.runtime.EvoRunner;
 import org.evosuite.runtime.EvoRunnerParameters;
@@ -11,14 +13,20 @@ import org.junit.runner.RunWith;
 
 public class URLCodec_ESTestTest21 extends URLCodec_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test20() throws Throwable {
-        URLCodec uRLCodec0 = new URLCodec();
-        // Undeclared exception!
-        try {
-            uRLCodec0.encode("org.apache.commons.codec.net.URLCodec", (String) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-        }
+    /**
+     * Verifies that encoding a string with a null charset name throws a NullPointerException.
+     * The underlying implementation attempts to use the charset name to get the string's bytes,
+     * which is not possible with a null reference.
+     */
+    @Test(expected = NullPointerException.class, timeout = 4000)
+    public void encodeStringWithCharsetShouldThrowNullPointerExceptionWhenCharsetIsNull() throws UnsupportedEncodingException {
+        // Arrange
+        URLCodec urlCodec = new URLCodec();
+        String inputText = "any string";
+        String nullCharset = null;
+
+        // Act & Assert
+        // This call is expected to throw a NullPointerException because the charset is null.
+        urlCodec.encode(inputText, nullCharset);
     }
 }
