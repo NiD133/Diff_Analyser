4c49d37f0404afe88273a399c76d8d33875c47d9
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:47:02 +0800

    gemini processedAutoTestCases dayofyear_estest36.java

--- dayofyear_estest36.java
+++ dayofyear_estest36.java
@@ -1,55 +1,23 @@
 package org.threeten.extra;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.time.Clock;
-import java.time.DateTimeException;
-import java.time.Duration;
-import java.time.LocalDate;
-import java.time.Month;
-import java.time.Year;
-import java.time.YearMonth;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
-import java.time.chrono.ChronoLocalDate;
-import java.time.chrono.HijrahDate;
-import java.time.chrono.ThaiBuddhistDate;
-import java.time.temporal.ChronoField;
-import java.time.temporal.ChronoUnit;
-import java.time.temporal.Temporal;
-import java.time.temporal.TemporalAccessor;
-import java.time.temporal.TemporalField;
-import java.time.temporal.TemporalQuery;
-import java.time.temporal.UnsupportedTemporalTypeException;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.evosuite.runtime.mock.java.time.MockClock;
-import org.evosuite.runtime.mock.java.time.MockYear;
-import org.evosuite.runtime.mock.java.time.MockYearMonth;
-import org.evosuite.runtime.mock.java.time.MockZonedDateTime;
-import org.evosuite.runtime.mock.java.time.chrono.MockHijrahDate;
-import org.evosuite.runtime.mock.java.time.chrono.MockThaiBuddhistDate;
-import org.junit.runner.RunWith;
 
-public class DayOfYear_ESTestTest36 extends DayOfYear_ESTest_scaffolding {
+/**
+ * Tests for {@link DayOfYear}.
+ */
+public class DayOfYearTest {
 
-    @Test(timeout = 4000)
-    public void test35() throws Throwable {
-        ZoneOffset zoneOffset0 = ZoneOffset.MAX;
-        DayOfYear dayOfYear0 = DayOfYear.now((ZoneId) zoneOffset0);
-        // Undeclared exception!
-        try {
-            dayOfYear0.getLong((TemporalField) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.threeten.extra.DayOfYear", e);
-        }
+    /**
+     * Tests that getLong() throws a NullPointerException when the field is null.
+     */
+    @Test(expected = NullPointerException.class)
+    public void getLong_whenFieldIsNull_throwsNullPointerException() {
+        // Arrange: Create an arbitrary DayOfYear instance.
+        // The specific value does not matter for this test.
+        DayOfYear dayOfYear = DayOfYear.of(150);
+
+        // Act: Call the method under test with a null argument.
+        // The @Test(expected) annotation will assert that an exception is thrown.
+        dayOfYear.getLong(null);
     }
 }
