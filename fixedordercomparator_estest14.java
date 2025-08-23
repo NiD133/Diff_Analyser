cb233480bd2def0ac05cd785e0c5dcb671f66190
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:51:22 +0800

    gemini processedAutoTestCases fixedordercomparator_estest14.java

--- fixedordercomparator_estest14.java
+++ fixedordercomparator_estest14.java
@@ -1,28 +1,23 @@
 package org.apache.commons.collections4.comparators;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.function.Function;
-import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.PredicateTransformer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertTrue;
 
-public class FixedOrderComparator_ESTestTest14 extends FixedOrderComparator_ESTest_scaffolding {
+/**
+ * Tests for {@link FixedOrderComparator}.
+ */
+public class FixedOrderComparatorTest {
 
-    @Test(timeout = 4000)
-    public void test13() throws Throwable {
-        FixedOrderComparator<Object> fixedOrderComparator0 = new FixedOrderComparator<Object>();
-        Object object0 = new Object();
-        boolean boolean0 = fixedOrderComparator0.add(object0);
-        assertTrue(boolean0);
+    @Test
+    public void addShouldReturnTrueWhenAddingNewObject() {
+        // Arrange
+        final FixedOrderComparator<Object> comparator = new FixedOrderComparator<>();
+        final Object newItem = new Object();
+
+        // Act
+        final boolean wasAdded = comparator.add(newItem);
+
+        // Assert
+        assertTrue("The add method should return true when a new object is successfully added.", wasAdded);
     }
 }
