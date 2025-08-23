ce5675c04731c0ccde949b53d8e53d6dbce8a6be
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:53:03 +0800

    gemini processedAutoTestCases hashcodeandequalssafeset_estest23.java

--- hashcodeandequalssafeset_estest23.java
+++ hashcodeandequalssafeset_estest23.java
@@ -1,34 +1,22 @@
 package org.mockito.internal.util.collections;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.LinkedHashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Vector;
-import java.util.function.Predicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class HashCodeAndEqualsSafeSet_ESTestTest23 extends HashCodeAndEqualsSafeSet_ESTest_scaffolding {
+/**
+ * Unit tests for {@link HashCodeAndEqualsSafeSet}.
+ */
+public class HashCodeAndEqualsSafeSetTest {
 
-    @Test(timeout = 4000)
-    public void test22() throws Throwable {
-        // Undeclared exception!
-        try {
-            HashCodeAndEqualsSafeSet.of((Object[]) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("java.util.Objects", e);
-        }
+    /**
+     * This test verifies that the static factory method `of()` correctly throws a
+     * NullPointerException when a null array is passed as an argument. This is the
+     * expected behavior as the method cannot create a set from a null collection.
+     */
+    @Test(expected = NullPointerException.class)
+    public void of_shouldThrowNullPointerException_whenGivenNullArray() {
+        // Act & Assert
+        // The following call is expected to throw a NullPointerException.
+        // The assertion is handled declaratively by the @Test(expected=...) annotation.
+        HashCodeAndEqualsSafeSet.of((Object[]) null);
     }
 }
