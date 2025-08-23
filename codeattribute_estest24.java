c86b4a082adc901423b65297d304122a1f35ff1f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:43:50 +0800

    Original processedAutoTestCases codeattribute_estest24.java

---
+++ codeattribute_estest24.java
@@ -0,0 +1,23 @@
+package org.apache.commons.compress.harmony.unpack200.bytecode;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.PipedOutputStream;
+import java.util.LinkedList;
+import java.util.List;
+import org.apache.commons.compress.harmony.unpack200.Segment;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockFileOutputStream;
+import org.junit.runner.RunWith;
+
+public class CodeAttribute_ESTestTest24 extends CodeAttribute_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test23() throws Throwable {
+        CodeAttribute.setAttributeName((CPUTF8) null);
+    }
+}
