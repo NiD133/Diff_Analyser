a6d960058d9ed3ba94bbe248ef97d44c837f5797
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:18:03 +0800

    Original processedAutoTestCases standardxybarpainter_estest20.java

---
+++ standardxybarpainter_estest20.java
@@ -0,0 +1,28 @@
+package org.jfree.chart.renderer.xy;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.font.FontRenderContext;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Arc2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.RoundRectangle2D;
+import java.awt.image.BufferedImage;
+import javax.swing.text.DefaultCaret;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.jfree.chart.api.RectangleEdge;
+import org.jfree.chart.util.GradientPaintTransformer;
+import org.junit.runner.RunWith;
+
+public class StandardXYBarPainter_ESTestTest20 extends StandardXYBarPainter_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test19() throws Throwable {
+        StandardXYBarPainter standardXYBarPainter0 = new StandardXYBarPainter();
+        standardXYBarPainter0.hashCode();
+    }
+}
