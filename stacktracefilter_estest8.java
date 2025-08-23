2aeafca9c4ec42802326b924c5a7a6e83cb067d4
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:17:43 +0800

    gemini processedAutoTestCases stacktracefilter_estest8.java

--- stacktracefilter_estest8.java
+++ stacktracefilter_estest8.java
@@ -1,28 +1,36 @@
 package org.mockito.internal.exceptions.stacktrace;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.lang.MockThrowable;
-import org.junit.runner.RunWith;
 
-public class StackTraceFilter_ESTestTest8 extends StackTraceFilter_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link StackTraceFilter} class.
+ *
+ * <p>This class was refactored from an auto-generated test to improve
+ * readability and adherence to standard testing practices.
+ */
+public class StackTraceFilterTest {
 
-    @Test(timeout = 4000)
-    public void test07() throws Throwable {
-        StackTraceFilter stackTraceFilter0 = new StackTraceFilter();
-        StackTraceElement[] stackTraceElementArray0 = new StackTraceElement[1];
-        // Undeclared exception!
-        try {
-            stackTraceFilter0.findSourceFile(stackTraceElementArray0, "org.mockito.internal.configuration.plugins.DefaultMockitoPlugins");
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.mockito.internal.exceptions.stacktrace.DefaultStackTraceCleaner", e);
-        }
+    /**
+     * Verifies that {@link StackTraceFilter#findSourceFile(StackTraceElement[], String)}
+     * throws a NullPointerException if the input array contains a null element.
+     * This ensures the method is robust against malformed stack trace data.
+     */
+    @Test(expected = NullPointerException.class)
+    public void findSourceFile_shouldThrowNPE_whenStackTraceContainsNullElement() {
+        // Given
+        // Note: In the version of Mockito this test was generated for,
+        // StackTraceFilter is a concrete class with a public constructor.
+        StackTraceFilter stackTraceFilter = new StackTraceFilter();
+        StackTraceElement[] stackTraceWithNull = new StackTraceElement[] { null };
+        String defaultValue = "Default.java";
+
+        // When
+        // The method is called with an array containing a null. It is expected to throw
+        // an NPE when it attempts to access a method on the null element.
+        stackTraceFilter.findSourceFile(stackTraceWithNull, defaultValue);
+
+        // Then
+        // A NullPointerException is thrown, as specified by the @Test(expected) annotation.
+        // The test will automatically pass if this exception occurs.
     }
 }
