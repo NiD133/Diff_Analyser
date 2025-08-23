21fcc7adfbb16cc745fd20be1129d7a52d7d0272
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:27:07 +0800

    gemini processedManualTestCases convertersettest5.java

--- convertersettest5.java
+++ convertersettest5.java
@@ -1,76 +1,76 @@
 package org.joda.time.convert;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Modifier;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
-import org.joda.time.DateMidnight;
-import org.joda.time.DateTime;
-import org.joda.time.ReadWritableDateTime;
-import org.joda.time.ReadWritableInstant;
-import org.joda.time.ReadableDateTime;
-import org.joda.time.ReadableInstant;
 
-public class ConverterSetTestTest5 extends TestCase {
+import java.util.Arrays;
+import java.util.List;
 
-    private static final Converter c1 = new Converter() {
+/**
+ * Test class for ConverterSet.
+ * This test focuses on the add() method.
+ */
+public class ConverterSetTest extends TestCase {
 
-        public Class getSupportedType() {
-            return Boolean.class;
-        }
-    };
+    // Descriptive names for converters, using a helper class to reduce boilerplate.
+    private static final Converter BOOLEAN_CONVERTER = new FixedTypeConverter(Boolean.class);
+    private static final Converter CHARACTER_CONVERTER = new FixedTypeConverter(Character.class);
+    private static final Converter BYTE_CONVERTER = new FixedTypeConverter(Byte.class);
+    private static final Converter ORIGINAL_SHORT_CONVERTER = new FixedTypeConverter(Short.class);
+    private static final Converter REPLACEMENT_SHORT_CONVERTER = new FixedTypeConverter(Short.class);
 
-    private static final Converter c2 = new Converter() {
+    /**
+     * Tests that adding a converter for a type that already exists in the set
+     * replaces the original converter and returns a new set instance.
+     */
+    public void testAdd_whenConverterForTypeExists_replacesOriginalConverter() {
+        // Arrange
+        Converter[] initialConverters = new Converter[]{
+            BOOLEAN_CONVERTER,
+            CHARACTER_CONVERTER,
+            BYTE_CONVERTER,
+            ORIGINAL_SHORT_CONVERTER
+        };
+        ConverterSet initialSet = new ConverterSet(initialConverters);
+        int expectedSize = initialConverters.length;
 
-        public Class getSupportedType() {
-            return Character.class;
-        }
-    };
+        // Act: Add a new converter for Short.class, which is already present.
+        // The 'removed' parameter is null, as we are not testing that output parameter here.
+        ConverterSet newSet = initialSet.add(REPLACEMENT_SHORT_CONVERTER, null);
 
-    private static final Converter c3 = new Converter() {
+        // Assert
+        assertNotNull("A new set should be returned.", newSet);
+        assertNotSame("A new set instance should be returned on modification to preserve immutability.", initialSet, newSet);
 
-        public Class getSupportedType() {
-            return Byte.class;
-        }
-    };
+        assertEquals("The original set's size should not change.", expectedSize, initialSet.size());
+        assertEquals("The new set should have the same size, as the operation was a replacement.", expectedSize, newSet.size());
 
-    private static final Converter c4 = new Converter() {
+        // Verify the content of the new set to confirm the replacement was successful.
+        Converter[] finalConverters = new Converter[newSet.size()];
+        newSet.copyInto(finalConverters);
+        List<Converter> finalConverterList = Arrays.asList(finalConverters);
 
-        public Class getSupportedType() {
-            return Short.class;
-        }
-    };
-
-    private static final Converter c4a = new Converter() {
-
-        public Class getSupportedType() {
-            return Short.class;
-        }
-    };
-
-    private static final Converter c5 = new Converter() {
-
-        public Class getSupportedType() {
-            return Integer.class;
-        }
-    };
-
-    public static void main(String[] args) {
-        junit.textui.TestRunner.run(suite());
+        assertTrue("The new set should contain the replacement converter.", finalConverterList.contains(REPLACEMENT_SHORT_CONVERTER));
+        assertFalse("The new set should not contain the original, replaced converter.", finalConverterList.contains(ORIGINAL_SHORT_CONVERTER));
     }
 
-    public static TestSuite suite() {
-        return new TestSuite(TestConverterSet.class);
-    }
+    /**
+     * A simple, reusable Converter implementation for tests.
+     */
+    private static class FixedTypeConverter implements Converter {
+        private final Class<?> iType;
 
-    public void testAddNullRemoved3() {
-        Converter[] array = new Converter[] { c1, c2, c3, c4 };
-        ConverterSet set = new ConverterSet(array);
-        ConverterSet result = set.add(c4a, null);
-        assertTrue(set != result);
-        assertEquals(4, set.size());
-        assertEquals(4, result.size());
+        FixedTypeConverter(Class<?> type) {
+            this.iType = type;
+        }
+
+        @Override
+        public Class<?> getSupportedType() {
+            return iType;
+        }
+
+        @Override
+        public String toString() {
+            return "Converter<" + iType.getSimpleName() + ">";
+        }
     }
 }
