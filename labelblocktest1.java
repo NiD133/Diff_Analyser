53b91fac6a47921dd9c435aeab5690c5f8acb12d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:30:29 +0800

    gemini processedManualTestCases labelblocktest1.java

--- labelblocktest1.java
+++ labelblocktest1.java
@@ -1,53 +1,135 @@
 package org.jfree.chart.block;
 
+import org.jfree.chart.api.RectangleAnchor;
+import org.jfree.chart.text.TextBlockAnchor;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.GradientPaint;
-import org.jfree.chart.TestUtils;
-import org.jfree.chart.text.TextBlockAnchor;
-import org.jfree.chart.api.RectangleAnchor;
-import org.jfree.chart.internal.CloneUtils;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
-public class LabelBlockTestTest1 {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
-    /**
-     * Confirm that the equals() method can distinguish all the required fields.
-     */
+/**
+ * A collection of tests for the equals() and hashCode() methods of the {@link LabelBlock} class.
+ */
+@DisplayName("LabelBlock Equality")
+class LabelBlockTestTest1 {
+
+    // Common attributes for creating LabelBlock instances
+    private static final String DEFAULT_TEXT = "ABC";
+    private static final Font DEFAULT_FONT = new Font("Dialog", Font.PLAIN, 12);
+    private static final Color DEFAULT_PAINT = Color.RED;
+
+    private LabelBlock block1;
+    private LabelBlock block2;
+
+    @BeforeEach
+    void setUp() {
+        // Create two identical LabelBlock instances before each test
+        block1 = new LabelBlock(DEFAULT_TEXT, DEFAULT_FONT, DEFAULT_PAINT);
+        block2 = new LabelBlock(DEFAULT_TEXT, DEFAULT_FONT, DEFAULT_PAINT);
+    }
+
     @Test
-    public void testEquals() {
-        LabelBlock b1 = new LabelBlock("ABC", new Font("Dialog", Font.PLAIN, 12), Color.RED);
-        LabelBlock b2 = new LabelBlock("ABC", new Font("Dialog", Font.PLAIN, 12), Color.RED);
-        assertEquals(b1, b2);
-        assertEquals(b2, b2);
-        b1 = new LabelBlock("XYZ", new Font("Dialog", Font.PLAIN, 12), Color.RED);
-        assertNotEquals(b1, b2);
-        b2 = new LabelBlock("XYZ", new Font("Dialog", Font.PLAIN, 12), Color.RED);
-        assertEquals(b1, b2);
-        b1 = new LabelBlock("XYZ", new Font("Dialog", Font.BOLD, 12), Color.RED);
-        assertNotEquals(b1, b2);
-        b2 = new LabelBlock("XYZ", new Font("Dialog", Font.BOLD, 12), Color.RED);
-        assertEquals(b1, b2);
-        b1 = new LabelBlock("XYZ", new Font("Dialog", Font.BOLD, 12), Color.BLUE);
-        assertNotEquals(b1, b2);
-        b2 = new LabelBlock("XYZ", new Font("Dialog", Font.BOLD, 12), Color.BLUE);
-        assertEquals(b1, b2);
-        b1.setToolTipText("Tooltip");
-        assertNotEquals(b1, b2);
-        b2.setToolTipText("Tooltip");
-        assertEquals(b1, b2);
-        b1.setURLText("URL");
-        assertNotEquals(b1, b2);
-        b2.setURLText("URL");
-        assertEquals(b1, b2);
-        b1.setContentAlignmentPoint(TextBlockAnchor.CENTER_RIGHT);
-        assertNotEquals(b1, b2);
-        b2.setContentAlignmentPoint(TextBlockAnchor.CENTER_RIGHT);
-        assertEquals(b1, b2);
-        b1.setTextAnchor(RectangleAnchor.BOTTOM_RIGHT);
-        assertNotEquals(b1, b2);
-        b2.setTextAnchor(RectangleAnchor.BOTTOM_RIGHT);
-        assertEquals(b1, b2);
+    @DisplayName("should be equal to an identical instance")
+    void testEquals_withIdenticalInstance() {
+        assertEquals(block1, block2, "Blocks with identical attributes should be equal");
+    }
+
+    @Test
+    @DisplayName("should have a consistent hash code for identical instances")
+    void testHashCode_isConsistentForEqualObjects() {
+        assertEquals(block1.hashCode(), block2.hashCode(), "Hash codes should be the same for equal objects");
+    }
+
+    @Test
+    @DisplayName("should not be equal to null")
+    void testEquals_withNull() {
+        assertNotEquals(null, block1, "A block should not be equal to null");
+    }
+
+    @Test
+    @DisplayName("should not be equal to an object of a different type")
+    void testEquals_withDifferentObjectType() {
+        assertNotEquals(block1, new Object(), "A block should not be equal to an object of a different class");
+    }
+
+    @Nested
+    @DisplayName("should differentiate based on")
+    class DifferentiatesBy {
+
+        @Test
+        @DisplayName("text content")
+        void testEquals_differentiatesByText() {
+            block1 = new LabelBlock("XYZ", DEFAULT_FONT, DEFAULT_PAINT);
+            assertNotEquals(block1, block2);
+
+            block2 = new LabelBlock("XYZ", DEFAULT_FONT, DEFAULT_PAINT);
+            assertEquals(block1, block2);
+        }
+
+        @Test
+        @DisplayName("font")
+        void testEquals_differentiatesByFont() {
+            block1 = new LabelBlock(DEFAULT_TEXT, new Font("Dialog", Font.BOLD, 12), DEFAULT_PAINT);
+            assertNotEquals(block1, block2);
+
+            block2 = new LabelBlock(DEFAULT_TEXT, new Font("Dialog", Font.BOLD, 12), DEFAULT_PAINT);
+            assertEquals(block1, block2);
+        }
+
+        @Test
+        @DisplayName("paint color")
+        void testEquals_differentiatesByPaint() {
+            block1 = new LabelBlock(DEFAULT_TEXT, DEFAULT_FONT, Color.BLUE);
+            assertNotEquals(block1, block2);
+
+            block2 = new LabelBlock(DEFAULT_TEXT, DEFAULT_FONT, Color.BLUE);
+            assertEquals(block1, block2);
+        }
+
+        @Test
+        @DisplayName("tool tip text")
+        void testEquals_differentiatesByToolTipText() {
+            block1.setToolTipText("Tooltip");
+            assertNotEquals(block1, block2);
+
+            block2.setToolTipText("Tooltip");
+            assertEquals(block1, block2);
+        }
+
+        @Test
+        @DisplayName("URL text")
+        void testEquals_differentiatesByUrlText() {
+            block1.setURLText("URL");
+            assertNotEquals(block1, block2);
+
+            block2.setURLText("URL");
+            assertEquals(block1, block2);
+        }
+
+        @Test
+        @DisplayName("content alignment point")
+        void testEquals_differentiatesByContentAlignmentPoint() {
+            block1.setContentAlignmentPoint(TextBlockAnchor.CENTER_RIGHT);
+            assertNotEquals(block1, block2);
+
+            block2.setContentAlignmentPoint(TextBlockAnchor.CENTER_RIGHT);
+            assertEquals(block1, block2);
+        }
+
+        @Test
+        @DisplayName("text anchor")
+        void testEquals_differentiatesByTextAnchor() {
+            block1.setTextAnchor(RectangleAnchor.BOTTOM_RIGHT);
+            assertNotEquals(block1, block2);
+
+            block2.setTextAnchor(RectangleAnchor.BOTTOM_RIGHT);
+            assertEquals(block1, block2);
+        }
     }
 }
