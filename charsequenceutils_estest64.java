dadbe1cb046c85dcbcd5ed63750f7510b79cf683
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:42:38 +0800

    gemini processedAutoTestCases charsequenceutils_estest64.java

--- charsequenceutils_estest64.java
+++ charsequenceutils_estest64.java
@@ -1,19 +1,28 @@
 package org.apache.commons.lang3;
 
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.nio.CharBuffer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class CharSequenceUtils_ESTestTest64 extends CharSequenceUtils_ESTest_scaffolding {
+/**
+ * Unit tests for {@link CharSequenceUtils}.
+ */
+public class CharSequenceUtilsTest {
 
-    @Test(timeout = 4000)
-    public void test63() throws Throwable {
-        StringBuilder stringBuilder0 = new StringBuilder();
-        int int0 = CharSequenceUtils.indexOf(stringBuilder0, (-1790), 3507);
-        assertEquals((-1), int0);
+    /**
+     * Tests that CharSequenceUtils.indexOf returns -1 when the starting index
+     * is greater than the length of the CharSequence.
+     */
+    @Test
+    public void indexOfShouldReturnNotFoundForStartIndexGreaterThanLength() {
+        // Arrange
+        final CharSequence sequence = new StringBuilder(); // An empty sequence has length 0
+        final int searchChar = 'a';
+        final int startIndex = 1; // An index greater than the sequence's length
+
+        // Act
+        final int result = CharSequenceUtils.indexOf(sequence, searchChar, startIndex);
+
+        // Assert
+        assertEquals("indexOf should return -1 for a start index beyond the sequence length", -1, result);
     }
 }
