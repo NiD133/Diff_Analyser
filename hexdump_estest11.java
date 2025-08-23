27d0cbd4963afdf848ab2bc6df99575414433210
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:53:12 +0800

    Original processedAutoTestCases hexdump_estest11.java

---
+++ hexdump_estest11.java
@@ -0,0 +1,41 @@
+package org.apache.commons.io;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PipedOutputStream;
+import java.io.PipedWriter;
+import java.io.StringWriter;
+import java.nio.BufferOverflowException;
+import java.nio.CharBuffer;
+import java.nio.ReadOnlyBufferException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.evosuite.runtime.mock.java.io.MockFileOutputStream;
+import org.evosuite.runtime.mock.java.io.MockFileWriter;
+import org.evosuite.runtime.mock.java.io.MockPrintStream;
+import org.junit.runner.RunWith;
+
+public class HexDump_ESTestTest11 extends HexDump_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test10() throws Throwable {
+        byte[] byteArray0 = new byte[37];
+        PipedOutputStream pipedOutputStream0 = new PipedOutputStream();
+        try {
+            HexDump.dump(byteArray0, 804L, (OutputStream) pipedOutputStream0, 16);
+            fail("Expecting exception: IOException");
+        } catch (IOException e) {
+            //
+            // Pipe not connected
+            //
+            verifyException("java.io.PipedOutputStream", e);
+        }
+    }
+}
