48a4380717e03fbe365d0a7497fdd4b1e3a7a1f5
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:50:23 +0800

    Original processedAutoTestCases fieldutils_estest40.java

---
+++ fieldutils_estest40.java
@@ -0,0 +1,24 @@
+package org.joda.time.field;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.math.RoundingMode;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.joda.time.DateTimeField;
+import org.joda.time.DateTimeFieldType;
+import org.joda.time.DateTimeZone;
+import org.joda.time.chrono.IslamicChronology;
+import org.joda.time.chrono.ZonedChronology;
+import org.junit.runner.RunWith;
+
+public class FieldUtils_ESTestTest40 extends FieldUtils_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test39() throws Throwable {
+        Integer integer0 = new Integer((-1));
+        boolean boolean0 = FieldUtils.equals((Object) integer0, (Object) null);
+        assertFalse(boolean0);
+    }
+}
