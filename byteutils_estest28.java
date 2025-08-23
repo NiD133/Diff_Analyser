df7f0264a5acb6c4b395a0898bfe14d9d8638da2
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:41:03 +0800

    gemini processedAutoTestCases byteutils_estest28.java

--- byteutils_estest28.java
+++ byteutils_estest28.java
@@ -1,42 +1,28 @@
 package org.apache.commons.compress.utils;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.BufferedInputStream;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.DataInput;
-import java.io.DataInputStream;
-import java.io.DataOutput;
-import java.io.DataOutputStream;
-import java.io.EOFException;
-import java.io.IOException;
+
 import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.PipedInputStream;
-import java.io.PipedOutputStream;
-import java.io.PushbackInputStream;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.evosuite.runtime.mock.java.io.MockFileOutputStream;
-import org.junit.runner.RunWith;
 
-public class ByteUtils_ESTestTest28 extends ByteUtils_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link ByteUtils} class, focusing on exception handling.
+ */
+public class ByteUtilsTest {
 
-    @Test(timeout = 4000)
-    public void test27() throws Throwable {
-        // Undeclared exception!
-        try {
-            ByteUtils.fromLittleEndian((InputStream) null, 8);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.commons.compress.utils.ByteUtils", e);
-        }
+    /**
+     * Tests that the fromLittleEndian method throws a NullPointerException
+     * when the provided InputStream is null.
+     *
+     * The method under test, {@link ByteUtils#fromLittleEndian(InputStream, int)},
+     * is deprecated, but its contract regarding null inputs should still be upheld.
+     */
+    @Test(expected = NullPointerException.class)
+    public void fromLittleEndianWithNullInputStreamShouldThrowNullPointerException() {
+        // Define a valid length for the second argument to isolate the test's focus
+        // on the null InputStream.
+        final int validLength = 8;
+
+        // This call is expected to throw a NullPointerException because the input stream is null.
+        ByteUtils.fromLittleEndian((InputStream) null, validLength);
     }
 }
