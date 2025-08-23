5f4f097212c5526c50fbe3a41b7fb29038a1804b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:49:50 +0800

    Original processedAutoTestCases extensionfunction_estest3.java

---
+++ extensionfunction_estest3.java
@@ -0,0 +1,46 @@
+package org.apache.commons.jxpath.ri.compiler;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.Locale;
+import org.apache.commons.jxpath.BasicNodeSet;
+import org.apache.commons.jxpath.Function;
+import org.apache.commons.jxpath.JXPathContext;
+import org.apache.commons.jxpath.ri.EvalContext;
+import org.apache.commons.jxpath.ri.JXPathContextReferenceImpl;
+import org.apache.commons.jxpath.ri.NamespaceResolver;
+import org.apache.commons.jxpath.ri.QName;
+import org.apache.commons.jxpath.ri.axes.InitialContext;
+import org.apache.commons.jxpath.ri.axes.NodeSetContext;
+import org.apache.commons.jxpath.ri.axes.RootContext;
+import org.apache.commons.jxpath.ri.model.NodePointer;
+import org.apache.commons.jxpath.ri.model.VariablePointer;
+import org.apache.commons.jxpath.ri.model.beans.BeanPointer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class ExtensionFunction_ESTestTest3 extends ExtensionFunction_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test02() throws Throwable {
+        Expression[] expressionArray0 = new Expression[1];
+        Expression[] expressionArray1 = new Expression[1];
+        CoreOperationOr coreOperationOr0 = new CoreOperationOr(expressionArray0);
+        expressionArray1[0] = (Expression) coreOperationOr0;
+        ExtensionFunction extensionFunction0 = new ExtensionFunction((QName) null, expressionArray1);
+        // Undeclared exception!
+        try {
+            extensionFunction0.toString();
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.evosuite.runtime.System", e);
+        }
+    }
+}
