56943825276ce3e43ab4abfecafce8b5055c6c78
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:52:12 +0800

    gemini processedAutoTestCases gregorianchronology_estest9.java

--- gregorianchronology_estest9.java
+++ gregorianchronology_estest9.java
@@ -1,31 +1,28 @@
 package org.joda.time.chrono;
 
+import org.joda.time.chrono.AssembledChronology.Fields;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.TimeZone;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.joda.time.Chronology;
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
-import org.joda.time.LocalDateTime;
-import org.junit.runner.RunWith;
 
-public class GregorianChronology_ESTestTest9 extends GregorianChronology_ESTest_scaffolding {
+/**
+ * Tests the internal behavior of the GregorianChronology class.
+ */
+public class GregorianChronology_ESTestTest9 {
 
-    @Test(timeout = 4000)
-    public void test08() throws Throwable {
-        GregorianChronology gregorianChronology0 = GregorianChronology.getInstance();
-        // Undeclared exception!
-        try {
-            gregorianChronology0.assemble((AssembledChronology.Fields) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.joda.time.chrono.BasicChronology", e);
-        }
+    /**
+     * Verifies that the internal assemble() method throws a NullPointerException
+     * when its 'fields' argument is null. This ensures the method is robust
+     * against invalid internal state during chronology construction.
+     */
+    @Test(expected = NullPointerException.class)
+    public void assemble_shouldThrowNullPointerException_whenFieldsAreNull() {
+        // Arrange
+        GregorianChronology chronology = GregorianChronology.getInstance();
+        Fields nullFields = null;
+
+        // Act & Assert
+        // The assemble() method is protected. We call it with a null argument
+        // to confirm it fails fast, as expected. The @Test annotation
+        // asserts that a NullPointerException is thrown.
+        chronology.assemble(nullFields);
     }
 }
