b2c2b6f6e5f6d52758be805ed62fa786fcab62c4
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:07:12 +0800

    Original processedAutoTestCases parallelscatterzipcreator_estest2.java

---
+++ parallelscatterzipcreator_estest2.java
@@ -0,0 +1,51 @@
+package org.apache.commons.compress.archivers.zip;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.File;
+import java.net.URI;
+import java.nio.file.Path;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import org.apache.commons.compress.archivers.jar.JarArchiveEntry;
+import org.apache.commons.compress.parallel.InputStreamSupplier;
+import org.apache.commons.compress.parallel.ScatterGatherBackingStoreSupplier;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.evosuite.runtime.mock.java.net.MockURI;
+import org.junit.runner.RunWith;
+
+public class ParallelScatterZipCreator_ESTestTest2 extends ParallelScatterZipCreator_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test01() throws Throwable {
+        ThreadPoolExecutor.DiscardOldestPolicy threadPoolExecutor_DiscardOldestPolicy0 = new ThreadPoolExecutor.DiscardOldestPolicy();
+        ScheduledThreadPoolExecutor scheduledThreadPoolExecutor0 = new ScheduledThreadPoolExecutor(0, threadPoolExecutor_DiscardOldestPolicy0);
+        scheduledThreadPoolExecutor0.prestartAllCoreThreads();
+        File file0 = MockFile.createTempFile("D;U(uA", ";,hSX[abfs+0dlNvz");
+        MockFile mockFile0 = new MockFile(file0, "");
+        Path path0 = mockFile0.toPath();
+        DefaultBackingStoreSupplier defaultBackingStoreSupplier0 = new DefaultBackingStoreSupplier(path0);
+        ParallelScatterZipCreator parallelScatterZipCreator0 = null;
+        try {
+            parallelScatterZipCreator0 = new ParallelScatterZipCreator(scheduledThreadPoolExecutor0, defaultBackingStoreSupplier0, (-1088));
+            fail("Expecting exception: IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //
+            // Compression level is expected between -1~9
+            //
+            verifyException("org.apache.commons.compress.archivers.zip.ParallelScatterZipCreator", e);
+        }
+    }
+}
