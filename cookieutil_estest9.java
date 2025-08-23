3f8707bfc65aba5e6f4aa2b36d44762c71aa92aa
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:45:54 +0800

    Original processedAutoTestCases cookieutil_estest9.java

---
+++ cookieutil_estest9.java
@@ -0,0 +1,32 @@
+package org.jsoup.helper;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiConsumer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.evosuite.runtime.mock.java.net.MockURL;
+import org.jsoup.nodes.Attributes;
+import org.jsoup.nodes.TextNode;
+import org.junit.runner.RunWith;
+
+public class CookieUtil_ESTestTest9 extends CookieUtil_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test08() throws Throwable {
+        HttpConnection.Response httpConnection_Response0 = new HttpConnection.Response();
+        CookieUtil.parseCookie("", httpConnection_Response0);
+        assertEquals(400, httpConnection_Response0.statusCode());
+    }
+}
