9761541bdf871d0515f3aaf4bccbf21b5197960b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:02:37 +0800

    gemini processedAutoTestCases metaphone_estest48.java

--- metaphone_estest48.java
+++ metaphone_estest48.java
@@ -1,19 +1,43 @@
 package org.apache.commons.codec.language;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertEquals;
 
-public class Metaphone_ESTestTest48 extends Metaphone_ESTest_scaffolding {
+/**
+ * Tests for the {@link Metaphone} class.
+ */
+public class MetaphoneTest {
 
-    @Test(timeout = 4000)
-    public void test47() throws Throwable {
-        Metaphone metaphone0 = new Metaphone();
-        boolean boolean0 = metaphone0.isMetaphoneEqual("I", "X");
-        assertEquals(4, metaphone0.getMaxCodeLen());
-        assertFalse(boolean0);
+    /**
+     * Tests that isMetaphoneEqual() returns false for two strings
+     * that have different Metaphone encodings.
+     */
+    @Test
+    public void testIsMetaphoneEqualReturnsFalseForDissimilarStrings() {
+        // Arrange
+        Metaphone metaphone = new Metaphone();
+
+        // Act & Assert
+        // The Metaphone code for "I" is "I", while for "X" it is "S".
+        // Therefore, the two strings are not considered equal.
+        assertFalse("Strings with different Metaphone codes should not be equal",
+                    metaphone.isMetaphoneEqual("I", "X"));
+    }
+
+    /**
+     * Tests that a new Metaphone instance has the default max code length of 4.
+     */
+    @Test
+    public void testDefaultMaxCodeLength() {
+        // Arrange
+        Metaphone metaphone = new Metaphone();
+        int expectedMaxCodeLen = 4;
+
+        // Act
+        int actualMaxCodeLen = metaphone.getMaxCodeLen();
+
+        // Assert
+        assertEquals(expectedMaxCodeLen, actualMaxCodeLen);
     }
 }
