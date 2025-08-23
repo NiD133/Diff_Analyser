efd5070fbd22dd01b4e380ed601a8de5e42e92ce
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:52:51 +0800

    gemini processedAutoTestCases gzipcompressoroutputstream_estest12.java

--- gzipcompressoroutputstream_estest12.java
+++ gzipcompressoroutputstream_estest12.java
@@ -1,38 +1,22 @@
 package org.apache.commons.compress.compressors.gzip;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
+
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FilterOutputStream;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.io.OutputStream;
-import java.io.PipedOutputStream;
-import java.io.PrintStream;
-import java.util.Locale;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.io.MockFile;
-import org.evosuite.runtime.mock.java.io.MockFileOutputStream;
-import org.evosuite.runtime.mock.java.io.MockPrintStream;
-import org.junit.runner.RunWith;
 
-public class GzipCompressorOutputStream_ESTestTest12 extends GzipCompressorOutputStream_ESTest_scaffolding {
+/**
+ * Unit tests for the GzipCompressorOutputStream class.
+ */
+public class GzipCompressorOutputStreamTest {
 
-    @Test(timeout = 4000)
-    public void test11() throws Throwable {
-        ByteArrayOutputStream byteArrayOutputStream0 = new ByteArrayOutputStream();
-        GzipCompressorOutputStream gzipCompressorOutputStream0 = null;
-        try {
-            gzipCompressorOutputStream0 = new GzipCompressorOutputStream(byteArrayOutputStream0, (GzipParameters) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.commons.compress.compressors.gzip.GzipCompressorOutputStream", e);
-        }
+    /**
+     * Verifies that the constructor throws a NullPointerException when the
+     * GzipParameters argument is null, as this is a required parameter.
+     */
+    @Test(expected = NullPointerException.class)
+    public void constructorShouldThrowNullPointerExceptionWhenParametersAreNull() throws IOException {
+        // Attempt to create an instance with null parameters, which should fail.
+        new GzipCompressorOutputStream(new ByteArrayOutputStream(), null);
     }
 }
