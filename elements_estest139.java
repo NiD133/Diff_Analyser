7947cff4604e2cc9d83cdaeed20ca4ebdc48d577
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:48:27 +0800

    Original processedAutoTestCases elements_estest139.java

---
+++ elements_estest139.java
@@ -0,0 +1,44 @@
+package org.jsoup.select;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.ConcurrentModificationException;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.jsoup.nodes.Comment;
+import org.jsoup.nodes.DataNode;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.FormElement;
+import org.jsoup.nodes.TextNode;
+import org.jsoup.parser.Parser;
+import org.junit.runner.RunWith;
+
+public class Elements_ESTestTest139 extends Elements_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test138() throws Throwable {
+        Document document0 = new Document("I");
+        Elements elements0 = document0.getAllElements();
+        // Undeclared exception!
+        try {
+            elements0.addClass((String) null);
+            fail("Expecting exception: IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //
+            // Object must not be null
+            //
+            verifyException("org.jsoup.helper.Validate", e);
+        }
+    }
+}
