4c26ce2a3a168933670d1c4db314f25373509b71
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:36:09 +0800

    Original processedManualTestCases xmltreebuildertest16.java

---
+++ xmltreebuildertest16.java
@@ -0,0 +1,32 @@
+package org.jsoup.parser;
+
+import org.jsoup.Jsoup;
+import org.jsoup.TextUtil;
+import org.jsoup.nodes.*;
+import org.jsoup.select.Elements;
+import org.junit.jupiter.api.Test;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import static org.jsoup.nodes.Document.OutputSettings.Syntax;
+import static org.jsoup.parser.Parser.NamespaceHtml;
+import static org.jsoup.parser.Parser.NamespaceXml;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class XmlTreeBuilderTestTest16 {
+
+    private static void assertXmlNamespace(Element el) {
+        assertEquals(NamespaceXml, el.tag().namespace(), String.format("Element %s not in XML namespace", el.tagName()));
+    }
+
+    @Test
+    public void preservesCaseByDefault() {
+        String xml = "<CHECK>One</CHECK><TEST ID=1>Check</TEST>";
+        Document doc = Jsoup.parse(xml, "", Parser.xmlParser());
+        assertEquals("<CHECK>One</CHECK><TEST ID=\"1\">Check</TEST>", TextUtil.stripNewlines(doc.html()));
+    }
+}
