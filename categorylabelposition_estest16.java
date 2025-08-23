20d4ae1a046b4aa1b8fc9d7c0a39e0e641abc2bf
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:41:51 +0800

    Original processedAutoTestCases categorylabelposition_estest16.java

---
+++ categorylabelposition_estest16.java
@@ -0,0 +1,24 @@
+package org.jfree.chart.axis;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.jfree.chart.api.RectangleAnchor;
+import org.jfree.chart.text.TextAnchor;
+import org.jfree.chart.text.TextBlockAnchor;
+import org.junit.runner.RunWith;
+
+public class CategoryLabelPosition_ESTestTest16 extends CategoryLabelPosition_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test15() throws Throwable {
+        CategoryLabelPosition categoryLabelPosition0 = new CategoryLabelPosition();
+        CategoryLabelPosition categoryLabelPosition1 = new CategoryLabelPosition();
+        boolean boolean0 = categoryLabelPosition0.equals(categoryLabelPosition1);
+        assertEquals(0.95F, categoryLabelPosition1.getWidthRatio(), 0.01F);
+        assertTrue(boolean0);
+        assertEquals(0.0, categoryLabelPosition1.getAngle(), 0.01);
+    }
+}
