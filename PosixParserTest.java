4e294baa1250c6c3addd617d261db8199758a3c1
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 05:34:42 +0800

    Original manualTestSuite PosixParserTest.java

--- PosixParserTest.java
+++ PosixParserTest.java
@@ -22,19 +22,11 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 /**
- * Test suite for the PosixParser class.
+ * Test case for the PosixParser.
  *
- * Note: These tests are currently disabled as the PosixParser does not support these features.
- *       Consider using DefaultParser for full support.
- *
- * TODO: Refactor to use JUnit parameterized tests for better coverage and maintainability.
+ * TODO Needs a rework using JUnit parameterized tests.
  */
 class PosixParserTest extends AbstractParserTestCase {
-
-    /**
-     * Sets up the test environment before each test.
-     * Initializes the parser to be an instance of PosixParser.
-     */
     @Override
     @SuppressWarnings("deprecation")
     @BeforeEach
@@ -43,113 +35,63 @@ class PosixParserTest extends AbstractParserTestCase {
         parser = new PosixParser();
     }
 
-    /**
-     * Test case for ambiguous long options without an equal sign using a single dash.
-     * Currently disabled as this feature is not supported by PosixParser.
-     */
     @Override
     @Test
-    @Disabled("Not supported by the PosixParser")
+    @Disabled("not supported by the PosixParser")
     void testAmbiguousLongWithoutEqualSingleDash() throws Exception {
-        // Test logic here (if supported)
     }
 
-    /**
-     * Test case for another scenario of ambiguous long options without an equal sign using a single dash.
-     * Currently disabled as this feature is not supported by PosixParser.
-     */
     @Override
     @Test
-    @Disabled("Not supported by the PosixParser")
+    @Disabled("not supported by the PosixParser")
     void testAmbiguousLongWithoutEqualSingleDash2() throws Exception {
-        // Test logic here (if supported)
     }
 
-    /**
-     * Test case for ambiguous partial long options.
-     * Currently disabled as this feature is not supported by PosixParser.
-     */
     @Override
     @Test
-    @Disabled("Not supported by the PosixParser")
+    @Disabled("not supported by the PosixParser")
     void testAmbiguousPartialLongOption4() throws Exception {
-        // Test logic here (if supported)
     }
 
-    /**
-     * Test case for double dash options.
-     * Currently disabled as this feature is not supported by PosixParser.
-     */
     @Override
     @Test
-    @Disabled("Not supported by the PosixParser")
+    @Disabled("not supported by the PosixParser")
     void testDoubleDash2() throws Exception {
-        // Test logic here (if supported)
     }
 
-    /**
-     * Test case for long options with an equal sign using a single dash.
-     * Currently disabled as this feature is not supported by PosixParser.
-     */
     @Override
     @Test
-    @Disabled("Not supported by the PosixParser")
+    @Disabled("not supported by the PosixParser")
     void testLongWithEqualSingleDash() throws Exception {
-        // Test logic here (if supported)
     }
 
-    /**
-     * Test case for long options without an equal sign using a single dash.
-     * Currently disabled as this feature is not supported by PosixParser.
-     */
     @Override
     @Test
-    @Disabled("Not supported by the PosixParser")
+    @Disabled("not supported by the PosixParser")
     void testLongWithoutEqualSingleDash() throws Exception {
-        // Test logic here (if supported)
     }
 
-    /**
-     * Test case for long options with unexpected arguments.
-     * Currently disabled as this feature is not supported by PosixParser.
-     */
     @Override
     @Test
-    @Disabled("Not supported by the PosixParser")
+    @Disabled("not supported by the PosixParser")
     void testLongWithUnexpectedArgument1() throws Exception {
-        // Test logic here (if supported)
     }
 
-    /**
-     * Test case for negative options.
-     * Currently disabled as this feature is not supported by PosixParser.
-     */
     @Override
     @Test
-    @Disabled("Not supported by the PosixParser (CLI-184)")
+    @Disabled("not supported by the PosixParser (CLI-184)")
     void testNegativeOption() throws Exception {
-        // Test logic here (if supported)
     }
 
-    /**
-     * Test case for short options with an equal sign.
-     * Currently disabled as this feature is not supported by PosixParser.
-     */
     @Override
     @Test
-    @Disabled("Not supported by the PosixParser")
+    @Disabled("not supported by the PosixParser")
     void testShortWithEqual() throws Exception {
-        // Test logic here (if supported)
     }
 
-    /**
-     * Test case for unambiguous partial long options.
-     * Currently disabled as this feature is not supported by PosixParser.
-     */
     @Override
     @Test
-    @Disabled("Not supported by the PosixParser")
+    @Disabled("not supported by the PosixParser")
     void testUnambiguousPartialLongOption4() throws Exception {
-        // Test logic here (if supported)
     }
 }
