a6f7e9b2ab7204e5984a26d14aec33ef0933e58a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:30:14 +0800

    Original processedManualTestCases jsontreereadertest6.java

---
+++ jsontreereadertest6.java
@@ -0,0 +1,29 @@
+package com.google.gson.internal.bind;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertThrows;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonNull;
+import com.google.gson.JsonObject;
+import com.google.gson.common.MoreAsserts;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import com.google.gson.stream.MalformedJsonException;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Test;
+
+public class JsonTreeReaderTestTest6 {
+
+    @Test
+    public void testSkipValue_atObjectEnd() throws IOException {
+        JsonTreeReader reader = new JsonTreeReader(new JsonObject());
+        reader.beginObject();
+        reader.skipValue();
+        assertThat(reader.peek()).isEqualTo(JsonToken.END_DOCUMENT);
+        assertThat(reader.getPath()).isEqualTo("$");
+    }
+}
