1c69377b0e6f565753f8a841c9aa045af2f93b31
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:39:39 +0800

    gemini processedAutoTestCases boundedreader_estest14.java

--- boundedreader_estest14.java
+++ boundedreader_estest14.java
@@ -1,30 +1,30 @@
 package org.apache.commons.io.input;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
 import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class BoundedReader_ESTestTest14 extends BoundedReader_ESTest_scaffolding {
+/**
+ * Tests for {@link BoundedReader}.
+ */
+public class BoundedReaderTest {
 
-    @Test(timeout = 4000)
-    public void test13() throws Throwable {
-        StringReader stringReader0 = new StringReader("");
-        stringReader0.close();
-        BoundedReader boundedReader0 = new BoundedReader(stringReader0, 202);
-        try {
-            boundedReader0.read();
-            fail("Expecting exception: IOException");
-        } catch (IOException e) {
-            //
-            // Stream closed
-            //
-            verifyException("java.io.StringReader", e);
-        }
+    /**
+     * Tests that attempting to read from a BoundedReader that wraps an already
+     * closed reader will propagate the IOException from the underlying reader.
+     */
+    @Test(expected = IOException.class)
+    public void testReadOnClosedReaderThrowsIOException() throws IOException {
+        // Arrange: Create a reader and close it.
+        Reader underlyingReader = new StringReader("test data");
+        underlyingReader.close();
+
+        // The bound value is not relevant for this test case.
+        BoundedReader boundedReader = new BoundedReader(underlyingReader, 100);
+
+        // Act: Attempt to read from the BoundedReader.
+        // This is expected to throw an IOException because the underlying reader is closed.
+        boundedReader.read();
     }
 }
