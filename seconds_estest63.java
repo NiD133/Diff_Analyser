5b26f7dddc122013ce14b3cc3a1f3bf0db3872bc
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:13:11 +0800

    gemini processedAutoTestCases seconds_estest63.java

--- seconds_estest63.java
+++ seconds_estest63.java
@@ -1,18 +1,26 @@
 package org.joda.time;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class Seconds_ESTestTest63 extends Seconds_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link Seconds} class.
+ */
+public class SecondsTest {
 
-    @Test(timeout = 4000)
-    public void test62() throws Throwable {
-        Seconds seconds0 = Seconds.TWO;
-        Minutes minutes0 = seconds0.toStandardMinutes();
-        assertEquals(0, minutes0.getMinutes());
+    /**
+     * Tests that converting a Seconds value that is less than a full minute (60 seconds)
+     * to standard minutes results in zero minutes, as the conversion truncates fractional parts.
+     */
+    @Test
+    public void toStandardMinutes_shouldReturnZero_whenConvertingLessThanSixtySeconds() {
+        // Arrange: A Seconds instance representing 2 seconds, which is less than a minute.
+        Seconds twoSeconds = Seconds.TWO;
+
+        // Act: Convert the Seconds object to standard minutes.
+        Minutes resultingMinutes = twoSeconds.toStandardMinutes();
+
+        // Assert: The result should be 0 minutes, as 2 seconds does not constitute a full minute.
+        assertEquals(0, resultingMinutes.getMinutes());
     }
 }
