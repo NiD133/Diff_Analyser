bc4863b21ab44a18624df22f4debdce3df77cd17
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:06:25 +0800

    gemini processedAutoTestCases optionformatter_estest24.java

--- optionformatter_estest24.java
+++ optionformatter_estest24.java
@@ -1,23 +1,29 @@
 package org.apache.commons.cli.help;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.function.BiFunction;
 import org.apache.commons.cli.Option;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class OptionFormatter_ESTestTest24 extends OptionFormatter_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
 
-    @Test(timeout = 4000)
-    public void test23() throws Throwable {
-        Option option0 = new Option((String) null, (String) null);
-        OptionFormatter optionFormatter0 = OptionFormatter.from(option0);
-        String string0 = optionFormatter0.getArgName();
-        assertEquals("", string0);
+/**
+ * Tests for {@link OptionFormatter}.
+ */
+public class OptionFormatterTest {
+
+    /**
+     * Verifies that getArgName() returns an empty string when the associated Option
+     * is configured to not have an argument.
+     */
+    @Test
+    public void getArgNameShouldReturnEmptyStringForOptionWithoutArgument() {
+        // Arrange: Create an option that is explicitly configured to not have an argument.
+        final Option optionWithoutArgument = new Option("o", "option", false, "An option without an argument");
+        final OptionFormatter formatter = OptionFormatter.from(optionWithoutArgument);
+
+        // Act: Get the formatted argument name.
+        final String actualArgName = formatter.getArgName();
+
+        // Assert: The result should be an empty string, as the option does not take an argument.
+        assertEquals("", actualArgName);
     }
 }
