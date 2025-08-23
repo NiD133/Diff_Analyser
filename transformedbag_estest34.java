69b9288e0d7e4ad40dc9d44a138198bbd5e40b2b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:23:04 +0800

    gemini processedAutoTestCases transformedbag_estest34.java

--- transformedbag_estest34.java
+++ transformedbag_estest34.java
@@ -1,57 +1,34 @@
 package org.apache.commons.collections4.bag;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Set;
-import java.util.Vector;
+import static org.junit.Assert.assertFalse;
+
 import org.apache.commons.collections4.Bag;
-import org.apache.commons.collections4.Factory;
-import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.SortedBag;
 import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AndPredicate;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ChainedTransformer;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantFactory;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.FactoryTransformer;
-import org.apache.commons.collections4.functors.FalsePredicate;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.InstantiateFactory;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.MapTransformer;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NotPredicate;
-import org.apache.commons.collections4.functors.OnePredicate;
-import org.apache.commons.collections4.functors.SwitchTransformer;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.apache.commons.collections4.functors.NOPTransformer;
+import org.junit.Test;
 
-public class TransformedBag_ESTestTest34 extends TransformedBag_ESTest_scaffolding {
+/**
+ * Contains tests for the {@link TransformedBag#equals(Object)} method.
+ */
+public class TransformedBag_ESTestTest34 { // The original class name is kept for context.
 
-    @Test(timeout = 4000)
-    public void test33() throws Throwable {
-        Integer integer0 = new Integer(2);
-        Comparator<Object> comparator0 = (Comparator<Object>) mock(Comparator.class, new ViolatedAssumptionAnswer());
-        TreeBag<Predicate<Object>> treeBag0 = new TreeBag<Predicate<Object>>(comparator0);
-        Transformer<Object, Predicate<Object>> transformer0 = InvokerTransformer.invokerTransformer("jA]A&z5z!a+Jbr(nH");
-        TransformedSortedBag<Predicate<Object>> transformedSortedBag0 = new TransformedSortedBag<Predicate<Object>>(treeBag0, transformer0);
-        boolean boolean0 = transformedSortedBag0.equals(integer0);
-        assertFalse(boolean0);
+    /**
+     * Tests that a TransformedBag is not equal to an object of a completely different type.
+     * The equals method should return false without throwing an exception.
+     */
+    @Test
+    public void equals_shouldReturnFalse_whenComparedWithObjectOfDifferentType() {
+        // Arrange
+        final Bag<Object> decoratedBag = new HashBag<>();
+        final Transformer<Object, Object> transformer = NOPTransformer.nopTransformer();
+        final Bag<Object> transformedBag = TransformedBag.transformingBag(decoratedBag, transformer);
+
+        final Object nonBagObject = Integer.valueOf(2);
+
+        // Act
+        final boolean isEqual = transformedBag.equals(nonBagObject);
+
+        // Assert
+        assertFalse("A TransformedBag should not be equal to an object that is not a Collection.", isEqual);
     }
 }
