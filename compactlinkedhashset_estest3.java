959110a668ed7905e2fcc86138f10b95d1168dc3
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:44:45 +0800

    Original processedAutoTestCases compactlinkedhashset_estest3.java

---
+++ compactlinkedhashset_estest3.java
@@ -0,0 +1,23 @@
+package com.google.common.collect;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.Collection;
+import java.util.Locale;
+import java.util.Set;
+import java.util.Spliterator;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class CompactLinkedHashSet_ESTestTest3 extends CompactLinkedHashSet_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test02() throws Throwable {
+        Object[] objectArray0 = new Object[2];
+        CompactLinkedHashSet<Object> compactLinkedHashSet0 = CompactLinkedHashSet.create(objectArray0);
+        compactLinkedHashSet0.init(0);
+        assertFalse(compactLinkedHashSet0.contains(0));
+    }
+}
