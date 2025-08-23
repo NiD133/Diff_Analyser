ec20b98554429d75a0614d2aeff0fbff48095d81
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:57:19 +0800

    Original processedAutoTestCases jsonreadcontext_estest10.java

---
+++ jsonreadcontext_estest10.java
@@ -0,0 +1,32 @@
+package com.fasterxml.jackson.core.json;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.fasterxml.jackson.core.ErrorReportConfiguration;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonFactoryBuilder;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonLocation;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.io.ContentReference;
+import java.io.IOException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class JsonReadContext_ESTestTest10 extends JsonReadContext_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test09() throws Throwable {
+        JsonReadContext jsonReadContext0 = JsonReadContext.createRootContext((DupDetector) null);
+        ContentReference contentReference0 = ContentReference.redacted();
+        JsonLocation jsonLocation0 = jsonReadContext0.startLocation(contentReference0);
+        assertEquals(1, jsonLocation0.getLineNr());
+        assertEquals(0, jsonReadContext0.getNestingDepth());
+        assertEquals(0, jsonLocation0.getColumnNr());
+        assertEquals((-1L), jsonLocation0.getCharOffset());
+        assertEquals(0, jsonReadContext0.getEntryCount());
+        assertEquals("ROOT", jsonReadContext0.getTypeDesc());
+    }
+}
