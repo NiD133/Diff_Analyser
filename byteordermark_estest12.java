19838d6daa75b56ea8c7b76a62138f5842fff5de
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:40:51 +0800

    gemini processedAutoTestCases byteordermark_estest12.java

--- byteordermark_estest12.java
+++ byteordermark_estest12.java
@@ -1,19 +1,29 @@
 package org.apache.commons.io;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertFalse;
 
-public class ByteOrderMark_ESTestTest12 extends ByteOrderMark_ESTest_scaffolding {
+/**
+ * Contains tests for the {@link ByteOrderMark} class.
+ */
+public class ByteOrderMarkTest {
 
-    @Test(timeout = 4000)
-    public void test11() throws Throwable {
-        ByteOrderMark byteOrderMark0 = ByteOrderMark.UTF_8;
-        int[] intArray0 = new int[1];
-        boolean boolean0 = byteOrderMark0.matches(intArray0);
-        assertFalse(boolean0);
+    /**
+     * Tests that the matches() method correctly returns false when the input array
+     * is shorter than the Byte Order Mark's byte sequence.
+     */
+    @Test
+    public void matches_shouldReturnFalse_whenInputIsShorterThanBom() {
+        // Arrange
+        // The UTF-8 BOM is 3 bytes long (0xEF, 0xBB, 0xBF).
+        final ByteOrderMark utf8Bom = ByteOrderMark.UTF_8;
+        // Create an input array that is shorter than the 3-byte BOM.
+        final int[] inputBytesShorterThanBom = { 0xEF, 0xBB };
+
+        // Act
+        final boolean isMatch = utf8Bom.matches(inputBytesShorterThanBom);
+
+        // Assert
+        assertFalse("A BOM should not match an array that is shorter than the BOM itself.", isMatch);
     }
 }
