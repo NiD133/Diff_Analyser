ba40a7f1fae896084f1c983faf978e0badd3c0d9
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:09:22 +0800

    gemini processedAutoTestCases predicatedmap_estest12.java

--- predicatedmap_estest12.java
+++ predicatedmap_estest12.java
@@ -1,50 +1,53 @@
 package org.apache.commons.collections4.map;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
 import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionPredicate;
-import org.apache.commons.collections4.functors.IdentityPredicate;
 import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NotNullPredicate;
-import org.apache.commons.collections4.functors.NullPredicate;
 import org.apache.commons.collections4.functors.OnePredicate;
-import org.apache.commons.collections4.functors.OrPredicate;
-import org.apache.commons.collections4.functors.PredicateTransformer;
-import org.apache.commons.collections4.functors.TransformerPredicate;
-import org.apache.commons.collections4.functors.TruePredicate;
 import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class PredicatedMap_ESTestTest12 extends PredicatedMap_ESTest_scaffolding {
+import java.lang.reflect.Array;
+import java.util.HashMap;
+import java.util.Map;
 
-    @Test(timeout = 4000)
-    public void test11() throws Throwable {
-        UniquePredicate<Object> uniquePredicate0 = new UniquePredicate<Object>();
-        HashMap<Object, Integer> hashMap0 = new HashMap<Object, Integer>();
-        Predicate<Object>[] predicateArray0 = (Predicate<Object>[]) Array.newInstance(Predicate.class, 2);
-        OnePredicate<Object> onePredicate0 = new OnePredicate<Object>(predicateArray0);
-        predicateArray0[0] = (Predicate<Object>) onePredicate0;
-        NonePredicate<Object> nonePredicate0 = new NonePredicate<Object>(predicateArray0);
-        PredicatedMap<Object, Integer> predicatedMap0 = PredicatedMap.predicatedMap((Map<Object, Integer>) hashMap0, (Predicate<? super Object>) uniquePredicate0, (Predicate<? super Integer>) nonePredicate0);
-        Integer integer0 = new Integer(4336);
-        // Undeclared exception!
-        predicatedMap0.put(integer0, integer0);
+/**
+ * Contains tests for edge cases in {@link PredicatedMap}.
+ */
+public class PredicatedMap_ESTestTest12 {
+
+    /**
+     * Tests that a {@code put} operation throws a {@link StackOverflowError}
+     * when the value predicate is defined recursively.
+     *
+     * <p>This scenario is engineered by creating a predicate that is a member of the
+     * collection of predicates it is supposed to evaluate, leading to infinite recursion.
+     */
+    @Test(expected = StackOverflowError.class)
+    public void putWithRecursiveValuePredicateShouldThrowStackOverflowError() {
+        // Arrange: Set up a map with a value predicate designed to cause infinite recursion.
+
+        // 1. Create an array to hold the predicates.
+        @SuppressWarnings("unchecked")
+        Predicate<Object>[] predicates = (Predicate<Object>[]) Array.newInstance(Predicate.class, 2);
+
+        // 2. Create a predicate that evaluates the predicates in the array.
+        OnePredicate<Object> recursivePredicate = new OnePredicate<>(predicates);
+
+        // 3. Create the recursion: add the predicate to the very array it evaluates.
+        // When recursivePredicate.evaluate() is called, it will eventually call itself.
+        predicates[0] = recursivePredicate;
+
+        // 4. The final value predicate will trigger the evaluation of the recursive predicate.
+        Predicate<Object> valuePredicate = new NonePredicate<>(predicates);
+        Predicate<Object> keyPredicate = new UniquePredicate<>();
+        Map<Object, Integer> baseMap = new HashMap<>();
+        Map<Object, Integer> predicatedMap = PredicatedMap.predicatedMap(baseMap, keyPredicate, valuePredicate);
+
+        Integer testKeyAndValue = 4336;
+
+        // Act & Assert: Attempting to put an entry will validate the value,
+        // triggering the recursive predicate and causing a StackOverflowError.
+        // The @Test(expected=...) annotation asserts that this error is thrown.
+        predicatedMap.put(testKeyAndValue, testKeyAndValue);
     }
 }
