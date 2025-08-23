f465b6b465047e238d1453792cafe7b849328f40
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:49:33 +0800

    Original processedAutoTestCases entities_estest48.java

---
+++ entities_estest48.java
@@ -0,0 +1,28 @@
+package org.jsoup.nodes;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.File;
+import java.io.PipedWriter;
+import java.io.StringWriter;
+import java.nio.BufferOverflowException;
+import java.nio.CharBuffer;
+import java.nio.ReadOnlyBufferException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.evosuite.runtime.mock.java.io.MockFileWriter;
+import org.evosuite.runtime.mock.java.io.MockPrintStream;
+import org.jsoup.internal.QuietAppendable;
+import org.junit.runner.RunWith;
+
+public class Entities_ESTestTest48 extends Entities_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test47() throws Throwable {
+        int[] intArray0 = new int[1];
+        int int0 = Entities.codepointsForName("US-ASCII", intArray0);
+        assertEquals(0, int0);
+    }
+}
