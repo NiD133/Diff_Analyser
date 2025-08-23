acbbed96867fc0d49c58eae914de74c1d4e45b59
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:58:16 +0800

    Original processedAutoTestCases jsontreereader_estest71.java

---
+++ jsontreereader_estest71.java
@@ -0,0 +1,38 @@
+package com.google.gson.internal.bind;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonNull;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.Strictness;
+import com.google.gson.stream.JsonToken;
+import java.io.IOException;
+import java.util.ConcurrentModificationException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class JsonTreeReader_ESTestTest71 extends JsonTreeReader_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test070() throws Throwable {
+        JsonObject jsonObject0 = new JsonObject();
+        jsonObject0.addProperty("h", "h");
+        JsonTreeReader jsonTreeReader0 = new JsonTreeReader(jsonObject0);
+        jsonTreeReader0.beginObject();
+        // Undeclared exception!
+        try {
+            jsonTreeReader0.nextJsonElement();
+            fail("Expecting exception: IllegalStateException");
+        } catch (IllegalStateException e) {
+            //
+            // Unexpected NAME when reading a JsonElement.
+            //
+            verifyException("com.google.gson.internal.bind.JsonTreeReader", e);
+        }
+    }
+}
