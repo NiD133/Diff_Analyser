d0a315779167c7f753378b85d78dd7f447cbe4cb
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:01:34 +0800

    gemini processedAutoTestCases lookuptranslator_estest9.java

--- lookuptranslator_estest9.java
+++ lookuptranslator_estest9.java
@@ -2,28 +2,24 @@ package org.apache.commons.lang3.text.translate;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.StringWriter;
-import java.io.Writer;
-import java.nio.CharBuffer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class LookupTranslator_ESTestTest9 extends LookupTranslator_ESTest_scaffolding {
+/**
+ * Tests for {@link LookupTranslator}.
+ */
+public class LookupTranslatorTest {
 
-    @Test(timeout = 4000)
-    public void test08() throws Throwable {
-        CharSequence[][] charSequenceArray0 = new CharSequence[1][0];
-        LookupTranslator lookupTranslator0 = null;
-        try {
-            lookupTranslator0 = new LookupTranslator(charSequenceArray0);
-            fail("Expecting exception: ArrayIndexOutOfBoundsException");
-        } catch (ArrayIndexOutOfBoundsException e) {
-            //
-            // 0
-            //
-            verifyException("org.apache.commons.lang3.text.translate.LookupTranslator", e);
-        }
+    /**
+     * Tests that the constructor throws an exception when a lookup array
+     * contains an empty inner array instead of a key-value pair.
+     */
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void constructorShouldThrowExceptionForMalformedLookupPair() {
+        // Arrange: Create a lookup table where an entry is an empty array,
+        // which is invalid because it should be a [key, value] pair.
+        final CharSequence[][] malformedLookupTable = new CharSequence[1][0];
+
+        // Act & Assert: Instantiating the translator with the malformed table
+        // should throw an ArrayIndexOutOfBoundsException.
+        new LookupTranslator(malformedLookupTable);
     }
 }
