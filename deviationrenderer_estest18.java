e21bd788a0db1538ddb359b83516dd44a991f77c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:47:59 +0800

    gemini processedAutoTestCases deviationrenderer_estest18.java

--- deviationrenderer_estest18.java
+++ deviationrenderer_estest18.java
@@ -1,43 +1,52 @@
 package org.jfree.chart.renderer.xy;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.awt.Graphics2D;
-import java.awt.geom.Rectangle2D;
-import java.util.Date;
-import java.util.Locale;
-import java.util.SimpleTimeZone;
-import java.util.TimeZone;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
 import org.jfree.chart.ChartRenderingInfo;
-import org.jfree.chart.axis.CategoryAnchor;
-import org.jfree.chart.axis.DateAxis;
-import org.jfree.chart.plot.DatasetRenderingOrder;
 import org.jfree.chart.plot.PlotRenderingInfo;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.data.time.TimeSeriesCollection;
-import org.jfree.data.xy.DefaultHighLowDataset;
-import org.jfree.data.xy.DefaultIntervalXYDataset;
-import org.jfree.data.xy.MatrixSeries;
-import org.jfree.data.xy.MatrixSeriesCollection;
-import org.junit.runner.RunWith;
+import org.jfree.data.xy.XYDataset;
+import org.junit.Test;
 
-public class DeviationRenderer_ESTestTest18 extends DeviationRenderer_ESTest_scaffolding {
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
 
-    @Test(timeout = 4000)
-    public void test17() throws Throwable {
-        DeviationRenderer deviationRenderer0 = new DeviationRenderer();
-        Rectangle2D.Double rectangle2D_Double0 = new Rectangle2D.Double((double) deviationRenderer0.ZERO, 90.0, 3048.34637057, 0.0);
-        XYPlot<CategoryAnchor> xYPlot0 = new XYPlot<CategoryAnchor>();
-        SimpleTimeZone simpleTimeZone0 = new SimpleTimeZone(10, "w@^qxE!G 6g");
-        TimeSeriesCollection<CategoryAnchor> timeSeriesCollection0 = new TimeSeriesCollection<CategoryAnchor>(simpleTimeZone0);
-        ChartRenderingInfo chartRenderingInfo0 = new ChartRenderingInfo();
-        PlotRenderingInfo plotRenderingInfo0 = new PlotRenderingInfo(chartRenderingInfo0);
-        XYItemRendererState xYItemRendererState0 = deviationRenderer0.initialise((Graphics2D) null, rectangle2D_Double0, xYPlot0, timeSeriesCollection0, plotRenderingInfo0);
-        assertTrue(deviationRenderer0.getDrawSeriesLineAsPath());
-        assertEquals(0.5F, deviationRenderer0.getAlpha(), 0.01F);
-        assertFalse(xYItemRendererState0.getProcessVisibleItemsOnly());
+import static org.junit.Assert.*;
+
+/**
+ * A set of tests for the {@link DeviationRenderer} class.
+ */
+public class DeviationRendererTest {
+
+    /**
+     * Verifies that the initialise() method returns a correctly configured state object
+     * and confirms that the renderer's default properties are set as expected upon instantiation.
+     */
+    @Test
+    public void initialise_shouldReturnCorrectStateAndConfirmDefaultProperties() {
+        // Arrange: Set up the renderer and the necessary mock/dummy objects for the initialise method.
+        DeviationRenderer renderer = new DeviationRenderer();
+        Rectangle2D dataArea = new Rectangle2D.Double();
+        XYPlot plot = new XYPlot();
+        XYDataset dataset = new TimeSeriesCollection();
+        PlotRenderingInfo plotInfo = new PlotRenderingInfo(new ChartRenderingInfo());
+        
+        // The Graphics2D context is not used in the initialise method, so null is acceptable.
+        Graphics2D g2 = null;
+
+        // Act: Call the initialise method to get the renderer state.
+        XYItemRendererState state = renderer.initialise(g2, dataArea, plot, dataset, plotInfo);
+
+        // Assert: Verify the default properties of the renderer and the returned state object.
+        assertNotNull("The initialised state object should not be null.", state);
+
+        // 1. Verify default properties of the DeviationRenderer instance.
+        assertTrue("The 'drawSeriesLineAsPath' property should be true by default for DeviationRenderer.",
+                renderer.getDrawSeriesLineAsPath());
+        assertEquals("The default alpha transparency should be 0.5f.", 0.5F, renderer.getAlpha(), 0.001F);
+
+        // 2. Verify a key property of the state object returned by initialise().
+        //    DeviationRenderer requires processing all items to draw deviation bands correctly.
+        assertFalse("The 'processVisibleItemsOnly' flag in the state should be false.",
+                state.getProcessVisibleItemsOnly());
     }
 }
