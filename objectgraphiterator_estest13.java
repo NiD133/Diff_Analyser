38fe047e7d4cde8b8f236fa535fe645f4b29989d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:05:38 +0800

    Original processedAutoTestCases objectgraphiterator_estest13.java

---
+++ objectgraphiterator_estest13.java
@@ -0,0 +1,56 @@
+package org.apache.commons.collections4.iterators;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.lang.reflect.Array;
+import java.util.Comparator;
+import java.util.ConcurrentModificationException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.function.Function;
+import org.apache.commons.collections4.Factory;
+import org.apache.commons.collections4.Predicate;
+import org.apache.commons.collections4.Transformer;
+import org.apache.commons.collections4.functors.AndPredicate;
+import org.apache.commons.collections4.functors.ComparatorPredicate;
+import org.apache.commons.collections4.functors.ConstantFactory;
+import org.apache.commons.collections4.functors.ConstantTransformer;
+import org.apache.commons.collections4.functors.EqualPredicate;
+import org.apache.commons.collections4.functors.ExceptionPredicate;
+import org.apache.commons.collections4.functors.ExceptionTransformer;
+import org.apache.commons.collections4.functors.FactoryTransformer;
+import org.apache.commons.collections4.functors.FalsePredicate;
+import org.apache.commons.collections4.functors.IdentityPredicate;
+import org.apache.commons.collections4.functors.IfTransformer;
+import org.apache.commons.collections4.functors.InstanceofPredicate;
+import org.apache.commons.collections4.functors.InvokerTransformer;
+import org.apache.commons.collections4.functors.MapTransformer;
+import org.apache.commons.collections4.functors.NonePredicate;
+import org.apache.commons.collections4.functors.NullIsFalsePredicate;
+import org.apache.commons.collections4.functors.NullIsTruePredicate;
+import org.apache.commons.collections4.functors.OrPredicate;
+import org.apache.commons.collections4.functors.PredicateTransformer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class ObjectGraphIterator_ESTestTest13 extends ObjectGraphIterator_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test12() throws Throwable {
+        Class<Integer> class0 = Integer.class;
+        InstanceofPredicate instanceofPredicate0 = new InstanceofPredicate(class0);
+        ConstantTransformer<InstanceofPredicate, InstanceofPredicate> constantTransformer0 = new ConstantTransformer<InstanceofPredicate, InstanceofPredicate>(instanceofPredicate0);
+        ObjectGraphIterator<InstanceofPredicate> objectGraphIterator0 = new ObjectGraphIterator<InstanceofPredicate>(instanceofPredicate0, constantTransformer0);
+        ObjectGraphIterator<InstanceofPredicate> objectGraphIterator1 = new ObjectGraphIterator<InstanceofPredicate>(instanceofPredicate0, constantTransformer0);
+        objectGraphIterator0.findNextByIterator(objectGraphIterator1);
+        assertFalse(objectGraphIterator0.equals((Object) objectGraphIterator1));
+    }
+}
