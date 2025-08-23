216298a6bc3e0bc4a2ea48c532cb52d0f67f5881
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:39:10 +0800

    gemini processedAutoTestCases bcodec_estest27.java

--- bcodec_estest27.java
+++ bcodec_estest27.java
@@ -1,29 +1,40 @@
 package org.apache.commons.codec.net;
 
+import org.apache.commons.codec.EncoderException;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.nio.charset.Charset;
-import java.nio.charset.IllegalCharsetNameException;
-import java.nio.charset.UnsupportedCharsetException;
-import org.apache.commons.codec.CodecPolicy;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class BCodec_ESTestTest27 extends BCodec_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
-    @Test(timeout = 4000)
-    public void test26() throws Throwable {
-        BCodec bCodec0 = new BCodec();
+/**
+ * Unit tests for the {@link BCodec} class.
+ */
+public class BCodecTest {
+
+    /**
+     * Tests that attempting to encode an object of an unsupported type results in an
+     * {@link EncoderException}. The {@code BCodec#encode(Object)} method is designed
+     * to handle {@code String} and {@code byte[]} inputs, and should fail for any other type.
+     */
+    @Test
+    public void encodeUnsupportedObjectTypeShouldThrowEncoderException() {
+        // Arrange: Create a BCodec instance and an object of an unsupported type.
+        // In this case, we use another BCodec instance as the invalid input.
+        final BCodec codec = new BCodec();
+        final Object unsupportedObject = new BCodec();
+
+        // Act & Assert: Verify that the encode operation throws the correct exception
+        // with the expected message.
         try {
-            bCodec0.encode((Object) bCodec0);
-            fail("Expecting exception: Exception");
-        } catch (Exception e) {
-            //
-            // Objects of type org.apache.commons.codec.net.BCodec cannot be encoded using BCodec
-            //
-            verifyException("org.apache.commons.codec.net.BCodec", e);
+            codec.encode(unsupportedObject);
+            fail("Expected an EncoderException to be thrown for an unsupported object type.");
+        } catch (final EncoderException e) {
+            // The exception is expected. Now, we verify its message for correctness.
+            final String expectedMessage = "Objects of type " +
+                    unsupportedObject.getClass().getName() +
+                    " cannot be encoded using " +
+                    codec.getClass().getName();
+            assertEquals("The exception message did not match the expected format.", expectedMessage, e.getMessage());
         }
     }
 }
