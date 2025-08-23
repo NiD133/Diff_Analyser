361a47d7ad2a75566e911196b85cf5c548fe9328
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:35:52 +0800

    gemini processedManualTestCases weekstest2.java

--- weekstest2.java
+++ weekstest2.java
@@ -1,42 +1,35 @@
 package org.joda.time;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
 
-public class WeeksTestTest2 extends TestCase {
-
-    // (before the late 90's they were all over the place)
-    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
-
-    public static void main(String[] args) {
-        junit.textui.TestRunner.run(suite());
-    }
-
-    public static TestSuite suite() {
-        return new TestSuite(TestWeeks.class);
-    }
-
-    @Override
-    protected void setUp() throws Exception {
-    }
-
-    @Override
-    protected void tearDown() throws Exception {
-    }
+/**
+ * Unit tests for the Weeks class, focusing on the factory method.
+ */
+public class WeeksTest {
 
     //-----------------------------------------------------------------------
-    public void testFactory_weeks_int() {
-        assertSame(Weeks.ZERO, Weeks.weeks(0));
-        assertSame(Weeks.ONE, Weeks.weeks(1));
-        assertSame(Weeks.TWO, Weeks.weeks(2));
-        assertSame(Weeks.THREE, Weeks.weeks(3));
-        assertSame(Weeks.MAX_VALUE, Weeks.weeks(Integer.MAX_VALUE));
-        assertSame(Weeks.MIN_VALUE, Weeks.weeks(Integer.MIN_VALUE));
-        assertEquals(-1, Weeks.weeks(-1).getWeeks());
-        assertEquals(4, Weeks.weeks(4).getWeeks());
+    // Test factory method: weeks(int)
+    //-----------------------------------------------------------------------
+
+    @Test
+    public void weeks_factory_returnsCachedInstancesForConstants() {
+        // The weeks(int) factory is expected to return cached singleton instances
+        // for commonly used values and for the min/max values.
+        assertSame("0 weeks should return the ZERO singleton", Weeks.ZERO, Weeks.weeks(0));
+        assertSame("1 week should return the ONE singleton", Weeks.ONE, Weeks.weeks(1));
+        assertSame("2 weeks should return the TWO singleton", Weeks.TWO, Weeks.weeks(2));
+        assertSame("3 weeks should return the THREE singleton", Weeks.THREE, Weeks.weeks(3));
+        assertSame("Integer.MAX_VALUE should return the MAX_VALUE singleton", Weeks.MAX_VALUE, Weeks.weeks(Integer.MAX_VALUE));
+        assertSame("Integer.MIN_VALUE should return the MIN_VALUE singleton", Weeks.MIN_VALUE, Weeks.weeks(Integer.MIN_VALUE));
+    }
+
+    @Test
+    public void weeks_factory_createsCorrectInstanceForOtherValues() {
+        // The weeks(int) factory should create a new Weeks instance
+        // with the correct value for numbers that are not cached constants.
+        assertEquals("Value of -1 weeks should be -1", -1, Weeks.weeks(-1).getWeeks());
+        assertEquals("Value of 4 weeks should be 4", 4, Weeks.weeks(4).getWeeks());
     }
 }
