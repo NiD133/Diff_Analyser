9f8d351cdf6db0b7939a10b3e503894e7de5cb9f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:05:34 +0800

    gemini processedAutoTestCases numericentityunescaper_estest2.java

--- numericentityunescaper_estest2.java
+++ numericentityunescaper_estest2.java
@@ -1,23 +1,33 @@
 package org.apache.commons.lang3.text.translate;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
+import static org.junit.Assert.assertEquals;
+
 import java.io.StringWriter;
-import java.io.Writer;
-import java.nio.CharBuffer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import java.io.IOException;
 
-public class NumericEntityUnescaper_ESTestTest2 extends NumericEntityUnescaper_ESTest_scaffolding {
+/**
+ * Unit tests for {@link NumericEntityUnescaper}.
+ */
+public class NumericEntityUnescaperTest {
 
-    @Test(timeout = 4000)
-    public void test01() throws Throwable {
-        NumericEntityUnescaper.OPTION[] numericEntityUnescaper_OPTIONArray0 = new NumericEntityUnescaper.OPTION[0];
-        StringWriter stringWriter0 = new StringWriter();
-        NumericEntityUnescaper numericEntityUnescaper0 = new NumericEntityUnescaper(numericEntityUnescaper_OPTIONArray0);
-        numericEntityUnescaper0.translate((CharSequence) "D+RTgb,eb:&],ms", (Writer) stringWriter0);
-        assertEquals("D+RTgb,eb:&],ms", stringWriter0.toString());
+    /**
+     * Tests that the translator does not change an input string
+     * that does not contain any valid numeric entities.
+     */
+    @Test
+    public void shouldNotModifyStringContainingNoNumericEntities() throws IOException {
+        // Arrange
+        // The unescaper is created with default options (semiColonRequired).
+        final NumericEntityUnescaper unescaper = new NumericEntityUnescaper();
+        final String input = "This string contains an ampersand & but no numeric entity.";
+        final StringWriter writer = new StringWriter();
+
+        // Act
+        unescaper.translate(input, writer);
+
+        // Assert
+        assertEquals("The output string should be identical to the input string.",
+                     input, writer.toString());
     }
 }
