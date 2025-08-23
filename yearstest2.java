b1f9e6b356a5d74a3cf8e2232bac7848de60c552
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:36:29 +0800

    Original processedManualTestCases yearstest2.java

---
+++ yearstest2.java
@@ -0,0 +1,42 @@
+package org.joda.time;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class YearsTestTest2 extends TestCase {
+
+    // (before the late 90's they were all over the place)
+    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
+
+    public static TestSuite suite() {
+        return new TestSuite(TestYears.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+    }
+
+    //-----------------------------------------------------------------------
+    public void testFactory_years_int() {
+        assertSame(Years.ZERO, Years.years(0));
+        assertSame(Years.ONE, Years.years(1));
+        assertSame(Years.TWO, Years.years(2));
+        assertSame(Years.THREE, Years.years(3));
+        assertSame(Years.MAX_VALUE, Years.years(Integer.MAX_VALUE));
+        assertSame(Years.MIN_VALUE, Years.years(Integer.MIN_VALUE));
+        assertEquals(-1, Years.years(-1).getYears());
+        assertEquals(4, Years.years(4).getYears());
+    }
+}
