8d505ae692c4d433e1c4d5ddadb2466bff5608e2
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:44:35 +0800

    gemini processedAutoTestCases collectionsortedbag_estest38.java

--- collectionsortedbag_estest38.java
+++ collectionsortedbag_estest38.java
@@ -1,52 +1,55 @@
 package org.apache.commons.collections4.bag;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.ConcurrentModificationException;
-import java.util.LinkedList;
-import java.util.Locale;
-import java.util.PriorityQueue;
-import java.util.Set;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.Predicate;
 import org.apache.commons.collections4.SortedBag;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionPredicate;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.IfClosure;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.NOPClosure;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.TransformedPredicate;
-import org.apache.commons.collections4.functors.TransformerPredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class CollectionSortedBag_ESTestTest38 extends CollectionSortedBag_ESTest_scaffolding {
+import java.util.Comparator;
 
-    @Test(timeout = 4000)
-    public void test37() throws Throwable {
-        Comparator<Object> comparator0 = (Comparator<Object>) mock(Comparator.class, new ViolatedAssumptionAnswer());
-        doReturn((-1959), (-1959), (-1959)).when(comparator0).compare(any(), any());
-        TreeBag<Object> treeBag0 = new TreeBag<Object>(comparator0);
-        IfClosure<Object> ifClosure0 = new IfClosure<Object>((Predicate<? super Object>) null, (Closure<? super Object>) null);
-        treeBag0.add((Object) ifClosure0);
-        CollectionSortedBag<Object> collectionSortedBag0 = new CollectionSortedBag<Object>(treeBag0);
-        boolean boolean0 = collectionSortedBag0.removeAll(treeBag0);
-        assertFalse(boolean0);
+import static org.junit.Assert.assertFalse;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+
+/**
+ * Contains tests for the {@link CollectionSortedBag} class.
+ */
+public class CollectionSortedBagTest {
+
+    /**
+     * Tests that removeAll returns false if the underlying bag's comparator
+     * is faulty and cannot find its own elements.
+     *
+     * The CollectionSortedBag#removeAll implementation relies on getCount() to determine
+     * how many items to remove. If the underlying TreeBag has a comparator that
+     * violates its contract (i.e., compare(x, x) != 0), its internal search
+     * will fail. Consequently, getCount() will return 0, no elements will be
+     * removed, and removeAll() should correctly report that the collection was
+     * not modified by returning false.
+     */
+    @Test
+    public void removeAllShouldReturnFalseWhenUnderlyingBagCannotFindItsOwnElements() {
+        // Arrange
+        // 1. Create a comparator that violates the contract by never returning 0.
+        // This will cause search operations in the TreeBag to fail.
+        @SuppressWarnings("unchecked")
+        final Comparator<Object> faultyComparator = mock(Comparator.class);
+        doReturn(-1).when(faultyComparator).compare(any(), any());
+
+        // 2. Create the underlying bag with the faulty comparator and add an element.
+        final SortedBag<Object> underlyingBag = new TreeBag<>(faultyComparator);
+        final String element = "test_element";
+        underlyingBag.add(element);
+
+        // 3. Decorate the bag. The decorated bag will inherit the faulty search behavior.
+        final SortedBag<Object> decoratedBag = CollectionSortedBag.collectionSortedBag(underlyingBag);
+
+        // Act
+        // Attempt to remove all elements from the decorated bag that are also in the
+        // underlying bag. Because of the faulty comparator, the elements won't be found.
+        final boolean wasModified = decoratedBag.removeAll(underlyingBag);
+
+        // Assert
+        // The operation should report that the collection was not modified.
+        assertFalse("Expected removeAll to return false as elements could not be found", wasModified);
     }
 }
