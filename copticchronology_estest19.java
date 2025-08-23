63a18c2c4bb4229bca7bca54ef8ecec6a701b9b5
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:45:57 +0800

    Original processedAutoTestCases copticchronology_estest19.java

---
+++ copticchronology_estest19.java
@@ -0,0 +1,20 @@
+package org.joda.time.chrono;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.joda.time.Chronology;
+import org.joda.time.DateTimeZone;
+import org.junit.runner.RunWith;
+
+public class CopticChronology_ESTestTest19 extends CopticChronology_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test18() throws Throwable {
+        CopticChronology copticChronology0 = CopticChronology.getInstanceUTC();
+        Chronology chronology0 = copticChronology0.withZone((DateTimeZone) null);
+        assertSame(copticChronology0, chronology0);
+    }
+}
