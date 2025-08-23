d9886c2c75c075db19b9f5f42f63a0c385402aa8
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:08:53 +0800

    Original processedAutoTestCases phoneticengine_estest8.java

---
+++ phoneticengine_estest8.java
@@ -0,0 +1,30 @@
+package org.apache.commons.codec.language.bm;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.nio.CharBuffer;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class PhoneticEngine_ESTestTest8 extends PhoneticEngine_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test07() throws Throwable {
+        NameType nameType0 = NameType.SEPHARDIC;
+        RuleType ruleType0 = RuleType.RULES;
+        PhoneticEngine phoneticEngine0 = null;
+        try {
+            phoneticEngine0 = new PhoneticEngine(nameType0, ruleType0, true, 772);
+            fail("Expecting exception: IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //
+            // ruleType must not be RULES
+            //
+            verifyException("org.apache.commons.codec.language.bm.PhoneticEngine", e);
+        }
+    }
+}
