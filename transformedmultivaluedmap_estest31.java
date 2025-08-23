1c4b908afebbf01d50d2a1a279b8d25b62316c1d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:23:20 +0800

    gemini processedAutoTestCases transformedmultivaluedmap_estest31.java

--- transformedmultivaluedmap_estest31.java
+++ transformedmultivaluedmap_estest31.java
@@ -1,55 +1,44 @@
 package org.apache.commons.collections4.multimap;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.AbstractMap;
-import java.util.ArrayDeque;
-import java.util.Comparator;
-import java.util.ConcurrentModificationException;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
-import java.util.PriorityQueue;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.Factory;
 import org.apache.commons.collections4.MultiValuedMap;
-import org.apache.commons.collections4.Predicate;
 import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AllPredicate;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ChainedTransformer;
-import org.apache.commons.collections4.functors.CloneTransformer;
-import org.apache.commons.collections4.functors.ClosureTransformer;
-import org.apache.commons.collections4.functors.ConstantFactory;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.ExceptionFactory;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.FactoryTransformer;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.MapTransformer;
 import org.apache.commons.collections4.functors.NOPTransformer;
-import org.apache.commons.collections4.functors.NotPredicate;
-import org.apache.commons.collections4.functors.NullIsExceptionPredicate;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.SwitchTransformer;
-import org.apache.commons.collections4.functors.TransformerClosure;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class TransformedMultiValuedMap_ESTestTest31 extends TransformedMultiValuedMap_ESTest_scaffolding {
+import static org.junit.Assert.assertFalse;
 
-    @Test(timeout = 4000)
-    public void test30() throws Throwable {
-        HashSetValuedHashMap<Integer, Integer> hashSetValuedHashMap0 = new HashSetValuedHashMap<Integer, Integer>(0);
-        Transformer<Object, Integer> transformer0 = InvokerTransformer.invokerTransformer("");
-        TransformedMultiValuedMap<Integer, Integer> transformedMultiValuedMap0 = new TransformedMultiValuedMap<Integer, Integer>(hashSetValuedHashMap0, transformer0, transformer0);
-        boolean boolean0 = transformedMultiValuedMap0.putAll((MultiValuedMap<? extends Integer, ? extends Integer>) hashSetValuedHashMap0);
-        assertFalse(boolean0);
+/**
+ * Contains tests for the {@link TransformedMultiValuedMap} class.
+ */
+public class TransformedMultiValuedMapTest {
+
+    /**
+     * Tests that calling putAll() with an empty map does not modify the
+     * destination map and correctly returns false.
+     */
+    @Test
+    public void putAllWithEmptyMapShouldReturnFalse() {
+        // Arrange
+        // Create the map that will be decorated.
+        final MultiValuedMap<String, Integer> decoratedMap = new HashSetValuedHashMap<>();
+        decoratedMap.put("A", 1); // Pre-populate to ensure it's not empty.
+
+        // Use a No-Operation transformer, as the transformation logic is not relevant
+        // for this test case.
+        final Transformer<Object, Object> nopTransformer = NOPTransformer.nopTransformer();
+
+        final TransformedMultiValuedMap<String, Integer> transformedMap =
+                new TransformedMultiValuedMap<>(decoratedMap, nopTransformer, nopTransformer);
+
+        // The source map for the putAll operation is empty.
+        final MultiValuedMap<String, Integer> emptySourceMap = new HashSetValuedHashMap<>();
+
+        // Act
+        // Attempt to add all entries from the empty source map.
+        final boolean wasModified = transformedMap.putAll(emptySourceMap);
+
+        // Assert
+        // The contract of putAll states it should return false if the collection was not changed.
+        assertFalse("putAll should return false when the source map is empty", wasModified);
     }
 }
