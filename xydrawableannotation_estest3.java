77b628c7a46116cc5c6f6cbf0ad47650cdb41852
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:29:37 +0800

    gemini processedAutoTestCases xydrawableannotation_estest3.java

--- xydrawableannotation_estest3.java
+++ xydrawableannotation_estest3.java
@@ -1,53 +1,37 @@
 package org.jfree.chart.annotations;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.awt.Font;
-import java.awt.Graphics2D;
-import java.awt.geom.Rectangle2D;
-import java.awt.geom.RoundRectangle2D;
-import java.awt.image.BufferedImage;
-import java.math.BigInteger;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.jfree.chart.ChartRenderingInfo;
-import org.jfree.chart.Drawable;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.axis.CyclicNumberAxis;
-import org.jfree.chart.axis.NumberAxis;
-import org.jfree.chart.axis.ValueAxis;
-import org.jfree.chart.block.BlockContainer;
-import org.jfree.chart.legend.LegendTitle;
-import org.jfree.chart.plot.MeterPlot;
-import org.jfree.chart.plot.PlotRenderingInfo;
-import org.jfree.chart.plot.PolarAxisLocation;
-import org.jfree.chart.plot.XYPlot;
-import org.jfree.chart.renderer.AbstractRenderer;
 import org.jfree.chart.renderer.category.ScatterRenderer;
-import org.jfree.chart.renderer.xy.XYBarRenderer;
-import org.jfree.chart.title.CompositeTitle;
-import org.jfree.chart.title.DateTitle;
-import org.jfree.chart.title.ShortTextTitle;
+import org.jfree.chart.title.LegendTitle;
 import org.jfree.chart.title.TextTitle;
-import org.jfree.data.general.DefaultValueDataset;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class XYDrawableAnnotation_ESTestTest3 extends XYDrawableAnnotation_ESTest_scaffolding {
+import static org.junit.Assert.assertFalse;
 
-    @Test(timeout = 4000)
-    public void test02() throws Throwable {
-        TextTitle textTitle0 = new TextTitle("");
-        XYDrawableAnnotation xYDrawableAnnotation0 = new XYDrawableAnnotation(0.0, 0.0, 0.0, (-1062.3), textTitle0);
-        ScatterRenderer scatterRenderer0 = new ScatterRenderer();
-        LegendTitle legendTitle0 = new LegendTitle(scatterRenderer0);
-        XYDrawableAnnotation xYDrawableAnnotation1 = new XYDrawableAnnotation(0.0, 0.0, 0.0, 26.840400407963966, 26.840400407963966, legendTitle0);
-        boolean boolean0 = xYDrawableAnnotation0.equals(xYDrawableAnnotation1);
-        assertFalse(boolean0);
-        assertEquals(0.0, xYDrawableAnnotation1.getX(), 0.01);
-        assertEquals(0.0, xYDrawableAnnotation1.getY(), 0.01);
-        assertEquals(0.0, xYDrawableAnnotation1.getDisplayWidth(), 0.01);
-        assertEquals((-1062.3), xYDrawableAnnotation0.getDisplayHeight(), 0.01);
-        assertEquals(1.0, xYDrawableAnnotation0.getDrawScaleFactor(), 0.01);
+/**
+ * Tests for the equals() method in the {@link XYDrawableAnnotation} class.
+ */
+public class XYDrawableAnnotationEqualsTest {
+
+    @Test
+    public void equals_whenObjectsHaveDifferentProperties_shouldReturnFalse() {
+        // Arrange
+        // Create a primary annotation instance. This constructor defaults the drawScaleFactor to 1.0.
+        TextTitle firstDrawable = new TextTitle("First Annotation");
+        XYDrawableAnnotation annotation1 = new XYDrawableAnnotation(
+                0.0, 0.0, 100.0, -50.0, firstDrawable);
+
+        // Create a second annotation with different dimensions, scale factor, and a different drawable object.
+        LegendTitle secondDrawable = new LegendTitle(new ScatterRenderer());
+        XYDrawableAnnotation annotation2 = new XYDrawableAnnotation(
+                0.0, 0.0, 100.0, 75.0, 1.5, secondDrawable);
+
+        // Act
+        // Compare the two different annotation objects.
+        boolean areEqual = annotation1.equals(annotation2);
+
+        // Assert
+        // The objects should not be considered equal because their display height,
+        // draw scale factor, and drawable objects are different.
+        assertFalse("Annotations with different properties should not be equal", areEqual);
     }
 }
