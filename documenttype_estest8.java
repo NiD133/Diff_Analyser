a2e24e22b9633cd32a4b9fbad49790326f7dfb89
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:48:13 +0800

    Original processedAutoTestCases documenttype_estest8.java

---
+++ documenttype_estest8.java
@@ -0,0 +1,29 @@
+package org.jsoup.nodes;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.StringWriter;
+import java.nio.BufferOverflowException;
+import java.nio.CharBuffer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.jsoup.internal.QuietAppendable;
+import org.junit.runner.RunWith;
+
+public class DocumentType_ESTestTest8 extends DocumentType_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test07() throws Throwable {
+        DocumentType documentType0 = null;
+        try {
+            documentType0 = new DocumentType("", "", (String) null);
+            fail("Expecting exception: IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //
+            // Object must not be null
+            //
+            verifyException("org.jsoup.helper.Validate", e);
+        }
+    }
+}
