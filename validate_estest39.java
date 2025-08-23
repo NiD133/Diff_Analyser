2310798cebf35f86625ef83b5374106ba088e6a9
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:26:03 +0800

    Original processedAutoTestCases validate_estest39.java

---
+++ validate_estest39.java
@@ -0,0 +1,32 @@
+package org.jsoup.helper;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.FormatFlagsConversionMismatchException;
+import java.util.IllegalFormatConversionException;
+import java.util.IllegalFormatFlagsException;
+import java.util.IllegalFormatWidthException;
+import java.util.MissingFormatArgumentException;
+import java.util.MissingFormatWidthException;
+import java.util.UnknownFormatConversionException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class Validate_ESTestTest39 extends Validate_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test38() throws Throwable {
+        // Undeclared exception!
+        try {
+            Validate.ensureNotNull((Object) null);
+            fail("Expecting exception: IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //
+            // Object must not be null
+            //
+            verifyException("org.jsoup.helper.Validate", e);
+        }
+    }
+}
