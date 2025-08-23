6024104aa90bd12523b32641a35c4266cc97e460
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:04:47 +0800

    gemini processedAutoTestCases nullreader_estest12.java

--- nullreader_estest12.java
+++ nullreader_estest12.java
@@ -1,28 +1,25 @@
 package org.apache.commons.io.input;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.EOFException;
 import java.io.IOException;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class NullReader_ESTestTest12 extends NullReader_ESTest_scaffolding {
+/**
+ * Contains unit tests for the {@link NullReader} class.
+ */
+public class NullReaderTest {
 
-    @Test(timeout = 4000)
-    public void test11() throws Throwable {
-        NullReader nullReader0 = new NullReader();
-        // Undeclared exception!
-        try {
-            nullReader0.read((char[]) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.commons.io.input.NullReader", e);
-        }
+    /**
+     * Verifies that the read(char[]) method throws a NullPointerException
+     * when a null buffer is passed as an argument. This is the expected behavior
+     * inherited from the base java.io.Reader class.
+     */
+    @Test(expected = NullPointerException.class)
+    public void readWithNullCharArrayShouldThrowNullPointerException() throws IOException {
+        // Arrange: Create a NullReader instance. The size is irrelevant for this test.
+        final NullReader reader = new NullReader();
+
+        // Act & Assert: Calling read with a null array should throw a NullPointerException.
+        // The @Test(expected=...) annotation handles the assertion.
+        reader.read(null);
     }
 }
