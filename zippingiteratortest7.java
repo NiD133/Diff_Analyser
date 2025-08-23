a59ae88ef12aa51849315751f6a764a17b02ab95
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:36:34 +0800

    Original processedManualTestCases zippingiteratortest7.java

---
+++ zippingiteratortest7.java
@@ -0,0 +1,67 @@
+package org.apache.commons.collections4.iterators;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.ArrayList;
+import org.apache.commons.collections4.IteratorUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ZippingIteratorTestTest7 extends AbstractIteratorTest<Integer> {
+
+    private ArrayList<Integer> evens;
+
+    private ArrayList<Integer> odds;
+
+    private ArrayList<Integer> fib;
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public ZippingIterator<Integer> makeEmptyIterator() {
+        return new ZippingIterator<>(IteratorUtils.<Integer>emptyIterator());
+    }
+
+    @Override
+    public ZippingIterator<Integer> makeObject() {
+        return new ZippingIterator<>(evens.iterator(), odds.iterator(), fib.iterator());
+    }
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        evens = new ArrayList<>();
+        odds = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            if (0 == i % 2) {
+                evens.add(i);
+            } else {
+                odds.add(i);
+            }
+        }
+        fib = new ArrayList<>();
+        fib.add(1);
+        fib.add(1);
+        fib.add(2);
+        fib.add(3);
+        fib.add(5);
+        fib.add(8);
+        fib.add(13);
+        fib.add(21);
+    }
+
+    @Test
+    void testRemoveFromSingle() {
+        @SuppressWarnings("unchecked")
+        final ZippingIterator<Integer> iter = new ZippingIterator<>(evens.iterator());
+        int expectedSize = evens.size();
+        while (iter.hasNext()) {
+            final Object o = iter.next();
+            final Integer val = (Integer) o;
+            if (val.intValue() % 4 == 0) {
+                expectedSize--;
+                iter.remove();
+            }
+        }
+        assertEquals(expectedSize, evens.size());
+    }
+}
