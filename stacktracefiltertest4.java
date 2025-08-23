b3379255fca88c9c4f2078e71b9cdcd5d055d457
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:34:00 +0800

    Original processedManualTestCases stacktracefiltertest4.java

---
+++ stacktracefiltertest4.java
@@ -0,0 +1,20 @@
+package org.mockito.internal.exceptions.stacktrace;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockitoutil.Conditions.onlyThoseClasses;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.mockito.exceptions.base.TraceBuilder;
+import org.mockitoutil.TestBase;
+
+public class StackTraceFilterTestTest4 extends TestBase {
+
+    private final StackTraceFilter filter = new StackTraceFilter();
+
+    @Test
+    public void shouldNotFilterOutTracesMiddleGoodTraces() {
+        StackTraceElement[] t = new TraceBuilder().classes("org.test.MockitoSampleTest", "org.test.TestSupport", "org.mockito.Mockito", "org.test.TestSupport", "org.mockito.Mockito").toTraceArray();
+        StackTraceElement[] filtered = filter.filter(t, false);
+        Assertions.assertThat(filtered).has(onlyThoseClasses("org.test.TestSupport", "org.test.TestSupport", "org.test.MockitoSampleTest"));
+    }
+}
