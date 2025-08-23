4e565fe6ee893c6e14abc9d32ae11a0da91e1e66
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:49:01 +0800

    Original processedAutoTestCases elements_estest80.java

---
+++ elements_estest80.java
@@ -0,0 +1,43 @@
+package org.jsoup.select;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.ConcurrentModificationException;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.jsoup.nodes.Comment;
+import org.jsoup.nodes.DataNode;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.FormElement;
+import org.jsoup.nodes.TextNode;
+import org.jsoup.parser.Parser;
+import org.junit.runner.RunWith;
+
+public class Elements_ESTestTest80 extends Elements_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test079() throws Throwable {
+        Elements elements0 = new Elements();
+        // Undeclared exception!
+        try {
+            elements0.is("{xrk0 ");
+            fail("Expecting exception: IllegalStateException");
+        } catch (IllegalStateException e) {
+            //
+            // Could not parse query '{xrk0': unexpected token at '{xrk0'
+            //
+            verifyException("org.jsoup.select.QueryParser", e);
+        }
+    }
+}
