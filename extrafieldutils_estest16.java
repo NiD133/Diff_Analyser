fd65c1f7a52603e7ba9c1cc8bf53f5c20ea10e12
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:49:55 +0800

    Original processedAutoTestCases extrafieldutils_estest16.java

---
+++ extrafieldutils_estest16.java
@@ -0,0 +1,27 @@
+package org.apache.commons.compress.archivers.zip;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.zip.ZipException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class ExtraFieldUtils_ESTestTest16 extends ExtraFieldUtils_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test15() throws Throwable {
+        AsiExtraField asiExtraField0 = new AsiExtraField();
+        byte[] byteArray0 = asiExtraField0.getLocalFileDataData();
+        try {
+            ExtraFieldUtils.parse(byteArray0);
+            fail("Expecting exception: ZipException");
+        } catch (ZipException e) {
+            //
+            // Bad extra field starting at 0.  Block length of 58250 bytes exceeds remaining data of 10 bytes.
+            //
+            verifyException("org.apache.commons.compress.archivers.zip.ExtraFieldUtils$UnparseableExtraField", e);
+        }
+    }
+}
