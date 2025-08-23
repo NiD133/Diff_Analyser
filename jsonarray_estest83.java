e145addb5b43720c72d9c0f2ce20427afe08d508
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:56:23 +0800

    Original processedAutoTestCases jsonarray_estest83.java

---
+++ jsonarray_estest83.java
@@ -0,0 +1,30 @@
+package com.google.gson;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Iterator;
+import java.util.List;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class JsonArray_ESTestTest83 extends JsonArray_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test82() throws Throwable {
+        JsonArray jsonArray0 = new JsonArray();
+        // Undeclared exception!
+        try {
+            jsonArray0.getAsCharacter();
+            fail("Expecting exception: IllegalStateException");
+        } catch (IllegalStateException e) {
+            //
+            // Array must have size 1, but has size 0
+            //
+            verifyException("com.google.gson.JsonArray", e);
+        }
+    }
+}
