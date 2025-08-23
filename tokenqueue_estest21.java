1ef1bc8ca213e84631d9fdfd00edfab8ccd883be
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:22:23 +0800

    gemini processedAutoTestCases tokenqueue_estest21.java

--- tokenqueue_estest21.java
+++ tokenqueue_estest21.java
@@ -1,25 +1,19 @@
 package org.jsoup.parser;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
 import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
 import org.junit.runner.RunWith;
 
+// The test class structure and runner are preserved from the original generated code.
+@RunWith(EvoRunner.class)
 public class TokenQueue_ESTestTest21 extends TokenQueue_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test20() throws Throwable {
-        // Undeclared exception!
-        try {
-            TokenQueue.unescape((String) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.jsoup.parser.TokenQueue", e);
-        }
+    /**
+     * Verifies that the static unescape method throws a NullPointerException
+     * when called with a null input string.
+     */
+    @Test(expected = NullPointerException.class)
+    public void unescapeShouldThrowNullPointerExceptionWhenInputIsNull() {
+        TokenQueue.unescape(null);
     }
 }
