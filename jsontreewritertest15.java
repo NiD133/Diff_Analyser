e096356f76be661ad83d34868c40883f2e4a8556
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:30:16 +0800

    Original processedManualTestCases jsontreewritertest15.java

---
+++ jsontreewritertest15.java
@@ -0,0 +1,24 @@
+package com.google.gson.internal.bind;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertThrows;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonNull;
+import com.google.gson.Strictness;
+import com.google.gson.common.MoreAsserts;
+import com.google.gson.stream.JsonWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Test;
+
+public class JsonTreeWriterTestTest15 {
+
+    @Test
+    public void testValueString() throws Exception {
+        JsonTreeWriter writer = new JsonTreeWriter();
+        String n = "as";
+        assertThat(writer.value(n)).isEqualTo(writer);
+    }
+}
