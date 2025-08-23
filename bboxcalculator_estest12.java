4d97cee3b36b67669e0172997151df589b496ed5
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:38:54 +0800

    Original processedAutoTestCases bboxcalculator_estest12.java

---
+++ bboxcalculator_estest12.java
@@ -0,0 +1,32 @@
+package org.locationtech.spatial4j.shape.impl;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.HashMap;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+import org.locationtech.spatial4j.context.SpatialContext;
+import org.locationtech.spatial4j.context.SpatialContextFactory;
+import org.locationtech.spatial4j.distance.GeodesicSphereDistCalc;
+import org.locationtech.spatial4j.shape.Point;
+import org.locationtech.spatial4j.shape.Rectangle;
+
+public class BBoxCalculator_ESTestTest12 extends BBoxCalculator_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test11() throws Throwable {
+        BBoxCalculator bBoxCalculator0 = new BBoxCalculator((SpatialContext) null);
+        // Undeclared exception!
+        try {
+            bBoxCalculator0.getBoundary();
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.locationtech.spatial4j.shape.impl.BBoxCalculator", e);
+        }
+    }
+}
