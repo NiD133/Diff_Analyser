bb8b2281bffd1dad7386ca5cb05aecc202d962bc
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:42:13 +0800

    gemini processedAutoTestCases charsequencereader_estest2.java

--- charsequencereader_estest2.java
+++ charsequencereader_estest2.java
@@ -1,24 +1,34 @@
 package com.google.common.io;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
+
 import java.io.IOException;
-import java.nio.CharBuffer;
-import java.nio.ReadOnlyBufferException;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class CharSequenceReader_ESTestTest2 extends CharSequenceReader_ESTest_scaffolding {
+/**
+ * Tests for {@link CharSequenceReader}.
+ */
+public class CharSequenceReaderTest {
 
-    @Test(timeout = 4000)
-    public void test01() throws Throwable {
-        char[] charArray0 = new char[6];
-        char[] charArray1 = new char[1];
-        CharBuffer charBuffer0 = CharBuffer.wrap(charArray1);
-        CharSequenceReader charSequenceReader0 = new CharSequenceReader(charBuffer0);
-        int int0 = charSequenceReader0.read(charArray0);
-        assertEquals(1, int0);
+    @Test
+    public void read_intoLargerBuffer_readsAllAvailableChars() throws IOException {
+        // Arrange
+        // Create a reader with a source sequence that is smaller than the destination buffer.
+        String sourceSequence = "abc";
+        CharSequenceReader reader = new CharSequenceReader(sourceSequence);
+        char[] destinationBuffer = new char[10];
+        
+        // Act
+        // Attempt to read from the source into the larger buffer.
+        int charsRead = reader.read(destinationBuffer);
+
+        // Assert
+        // Verify that the number of characters read is equal to the length of the source sequence.
+        assertEquals(sourceSequence.length(), charsRead);
+
+        // Also, verify that the content was correctly copied into the buffer.
+        char[] expectedBufferContent = new char[]{'a', 'b', 'c', '\0', '\0', '\0', '\0', '\0', '\0', '\0'};
+        assertArrayEquals(expectedBufferContent, destinationBuffer);
     }
 }
