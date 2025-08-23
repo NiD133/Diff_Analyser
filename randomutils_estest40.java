ab6bec0bbdcf58e8599007317e4ed5a85e6047d2
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:12:23 +0800

    Original processedAutoTestCases randomutils_estest40.java

---
+++ randomutils_estest40.java
@@ -0,0 +1,19 @@
+package org.apache.commons.lang3;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.security.SecureRandom;
+import java.util.Random;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class RandomUtils_ESTestTest40 extends RandomUtils_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test39() throws Throwable {
+        long long0 = RandomUtils.nextLong();
+        //  // Unstable assertion: assertEquals(1627130694281105876L, long0);
+    }
+}
