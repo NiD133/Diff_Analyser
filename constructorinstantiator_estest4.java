d891324ec5242416a117b2251d137a7b7b2e5a71
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:45:32 +0800

    gemini processedAutoTestCases constructorinstantiator_estest4.java

--- constructorinstantiator_estest4.java
+++ constructorinstantiator_estest4.java
@@ -1,24 +1,35 @@
 package org.mockito.internal.creation.instance;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import org.mockito.creation.instance.InstantiationException;
 
+/**
+ * This test class contains tests for {@link ConstructorInstantiator}.
+ * This particular test was improved for understandability.
+ */
 public class ConstructorInstantiator_ESTestTest4 extends ConstructorInstantiator_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test3() throws Throwable {
-        Object[] objectArray0 = new Object[1];
-        Class<Integer> class0 = Integer.class;
-        ConstructorInstantiator constructorInstantiator0 = new ConstructorInstantiator(true, objectArray0);
-        // Undeclared exception!
-        try {
-            constructorInstantiator0.newInstance(class0);
-            fail("Expecting exception: RuntimeException");
-        } catch (RuntimeException e) {
-        }
+    /**
+     * Verifies that if a suitable constructor is found, but its invocation throws an exception,
+     * the ConstructorInstantiator wraps and re-throws it as an InstantiationException.
+     *
+     * In this scenario, the instantiator will select the Integer(String) constructor.
+     * Calling `new Integer((String) null)` throws a NumberFormatException, which Mockito
+     * should wrap in its own InstantiationException.
+     */
+    @Test(expected = InstantiationException.class)
+    public void shouldThrowExceptionWhenConstructorInvocationFails() {
+        // Arrange
+        // We provide a single null argument. This will match the Integer(String) constructor.
+        Object[] constructorArgs = new Object[]{null};
+        Class<Integer> classToInstantiate = Integer.class;
+
+        // The 'hasOuterClassInstance' flag is not relevant here as Integer is not an inner class.
+        ConstructorInstantiator instantiator = new ConstructorInstantiator(false, constructorArgs);
+
+        // Act & Assert
+        // The newInstance call is expected to throw an InstantiationException because the
+        // underlying constructor call (new Integer(null)) will fail.
+        instantiator.newInstance(classToInstantiate);
     }
 }
