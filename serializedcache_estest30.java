b28796798f9a5b82ae66ea79767c04680b5ee4e3
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:14:56 +0800

    gemini processedAutoTestCases serializedcache_estest30.java

--- serializedcache_estest30.java
+++ serializedcache_estest30.java
@@ -1,34 +1,25 @@
 package org.apache.ibatis.cache.decorators;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.EOFException;
-import java.io.SequenceInputStream;
-import java.util.Enumeration;
 import org.apache.ibatis.cache.Cache;
-import org.apache.ibatis.cache.impl.PerpetualCache;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.evosuite.runtime.mock.java.io.MockFileInputStream;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class SerializedCache_ESTestTest30 extends SerializedCache_ESTest_scaffolding {
+/**
+ * Test suite for the SerializedCache decorator.
+ */
+public class SerializedCacheTest {
 
-    @Test(timeout = 4000)
-    public void test29() throws Throwable {
-        SerializedCache serializedCache0 = new SerializedCache((Cache) null);
-        // Undeclared exception!
-        try {
-            serializedCache0.equals((Object) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.ibatis.cache.decorators.SerializedCache", e);
-        }
+    /**
+     * Verifies that calling equals() on a SerializedCache initialized with a null delegate
+     * throws a NullPointerException. This behavior is expected because the equals() method
+     * delegates the call to the underlying cache instance.
+     */
+    @Test(expected = NullPointerException.class)
+    public void shouldThrowNpeOnEqualsWhenDelegateIsNull() {
+        // Arrange: Create a SerializedCache with a null delegate cache.
+        Cache serializedCache = new SerializedCache(null);
+
+        // Act & Assert: Calling equals() on this instance must throw a NullPointerException.
+        // The argument to equals() does not matter in this scenario.
+        serializedCache.equals(new Object());
     }
 }
