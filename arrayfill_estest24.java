c833c9755daace8b9bcdae43b0cc9b22f9c33ec3
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:37:30 +0800

    Original processedAutoTestCases arrayfill_estest24.java

---
+++ arrayfill_estest24.java
@@ -0,0 +1,20 @@
+package org.apache.commons.lang3;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.Arrays;
+import org.apache.commons.lang3.function.FailableIntFunction;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.lang.MockThrowable;
+import org.junit.runner.RunWith;
+
+public class ArrayFill_ESTestTest24 extends ArrayFill_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test23() throws Throwable {
+        float[] floatArray0 = ArrayFill.fill((float[]) null, (-4645.361F));
+        assertNull(floatArray0);
+    }
+}
