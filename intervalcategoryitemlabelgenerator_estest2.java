d05c95d8350d3b164187cd3365e49fa3dce43398
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:54:42 +0800

    Original processedAutoTestCases intervalcategoryitemlabelgenerator_estest2.java

---
+++ intervalcategoryitemlabelgenerator_estest2.java
@@ -0,0 +1,35 @@
+package org.jfree.chart.labels;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.text.DateFormat;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.time.chrono.ThaiBuddhistEra;
+import javax.swing.JLayeredPane;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.text.MockDateFormat;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.category.DefaultIntervalCategoryDataset;
+import org.jfree.data.statistics.DefaultStatisticalCategoryDataset;
+import org.junit.runner.RunWith;
+
+public class IntervalCategoryItemLabelGenerator_ESTestTest2 extends IntervalCategoryItemLabelGenerator_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test01() throws Throwable {
+        IntervalCategoryItemLabelGenerator intervalCategoryItemLabelGenerator0 = new IntervalCategoryItemLabelGenerator();
+        DefaultStatisticalCategoryDataset<ThaiBuddhistEra, ThaiBuddhistEra> defaultStatisticalCategoryDataset0 = new DefaultStatisticalCategoryDataset<ThaiBuddhistEra, ThaiBuddhistEra>();
+        ThaiBuddhistEra thaiBuddhistEra0 = ThaiBuddhistEra.BE;
+        Integer integer0 = JLayeredPane.DRAG_LAYER;
+        defaultStatisticalCategoryDataset0.add((Number) integer0, (Number) integer0, thaiBuddhistEra0, thaiBuddhistEra0);
+        // Undeclared exception!
+        try {
+            intervalCategoryItemLabelGenerator0.createItemArray(defaultStatisticalCategoryDataset0, 0, 1);
+            fail("Expecting exception: IndexOutOfBoundsException");
+        } catch (IndexOutOfBoundsException e) {
+        }
+    }
+}
