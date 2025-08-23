a49cb4a97e7bd623cb2fc4adcb7426b9cc6859e6
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:47:11 +0800

    gemini processedAutoTestCases defaultflowdataset_estest10.java

--- defaultflowdataset_estest10.java
+++ defaultflowdataset_estest10.java
@@ -2,27 +2,29 @@ package org.jfree.data.flow;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.time.chrono.HijrahEra;
-import java.util.List;
-import java.util.Set;
-import javax.swing.Icon;
-import javax.swing.JLayeredPane;
-import javax.swing.JRadioButtonMenuItem;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class DefaultFlowDataset_ESTestTest10 extends DefaultFlowDataset_ESTest_scaffolding {
+/**
+ * Tests for the {@link DefaultFlowDataset} class, focusing on node properties.
+ */
+public class DefaultFlowDatasetTest {
 
-    @Test(timeout = 4000)
-    public void test09() throws Throwable {
-        DefaultFlowDataset<Integer> defaultFlowDataset0 = new DefaultFlowDataset<Integer>();
-        Integer integer0 = JLayeredPane.POPUP_LAYER;
-        NodeKey<Integer> nodeKey0 = new NodeKey<Integer>(0, integer0);
-        JRadioButtonMenuItem jRadioButtonMenuItem0 = new JRadioButtonMenuItem((Icon) null);
-        defaultFlowDataset0.setNodeProperty(nodeKey0, "selected", jRadioButtonMenuItem0);
-        JRadioButtonMenuItem jRadioButtonMenuItem1 = (JRadioButtonMenuItem) defaultFlowDataset0.getNodeProperty(nodeKey0, "selected");
-        assertFalse(jRadioButtonMenuItem1.isFocusTraversalPolicySet());
+    /**
+     * Verifies that a property set on a node can be correctly retrieved.
+     */
+    @Test
+    public void setNodeProperty_shouldStoreAndRetrievePropertyValue() {
+        // Arrange
+        DefaultFlowDataset<Integer> dataset = new DefaultFlowDataset<>();
+        NodeKey<Integer> nodeKey = new NodeKey<>(0, 100); // A node at stage 0 with ID 100
+        String propertyKey = "status";
+        String expectedPropertyValue = "active";
+
+        // Act
+        dataset.setNodeProperty(nodeKey, propertyKey, expectedPropertyValue);
+        Object actualPropertyValue = dataset.getNodeProperty(nodeKey, propertyKey);
+
+        // Assert
+        assertSame("The retrieved property value should be the same instance that was set.",
+                expectedPropertyValue, actualPropertyValue);
     }
 }
