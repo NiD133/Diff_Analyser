be8b3f80ebd489a9374aa0c73d4f0ad35afcb1ee
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:23:04 +0800

    gemini processedAutoTestCases transformedbag_estest33.java

--- transformedbag_estest33.java
+++ transformedbag_estest33.java
@@ -1,59 +1,49 @@
 package org.apache.commons.collections4.bag;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Set;
-import java.util.Vector;
-import org.apache.commons.collections4.Bag;
-import org.apache.commons.collections4.Factory;
-import org.apache.commons.collections4.Predicate;
 import org.apache.commons.collections4.SortedBag;
 import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AndPredicate;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ChainedTransformer;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantFactory;
 import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.FactoryTransformer;
-import org.apache.commons.collections4.functors.FalsePredicate;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.InstantiateFactory;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.MapTransformer;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NotPredicate;
-import org.apache.commons.collections4.functors.OnePredicate;
-import org.apache.commons.collections4.functors.SwitchTransformer;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class TransformedBag_ESTestTest33 extends TransformedBag_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
-    @Test(timeout = 4000)
-    public void test32() throws Throwable {
-        HashBag<Integer> hashBag0 = new HashBag<Integer>();
-        Integer integer0 = new Integer(165);
-        ConstantFactory<Integer> constantFactory0 = new ConstantFactory<Integer>(integer0);
-        FactoryTransformer<Object, Integer> factoryTransformer0 = new FactoryTransformer<Object, Integer>(constantFactory0);
-        TreeBag<Integer> treeBag0 = new TreeBag<Integer>((Collection<? extends Integer>) hashBag0);
-        TransformedSortedBag<Integer> transformedSortedBag0 = TransformedSortedBag.transformingSortedBag((SortedBag<Integer>) treeBag0, (Transformer<? super Integer, ? extends Integer>) factoryTransformer0);
-        TransformedSortedBag<Integer> transformedSortedBag1 = TransformedSortedBag.transformedSortedBag((SortedBag<Integer>) transformedSortedBag0, (Transformer<? super Integer, ? extends Integer>) factoryTransformer0);
-        boolean boolean0 = transformedSortedBag1.equals(transformedSortedBag0);
-        assertTrue(boolean0);
+/**
+ * Contains tests for the equals() method of TransformedBag and its subclasses.
+ */
+public class TransformedBagEqualsTest {
+
+    /**
+     * Tests that the equals() method returns true when comparing two different
+     * but empty TransformedSortedBag instances.
+     *
+     * This test verifies that equality is correctly determined even when one
+     * transformed bag is created by decorating another already transformed bag.
+     */
+    @Test
+    public void testEqualsReturnsTrueForTwoEmptyAndEqualTransformedBags() {
+        // Arrange
+        // A transformer that converts any input to a constant value.
+        // This transformer is required for instantiation but will not be invoked
+        // as the bags remain empty throughout the test.
+        final Transformer<Integer, Integer> transformer = ConstantTransformer.constantTransformer(165);
+
+        // Create the first empty transformed bag.
+        final SortedBag<Integer> firstTransformedBag =
+                TransformedSortedBag.transformingSortedBag(new TreeBag<>(), transformer);
+
+        // Create a second transformed bag by decorating the first one.
+        final SortedBag<Integer> secondTransformedBag =
+                TransformedSortedBag.transformedSortedBag(firstTransformedBag, transformer);
+
+        // Act & Assert
+        // The two bags should be considered equal because they are both empty.
+        // The equals() method on the decorated collection should delegate to the
+        // underlying collection's equals() method, which will find them equal.
+        boolean areEqual = secondTransformedBag.equals(firstTransformedBag);
+        assertTrue("Two empty transformed bags should be equal", areEqual);
+
+        // A more idiomatic assertion using assertEquals provides a better failure message.
+        assertEquals("The two bags should be equal", firstTransformedBag, secondTransformedBag);
     }
 }
