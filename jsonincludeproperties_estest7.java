0a0a607eeb6c731ea19c3d61d31f92cc65a7922e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:56:50 +0800

    gemini processedAutoTestCases jsonincludeproperties_estest7.java

--- jsonincludeproperties_estest7.java
+++ jsonincludeproperties_estest7.java
@@ -1,21 +1,24 @@
 package com.fasterxml.jackson.annotation;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import java.util.LinkedHashSet;
-import java.util.Set;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class JsonIncludeProperties_ESTestTest7 extends JsonIncludeProperties_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link JsonIncludeProperties.Value} class.
+ */
+public class JsonIncludePropertiesValueTest {
 
-    @Test(timeout = 4000)
-    public void test06() throws Throwable {
-        JsonIncludeProperties.Value jsonIncludeProperties_Value0 = JsonIncludeProperties.Value.all();
-        boolean boolean0 = jsonIncludeProperties_Value0.equals(jsonIncludeProperties_Value0);
-        assertTrue(boolean0);
+    /**
+     * Verifies the reflexive property of the equals() method.
+     * An object must always be equal to itself.
+     */
+    @Test
+    public void shouldBeEqualToItself() {
+        // Arrange
+        JsonIncludeProperties.Value allPropertiesValue = JsonIncludeProperties.Value.all();
+
+        // Act & Assert
+        // According to the equals() contract, an object must be equal to itself.
+        assertEquals(allPropertiesValue, allPropertiesValue);
     }
 }
