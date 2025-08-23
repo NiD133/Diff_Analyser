da25154df2a140c78369dadd310dacf348e41858
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:01:48 +0800

    Original processedAutoTestCases lzmautils_estest15.java

---
+++ lzmautils_estest15.java
@@ -0,0 +1,18 @@
+package org.apache.commons.compress.compressors.lzma;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class LZMAUtils_ESTestTest15 extends LZMAUtils_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test14() throws Throwable {
+        byte[] byteArray0 = new byte[4];
+        boolean boolean0 = LZMAUtils.matches(byteArray0, 117);
+        assertFalse(boolean0);
+    }
+}
