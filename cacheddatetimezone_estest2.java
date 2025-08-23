a2b7108a5cde993d4d593001bebee27909e4a05d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:41:18 +0800

    gemini processedAutoTestCases cacheddatetimezone_estest2.java

--- cacheddatetimezone_estest2.java
+++ cacheddatetimezone_estest2.java
@@ -1,23 +1,38 @@
 package org.joda.time.tz;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.joda.time.Chronology;
+import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
-import org.joda.time.Instant;
-import org.joda.time.LocalDateTime;
-import org.joda.time.chrono.GregorianChronology;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Test suite for {@link CachedDateTimeZone}.
+ * This test focuses on verifying the behavior of time zones that are expected to be cached.
+ */
 public class CachedDateTimeZone_ESTestTest2 extends CachedDateTimeZone_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test01() throws Throwable {
-        DateTimeZone dateTimeZone0 = DateTimeZone.forID("WET");
-        String string0 = dateTimeZone0.getName(686653601480704L);
-        assertEquals("Western European Time", string0);
+    /**
+     * Tests that getName() for a non-fixed time zone returns the correct standard time name
+     * for an instant that falls during the winter.
+     */
+    @Test
+    public void getNameForWinterInstantShouldReturnStandardTimeName() {
+        // Arrange
+        // The "WET" zone (Western European Time) is not fixed due to its daylight saving
+        // variant, "WEST". Non-fixed zones are wrapped in a CachedDateTimeZone by default.
+        DateTimeZone wetZone = DateTimeZone.forID("WET");
+
+        // An instant during winter, when standard time (WET) is in effect.
+        // Using a concrete date makes the test's intent clear.
+        long winterInstant = new DateTime(2023, 1, 15, 12, 0, DateTimeZone.UTC).getMillis();
+
+        String expectedName = "Western European Time";
+
+        // Act
+        String actualName = wetZone.getName(winterInstant);
+
+        // Assert
+        assertEquals(expectedName, actualName);
     }
 }
