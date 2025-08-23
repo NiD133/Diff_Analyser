cdc25a30f35b2d8433b1eebc77d59a47859b7a10
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:35:55 +0800

    Original processedManualTestCases weekstest5.java

---
+++ weekstest5.java
@@ -0,0 +1,41 @@
+package org.joda.time;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class WeeksTestTest5 extends TestCase {
+
+    // (before the late 90's they were all over the place)
+    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
+
+    public static TestSuite suite() {
+        return new TestSuite(TestWeeks.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+    }
+
+    public void testFactory_weeksIn_RInterval() {
+        DateTime start = new DateTime(2006, 6, 9, 12, 0, 0, 0, PARIS);
+        DateTime end1 = new DateTime(2006, 6, 30, 12, 0, 0, 0, PARIS);
+        DateTime end2 = new DateTime(2006, 7, 21, 12, 0, 0, 0, PARIS);
+        assertEquals(0, Weeks.weeksIn((ReadableInterval) null).getWeeks());
+        assertEquals(3, Weeks.weeksIn(new Interval(start, end1)).getWeeks());
+        assertEquals(0, Weeks.weeksIn(new Interval(start, start)).getWeeks());
+        assertEquals(0, Weeks.weeksIn(new Interval(end1, end1)).getWeeks());
+        assertEquals(6, Weeks.weeksIn(new Interval(start, end2)).getWeeks());
+    }
+}
