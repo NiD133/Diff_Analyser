79d498a528f1865c50758fd9c50c49da69e9facb
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:27:07 +0800

    gemini processedAutoTestCases weeks_estest19.java

--- weeks_estest19.java
+++ weeks_estest19.java
@@ -2,17 +2,26 @@ package org.joda.time;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class Weeks_ESTestTest19 extends Weeks_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link Weeks} class.
+ */
+public class WeeksTest {
 
-    @Test(timeout = 4000)
-    public void test18() throws Throwable {
-        Weeks weeks0 = Weeks.parseWeeks((String) null);
-        Weeks weeks1 = weeks0.multipliedBy((-1060));
-        assertEquals(0, weeks1.getWeeks());
+    @Test
+    public void multiplyingZeroWeeksByAnyNumber_shouldReturnZeroWeeks() {
+        // Arrange
+        // The canonical representation of zero weeks.
+        Weeks zeroWeeks = Weeks.ZERO;
+        int multiplier = -1060;
+
+        // Act
+        // Multiply zero weeks by a non-zero number.
+        Weeks result = zeroWeeks.multipliedBy(multiplier);
+
+        // Assert
+        // The result of 0 * x should always be 0.
+        assertEquals("Multiplying zero weeks by any number should result in zero weeks.",
+                Weeks.ZERO, result);
     }
 }
