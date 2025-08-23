07ed89325bd5df9f131988ed088f694a939753c5
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:23:10 +0800

    gemini processedAutoTestCases transformedmultivaluedmap_estest1.java

--- transformedmultivaluedmap_estest1.java
+++ transformedmultivaluedmap_estest1.java
@@ -1,60 +1,58 @@
 package org.apache.commons.collections4.multimap;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.AbstractMap;
-import java.util.ArrayDeque;
-import java.util.Comparator;
-import java.util.ConcurrentModificationException;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
-import java.util.PriorityQueue;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.Factory;
 import org.apache.commons.collections4.MultiValuedMap;
-import org.apache.commons.collections4.Predicate;
 import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AllPredicate;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ChainedTransformer;
-import org.apache.commons.collections4.functors.CloneTransformer;
-import org.apache.commons.collections4.functors.ClosureTransformer;
-import org.apache.commons.collections4.functors.ConstantFactory;
 import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.ExceptionFactory;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.FactoryTransformer;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.MapTransformer;
-import org.apache.commons.collections4.functors.NOPTransformer;
-import org.apache.commons.collections4.functors.NotPredicate;
-import org.apache.commons.collections4.functors.NullIsExceptionPredicate;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.SwitchTransformer;
-import org.apache.commons.collections4.functors.TransformerClosure;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
+import java.util.Collection;
+import java.util.Collections;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Test suite for {@link TransformedMultiValuedMap}.
+ */
 public class TransformedMultiValuedMap_ESTestTest1 extends TransformedMultiValuedMap_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test00() throws Throwable {
-        Integer integer0 = new Integer(1270);
-        ConstantTransformer<Object, Integer> constantTransformer0 = new ConstantTransformer<Object, Integer>(integer0);
-        ArrayDeque<Integer> arrayDeque0 = new ArrayDeque<Integer>(2934);
-        ArrayListValuedLinkedHashMap<Integer, Object> arrayListValuedLinkedHashMap0 = new ArrayListValuedLinkedHashMap<Integer, Object>(2934, 1270);
-        TransformedMultiValuedMap<Integer, Object> transformedMultiValuedMap0 = new TransformedMultiValuedMap<Integer, Object>(arrayListValuedLinkedHashMap0, constantTransformer0, constantTransformer0);
-        arrayDeque0.offerFirst(integer0);
-        transformedMultiValuedMap0.putAll(integer0, (Iterable<?>) arrayDeque0);
-        transformedMultiValuedMap0.putAll((Integer) null, (Iterable<?>) arrayDeque0);
-        TransformedMultiValuedMap.transformedMap((MultiValuedMap<Integer, Object>) arrayListValuedLinkedHashMap0, (Transformer<? super Integer, ? extends Integer>) constantTransformer0, (Transformer<? super Object, ?>) constantTransformer0);
-        assertFalse(arrayListValuedLinkedHashMap0.isEmpty());
+    /**
+     * Tests that putAll() correctly transforms both keys and values before adding them
+     * to the underlying map.
+     */
+    @Test
+    public void testPutAllTransformsKeysAndValues() {
+        // Arrange: Set up a map that transforms all keys and values to a constant integer.
+        final Integer transformedValue = 1270;
+        final Transformer<Object, Integer> constantTransformer = new ConstantTransformer<>(transformedValue);
+
+        // The map that will be decorated and will hold the transformed data.
+        final MultiValuedMap<Integer, Object> underlyingMap = new ArrayListValuedLinkedHashMap<>();
+
+        // The decorator map under test.
+        // Any key/value put into this map will be transformed by the constantTransformer.
+        final TransformedMultiValuedMap<Integer, Object> transformedMap =
+                new TransformedMultiValuedMap<>(underlyingMap, constantTransformer, constantTransformer);
+
+        // The values to be added. The actual content (e.g., 999) doesn't matter
+        // as the transformer will convert it to 1270.
+        final Collection<Integer> valuesToAdd = Collections.singletonList(999);
+
+        // Act: Add entries using different keys. Both keys should be transformed to 1270.
+        transformedMap.putAll(1, valuesToAdd);
+        transformedMap.putAll(null, valuesToAdd);
+
+        // Assert: Verify that the underlying map contains the transformed keys and values.
+        // We expect a single key (1270) with two transformed values (both 1270).
+        assertEquals("The underlying map should contain only one key after transformation.", 1, underlyingMap.keySet().size());
+        assertTrue("The key in the underlying map should be the transformed value.", underlyingMap.containsKey(transformedValue));
+
+        final Collection<Object> resultingValues = underlyingMap.get(transformedValue);
+        assertEquals("There should be two values associated with the transformed key.", 2, resultingValues.size());
+
+        // Check that both values were correctly transformed.
+        for (final Object value : resultingValues) {
+            assertEquals("Each value should have been converted to the transformed value.", transformedValue, value);
+        }
     }
 }
