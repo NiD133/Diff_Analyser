495e4225db3eb37089899c1a45844b307a0200f0
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:55:59 +0800

    Original processedAutoTestCases jsonarray_estest19.java

---
+++ jsonarray_estest19.java
@@ -0,0 +1,24 @@
+package com.google.gson;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Iterator;
+import java.util.List;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class JsonArray_ESTestTest19 extends JsonArray_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test18() throws Throwable {
+        JsonArray jsonArray0 = new JsonArray();
+        Float float0 = new Float(0.0);
+        jsonArray0.add((Number) float0);
+        char char0 = jsonArray0.getAsCharacter();
+        assertEquals('j', char0);
+    }
+}
