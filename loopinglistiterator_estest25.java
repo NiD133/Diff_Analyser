84df53ca08e20229d9083cb0ede3fce3b875902e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:01:40 +0800

    gemini processedAutoTestCases loopinglistiterator_estest25.java

--- loopinglistiterator_estest25.java
+++ loopinglistiterator_estest25.java
@@ -1,27 +1,32 @@
 package org.apache.commons.collections4.iterators;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.ConcurrentModificationException;
+import static org.junit.Assert.assertNull;
+
 import java.util.LinkedList;
 import java.util.List;
-import java.util.NoSuchElementException;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class LoopingListIterator_ESTestTest25 extends LoopingListIterator_ESTest_scaffolding {
+/**
+ * Contains tests for the {@link LoopingListIterator}.
+ */
+public class LoopingListIteratorTest {
 
-    @Test(timeout = 4000)
-    public void test24() throws Throwable {
-        LinkedList<Integer> linkedList0 = new LinkedList<Integer>();
-        linkedList0.add((Integer) null);
-        LoopingListIterator<Integer> loopingListIterator0 = new LoopingListIterator<Integer>(linkedList0);
-        Integer integer0 = loopingListIterator0.next();
-        assertNull(integer0);
+    /**
+     * Tests that the iterator can correctly handle and return a null element
+     * from the underlying list.
+     */
+    @Test
+    public void testNextShouldReturnNullWhenListContainsNull() {
+        // Arrange: Create a list containing a single null element.
+        final List<Integer> listWithNull = new LinkedList<>();
+        listWithNull.add(null);
+        
+        final LoopingListIterator<Integer> iterator = new LoopingListIterator<>(listWithNull);
+
+        // Act: Retrieve the first element from the iterator.
+        final Integer firstElement = iterator.next();
+
+        // Assert: The retrieved element should be the null that was added.
+        assertNull("The iterator should return the null element present in the list.", firstElement);
     }
 }
