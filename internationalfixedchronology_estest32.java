183b3c95ae20f72031c26677674292818376773a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:54:25 +0800

    Original processedAutoTestCases internationalfixedchronology_estest32.java

---
+++ internationalfixedchronology_estest32.java
@@ -0,0 +1,59 @@
+package org.threeten.extra.chrono;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.time.Clock;
+import java.time.DateTimeException;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.Period;
+import java.time.Year;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.chrono.ChronoLocalDateTime;
+import java.time.chrono.ChronoZonedDateTime;
+import java.time.chrono.Era;
+import java.time.chrono.IsoEra;
+import java.time.chrono.JapaneseEra;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAccessor;
+import java.time.temporal.TemporalAmount;
+import java.time.temporal.UnsupportedTemporalTypeException;
+import java.time.temporal.ValueRange;
+import java.util.List;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.System;
+import org.evosuite.runtime.mock.java.time.MockClock;
+import org.evosuite.runtime.mock.java.time.MockInstant;
+import org.evosuite.runtime.mock.java.time.MockLocalDateTime;
+import org.evosuite.runtime.mock.java.time.MockYear;
+import org.evosuite.runtime.mock.java.time.MockZonedDateTime;
+import org.junit.runner.RunWith;
+
+public class InternationalFixedChronology_ESTestTest32 extends InternationalFixedChronology_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test31() throws Throwable {
+        InternationalFixedChronology internationalFixedChronology0 = InternationalFixedChronology.INSTANCE;
+        Clock clock0 = MockClock.systemDefaultZone();
+        ChronoUnit chronoUnit0 = ChronoUnit.ERAS;
+        Duration duration0 = chronoUnit0.getDuration();
+        Clock clock1 = MockClock.offset(clock0, duration0);
+        // Undeclared exception!
+        try {
+            internationalFixedChronology0.dateNow(clock1);
+            fail("Expecting exception: DateTimeException");
+        } catch (DateTimeException e) {
+            //
+            // Instant exceeds minimum or maximum instant
+            //
+            verifyException("java.time.Instant", e);
+        }
+    }
+}
