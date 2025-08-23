9ab943e6f2c199a8f1d2a02753c6615c0d7ef2df
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:53:22 +0800

    gemini processedAutoTestCases hostspecifier_estest16.java

--- hostspecifier_estest16.java
+++ hostspecifier_estest16.java
@@ -1,18 +1,30 @@
 package com.google.common.net;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
 import java.text.ParseException;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class HostSpecifier_ESTestTest16 extends HostSpecifier_ESTest_scaffolding {
+/**
+ * Tests for {@link HostSpecifier}.
+ */
+public class HostSpecifierTest {
 
-    @Test(timeout = 4000)
-    public void test15() throws Throwable {
-        HostSpecifier hostSpecifier0 = HostSpecifier.from("127.0.0.1");
-        hostSpecifier0.hashCode();
+    /**
+     * Verifies that two equal HostSpecifier instances produce the same hash code,
+     * as required by the Object.hashCode() contract.
+     */
+    @Test
+    public void hashCode_forEqualInstances_returnsSameValue() throws ParseException {
+        // Arrange: Create two separate HostSpecifier instances from the same valid IP address string.
+        // These two objects should be considered equal.
+        HostSpecifier specifier1 = HostSpecifier.from("127.0.0.1");
+        HostSpecifier specifier2 = HostSpecifier.from("127.0.0.1");
+
+        // Act & Assert:
+        // First, confirm that the instances are indeed equal to each other.
+        assertEquals("Instances created from the same string should be equal", specifier1, specifier2);
+
+        // Then, assert that their hash codes are also equal, fulfilling the contract.
+        assertEquals("Equal instances must have equal hash codes", specifier1.hashCode(), specifier2.hashCode());
     }
 }
