9c9ed0442070522a2339559a4c440d942bbcb8bf
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:54:10 +0800

    gemini processedAutoTestCases indexedcollection_estest63.java

--- indexedcollection_estest63.java
+++ indexedcollection_estest63.java
@@ -1,57 +1,43 @@
 package org.apache.commons.collections4.collection;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.ConcurrentModificationException;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Set;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.Predicate;
 import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AllPredicate;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ChainedTransformer;
-import org.apache.commons.collections4.functors.CloneTransformer;
-import org.apache.commons.collections4.functors.ClosureTransformer;
-import org.apache.commons.collections4.functors.ConstantFactory;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.DefaultEquator;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.FactoryTransformer;
-import org.apache.commons.collections4.functors.FalsePredicate;
-import org.apache.commons.collections4.functors.ForClosure;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.NOPClosure;
-import org.apache.commons.collections4.functors.NOPTransformer;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NotNullPredicate;
-import org.apache.commons.collections4.functors.NullIsFalsePredicate;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.SwitchTransformer;
-import org.apache.commons.collections4.functors.TransformedPredicate;
-import org.apache.commons.collections4.functors.TransformerClosure;
-import org.apache.commons.collections4.functors.TransformerPredicate;
-import org.apache.commons.collections4.functors.TruePredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+
+import static org.junit.Assert.assertNull;
+
+/**
+ * This test class contains an improved version of a test from IndexedCollection_ESTestTest63.
+ */
 public class IndexedCollection_ESTestTest63 extends IndexedCollection_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test62() throws Throwable {
-        Transformer<Object, Object> transformer0 = ConstantTransformer.nullTransformer();
-        LinkedList<Object> linkedList0 = new LinkedList<Object>();
-        IndexedCollection<Object, Object> indexedCollection0 = IndexedCollection.uniqueIndexedCollection((Collection<Object>) linkedList0, transformer0);
-        Collection<Object> collection0 = indexedCollection0.values(transformer0);
-        assertNull(collection0);
+    /**
+     * Tests that the values() method returns null when called with a key that does not exist in the index.
+     * The original test was functionally correct but used confusing inputs, making its purpose unclear.
+     */
+    @Test
+    public void valuesShouldReturnNullForNonExistentKey() {
+        // Arrange
+        // A transformer that uses an object's string representation as the key.
+        final Transformer<Object, String> keyTransformer = Object::toString;
+
+        // An empty source collection, ensuring no keys are present in the index.
+        final Collection<Object> sourceCollection = new ArrayList<>();
+
+        final IndexedCollection<String, Object> indexedCollection =
+                IndexedCollection.uniqueIndexedCollection(sourceCollection, keyTransformer);
+
+        final String nonExistentKey = "a key that is guaranteed not to be in the index";
+
+        // Act
+        // Attempt to retrieve values for the non-existent key.
+        final Collection<Object> result = indexedCollection.values(nonExistentKey);
+
+        // Assert
+        // The Javadoc for values(K) states it should return null if the key is not found.
+        assertNull("Expected null when retrieving values for a non-existent key", result);
     }
 }
