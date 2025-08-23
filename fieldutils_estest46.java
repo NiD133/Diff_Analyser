463585caf191e96d7eb3532532f98014eb3a883b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:50:25 +0800

    Original processedAutoTestCases fieldutils_estest46.java

---
+++ fieldutils_estest46.java
@@ -0,0 +1,31 @@
+package org.joda.time.field;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.math.RoundingMode;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.joda.time.DateTimeField;
+import org.joda.time.DateTimeFieldType;
+import org.joda.time.DateTimeZone;
+import org.joda.time.chrono.IslamicChronology;
+import org.joda.time.chrono.ZonedChronology;
+import org.junit.runner.RunWith;
+
+public class FieldUtils_ESTestTest46 extends FieldUtils_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test45() throws Throwable {
+        // Undeclared exception!
+        try {
+            FieldUtils.getWrappedValue(697, 697, 697);
+            fail("Expecting exception: IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //
+            // MIN > MAX
+            //
+            verifyException("org.joda.time.field.FieldUtils", e);
+        }
+    }
+}
