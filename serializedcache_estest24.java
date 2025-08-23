0d49bf25fc7c41b04fc84db4840f28bffd153e2e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:14:53 +0800

    gemini processedAutoTestCases serializedcache_estest24.java

--- serializedcache_estest24.java
+++ serializedcache_estest24.java
@@ -1,34 +1,29 @@
 package org.apache.ibatis.cache.decorators;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.EOFException;
-import java.io.SequenceInputStream;
-import java.util.Enumeration;
 import org.apache.ibatis.cache.Cache;
-import org.apache.ibatis.cache.impl.PerpetualCache;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.evosuite.runtime.mock.java.io.MockFileInputStream;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class SerializedCache_ESTestTest24 extends SerializedCache_ESTest_scaffolding {
+import static org.junit.Assert.assertThrows;
 
-    @Test(timeout = 4000)
-    public void test23() throws Throwable {
-        SerializedCache serializedCache0 = new SerializedCache((Cache) null);
-        // Undeclared exception!
-        try {
-            serializedCache0.hashCode();
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.ibatis.cache.decorators.SerializedCache", e);
-        }
+/**
+ * Test suite for the {@link SerializedCache} decorator.
+ */
+public class SerializedCacheTest {
+
+    /**
+     * Verifies that calling hashCode() on a SerializedCache with a null delegate
+     * throws a NullPointerException. This is expected because the decorator should
+     * forward the call to its delegate.
+     */
+    @Test
+    public void hashCodeShouldThrowNullPointerExceptionWhenDelegateIsNull() {
+        // Arrange: Create a SerializedCache with a null delegate cache.
+        // The 'delegate' field in SerializedCache will be null.
+        Cache serializedCache = new SerializedCache(null);
+
+        // Act & Assert: Verify that calling hashCode() on this instance
+        // throws a NullPointerException. The lambda expression concisely
+        // represents the action that is expected to fail.
+        assertThrows(NullPointerException.class, serializedCache::hashCode);
     }
 }
