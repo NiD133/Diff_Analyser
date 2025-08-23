d093174f342770f8df9c120cea1252dda6ee9499
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:23:01 +0800

    gemini processedAutoTestCases transformedbag_estest28.java

--- transformedbag_estest28.java
+++ transformedbag_estest28.java
@@ -1,67 +1,62 @@
 package org.apache.commons.collections4.bag;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Set;
-import java.util.Vector;
 import org.apache.commons.collections4.Bag;
-import org.apache.commons.collections4.Factory;
 import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.SortedBag;
 import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AndPredicate;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ChainedTransformer;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantFactory;
 import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.FactoryTransformer;
-import org.apache.commons.collections4.functors.FalsePredicate;
 import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.InstantiateFactory;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.MapTransformer;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NotPredicate;
-import org.apache.commons.collections4.functors.OnePredicate;
-import org.apache.commons.collections4.functors.SwitchTransformer;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class TransformedBag_ESTestTest28 extends TransformedBag_ESTest_scaffolding {
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
-    @Test(timeout = 4000)
-    public void test27() throws Throwable {
-        HashBag<Integer> hashBag0 = new HashBag<Integer>();
-        TreeBag<Object> treeBag0 = new TreeBag<Object>((Iterable<?>) hashBag0);
-        Predicate<Object> predicate0 = IdentityPredicate.identityPredicate((Object) hashBag0);
-        PredicatedSortedBag<Object> predicatedSortedBag0 = PredicatedSortedBag.predicatedSortedBag((SortedBag<Object>) treeBag0, (Predicate<? super Object>) predicate0);
-        Integer integer0 = new Integer(1480);
-        Transformer<Object, Integer> transformer0 = ConstantTransformer.constantTransformer(integer0);
-        TransformedSortedBag<Object> transformedSortedBag0 = TransformedSortedBag.transformedSortedBag((SortedBag<Object>) predicatedSortedBag0, (Transformer<? super Object, ?>) transformer0);
-        // Undeclared exception!
+/**
+ * Tests for {@link TransformedBag}.
+ * This class was improved for understandability from an auto-generated test.
+ */
+public class TransformedBag_ESTestTest28 {
+
+    /**
+     * Tests that adding an element to a TransformedBag propagates the IllegalArgumentException
+     * thrown by the decorated bag if its predicate rejects the addition.
+     */
+    @Test
+    public void addShouldThrowExceptionWhenDecoratedBagRejectsElement() {
+        // Arrange
+        // 1. Create a predicate that will reject the element we intend to add.
+        // The IdentityPredicate checks for object identity (==). It is configured to only
+        // accept a specific 'sentinel' object, thus rejecting all other objects.
+        final Object predicateSentinel = new Object();
+        final Predicate<Object> rejectingPredicate = IdentityPredicate.identityPredicate(predicateSentinel);
+
+        // 2. Create a bag decorated with this predicate. Any attempt to add an object
+        // other than 'predicateSentinel' will result in an IllegalArgumentException.
+        final Bag<Object> predicatedBag = PredicatedBag.predicatedBag(new HashBag<>(), rejectingPredicate);
+
+        // 3. Define the object that the transformer will produce. This is the object
+        // that will be passed to the predicatedBag and subsequently rejected.
+        final Integer transformedElement = 1480;
+        final Transformer<Object, Object> transformer = ConstantTransformer.constantTransformer(transformedElement);
+
+        // 4. Create the TransformedBag under test, decorating the predicatedBag.
+        final Bag<Object> transformedBag = TransformedBag.transformingBag(predicatedBag, transformer);
+
+        final String elementToAdd = "any element";
+        final int numberOfCopies = 39;
+
+        // Act & Assert
         try {
-            transformedSortedBag0.add((Object) integer0, 39);
-            fail("Expecting exception: IllegalArgumentException");
-        } catch (IllegalArgumentException e) {
-            //
-            // Cannot add Object '1480' - Predicate 'org.apache.commons.collections4.functors.IdentityPredicate@4' rejected it
-            //
-            verifyException("org.apache.commons.collections4.collection.PredicatedCollection", e);
+            // Attempt to add an element. The transformer will convert 'elementToAdd' to 'transformedElement' (1480).
+            // The underlying predicatedBag will then reject 1480 because it does not match the predicate's sentinel object.
+            transformedBag.add(elementToAdd, numberOfCopies);
+            fail("Expected an IllegalArgumentException because the underlying predicate should reject the element.");
+        } catch (final IllegalArgumentException e) {
+            // Verify that the exception was thrown for the correct reason.
+            final String expectedMessageFragment = "Cannot add Object '" + transformedElement + "' - Predicate";
+            assertTrue(
+                "The exception message should indicate that the transformed element was rejected by the predicate.",
+                e.getMessage().startsWith(expectedMessageFragment)
+            );
         }
     }
 }
