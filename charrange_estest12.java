98ba174ca43e4395ad247deff611c39626da563c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:41:58 +0800

    gemini processedAutoTestCases charrange_estest12.java

--- charrange_estest12.java
+++ charrange_estest12.java
@@ -1,29 +1,20 @@
 package org.apache.commons.lang3;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.function.Consumer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
 
-public class CharRange_ESTestTest12 extends CharRange_ESTest_scaffolding {
+/**
+ * Tests for {@link CharRange#contains(CharRange)}.
+ */
+public class CharRangeTest {
 
-    @Test(timeout = 4000)
-    public void test11() throws Throwable {
-        CharRange charRange0 = CharRange.is('5');
-        // Undeclared exception!
-        try {
-            charRange0.contains((CharRange) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // range
-            //
-            verifyException("java.util.Objects", e);
-        }
+    @Test(expected = NullPointerException.class)
+    public void containsRangeShouldThrowNullPointerExceptionForNullInput() {
+        // Arrange
+        final CharRange range = CharRange.is('a');
+
+        // Act
+        range.contains((CharRange) null);
+
+        // Assert: The @Test(expected) annotation handles the assertion.
     }
 }
