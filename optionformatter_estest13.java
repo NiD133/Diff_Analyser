26aee8b86403d3b5b72581ac4bec5e366391641a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:06:21 +0800

    Original processedAutoTestCases optionformatter_estest13.java

---
+++ optionformatter_estest13.java
@@ -0,0 +1,30 @@
+package org.apache.commons.cli.help;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.function.BiFunction;
+import org.apache.commons.cli.Option;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class OptionFormatter_ESTestTest13 extends OptionFormatter_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test12() throws Throwable {
+        OptionFormatter optionFormatter0 = OptionFormatter.from((Option) null);
+        // Undeclared exception!
+        try {
+            optionFormatter0.getOpt();
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.apache.commons.cli.help.OptionFormatter", e);
+        }
+    }
+}
