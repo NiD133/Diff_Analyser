3016fd201f4c6d58eee8030009c2e3b180f7320a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:23:22 +0800

    Original processedAutoTestCases transformedmultivaluedmap_estest4.java

---
+++ transformedmultivaluedmap_estest4.java
@@ -0,0 +1,58 @@
+package org.apache.commons.collections4.multimap;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.lang.reflect.Array;
+import java.util.AbstractMap;
+import java.util.ArrayDeque;
+import java.util.Comparator;
+import java.util.ConcurrentModificationException;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.PriorityQueue;
+import org.apache.commons.collections4.Closure;
+import org.apache.commons.collections4.Factory;
+import org.apache.commons.collections4.MultiValuedMap;
+import org.apache.commons.collections4.Predicate;
+import org.apache.commons.collections4.Transformer;
+import org.apache.commons.collections4.functors.AllPredicate;
+import org.apache.commons.collections4.functors.AnyPredicate;
+import org.apache.commons.collections4.functors.ChainedTransformer;
+import org.apache.commons.collections4.functors.CloneTransformer;
+import org.apache.commons.collections4.functors.ClosureTransformer;
+import org.apache.commons.collections4.functors.ConstantFactory;
+import org.apache.commons.collections4.functors.ConstantTransformer;
+import org.apache.commons.collections4.functors.ExceptionFactory;
+import org.apache.commons.collections4.functors.ExceptionTransformer;
+import org.apache.commons.collections4.functors.FactoryTransformer;
+import org.apache.commons.collections4.functors.IfTransformer;
+import org.apache.commons.collections4.functors.InvokerTransformer;
+import org.apache.commons.collections4.functors.MapTransformer;
+import org.apache.commons.collections4.functors.NOPTransformer;
+import org.apache.commons.collections4.functors.NotPredicate;
+import org.apache.commons.collections4.functors.NullIsExceptionPredicate;
+import org.apache.commons.collections4.functors.NullPredicate;
+import org.apache.commons.collections4.functors.SwitchTransformer;
+import org.apache.commons.collections4.functors.TransformerClosure;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class TransformedMultiValuedMap_ESTestTest4 extends TransformedMultiValuedMap_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test03() throws Throwable {
+        Transformer<Integer, Integer> transformer0 = NOPTransformer.nopTransformer();
+        HashMap<Integer, Boolean> hashMap0 = new HashMap<Integer, Boolean>();
+        ArrayListValuedHashMap<Integer, Boolean> arrayListValuedHashMap0 = new ArrayListValuedHashMap<Integer, Boolean>(hashMap0);
+        LinkedHashSetValuedLinkedHashMap<Integer, Boolean> linkedHashSetValuedLinkedHashMap0 = new LinkedHashSetValuedLinkedHashMap<Integer, Boolean>(arrayListValuedHashMap0);
+        Transformer<Boolean, Boolean> transformer1 = CloneTransformer.cloneTransformer();
+        TransformedMultiValuedMap<Integer, Boolean> transformedMultiValuedMap0 = TransformedMultiValuedMap.transformedMap((MultiValuedMap<Integer, Boolean>) linkedHashSetValuedLinkedHashMap0, (Transformer<? super Integer, ? extends Integer>) transformer0, (Transformer<? super Boolean, ? extends Boolean>) transformer1);
+        Boolean boolean0 = transformedMultiValuedMap0.transformValue((Boolean) null);
+        assertNull(boolean0);
+    }
+}
