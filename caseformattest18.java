11cf9b34c8ad4e05a53afb233c39de2c48c71360
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:26:21 +0800

    gemini processedManualTestCases caseformattest18.java

--- caseformattest18.java
+++ caseformattest18.java
@@ -1,24 +1,31 @@
 package com.google.common.base;
 
-import static com.google.common.base.CaseFormat.LOWER_CAMEL;
 import static com.google.common.base.CaseFormat.LOWER_HYPHEN;
-import static com.google.common.base.CaseFormat.LOWER_UNDERSCORE;
 import static com.google.common.base.CaseFormat.UPPER_CAMEL;
-import static com.google.common.base.CaseFormat.UPPER_UNDERSCORE;
 import static com.google.common.truth.Truth.assertThat;
-import static com.google.common.truth.Truth.assertWithMessage;
-import com.google.common.annotations.GwtCompatible;
-import com.google.common.annotations.GwtIncompatible;
-import com.google.common.annotations.J2ktIncompatible;
-import com.google.common.testing.NullPointerTester;
-import com.google.common.testing.SerializableTester;
-import junit.framework.TestCase;
-import org.jspecify.annotations.NullUnmarked;
 
-public class CaseFormatTestTest18 extends TestCase {
+import junit.framework.TestCase;
+
+/**
+ * Tests for {@link CaseFormat}.
+ */
+public class CaseFormatTest extends TestCase {
 
     public void testUpperCamelToLowerHyphen() {
-        assertThat(UPPER_CAMEL.to(LOWER_HYPHEN, "Foo")).isEqualTo("foo");
-        assertThat(UPPER_CAMEL.to(LOWER_HYPHEN, "FooBar")).isEqualTo("foo-bar");
+        // Arrange: A single-word string in UpperCamel format.
+        String singleWordInput = "Foo";
+        String expectedSingleWordOutput = "foo";
+
+        // Act & Assert: Verify the conversion to lower-hyphen.
+        assertThat(UPPER_CAMEL.to(LOWER_HYPHEN, singleWordInput))
+            .isEqualTo(expectedSingleWordOutput);
+
+        // Arrange: A multi-word string in UpperCamel format.
+        String multiWordInput = "FooBar";
+        String expectedMultiWordOutput = "foo-bar";
+
+        // Act & Assert: Verify the conversion to lower-hyphen.
+        assertThat(UPPER_CAMEL.to(LOWER_HYPHEN, multiWordInput))
+            .isEqualTo(expectedMultiWordOutput);
     }
 }
