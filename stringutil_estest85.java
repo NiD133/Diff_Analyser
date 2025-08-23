6f4566b5720b5598d7a568421f22dfbacfb8b573
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:19:12 +0800

    gemini processedAutoTestCases stringutil_estest85.java

--- stringutil_estest85.java
+++ stringutil_estest85.java
@@ -1,28 +1,20 @@
 package org.jsoup.internal;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Collection;
-import java.util.ConcurrentModificationException;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.ListIterator;
-import java.util.stream.Collector;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.evosuite.runtime.mock.java.net.MockURL;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertTrue;
 
-public class StringUtil_ESTestTest85 extends StringUtil_ESTest_scaffolding {
+/**
+ * Test suite for the {@link StringUtil#isBlank(String)} method.
+ */
+public class StringUtilTest {
 
-    @Test(timeout = 4000)
-    public void test84() throws Throwable {
-        boolean boolean0 = StringUtil.isBlank("                    ");
-        assertTrue(boolean0);
+    /**
+     * Verifies that isBlank() returns true for a string that contains only space characters.
+     */
+    @Test
+    public void isBlankReturnsTrueForStringContainingOnlySpaces() {
+        // The isBlank method should identify a string composed of only spaces as "blank".
+        assertTrue("A string containing only spaces should be considered blank", 
+                   StringUtil.isBlank("                    "));
     }
 }
