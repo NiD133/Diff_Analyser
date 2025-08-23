69aa8882a6686bd655f9b54d650987b9eecc9c74
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:46:03 +0800

    Original processedAutoTestCases coreoperation_estest5.java

---
+++ coreoperation_estest5.java
@@ -0,0 +1,38 @@
+package org.apache.commons.jxpath.ri.compiler;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.apache.commons.jxpath.JXPathContext;
+import org.apache.commons.jxpath.ri.EvalContext;
+import org.apache.commons.jxpath.ri.JXPathContextReferenceImpl;
+import org.apache.commons.jxpath.ri.QName;
+import org.apache.commons.jxpath.ri.axes.InitialContext;
+import org.apache.commons.jxpath.ri.axes.ParentContext;
+import org.apache.commons.jxpath.ri.axes.PrecedingOrFollowingContext;
+import org.apache.commons.jxpath.ri.axes.RootContext;
+import org.apache.commons.jxpath.ri.axes.SelfContext;
+import org.apache.commons.jxpath.ri.axes.UnionContext;
+import org.apache.commons.jxpath.ri.model.NodePointer;
+import org.apache.commons.jxpath.ri.model.VariablePointer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class CoreOperation_ESTestTest5 extends CoreOperation_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test04() throws Throwable {
+        NameAttributeTest nameAttributeTest0 = new NameAttributeTest((Expression) null, (Expression) null);
+        // Undeclared exception!
+        try {
+            nameAttributeTest0.toString();
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.evosuite.runtime.System", e);
+        }
+    }
+}
