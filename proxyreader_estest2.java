77853e9e5c64ca1ef22f729b9174eb702f1cbe0c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:09:36 +0800

    Original processedAutoTestCases proxyreader_estest2.java

---
+++ proxyreader_estest2.java
@@ -0,0 +1,24 @@
+package org.apache.commons.io.input;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.CharArrayWriter;
+import java.io.IOException;
+import java.io.PipedReader;
+import java.io.StringReader;
+import java.nio.CharBuffer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockIOException;
+import org.junit.runner.RunWith;
+
+public class ProxyReader_ESTestTest2 extends ProxyReader_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test01() throws Throwable {
+        StringReader stringReader0 = new StringReader("");
+        CloseShieldReader closeShieldReader0 = CloseShieldReader.wrap(stringReader0);
+        closeShieldReader0.afterRead(1139);
+    }
+}
