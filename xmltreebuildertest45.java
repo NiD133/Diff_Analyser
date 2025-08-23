d5b4e5591a7f28cc6ba57dd8f651bc45ba952b4a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:36:17 +0800

    gemini processedManualTestCases xmltreebuildertest45.java

--- xmltreebuildertest45.java
+++ xmltreebuildertest45.java
@@ -1,43 +1,56 @@
 package org.jsoup.parser;
 
 import org.jsoup.Jsoup;
-import org.jsoup.TextUtil;
-import org.jsoup.nodes.*;
-import org.jsoup.select.Elements;
+import org.jsoup.nodes.Attribute;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.junit.jupiter.api.Test;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URISyntaxException;
-import java.nio.charset.StandardCharsets;
-import java.util.List;
-import static org.jsoup.nodes.Document.OutputSettings.Syntax;
-import static org.jsoup.parser.Parser.NamespaceHtml;
-import static org.jsoup.parser.Parser.NamespaceXml;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-public class XmlTreeBuilderTestTest45 {
+/**
+ * Tests for the {@link XmlTreeBuilder} focusing on XML namespace handling.
+ */
+public class XmlTreeBuilderTest {
 
-    private static void assertXmlNamespace(Element el) {
-        assertEquals(NamespaceXml, el.tag().namespace(), String.format("Element %s not in XML namespace", el.tagName()));
-    }
-
+    /**
+     * Verifies that the XML parser correctly processes attributes with and without namespaces.
+     * It checks that for a namespaced attribute, the prefix, local name, and namespace URI are
+     * correctly identified. For a regular attribute, it confirms that namespace-related
+     * properties are empty.
+     */
     @Test
-    void namespacedAttribute() {
-        String xml = "<x xmlns:edi='http://ecommerce.example.org/schema'>\n" + "  <!-- the 'taxClass' attribute's namespace is http://ecommerce.example.org/schema -->\n" + "  <lineItem edi:taxClass=\"exempt\" other=foo>Baby food</lineItem>\n" + "</x>";
+    void parsesNamespacedAttributesCorrectly() {
+        // Arrange: Define an XML string with a namespace declaration and two attributes:
+        // one namespaced ('edi:taxClass') and one regular ('other').
+        String xml = """
+            <x xmlns:edi='http://ecommerce.example.org/schema'>
+              <lineItem edi:taxClass="exempt" other="foo">Baby food</lineItem>
+            </x>""";
+
+        // Act: Parse the XML string using the dedicated XML parser.
         Document doc = Jsoup.parse(xml, Parser.xmlParser());
         Element lineItem = doc.expectFirst("lineItem");
-        Attribute taxClass = lineItem.attribute("edi:taxClass");
-        assertNotNull(taxClass);
-        assertEquals("edi", taxClass.prefix());
-        assertEquals("taxClass", taxClass.localName());
-        assertEquals("http://ecommerce.example.org/schema", taxClass.namespace());
-        Attribute other = lineItem.attribute("other");
-        assertNotNull(other);
-        assertEquals("foo", other.getValue());
-        assertEquals("", other.prefix());
-        assertEquals("other", other.localName());
-        assertEquals("", other.namespace());
+
+        // Assert: Verify the properties of both attributes.
+
+        // 1. Check the namespaced attribute 'edi:taxClass'
+        Attribute taxClassAttr = lineItem.attribute("edi:taxClass");
+        assertNotNull(taxClassAttr, "The 'edi:taxClass' attribute should exist.");
+        assertAll("Properties of namespaced attribute 'edi:taxClass'",
+            () -> assertEquals("edi", taxClassAttr.prefix(), "Prefix should be 'edi'"),
+            () -> assertEquals("taxClass", taxClassAttr.localName(), "Local name should be 'taxClass'"),
+            () -> assertEquals("http://ecommerce.example.org/schema", taxClassAttr.namespace(), "Namespace URI should match the declaration")
+        );
+
+        // 2. Check the regular attribute 'other'
+        Attribute otherAttr = lineItem.attribute("other");
+        assertNotNull(otherAttr, "The 'other' attribute should exist.");
+        assertAll("Properties of regular attribute 'other'",
+            () -> assertEquals("foo", otherAttr.getValue(), "Value should be 'foo'"),
+            () -> assertEquals("", otherAttr.prefix(), "Prefix should be empty"),
+            () -> assertEquals("other", otherAttr.localName(), "Local name should be 'other'"),
+            () -> assertEquals("", otherAttr.namespace(), "Namespace should be empty")
+        );
     }
 }
