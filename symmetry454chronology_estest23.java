dd0f1566784eee1cd692db919550a7a3a16cd94e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:19:52 +0800

    Original processedAutoTestCases symmetry454chronology_estest23.java

---
+++ symmetry454chronology_estest23.java
@@ -0,0 +1,52 @@
+package org.threeten.extra.chrono;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.time.Clock;
+import java.time.DateTimeException;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.chrono.ChronoLocalDateTime;
+import java.time.chrono.ChronoZonedDateTime;
+import java.time.chrono.Era;
+import java.time.chrono.HijrahEra;
+import java.time.chrono.IsoEra;
+import java.time.chrono.JapaneseEra;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAccessor;
+import java.time.temporal.TemporalUnit;
+import java.time.temporal.UnsupportedTemporalTypeException;
+import java.time.temporal.ValueRange;
+import java.util.List;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.System;
+import org.evosuite.runtime.mock.java.time.MockClock;
+import org.evosuite.runtime.mock.java.time.MockInstant;
+import org.evosuite.runtime.mock.java.time.MockLocalDateTime;
+import org.evosuite.runtime.mock.java.time.MockOffsetDateTime;
+import org.junit.runner.RunWith;
+
+public class Symmetry454Chronology_ESTestTest23 extends Symmetry454Chronology_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test22() throws Throwable {
+        Symmetry454Chronology symmetry454Chronology0 = new Symmetry454Chronology();
+        IsoEra isoEra0 = IsoEra.BCE;
+        // Undeclared exception!
+        try {
+            symmetry454Chronology0.dateYearDay((Era) isoEra0, (-313), (-313));
+            fail("Expecting exception: DateTimeException");
+        } catch (DateTimeException e) {
+            //
+            // Invalid value for DayOfYear (valid values 1 - 364/371): -313
+            //
+            verifyException("java.time.temporal.ValueRange", e);
+        }
+    }
+}
