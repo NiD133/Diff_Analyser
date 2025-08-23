a69db28ef8dcd4f01ecc8ed3526a1cecae5fa323
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:37:26 +0800

    gemini processedAutoTestCases arrayfill_estest11.java

--- arrayfill_estest11.java
+++ arrayfill_estest11.java
@@ -1,30 +1,47 @@
 package org.apache.commons.lang3;
 
+import static org.junit.Assert.assertThrows;
+
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.Arrays;
-import org.apache.commons.lang3.function.FailableIntFunction;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.lang.MockThrowable;
-import org.junit.runner.RunWith;
 
-public class ArrayFill_ESTestTest11 extends ArrayFill_ESTest_scaffolding {
+/**
+ * Tests for {@link ArrayFill}.
+ */
+public class ArrayFillTest {
 
-    @Test(timeout = 4000)
-    public void test10() throws Throwable {
-        Throwable[] throwableArray0 = new Throwable[3];
-        FailableIntFunction<Object, Throwable> failableIntFunction0 = FailableIntFunction.nop();
-        // Undeclared exception!
-        try {
-            ArrayFill.fill((Object[]) throwableArray0, (Object) failableIntFunction0);
-            fail("Expecting exception: ArrayStoreException");
-        } catch (ArrayStoreException e) {
-            //
-            // org.apache.commons.lang3.function.FailableIntFunction$$Lambda$153/1790855301
-            //
-            verifyException("java.util.Arrays", e);
-        }
+    /**
+     * Tests that calling {@code ArrayFill.fill(T[], T)} with an element of a type
+     * incompatible with the array's component type throws an {@link ArrayStoreException}.
+     * This behavior is consistent with {@link java.util.Arrays#fill(Object[], Object)}.
+     */
+    @Test
+    public void testFillObjectArrayWithIncompatibleTypeThrowsArrayStoreException() {
+        // Arrange: Create an array of a specific reference type.
+        final Integer[] integerArray = new Integer[3];
+
+        // Arrange: Create an element of a type that cannot be stored in the array.
+        final String incompatibleElement = "I am not an integer";
+
+        // Act & Assert: Attempting to fill the Integer array with a String should fail.
+        // The compiler allows this call by inferring the common supertype 'Object' for T,
+        // but the JVM throws an ArrayStoreException at runtime.
+        assertThrows(ArrayStoreException.class, () -> {
+            ArrayFill.fill(integerArray, incompatibleElement);
+        });
+    }
+
+    /**
+     * This is an alternative implementation using JUnit 4's 'expected' parameter,
+     * which was common before JUnit 5 and lambda expressions.
+     * It achieves the same goal as the test above.
+     */
+    @Test(expected = ArrayStoreException.class)
+    public void testFillObjectArrayWithIncompatibleTypeThrowsArrayStoreException_Junit4Style() {
+        // Arrange
+        final Integer[] integerArray = new Integer[3];
+        final String incompatibleElement = "I am not an integer";
+
+        // Act: This call will throw the expected ArrayStoreException.
+        ArrayFill.fill(integerArray, incompatibleElement);
     }
 }
