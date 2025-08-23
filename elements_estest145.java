4ebab300bdbaeb57e8024d2c641cc814e0f6bc76
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:48:29 +0800

    gemini processedAutoTestCases elements_estest145.java

--- elements_estest145.java
+++ elements_estest145.java
@@ -1,36 +1,31 @@
 package org.jsoup.select;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.ConcurrentModificationException;
-import java.util.LinkedHashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.function.Predicate;
-import java.util.function.UnaryOperator;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.jsoup.nodes.Comment;
-import org.jsoup.nodes.DataNode;
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.nodes.FormElement;
-import org.jsoup.nodes.TextNode;
-import org.jsoup.parser.Parser;
-import org.junit.runner.RunWith;
+import org.junit.Test;
+import static org.junit.Assert.assertFalse;
 
-public class Elements_ESTestTest145 extends Elements_ESTest_scaffolding {
+/**
+ * Test for the {@link Elements#hasAttr(String)} method.
+ */
+public class ElementsHasAttrTest {
 
-    @Test(timeout = 4000)
-    public void test144() throws Throwable {
-        Document document0 = Parser.parseBodyFragment("Only http & https protocols supported", "Only http & https protocols supported");
-        Elements elements0 = document0.getAllElements();
-        boolean boolean0 = elements0.hasAttr("Only http & https protocols supported");
-        assertFalse(boolean0);
+    /**
+     * Verifies that hasAttr() returns false when none of the elements in the collection
+     * possess the specified attribute.
+     */
+    @Test
+    public void hasAttrShouldReturnFalseWhenNoElementHasAttribute() {
+        // Arrange: Create a document with several elements. Note that one element has a 'class'
+        // attribute, but none have an 'id' attribute.
+        String html = "<div><p class='greeting'>Hello</p><span>World</span></div>";
+        Document document = Jsoup.parse(html);
+        Elements elements = document.getAllElements(); // Contains html, head, body, div, p, span
+
+        // Act: Check for an attribute that does not exist on any element.
+        boolean hasIdAttribute = elements.hasAttr("id");
+
+        // Assert: The result should be false, as no element has an 'id'.
+        assertFalse("Expected hasAttr('id') to be false because no element has an 'id' attribute.", hasIdAttribute);
     }
 }
