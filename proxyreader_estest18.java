1900d409ff74638eadd72d866e83d29d5c6f8c72
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:09:36 +0800

    gemini processedAutoTestCases proxyreader_estest18.java

--- proxyreader_estest18.java
+++ proxyreader_estest18.java
@@ -1,32 +1,29 @@
 package org.apache.commons.io.input;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.CharArrayWriter;
+
 import java.io.IOException;
 import java.io.PipedReader;
-import java.io.StringReader;
-import java.nio.CharBuffer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.io.MockIOException;
-import org.junit.runner.RunWith;
+import java.io.Reader;
 
-public class ProxyReader_ESTestTest18 extends ProxyReader_ESTest_scaffolding {
+/**
+ * Tests for {@link ProxyReader} to ensure it correctly delegates method calls.
+ */
+public class ProxyReaderTest {
 
-    @Test(timeout = 4000)
-    public void test17() throws Throwable {
-        PipedReader pipedReader0 = new PipedReader();
-        TaggedReader taggedReader0 = new TaggedReader(pipedReader0);
-        try {
-            taggedReader0.reset();
-            fail("Expecting exception: IOException");
-        } catch (IOException e) {
-            //
-            // reset() not supported
-            //
-            verifyException("org.apache.commons.io.input.TaggedReader", e);
-        }
+    /**
+     * Tests that calling reset() on a ProxyReader throws an IOException if the
+     * underlying delegate reader does not support the operation.
+     */
+    @Test(expected = IOException.class)
+    public void resetShouldThrowIOExceptionWhenDelegateDoesNotSupportReset() throws IOException {
+        // Arrange: Create a ProxyReader with a delegate that is known to not support reset().
+        // PipedReader is a good example as its reset() method always throws an IOException.
+        final Reader delegateReader = new PipedReader();
+        final ProxyReader proxyReader = new TaggedReader(delegateReader);
+
+        // Act: Attempt to reset the proxy reader. This call should be delegated
+        // to the PipedReader, which will throw the expected IOException.
+        proxyReader.reset();
     }
 }
