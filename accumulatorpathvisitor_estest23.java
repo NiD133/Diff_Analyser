8bbedfbbb8b22f37e4ea70926f3d1041d9373871
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:37:05 +0800

    gemini processedAutoTestCases accumulatorpathvisitor_estest23.java

--- accumulatorpathvisitor_estest23.java
+++ accumulatorpathvisitor_estest23.java
@@ -1,35 +1,40 @@
 package org.apache.commons.io.file;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.IOException;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Path;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.Comparator;
-import java.util.List;
-import java.util.regex.Pattern;
-import org.apache.commons.io.filefilter.CanExecuteFileFilter;
-import org.apache.commons.io.filefilter.CanReadFileFilter;
-import org.apache.commons.io.filefilter.CanWriteFileFilter;
-import org.apache.commons.io.filefilter.HiddenFileFilter;
-import org.apache.commons.io.filefilter.PrefixFileFilter;
-import org.apache.commons.io.filefilter.RegexFileFilter;
-import org.apache.commons.io.function.IOBiFunction;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.evosuite.runtime.mock.java.io.MockFile;
-import org.evosuite.runtime.mock.java.io.MockIOException;
-import org.junit.runner.RunWith;
 
-public class AccumulatorPathVisitor_ESTestTest23 extends AccumulatorPathVisitor_ESTest_scaffolding {
+/**
+ * Tests for the hashCode() and equals() contract of {@link AccumulatorPathVisitor}.
+ */
+public class AccumulatorPathVisitorTest {
 
-    @Test(timeout = 4000)
-    public void test22() throws Throwable {
-        AccumulatorPathVisitor accumulatorPathVisitor0 = AccumulatorPathVisitor.withBigIntegerCounters();
-        accumulatorPathVisitor0.hashCode();
+    /**
+     * Tests that two equal AccumulatorPathVisitor instances have the same hash code,
+     * and that two unequal instances have different hash codes.
+     */
+    @Test
+    public void testHashCodeContract() {
+        // Arrange
+        // Create two identical visitors. They should be equal to each other.
+        AccumulatorPathVisitor visitor1 = AccumulatorPathVisitor.withBigIntegerCounters();
+        AccumulatorPathVisitor visitor2 = AccumulatorPathVisitor.withBigIntegerCounters();
+
+        // Create a different visitor that should not be equal to the first two.
+        AccumulatorPathVisitor differentVisitor = AccumulatorPathVisitor.withLongCounters();
+
+        // Assert
+        // 1. Verify the equals() contract as a prerequisite for testing hashCode().
+        assertEquals("Two visitors created with the same configuration should be equal.", visitor1, visitor2);
+        assertNotEquals("Visitors with different counter types should not be equal.", visitor1, differentVisitor);
+
+        // 2. Verify the hashCode() contract.
+        assertEquals("Equal objects must have equal hash codes.", visitor1.hashCode(), visitor2.hashCode());
+        
+        // 3. While not a strict requirement of the contract, unequal objects should
+        //    ideally have different hash codes. This is a good check for the quality
+        //    of the hashCode() implementation.
+        assertNotEquals("Unequal objects should ideally have different hash codes.", visitor1.hashCode(), differentVisitor.hashCode());
     }
 }
