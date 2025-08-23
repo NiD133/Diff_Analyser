c0afcdcebca40fcbc4d684527eae2870aaa32a7f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:16:35 +0800

    gemini processedAutoTestCases simplepathinterpreter_estest33.java

--- simplepathinterpreter_estest33.java
+++ simplepathinterpreter_estest33.java
@@ -1,66 +1,58 @@
 package org.apache.commons.jxpath.ri.axes;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.Locale;
 import org.apache.commons.jxpath.BasicVariables;
-import org.apache.commons.jxpath.JXPathBasicBeanInfo;
-import org.apache.commons.jxpath.JXPathContext;
-import org.apache.commons.jxpath.ri.EvalContext;
-import org.apache.commons.jxpath.ri.JXPathContextReferenceImpl;
 import org.apache.commons.jxpath.ri.QName;
-import org.apache.commons.jxpath.ri.compiler.Constant;
-import org.apache.commons.jxpath.ri.compiler.CoreFunction;
-import org.apache.commons.jxpath.ri.compiler.CoreOperationAnd;
-import org.apache.commons.jxpath.ri.compiler.CoreOperationEqual;
-import org.apache.commons.jxpath.ri.compiler.CoreOperationGreaterThanOrEqual;
-import org.apache.commons.jxpath.ri.compiler.CoreOperationLessThanOrEqual;
-import org.apache.commons.jxpath.ri.compiler.CoreOperationMod;
-import org.apache.commons.jxpath.ri.compiler.CoreOperationMultiply;
-import org.apache.commons.jxpath.ri.compiler.CoreOperationNegate;
-import org.apache.commons.jxpath.ri.compiler.CoreOperationNotEqual;
-import org.apache.commons.jxpath.ri.compiler.CoreOperationOr;
-import org.apache.commons.jxpath.ri.compiler.CoreOperationSubtract;
-import org.apache.commons.jxpath.ri.compiler.CoreOperationUnion;
 import org.apache.commons.jxpath.ri.compiler.Expression;
-import org.apache.commons.jxpath.ri.compiler.NameAttributeTest;
-import org.apache.commons.jxpath.ri.compiler.NodeNameTest;
-import org.apache.commons.jxpath.ri.compiler.NodeTest;
-import org.apache.commons.jxpath.ri.compiler.NodeTypeTest;
-import org.apache.commons.jxpath.ri.compiler.ProcessingInstructionTest;
 import org.apache.commons.jxpath.ri.compiler.Step;
-import org.apache.commons.jxpath.ri.compiler.VariableReference;
-import org.apache.commons.jxpath.ri.model.NodePointer;
 import org.apache.commons.jxpath.ri.model.VariablePointer;
-import org.apache.commons.jxpath.ri.model.beans.BeanPointer;
-import org.apache.commons.jxpath.ri.model.beans.BeanPropertyPointer;
-import org.apache.commons.jxpath.ri.model.beans.NullPointer;
-import org.apache.commons.jxpath.ri.model.beans.NullPropertyPointer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class SimplePathInterpreter_ESTestTest33 extends SimplePathInterpreter_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
-    @Test(timeout = 4000)
-    public void test32() throws Throwable {
-        QName qName0 = new QName("<<unknon namespce>>");
-        Step[] stepArray0 = new Step[1];
-        BasicVariables basicVariables0 = new BasicVariables();
-        VariablePointer variablePointer0 = new VariablePointer(basicVariables0, qName0);
-        Expression[] expressionArray0 = new Expression[0];
-        // Undeclared exception!
+/**
+ * Contains tests for the {@link SimplePathInterpreter} class.
+ * This test focuses on how the interpreter handles paths involving variables.
+ */
+public class SimplePathInterpreterTest {
+
+    /**
+     * Tests that interpretSimpleExpressionPath throws an IllegalArgumentException
+     * when the path starts with a variable that has not been declared.
+     */
+    @Test
+    public void interpretPathWithUndeclaredVariableShouldThrowException() {
+        // Arrange
+        // 1. Define a variable name that will not be present in the variables context.
+        final String variableName = "undeclared_variable";
+        QName undeclaredVariableName = new QName(variableName);
+
+        // 2. Create an empty set of variables.
+        BasicVariables variables = new BasicVariables();
+
+        // 3. Create a pointer to the undeclared variable. This pointer acts as the
+        //    root of the path to be interpreted.
+        VariablePointer undeclaredVariablePointer = new VariablePointer(variables, undeclaredVariableName);
+
+        // 4. The method requires non-null arrays for steps and predicates. Their content
+        //    is irrelevant for this test, as the failure should occur before they are processed.
+        Step[] pathSteps = new Step[1];
+        Expression[] predicates = new Expression[0];
+
+        // Act & Assert
         try {
-            SimplePathInterpreter.interpretSimpleExpressionPath((EvalContext) null, variablePointer0, expressionArray0, stepArray0);
-            fail("Expecting exception: IllegalArgumentException");
+            // Attempt to interpret a path starting from the undeclared variable.
+            // The EvalContext can be null because it is not accessed before the variable lookup fails.
+            SimplePathInterpreter.interpretSimpleExpressionPath(
+                    null,
+                    undeclaredVariablePointer,
+                    predicates,
+                    pathSteps);
+            fail("Expected an IllegalArgumentException because the root variable is not declared.");
         } catch (IllegalArgumentException e) {
-            //
-            // No such variable: '<<unknon namespce>>'
-            //
-            verifyException("org.apache.commons.jxpath.BasicVariables", e);
+            // Verify that the exception was thrown for the correct reason.
+            String expectedMessage = "No such variable: '" + variableName + "'";
+            assertEquals(expectedMessage, e.getMessage());
         }
     }
 }
