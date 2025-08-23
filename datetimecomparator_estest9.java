afdf026b6ef8e9625f97e619c7d9bf165ed85317
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:46:39 +0800

    Original processedAutoTestCases datetimecomparator_estest9.java

---
+++ datetimecomparator_estest9.java
@@ -0,0 +1,19 @@
+package org.joda.time;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class DateTimeComparator_ESTestTest9 extends DateTimeComparator_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test08() throws Throwable {
+        DateTimeComparator dateTimeComparator0 = DateTimeComparator.getTimeOnlyInstance();
+        DateTimeComparator dateTimeComparator1 = DateTimeComparator.getInstance();
+        boolean boolean0 = dateTimeComparator1.equals(dateTimeComparator0);
+        assertFalse(boolean0);
+    }
+}
