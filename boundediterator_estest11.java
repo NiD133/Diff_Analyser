1e109d2791050eafbe4d1f1eb09adb5423b98255
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:39:34 +0800

    gemini processedAutoTestCases boundediterator_estest11.java

--- boundediterator_estest11.java
+++ boundediterator_estest11.java
@@ -1,37 +1,32 @@
 package org.apache.commons.collections4.iterators;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.ConcurrentModificationException;
+import static org.junit.Assert.assertThrows;
+
+import java.util.Collections;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.NoSuchElementException;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.functors.NOPClosure;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class BoundedIterator_ESTestTest11 extends BoundedIterator_ESTest_scaffolding {
+/**
+ * Unit tests for {@link BoundedIterator}.
+ */
+public class BoundedIteratorTest {
 
-    @Test(timeout = 4000)
-    public void test10() throws Throwable {
-        LinkedList<Integer> linkedList0 = new LinkedList<Integer>();
-        Iterator<Integer> iterator0 = linkedList0.descendingIterator();
-        BoundedIterator<Integer> boundedIterator0 = new BoundedIterator<Integer>(iterator0, 0L, 0L);
-        // Undeclared exception!
-        try {
-            boundedIterator0.next();
-            fail("Expecting exception: NoSuchElementException");
-        } catch (NoSuchElementException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.commons.collections4.iterators.BoundedIterator", e);
-        }
+    /**
+     * Tests that calling next() on an iterator bounded to a zero-element range
+     * throws a NoSuchElementException.
+     */
+    @Test
+    public void nextShouldThrowNoSuchElementExceptionForEmptyBoundedRange() {
+        // Arrange: Create a BoundedIterator with a maximum size of 0.
+        // This means it should never have a next element, regardless of the
+        // underlying iterator's contents.
+        final Iterator<String> emptySourceIterator = Collections.emptyIterator();
+        final BoundedIterator<String> boundedIterator =
+            new BoundedIterator<>(emptySourceIterator, 0L, 0L);
+
+        // Act & Assert: Verify that calling next() throws NoSuchElementException.
+        // The lambda expression `boundedIterator::next` is the action to be performed.
+        assertThrows(NoSuchElementException.class, boundedIterator::next);
     }
 }
