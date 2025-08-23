70df5b2363e89a8cdba35f3c2b78126be4aac0c8
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:37:18 +0800

    Original processedAutoTestCases ararchiveoutputstream_estest19.java

---
+++ ararchiveoutputstream_estest19.java
@@ -0,0 +1,35 @@
+package org.apache.commons.compress.archivers.ar;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.LinkOption;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.System;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.evosuite.runtime.mock.java.io.MockFileOutputStream;
+import org.evosuite.runtime.mock.java.io.MockPrintStream;
+import org.evosuite.runtime.testdata.FileSystemHandling;
+import org.junit.runner.RunWith;
+
+public class ArArchiveOutputStream_ESTestTest19 extends ArArchiveOutputStream_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test18() throws Throwable {
+        MockPrintStream mockPrintStream0 = new MockPrintStream("HU)HAv");
+        File file0 = MockFile.createTempFile("HU)HAv", "HU)HAv", (File) null);
+        ArArchiveOutputStream arArchiveOutputStream0 = new ArArchiveOutputStream(mockPrintStream0);
+        ArArchiveEntry arArchiveEntry0 = arArchiveOutputStream0.createArchiveEntry(file0, "XN}!P");
+        arArchiveOutputStream0.putArchiveEntry(arArchiveEntry0);
+        arArchiveOutputStream0.closeArchiveEntry();
+        arArchiveOutputStream0.finish();
+        assertEquals(68L, arArchiveOutputStream0.getBytesWritten());
+    }
+}
