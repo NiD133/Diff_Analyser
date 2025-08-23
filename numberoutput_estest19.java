cea11c275afe695d38d1876048512bbc7a6923b3
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:04:59 +0800

    Original processedAutoTestCases numberoutput_estest19.java

---
+++ numberoutput_estest19.java
@@ -0,0 +1,25 @@
+package com.fasterxml.jackson.core.io;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class NumberOutput_ESTestTest19 extends NumberOutput_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test18() throws Throwable {
+        // Undeclared exception!
+        try {
+            NumberOutput.outputLong(1L, (char[]) null, 0);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("com.fasterxml.jackson.core.io.NumberOutput", e);
+        }
+    }
+}
