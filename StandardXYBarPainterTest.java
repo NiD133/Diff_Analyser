d002de47ba265aa20df2e0993a3864ae000c035a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 05:35:14 +0800

    gpt manualTestSuite StandardXYBarPainterTest.java

--- StandardXYBarPainterTest.java
+++ StandardXYBarPainterTest.java
@@ -1,39 +1,3 @@
-/* ======================================================
- * JFreeChart : a chart library for the Java(tm) platform
- * ======================================================
- *
- * (C) Copyright 2000-present, by David Gilbert and Contributors.
- *
- * Project Info:  https://www.jfree.org/jfreechart/index.html
- *
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 2.1 of the License, or
- * (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
- * License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
- * USA.
- *
- * [Oracle and Java are registered trademarks of Oracle and/or its affiliates. 
- * Other names may be trademarks of their respective owners.]
- *
- * -----------------------------
- * StandardXYBarPainterTest.java
- * -----------------------------
- * (C) Copyright 2008-present, by David Gilbert and Contributors.
- *
- * Original Author:  David Gilbert;
- * Contributor(s):   -;
- *
- */
-
 package org.jfree.chart.renderer.xy;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -42,56 +6,54 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.jfree.chart.TestUtils;
 import org.jfree.chart.api.PublicCloneable;
-
 import org.junit.jupiter.api.Test;
 
 /**
- * Tests for the {@link StandardXYBarPainter} class.
+ * Unit tests for the {@link StandardXYBarPainter} class.
  */
 public class StandardXYBarPainterTest {
 
     /**
-     * Check that the equals() method distinguishes all fields.
+     * Tests that two instances of {@link StandardXYBarPainter} are equal.
      */
     @Test
     public void testEquals() {
-        StandardXYBarPainter p1 = new StandardXYBarPainter();
-        StandardXYBarPainter p2 = new StandardXYBarPainter();
-        assertEquals(p1, p2);
+        StandardXYBarPainter painter1 = new StandardXYBarPainter();
+        StandardXYBarPainter painter2 = new StandardXYBarPainter();
+        assertEquals(painter1, painter2, "Two new instances should be equal");
     }
 
     /**
-     * Two objects that are equal are required to return the same hashCode.
+     * Tests that two equal instances of {@link StandardXYBarPainter} have the same hash code.
      */
     @Test
     public void testHashcode() {
-        StandardXYBarPainter p1 = new StandardXYBarPainter();
-        StandardXYBarPainter p2 = new StandardXYBarPainter();
-        assertEquals(p1, p2);
-        int h1 = p1.hashCode();
-        int h2 = p2.hashCode();
-        assertEquals(h1, h2);
+        StandardXYBarPainter painter1 = new StandardXYBarPainter();
+        StandardXYBarPainter painter2 = new StandardXYBarPainter();
+        assertEquals(painter1, painter2, "Two new instances should be equal");
+        int hashCode1 = painter1.hashCode();
+        int hashCode2 = painter2.hashCode();
+        assertEquals(hashCode1, hashCode2, "Equal instances should have the same hash code");
     }
 
     /**
-     * Confirm that cloning isn't implemented (it isn't required, because
-     * instances of the class are immutable).
+     * Tests that {@link StandardXYBarPainter} does not implement cloning.
+     * This is expected because instances of this class are immutable.
      */
     @Test
     public void testCloning() {
-        StandardXYBarPainter p1 = new StandardXYBarPainter();
-        assertFalse(p1 instanceof Cloneable);
-        assertFalse(p1 instanceof PublicCloneable);
+        StandardXYBarPainter painter = new StandardXYBarPainter();
+        assertFalse(painter instanceof Cloneable, "StandardXYBarPainter should not be cloneable");
+        assertFalse(painter instanceof PublicCloneable, "StandardXYBarPainter should not be publicly cloneable");
     }
 
     /**
-     * Serialize an instance, restore it, and check for equality.
+     * Tests that a serialized and deserialized instance of {@link StandardXYBarPainter} is equal to the original.
      */
     @Test
     public void testSerialization() {
-        StandardXYBarPainter p1 = new StandardXYBarPainter();
-        StandardXYBarPainter p2 = TestUtils.serialised(p1);
-        assertEquals(p1, p2);
+        StandardXYBarPainter originalPainter = new StandardXYBarPainter();
+        StandardXYBarPainter deserializedPainter = TestUtils.serialised(originalPainter);
+        assertEquals(originalPainter, deserializedPainter, "Deserialized instance should be equal to the original");
     }
-
 }
