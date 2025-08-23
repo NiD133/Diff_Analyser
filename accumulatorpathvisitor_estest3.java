339e5c261694775077083aa0425592fc8fe73fb4
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:37:06 +0800

    Original processedAutoTestCases accumulatorpathvisitor_estest3.java

---
+++ accumulatorpathvisitor_estest3.java
@@ -0,0 +1,42 @@
+package org.apache.commons.io.file;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Comparator;
+import java.util.List;
+import java.util.regex.Pattern;
+import org.apache.commons.io.filefilter.CanExecuteFileFilter;
+import org.apache.commons.io.filefilter.CanReadFileFilter;
+import org.apache.commons.io.filefilter.CanWriteFileFilter;
+import org.apache.commons.io.filefilter.HiddenFileFilter;
+import org.apache.commons.io.filefilter.PrefixFileFilter;
+import org.apache.commons.io.filefilter.RegexFileFilter;
+import org.apache.commons.io.function.IOBiFunction;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.evosuite.runtime.mock.java.io.MockIOException;
+import org.junit.runner.RunWith;
+
+public class AccumulatorPathVisitor_ESTestTest3 extends AccumulatorPathVisitor_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test02() throws Throwable {
+        AccumulatorPathVisitor accumulatorPathVisitor0 = new AccumulatorPathVisitor();
+        MockFile mockFile0 = new MockFile("", "");
+        Path path0 = mockFile0.toPath();
+        BasicFileAttributes basicFileAttributes0 = mock(BasicFileAttributes.class, new ViolatedAssumptionAnswer());
+        doReturn(0L).when(basicFileAttributes0).size();
+        accumulatorPathVisitor0.updateFileCounters(path0, basicFileAttributes0);
+        Comparator<Object> comparator0 = (Comparator<Object>) mock(Comparator.class, new ViolatedAssumptionAnswer());
+        List<Path> list0 = accumulatorPathVisitor0.relativizeFiles(path0, true, comparator0);
+        assertFalse(list0.contains(path0));
+    }
+}
