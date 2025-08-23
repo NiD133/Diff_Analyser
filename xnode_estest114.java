9c28b5806aa174670a78a6edf0f77da58ccf501e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:28:38 +0800

    gemini processedAutoTestCases xnode_estest114.java

--- xnode_estest114.java
+++ xnode_estest114.java
@@ -1,38 +1,34 @@
 package org.apache.ibatis.parsing;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.List;
-import java.util.Locale;
-import java.util.Properties;
-import java.util.function.Supplier;
-import javax.imageio.metadata.IIOMetadataNode;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
-import org.xml.sax.ext.DefaultHandler2;
 
-public class XNode_ESTestTest114 extends XNode_ESTest_scaffolding {
+import javax.imageio.metadata.IIOMetadataNode;
+import java.util.Properties;
 
-    @Test(timeout = 4000)
-    public void test113() throws Throwable {
-        IIOMetadataNode iIOMetadataNode0 = new IIOMetadataNode();
-        XPathParser xPathParser0 = new XPathParser((Document) null, false);
-        Properties properties0 = new Properties();
-        XNode xNode0 = new XNode(xPathParser0, iIOMetadataNode0, properties0);
-        // Undeclared exception!
-        try {
-            xNode0.newXNode((Node) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-        }
+/**
+ * Test suite for the {@link XNode} class.
+ */
+public class XNodeTest {
+
+    /**
+     * Verifies that newXNode throws a NullPointerException when passed a null Node.
+     * This is expected because the XNode constructor requires a non-null Node to operate on.
+     */
+    @Test(expected = NullPointerException.class)
+    public void newXNodeShouldThrowNullPointerExceptionForNullNode() {
+        // Arrange: Create a valid XNode instance to work with.
+        // The contents of the parser and node are not relevant to this specific test.
+        XPathParser xPathParser = new XPathParser((Document) null, false);
+        Node dummyNode = new IIOMetadataNode();
+        Properties variables = new Properties();
+        XNode xNode = new XNode(xPathParser, dummyNode, variables);
+
+        // Act: Call the method under test with a null argument.
+        xNode.newXNode(null);
+
+        // Assert: The test passes if a NullPointerException is thrown,
+        // which is handled by the @Test(expected) annotation.
     }
 }
