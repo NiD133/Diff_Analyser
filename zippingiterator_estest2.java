46912ff445bd7a6f827725cc7eaa27ddaa51f3fa
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:30:44 +0800

    gemini processedAutoTestCases zippingiterator_estest2.java

--- zippingiterator_estest2.java
+++ zippingiterator_estest2.java
@@ -1,28 +1,48 @@
 package org.apache.commons.collections4.iterators;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.ConcurrentModificationException;
+
+import java.util.Arrays;
 import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.ListIterator;
-import java.util.NoSuchElementException;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class ZippingIterator_ESTestTest2 extends ZippingIterator_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
-    @Test(timeout = 4000)
-    public void test01() throws Throwable {
-        LinkedList<Integer> linkedList0 = new LinkedList<Integer>();
-        linkedList0.add((Integer) null);
-        Iterator<Integer> iterator0 = linkedList0.descendingIterator();
-        ZippingIterator<Object> zippingIterator0 = new ZippingIterator<Object>(iterator0, iterator0);
-        Object object0 = zippingIterator0.next();
-        assertNull(object0);
+/**
+ * Unit tests for {@link ZippingIterator}.
+ */
+public class ZippingIteratorTest {
+
+    /**
+     * Tests that the ZippingIterator correctly returns a null element
+     * when one of its underlying iterators provides it.
+     */
+    @Test
+    public void nextShouldReturnNullWhenUnderlyingIteratorProvidesNull() {
+        // Arrange: Create two iterators to be zipped. The first contains a null element,
+        // and the second contains a non-null element.
+        final Iterator<String> iteratorA = Arrays.asList((String) null).iterator();
+        final Iterator<String> iteratorB = Arrays.asList("B").iterator();
+
+        final ZippingIterator<String> zippingIterator = new ZippingIterator<>(iteratorA, iteratorB);
+
+        // Assert that the iterator has elements before we start.
+        assertTrue("Iterator should have elements before iteration", zippingIterator.hasNext());
+
+        // Act & Assert: First call to next() should return the element from iteratorA.
+        final String firstElement = zippingIterator.next();
+        assertNull("The first element from iteratorA should be null", firstElement);
+
+        // The iterator should still have the element from iteratorB.
+        assertTrue("Iterator should still have an element from iteratorB", zippingIterator.hasNext());
+
+        // Act & Assert: Second call to next() should return the element from iteratorB.
+        final String secondElement = zippingIterator.next();
+        assertEquals("The second element from iteratorB should be 'B'", "B", secondElement);
+
+        // The iterator should now be exhausted.
+        assertFalse("Iterator should be exhausted after iterating all elements", zippingIterator.hasNext());
     }
 }
