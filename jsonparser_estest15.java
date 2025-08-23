1f6f52df522dec20b221018174ac0ba36c420e91
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:57:10 +0800

    Original processedAutoTestCases jsonparser_estest15.java

---
+++ jsonparser_estest15.java
@@ -0,0 +1,28 @@
+package com.google.gson;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.google.gson.stream.JsonReader;
+import java.io.Reader;
+import java.io.StringReader;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class JsonParser_ESTestTest15 extends JsonParser_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test14() throws Throwable {
+        // Undeclared exception!
+        try {
+            JsonParser.parseReader((JsonReader) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("com.google.gson.JsonParser", e);
+        }
+    }
+}
