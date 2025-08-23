3b2ce35db6d314d7bef95c0bcc7d3e71a0e97625
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:06:00 +0800

    Original processedAutoTestCases observableinputstream_estest35.java

---
+++ observableinputstream_estest35.java
@@ -0,0 +1,44 @@
+package org.apache.commons.io.input;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.FileDescriptor;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.io.PushbackInputStream;
+import java.io.SequenceInputStream;
+import java.io.StringWriter;
+import java.nio.CharBuffer;
+import java.nio.file.NoSuchFileException;
+import java.security.MessageDigest;
+import java.util.Enumeration;
+import java.util.LinkedList;
+import java.util.List;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.io.MockFileInputStream;
+import org.evosuite.runtime.mock.java.io.MockIOException;
+import org.junit.runner.RunWith;
+
+public class ObservableInputStream_ESTestTest35 extends ObservableInputStream_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test34() throws Throwable {
+        TimestampedObserver timestampedObserver0 = new TimestampedObserver();
+        MockIOException mockIOException0 = new MockIOException("org.apache.commons.io.input.ObservableInputStream$Builder");
+        try {
+            timestampedObserver0.error(mockIOException0);
+            fail("Expecting exception: IOException");
+        } catch (IOException e) {
+        }
+    }
+}
