df2802b3818dd74aabeee4f6ad0a28edfb3cbd0f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:03:26 +0800

    Original processedAutoTestCases minutes_estest73.java

---
+++ minutes_estest73.java
@@ -0,0 +1,26 @@
+package org.joda.time;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class Minutes_ESTestTest73 extends Minutes_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test72() throws Throwable {
+        Minutes minutes0 = Minutes.MIN_VALUE;
+        // Undeclared exception!
+        try {
+            minutes0.negated();
+            fail("Expecting exception: ArithmeticException");
+        } catch (ArithmeticException e) {
+            //
+            // Integer.MIN_VALUE cannot be negated
+            //
+            verifyException("org.joda.time.field.FieldUtils", e);
+        }
+    }
+}
