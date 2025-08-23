4aa943b760cbbcc042ef322377ee646b9e161a5f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:45:43 +0800

    Original processedAutoTestCases converterset_estest17.java

---
+++ converterset_estest17.java
@@ -0,0 +1,34 @@
+package org.joda.time.convert;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.joda.time.Hours;
+import org.joda.time.Interval;
+import org.joda.time.MutablePeriod;
+import org.joda.time.PeriodType;
+import org.joda.time.Seconds;
+import org.joda.time.chrono.CopticChronology;
+import org.junit.runner.RunWith;
+
+public class ConverterSet_ESTestTest17 extends ConverterSet_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test16() throws Throwable {
+        Hours hours0 = Hours.ONE;
+        Seconds seconds0 = hours0.toStandardSeconds();
+        PeriodType periodType0 = seconds0.getPeriodType();
+        MutablePeriod mutablePeriod0 = null;
+        try {
+            mutablePeriod0 = new MutablePeriod(periodType0, periodType0);
+            fail("Expecting exception: IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //
+            // No period converter found for type: org.joda.time.PeriodType
+            //
+            verifyException("org.joda.time.convert.ConverterManager", e);
+        }
+    }
+}
