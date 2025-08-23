f8b06c304d4f5da2c555c7a2188d2648264f904a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:06:31 +0800

    Original processedAutoTestCases optionformatter_estest4.java

---
+++ optionformatter_estest4.java
@@ -0,0 +1,28 @@
+package org.apache.commons.cli.help;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.function.BiFunction;
+import org.apache.commons.cli.Option;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class OptionFormatter_ESTestTest4 extends OptionFormatter_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test03() throws Throwable {
+        Option option0 = new Option("NO_ARGS_ALLOWED", "iRi[{-|Um");
+        OptionFormatter optionFormatter0 = OptionFormatter.from(option0);
+        OptionFormatter.Builder optionFormatter_Builder0 = new OptionFormatter.Builder(optionFormatter0);
+        BiFunction<OptionFormatter, Boolean, String> biFunction0 = (BiFunction<OptionFormatter, Boolean, String>) mock(BiFunction.class, new ViolatedAssumptionAnswer());
+        doReturn((Object) null).when(biFunction0).apply(any(org.apache.commons.cli.help.OptionFormatter.class), anyBoolean());
+        optionFormatter_Builder0.setSyntaxFormatFunction(biFunction0);
+        OptionFormatter optionFormatter1 = optionFormatter_Builder0.build(option0);
+        String string0 = optionFormatter1.toSyntaxOption(false);
+        assertNull(string0);
+    }
+}
