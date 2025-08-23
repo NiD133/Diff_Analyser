d844c1ce104ca63bd4aaa23894c964db14dd2aca
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:29:35 +0800

    Original processedManualTestCases intervalcategorytooltipgeneratortest4.java

---
+++ intervalcategorytooltipgeneratortest4.java
@@ -0,0 +1,26 @@
+package org.jfree.chart.labels;
+
+import java.text.DateFormat;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import org.jfree.chart.TestUtils;
+import org.jfree.chart.internal.CloneUtils;
+import org.jfree.chart.api.PublicCloneable;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class IntervalCategoryToolTipGeneratorTestTest4 {
+
+    /**
+     * Confirm that cloning works.
+     */
+    @Test
+    public void testCloning() throws CloneNotSupportedException {
+        IntervalCategoryToolTipGenerator g1 = new IntervalCategoryToolTipGenerator();
+        IntervalCategoryToolTipGenerator g2 = CloneUtils.clone(g1);
+        assertNotSame(g1, g2);
+        assertSame(g1.getClass(), g2.getClass());
+        assertEquals(g1, g2);
+    }
+}
