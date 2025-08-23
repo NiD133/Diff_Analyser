a9df1bfe1e6a8ed509e14b6d5ec807bc272ecd48
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:26:21 +0800

    gemini processedManualTestCases caseformattest19.java

--- caseformattest19.java
+++ caseformattest19.java
@@ -1,24 +1,43 @@
 package com.google.common.base;
 
-import static com.google.common.base.CaseFormat.LOWER_CAMEL;
-import static com.google.common.base.CaseFormat.LOWER_HYPHEN;
 import static com.google.common.base.CaseFormat.LOWER_UNDERSCORE;
 import static com.google.common.base.CaseFormat.UPPER_CAMEL;
-import static com.google.common.base.CaseFormat.UPPER_UNDERSCORE;
 import static com.google.common.truth.Truth.assertThat;
-import static com.google.common.truth.Truth.assertWithMessage;
-import com.google.common.annotations.GwtCompatible;
-import com.google.common.annotations.GwtIncompatible;
-import com.google.common.annotations.J2ktIncompatible;
-import com.google.common.testing.NullPointerTester;
-import com.google.common.testing.SerializableTester;
-import junit.framework.TestCase;
-import org.jspecify.annotations.NullUnmarked;
 
-public class CaseFormatTestTest19 extends TestCase {
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
-    public void testUpperCamelToLowerUnderscore() {
-        assertThat(UPPER_CAMEL.to(LOWER_UNDERSCORE, "Foo")).isEqualTo("foo");
-        assertThat(UPPER_CAMEL.to(LOWER_UNDERSCORE, "FooBar")).isEqualTo("foo_bar");
-    }
+/**
+ * Tests for {@link CaseFormat}, focusing on the conversion from {@code UPPER_CAMEL} to {@code
+ * LOWER_UNDERSCORE}.
+ */
+@RunWith(JUnit4.class)
+public class CaseFormatTest {
+
+  @Test
+  public void upperCamelToLowerUnderscore_convertsSingleWord() {
+    // Arrange
+    String input = "Foo";
+    String expected = "foo";
+
+    // Act
+    String result = UPPER_CAMEL.to(LOWER_UNDERSCORE, input);
+
+    // Assert
+    assertThat(result).isEqualTo(expected);
+  }
+
+  @Test
+  public void upperCamelToLowerUnderscore_convertsMultipleWords() {
+    // Arrange
+    String input = "FooBar";
+    String expected = "foo_bar";
+
+    // Act
+    String result = UPPER_CAMEL.to(LOWER_UNDERSCORE, input);
+
+    // Assert
+    assertThat(result).isEqualTo(expected);
+  }
 }
