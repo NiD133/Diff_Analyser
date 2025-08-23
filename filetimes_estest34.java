cdae1605ba0aba21efb8a4318c6bff5a1abfabb0
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:50:50 +0800

    Original processedAutoTestCases filetimes_estest34.java

---
+++ filetimes_estest34.java
@@ -0,0 +1,32 @@
+package org.apache.commons.io.file.attribute;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.math.BigDecimal;
+import java.nio.file.Path;
+import java.nio.file.attribute.FileTime;
+import java.time.DateTimeException;
+import java.time.Instant;
+import java.util.Date;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.time.MockInstant;
+import org.junit.runner.RunWith;
+
+public class FileTimes_ESTestTest34 extends FileTimes_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test33() throws Throwable {
+        // Undeclared exception!
+        try {
+            FileTimes.minusNanos((FileTime) null, 0L);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.apache.commons.io.file.attribute.FileTimes", e);
+        }
+    }
+}
