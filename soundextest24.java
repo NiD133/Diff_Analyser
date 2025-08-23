fd188a339d8e5a54d22e1e8723751c3c2fa1a185
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:33:55 +0800

    gemini processedManualTestCases soundextest24.java

--- soundextest24.java
+++ soundextest24.java
@@ -1,13 +1,15 @@
 package org.apache.commons.codec.language;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.apache.commons.codec.AbstractStringEncoderTest;
-import org.apache.commons.codec.EncoderException;
 import org.junit.jupiter.api.Test;
 
-public class SoundexTestTest24 extends AbstractStringEncoderTest<Soundex> {
+/**
+ * Tests the {@link Soundex} class.
+ */
+// Renamed from SoundexTestTest24 for clarity and to follow standard naming conventions.
+public class SoundexTest extends AbstractStringEncoderTest<Soundex> {
 
     @Override
     protected Soundex createStringEncoder() {
@@ -15,10 +17,23 @@ public class SoundexTestTest24 extends AbstractStringEncoderTest<Soundex> {
     }
 
     /**
-     * https://issues.apache.org/jira/browse/CODEC-54 https://issues.apache.org/jira/browse/CODEC-56
+     * Tests that the static {@link Soundex#US_ENGLISH} instance correctly encodes a name.
+     * This test case was added to address issues reported in CODEC-54 and CODEC-56.
+     *
+     * @see <a href="https://issues.apache.org/jira/browse/CODEC-54">CODEC-54</a>
+     * @see <a href="https://issues.apache.org/jira/browse/CODEC-56">CODEC-56</a>
      */
     @Test
-    void testUsEnglishStatic() {
-        assertEquals("W452", Soundex.US_ENGLISH.soundex("Williams"));
+    // Renamed from testUsEnglishStatic to better describe the behavior being tested.
+    void shouldEncodeNameUsingUsEnglishStaticInstance() {
+        // Arrange
+        final String nameToEncode = "Williams";
+        final String expectedSoundex = "W452";
+
+        // Act
+        final String actualSoundex = Soundex.US_ENGLISH.soundex(nameToEncode);
+
+        // Assert
+        assertEquals(expectedSoundex, actualSoundex, "Encoding 'Williams' with US_ENGLISH Soundex should produce 'W452'");
     }
 }
