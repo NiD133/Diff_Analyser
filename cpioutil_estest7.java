28c00e9e61e0d8d138c59810290a73a04741b999
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:46:18 +0800

    gemini processedAutoTestCases cpioutil_estest7.java

--- cpioutil_estest7.java
+++ cpioutil_estest7.java
@@ -1,26 +1,23 @@
 package org.apache.commons.compress.archivers.cpio;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class CpioUtil_ESTestTest7 extends CpioUtil_ESTest_scaffolding {
+/**
+ * Unit tests for the CpioUtil class.
+ */
+public class CpioUtilTest {
 
-    @Test(timeout = 4000)
-    public void test06() throws Throwable {
-        byte[] byteArray0 = new byte[0];
-        // Undeclared exception!
-        try {
-            CpioUtil.byteArray2long(byteArray0, false);
-            fail("Expecting exception: ArrayIndexOutOfBoundsException");
-        } catch (ArrayIndexOutOfBoundsException e) {
-            //
-            // 0
-            //
-            verifyException("org.apache.commons.compress.archivers.cpio.CpioUtil", e);
-        }
+    /**
+     * Tests that byteArray2long throws an ArrayIndexOutOfBoundsException
+     * when called with an empty byte array, as it cannot read any data.
+     */
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void byteArray2longShouldThrowExceptionForEmptyArray() {
+        // Arrange: Create an empty byte array
+        byte[] emptyArray = new byte[0];
+
+        // Act: Attempt to convert the empty array to a long.
+        // Assert: An ArrayIndexOutOfBoundsException is expected, verified by the @Test annotation.
+        CpioUtil.byteArray2long(emptyArray, false);
     }
 }
