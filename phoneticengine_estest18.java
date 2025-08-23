adc0797a099a6d20e60ff405acf4033095a5d44c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:08:46 +0800

    Original processedAutoTestCases phoneticengine_estest18.java

---
+++ phoneticengine_estest18.java
@@ -0,0 +1,24 @@
+package org.apache.commons.codec.language.bm;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.nio.CharBuffer;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class PhoneticEngine_ESTestTest18 extends PhoneticEngine_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test17() throws Throwable {
+        LinkedHashSet<String> linkedHashSet0 = new LinkedHashSet<String>();
+        Languages.LanguageSet languages_LanguageSet0 = Languages.LanguageSet.from(linkedHashSet0);
+        PhoneticEngine.PhonemeBuilder phoneticEngine_PhonemeBuilder0 = PhoneticEngine.PhonemeBuilder.empty(languages_LanguageSet0);
+        Rule.Phoneme rule_Phoneme0 = new Rule.Phoneme("", languages_LanguageSet0);
+        phoneticEngine_PhonemeBuilder0.apply(rule_Phoneme0, 600);
+        assertEquals(1, rule_Phoneme0.size());
+    }
+}
