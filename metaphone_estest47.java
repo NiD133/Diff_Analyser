65a16377ee692645c08e75aa270f2de107abad26
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:02:36 +0800

    Original processedAutoTestCases metaphone_estest47.java

---
+++ metaphone_estest47.java
@@ -0,0 +1,19 @@
+package org.apache.commons.codec.language;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class Metaphone_ESTestTest47 extends Metaphone_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test46() throws Throwable {
+        Metaphone metaphone0 = new Metaphone();
+        String string0 = metaphone0.metaphone("CGH");
+        assertEquals("K", string0);
+        assertEquals(4, metaphone0.getMaxCodeLen());
+    }
+}
