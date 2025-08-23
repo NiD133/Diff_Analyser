3d93e9b694d0ad62254984ab9709408777f5025e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:26:58 +0800

    gemini processedAutoTestCases w3cdom_estest52.java

--- w3cdom_estest52.java
+++ w3cdom_estest52.java
@@ -1,38 +1,23 @@
 package org.jsoup.helper;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import com.sun.org.apache.xerces.internal.dom.DocumentImpl;
-import java.io.Reader;
-import java.io.StringReader;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
-import javax.imageio.metadata.IIOMetadataNode;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.jsoup.nodes.Attributes;
-import org.jsoup.nodes.DataNode;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.DocumentType;
-import org.jsoup.nodes.Element;
-import org.jsoup.nodes.FormElement;
-import org.jsoup.nodes.XmlDeclaration;
-import org.jsoup.parser.Parser;
-import org.jsoup.parser.Tag;
-import org.junit.runner.RunWith;
-import org.w3c.dom.DOMException;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import static org.junit.Assert.assertTrue;
 
-public class W3CDom_ESTestTest52 extends W3CDom_ESTest_scaffolding {
+/**
+ * Tests for the {@link W3CDom} helper class.
+ */
+public class W3CDomTest {
 
-    @Test(timeout = 4000)
-    public void test51() throws Throwable {
-        W3CDom w3CDom0 = new W3CDom();
-        boolean boolean0 = w3CDom0.namespaceAware();
-        assertTrue(boolean0);
+    /**
+     * Verifies that a new W3CDom instance is configured to be namespace-aware by default.
+     * This is the expected initial state for the object.
+     */
+    @Test
+    public void newW3CDomIsNamespaceAwareByDefault() {
+        // Arrange: Create a new instance of the class under test.
+        W3CDom w3cDom = new W3CDom();
+
+        // Act & Assert: Check the default value of the namespaceAware property.
+        assertTrue("W3CDom should be namespace-aware by default upon instantiation.", w3cDom.namespaceAware());
     }
 }
