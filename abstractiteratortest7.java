696bc061572f52a24e5c521d59aa3903bad4b915
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:24:38 +0800

    Original processedManualTestCases abstractiteratortest7.java

---
+++ abstractiteratortest7.java
@@ -0,0 +1,31 @@
+package com.google.common.collect;
+
+import static com.google.common.collect.ReflectionFreeAssertThrows.assertThrows;
+import static com.google.common.collect.SneakyThrows.sneakyThrow;
+import com.google.common.annotations.GwtCompatible;
+import com.google.common.annotations.GwtIncompatible;
+import com.google.common.annotations.J2ktIncompatible;
+import com.google.common.collect.TestExceptions.SomeCheckedException;
+import com.google.common.collect.TestExceptions.SomeUncheckedException;
+import com.google.common.testing.GcFinalization;
+import java.lang.ref.WeakReference;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import junit.framework.TestCase;
+import org.jspecify.annotations.NullMarked;
+import org.jspecify.annotations.Nullable;
+
+public class AbstractIteratorTestTest7 extends TestCase {
+
+    public void testExceptionAfterEndOfData() {
+        Iterator<Integer> iter = new AbstractIterator<Integer>() {
+
+            @Override
+            public Integer computeNext() {
+                endOfData();
+                throw new SomeUncheckedException();
+            }
+        };
+        assertThrows(SomeUncheckedException.class, iter::hasNext);
+    }
+}
