c72279a9acca0fb5c9282da92e0c487d585117a4
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:30:53 +0800

    Original processedManualTestCases logfactorytest8.java

---
+++ logfactorytest8.java
@@ -0,0 +1,40 @@
+package org.apache.ibatis.logging;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.io.Reader;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.logging.commons.JakartaCommonsLoggingImpl;
+import org.apache.ibatis.logging.jdk14.Jdk14LoggingImpl;
+import org.apache.ibatis.logging.log4j.Log4jImpl;
+import org.apache.ibatis.logging.log4j2.Log4j2Impl;
+import org.apache.ibatis.logging.nologging.NoLoggingImpl;
+import org.apache.ibatis.logging.slf4j.Slf4jImpl;
+import org.apache.ibatis.logging.stdout.StdOutImpl;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+
+public class LogFactoryTestTest8 {
+
+    @AfterAll
+    static void restore() {
+        LogFactory.useSlf4jLogging();
+    }
+
+    private void logSomething(Log log) {
+        log.warn("Warning message.");
+        log.debug("Debug message.");
+        log.error("Error message.");
+        log.error("Error with Exception.", new Exception("Test exception."));
+    }
+
+    @Test
+    void shouldReadLogImplFromSettings() throws Exception {
+        try (Reader reader = Resources.getResourceAsReader("org/apache/ibatis/logging/mybatis-config.xml")) {
+            new SqlSessionFactoryBuilder().build(reader);
+        }
+        Log log = LogFactory.getLog(Object.class);
+        log.debug("Debug message.");
+        assertEquals(log.getClass().getName(), NoLoggingImpl.class.getName());
+    }
+}
