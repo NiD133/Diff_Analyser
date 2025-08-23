e3401e231971f11ea9c01b054442222edbedda42
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:44:16 +0800

    gemini processedAutoTestCases collectionbag_estest44.java

--- collectionbag_estest44.java
+++ collectionbag_estest44.java
@@ -1,40 +1,30 @@
 package org.apache.commons.collections4.bag;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.ConcurrentModificationException;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Stack;
-import java.util.TreeSet;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.apache.commons.collections4.Bag;
-import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.SortedBag;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.FalsePredicate;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.MapTransformer;
-import org.apache.commons.collections4.functors.NullIsExceptionPredicate;
-import org.apache.commons.collections4.functors.TransformerPredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class CollectionBag_ESTestTest44 extends CollectionBag_ESTest_scaffolding {
+/**
+ * Tests for {@link CollectionBag}.
+ */
+public class CollectionBagTest {
 
-    @Test(timeout = 4000)
-    public void test43() throws Throwable {
-        HashBag<ComparatorPredicate.Criterion> hashBag0 = new HashBag<ComparatorPredicate.Criterion>();
-        Bag<ComparatorPredicate.Criterion> bag0 = CollectionBag.collectionBag((Bag<ComparatorPredicate.Criterion>) hashBag0);
-        assertEquals(0, bag0.size());
+    /**
+     * Tests that decorating an empty bag with CollectionBag.collectionBag()
+     * results in a new empty bag.
+     */
+    @Test
+    public void testCollectionBagFactoryWithEmptyBag() {
+        // Arrange: Create an empty source bag.
+        final Bag<String> sourceBag = new HashBag<>();
+
+        // Act: Decorate the empty bag using the factory method.
+        final Bag<String> decoratedBag = CollectionBag.collectionBag(sourceBag);
+
+        // Assert: The decorated bag should also be empty.
+        assertTrue("The decorated bag should be empty", decoratedBag.isEmpty());
+        assertEquals("The decorated bag should have a size of 0", 0, decoratedBag.size());
     }
 }
