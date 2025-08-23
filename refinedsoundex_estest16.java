aa3509ba6416d38141a11b0c07c0dfe30b1e2402
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:12:31 +0800

    gemini processedAutoTestCases refinedsoundex_estest16.java

--- refinedsoundex_estest16.java
+++ refinedsoundex_estest16.java
@@ -1,25 +1,33 @@
 package org.apache.commons.codec.language;
 
+import org.apache.commons.codec.EncoderException;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class RefinedSoundex_ESTestTest16 extends RefinedSoundex_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
-    @Test(timeout = 4000)
-    public void test15() throws Throwable {
-        RefinedSoundex refinedSoundex0 = new RefinedSoundex();
+/**
+ * Tests for the {@link RefinedSoundex} class, focusing on exception handling.
+ */
+public class RefinedSoundexTest {
+
+    /**
+     * Tests that the encode(Object) method throws an EncoderException when the input is null.
+     * The RefinedSoundex encoder is designed to work only with Strings.
+     */
+    @Test
+    public void encodeObjectWithNullShouldThrowEncoderException() {
+        // Arrange: Create an instance of the encoder.
+        final RefinedSoundex encoder = new RefinedSoundex();
+        final String expectedMessage = "Parameter supplied to RefinedSoundex encode is not of type java.lang.String";
+
+        // Act & Assert: Verify that encoding a null object throws the expected exception.
         try {
-            refinedSoundex0.encode((Object) null);
-            fail("Expecting exception: Exception");
-        } catch (Exception e) {
-            //
-            // Parameter supplied to RefinedSoundex encode is not of type java.lang.String
-            //
-            verifyException("org.apache.commons.codec.language.RefinedSoundex", e);
+            encoder.encode((Object) null);
+            fail("Expected an EncoderException to be thrown for a null input.");
+        } catch (final EncoderException e) {
+            // Verify that the exception is of the correct type and has the expected message.
+            assertEquals(expectedMessage, e.getMessage());
         }
     }
 }
