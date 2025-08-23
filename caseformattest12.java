b051b5764d7b87140ba1104630b9e4d572557ae0
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:26:20 +0800

    gemini processedManualTestCases caseformattest12.java

--- caseformattest12.java
+++ caseformattest12.java
@@ -1,24 +1,38 @@
 package com.google.common.base;
 
-import static com.google.common.base.CaseFormat.LOWER_CAMEL;
-import static com.google.common.base.CaseFormat.LOWER_HYPHEN;
 import static com.google.common.base.CaseFormat.LOWER_UNDERSCORE;
-import static com.google.common.base.CaseFormat.UPPER_CAMEL;
 import static com.google.common.base.CaseFormat.UPPER_UNDERSCORE;
 import static com.google.common.truth.Truth.assertThat;
-import static com.google.common.truth.Truth.assertWithMessage;
-import com.google.common.annotations.GwtCompatible;
-import com.google.common.annotations.GwtIncompatible;
-import com.google.common.annotations.J2ktIncompatible;
-import com.google.common.testing.NullPointerTester;
-import com.google.common.testing.SerializableTester;
+
 import junit.framework.TestCase;
-import org.jspecify.annotations.NullUnmarked;
 
-public class CaseFormatTestTest12 extends TestCase {
+/**
+ * Tests for conversions from {@link CaseFormat#LOWER_UNDERSCORE} to {@link
+ * CaseFormat#UPPER_UNDERSCORE}.
+ */
+public class CaseFormatTest extends TestCase {
 
-    public void testLowerUnderscoreToUpperUnderscore() {
-        assertThat(LOWER_UNDERSCORE.to(UPPER_UNDERSCORE, "foo")).isEqualTo("FOO");
-        assertThat(LOWER_UNDERSCORE.to(UPPER_UNDERSCORE, "foo_bar")).isEqualTo("FOO_BAR");
-    }
+  public void testLowerUnderscoreToUpperUnderscore_forSingleWord() {
+    // Arrange: Define the input and the expected outcome.
+    String input = "foo";
+    String expectedOutput = "FOO";
+
+    // Act: Perform the conversion.
+    String actualOutput = LOWER_UNDERSCORE.to(UPPER_UNDERSCORE, input);
+
+    // Assert: Verify the result is as expected.
+    assertThat(actualOutput).isEqualTo(expectedOutput);
+  }
+
+  public void testLowerUnderscoreToUpperUnderscore_forMultipleWords() {
+    // Arrange
+    String input = "foo_bar";
+    String expectedOutput = "FOO_BAR";
+
+    // Act
+    String actualOutput = LOWER_UNDERSCORE.to(UPPER_UNDERSCORE, input);
+
+    // Assert
+    assertThat(actualOutput).isEqualTo(expectedOutput);
+  }
 }
