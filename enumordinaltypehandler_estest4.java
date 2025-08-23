5dcffb7d1d2062bdf708a965c9b1922d5168c258
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:49:39 +0800

    Original processedAutoTestCases enumordinaltypehandler_estest4.java

---
+++ enumordinaltypehandler_estest4.java
@@ -0,0 +1,34 @@
+package org.apache.ibatis.type;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class EnumOrdinalTypeHandler_ESTestTest4 extends EnumOrdinalTypeHandler_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test03() throws Throwable {
+        Class<JdbcType> class0 = JdbcType.class;
+        EnumOrdinalTypeHandler<JdbcType> enumOrdinalTypeHandler0 = new EnumOrdinalTypeHandler<JdbcType>(class0);
+        ResultSet resultSet0 = mock(ResultSet.class, new ViolatedAssumptionAnswer());
+        doReturn((-874)).when(resultSet0).getInt(anyString());
+        // Undeclared exception!
+        try {
+            enumOrdinalTypeHandler0.getNullableResult(resultSet0, "");
+            fail("Expecting exception: IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //
+            // Cannot convert -874 to JdbcType by ordinal value.
+            //
+            verifyException("org.apache.ibatis.type.EnumOrdinalTypeHandler", e);
+        }
+    }
+}
