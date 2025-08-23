8b3c711f50e75ae1d1eb25307dddc42ecdcfd532
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:05:34 +0800

    gemini processedAutoTestCases numericentityunescaper_estest15.java

--- numericentityunescaper_estest15.java
+++ numericentityunescaper_estest15.java
@@ -1,29 +1,23 @@
 package org.apache.commons.lang3.text.translate;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.StringWriter;
-import java.io.Writer;
-import java.nio.CharBuffer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class NumericEntityUnescaper_ESTestTest15 extends NumericEntityUnescaper_ESTest_scaffolding {
+/**
+ * Tests for {@link NumericEntityUnescaper}.
+ */
+public class NumericEntityUnescaperTest {
 
-    @Test(timeout = 4000)
-    public void test14() throws Throwable {
-        NumericEntityUnescaper.OPTION[] numericEntityUnescaper_OPTIONArray0 = new NumericEntityUnescaper.OPTION[1];
-        NumericEntityUnescaper numericEntityUnescaper0 = null;
-        try {
-            numericEntityUnescaper0 = new NumericEntityUnescaper(numericEntityUnescaper_OPTIONArray0);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("java.util.EnumSet", e);
-        }
+    /**
+     * Tests that the constructor throws a NullPointerException when the options array contains a null element.
+     * This is because the constructor internally uses EnumSet.copyOf, which does not permit null elements.
+     */
+    @Test(expected = NullPointerException.class)
+    public void constructorShouldThrowNPEWhenOptionsArrayContainsNull() {
+        // Arrange: Create an array of options that includes a null value.
+        final NumericEntityUnescaper.OPTION[] optionsWithNull = { null };
+
+        // Act: Attempt to create an instance with the invalid options.
+        // An exception is expected.
+        new NumericEntityUnescaper(optionsWithNull);
     }
 }
