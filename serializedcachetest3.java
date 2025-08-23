012c80ba89e2d69553d71f6a9f61725b7d4ed996
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:33:34 +0800

    Original processedManualTestCases serializedcachetest3.java

---
+++ serializedcachetest3.java
@@ -0,0 +1,73 @@
+package org.apache.ibatis.cache;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.io.Serializable;
+import java.util.Objects;
+import org.apache.ibatis.cache.decorators.SerializedCache;
+import org.apache.ibatis.cache.impl.PerpetualCache;
+import org.junit.jupiter.api.Test;
+
+public class SerializedCacheTestTest3 {
+
+    static class CachingObject implements Serializable {
+
+        private static final long serialVersionUID = 1L;
+
+        int x;
+
+        public CachingObject(int x) {
+            this.x = x;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            CachingObject obj = (CachingObject) o;
+            return x == obj.x;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x);
+        }
+    }
+
+    static class CachingObjectWithoutSerializable {
+
+        int x;
+
+        public CachingObjectWithoutSerializable(int x) {
+            this.x = x;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            CachingObjectWithoutSerializable obj = (CachingObjectWithoutSerializable) o;
+            return x == obj.x;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x);
+        }
+    }
+
+    @Test
+    void throwExceptionWhenTryingToCacheNonSerializableObject() {
+        SerializedCache cache = new SerializedCache(new PerpetualCache("default"));
+        assertThrows(CacheException.class, () -> cache.putObject(0, new CachingObjectWithoutSerializable(0)));
+    }
+}
