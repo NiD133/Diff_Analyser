92cd3fff1a69cfc1e8f6a84ca9be07c8e8388dd1
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:53:30 +0800

    gemini processedAutoTestCases ieee754rutils_estest25.java

--- ieee754rutils_estest25.java
+++ ieee754rutils_estest25.java
@@ -1,25 +1,20 @@
 package org.apache.commons.lang3.math;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class IEEE754rUtils_ESTestTest25 extends IEEE754rUtils_ESTest_scaffolding {
+/**
+ * Unit tests for {@link IEEE754rUtils}.
+ */
+public class IEEE754rUtilsTest {
 
-    @Test(timeout = 4000)
-    public void test24() throws Throwable {
-        // Undeclared exception!
-        try {
-            IEEE754rUtils.min((float[]) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // array
-            //
-            verifyException("java.util.Objects", e);
-        }
+    /**
+     * Tests that min(float...) throws a NullPointerException when the input array is null,
+     * as specified by the method's contract.
+     */
+    @Test(expected = NullPointerException.class)
+    public void minFloatArrayShouldThrowNullPointerExceptionForNullInput() {
+        // The cast to (float[]) is necessary to resolve ambiguity and select the
+        // correct varargs method overload, min(float...), when passing null.
+        IEEE754rUtils.min((float[]) null);
     }
 }
