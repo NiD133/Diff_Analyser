6d837a1f050440b6885734cc10da241d53cbc53f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:29:33 +0800

    Original processedAutoTestCases xydrawableannotation_estest21.java

---
+++ xydrawableannotation_estest21.java
@@ -0,0 +1,51 @@
+package org.jfree.chart.annotations;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.RoundRectangle2D;
+import java.awt.image.BufferedImage;
+import java.math.BigInteger;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.jfree.chart.ChartRenderingInfo;
+import org.jfree.chart.Drawable;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.CyclicNumberAxis;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.axis.ValueAxis;
+import org.jfree.chart.block.BlockContainer;
+import org.jfree.chart.legend.LegendTitle;
+import org.jfree.chart.plot.MeterPlot;
+import org.jfree.chart.plot.PlotRenderingInfo;
+import org.jfree.chart.plot.PolarAxisLocation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.AbstractRenderer;
+import org.jfree.chart.renderer.category.ScatterRenderer;
+import org.jfree.chart.renderer.xy.XYBarRenderer;
+import org.jfree.chart.title.CompositeTitle;
+import org.jfree.chart.title.DateTitle;
+import org.jfree.chart.title.ShortTextTitle;
+import org.jfree.chart.title.TextTitle;
+import org.jfree.data.general.DefaultValueDataset;
+import org.junit.runner.RunWith;
+
+public class XYDrawableAnnotation_ESTestTest21 extends XYDrawableAnnotation_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test20() throws Throwable {
+        XYDrawableAnnotation xYDrawableAnnotation0 = null;
+        try {
+            xYDrawableAnnotation0 = new XYDrawableAnnotation((-98.734775112), (-98.734775112), (-98.734775112), (-98.734775112), (-98.734775112), (Drawable) null);
+            fail("Expecting exception: IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //
+            // Null 'drawable' argument.
+            //
+            verifyException("org.jfree.chart.internal.Args", e);
+        }
+    }
+}
