5518d84d745fd0b4fec812f194b24dcc3ffd6904
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:48:32 +0800

    Original processedAutoTestCases elements_estest153.java

---
+++ elements_estest153.java
@@ -0,0 +1,35 @@
+package org.jsoup.select;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.ConcurrentModificationException;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.jsoup.nodes.Comment;
+import org.jsoup.nodes.DataNode;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.FormElement;
+import org.jsoup.nodes.TextNode;
+import org.jsoup.parser.Parser;
+import org.junit.runner.RunWith;
+
+public class Elements_ESTestTest153 extends Elements_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test152() throws Throwable {
+        Elements elements0 = new Elements();
+        ArrayList<Element> arrayList0 = elements0.asList();
+        assertTrue(arrayList0.isEmpty());
+    }
+}
