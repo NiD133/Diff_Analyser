825c1cae91de6c9e2bc417c622c250dce41156b0
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:17:49 +0800

    gemini processedAutoTestCases standardbarpainter_estest21.java

--- standardbarpainter_estest21.java
+++ standardbarpainter_estest21.java
@@ -1,26 +1,29 @@
 package org.jfree.chart.renderer.category;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.awt.Graphics2D;
-import java.awt.geom.Arc2D;
-import java.awt.geom.Ellipse2D;
-import java.awt.geom.Rectangle2D;
-import java.awt.geom.RectangularShape;
-import java.awt.image.BufferedImage;
-import javax.swing.text.DefaultCaret;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.jfree.chart.api.RectangleEdge;
-import org.jfree.chart.util.GradientPaintTransformer;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class StandardBarPainter_ESTestTest21 extends StandardBarPainter_ESTest_scaffolding {
+/**
+ * Tests for the {@link StandardBarPainter} class.
+ */
+public class StandardBarPainterTest {
 
-    @Test(timeout = 4000)
-    public void test20() throws Throwable {
-        StandardBarPainter standardBarPainter0 = new StandardBarPainter();
-        standardBarPainter0.hashCode();
+    /**
+     * Verifies that the hashCode() method is consistent for different instances.
+     * <p>
+     * The {@link StandardBarPainter} class is stateless. According to the
+     * {@code Object.hashCode()} contract, two objects that are equal must have
+     * the same hash code. Therefore, any two instances of StandardBarPainter
+     * should be considered equal and produce the same hash code.
+     */
+    @Test
+    public void hashCode_shouldBeConsistentAcrossInstances() {
+        // Arrange: Create two separate instances of the stateless painter.
+        StandardBarPainter painter1 = new StandardBarPainter();
+        StandardBarPainter painter2 = new StandardBarPainter();
+
+        // Act & Assert: The hash codes should be identical.
+        assertEquals("Hash codes of two stateless StandardBarPainter instances must be equal.",
+                painter1.hashCode(), painter2.hashCode());
     }
 }
