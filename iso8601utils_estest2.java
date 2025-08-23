f54ebcc55084ef42056aed49d7eba659599bff36
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:55:32 +0800

    Original processedAutoTestCases iso8601utils_estest2.java

---
+++ iso8601utils_estest2.java
@@ -0,0 +1,24 @@
+package com.google.gson.internal.bind.util;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.text.ParseException;
+import java.text.ParsePosition;
+import java.util.Date;
+import java.util.SimpleTimeZone;
+import java.util.TimeZone;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.util.MockDate;
+import org.junit.runner.RunWith;
+
+public class ISO8601Utils_ESTestTest2 extends ISO8601Utils_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test01() throws Throwable {
+        MockDate mockDate0 = new MockDate((-2147483646), (-2147483646), (-2147483646), (-2147483646), (-2147483646));
+        String string0 = ISO8601Utils.format((Date) mockDate0);
+        assertEquals("208737754-05-17T20:36:25Z", string0);
+    }
+}
