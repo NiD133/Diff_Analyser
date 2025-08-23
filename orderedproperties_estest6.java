f505f40c0d83d4de2ad1524a4915785222fa4e21
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:06:53 +0800

    gemini processedAutoTestCases orderedproperties_estest6.java

--- orderedproperties_estest6.java
+++ orderedproperties_estest6.java
@@ -1,53 +1,37 @@
 package org.apache.commons.collections4.properties;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
+
+import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.LinkedHashSet;
 import java.util.Map;
-import java.util.NoSuchElementException;
 import java.util.Set;
-import java.util.function.BiConsumer;
-import java.util.function.BiFunction;
-import java.util.function.Function;
-import org.apache.commons.collections4.Equator;
-import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AllPredicate;
-import org.apache.commons.collections4.functors.CloneTransformer;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.DefaultEquator;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.NOPTransformer;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NotNullPredicate;
-import org.apache.commons.collections4.functors.NullIsTruePredicate;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.SwitchTransformer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
 
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Tests for {@link OrderedProperties}.
+ * This test focuses on the behavior of the entrySet after loading properties from a Reader.
+ */
 public class OrderedProperties_ESTestTest6 extends OrderedProperties_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test05() throws Throwable {
-        OrderedProperties orderedProperties0 = new OrderedProperties();
-        StringReader stringReader0 = new StringReader("N\"e,'{*4`uWJGD{G");
-        orderedProperties0.load((Reader) stringReader0);
-        Set<Map.Entry<Object, Object>> set0 = (Set<Map.Entry<Object, Object>>) orderedProperties0.entrySet();
-        assertFalse(set0.isEmpty());
+    /**
+     * Tests that the entrySet is correctly populated after loading a property from a Reader.
+     */
+    @Test
+    public void entrySetShouldContainOneEntryAfterLoadingSingleProperty() throws IOException {
+        // Arrange
+        final OrderedProperties properties = new OrderedProperties();
+        final String propertyString = "test.key=test.value";
+        final Reader propertyReader = new StringReader(propertyString);
+
+        // Act
+        properties.load(propertyReader);
+        final Set<Map.Entry<Object, Object>> entrySet = properties.entrySet();
+
+        // Assert
+        // Verify that exactly one entry has been loaded.
+        assertEquals(1, entrySet.size());
     }
 }
