585c9013217e9f58bdb89395c048d2aacf4d810c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:30:13 +0800

    gemini processedAutoTestCases years_estest16.java

--- years_estest16.java
+++ years_estest16.java
@@ -1,19 +1,28 @@
 package org.joda.time;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class Years_ESTestTest16 extends Years_ESTest_scaffolding {
+/**
+ * Unit tests for the Years class.
+ */
+public class YearsTest {
 
-    @Test(timeout = 4000)
-    public void test15() throws Throwable {
-        Years years0 = Years.years(0);
-        Years years1 = years0.TWO.plus(2);
-        int int0 = years1.getYears();
-        assertEquals(4, int0);
+    /**
+     * Verifies that the plus(int) method correctly adds a given number of years
+     * to an existing Years instance.
+     */
+    @Test
+    public void plus_addsYearsToInstance() {
+        // Arrange: Start with a Years instance representing 2 years.
+        Years twoYears = Years.TWO;
+        int yearsToAdd = 2;
+        int expectedTotalYears = 4;
+
+        // Act: Add 2 more years.
+        Years result = twoYears.plus(yearsToAdd);
+
+        // Assert: The new instance should represent 4 years.
+        assertEquals(expectedTotalYears, result.getYears());
     }
 }
