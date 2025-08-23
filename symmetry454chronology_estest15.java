efd18de40fcdb280e879d5b2669b8a19454fc2f0
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:19:49 +0800

    gemini processedAutoTestCases symmetry454chronology_estest15.java

--- symmetry454chronology_estest15.java
+++ symmetry454chronology_estest15.java
@@ -1,44 +1,29 @@
 package org.threeten.extra.chrono;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.time.Clock;
-import java.time.DateTimeException;
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.OffsetDateTime;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
-import java.time.chrono.ChronoLocalDateTime;
-import java.time.chrono.ChronoZonedDateTime;
+import static org.junit.Assert.assertEquals;
+
 import java.time.chrono.Era;
-import java.time.chrono.HijrahEra;
 import java.time.chrono.IsoEra;
-import java.time.chrono.JapaneseEra;
-import java.time.temporal.ChronoField;
-import java.time.temporal.ChronoUnit;
-import java.time.temporal.TemporalAccessor;
-import java.time.temporal.TemporalUnit;
-import java.time.temporal.UnsupportedTemporalTypeException;
-import java.time.temporal.ValueRange;
-import java.util.List;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.System;
-import org.evosuite.runtime.mock.java.time.MockClock;
-import org.evosuite.runtime.mock.java.time.MockInstant;
-import org.evosuite.runtime.mock.java.time.MockLocalDateTime;
-import org.evosuite.runtime.mock.java.time.MockOffsetDateTime;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class Symmetry454Chronology_ESTestTest15 extends Symmetry454Chronology_ESTest_scaffolding {
+/**
+ * Tests for {@link Symmetry454Chronology}.
+ */
+public class Symmetry454ChronologyTest {
 
-    @Test(timeout = 4000)
-    public void test14() throws Throwable {
-        Symmetry454Chronology symmetry454Chronology0 = Symmetry454Chronology.INSTANCE;
-        IsoEra isoEra0 = IsoEra.CE;
-        Symmetry454Date symmetry454Date0 = symmetry454Chronology0.date((Era) isoEra0, 4, 4, 4);
-        assertEquals(IsoEra.CE, symmetry454Date0.getEra());
+    @Test
+    public void date_withEra_shouldCreateDateWithCorrectEra() {
+        // Arrange
+        Symmetry454Chronology chronology = Symmetry454Chronology.INSTANCE;
+        Era expectedEra = IsoEra.CE;
+        int yearOfEra = 4;
+        int month = 4;
+        int dayOfMonth = 4;
+
+        // Act
+        Symmetry454Date date = chronology.date(expectedEra, yearOfEra, month, dayOfMonth);
+
+        // Assert
+        assertEquals("The era of the created date should match the input era.", expectedEra, date.getEra());
     }
 }
