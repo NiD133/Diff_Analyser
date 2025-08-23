353a730ece5883e197f42eb7f2384bbff158a6b8
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:27:44 +0800

    Original processedAutoTestCases xmlstreamwriter_estest14.java

---
+++ xmlstreamwriter_estest14.java
@@ -0,0 +1,37 @@
+package org.apache.commons.io.output;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.io.Writer;
+import java.nio.CharBuffer;
+import java.nio.charset.IllegalCharsetNameException;
+import java.nio.charset.UnsupportedCharsetException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.evosuite.runtime.mock.java.io.MockPrintStream;
+import org.junit.runner.RunWith;
+
+public class XmlStreamWriter_ESTestTest14 extends XmlStreamWriter_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test13() throws Throwable {
+        XmlStreamWriter xmlStreamWriter0 = null;
+        try {
+            xmlStreamWriter0 = new XmlStreamWriter((File) null, "org.apache.commons.io.output.XmlStreamWriter$Builder");
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("java.io.File", e);
+        }
+    }
+}
