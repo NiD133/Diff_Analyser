e4bccbe9dfcc9309ab67fcdc2795c31f6a5ffc29
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:38:31 +0800

    gemini processedAutoTestCases backgroundinitializer_estest11.java

--- backgroundinitializer_estest11.java
+++ backgroundinitializer_estest11.java
@@ -1,40 +1,35 @@
 package org.apache.commons.lang3.concurrent;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.concurrent.Delayed;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.RejectedExecutionException;
-import java.util.concurrent.SynchronousQueue;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.evosuite.runtime.mock.java.lang.MockException;
-import org.evosuite.runtime.mock.java.lang.MockThrowable;
-import org.evosuite.runtime.testdata.FileSystemHandling;
-import org.junit.runner.RunWith;
 
-public class BackgroundInitializer_ESTestTest11 extends BackgroundInitializer_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
-    @Test(timeout = 4000)
-    public void test10() throws Throwable {
-        ForkJoinPool forkJoinPool0 = new ForkJoinPool();
-        BackgroundInitializer<Object> backgroundInitializer0 = new BackgroundInitializer<Object>(forkJoinPool0);
-        // Undeclared exception!
-        try {
-            backgroundInitializer0.getFuture();
-            fail("Expecting exception: IllegalStateException");
-        } catch (IllegalStateException e) {
-            //
-            // start() must be called first!
-            //
-            verifyException("org.apache.commons.lang3.concurrent.BackgroundInitializer", e);
-        }
+/**
+ * Tests for {@link BackgroundInitializer}.
+ */
+public class BackgroundInitializerTest {
+
+    /**
+     * Tests that calling getFuture() before start() throws an IllegalStateException.
+     */
+    @Test
+    public void getFutureShouldThrowIllegalStateExceptionWhenNotStarted() {
+        // Arrange: Create a BackgroundInitializer but do not start it.
+        final ExecutorService executor = new ForkJoinPool();
+        final BackgroundInitializer<Object> initializer = new BackgroundInitializer<>(executor);
+
+        // Act & Assert: Verify that calling getFuture() throws the expected exception.
+        final IllegalStateException thrown = assertThrows(
+            "getFuture() should throw an exception if start() has not been called.",
+            IllegalStateException.class,
+            initializer::getFuture
+        );
+
+        // Further assert on the exception message to ensure it's the correct one.
+        assertEquals("start() must be called first!", thrown.getMessage());
     }
 }
