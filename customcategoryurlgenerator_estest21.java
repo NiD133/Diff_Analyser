faeb03d81d09a2038e1a4d7aeb7b6c7e47033d0f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:46:23 +0800

    Original processedAutoTestCases customcategoryurlgenerator_estest21.java

---
+++ customcategoryurlgenerator_estest21.java
@@ -0,0 +1,24 @@
+package org.jfree.chart.urls;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+import java.util.Vector;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.general.DefaultKeyedValues2DDataset;
+import org.junit.runner.RunWith;
+
+public class CustomCategoryURLGenerator_ESTestTest21 extends CustomCategoryURLGenerator_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test20() throws Throwable {
+        CustomCategoryURLGenerator customCategoryURLGenerator0 = new CustomCategoryURLGenerator();
+        boolean boolean0 = customCategoryURLGenerator0.equals(customCategoryURLGenerator0);
+        assertTrue(boolean0);
+    }
+}
