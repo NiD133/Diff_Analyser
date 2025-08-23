20abfd8c0c8e13dc17f831a9a7aeecacd51fe0e7
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 05:32:49 +0800

    gpt5 manualTestSuite ChartRenderingInfoTest.java

--- ChartRenderingInfoTest.java
+++ ChartRenderingInfoTest.java
@@ -1,133 +1,162 @@
-/* ======================================================
- * JFreeChart : a chart library for the Java(tm) platform
- * ======================================================
- *
- * (C) Copyright 2000-present, by David Gilbert and Contributors.
- *
- * Project Info:  https://www.jfree.org/jfreechart/index.html
- *
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 2.1 of the License, or
- * (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
- * License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
- * USA.
- *
- * [Oracle and Java are registered trademarks of Oracle and/or its affiliates. 
- * Other names may be trademarks of their respective owners.]
- *
- * ---------------------------
- * ChartRenderingInfoTest.java
- * ---------------------------
- * (C) Copyright 2004-present, by David Gilbert and Contributors.
- *
- * Original Author:  David Gilbert;
- * Contributor(s):   -;
- *
- */
-
 package org.jfree.chart;
+
+import org.jfree.chart.entity.ChartEntity;
+import org.jfree.chart.entity.EntityCollection;
+import org.jfree.chart.entity.StandardEntityCollection;
+import org.jfree.chart.internal.CloneUtils;
+import org.jfree.chart.plot.PlotRenderingInfo;
+import org.junit.jupiter.api.Test;
 
 import java.awt.Rectangle;
 import java.awt.geom.Rectangle2D;
 
-import org.jfree.chart.entity.ChartEntity;
-import org.jfree.chart.entity.StandardEntityCollection;
-import org.jfree.chart.internal.CloneUtils;
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * Tests for the {@link ChartRenderingInfo} class.
+ * Tests for {@link ChartRenderingInfo}.
+ *
+ * The tests are organized to emphasize intent and isolate responsibilities:
+ * - equals(): verifies differences in each significant field
+ * - clone(): verifies deep copy and independence
+ * - serialization: verifies state and internal references are preserved
  */
-public class ChartRenderingInfoTest  {
+public class ChartRenderingInfoTest {
 
-    /**
-     * Confirm that the equals method can distinguish all the required fields.
-     */
+    // -------- equals() -------------------------------------------------------
+
     @Test
-    public void testEquals() {
-        ChartRenderingInfo i1 = new ChartRenderingInfo();
-        ChartRenderingInfo i2 = new ChartRenderingInfo();
-        assertEquals(i1, i2);
+    public void equals_defaultInstancesAreEqual() {
+        ChartRenderingInfo left = new ChartRenderingInfo();
+        ChartRenderingInfo right = new ChartRenderingInfo();
 
-        i1.setChartArea(new Rectangle2D.Double(1.0, 2.0, 3.0, 4.0));
-        assertNotEquals(i1, i2);
-        i2.setChartArea(new Rectangle2D.Double(1.0, 2.0, 3.0, 4.0));
-        assertEquals(i1, i2);
-
-        i1.getPlotInfo().setDataArea(new Rectangle(1, 2, 3, 4));
-        assertNotEquals(i1, i2);
-        i2.getPlotInfo().setDataArea(new Rectangle(1, 2, 3, 4));
-        assertEquals(i1, i2);
-
-        StandardEntityCollection e1 = new StandardEntityCollection();
-        e1.add(new ChartEntity(new Rectangle(1, 2, 3, 4)));
-        i1.setEntityCollection(e1);
-        assertNotEquals(i1, i2);
-        StandardEntityCollection e2 = new StandardEntityCollection();
-        e2.add(new ChartEntity(new Rectangle(1, 2, 3, 4)));
-        i2.setEntityCollection(e2);
+        assertEquals(left, right);
     }
 
-    /**
-     * Confirm that cloning works.
-     * @throws java.lang.CloneNotSupportedException
-     */
     @Test
-    public void testCloning() throws CloneNotSupportedException {
-        ChartRenderingInfo i1 = new ChartRenderingInfo();
-        ChartRenderingInfo i2 = CloneUtils.clone(i1);
+    public void equals_detectsDifferenceInChartArea() {
+        ChartRenderingInfo left = new ChartRenderingInfo();
+        ChartRenderingInfo right = new ChartRenderingInfo();
 
-        assertNotSame(i1, i2);
-        assertSame(i1.getClass(), i2.getClass());
-        assertEquals(i1, i2);
+        left.setChartArea(rectD(1, 2, 3, 4));
+        assertNotEquals(left, right);
 
-        // check independence
-        i1.getChartArea().setRect(4.0, 3.0, 2.0, 1.0);
-        assertNotEquals(i1, i2);
-        i2.getChartArea().setRect(4.0, 3.0, 2.0, 1.0);
-        assertEquals(i1, i2);
-
-        i1.getEntityCollection().add(new ChartEntity(new Rectangle(1, 2, 2,
-                1)));
-        assertNotEquals(i1, i2);
-        i2.getEntityCollection().add(new ChartEntity(new Rectangle(1, 2, 2,
-                1)));
-        assertEquals(i1, i2);
-
+        right.setChartArea(rectD(1, 2, 3, 4));
+        assertEquals(left, right);
     }
 
-    /**
-     * Serialize an instance, restore it, and check for equality.
-     */
     @Test
-    public void testSerialization() {
-        ChartRenderingInfo i1 = new ChartRenderingInfo();
-        i1.setChartArea(new Rectangle2D.Double(1.0, 2.0, 3.0, 4.0));
-        ChartRenderingInfo i2 = TestUtils.serialised(i1);
-        assertEquals(i1, i2);
+    public void equals_detectsDifferenceInPlotDataArea() {
+        ChartRenderingInfo left = new ChartRenderingInfo();
+        ChartRenderingInfo right = new ChartRenderingInfo();
+
+        left.getPlotInfo().setDataArea(rectI(1, 2, 3, 4));
+        assertNotEquals(left, right);
+
+        right.getPlotInfo().setDataArea(rectI(1, 2, 3, 4));
+        assertEquals(left, right);
     }
 
-    /**
-     * Serialize an instance, restore it, and check for equality.
-     */
     @Test
-    public void testSerialization2() {
-        ChartRenderingInfo i1 = new ChartRenderingInfo();
-        i1.getPlotInfo().setDataArea(new Rectangle2D.Double(1.0, 2.0, 3.0,
-                4.0));
-        ChartRenderingInfo i2 = TestUtils.serialised(i1);
-        assertEquals(i1, i2);
-        assertEquals(i2, i2.getPlotInfo().getOwner());
+    public void equals_detectsDifferenceInEntityCollection() {
+        ChartRenderingInfo left = new ChartRenderingInfo();
+        ChartRenderingInfo right = new ChartRenderingInfo();
+
+        StandardEntityCollection leftEntities = entityCollectionWithOneEntity(rectI(1, 2, 3, 4));
+        left.setEntityCollection(leftEntities);
+        assertNotEquals(left, right);
+
+        StandardEntityCollection rightEntities = entityCollectionWithOneEntity(rectI(1, 2, 3, 4));
+        right.setEntityCollection(rightEntities);
+        assertEquals(left, right);
+    }
+
+    // -------- clone() --------------------------------------------------------
+
+    @Test
+    public void clone_createsDeepCopyAndMaintainsIndependence() throws CloneNotSupportedException {
+        // Given
+        ChartRenderingInfo original = new ChartRenderingInfo();
+
+        // When
+        ChartRenderingInfo clone = CloneUtils.clone(original);
+
+        // Then
+        assertNotSame(original, clone);
+        assertSame(original.getClass(), clone.getClass());
+        assertEquals(original, clone);
+
+        // Mutate chart area in original only => should diverge
+        original.getChartArea().setRect(4.0, 3.0, 2.0, 1.0);
+        assertNotEquals(original, clone);
+
+        // Align clone's chart area => should be equal again
+        clone.getChartArea().setRect(4.0, 3.0, 2.0, 1.0);
+        assertEquals(original, clone);
+
+        // Mutate entity collection in original only => should diverge
+        original.getEntityCollection().add(new ChartEntity(rectI(1, 2, 2, 1)));
+        assertNotEquals(original, clone);
+
+        // Align clone's entity collection => should be equal again
+        clone.getEntityCollection().add(new ChartEntity(rectI(1, 2, 2, 1)));
+        assertEquals(original, clone);
+
+        // Sanity: entity collections are distinct objects (deep copy)
+        EntityCollection originalEntities = original.getEntityCollection();
+        EntityCollection cloneEntities = clone.getEntityCollection();
+        if (originalEntities != null && cloneEntities != null) {
+            assertNotSame(originalEntities, cloneEntities);
+        }
+
+        // Sanity: plot info is also a distinct object (deep copy)
+        PlotRenderingInfo originalPlot = original.getPlotInfo();
+        PlotRenderingInfo clonePlot = clone.getPlotInfo();
+        assertNotSame(originalPlot, clonePlot);
+    }
+
+    // -------- serialization --------------------------------------------------
+
+    @Test
+    public void serialization_preservesChartArea() {
+        // Given
+        ChartRenderingInfo before = new ChartRenderingInfo();
+        before.setChartArea(rectD(1, 2, 3, 4));
+
+        // When
+        ChartRenderingInfo after = TestUtils.serialised(before);
+
+        // Then
+        assertEquals(before, after);
+    }
+
+    @Test
+    public void serialization_preservesPlotOwnerReference() {
+        // Given
+        ChartRenderingInfo before = new ChartRenderingInfo();
+        before.getPlotInfo().setDataArea(rectD(1, 2, 3, 4));
+
+        // When
+        ChartRenderingInfo after = TestUtils.serialised(before);
+
+        // Then
+        assertEquals(before, after);
+        // After deserialization, PlotRenderingInfo should reference its owning ChartRenderingInfo
+        assertSame(after, after.getPlotInfo().getOwner());
+    }
+
+    // -------- helpers --------------------------------------------------------
+
+    private static Rectangle2D rectD(double x, double y, double w, double h) {
+        return new Rectangle2D.Double(x, y, w, h);
+    }
+
+    private static Rectangle rectI(int x, int y, int w, int h) {
+        return new Rectangle(x, y, w, h);
+    }
+
+    private static StandardEntityCollection entityCollectionWithOneEntity(Rectangle bounds) {
+        StandardEntityCollection entities = new StandardEntityCollection();
+        entities.add(new ChartEntity(bounds));
+        return entities;
     }
 }
