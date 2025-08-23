93e25186089608901753c93f090c49f2480b7a84
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:39:21 +0800

    gemini processedAutoTestCases binarycodec_estest14.java

--- binarycodec_estest14.java
+++ binarycodec_estest14.java
@@ -1,25 +1,9 @@
-package org.apache.commons.codec.binary;
+// Source: org.apache.commons.codec.binary.BinaryCodec
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
-
-public class BinaryCodec_ESTestTest14 extends BinaryCodec_ESTest_scaffolding {
-
-    @Test(timeout = 4000)
-    public void test13() throws Throwable {
-        BinaryCodec binaryCodec0 = new BinaryCodec();
-        try {
-            binaryCodec0.encode((Object) binaryCodec0);
-            fail("Expecting exception: Exception");
-        } catch (Exception e) {
-            //
-            // argument not a byte array
-            //
-            verifyException("org.apache.commons.codec.binary.BinaryCodec", e);
-        }
+@Override
+public Object encode(final Object pObject) throws EncoderException {
+    if (!(pObject instanceof byte[])) {
+        throw new EncoderException("argument not a byte array");
     }
+    return toAsciiChars((byte[]) pObject);
 }
