a68bb4885525e1a8b7f81d57cc445c5b13264735
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:42:55 +0800

    gemini processedAutoTestCases charset_estest8.java

--- charset_estest8.java
+++ charset_estest8.java
@@ -1,20 +1,27 @@
 package org.apache.commons.lang3;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertFalse;
 
-public class CharSet_ESTestTest8 extends CharSet_ESTest_scaffolding {
+/**
+ * Tests for {@link CharSet#equals(Object)}.
+ */
+public class CharSetTest {
 
-    @Test(timeout = 4000)
-    public void test07() throws Throwable {
-        String[] stringArray0 = new String[9];
-        CharSet charSet0 = CharSet.getInstance(stringArray0);
-        Object object0 = new Object();
-        boolean boolean0 = charSet0.equals(object0);
-        assertFalse(boolean0);
+    /**
+     * Tests that a CharSet instance is not equal to an object of a different type.
+     * This is a standard requirement of the equals() contract.
+     */
+    @Test
+    public void testEquals_returnsFalse_whenComparedWithDifferentType() {
+        // Arrange
+        final CharSet charSet = CharSet.getInstance("a-z");
+        final Object nonCharSetObject = new Object();
+
+        // Act
+        final boolean isEqual = charSet.equals(nonCharSetObject);
+
+        // Assert
+        assertFalse("CharSet should not be equal to an object of a different type", isEqual);
     }
 }
