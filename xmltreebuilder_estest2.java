6003d47c3d3a055de58b1221cdf6228744db9014
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:27:58 +0800

    Original processedAutoTestCases xmltreebuilder_estest2.java

---
+++ xmltreebuilder_estest2.java
@@ -0,0 +1,41 @@
+package org.jsoup.parser;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.PipedReader;
+import java.io.PipedWriter;
+import java.io.Reader;
+import java.io.StringReader;
+import java.io.UncheckedIOException;
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.jsoup.nodes.Attributes;
+import org.jsoup.nodes.CDataNode;
+import org.jsoup.nodes.Comment;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.LeafNode;
+import org.jsoup.select.Elements;
+import org.junit.runner.RunWith;
+
+public class XmlTreeBuilder_ESTestTest2 extends XmlTreeBuilder_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test01() throws Throwable {
+        XmlTreeBuilder xmlTreeBuilder0 = new XmlTreeBuilder();
+        xmlTreeBuilder0.parse("jHQIs@:5#c&", "2#0f[w|.T$lBU[oa^f");
+        // Undeclared exception!
+        try {
+            xmlTreeBuilder0.processStartTag(":contains(%s)");
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.jsoup.parser.TreeBuilder", e);
+        }
+    }
+}
