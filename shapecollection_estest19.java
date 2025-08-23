66c5fdc5a1db4c77b137594ed4ec8b21f6c6ea8f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:15:24 +0800

    Original processedAutoTestCases shapecollection_estest19.java

---
+++ shapecollection_estest19.java
@@ -0,0 +1,41 @@
+package org.locationtech.spatial4j.shape;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Stack;
+import java.util.Vector;
+import java.util.function.Predicate;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+import org.locationtech.spatial4j.context.SpatialContext;
+import org.locationtech.spatial4j.context.SpatialContextFactory;
+import org.locationtech.spatial4j.distance.GeodesicSphereDistCalc;
+import org.locationtech.spatial4j.shape.impl.PointImpl;
+import org.locationtech.spatial4j.shape.jts.JtsPoint;
+
+public class ShapeCollection_ESTestTest19 extends ShapeCollection_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test18() throws Throwable {
+        ArrayList<JtsPoint> arrayList0 = new ArrayList<JtsPoint>();
+        SpatialContext spatialContext0 = SpatialContext.GEO;
+        ShapeCollection<JtsPoint> shapeCollection0 = new ShapeCollection<JtsPoint>(arrayList0, spatialContext0);
+        arrayList0.add((JtsPoint) null);
+        // Undeclared exception!
+        try {
+            shapeCollection0.hasArea();
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.locationtech.spatial4j.shape.ShapeCollection", e);
+        }
+    }
+}
