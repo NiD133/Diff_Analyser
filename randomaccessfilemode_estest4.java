e1908255b33783c81cdf985db64321c7f929b53d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:11:06 +0800

    Original processedAutoTestCases randomaccessfilemode_estest4.java

---
+++ randomaccessfilemode_estest4.java
@@ -0,0 +1,30 @@
+package org.apache.commons.io;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.file.OpenOption;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import org.apache.commons.io.function.IOConsumer;
+import org.apache.commons.io.function.IOFunction;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.evosuite.runtime.testdata.EvoSuiteFile;
+import org.evosuite.runtime.testdata.FileSystemHandling;
+import org.junit.runner.RunWith;
+
+public class RandomAccessFileMode_ESTestTest4 extends RandomAccessFileMode_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test03() throws Throwable {
+        RandomAccessFileMode randomAccessFileMode0 = RandomAccessFileMode.READ_WRITE_SYNC_CONTENT;
+        IORandomAccessFile iORandomAccessFile0 = randomAccessFileMode0.io(".4");
+        assertEquals(0L, iORandomAccessFile0.length());
+    }
+}
