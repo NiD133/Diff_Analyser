b54cf256c849b6c875d1ac2bbd179d3dc9ea8f64
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:08:18 +0800

    gemini processedAutoTestCases percentcodec_estest13.java

--- percentcodec_estest13.java
+++ percentcodec_estest13.java
@@ -1,19 +1,29 @@
 package org.apache.commons.codec.net;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertNull;
+import org.apache.commons.codec.DecoderException;
 
-public class PercentCodec_ESTestTest13 extends PercentCodec_ESTest_scaffolding {
+/**
+ * Unit tests for the PercentCodec class.
+ */
+public class PercentCodecTest {
 
-    @Test(timeout = 4000)
-    public void test12() throws Throwable {
-        byte[] byteArray0 = new byte[5];
-        PercentCodec percentCodec0 = new PercentCodec(byteArray0, true);
-        Object object0 = percentCodec0.decode((Object) null);
-        assertNull(object0);
+    /**
+     * Tests that decoding a null Object returns null, as per the contract of the
+     * {@link org.apache.commons.codec.Decoder} interface.
+     */
+    @Test
+    public void decodeNullObjectShouldReturnNull() throws DecoderException {
+        // Arrange
+        // The specific configuration of the codec does not affect this test case,
+        // so a default instance is sufficient.
+        PercentCodec codec = new PercentCodec();
+
+        // Act
+        Object result = codec.decode((Object) null);
+
+        // Assert
+        assertNull("Decoding a null object should result in null.", result);
     }
 }
