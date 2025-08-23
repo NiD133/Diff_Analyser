a7e9377212ac1e6eb94673df5a11b5251c52bc73
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 05:35:43 +0800

    Original manualTestSuite WKTWriterTest.java

--- WKTWriterTest.java
+++ WKTWriterTest.java
@@ -7,57 +7,31 @@ import org.locationtech.spatial4j.context.SpatialContext;
 import org.locationtech.spatial4j.shape.Point;
 import org.locationtech.spatial4j.shape.ShapeCollection;
 
-/**
- * Test suite for the WKTWriter class.
- */
 public class WKTWriterTest {
 
-  // Spatial context used for creating shapes and obtaining the WKT writer.
-  private SpatialContext spatialContext;
+  private SpatialContext ctx;
 
-  /**
-   * Constructor that allows specifying a custom SpatialContext.
-   * 
-   * @param spatialContext the spatial context to use
-   */
-  protected WKTWriterTest(SpatialContext spatialContext) {
-    this.spatialContext = spatialContext;
+  protected WKTWriterTest(SpatialContext ctx) {
+    this.ctx = ctx;
   }
 
-  /**
-   * Default constructor using the GEO spatial context.
-   */
   public WKTWriterTest() {
     this(SpatialContext.GEO);
   }
 
-  /**
-   * Test the WKTWriter's ability to correctly handle an empty Point.
-   */
   @Test
   public void testToStringOnEmptyPoint() throws Exception {
-    // Obtain the WKT writer from the spatial context
-    ShapeWriter wktWriter = spatialContext.getFormats().getWktWriter();
-    
-    // Create an empty Point (NaN coordinates)
-    Point emptyPoint = spatialContext.makePoint(Double.NaN, Double.NaN);
+    ShapeWriter writer = ctx.getFormats().getWktWriter();
+    Point emptyPoint = ctx.makePoint(Double.NaN, Double.NaN);
 
-    // Verify that the WKT representation of an empty Point is "POINT EMPTY"
-    assertEquals("POINT EMPTY", wktWriter.toString(emptyPoint));
+    assertEquals("POINT EMPTY", writer.toString(emptyPoint));
   }
 
-  /**
-   * Test the WKTWriter's ability to correctly handle an empty ShapeCollection.
-   */
   @Test
   public void testToStringOnEmptyShapeCollection() throws Exception {
-    // Obtain the WKT writer from the spatial context
-    ShapeWriter wktWriter = spatialContext.getFormats().getWktWriter();
-    
-    // Create an empty ShapeCollection
-    ShapeCollection<Point> emptyCollection = spatialContext.makeCollection(new ArrayList<>());
+    ShapeWriter writer = ctx.getFormats().getWktWriter();
+    ShapeCollection<Point> emptyCollection = ctx.makeCollection(new ArrayList<>());
 
-    // Verify that the WKT representation of an empty ShapeCollection is "GEOMETRYCOLLECTION EMPTY"
-    assertEquals("GEOMETRYCOLLECTION EMPTY", wktWriter.toString(emptyCollection));
+    assertEquals("GEOMETRYCOLLECTION EMPTY", writer.toString(emptyCollection));
   }
 }
