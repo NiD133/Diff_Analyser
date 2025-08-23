250f2560bf75111c8200580f858125835deec343
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:17:47 +0800

    Original processedAutoTestCases standardbarpainter_estest17.java

---
+++ standardbarpainter_estest17.java
@@ -0,0 +1,38 @@
+package org.jfree.chart.renderer.category;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.awt.Graphics2D;
+import java.awt.geom.Arc2D;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.RectangularShape;
+import java.awt.image.BufferedImage;
+import javax.swing.text.DefaultCaret;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.jfree.chart.api.RectangleEdge;
+import org.jfree.chart.util.GradientPaintTransformer;
+import org.junit.runner.RunWith;
+
+public class StandardBarPainter_ESTestTest17 extends StandardBarPainter_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test16() throws Throwable {
+        StandardBarPainter standardBarPainter0 = new StandardBarPainter();
+        GanttRenderer ganttRenderer0 = new GanttRenderer();
+        DefaultCaret defaultCaret0 = new DefaultCaret();
+        RectangleEdge rectangleEdge0 = RectangleEdge.BOTTOM;
+        // Undeclared exception!
+        try {
+            standardBarPainter0.paintBarShadow((Graphics2D) null, ganttRenderer0, 0, 0, defaultCaret0, rectangleEdge0, true);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.jfree.chart.renderer.category.StandardBarPainter", e);
+        }
+    }
+}
