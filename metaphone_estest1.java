b28fda83129fe38cf758d2f55f8bb59032b89667
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:02:22 +0800

    gemini processedAutoTestCases metaphone_estest1.java

--- metaphone_estest1.java
+++ metaphone_estest1.java
@@ -1,19 +1,50 @@
 package org.apache.commons.codec.language;
 
+import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class Metaphone_ESTestTest1 extends Metaphone_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link Metaphone} class.
+ */
+public class MetaphoneTest {
 
-    @Test(timeout = 4000)
-    public void test00() throws Throwable {
-        Metaphone metaphone0 = new Metaphone();
-        String string0 = metaphone0.metaphone("acw");
-        assertEquals("AK", string0);
-        assertEquals(4, metaphone0.getMaxCodeLen());
+    private Metaphone metaphone;
+
+    @Before
+    public void setUp() {
+        this.metaphone = new Metaphone();
+    }
+
+    /**
+     * Tests that a new Metaphone instance is configured with the correct
+     * default maximum code length.
+     */
+    @Test
+    public void shouldHaveDefaultMaxCodeLengthOfFour() {
+        // Arrange
+        final int expectedMaxCodeLen = 4;
+
+        // Act
+        final int actualMaxCodeLen = metaphone.getMaxCodeLen();
+
+        // Assert
+        assertEquals("Default max code length should be 4", expectedMaxCodeLen, actualMaxCodeLen);
+    }
+
+    /**
+     * Tests a basic encoding case to ensure the algorithm produces the correct output.
+     */
+    @Test
+    public void shouldEncodeWordCorrectly() {
+        // Arrange
+        final String input = "acw";
+        final String expectedCode = "AK";
+
+        // Act
+        final String actualCode = metaphone.metaphone(input);
+
+        // Assert
+        assertEquals("The Metaphone code for 'acw' should be 'AK'", expectedCode, actualCode);
     }
 }
