16dc911d7220de7f3688514e70a253937fcbb8dd
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:28:19 +0800

    Original processedManualTestCases endpointpairtest11.java

---
+++ endpointpairtest11.java
@@ -0,0 +1,63 @@
+package com.google.common.graph;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertThrows;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.testing.EqualsTester;
+import java.util.Collection;
+import java.util.Set;
+import org.jspecify.annotations.NullUnmarked;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+public class EndpointPairTestTest11 {
+
+    private static final Integer N0 = 0;
+
+    private static final Integer N1 = 1;
+
+    private static final Integer N2 = 2;
+
+    private static final Integer N3 = 3;
+
+    private static final Integer N4 = 4;
+
+    private static final String E12 = "1-2";
+
+    private static final String E12_A = "1-2a";
+
+    private static final String E21 = "2-1";
+
+    private static final String E13 = "1-3";
+
+    private static final String E44 = "4-4";
+
+    private static void containsExactlySanityCheck(Collection<?> collection, Object... varargs) {
+        assertThat(collection).hasSize(varargs.length);
+        for (Object obj : varargs) {
+            assertThat(collection).contains(obj);
+        }
+        assertThat(collection).containsExactly(varargs);
+    }
+
+    @Test
+    public void endpointPair_undirected_contains() {
+        MutableGraph<Integer> undirectedGraph = GraphBuilder.undirected().allowsSelfLoops(true).build();
+        undirectedGraph.putEdge(N1, N1);
+        undirectedGraph.putEdge(N1, N2);
+        Set<EndpointPair<Integer>> edges = undirectedGraph.edges();
+        assertThat(edges).hasSize(2);
+        assertThat(edges).contains(EndpointPair.unordered(N1, N1));
+        assertThat(edges).contains(EndpointPair.unordered(N1, N2));
+        // equal to unordered(N1, N2)
+        assertThat(edges).contains(EndpointPair.unordered(N2, N1));
+        // ordered endpoints not compatible with undirected graph
+        assertThat(edges).doesNotContain(EndpointPair.ordered(N1, N2));
+        // edge not present
+        assertThat(edges).doesNotContain(EndpointPair.unordered(N2, N2));
+        // nodes not in graph
+        assertThat(edges).doesNotContain(EndpointPair.unordered(N3, N4));
+    }
+}
