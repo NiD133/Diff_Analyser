72a487b8717e37c748c05ea9e269a82ebb10ef78
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:04:09 +0800

    gemini processedAutoTestCases multireadonlyseekablebytechannel_estest16.java

--- multireadonlyseekablebytechannel_estest16.java
+++ multireadonlyseekablebytechannel_estest16.java
@@ -1,39 +1,26 @@
 package org.apache.commons.compress.utils;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
 import java.io.File;
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.channels.ClosedChannelException;
-import java.nio.channels.FileChannel;
-import java.nio.channels.NonWritableChannelException;
-import java.nio.channels.SeekableByteChannel;
-import java.nio.file.NoSuchFileException;
-import java.nio.file.OpenOption;
-import java.nio.file.Path;
-import java.util.LinkedList;
-import java.util.List;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.io.MockFile;
-import org.junit.runner.RunWith;
 
-public class MultiReadOnlySeekableByteChannel_ESTestTest16 extends MultiReadOnlySeekableByteChannel_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link MultiReadOnlySeekableByteChannel} class.
+ */
+public class MultiReadOnlySeekableByteChannelTest {
 
-    @Test(timeout = 4000)
-    public void test15() throws Throwable {
-        File[] fileArray0 = new File[2];
-        // Undeclared exception!
-        try {
-            MultiReadOnlySeekableByteChannel.forFiles(fileArray0);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.commons.compress.utils.MultiReadOnlySeekableByteChannel", e);
-        }
+    /**
+     * Tests that MultiReadOnlySeekableByteChannel.forFiles() throws a NullPointerException
+     * when the input array of files contains a null element.
+     */
+    @Test(expected = NullPointerException.class)
+    public void forFilesShouldThrowNullPointerExceptionWhenFileArrayContainsNull() {
+        // Arrange: Create an array of File objects where at least one element is null.
+        // The method under test is expected to throw an exception when it attempts
+        // to process the null element.
+        final File[] filesWithNullElement = { null };
+
+        // Act: Call the method with the invalid input.
+        // The @Test(expected=...) annotation handles the assertion.
+        MultiReadOnlySeekableByteChannel.forFiles(filesWithNullElement);
     }
 }
