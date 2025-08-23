a9a3697971476e3dddae04701770ffe65cd7727a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:58:14 +0800

    Original processedAutoTestCases jsontreereader_estest64.java

---
+++ jsontreereader_estest64.java
@@ -0,0 +1,29 @@
+package com.google.gson.internal.bind;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonNull;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.Strictness;
+import com.google.gson.stream.JsonToken;
+import java.io.IOException;
+import java.util.ConcurrentModificationException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class JsonTreeReader_ESTestTest64 extends JsonTreeReader_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test063() throws Throwable {
+        JsonArray jsonArray0 = new JsonArray();
+        JsonTreeReader jsonTreeReader0 = new JsonTreeReader(jsonArray0);
+        jsonTreeReader0.beginArray();
+        String string0 = jsonTreeReader0.getPreviousPath();
+        assertEquals("$[0]", string0);
+    }
+}
