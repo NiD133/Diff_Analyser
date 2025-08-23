0ae3af669978bf1e1051dc61cf3fcfb5745cf07d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:38:43 +0800

    Original processedAutoTestCases basetypehandler_estest11.java

---
+++ basetypehandler_estest11.java
@@ -0,0 +1,33 @@
+package org.apache.ibatis.type;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.time.YearMonth;
+import org.apache.ibatis.session.Configuration;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class BaseTypeHandler_ESTestTest11 extends BaseTypeHandler_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test10() throws Throwable {
+        ArrayTypeHandler arrayTypeHandler0 = new ArrayTypeHandler();
+        // Undeclared exception!
+        try {
+            arrayTypeHandler0.getNullableResult((CallableStatement) null, 0);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.apache.ibatis.type.ArrayTypeHandler", e);
+        }
+    }
+}
