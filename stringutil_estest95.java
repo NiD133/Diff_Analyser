ba4a857dc69cd219d6b74c71f418a158579411c0
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:19:17 +0800

    gemini processedAutoTestCases stringutil_estest95.java

--- stringutil_estest95.java
+++ stringutil_estest95.java
@@ -1,28 +1,38 @@
 package org.jsoup.internal;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Collection;
-import java.util.ConcurrentModificationException;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.ListIterator;
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collector;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.evosuite.runtime.mock.java.net.MockURL;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
+/*
+ * Note: The class name `StringUtil_ESTestTest95` and the `extends` clause
+ * are preserved from the original code. In a typical project, this would be
+ * renamed to `StringUtilTest`.
+ */
 public class StringUtil_ESTestTest95 extends StringUtil_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test94() throws Throwable {
-        Collector<CharSequence, ?, String> collector0 = StringUtil.joining(" ");
-        assertNotNull(collector0);
+    /**
+     * Tests that the Collector returned by StringUtil.joining() correctly
+     * concatenates a stream of strings with a given delimiter.
+     */
+    @Test
+    public void joiningCollectorShouldCorrectlyJoinStrings() {
+        // Arrange: Set up the input data and the expected outcome.
+        List<String> stringsToJoin = Arrays.asList("HTML", "CSS", "JavaScript");
+        String delimiter = ", ";
+        String expectedOutput = "HTML, CSS, JavaScript";
+
+        // Act: Get the collector from the method under test.
+        Collector<CharSequence, ?, String> joiningCollector = StringUtil.joining(delimiter);
+        
+        // Use the collector to process the stream of strings.
+        String actualOutput = stringsToJoin.stream().collect(joiningCollector);
+
+        // Assert: Verify that the collector works as expected.
+        assertNotNull("The collector itself should not be null.", joiningCollector);
+        assertEquals("The strings should be joined with the correct delimiter.", expectedOutput, actualOutput);
     }
 }
