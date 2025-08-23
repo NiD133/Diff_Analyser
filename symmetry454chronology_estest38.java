ae5de316246f87221f5d9183cda854c06cecda9d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:19:59 +0800

    gemini processedAutoTestCases symmetry454chronology_estest38.java

--- symmetry454chronology_estest38.java
+++ symmetry454chronology_estest38.java
@@ -1,44 +1,27 @@
 package org.threeten.extra.chrono;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.time.Clock;
-import java.time.DateTimeException;
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.OffsetDateTime;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
-import java.time.chrono.ChronoLocalDateTime;
-import java.time.chrono.ChronoZonedDateTime;
-import java.time.chrono.Era;
-import java.time.chrono.HijrahEra;
-import java.time.chrono.IsoEra;
-import java.time.chrono.JapaneseEra;
 import java.time.temporal.ChronoField;
-import java.time.temporal.ChronoUnit;
-import java.time.temporal.TemporalAccessor;
-import java.time.temporal.TemporalUnit;
-import java.time.temporal.UnsupportedTemporalTypeException;
 import java.time.temporal.ValueRange;
-import java.util.List;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.System;
-import org.evosuite.runtime.mock.java.time.MockClock;
-import org.evosuite.runtime.mock.java.time.MockInstant;
-import org.evosuite.runtime.mock.java.time.MockLocalDateTime;
-import org.evosuite.runtime.mock.java.time.MockOffsetDateTime;
-import org.junit.runner.RunWith;
 
-public class Symmetry454Chronology_ESTestTest38 extends Symmetry454Chronology_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
 
-    @Test(timeout = 4000)
-    public void test37() throws Throwable {
-        Symmetry454Chronology symmetry454Chronology0 = new Symmetry454Chronology();
-        ChronoField chronoField0 = ChronoField.SECOND_OF_MINUTE;
-        ValueRange valueRange0 = symmetry454Chronology0.range(chronoField0);
-        assertNotNull(valueRange0);
+/**
+ * Tests for the range of fields in the Symmetry454Chronology.
+ */
+public class Symmetry454ChronologyRangeTest {
+
+    @Test
+    public void range_forSecondOfMinute_returnsStandardTimeRange() {
+        // Arrange
+        Symmetry454Chronology chronology = Symmetry454Chronology.INSTANCE;
+        // The expected range for SECOND_OF_MINUTE is standard across most chronologies.
+        ValueRange expectedRange = ValueRange.of(0, 59);
+
+        // Act
+        ValueRange actualRange = chronology.range(ChronoField.SECOND_OF_MINUTE);
+
+        // Assert
+        assertEquals("The range for SECOND_OF_MINUTE should be the standard 0-59.", expectedRange, actualRange);
     }
 }
