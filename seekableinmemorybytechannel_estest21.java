c86a1a26178cb30da54bae5d6d336288cdb9f347
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:13:20 +0800

    Original processedAutoTestCases seekableinmemorybytechannel_estest21.java

---
+++ seekableinmemorybytechannel_estest21.java
@@ -0,0 +1,24 @@
+package org.apache.commons.compress.utils;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.ClosedChannelException;
+import java.nio.channels.SeekableByteChannel;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class SeekableInMemoryByteChannel_ESTestTest21 extends SeekableInMemoryByteChannel_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test20() throws Throwable {
+        SeekableInMemoryByteChannel seekableInMemoryByteChannel0 = new SeekableInMemoryByteChannel(1);
+        ByteBuffer byteBuffer0 = ByteBuffer.allocateDirect(1);
+        seekableInMemoryByteChannel0.read(byteBuffer0);
+        seekableInMemoryByteChannel0.truncate((byte) 0);
+        assertEquals(0L, seekableInMemoryByteChannel0.size());
+    }
+}
