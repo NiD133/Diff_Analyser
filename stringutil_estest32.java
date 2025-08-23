2232240b546e12382137821acdd689d709dddc25
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:18:50 +0800

    gemini processedAutoTestCases stringutil_estest32.java

--- stringutil_estest32.java
+++ stringutil_estest32.java
@@ -1,36 +1,21 @@
 package org.jsoup.internal;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.Collection;
-import java.util.ConcurrentModificationException;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.ListIterator;
-import java.util.stream.Collector;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.evosuite.runtime.mock.java.net.MockURL;
-import org.junit.runner.RunWith;
 
-public class StringUtil_ESTestTest32 extends StringUtil_ESTest_scaffolding {
+/**
+ * Test suite focusing on exception handling for the {@link StringUtil#join(Collection, String)} method.
+ */
+public class StringUtilTest {
 
-    @Test(timeout = 4000)
-    public void test31() throws Throwable {
-        // Undeclared exception!
-        try {
-            StringUtil.join((Collection<?>) null, "M");
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.jsoup.internal.StringUtil", e);
-        }
+    /**
+     * Verifies that calling {@code StringUtil.join()} with a null collection
+     * correctly throws a NullPointerException.
+     */
+    @Test(expected = NullPointerException.class)
+    public void joinWithNullCollectionThrowsNullPointerException() {
+        // The method under test should reject a null collection argument.
+        // The @Test(expected) annotation asserts that the expected exception is thrown.
+        StringUtil.join((Collection<?>) null, ", ");
     }
 }
