3f4bb7c2f87719e83dd07f45950bd0f6e4e00ef2
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:44:14 +0800

    gemini processedAutoTestCases collectionbag_estest38.java

--- collectionbag_estest38.java
+++ collectionbag_estest38.java
@@ -1,47 +1,58 @@
 package org.apache.commons.collections4.bag;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.ConcurrentModificationException;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Stack;
-import java.util.TreeSet;
 import org.apache.commons.collections4.Bag;
-import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.SortedBag;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.FalsePredicate;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.MapTransformer;
-import org.apache.commons.collections4.functors.NullIsExceptionPredicate;
-import org.apache.commons.collections4.functors.TransformerPredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class CollectionBag_ESTestTest38 extends CollectionBag_ESTest_scaffolding {
+import java.util.Arrays;
+import java.util.Collection;
 
-    @Test(timeout = 4000)
-    public void test37() throws Throwable {
-        TreeBag<Integer> treeBag0 = new TreeBag<Integer>();
-        Integer integer0 = new Integer(2616);
-        treeBag0.add(integer0);
-        CollectionBag<Integer> collectionBag0 = new CollectionBag<Integer>(treeBag0);
-        collectionBag0.addAll(treeBag0);
-        HashBag<Object> hashBag0 = new HashBag<Object>((Collection<?>) treeBag0);
-        CollectionBag<Object> collectionBag1 = new CollectionBag<Object>(hashBag0);
-        boolean boolean0 = collectionBag1.removeAll(treeBag0);
-        assertEquals(2, treeBag0.size());
-        assertTrue(boolean0);
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Contains an improved version of a test for the {@link CollectionBag#removeAll(Collection)} method.
+ */
+public class CollectionBag_ESTestTest38 {
+
+    /**
+     * Tests that {@link CollectionBag#removeAll(Collection)} removes all occurrences of
+     * the elements present in the given collection, ignoring cardinality.
+     * <p>
+     * The Javadoc for CollectionBag#removeAll states:
+     * "Remove all elements represented in the given collection,
+     * <strong>not</strong> respecting cardinality. That is, remove <em>all</em>
+     * occurrences of every object contained in the given collection."
+     */
+    @Test
+    public void removeAllShouldRemoveAllCopiesOfContainedElements() {
+        // Arrange
+        // Create a bag with multiple copies of elements "A" and "B".
+        final Bag<String> decoratedBag = new HashBag<>();
+        decoratedBag.add("A", 2); // 2 copies of "A"
+        decoratedBag.add("B", 3); // 3 copies of "B"
+        final Bag<String> bagUnderTest = CollectionBag.collectionBag(decoratedBag);
+
+        // The collection of elements to remove contains one "A" and one "C".
+        // "C" is not present in the original bag, which tests that no errors occur.
+        final Collection<String> elementsToRemove = Arrays.asList("A", "C");
+
+        // Act
+        // This should remove all copies of "A" and do nothing for "C".
+        final boolean wasModified = bagUnderTest.removeAll(elementsToRemove);
+
+        // Assert
+        // 1. The method should return true, as the bag was modified by removing "A".
+        assertTrue("removeAll should return true because an element was removed.", wasModified);
+
+        // 2. The bag should no longer contain any copies of "A".
+        assertFalse("Bag should not contain 'A' after removeAll.", bagUnderTest.contains("A"));
+        assertEquals("The count of 'A' should be 0.", 0, bagUnderTest.getCount("A"));
+
+        // 3. The element "B" should be unaffected.
+        assertEquals("The count of 'B' should remain 3.", 3, bagUnderTest.getCount("B"));
+
+        // 4. The total size should reflect the removal of all copies of "A".
+        assertEquals("The total size of the bag should be 3 (the remaining copies of 'B').", 3, bagUnderTest.size());
     }
 }
