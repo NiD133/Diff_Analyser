631bc01878b8a0b2afd99f4472fd324bd6cca428
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:21:13 +0800

    Original processedAutoTestCases texthelpappendable_estest73.java

---
+++ texthelpappendable_estest73.java
@@ -0,0 +1,57 @@
+package org.apache.commons.cli.help;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.IOException;
+import java.io.PipedWriter;
+import java.io.StringWriter;
+import java.nio.BufferOverflowException;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.ReadOnlyBufferException;
+import java.nio.charset.Charset;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.SortedSet;
+import java.util.Stack;
+import java.util.TreeSet;
+import java.util.Vector;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class TextHelpAppendable_ESTestTest73 extends TextHelpAppendable_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test72() throws Throwable {
+        TextHelpAppendable textHelpAppendable0 = TextHelpAppendable.systemOut();
+        ArrayList<TextStyle> arrayList0 = new ArrayList<TextStyle>();
+        TextStyle textStyle0 = TextStyle.DEFAULT;
+        arrayList0.add(textStyle0);
+        LinkedHashSet<List<String>> linkedHashSet0 = new LinkedHashSet<List<String>>();
+        ArrayList<String> arrayList1 = new ArrayList<String>();
+        TableDefinition tableDefinition0 = TableDefinition.from("", arrayList0, arrayList1, linkedHashSet0);
+        // Undeclared exception!
+        try {
+            textHelpAppendable0.adjustTableFormat(tableDefinition0);
+            fail("Expecting exception: IndexOutOfBoundsException");
+        } catch (IndexOutOfBoundsException e) {
+            //
+            // Index: 0, Size: 0
+            //
+            verifyException("java.util.ArrayList", e);
+        }
+    }
+}
