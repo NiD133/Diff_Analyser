8708b39217e10514e7d27cca1e7e0fd1fd04dce4
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:59:33 +0800

    gemini processedAutoTestCases julianchronology_estest9.java

--- julianchronology_estest9.java
+++ julianchronology_estest9.java
@@ -1,55 +1,29 @@
 package org.threeten.extra.chrono;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.time.Clock;
-import java.time.DateTimeException;
-import java.time.Duration;
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.Month;
-import java.time.OffsetDateTime;
-import java.time.Period;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
-import java.time.chrono.ChronoLocalDateTime;
-import java.time.chrono.ChronoZonedDateTime;
-import java.time.chrono.Era;
-import java.time.chrono.HijrahDate;
-import java.time.chrono.JapaneseDate;
-import java.time.chrono.JapaneseEra;
-import java.time.chrono.ThaiBuddhistEra;
-import java.time.format.ResolverStyle;
-import java.time.temporal.ChronoField;
-import java.time.temporal.ChronoUnit;
-import java.time.temporal.TemporalAccessor;
-import java.time.temporal.TemporalAmount;
-import java.time.temporal.TemporalField;
-import java.time.temporal.TemporalUnit;
-import java.time.temporal.UnsupportedTemporalTypeException;
-import java.time.temporal.ValueRange;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.time.MockClock;
-import org.evosuite.runtime.mock.java.time.MockInstant;
-import org.evosuite.runtime.mock.java.time.MockLocalDate;
-import org.evosuite.runtime.mock.java.time.MockLocalDateTime;
-import org.evosuite.runtime.mock.java.time.MockOffsetDateTime;
-import org.evosuite.runtime.mock.java.time.chrono.MockHijrahDate;
-import org.evosuite.runtime.mock.java.time.chrono.MockJapaneseDate;
-import org.junit.runner.RunWith;
 
-public class JulianChronology_ESTestTest9 extends JulianChronology_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
 
-    @Test(timeout = 4000)
-    public void test08() throws Throwable {
-        JulianChronology julianChronology0 = new JulianChronology();
-        JulianDate julianDate0 = julianChronology0.dateYearDay(327, 327);
-        assertEquals(JulianEra.AD, julianDate0.getEra());
+/**
+ * Tests for the {@link JulianChronology} class.
+ */
+public class JulianChronologyTest {
+
+    /**
+     * Tests that creating a date with a positive proleptic year results in the AD era.
+     * The Julian calendar considers years from 1 onwards as 'Anno Domini' (AD).
+     */
+    @Test
+    public void dateYearDay_withPositiveYear_returnsDateInAdEra() {
+        // Arrange
+        JulianChronology julianChronology = JulianChronology.INSTANCE;
+        int prolepticYear = 327;
+        int dayOfYear = 327; // Corresponds to November 23rd in a non-leap year.
+
+        // Act
+        JulianDate julianDate = julianChronology.dateYearDay(prolepticYear, dayOfYear);
+
+        // Assert
+        assertEquals("The era for a positive year should be AD", JulianEra.AD, julianDate.getEra());
     }
 }
