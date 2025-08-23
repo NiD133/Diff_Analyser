b05cdba4575c85a1d93d4f07a9618516d857cdcd
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:25:04 +0800

    Original processedManualTestCases attributetest5.java

---
+++ attributetest5.java
@@ -0,0 +1,18 @@
+package org.jsoup.nodes;
+
+import org.jsoup.Jsoup;
+import org.jsoup.parser.ParseSettings;
+import org.jsoup.parser.Parser;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AttributeTestTest5 {
+
+    @Test
+    public void validatesKeysNotEmptyViaSet() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Attribute attr = new Attribute("One", "Check");
+            attr.setKey(" ");
+        });
+    }
+}
