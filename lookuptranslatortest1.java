e29ecb5d8b92a7b27a6e76b5569462656d7d686a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:30:54 +0800

    gemini processedManualTestCases lookuptranslatortest1.java

--- lookuptranslatortest1.java
+++ lookuptranslatortest1.java
@@ -1,19 +1,38 @@
 package org.apache.commons.lang3.text.translate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.io.IOException;
 import java.io.StringWriter;
 import org.apache.commons.lang3.AbstractLangTest;
 import org.junit.jupiter.api.Test;
 
-public class LookupTranslatorTestTest1 extends AbstractLangTest {
+/**
+ * Tests for {@link LookupTranslator}.
+ */
+// Class name improved for clarity and to follow standard naming conventions.
+// The original "TestTest1" suffix was redundant.
+public class LookupTranslatorTest extends AbstractLangTest {
 
     @Test
-    void testBasicLookup() throws IOException {
-        final LookupTranslator lt = new LookupTranslator(new CharSequence[][] { { "one", "two" } });
-        final StringWriter out = new StringWriter();
-        final int result = lt.translate("one", 0, out);
-        assertEquals(3, result, "Incorrect code point consumption");
-        assertEquals("two", out.toString(), "Incorrect value");
+    void translate_whenMatchFound_shouldWriteTranslationAndReturnConsumedLength() throws IOException {
+        // Arrange
+        // Define a lookup table to translate "one" into "two".
+        final CharSequence[][] lookupTable = {{"one", "two"}};
+        final LookupTranslator translator = new LookupTranslator(lookupTable);
+        final StringWriter writer = new StringWriter();
+        final String input = "one";
+
+        // Act
+        // Attempt to translate the input string from the beginning (index 0).
+        final int consumedChars = translator.translate(input, 0, writer);
+
+        // Assert
+        // The method should consume the entire input "one", which has a length of 3.
+        assertEquals(3, consumedChars, "The number of consumed characters should be the length of the matched input 'one'.");
+        
+        // The writer should contain the translated value "two".
+        final String translatedOutput = writer.toString();
+        assertEquals("two", translatedOutput, "The translated output should be 'two'.");
     }
 }
