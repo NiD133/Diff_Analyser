faab8a524fbe5a5662ec16c281b3802d1fc7ffb4
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:25:47 +0800

    Original processedManualTestCases boundediteratortest5.java

---
+++ boundediteratortest5.java
@@ -0,0 +1,51 @@
+package org.apache.commons.collections4.iterators;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class BoundedIteratorTestTest5<E> extends AbstractIteratorTest<E> {
+
+    /**
+     * Test array of size 7
+     */
+    private final String[] testArray = { "a", "b", "c", "d", "e", "f", "g" };
+
+    private List<E> testList;
+
+    @Override
+    public Iterator<E> makeEmptyIterator() {
+        return new BoundedIterator<>(Collections.<E>emptyList().iterator(), 0, 10);
+    }
+
+    @Override
+    public Iterator<E> makeObject() {
+        return new BoundedIterator<>(new ArrayList<>(testList).iterator(), 1, testList.size() - 1);
+    }
+
+    @SuppressWarnings("unchecked")
+    @BeforeEach
+    public void setUp() throws Exception {
+        testList = Arrays.asList((E[]) testArray);
+    }
+
+    /**
+     * Test the case if a negative {@code offset} is passed to the
+     * constructor. {@link IllegalArgumentException} is expected.
+     */
+    @Test
+    void testNegativeOffset() {
+        final IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> new BoundedIterator<>(testList.iterator(), -1, 4));
+        assertEquals("Offset parameter must not be negative.", thrown.getMessage());
+    }
+}
