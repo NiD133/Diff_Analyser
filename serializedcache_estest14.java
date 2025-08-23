0e2ecb5c7ad355e4b81c2c68b12c3824a7e06155
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:14:48 +0800

    Original processedAutoTestCases serializedcache_estest14.java

---
+++ serializedcache_estest14.java
@@ -0,0 +1,35 @@
+package org.apache.ibatis.cache.decorators;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.EOFException;
+import java.io.SequenceInputStream;
+import java.util.Enumeration;
+import org.apache.ibatis.cache.Cache;
+import org.apache.ibatis.cache.impl.PerpetualCache;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.io.MockFileInputStream;
+import org.junit.runner.RunWith;
+
+public class SerializedCache_ESTestTest14 extends SerializedCache_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test13() throws Throwable {
+        SynchronizedCache synchronizedCache0 = new SynchronizedCache((Cache) null);
+        SerializedCache serializedCache0 = new SerializedCache(synchronizedCache0);
+        // Undeclared exception!
+        try {
+            serializedCache0.removeObject(synchronizedCache0);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.apache.ibatis.cache.decorators.SynchronizedCache", e);
+        }
+    }
+}
