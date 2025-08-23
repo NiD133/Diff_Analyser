7ff0a06adeb82f0e6888957fbf6c898cebb79e10
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:27:23 +0800

    gemini processedAutoTestCases weeks_estest55.java

--- weeks_estest55.java
+++ weeks_estest55.java
@@ -1,17 +1,23 @@
 package org.joda.time;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class Weeks_ESTestTest55 extends Weeks_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link Weeks} class.
+ */
+public class WeeksTest {
 
-    @Test(timeout = 4000)
-    public void test54() throws Throwable {
-        Weeks weeks0 = Weeks.weeks(3);
-        assertEquals(3, weeks0.getWeeks());
+    @Test
+    public void weeksFactory_createsInstanceWithCorrectValue() {
+        // Arrange
+        final int expectedWeeks = 3;
+
+        // Act
+        Weeks threeWeeks = Weeks.weeks(expectedWeeks);
+
+        // Assert
+        assertEquals("The number of weeks should match the value provided to the factory method.",
+                expectedWeeks, threeWeeks.getWeeks());
     }
 }
