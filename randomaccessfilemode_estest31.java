4b1a982f6aea7b1ea304cab8d7fb00e167a4c45e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:11:04 +0800

    gemini processedAutoTestCases randomaccessfilemode_estest31.java

--- randomaccessfilemode_estest31.java
+++ randomaccessfilemode_estest31.java
@@ -1,29 +1,28 @@
 package org.apache.commons.io;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.RandomAccessFile;
-import java.nio.file.OpenOption;
-import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
-import org.apache.commons.io.function.IOConsumer;
-import org.apache.commons.io.function.IOFunction;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.io.MockFile;
-import org.evosuite.runtime.testdata.EvoSuiteFile;
-import org.evosuite.runtime.testdata.FileSystemHandling;
-import org.junit.runner.RunWith;
 
-public class RandomAccessFileMode_ESTestTest31 extends RandomAccessFileMode_ESTest_scaffolding {
+/**
+ * Tests for the {@link RandomAccessFileMode} enum.
+ */
+public class RandomAccessFileModeTest {
 
-    @Test(timeout = 4000)
-    public void test30() throws Throwable {
-        RandomAccessFileMode randomAccessFileMode0 = RandomAccessFileMode.valueOfMode("rws");
-        assertEquals(RandomAccessFileMode.READ_WRITE_SYNC_ALL, randomAccessFileMode0);
+    /**
+     * Tests that {@link RandomAccessFileMode#valueOfMode(String)} correctly maps
+     * the "rws" string to the READ_WRITE_SYNC_ALL enum constant.
+     */
+    @Test
+    public void valueOfMode_shouldReturnReadWriteSyncAll_forRwsString() {
+        // Arrange
+        final String modeString = "rws";
+        final RandomAccessFileMode expectedMode = RandomAccessFileMode.READ_WRITE_SYNC_ALL;
+
+        // Act
+        final RandomAccessFileMode actualMode = RandomAccessFileMode.valueOfMode(modeString);
+
+        // Assert
+        assertEquals("The mode string 'rws' should map to READ_WRITE_SYNC_ALL", expectedMode, actualMode);
     }
 }
