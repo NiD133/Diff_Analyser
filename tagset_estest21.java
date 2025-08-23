75f7f099611f6bdd589b1e14bd127b428aa78138
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:20:16 +0800

    gemini processedAutoTestCases tagset_estest21.java

--- tagset_estest21.java
+++ tagset_estest21.java
@@ -2,20 +2,30 @@ package org.jsoup.parser;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.function.Consumer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
 
-public class TagSet_ESTestTest21 extends TagSet_ESTest_scaffolding {
+/**
+ * Test suite for the {@link TagSet} class.
+ */
+public class TagSetTest {
 
-    @Test(timeout = 4000)
-    public void test20() throws Throwable {
-        TagSet tagSet0 = TagSet.Html();
-        Tag tag0 = tagSet0.get("pre", "http://www.w3.org/1999/xhtml");
-        assertEquals(4, Tag.Block);
+    /**
+     * Verifies that the get() method can retrieve a known, pre-defined tag
+     * from the default HTML TagSet using its name and namespace.
+     */
+    @Test
+    public void getShouldReturnKnownTagFromHtmlSet() {
+        // Arrange: Get the default HTML tag set and define the tag to look for.
+        TagSet htmlTagSet = TagSet.Html();
+        final String tagName = "pre";
+        final String htmlNamespace = "http://www.w3.org/1999/xhtml";
+
+        // Act: Attempt to retrieve the <pre> tag.
+        Tag preTag = htmlTagSet.get(tagName, htmlNamespace);
+
+        // Assert: Verify that the correct Tag object was returned.
+        assertNotNull("The 'pre' tag should exist in the default HTML TagSet.", preTag);
+        assertEquals("The tag name should be correct.", tagName, preTag.getName());
+        assertTrue("The 'pre' tag should be a block-level element.", preTag.isBlock());
+        assertTrue("The 'pre' tag should preserve whitespace.", preTag.preserveWhitespace());
     }
 }
