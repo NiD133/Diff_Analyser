cbc2cba74703e849eaa8cd92cd01251a6355910d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:53:12 +0800

    Original processedAutoTestCases hexdump_estest1.java

---
+++ hexdump_estest1.java
@@ -0,0 +1,36 @@
+package org.apache.commons.io;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PipedOutputStream;
+import java.io.PipedWriter;
+import java.io.StringWriter;
+import java.nio.BufferOverflowException;
+import java.nio.CharBuffer;
+import java.nio.ReadOnlyBufferException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.evosuite.runtime.mock.java.io.MockFileOutputStream;
+import org.evosuite.runtime.mock.java.io.MockFileWriter;
+import org.evosuite.runtime.mock.java.io.MockPrintStream;
+import org.junit.runner.RunWith;
+
+public class HexDump_ESTestTest1 extends HexDump_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test00() throws Throwable {
+        byte[] byteArray0 = new byte[3];
+        MockFile mockFile0 = new MockFile("org.apache.commons.io.filefilter.EmptyFileFilter");
+        MockFileOutputStream mockFileOutputStream0 = new MockFileOutputStream(mockFile0);
+        MockPrintStream mockPrintStream0 = new MockPrintStream(mockFileOutputStream0);
+        HexDump.dump(byteArray0, (-1131L), (OutputStream) mockPrintStream0, (int) (byte) 0);
+        assertEquals(61L, mockFile0.length());
+    }
+}
