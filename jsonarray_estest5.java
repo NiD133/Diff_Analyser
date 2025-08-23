132abf1b3ff2fd08c47388a55ead7b11e424923e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:56:10 +0800

    Original processedAutoTestCases jsonarray_estest5.java

---
+++ jsonarray_estest5.java
@@ -0,0 +1,22 @@
+package com.google.gson;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Iterator;
+import java.util.List;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class JsonArray_ESTestTest5 extends JsonArray_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test04() throws Throwable {
+        JsonArray jsonArray0 = new JsonArray();
+        Iterator<JsonElement> iterator0 = jsonArray0.iterator();
+        assertNotNull(iterator0);
+    }
+}
