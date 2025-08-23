20e21ab790af740f73c66f720c5f56f89d210630
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:18:19 +0800

    Original processedAutoTestCases streamcompressor_estest7.java

---
+++ streamcompressor_estest7.java
@@ -0,0 +1,41 @@
+package org.apache.commons.compress.archivers.zip;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.io.SequenceInputStream;
+import java.nio.channels.SeekableByteChannel;
+import java.util.Enumeration;
+import java.util.zip.Deflater;
+import org.apache.commons.compress.parallel.ScatterGatherBackingStore;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.io.MockFileInputStream;
+import org.evosuite.runtime.mock.java.io.MockFileOutputStream;
+import org.junit.runner.RunWith;
+
+public class StreamCompressor_ESTestTest7 extends StreamCompressor_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test06() throws Throwable {
+        PipedOutputStream pipedOutputStream0 = new PipedOutputStream();
+        PipedInputStream pipedInputStream0 = new PipedInputStream(pipedOutputStream0);
+        StreamCompressor streamCompressor0 = StreamCompressor.create((OutputStream) pipedOutputStream0);
+        byte[] byteArray0 = new byte[2];
+        streamCompressor0.write(byteArray0, 1, (byte) 1, 1871);
+        long long0 = streamCompressor0.getTotalBytesWritten();
+        assertEquals(3523407757L, streamCompressor0.getCrc32());
+        assertEquals(1L, long0);
+    }
+}
