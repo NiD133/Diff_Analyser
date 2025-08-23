7b464217a57c91f716e653e4e1e767bba1c0c9c4
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:05:55 +0800

    gemini processedAutoTestCases observableinputstream_estest22.java

--- observableinputstream_estest22.java
+++ observableinputstream_estest22.java
@@ -1,48 +1,28 @@
 package org.apache.commons.io.input;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.BufferedInputStream;
-import java.io.ByteArrayInputStream;
-import java.io.DataInputStream;
-import java.io.FileDescriptor;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.ObjectInputStream;
 import java.io.PipedInputStream;
-import java.io.PipedOutputStream;
-import java.io.PushbackInputStream;
-import java.io.SequenceInputStream;
-import java.io.StringWriter;
-import java.nio.CharBuffer;
-import java.nio.file.NoSuchFileException;
-import java.security.MessageDigest;
-import java.util.Enumeration;
-import java.util.LinkedList;
-import java.util.List;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.evosuite.runtime.mock.java.io.MockFileInputStream;
-import org.evosuite.runtime.mock.java.io.MockIOException;
-import org.junit.runner.RunWith;
 
-public class ObservableInputStream_ESTestTest22 extends ObservableInputStream_ESTest_scaffolding {
+/**
+ * Tests for {@link ObservableInputStream} focusing on exception propagation from the underlying stream.
+ */
+public class ObservableInputStreamTest {
 
-    @Test(timeout = 4000)
-    public void test21() throws Throwable {
-        PipedInputStream pipedInputStream0 = new PipedInputStream();
-        ObservableInputStream observableInputStream0 = new ObservableInputStream(pipedInputStream0);
-        try {
-            observableInputStream0.read();
-            fail("Expecting exception: IOException");
-        } catch (IOException e) {
-            //
-            // Pipe not connected
-            //
-            verifyException("java.io.PipedInputStream", e);
-        }
+    /**
+     * Tests that an {@link IOException} thrown by the underlying stream is correctly
+     * propagated by the {@link ObservableInputStream#read()} method.
+     */
+    @Test(expected = IOException.class, timeout = 4000)
+    public void readShouldPropagateIOExceptionFromUnderlyingStream() throws IOException {
+        // Arrange: Create an ObservableInputStream with an underlying stream that is
+        // guaranteed to throw an IOException on read. A PipedInputStream that is not
+        // connected to a PipedOutputStream serves this purpose.
+        final PipedInputStream unconnectedPipe = new PipedInputStream();
+        final ObservableInputStream observableStream = new ObservableInputStream(unconnectedPipe);
+
+        // Act: Attempt to read from the stream. This should trigger the IOException
+        // from the unconnected pipe, which is expected to be propagated.
+        observableStream.read();
     }
 }
