cbde9148a8945fa402fb7495175f941fde29ce50
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:55:37 +0800

    gemini processedAutoTestCases isochronology_estest3.java

--- isochronology_estest3.java
+++ isochronology_estest3.java
@@ -1,20 +1,25 @@
 package org.joda.time.chrono;
 
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.joda.time.Chronology;
-import org.joda.time.DateTimeZone;
-import org.junit.runner.RunWith;
 
-public class ISOChronology_ESTestTest3 extends ISOChronology_ESTest_scaffolding {
+/**
+ * Unit tests for {@link ISOChronology}.
+ */
+public class ISOChronologyTest {
 
-    @Test(timeout = 4000)
-    public void test02() throws Throwable {
-        ISOChronology iSOChronology0 = ISOChronology.getInstanceUTC();
-        boolean boolean0 = iSOChronology0.equals(iSOChronology0);
-        assertTrue(boolean0);
+    /**
+     * Tests the reflexive property of the equals() method.
+     * An object must always be equal to itself.
+     */
+    @Test
+    public void equals_returnsTrue_whenComparingInstanceToItself() {
+        // Arrange
+        ISOChronology chronology = ISOChronology.getInstanceUTC();
+
+        // Act & Assert
+        // An object must be equal to itself. Using assertEquals is more idiomatic
+        // for equality checks and provides better failure messages than assertTrue.
+        assertEquals(chronology, chronology);
     }
 }
