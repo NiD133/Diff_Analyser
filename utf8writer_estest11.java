f57effd47cdecbc707ae9bbb1fb7d17b4ecb4314
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:25:32 +0800

    Original processedAutoTestCases utf8writer_estest11.java

---
+++ utf8writer_estest11.java
@@ -0,0 +1,40 @@
+package com.fasterxml.jackson.core.io;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.fasterxml.jackson.core.ErrorReportConfiguration;
+import com.fasterxml.jackson.core.StreamReadConstraints;
+import com.fasterxml.jackson.core.StreamWriteConstraints;
+import com.fasterxml.jackson.core.util.BufferRecycler;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PipedOutputStream;
+import java.io.Writer;
+import java.nio.CharBuffer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockFileOutputStream;
+import org.evosuite.runtime.mock.java.io.MockPrintStream;
+import org.junit.runner.RunWith;
+
+public class UTF8Writer_ESTestTest11 extends UTF8Writer_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test10() throws Throwable {
+        StreamReadConstraints streamReadConstraints0 = StreamReadConstraints.defaults();
+        StreamWriteConstraints streamWriteConstraints0 = StreamWriteConstraints.defaults();
+        ErrorReportConfiguration errorReportConfiguration0 = ErrorReportConfiguration.defaults();
+        BufferRecycler bufferRecycler0 = new BufferRecycler();
+        ContentReference contentReference0 = ContentReference.unknown();
+        IOContext iOContext0 = new IOContext(streamReadConstraints0, streamWriteConstraints0, errorReportConfiguration0, bufferRecycler0, contentReference0, true);
+        MockFileOutputStream mockFileOutputStream0 = new MockFileOutputStream("_C14\u0007{V6YYhZLLho", false);
+        UTF8Writer uTF8Writer0 = new UTF8Writer(iOContext0, mockFileOutputStream0);
+        char[] charArray0 = new char[26];
+        uTF8Writer0.write(charArray0, 1114111, Integer.MAX_VALUE);
+        assertEquals((-56613888), UTF8Writer.SURROGATE_BASE);
+    }
+}
