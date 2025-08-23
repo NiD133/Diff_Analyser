e4f08da6117417cf64dd9fc7b27f04c52643f757
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:17:45 +0800

    Original processedAutoTestCases standardbarpainter_estest11.java

---
+++ standardbarpainter_estest11.java
@@ -0,0 +1,27 @@
+package org.jfree.chart.renderer.category;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.awt.Graphics2D;
+import java.awt.geom.Arc2D;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.RectangularShape;
+import java.awt.image.BufferedImage;
+import javax.swing.text.DefaultCaret;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.jfree.chart.api.RectangleEdge;
+import org.jfree.chart.util.GradientPaintTransformer;
+import org.junit.runner.RunWith;
+
+public class StandardBarPainter_ESTestTest11 extends StandardBarPainter_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test10() throws Throwable {
+        StandardBarPainter standardBarPainter0 = new StandardBarPainter();
+        boolean boolean0 = standardBarPainter0.equals(standardBarPainter0);
+        assertTrue(boolean0);
+    }
+}
