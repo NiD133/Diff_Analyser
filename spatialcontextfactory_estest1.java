51bef63564b6512eee34f4d991063217da2bdcc9
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:17:14 +0800

    Original processedAutoTestCases spatialcontextfactory_estest1.java

---
+++ spatialcontextfactory_estest1.java
@@ -0,0 +1,32 @@
+package org.locationtech.spatial4j.context;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.HashMap;
+import java.util.Map;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+import org.locationtech.spatial4j.io.PolyshapeReader;
+import org.locationtech.spatial4j.shape.ShapeFactory;
+
+public class SpatialContextFactory_ESTestTest1 extends SpatialContextFactory_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test00() throws Throwable {
+        HashMap<String, String> hashMap0 = new HashMap<String, String>();
+        hashMap0.put("shapeFactoryClass", "org.locationtech.spatial4j.shape.impl.BufferedLineString");
+        ClassLoader classLoader0 = ClassLoader.getSystemClassLoader();
+        // Undeclared exception!
+        try {
+            SpatialContextFactory.makeSpatialContext(hashMap0, classLoader0);
+            fail("Expecting exception: RuntimeException");
+        } catch (RuntimeException e) {
+            //
+            // class org.locationtech.spatial4j.shape.impl.BufferedLineString needs a constructor that takes: [SpatialContext{geo=true, calculator=null, worldBounds=null}, org.locationtech.spatial4j.context.SpatialContextFactory@1]
+            //
+            verifyException("org.locationtech.spatial4j.context.SpatialContextFactory", e);
+        }
+    }
+}
