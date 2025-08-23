a40daad9f57c2f839644505e94c43a8e7f47e453
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:37:06 +0800

    gemini processedAutoTestCases accumulatorpathvisitor_estest26.java

--- accumulatorpathvisitor_estest26.java
+++ accumulatorpathvisitor_estest26.java
@@ -1,45 +1,32 @@
 package org.apache.commons.io.file;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
+
 import java.io.IOException;
-import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.Comparator;
-import java.util.List;
-import java.util.regex.Pattern;
-import org.apache.commons.io.filefilter.CanExecuteFileFilter;
-import org.apache.commons.io.filefilter.CanReadFileFilter;
-import org.apache.commons.io.filefilter.CanWriteFileFilter;
-import org.apache.commons.io.filefilter.HiddenFileFilter;
-import org.apache.commons.io.filefilter.PrefixFileFilter;
-import org.apache.commons.io.filefilter.RegexFileFilter;
-import org.apache.commons.io.function.IOBiFunction;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.evosuite.runtime.mock.java.io.MockFile;
-import org.evosuite.runtime.mock.java.io.MockIOException;
-import org.junit.runner.RunWith;
 
-public class AccumulatorPathVisitor_ESTestTest26 extends AccumulatorPathVisitor_ESTest_scaffolding {
+import static org.junit.Assert.assertThrows;
 
-    @Test(timeout = 4000)
-    public void test25() throws Throwable {
-        AccumulatorPathVisitor accumulatorPathVisitor0 = AccumulatorPathVisitor.withBigIntegerCounters();
-        MockIOException mockIOException0 = new MockIOException();
-        // Undeclared exception!
-        try {
-            accumulatorPathVisitor0.updateDirCounter((Path) null, mockIOException0);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.commons.io.file.AccumulatorPathVisitor", e);
-        }
+/**
+ * Tests for {@link AccumulatorPathVisitor}.
+ * This class focuses on specific behaviors of the visitor's methods.
+ */
+public class AccumulatorPathVisitorTest {
+
+    /**
+     * Tests that calling updateDirCounter with a null Path argument
+     * results in a NullPointerException.
+     */
+    @Test
+    public void updateDirCounterWithNullPathShouldThrowNullPointerException() {
+        // Arrange
+        final AccumulatorPathVisitor visitor = AccumulatorPathVisitor.withBigIntegerCounters();
+        final IOException ioException = new IOException("Test exception");
+
+        // Act & Assert
+        // The method under test is expected to throw a NullPointerException because the 'dir' path is null.
+        assertThrows(NullPointerException.class, () -> {
+            visitor.updateDirCounter(null, ioException);
+        });
     }
 }
