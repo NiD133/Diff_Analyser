1ee2e10b3fac9ee7d9e11dd0a5ca12e1ca733a83
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:22:26 +0800

    gemini processedAutoTestCases tokenqueue_estest29.java

--- tokenqueue_estest29.java
+++ tokenqueue_estest29.java
@@ -1,27 +1,25 @@
 package org.jsoup.parser;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class TokenQueue_ESTestTest29 extends TokenQueue_ESTest_scaffolding {
+/**
+ * Test for verifying the behavior of a TokenQueue after it has been closed.
+ */
+public class TokenQueueClosedStateTest {
 
-    @Test(timeout = 4000)
-    public void test28() throws Throwable {
-        TokenQueue tokenQueue0 = new TokenQueue("v\u0000{Z^PZ");
-        tokenQueue0.close();
-        // Undeclared exception!
-        try {
-            tokenQueue0.matchChomp("v\u0000{Z^PZ");
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.jsoup.parser.CharacterReader", e);
-        }
+    /**
+     * Verifies that attempting to use a TokenQueue after it has been closed
+     * throws a NullPointerException. The close() method nullifies the internal
+     * CharacterReader, and subsequent operations that rely on it should fail.
+     */
+    @Test(expected = NullPointerException.class)
+    public void callingMatchChompOnClosedQueueThrowsNullPointerException() {
+        // Arrange: Create a TokenQueue and immediately close it.
+        TokenQueue queue = new TokenQueue("some data");
+        queue.close();
+
+        // Act & Assert: Attempting to use the closed queue should throw a NullPointerException.
+        // The expected exception is declared in the @Test annotation.
+        queue.matchChomp("some data");
     }
 }
