cc53806915da34ed166ed9117f0ed5271a1fde7d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:37:53 +0800

    Original processedAutoTestCases atomicdoublearray_estest29.java

---
+++ atomicdoublearray_estest29.java
@@ -0,0 +1,30 @@
+package com.google.common.util.concurrent;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.function.DoubleBinaryOperator;
+import java.util.function.DoubleUnaryOperator;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class AtomicDoubleArray_ESTestTest29 extends AtomicDoubleArray_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test28() throws Throwable {
+        AtomicDoubleArray atomicDoubleArray0 = new AtomicDoubleArray(3);
+        // Undeclared exception!
+        try {
+            atomicDoubleArray0.getAndAccumulate(3, 3, (DoubleBinaryOperator) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("com.google.common.base.Preconditions", e);
+        }
+    }
+}
