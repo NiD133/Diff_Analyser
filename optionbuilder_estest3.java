1ddb02703b734086409a4ca9040ed6c3e9ece943
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:06:17 +0800

    gemini processedAutoTestCases optionbuilder_estest3.java

--- optionbuilder_estest3.java
+++ optionbuilder_estest3.java
@@ -1,19 +1,33 @@
 package org.apache.commons.cli;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class OptionBuilder_ESTestTest3 extends OptionBuilder_ESTest_scaffolding {
+/**
+ * Tests for the {@link OptionBuilder} class.
+ *
+ * Note: {@link OptionBuilder} is a deprecated, stateful static utility.
+ * Tests on such classes require careful management of the static state
+ * between test runs, typically by resetting it in a @Before method.
+ */
+public class OptionBuilderTest {
 
-    @Test(timeout = 4000)
-    public void test02() throws Throwable {
-        OptionBuilder.withValueSeparator('>');
-        Option option0 = OptionBuilder.create((String) null);
-        assertEquals('>', option0.getValueSeparator());
-        assertEquals((-1), option0.getArgs());
+    @Test
+    public void withValueSeparatorShouldSetSeparatorOnCreatedOption() {
+        // Arrange: Configure the builder to use a specific value separator.
+        final char expectedSeparator = '>';
+        OptionBuilder.withValueSeparator(expectedSeparator);
+
+        // Act: Create an Option instance. The null argument signifies that
+        // the option has no short name (e.g., "-f").
+        Option option = OptionBuilder.create((String) null);
+
+        // Assert: Verify that the created option has the correct properties.
+        assertEquals("The value separator should be set on the created option.",
+                     expectedSeparator, option.getValueSeparator());
+
+        // Verify that other properties remain at their default state.
+        assertEquals("The number of arguments should be uninitialized by default.",
+                     Option.UNINITIALIZED, option.getArgs());
     }
 }
