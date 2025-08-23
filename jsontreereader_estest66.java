f6d06572a6c651643e1596a843ab5bbe63b4841a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:58:14 +0800

    Original processedAutoTestCases jsontreereader_estest66.java

---
+++ jsontreereader_estest66.java
@@ -0,0 +1,29 @@
+package com.google.gson.internal.bind;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonNull;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.Strictness;
+import com.google.gson.stream.JsonToken;
+import java.io.IOException;
+import java.util.ConcurrentModificationException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class JsonTreeReader_ESTestTest66 extends JsonTreeReader_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test065() throws Throwable {
+        JsonNull jsonNull0 = JsonNull.INSTANCE;
+        JsonTreeReader jsonTreeReader0 = new JsonTreeReader(jsonNull0);
+        jsonTreeReader0.nextJsonElement();
+        jsonTreeReader0.skipValue();
+        assertFalse(jsonTreeReader0.isLenient());
+    }
+}
