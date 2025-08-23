42aa613e3e7dc7f3cf4babb5043f9e0fb149bb56
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:53:56 +0800

    gemini processedAutoTestCases indexedcollection_estest21.java

--- indexedcollection_estest21.java
+++ indexedcollection_estest21.java
@@ -1,68 +1,49 @@
 package org.apache.commons.collections4.collection;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.ConcurrentModificationException;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Set;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.Predicate;
 import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AllPredicate;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ChainedTransformer;
-import org.apache.commons.collections4.functors.CloneTransformer;
-import org.apache.commons.collections4.functors.ClosureTransformer;
-import org.apache.commons.collections4.functors.ConstantFactory;
 import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.DefaultEquator;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.FactoryTransformer;
-import org.apache.commons.collections4.functors.FalsePredicate;
-import org.apache.commons.collections4.functors.ForClosure;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.NOPClosure;
-import org.apache.commons.collections4.functors.NOPTransformer;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NotNullPredicate;
-import org.apache.commons.collections4.functors.NullIsFalsePredicate;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.SwitchTransformer;
-import org.apache.commons.collections4.functors.TransformedPredicate;
-import org.apache.commons.collections4.functors.TransformerClosure;
-import org.apache.commons.collections4.functors.TransformerPredicate;
-import org.apache.commons.collections4.functors.TruePredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
+import java.util.LinkedList;
+import java.util.ConcurrentModificationException;
+
+import static org.junit.Assert.fail;
+
+/**
+ * Test suite for {@link IndexedCollection}.
+ * This class contains the refactored test case.
+ */
 public class IndexedCollection_ESTestTest21 extends IndexedCollection_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test20() throws Throwable {
-        Transformer<Object, Object> transformer0 = ConstantTransformer.nullTransformer();
-        LinkedList<Object> linkedList0 = new LinkedList<Object>();
-        IndexedCollection<Object, Object> indexedCollection0 = IndexedCollection.nonUniqueIndexedCollection((Collection<Object>) linkedList0, transformer0);
-        linkedList0.add((Object) indexedCollection0);
-        linkedList0.offerFirst(transformer0);
-        indexedCollection0.add(linkedList0);
-        // Undeclared exception!
+    /**
+     * Tests that a ConcurrentModificationException is thrown when removeAll is called
+     * with the underlying collection as its argument.
+     *
+     * The removeAll() implementation iterates over the provided collection and calls
+     * remove() on the decorated collection for each element. When the argument is the
+     * decorated collection itself, this leads to modifying the collection while iterating
+     * over it, which is an illegal state.
+     */
+    @Test
+    public void removeAll_whenModifyingUnderlyingCollectionDuringIteration_throwsConcurrentModificationException() {
+        // Arrange
+        final LinkedList<String> baseList = new LinkedList<>();
+        baseList.add("Element 1");
+        baseList.add("Element 2");
+
+        // A transformer is required for the constructor, but its behavior is not relevant to this test.
+        final Transformer<String, String> keyTransformer = ConstantTransformer.constantTransformer("anyKey");
+
+        final IndexedCollection<String, String> indexedCollection =
+            IndexedCollection.nonUniqueIndexedCollection(baseList, keyTransformer);
+
+        // Act & Assert
         try {
-            indexedCollection0.removeAll(linkedList0);
-            fail("Expecting exception: ConcurrentModificationException");
-        } catch (ConcurrentModificationException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("java.util.LinkedList$ListItr", e);
+            // This call will fail because it attempts to modify baseList while iterating over it.
+            indexedCollection.removeAll(baseList);
+            fail("Expected a ConcurrentModificationException to be thrown.");
+        } catch (final ConcurrentModificationException e) {
+            // This is the expected outcome. The test passes.
         }
     }
 }
