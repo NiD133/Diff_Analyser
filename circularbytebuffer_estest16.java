7040e1f84a4127dfca6b686421f273b54271effb
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:43:17 +0800

    gemini processedAutoTestCases circularbytebuffer_estest16.java

--- circularbytebuffer_estest16.java
+++ circularbytebuffer_estest16.java
@@ -1,26 +1,26 @@
 package org.apache.commons.io.input.buffer;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class CircularByteBuffer_ESTestTest16 extends CircularByteBuffer_ESTest_scaffolding {
+/**
+ * Tests for the {@link CircularByteBuffer} class.
+ */
+public class CircularByteBufferTest {
 
-    @Test(timeout = 4000)
-    public void test15() throws Throwable {
-        CircularByteBuffer circularByteBuffer0 = new CircularByteBuffer();
-        // Undeclared exception!
-        try {
-            circularByteBuffer0.read((byte[]) null, 30, 30);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // targetBuffer
-            //
-            verifyException("java.util.Objects", e);
-        }
+    /**
+     * Tests that the read(byte[], int, int) method throws a NullPointerException
+     * when the target buffer argument is null.
+     */
+    @Test(expected = NullPointerException.class)
+    public void readShouldThrowNullPointerExceptionForNullTargetBuffer() {
+        // Arrange: Create a buffer instance. The offset and length values are arbitrary
+        // as the method should fail before they are used.
+        final CircularByteBuffer buffer = new CircularByteBuffer();
+        final int offset = 30;
+        final int length = 30;
+
+        // Act & Assert: Call the read method with a null buffer.
+        // The @Test(expected) annotation asserts that a NullPointerException is thrown.
+        buffer.read(null, offset, length);
     }
 }
