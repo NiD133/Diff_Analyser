cdf70e70c55c9f6ce33cf92df128a01d5551731c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:31:00 +0800

    gemini processedManualTestCases metaphonetest12.java

--- metaphonetest12.java
+++ metaphonetest12.java
@@ -2,57 +2,55 @@ package org.apache.commons.codec.language;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.stream.Stream;
 import org.apache.commons.codec.AbstractStringEncoderTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
-public class MetaphoneTestTest12 extends AbstractStringEncoderTest<Metaphone> {
-
-    public void assertIsMetaphoneEqual(final String source, final String[] matches) {
-        // match source to all matches
-        for (final String matche : matches) {
-            assertTrue(getStringEncoder().isMetaphoneEqual(source, matche), "Source: " + source + ", should have same Metaphone as: " + matche);
-        }
-        // match to each other
-        for (final String matche : matches) {
-            for (final String matche2 : matches) {
-                assertTrue(getStringEncoder().isMetaphoneEqual(matche, matche2));
-            }
-        }
-    }
-
-    public void assertMetaphoneEqual(final String[][] pairs) {
-        validateFixture(pairs);
-        for (final String[] pair : pairs) {
-            final String name0 = pair[0];
-            final String name1 = pair[1];
-            final String failMsg = "Expected match between " + name0 + " and " + name1;
-            assertTrue(getStringEncoder().isMetaphoneEqual(name0, name1), failMsg);
-            assertTrue(getStringEncoder().isMetaphoneEqual(name1, name0), failMsg);
-        }
-    }
+/**
+ * Tests the Metaphone class, focusing on its ability to identify phonetically similar strings.
+ */
+class MetaphoneTest extends AbstractStringEncoderTest<Metaphone> {
 
     @Override
     protected Metaphone createStringEncoder() {
         return new Metaphone();
     }
 
-    public void validateFixture(final String[][] pairs) {
-        if (pairs.length == 0) {
-            fail("Test fixture is empty");
-        }
-        for (int i = 0; i < pairs.length; i++) {
-            if (pairs[i].length != 2) {
-                fail("Error in test fixture in the data array at index " + i);
-            }
-        }
+    /**
+     * Provides a stream of names that are phonetically equivalent to "Mary".
+     * According to the Metaphone algorithm, all these names should produce the code "MR".
+     * Data was originally computed from http://www.lanw.com/java/phonetic/default.htm
+     *
+     * @return A stream of strings.
+     */
+    static Stream<String> phoneticEquivalentsOfMary() {
+        return Stream.of(
+            "Mary", "Mair", "Maire", "Mara", "Mareah", "Mari", "Maria",
+            "Marie", "Maura", "Maure", "Meara", "Merrie", "Merry", "Mira",
+            "Moira", "Mora", "Moria", "Moyra", "Muire", "Myra", "Myrah"
+        );
     }
 
-    /**
-     * Match data computed from http://www.lanw.com/java/phonetic/default.htm
-     */
-    @Test
-    void testIsMetaphoneEqualMary() {
-        assertIsMetaphoneEqual("Mary", new String[] { "Mair", "Maire", "Mara", "Mareah", "Mari", "Maria", "Marie", "Mary", "Maura", "Maure", "Meara", "Merrie", "Merry", "Mira", "Moira", "Mora", "Moria", "Moyra", "Muire", "Myra", "Myrah" });
+    @DisplayName("isMetaphoneEqual should return true for all phonetic equivalents of 'Mary'")
+    @ParameterizedTest(name = "isMetaphoneEqual(\"Mary\", \"{0}\")")
+    @MethodSource("phoneticEquivalentsOfMary")
+    void testIsMetaphoneEqualWithPhoneticEquivalents(final String name) {
+        // All names in the source stream are phonetically equivalent to "Mary".
+        assertTrue(getStringEncoder().isMetaphoneEqual("Mary", name),
+            () -> "Expected '" + name + "' to be phonetically equal to 'Mary'");
+    }
+
+    @DisplayName("metaphone should return 'MR' for all phonetic equivalents of 'Mary'")
+    @ParameterizedTest(name = "metaphone(\"{0}\")")
+    @MethodSource("phoneticEquivalentsOfMary")
+    void testMetaphoneEncodingOfPhoneticEquivalents(final String name) {
+        // This is a more direct test that verifies the actual Metaphone code.
+        // The core functionality of isMetaphoneEqual(a, b) is metaphone(a).equals(metaphone(b)).
+        // Testing the direct output is more robust.
+        assertEquals("MR", getStringEncoder().metaphone(name));
     }
 }
