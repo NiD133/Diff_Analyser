eb28b0a3f0cc8802a0ba4ad28f978f416d351148
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:47:44 +0800

    gemini processedAutoTestCases defaultpiedataset_estest33.java

--- defaultpiedataset_estest33.java
+++ defaultpiedataset_estest33.java
@@ -1,31 +1,20 @@
 package org.jfree.data.general;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.time.chrono.ChronoLocalDate;
-import java.util.List;
-import java.util.Vector;
-import javax.swing.JLayeredPane;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.jfree.chart.api.SortOrder;
-import org.jfree.chart.api.TableOrder;
-import org.jfree.data.DefaultKeyedValues;
-import org.jfree.data.KeyedValues;
-import org.jfree.data.category.CategoryDataset;
-import org.jfree.data.category.CategoryToPieDataset;
-import org.jfree.data.category.DefaultIntervalCategoryDataset;
-import org.jfree.data.category.SlidingCategoryDataset;
-import org.jfree.data.statistics.DefaultBoxAndWhiskerCategoryDataset;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertTrue;
 
-public class DefaultPieDataset_ESTestTest33 extends DefaultPieDataset_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link DefaultPieDataset} class.
+ */
+public class DefaultPieDatasetTest {
 
-    @Test(timeout = 4000)
-    public void test32() throws Throwable {
-        DefaultKeyedValuesDataset<Integer> defaultKeyedValuesDataset0 = new DefaultKeyedValuesDataset<Integer>();
-        boolean boolean0 = defaultKeyedValuesDataset0.equals(defaultKeyedValuesDataset0);
-        assertTrue(boolean0);
+    @Test
+    public void equals_returnsTrueForSameInstance() {
+        // Arrange
+        DefaultPieDataset<String> dataset = new DefaultPieDataset<>();
+
+        // Act & Assert
+        // According to the contract of Object.equals(), an object must be equal to itself (reflexivity).
+        assertTrue(dataset.equals(dataset));
     }
 }
