5a1bb052677c5174d841339115081edae66ffd7e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:08:08 +0800

    Original processedAutoTestCases pdfdictionary_estest7.java

---
+++ pdfdictionary_estest7.java
@@ -0,0 +1,31 @@
+package com.itextpdf.text.pdf;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.collection.PdfCollectionField;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PipedOutputStream;
+import java.util.LinkedHashMap;
+import java.util.Set;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockPrintStream;
+import org.junit.runner.RunWith;
+
+public class PdfDictionary_ESTestTest7 extends PdfDictionary_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test06() throws Throwable {
+        PdfName pdfName0 = PdfWriter.PDF_VERSION_1_6;
+        PdfResources pdfResources0 = new PdfResources();
+        pdfResources0.toString();
+        pdfResources0.putEx(pdfName0, pdfName0);
+        pdfResources0.getAsString(pdfName0);
+        PdfObject pdfObject0 = pdfResources0.getDirectObject(pdfName0);
+        assertEquals(4, PdfObject.NAME);
+    }
+}
