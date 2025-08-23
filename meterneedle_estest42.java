a9c77dd3d08cb1bb877621cdebc95c630359d30c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:02:59 +0800

    Original processedAutoTestCases meterneedle_estest42.java

---
+++ meterneedle_estest42.java
@@ -0,0 +1,34 @@
+package org.jfree.chart.plot.compass;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Paint;
+import java.awt.Rectangle;
+import java.awt.Stroke;
+import java.awt.SystemColor;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.text.AttributedCharacterIterator;
+import javax.swing.JScrollPane;
+import javax.swing.text.DefaultCaret;
+import javax.swing.tree.DefaultTreeCellRenderer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class MeterNeedle_ESTestTest42 extends MeterNeedle_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test41() throws Throwable {
+        LongNeedle longNeedle0 = new LongNeedle();
+        double double0 = longNeedle0.getRotateY();
+        assertEquals(0.5, longNeedle0.getRotateX(), 0.01);
+        assertEquals(0.8, double0, 0.01);
+        assertEquals(5, longNeedle0.getSize());
+    }
+}
