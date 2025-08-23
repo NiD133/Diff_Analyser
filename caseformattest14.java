a68eb98eae543a8f622c8df2da1e5623efd7b6ca
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:26:20 +0800

    gemini processedManualTestCases caseformattest14.java

--- caseformattest14.java
+++ caseformattest14.java
@@ -1,25 +1,42 @@
 package com.google.common.base;
 
 import static com.google.common.base.CaseFormat.LOWER_CAMEL;
-import static com.google.common.base.CaseFormat.LOWER_HYPHEN;
 import static com.google.common.base.CaseFormat.LOWER_UNDERSCORE;
-import static com.google.common.base.CaseFormat.UPPER_CAMEL;
-import static com.google.common.base.CaseFormat.UPPER_UNDERSCORE;
 import static com.google.common.truth.Truth.assertThat;
-import static com.google.common.truth.Truth.assertWithMessage;
-import com.google.common.annotations.GwtCompatible;
-import com.google.common.annotations.GwtIncompatible;
-import com.google.common.annotations.J2ktIncompatible;
-import com.google.common.testing.NullPointerTester;
-import com.google.common.testing.SerializableTester;
-import junit.framework.TestCase;
-import org.jspecify.annotations.NullUnmarked;
 
+import junit.framework.TestCase;
+
+/**
+ * Tests for converting from {@link CaseFormat#LOWER_CAMEL} to {@link CaseFormat#LOWER_UNDERSCORE}.
+ */
 public class CaseFormatTestTest14 extends TestCase {
 
-    public void testLowerCamelToLowerUnderscore() {
-        assertThat(LOWER_CAMEL.to(LOWER_UNDERSCORE, "foo")).isEqualTo("foo");
-        assertThat(LOWER_CAMEL.to(LOWER_UNDERSCORE, "fooBar")).isEqualTo("foo_bar");
-        assertThat(LOWER_CAMEL.to(LOWER_UNDERSCORE, "hTTP")).isEqualTo("h_t_t_p");
-    }
+  /**
+   * Tests that a single, all-lowercase word is not modified by the conversion, as it contains no
+   * word boundaries.
+   */
+  public void testLowerCamelToUnderscore_singleWordIsUnchanged() {
+    String input = "foo";
+    String result = LOWER_CAMEL.to(LOWER_UNDERSCORE, input);
+    assertThat(result).isEqualTo("foo");
+  }
+
+  /**
+   * Tests the standard conversion of a multi-word lowerCamelCase string to lower_underscore_case.
+   */
+  public void testLowerCamelToUnderscore_convertsStandardInput() {
+    String input = "fooBar";
+    String result = LOWER_CAMEL.to(LOWER_UNDERSCORE, input);
+    assertThat(result).isEqualTo("foo_bar");
+  }
+
+  /**
+   * Tests that consecutive capital letters, often found in acronyms, are each treated as a new
+   * word, separated by underscores.
+   */
+  public void testLowerCamelToUnderscore_handlesAcronyms() {
+    String input = "hTTP";
+    String result = LOWER_CAMEL.to(LOWER_UNDERSCORE, input);
+    assertThat(result).isEqualTo("h_t_t_p");
+  }
 }
