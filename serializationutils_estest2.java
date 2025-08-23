74db4ad9b4467c7254c811cede303443bb34e833
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:14:44 +0800

    gemini processedAutoTestCases serializationutils_estest2.java

--- serializationutils_estest2.java
+++ serializationutils_estest2.java
@@ -1,38 +1,28 @@
 package org.apache.commons.lang3;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.ByteArrayInputStream;
-import java.io.File;
+import static org.junit.Assert.assertThrows;
+
 import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.PipedInputStream;
-import java.io.PipedOutputStream;
-import java.io.SequenceInputStream;
-import java.io.Serializable;
-import java.util.HashMap;
-import java.util.Locale;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.io.MockFile;
-import org.evosuite.runtime.mock.java.io.MockFileInputStream;
-import org.evosuite.runtime.mock.java.io.MockPrintStream;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class SerializationUtils_ESTestTest2 extends SerializationUtils_ESTest_scaffolding {
+/**
+ * Unit tests for the inner classes of {@link SerializationUtils}.
+ */
+public class SerializationUtilsTest {
 
-    @Test(timeout = 4000)
-    public void test01() throws Throwable {
-        ClassLoader classLoader0 = ClassLoader.getSystemClassLoader();
-        SerializationUtils.ClassLoaderAwareObjectInputStream serializationUtils_ClassLoaderAwareObjectInputStream0 = null;
-        try {
-            serializationUtils_ClassLoaderAwareObjectInputStream0 = new SerializationUtils.ClassLoaderAwareObjectInputStream((InputStream) null, classLoader0);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-        }
+    /**
+     * Tests that the constructor of {@code ClassLoaderAwareObjectInputStream} throws
+     * a {@code NullPointerException} when the provided {@code InputStream} is null.
+     */
+    @Test
+    public void classLoaderAwareObjectInputStreamConstructor_throwsNPE_forNullInputStream() {
+        // Arrange
+        final ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+
+        // Act & Assert
+        // The constructor's call to super(InputStream) is expected to throw the NPE.
+        assertThrows(NullPointerException.class, () ->
+                new SerializationUtils.ClassLoaderAwareObjectInputStream(null, classLoader)
+        );
     }
 }
