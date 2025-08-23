43313b4bca73f97cb393086c73081e3a4e9dd429
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:34:20 +0800

    Original processedManualTestCases stringutiltest8.java

---
+++ stringutiltest8.java
@@ -0,0 +1,21 @@
+package org.jsoup.internal;
+
+import org.jsoup.Jsoup;
+import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import java.util.Collections;
+import static org.jsoup.internal.StringUtil.normaliseWhitespace;
+import static org.jsoup.internal.StringUtil.resolve;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StringUtilTestTest8 {
+
+    @Test
+    public void normaliseWhiteSpaceHandlesHighSurrogates() {
+        String test71540chars = "\ud869\udeb2\u304b\u309a  1";
+        String test71540charsExpectedSingleWhitespace = "\ud869\udeb2\u304b\u309a 1";
+        assertEquals(test71540charsExpectedSingleWhitespace, normaliseWhitespace(test71540chars));
+        String extractedText = Jsoup.parse(test71540chars).text();
+        assertEquals(test71540charsExpectedSingleWhitespace, extractedText);
+    }
+}
