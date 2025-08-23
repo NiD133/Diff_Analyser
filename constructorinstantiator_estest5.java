8d9acc45e9bb99014376c3a5d3663530b4955925
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:45:33 +0800

    gemini processedAutoTestCases constructorinstantiator_estest5.java

--- constructorinstantiator_estest5.java
+++ constructorinstantiator_estest5.java
@@ -1,24 +1,46 @@
 package org.mockito.internal.creation.instance;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import org.mockito.creation.instance.InstantiationException;
+import org.mockito.creation.instance.Instantiator;
 
-public class ConstructorInstantiator_ESTestTest5 extends ConstructorInstantiator_ESTest_scaffolding {
+import static org.junit.Assert.fail;
+import static org.junit.Assert.assertTrue;
 
-    @Test(timeout = 4000)
-    public void test4() throws Throwable {
-        Object[] objectArray0 = new Object[7];
-        ConstructorInstantiator constructorInstantiator0 = new ConstructorInstantiator(true, objectArray0);
-        Class<Object> class0 = Object.class;
-        // Undeclared exception!
+/**
+ * Test suite for {@link ConstructorInstantiator}.
+ */
+public class ConstructorInstantiatorTest {
+
+    /**
+     * This test verifies that the ConstructorInstantiator throws an InstantiationException
+     * when it is asked to create an instance of a class for which no constructor
+     * matches the provided arguments.
+     */
+    @Test
+    public void newInstance_shouldThrowException_whenNoConstructorMatchesArguments() {
+        // Arrange: Create an instantiator with arguments that do not match any
+        // constructor of the target class. The java.lang.Object class only has a
+        // default, no-argument constructor.
+        Object[] constructorArgs = new Object[]{"some-argument"};
+        Instantiator instantiator = new ConstructorInstantiator(false, constructorArgs);
+        Class<Object> classToInstantiate = Object.class;
+
+        // Act & Assert: We expect an InstantiationException because no constructor
+        // in Object.class accepts a single argument.
         try {
-            constructorInstantiator0.newInstance(class0);
-            fail("Expecting exception: RuntimeException");
-        } catch (RuntimeException e) {
+            instantiator.newInstance(classToInstantiate);
+            fail("Expected an InstantiationException to be thrown, but the method completed successfully.");
+        } catch (InstantiationException e) {
+            // Success: The expected exception was caught.
+            // For a more robust test, we can verify the exception message.
+            String expectedMessagePart = "Unable to create instance of 'Object'";
+            assertTrue(
+                "Exception message should indicate which class could not be instantiated.",
+                e.getMessage().contains(expectedMessagePart)
+            );
+        } catch (Exception e) {
+            fail("Expected an InstantiationException, but a different exception was thrown: " + e.getClass().getName());
         }
     }
 }
