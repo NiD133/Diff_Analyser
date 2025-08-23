8f4a0d84b8faca5ee29aee9e3f472b0ed3985e80
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:04:10 +0800

    Original processedAutoTestCases multireadonlyseekablebytechannel_estest18.java

---
+++ multireadonlyseekablebytechannel_estest18.java
@@ -0,0 +1,38 @@
+package org.apache.commons.compress.utils;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.ClosedChannelException;
+import java.nio.channels.FileChannel;
+import java.nio.channels.NonWritableChannelException;
+import java.nio.channels.SeekableByteChannel;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.OpenOption;
+import java.nio.file.Path;
+import java.util.LinkedList;
+import java.util.List;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.junit.runner.RunWith;
+
+public class MultiReadOnlySeekableByteChannel_ESTestTest18 extends MultiReadOnlySeekableByteChannel_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test17() throws Throwable {
+        MultiReadOnlySeekableByteChannel multiReadOnlySeekableByteChannel0 = null;
+        try {
+            multiReadOnlySeekableByteChannel0 = new MultiReadOnlySeekableByteChannel((List<SeekableByteChannel>) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // channels
+            //
+            verifyException("java.util.Objects", e);
+        }
+    }
+}
