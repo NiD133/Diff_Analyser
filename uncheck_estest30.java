1829d946c0940e885c962e4b615e950daaaddd55
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:24:34 +0800

    gemini processedAutoTestCases uncheck_estest30.java

--- uncheck_estest30.java
+++ uncheck_estest30.java
@@ -1,37 +1,20 @@
 package org.apache.commons.io.function;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.time.chrono.HijrahEra;
-import java.util.Comparator;
-import java.util.concurrent.ForkJoinTask;
-import java.util.function.BiConsumer;
-import java.util.function.BinaryOperator;
-import java.util.function.Function;
 import java.util.function.Supplier;
-import java.util.stream.Collector;
-import java.util.stream.LongStream;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class Uncheck_ESTestTest30 extends Uncheck_ESTest_scaffolding {
+/**
+ * Tests for {@link Uncheck}.
+ */
+public class UncheckTest {
 
-    @Test(timeout = 4000)
-    public void test29() throws Throwable {
-        // Undeclared exception!
-        try {
-            Uncheck.get((IOSupplier<String>) null, (Supplier<String>) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.commons.io.function.Uncheck", e);
-        }
+    /**
+     * Tests that {@link Uncheck#get(IOSupplier, Supplier)} throws a
+     * NullPointerException when the IOSupplier argument is null.
+     */
+    @Test(expected = NullPointerException.class)
+    public void testGetWithNullIOSupplierThrowsNullPointerException() {
+        // The call should fail because the first argument (the supplier) is null.
+        Uncheck.get((IOSupplier<String>) null, (Supplier<String>) null);
     }
 }
