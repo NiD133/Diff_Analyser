55ccbcd6db647aa5c11a8a803e786983d29235de
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:02:45 +0800

    gemini processedAutoTestCases meterinterval_estest5.java

--- meterinterval_estest5.java
+++ meterinterval_estest5.java
@@ -1,29 +1,37 @@
 package org.jfree.chart.plot;
 
+import org.jfree.data.Range;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.awt.BasicStroke;
+
 import java.awt.Color;
-import java.awt.GradientPaint;
 import java.awt.Paint;
 import java.awt.Stroke;
-import java.awt.SystemColor;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.jfree.data.Range;
-import org.jfree.data.statistics.DefaultBoxAndWhiskerCategoryDataset;
-import org.jfree.data.statistics.HistogramType;
-import org.junit.runner.RunWith;
 
-public class MeterInterval_ESTestTest5 extends MeterInterval_ESTest_scaffolding {
+import static org.junit.Assert.assertNull;
 
-    @Test(timeout = 4000)
-    public void test04() throws Throwable {
-        Range range0 = new Range((-229.41607020985958), (-229.41607020985958));
-        Color color0 = Color.gray;
-        MeterInterval meterInterval0 = new MeterInterval(",", range0, color0, (Stroke) null, color0);
-        Stroke stroke0 = meterInterval0.getOutlineStroke();
-        assertNull(stroke0);
+/**
+ * Unit tests for the {@link MeterInterval} class.
+ */
+public class MeterIntervalTest {
+
+    /**
+     * Verifies that getOutlineStroke() returns null when the MeterInterval
+     * is constructed with a null outline stroke.
+     */
+    @Test
+    public void getOutlineStroke_shouldReturnNull_whenConstructedWithNullStroke() {
+        // Arrange: Create a MeterInterval, explicitly passing null for the outline stroke.
+        // The other parameters are simple placeholders, as they are not relevant to this test.
+        String label = "Test Interval";
+        Range range = new Range(0.0, 50.0);
+        Paint dummyPaint = Color.RED;
+        
+        MeterInterval meterInterval = new MeterInterval(label, range, dummyPaint, null, dummyPaint);
+
+        // Act: Retrieve the outline stroke from the object.
+        Stroke retrievedStroke = meterInterval.getOutlineStroke();
+
+        // Assert: The retrieved stroke should be the same null value passed to the constructor.
+        assertNull("The outline stroke should be null as it was set in the constructor.", retrievedStroke);
     }
 }
