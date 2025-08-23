3f711342c508be0f24dfb5f294651a85cc34d900
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:36:09 +0800

    gemini processedManualTestCases xmltreebuildertest19.java

--- xmltreebuildertest19.java
+++ xmltreebuildertest19.java
@@ -2,31 +2,35 @@ package org.jsoup.parser;
 
 import org.jsoup.Jsoup;
 import org.jsoup.TextUtil;
-import org.jsoup.nodes.*;
-import org.jsoup.select.Elements;
+import org.jsoup.nodes.Document;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URISyntaxException;
-import java.nio.charset.StandardCharsets;
-import java.util.List;
-import static org.jsoup.nodes.Document.OutputSettings.Syntax;
-import static org.jsoup.parser.Parser.NamespaceHtml;
-import static org.jsoup.parser.Parser.NamespaceXml;
-import static org.junit.jupiter.api.Assertions.*;
 
-public class XmlTreeBuilderTestTest19 {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-    private static void assertXmlNamespace(Element el) {
-        assertEquals(NamespaceXml, el.tag().namespace(), String.format("Element %s not in XML namespace", el.tagName()));
-    }
+/**
+ * Tests for the configuration of the Jsoup XML Parser.
+ */
+public class XmlParserSettingsTest {
 
     @Test
-    public void canNormalizeCase() {
-        String xml = "<TEST ID=1>Check</TEST>";
-        Document doc = Jsoup.parse(xml, "", Parser.xmlParser().settings(ParseSettings.htmlDefault));
-        assertEquals("<test id=\"1\">Check</test>", TextUtil.stripNewlines(doc.html()));
+    @DisplayName("XML parser with HTML settings should normalize tag and attribute case")
+    void xmlParserShouldNormalizeCaseWhenUsingHtmlSettings() {
+        // Arrange
+        // This test verifies that parser settings can override a parser's default behavior.
+        // We provide an XML string with uppercase tags and attributes.
+        String inputXmlWithUppercase = "<TEST ID=1>Check</TEST>";
+        String expectedNormalizedOutput = "<test id=\"1\">Check</test>";
+
+        // Act
+        // We configure the XML parser to use HTML's default parse settings.
+        // The XML parser normally preserves case, but HTML settings enforce lowercase normalization.
+        Parser parser = Parser.xmlParser().settings(ParseSettings.htmlDefault);
+        Document doc = Jsoup.parse(inputXmlWithUppercase, "", parser);
+        String actualOutput = TextUtil.stripNewlines(doc.html());
+
+        // Assert
+        // The resulting HTML should have lowercase tag and attribute names, as dictated by the HTML settings.
+        assertEquals(expectedNormalizedOutput, actualOutput);
     }
 }
