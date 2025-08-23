e2eb972f84f4550236da4850cf02350ad9165f8a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:17:51 +0800

    Original processedAutoTestCases standardbarpainter_estest8.java

---
+++ standardbarpainter_estest8.java
@@ -0,0 +1,32 @@
+package org.jfree.chart.renderer.category;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.awt.Graphics2D;
+import java.awt.geom.Arc2D;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.RectangularShape;
+import java.awt.image.BufferedImage;
+import javax.swing.text.DefaultCaret;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.jfree.chart.api.RectangleEdge;
+import org.jfree.chart.util.GradientPaintTransformer;
+import org.junit.runner.RunWith;
+
+public class StandardBarPainter_ESTestTest8 extends StandardBarPainter_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test07() throws Throwable {
+        StandardBarPainter standardBarPainter0 = new StandardBarPainter();
+        BufferedImage bufferedImage0 = new BufferedImage(1, 599, 1);
+        Graphics2D graphics2D0 = bufferedImage0.createGraphics();
+        StackedBarRenderer stackedBarRenderer0 = new StackedBarRenderer();
+        Rectangle2D.Double rectangle2D_Double0 = new Rectangle2D.Double(1, (double) stackedBarRenderer0.ZERO, 0.0, 0.0);
+        RectangleEdge rectangleEdge0 = RectangleEdge.TOP;
+        standardBarPainter0.paintBarShadow(graphics2D0, stackedBarRenderer0, 1, 255, rectangle2D_Double0, rectangleEdge0, true);
+        assertTrue(stackedBarRenderer0.getShadowsVisible());
+    }
+}
