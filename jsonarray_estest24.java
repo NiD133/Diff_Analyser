9078691d825f73479bbcf429c7f4d0ec8939bb42
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:56:01 +0800

    Original processedAutoTestCases jsonarray_estest24.java

---
+++ jsonarray_estest24.java
@@ -0,0 +1,24 @@
+package com.google.gson;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Iterator;
+import java.util.List;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class JsonArray_ESTestTest24 extends JsonArray_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test23() throws Throwable {
+        JsonArray jsonArray0 = new JsonArray();
+        Character character0 = Character.valueOf('6');
+        jsonArray0.add(character0);
+        BigDecimal bigDecimal0 = jsonArray0.getAsBigDecimal();
+        assertEquals((short) 6, bigDecimal0.shortValue());
+    }
+}
