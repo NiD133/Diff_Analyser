b011467a02b22e57aca07a4abe39718542cb025c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:07:23 +0800

    Original processedAutoTestCases parameter_estest20.java

---
+++ parameter_estest20.java
@@ -0,0 +1,25 @@
+package com.google.common.reflect;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.lang.annotation.Annotation;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class Parameter_ESTestTest20 extends Parameter_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test19() throws Throwable {
+        Invokable<Object, Annotation> invokable0 = (Invokable<Object, Annotation>) mock(Invokable.class, new ViolatedAssumptionAnswer());
+        Class<Annotation> class0 = Annotation.class;
+        TypeToken<Annotation> typeToken0 = TypeToken.of(class0);
+        Annotation[] annotationArray0 = new Annotation[0];
+        Parameter parameter0 = new Parameter(invokable0, 0, typeToken0, annotationArray0, invokable0);
+        Annotation[] annotationArray1 = parameter0.getAnnotations();
+        assertEquals(0, annotationArray1.length);
+    }
+}
