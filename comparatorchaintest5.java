c7b3660555601ff7abc03171728011d606818410
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:26:59 +0800

    Original processedManualTestCases comparatorchaintest5.java

---
+++ comparatorchaintest5.java
@@ -0,0 +1,110 @@
+package org.apache.commons.collections4.comparators;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class ComparatorChainTestTest5 extends AbstractComparatorTest<ComparatorChainTest.PseudoRow> {
+
+    @Override
+    public List<PseudoRow> getComparableObjectsOrdered() {
+        // this is the correct order assuming a
+        // "0th forward, 1st reverse, 2nd forward" sort
+        return new LinkedList<>(Arrays.asList(new PseudoRow(1, 2, 3), new PseudoRow(2, 3, 5), new PseudoRow(2, 2, 4), new PseudoRow(2, 2, 8), new PseudoRow(3, 1, 0), new PseudoRow(4, 4, 4), new PseudoRow(4, 4, 7)));
+    }
+
+    @Override
+    public String getCompatibilityVersion() {
+        return "4";
+    }
+
+    @Override
+    public Comparator<PseudoRow> makeObject() {
+        final ComparatorChain<PseudoRow> chain = new ComparatorChain<>(new ColumnComparator(0));
+        // reverse the second column
+        chain.addComparator(new ColumnComparator(1), true);
+        chain.addComparator(new ColumnComparator(2), false);
+        return chain;
+    }
+
+    public static class ColumnComparator implements Comparator<PseudoRow>, Serializable {
+
+        private static final long serialVersionUID = -2284880866328872105L;
+
+        protected int colIndex;
+
+        ColumnComparator(final int colIndex) {
+            this.colIndex = colIndex;
+        }
+
+        @Override
+        public int compare(final PseudoRow o1, final PseudoRow o2) {
+            return Integer.compare(o1.getColumn(colIndex), o2.getColumn(colIndex));
+        }
+
+        @Override
+        public boolean equals(final Object that) {
+            return that instanceof ColumnComparator && colIndex == ((ColumnComparator) that).colIndex;
+        }
+
+        @Override
+        public int hashCode() {
+            return colIndex;
+        }
+    }
+
+    public static class PseudoRow implements Serializable {
+
+        /**
+         * Generated serial version ID.
+         */
+        private static final long serialVersionUID = 8085570439751032499L;
+
+        public int[] cols = new int[3];
+
+        PseudoRow(final int col1, final int col2, final int col3) {
+            cols[0] = col1;
+            cols[1] = col2;
+            cols[2] = col3;
+        }
+
+        @Override
+        public boolean equals(final Object o) {
+            if (!(o instanceof PseudoRow)) {
+                return false;
+            }
+            final PseudoRow row = (PseudoRow) o;
+            return getColumn(0) == row.getColumn(0) && getColumn(1) == row.getColumn(1) && getColumn(2) == row.getColumn(2);
+        }
+
+        public int getColumn(final int colIndex) {
+            return cols[colIndex];
+        }
+
+        @Override
+        public int hashCode() {
+            return Arrays.hashCode(cols);
+        }
+
+        @Override
+        public String toString() {
+            return "[" + cols[0] + "," + cols[1] + "," + cols[2] + "]";
+        }
+    }
+
+    @Test
+    void testNoopComparatorChain() {
+        final ComparatorChain<Integer> chain = new ComparatorChain<>();
+        final Integer i1 = 4;
+        final Integer i2 = 6;
+        chain.addComparator(new ComparableComparator<>());
+        final int correctValue = i1.compareTo(i2);
+        assertEquals(chain.compare(i1, i2), correctValue, "Comparison returns the right order");
+    }
+}
