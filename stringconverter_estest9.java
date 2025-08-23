d37974cf71098bcad848b1976dadeb6e594285f6
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:18:35 +0800

    gemini processedAutoTestCases stringconverter_estest9.java

--- stringconverter_estest9.java
+++ stringconverter_estest9.java
@@ -1,55 +1,30 @@
 package org.joda.time.convert;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.joda.time.Chronology;
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeFieldType;
-import org.joda.time.DateTimeZone;
-import org.joda.time.LocalDate;
-import org.joda.time.MonthDay;
-import org.joda.time.MutableDateTime;
 import org.joda.time.MutableInterval;
-import org.joda.time.MutablePeriod;
-import org.joda.time.Partial;
-import org.joda.time.PeriodType;
-import org.joda.time.ReadWritableInterval;
-import org.joda.time.ReadWritablePeriod;
-import org.joda.time.ReadableInstant;
-import org.joda.time.ReadablePartial;
-import org.joda.time.chrono.CopticChronology;
-import org.joda.time.chrono.EthiopicChronology;
-import org.joda.time.chrono.GJChronology;
-import org.joda.time.chrono.GregorianChronology;
-import org.joda.time.chrono.ISOChronology;
-import org.joda.time.chrono.IslamicChronology;
-import org.joda.time.chrono.JulianChronology;
-import org.joda.time.chrono.ZonedChronology;
-import org.joda.time.format.DateTimeFormatter;
-import org.joda.time.format.DateTimeParser;
-import org.joda.time.format.DateTimePrinter;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class StringConverter_ESTestTest9 extends StringConverter_ESTest_scaffolding {
+/**
+ * Unit tests for {@link StringConverter}.
+ */
+public class StringConverterTest {
 
-    @Test(timeout = 4000)
-    public void test08() throws Throwable {
-        StringConverter stringConverter0 = new StringConverter();
-        MutableInterval mutableInterval0 = new MutableInterval();
-        // Undeclared exception!
-        try {
-            stringConverter0.setInto((ReadWritableInterval) mutableInterval0, (Object) mutableInterval0, (Chronology) null);
-            fail("Expecting exception: ClassCastException");
-        } catch (ClassCastException e) {
-            //
-            // org.joda.time.MutableInterval cannot be cast to java.lang.String
-            //
-            verifyException("org.joda.time.convert.StringConverter", e);
-        }
+    /**
+     * Tests that setInto() for an interval throws a ClassCastException when the
+     * object to be converted is not a String. The converter is specifically designed
+     * to handle Strings, so any other type should result in an error.
+     */
+    @Test(expected = ClassCastException.class)
+    public void setIntoInterval_withNonStringObject_throwsClassCastException() {
+        // Arrange
+        StringConverter converter = new StringConverter();
+        MutableInterval intervalToUpdate = new MutableInterval();
+        
+        // This is the invalid input object. The converter expects a String.
+        Object invalidInput = new MutableInterval();
+
+        // Act
+        converter.setInto(intervalToUpdate, invalidInput, null);
+
+        // Assert: The test expects a ClassCastException, as declared in the @Test annotation.
     }
 }
