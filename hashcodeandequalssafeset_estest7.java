bdfb5ec9416007941d02ae5dbac72410902c91ec
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:53:11 +0800

    Original processedAutoTestCases hashcodeandequalssafeset_estest7.java

---
+++ hashcodeandequalssafeset_estest7.java
@@ -0,0 +1,27 @@
+package org.mockito.internal.util.collections;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Vector;
+import java.util.function.Predicate;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class HashCodeAndEqualsSafeSet_ESTestTest7 extends HashCodeAndEqualsSafeSet_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test06() throws Throwable {
+        HashCodeAndEqualsSafeSet hashCodeAndEqualsSafeSet0 = new HashCodeAndEqualsSafeSet();
+        boolean boolean0 = hashCodeAndEqualsSafeSet0.removeAll(hashCodeAndEqualsSafeSet0);
+        assertFalse(boolean0);
+    }
+}
