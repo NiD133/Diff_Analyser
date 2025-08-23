6cb39ceb0aa75f34686da8441344c8e8d4d639fc
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:27:40 +0800

    Original processedAutoTestCases wktwriter_estest3.java

---
+++ wktwriter_estest3.java
@@ -0,0 +1,40 @@
+package org.locationtech.spatial4j.io;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.text.ChoiceFormat;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.LinkedList;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+import org.locationtech.spatial4j.context.SpatialContext;
+import org.locationtech.spatial4j.context.SpatialContextFactory;
+import org.locationtech.spatial4j.distance.GeodesicSphereDistCalc;
+import org.locationtech.spatial4j.shape.Circle;
+import org.locationtech.spatial4j.shape.Point;
+import org.locationtech.spatial4j.shape.Rectangle;
+import org.locationtech.spatial4j.shape.Shape;
+import org.locationtech.spatial4j.shape.ShapeCollection;
+import org.locationtech.spatial4j.shape.impl.BufferedLine;
+import org.locationtech.spatial4j.shape.impl.BufferedLineString;
+import org.locationtech.spatial4j.shape.impl.PointImpl;
+
+public class WKTWriter_ESTestTest3 extends WKTWriter_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test02() throws Throwable {
+        WKTWriter wKTWriter0 = new WKTWriter();
+        StringBuilder stringBuilder0 = new StringBuilder((CharSequence) "WKT");
+        SpatialContextFactory spatialContextFactory0 = new SpatialContextFactory();
+        SpatialContext spatialContext0 = new SpatialContext(spatialContextFactory0);
+        PointImpl pointImpl0 = new PointImpl(4883.694876, 4.0, spatialContext0);
+        NumberFormat numberFormat0 = NumberFormat.getPercentInstance();
+        wKTWriter0.append(stringBuilder0, pointImpl0, numberFormat0);
+        assertEquals("WKT488,369% 400%", stringBuilder0.toString());
+    }
+}
