2481c887160737893b1dfc7067a06d96b8f5dbe1
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:00:14 +0800

    gemini processedAutoTestCases legenditementity_estest11.java

--- legenditementity_estest11.java
+++ legenditementity_estest11.java
@@ -1,28 +1,29 @@
 package org.jfree.chart.entity;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.awt.Rectangle;
-import java.awt.Shape;
-import java.awt.geom.Line2D;
-import java.awt.geom.Rectangle2D;
-import java.time.chrono.HijrahEra;
-import javax.swing.JLayeredPane;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
 import org.jfree.data.general.Dataset;
-import org.jfree.data.xy.DefaultTableXYDataset;
-import org.jfree.data.xy.XIntervalSeriesCollection;
-import org.junit.runner.RunWith;
+import org.junit.Test;
+import java.awt.geom.Rectangle2D;
+import static org.junit.Assert.assertNull;
 
-public class LegendItemEntity_ESTestTest11 extends LegendItemEntity_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link LegendItemEntity} class.
+ */
+public class LegendItemEntityTest {
 
-    @Test(timeout = 4000)
-    public void test10() throws Throwable {
-        Rectangle2D.Double rectangle2D_Double0 = new Rectangle2D.Double();
-        LegendItemEntity<Integer> legendItemEntity0 = new LegendItemEntity<Integer>(rectangle2D_Double0);
-        Dataset dataset0 = legendItemEntity0.getDataset();
-        assertNull(dataset0);
+    /**
+     * Verifies that the dataset is null by default when a LegendItemEntity is
+     * created without one being explicitly set.
+     */
+    @Test
+    public void getDatasetShouldReturnNullByDefault() {
+        // Arrange: Create a LegendItemEntity with a shape but no dataset.
+        Rectangle2D.Double area = new Rectangle2D.Double();
+        LegendItemEntity<String> legendItemEntity = new LegendItemEntity<>(area);
+
+        // Act: Retrieve the dataset from the entity.
+        Dataset dataset = legendItemEntity.getDataset();
+
+        // Assert: The retrieved dataset should be null.
+        assertNull("The dataset should be null for a newly created entity", dataset);
     }
 }
