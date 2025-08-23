32aec6f95c1989b90d1fb56b2c0693c1764ad9ec
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:38:24 +0800

    gemini processedAutoTestCases attribute_estest64.java

--- attribute_estest64.java
+++ attribute_estest64.java
@@ -1,30 +1,26 @@
 package org.jsoup.nodes;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.ByteArrayOutputStream;
-import java.io.FilterOutputStream;
-import java.io.OutputStreamWriter;
-import java.io.PipedWriter;
-import java.io.StringWriter;
-import java.nio.BufferOverflowException;
-import java.nio.CharBuffer;
-import java.nio.charset.Charset;
-import java.nio.charset.CharsetEncoder;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.io.MockPrintWriter;
-import org.jsoup.internal.QuietAppendable;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class Attribute_ESTestTest64 extends Attribute_ESTest_scaffolding {
+/**
+ * Tests for the {@link Attribute#namespace()} method.
+ */
+public class AttributeNamespaceTest {
 
-    @Test(timeout = 4000)
-    public void test63() throws Throwable {
-        Attribute attribute0 = Attribute.createFromEncoded("_Tr_2_", "ope3n");
-        attribute0.namespace();
-        assertEquals("ope3n", attribute0.getValue());
-        assertEquals("_Tr_2_", attribute0.getKey());
+    @Test
+    public void namespaceShouldBeEmptyForAttributeWithoutPrefix() {
+        // Arrange: Create an attribute with a key that does not have a namespace prefix.
+        // The parent of the attribute is null when created via this static factory method.
+        Attribute attribute = Attribute.createFromEncoded("_Tr_2_", "ope3n");
+
+        // Act: Retrieve the namespace from the attribute.
+        String namespace = attribute.namespace();
+
+        // Assert: The namespace should be an empty string because the key has no prefix.
+        // We also verify that the key and value were correctly initialized.
+        assertEquals("", namespace);
+        assertEquals("_Tr_2_", attribute.getKey());
+        assertEquals("ope3n", attribute.getValue());
     }
 }
