d873f9a3fb9ad28092c7b0622b4a2cea2f095a14
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:26:16 +0800

    Original processedManualTestCases calendarconvertertest7.java

---
+++ calendarconvertertest7.java
@@ -0,0 +1,55 @@
+package org.joda.time.convert;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.joda.time.Chronology;
+import org.joda.time.DateTimeZone;
+import org.joda.time.TimeOfDay;
+import org.joda.time.chrono.BuddhistChronology;
+import org.joda.time.chrono.GJChronology;
+import org.joda.time.chrono.GregorianChronology;
+import org.joda.time.chrono.ISOChronology;
+import org.joda.time.chrono.JulianChronology;
+
+public class CalendarConverterTestTest7 extends TestCase {
+
+    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
+
+    private static final DateTimeZone MOSCOW = DateTimeZone.forID("Europe/Moscow");
+
+    private static Chronology JULIAN;
+
+    private static Chronology ISO;
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
+
+    public static TestSuite suite() {
+        return new TestSuite(TestCalendarConverter.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        JULIAN = JulianChronology.getInstance();
+        ISO = ISOChronology.getInstance();
+    }
+
+    //-----------------------------------------------------------------------
+    public void testGetPartialValues() throws Exception {
+        GregorianCalendar cal = new GregorianCalendar();
+        cal.setTime(new Date(12345678L));
+        TimeOfDay tod = new TimeOfDay();
+        int[] expected = ISO.get(tod, 12345678L);
+        int[] actual = CalendarConverter.INSTANCE.getPartialValues(tod, cal, ISO);
+        assertEquals(true, Arrays.equals(expected, actual));
+    }
+}
