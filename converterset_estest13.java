eb42a3fbd08f16d14c77d7079c83b63f4c5264da
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:45:42 +0800

    Original processedAutoTestCases converterset_estest13.java

---
+++ converterset_estest13.java
@@ -0,0 +1,30 @@
+package org.joda.time.convert;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.joda.time.Hours;
+import org.joda.time.Interval;
+import org.joda.time.MutablePeriod;
+import org.joda.time.PeriodType;
+import org.joda.time.Seconds;
+import org.joda.time.chrono.CopticChronology;
+import org.junit.runner.RunWith;
+
+public class ConverterSet_ESTestTest13 extends ConverterSet_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test12() throws Throwable {
+        Converter[] converterArray0 = new Converter[7];
+        ConverterSet converterSet0 = new ConverterSet(converterArray0);
+        Converter[] converterArray1 = new Converter[1];
+        // Undeclared exception!
+        try {
+            converterSet0.copyInto(converterArray1);
+            fail("Expecting exception: ArrayIndexOutOfBoundsException");
+        } catch (ArrayIndexOutOfBoundsException e) {
+        }
+    }
+}
