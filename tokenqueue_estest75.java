fcd38585e58d6f32344af06820729d50d7b32b1e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:22:46 +0800

    Original processedAutoTestCases tokenqueue_estest75.java

---
+++ tokenqueue_estest75.java
@@ -0,0 +1,18 @@
+package org.jsoup.parser;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class TokenQueue_ESTestTest75 extends TokenQueue_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test74() throws Throwable {
+        TokenQueue tokenQueue0 = new TokenQueue("f7(L)+X");
+        boolean boolean0 = tokenQueue0.matchesWhitespace();
+        assertFalse(boolean0);
+    }
+}
