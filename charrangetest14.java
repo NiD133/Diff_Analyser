bd6be4019938996f8a2a6757958b689bf28c03e5
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:26:30 +0800

    gemini processedManualTestCases charrangetest14.java

--- charrangetest14.java
+++ charrangetest14.java
@@ -1,35 +1,43 @@
 package org.apache.commons.lang3;
 
-import static org.apache.commons.lang3.LangAssertions.assertNullPointerException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import java.lang.reflect.Modifier;
-import java.util.Iterator;
-import java.util.NoSuchElementException;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class CharRangeTestTest14 extends AbstractLangTest {
+/**
+ * Tests the hashCode() method for the {@link CharRange} class.
+ */
+class CharRangeHashCodeTest {
 
     @Test
-    void testHashCode() {
-        final CharRange rangea = CharRange.is('a');
-        final CharRange rangeae = CharRange.isIn('a', 'e');
-        final CharRange rangenotbf = CharRange.isIn('b', 'f');
-        assertEquals(rangea.hashCode(), rangea.hashCode());
-        assertEquals(rangea.hashCode(), CharRange.is('a').hashCode());
-        assertEquals(rangeae.hashCode(), rangeae.hashCode());
-        assertEquals(rangeae.hashCode(), CharRange.isIn('a', 'e').hashCode());
-        assertEquals(rangenotbf.hashCode(), rangenotbf.hashCode());
-        assertEquals(rangenotbf.hashCode(), CharRange.isIn('b', 'f').hashCode());
-        assertNotEquals(rangea.hashCode(), rangeae.hashCode());
-        assertNotEquals(rangea.hashCode(), rangenotbf.hashCode());
-        assertNotEquals(rangeae.hashCode(), rangea.hashCode());
-        assertNotEquals(rangeae.hashCode(), rangenotbf.hashCode());
-        assertNotEquals(rangenotbf.hashCode(), rangea.hashCode());
-        assertNotEquals(rangenotbf.hashCode(), rangeae.hashCode());
+    @DisplayName("hashCode() should be consistent with the equals() contract")
+    void hashCodeShouldBeConsistentWithEquals() {
+        // Arrange: Create different CharRange instances for comparison.
+        final CharRange rangeA = CharRange.is('a');
+        final CharRange rangeAtoE = CharRange.isIn('a', 'e');
+        final CharRange rangeBtoF = CharRange.isIn('b', 'f');
+
+        // --- Assert: Equal objects must have equal hash codes ---
+
+        // A new, but logically equal, object should have the same hash code.
+        assertEquals(rangeA.hashCode(), CharRange.is('a').hashCode(),
+            "Hash code should be same for equal single-char ranges");
+        assertEquals(rangeAtoE.hashCode(), CharRange.isIn('a', 'e').hashCode(),
+            "Hash code should be same for equal multi-char ranges");
+        assertEquals(rangeBtoF.hashCode(), CharRange.isIn('b', 'f').hashCode(),
+            "Hash code should be same for equal multi-char ranges");
+
+        // --- Assert: Unequal objects should have unequal hash codes ---
+        // Note: This is not a strict requirement of the hashCode contract,
+        // but it is highly desirable for performance in hash-based collections.
+
+        assertNotEquals(rangeA.hashCode(), rangeAtoE.hashCode(),
+            "Hash code for 'a' and 'a-e' should be different");
+        assertNotEquals(rangeA.hashCode(), rangeBtoF.hashCode(),
+            "Hash code for 'a' and 'b-f' should be different");
+        assertNotEquals(rangeAtoE.hashCode(), rangeBtoF.hashCode(),
+            "Hash code for 'a-e' and 'b-f' should be different");
     }
 }
