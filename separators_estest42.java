812121504852459541471c2b6a4531bb2a6c1a58
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:14:24 +0800

    Original processedAutoTestCases separators_estest42.java

---
+++ separators_estest42.java
@@ -0,0 +1,22 @@
+package com.fasterxml.jackson.core.util;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class Separators_ESTestTest42 extends Separators_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test41() throws Throwable {
+        Separators separators0 = Separators.createDefaultInstance();
+        char char0 = separators0.getArrayValueSeparator();
+        assertEquals(Separators.Spacing.NONE, separators0.getArrayValueSpacing());
+        assertEquals(',', separators0.getObjectEntrySeparator());
+        assertEquals(Separators.Spacing.BOTH, separators0.getObjectFieldValueSpacing());
+        assertEquals(Separators.Spacing.NONE, separators0.getObjectEntrySpacing());
+        assertEquals(':', separators0.getObjectFieldValueSeparator());
+        assertEquals(',', char0);
+    }
+}
