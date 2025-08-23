0419d654f63c3b717e1b456bb76dc53c28829697
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:31:29 +0800

    Original processedManualTestCases nodeiteratortest1.java

---
+++ nodeiteratortest1.java
@@ -0,0 +1,58 @@
+package org.jsoup.nodes;
+
+import org.jsoup.Jsoup;
+import org.junit.jupiter.api.Test;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class NodeIteratorTestTest1 {
+
+    String html = "<div id=1><p>One<p>Two</div><div id=2><p>Three<p>Four</div>";
+
+    static <T extends Node> void assertIterates(Iterator<T> it, String expected) {
+        Node previous = null;
+        StringBuilder actual = new StringBuilder();
+        while (it.hasNext()) {
+            Node node = it.next();
+            assertNotNull(node);
+            assertNotSame(previous, node);
+            trackSeen(node, actual);
+            previous = node;
+        }
+        assertEquals(expected, actual.toString());
+    }
+
+    static void assertContents(Element el, String expected) {
+        NodeIterator<Node> it = NodeIterator.from(el);
+        assertIterates(it, expected);
+    }
+
+    public static void trackSeen(Node node, StringBuilder actual) {
+        if (node instanceof Element) {
+            Element el = (Element) node;
+            actual.append(el.tagName());
+            if (el.hasAttr("id"))
+                actual.append("#").append(el.id());
+        } else if (node instanceof TextNode)
+            actual.append(((TextNode) node).text());
+        else
+            actual.append(node.nodeName());
+        actual.append(";");
+    }
+
+    @Test
+    void canIterateNodes() {
+        Document doc = Jsoup.parse(html);
+        NodeIterator<Node> it = NodeIterator.from(doc);
+        assertIterates(it, "#root;html;head;body;div#1;p;One;p;Two;div#2;p;Three;p;Four;");
+        assertFalse(it.hasNext());
+        boolean threw = false;
+        try {
+            it.next();
+        } catch (NoSuchElementException e) {
+            threw = true;
+        }
+        assertTrue(threw);
+    }
+}
