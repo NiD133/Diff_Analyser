bc8758d37c6a8e4bc0860b1ea127c3676aa8f84f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:46:02 +0800

    gemini processedAutoTestCases coreoperation_estest2.java

--- coreoperation_estest2.java
+++ coreoperation_estest2.java
@@ -1,31 +1,29 @@
 package org.apache.commons.jxpath.ri.compiler;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.apache.commons.jxpath.JXPathContext;
-import org.apache.commons.jxpath.ri.EvalContext;
-import org.apache.commons.jxpath.ri.JXPathContextReferenceImpl;
-import org.apache.commons.jxpath.ri.QName;
-import org.apache.commons.jxpath.ri.axes.InitialContext;
-import org.apache.commons.jxpath.ri.axes.ParentContext;
-import org.apache.commons.jxpath.ri.axes.PrecedingOrFollowingContext;
-import org.apache.commons.jxpath.ri.axes.RootContext;
-import org.apache.commons.jxpath.ri.axes.SelfContext;
-import org.apache.commons.jxpath.ri.axes.UnionContext;
-import org.apache.commons.jxpath.ri.model.NodePointer;
-import org.apache.commons.jxpath.ri.model.VariablePointer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
+/**
+ * This test suite focuses on verifying the behavior of {@link CoreOperation} subclasses.
+ */
 public class CoreOperation_ESTestTest2 extends CoreOperation_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test01() throws Throwable {
-        CoreOperationGreaterThan coreOperationGreaterThan0 = new CoreOperationGreaterThan((Expression) null, (Expression) null);
-        CoreOperationLessThanOrEqual coreOperationLessThanOrEqual0 = new CoreOperationLessThanOrEqual(coreOperationGreaterThan0, (Expression) null);
-        String string0 = coreOperationLessThanOrEqual0.getSymbol();
-        assertEquals("<=", string0);
+    /**
+     * Verifies that the getSymbol() method of CoreOperationLessThanOrEqual
+     * returns the correct XPath symbol "<=".
+     */
+    @Test
+    public void lessThanOrEqualOperationReturnsCorrectSymbol() {
+        // Arrange
+        // The constructor arguments are not relevant for testing getSymbol(),
+        // so we can pass null.
+        CoreOperationLessThanOrEqual lessThanOrEqualOperation = new CoreOperationLessThanOrEqual(null, null);
+        String expectedSymbol = "<=";
+
+        // Act
+        String actualSymbol = lessThanOrEqualOperation.getSymbol();
+
+        // Assert
+        assertEquals("The symbol for less than or equal should be '<='", expectedSymbol, actualSymbol);
     }
 }
