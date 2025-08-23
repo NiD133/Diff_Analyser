d67e85cb7af4170d9a2632ac3fa0dfb1dcab1ba9
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:03:36 +0800

    Original processedAutoTestCases moduloaxis_estest30.java

---
+++ moduloaxis_estest30.java
@@ -0,0 +1,50 @@
+package org.jfree.chart.axis;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.awt.Polygon;
+import java.awt.Rectangle;
+import java.awt.geom.Line2D;
+import java.awt.geom.Rectangle2D;
+import java.util.Calendar;
+import java.util.TimeZone;
+import javax.swing.DropMode;
+import javax.swing.JScrollPane;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.util.MockGregorianCalendar;
+import org.jfree.chart.api.RectangleEdge;
+import org.jfree.chart.legend.PaintScaleLegend;
+import org.jfree.chart.plot.MeterPlot;
+import org.jfree.chart.plot.ThermometerPlot;
+import org.jfree.chart.renderer.LookupPaintScale;
+import org.jfree.chart.renderer.PaintScale;
+import org.jfree.chart.renderer.xy.XYShapeRenderer;
+import org.jfree.data.Range;
+import org.jfree.data.general.DefaultValueDataset;
+import org.jfree.data.statistics.DefaultMultiValueCategoryDataset;
+import org.jfree.data.time.DateRange;
+import org.jfree.data.time.TimePeriodAnchor;
+import org.jfree.data.time.TimeSeries;
+import org.junit.runner.RunWith;
+
+public class ModuloAxis_ESTestTest30 extends ModuloAxis_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test29() throws Throwable {
+        DefaultMultiValueCategoryDataset<DropMode, DropMode> defaultMultiValueCategoryDataset0 = new DefaultMultiValueCategoryDataset<DropMode, DropMode>();
+        Range range0 = defaultMultiValueCategoryDataset0.getRangeBounds(false);
+        ModuloAxis moduloAxis0 = new ModuloAxis((String) null, range0);
+        // Undeclared exception!
+        try {
+            moduloAxis0.resizeRange((-1471.70084));
+            fail("Expecting exception: IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //
+            // A positive range length is required: Range[0.0,0.0]
+            //
+            verifyException("org.jfree.chart.axis.ValueAxis", e);
+        }
+    }
+}
