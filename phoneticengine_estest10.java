1dc44cb170fed5e9dd79dde85bfa8fb927637f97
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:08:43 +0800

    Original processedAutoTestCases phoneticengine_estest10.java

---
+++ phoneticengine_estest10.java
@@ -0,0 +1,26 @@
+package org.apache.commons.codec.language.bm;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.nio.CharBuffer;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class PhoneticEngine_ESTestTest10 extends PhoneticEngine_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test09() throws Throwable {
+        NameType nameType0 = NameType.ASHKENAZI;
+        RuleType ruleType0 = RuleType.EXACT;
+        PhoneticEngine phoneticEngine0 = new PhoneticEngine(nameType0, ruleType0, false, 0);
+        LinkedHashSet<String> linkedHashSet0 = new LinkedHashSet<String>();
+        Languages.LanguageSet languages_LanguageSet0 = Languages.LanguageSet.from(linkedHashSet0);
+        phoneticEngine0.encode("ben", languages_LanguageSet0);
+        assertFalse(phoneticEngine0.isConcat());
+        assertEquals(0, phoneticEngine0.getMaxPhonemes());
+    }
+}
