ebaff998e3c250055a4d3256f82c5e4129b92f45
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:26:04 +0800

    Original processedManualTestCases byteordermarktest8.java

---
+++ byteordermarktest8.java
@@ -0,0 +1,30 @@
+package org.apache.commons.io;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.nio.charset.Charset;
+import org.junit.jupiter.api.Test;
+
+public class ByteOrderMarkTestTest8 {
+
+    private static final ByteOrderMark TEST_BOM_1 = new ByteOrderMark("test1", 1);
+
+    private static final ByteOrderMark TEST_BOM_2 = new ByteOrderMark("test2", 1, 2);
+
+    private static final ByteOrderMark TEST_BOM_3 = new ByteOrderMark("test3", 1, 2, 3);
+
+    /**
+     * Tests {@link ByteOrderMark#length()}
+     */
+    @Test
+    void testLength() {
+        assertEquals(1, TEST_BOM_1.length(), "test1 length");
+        assertEquals(2, TEST_BOM_2.length(), "test2 length");
+        assertEquals(3, TEST_BOM_3.length(), "test3 length");
+    }
+}
