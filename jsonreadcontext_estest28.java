8bb8423f613125920709be7a540bc458775d81ea
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:57:25 +0800

    Original processedAutoTestCases jsonreadcontext_estest28.java

---
+++ jsonreadcontext_estest28.java
@@ -0,0 +1,33 @@
+package com.fasterxml.jackson.core.json;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.fasterxml.jackson.core.ErrorReportConfiguration;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonFactoryBuilder;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonLocation;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.io.ContentReference;
+import java.io.IOException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class JsonReadContext_ESTestTest28 extends JsonReadContext_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test27() throws Throwable {
+        DupDetector dupDetector0 = DupDetector.rootDetector((JsonGenerator) null);
+        JsonReadContext jsonReadContext0 = JsonReadContext.createRootContext(dupDetector0);
+        JsonReadContext jsonReadContext1 = JsonReadContext.createRootContext((-2391), 1, dupDetector0);
+        jsonReadContext1._child = jsonReadContext0;
+        assertEquals("ROOT", jsonReadContext0.getTypeDesc());
+        JsonReadContext jsonReadContext2 = jsonReadContext1.createChildArrayContext(547, (-2391));
+        assertEquals(0, jsonReadContext2.getCurrentIndex());
+        assertEquals(0, jsonReadContext1.getNestingDepth());
+        assertEquals("ARRAY", jsonReadContext2.getTypeDesc());
+        assertEquals(0, jsonReadContext2.getNestingDepth());
+    }
+}
