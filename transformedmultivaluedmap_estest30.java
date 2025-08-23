5b65a6caaffd98a834c6fb2e60ed829d8b4cdf40
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:23:20 +0800

    gemini processedAutoTestCases transformedmultivaluedmap_estest30.java

--- transformedmultivaluedmap_estest30.java
+++ transformedmultivaluedmap_estest30.java
@@ -1,65 +1,49 @@
 package org.apache.commons.collections4.multimap;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.AbstractMap;
-import java.util.ArrayDeque;
-import java.util.Comparator;
-import java.util.ConcurrentModificationException;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
-import java.util.PriorityQueue;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.Factory;
 import org.apache.commons.collections4.MultiValuedMap;
-import org.apache.commons.collections4.Predicate;
 import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AllPredicate;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ChainedTransformer;
-import org.apache.commons.collections4.functors.CloneTransformer;
-import org.apache.commons.collections4.functors.ClosureTransformer;
-import org.apache.commons.collections4.functors.ConstantFactory;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.ExceptionFactory;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.FactoryTransformer;
-import org.apache.commons.collections4.functors.IfTransformer;
 import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.MapTransformer;
-import org.apache.commons.collections4.functors.NOPTransformer;
-import org.apache.commons.collections4.functors.NotPredicate;
-import org.apache.commons.collections4.functors.NullIsExceptionPredicate;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.SwitchTransformer;
-import org.apache.commons.collections4.functors.TransformerClosure;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class TransformedMultiValuedMap_ESTestTest30 extends TransformedMultiValuedMap_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
-    @Test(timeout = 4000)
-    public void test29() throws Throwable {
-        HashSetValuedHashMap<Integer, Integer> hashSetValuedHashMap0 = new HashSetValuedHashMap<Integer, Integer>(0);
-        Transformer<Object, Integer> transformer0 = InvokerTransformer.invokerTransformer("");
-        Integer integer0 = new Integer((-3473));
-        TransformedMultiValuedMap<Integer, Integer> transformedMultiValuedMap0 = new TransformedMultiValuedMap<Integer, Integer>(hashSetValuedHashMap0, transformer0, transformer0);
-        hashSetValuedHashMap0.put(integer0, integer0);
-        // Undeclared exception!
+/**
+ * This class contains tests for {@link TransformedMultiValuedMap}.
+ * This particular test was improved for clarity from an auto-generated test case.
+ */
+public class TransformedMultiValuedMap_ESTestTest30 {
+
+    /**
+     * Tests that {@link TransformedMultiValuedMap#putAll(MultiValuedMap)} throws a RuntimeException
+     * if the key transformation fails during the operation.
+     */
+    @Test
+    public void putAllShouldThrowExceptionWhenKeyTransformationFails() {
+        // Arrange
+        // 1. Define a transformer that is guaranteed to fail for an Integer input.
+        // The InvokerTransformer tries to call a method by name on the input object.
+        // Since the Integer class has no method named "", this will throw a RuntimeException.
+        final Transformer<Object, Integer> failingTransformer = InvokerTransformer.invokerTransformer("");
+
+        // 2. Create the map under test, decorated with the failing transformer for its keys.
+        final MultiValuedMap<Integer, Integer> decoratedMap =
+                new TransformedMultiValuedMap<>(new HashSetValuedHashMap<>(), failingTransformer, null);
+
+        // 3. Create a source map containing data to be added to the decorated map.
+        final MultiValuedMap<Integer, Integer> sourceMap = new HashSetValuedHashMap<>();
+        sourceMap.put(1, 100); // The key '1' will be passed to the failing transformer.
+
+        // Act & Assert
         try {
-            transformedMultiValuedMap0.putAll((MultiValuedMap<? extends Integer, ? extends Integer>) hashSetValuedHashMap0);
-            fail("Expecting exception: RuntimeException");
-        } catch (RuntimeException e) {
-            //
-            // InvokerTransformer: The method '' on 'class java.lang.Integer' does not exist
-            //
-            verifyException("org.apache.commons.collections4.functors.InvokerTransformer", e);
+            // This call should fail because the key '1' from the source map
+            // cannot be transformed by the failingTransformer.
+            decoratedMap.putAll(sourceMap);
+            fail("Expected a RuntimeException because the key transformation should fail.");
+        } catch (final RuntimeException e) {
+            // Verify that the exception is the one expected from InvokerTransformer.
+            final String expectedMessage = "InvokerTransformer: The method '' on 'class java.lang.Integer' does not exist";
+            assertEquals(expectedMessage, e.getMessage());
         }
     }
 }
