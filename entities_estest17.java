55a56fb1a5f0f93a930092442ecc5267e8fc0fc9
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:49:22 +0800

    Original processedAutoTestCases entities_estest17.java

---
+++ entities_estest17.java
@@ -0,0 +1,36 @@
+package org.jsoup.nodes;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.File;
+import java.io.PipedWriter;
+import java.io.StringWriter;
+import java.nio.BufferOverflowException;
+import java.nio.CharBuffer;
+import java.nio.ReadOnlyBufferException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.evosuite.runtime.mock.java.io.MockFileWriter;
+import org.evosuite.runtime.mock.java.io.MockPrintStream;
+import org.jsoup.internal.QuietAppendable;
+import org.junit.runner.RunWith;
+
+public class Entities_ESTestTest17 extends Entities_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test16() throws Throwable {
+        int[] intArray0 = new int[0];
+        // Undeclared exception!
+        try {
+            Entities.codepointsForName("amp", intArray0);
+            fail("Expecting exception: ArrayIndexOutOfBoundsException");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            //
+            // 0
+            //
+            verifyException("org.jsoup.nodes.Entities", e);
+        }
+    }
+}
