33db211c2e5997b4a976a0859bd3d4e2cf19eed1
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:09:21 +0800

    gemini processedAutoTestCases precisedurationdatetimefield_estest9.java

--- precisedurationdatetimefield_estest9.java
+++ precisedurationdatetimefield_estest9.java
@@ -1,43 +1,46 @@
 package org.joda.time.field;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.TimeZone;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
 import org.joda.time.Chronology;
 import org.joda.time.DateTimeFieldType;
-import org.joda.time.DateTimeZone;
-import org.joda.time.Days;
 import org.joda.time.DurationField;
-import org.joda.time.DurationFieldType;
-import org.joda.time.LocalDate;
-import org.joda.time.LocalTime;
-import org.joda.time.Weeks;
-import org.joda.time.chrono.EthiopicChronology;
-import org.joda.time.chrono.GJChronology;
 import org.joda.time.chrono.GregorianChronology;
-import org.joda.time.chrono.IslamicChronology;
-import org.joda.time.chrono.JulianChronology;
-import org.joda.time.chrono.LenientChronology;
-import org.joda.time.chrono.ZonedChronology;
-import org.junit.runner.RunWith;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public class PreciseDurationDateTimeField_ESTestTest9 extends PreciseDurationDateTimeField_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test08() throws Throwable {
-        DateTimeFieldType dateTimeFieldType0 = DateTimeFieldType.dayOfWeek();
-        GregorianChronology gregorianChronology0 = GregorianChronology.getInstanceUTC();
-        LenientChronology lenientChronology0 = LenientChronology.getInstance(gregorianChronology0);
-        DateTimeZone dateTimeZone0 = DateTimeZone.forID((String) null);
-        ZonedChronology zonedChronology0 = ZonedChronology.getInstance(lenientChronology0, dateTimeZone0);
-        DurationField durationField0 = zonedChronology0.minutes();
-        DurationField durationField1 = zonedChronology0.halfdays();
-        PreciseDateTimeField preciseDateTimeField0 = new PreciseDateTimeField(dateTimeFieldType0, durationField0, durationField1);
-        long long0 = preciseDateTimeField0.roundFloor(992280585600000L);
-        assertEquals(992280585600000L, long0);
-        assertFalse(preciseDateTimeField0.isLenient());
+    /**
+     * Tests that roundFloor() returns the same instant when the given instant
+     * is already aligned with the field's unit (e.g., on an exact minute).
+     */
+    @Test
+    public void roundFloor_whenInstantIsOnUnitBoundary_returnsSameInstant() {
+        // Arrange
+        // We need a chronology to obtain duration fields. UTC is used for consistency.
+        Chronology chronology = GregorianChronology.getInstanceUTC();
+        DurationField minutesField = chronology.minutes(); // The unit of the field under test.
+        DurationField daysField = chronology.days();       // A range field, required by the constructor but not used by roundFloor.
+
+        // The System Under Test (SUT) is a field with a unit duration of minutes.
+        // We use a concrete implementation, PreciseDateTimeField, for this test,
+        // as PreciseDurationDateTimeField is abstract.
+        PreciseDurationDateTimeField field = new PreciseDateTimeField(
+                DateTimeFieldType.dayOfWeek(), minutesField, daysField);
+
+        // An instant that is already rounded to the nearest minute (the unit of our field).
+        // This corresponds to 2022-06-15 10:30:00.000 UTC.
+        long instantOnMinuteBoundary = 1655289000000L;
+
+        // Act
+        long roundedInstant = field.roundFloor(instantOnMinuteBoundary);
+
+        // Assert
+        assertEquals("Rounding an instant that is already on a minute boundary should not change it.",
+                instantOnMinuteBoundary, roundedInstant);
+        
+        // Also, verify a basic property of the constructed field.
+        assertFalse("The field should not be lenient by default.", field.isLenient());
     }
 }
