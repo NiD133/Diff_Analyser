1fbdca0facfc3ea6a857ee4ea94f4604b6fc991b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:03:37 +0800

    Original processedAutoTestCases moduloaxis_estest33.java

---
+++ moduloaxis_estest33.java
@@ -0,0 +1,44 @@
+package org.jfree.chart.axis;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.awt.Polygon;
+import java.awt.Rectangle;
+import java.awt.geom.Line2D;
+import java.awt.geom.Rectangle2D;
+import java.util.Calendar;
+import java.util.TimeZone;
+import javax.swing.DropMode;
+import javax.swing.JScrollPane;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.util.MockGregorianCalendar;
+import org.jfree.chart.api.RectangleEdge;
+import org.jfree.chart.legend.PaintScaleLegend;
+import org.jfree.chart.plot.MeterPlot;
+import org.jfree.chart.plot.ThermometerPlot;
+import org.jfree.chart.renderer.LookupPaintScale;
+import org.jfree.chart.renderer.PaintScale;
+import org.jfree.chart.renderer.xy.XYShapeRenderer;
+import org.jfree.data.Range;
+import org.jfree.data.general.DefaultValueDataset;
+import org.jfree.data.statistics.DefaultMultiValueCategoryDataset;
+import org.jfree.data.time.DateRange;
+import org.jfree.data.time.TimePeriodAnchor;
+import org.jfree.data.time.TimeSeries;
+import org.junit.runner.RunWith;
+
+public class ModuloAxis_ESTestTest33 extends ModuloAxis_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test32() throws Throwable {
+        DefaultValueDataset defaultValueDataset0 = new DefaultValueDataset();
+        ThermometerPlot thermometerPlot0 = new ThermometerPlot(defaultValueDataset0);
+        Range range0 = thermometerPlot0.getDataRange((ValueAxis) null);
+        ModuloAxis moduloAxis0 = new ModuloAxis("", range0);
+        moduloAxis0.setDisplayRange(0.0, 10);
+        double double0 = moduloAxis0.getDisplayStart();
+        assertEquals(0.0, double0, 0.01);
+    }
+}
