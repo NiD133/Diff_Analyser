94f83eb95a0a40e2c673f3ae4c168f80aceb1d0e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:09:43 +0800

    Original processedAutoTestCases proxyreader_estest37.java

---
+++ proxyreader_estest37.java
@@ -0,0 +1,29 @@
+package org.apache.commons.io.input;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.CharArrayWriter;
+import java.io.IOException;
+import java.io.PipedReader;
+import java.io.StringReader;
+import java.nio.CharBuffer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockIOException;
+import org.junit.runner.RunWith;
+
+public class ProxyReader_ESTestTest37 extends ProxyReader_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test36() throws Throwable {
+        PipedReader pipedReader0 = new PipedReader();
+        CloseShieldReader closeShieldReader0 = CloseShieldReader.wrap(pipedReader0);
+        MockIOException mockIOException0 = new MockIOException();
+        try {
+            closeShieldReader0.handleIOException(mockIOException0);
+            fail("Expecting exception: IOException");
+        } catch (IOException e) {
+        }
+    }
+}
