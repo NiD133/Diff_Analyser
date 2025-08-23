c6ae8d72de99c2d937ad448b20a273e9436a4946
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:55:37 +0800

    Original processedAutoTestCases isochronology_estest11.java

---
+++ isochronology_estest11.java
@@ -0,0 +1,20 @@
+package org.joda.time.chrono;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.joda.time.Chronology;
+import org.joda.time.DateTimeZone;
+import org.junit.runner.RunWith;
+
+public class ISOChronology_ESTestTest11 extends ISOChronology_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test10() throws Throwable {
+        ISOChronology iSOChronology0 = ISOChronology.getInstance();
+        Chronology chronology0 = iSOChronology0.withZone((DateTimeZone) null);
+        assertSame(chronology0, iSOChronology0);
+    }
+}
