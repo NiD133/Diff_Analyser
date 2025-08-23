4116d477679401130f16b58d5c4aa8b945b39021
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:03:50 +0800

    Original processedAutoTestCases multibackgroundinitializer_estest22.java

---
+++ multibackgroundinitializer_estest22.java
@@ -0,0 +1,43 @@
+package org.apache.commons.lang3.concurrent;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.NoSuchElementException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.lang.MockException;
+import org.junit.runner.RunWith;
+
+public class MultiBackgroundInitializer_ESTestTest22 extends MultiBackgroundInitializer_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test21() throws Throwable {
+        MultiBackgroundInitializer multiBackgroundInitializer0 = new MultiBackgroundInitializer((ExecutorService) null);
+        BackgroundInitializer.builder();
+        MockException mockException0 = new MockException("RY`@<.S-tgH |");
+        Exception exception0 = multiBackgroundInitializer0.getTypedException(mockException0);
+        MultiBackgroundInitializer.MultiBackgroundInitializerResults multiBackgroundInitializer_MultiBackgroundInitializerResults0 = multiBackgroundInitializer0.initialize();
+        multiBackgroundInitializer0.getTypedException(exception0);
+        multiBackgroundInitializer_MultiBackgroundInitializerResults0.initializerNames();
+        // Undeclared exception!
+        try {
+            multiBackgroundInitializer_MultiBackgroundInitializerResults0.getException((String) null);
+            fail("Expecting exception: NoSuchElementException");
+        } catch (NoSuchElementException e) {
+            //
+            // No child initializer with name null
+            //
+            verifyException("org.apache.commons.lang3.concurrent.MultiBackgroundInitializer$MultiBackgroundInitializerResults", e);
+        }
+    }
+}
