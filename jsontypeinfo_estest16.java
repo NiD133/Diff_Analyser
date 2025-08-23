459bb3db5f6c57e740503061ff785cb95bef62aa
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:58:51 +0800

    gemini processedAutoTestCases jsontypeinfo_estest16.java

--- jsontypeinfo_estest16.java
+++ jsontypeinfo_estest16.java
@@ -2,28 +2,45 @@ package com.fasterxml.jackson.annotation;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
 
+/**
+ * This test class focuses on the behavior of the {@link JsonTypeInfo.Value} class,
+ * particularly its "wither" methods which create new instances with modified properties.
+ */
 public class JsonTypeInfo_ESTestTest16 extends JsonTypeInfo_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test15() throws Throwable {
-        JsonTypeInfo.Id jsonTypeInfo_Id0 = JsonTypeInfo.Id.MINIMAL_CLASS;
-        JsonTypeInfo.As jsonTypeInfo_As0 = JsonTypeInfo.As.WRAPPER_OBJECT;
-        Class<Object> class0 = Object.class;
-        Boolean boolean0 = Boolean.valueOf("@2LLQRbW9{J2*\"1GY");
-        JsonTypeInfo.Value jsonTypeInfo_Value0 = JsonTypeInfo.Value.construct(jsonTypeInfo_Id0, jsonTypeInfo_As0, "@2LLQRbW9{J2*\"1GY", class0, false, boolean0);
-        Boolean boolean1 = new Boolean("@2LLQRbW9{J2*\"1GY");
-        JsonTypeInfo.Value jsonTypeInfo_Value1 = jsonTypeInfo_Value0.withRequireTypeIdForSubtypes(boolean1);
-        boolean boolean2 = jsonTypeInfo_Value1.equals(jsonTypeInfo_Value0);
-        assertFalse(jsonTypeInfo_Value1.getIdVisible());
-        assertTrue(boolean2);
-        assertNotSame(jsonTypeInfo_Value1, jsonTypeInfo_Value0);
-        assertEquals("@2LLQRbW9{J2*\"1GY", jsonTypeInfo_Value1.getPropertyName());
+    /**
+     * Tests that calling {@code withRequireTypeIdForSubtypes} with the same value
+     * that the object already holds results in a new, but logically equal, instance.
+     * This verifies the immutability contract of the Value object.
+     */
+    @Test
+    public void withRequireTypeIdForSubtypes_whenCalledWithSameValue_returnsEqualButNotSameInstance() {
+        // Arrange: Create an initial JsonTypeInfo.Value instance.
+        // The original test used a confusing string-to-boolean conversion;
+        // using Boolean.FALSE is much clearer.
+        final Boolean requireTypeId = Boolean.FALSE;
+        final String propertyName = "@2LLQRbW9{J2*\"1GY";
+
+        JsonTypeInfo.Value initialValue = JsonTypeInfo.Value.construct(
+                JsonTypeInfo.Id.MINIMAL_CLASS,
+                JsonTypeInfo.As.WRAPPER_OBJECT,
+                propertyName,
+                Object.class,
+                false, // idVisible
+                requireTypeId
+        );
+
+        // Act: Call the method under test with the same boolean value.
+        JsonTypeInfo.Value updatedValue = initialValue.withRequireTypeIdForSubtypes(requireTypeId);
+
+        // Assert: Verify that the new instance is equal but not the same object.
+        // This confirms the immutable nature of the Value class.
+        assertEquals("The new instance should be logically equal to the original.", initialValue, updatedValue);
+        assertNotSame("The 'with...' method should always return a new instance.", initialValue, updatedValue);
+
+        // Also, verify that other properties were not unintentionally changed.
+        assertEquals(propertyName, updatedValue.getPropertyName());
+        assertFalse(updatedValue.getIdVisible());
     }
 }
