6879aad7b9bf9b8e369fbb90280c34eff60221b1
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:40:25 +0800

    Original processedAutoTestCases bufferedline_estest55.java

---
+++ bufferedline_estest55.java
@@ -0,0 +1,35 @@
+package org.locationtech.spatial4j.shape.impl;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.HashMap;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+import org.locationtech.spatial4j.context.SpatialContext;
+import org.locationtech.spatial4j.context.SpatialContextFactory;
+import org.locationtech.spatial4j.distance.CartesianDistCalc;
+import org.locationtech.spatial4j.shape.Point;
+import org.locationtech.spatial4j.shape.Rectangle;
+import org.locationtech.spatial4j.shape.Shape;
+import org.locationtech.spatial4j.shape.SpatialRelation;
+
+public class BufferedLine_ESTestTest55 extends BufferedLine_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test54() throws Throwable {
+        SpatialContext spatialContext0 = SpatialContext.GEO;
+        PointImpl pointImpl0 = new PointImpl(2.0, Double.POSITIVE_INFINITY, spatialContext0);
+        BufferedLine bufferedLine0 = null;
+        try {
+            bufferedLine0 = new BufferedLine(pointImpl0, pointImpl0, 2.0, spatialContext0);
+            fail("Expecting exception: RuntimeException");
+        } catch (RuntimeException e) {
+            //
+            // maxY must be >= minY: Infinity to 90.0
+            //
+            verifyException("org.locationtech.spatial4j.shape.impl.ShapeFactoryImpl", e);
+        }
+    }
+}
