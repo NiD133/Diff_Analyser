4999a47c29ae9877223328d9c30b441a40628771
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:25:44 +0800

    Original processedAutoTestCases utf8writer_estest40.java

---
+++ utf8writer_estest40.java
@@ -0,0 +1,47 @@
+package com.fasterxml.jackson.core.io;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.fasterxml.jackson.core.ErrorReportConfiguration;
+import com.fasterxml.jackson.core.StreamReadConstraints;
+import com.fasterxml.jackson.core.StreamWriteConstraints;
+import com.fasterxml.jackson.core.util.BufferRecycler;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PipedOutputStream;
+import java.io.Writer;
+import java.nio.CharBuffer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockFileOutputStream;
+import org.evosuite.runtime.mock.java.io.MockPrintStream;
+import org.junit.runner.RunWith;
+
+public class UTF8Writer_ESTestTest40 extends UTF8Writer_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test39() throws Throwable {
+        StreamReadConstraints streamReadConstraints0 = StreamReadConstraints.defaults();
+        StreamWriteConstraints streamWriteConstraints0 = StreamWriteConstraints.defaults();
+        ErrorReportConfiguration errorReportConfiguration0 = ErrorReportConfiguration.defaults();
+        BufferRecycler bufferRecycler0 = new BufferRecycler();
+        ContentReference contentReference0 = ContentReference.rawReference(true, (Object) null);
+        IOContext iOContext0 = new IOContext(streamReadConstraints0, streamWriteConstraints0, errorReportConfiguration0, bufferRecycler0, contentReference0, true);
+        PipedOutputStream pipedOutputStream0 = new PipedOutputStream();
+        UTF8Writer uTF8Writer0 = new UTF8Writer(iOContext0, pipedOutputStream0);
+        uTF8Writer0.write(55296);
+        try {
+            uTF8Writer0.append('M');
+            fail("Expecting exception: IOException");
+        } catch (IOException e) {
+            //
+            // Broken surrogate pair: first char 0xd800, second 0x4d; illegal combination
+            //
+            verifyException("com.fasterxml.jackson.core.io.UTF8Writer", e);
+        }
+    }
+}
