6ae17064427f412cc6ffdc287b01ce3737da4e8b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:54:10 +0800

    gemini processedAutoTestCases indexedcollection_estest62.java

--- indexedcollection_estest62.java
+++ indexedcollection_estest62.java
@@ -1,64 +1,36 @@
 package org.apache.commons.collections4.collection;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.ConcurrentModificationException;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Set;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.Predicate;
 import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AllPredicate;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ChainedTransformer;
-import org.apache.commons.collections4.functors.CloneTransformer;
-import org.apache.commons.collections4.functors.ClosureTransformer;
-import org.apache.commons.collections4.functors.ConstantFactory;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.DefaultEquator;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.FactoryTransformer;
-import org.apache.commons.collections4.functors.FalsePredicate;
-import org.apache.commons.collections4.functors.ForClosure;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.NOPClosure;
-import org.apache.commons.collections4.functors.NOPTransformer;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NotNullPredicate;
-import org.apache.commons.collections4.functors.NullIsFalsePredicate;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.SwitchTransformer;
-import org.apache.commons.collections4.functors.TransformedPredicate;
-import org.apache.commons.collections4.functors.TransformerClosure;
-import org.apache.commons.collections4.functors.TransformerPredicate;
-import org.apache.commons.collections4.functors.TruePredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class IndexedCollection_ESTestTest62 extends IndexedCollection_ESTest_scaffolding {
+import java.util.Collection;
+import java.util.LinkedList;
 
-    @Test(timeout = 4000)
-    public void test61() throws Throwable {
-        LinkedList<Integer> linkedList0 = new LinkedList<Integer>();
-        IndexedCollection<Integer, Integer> indexedCollection0 = IndexedCollection.uniqueIndexedCollection((Collection<Integer>) linkedList0, (Transformer<Integer, Integer>) null);
-        // Undeclared exception!
-        try {
-            indexedCollection0.contains((Object) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.commons.collections4.collection.IndexedCollection", e);
-        }
+/**
+ * This test class focuses on specific behaviors of the IndexedCollection.
+ * The original test was auto-generated and has been rewritten for clarity.
+ */
+public class IndexedCollectionRefactoredTest {
+
+    /**
+     * Tests that {@link IndexedCollection#contains(Object)} throws a NullPointerException
+     * if the collection was created with a null keyTransformer.
+     * <p>
+     * The {@code contains} method requires the transformer to generate a key from the
+     * input object before performing a lookup. It cannot operate if the transformer is null.
+     */
+    @Test(expected = NullPointerException.class)
+    public void containsShouldThrowNullPointerExceptionWhenKeyTransformerIsNull() {
+        // Arrange: Create an IndexedCollection with a null transformer.
+        // The underlying collection can be empty as it's not relevant to this test.
+        final Collection<Object> emptyCollection = new LinkedList<>();
+        final Transformer<Object, Object> nullTransformer = null;
+
+        final IndexedCollection<Object, Object> indexedCollection =
+                IndexedCollection.uniqueIndexedCollection(emptyCollection, nullTransformer);
+
+        // Act & Assert: Calling contains() should immediately throw a NullPointerException
+        // because it attempts to use the null transformer. The argument to contains() is irrelevant.
+        indexedCollection.contains(new Object());
     }
 }
