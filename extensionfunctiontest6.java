75fd1a6d3252f575f824033cdf5799cfaa6ab6ae
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:28:39 +0800

    gemini processedManualTestCases extensionfunctiontest6.java

--- extensionfunctiontest6.java
+++ extensionfunctiontest6.java
@@ -2,83 +2,52 @@ package org.apache.commons.jxpath.ri.compiler;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
-import java.util.ArrayList;
-import java.util.Collection;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import java.util.List;
 import java.util.Locale;
-import org.apache.commons.jxpath.AbstractJXPathTest;
 import org.apache.commons.jxpath.ClassFunctions;
 import org.apache.commons.jxpath.ExpressionContext;
 import org.apache.commons.jxpath.Function;
-import org.apache.commons.jxpath.FunctionLibrary;
-import org.apache.commons.jxpath.Functions;
 import org.apache.commons.jxpath.JXPathContext;
-import org.apache.commons.jxpath.NodeSet;
-import org.apache.commons.jxpath.PackageFunctions;
 import org.apache.commons.jxpath.Pointer;
-import org.apache.commons.jxpath.TestBean;
-import org.apache.commons.jxpath.Variables;
+import org.apache.commons.jxpath.TestFunctions;
 import org.apache.commons.jxpath.ri.model.NodePointer;
-import org.apache.commons.jxpath.util.JXPath11CompatibleTypeConverter;
-import org.apache.commons.jxpath.util.TypeConverter;
-import org.apache.commons.jxpath.util.TypeUtils;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class ExtensionFunctionTestTest6 extends AbstractJXPathTest {
+/**
+ * Tests the constructor resolution mechanism of {@link ClassFunctions}.
+ *
+ * This test verifies a specific feature: when resolving a constructor (using the "new" function name),
+ * ClassFunctions can use the context node from the {@link ExpressionContext} as the first argument,
+ * combining it with other explicitly provided arguments.
+ */
+class ClassFunctionsConstructorResolutionTest {
 
-    private Functions functions;
+    /**
+     * A mock {@link ExpressionContext} that provides a specific object as the context node.
+     * This is used to simulate the JXPath evaluation environment for function resolution.
+     */
+    private static class TestExpressionContext implements ExpressionContext {
+        private final Pointer contextNodePointer;
 
-    private JXPathContext context;
-
-    private TestBean testBean;
-
-    private TypeConverter typeConverter;
-
-    @Override
-    @BeforeEach
-    public void setUp() {
-        if (context == null) {
-            testBean = new TestBean();
-            context = JXPathContext.newContext(testBean);
-            final Variables vars = context.getVariables();
-            vars.declareVariable("test", new TestFunctions(4, "test"));
-            final FunctionLibrary lib = new FunctionLibrary();
-            lib.addFunctions(new ClassFunctions(TestFunctions.class, "test"));
-            lib.addFunctions(new ClassFunctions(TestFunctions2.class, "test"));
-            lib.addFunctions(new PackageFunctions("", "call"));
-            lib.addFunctions(new PackageFunctions("org.apache.commons.jxpath.ri.compiler.", "jxpathtest"));
-            lib.addFunctions(new PackageFunctions("", null));
-            context.setFunctions(lib);
-            context.getVariables().declareVariable("List.class", List.class);
-            context.getVariables().declareVariable("NodeSet.class", NodeSet.class);
-        }
-        functions = new ClassFunctions(TestFunctions.class, "test");
-        typeConverter = TypeUtils.getTypeConverter();
-    }
-
-    @AfterEach
-    public void tearDown() {
-        TypeUtils.setTypeConverter(typeConverter);
-    }
-
-    private static final class Context implements ExpressionContext {
-
-        private final Object object;
-
-        public Context(final Object object) {
-            this.object = object;
-        }
-
-        @Override
-        public List<Pointer> getContextNodeList() {
-            return null;
+        /**
+         * Creates a context with the given object as the context node.
+         * @param contextNode the object to be wrapped in a NodePointer.
+         */
+        public TestExpressionContext(final Object contextNode) {
+            this.contextNodePointer = NodePointer.newNodePointer(null, contextNode, Locale.getDefault());
         }
 
         @Override
         public Pointer getContextNodePointer() {
-            return NodePointer.newNodePointer(null, object, Locale.getDefault());
+            return contextNodePointer;
+        }
+
+        // Methods below are not used in this test and can return default values.
+        @Override
+        public List<Pointer> getContextNodeList() {
+            return null;
         }
 
         @Override
@@ -93,9 +62,31 @@ public class ExtensionFunctionTestTest6 extends AbstractJXPathTest {
     }
 
     @Test
-    void testConstructorLookupWithExpressionContext() {
-        final Object[] args = { "baz" };
-        final Function func = functions.getFunction("test", "new", args);
-        assertEquals("foo=1; bar=baz", func.invoke(new Context(Integer.valueOf(1)), args).toString(), "test:new('baz')");
+    void getFunction_shouldFindConstructor_whenUsingContextNodeAsFirstArgument() {
+        // ARRANGE
+        // We are testing the ability to find a constructor like 'new TestFunctions(int, String)'.
+        // The 'int' will come from the ExpressionContext, and the 'String' from the explicit arguments.
+        final ClassFunctions functions = new ClassFunctions(TestFunctions.class, "test");
+        final String constructorName = "new";
+        final Object[] explicitArgs = { "baz" }; // This will be the second argument.
+
+        // The ExpressionContext provides the value for the first constructor argument.
+        final Integer firstArgumentFromContext = 1;
+        final ExpressionContext context = new TestExpressionContext(firstArgumentFromContext);
+
+        final String expectedToString = "foo=1; bar=baz";
+
+        // ACT
+        // Find the constructor. ClassFunctions should match the call signature (context_node, "baz")
+        // to the actual constructor 'TestFunctions(int, String)'.
+        final Function constructor = functions.getFunction("test", constructorName, explicitArgs);
+
+        // Invoke the found constructor function.
+        final Object newInstance = constructor.invoke(context, explicitArgs);
+
+        // ASSERT
+        assertNotNull(constructor, "A constructor function should have been found.");
+        assertInstanceOf(TestFunctions.class, newInstance, "The created object should be an instance of TestFunctions.");
+        assertEquals(expectedToString, newInstance.toString(), "The new instance should be correctly initialized.");
     }
 }
