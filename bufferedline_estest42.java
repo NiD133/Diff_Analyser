bec4d8a8acc8c11511eb5499a2b3e3d3c45ebd12
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:40:21 +0800

    Original processedAutoTestCases bufferedline_estest42.java

---
+++ bufferedline_estest42.java
@@ -0,0 +1,30 @@
+package org.locationtech.spatial4j.shape.impl;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.HashMap;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+import org.locationtech.spatial4j.context.SpatialContext;
+import org.locationtech.spatial4j.context.SpatialContextFactory;
+import org.locationtech.spatial4j.distance.CartesianDistCalc;
+import org.locationtech.spatial4j.shape.Point;
+import org.locationtech.spatial4j.shape.Rectangle;
+import org.locationtech.spatial4j.shape.Shape;
+import org.locationtech.spatial4j.shape.SpatialRelation;
+
+public class BufferedLine_ESTestTest42 extends BufferedLine_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test41() throws Throwable {
+        SpatialContextFactory spatialContextFactory0 = new SpatialContextFactory();
+        SpatialContext spatialContext0 = new SpatialContext(spatialContextFactory0);
+        PointImpl pointImpl0 = new PointImpl(3.150319189088167, Double.POSITIVE_INFINITY, spatialContext0);
+        BufferedLine bufferedLine0 = new BufferedLine(pointImpl0, pointImpl0, Double.POSITIVE_INFINITY, spatialContext0);
+        SpatialRelation spatialRelation0 = bufferedLine0.relate((Shape) pointImpl0);
+        assertEquals(SpatialRelation.DISJOINT, spatialRelation0);
+        assertTrue(bufferedLine0.hasArea());
+    }
+}
