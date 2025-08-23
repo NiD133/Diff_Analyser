b07f5de3a8b1385dabda63d30879914d0a9a5f72
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:26:23 +0800

    gemini processedManualTestCases caseformattest25.java

--- caseformattest25.java
+++ caseformattest25.java
@@ -1,24 +1,38 @@
 package com.google.common.base;
 
 import static com.google.common.base.CaseFormat.LOWER_CAMEL;
-import static com.google.common.base.CaseFormat.LOWER_HYPHEN;
-import static com.google.common.base.CaseFormat.LOWER_UNDERSCORE;
-import static com.google.common.base.CaseFormat.UPPER_CAMEL;
 import static com.google.common.base.CaseFormat.UPPER_UNDERSCORE;
 import static com.google.common.truth.Truth.assertThat;
-import static com.google.common.truth.Truth.assertWithMessage;
-import com.google.common.annotations.GwtCompatible;
-import com.google.common.annotations.GwtIncompatible;
-import com.google.common.annotations.J2ktIncompatible;
-import com.google.common.testing.NullPointerTester;
-import com.google.common.testing.SerializableTester;
-import junit.framework.TestCase;
-import org.jspecify.annotations.NullUnmarked;
 
+import junit.framework.TestCase;
+
+/**
+ * Tests for the conversion from {@link CaseFormat#UPPER_UNDERSCORE} to {@link
+ * CaseFormat#LOWER_CAMEL}.
+ */
 public class CaseFormatTestTest25 extends TestCase {
 
-    public void testUpperUnderscoreToLowerCamel() {
-        assertThat(UPPER_UNDERSCORE.to(LOWER_CAMEL, "FOO")).isEqualTo("foo");
-        assertThat(UPPER_UNDERSCORE.to(LOWER_CAMEL, "FOO_BAR")).isEqualTo("fooBar");
-    }
+  public void testUpperUnderscoreToLowerCamel_convertsSingleWord() {
+    // Arrange
+    String input = "FOO";
+    String expectedOutput = "foo";
+
+    // Act
+    String actualOutput = UPPER_UNDERSCORE.to(LOWER_CAMEL, input);
+
+    // Assert
+    assertThat(actualOutput).isEqualTo(expectedOutput);
+  }
+
+  public void testUpperUnderscoreToLowerCamel_convertsMultipleWords() {
+    // Arrange
+    String input = "FOO_BAR";
+    String expectedOutput = "fooBar";
+
+    // Act
+    String actualOutput = UPPER_UNDERSCORE.to(LOWER_CAMEL, input);
+
+    // Assert
+    assertThat(actualOutput).isEqualTo(expectedOutput);
+  }
 }
