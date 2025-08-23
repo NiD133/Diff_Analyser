8921f25ffd6f677b641acbd033df932fb2604232
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:27:53 +0800

    gemini processedManualTestCases dayofyeartest51.java

--- dayofyeartest51.java
+++ dayofyeartest51.java
@@ -1,143 +1,53 @@
 package org.threeten.extra;
 
-import static java.time.temporal.ChronoField.ALIGNED_DAY_OF_WEEK_IN_MONTH;
-import static java.time.temporal.ChronoField.ALIGNED_DAY_OF_WEEK_IN_YEAR;
-import static java.time.temporal.ChronoField.ALIGNED_WEEK_OF_MONTH;
-import static java.time.temporal.ChronoField.ALIGNED_WEEK_OF_YEAR;
-import static java.time.temporal.ChronoField.AMPM_OF_DAY;
-import static java.time.temporal.ChronoField.CLOCK_HOUR_OF_AMPM;
-import static java.time.temporal.ChronoField.CLOCK_HOUR_OF_DAY;
-import static java.time.temporal.ChronoField.DAY_OF_MONTH;
-import static java.time.temporal.ChronoField.DAY_OF_WEEK;
-import static java.time.temporal.ChronoField.DAY_OF_YEAR;
-import static java.time.temporal.ChronoField.EPOCH_DAY;
-import static java.time.temporal.ChronoField.ERA;
-import static java.time.temporal.ChronoField.HOUR_OF_AMPM;
-import static java.time.temporal.ChronoField.HOUR_OF_DAY;
-import static java.time.temporal.ChronoField.INSTANT_SECONDS;
-import static java.time.temporal.ChronoField.MICRO_OF_DAY;
-import static java.time.temporal.ChronoField.MICRO_OF_SECOND;
-import static java.time.temporal.ChronoField.MILLI_OF_DAY;
-import static java.time.temporal.ChronoField.MILLI_OF_SECOND;
-import static java.time.temporal.ChronoField.MINUTE_OF_DAY;
-import static java.time.temporal.ChronoField.MINUTE_OF_HOUR;
-import static java.time.temporal.ChronoField.MONTH_OF_YEAR;
-import static java.time.temporal.ChronoField.NANO_OF_DAY;
-import static java.time.temporal.ChronoField.NANO_OF_SECOND;
-import static java.time.temporal.ChronoField.OFFSET_SECONDS;
-import static java.time.temporal.ChronoField.PROLEPTIC_MONTH;
-import static java.time.temporal.ChronoField.SECOND_OF_DAY;
-import static java.time.temporal.ChronoField.SECOND_OF_MINUTE;
-import static java.time.temporal.ChronoField.YEAR;
-import static java.time.temporal.ChronoField.YEAR_OF_ERA;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertSame;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
+
 import java.time.Clock;
-import java.time.DateTimeException;
 import java.time.Instant;
 import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.Year;
 import java.time.ZoneId;
-import java.time.chrono.IsoChronology;
-import java.time.chrono.JapaneseDate;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
-import java.time.temporal.IsoFields;
-import java.time.temporal.Temporal;
-import java.time.temporal.TemporalAccessor;
-import java.time.temporal.TemporalAdjuster;
-import java.time.temporal.TemporalField;
-import java.time.temporal.TemporalQueries;
-import java.time.temporal.TemporalUnit;
-import java.time.temporal.UnsupportedTemporalTypeException;
-import java.time.temporal.ValueRange;
-import org.junit.jupiter.api.Test;
-import org.junitpioneer.jupiter.RetryingTest;
-import com.google.common.testing.EqualsTester;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
+/**
+ * Tests for {@link DayOfYear#now(Clock)}.
+ */
 public class DayOfYearTestTest51 {
 
-    private static final Year YEAR_STANDARD = Year.of(2007);
-
-    private static final Year YEAR_LEAP = Year.of(2008);
-
-    private static final int STANDARD_YEAR_LENGTH = 365;
-
-    private static final int LEAP_YEAR_LENGTH = 366;
-
-    private static final DayOfYear TEST = DayOfYear.of(12);
-
     private static final ZoneId PARIS = ZoneId.of("Europe/Paris");
+    private static final int LEAP_YEAR = 2008;
 
-    private static class TestingField implements TemporalField {
-
-        public static final TestingField INSTANCE = new TestingField();
-
-        @Override
-        public TemporalUnit getBaseUnit() {
-            return ChronoUnit.DAYS;
-        }
-
-        @Override
-        public TemporalUnit getRangeUnit() {
-            return ChronoUnit.YEARS;
-        }
-
-        @Override
-        public ValueRange range() {
-            return ValueRange.of(1, 365, 366);
-        }
-
-        @Override
-        public boolean isDateBased() {
-            return true;
-        }
-
-        @Override
-        public boolean isTimeBased() {
-            return false;
-        }
-
-        @Override
-        public boolean isSupportedBy(TemporalAccessor temporal) {
-            return temporal.isSupported(DAY_OF_YEAR);
-        }
-
-        @Override
-        public ValueRange rangeRefinedBy(TemporalAccessor temporal) {
-            return range();
-        }
-
-        @Override
-        public long getFrom(TemporalAccessor temporal) {
-            return temporal.getLong(DAY_OF_YEAR);
-        }
-
-        @Override
-        @SuppressWarnings("unchecked")
-        public <R extends Temporal> R adjustInto(R temporal, long newValue) {
-            return (R) temporal.with(DAY_OF_YEAR, newValue);
-        }
+    /**
+     * Provides a stream of every date within a single leap year.
+     * This stream is used as the source for the parameterized test.
+     *
+     * @return A stream of {@link LocalDate} objects for the year 2008.
+     */
+    private static Stream<LocalDate> leapYearDateProvider() {
+        LocalDate startOfLeapYear = LocalDate.of(LEAP_YEAR, 1, 1);
+        LocalDate startOfNextYear = startOfLeapYear.plusYears(1);
+        return startOfLeapYear.datesUntil(startOfNextYear);
     }
 
-    @Test
-    public void test_now_clock_leapYear() {
-        LocalDate date = LocalDate.of(2008, 1, 1);
-        for (int i = 1; i <= LEAP_YEAR_LENGTH; i++) {
-            Instant instant = date.atStartOfDay(PARIS).toInstant();
-            Clock clock = Clock.fixed(instant, PARIS);
-            DayOfYear test = DayOfYear.now(clock);
-            assertEquals(i, test.getValue());
-            date = date.plusDays(1);
-        }
+    /**
+     * Verifies that DayOfYear.now(clock) correctly determines the day-of-year
+     * for any given date within a leap year.
+     *
+     * @param date The specific date in the leap year to be tested.
+     */
+    @ParameterizedTest(name = "for {0}, day-of-year should be {0, dayOfYear}")
+    @MethodSource("leapYearDateProvider")
+    void nowWithClock_returnsCorrectDayOfYear_forEveryDayInLeapYear(LocalDate date) {
+        // Arrange: Create a clock fixed to the start of the given date in the Paris time zone.
+        int expectedDayOfYear = date.getDayOfYear();
+        Instant instant = date.atStartOfDay(PARIS).toInstant();
+        Clock clock = Clock.fixed(instant, PARIS);
+
+        // Act: Obtain the DayOfYear from the fixed clock.
+        DayOfYear actualDayOfYear = DayOfYear.now(clock);
+
+        // Assert: The obtained DayOfYear's value should match the expected day of the year.
+        assertEquals(expectedDayOfYear, actualDayOfYear.getValue());
     }
 }
