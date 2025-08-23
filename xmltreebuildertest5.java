93c51699e9c416a32c9189f6c02b881db0ad074f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:36:18 +0800

    gemini processedManualTestCases xmltreebuildertest5.java

--- xmltreebuildertest5.java
+++ xmltreebuildertest5.java
@@ -2,32 +2,37 @@ package org.jsoup.parser;
 
 import org.jsoup.Jsoup;
 import org.jsoup.TextUtil;
-import org.jsoup.nodes.*;
-import org.jsoup.select.Elements;
+import org.jsoup.nodes.Document;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import java.io.File;
-import java.io.FileInputStream;
+
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URISyntaxException;
-import java.nio.charset.StandardCharsets;
-import java.util.List;
-import static org.jsoup.nodes.Document.OutputSettings.Syntax;
-import static org.jsoup.parser.Parser.NamespaceHtml;
-import static org.jsoup.parser.Parser.NamespaceXml;
-import static org.junit.jupiter.api.Assertions.*;
 
-public class XmlTreeBuilderTestTest5 {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-    private static void assertXmlNamespace(Element el) {
-        assertEquals(NamespaceXml, el.tag().namespace(), String.format("Element %s not in XML namespace", el.tagName()));
-    }
+/**
+ * Tests for the {@link XmlTreeBuilder}, focusing on its ability to parse from various sources.
+ */
+public class XmlTreeBuilderTest {
 
     @Test
-    public void testSupplyParserToDataStream() throws IOException, URISyntaxException {
-        File xmlFile = new File(XmlTreeBuilder.class.getResource("/htmltests/xml-test.xml").toURI());
-        InputStream inStream = new FileInputStream(xmlFile);
-        Document doc = Jsoup.parse(inStream, null, "http://foo.com", Parser.xmlParser());
-        assertEquals("<doc><val>One<val>Two</val>Three</val></doc>", TextUtil.stripNewlines(doc.html()));
+    @DisplayName("should correctly parse an XML document from an InputStream")
+    void xmlParserCanParseFromInputStream() throws IOException {
+        // Arrange
+        String resourcePath = "/htmltests/xml-test.xml";
+        String baseUrl = "http://foo.com";
+        String expectedXml = "<doc><val>One<val>Two</val>Three</val></doc>";
+
+        try (InputStream xmlInputStream = XmlTreeBuilder.class.getResourceAsStream(resourcePath)) {
+            assertNotNull(xmlInputStream, "Test resource file not found: " + resourcePath);
+
+            // Act
+            Document doc = Jsoup.parse(xmlInputStream, null, baseUrl, Parser.xmlParser());
+
+            // Assert
+            assertEquals(expectedXml, TextUtil.stripNewlines(doc.html()));
+        }
     }
 }
