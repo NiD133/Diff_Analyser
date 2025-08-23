45cef877720c54e071f018085a6822b94271b6b0
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:36:26 +0800

    Original processedManualTestCases yearstest10.java

---
+++ yearstest10.java
@@ -0,0 +1,39 @@
+package org.joda.time;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class YearsTestTest10 extends TestCase {
+
+    // (before the late 90's they were all over the place)
+    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
+
+    public static TestSuite suite() {
+        return new TestSuite(TestYears.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+    }
+
+    //-----------------------------------------------------------------------
+    public void testIsGreaterThan() {
+        assertEquals(true, Years.THREE.isGreaterThan(Years.TWO));
+        assertEquals(false, Years.THREE.isGreaterThan(Years.THREE));
+        assertEquals(false, Years.TWO.isGreaterThan(Years.THREE));
+        assertEquals(true, Years.ONE.isGreaterThan(null));
+        assertEquals(false, Years.years(-1).isGreaterThan(null));
+    }
+}
