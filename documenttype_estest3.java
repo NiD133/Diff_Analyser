2ef5644e8b5adf52b7e9d42c9529d18b1dea6d6f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:48:11 +0800

    Original processedAutoTestCases documenttype_estest3.java

---
+++ documenttype_estest3.java
@@ -0,0 +1,23 @@
+package org.jsoup.nodes;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.StringWriter;
+import java.nio.BufferOverflowException;
+import java.nio.CharBuffer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.jsoup.internal.QuietAppendable;
+import org.junit.runner.RunWith;
+
+public class DocumentType_ESTestTest3 extends DocumentType_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test02() throws Throwable {
+        DocumentType documentType0 = new DocumentType("SYSTEM", "SYSTEM", "f}#ng}$s-@#ua");
+        String string0 = documentType0.publicId();
+        assertEquals("SYSTEM", string0);
+        assertEquals("#doctype", documentType0.nodeName());
+    }
+}
