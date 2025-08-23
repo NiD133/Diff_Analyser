24214c9bb816af83e2132ddf5964fc09cd68b674
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:18:05 +0800

    Original processedAutoTestCases standardxybarpainter_estest6.java

---
+++ standardxybarpainter_estest6.java
@@ -0,0 +1,41 @@
+package org.jfree.chart.renderer.xy;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.font.FontRenderContext;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Arc2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.RoundRectangle2D;
+import java.awt.image.BufferedImage;
+import javax.swing.text.DefaultCaret;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.jfree.chart.api.RectangleEdge;
+import org.jfree.chart.util.GradientPaintTransformer;
+import org.junit.runner.RunWith;
+
+public class StandardXYBarPainter_ESTestTest6 extends StandardXYBarPainter_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test05() throws Throwable {
+        StandardXYBarPainter standardXYBarPainter0 = new StandardXYBarPainter();
+        Rectangle2D.Float rectangle2D_Float0 = new Rectangle2D.Float();
+        XYBarRenderer xYBarRenderer0 = new XYBarRenderer();
+        RectangleEdge rectangleEdge0 = RectangleEdge.TOP;
+        xYBarRenderer0.setShadowYOffset(0.0);
+        // Undeclared exception!
+        try {
+            standardXYBarPainter0.paintBarShadow((Graphics2D) null, xYBarRenderer0, 0, 0, rectangle2D_Float0, rectangleEdge0, false);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.jfree.chart.renderer.xy.StandardXYBarPainter", e);
+        }
+    }
+}
