f98cff980ae69311979a902f559c618ea2f139ec
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:42:23 +0800

    Original processedAutoTestCases charsequenceutils_estest19.java

---
+++ charsequenceutils_estest19.java
@@ -0,0 +1,19 @@
+package org.apache.commons.lang3;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.nio.CharBuffer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class CharSequenceUtils_ESTestTest19 extends CharSequenceUtils_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test18() throws Throwable {
+        StringBuilder stringBuilder0 = new StringBuilder(0);
+        CharSequence charSequence0 = CharSequenceUtils.subSequence(stringBuilder0, 0);
+        assertEquals("", charSequence0);
+    }
+}
