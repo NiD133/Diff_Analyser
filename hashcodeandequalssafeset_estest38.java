5100541592b4ca9b458bf0f94ff16fd6aaa03ce2
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:53:08 +0800

    Original processedAutoTestCases hashcodeandequalssafeset_estest38.java

---
+++ hashcodeandequalssafeset_estest38.java
@@ -0,0 +1,37 @@
+package org.mockito.internal.util.collections;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Vector;
+import java.util.function.Predicate;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class HashCodeAndEqualsSafeSet_ESTestTest38 extends HashCodeAndEqualsSafeSet_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test37() throws Throwable {
+        HashCodeAndEqualsSafeSet hashCodeAndEqualsSafeSet0 = new HashCodeAndEqualsSafeSet();
+        Object object0 = new Object();
+        HashCodeAndEqualsMockWrapper hashCodeAndEqualsMockWrapper0 = HashCodeAndEqualsMockWrapper.of(object0);
+        List<HashCodeAndEqualsMockWrapper> list0 = List.of(hashCodeAndEqualsMockWrapper0, hashCodeAndEqualsMockWrapper0, hashCodeAndEqualsMockWrapper0, hashCodeAndEqualsMockWrapper0);
+        // Undeclared exception!
+        try {
+            hashCodeAndEqualsSafeSet0.removeAll(list0);
+            fail("Expecting exception: AssertionError");
+        } catch (AssertionError e) {
+            //
+            // WRONG
+            //
+        }
+    }
+}
