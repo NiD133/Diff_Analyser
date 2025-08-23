2172c03c74c5f552b774310de04d8fdb62be7020
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:26:57 +0800

    gemini processedAutoTestCases w3cdom_estest50.java

--- w3cdom_estest50.java
+++ w3cdom_estest50.java
@@ -1,48 +1,43 @@
 package org.jsoup.helper;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import com.sun.org.apache.xerces.internal.dom.DocumentImpl;
-import java.io.Reader;
-import java.io.StringReader;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
-import javax.imageio.metadata.IIOMetadataNode;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.jsoup.nodes.Attributes;
-import org.jsoup.nodes.DataNode;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.DocumentType;
-import org.jsoup.nodes.Element;
-import org.jsoup.nodes.FormElement;
-import org.jsoup.nodes.XmlDeclaration;
 import org.jsoup.parser.Parser;
-import org.jsoup.parser.Tag;
-import org.junit.runner.RunWith;
-import org.w3c.dom.DOMException;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import org.junit.Test;
 
+/**
+ * This test class focuses on an edge case for the W3CDom#convert method.
+ * Note: The original class name 'W3CDom_ESTestTest50' is kept to match the file,
+ * but a more descriptive name like 'W3CDomConvertEdgeCasesTest' would be preferable.
+ */
 public class W3CDom_ESTestTest50 extends W3CDom_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test49() throws Throwable {
-        W3CDom w3CDom0 = new W3CDom();
-        Document document0 = Parser.parse("jsoupSource", "jsoupSource");
-        org.w3c.dom.Document document1 = w3CDom0.fromJsoup(document0);
-        Document document2 = document0.shallowClone();
-        // Undeclared exception!
-        try {
-            w3CDom0.convert(document2, document1);
-            fail("Expecting exception: AssertionError");
-        } catch (AssertionError e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-        }
+    /**
+     * Verifies that converting a cloned Jsoup document into a W3C document
+     * that was previously populated from the *original* Jsoup document fails with an
+     * AssertionError.
+     * <p>
+     * This test case reveals that the {@code convert} method likely has an internal
+     * state check. It fails when the target W3C document is detected to be associated
+     * with a different source Jsoup document instance than the one currently being converted.
+     * </p>
+     */
+    @Test(timeout = 4000, expected = AssertionError.class)
+    public void convertingClonedJsoupDocIntoExistingW3cDocShouldFail() {
+        // Arrange
+        W3CDom w3cDom = new W3CDom();
+        Document originalJsoupDoc = Parser.parse("<html><body>jsoup</body></html>", "http://example.com/");
+
+        // 1. Create a W3C document from the original Jsoup document.
+        org.w3c.dom.Document w3cDoc = w3cDom.fromJsoup(originalJsoupDoc);
+
+        // 2. Create a shallow clone of the original document. A shallow clone is a new
+        // object and does not share the same identity as the original.
+        Document clonedJsoupDoc = originalJsoupDoc.shallowClone();
+
+        // Act: Attempt to convert the cloned document into the already-populated W3C document.
+        // This action is expected to trigger an internal assertion failure.
+        w3cDom.convert(clonedJsoupDoc, w3cDoc);
+
+        // Assert: The test expects an AssertionError, as specified by the @Test(expected=...) annotation.
     }
 }
