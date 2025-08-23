60b37c7c74137317cf3d416898a4019cc436c9c6
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:46:02 +0800

    gemini processedAutoTestCases coreoperation_estest14.java

--- coreoperation_estest14.java
+++ coreoperation_estest14.java
@@ -1,46 +1,32 @@
 package org.apache.commons.jxpath.ri.compiler;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.apache.commons.jxpath.JXPathContext;
-import org.apache.commons.jxpath.ri.EvalContext;
-import org.apache.commons.jxpath.ri.JXPathContextReferenceImpl;
-import org.apache.commons.jxpath.ri.QName;
-import org.apache.commons.jxpath.ri.axes.InitialContext;
-import org.apache.commons.jxpath.ri.axes.ParentContext;
-import org.apache.commons.jxpath.ri.axes.PrecedingOrFollowingContext;
-import org.apache.commons.jxpath.ri.axes.RootContext;
-import org.apache.commons.jxpath.ri.axes.SelfContext;
-import org.apache.commons.jxpath.ri.axes.UnionContext;
-import org.apache.commons.jxpath.ri.model.NodePointer;
-import org.apache.commons.jxpath.ri.model.VariablePointer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class CoreOperation_ESTestTest14 extends CoreOperation_ESTest_scaffolding {
+/**
+ * Contains tests for the abstract class {@link CoreOperation} and its subclasses.
+ */
+public class CoreOperationTest {
 
-    @Test(timeout = 4000)
-    public void test13() throws Throwable {
-        Expression[] expressionArray0 = new Expression[8];
-        CoreFunction coreFunction0 = new CoreFunction(16, expressionArray0);
-        CoreOperationLessThanOrEqual coreOperationLessThanOrEqual0 = new CoreOperationLessThanOrEqual(coreFunction0, coreFunction0);
-        CoreOperationNegate coreOperationNegate0 = new CoreOperationNegate(coreOperationLessThanOrEqual0);
-        Expression[] expressionArray1 = new Expression[6];
-        expressionArray1[0] = (Expression) coreFunction0;
-        expressionArray1[1] = (Expression) coreOperationNegate0;
-        CoreOperationUnion coreOperationUnion0 = new CoreOperationUnion(expressionArray1);
-        expressionArray0[1] = (Expression) coreOperationUnion0;
-        // Undeclared exception!
-        try {
-            coreOperationUnion0.computeValue((EvalContext) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.evosuite.runtime.System", e);
-        }
+    /**
+     * Tests that calling computeValue on a CoreOperation with a null EvalContext
+     * throws a NullPointerException.
+     *
+     * This behavior is fundamental, as most operations require a valid context
+     * to access data or functions. We test this using a CoreOperationUnion,
+     * which delegates computation to its arguments. The union is set up with a
+     * CoreFunction argument, which is known to dereference the context and thus
+     * trigger the expected NPE.
+     */
+    @Test(expected = NullPointerException.class)
+    public void computeValueWithNullContextThrowsNullPointerException() {
+        // Arrange: Create a union operation with an argument that requires a valid context.
+        // A CoreFunction is a simple choice, as it will attempt to access the context
+        // to resolve the function, causing an NPE if the context is null.
+        Expression[] arguments = {new CoreFunction(0, new Expression[0])};
+        CoreOperation unionOperation = new CoreOperationUnion(arguments);
+
+        // Act: Attempt to compute the value with a null context.
+        // This is expected to throw a NullPointerException.
+        unionOperation.computeValue(null);
     }
 }
