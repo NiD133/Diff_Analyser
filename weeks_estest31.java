5faa0063c8ac9f4821a9851900ce4df1618b2589
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:27:12 +0800

    gemini processedAutoTestCases weeks_estest31.java

--- weeks_estest31.java
+++ weeks_estest31.java
@@ -1,18 +1,36 @@
 package org.joda.time;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class Weeks_ESTestTest31 extends Weeks_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link Weeks} class.
+ */
+public class WeeksTest {
 
-    @Test(timeout = 4000)
-    public void test30() throws Throwable {
-        Hours hours0 = Hours.MIN_VALUE;
-        Weeks weeks0 = Weeks.standardWeeksIn(hours0);
-        assertEquals((-12782640), weeks0.getWeeks());
+    /**
+     * Tests that {@link Weeks#standardWeeksIn(ReadablePeriod)} correctly calculates
+     * the number of weeks from the minimum possible number of hours.
+     * This serves as a test for boundary conditions.
+     */
+    @Test
+    public void standardWeeksIn_withMinimumValueHours_calculatesCorrectly() {
+        // Arrange
+        // A standard week has 7 days, and a standard day has 24 hours.
+        final int HOURS_PER_WEEK = 24 * 7;
+        
+        // Use Hours.MIN_VALUE to test the lower boundary condition.
+        Hours minHours = Hours.MIN_VALUE;
+        
+        // The expected result is the total number of hours divided by the hours in a week.
+        // This is an integer division, so any remainder is truncated.
+        int expectedWeeks = Integer.MIN_VALUE / HOURS_PER_WEEK;
+
+        // Act
+        Weeks actualWeeks = Weeks.standardWeeksIn(minHours);
+
+        // Assert
+        assertEquals("The number of weeks should be correctly calculated from MIN_VALUE hours.",
+                expectedWeeks, actualWeeks.getWeeks());
     }
 }
