55d5d0bbb7aa2a485bee809a665a91c5b17f962f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:38:33 +0800

    gemini processedAutoTestCases backgroundinitializer_estest19.java

--- backgroundinitializer_estest19.java
+++ backgroundinitializer_estest19.java
@@ -1,41 +1,24 @@
 package org.apache.commons.lang3.concurrent;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.concurrent.Delayed;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.RejectedExecutionException;
-import java.util.concurrent.SynchronousQueue;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.evosuite.runtime.mock.java.lang.MockException;
-import org.evosuite.runtime.mock.java.lang.MockThrowable;
-import org.evosuite.runtime.testdata.FileSystemHandling;
-import org.junit.runner.RunWith;
 
-public class BackgroundInitializer_ESTestTest19 extends BackgroundInitializer_ESTest_scaffolding {
+/**
+ * Unit tests for {@link BackgroundInitializer}.
+ */
+public class BackgroundInitializerTest {
 
-    @Test(timeout = 4000)
-    public void test18() throws Throwable {
-        BackgroundInitializer<Exception> backgroundInitializer0 = new BackgroundInitializer<Exception>();
-        BackgroundInitializer.builder();
-        backgroundInitializer0.close();
-        // Undeclared exception!
-        try {
-            backgroundInitializer0.get();
-            fail("Expecting exception: IllegalStateException");
-        } catch (IllegalStateException e) {
-            //
-            // start() must be called first!
-            //
-            verifyException("org.apache.commons.lang3.concurrent.BackgroundInitializer", e);
-        }
+    /**
+     * Tests that calling get() on an initializer that has not been started
+     * throws an IllegalStateException, as specified by the method's contract.
+     */
+    @Test(expected = IllegalStateException.class)
+    public void getShouldThrowIllegalStateExceptionWhenNotStarted() {
+        // Arrange: Create an initializer but do not call start().
+        final BackgroundInitializer<Object> initializer = new BackgroundInitializer<>();
+
+        // Act: Attempt to get the result before initialization has begun.
+        // Assert: An IllegalStateException is expected, as declared by the @Test annotation.
+        // The expected message is "start() must be called first!".
+        initializer.get();
     }
 }
