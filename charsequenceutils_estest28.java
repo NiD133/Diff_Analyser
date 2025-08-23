4ab5c52d521d4ad6b2620458e0747dcb42a7a0a7
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:42:26 +0800

    gemini processedAutoTestCases charsequenceutils_estest28.java

--- charsequenceutils_estest28.java
+++ charsequenceutils_estest28.java
@@ -1,26 +1,20 @@
 package org.apache.commons.lang3;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.nio.CharBuffer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class CharSequenceUtils_ESTestTest28 extends CharSequenceUtils_ESTest_scaffolding {
+/**
+ * Unit tests for {@link org.apache.commons.lang3.CharSequenceUtils}.
+ */
+public class CharSequenceUtilsTest {
 
-    @Test(timeout = 4000)
-    public void test27() throws Throwable {
-        // Undeclared exception!
-        try {
-            CharSequenceUtils.indexOf((CharSequence) null, 113, 113);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.commons.lang3.CharSequenceUtils", e);
-        }
+    /**
+     * Tests that {@link CharSequenceUtils#indexOf(CharSequence, int, int)} throws
+     * a NullPointerException when the input CharSequence is null.
+     */
+    @Test(expected = NullPointerException.class)
+    public void indexOf_withNullCharSequence_shouldThrowNullPointerException() {
+        // The specific character ('a') and start index (0) are arbitrary values,
+        // as the method should throw the exception before using them.
+        CharSequenceUtils.indexOf(null, 'a', 0);
     }
 }
