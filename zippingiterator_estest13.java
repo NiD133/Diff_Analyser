8fdaf37688993f37a9a698c71e86665087df3ae6
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:30:43 +0800

    gemini processedAutoTestCases zippingiterator_estest13.java

--- zippingiterator_estest13.java
+++ zippingiterator_estest13.java
@@ -1,34 +1,31 @@
 package org.apache.commons.collections4.iterators;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.ConcurrentModificationException;
+
+import java.util.Collections;
 import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.ListIterator;
 import java.util.NoSuchElementException;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class ZippingIterator_ESTestTest13 extends ZippingIterator_ESTest_scaffolding {
+/**
+ * Unit tests for {@link ZippingIterator}.
+ */
+public class ZippingIteratorTest {
 
-    @Test(timeout = 4000)
-    public void test12() throws Throwable {
-        LinkedList<Integer> linkedList0 = new LinkedList<Integer>();
-        Iterator<Integer> iterator0 = linkedList0.iterator();
-        ZippingIterator<Object> zippingIterator0 = new ZippingIterator<Object>(iterator0, iterator0, iterator0);
-        try {
-            zippingIterator0.next();
-            fail("Expecting exception: NoSuchElementException");
-        } catch (NoSuchElementException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.commons.collections4.iterators.ZippingIterator", e);
-        }
+    /**
+     * Tests that calling next() on a ZippingIterator created with only empty
+     * iterators throws a NoSuchElementException.
+     */
+    @Test(expected = NoSuchElementException.class)
+    public void testNextShouldThrowExceptionWhenAllIteratorsAreEmpty() {
+        // Arrange: Create a ZippingIterator from three empty iterators.
+        // Using Collections.emptyIterator() is a standard and clear way to get an empty iterator.
+        final ZippingIterator<Object> zippingIterator = new ZippingIterator<>(
+                Collections.emptyIterator(),
+                Collections.emptyIterator(),
+                Collections.emptyIterator());
+
+        // Act & Assert: Calling next() should throw the expected exception.
+        // The assertion is handled by the @Test(expected=...) annotation.
+        zippingIterator.next();
     }
 }
