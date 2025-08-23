39b5b456b743fd3f6003e28b7929ed3b42de548d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:54:44 +0800

    Original processedAutoTestCases intervalcategoryitemlabelgenerator_estest7.java

---
+++ intervalcategoryitemlabelgenerator_estest7.java
@@ -0,0 +1,34 @@
+package org.jfree.chart.labels;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.text.DateFormat;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.time.chrono.ThaiBuddhistEra;
+import javax.swing.JLayeredPane;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.text.MockDateFormat;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.category.DefaultIntervalCategoryDataset;
+import org.jfree.data.statistics.DefaultStatisticalCategoryDataset;
+import org.junit.runner.RunWith;
+
+public class IntervalCategoryItemLabelGenerator_ESTestTest7 extends IntervalCategoryItemLabelGenerator_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test06() throws Throwable {
+        IntervalCategoryItemLabelGenerator intervalCategoryItemLabelGenerator0 = null;
+        try {
+            intervalCategoryItemLabelGenerator0 = new IntervalCategoryItemLabelGenerator("{2}", (DateFormat) null);
+            fail("Expecting exception: IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //
+            // Null 'formatter' argument.
+            //
+            verifyException("org.jfree.chart.internal.Args", e);
+        }
+    }
+}
