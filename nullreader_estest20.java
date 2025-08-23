3646ed3297115644d5b60b4ac2384ee9ce902ac6
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:04:50 +0800

    gemini processedAutoTestCases nullreader_estest20.java

--- nullreader_estest20.java
+++ nullreader_estest20.java
@@ -1,28 +1,36 @@
 package org.apache.commons.io.input;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.EOFException;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
 import java.io.IOException;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class NullReader_ESTestTest20 extends NullReader_ESTest_scaffolding {
+/**
+ * Contains tests for the {@link NullReader} class.
+ */
+public class NullReaderTest {
 
-    @Test(timeout = 4000)
-    public void test19() throws Throwable {
-        NullReader nullReader0 = NullReader.INSTANCE;
+    /**
+     * Verifies that calling read() after the end of the file has been reached
+     * throws an IOException.
+     */
+    @Test
+    public void testReadAfterEndOfFileThrowsIOException() throws IOException {
+        // Arrange: Use the singleton instance which has a size of 0.
+        // This means it is already at the end of the file.
+        final NullReader reader = NullReader.INSTANCE;
+
+        // Act 1: The first read should return -1 to signal the end of the file.
+        assertEquals("Expected end of file on first read", -1, reader.read());
+
+        // Act 2 & Assert: A subsequent read should throw an IOException.
         try {
-            nullReader0.read();
-            //  fail("Expecting exception: IOException");
-            // Unstable assertion
-        } catch (IOException e) {
-            //
-            // Read after end of file
-            //
-            verifyException("org.apache.commons.io.input.NullReader", e);
+            reader.read();
+            fail("Expected an IOException to be thrown when reading past the end of the file.");
+        } catch (final IOException e) {
+            // Verify the exception has the expected message.
+            assertEquals("Read after end of file", e.getMessage());
         }
     }
 }
