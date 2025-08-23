93faf160819ccc8b7a1c56bbf741dd42d752ada7
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:41:36 +0800

    gemini processedAutoTestCases cartesianproductiterator_estest7.java

--- cartesianproductiterator_estest7.java
+++ cartesianproductiterator_estest7.java
@@ -1,36 +1,26 @@
 package org.apache.commons.collections4.iterators;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.sql.SQLNonTransientConnectionException;
-import java.util.ConcurrentModificationException;
-import java.util.EnumSet;
-import java.util.List;
-import java.util.Locale;
-import java.util.NoSuchElementException;
-import java.util.PriorityQueue;
-import java.util.Stack;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class CartesianProductIterator_ESTestTest7 extends CartesianProductIterator_ESTest_scaffolding {
+/**
+ * Tests for {@link CartesianProductIterator}.
+ * This class focuses on specific behaviors not covered in other tests.
+ */
+public class CartesianProductIteratorTest {
 
-    @Test(timeout = 4000)
-    public void test6() throws Throwable {
-        Iterable<Locale.Category>[] iterableArray0 = (Iterable<Locale.Category>[]) Array.newInstance(Iterable.class, 0);
-        CartesianProductIterator<Locale.Category> cartesianProductIterator0 = new CartesianProductIterator<Locale.Category>(iterableArray0);
-        // Undeclared exception!
-        try {
-            cartesianProductIterator0.remove();
-            fail("Expecting exception: UnsupportedOperationException");
-        } catch (UnsupportedOperationException e) {
-            //
-            // remove
-            //
-            verifyException("org.apache.commons.collections4.iterators.CartesianProductIterator", e);
-        }
+    /**
+     * Tests that calling the remove() method throws an UnsupportedOperationException,
+     * as specified in the iterator's contract.
+     */
+    @Test(expected = UnsupportedOperationException.class)
+    public void removeShouldThrowUnsupportedOperationException() {
+        // The iterator is created with no iterables. The behavior of remove()
+        // should be the same regardless of the input iterables.
+        final CartesianProductIterator<Object> iterator = new CartesianProductIterator<>();
+
+        // Action: Attempt to call the unsupported remove() operation.
+        // The @Test(expected=...) annotation will cause the test to pass
+        // if this line throws an UnsupportedOperationException.
+        iterator.remove();
     }
 }
