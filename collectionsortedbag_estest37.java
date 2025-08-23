def51804b6c01eb920dcb21087674b7c273b0e8c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:44:35 +0800

    Original processedAutoTestCases collectionsortedbag_estest37.java

---
+++ collectionsortedbag_estest37.java
@@ -0,0 +1,55 @@
+package org.apache.commons.collections4.bag;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.lang.reflect.Array;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.ConcurrentModificationException;
+import java.util.LinkedList;
+import java.util.Locale;
+import java.util.PriorityQueue;
+import java.util.Set;
+import org.apache.commons.collections4.Closure;
+import org.apache.commons.collections4.Predicate;
+import org.apache.commons.collections4.SortedBag;
+import org.apache.commons.collections4.Transformer;
+import org.apache.commons.collections4.functors.ComparatorPredicate;
+import org.apache.commons.collections4.functors.ConstantTransformer;
+import org.apache.commons.collections4.functors.EqualPredicate;
+import org.apache.commons.collections4.functors.ExceptionPredicate;
+import org.apache.commons.collections4.functors.ExceptionTransformer;
+import org.apache.commons.collections4.functors.IdentityPredicate;
+import org.apache.commons.collections4.functors.IfClosure;
+import org.apache.commons.collections4.functors.IfTransformer;
+import org.apache.commons.collections4.functors.InstanceofPredicate;
+import org.apache.commons.collections4.functors.InvokerTransformer;
+import org.apache.commons.collections4.functors.NOPClosure;
+import org.apache.commons.collections4.functors.NullPredicate;
+import org.apache.commons.collections4.functors.TransformedPredicate;
+import org.apache.commons.collections4.functors.TransformerPredicate;
+import org.apache.commons.collections4.functors.UniquePredicate;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class CollectionSortedBag_ESTestTest37 extends CollectionSortedBag_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test36() throws Throwable {
+        Comparator<Object> comparator0 = (Comparator<Object>) mock(Comparator.class, new ViolatedAssumptionAnswer());
+        doReturn((-1959), (-1959), (-1959), 0, 0).when(comparator0).compare(any(), any());
+        TreeBag<Object> treeBag0 = new TreeBag<Object>(comparator0);
+        IfClosure<Object> ifClosure0 = new IfClosure<Object>((Predicate<? super Object>) null, (Closure<? super Object>) null);
+        treeBag0.add((Object) ifClosure0);
+        CollectionSortedBag<Object> collectionSortedBag0 = new CollectionSortedBag<Object>(treeBag0);
+        treeBag0.add((Object) null);
+        HashBag<Object> hashBag0 = new HashBag<Object>((Iterable<?>) collectionSortedBag0);
+        boolean boolean0 = collectionSortedBag0.removeAll(hashBag0);
+        assertFalse(collectionSortedBag0.contains(0));
+        assertTrue(boolean0);
+    }
+}
