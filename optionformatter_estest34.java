fa6949cd13c044063bb374cf2de5ef6622de3cc2
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:06:29 +0800

    Original processedAutoTestCases optionformatter_estest34.java

---
+++ optionformatter_estest34.java
@@ -0,0 +1,24 @@
+package org.apache.commons.cli.help;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.function.BiFunction;
+import org.apache.commons.cli.Option;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class OptionFormatter_ESTestTest34 extends OptionFormatter_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test33() throws Throwable {
+        Option option0 = new Option("NO_ARGS_ALLOWED", "iRi[{-|Um");
+        OptionFormatter optionFormatter0 = OptionFormatter.from(option0);
+        OptionFormatter.Builder optionFormatter_Builder0 = new OptionFormatter.Builder(optionFormatter0);
+        OptionFormatter.Builder optionFormatter_Builder1 = optionFormatter_Builder0.setArgumentNameDelimiters("<EEE MMM dd HH:mm:ss zzz yyyy>", "usage: ");
+        assertSame(optionFormatter_Builder1, optionFormatter_Builder0);
+    }
+}
