ad6266e06b744a2d047b7b3ffa35491122751e27
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:51:22 +0800

    gemini processedAutoTestCases fixedordercomparator_estest16.java

--- fixedordercomparator_estest16.java
+++ fixedordercomparator_estest16.java
@@ -1,27 +1,26 @@
 package org.apache.commons.collections4.comparators;
 
+import static org.junit.Assert.assertFalse;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.function.Function;
-import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.PredicateTransformer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class FixedOrderComparator_ESTestTest16 extends FixedOrderComparator_ESTest_scaffolding {
+/**
+ * Unit tests for {@link FixedOrderComparator}.
+ */
+public class FixedOrderComparatorTest {
 
-    @Test(timeout = 4000)
-    public void test15() throws Throwable {
-        FixedOrderComparator<FixedOrderComparator.UnknownObjectBehavior> fixedOrderComparator0 = new FixedOrderComparator<FixedOrderComparator.UnknownObjectBehavior>();
-        boolean boolean0 = fixedOrderComparator0.isLocked();
-        assertFalse(boolean0);
+    /**
+     * Tests that a newly created comparator is not locked by default.
+     * A comparator only becomes locked after the first comparison is performed.
+     */
+    @Test
+    public void newComparatorShouldNotBeLocked() {
+        // Arrange: Create a new, empty comparator.
+        final FixedOrderComparator<Object> comparator = new FixedOrderComparator<>();
+
+        // Act: Check the lock status.
+        final boolean isLocked = comparator.isLocked();
+
+        // Assert: The comparator should not be locked.
+        assertFalse("A new comparator should not be locked upon instantiation", isLocked);
     }
 }
