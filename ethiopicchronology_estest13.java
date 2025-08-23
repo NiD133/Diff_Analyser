b22074a0dee6bddef51b189034762677aabbe096
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:49:43 +0800

    gemini processedAutoTestCases ethiopicchronology_estest13.java

--- ethiopicchronology_estest13.java
+++ ethiopicchronology_estest13.java
@@ -1,23 +1,45 @@
 package org.joda.time.chrono;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
 import org.joda.time.Chronology;
 import org.joda.time.DateTimeZone;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class EthiopicChronology_ESTestTest13 extends EthiopicChronology_ESTest_scaffolding {
+import static org.junit.Assert.assertFalse;
 
-    @Test(timeout = 4000)
-    public void test12() throws Throwable {
-        DateTimeZone dateTimeZone0 = DateTimeZone.forOffsetMillis((-1962));
-        EthiopicChronology ethiopicChronology0 = EthiopicChronology.getInstance(dateTimeZone0);
-        LenientChronology lenientChronology0 = LenientChronology.getInstance(ethiopicChronology0);
-        Object object0 = new Object();
-        EthiopicChronology ethiopicChronology1 = new EthiopicChronology(lenientChronology0, object0, 1);
-        assertFalse(ethiopicChronology1.equals((Object) ethiopicChronology0));
+/**
+ * Contains tests for the EthiopicChronology class, focusing on its object equality.
+ */
+public class EthiopicChronologyTest {
+
+    @Test
+    public void equals_returnsFalse_forInstancesWithDifferentInternalConfigurations() {
+        // This test verifies that two EthiopicChronology instances are not considered equal
+        // if they have different internal configurations, such as a different base
+        // chronology or minimum days in the first week.
+
+        // Arrange: Create a standard EthiopicChronology instance using the factory method.
+        // This instance will have default internal parameters.
+        DateTimeZone zone = DateTimeZone.forOffsetMillis(-1962);
+        EthiopicChronology standardInstance = EthiopicChronology.getInstance(zone);
+
+        // Arrange: Create a second, custom EthiopicChronology instance using its
+        // package-private constructor. This allows us to provide a different internal
+        // configuration for the purpose of this test.
+        Chronology baseForCustomInstance = LenientChronology.getInstance(standardInstance);
+        Object dummyParam = new Object(); // A non-null parameter for the constructor.
+        int minDaysInFirstWeek = 1;       // Different from the default of 4.
+
+        EthiopicChronology customInstance = new EthiopicChronology(
+                baseForCustomInstance,
+                dummyParam,
+                minDaysInFirstWeek
+        );
+
+        // Act & Assert: The two instances should not be equal because their underlying
+        // configurations (base chronology, params, minDaysInFirstWeek) are different.
+        assertFalse(
+            "Instances with different configurations should not be equal",
+            customInstance.equals(standardInstance)
+        );
     }
 }
