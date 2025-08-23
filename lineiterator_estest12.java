1bd60d9ab6d18af68bebd91e43adf0c7b16965c6
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:00:18 +0800

    gemini processedAutoTestCases lineiterator_estest12.java

--- lineiterator_estest12.java
+++ lineiterator_estest12.java
@@ -1,31 +1,27 @@
 package org.apache.commons.io;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.BufferedReader;
-import java.io.Reader;
+
 import java.io.StringReader;
 import java.util.NoSuchElementException;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class LineIterator_ESTestTest12 extends LineIterator_ESTest_scaffolding {
+/**
+ * Unit tests for {@link LineIterator}.
+ */
+public class LineIteratorTest {
 
-    @Test(timeout = 4000)
-    public void test11() throws Throwable {
-        StringReader stringReader0 = new StringReader("");
-        LineIterator lineIterator0 = new LineIterator(stringReader0);
-        // Undeclared exception!
-        try {
-            lineIterator0.nextLine();
-            fail("Expecting exception: NoSuchElementException");
-        } catch (NoSuchElementException e) {
-            //
-            // No more lines
-            //
-            verifyException("org.apache.commons.io.LineIterator", e);
-        }
+    /**
+     * Verifies that calling next() on an iterator created from an empty reader
+     * correctly throws a NoSuchElementException, as there are no lines to return.
+     */
+    @Test(expected = NoSuchElementException.class)
+    public void nextShouldThrowNoSuchElementExceptionForEmptyReader() {
+        // Arrange: Create a LineIterator with an empty source.
+        final StringReader emptyReader = new StringReader("");
+        final LineIterator iterator = new LineIterator(emptyReader);
+
+        // Act & Assert: Attempting to get the next line should throw the expected exception.
+        // The @Test(expected=...) annotation handles the assertion.
+        iterator.next();
     }
 }
