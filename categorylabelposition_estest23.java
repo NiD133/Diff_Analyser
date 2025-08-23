6af30d05b9a585856b1ef954c39b4de47b947844
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:41:53 +0800

    Original processedAutoTestCases categorylabelposition_estest23.java

---
+++ categorylabelposition_estest23.java
@@ -0,0 +1,30 @@
+package org.jfree.chart.axis;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.jfree.chart.api.RectangleAnchor;
+import org.jfree.chart.text.TextAnchor;
+import org.jfree.chart.text.TextBlockAnchor;
+import org.junit.runner.RunWith;
+
+public class CategoryLabelPosition_ESTestTest23 extends CategoryLabelPosition_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test22() throws Throwable {
+        RectangleAnchor rectangleAnchor0 = RectangleAnchor.CENTER;
+        TextBlockAnchor textBlockAnchor0 = TextBlockAnchor.CENTER;
+        TextAnchor textAnchor0 = TextAnchor.TOP_CENTER;
+        CategoryLabelWidthType categoryLabelWidthType0 = CategoryLabelWidthType.RANGE;
+        CategoryLabelPosition categoryLabelPosition0 = new CategoryLabelPosition(rectangleAnchor0, textBlockAnchor0, textAnchor0, 1.0, categoryLabelWidthType0, 0.0F);
+        RectangleAnchor rectangleAnchor1 = categoryLabelPosition0.getCategoryAnchor();
+        CategoryLabelPosition categoryLabelPosition1 = new CategoryLabelPosition(rectangleAnchor1, textBlockAnchor0, categoryLabelWidthType0, 428.78F);
+        CategoryLabelPosition categoryLabelPosition2 = new CategoryLabelPosition(rectangleAnchor0, textBlockAnchor0);
+        boolean boolean0 = categoryLabelPosition1.equals(categoryLabelPosition2);
+        assertEquals(0.0, categoryLabelPosition2.getAngle(), 0.01);
+        assertEquals(0.95F, categoryLabelPosition2.getWidthRatio(), 0.01F);
+        assertFalse(boolean0);
+    }
+}
