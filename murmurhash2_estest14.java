07118151ee640a9486afbb1c9520b89277c14af4
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:04:22 +0800

    gemini processedAutoTestCases murmurhash2_estest14.java

--- murmurhash2_estest14.java
+++ murmurhash2_estest14.java
@@ -1,21 +1,25 @@
 package org.apache.commons.codec.digest;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class MurmurHash2_ESTestTest14 extends MurmurHash2_ESTest_scaffolding {
+/**
+ * Tests for {@link MurmurHash2}.
+ */
+public class MurmurHash2Test {
 
-    @Test(timeout = 4000)
-    public void test13() throws Throwable {
-        // Undeclared exception!
-        try {
-            MurmurHash2.hash64(",;9b|Qi Zv", (-2432), (-2432));
-            fail("Expecting exception: StringIndexOutOfBoundsException");
-        } catch (StringIndexOutOfBoundsException e) {
-        }
+    /**
+     * Tests that hash64 throws a StringIndexOutOfBoundsException when called with a negative 'from' index.
+     * This is the expected behavior, as the method internally calls String.substring(), which enforces
+     * non-negative indices.
+     */
+    @Test(expected = StringIndexOutOfBoundsException.class)
+    public void testHash64StringWithNegativeFromIndexShouldThrowException() {
+        // Define test inputs with clear names
+        final String text = "any string";
+        final int negativeFromIndex = -1;
+        final int anyLength = 5;
+
+        // This call is expected to throw StringIndexOutOfBoundsException
+        MurmurHash2.hash64(text, negativeFromIndex, anyLength);
     }
 }
