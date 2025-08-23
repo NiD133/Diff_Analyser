473bd33c43ba9b1f0a81bc62a1735a855e195da2
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:01:27 +0800

    Original processedAutoTestCases logfactory_estest2.java

---
+++ logfactory_estest2.java
@@ -0,0 +1,26 @@
+package org.apache.ibatis.logging;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.apache.ibatis.logging.commons.JakartaCommonsLoggingImpl;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class LogFactory_ESTestTest2 extends LogFactory_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test01() throws Throwable {
+        // Undeclared exception!
+        try {
+            LogFactory.getLog((Class<?>) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.apache.ibatis.logging.LogFactory", e);
+        }
+    }
+}
