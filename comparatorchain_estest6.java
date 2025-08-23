f5c97f3597bf243476f2f5083dd8b172ae085493
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:45:04 +0800

    Original processedAutoTestCases comparatorchain_estest6.java

---
+++ comparatorchain_estest6.java
@@ -0,0 +1,42 @@
+package org.apache.commons.collections4.comparators;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.nio.ByteBuffer;
+import java.nio.LongBuffer;
+import java.util.BitSet;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.ToIntFunction;
+import java.util.function.ToLongFunction;
+import org.apache.commons.collections4.Closure;
+import org.apache.commons.collections4.functors.ClosureTransformer;
+import org.apache.commons.collections4.functors.ComparatorPredicate;
+import org.apache.commons.collections4.functors.ExceptionClosure;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class ComparatorChain_ESTestTest6 extends ComparatorChain_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test05() throws Throwable {
+        LinkedList<Comparator<ComparatorPredicate.Criterion>> linkedList0 = new LinkedList<Comparator<ComparatorPredicate.Criterion>>();
+        ComparatorChain<ComparatorPredicate.Criterion> comparatorChain0 = new ComparatorChain<ComparatorPredicate.Criterion>(linkedList0);
+        // Undeclared exception!
+        try {
+            comparatorChain0.setReverseSort((-2145));
+            fail("Expecting exception: IndexOutOfBoundsException");
+        } catch (IndexOutOfBoundsException e) {
+            //
+            // bitIndex < 0: -2145
+            //
+            verifyException("java.util.BitSet", e);
+        }
+    }
+}
