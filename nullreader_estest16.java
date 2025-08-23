ab5470acd402b9fe4c1d8aeca83bc79bb58cfd1f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:04:48 +0800

    Original processedAutoTestCases nullreader_estest16.java

---
+++ nullreader_estest16.java
@@ -0,0 +1,21 @@
+package org.apache.commons.io.input;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.EOFException;
+import java.io.IOException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class NullReader_ESTestTest16 extends NullReader_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test15() throws Throwable {
+        NullReader nullReader0 = new NullReader(959L, true, true);
+        long long0 = nullReader0.skip(0L);
+        assertTrue(nullReader0.markSupported());
+        assertEquals(0L, long0);
+    }
+}
