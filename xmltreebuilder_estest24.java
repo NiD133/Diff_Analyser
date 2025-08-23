e006fd1c726d1c620a57053bff1f802635cf6c93
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:28:00 +0800

    Original processedAutoTestCases xmltreebuilder_estest24.java

---
+++ xmltreebuilder_estest24.java
@@ -0,0 +1,41 @@
+package org.jsoup.parser;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.PipedReader;
+import java.io.PipedWriter;
+import java.io.Reader;
+import java.io.StringReader;
+import java.io.UncheckedIOException;
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.jsoup.nodes.Attributes;
+import org.jsoup.nodes.CDataNode;
+import org.jsoup.nodes.Comment;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.LeafNode;
+import org.jsoup.select.Elements;
+import org.junit.runner.RunWith;
+
+public class XmlTreeBuilder_ESTestTest24 extends XmlTreeBuilder_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test23() throws Throwable {
+        XmlTreeBuilder xmlTreeBuilder0 = new XmlTreeBuilder();
+        PipedReader pipedReader0 = new PipedReader();
+        // Undeclared exception!
+        try {
+            xmlTreeBuilder0.parse(pipedReader0, "kxrewut");
+            fail("Expecting exception: UncheckedIOException");
+        } catch (UncheckedIOException e) {
+            //
+            // java.io.IOException: Pipe not connected
+            //
+            verifyException("org.jsoup.parser.CharacterReader", e);
+        }
+    }
+}
