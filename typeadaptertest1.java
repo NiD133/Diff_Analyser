19a510107b0b4575ad0959df7a4cc9e3ebe3ad37
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:35:08 +0800

    Original processedManualTestCases typeadaptertest1.java

---
+++ typeadaptertest1.java
@@ -0,0 +1,50 @@
+package com.google.gson;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertThrows;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+import java.io.IOException;
+import java.io.StringReader;
+import org.junit.Test;
+
+public class TypeAdapterTestTest1 {
+
+    private static final TypeAdapter<String> assertionErrorAdapter = new TypeAdapter<>() {
+
+        @Override
+        public void write(JsonWriter out, String value) {
+            throw new AssertionError("unexpected call");
+        }
+
+        @Override
+        public String read(JsonReader in) {
+            throw new AssertionError("unexpected call");
+        }
+
+        @Override
+        public String toString() {
+            return "assertionErrorAdapter";
+        }
+    };
+
+    private static final TypeAdapter<String> adapter = new TypeAdapter<>() {
+
+        @Override
+        public void write(JsonWriter out, String value) throws IOException {
+            out.value(value);
+        }
+
+        @Override
+        public String read(JsonReader in) throws IOException {
+            return in.nextString();
+        }
+    };
+
+    @Test
+    public void testNullSafe() throws IOException {
+        TypeAdapter<String> adapter = assertionErrorAdapter.nullSafe();
+        assertThat(adapter.toJson(null)).isEqualTo("null");
+        assertThat(adapter.fromJson("null")).isNull();
+    }
+}
