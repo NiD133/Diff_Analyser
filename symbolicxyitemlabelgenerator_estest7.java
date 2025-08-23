f9bda9a4eacd0d8d739691d9e284f6a0c4f470ef
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:19:21 +0800

    gemini processedAutoTestCases symbolicxyitemlabelgenerator_estest7.java

--- symbolicxyitemlabelgenerator_estest7.java
+++ symbolicxyitemlabelgenerator_estest7.java
@@ -1,27 +1,25 @@
 package org.jfree.chart.labels;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.time.chrono.ChronoLocalDate;
-import java.util.Date;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.util.MockDate;
-import org.jfree.chart.date.SerialDate;
-import org.jfree.data.time.DynamicTimeSeriesCollection;
-import org.jfree.data.time.TimeSeriesCollection;
-import org.jfree.data.xy.DefaultHighLowDataset;
-import org.jfree.data.xy.DefaultTableXYDataset;
-import org.jfree.data.xy.XYDataset;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertTrue;
 
-public class SymbolicXYItemLabelGenerator_ESTestTest7 extends SymbolicXYItemLabelGenerator_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link SymbolicXYItemLabelGenerator} class.
+ */
+public class SymbolicXYItemLabelGeneratorTest {
 
-    @Test(timeout = 4000)
-    public void test6() throws Throwable {
-        SymbolicXYItemLabelGenerator symbolicXYItemLabelGenerator0 = new SymbolicXYItemLabelGenerator();
-        boolean boolean0 = symbolicXYItemLabelGenerator0.equals(symbolicXYItemLabelGenerator0);
-        assertTrue(boolean0);
+    /**
+     * Verifies the reflexive property of the equals() method.
+     * An object must be equal to itself.
+     */
+    @Test
+    public void equals_onSameInstance_shouldReturnTrue() {
+        // Arrange
+        SymbolicXYItemLabelGenerator generator = new SymbolicXYItemLabelGenerator();
+
+        // Act & Assert
+        // According to the Java contract for equals(), an object must equal itself.
+        boolean isEqual = generator.equals(generator);
+        assertTrue("An instance should be equal to itself.", isEqual);
     }
 }
