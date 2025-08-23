08e69c7b79f766a094490899a23703c820b1f963
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:16:44 +0800

    Original processedAutoTestCases simpletextextractionstrategy_estest4.java

---
+++ simpletextextractionstrategy_estest4.java
@@ -0,0 +1,48 @@
+package com.itextpdf.text.pdf.parser;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.itextpdf.text.pdf.CMapAwareDocumentFont;
+import com.itextpdf.text.pdf.PdfAction;
+import com.itextpdf.text.pdf.PdfDate;
+import com.itextpdf.text.pdf.PdfDictionary;
+import com.itextpdf.text.pdf.PdfIndirectReference;
+import com.itextpdf.text.pdf.PdfSigLockDictionary;
+import com.itextpdf.text.pdf.PdfString;
+import java.nio.CharBuffer;
+import java.nio.charset.IllegalCharsetNameException;
+import java.nio.charset.UnsupportedCharsetException;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import javax.swing.text.Segment;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class SimpleTextExtractionStrategy_ESTestTest4 extends SimpleTextExtractionStrategy_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test03() throws Throwable {
+        SimpleTextExtractionStrategy simpleTextExtractionStrategy0 = new SimpleTextExtractionStrategy();
+        GraphicsState graphicsState0 = new GraphicsState();
+        Matrix matrix0 = new Matrix();
+        LinkedHashSet<MarkedContentInfo> linkedHashSet0 = new LinkedHashSet<MarkedContentInfo>();
+        PdfAction pdfAction0 = PdfAction.createLaunch("", "", "PDF", "");
+        CMapAwareDocumentFont cMapAwareDocumentFont0 = new CMapAwareDocumentFont(pdfAction0);
+        graphicsState0.font = cMapAwareDocumentFont0;
+        PdfString pdfString0 = new PdfString("Identity-H", ">|");
+        TextRenderInfo textRenderInfo0 = new TextRenderInfo(pdfString0, graphicsState0, matrix0, linkedHashSet0);
+        // Undeclared exception!
+        try {
+            simpleTextExtractionStrategy0.renderText(textRenderInfo0);
+            fail("Expecting exception: IllegalCharsetNameException");
+        } catch (IllegalCharsetNameException e) {
+            //
+            // >|
+            //
+            verifyException("java.nio.charset.Charset", e);
+        }
+    }
+}
