f25ee587239d3958ff2bba8b328ec675d3837bd3
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:26:21 +0800

    gemini processedManualTestCases caseformattest16.java

--- caseformattest16.java
+++ caseformattest16.java
@@ -1,25 +1,49 @@
 package com.google.common.base;
 
 import static com.google.common.base.CaseFormat.LOWER_CAMEL;
-import static com.google.common.base.CaseFormat.LOWER_HYPHEN;
-import static com.google.common.base.CaseFormat.LOWER_UNDERSCORE;
 import static com.google.common.base.CaseFormat.UPPER_CAMEL;
-import static com.google.common.base.CaseFormat.UPPER_UNDERSCORE;
 import static com.google.common.truth.Truth.assertThat;
-import static com.google.common.truth.Truth.assertWithMessage;
-import com.google.common.annotations.GwtCompatible;
-import com.google.common.annotations.GwtIncompatible;
-import com.google.common.annotations.J2ktIncompatible;
-import com.google.common.testing.NullPointerTester;
-import com.google.common.testing.SerializableTester;
+
 import junit.framework.TestCase;
-import org.jspecify.annotations.NullUnmarked;
 
-public class CaseFormatTestTest16 extends TestCase {
+/**
+ * Tests for conversions from {@link CaseFormat#LOWER_CAMEL} to {@link CaseFormat#UPPER_CAMEL}.
+ */
+public class CaseFormatTest extends TestCase {
 
-    public void testLowerCamelToUpperCamel() {
-        assertThat(LOWER_CAMEL.to(UPPER_CAMEL, "foo")).isEqualTo("Foo");
-        assertThat(LOWER_CAMEL.to(UPPER_CAMEL, "fooBar")).isEqualTo("FooBar");
-        assertThat(LOWER_CAMEL.to(UPPER_CAMEL, "hTTP")).isEqualTo("HTTP");
-    }
+  public void testLowerCamelToUpperCamel() {
+    // Test case 1: A single word should just have its first letter capitalized.
+    // Arrange
+    String singleWordInput = "foo";
+    String expectedSingleWordOutput = "Foo";
+
+    // Act
+    String actualOutput = LOWER_CAMEL.to(UPPER_CAMEL, singleWordInput);
+
+    // Assert
+    assertThat(actualOutput).isEqualTo(expectedSingleWordOutput);
+
+    // Test case 2: A standard multi-word lowerCamel string.
+    // Arrange
+    String multiWordInput = "fooBar";
+    String expectedMultiWordOutput = "FooBar";
+
+    // Act
+    actualOutput = LOWER_CAMEL.to(UPPER_CAMEL, multiWordInput);
+
+    // Assert
+    assertThat(actualOutput).isEqualTo(expectedMultiWordOutput);
+
+    // Test case 3: An edge case with an acronym-like part.
+    // The converter should treat "h" and "TTP" as separate words.
+    // Arrange
+    String acronymInput = "hTTP";
+    String expectedAcronymOutput = "HTTP";
+
+    // Act
+    actualOutput = LOWER_CAMEL.to(UPPER_CAMEL, acronymInput);
+
+    // Assert
+    assertThat(actualOutput).isEqualTo(expectedAcronymOutput);
+  }
 }
