7f297d9f2d8af77cf51f4c8063152e86038fe619
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:40:03 +0800

    Original processedAutoTestCases bufferedlinestring_estest22.java

---
+++ bufferedlinestring_estest22.java
@@ -0,0 +1,39 @@
+package org.locationtech.spatial4j.shape.impl;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.HashMap;
+import java.util.LinkedList;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+import org.locationtech.spatial4j.context.SpatialContext;
+import org.locationtech.spatial4j.context.SpatialContextFactory;
+import org.locationtech.spatial4j.distance.GeodesicSphereDistCalc;
+import org.locationtech.spatial4j.shape.Point;
+import org.locationtech.spatial4j.shape.Rectangle;
+import org.locationtech.spatial4j.shape.Shape;
+import org.locationtech.spatial4j.shape.ShapeCollection;
+import org.locationtech.spatial4j.shape.SpatialRelation;
+
+public class BufferedLineString_ESTestTest22 extends BufferedLineString_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test21() throws Throwable {
+        LinkedList<Point> linkedList0 = new LinkedList<Point>();
+        SpatialContext spatialContext0 = SpatialContext.GEO;
+        PointImpl pointImpl0 = new PointImpl((-1.0), (-1.0), spatialContext0);
+        linkedList0.add((Point) pointImpl0);
+        BufferedLineString bufferedLineString0 = null;
+        try {
+            bufferedLineString0 = new BufferedLineString(linkedList0, (-1.0), true, spatialContext0);
+            fail("Expecting exception: RuntimeException");
+        } catch (RuntimeException e) {
+            //
+            // maxY must be >= minY: 0.0 to -2.0
+            //
+            verifyException("org.locationtech.spatial4j.shape.impl.ShapeFactoryImpl", e);
+        }
+    }
+}
