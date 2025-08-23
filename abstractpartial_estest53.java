1739ee37b920e7102a231e4df9761d0820364261
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:36:51 +0800

    Original processedAutoTestCases abstractpartial_estest53.java

---
+++ abstractpartial_estest53.java
@@ -0,0 +1,46 @@
+package org.joda.time.base;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.Date;
+import java.util.Locale;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.util.MockGregorianCalendar;
+import org.joda.time.Chronology;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeField;
+import org.joda.time.DateTimeFieldType;
+import org.joda.time.DateTimeZone;
+import org.joda.time.DurationFieldType;
+import org.joda.time.Instant;
+import org.joda.time.LocalDate;
+import org.joda.time.LocalDateTime;
+import org.joda.time.LocalTime;
+import org.joda.time.MonthDay;
+import org.joda.time.Partial;
+import org.joda.time.ReadablePartial;
+import org.joda.time.Weeks;
+import org.joda.time.YearMonth;
+import org.joda.time.Years;
+import org.joda.time.chrono.CopticChronology;
+import org.joda.time.chrono.GJChronology;
+import org.joda.time.chrono.GregorianChronology;
+import org.joda.time.chrono.IslamicChronology;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.DateTimeParser;
+import org.joda.time.format.DateTimePrinter;
+import org.junit.runner.RunWith;
+
+public class AbstractPartial_ESTestTest53 extends AbstractPartial_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test52() throws Throwable {
+        LocalDateTime localDateTime0 = LocalDateTime.now();
+        int[] intArray0 = localDateTime0.getValues();
+        assertArrayEquals(new int[] { 2014, 2, 14, 73281320 }, intArray0);
+    }
+}
