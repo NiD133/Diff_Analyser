0338f757a3544524c71e0fadd92497d7eeda6187
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:43:16 +0800

    Original processedAutoTestCases circularbytebuffer_estest11.java

---
+++ circularbytebuffer_estest11.java
@@ -0,0 +1,27 @@
+package org.apache.commons.io.input.buffer;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class CircularByteBuffer_ESTestTest11 extends CircularByteBuffer_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test10() throws Throwable {
+        CircularByteBuffer circularByteBuffer0 = new CircularByteBuffer(0);
+        byte[] byteArray0 = new byte[3];
+        // Undeclared exception!
+        try {
+            circularByteBuffer0.add(byteArray0, 1886, 0);
+            fail("Expecting exception: IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //
+            // Illegal offset: 1886
+            //
+            verifyException("org.apache.commons.io.input.buffer.CircularByteBuffer", e);
+        }
+    }
+}
