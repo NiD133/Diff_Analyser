2114eb9c8055fb2354abcfc99dce707256f43c0a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:41:53 +0800

    Original processedAutoTestCases categorylabelposition_estest22.java

---
+++ categorylabelposition_estest22.java
@@ -0,0 +1,22 @@
+package org.jfree.chart.axis;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.jfree.chart.api.RectangleAnchor;
+import org.jfree.chart.text.TextAnchor;
+import org.jfree.chart.text.TextBlockAnchor;
+import org.junit.runner.RunWith;
+
+public class CategoryLabelPosition_ESTestTest22 extends CategoryLabelPosition_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test21() throws Throwable {
+        CategoryLabelPosition categoryLabelPosition0 = new CategoryLabelPosition();
+        categoryLabelPosition0.hashCode();
+        assertEquals(0.0, categoryLabelPosition0.getAngle(), 0.01);
+        assertEquals(0.95F, categoryLabelPosition0.getWidthRatio(), 0.01F);
+    }
+}
