26c09799ce7edab41809df9c591386431b9d3609
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:38:53 +0800

    Original processedAutoTestCases basicnodeset_estest9.java

---
+++ basicnodeset_estest9.java
@@ -0,0 +1,20 @@
+package org.apache.commons.jxpath;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.List;
+import org.apache.commons.jxpath.ri.QName;
+import org.apache.commons.jxpath.ri.model.VariablePointer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class BasicNodeSet_ESTestTest9 extends BasicNodeSet_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test08() throws Throwable {
+        BasicNodeSet basicNodeSet0 = new BasicNodeSet();
+        basicNodeSet0.remove((Pointer) null);
+    }
+}
