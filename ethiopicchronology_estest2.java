a6b9e2e42983b5359cfc3a309506afb826b17867
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:49:45 +0800

    gemini processedAutoTestCases ethiopicchronology_estest2.java

--- ethiopicchronology_estest2.java
+++ ethiopicchronology_estest2.java
@@ -1,21 +1,21 @@
 package org.joda.time.chrono;
 
+import org.joda.time.DateTimeConstants;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.joda.time.Chronology;
-import org.joda.time.DateTimeZone;
-import org.junit.runner.RunWith;
 
+import static org.junit.Assert.assertEquals;
+
+// The original test class structure is preserved.
 public class EthiopicChronology_ESTestTest2 extends EthiopicChronology_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test01() throws Throwable {
-        EthiopicChronology ethiopicChronology0 = EthiopicChronology.getInstanceUTC();
-        AssembledChronology.Fields assembledChronology_Fields0 = new AssembledChronology.Fields();
-        ethiopicChronology0.assemble(assembledChronology_Fields0);
-        assertEquals(1, EthiopicChronology.EE);
+    /**
+     * Tests that the Ethiopic Era (EE) constant is correctly defined as
+     * equivalent to the Common Era (CE).
+     */
+    @Test
+    public void testEEConstantIsEqualToCE() {
+        // The EthiopicChronology.EE constant is documented to be equivalent to CE.
+        // This test verifies that its value matches DateTimeConstants.CE.
+        assertEquals(DateTimeConstants.CE, EthiopicChronology.EE);
     }
 }
