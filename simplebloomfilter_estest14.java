62ab2136ca95896217d7aaa369023bf7f0455d84
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:16:03 +0800

    Original processedAutoTestCases simplebloomfilter_estest14.java

---
+++ simplebloomfilter_estest14.java
@@ -0,0 +1,28 @@
+package org.apache.commons.collections4.bloomfilter;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.function.IntPredicate;
+import java.util.function.LongPredicate;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class SimpleBloomFilter_ESTestTest14 extends SimpleBloomFilter_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test13() throws Throwable {
+        Shape shape0 = Shape.fromNM(38, 38);
+        SimpleBloomFilter simpleBloomFilter0 = new SimpleBloomFilter(shape0);
+        EnhancedDoubleHasher enhancedDoubleHasher0 = new EnhancedDoubleHasher(38, 38);
+        boolean boolean0 = simpleBloomFilter0.merge((Hasher) enhancedDoubleHasher0);
+        int int0 = simpleBloomFilter0.cardinality();
+        assertEquals(1, int0);
+        boolean boolean1 = simpleBloomFilter0.isEmpty();
+        assertFalse(boolean1 == boolean0);
+        assertFalse(boolean1);
+    }
+}
