fbbf1b1700cfff1926e9022cce105b9334b4f14a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:24:55 +0800

    Original processedManualTestCases arraytypehandlertest7.java

---
+++ arraytypehandlertest7.java
@@ -0,0 +1,33 @@
+package org.apache.ibatis.type;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import java.sql.Array;
+import java.sql.Connection;
+import java.sql.Types;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+public class ArrayTypeHandlerTestTest7 extends BaseTypeHandlerTest {
+
+    private static final TypeHandler<Object> TYPE_HANDLER = new ArrayTypeHandler();
+
+    @Mock
+    Array mockArray;
+
+    @Override
+    @Test
+    public void shouldGetResultFromResultSetByPosition() throws Exception {
+        when(rs.getArray(1)).thenReturn(mockArray);
+        String[] stringArray = { "a", "b" };
+        when(mockArray.getArray()).thenReturn(stringArray);
+        assertEquals(stringArray, TYPE_HANDLER.getResult(rs, 1));
+        verify(mockArray).free();
+    }
+}
