89fc8465b07bfd90c076f94ba844a5816d12fc01
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:07:29 +0800

    Original processedAutoTestCases patternoptionbuilder_estest11.java

---
+++ patternoptionbuilder_estest11.java
@@ -0,0 +1,17 @@
+package org.apache.commons.cli;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class PatternOptionBuilder_ESTestTest11 extends PatternOptionBuilder_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test10() throws Throwable {
+        boolean boolean0 = PatternOptionBuilder.isValueCode('*');
+        assertTrue(boolean0);
+    }
+}
