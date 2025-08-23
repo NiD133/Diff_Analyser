060d4c665d83a305a5dea53625f76e4ff955f2fa
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:50:04 +0800

    gemini processedAutoTestCases extrafieldutils_estest4.java

--- extrafieldutils_estest4.java
+++ extrafieldutils_estest4.java
@@ -1,28 +1,35 @@
 package org.apache.commons.compress.archivers.zip;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
 import java.util.zip.ZipException;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class ExtraFieldUtils_ESTestTest4 extends ExtraFieldUtils_ESTest_scaffolding {
+/**
+ * Tests for the {@link ExtraFieldUtils.UnparseableExtraField} class.
+ */
+public class ExtraFieldUtilsTest {
 
-    @Test(timeout = 4000)
-    public void test03() throws Throwable {
-        byte[] byteArray0 = new byte[0];
-        ExtraFieldUtils.UnparseableExtraField extraFieldUtils_UnparseableExtraField0 = ExtraFieldUtils.UnparseableExtraField.READ;
-        // Undeclared exception!
-        try {
-            extraFieldUtils_UnparseableExtraField0.onUnparseableExtraField(byteArray0, 3722, 0, true, 0);
-            fail("Expecting exception: ArrayIndexOutOfBoundsException");
-        } catch (ArrayIndexOutOfBoundsException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("java.util.Arrays", e);
-        }
+    /**
+     * Tests that the onUnparseableExtraField method throws an
+     * ArrayIndexOutOfBoundsException when the provided offset is outside
+     * the bounds of the data array.
+     */
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void onUnparseableExtraFieldWithInvalidOffsetShouldThrowException() throws ZipException {
+        // Arrange
+        // The behavior to test is READ, which attempts to access the data array.
+        final ExtraFieldUtils.UnparseableExtraField readBehavior = ExtraFieldUtils.UnparseableExtraField.READ;
+        final byte[] data = new byte[0];
+
+        // An offset of 1 is invalid for an empty array.
+        final int offsetOutOfBounds = 1;
+        final int length = 0;
+        final boolean isLocal = true;
+        final int claimedLength = 0;
+
+        // Act & Assert
+        // This call is expected to throw an ArrayIndexOutOfBoundsException because the
+        // offset is greater than the array's length. The assertion is handled by the
+        // @Test(expected=...) annotation.
+        readBehavior.onUnparseableExtraField(data, offsetOutOfBounds, length, isLocal, claimedLength);
     }
 }
