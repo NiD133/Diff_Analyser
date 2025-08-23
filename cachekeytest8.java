b78cb064f8ce157e7ce7ff6e54df6a198c579679
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:26:14 +0800

    Original processedManualTestCases cachekeytest8.java

---
+++ cachekeytest8.java
@@ -0,0 +1,30 @@
+package org.apache.ibatis.cache;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.NotSerializableException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Date;
+import org.junit.jupiter.api.Test;
+
+public class CacheKeyTestTest8 {
+
+    private static <T> T serialize(T object) throws Exception {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        new ObjectOutputStream(baos).writeObject(object);
+        ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+        return (T) new ObjectInputStream(bais).readObject();
+    }
+
+    @Test
+    void shouldDemonstrateClonedNullCacheKeysAreEqual() throws Exception {
+        CacheKey cacheKey = CacheKey.NULL_CACHE_KEY;
+        CacheKey clonedCacheKey = cacheKey.clone();
+        assertEquals(cacheKey, clonedCacheKey);
+        assertEquals(cacheKey.hashCode(), clonedCacheKey.hashCode());
+    }
+}
