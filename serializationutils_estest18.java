c53e466b948f834e576d2e596a7cf3b375158606
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:14:43 +0800

    Original processedAutoTestCases serializationutils_estest18.java

---
+++ serializationutils_estest18.java
@@ -0,0 +1,29 @@
+package org.apache.commons.lang3;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.io.SequenceInputStream;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Locale;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.evosuite.runtime.mock.java.io.MockFileInputStream;
+import org.evosuite.runtime.mock.java.io.MockPrintStream;
+import org.junit.runner.RunWith;
+
+public class SerializationUtils_ESTestTest18 extends SerializationUtils_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test17() throws Throwable {
+        SerializationUtils serializationUtils0 = new SerializationUtils();
+    }
+}
