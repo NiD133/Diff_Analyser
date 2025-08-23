016661952de15f5532cc15ad9072285ff6d675b3
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:50:22 +0800

    Original processedAutoTestCases fieldutils_estest38.java

---
+++ fieldutils_estest38.java
@@ -0,0 +1,31 @@
+package org.joda.time.field;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.math.RoundingMode;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.joda.time.DateTimeField;
+import org.joda.time.DateTimeFieldType;
+import org.joda.time.DateTimeZone;
+import org.joda.time.chrono.IslamicChronology;
+import org.joda.time.chrono.ZonedChronology;
+import org.junit.runner.RunWith;
+
+public class FieldUtils_ESTestTest38 extends FieldUtils_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test37() throws Throwable {
+        // Undeclared exception!
+        try {
+            FieldUtils.safeToInt((-2147483671L));
+            fail("Expecting exception: ArithmeticException");
+        } catch (ArithmeticException e) {
+            //
+            // Value cannot fit in an int: -2147483671
+            //
+            verifyException("org.joda.time.field.FieldUtils", e);
+        }
+    }
+}
