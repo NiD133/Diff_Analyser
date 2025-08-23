697261238ddfb920959665ecd27cc6f5a8a4fc98
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:01:45 +0800

    Original processedAutoTestCases loopinglistiterator_estest7.java

---
+++ loopinglistiterator_estest7.java
@@ -0,0 +1,35 @@
+package org.apache.commons.collections4.iterators;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.ConcurrentModificationException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import org.apache.commons.collections4.Closure;
+import org.apache.commons.collections4.functors.InstanceofPredicate;
+import org.apache.commons.collections4.functors.UniquePredicate;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class LoopingListIterator_ESTestTest7 extends LoopingListIterator_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test06() throws Throwable {
+        LinkedList<Object> linkedList0 = new LinkedList<Object>();
+        LoopingListIterator<Object> loopingListIterator0 = new LoopingListIterator<Object>(linkedList0);
+        linkedList0.add((Object) loopingListIterator0);
+        // Undeclared exception!
+        try {
+            loopingListIterator0.remove();
+            fail("Expecting exception: ConcurrentModificationException");
+        } catch (ConcurrentModificationException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("java.util.LinkedList$ListItr", e);
+        }
+    }
+}
