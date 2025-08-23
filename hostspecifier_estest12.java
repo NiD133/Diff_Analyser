a94a034ef227e10320bd8eb32e8e6c875bb61b40
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:53:20 +0800

    Original processedAutoTestCases hostspecifier_estest12.java

---
+++ hostspecifier_estest12.java
@@ -0,0 +1,19 @@
+package com.google.common.net;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.text.ParseException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class HostSpecifier_ESTestTest12 extends HostSpecifier_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test11() throws Throwable {
+        HostSpecifier hostSpecifier0 = HostSpecifier.from("127.0.0.1");
+        boolean boolean0 = hostSpecifier0.equals(hostSpecifier0);
+        assertTrue(boolean0);
+    }
+}
