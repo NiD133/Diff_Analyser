a916076025383ac7d8bc9d083dec87a77e12fb0b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:47:13 +0800

    Original processedAutoTestCases defaultflowdataset_estest17.java

---
+++ defaultflowdataset_estest17.java
@@ -0,0 +1,32 @@
+package org.jfree.data.flow;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.time.chrono.HijrahEra;
+import java.util.List;
+import java.util.Set;
+import javax.swing.Icon;
+import javax.swing.JLayeredPane;
+import javax.swing.JRadioButtonMenuItem;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class DefaultFlowDataset_ESTestTest17 extends DefaultFlowDataset_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test16() throws Throwable {
+        DefaultFlowDataset<Integer> defaultFlowDataset0 = new DefaultFlowDataset<Integer>();
+        // Undeclared exception!
+        try {
+            defaultFlowDataset0.getInFlows((NodeKey) null);
+            fail("Expecting exception: IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //
+            // Null 'nodeKey' argument.
+            //
+            verifyException("org.jfree.chart.internal.Args", e);
+        }
+    }
+}
