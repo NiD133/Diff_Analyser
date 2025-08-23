a1a1f18bc03dde047806ee419ee4e5aef27c8778
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:43:40 +0800

    gemini processedAutoTestCases clusteredxybarrenderer_estest18.java

--- clusteredxybarrenderer_estest18.java
+++ clusteredxybarrenderer_estest18.java
@@ -1,46 +1,34 @@
 package org.jfree.chart.renderer.xy;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.awt.Graphics2D;
-import java.awt.geom.Rectangle2D;
-import java.text.DateFormatSymbols;
-import java.util.Date;
-import java.util.Locale;
-import javax.swing.JLayeredPane;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.text.MockSimpleDateFormat;
-import org.jfree.chart.ChartRenderingInfo;
-import org.jfree.chart.axis.CyclicNumberAxis;
-import org.jfree.chart.plot.CategoryCrosshairState;
-import org.jfree.chart.plot.CombinedRangeXYPlot;
-import org.jfree.chart.plot.PlotRenderingInfo;
-import org.jfree.chart.util.DirectionalGradientPaintTransformer;
-import org.jfree.data.Range;
-import org.jfree.data.statistics.DefaultBoxAndWhiskerXYDataset;
-import org.jfree.data.statistics.SimpleHistogramBin;
-import org.jfree.data.statistics.SimpleHistogramDataset;
-import org.jfree.data.time.TimeSeriesDataItem;
-import org.jfree.data.xy.CategoryTableXYDataset;
-import org.jfree.data.xy.DefaultOHLCDataset;
-import org.jfree.data.xy.DefaultWindDataset;
-import org.jfree.data.xy.DefaultXYZDataset;
-import org.jfree.data.xy.IntervalXYDataset;
-import org.jfree.data.xy.OHLCDataItem;
-import org.jfree.data.xy.XYDataset;
-import org.jfree.data.xy.XYSeries;
-import org.jfree.data.xy.XYSeriesCollection;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 
+/**
+ * Contains tests for the {@link ClusteredXYBarRenderer#equals(Object)} method.
+ * This class focuses on verifying the equality contract of the renderer.
+ */
 public class ClusteredXYBarRenderer_ESTestTest18 extends ClusteredXYBarRenderer_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test17() throws Throwable {
-        ClusteredXYBarRenderer clusteredXYBarRenderer0 = new ClusteredXYBarRenderer();
-        ClusteredXYBarRenderer clusteredXYBarRenderer1 = new ClusteredXYBarRenderer(10, true);
-        boolean boolean0 = clusteredXYBarRenderer1.equals(clusteredXYBarRenderer0);
-        assertFalse(boolean0);
+    /**
+     * Verifies that the equals() method returns false when comparing two
+     * ClusteredXYBarRenderer instances that were constructed with different properties.
+     */
+    @Test
+    public void equals_shouldReturnFalse_forRenderersWithDifferentProperties() {
+        // Arrange: Create a renderer with default properties.
+        // The default constructor sets margin to 0.0 and centerBarAtStartValue to false.
+        ClusteredXYBarRenderer renderer1 = new ClusteredXYBarRenderer();
+
+        // Arrange: Create a second renderer with non-default properties.
+        ClusteredXYBarRenderer renderer2 = new ClusteredXYBarRenderer(10.0, true);
+
+        // Act & Assert: The two instances should not be considered equal.
+        // Using assertNotEquals is more semantic for this comparison.
+        assertNotEquals(renderer1, renderer2);
+
+        // For completeness, we can also check the direct boolean result.
+        boolean areEqual = renderer1.equals(renderer2);
+        assertFalse("Renderers with different margin and centerBarAtStartValue properties should not be equal.", areEqual);
     }
 }
