c3a16a51f489c1261127cf3730b228326d4782f3
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:04:08 +0800

    Original processedAutoTestCases multireadonlyseekablebytechannel_estest14.java

---
+++ multireadonlyseekablebytechannel_estest14.java
@@ -0,0 +1,37 @@
+package org.apache.commons.compress.utils;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.ClosedChannelException;
+import java.nio.channels.FileChannel;
+import java.nio.channels.NonWritableChannelException;
+import java.nio.channels.SeekableByteChannel;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.OpenOption;
+import java.nio.file.Path;
+import java.util.LinkedList;
+import java.util.List;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.junit.runner.RunWith;
+
+public class MultiReadOnlySeekableByteChannel_ESTestTest14 extends MultiReadOnlySeekableByteChannel_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test13() throws Throwable {
+        Path[] pathArray0 = new Path[1];
+        MockFile mockFile0 = new MockFile("files");
+        Path path0 = mockFile0.toPath();
+        pathArray0[0] = path0;
+        try {
+            MultiReadOnlySeekableByteChannel.forPaths(pathArray0);
+            fail("Expecting exception: NoSuchFileException");
+        } catch (NoSuchFileException e) {
+        }
+    }
+}
