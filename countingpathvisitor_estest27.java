360a9023c614fd631c02a305837a42d2d903ceb3
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:46:10 +0800

    gemini processedAutoTestCases countingpathvisitor_estest27.java

--- countingpathvisitor_estest27.java
+++ countingpathvisitor_estest27.java
@@ -1,38 +1,31 @@
 package org.apache.commons.io.file;
 
+import org.apache.commons.io.file.CountingPathVisitor.Builder;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.FileVisitResult;
-import java.nio.file.NoSuchFileException;
+
 import java.nio.file.Path;
-import java.nio.file.attribute.BasicFileAttributes;
 import java.util.function.UnaryOperator;
-import org.apache.commons.io.filefilter.CanWriteFileFilter;
-import org.apache.commons.io.filefilter.EmptyFileFilter;
-import org.apache.commons.io.filefilter.FileFileFilter;
-import org.apache.commons.io.filefilter.HiddenFileFilter;
-import org.apache.commons.io.filefilter.IOFileFilter;
-import org.apache.commons.io.filefilter.NotFileFilter;
-import org.apache.commons.io.filefilter.PathEqualsFileFilter;
-import org.apache.commons.io.filefilter.SuffixFileFilter;
-import org.apache.commons.io.function.IOBiFunction;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.evosuite.runtime.mock.java.io.MockFile;
-import org.evosuite.runtime.mock.java.io.MockIOException;
-import org.junit.runner.RunWith;
 
-public class CountingPathVisitor_ESTestTest27 extends CountingPathVisitor_ESTest_scaffolding {
+import static org.junit.Assert.assertSame;
 
-    @Test(timeout = 4000)
-    public void test26() throws Throwable {
-        CountingPathVisitor.Builder countingPathVisitor_Builder0 = new CountingPathVisitor.Builder();
-        CountingPathVisitor.Builder countingPathVisitor_Builder1 = countingPathVisitor_Builder0.setDirectoryPostTransformer((UnaryOperator<Path>) null);
-        assertSame(countingPathVisitor_Builder1, countingPathVisitor_Builder0);
+/**
+ * Tests for the fluent interface of {@link CountingPathVisitor.Builder}.
+ */
+public class CountingPathVisitorBuilderTest {
+
+    /**
+     * Tests that setDirectoryPostTransformer() returns the same builder instance,
+     * confirming the method supports a fluent (chainable) API.
+     */
+    @Test
+    public void testSetDirectoryPostTransformerIsFluent() {
+        // Arrange: Create a new builder instance.
+        final Builder builder = new Builder();
+
+        // Act: Call the setter method. The cast is necessary because 'null' has no type.
+        final Builder returnedBuilder = builder.setDirectoryPostTransformer((UnaryOperator<Path>) null);
+
+        // Assert: The method should return the same instance to allow for method chaining.
+        assertSame("The setter should return the same builder instance.", builder, returnedBuilder);
     }
 }
