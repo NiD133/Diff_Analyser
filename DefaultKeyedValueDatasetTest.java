1c3e3772b91ada95ffce42e603c3da24e9b5037b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 05:33:13 +0800

    Original manualTestSuite DefaultKeyedValueDatasetTest.java

--- DefaultKeyedValueDatasetTest.java
+++ DefaultKeyedValueDatasetTest.java
@@ -1,83 +1,111 @@
+/* ======================================================
+ * JFreeChart : a chart library for the Java(tm) platform
+ * ======================================================
+ *
+ * (C) Copyright 2000-present, by David Gilbert and Contributors.
+ *
+ * Project Info:  https://www.jfree.org/jfreechart/index.html
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ *
+ * [Oracle and Java are registered trademarks of Oracle and/or its affiliates. 
+ * Other names may be trademarks of their respective owners.]
+ *
+ * ---------------------------------
+ * DefaultKeyedValueDatasetTest.java
+ * ---------------------------------
+ * (C) Copyright 2003-present, by David Gilbert and Contributors.
+ *
+ * Original Author:  David Gilbert;
+ * Contributor(s):   -;
+ *
+ */
+
 package org.jfree.data.general;
 
 import org.jfree.chart.TestUtils;
 import org.jfree.chart.internal.CloneUtils;
+
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * Unit tests for the {@link DefaultKeyedValueDataset} class.
+ * Tests for the {@link DefaultKeyedValueDataset} class.
  */
 public class DefaultKeyedValueDatasetTest {
 
     /**
-     * Tests the {@code equals} method to ensure it correctly identifies equal and unequal datasets.
+     * Confirm that the equals method can distinguish all the required fields.
      */
     @Test
-    public void testEqualsMethod() {
-        DefaultKeyedValueDataset dataset1 = new DefaultKeyedValueDataset("Test", 45.5);
-        DefaultKeyedValueDataset dataset2 = new DefaultKeyedValueDataset("Test", 45.5);
+    public void testEquals() {
 
-        // Test equality for identical datasets
-        assertEquals(dataset1, dataset2);
-        assertEquals(dataset2, dataset1);
+        DefaultKeyedValueDataset d1 = new DefaultKeyedValueDataset("Test", 45.5);
+        DefaultKeyedValueDataset d2 = new DefaultKeyedValueDataset("Test", 45.5);
+        assertEquals(d1, d2);
+        assertEquals(d2, d1);
 
-        // Test inequality for datasets with different keys
-        dataset1 = new DefaultKeyedValueDataset("Test 1", 45.5);
-        dataset2 = new DefaultKeyedValueDataset("Test 2", 45.5);
-        assertNotEquals(dataset1, dataset2);
+        d1 = new DefaultKeyedValueDataset("Test 1", 45.5);
+        d2 = new DefaultKeyedValueDataset("Test 2", 45.5);
+        assertNotEquals(d1, d2);
 
-        // Test inequality for datasets with different values
-        dataset1 = new DefaultKeyedValueDataset("Test", 45.5);
-        dataset2 = new DefaultKeyedValueDataset("Test", 45.6);
-        assertNotEquals(dataset1, dataset2);
+        d1 = new DefaultKeyedValueDataset("Test", 45.5);
+        d2 = new DefaultKeyedValueDataset("Test", 45.6);
+        assertNotEquals(d1, d2);
+
     }
 
     /**
-     * Tests the cloning functionality to ensure a dataset can be cloned correctly.
-     * @throws CloneNotSupportedException if the dataset cannot be cloned.
+     * Confirm that cloning works.
+     * @throws java.lang.CloneNotSupportedException
      */
     @Test
-    public void testCloningFunctionality() throws CloneNotSupportedException {
-        DefaultKeyedValueDataset originalDataset = new DefaultKeyedValueDataset("Test", 45.5);
-        DefaultKeyedValueDataset clonedDataset = (DefaultKeyedValueDataset) originalDataset.clone();
-
-        // Ensure the cloned dataset is a different instance but equal in content
-        assertNotSame(originalDataset, clonedDataset);
-        assertSame(originalDataset.getClass(), clonedDataset.getClass());
-        assertEquals(originalDataset, clonedDataset);
+    public void testCloning() throws CloneNotSupportedException {
+        DefaultKeyedValueDataset d1 = new DefaultKeyedValueDataset("Test", 45.5);
+        DefaultKeyedValueDataset d2 = (DefaultKeyedValueDataset) d1.clone();
+        assertNotSame(d1, d2);
+        assertSame(d1.getClass(), d2.getClass());
+        assertEquals(d1, d2);
     }
 
     /**
-     * Tests that a cloned dataset is independent of the original dataset.
-     * @throws CloneNotSupportedException if the dataset cannot be cloned.
+     * Confirm that the clone is independent of the original.
+     * @throws java.lang.CloneNotSupportedException
      */
     @Test
     public void testCloneIndependence() throws CloneNotSupportedException {
-        DefaultKeyedValueDataset originalDataset = new DefaultKeyedValueDataset("Key", 10.0);
-        DefaultKeyedValueDataset clonedDataset = CloneUtils.clone(originalDataset);
-
-        // Ensure the cloned dataset is initially equal to the original
-        assertEquals(originalDataset, clonedDataset);
-
-        // Modify the cloned dataset and ensure it is no longer equal to the original
-        clonedDataset.updateValue(99.9);
-        assertNotEquals(originalDataset, clonedDataset);
-
-        // Revert the change and ensure equality is restored
-        clonedDataset.updateValue(10.0);
-        assertEquals(originalDataset, clonedDataset);
+        DefaultKeyedValueDataset d1
+            = new DefaultKeyedValueDataset("Key", 10.0);
+        DefaultKeyedValueDataset d2 = CloneUtils.clone(d1);
+        assertEquals(d1, d2);
+        d2.updateValue(99.9);
+        assertNotEquals(d1, d2);
+        d2.updateValue(10.0);
+        assertEquals(d1, d2);
     }
 
     /**
-     * Tests the serialization and deserialization process to ensure dataset integrity.
+     * Serialize an instance, restore it, and check for equality.
      */
     @Test
-    public void testSerializationIntegrity() {
-        DefaultKeyedValueDataset originalDataset = new DefaultKeyedValueDataset("Test", 25.3);
-        DefaultKeyedValueDataset deserializedDataset = TestUtils.serialised(originalDataset);
-
-        // Ensure the deserialized dataset is equal to the original
-        assertEquals(originalDataset, deserializedDataset);
+    public void testSerialization() {
+        DefaultKeyedValueDataset d1 = new DefaultKeyedValueDataset("Test", 25.3);
+        DefaultKeyedValueDataset d2 = TestUtils.serialised(d1);
+        assertEquals(d1, d2);
     }
+
 }
