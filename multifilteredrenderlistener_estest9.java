4b8290f5f5b532a0c43f5652be1e87df36d01bfa
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:03:58 +0800

    Original processedAutoTestCases multifilteredrenderlistener_estest9.java

---
+++ multifilteredrenderlistener_estest9.java
@@ -0,0 +1,20 @@
+package com.itextpdf.text.pdf.parser;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.itextpdf.awt.geom.Rectangle2D;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class MultiFilteredRenderListener_ESTestTest9 extends MultiFilteredRenderListener_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test8() throws Throwable {
+        MultiFilteredRenderListener multiFilteredRenderListener0 = new MultiFilteredRenderListener();
+        multiFilteredRenderListener0.beginTextBlock();
+    }
+}
