010ea005f8296666a03ac36f9a883aed1ee2ed72
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:26:55 +0800

    Original processedManualTestCases collectionbagtest3.java

---
+++ collectionbagtest3.java
@@ -0,0 +1,71 @@
+package org.apache.commons.collections4.bag;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
+import org.apache.commons.collections4.Bag;
+import org.apache.commons.collections4.Predicate;
+import org.apache.commons.collections4.collection.AbstractCollectionTest;
+import org.apache.commons.collections4.functors.NonePredicate;
+import org.junit.jupiter.api.Test;
+
+public class CollectionBagTestTest3<T> extends AbstractCollectionTest<T> {
+
+    @Override
+    public String getCompatibilityVersion() {
+        return "4";
+    }
+
+    @Override
+    protected int getIterationBehaviour() {
+        return UNORDERED;
+    }
+
+    /**
+     * Returns an empty List for use in modification testing.
+     *
+     * @return a confirmed empty collection
+     */
+    @Override
+    public Collection<T> makeConfirmedCollection() {
+        return new ArrayList<>();
+    }
+
+    /**
+     * Returns a full Set for use in modification testing.
+     *
+     * @return a confirmed full collection
+     */
+    @Override
+    public Collection<T> makeConfirmedFullCollection() {
+        final Collection<T> set = makeConfirmedCollection();
+        set.addAll(Arrays.asList(getFullElements()));
+        return set;
+    }
+
+    @Override
+    public Bag<T> makeObject() {
+        return CollectionBag.collectionBag(new HashBag<>());
+    }
+
+    /**
+     * Compares the current serialized form of the Bag
+     * against the canonical version in SCM.
+     */
+    @Test
+    void testEmptyBagCompatibility() throws IOException, ClassNotFoundException {
+        // test to make sure the canonical form has been preserved
+        final Bag<T> bag = makeObject();
+        if (bag instanceof Serializable && !skipSerializedCanonicalTests() && isTestSerialization()) {
+            final Bag<?> bag2 = (Bag<?>) readExternalFormFromDisk(getCanonicalEmptyCollectionName(bag));
+            assertTrue(bag2.isEmpty(), "Bag is empty");
+            assertEquals(bag, bag2);
+        }
+    }
+}
