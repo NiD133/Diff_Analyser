259cfa1769d2fd8961a7788eb13b1e4fde6216b9
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:37:44 +0800

    Original processedAutoTestCases arraytypehandler_estest10.java

---
+++ arraytypehandler_estest10.java
@@ -0,0 +1,40 @@
+package org.apache.ibatis.type;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.sql.Array;
+import java.sql.CallableStatement;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.Month;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.time.MockInstant;
+import org.evosuite.runtime.mock.java.time.MockLocalDate;
+import org.junit.runner.RunWith;
+
+public class ArrayTypeHandler_ESTestTest10 extends ArrayTypeHandler_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test09() throws Throwable {
+        ArrayTypeHandler arrayTypeHandler0 = new ArrayTypeHandler();
+        // Undeclared exception!
+        try {
+            arrayTypeHandler0.getNullableResult((ResultSet) null, 0);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.apache.ibatis.type.ArrayTypeHandler", e);
+        }
+    }
+}
