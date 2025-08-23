1cffb4940864a94d85585b1bd7bb24f254adbc26
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:09:58 +0800

    Original processedAutoTestCases queryparser_estest15.java

---
+++ queryparser_estest15.java
@@ -0,0 +1,17 @@
+package org.jsoup.select;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class QueryParser_ESTestTest15 extends QueryParser_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test14() throws Throwable {
+        Evaluator evaluator0 = QueryParser.parse(":containsWholeText(%s)");
+        assertNotNull(evaluator0);
+    }
+}
