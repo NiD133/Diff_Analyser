cc08dbae1ccb191fece1d9edad20b9f9fa3a6e95
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:12:59 +0800

    gemini processedAutoTestCases seconds_estest33.java

--- seconds_estest33.java
+++ seconds_estest33.java
@@ -1,26 +1,23 @@
 package org.joda.time;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class Seconds_ESTestTest33 extends Seconds_ESTest_scaffolding {
+/**
+ * A test suite for the {@link Seconds} class, focusing on edge cases.
+ */
+public class SecondsTest {
 
-    @Test(timeout = 4000)
-    public void test32() throws Throwable {
-        Seconds seconds0 = Seconds.MIN_VALUE;
-        // Undeclared exception!
-        try {
-            seconds0.plus((-674));
-            fail("Expecting exception: ArithmeticException");
-        } catch (ArithmeticException e) {
-            //
-            // The calculation caused an overflow: -2147483648 + -674
-            //
-            verifyException("org.joda.time.field.FieldUtils", e);
-        }
+    /**
+     * Verifies that adding a negative number to Seconds.MIN_VALUE correctly
+     * throws an ArithmeticException due to integer underflow.
+     */
+    @Test(expected = ArithmeticException.class)
+    public void plus_whenResultUnderflows_throwsArithmeticException() {
+        // Arrange: Start with the minimum possible value for Seconds.
+        Seconds minValue = Seconds.MIN_VALUE;
+
+        // Act: Attempt to subtract, which should cause an integer underflow.
+        // The test will pass if an ArithmeticException is thrown.
+        minValue.plus(-1);
     }
 }
