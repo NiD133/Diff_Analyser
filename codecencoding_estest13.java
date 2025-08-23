b07a60d4a2366ada1cc09923d4aecda36917ca37
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:43:55 +0800

    Original processedAutoTestCases codecencoding_estest13.java

---
+++ codecencoding_estest13.java
@@ -0,0 +1,38 @@
+package org.apache.commons.compress.harmony.pack200;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.io.PushbackInputStream;
+import java.io.SequenceInputStream;
+import java.util.Enumeration;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class CodecEncoding_ESTestTest13 extends CodecEncoding_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test12() throws Throwable {
+        BHSDCodec bHSDCodec0 = Codec.BYTE1;
+        // Undeclared exception!
+        try {
+            CodecEncoding.getCodec(128, (InputStream) null, bHSDCodec0);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.apache.commons.compress.harmony.pack200.CodecEncoding", e);
+        }
+    }
+}
