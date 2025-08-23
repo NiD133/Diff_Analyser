854fe515bd37f57f14f9fcc66e7ab9d67bfffff6
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:31:01 +0800

    gemini processedManualTestCases metaphonetest16.java

--- metaphonetest16.java
+++ metaphonetest16.java
@@ -1,58 +1,47 @@
 package org.apache.commons.codec.language;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
+
 import org.apache.commons.codec.AbstractStringEncoderTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class MetaphoneTestTest16 extends AbstractStringEncoderTest<Metaphone> {
-
-    public void assertIsMetaphoneEqual(final String source, final String[] matches) {
-        // match source to all matches
-        for (final String matche : matches) {
-            assertTrue(getStringEncoder().isMetaphoneEqual(source, matche), "Source: " + source + ", should have same Metaphone as: " + matche);
-        }
-        // match to each other
-        for (final String matche : matches) {
-            for (final String matche2 : matches) {
-                assertTrue(getStringEncoder().isMetaphoneEqual(matche, matche2));
-            }
-        }
-    }
-
-    public void assertMetaphoneEqual(final String[][] pairs) {
-        validateFixture(pairs);
-        for (final String[] pair : pairs) {
-            final String name0 = pair[0];
-            final String name1 = pair[1];
-            final String failMsg = "Expected match between " + name0 + " and " + name1;
-            assertTrue(getStringEncoder().isMetaphoneEqual(name0, name1), failMsg);
-            assertTrue(getStringEncoder().isMetaphoneEqual(name1, name0), failMsg);
-        }
-    }
+/**
+ * Tests the {@link Metaphone} encoder.
+ */
+class MetaphoneTest extends AbstractStringEncoderTest<Metaphone> {
 
     @Override
     protected Metaphone createStringEncoder() {
         return new Metaphone();
     }
 
-    public void validateFixture(final String[][] pairs) {
-        if (pairs.length == 0) {
-            fail("Test fixture is empty");
-        }
-        for (int i = 0; i < pairs.length; i++) {
-            if (pairs[i].length != 2) {
-                fail("Error in test fixture in the data array at index " + i);
-            }
-        }
-    }
-
     /**
-     * Match data computed from http://www.lanw.com/java/phonetic/default.htm
+     * This test verifies that a group of names that are phonetically similar
+     * to "Susan" are all encoded to the same Metaphone value. This ensures the
+     * {@code isMetaphoneEqual} method correctly identifies them as equivalent.
+     * The test data was originally sourced from an external phonetic calculator.
      */
     @Test
-    void testIsMetaphoneEqualSusan() {
-        assertIsMetaphoneEqual("Susan", new String[] { "Siusan", "Sosanna", "Susan", "Susana", "Susann", "Susanna", "Susannah", "Susanne", "Suzann", "Suzanna", "Suzanne", "Zuzana" });
+    @DisplayName("Should encode variations of 'Susan' to the same Metaphone code")
+    void shouldEncodeSusanVariationsToSameCode() {
+        // Arrange
+        final String[] phoneticallySimilarNames = {
+            "Siusan", "Sosanna", "Susan", "Susana", "Susann", "Susanna",
+            "Susannah", "Susanne", "Suzann", "Suzanna", "Suzanne", "Zuzana"
+        };
+        final Metaphone metaphone = createStringEncoder();
+        final String expectedCode = metaphone.metaphone("Susan"); // Expected code: SSN
+
+        // Act & Assert
+        assertAll("All variations of 'Susan' should produce the same Metaphone code",
+            () -> {
+                for (final String name : phoneticallySimilarNames) {
+                    assertEquals(expectedCode, metaphone.metaphone(name),
+                        () -> "Metaphone for '" + name + "' should match the code for 'Susan'");
+                }
+            }
+        );
     }
 }
