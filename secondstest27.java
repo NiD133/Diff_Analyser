1440315c4f7b76af6240a1c4ac727dc572aadef5
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:33:16 +0800

    Original processedManualTestCases secondstest27.java

---
+++ secondstest27.java
@@ -0,0 +1,38 @@
+package org.joda.time;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class SecondsTestTest27 extends TestCase {
+
+    // (before the late 90's they were all over the place)
+    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
+
+    public static TestSuite suite() {
+        return new TestSuite(TestSeconds.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+    }
+
+    //-----------------------------------------------------------------------
+    public void testAddToLocalDate() {
+        Seconds test = Seconds.seconds(26);
+        LocalDateTime date = new LocalDateTime(2006, 6, 1, 0, 0, 0, 0);
+        LocalDateTime expected = new LocalDateTime(2006, 6, 1, 0, 0, 26, 0);
+        assertEquals(expected, date.plus(test));
+    }
+}
