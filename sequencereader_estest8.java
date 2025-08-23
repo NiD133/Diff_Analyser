d7531d9b3bc757dbe4acfbd320cc4587e18fdfa1
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:14:33 +0800

    Original processedAutoTestCases sequencereader_estest8.java

---
+++ sequencereader_estest8.java
@@ -0,0 +1,34 @@
+package org.apache.commons.io.input;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.IOException;
+import java.io.PipedReader;
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.ConcurrentModificationException;
+import java.util.List;
+import java.util.Vector;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class SequenceReader_ESTestTest8 extends SequenceReader_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test07() throws Throwable {
+        SequenceReader sequenceReader0 = null;
+        try {
+            sequenceReader0 = new SequenceReader((Reader[]) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("java.util.Objects", e);
+        }
+    }
+}
