15d0804f87d3afa37408c21c410843d68b3861d3
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:37:15 +0800

    gemini processedAutoTestCases appendablejoiner_estest9.java

--- appendablejoiner_estest9.java
+++ appendablejoiner_estest9.java
@@ -1,41 +1,27 @@
 package org.apache.commons.lang3;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.IOException;
-import java.io.PipedWriter;
-import java.nio.BufferOverflowException;
-import java.nio.CharBuffer;
-import java.nio.ReadOnlyBufferException;
-import java.nio.charset.Charset;
-import java.sql.SQLNonTransientConnectionException;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.EnumSet;
-import java.util.LinkedHashSet;
-import java.util.Locale;
-import org.apache.commons.lang3.function.FailableBiConsumer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class AppendableJoiner_ESTestTest9 extends AppendableJoiner_ESTest_scaffolding {
+/**
+ * Unit tests for {@link AppendableJoiner}.
+ */
+public class AppendableJoinerTest {
 
-    @Test(timeout = 4000)
-    public void test08() throws Throwable {
-        AppendableJoiner.Builder<StringBuilder> appendableJoiner_Builder0 = AppendableJoiner.builder();
-        AppendableJoiner<StringBuilder> appendableJoiner0 = appendableJoiner_Builder0.get();
-        StringBuilder[] stringBuilderArray0 = new StringBuilder[0];
-        // Undeclared exception!
-        try {
-            appendableJoiner0.joinA((StringBuilder) null, stringBuilderArray0);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.commons.lang3.AppendableJoiner", e);
-        }
+    /**
+     * Tests that calling joinA with a null Appendable throws a NullPointerException,
+     * as the target for appending cannot be null.
+     */
+    @Test(expected = NullPointerException.class)
+    public void joinAWithNullAppendableShouldThrowNullPointerException() {
+        // Arrange: Create a default joiner and an empty array of elements.
+        final AppendableJoiner<StringBuilder> joiner = AppendableJoiner.builder().get();
+        final StringBuilder[] emptyArray = {};
+
+        // Act: Attempt to join into a null Appendable.
+        // This call is expected to throw the NullPointerException.
+        joiner.joinA(null, emptyArray);
+
+        // Assert: The test passes if the expected exception is thrown.
+        // This is handled by the @Test(expected=...) annotation.
     }
 }
