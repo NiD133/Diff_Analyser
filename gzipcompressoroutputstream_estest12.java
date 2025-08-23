9e9db7ab765e2a2b0a6721fd55de9bda40cd8346
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:52:51 +0800

    Original processedAutoTestCases gzipcompressoroutputstream_estest12.java

---
+++ gzipcompressoroutputstream_estest12.java
@@ -0,0 +1,38 @@
+package org.apache.commons.compress.compressors.gzip;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FilterOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.io.PipedOutputStream;
+import java.io.PrintStream;
+import java.util.Locale;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.evosuite.runtime.mock.java.io.MockFileOutputStream;
+import org.evosuite.runtime.mock.java.io.MockPrintStream;
+import org.junit.runner.RunWith;
+
+public class GzipCompressorOutputStream_ESTestTest12 extends GzipCompressorOutputStream_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test11() throws Throwable {
+        ByteArrayOutputStream byteArrayOutputStream0 = new ByteArrayOutputStream();
+        GzipCompressorOutputStream gzipCompressorOutputStream0 = null;
+        try {
+            gzipCompressorOutputStream0 = new GzipCompressorOutputStream(byteArrayOutputStream0, (GzipParameters) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.apache.commons.compress.compressors.gzip.GzipCompressorOutputStream", e);
+        }
+    }
+}
