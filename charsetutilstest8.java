7cb0da63ae90412f229cabec90980aeab29a704c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:26:42 +0800

    Original processedManualTestCases charsetutilstest8.java

---
+++ charsetutilstest8.java
@@ -0,0 +1,28 @@
+package org.apache.commons.lang3;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Modifier;
+import org.junit.jupiter.api.Test;
+
+public class CharSetUtilsTestTest8 extends AbstractLangTest {
+
+    @Test
+    void testKeep_StringString() {
+        assertNull(CharSetUtils.keep(null, (String) null));
+        assertNull(CharSetUtils.keep(null, ""));
+        assertEquals("", CharSetUtils.keep("", (String) null));
+        assertEquals("", CharSetUtils.keep("", ""));
+        assertEquals("", CharSetUtils.keep("", "a-e"));
+        assertEquals("", CharSetUtils.keep("hello", (String) null));
+        assertEquals("", CharSetUtils.keep("hello", ""));
+        assertEquals("", CharSetUtils.keep("hello", "xyz"));
+        assertEquals("hello", CharSetUtils.keep("hello", "a-z"));
+        assertEquals("hello", CharSetUtils.keep("hello", "oleh"));
+        assertEquals("ell", CharSetUtils.keep("hello", "el"));
+    }
+}
