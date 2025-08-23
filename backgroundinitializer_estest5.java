96df97ea37ea6c94ebdcc3840ad02ffc48c33d10
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:38:34 +0800

    Original processedAutoTestCases backgroundinitializer_estest5.java

---
+++ backgroundinitializer_estest5.java
@@ -0,0 +1,35 @@
+package org.apache.commons.lang3.concurrent;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.concurrent.Delayed;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.lang.MockException;
+import org.evosuite.runtime.mock.java.lang.MockThrowable;
+import org.evosuite.runtime.testdata.FileSystemHandling;
+import org.junit.runner.RunWith;
+
+public class BackgroundInitializer_ESTestTest5 extends BackgroundInitializer_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test04() throws Throwable {
+        BackgroundInitializer<Delayed> backgroundInitializer0 = new BackgroundInitializer<Delayed>();
+        ForkJoinPool forkJoinPool0 = ForkJoinPool.commonPool();
+        backgroundInitializer0.setExternalExecutor(forkJoinPool0);
+        boolean boolean0 = backgroundInitializer0.start();
+        assertTrue(boolean0);
+        boolean boolean1 = FileSystemHandling.shouldAllThrowIOExceptions();
+        assertTrue(boolean1 == boolean0);
+    }
+}
