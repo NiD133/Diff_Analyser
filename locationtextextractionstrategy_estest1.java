a5d07988b4650038cb400cccf5928d4915e80a23
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:01:11 +0800

    Original processedAutoTestCases locationtextextractionstrategy_estest1.java

---
+++ locationtextextractionstrategy_estest1.java
@@ -0,0 +1,33 @@
+package com.itextpdf.text.pdf.parser;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.itextpdf.text.pdf.CMapAwareDocumentFont;
+import com.itextpdf.text.pdf.PdfDate;
+import com.itextpdf.text.pdf.PdfDictionary;
+import com.itextpdf.text.pdf.PdfIndirectReference;
+import com.itextpdf.text.pdf.PdfOCProperties;
+import com.itextpdf.text.pdf.PdfSigLockDictionary;
+import com.itextpdf.text.pdf.PdfString;
+import java.nio.charset.IllegalCharsetNameException;
+import java.util.Collection;
+import java.util.LinkedList;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class LocationTextExtractionStrategy_ESTestTest1 extends LocationTextExtractionStrategy_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test00() throws Throwable {
+        Vector vector0 = new Vector(0.0F, (-2030.0F), 0.0F);
+        LocationTextExtractionStrategy.TextChunk locationTextExtractionStrategy_TextChunk0 = new LocationTextExtractionStrategy.TextChunk("", vector0, vector0, 0.0F);
+        LocationTextExtractionStrategy.TextChunk locationTextExtractionStrategy_TextChunk1 = new LocationTextExtractionStrategy.TextChunk("Lm", vector0, vector0, 2);
+        float float0 = locationTextExtractionStrategy_TextChunk0.distanceFromEndOf(locationTextExtractionStrategy_TextChunk1);
+        assertEquals(0.0F, float0, 0.01F);
+        assertEquals(2.0F, locationTextExtractionStrategy_TextChunk1.getCharSpaceWidth(), 0.01F);
+    }
+}
