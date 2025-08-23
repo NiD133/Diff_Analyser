9976c60fb706c78198fc0aba51ddef6272077c1a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:21:54 +0800

    gemini processedAutoTestCases thresholdingoutputstream_estest15.java

--- thresholdingoutputstream_estest15.java
+++ thresholdingoutputstream_estest15.java
@@ -1,23 +1,50 @@
 package org.apache.commons.io.output;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.OutputStream;
-import org.apache.commons.io.function.IOConsumer;
-import org.apache.commons.io.function.IOFunction;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class ThresholdingOutputStream_ESTestTest15 extends ThresholdingOutputStream_ESTest_scaffolding {
+import java.io.IOException;
 
-    @Test(timeout = 4000)
-    public void test14() throws Throwable {
-        ThresholdingOutputStream thresholdingOutputStream0 = new ThresholdingOutputStream((-2596));
-        byte[] byteArray0 = new byte[1];
-        thresholdingOutputStream0.write(byteArray0);
-        thresholdingOutputStream0.write(byteArray0, (-1473), 1950);
-        assertEquals(1951L, thresholdingOutputStream0.getByteCount());
+/**
+ * Tests for {@link ThresholdingOutputStream}.
+ */
+public class ThresholdingOutputStreamTest {
+
+    /**
+     * Tests that the internal byte count is updated correctly even when the
+     * write(byte[], int, int) method is called with parameters that would
+     * normally cause an IndexOutOfBoundsException.
+     * <p>
+     * This behavior is due to the default underlying stream being a
+     * {@link NullOutputStream}, which ignores invalid parameters and does not
+     * throw an exception.
+     * </p>
+     */
+    @Test
+    public void testByteCountIsUpdatedEvenWithInvalidWriteParameters() throws IOException {
+        // Arrange
+        // A negative threshold is treated as 0 by the constructor.
+        final int negativeThreshold = -2596;
+        final ThresholdingOutputStream stream = new ThresholdingOutputStream(negativeThreshold);
+
+        final byte[] data = new byte[1];
+        final int firstWriteLength = data.length;
+        final int secondWriteLength = 1950;
+        final int invalidOffset = -1473;
+
+        // Act
+        // Perform a first write to initialize the byte count.
+        stream.write(data);
+
+        // Perform a second write with an invalid offset. This does not throw an
+        // exception because the default underlying stream is NullOutputStream,
+        // which simply discards the output. The internal byte count, however,
+        // is still incremented.
+        stream.write(data, invalidOffset, secondWriteLength);
+
+        // Assert
+        final long expectedByteCount = (long) firstWriteLength + secondWriteLength;
+        assertEquals("The byte count should be the sum of the lengths of all write operations.",
+                expectedByteCount, stream.getByteCount());
     }
 }
