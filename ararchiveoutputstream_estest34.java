33895750eba47e5918c298d4b4231f27f006b5d0
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:37:23 +0800

    Original processedAutoTestCases ararchiveoutputstream_estest34.java

---
+++ ararchiveoutputstream_estest34.java
@@ -0,0 +1,38 @@
+package org.apache.commons.compress.archivers.ar;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.LinkOption;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.System;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.evosuite.runtime.mock.java.io.MockFileOutputStream;
+import org.evosuite.runtime.mock.java.io.MockPrintStream;
+import org.evosuite.runtime.testdata.FileSystemHandling;
+import org.junit.runner.RunWith;
+
+public class ArArchiveOutputStream_ESTestTest34 extends ArArchiveOutputStream_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test33() throws Throwable {
+        ArArchiveOutputStream arArchiveOutputStream0 = new ArArchiveOutputStream((OutputStream) null);
+        // Undeclared exception!
+        try {
+            arArchiveOutputStream0.putArchiveEntry((ArArchiveEntry) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.apache.commons.compress.archivers.ar.ArArchiveOutputStream", e);
+        }
+    }
+}
