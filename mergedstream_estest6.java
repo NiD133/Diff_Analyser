1fe7febd43c3de49cb2b15edb6e0cec486788bcb
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:02:16 +0800

    Original processedAutoTestCases mergedstream_estest6.java

---
+++ mergedstream_estest6.java
@@ -0,0 +1,47 @@
+package com.fasterxml.jackson.core.io;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.fasterxml.jackson.core.ErrorReportConfiguration;
+import com.fasterxml.jackson.core.StreamReadConstraints;
+import com.fasterxml.jackson.core.StreamWriteConstraints;
+import com.fasterxml.jackson.core.util.BufferRecycler;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.io.PushbackInputStream;
+import java.io.SequenceInputStream;
+import java.util.Enumeration;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.evosuite.runtime.mock.java.io.MockFileInputStream;
+import org.junit.runner.RunWith;
+
+public class MergedStream_ESTestTest6 extends MergedStream_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test05() throws Throwable {
+        StreamReadConstraints streamReadConstraints0 = StreamReadConstraints.defaults();
+        StreamWriteConstraints streamWriteConstraints0 = StreamWriteConstraints.defaults();
+        ErrorReportConfiguration errorReportConfiguration0 = ErrorReportConfiguration.defaults();
+        BufferRecycler bufferRecycler0 = new BufferRecycler();
+        ContentReference contentReference0 = ContentReference.rawReference(false, (Object) streamReadConstraints0);
+        IOContext iOContext0 = new IOContext(streamReadConstraints0, streamWriteConstraints0, errorReportConfiguration0, bufferRecycler0, contentReference0, false);
+        PipedOutputStream pipedOutputStream0 = new PipedOutputStream();
+        PipedInputStream pipedInputStream0 = new PipedInputStream(pipedOutputStream0, 20000000);
+        byte[] byteArray0 = new byte[2];
+        byteArray0[0] = (byte) (-25);
+        MergedStream mergedStream0 = new MergedStream(iOContext0, pipedInputStream0, byteArray0, 0, 2);
+        int int0 = mergedStream0.read();
+        assertEquals(231, int0);
+    }
+}
