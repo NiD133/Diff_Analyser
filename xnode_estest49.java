52d7d74192f4060b13c64cbc2b33ff1d0ed5887a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:28:59 +0800

    Original processedAutoTestCases xnode_estest49.java

---
+++ xnode_estest49.java
@@ -0,0 +1,38 @@
+package org.apache.ibatis.parsing;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.List;
+import java.util.Locale;
+import java.util.Properties;
+import java.util.function.Supplier;
+import javax.imageio.metadata.IIOMetadataNode;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.xml.sax.ext.DefaultHandler2;
+
+public class XNode_ESTestTest49 extends XNode_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test048() throws Throwable {
+        IIOMetadataNode iIOMetadataNode0 = new IIOMetadataNode();
+        Properties properties0 = new Properties();
+        XNode xNode0 = new XNode((XPathParser) null, iIOMetadataNode0, properties0);
+        Long long0 = new Long(94L);
+        // Undeclared exception!
+        try {
+            xNode0.getLongAttribute((String) null, long0);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+        }
+    }
+}
