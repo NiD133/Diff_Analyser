c19adc49e86f7b5f1b0b3e16f1ef1f223e6e040e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:14:10 +0800

    gemini processedAutoTestCases segmentutils_estest7.java

--- segmentutils_estest7.java
+++ segmentutils_estest7.java
@@ -1,25 +1,18 @@
 package org.apache.commons.compress.harmony.unpack200;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class SegmentUtils_ESTestTest7 extends SegmentUtils_ESTest_scaffolding {
+/**
+ * Tests for the {@link SegmentUtils} class.
+ */
+public class SegmentUtilsTest {
 
-    @Test(timeout = 4000)
-    public void test06() throws Throwable {
-        // Undeclared exception!
-        try {
-            SegmentUtils.countBit16((long[][]) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.commons.compress.harmony.unpack200.SegmentUtils", e);
-        }
+    /**
+     * Verifies that countBit16 throws a NullPointerException when passed a null 2D array,
+     * as the method cannot iterate over a null input.
+     */
+    @Test(expected = NullPointerException.class)
+    public void countBit16ShouldThrowNullPointerExceptionForNull2DArray() {
+        SegmentUtils.countBit16((long[][]) null);
     }
 }
