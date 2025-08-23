c066f9db87c974bc348f128ce64d9245a817c2bd
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:41:17 +0800

    Original processedAutoTestCases cacheddatetimezone_estest2.java

---
+++ cacheddatetimezone_estest2.java
@@ -0,0 +1,23 @@
+package org.joda.time.tz;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.joda.time.Chronology;
+import org.joda.time.DateTimeZone;
+import org.joda.time.Instant;
+import org.joda.time.LocalDateTime;
+import org.joda.time.chrono.GregorianChronology;
+import org.junit.runner.RunWith;
+
+public class CachedDateTimeZone_ESTestTest2 extends CachedDateTimeZone_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test01() throws Throwable {
+        DateTimeZone dateTimeZone0 = DateTimeZone.forID("WET");
+        String string0 = dateTimeZone0.getName(686653601480704L);
+        assertEquals("Western European Time", string0);
+    }
+}
