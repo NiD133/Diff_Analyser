23a1fb5e9cf81e0ca3c4be9f092cc440b279ed96
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:50:55 +0800

    Original processedAutoTestCases filetimes_estest49.java

---
+++ filetimes_estest49.java
@@ -0,0 +1,33 @@
+package org.apache.commons.io.file.attribute;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.math.BigDecimal;
+import java.nio.file.Path;
+import java.nio.file.attribute.FileTime;
+import java.time.DateTimeException;
+import java.time.Instant;
+import java.util.Date;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.time.MockInstant;
+import org.junit.runner.RunWith;
+
+public class FileTimes_ESTestTest49 extends FileTimes_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test48() throws Throwable {
+        FileTime fileTime0 = FileTimes.now();
+        // Undeclared exception!
+        try {
+            FileTimes.plusSeconds(fileTime0, 9223372036854775800L);
+            fail("Expecting exception: ArithmeticException");
+        } catch (ArithmeticException e) {
+            //
+            // long overflow
+            //
+            verifyException("java.lang.Math", e);
+        }
+    }
+}
