803146488b81f38c5285620ea8a5a235c460cf5f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:44:49 +0800

    gemini processedAutoTestCases compactlinkedhashset_estest9.java

--- compactlinkedhashset_estest9.java
+++ compactlinkedhashset_estest9.java
@@ -1,22 +1,29 @@
 package com.google.common.collect;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.Collection;
-import java.util.Locale;
-import java.util.Set;
-import java.util.Spliterator;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class CompactLinkedHashSet_ESTestTest9 extends CompactLinkedHashSet_ESTest_scaffolding {
+/**
+ * Tests for {@link CompactLinkedHashSet}.
+ */
+public class CompactLinkedHashSetTest {
 
-    @Test(timeout = 4000)
-    public void test08() throws Throwable {
-        CompactLinkedHashSet<Object> compactLinkedHashSet0 = CompactLinkedHashSet.create();
-        int int0 = compactLinkedHashSet0.firstEntryIndex();
-        assertEquals((-2), int0);
+    @Test
+    public void firstEntryIndex_onEmptySet_returnsNegativeTwo() {
+        // Arrange: Create an empty CompactLinkedHashSet.
+        // The expected value -2 corresponds to the internal `ENDPOINT` constant,
+        // which indicates that the set is empty.
+        CompactLinkedHashSet<Object> set = CompactLinkedHashSet.create();
+        int expectedIndex = -2;
+
+        // Act: Get the index of the first entry.
+        int actualIndex = set.firstEntryIndex();
+
+        // Assert: Verify the index is the special value for an empty set.
+        assertEquals(
+            "The first entry index of a newly created, empty set should be -2.",
+            expectedIndex,
+            actualIndex);
     }
 }
