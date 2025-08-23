8446ab9fe6520b5cafceaa8bbabcf356cfe2967c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:14:45 +0800

    gemini processedAutoTestCases serializationutils_estest5.java

--- serializationutils_estest5.java
+++ serializationutils_estest5.java
@@ -1,32 +1,34 @@
 package org.apache.commons.lang3;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.PipedInputStream;
-import java.io.PipedOutputStream;
-import java.io.SequenceInputStream;
-import java.io.Serializable;
-import java.util.HashMap;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
+
 import java.util.Locale;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.io.MockFile;
-import org.evosuite.runtime.mock.java.io.MockFileInputStream;
-import org.evosuite.runtime.mock.java.io.MockPrintStream;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class SerializationUtils_ESTestTest5 extends SerializationUtils_ESTest_scaffolding {
+/**
+ * Unit tests for {@link org.apache.commons.lang3.SerializationUtils}.
+ */
+public class SerializationUtilsTest {
 
-    @Test(timeout = 4000)
-    public void test04() throws Throwable {
-        Locale locale0 = Locale.PRC;
-        byte[] byteArray0 = SerializationUtils.serialize((Serializable) locale0);
-        Locale locale1 = (Locale) SerializationUtils.deserialize(byteArray0);
-        assertEquals("", locale1.getVariant());
+    /**
+     * Tests that an object is equivalent to the original after a serialization
+     * and deserialization round trip. This verifies the basic functionality of
+     * the serialize() and deserialize() methods.
+     */
+    @Test
+    public void testSerializationRoundTrip() {
+        // Arrange: Create a serializable object to be tested.
+        // Locale is a standard, simple serializable class suitable for this test.
+        final Locale originalLocale = Locale.PRC;
+
+        // Act: Serialize the object to a byte array, then deserialize it back into a new object.
+        final byte[] serializedData = SerializationUtils.serialize(originalLocale);
+        final Locale deserializedLocale = SerializationUtils.deserialize(serializedData);
+
+        // Assert: The deserialized object should be equal to the original,
+        // but it should be a different instance in memory.
+        assertEquals(originalLocale, deserializedLocale);
+        assertNotSame(originalLocale, deserializedLocale);
     }
 }
