061c9b23fb44c00e5c80601452d82af82a886b44
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:05:16 +0800

    Original processedAutoTestCases numberoutput_estest6.java

---
+++ numberoutput_estest6.java
@@ -0,0 +1,26 @@
+package com.fasterxml.jackson.core.io;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class NumberOutput_ESTestTest6 extends NumberOutput_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test05() throws Throwable {
+        byte[] byteArray0 = new byte[21];
+        // Undeclared exception!
+        try {
+            NumberOutput.outputLong(1000000000000L, byteArray0, 1668);
+            fail("Expecting exception: ArrayIndexOutOfBoundsException");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            //
+            // 1668
+            //
+            verifyException("com.fasterxml.jackson.core.io.NumberOutput", e);
+        }
+    }
+}
