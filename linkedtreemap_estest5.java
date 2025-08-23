257ef4c2bc889f82f971ac9b16a288a745cb4ea4
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:00:37 +0800

    Original processedAutoTestCases linkedtreemap_estest5.java

---
+++ linkedtreemap_estest5.java
@@ -0,0 +1,28 @@
+package com.google.gson.internal;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.AbstractMap;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.BiFunction;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class LinkedTreeMap_ESTestTest5 extends LinkedTreeMap_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test04() throws Throwable {
+        Comparator<Object> comparator0 = (Comparator<Object>) mock(Comparator.class, new ViolatedAssumptionAnswer());
+        LinkedTreeMap<Object, Object> linkedTreeMap0 = new LinkedTreeMap<Object, Object>(comparator0, true);
+        LinkedTreeMap.Node<Object, Object> linkedTreeMap_Node0 = new LinkedTreeMap.Node<Object, Object>(true);
+        linkedTreeMap0.removeInternal(linkedTreeMap_Node0, true);
+        int int0 = linkedTreeMap0.size();
+        assertEquals((-1), int0);
+    }
+}
