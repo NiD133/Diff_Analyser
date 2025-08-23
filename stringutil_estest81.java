300934b841c7b610bb461cba846078d329840098
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:19:11 +0800

    gemini processedAutoTestCases stringutil_estest81.java

--- stringutil_estest81.java
+++ stringutil_estest81.java
@@ -1,28 +1,17 @@
 package org.jsoup.internal;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Collection;
-import java.util.ConcurrentModificationException;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.ListIterator;
-import java.util.stream.Collector;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.evosuite.runtime.mock.java.net.MockURL;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertFalse;
 
-public class StringUtil_ESTestTest81 extends StringUtil_ESTest_scaffolding {
+/**
+ * Test suite for {@link StringUtil}.
+ */
+public class StringUtilTest {
 
-    @Test(timeout = 4000)
-    public void test80() throws Throwable {
-        boolean boolean0 = StringUtil.isNumeric((String) null);
-        assertFalse(boolean0);
+    @Test
+    public void isNumericShouldReturnFalseForNullInput() {
+        // The isNumeric method should handle null input gracefully by returning false.
+        boolean result = StringUtil.isNumeric(null);
+        assertFalse("isNumeric(null) should return false", result);
     }
 }
