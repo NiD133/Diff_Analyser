c154507e007c7491c5a58deb2e1494f1f9418beb
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:29:57 +0800

    gemini processedManualTestCases jsonarraytest11.java

--- jsonarraytest11.java
+++ jsonarraytest11.java
@@ -1,27 +1,38 @@
 package com.google.gson;
 
 import static com.google.common.truth.Truth.assertThat;
-import static org.junit.Assert.assertThrows;
-import com.google.common.testing.EqualsTester;
-import com.google.gson.common.MoreAsserts;
-import java.math.BigInteger;
+
 import org.junit.Test;
 
-public class JsonArrayTestTest11 {
+/**
+ * Test class for {@link JsonArray}.
+ * Renamed from JsonArrayTestTest11 for clarity.
+ */
+public class JsonArrayTest {
 
     @Test
-    public void testDoublePrimitiveAddition() {
-        JsonArray jsonArray = new JsonArray();
-        double x = 1.0;
-        jsonArray.add(x);
-        x = 2.13232;
-        jsonArray.add(x);
-        x = 0.121;
-        jsonArray.add(x);
-        jsonArray.add((Double) null);
-        x = -0.00234;
-        jsonArray.add(x);
-        jsonArray.add((Double) null);
-        assertThat(jsonArray.toString()).isEqualTo("[1.0,2.13232,0.121,null,-0.00234,null]");
+    public void add_withDoubleAndNull_buildsCorrectArray() {
+        // Arrange
+        JsonArray actualArray = new JsonArray();
+
+        // Act
+        actualArray.add(1.0);
+        actualArray.add(2.13232);
+        actualArray.add(0.121);
+        // According to JsonArray's contract, null values are converted to JsonNull
+        actualArray.add((Double) null);
+        actualArray.add(-0.00234);
+        actualArray.add((Double) null);
+
+        // Assert
+        JsonArray expectedArray = new JsonArray();
+        expectedArray.add(new JsonPrimitive(1.0));
+        expectedArray.add(new JsonPrimitive(2.13232));
+        expectedArray.add(new JsonPrimitive(0.121));
+        expectedArray.add(JsonNull.INSTANCE);
+        expectedArray.add(new JsonPrimitive(-0.00234));
+        expectedArray.add(JsonNull.INSTANCE);
+
+        assertThat(actualArray).isEqualTo(expectedArray);
     }
 }
