a5860c8bbb4fe32c103bcbbcf36b92f92829c314
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:49:37 +0800

    Original processedAutoTestCases enumordinaltypehandler_estest14.java

---
+++ enumordinaltypehandler_estest14.java
@@ -0,0 +1,27 @@
+package org.apache.ibatis.type;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class EnumOrdinalTypeHandler_ESTestTest14 extends EnumOrdinalTypeHandler_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test13() throws Throwable {
+        Class<JdbcType> class0 = JdbcType.class;
+        EnumOrdinalTypeHandler<JdbcType> enumOrdinalTypeHandler0 = new EnumOrdinalTypeHandler<JdbcType>(class0);
+        ResultSet resultSet0 = mock(ResultSet.class, new ViolatedAssumptionAnswer());
+        doReturn(0).when(resultSet0).getInt(anyString());
+        doReturn(false).when(resultSet0).wasNull();
+        JdbcType jdbcType0 = enumOrdinalTypeHandler0.getNullableResult(resultSet0, "+%+\"nk/Mp#S]");
+        assertEquals(JdbcType.ARRAY, jdbcType0);
+    }
+}
