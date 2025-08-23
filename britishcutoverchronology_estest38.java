2b32172c1d856b4c375dc412f617565838a079c0
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:39:52 +0800

    gemini processedAutoTestCases britishcutoverchronology_estest38.java

--- britishcutoverchronology_estest38.java
+++ britishcutoverchronology_estest38.java
@@ -1,48 +1,29 @@
 package org.threeten.extra.chrono;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.time.Clock;
-import java.time.DateTimeException;
-import java.time.Duration;
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
-import java.time.chrono.ChronoLocalDateTime;
-import java.time.chrono.ChronoPeriod;
-import java.time.chrono.ChronoZonedDateTime;
-import java.time.chrono.Era;
-import java.time.chrono.IsoEra;
-import java.time.chrono.JapaneseEra;
-import java.time.chrono.MinguoEra;
-import java.time.chrono.ThaiBuddhistEra;
-import java.time.format.ResolverStyle;
 import java.time.temporal.ChronoField;
-import java.time.temporal.ChronoUnit;
-import java.time.temporal.TemporalAccessor;
-import java.time.temporal.TemporalAmount;
-import java.time.temporal.TemporalField;
 import java.time.temporal.ValueRange;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.time.MockClock;
-import org.evosuite.runtime.mock.java.time.MockInstant;
-import org.evosuite.runtime.mock.java.time.MockZonedDateTime;
-import org.junit.runner.RunWith;
 
-public class BritishCutoverChronology_ESTestTest38 extends BritishCutoverChronology_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
 
-    @Test(timeout = 4000)
-    public void test37() throws Throwable {
-        BritishCutoverChronology britishCutoverChronology0 = new BritishCutoverChronology();
-        ChronoField chronoField0 = ChronoField.ALIGNED_WEEK_OF_MONTH;
-        ValueRange valueRange0 = britishCutoverChronology0.range(chronoField0);
-        assertNotNull(valueRange0);
+/**
+ * Tests for the range of fields in {@link BritishCutoverChronology}.
+ */
+public class BritishCutoverChronologyRangeTest {
+
+    @Test
+    public void range_forAlignedWeekOfMonth_returnsCorrectFixedRange() {
+        // Arrange
+        // The BritishCutoverChronology defines a specific, fixed range for ALIGNED_WEEK_OF_MONTH.
+        // This test verifies that the range() method returns this expected value.
+        BritishCutoverChronology chronology = BritishCutoverChronology.INSTANCE;
+        ValueRange expectedRange = ValueRange.of(1, 3, 5);
+
+        // Act
+        ValueRange actualRange = chronology.range(ChronoField.ALIGNED_WEEK_OF_MONTH);
+
+        // Assert
+        assertEquals("The range for ALIGNED_WEEK_OF_MONTH should be the fixed value defined by the chronology.",
+                     expectedRange, actualRange);
     }
 }
