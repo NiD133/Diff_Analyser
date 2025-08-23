f147e8ccd8f97dd77d15fd688b21a7aadf885789
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:32:09 +0800

    Original processedManualTestCases patternoptionbuildertest2.java

---
+++ patternoptionbuildertest2.java
@@ -0,0 +1,27 @@
+package org.apache.commons.cli;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Vector;
+import org.junit.jupiter.api.Test;
+
+public class PatternOptionBuilderTestTest2 {
+
+    @Test
+    void testEmptyPattern() {
+        final Options options = PatternOptionBuilder.parsePattern("");
+        assertTrue(options.getOptions().isEmpty());
+    }
+}
