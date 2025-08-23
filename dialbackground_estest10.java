c45185c82a0d468b064587f1a64e9c8f9225a7ec
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:48:03 +0800

    Original processedAutoTestCases dialbackground_estest10.java

---
+++ dialbackground_estest10.java
@@ -0,0 +1,43 @@
+package org.jfree.chart.plot.dial;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.awt.Color;
+import java.awt.GradientPaint;
+import java.awt.Graphics2D;
+import java.awt.Paint;
+import java.awt.Rectangle;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.jfree.chart.ChartRenderingInfo;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.entity.EntityCollection;
+import org.jfree.chart.plot.FastScatterPlot;
+import org.jfree.chart.plot.pie.PiePlot;
+import org.jfree.chart.util.GradientPaintTransformType;
+import org.jfree.chart.util.GradientPaintTransformer;
+import org.jfree.chart.util.StandardGradientPaintTransformer;
+import org.jfree.data.general.DefaultValueDataset;
+import org.junit.runner.RunWith;
+
+public class DialBackground_ESTestTest10 extends DialBackground_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test09() throws Throwable {
+        DialBackground dialBackground0 = new DialBackground();
+        DialPlot dialPlot0 = new DialPlot();
+        // Undeclared exception!
+        try {
+            dialBackground0.draw((Graphics2D) null, dialPlot0, (Rectangle2D) null, (Rectangle2D) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.jfree.chart.plot.dial.DialBackground", e);
+        }
+    }
+}
