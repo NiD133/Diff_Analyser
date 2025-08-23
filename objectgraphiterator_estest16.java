14f01da72fe3185d9feefd94f7aedebfe102c75b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:05:40 +0800

    gemini processedAutoTestCases objectgraphiterator_estest16.java

--- objectgraphiterator_estest16.java
+++ objectgraphiterator_estest16.java
@@ -1,68 +1,38 @@
 package org.apache.commons.collections4.iterators;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Comparator;
-import java.util.ConcurrentModificationException;
-import java.util.HashMap;
+
+import java.util.Collections;
 import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.ListIterator;
-import java.util.Map;
-import java.util.NoSuchElementException;
-import java.util.function.Function;
-import org.apache.commons.collections4.Factory;
-import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AndPredicate;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantFactory;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionPredicate;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.FactoryTransformer;
-import org.apache.commons.collections4.functors.FalsePredicate;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.MapTransformer;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NullIsFalsePredicate;
-import org.apache.commons.collections4.functors.NullIsTruePredicate;
-import org.apache.commons.collections4.functors.OrPredicate;
-import org.apache.commons.collections4.functors.PredicateTransformer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import java.util.List;
 
-public class ObjectGraphIterator_ESTestTest16 extends ObjectGraphIterator_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
-    @Test(timeout = 4000)
-    public void test15() throws Throwable {
-        LinkedList<Integer> linkedList0 = new LinkedList<Integer>();
-        Integer integer0 = new Integer(0);
-        linkedList0.add(integer0);
-        ListIterator<Integer> listIterator0 = linkedList0.listIterator();
-        ObjectGraphIterator<Object> objectGraphIterator0 = new ObjectGraphIterator<Object>(listIterator0);
-        objectGraphIterator0.hasNext();
-        objectGraphIterator0.findNextByIterator(listIterator0);
-        objectGraphIterator0.hasNext();
-        objectGraphIterator0.findNextByIterator(listIterator0);
-        // Undeclared exception!
+/**
+ * Contains tests for the {@link ObjectGraphIterator}.
+ * This class focuses on specific edge cases and contract adherence.
+ */
+public class ObjectGraphIterator_ESTestTest16 {
+
+    /**
+     * Tests that calling remove() before a call to next() throws an IllegalStateException.
+     * This behavior is mandated by the java.util.Iterator interface contract.
+     */
+    @Test
+    public void removeShouldThrowIllegalStateExceptionWhenCalledBeforeNext() {
+        // Arrange: Create an ObjectGraphIterator with a non-empty root iterator.
+        final List<Integer> sourceList = Collections.singletonList(42);
+        final Iterator<Integer> rootIterator = sourceList.iterator();
+        final ObjectGraphIterator<Object> graphIterator = new ObjectGraphIterator<>(rootIterator);
+
+        // Act & Assert: Attempting to call remove() before next() must throw an exception.
         try {
-            objectGraphIterator0.remove();
-            fail("Expecting exception: IllegalStateException");
-        } catch (IllegalStateException e) {
-            //
-            // Iterator remove() cannot be called at this time
-            //
-            verifyException("org.apache.commons.collections4.iterators.ObjectGraphIterator", e);
+            graphIterator.remove();
+            fail("Expected an IllegalStateException to be thrown.");
+        } catch (final IllegalStateException e) {
+            // Verify the exception message to ensure the failure is for the correct reason.
+            assertEquals("Iterator remove() cannot be called at this time", e.getMessage());
         }
     }
 }
