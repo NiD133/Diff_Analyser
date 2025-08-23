ef9e5bb6bbaac275bdbaef21cd57dd2ca37222dd
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:03:49 +0800

    gemini processedAutoTestCases multibackgroundinitializer_estest20.java

--- multibackgroundinitializer_estest20.java
+++ multibackgroundinitializer_estest20.java
@@ -1,39 +1,30 @@
 package org.apache.commons.lang3.concurrent;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.NoSuchElementException;
+
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.PriorityBlockingQueue;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.evosuite.runtime.mock.java.lang.MockException;
-import org.junit.runner.RunWith;
+import java.util.concurrent.Executors;
 
-public class MultiBackgroundInitializer_ESTestTest20 extends MultiBackgroundInitializer_ESTest_scaffolding {
+/**
+ * Test suite for {@link MultiBackgroundInitializer}.
+ */
+public class MultiBackgroundInitializerTest {
 
-    @Test(timeout = 4000)
-    public void test19() throws Throwable {
-        ForkJoinPool forkJoinPool0 = ForkJoinPool.commonPool();
-        MultiBackgroundInitializer multiBackgroundInitializer0 = new MultiBackgroundInitializer(forkJoinPool0);
-        BackgroundInitializer<Object> backgroundInitializer0 = new BackgroundInitializer<Object>(forkJoinPool0);
-        // Undeclared exception!
-        try {
-            multiBackgroundInitializer0.addInitializer((String) null, backgroundInitializer0);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // name
-            //
-            verifyException("java.util.Objects", e);
-        }
+    /**
+     * Tests that addInitializer() throws a NullPointerException when the initializer name is null.
+     */
+    @Test(expected = NullPointerException.class)
+    public void addInitializerShouldThrowNullPointerExceptionWhenNameIsNull() {
+        // Arrange
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        MultiBackgroundInitializer multiInitializer = new MultiBackgroundInitializer(executor);
+        BackgroundInitializer<Object> childInitializer = new BackgroundInitializer<>(executor);
+
+        // Act: Attempt to add an initializer with a null name.
+        // This action is expected to throw a NullPointerException.
+        multiInitializer.addInitializer(null, childInitializer);
+
+        // Assert: The test passes if the expected NullPointerException is thrown.
+        // This is handled by the @Test(expected = ...) annotation.
     }
 }
