4020d2c3359ad22fda8cb4407cf277d7a5c4f835
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:30:06 +0800

    gemini processedManualTestCases jsonparsertest1.java

--- jsonparsertest1.java
+++ jsonparsertest1.java
@@ -1,20 +1,20 @@
 package com.google.gson;
 
-import static com.google.common.truth.Truth.assertThat;
 import static org.junit.Assert.assertThrows;
-import com.google.gson.common.TestTypes.BagOfPrimitives;
-import com.google.gson.internal.Streams;
-import com.google.gson.stream.JsonReader;
-import java.io.CharArrayReader;
-import java.io.CharArrayWriter;
-import java.io.IOException;
-import java.io.StringReader;
+
 import org.junit.Test;
 
-public class JsonParserTestTest1 {
+/**
+ * Unit tests for {@link JsonParser}.
+ */
+public class JsonParserTest {
 
-    @Test
-    public void testParseInvalidJson() {
-        assertThrows(JsonSyntaxException.class, () -> JsonParser.parseString("[[]"));
-    }
+  @Test
+  public void parseString_withMalformedJson_throwsJsonSyntaxException() {
+    // This JSON string is malformed because the inner array is not closed.
+    String malformedJson = "[[]";
+
+    // Expect a JsonSyntaxException when trying to parse malformed JSON.
+    assertThrows(JsonSyntaxException.class, () -> JsonParser.parseString(malformedJson));
+  }
 }
