e989438e195a90077cacda11c034e1c72ad864bb
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:41:05 +0800

    Original processedAutoTestCases byteutils_estest34.java

---
+++ byteutils_estest34.java
@@ -0,0 +1,44 @@
+package org.apache.commons.compress.utils;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInput;
+import java.io.DataInputStream;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.io.PushbackInputStream;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.io.MockFileOutputStream;
+import org.junit.runner.RunWith;
+
+public class ByteUtils_ESTestTest34 extends ByteUtils_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test33() throws Throwable {
+        byte[] byteArray0 = new byte[2];
+        ByteArrayInputStream byteArrayInputStream0 = new ByteArrayInputStream(byteArray0);
+        DataInputStream dataInputStream0 = new DataInputStream(byteArrayInputStream0);
+        try {
+            ByteUtils.fromLittleEndian((DataInput) dataInputStream0, 6);
+            fail("Expecting exception: EOFException");
+        } catch (EOFException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("java.io.DataInputStream", e);
+        }
+    }
+}
