590317657111fc00acc8a31ba5bc302880cb2e08
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:38:57 +0800

    Original processedAutoTestCases bboxcalculator_estest19.java

---
+++ bboxcalculator_estest19.java
@@ -0,0 +1,28 @@
+package org.locationtech.spatial4j.shape.impl;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.HashMap;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+import org.locationtech.spatial4j.context.SpatialContext;
+import org.locationtech.spatial4j.context.SpatialContextFactory;
+import org.locationtech.spatial4j.distance.GeodesicSphereDistCalc;
+import org.locationtech.spatial4j.shape.Point;
+import org.locationtech.spatial4j.shape.Rectangle;
+
+public class BBoxCalculator_ESTestTest19 extends BBoxCalculator_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test18() throws Throwable {
+        SpatialContext spatialContext0 = SpatialContext.GEO;
+        BBoxCalculator bBoxCalculator0 = new BBoxCalculator(spatialContext0);
+        bBoxCalculator0.expandRange((-180.0), (-431.7), (-180.0), (-431.7));
+        bBoxCalculator0.getMaxX();
+        boolean boolean0 = bBoxCalculator0.doesXWorldWrap();
+        assertEquals((-180.0), bBoxCalculator0.getMinY(), 0.01);
+        assertFalse(boolean0);
+    }
+}
