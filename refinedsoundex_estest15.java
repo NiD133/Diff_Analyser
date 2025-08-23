5d781046da688eca11bf3691ad3b97aab022a4b2
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:12:31 +0800

    Original processedAutoTestCases refinedsoundex_estest15.java

---
+++ refinedsoundex_estest15.java
@@ -0,0 +1,25 @@
+package org.apache.commons.codec.language;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class RefinedSoundex_ESTestTest15 extends RefinedSoundex_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test14() throws Throwable {
+        RefinedSoundex refinedSoundex0 = null;
+        try {
+            refinedSoundex0 = new RefinedSoundex((char[]) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.apache.commons.codec.language.RefinedSoundex", e);
+        }
+    }
+}
