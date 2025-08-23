4b1b1589ed4cf5ae3c32c59247735b5e00861b3e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:51:15 +0800

    gemini processedAutoTestCases filterlistiterator_estest53.java

--- filterlistiterator_estest53.java
+++ filterlistiterator_estest53.java
@@ -1,58 +1,24 @@
 package org.apache.commons.collections4.iterators;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.ConcurrentModificationException;
-import java.util.LinkedList;
-import java.util.ListIterator;
-import java.util.Map;
-import java.util.NoSuchElementException;
-import java.util.function.Consumer;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.Equator;
-import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ClosureTransformer;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.DefaultEquator;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionClosure;
-import org.apache.commons.collections4.functors.ExceptionPredicate;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.IfClosure;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.MapTransformer;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NotNullPredicate;
-import org.apache.commons.collections4.functors.NotPredicate;
-import org.apache.commons.collections4.functors.NullIsExceptionPredicate;
-import org.apache.commons.collections4.functors.NullIsFalsePredicate;
-import org.apache.commons.collections4.functors.NullIsTruePredicate;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.OnePredicate;
-import org.apache.commons.collections4.functors.TransformedPredicate;
-import org.apache.commons.collections4.functors.TransformerPredicate;
-import org.apache.commons.collections4.functors.TruePredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.apache.commons.collections4.functors.WhileClosure;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertFalse;
 
-public class FilterListIterator_ESTestTest53 extends FilterListIterator_ESTest_scaffolding {
+/**
+ * Unit tests for {@link FilterListIterator}.
+ */
+public class FilterListIteratorTest {
 
-    @Test(timeout = 4000)
-    public void test52() throws Throwable {
-        FilterListIterator<Transformer<Object, Object>> filterListIterator0 = new FilterListIterator<Transformer<Object, Object>>();
-        boolean boolean0 = filterListIterator0.hasNext();
-        assertFalse(boolean0);
+    /**
+     * Tests that hasNext() returns false when the FilterListIterator is created
+     * with the default constructor and has no underlying iterator set.
+     */
+    @Test
+    public void hasNextShouldReturnFalseWhenNoIteratorIsSet() {
+        // Arrange: Create a FilterListIterator using the default constructor.
+        // This iterator is not backed by any collection.
+        FilterListIterator<Object> emptyIterator = new FilterListIterator<>();
+
+        // Act & Assert: Verify that hasNext() correctly reports no more elements.
+        assertFalse("hasNext() must return false for an iterator with no underlying collection", emptyIterator.hasNext());
     }
 }
