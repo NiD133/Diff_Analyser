2ca26d5bcb11f31a30acb755f7397672d18c0d96
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:43:36 +0800

    Original processedAutoTestCases cleaningpathvisitor_estest6.java

---
+++ cleaningpathvisitor_estest6.java
@@ -0,0 +1,39 @@
+package org.apache.commons.io.file;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.junit.runner.RunWith;
+
+public class CleaningPathVisitor_ESTestTest6 extends CleaningPathVisitor_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test05() throws Throwable {
+        CountingPathVisitor countingPathVisitor0 = CleaningPathVisitor.withBigIntegerCounters();
+        MockFile mockFile0 = new MockFile("bQ3a(", "bQ3a(");
+        Path path0 = mockFile0.toPath();
+        // Undeclared exception!
+        try {
+            countingPathVisitor0.visitFile(path0, (BasicFileAttributes) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.apache.commons.io.file.CountingPathVisitor", e);
+        }
+    }
+}
