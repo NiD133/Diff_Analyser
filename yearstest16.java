077ea41d790c781fb16070c81adb570c4bed1652
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:36:28 +0800

    gemini processedManualTestCases yearstest16.java

--- yearstest16.java
+++ yearstest16.java
@@ -1,44 +1,50 @@
 package org.joda.time;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
-public class YearsTestTest16 extends TestCase {
+/**
+ * Test suite for the minus(int) method of the {@link Years} class.
+ */
+public class YearsTest {
 
-    // (before the late 90's they were all over the place)
-    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
-
-    public static void main(String[] args) {
-        junit.textui.TestRunner.run(suite());
+    @Test
+    public void minus_shouldSubtractValueAndReturnNewInstance() {
+        // Arrange
+        final Years initialYears = Years.years(2);
+        final int yearsToSubtract = 3;
+        
+        // Act
+        final Years result = initialYears.minus(yearsToSubtract);
+        
+        // Assert
+        // Check that the new instance has the correct subtracted value
+        assertEquals("2 years - 3 years should be -1 years", -1, result.getYears());
+        
+        // Verify that the original instance is immutable and remains unchanged
+        assertEquals("Original instance should not be mutated", 2, initialYears.getYears());
     }
 
-    public static TestSuite suite() {
-        return new TestSuite(TestYears.class);
+    @Test
+    public void minus_whenSubtractingZero_shouldReturnEqualInstance() {
+        // Arrange
+        final Years oneYear = Years.ONE;
+        
+        // Act
+        final Years result = oneYear.minus(0);
+        
+        // Assert
+        assertEquals("Subtracting zero should not change the value", 1, result.getYears());
     }
 
-    @Override
-    protected void setUp() throws Exception {
-    }
-
-    @Override
-    protected void tearDown() throws Exception {
-    }
-
-    public void testMinus_int() {
-        Years test2 = Years.years(2);
-        Years result = test2.minus(3);
-        assertEquals(2, test2.getYears());
-        assertEquals(-1, result.getYears());
-        assertEquals(1, Years.ONE.minus(0).getYears());
-        try {
-            Years.MIN_VALUE.minus(1);
-            fail();
-        } catch (ArithmeticException ex) {
-            // expected
-        }
+    @Test(expected = ArithmeticException.class)
+    public void minus_whenResultUnderflows_shouldThrowArithmeticException() {
+        // Arrange
+        final Years minYears = Years.MIN_VALUE;
+        
+        // Act: This operation should cause an integer underflow and throw an exception.
+        minYears.minus(1);
+        
+        // Assert: The test passes if an ArithmeticException is thrown.
     }
 }
