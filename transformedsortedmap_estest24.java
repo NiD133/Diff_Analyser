144e26e3e78a6e81c919617d95fc0cd7344ada83
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:23:31 +0800

    Original processedAutoTestCases transformedsortedmap_estest24.java

---
+++ transformedsortedmap_estest24.java
@@ -0,0 +1,47 @@
+package org.apache.commons.collections4.map;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.lang.reflect.Array;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.NoSuchElementException;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import org.apache.commons.collections4.Closure;
+import org.apache.commons.collections4.Factory;
+import org.apache.commons.collections4.Predicate;
+import org.apache.commons.collections4.Transformer;
+import org.apache.commons.collections4.functors.ChainedTransformer;
+import org.apache.commons.collections4.functors.ConstantFactory;
+import org.apache.commons.collections4.functors.ConstantTransformer;
+import org.apache.commons.collections4.functors.ExceptionTransformer;
+import org.apache.commons.collections4.functors.FactoryTransformer;
+import org.apache.commons.collections4.functors.InvokerTransformer;
+import org.apache.commons.collections4.functors.MapTransformer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class TransformedSortedMap_ESTestTest24 extends TransformedSortedMap_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test23() throws Throwable {
+        TransformedSortedMap<Object, Integer> transformedSortedMap0 = null;
+        try {
+            transformedSortedMap0 = new TransformedSortedMap<Object, Integer>((SortedMap<Object, Integer>) null, (Transformer<? super Object, ?>) null, (Transformer<? super Integer, ? extends Integer>) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // map
+            //
+            verifyException("java.util.Objects", e);
+        }
+    }
+}
