5f23139dbaa83cbab17edf569d6c791fd0dd7e46
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:13:04 +0800

    gemini processedAutoTestCases seconds_estest46.java

--- seconds_estest46.java
+++ seconds_estest46.java
@@ -1,26 +1,30 @@
 package org.joda.time;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
-public class Seconds_ESTestTest46 extends Seconds_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link Seconds} class.
+ */
+public class SecondsTest {
 
-    @Test(timeout = 4000)
-    public void test45() throws Throwable {
-        Seconds seconds0 = Seconds.MAX_VALUE;
-        // Undeclared exception!
-        try {
-            seconds0.dividedBy(0);
-            fail("Expecting exception: ArithmeticException");
-        } catch (ArithmeticException e) {
-            //
-            // / by zero
-            //
-            verifyException("org.joda.time.Seconds", e);
-        }
+    /**
+     * Tests that dividing a Seconds instance by zero throws an ArithmeticException.
+     */
+    @Test
+    public void dividedBy_zeroDivisor_throwsArithmeticException() {
+        // Arrange
+        Seconds seconds = Seconds.ONE;
+
+        // Act & Assert
+        // We expect an ArithmeticException to be thrown when dividing by zero.
+        ArithmeticException thrown = assertThrows(
+            ArithmeticException.class,
+            () -> seconds.dividedBy(0)
+        );
+
+        // Verify that the exception message is as expected.
+        assertEquals("/ by zero", thrown.getMessage());
     }
 }
