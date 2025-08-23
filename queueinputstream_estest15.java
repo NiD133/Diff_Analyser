c9248c70554e549d4db3cdef4079132c615a03fd
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:10:19 +0800

    Original processedAutoTestCases queueinputstream_estest15.java

---
+++ queueinputstream_estest15.java
@@ -0,0 +1,24 @@
+package org.apache.commons.io.input;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.PriorityBlockingQueue;
+import org.apache.commons.io.output.QueueOutputStream;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class QueueInputStream_ESTestTest15 extends QueueInputStream_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test14() throws Throwable {
+        QueueInputStream.Builder queueInputStream_Builder0 = new QueueInputStream.Builder();
+        QueueInputStream.Builder queueInputStream_Builder1 = queueInputStream_Builder0.setTimeout((Duration) null);
+        assertSame(queueInputStream_Builder0, queueInputStream_Builder1);
+    }
+}
