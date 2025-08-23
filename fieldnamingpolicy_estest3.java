9ef091a0d67ffa8883f466ec52d6534147235012
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:50:10 +0800

    gemini processedAutoTestCases fieldnamingpolicy_estest3.java

--- fieldnamingpolicy_estest3.java
+++ fieldnamingpolicy_estest3.java
@@ -1,26 +1,18 @@
 package com.google.gson;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Field;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class FieldNamingPolicy_ESTestTest3 extends FieldNamingPolicy_ESTest_scaffolding {
+/**
+ * Tests for the static helper methods in {@link FieldNamingPolicy}.
+ */
+public class FieldNamingPolicyTest {
 
-    @Test(timeout = 4000)
-    public void test02() throws Throwable {
-        // Undeclared exception!
-        try {
-            FieldNamingPolicy.upperCaseFirstLetter((String) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("com.google.gson.FieldNamingPolicy", e);
-        }
+    /**
+     * Verifies that calling upperCaseFirstLetter with a null input
+     * correctly throws a NullPointerException.
+     */
+    @Test(expected = NullPointerException.class)
+    public void upperCaseFirstLetter_withNullInput_throwsNullPointerException() {
+        FieldNamingPolicy.upperCaseFirstLetter(null);
     }
 }
