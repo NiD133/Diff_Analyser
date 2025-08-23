c0a47663d0ef02f451390e886ec9c6320efbdeb9
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:03:22 +0800

    gemini processedAutoTestCases minutes_estest61.java

--- minutes_estest61.java
+++ minutes_estest61.java
@@ -1,19 +1,32 @@
 package org.joda.time;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class Minutes_ESTestTest61 extends Minutes_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link Minutes} class.
+ */
+public class MinutesTest {
 
-    @Test(timeout = 4000)
-    public void test60() throws Throwable {
-        Minutes minutes0 = Minutes.minutes(Integer.MIN_VALUE);
-        Hours hours0 = minutes0.toStandardHours();
-        assertEquals(Integer.MIN_VALUE, minutes0.getMinutes());
-        assertEquals((-35791394), hours0.getHours());
+    /**
+     * Tests that converting the minimum possible number of minutes to hours
+     * correctly handles integer division and potential overflow.
+     */
+    @Test
+    public void toStandardHours_withMinValue_returnsCorrectlyCalculatedHours() {
+        // Arrange
+        // This is an edge case test using the smallest possible integer value for minutes.
+        Minutes minMinutes = Minutes.minutes(Integer.MIN_VALUE);
+
+        // The expected number of hours is calculated using integer division,
+        // which truncates any fractional part.
+        // -2,147,483,648 / 60 = -35,791,394
+        int expectedHours = Integer.MIN_VALUE / 60;
+
+        // Act
+        Hours actualHours = minMinutes.toStandardHours();
+
+        // Assert
+        assertEquals(expectedHours, actualHours.getHours());
     }
 }
