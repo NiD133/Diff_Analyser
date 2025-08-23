59778a351ab52df30dfd6534ba9da62d88ebfbe8
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:54:06 +0800

    gemini processedAutoTestCases indexedcollection_estest5.java

--- indexedcollection_estest5.java
+++ indexedcollection_estest5.java
@@ -1,59 +1,42 @@
 package org.apache.commons.collections4.collection;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.ConcurrentModificationException;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Set;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.Predicate;
 import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AllPredicate;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ChainedTransformer;
-import org.apache.commons.collections4.functors.CloneTransformer;
-import org.apache.commons.collections4.functors.ClosureTransformer;
-import org.apache.commons.collections4.functors.ConstantFactory;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.DefaultEquator;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.FactoryTransformer;
-import org.apache.commons.collections4.functors.FalsePredicate;
-import org.apache.commons.collections4.functors.ForClosure;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.NOPClosure;
 import org.apache.commons.collections4.functors.NOPTransformer;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NotNullPredicate;
-import org.apache.commons.collections4.functors.NullIsFalsePredicate;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.SwitchTransformer;
-import org.apache.commons.collections4.functors.TransformedPredicate;
-import org.apache.commons.collections4.functors.TransformerClosure;
-import org.apache.commons.collections4.functors.TransformerPredicate;
-import org.apache.commons.collections4.functors.TruePredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class IndexedCollection_ESTestTest5 extends IndexedCollection_ESTest_scaffolding {
+import java.util.ArrayList;
+import java.util.List;
 
-    @Test(timeout = 4000)
-    public void test04() throws Throwable {
-        LinkedList<Predicate<Object>> linkedList0 = new LinkedList<Predicate<Object>>();
-        Transformer<Predicate<Object>, Predicate<Object>> transformer0 = NOPTransformer.nopTransformer();
-        AllPredicate<Object> allPredicate0 = new AllPredicate<Object>((Predicate<? super Object>[]) null);
-        linkedList0.add((Predicate<Object>) allPredicate0);
-        IndexedCollection<Predicate<Object>, Predicate<Object>> indexedCollection0 = IndexedCollection.nonUniqueIndexedCollection((Collection<Predicate<Object>>) linkedList0, transformer0);
-        Predicate<Object> predicate0 = indexedCollection0.get(allPredicate0);
-        assertTrue(linkedList0.contains(predicate0));
+import static org.junit.Assert.assertSame;
+
+/**
+ * Contains tests for the {@link IndexedCollection} class.
+ */
+public class IndexedCollectionTest {
+
+    /**
+     * Tests that the get() method correctly retrieves an element using its key.
+     */
+    @Test
+    public void getShouldReturnCorrectObjectForExistingKey() {
+        // Arrange
+        final List<String> sourceList = new ArrayList<>();
+        final String elementToAdd = "test_element";
+        sourceList.add(elementToAdd);
+
+        // The NOPTransformer (No-Operation) uses the element itself as the key.
+        final Transformer<String, String> identityTransformer = NOPTransformer.nopTransformer();
+
+        final IndexedCollection<String, String> indexedCollection =
+                IndexedCollection.nonUniqueIndexedCollection(sourceList, identityTransformer);
+
+        // Act
+        // Retrieve the element using its value as the key.
+        final String retrievedElement = indexedCollection.get(elementToAdd);
+
+        // Assert
+        // The retrieved object should be the exact same instance that was added.
+        assertSame("The element retrieved via get() should be the same instance as the one originally added.",
+                     elementToAdd, retrievedElement);
     }
 }
