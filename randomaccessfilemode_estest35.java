7a0a7aa3da68a7db2d0b415184d25ccbb50083e8
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:11:06 +0800

    gemini processedAutoTestCases randomaccessfilemode_estest35.java

--- randomaccessfilemode_estest35.java
+++ randomaccessfilemode_estest35.java
@@ -1,30 +1,26 @@
 package org.apache.commons.io;
 
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.RandomAccessFile;
-import java.nio.file.OpenOption;
-import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
-import org.apache.commons.io.function.IOConsumer;
-import org.apache.commons.io.function.IOFunction;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.io.MockFile;
-import org.evosuite.runtime.testdata.EvoSuiteFile;
-import org.evosuite.runtime.testdata.FileSystemHandling;
-import org.junit.runner.RunWith;
 
-public class RandomAccessFileMode_ESTestTest35 extends RandomAccessFileMode_ESTest_scaffolding {
+/**
+ * Tests for the {@link RandomAccessFileMode} enum.
+ */
+public class RandomAccessFileModeTest {
 
-    @Test(timeout = 4000)
-    public void test34() throws Throwable {
-        RandomAccessFileMode randomAccessFileMode0 = RandomAccessFileMode.READ_WRITE_SYNC_ALL;
-        String string0 = randomAccessFileMode0.getMode();
-        assertEquals("rws", string0);
+    /**
+     * Tests that getMode() for READ_WRITE_SYNC_ALL returns the correct string "rws".
+     */
+    @Test
+    public void testGetModeForReadWriteSyncAllReturnsCorrectString() {
+        // Arrange
+        final RandomAccessFileMode mode = RandomAccessFileMode.READ_WRITE_SYNC_ALL;
+        final String expectedModeString = "rws";
+
+        // Act
+        final String actualModeString = mode.getMode();
+
+        // Assert
+        assertEquals("The mode string should match the expected value.", expectedModeString, actualModeString);
     }
 }
