7a69047a70bd110b2a58b698b06994841246e82c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:17:25 +0800

    Original processedAutoTestCases spatialcontextfactory_estest34.java

---
+++ spatialcontextfactory_estest34.java
@@ -0,0 +1,32 @@
+package org.locationtech.spatial4j.context;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.HashMap;
+import java.util.Map;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+import org.locationtech.spatial4j.io.PolyshapeReader;
+import org.locationtech.spatial4j.shape.ShapeFactory;
+
+public class SpatialContextFactory_ESTestTest34 extends SpatialContextFactory_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test33() throws Throwable {
+        HashMap<String, String> hashMap0 = new HashMap<String, String>();
+        hashMap0.put("wktShapeParserClass", "wktShapeParserClass");
+        ClassLoader classLoader0 = ClassLoader.getSystemClassLoader();
+        // Undeclared exception!
+        try {
+            SpatialContextFactory.makeSpatialContext(hashMap0, classLoader0);
+            fail("Expecting exception: RuntimeException");
+        } catch (RuntimeException e) {
+            //
+            // Unable to find format class
+            //
+            verifyException("org.locationtech.spatial4j.context.SpatialContextFactory", e);
+        }
+    }
+}
