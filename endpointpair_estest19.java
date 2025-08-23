22c6fc3ca1e35038cb8c3e06eceadd6824b228b1
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:49:12 +0800

    Original processedAutoTestCases endpointpair_estest19.java

---
+++ endpointpair_estest19.java
@@ -0,0 +1,25 @@
+package com.google.common.graph;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.google.common.collect.UnmodifiableIterator;
+import java.util.Locale;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class EndpointPair_ESTestTest19 extends EndpointPair_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test18() throws Throwable {
+        Integer integer0 = new Integer((-1068));
+        NetworkBuilder<Object, Object> networkBuilder0 = NetworkBuilder.directed();
+        StandardNetwork<Object, Object> standardNetwork0 = new StandardNetwork<Object, Object>(networkBuilder0);
+        Integer integer1 = new Integer(0);
+        EndpointPair<Object> endpointPair0 = EndpointPair.ordered((Object) integer0, (Object) integer1);
+        EndpointPair<Object> endpointPair1 = EndpointPair.of((Network<?, ?>) standardNetwork0, (Object) integer0, (Object) integer0);
+        boolean boolean0 = endpointPair0.equals(endpointPair1);
+        assertFalse(boolean0);
+    }
+}
