47eaf298016cf87e70759a0e2128f2b4b8a875d7
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:33:16 +0800

    gemini processedManualTestCases secondstest25.java

--- secondstest25.java
+++ secondstest25.java
@@ -1,47 +1,72 @@
 package org.joda.time;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+import org.junit.Test;
 
-public class SecondsTestTest25 extends TestCase {
+/**
+ * Unit tests for the Seconds class, focusing on the dividedBy(int) method.
+ */
+public class SecondsTest {
 
-    // (before the late 90's they were all over the place)
-    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
+    @Test
+    public void dividedBy_withEvenDivisor_returnsCorrectResult() {
+        // Given
+        final Seconds twelveSeconds = Seconds.seconds(12);
+        final Seconds expected = Seconds.seconds(6);
 
-    public static void main(String[] args) {
-        junit.textui.TestRunner.run(suite());
+        // When
+        Seconds actual = twelveSeconds.dividedBy(2);
+
+        // Then
+        assertEquals(expected, actual);
     }
 
-    public static TestSuite suite() {
-        return new TestSuite(TestSeconds.class);
+    @Test
+    public void dividedBy_withUnevenDivisor_truncatesResult() {
+        // Given
+        final Seconds twelveSeconds = Seconds.seconds(12);
+        // 12 / 5 = 2.4, which should be truncated to 2
+        final Seconds expected = Seconds.seconds(2);
+
+        // When
+        Seconds actual = twelveSeconds.dividedBy(5);
+
+        // Then
+        assertEquals(expected, actual);
     }
 
-    @Override
-    protected void setUp() throws Exception {
+    @Test
+    public void dividedBy_isImmutable() {
+        // Given
+        final Seconds originalSeconds = Seconds.seconds(12);
+        final int originalValue = originalSeconds.getSeconds();
+
+        // When
+        originalSeconds.dividedBy(2);
+
+        // Then
+        // Assert that the original object was not modified
+        assertEquals(originalValue, originalSeconds.getSeconds());
     }
 
-    @Override
-    protected void tearDown() throws Exception {
+    @Test
+    public void dividedBy_one_returnsSameInstance() {
+        // Given
+        final Seconds twelveSeconds = Seconds.seconds(12);
+
+        // When
+        Seconds result = twelveSeconds.dividedBy(1);
+
+        // Then
+        // Dividing by 1 is a no-op and should return the same instance for efficiency
+        assertSame(twelveSeconds, result);
     }
 
-    public void testDividedBy_int() {
-        Seconds test = Seconds.seconds(12);
-        assertEquals(6, test.dividedBy(2).getSeconds());
-        assertEquals(12, test.getSeconds());
-        assertEquals(4, test.dividedBy(3).getSeconds());
-        assertEquals(3, test.dividedBy(4).getSeconds());
-        assertEquals(2, test.dividedBy(5).getSeconds());
-        assertEquals(2, test.dividedBy(6).getSeconds());
-        assertSame(test, test.dividedBy(1));
-        try {
-            Seconds.ONE.dividedBy(0);
-            fail();
-        } catch (ArithmeticException ex) {
-            // expected
-        }
+    @Test(expected = ArithmeticException.class)
+    public void dividedBy_zero_throwsArithmeticException() {
+        // When
+        Seconds.ONE.dividedBy(0);
+        // Then: an ArithmeticException is expected
     }
 }
