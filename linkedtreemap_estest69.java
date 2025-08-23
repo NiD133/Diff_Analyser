99649649763f6fc4cb6223aaf2ea87ded55791bb
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:00:44 +0800

    gemini processedAutoTestCases linkedtreemap_estest69.java

--- linkedtreemap_estest69.java
+++ linkedtreemap_estest69.java
@@ -1,28 +1,37 @@
 package com.google.gson.internal;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.AbstractMap;
-import java.util.Comparator;
-import java.util.Map;
-import java.util.Set;
-import java.util.function.BiFunction;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class LinkedTreeMap_ESTestTest69 extends LinkedTreeMap_ESTest_scaffolding {
+/**
+ * Test suite for the internal Node class of LinkedTreeMap.
+ * This focuses on behavior not easily accessible through the public Map API.
+ */
+public class LinkedTreeMapNodeTest {
 
-    @Test(timeout = 4000)
-    public void test68() throws Throwable {
-        Integer integer0 = new Integer((-1960));
-        LinkedTreeMap<Integer, Integer> linkedTreeMap0 = new LinkedTreeMap<Integer, Integer>();
-        LinkedTreeMap.Node<Integer, Integer> linkedTreeMap_Node0 = linkedTreeMap0.find(integer0, true);
-        String string0 = linkedTreeMap_Node0.toString();
-        assertEquals(1, linkedTreeMap0.size());
-        assertEquals("-1960=null", string0);
+    /**
+     * Verifies that a new Node created via `find(key, create=true)`
+     * has a `toString()` representation of "key=null".
+     */
+    @Test
+    public void toStringOnNewNodeReturnsKeyEqualsNull() {
+        // Arrange
+        LinkedTreeMap<Integer, String> map = new LinkedTreeMap<>();
+        Integer key = -1960;
+        String expectedNodeString = "-1960=null";
+
+        // Act
+        // The find(key, true) method creates and returns a new node if the key is not present.
+        // The new node's value should be null by default.
+        LinkedTreeMap.Node<Integer, String> newNode = map.find(key, true);
+        String actualNodeString = newNode.toString();
+
+        // Assert
+        // 1. Check that a new node was indeed added to the map.
+        assertEquals("A new node should have been added to the map.", 1, map.size());
+
+        // 2. Check that the new node's string representation is correct.
+        assertEquals("The new node's toString() should format as 'key=null'.",
+                expectedNodeString, actualNodeString);
     }
 }
