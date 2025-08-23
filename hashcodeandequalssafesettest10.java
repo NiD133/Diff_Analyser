4e14ea8b8e46477c26381439378b4a13b8ef584f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:29:13 +0800

    gemini processedManualTestCases hashcodeandequalssafesettest10.java

--- hashcodeandequalssafesettest10.java
+++ hashcodeandequalssafesettest10.java
@@ -1,44 +1,23 @@
 package org.mockito.internal.util.collections;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.Mockito.mock;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Observer;
-import org.junit.Rule;
+
 import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnit;
-import org.mockito.junit.MockitoRule;
 
-public class HashCodeAndEqualsSafeSetTestTest10 {
-
-    @Rule
-    public MockitoRule r = MockitoJUnit.rule();
-
-    @Mock
-    private UnmockableHashCodeAndEquals mock1;
-
-    private static class UnmockableHashCodeAndEquals {
-
-        @Override
-        public final int hashCode() {
-            throw new NullPointerException("I'm failing on hashCode and I don't care");
-        }
-
-        @Override
-        public final boolean equals(Object obj) {
-            throw new NullPointerException("I'm failing on equals and I don't care");
-        }
-    }
+/**
+ * Tests for {@link HashCodeAndEqualsSafeSet}.
+ */
+public class HashCodeAndEqualsSafeSetTest {
 
     @Test
-    public void cloneIsNotSupported() {
-        assertThatThrownBy(() -> {
-            HashCodeAndEqualsSafeSet.of().clone();
-        }).isInstanceOf(CloneNotSupportedException.class);
+    public void cloneShouldThrowCloneNotSupportedException() {
+        // Arrange: Create an instance of the set.
+        HashCodeAndEqualsSafeSet set = HashCodeAndEqualsSafeSet.of();
+
+        // Act & Assert: Verify that calling clone() throws CloneNotSupportedException,
+        // as the class does not implement the Cloneable interface.
+        assertThatThrownBy(set::clone)
+            .isInstanceOf(CloneNotSupportedException.class)
+            .withMessage(null); // The default implementation has no message
     }
 }
