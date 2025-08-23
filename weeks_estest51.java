4874b196f69b7109c638d5cf646b53dbf37260a6
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:27:21 +0800

    gemini processedAutoTestCases weeks_estest51.java

--- weeks_estest51.java
+++ weeks_estest51.java
@@ -2,17 +2,30 @@ package org.joda.time;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class Weeks_ESTestTest51 extends Weeks_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link Weeks} class.
+ */
+public class WeeksTest {
 
-    @Test(timeout = 4000)
-    public void test50() throws Throwable {
-        Weeks weeks0 = Weeks.parseWeeks((String) null);
-        Weeks weeks1 = weeks0.plus(0);
-        assertEquals(0, weeks1.getWeeks());
+    /**
+     * Tests that adding zero to a Weeks instance does not change its value.
+     * This test specifically starts with a Weeks object created by parsing a null string,
+     * which is defined to result in zero weeks.
+     */
+    @Test
+    public void testPlus_addingZeroToWeeksFromNull_returnsZeroWeeks() {
+        // Arrange
+        // According to the Javadoc, parseWeeks(null) returns a Weeks object representing zero.
+        Weeks zeroWeeks = Weeks.parseWeeks(null);
+
+        // Act
+        // The operation under test: adding zero weeks.
+        Weeks result = zeroWeeks.plus(0);
+
+        // Assert
+        // The result should be equal to the canonical ZERO instance.
+        assertEquals("The number of weeks should be 0", 0, result.getWeeks());
+        assertSame("Adding 0 should return the canonical ZERO instance", Weeks.ZERO, result);
     }
 }
