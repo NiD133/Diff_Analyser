1b0b570c97a1c430e987c67eb4bbc8e50e9b77d0
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:28:11 +0800

    gemini processedAutoTestCases xmltreebuilder_estest5.java

--- xmltreebuilder_estest5.java
+++ xmltreebuilder_estest5.java
@@ -1,38 +1,40 @@
 package org.jsoup.parser;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.PipedReader;
-import java.io.PipedWriter;
-import java.io.Reader;
-import java.io.StringReader;
-import java.io.UncheckedIOException;
-import java.util.ArrayList;
-import java.util.NoSuchElementException;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.jsoup.nodes.Attributes;
-import org.jsoup.nodes.CDataNode;
-import org.jsoup.nodes.Comment;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.nodes.LeafNode;
-import org.jsoup.select.Elements;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class XmlTreeBuilder_ESTestTest5 extends XmlTreeBuilder_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
 
-    @Test(timeout = 4000)
-    public void test04() throws Throwable {
-        XmlTreeBuilder xmlTreeBuilder0 = new XmlTreeBuilder();
-        Document document0 = xmlTreeBuilder0.parse(">: ", ">: ");
-        Parser parser0 = new Parser(xmlTreeBuilder0);
-        StreamParser streamParser0 = new StreamParser(parser0);
-        streamParser0.parseFragment("http://www.w3.org/2000/svg", (Element) document0, "http://www.w3.org/XML/1998/namespace");
-        assertEquals(">: ", document0.location());
-        Comment comment0 = new Comment("KRq7");
-        xmlTreeBuilder0.insertLeafNode(comment0);
-        assertEquals("http://www.w3.org/XML/1998/namespace", xmlTreeBuilder0.defaultNamespace());
+/**
+ * Test suite for {@link XmlTreeBuilder}.
+ * This refactored test clarifies the original, auto-generated test's intent.
+ */
+public class XmlTreeBuilderTest {
+
+    /**
+     * Tests that the default namespace of the XmlTreeBuilder is updated from the base URI
+     * when a new fragment parsing operation is initialized.
+     */
+    @Test
+    public void defaultNamespaceIsSetFromBaseUriWhenParsingFragment() {
+        // Arrange
+        XmlTreeBuilder treeBuilder = new XmlTreeBuilder();
+        Parser parser = new Parser(treeBuilder);
+        
+        // The base URI provided to parseFragment is expected to set the builder's default namespace state.
+        String expectedNamespaceUri = "http://www.w3.org/XML/1998/namespace";
+        
+        // A simple context element and fragment are sufficient for this test.
+        Element context = new Document(""); 
+        String fragment = "<data>test</data>";
+
+        // Act
+        // Initializing a fragment parse, which should update the internal state of the tree builder.
+        parser.parseFragment(fragment, context, expectedNamespaceUri);
+
+        // Assert
+        // Verify that the builder's default namespace was set to the base URI of the parse.
+        assertEquals(expectedNamespaceUri, treeBuilder.defaultNamespace());
     }
 }
