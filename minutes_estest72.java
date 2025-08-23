8ba0f0fa55e9468183c3c7776c020240025dbd94
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:03:26 +0800

    gemini processedAutoTestCases minutes_estest72.java

--- minutes_estest72.java
+++ minutes_estest72.java
@@ -1,18 +1,23 @@
 package org.joda.time;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class Minutes_ESTestTest72 extends Minutes_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link Minutes} class.
+ */
+public class MinutesTest {
 
-    @Test(timeout = 4000)
-    public void test71() throws Throwable {
-        Minutes minutes0 = Minutes.ZERO;
-        Days days0 = minutes0.toStandardDays();
-        assertEquals(0, days0.getDays());
+    @Test
+    public void toStandardDays_forZeroMinutes_returnsZeroDays() {
+        // Arrange
+        Minutes zeroMinutes = Minutes.ZERO;
+        Days expectedDays = Days.ZERO;
+
+        // Act
+        Days actualDays = zeroMinutes.toStandardDays();
+
+        // Assert
+        assertEquals(expectedDays, actualDays);
     }
 }
