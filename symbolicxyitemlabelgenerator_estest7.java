7042af5f0e05d7b79389702b74a03ae131e6c07e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:19:21 +0800

    Original processedAutoTestCases symbolicxyitemlabelgenerator_estest7.java

---
+++ symbolicxyitemlabelgenerator_estest7.java
@@ -0,0 +1,27 @@
+package org.jfree.chart.labels;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.time.chrono.ChronoLocalDate;
+import java.util.Date;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.util.MockDate;
+import org.jfree.chart.date.SerialDate;
+import org.jfree.data.time.DynamicTimeSeriesCollection;
+import org.jfree.data.time.TimeSeriesCollection;
+import org.jfree.data.xy.DefaultHighLowDataset;
+import org.jfree.data.xy.DefaultTableXYDataset;
+import org.jfree.data.xy.XYDataset;
+import org.junit.runner.RunWith;
+
+public class SymbolicXYItemLabelGenerator_ESTestTest7 extends SymbolicXYItemLabelGenerator_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test6() throws Throwable {
+        SymbolicXYItemLabelGenerator symbolicXYItemLabelGenerator0 = new SymbolicXYItemLabelGenerator();
+        boolean boolean0 = symbolicXYItemLabelGenerator0.equals(symbolicXYItemLabelGenerator0);
+        assertTrue(boolean0);
+    }
+}
