a07a851b5cc1b8b8456a32d1ffbc2b9dc4d72d6b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:06:07 +0800

    gemini processedAutoTestCases observableinputstream_estest53.java

--- observableinputstream_estest53.java
+++ observableinputstream_estest53.java
@@ -1,52 +1,35 @@
 package org.apache.commons.io.input;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.BufferedInputStream;
 import java.io.ByteArrayInputStream;
-import java.io.DataInputStream;
-import java.io.FileDescriptor;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.ObjectInputStream;
-import java.io.PipedInputStream;
-import java.io.PipedOutputStream;
-import java.io.PushbackInputStream;
-import java.io.SequenceInputStream;
-import java.io.StringWriter;
-import java.nio.CharBuffer;
-import java.nio.file.NoSuchFileException;
-import java.security.MessageDigest;
-import java.util.Enumeration;
-import java.util.LinkedList;
-import java.util.List;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.evosuite.runtime.mock.java.io.MockFileInputStream;
-import org.evosuite.runtime.mock.java.io.MockIOException;
-import org.junit.runner.RunWith;
 
+/**
+ * This test verifies the behavior of the {@link ObservableInputStream} when
+ * {@code removeAllObservers()} is called on an instance created by a builder
+ * where no observers were configured.
+ */
 public class ObservableInputStream_ESTestTest53 extends ObservableInputStream_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test52() throws Throwable {
-        ObservableInputStream.Builder observableInputStream_Builder0 = new ObservableInputStream.Builder();
-        StringWriter stringWriter0 = new StringWriter();
-        StringBuffer stringBuffer0 = stringWriter0.getBuffer();
-        ObservableInputStream.Builder observableInputStream_Builder1 = observableInputStream_Builder0.setCharSequence(stringBuffer0);
-        ObservableInputStream observableInputStream0 = observableInputStream_Builder1.get();
-        // Undeclared exception!
-        try {
-            observableInputStream0.removeAllObservers();
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.commons.io.input.ObservableInputStream", e);
-        }
+    /**
+     * Tests that calling removeAllObservers() throws a NullPointerException if the
+     * stream was constructed via a builder without setting any observers. This occurs
+     * because the internal list of observers is not initialized in this scenario.
+     */
+    @Test(expected = NullPointerException.class)
+    public void removeAllObservers_throwsNpe_whenBuiltWithoutObservers() throws IOException {
+        // Arrange: Create an ObservableInputStream using a builder without providing an observer list.
+        // A dummy input stream is required for the builder to create an instance.
+        final InputStream dummyInputStream = new ByteArrayInputStream(new byte[0]);
+        final ObservableInputStream.Builder builder = new ObservableInputStream.Builder()
+                .setInputStream(dummyInputStream);
+        final ObservableInputStream observableInputStream = builder.get();
+
+        // Act: Attempt to remove all observers. This should fail because the internal
+        // observer list is null.
+        observableInputStream.removeAllObservers();
+
+        // Assert: The @Test(expected) annotation verifies that a NullPointerException is thrown.
     }
 }
