298eec6c55a023122145236fe25096d363cfaf6c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:37:05 +0800

    Original processedAutoTestCases accumulatorpathvisitor_estest24.java

---
+++ accumulatorpathvisitor_estest24.java
@@ -0,0 +1,44 @@
+package org.apache.commons.io.file;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Comparator;
+import java.util.List;
+import java.util.regex.Pattern;
+import org.apache.commons.io.filefilter.CanExecuteFileFilter;
+import org.apache.commons.io.filefilter.CanReadFileFilter;
+import org.apache.commons.io.filefilter.CanWriteFileFilter;
+import org.apache.commons.io.filefilter.HiddenFileFilter;
+import org.apache.commons.io.filefilter.PrefixFileFilter;
+import org.apache.commons.io.filefilter.RegexFileFilter;
+import org.apache.commons.io.function.IOBiFunction;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.evosuite.runtime.mock.java.io.MockIOException;
+import org.junit.runner.RunWith;
+
+public class AccumulatorPathVisitor_ESTestTest24 extends AccumulatorPathVisitor_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test23() throws Throwable {
+        AccumulatorPathVisitor accumulatorPathVisitor0 = new AccumulatorPathVisitor();
+        // Undeclared exception!
+        try {
+            accumulatorPathVisitor0.relativizeDirectories((Path) null, true, (Comparator<? super Path>) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("java.util.Objects", e);
+        }
+    }
+}
