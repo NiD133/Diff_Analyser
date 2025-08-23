df59e01a8bfaf624afe93acc47c8c95a9e5a4a8e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:29:35 +0800

    gemini processedAutoTestCases xydrawableannotation_estest25.java

--- xydrawableannotation_estest25.java
+++ xydrawableannotation_estest25.java
@@ -1,51 +1,50 @@
 package org.jfree.chart.annotations;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.awt.Font;
-import java.awt.Graphics2D;
-import java.awt.geom.Rectangle2D;
-import java.awt.geom.RoundRectangle2D;
-import java.awt.image.BufferedImage;
-import java.math.BigInteger;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.jfree.chart.ChartRenderingInfo;
 import org.jfree.chart.Drawable;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.axis.CyclicNumberAxis;
-import org.jfree.chart.axis.NumberAxis;
-import org.jfree.chart.axis.ValueAxis;
 import org.jfree.chart.block.BlockContainer;
-import org.jfree.chart.legend.LegendTitle;
-import org.jfree.chart.plot.MeterPlot;
-import org.jfree.chart.plot.PlotRenderingInfo;
-import org.jfree.chart.plot.PolarAxisLocation;
-import org.jfree.chart.plot.XYPlot;
-import org.jfree.chart.renderer.AbstractRenderer;
-import org.jfree.chart.renderer.category.ScatterRenderer;
-import org.jfree.chart.renderer.xy.XYBarRenderer;
-import org.jfree.chart.title.CompositeTitle;
-import org.jfree.chart.title.DateTitle;
-import org.jfree.chart.title.ShortTextTitle;
-import org.jfree.chart.title.TextTitle;
-import org.jfree.data.general.DefaultValueDataset;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class XYDrawableAnnotation_ESTestTest25 extends XYDrawableAnnotation_ESTest_scaffolding {
+import static org.junit.Assert.assertFalse;
 
-    @Test(timeout = 4000)
-    public void test24() throws Throwable {
-        BlockContainer blockContainer0 = new BlockContainer();
-        XYDrawableAnnotation xYDrawableAnnotation0 = new XYDrawableAnnotation(2929.6115057820657, 2929.6115057820657, 2929.6115057820657, 2929.6115057820657, 2929.6115057820657, blockContainer0);
-        XYDrawableAnnotation xYDrawableAnnotation1 = new XYDrawableAnnotation(2929.6115057820657, 2929.6115057820657, (-1095.6), 2929.6115057820657, (-1095.6), blockContainer0);
-        boolean boolean0 = xYDrawableAnnotation0.equals(xYDrawableAnnotation1);
-        assertEquals(2929.6115057820657, xYDrawableAnnotation1.getY(), 0.01);
-        assertEquals(2929.6115057820657, xYDrawableAnnotation1.getDisplayHeight(), 0.01);
-        assertFalse(boolean0);
-        assertEquals((-1095.6), xYDrawableAnnotation1.getDrawScaleFactor(), 0.01);
-        assertEquals((-1095.6), xYDrawableAnnotation1.getDisplayWidth(), 0.01);
-        assertEquals(2929.6115057820657, xYDrawableAnnotation1.getX(), 0.01);
+/**
+ * Unit tests for the {@link XYDrawableAnnotation} class, focusing on the equals() method.
+ */
+public class XYDrawableAnnotationTest {
+
+    /**
+     * Verifies that the equals() method returns false when comparing two annotations
+     * that have different properties.
+     */
+    @Test
+    public void testEqualsReturnsFalseForDifferentProperties() {
+        // Arrange
+        Drawable dummyDrawable = new BlockContainer();
+
+        // Create a base annotation for comparison.
+        XYDrawableAnnotation baseAnnotation = new XYDrawableAnnotation(
+                10.0,   // x
+                20.0,   // y
+                100.0,  // displayWidth
+                50.0,   // displayHeight
+                1.0,    // drawScaleFactor
+                dummyDrawable);
+
+        // Create another annotation with a different displayWidth and drawScaleFactor.
+        XYDrawableAnnotation differentAnnotation = new XYDrawableAnnotation(
+                10.0,   // x (same)
+                20.0,   // y (same)
+                200.0,  // displayWidth (different)
+                50.0,   // displayHeight (same)
+                2.0,    // drawScaleFactor (different)
+                dummyDrawable);
+
+        // Act & Assert
+        // The two annotations should not be considered equal.
+        boolean areEqual = baseAnnotation.equals(differentAnnotation);
+        assertFalse("Annotations with different properties should not be equal.", areEqual);
+
+        // It is also good practice to test for symmetry in the equals contract.
+        boolean areEqualSymmetric = differentAnnotation.equals(baseAnnotation);
+        assertFalse("The equals() method should be symmetric.", areEqualSymmetric);
     }
 }
