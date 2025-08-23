cc3809328b79ebd7e78a8628f2dce48c992fb95a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:40:43 +0800

    Original processedAutoTestCases bytearraybuilder_estest53.java

---
+++ bytearraybuilder_estest53.java
@@ -0,0 +1,21 @@
+package com.fasterxml.jackson.core.util;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class ByteArrayBuilder_ESTestTest53 extends ByteArrayBuilder_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test52() throws Throwable {
+        JsonRecyclerPools.NonRecyclingPool jsonRecyclerPools_NonRecyclingPool0 = JsonRecyclerPools.NonRecyclingPool.GLOBAL;
+        BufferRecycler bufferRecycler0 = jsonRecyclerPools_NonRecyclingPool0.acquirePooled();
+        ByteArrayBuilder byteArrayBuilder0 = new ByteArrayBuilder(bufferRecycler0, 1);
+        byteArrayBuilder0.release();
+        byteArrayBuilder0.release();
+        assertEquals(0, byteArrayBuilder0.getCurrentSegmentLength());
+    }
+}
