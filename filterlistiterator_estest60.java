88079f4f008d494aed42355cce95abd6d925b678
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:51:18 +0800

    gemini processedAutoTestCases filterlistiterator_estest60.java

--- filterlistiterator_estest60.java
+++ filterlistiterator_estest60.java
@@ -1,66 +1,28 @@
 package org.apache.commons.collections4.iterators;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.ConcurrentModificationException;
-import java.util.LinkedList;
-import java.util.ListIterator;
-import java.util.Map;
+import static org.junit.Assert.assertThrows;
+
 import java.util.NoSuchElementException;
-import java.util.function.Consumer;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.Equator;
-import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ClosureTransformer;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.DefaultEquator;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionClosure;
-import org.apache.commons.collections4.functors.ExceptionPredicate;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.IfClosure;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.MapTransformer;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NotNullPredicate;
-import org.apache.commons.collections4.functors.NotPredicate;
-import org.apache.commons.collections4.functors.NullIsExceptionPredicate;
-import org.apache.commons.collections4.functors.NullIsFalsePredicate;
-import org.apache.commons.collections4.functors.NullIsTruePredicate;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.OnePredicate;
-import org.apache.commons.collections4.functors.TransformedPredicate;
-import org.apache.commons.collections4.functors.TransformerPredicate;
-import org.apache.commons.collections4.functors.TruePredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.apache.commons.collections4.functors.WhileClosure;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class FilterListIterator_ESTestTest60 extends FilterListIterator_ESTest_scaffolding {
+/**
+ * Contains tests for the {@link FilterListIterator} class.
+ */
+public class FilterListIteratorTest {
 
-    @Test(timeout = 4000)
-    public void test59() throws Throwable {
-        FilterListIterator<Object> filterListIterator0 = new FilterListIterator<Object>();
-        // Undeclared exception!
-        try {
-            filterListIterator0.previous();
-            fail("Expecting exception: NoSuchElementException");
-        } catch (NoSuchElementException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.commons.collections4.iterators.FilterListIterator", e);
-        }
+    /**
+     * Verifies that calling previous() on a FilterListIterator that has not been
+     * initialized with an underlying iterator correctly throws a NoSuchElementException.
+     * This simulates iterating backward over an empty sequence.
+     */
+    @Test
+    public void previousShouldThrowNoSuchElementExceptionForUninitializedIterator() {
+        // Arrange: Create a FilterListIterator using its default constructor, which
+        // leaves the underlying iterator as null.
+        final FilterListIterator<Object> emptyIterator = new FilterListIterator<>();
+
+        // Act & Assert: Expect a NoSuchElementException when previous() is called,
+        // as there are no elements to retrieve.
+        assertThrows(NoSuchElementException.class, () -> emptyIterator.previous());
     }
 }
