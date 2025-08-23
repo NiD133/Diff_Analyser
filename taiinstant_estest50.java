355e5c7216bbf71c96f457be21171825a44e178d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:20:40 +0800

    Original processedAutoTestCases taiinstant_estest50.java

---
+++ taiinstant_estest50.java
@@ -0,0 +1,28 @@
+package org.threeten.extra.scale;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.format.DateTimeParseException;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAmount;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.time.MockClock;
+import org.evosuite.runtime.mock.java.time.MockInstant;
+import org.junit.runner.RunWith;
+
+public class TaiInstant_ESTestTest50 extends TaiInstant_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test49() throws Throwable {
+        TaiInstant taiInstant0 = TaiInstant.ofTaiSeconds(37L, 37L);
+        boolean boolean0 = taiInstant0.isAfter(taiInstant0);
+        assertFalse(boolean0);
+        assertEquals(37L, taiInstant0.getTaiSeconds());
+        assertEquals(37, taiInstant0.getNano());
+    }
+}
