6138445e50c13a054024358d1b0dcd7972f5bf98
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:29:40 +0800

    gemini processedManualTestCases iocasetest22.java

--- iocasetest22.java
+++ iocasetest22.java
@@ -1,49 +1,26 @@
 package org.apache.commons.io;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertSame;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.util.Arrays;
-import org.junit.jupiter.api.Test;
 
-public class IOCaseTestTest22 {
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-    private static final boolean WINDOWS = File.separatorChar == '\\';
+/**
+ * Tests for the {@link IOCase#toString()} method.
+ */
+class IOCaseToStringTest {
 
-    private void assert0(final byte[] arr) {
-        for (final byte e : arr) {
-            assertEquals(0, e);
-        }
-    }
+    @ParameterizedTest(name = "[{index}] {0}.toString() should return \"{1}\"")
+    @CsvSource({
+        "SENSITIVE, Sensitive",
+        "INSENSITIVE, Insensitive",
+        "SYSTEM, System"
+    })
+    void toString_shouldReturnTheCorrectName(final IOCase ioCase, final String expectedName) {
+        // Act
+        final String actualName = ioCase.toString();
 
-    private void assert0(final char[] arr) {
-        for (final char e : arr) {
-            assertEquals(0, e);
-        }
-    }
-
-    private IOCase serialize(final IOCase value) throws Exception {
-        final ByteArrayOutputStream buf = new ByteArrayOutputStream();
-        try (ObjectOutputStream out = new ObjectOutputStream(buf)) {
-            out.writeObject(value);
-            out.flush();
-        }
-        final ByteArrayInputStream bufin = new ByteArrayInputStream(buf.toByteArray());
-        final ObjectInputStream in = new ObjectInputStream(bufin);
-        return (IOCase) in.readObject();
-    }
-
-    @Test
-    void test_toString() {
-        assertEquals("Sensitive", IOCase.SENSITIVE.toString());
-        assertEquals("Insensitive", IOCase.INSENSITIVE.toString());
-        assertEquals("System", IOCase.SYSTEM.toString());
+        // Assert
+        assertEquals(expectedName, actualName);
     }
 }
