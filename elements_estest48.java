a241f2bb94a4f6fb1eb383ca5045688da24c1ca2
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:48:50 +0800

    Original processedAutoTestCases elements_estest48.java

---
+++ elements_estest48.java
@@ -0,0 +1,36 @@
+package org.jsoup.select;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.ConcurrentModificationException;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.jsoup.nodes.Comment;
+import org.jsoup.nodes.DataNode;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.FormElement;
+import org.jsoup.nodes.TextNode;
+import org.jsoup.parser.Parser;
+import org.junit.runner.RunWith;
+
+public class Elements_ESTestTest48 extends Elements_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test047() throws Throwable {
+        Document document0 = Parser.parse("<m-2,eXTA:N5y7", "<m-2,eXTA:N5y7");
+        Elements elements0 = document0.children();
+        Elements elements1 = elements0.before("<m-2,eXTA:N5y7");
+        assertEquals(1, elements1.size());
+    }
+}
