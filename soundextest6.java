5c2efc6182a14ed5f7d7bbc6b0e9c5e88d566db5
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:33:57 +0800

    gemini processedManualTestCases soundextest6.java

--- soundextest6.java
+++ soundextest6.java
@@ -1,13 +1,16 @@
 package org.apache.commons.codec.language;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import org.apache.commons.codec.AbstractStringEncoderTest;
-import org.apache.commons.codec.EncoderException;
-import org.junit.jupiter.api.Test;
 
-public class SoundexTestTest6 extends AbstractStringEncoderTest<Soundex> {
+import org.apache.commons.codec.AbstractStringEncoderTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+/**
+ * Tests the {@link Soundex} encoder against a set of examples from the U.S. National Archives.
+ */
+public class SoundexTest extends AbstractStringEncoderTest<Soundex> {
 
     @Override
     protected Soundex createStringEncoder() {
@@ -15,18 +18,28 @@ public class SoundexTestTest6 extends AbstractStringEncoderTest<Soundex> {
     }
 
     /**
-     * Examples from http://www.archives.gov/research_room/genealogy/census/soundex.html
+     * Tests encoding based on examples from the
+     * <a href="http://www.archives.gov/research_room/genealogy/census/soundex.html">U.S. National Archives</a>.
+     * <p>
+     * Note: For "VanDeusen", the archives list D-250 as a possible encoding.
+     * This implementation produces V-532, which is also a valid interpretation of the Soundex algorithm.
+     * </p>
+     *
+     * @param name the input name to encode
+     * @param expectedCode the expected Soundex code
      */
-    @Test
-    void testEncodeBatch3() {
-        assertEquals("W252", getStringEncoder().encode("Washington"));
-        assertEquals("L000", getStringEncoder().encode("Lee"));
-        assertEquals("G362", getStringEncoder().encode("Gutierrez"));
-        assertEquals("P236", getStringEncoder().encode("Pfister"));
-        assertEquals("J250", getStringEncoder().encode("Jackson"));
-        assertEquals("T522", getStringEncoder().encode("Tymczak"));
-        // For VanDeusen: D-250 (D, 2 for the S, 5 for the N, 0 added) is also
-        // possible.
-        assertEquals("V532", getStringEncoder().encode("VanDeusen"));
+    @ParameterizedTest
+    @CsvSource({
+        "Washington, W252",
+        "Lee, L000",
+        "Gutierrez, G362",
+        "Pfister, P236",
+        "Jackson, J250",
+        "Tymczak, T522",
+        "VanDeusen, V532"
+    })
+    @DisplayName("Encoding '{0}' should result in '{1}'")
+    void testNationalArchivesExamples(final String name, final String expectedCode) {
+        assertEquals(expectedCode, getStringEncoder().encode(name));
     }
 }
