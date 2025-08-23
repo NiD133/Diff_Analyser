5d73f620ae91ade19418a0e092bf474dcc80f445
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:51:31 +0800

    Original processedAutoTestCases fixedordercomparator_estest5.java

---
+++ fixedordercomparator_estest5.java
@@ -0,0 +1,32 @@
+package org.apache.commons.collections4.comparators;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.lang.reflect.Array;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Function;
+import org.apache.commons.collections4.Predicate;
+import org.apache.commons.collections4.functors.ConstantTransformer;
+import org.apache.commons.collections4.functors.IdentityPredicate;
+import org.apache.commons.collections4.functors.InstanceofPredicate;
+import org.apache.commons.collections4.functors.PredicateTransformer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class FixedOrderComparator_ESTestTest5 extends FixedOrderComparator_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test04() throws Throwable {
+        LinkedList<Object> linkedList0 = new LinkedList<Object>();
+        FixedOrderComparator<Object> fixedOrderComparator0 = new FixedOrderComparator<Object>(linkedList0);
+        Object object0 = new Object();
+        FixedOrderComparator.UnknownObjectBehavior fixedOrderComparator_UnknownObjectBehavior0 = FixedOrderComparator.UnknownObjectBehavior.AFTER;
+        fixedOrderComparator0.setUnknownObjectBehavior(fixedOrderComparator_UnknownObjectBehavior0);
+        fixedOrderComparator0.compare(linkedList0, object0);
+        boolean boolean0 = fixedOrderComparator0.isLocked();
+        assertTrue(boolean0);
+    }
+}
