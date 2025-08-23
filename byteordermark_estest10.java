ac68e6540de48a159daf596c00828fe1b5741be6
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:40:50 +0800

    gemini processedAutoTestCases byteordermark_estest10.java

--- byteordermark_estest10.java
+++ byteordermark_estest10.java
@@ -1,19 +1,31 @@
 package org.apache.commons.io;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class ByteOrderMark_ESTestTest10 extends ByteOrderMark_ESTest_scaffolding {
+/**
+ * Contains tests for the {@link ByteOrderMark} class.
+ */
+public class ByteOrderMarkTest {
 
-    @Test(timeout = 4000)
-    public void test09() throws Throwable {
-        int[] intArray0 = new int[6];
-        ByteOrderMark byteOrderMark0 = new ByteOrderMark("Bt^D2-fqe2We[-^J#", intArray0);
-        String string0 = byteOrderMark0.toString();
-        assertEquals("ByteOrderMark[Bt^D2-fqe2We[-^J#: 0x0,0x0,0x0,0x0,0x0,0x0]", string0);
+    /**
+     * Tests that the toString() method produces a correctly formatted string,
+     * including the charset name and a hexadecimal representation of the BOM bytes.
+     */
+    @Test
+    public void testToStringFormat() {
+        // Arrange: Set up the test data and expected outcome.
+        final String charsetName = "CUSTOM-ENCODING";
+        final int[] bomBytes = {0xCA, 0xFE, 0xBA, 0xBE}; // Use distinct, non-zero values for a clear test.
+        final ByteOrderMark bom = new ByteOrderMark(charsetName, bomBytes);
+
+        // The expected format is "ByteOrderMark[<charsetName>: <hexBytes>]".
+        final String expectedString = "ByteOrderMark[CUSTOM-ENCODING: 0xCA,0xFE,0xBA,0xBE]";
+
+        // Act: Call the method under test.
+        final String actualString = bom.toString();
+
+        // Assert: Verify the result is as expected.
+        assertEquals("The toString() output should match the expected format.", expectedString, actualString);
     }
 }
