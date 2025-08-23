f79cf6b2959fd327ed171acc9540d4b125be4df9
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:54:12 +0800

    gemini processedAutoTestCases indexedcollection_estest9.java

--- indexedcollection_estest9.java
+++ indexedcollection_estest9.java
@@ -1,65 +1,46 @@
 package org.apache.commons.collections4.collection;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.ConcurrentModificationException;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Set;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.Predicate;
 import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AllPredicate;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ChainedTransformer;
-import org.apache.commons.collections4.functors.CloneTransformer;
-import org.apache.commons.collections4.functors.ClosureTransformer;
-import org.apache.commons.collections4.functors.ConstantFactory;
 import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.DefaultEquator;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.FactoryTransformer;
-import org.apache.commons.collections4.functors.FalsePredicate;
-import org.apache.commons.collections4.functors.ForClosure;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.NOPClosure;
-import org.apache.commons.collections4.functors.NOPTransformer;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NotNullPredicate;
-import org.apache.commons.collections4.functors.NullIsFalsePredicate;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.SwitchTransformer;
-import org.apache.commons.collections4.functors.TransformedPredicate;
-import org.apache.commons.collections4.functors.TransformerClosure;
-import org.apache.commons.collections4.functors.TransformerPredicate;
-import org.apache.commons.collections4.functors.TruePredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
+import java.util.LinkedList;
+
+/**
+ * Contains tests for edge cases in the {@link IndexedCollection} class.
+ * This class retains the original test's structure but improves its clarity.
+ */
 public class IndexedCollection_ESTestTest9 extends IndexedCollection_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test08() throws Throwable {
-        LinkedList<Object> linkedList0 = new LinkedList<Object>();
-        Transformer<Object, Object> transformer0 = ConstantTransformer.nullTransformer();
-        IndexedCollection<Object, Object> indexedCollection0 = IndexedCollection.nonUniqueIndexedCollection((Collection<Object>) linkedList0, transformer0);
-        indexedCollection0.add(linkedList0);
-        // Undeclared exception!
-        try {
-            indexedCollection0.values(linkedList0);
-            fail("Expecting exception: StackOverflowError");
-        } catch (StackOverflowError e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-        }
+    /**
+     * Tests that calling {@code values()} with a self-referential collection as a key
+     * results in a StackOverflowError.
+     * <p>
+     * This is an edge case where the underlying map, which powers the index,
+     * attempts to compute the hashCode of the key. For a collection that contains
+     * itself, this leads to infinite recursion.
+     */
+    @Test(timeout = 4000, expected = StackOverflowError.class)
+    public void valuesWithSelfReferentialListAsKeyShouldThrowStackOverflow() {
+        // Arrange
+        // A list that will be used as the decorated collection and also as an element within itself.
+        final LinkedList<Object> selfReferentialList = new LinkedList<>();
+
+        // A transformer is required for construction, but its behavior is not central to this test.
+        final Transformer<Object, Object> keyTransformer = ConstantTransformer.nullTransformer();
+        final IndexedCollection<Object, Object> indexedCollection =
+                IndexedCollection.nonUniqueIndexedCollection(selfReferentialList, keyTransformer);
+
+        // Add the list to the decorated collection. Since the decorated collection
+        // IS the list, the list now contains itself, creating a circular reference.
+        indexedCollection.add(selfReferentialList);
+
+        // Act
+        // Attempt to look up values using the self-referential list as the key.
+        // This triggers the list's hashCode() method, which will recurse infinitely
+        // and cause a StackOverflowError before any value can be returned.
+        indexedCollection.values(selfReferentialList);
+
+        // Assert is handled by the 'expected' parameter in the @Test annotation.
     }
 }
