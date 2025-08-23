6f8804ed881fbe00db741fb824c8bdf602835897
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:39:34 +0800

    Original processedAutoTestCases boundediterator_estest10.java

---
+++ boundediterator_estest10.java
@@ -0,0 +1,29 @@
+package org.apache.commons.collections4.iterators;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+import org.apache.commons.collections4.Closure;
+import org.apache.commons.collections4.Predicate;
+import org.apache.commons.collections4.functors.NOPClosure;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class BoundedIterator_ESTestTest10 extends BoundedIterator_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test09() throws Throwable {
+        LinkedList<Object> linkedList0 = new LinkedList<Object>();
+        Iterator<Object> iterator0 = linkedList0.iterator();
+        BoundedIterator<Object> boundedIterator0 = new BoundedIterator<Object>(iterator0, 0L, 0L);
+        boolean boolean0 = boundedIterator0.hasNext();
+        assertFalse(boolean0);
+    }
+}
