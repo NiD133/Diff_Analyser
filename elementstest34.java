d3dc8ef402dca545a25e846331ac0e96c32847fe
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:28:12 +0800

    Original processedManualTestCases elementstest34.java

---
+++ elementstest34.java
@@ -0,0 +1,24 @@
+package org.jsoup.select;
+
+import org.jsoup.Jsoup;
+import org.jsoup.TextUtil;
+import org.jsoup.nodes.Comment;
+import org.jsoup.nodes.DataNode;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.FormElement;
+import org.jsoup.nodes.Node;
+import org.jsoup.nodes.TextNode;
+import org.junit.jupiter.api.Test;
+import java.util.Iterator;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ElementsTestTest34 {
+
+    @Test
+    public void nodesEmpty() {
+        Document doc = Jsoup.parse("<p>");
+        assertEquals(0, doc.select("form").textNodes().size());
+    }
+}
