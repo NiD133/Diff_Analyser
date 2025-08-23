5f2a0590cbdab6860ffc53e4ce39821cbd0d7131
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:42:37 +0800

    Original processedAutoTestCases charsequenceutils_estest60.java

---
+++ charsequenceutils_estest60.java
@@ -0,0 +1,20 @@
+package org.apache.commons.lang3;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.nio.CharBuffer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class CharSequenceUtils_ESTestTest60 extends CharSequenceUtils_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test59() throws Throwable {
+        StringBuilder stringBuilder0 = new StringBuilder();
+        StringBuilder stringBuilder1 = stringBuilder0.appendCodePoint(65571);
+        int int0 = CharSequenceUtils.indexOf(stringBuilder1, 65571, (-2194));
+        assertEquals(0, int0);
+    }
+}
