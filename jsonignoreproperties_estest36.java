d48716e05a878d95e7e1415960cdb4e8c7bf3152
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:56:33 +0800

    Original processedAutoTestCases jsonignoreproperties_estest36.java

---
+++ jsonignoreproperties_estest36.java
@@ -0,0 +1,23 @@
+package com.fasterxml.jackson.annotation;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.function.Predicate;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class JsonIgnoreProperties_ESTestTest36 extends JsonIgnoreProperties_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test35() throws Throwable {
+        JsonIgnoreProperties.Value jsonIgnoreProperties_Value0 = JsonIgnoreProperties.Value.EMPTY;
+        Predicate<Object> predicate0 = Predicate.isEqual((Object) jsonIgnoreProperties_Value0);
+        boolean boolean0 = predicate0.test(jsonIgnoreProperties_Value0);
+        assertTrue(boolean0);
+    }
+}
