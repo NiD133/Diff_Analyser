069b4817150aa7fdcbf5f6af83849890965e1045
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:31:22 +0800

    Original processedManualTestCases multiinputstreamtest6.java

---
+++ multiinputstreamtest6.java
@@ -0,0 +1,44 @@
+package com.google.common.io;
+
+import java.io.ByteArrayInputStream;
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import org.jspecify.annotations.NullUnmarked;
+
+public class MultiInputStreamTestTest6 extends IoTestCase {
+
+    private void joinHelper(Integer... spans) throws Exception {
+        List<ByteSource> sources = new ArrayList<>();
+        int start = 0;
+        for (Integer span : spans) {
+            sources.add(newByteSource(start, span));
+            start += span;
+        }
+        ByteSource joined = ByteSource.concat(sources);
+        assertTrue(newByteSource(0, start).contentEquals(joined));
+    }
+
+    private static MultiInputStream tenMillionEmptySources() throws IOException {
+        return new MultiInputStream(Collections.nCopies(10_000_000, ByteSource.empty()).iterator());
+    }
+
+    private static ByteSource newByteSource(int start, int size) {
+        return new ByteSource() {
+
+            @Override
+            public InputStream openStream() {
+                return new ByteArrayInputStream(newPreFilledByteArray(start, size));
+            }
+        };
+    }
+
+    public void testReadArray_noStackOverflow() throws IOException {
+        // https://github.com/google/guava/issues/2996
+        // no data, just testing that there's no StackOverflowException
+        assertEquals(-1, tenMillionEmptySources().read(new byte[1]));
+    }
+}
