9b1b58fe9e23f206da86d5af6f6e4ec4a1f5c89d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:24:47 +0800

    Original processedManualTestCases arrayfilltest12.java

---
+++ arrayfilltest12.java
@@ -0,0 +1,22 @@
+package org.apache.commons.lang3;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import org.apache.commons.lang3.function.FailableIntFunction;
+import org.junit.jupiter.api.Test;
+
+public class ArrayFillTestTest12 extends AbstractLangTest {
+
+    @Test
+    void testFillIntArray() {
+        final int[] array = new int[3];
+        final int val = 1;
+        final int[] actual = ArrayFill.fill(array, val);
+        assertSame(array, actual);
+        for (final int v : actual) {
+            assertEquals(val, v);
+        }
+    }
+}
