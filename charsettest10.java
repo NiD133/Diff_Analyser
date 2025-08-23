1d8c226efc11ddb905263be410aaa1511b05f0e9
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:26:36 +0800

    Original processedManualTestCases charsettest10.java

---
+++ charsettest10.java
@@ -0,0 +1,24 @@
+package org.apache.commons.lang3;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.lang.reflect.Modifier;
+import org.junit.jupiter.api.Test;
+
+public class CharSetTestTest10 extends AbstractLangTest {
+
+    @Test
+    void testGetInstance() {
+        assertSame(CharSet.EMPTY, CharSet.getInstance((String) null));
+        assertSame(CharSet.EMPTY, CharSet.getInstance(""));
+        assertSame(CharSet.ASCII_ALPHA, CharSet.getInstance("a-zA-Z"));
+        assertSame(CharSet.ASCII_ALPHA, CharSet.getInstance("A-Za-z"));
+        assertSame(CharSet.ASCII_ALPHA_LOWER, CharSet.getInstance("a-z"));
+        assertSame(CharSet.ASCII_ALPHA_UPPER, CharSet.getInstance("A-Z"));
+        assertSame(CharSet.ASCII_NUMERIC, CharSet.getInstance("0-9"));
+    }
+}
