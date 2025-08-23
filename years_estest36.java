3d2236bf79c5d4cbd1b70ee68e6f61d7a303b983
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:30:22 +0800

    gemini processedAutoTestCases years_estest36.java

--- years_estest36.java
+++ years_estest36.java
@@ -1,18 +1,22 @@
 package org.joda.time;
 
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class Years_ESTestTest36 extends Years_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link Years} class.
+ */
+public class YearsTest {
 
-    @Test(timeout = 4000)
-    public void test35() throws Throwable {
-        Years years0 = Years.THREE;
-        Years years1 = years0.minus(years0);
-        assertEquals(0, years1.getYears());
+    @Test
+    public void minus_whenSubtractingYearsFromItself_shouldReturnZero() {
+        // Arrange
+        Years threeYears = Years.THREE;
+
+        // Act
+        Years result = threeYears.minus(threeYears);
+
+        // Assert
+        assertEquals(Years.ZERO, result);
     }
 }
