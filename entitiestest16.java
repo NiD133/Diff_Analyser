91ec4350416bbfa4017b138df689e9232c6c3bcc
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:28:23 +0800

    gemini processedManualTestCases entitiestest16.java

--- entitiestest16.java
+++ entitiestest16.java
@@ -1,18 +1,24 @@
 package org.jsoup.nodes;
 
-import org.jsoup.Jsoup;
-import org.jsoup.parser.Parser;
 import org.junit.jupiter.api.Test;
-import static org.jsoup.nodes.Document.OutputSettings;
-import static org.jsoup.nodes.Entities.EscapeMode.*;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class EntitiesTestTest16 {
+/**
+ * Tests for the {@link Entities} class.
+ */
+public class EntitiesTest {
 
     @Test
-    public void noSpuriousDecodes() {
-        String string = "http://www.foo.com?a=1&num_rooms=1&children=0&int=VA&b=2";
-        assertEquals(string, Entities.unescape(string));
+    void unescapeDoesNotAlterStringWithNonEntityAmpersands() {
+        // Arrange: A string containing ampersands that are not part of any valid HTML entity.
+        // A URL with query parameters is a common real-world example.
+        String urlWithNonEntityAmpersands = "http://www.foo.com?a=1&num_rooms=1&children=0&int=VA&b=2";
+
+        // Act: Attempt to unescape the string.
+        String result = Entities.unescape(urlWithNonEntityAmpersands);
+
+        // Assert: The string should remain unchanged. This verifies that substrings like
+        // "&num_rooms" or "&int" are not incorrectly interpreted as HTML entities.
+        assertEquals(urlWithNonEntityAmpersands, result);
     }
 }
