3659c1a35634a6705b2afd478fcf19751f7bc545
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:13:32 +0800

    Original processedAutoTestCases segmentconstantpoolarraycache_estest6.java

---
+++ segmentconstantpoolarraycache_estest6.java
@@ -0,0 +1,29 @@
+package org.apache.commons.compress.harmony.unpack200;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.IdentityHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class SegmentConstantPoolArrayCache_ESTestTest6 extends SegmentConstantPoolArrayCache_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test05() throws Throwable {
+        SegmentConstantPoolArrayCache segmentConstantPoolArrayCache0 = new SegmentConstantPoolArrayCache();
+        // Undeclared exception!
+        try {
+            segmentConstantPoolArrayCache0.indexesForArrayKey((String[]) null, "");
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.apache.commons.compress.harmony.unpack200.SegmentConstantPoolArrayCache$CachedArray", e);
+        }
+    }
+}
