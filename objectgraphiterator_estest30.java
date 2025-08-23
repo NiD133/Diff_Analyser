2398e8a62494b7f61069fbee7bcffe2089c57c78
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:05:45 +0800

    gemini processedAutoTestCases objectgraphiterator_estest30.java

--- objectgraphiterator_estest30.java
+++ objectgraphiterator_estest30.java
@@ -1,62 +1,28 @@
 package org.apache.commons.collections4.iterators;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Comparator;
-import java.util.ConcurrentModificationException;
-import java.util.HashMap;
+
+import java.util.Collections;
 import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.ListIterator;
-import java.util.Map;
-import java.util.NoSuchElementException;
-import java.util.function.Function;
-import org.apache.commons.collections4.Factory;
-import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AndPredicate;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantFactory;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionPredicate;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.FactoryTransformer;
-import org.apache.commons.collections4.functors.FalsePredicate;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.MapTransformer;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NullIsFalsePredicate;
-import org.apache.commons.collections4.functors.NullIsTruePredicate;
-import org.apache.commons.collections4.functors.OrPredicate;
-import org.apache.commons.collections4.functors.PredicateTransformer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
 
-public class ObjectGraphIterator_ESTestTest30 extends ObjectGraphIterator_ESTest_scaffolding {
+/**
+ * Unit tests for {@link ObjectGraphIterator}.
+ */
+public class ObjectGraphIteratorTest {
 
-    @Test(timeout = 4000)
-    public void test29() throws Throwable {
-        LinkedList<Integer> linkedList0 = new LinkedList<Integer>();
-        ListIterator<Integer> listIterator0 = linkedList0.listIterator();
-        ObjectGraphIterator<Object> objectGraphIterator0 = new ObjectGraphIterator<Object>(listIterator0);
-        // Undeclared exception!
-        try {
-            objectGraphIterator0.remove();
-            fail("Expecting exception: IllegalStateException");
-        } catch (IllegalStateException e) {
-            //
-            // Iterator remove() cannot be called at this time
-            //
-            verifyException("org.apache.commons.collections4.iterators.ObjectGraphIterator", e);
-        }
+    /**
+     * Tests that calling remove() before a call to next() throws an IllegalStateException.
+     * This behavior is required by the java.util.Iterator contract.
+     */
+    @Test(expected = IllegalStateException.class)
+    public void removeShouldThrowIllegalStateExceptionWhenCalledBeforeNext() {
+        // Arrange: Create an ObjectGraphIterator. An empty underlying iterator is
+        // sufficient, as we are testing the state before any iteration begins.
+        final Iterator<Object> emptyRootIterator = Collections.emptyIterator();
+        final ObjectGraphIterator<Object> graphIterator = new ObjectGraphIterator<>(emptyRootIterator);
+
+        // Act & Assert: Calling remove() immediately should throw an IllegalStateException.
+        // The @Test(expected) annotation handles the assertion.
+        graphIterator.remove();
     }
 }
