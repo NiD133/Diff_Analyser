189c8efd18fffd01832a72331a53d27416739bb6
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:28:01 +0800

    gemini processedManualTestCases deprecatedattributestest4.java

--- deprecatedattributestest4.java
+++ deprecatedattributestest4.java
@@ -1,12 +1,27 @@
 package org.apache.commons.cli;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class DeprecatedAttributesTestTest4 {
+/**
+ * Tests for the {@link DeprecatedAttributes} class.
+ */
+class DeprecatedAttributesTest {
 
     @Test
-    void testDefaultToString() {
-        assertEquals("Deprecated", DeprecatedAttributes.DEFAULT.toString());
+    @DisplayName("toString() on the DEFAULT instance should return a constant string")
+    void toStringForDefaultInstanceShouldReturnConstantString() {
+        // Arrange
+        final String expectedRepresentation = "Deprecated";
+        final DeprecatedAttributes defaultAttributes = DeprecatedAttributes.DEFAULT;
+
+        // Act
+        final String actualRepresentation = defaultAttributes.toString();
+
+        // Assert
+        assertEquals(expectedRepresentation, actualRepresentation,
+                "The string representation of the default instance should be a specific, constant value.");
     }
 }
