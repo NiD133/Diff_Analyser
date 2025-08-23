f7e5a46f84c4602da8134770fc71a4369ccf8771
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:43:31 +0800

    gemini processedAutoTestCases classloaderutil_estest4.java

--- classloaderutil_estest4.java
+++ classloaderutil_estest4.java
@@ -1,17 +1,26 @@
 package org.apache.commons.jxpath.util;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class ClassLoaderUtil_ESTestTest4 extends ClassLoaderUtil_ESTest_scaffolding {
+/**
+ * A test suite for the {@link ClassLoaderUtil} class.
+ */
+public class ClassLoaderUtilTest {
 
-    @Test(timeout = 4000)
-    public void test3() throws Throwable {
-        Class<String> class0 = ClassLoaderUtil.getClass("long", true);
-        assertFalse(class0.isAnnotation());
+    /**
+     * Verifies that {@link ClassLoaderUtil#getClass(String, boolean)}
+     * can correctly resolve the Class object for a primitive type name.
+     */
+    @Test
+    public void getClassShouldResolvePrimitiveTypeName() throws ClassNotFoundException {
+        // Arrange: Define the name of a primitive type.
+        String primitiveName = "long";
+
+        // Act: Call the method under test to get the class.
+        Class<?> resolvedClass = ClassLoaderUtil.getClass(primitiveName, true);
+
+        // Assert: The returned class should be exactly the primitive long class.
+        assertEquals(long.class, resolvedClass);
     }
 }
