db7d92691fc1da8ed4894e62890fea2134c12bec
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:56:43 +0800

    gemini processedAutoTestCases jsonignoreproperties_estest60.java

--- jsonignoreproperties_estest60.java
+++ jsonignoreproperties_estest60.java
@@ -1,27 +1,42 @@
 package com.fasterxml.jackson.annotation;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import java.util.LinkedHashSet;
+
+import java.util.Collections;
 import java.util.Set;
-import java.util.function.Predicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class JsonIgnoreProperties_ESTestTest60 extends JsonIgnoreProperties_ESTest_scaffolding {
 
+    /**
+     * Tests that the `JsonIgnoreProperties.Value.construct` factory method correctly
+     * creates an instance with the specified properties.
+     */
     @Test(timeout = 4000)
-    public void test59() throws Throwable {
-        JsonIgnoreProperties.Value jsonIgnoreProperties_Value0 = JsonIgnoreProperties.Value.from((JsonIgnoreProperties) null);
-        Set<String> set0 = jsonIgnoreProperties_Value0.findIgnoredForSerialization();
-        JsonIgnoreProperties.Value jsonIgnoreProperties_Value1 = JsonIgnoreProperties.Value.construct(set0, false, true, true, true);
-        boolean boolean0 = jsonIgnoreProperties_Value1.getIgnoreUnknown();
-        assertTrue(jsonIgnoreProperties_Value1.getAllowGetters());
-        assertTrue(jsonIgnoreProperties_Value1.getAllowSetters());
-        assertTrue(jsonIgnoreProperties_Value1.getMerge());
-        assertFalse(boolean0);
+    public void constructShouldCorrectlySetAllProperties() {
+        // Arrange: Define the properties for the Value object.
+        // The original test used a roundabout way to get an empty set; this is more direct.
+        Set<String> ignoredProperties = Collections.emptySet();
+        boolean ignoreUnknown = false;
+        boolean allowGetters = true;
+        boolean allowSetters = true;
+        boolean merge = true;
+
+        // Act: Construct the Value object using the factory method under test.
+        JsonIgnoreProperties.Value constructedValue = JsonIgnoreProperties.Value.construct(
+                ignoredProperties,
+                ignoreUnknown,
+                allowGetters,
+                allowSetters,
+                merge
+        );
+
+        // Assert: Verify that the getters return the values used during construction.
+        assertFalse("ignoreUnknown should be false", constructedValue.getIgnoreUnknown());
+        assertTrue("allowGetters should be true", constructedValue.getAllowGetters());
+        assertTrue("allowSetters should be true", constructedValue.getAllowSetters());
+        assertTrue("merge should be true", constructedValue.getMerge());
     }
 }
