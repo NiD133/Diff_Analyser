84789cabc6347dad53edf2ede0f4bac1f87f76d9
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:42:52 +0800

    Original processedAutoTestCases charset_estest15.java

---
+++ charset_estest15.java
@@ -0,0 +1,19 @@
+package org.apache.commons.lang3;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class CharSet_ESTestTest15 extends CharSet_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test14() throws Throwable {
+        String[] stringArray0 = new String[9];
+        CharSet charSet0 = CharSet.getInstance(stringArray0);
+        boolean boolean0 = charSet0.contains('T');
+        assertFalse(boolean0);
+    }
+}
