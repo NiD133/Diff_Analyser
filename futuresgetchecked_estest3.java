44e2811b0d3ec87d062a06239ec14652d9c52db3
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:51:40 +0800

    Original processedAutoTestCases futuresgetchecked_estest3.java

---
+++ futuresgetchecked_estest3.java
@@ -0,0 +1,32 @@
+package com.google.common.util.concurrent;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.sql.SQLException;
+import java.sql.SQLInvalidAuthorizationSpecException;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Delayed;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.lang.MockThread;
+import org.junit.runner.RunWith;
+
+public class FuturesGetChecked_ESTestTest3 extends FuturesGetChecked_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test02() throws Throwable {
+        SQLException sQLException0 = new SQLException("8Ov");
+        CompletableFuture<Exception> completableFuture0 = CompletableFuture.completedFuture((Exception) sQLException0);
+        Class<Exception> class0 = Exception.class;
+        FuturesGetChecked.getChecked((Future<Exception>) completableFuture0, class0);
+    }
+}
