4cef38806335e7d1a060056bc0e134fc1e32d81c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:02:14 +0800

    Original processedAutoTestCases mergedstream_estest36.java

---
+++ mergedstream_estest36.java
@@ -0,0 +1,46 @@
+package com.fasterxml.jackson.core.io;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.fasterxml.jackson.core.ErrorReportConfiguration;
+import com.fasterxml.jackson.core.StreamReadConstraints;
+import com.fasterxml.jackson.core.StreamWriteConstraints;
+import com.fasterxml.jackson.core.util.BufferRecycler;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.io.PushbackInputStream;
+import java.io.SequenceInputStream;
+import java.util.Enumeration;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.evosuite.runtime.mock.java.io.MockFileInputStream;
+import org.junit.runner.RunWith;
+
+public class MergedStream_ESTestTest36 extends MergedStream_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test35() throws Throwable {
+        byte[] byteArray0 = new byte[3];
+        MergedStream mergedStream0 = new MergedStream((IOContext) null, (InputStream) null, byteArray0, 0, (byte) 98);
+        // Undeclared exception!
+        try {
+            mergedStream0.close();
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("com.fasterxml.jackson.core.io.MergedStream", e);
+        }
+    }
+}
