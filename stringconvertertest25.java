31a6122c3e518a6fc1dfeb47d787d12c0751b9e8
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:34:10 +0800

    gemini processedManualTestCases stringconvertertest25.java

--- stringconvertertest25.java
+++ stringconvertertest25.java
@@ -1,87 +1,43 @@
 package org.joda.time.convert;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-import java.util.Arrays;
-import java.util.Locale;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-import org.joda.time.Chronology;
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
-import org.joda.time.MutableInterval;
+import static org.junit.Assert.assertEquals;
+
 import org.joda.time.MutablePeriod;
+import org.joda.time.Period;
 import org.joda.time.PeriodType;
-import org.joda.time.TimeOfDay;
-import org.joda.time.chrono.BuddhistChronology;
-import org.joda.time.chrono.ISOChronology;
-import org.joda.time.chrono.JulianChronology;
+import org.junit.Test;
 
-public class StringConverterTestTest25 extends TestCase {
+/**
+ * Unit tests for {@link StringConverter}.
+ * This test focuses on parsing period strings.
+ */
+public class StringConverterTest {
 
-    private static final DateTimeZone ONE_HOUR = DateTimeZone.forOffsetHours(1);
+    /**
+     * Tests that parsing an ISO 8601 period string with more than three fractional
+     * second digits correctly populates a MutablePeriod, truncating the fractions
+     * to millisecond precision.
+     */
+    @Test
+    public void setInto_periodFromString_shouldTruncateToMillisecondPrecision() {
+        // Arrange
+        // An ISO period string with nanosecond precision in the seconds field.
+        final String ISO_PERIOD_STRING_WITH_NANOS = "P2Y4W3DT12H24M56.1234567S";
+        
+        // The period type must include all fields present in the string.
+        final PeriodType periodType = PeriodType.yearWeekDayTime();
+        
+        // The expected period after parsing and truncation.
+        final Period expectedPeriod = new Period(2, 0, 4, 3, 12, 24, 56, 123, periodType);
+        
+        MutablePeriod actualPeriod = new MutablePeriod(periodType);
 
-    private static final DateTimeZone SIX = DateTimeZone.forOffsetHours(6);
+        // Act
+        StringConverter.INSTANCE.setInto(actualPeriod, ISO_PERIOD_STRING_WITH_NANOS, null);
 
-    private static final DateTimeZone SEVEN = DateTimeZone.forOffsetHours(7);
-
-    private static final DateTimeZone EIGHT = DateTimeZone.forOffsetHours(8);
-
-    private static final DateTimeZone UTC = DateTimeZone.UTC;
-
-    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
-
-    private static final DateTimeZone LONDON = DateTimeZone.forID("Europe/London");
-
-    private static final Chronology ISO_EIGHT = ISOChronology.getInstance(EIGHT);
-
-    private static final Chronology ISO_PARIS = ISOChronology.getInstance(PARIS);
-
-    private static final Chronology ISO_LONDON = ISOChronology.getInstance(LONDON);
-
-    private static Chronology ISO;
-
-    private static Chronology JULIAN;
-
-    private DateTimeZone zone = null;
-
-    private Locale locale = null;
-
-    public static void main(String[] args) {
-        junit.textui.TestRunner.run(suite());
-    }
-
-    public static TestSuite suite() {
-        return new TestSuite(TestStringConverter.class);
-    }
-
-    @Override
-    protected void setUp() throws Exception {
-        zone = DateTimeZone.getDefault();
-        locale = Locale.getDefault();
-        DateTimeZone.setDefault(LONDON);
-        Locale.setDefault(Locale.UK);
-        JULIAN = JulianChronology.getInstance();
-        ISO = ISOChronology.getInstance();
-    }
-
-    @Override
-    protected void tearDown() throws Exception {
-        DateTimeZone.setDefault(zone);
-        Locale.setDefault(locale);
-        zone = null;
-    }
-
-    public void testSetIntoPeriod_Object6() throws Exception {
-        MutablePeriod m = new MutablePeriod(PeriodType.yearWeekDayTime());
-        StringConverter.INSTANCE.setInto(m, "P2Y4W3DT12H24M56.1234567S", null);
-        assertEquals(2, m.getYears());
-        assertEquals(4, m.getWeeks());
-        assertEquals(3, m.getDays());
-        assertEquals(12, m.getHours());
-        assertEquals(24, m.getMinutes());
-        assertEquals(56, m.getSeconds());
-        assertEquals(123, m.getMillis());
+        // Assert
+        // The converter should parse all fields correctly and truncate the
+        // fractional seconds (.1234567S) to milliseconds (123).
+        assertEquals(expectedPeriod, actualPeriod);
     }
 }
