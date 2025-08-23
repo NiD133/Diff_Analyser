45b4c36f5e5e2e86c22f4ac21a66826ab8276574
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:45:38 +0800

    Original processedAutoTestCases constructorutils_estest25.java

---
+++ constructorutils_estest25.java
@@ -0,0 +1,24 @@
+package org.apache.commons.lang3.reflect;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.AccessibleObject;
+import java.lang.reflect.Array;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class ConstructorUtils_ESTestTest25 extends ConstructorUtils_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test24() throws Throwable {
+        Class<Integer>[] classArray0 = (Class<Integer>[]) Array.newInstance(Class.class, 1);
+        Class<Object> class0 = Object.class;
+        Constructor<Object> constructor0 = ConstructorUtils.getAccessibleConstructor(class0, (Class<?>[]) classArray0);
+        assertNull(constructor0);
+    }
+}
