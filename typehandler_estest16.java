b66553a98672cbb65960e25a8009d0fa6345aa81
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:24:16 +0800

    gemini processedAutoTestCases typehandler_estest16.java

--- typehandler_estest16.java
+++ typehandler_estest16.java
@@ -1,28 +1,21 @@
 package org.apache.commons.cli;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.File;
-import java.util.Date;
-import java.util.Map;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.lang.MockThrowable;
-import org.junit.runner.RunWith;
 
-public class TypeHandler_ESTestTest16 extends TypeHandler_ESTest_scaffolding {
+/**
+ * Tests for the {@link TypeHandler} class, focusing on value creation methods.
+ */
+public class TypeHandlerTest {
 
-    @Test(timeout = 4000)
-    public void test15() throws Throwable {
-        try {
-            TypeHandler.createURL("N)i");
-            fail("Expecting exception: Exception");
-        } catch (Exception e) {
-            //
-            // java.net.MalformedURLException: no protocol: N)i
-            //
-            verifyException("org.apache.commons.cli.ParseException", e);
-        }
+    /**
+     * Verifies that createURL() throws a ParseException when given a string
+     * that is not a valid URL because it lacks a protocol.
+     *
+     * @throws ParseException because the input string is a malformed URL.
+     */
+    @Test(expected = ParseException.class)
+    public void createURL_withMalformedURL_shouldThrowParseException() throws ParseException {
+        // The input string "N)i" is not a valid URL as it lacks a protocol (e.g., "http://").
+        TypeHandler.createURL("N)i");
     }
 }
