b39395647e6fb5b68282020ed0c4b7436bdfe3aa
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:24:08 +0800

    Original processedAutoTestCases typeadapter_estest31.java

---
+++ typeadapter_estest31.java
@@ -0,0 +1,37 @@
+package com.google.gson;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.io.Writer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class TypeAdapter_ESTestTest31 extends TypeAdapter_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test30() throws Throwable {
+        Gson.FutureTypeAdapter<Object> gson_FutureTypeAdapter0 = new Gson.FutureTypeAdapter<Object>();
+        Character character0 = new Character('B');
+        JsonPrimitive jsonPrimitive0 = new JsonPrimitive(character0);
+        TypeAdapter<Object> typeAdapter0 = gson_FutureTypeAdapter0.nullSafe();
+        // Undeclared exception!
+        try {
+            typeAdapter0.fromJsonTree(jsonPrimitive0);
+            fail("Expecting exception: IllegalStateException");
+        } catch (IllegalStateException e) {
+            //
+            // Adapter for type with cyclic dependency has been used before dependency has been resolved
+            //
+            verifyException("com.google.gson.Gson$FutureTypeAdapter", e);
+        }
+    }
+}
