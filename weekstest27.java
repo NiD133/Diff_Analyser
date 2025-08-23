edfdb98db43360987f9fe4579803b967398a09d1
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:35:54 +0800

    Original processedManualTestCases weekstest27.java

---
+++ weekstest27.java
@@ -0,0 +1,38 @@
+package org.joda.time;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class WeeksTestTest27 extends TestCase {
+
+    // (before the late 90's they were all over the place)
+    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
+
+    public static TestSuite suite() {
+        return new TestSuite(TestWeeks.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+    }
+
+    //-----------------------------------------------------------------------
+    public void testAddToLocalDate() {
+        Weeks test = Weeks.weeks(3);
+        LocalDate date = new LocalDate(2006, 6, 1);
+        LocalDate expected = new LocalDate(2006, 6, 22);
+        assertEquals(expected, date.plus(test));
+    }
+}
