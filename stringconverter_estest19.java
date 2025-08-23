c5adf579f814ab50472ced1a599f60454cc50e21
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:18:24 +0800

    gemini processedAutoTestCases stringconverter_estest19.java

--- stringconverter_estest19.java
+++ stringconverter_estest19.java
@@ -1,48 +1,46 @@
 package org.joda.time.convert;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
 import org.joda.time.Chronology;
 import org.joda.time.DateTime;
-import org.joda.time.DateTimeFieldType;
-import org.joda.time.DateTimeZone;
-import org.joda.time.LocalDate;
-import org.joda.time.MonthDay;
-import org.joda.time.MutableDateTime;
 import org.joda.time.MutableInterval;
-import org.joda.time.MutablePeriod;
-import org.joda.time.Partial;
-import org.joda.time.PeriodType;
-import org.joda.time.ReadWritableInterval;
-import org.joda.time.ReadWritablePeriod;
-import org.joda.time.ReadableInstant;
-import org.joda.time.ReadablePartial;
-import org.joda.time.chrono.CopticChronology;
-import org.joda.time.chrono.EthiopicChronology;
-import org.joda.time.chrono.GJChronology;
-import org.joda.time.chrono.GregorianChronology;
-import org.joda.time.chrono.ISOChronology;
 import org.joda.time.chrono.IslamicChronology;
-import org.joda.time.chrono.JulianChronology;
-import org.joda.time.chrono.ZonedChronology;
-import org.joda.time.format.DateTimeFormatter;
-import org.joda.time.format.DateTimeParser;
-import org.joda.time.format.DateTimePrinter;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class StringConverter_ESTestTest19 extends StringConverter_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
 
-    @Test(timeout = 4000)
-    public void test18() throws Throwable {
-        MutableInterval mutableInterval0 = MutableInterval.parse("6/8");
-        StringConverter stringConverter0 = StringConverter.INSTANCE;
-        IslamicChronology islamicChronology0 = IslamicChronology.getInstanceUTC();
-        stringConverter0.setInto((ReadWritableInterval) mutableInterval0, (Object) "6/8", (Chronology) islamicChronology0);
-        assertEquals((-42368486400000L), mutableInterval0.getStartMillis());
+/**
+ * Tests for {@link StringConverter}.
+ * This focuses on the setInto(ReadWritableInterval, Object, Chronology) method.
+ */
+public class StringConverter_ESTestTest19 { // Note: Class name kept from original for context.
+
+    /**
+     * Verifies that setInto() correctly parses an interval string using a specific,
+     * non-default chronology. The string "6/8" should be interpreted as an interval
+     * from the start of year 6 to the start of year 8 in the provided Islamic calendar.
+     */
+    @Test
+    public void setIntoShouldParseIntervalStringUsingProvidedChronology() {
+        // Arrange
+        final String intervalString = "6/8";
+        final Chronology islamicChronology = IslamicChronology.getInstanceUTC();
+        final StringConverter converter = StringConverter.INSTANCE;
+
+        // An empty interval that will be modified by the converter.
+        final MutableInterval intervalToUpdate = new MutableInterval(0L, 0L);
+
+        // For clarity, explicitly define the expected start and end times instead of using magic numbers.
+        // In the Islamic calendar, "6" is parsed as the beginning of year 6.
+        final DateTime expectedStart = new DateTime(6, 1, 1, 0, 0, islamicChronology);
+        final DateTime expectedEnd = new DateTime(8, 1, 1, 0, 0, islamicChronology);
+
+        // Act
+        converter.setInto(intervalToUpdate, intervalString, islamicChronology);
+
+        // Assert
+        assertEquals("The start of the interval should be updated to the start of year 6 in the Islamic calendar.",
+                expectedStart.getMillis(), intervalToUpdate.getStartMillis());
+        assertEquals("The end of the interval should be updated to the start of year 8 in the Islamic calendar.",
+                expectedEnd.getMillis(), intervalToUpdate.getEndMillis());
     }
 }
