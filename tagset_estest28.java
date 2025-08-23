12f5a6e1167e7ee2f19d5f2a7806ab032ac7b5f3
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:20:18 +0800

    Original processedAutoTestCases tagset_estest28.java

---
+++ tagset_estest28.java
@@ -0,0 +1,20 @@
+package org.jsoup.parser;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.function.Consumer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class TagSet_ESTestTest28 extends TagSet_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test27() throws Throwable {
+        TagSet tagSet0 = new TagSet();
+        tagSet0.hashCode();
+    }
+}
