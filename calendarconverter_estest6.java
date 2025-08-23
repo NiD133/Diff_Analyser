107f0c6eddcc6ca6b30c3fa5a27c66d24daa7e86
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:41:33 +0800

    gemini processedAutoTestCases calendarconverter_estest6.java

--- calendarconverter_estest6.java
+++ calendarconverter_estest6.java
@@ -1,37 +1,29 @@
 package org.joda.time.convert;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
-import java.util.GregorianCalendar;
-import java.util.TimeZone;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.System;
-import org.evosuite.runtime.mock.java.time.MockZonedDateTime;
-import org.evosuite.runtime.mock.java.util.MockDate;
-import org.evosuite.runtime.mock.java.util.MockGregorianCalendar;
-import org.joda.time.Chronology;
 import org.joda.time.DateTimeZone;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class CalendarConverter_ESTestTest6 extends CalendarConverter_ESTest_scaffolding {
+/**
+ * Unit tests for {@link CalendarConverter}.
+ */
+public class CalendarConverterTest {
 
-    @Test(timeout = 4000)
-    public void test05() throws Throwable {
-        CalendarConverter calendarConverter0 = CalendarConverter.INSTANCE;
-        // Undeclared exception!
-        try {
-            calendarConverter0.getChronology((Object) null, (DateTimeZone) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.joda.time.convert.CalendarConverter", e);
-        }
+    private final CalendarConverter converter = CalendarConverter.INSTANCE;
+
+    /**
+     * Tests that getChronology() throws a NullPointerException if the object to be converted is null.
+     * The Javadoc for the method explicitly states this behavior.
+     */
+    @Test(expected = NullPointerException.class)
+    public void getChronology_whenObjectIsNull_throwsNullPointerException() {
+        // Arrange: The object to convert is null.
+        // The second argument (DateTimeZone) is also null, but the check for the
+        // first argument's nullity should take precedence.
+        Object nullCalendar = null;
+
+        // Act & Assert: Call the method and expect a NullPointerException.
+        // The cast to DateTimeZone is needed to resolve ambiguity between the two
+        // overloaded getChronology methods.
+        converter.getChronology(nullCalendar, (DateTimeZone) null);
     }
 }
