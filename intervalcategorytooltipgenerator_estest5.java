f0ba28be904812934fdac78b0f954ff517cf5e44
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:54:48 +0800

    Original processedAutoTestCases intervalcategorytooltipgenerator_estest5.java

---
+++ intervalcategorytooltipgenerator_estest5.java
@@ -0,0 +1,33 @@
+package org.jfree.chart.labels;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.text.DateFormat;
+import java.text.NumberFormat;
+import java.time.chrono.ChronoLocalDate;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.text.MockDateFormat;
+import org.evosuite.runtime.mock.java.text.MockSimpleDateFormat;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.category.DefaultIntervalCategoryDataset;
+import org.jfree.data.statistics.DefaultBoxAndWhiskerCategoryDataset;
+import org.junit.runner.RunWith;
+
+public class IntervalCategoryToolTipGenerator_ESTestTest5 extends IntervalCategoryToolTipGenerator_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test04() throws Throwable {
+        IntervalCategoryToolTipGenerator intervalCategoryToolTipGenerator0 = null;
+        try {
+            intervalCategoryToolTipGenerator0 = new IntervalCategoryToolTipGenerator("({0}, {1}) = {2}", (NumberFormat) null);
+            fail("Expecting exception: IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //
+            // Null 'formatter' argument.
+            //
+            verifyException("org.jfree.chart.internal.Args", e);
+        }
+    }
+}
