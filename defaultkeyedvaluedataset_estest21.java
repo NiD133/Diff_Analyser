0da1013cf842a4ee263a1b8913886fa26c0db732
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:47:33 +0800

    Original processedAutoTestCases defaultkeyedvaluedataset_estest21.java

---
+++ defaultkeyedvaluedataset_estest21.java
@@ -0,0 +1,27 @@
+package org.jfree.data.general;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.math.BigInteger;
+import javax.swing.JLayeredPane;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.util.MockDate;
+import org.jfree.chart.date.SerialDate;
+import org.jfree.chart.date.SpreadsheetDate;
+import org.jfree.data.statistics.SimpleHistogramBin;
+import org.jfree.data.xy.OHLCDataItem;
+import org.junit.runner.RunWith;
+
+public class DefaultKeyedValueDataset_ESTestTest21 extends DefaultKeyedValueDataset_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test20() throws Throwable {
+        DefaultKeyedValueDataset defaultKeyedValueDataset0 = new DefaultKeyedValueDataset();
+        Object object0 = defaultKeyedValueDataset0.clone();
+        boolean boolean0 = defaultKeyedValueDataset0.equals(object0);
+        assertNotSame(object0, defaultKeyedValueDataset0);
+        assertTrue(boolean0);
+    }
+}
