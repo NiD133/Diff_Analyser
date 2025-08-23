6f783d49e5001437d84fd6d3a28cc11441b77b36
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:05:45 +0800

    gemini processedAutoTestCases objectgraphiterator_estest3.java

--- objectgraphiterator_estest3.java
+++ objectgraphiterator_estest3.java
@@ -1,65 +1,49 @@
 package org.apache.commons.collections4.iterators;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Comparator;
-import java.util.ConcurrentModificationException;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.ListIterator;
-import java.util.Map;
-import java.util.NoSuchElementException;
-import java.util.function.Function;
-import org.apache.commons.collections4.Factory;
 import org.apache.commons.collections4.Predicate;
 import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AndPredicate;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantFactory;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionPredicate;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.FactoryTransformer;
-import org.apache.commons.collections4.functors.FalsePredicate;
-import org.apache.commons.collections4.functors.IdentityPredicate;
 import org.apache.commons.collections4.functors.IfTransformer;
 import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.MapTransformer;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NullIsFalsePredicate;
-import org.apache.commons.collections4.functors.NullIsTruePredicate;
-import org.apache.commons.collections4.functors.OrPredicate;
-import org.apache.commons.collections4.functors.PredicateTransformer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class ObjectGraphIterator_ESTestTest3 extends ObjectGraphIterator_ESTest_scaffolding {
+/**
+ * Test class for {@link ObjectGraphIterator}.
+ */
+public class ObjectGraphIterator_ESTestTest3 {
 
-    @Test(timeout = 4000)
-    public void test02() throws Throwable {
-        Integer integer0 = new Integer(1);
-        Class<Boolean> class0 = Boolean.class;
-        Predicate<Object> predicate0 = InstanceofPredicate.instanceOfPredicate(class0);
-        IfTransformer<Object, Integer> ifTransformer0 = new IfTransformer<Object, Integer>(predicate0, (Transformer<? super Object, ? extends Integer>) null, (Transformer<? super Object, ? extends Integer>) null);
-        ObjectGraphIterator<Integer> objectGraphIterator0 = new ObjectGraphIterator<Integer>(integer0, ifTransformer0);
-        ObjectGraphIterator<Integer> objectGraphIterator1 = new ObjectGraphIterator<Integer>(objectGraphIterator0);
-        // Undeclared exception!
-        try {
-            objectGraphIterator1.updateCurrentIterator();
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.commons.collections4.functors.IfTransformer", e);
-        }
+    /**
+     * Tests that an exception thrown by a nested iterator's transformer
+     * is correctly propagated by the outer iterator.
+     */
+    @Test(expected = NullPointerException.class)
+    public void updateCurrentIteratorWithFailingNestedIteratorShouldPropagateNPE() {
+        // ARRANGE
+        // 1. Define a root element for an inner iterator.
+        final Integer innerIteratorRoot = 1;
+
+        // 2. Create a transformer that is designed to fail.
+        // The predicate will check if the input is a Boolean. Since our root is an Integer,
+        // the predicate will return false.
+        final Predicate<Object> isBooleanPredicate = InstanceofPredicate.instanceOfPredicate(Boolean.class);
+
+        // The IfTransformer is constructed with null for both its "true" and "false"
+        // delegate transformers. When it attempts to use the "false" transformer,
+        // it will throw a NullPointerException.
+        final Transformer<Object, Integer> failingTransformer =
+                new IfTransformer<>(isBooleanPredicate, null, null);
+
+        // 3. Create an inner iterator that uses the failing transformer.
+        final ObjectGraphIterator<Integer> innerIterator =
+                new ObjectGraphIterator<>(innerIteratorRoot, failingTransformer);
+
+        // 4. Create an outer iterator that wraps the inner one.
+        final ObjectGraphIterator<Integer> outerIterator =
+                new ObjectGraphIterator<>(innerIterator);
+
+        // ACT
+        // This call will trigger traversal of the inner iterator, which in turn will
+        // use the failing transformer, causing a NullPointerException to be thrown.
+        // The @Test(expected) annotation verifies that this exception is thrown as expected.
+        outerIterator.updateCurrentIterator();
     }
 }
