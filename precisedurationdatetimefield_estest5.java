3020d886f4c4a0ce9da50a35095fb6fc9c5d7510
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:09:19 +0800

    Original processedAutoTestCases precisedurationdatetimefield_estest5.java

---
+++ precisedurationdatetimefield_estest5.java
@@ -0,0 +1,35 @@
+package org.joda.time.field;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.TimeZone;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.joda.time.Chronology;
+import org.joda.time.DateTimeFieldType;
+import org.joda.time.DateTimeZone;
+import org.joda.time.Days;
+import org.joda.time.DurationField;
+import org.joda.time.DurationFieldType;
+import org.joda.time.LocalDate;
+import org.joda.time.LocalTime;
+import org.joda.time.Weeks;
+import org.joda.time.chrono.EthiopicChronology;
+import org.joda.time.chrono.GJChronology;
+import org.joda.time.chrono.GregorianChronology;
+import org.joda.time.chrono.IslamicChronology;
+import org.joda.time.chrono.JulianChronology;
+import org.joda.time.chrono.LenientChronology;
+import org.joda.time.chrono.ZonedChronology;
+import org.junit.runner.RunWith;
+
+public class PreciseDurationDateTimeField_ESTestTest5 extends PreciseDurationDateTimeField_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test04() throws Throwable {
+        LocalDate localDate0 = LocalDate.now();
+        LocalDate localDate1 = localDate0.withDayOfYear(365);
+        assertNotSame(localDate1, localDate0);
+    }
+}
