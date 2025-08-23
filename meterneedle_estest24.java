e340faeef2cf4b78542f2e143e6c2a127c4fb6fe
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:02:52 +0800

    gemini processedAutoTestCases meterneedle_estest24.java

--- meterneedle_estest24.java
+++ meterneedle_estest24.java
@@ -1,38 +1,49 @@
 package org.jfree.chart.plot.compass;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.awt.BasicStroke;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import java.awt.Color;
-import java.awt.Graphics2D;
-import java.awt.Paint;
-import java.awt.Rectangle;
-import java.awt.Stroke;
-import java.awt.SystemColor;
-import java.awt.geom.Point2D;
-import java.awt.geom.Rectangle2D;
-import java.awt.image.BufferedImage;
-import java.text.AttributedCharacterIterator;
-import javax.swing.JScrollPane;
-import javax.swing.text.DefaultCaret;
-import javax.swing.tree.DefaultTreeCellRenderer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class MeterNeedle_ESTestTest24 extends MeterNeedle_ESTest_scaffolding {
+/**
+ * Tests for the equals() method in the MeterNeedle class and its subclasses.
+ */
+public class MeterNeedleEqualityTest {
 
-    @Test(timeout = 4000)
-    public void test23() throws Throwable {
-        ShipNeedle shipNeedle0 = new ShipNeedle();
-        PointerNeedle pointerNeedle0 = new PointerNeedle();
-        Color color0 = Color.darkGray;
-        pointerNeedle0.setHighlightPaint(color0);
-        shipNeedle0.equals(pointerNeedle0);
-        assertEquals(5, pointerNeedle0.getSize());
-        assertEquals(0.5, pointerNeedle0.getRotateY(), 0.01);
-        assertEquals(0.5, pointerNeedle0.getRotateX(), 0.01);
-        assertEquals(0.5, shipNeedle0.getRotateX(), 0.01);
+    /**
+     * Verifies that two needle instances are not equal if one has a different
+     * highlight paint property.
+     */
+    @Test
+    public void equals_shouldReturnFalse_whenHighlightPaintDiffers() {
+        // Arrange: Create two identical PointerNeedle instances.
+        // By default, their highlightPaint is null, so they are equal.
+        PointerNeedle needle1 = new PointerNeedle();
+        PointerNeedle needle2 = new PointerNeedle();
+        assertTrue("Needles should be equal initially", needle1.equals(needle2));
+
+        // Act: Change the highlight paint on only one of the needles.
+        needle2.setHighlightPaint(Color.RED);
+
+        // Assert: The needles should no longer be considered equal.
+        assertFalse("Needles should not be equal after one's highlight paint is changed", needle1.equals(needle2));
+    }
+
+    /**
+     * Verifies that two different types of MeterNeedle are not considered equal,
+     * even if their properties are otherwise identical.
+     *
+     * This test case clarifies another potential ambiguity from the original code,
+     * which compared a ShipNeedle to a PointerNeedle.
+     */
+    @Test
+    public void equals_shouldReturnFalse_whenComparingDifferentNeedleTypes() {
+        // Arrange
+        MeterNeedle shipNeedle = new ShipNeedle();
+        MeterNeedle pointerNeedle = new PointerNeedle();
+
+        // Act & Assert
+        assertFalse("Instances of different needle subclasses should not be equal", shipNeedle.equals(pointerNeedle));
     }
 }
