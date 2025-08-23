56f9f8a637a742e35da799f650f8e13cbc84d4ff
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:54:30 +0800

    Original processedAutoTestCases internationalfixedchronology_estest47.java

---
+++ internationalfixedchronology_estest47.java
@@ -0,0 +1,56 @@
+package org.threeten.extra.chrono;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.time.Clock;
+import java.time.DateTimeException;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.Period;
+import java.time.Year;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.chrono.ChronoLocalDateTime;
+import java.time.chrono.ChronoZonedDateTime;
+import java.time.chrono.Era;
+import java.time.chrono.IsoEra;
+import java.time.chrono.JapaneseEra;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAccessor;
+import java.time.temporal.TemporalAmount;
+import java.time.temporal.UnsupportedTemporalTypeException;
+import java.time.temporal.ValueRange;
+import java.util.List;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.System;
+import org.evosuite.runtime.mock.java.time.MockClock;
+import org.evosuite.runtime.mock.java.time.MockInstant;
+import org.evosuite.runtime.mock.java.time.MockLocalDateTime;
+import org.evosuite.runtime.mock.java.time.MockYear;
+import org.evosuite.runtime.mock.java.time.MockZonedDateTime;
+import org.junit.runner.RunWith;
+
+public class InternationalFixedChronology_ESTestTest47 extends InternationalFixedChronology_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test46() throws Throwable {
+        InternationalFixedChronology internationalFixedChronology0 = InternationalFixedChronology.INSTANCE;
+        IsoEra isoEra0 = IsoEra.BCE;
+        // Undeclared exception!
+        try {
+            internationalFixedChronology0.prolepticYear(isoEra0, (-985));
+            fail("Expecting exception: ClassCastException");
+        } catch (ClassCastException e) {
+            //
+            // Invalid era: BCE
+            //
+            verifyException("org.threeten.extra.chrono.InternationalFixedChronology", e);
+        }
+    }
+}
