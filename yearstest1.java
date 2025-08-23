f001c4744d6f90de4b59730c220ec23db56faf36
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:36:26 +0800

    gemini processedManualTestCases yearstest1.java

--- yearstest1.java
+++ yearstest1.java
@@ -1,40 +1,21 @@
 package org.joda.time;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
 
-public class YearsTestTest1 extends TestCase {
+import org.junit.Test;
 
-    // (before the late 90's they were all over the place)
-    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
+/**
+ * Unit tests for the {@link Years} class.
+ */
+public class YearsTest {
 
-    public static void main(String[] args) {
-        junit.textui.TestRunner.run(suite());
-    }
-
-    public static TestSuite suite() {
-        return new TestSuite(TestYears.class);
-    }
-
-    @Override
-    protected void setUp() throws Exception {
-    }
-
-    @Override
-    protected void tearDown() throws Exception {
-    }
-
-    //-----------------------------------------------------------------------
-    public void testConstants() {
-        assertEquals(0, Years.ZERO.getYears());
-        assertEquals(1, Years.ONE.getYears());
-        assertEquals(2, Years.TWO.getYears());
-        assertEquals(3, Years.THREE.getYears());
-        assertEquals(Integer.MAX_VALUE, Years.MAX_VALUE.getYears());
-        assertEquals(Integer.MIN_VALUE, Years.MIN_VALUE.getYears());
+    @Test
+    public void constants_shouldHoldCorrectValues() {
+        assertEquals("Years.ZERO should have a value of 0", 0, Years.ZERO.getYears());
+        assertEquals("Years.ONE should have a value of 1", 1, Years.ONE.getYears());
+        assertEquals("Years.TWO should have a value of 2", 2, Years.TWO.getYears());
+        assertEquals("Years.THREE should have a value of 3", 3, Years.THREE.getYears());
+        assertEquals("Years.MAX_VALUE should be Integer.MAX_VALUE", Integer.MAX_VALUE, Years.MAX_VALUE.getYears());
+        assertEquals("Years.MIN_VALUE should be Integer.MIN_VALUE", Integer.MIN_VALUE, Years.MIN_VALUE.getYears());
     }
 }
