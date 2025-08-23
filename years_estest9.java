32c54a0c0d5eabf0864ca689e500749711038a1f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:30:41 +0800

    gemini processedAutoTestCases years_estest9.java

--- years_estest9.java
+++ years_estest9.java
@@ -1,18 +1,23 @@
 package org.joda.time;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class Years_ESTestTest9 extends Years_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link Years} class.
+ */
+public class YearsTest {
 
-    @Test(timeout = 4000)
-    public void test08() throws Throwable {
-        Years years0 = Years.TWO;
-        Years years1 = years0.negated();
-        assertEquals((-2), years1.getYears());
+    @Test
+    public void negated_shouldReturnYearsWithOppositeSign() {
+        // Arrange: Create a Years object representing 2 years.
+        Years twoYears = Years.TWO;
+        int expectedNegatedValue = -2;
+
+        // Act: Negate the Years object.
+        Years result = twoYears.negated();
+
+        // Assert: The new Years object should represent -2 years.
+        assertEquals(expectedNegatedValue, result.getYears());
     }
 }
