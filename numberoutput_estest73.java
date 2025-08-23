f967f9abea23e27ea2471687311906531c3f92fd
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:05:21 +0800

    gemini processedAutoTestCases numberoutput_estest73.java

--- numberoutput_estest73.java
+++ numberoutput_estest73.java
@@ -1,16 +1,26 @@
 package com.fasterxml.jackson.core.io;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertNotNull;
 
-public class NumberOutput_ESTestTest73 extends NumberOutput_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link NumberOutput} utility class.
+ */
+public class NumberOutputTest {
 
-    @Test(timeout = 4000)
-    public void test72() throws Throwable {
-        NumberOutput numberOutput0 = new NumberOutput();
+    /**
+     * Verifies that an instance of the {@link NumberOutput} utility class can be created.
+     *
+     * Although this class is primarily used via its static methods, this test ensures
+     * the default constructor is callable without throwing an exception. This is a common
+     * practice for achieving full code coverage on utility classes.
+     */
+    @Test
+    public void testConstructor() {
+        // Act: Instantiate the NumberOutput class.
+        NumberOutput instance = new NumberOutput();
+
+        // Assert: The instance should be successfully created and not null.
+        assertNotNull("Instantiation of NumberOutput should result in a non-null object.", instance);
     }
 }
