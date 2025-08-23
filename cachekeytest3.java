5b17b2733f593c7b8c04598f728ab907ae1cff53
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:26:13 +0800

    Original processedManualTestCases cachekeytest3.java

---
+++ cachekeytest3.java
@@ -0,0 +1,33 @@
+package org.apache.ibatis.cache;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.NotSerializableException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Date;
+import org.junit.jupiter.api.Test;
+
+public class CacheKeyTestTest3 {
+
+    private static <T> T serialize(T object) throws Exception {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        new ObjectOutputStream(baos).writeObject(object);
+        ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+        return (T) new ObjectInputStream(bais).readObject();
+    }
+
+    @Test
+    void shouldTestCacheKeysNotEqualDueToOrder() throws Exception {
+        CacheKey key1 = new CacheKey(new Object[] { 1, "hello", null });
+        Thread.sleep(1000);
+        CacheKey key2 = new CacheKey(new Object[] { 1, null, "hello" });
+        assertNotEquals(key1, key2);
+        assertNotEquals(key2, key1);
+        assertNotEquals(key1.hashCode(), key2.hashCode());
+        assertNotEquals(key1.toString(), key2.toString());
+    }
+}
