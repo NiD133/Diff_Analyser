c71dd41be2242ac8044b73ae466cd7f07d359f5c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:37:23 +0800

    gemini processedAutoTestCases ararchiveoutputstream_estest34.java

--- ararchiveoutputstream_estest34.java
+++ ararchiveoutputstream_estest34.java
@@ -1,38 +1,29 @@
 package org.apache.commons.compress.archivers.ar;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
+
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.nio.file.LinkOption;
-import java.nio.file.NoSuchFileException;
-import java.nio.file.Path;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.System;
-import org.evosuite.runtime.mock.java.io.MockFile;
-import org.evosuite.runtime.mock.java.io.MockFileOutputStream;
-import org.evosuite.runtime.mock.java.io.MockPrintStream;
-import org.evosuite.runtime.testdata.FileSystemHandling;
-import org.junit.runner.RunWith;
 
-public class ArArchiveOutputStream_ESTestTest34 extends ArArchiveOutputStream_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link ArArchiveOutputStream} class.
+ */
+public class ArArchiveOutputStreamTest {
 
-    @Test(timeout = 4000)
-    public void test33() throws Throwable {
-        ArArchiveOutputStream arArchiveOutputStream0 = new ArArchiveOutputStream((OutputStream) null);
-        // Undeclared exception!
-        try {
-            arArchiveOutputStream0.putArchiveEntry((ArArchiveEntry) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.commons.compress.archivers.ar.ArArchiveOutputStream", e);
-        }
+    /**
+     * Verifies that calling putArchiveEntry() with a null argument
+     * correctly throws a NullPointerException.
+     */
+    @Test(expected = NullPointerException.class)
+    public void putArchiveEntryShouldThrowNullPointerExceptionWhenEntryIsNull() throws IOException {
+        // Arrange: Create a valid ArArchiveOutputStream instance.
+        // Using a ByteArrayOutputStream is better practice than passing null,
+        // as it ensures the stream object itself is in a valid state.
+        final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        final ArArchiveOutputStream arArchiveOutputStream = new ArArchiveOutputStream(outputStream);
+
+        // Act: Attempt to add a null entry to the archive.
+        // The @Test(expected=...) annotation asserts that a NullPointerException is thrown.
+        arArchiveOutputStream.putArchiveEntry(null);
     }
 }
