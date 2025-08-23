ccc02289cb926e7d60afda0421eb3db7d04d5185
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:21:03 +0800

    Original processedAutoTestCases texthelpappendable_estest49.java

---
+++ texthelpappendable_estest49.java
@@ -0,0 +1,55 @@
+package org.apache.commons.cli.help;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.IOException;
+import java.io.PipedWriter;
+import java.io.StringWriter;
+import java.nio.BufferOverflowException;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.ReadOnlyBufferException;
+import java.nio.charset.Charset;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.SortedSet;
+import java.util.Stack;
+import java.util.TreeSet;
+import java.util.Vector;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class TextHelpAppendable_ESTestTest49 extends TextHelpAppendable_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test48() throws Throwable {
+        CharBuffer charBuffer0 = CharBuffer.wrap((CharSequence) "");
+        TextHelpAppendable textHelpAppendable0 = new TextHelpAppendable(charBuffer0);
+        Charset charset0 = Charset.defaultCharset();
+        ByteBuffer byteBuffer0 = ByteBuffer.allocate(3);
+        CharBuffer charBuffer1 = charset0.decode(byteBuffer0);
+        // Undeclared exception!
+        try {
+            textHelpAppendable0.appendTitle(charBuffer1);
+            fail("Expecting exception: ReadOnlyBufferException");
+        } catch (ReadOnlyBufferException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("java.nio.CharBuffer", e);
+        }
+    }
+}
