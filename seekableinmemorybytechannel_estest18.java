c2d36c1e9510c6d491e4e5a8279d9c80844d15ab
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:13:19 +0800

    gemini processedAutoTestCases seekableinmemorybytechannel_estest18.java

--- seekableinmemorybytechannel_estest18.java
+++ seekableinmemorybytechannel_estest18.java
@@ -1,29 +1,21 @@
 package org.apache.commons.compress.utils;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.channels.ClosedChannelException;
-import java.nio.channels.SeekableByteChannel;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class SeekableInMemoryByteChannel_ESTestTest18 extends SeekableInMemoryByteChannel_ESTest_scaffolding {
+/**
+ * Test suite for the SeekableInMemoryByteChannel class.
+ */
+public class SeekableInMemoryByteChannelTest {
 
-    @Test(timeout = 4000)
-    public void test17() throws Throwable {
-        SeekableInMemoryByteChannel seekableInMemoryByteChannel0 = null;
-        try {
-            seekableInMemoryByteChannel0 = new SeekableInMemoryByteChannel((-1));
-            fail("Expecting exception: NegativeArraySizeException");
-        } catch (NegativeArraySizeException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.commons.compress.utils.SeekableInMemoryByteChannel", e);
-        }
+    /**
+     * Verifies that the constructor throws a NegativeArraySizeException
+     * when initialized with a negative size. The underlying byte array
+     * cannot be allocated with a negative capacity.
+     */
+    @Test(expected = NegativeArraySizeException.class)
+    public void constructorShouldThrowExceptionForNegativeSize() {
+        // Act: Attempt to create a channel with a negative initial size.
+        // Assert: Expect a NegativeArraySizeException to be thrown.
+        new SeekableInMemoryByteChannel(-1);
     }
 }
