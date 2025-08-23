010b877ae3445c3ed08f0f9f7acf4515e7d855de
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:33:55 +0800

    gemini processedManualTestCases soundextest26.java

--- soundextest26.java
+++ soundextest26.java
@@ -1,13 +1,14 @@
 package org.apache.commons.codec.language;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.apache.commons.codec.AbstractStringEncoderTest;
-import org.apache.commons.codec.EncoderException;
 import org.junit.jupiter.api.Test;
 
-public class SoundexTestTest26 extends AbstractStringEncoderTest<Soundex> {
+/**
+ * Tests for the {@link Soundex} class.
+ */
+public class SoundexTest extends AbstractStringEncoderTest<Soundex> {
 
     @Override
     protected Soundex createStringEncoder() {
@@ -15,19 +16,21 @@ public class SoundexTestTest26 extends AbstractStringEncoderTest<Soundex> {
     }
 
     /**
-     * Fancy characters are not mapped by the default US mapping.
+     * Tests that the default US English mapping, which only supports standard English letters,
+     * throws an exception for unmapped characters like 'ö'. The Soundex algorithm is
+     * generally not defined for characters outside the A-Z range.
      *
-     * https://issues.apache.org/jira/browse/CODEC-30
+     * @see <a href="https://issues.apache.org/jira/browse/CODEC-30">CODEC-30</a>
      */
     @Test
-    void testUsMappingOWithDiaeresis() {
-        assertEquals("O000", getStringEncoder().encode("o"));
-        if (Character.isLetter('\u00f6')) {
-            // o-umlaut
-            //         uppercase O-umlaut
-            assertThrows(IllegalArgumentException.class, () -> getStringEncoder().encode("\u00f6"));
-        } else {
-            assertEquals("", getStringEncoder().encode("\u00f6"));
-        }
+    void shouldThrowExceptionWhenEncodingUnmappedCharacter() {
+        final Soundex soundex = createStringEncoder();
+        final String oWithDiaeresis = "\u00f6"; // The character 'ö'
+
+        // The default Soundex mapping only includes A-Z.
+        // Encoding a character outside this set should fail.
+        assertThrows(IllegalArgumentException.class, () -> {
+            soundex.encode(oWithDiaeresis);
+        }, "Encoding a character not in the A-Z range should throw an IllegalArgumentException.");
     }
 }
