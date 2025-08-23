1eae0619dc4c490fb2bd02924d7fc8b9e27df60f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:37:17 +0800

    Original processedAutoTestCases ararchiveoutputstream_estest15.java

---
+++ ararchiveoutputstream_estest15.java
@@ -0,0 +1,39 @@
+package org.apache.commons.compress.archivers.ar;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.LinkOption;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.System;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.evosuite.runtime.mock.java.io.MockFileOutputStream;
+import org.evosuite.runtime.mock.java.io.MockPrintStream;
+import org.evosuite.runtime.testdata.FileSystemHandling;
+import org.junit.runner.RunWith;
+
+public class ArArchiveOutputStream_ESTestTest15 extends ArArchiveOutputStream_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test14() throws Throwable {
+        ArArchiveOutputStream arArchiveOutputStream0 = new ArArchiveOutputStream((OutputStream) null);
+        arArchiveOutputStream0.finish();
+        LinkOption[] linkOptionArray0 = new LinkOption[0];
+        try {
+            arArchiveOutputStream0.createArchiveEntry((Path) null, "", linkOptionArray0);
+            fail("Expecting exception: IOException");
+        } catch (IOException e) {
+            //
+            // Stream has already been finished.
+            //
+            verifyException("org.apache.commons.compress.archivers.ArchiveOutputStream", e);
+        }
+    }
+}
