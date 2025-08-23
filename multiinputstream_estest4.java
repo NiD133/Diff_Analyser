a35fc6d1081dbe0c6775fd7e438ef1e6cc32de53
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:04:04 +0800

    Original processedAutoTestCases multiinputstream_estest4.java

---
+++ multiinputstream_estest4.java
@@ -0,0 +1,40 @@
+package com.google.common.io;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.ListIterator;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class MultiInputStream_ESTestTest4 extends MultiInputStream_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test03() throws Throwable {
+        ByteSource byteSource0 = ByteSource.empty();
+        ArrayDeque<ByteSource> arrayDeque0 = new ArrayDeque<ByteSource>();
+        arrayDeque0.add(byteSource0);
+        arrayDeque0.add(byteSource0);
+        Iterator<ByteSource> iterator0 = arrayDeque0.iterator();
+        MultiInputStream multiInputStream0 = new MultiInputStream(iterator0);
+        arrayDeque0.add(byteSource0);
+        // Undeclared exception!
+        try {
+            multiInputStream0.skip(4134L);
+            fail("Expecting exception: ConcurrentModificationException");
+        } catch (ConcurrentModificationException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("java.util.ArrayDeque$DeqIterator", e);
+        }
+    }
+}
