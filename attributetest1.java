a180bbb0f94c5091492cbb7caf67104f505fbb65
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:25:02 +0800

    Original processedManualTestCases attributetest1.java

---
+++ attributetest1.java
@@ -0,0 +1,17 @@
+package org.jsoup.nodes;
+
+import org.jsoup.Jsoup;
+import org.jsoup.parser.ParseSettings;
+import org.jsoup.parser.Parser;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AttributeTestTest1 {
+
+    @Test
+    public void html() {
+        Attribute attr = new Attribute("key", "value &");
+        assertEquals("key=\"value &amp;\"", attr.html());
+        assertEquals(attr.html(), attr.toString());
+    }
+}
