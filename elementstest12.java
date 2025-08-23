c6a677f6694d3eb9c01cdf4f3ce38a6a88a27e58
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:28:06 +0800

    Original processedManualTestCases elementstest12.java

---
+++ elementstest12.java
@@ -0,0 +1,25 @@
+package org.jsoup.select;
+
+import org.jsoup.Jsoup;
+import org.jsoup.TextUtil;
+import org.jsoup.nodes.Comment;
+import org.jsoup.nodes.DataNode;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.FormElement;
+import org.jsoup.nodes.Node;
+import org.jsoup.nodes.TextNode;
+import org.junit.jupiter.api.Test;
+import java.util.Iterator;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ElementsTestTest12 {
+
+    @Test
+    public void outerHtml() {
+        Document doc = Jsoup.parse("<div><p>Hello</p></div><div><p>There</p></div>");
+        Elements divs = doc.select("div");
+        assertEquals("<div><p>Hello</p></div><div><p>There</p></div>", TextUtil.stripNewlines(divs.outerHtml()));
+    }
+}
