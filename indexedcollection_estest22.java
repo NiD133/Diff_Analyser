a54c412ccfee1dba4e21b64b8981def262d850f0
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:53:56 +0800

    gemini processedAutoTestCases indexedcollection_estest22.java

--- indexedcollection_estest22.java
+++ indexedcollection_estest22.java
@@ -1,65 +1,31 @@
 package org.apache.commons.collections4.collection;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.ConcurrentModificationException;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Set;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.Predicate;
 import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AllPredicate;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ChainedTransformer;
-import org.apache.commons.collections4.functors.CloneTransformer;
-import org.apache.commons.collections4.functors.ClosureTransformer;
-import org.apache.commons.collections4.functors.ConstantFactory;
 import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.DefaultEquator;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.FactoryTransformer;
-import org.apache.commons.collections4.functors.FalsePredicate;
-import org.apache.commons.collections4.functors.ForClosure;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.NOPClosure;
-import org.apache.commons.collections4.functors.NOPTransformer;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NotNullPredicate;
-import org.apache.commons.collections4.functors.NullIsFalsePredicate;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.SwitchTransformer;
-import org.apache.commons.collections4.functors.TransformedPredicate;
-import org.apache.commons.collections4.functors.TransformerClosure;
-import org.apache.commons.collections4.functors.TransformerPredicate;
-import org.apache.commons.collections4.functors.TruePredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class IndexedCollection_ESTestTest22 extends IndexedCollection_ESTest_scaffolding {
+import java.util.Collection;
+import java.util.LinkedList;
 
-    @Test(timeout = 4000)
-    public void test21() throws Throwable {
-        LinkedList<Integer> linkedList0 = new LinkedList<Integer>();
-        Transformer<Integer, Integer> transformer0 = ConstantTransformer.nullTransformer();
-        IndexedCollection<Integer, Integer> indexedCollection0 = IndexedCollection.uniqueIndexedCollection((Collection<Integer>) linkedList0, transformer0);
-        // Undeclared exception!
-        try {
-            indexedCollection0.removeAll((Collection<?>) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.commons.collections4.collection.IndexedCollection", e);
-        }
+/**
+ * Contains tests for the {@link IndexedCollection} class, focusing on edge cases and exception handling.
+ */
+public class IndexedCollectionTest {
+
+    /**
+     * Tests that calling removeAll() with a null argument throws a NullPointerException.
+     * The behavior is mandated by the java.util.Collection interface specification.
+     */
+    @Test(expected = NullPointerException.class)
+    public void removeAllWithNullCollectionShouldThrowNullPointerException() {
+        // Arrange: Create an empty IndexedCollection. The specific contents do not matter
+        // for this test, as the null check should happen before any elements are processed.
+        Collection<Integer> decoratedCollection = new LinkedList<>();
+        Transformer<Integer, Integer> keyTransformer = ConstantTransformer.nullTransformer();
+        IndexedCollection<Integer, Integer> indexedCollection =
+                IndexedCollection.uniqueIndexedCollection(decoratedCollection, keyTransformer);
+
+        // Act & Assert: Calling removeAll with a null collection is expected to throw.
+        indexedCollection.removeAll(null);
     }
 }
