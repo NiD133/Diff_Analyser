9584a45cacf10318216e1eba3a8918e27a109a85
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:23:17 +0800

    gemini processedAutoTestCases transformedmultivaluedmap_estest24.java

--- transformedmultivaluedmap_estest24.java
+++ transformedmultivaluedmap_estest24.java
@@ -1,64 +1,35 @@
 package org.apache.commons.collections4.multimap;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.AbstractMap;
-import java.util.ArrayDeque;
-import java.util.Comparator;
-import java.util.ConcurrentModificationException;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
-import java.util.PriorityQueue;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.Factory;
 import org.apache.commons.collections4.MultiValuedMap;
-import org.apache.commons.collections4.Predicate;
 import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AllPredicate;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ChainedTransformer;
-import org.apache.commons.collections4.functors.CloneTransformer;
-import org.apache.commons.collections4.functors.ClosureTransformer;
-import org.apache.commons.collections4.functors.ConstantFactory;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.ExceptionFactory;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.FactoryTransformer;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.MapTransformer;
 import org.apache.commons.collections4.functors.NOPTransformer;
-import org.apache.commons.collections4.functors.NotPredicate;
-import org.apache.commons.collections4.functors.NullIsExceptionPredicate;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.SwitchTransformer;
-import org.apache.commons.collections4.functors.TransformerClosure;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class TransformedMultiValuedMap_ESTestTest24 extends TransformedMultiValuedMap_ESTest_scaffolding {
+/**
+ * Unit tests for {@link TransformedMultiValuedMap}.
+ */
+public class TransformedMultiValuedMapTest {
 
-    @Test(timeout = 4000)
-    public void test23() throws Throwable {
-        ArrayListValuedLinkedHashMap<Integer, Integer> arrayListValuedLinkedHashMap0 = new ArrayListValuedLinkedHashMap<Integer, Integer>();
-        Transformer<Integer, Integer> transformer0 = NOPTransformer.nopTransformer();
-        TransformedMultiValuedMap<Integer, Integer> transformedMultiValuedMap0 = TransformedMultiValuedMap.transformedMap((MultiValuedMap<Integer, Integer>) arrayListValuedLinkedHashMap0, (Transformer<? super Integer, ? extends Integer>) transformer0, (Transformer<? super Integer, ? extends Integer>) transformer0);
-        Integer integer0 = new Integer(2);
-        // Undeclared exception!
-        try {
-            transformedMultiValuedMap0.putAll(integer0, (Iterable<? extends Integer>) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // values
-            //
-            verifyException("java.util.Objects", e);
-        }
+    /**
+     * Tests that calling putAll() with a null iterable for the values
+     * throws a NullPointerException.
+     */
+    @Test(expected = NullPointerException.class)
+    public void putAllWithNullIterableShouldThrowNullPointerException() {
+        // Arrange
+        final MultiValuedMap<Integer, Integer> baseMap = new ArrayListValuedLinkedHashMap<>();
+        final Transformer<Integer, Integer> identityTransformer = NOPTransformer.nopTransformer();
+
+        // The map under test, which transforms keys and values upon insertion.
+        // Here, we use a no-op transformer that doesn't change the elements.
+        final MultiValuedMap<Integer, Integer> transformedMap =
+                TransformedMultiValuedMap.transformedMap(baseMap, identityTransformer, identityTransformer);
+
+        final Integer key = 1;
+        final Iterable<Integer> nullValues = null;
+
+        // Act: Attempt to add a null collection of values for a given key.
+        // Assert: The @Test(expected) annotation asserts that a NullPointerException is thrown.
+        transformedMap.putAll(key, nullValues);
     }
 }
