d7d1e7e28fa45c29c24c4a5f272eb2562ed9ed03
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:18:41 +0800

    Original processedAutoTestCases stringutil_estest100.java

---
+++ stringutil_estest100.java
@@ -0,0 +1,37 @@
+package org.jsoup.internal;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Collection;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.stream.Collector;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.net.MockURL;
+import org.junit.runner.RunWith;
+
+public class StringUtil_ESTestTest100 extends StringUtil_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test99() throws Throwable {
+        String[] stringArray0 = new String[8];
+        // Undeclared exception!
+        try {
+            StringUtil.join(stringArray0, "oU%A");
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.evosuite.runtime.System", e);
+        }
+    }
+}
