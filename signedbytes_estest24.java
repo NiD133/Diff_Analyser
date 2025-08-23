6c3111285347911ed6e7e0ff3aa1e1f46dff301f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:15:55 +0800

    Original processedAutoTestCases signedbytes_estest24.java

---
+++ signedbytes_estest24.java
@@ -0,0 +1,20 @@
+package com.google.common.primitives;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.Comparator;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class SignedBytes_ESTestTest24 extends SignedBytes_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test23() throws Throwable {
+        byte[] byteArray0 = new byte[8];
+        byteArray0[0] = (byte) 16;
+        byte byte0 = SignedBytes.min(byteArray0);
+        assertEquals((byte) 0, byte0);
+    }
+}
