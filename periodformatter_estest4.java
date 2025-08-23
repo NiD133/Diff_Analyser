a3716d94f7e67a1a6e8dd91f17f05f9544eb9d7d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:08:36 +0800

    Original processedAutoTestCases periodformatter_estest4.java

---
+++ periodformatter_estest4.java
@@ -0,0 +1,39 @@
+package org.joda.time.format;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.IOException;
+import java.io.PipedWriter;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.LinkedList;
+import java.util.Locale;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.joda.time.Duration;
+import org.joda.time.Hours;
+import org.joda.time.Minutes;
+import org.joda.time.MutablePeriod;
+import org.joda.time.Period;
+import org.joda.time.PeriodType;
+import org.joda.time.ReadWritablePeriod;
+import org.joda.time.ReadablePeriod;
+import org.joda.time.Seconds;
+import org.joda.time.Weeks;
+import org.joda.time.Years;
+import org.junit.runner.RunWith;
+
+public class PeriodFormatter_ESTestTest4 extends PeriodFormatter_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test03() throws Throwable {
+        PeriodFormatterBuilder.Literal periodFormatterBuilder_Literal0 = new PeriodFormatterBuilder.Literal("PeriodFormat.months.list");
+        Locale locale0 = Locale.KOREA;
+        PeriodFormatter periodFormatter0 = new PeriodFormatter(periodFormatterBuilder_Literal0, periodFormatterBuilder_Literal0, locale0, (PeriodType) null);
+        Period period0 = new Period();
+        Minutes minutes0 = period0.toStandardMinutes();
+        String string0 = periodFormatter0.print(minutes0);
+        assertEquals("PeriodFormat.months.list", string0);
+    }
+}
