ef9251d3395da541342d75a07d7d2bc1970e5d9d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:59:12 +0800

    Original processedAutoTestCases jsonwritecontext_estest42.java

---
+++ jsonwritecontext_estest42.java
@@ -0,0 +1,40 @@
+package com.fasterxml.jackson.core.json;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.filter.FilteringGeneratorDelegate;
+import com.fasterxml.jackson.core.filter.TokenFilter;
+import com.fasterxml.jackson.core.util.JsonGeneratorDelegate;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.PipedInputStream;
+import java.io.StringWriter;
+import java.io.Writer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class JsonWriteContext_ESTestTest42 extends JsonWriteContext_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test41() throws Throwable {
+        DupDetector dupDetector0 = DupDetector.rootDetector((JsonGenerator) null);
+        JsonWriteContext jsonWriteContext0 = new JsonWriteContext((byte) (-16), (JsonWriteContext) null, dupDetector0);
+        JsonWriteContext jsonWriteContext1 = jsonWriteContext0.createChildArrayContext();
+        assertNotNull(jsonWriteContext1);
+        Object object0 = new Object();
+        JsonWriteContext jsonWriteContext2 = jsonWriteContext1.createChildObjectContext(object0);
+        JsonWriteContext jsonWriteContext3 = jsonWriteContext2.getParent();
+        assertFalse(jsonWriteContext0.inArray());
+        assertEquals(0, jsonWriteContext3.getEntryCount());
+        assertNotNull(jsonWriteContext3);
+        assertEquals(2, jsonWriteContext2.getNestingDepth());
+        assertTrue(jsonWriteContext3.inArray());
+        assertEquals(0, jsonWriteContext2.getEntryCount());
+        assertEquals("Object", jsonWriteContext2.typeDesc());
+    }
+}
