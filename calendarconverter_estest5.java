1bf7183c80b27648a4be281b9afb9e2d7b66d0c7
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:41:32 +0800

    Original processedAutoTestCases calendarconverter_estest5.java

---
+++ calendarconverter_estest5.java
@@ -0,0 +1,37 @@
+package org.joda.time.convert;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.System;
+import org.evosuite.runtime.mock.java.time.MockZonedDateTime;
+import org.evosuite.runtime.mock.java.util.MockDate;
+import org.evosuite.runtime.mock.java.util.MockGregorianCalendar;
+import org.joda.time.Chronology;
+import org.joda.time.DateTimeZone;
+import org.junit.runner.RunWith;
+
+public class CalendarConverter_ESTestTest5 extends CalendarConverter_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test04() throws Throwable {
+        CalendarConverter calendarConverter0 = CalendarConverter.INSTANCE;
+        // Undeclared exception!
+        try {
+            calendarConverter0.getInstantMillis((Object) null, (Chronology) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.joda.time.convert.CalendarConverter", e);
+        }
+    }
+}
