9b886000d86a8df8aa871d35f2b38367c92940d0
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:25:43 +0800

    Original processedManualTestCases bigdecimalparsertest2.java

---
+++ bigdecimalparsertest2.java
@@ -0,0 +1,39 @@
+package com.fasterxml.jackson.core.io;
+
+import java.math.BigDecimal;
+import ch.randelshofer.fastdoubleparser.JavaBigDecimalParser;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BigDecimalParserTestTest2 extends com.fasterxml.jackson.core.JUnit5TestBase {
+
+    static String genLongInvalidString() {
+        final int len = 1500;
+        final StringBuilder sb = new StringBuilder(len);
+        for (int i = 0; i < len; i++) {
+            sb.append("A");
+        }
+        return sb.toString();
+    }
+
+    static String genLongValidString(int len) {
+        final StringBuilder sb = new StringBuilder(len + 5);
+        sb.append("0.");
+        for (int i = 0; i < len; i++) {
+            sb.append('0');
+        }
+        sb.append('1');
+        return sb.toString();
+    }
+
+    @Test
+    void longInvalidStringFastParse() {
+        try {
+            BigDecimalParser.parseWithFastParser(genLongInvalidString());
+            fail("expected NumberFormatException");
+        } catch (NumberFormatException nfe) {
+            assertTrue(nfe.getMessage().startsWith("Value \"AAAAA"), "exception message starts as expected?");
+            assertTrue(nfe.getMessage().contains("truncated"), "exception message value contains truncated");
+        }
+    }
+}
