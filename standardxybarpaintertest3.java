95ac0c21d72f8bc2921e7220e7706a7b54a745b1
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:34:04 +0800

    Original processedManualTestCases standardxybarpaintertest3.java

---
+++ standardxybarpaintertest3.java
@@ -0,0 +1,22 @@
+package org.jfree.chart.renderer.xy;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import org.jfree.chart.TestUtils;
+import org.jfree.chart.api.PublicCloneable;
+import org.junit.jupiter.api.Test;
+
+public class StandardXYBarPainterTestTest3 {
+
+    /**
+     * Confirm that cloning isn't implemented (it isn't required, because
+     * instances of the class are immutable).
+     */
+    @Test
+    public void testCloning() {
+        StandardXYBarPainter p1 = new StandardXYBarPainter();
+        assertFalse(p1 instanceof Cloneable);
+        assertFalse(p1 instanceof PublicCloneable);
+    }
+}
