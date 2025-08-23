cdc96a520aacb9b72ef4364dd6c124a41ab9f869
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:44:17 +0800

    Original processedAutoTestCases collectionbag_estest47.java

---
+++ collectionbag_estest47.java
@@ -0,0 +1,42 @@
+package org.apache.commons.collections4.bag;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.lang.reflect.Array;
+import java.util.Collection;
+import java.util.ConcurrentModificationException;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Stack;
+import java.util.TreeSet;
+import org.apache.commons.collections4.Bag;
+import org.apache.commons.collections4.Predicate;
+import org.apache.commons.collections4.SortedBag;
+import org.apache.commons.collections4.Transformer;
+import org.apache.commons.collections4.functors.ComparatorPredicate;
+import org.apache.commons.collections4.functors.ConstantTransformer;
+import org.apache.commons.collections4.functors.FalsePredicate;
+import org.apache.commons.collections4.functors.IdentityPredicate;
+import org.apache.commons.collections4.functors.IfTransformer;
+import org.apache.commons.collections4.functors.InvokerTransformer;
+import org.apache.commons.collections4.functors.MapTransformer;
+import org.apache.commons.collections4.functors.NullIsExceptionPredicate;
+import org.apache.commons.collections4.functors.TransformerPredicate;
+import org.apache.commons.collections4.functors.UniquePredicate;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class CollectionBag_ESTestTest47 extends CollectionBag_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test46() throws Throwable {
+        TreeBag<Integer> treeBag0 = new TreeBag<Integer>();
+        HashBag<Object> hashBag0 = new HashBag<Object>((Collection<?>) treeBag0);
+        CollectionBag<Object> collectionBag0 = new CollectionBag<Object>(hashBag0);
+        boolean boolean0 = collectionBag0.containsAll(treeBag0);
+        assertTrue(boolean0);
+    }
+}
