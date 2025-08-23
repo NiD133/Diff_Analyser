2a673ba8ac63a9e82e1df4a32842b7044f608dfb
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:15:26 +0800

    Original processedAutoTestCases shapecollection_estest22.java

---
+++ shapecollection_estest22.java
@@ -0,0 +1,37 @@
+package org.locationtech.spatial4j.shape;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Stack;
+import java.util.Vector;
+import java.util.function.Predicate;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+import org.locationtech.spatial4j.context.SpatialContext;
+import org.locationtech.spatial4j.context.SpatialContextFactory;
+import org.locationtech.spatial4j.distance.GeodesicSphereDistCalc;
+import org.locationtech.spatial4j.shape.impl.PointImpl;
+import org.locationtech.spatial4j.shape.jts.JtsPoint;
+
+public class ShapeCollection_ESTestTest22 extends ShapeCollection_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test21() throws Throwable {
+        // Undeclared exception!
+        try {
+            ShapeCollection.computeMutualDisjoint((List<? extends Shape>) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.locationtech.spatial4j.shape.ShapeCollection", e);
+        }
+    }
+}
