660dcedb90daadc29a09363da2d594eb21dd6f90
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:50:02 +0800

    Original processedAutoTestCases extrafieldutils_estest36.java

---
+++ extrafieldutils_estest36.java
@@ -0,0 +1,21 @@
+package org.apache.commons.compress.archivers.zip;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.zip.ZipException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class ExtraFieldUtils_ESTestTest36 extends ExtraFieldUtils_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test35() throws Throwable {
+        AsiExtraField asiExtraField0 = new AsiExtraField();
+        byte[] byteArray0 = asiExtraField0.getCentralDirectoryData();
+        ExtraFieldUtils.UnparseableExtraField extraFieldUtils_UnparseableExtraField0 = ExtraFieldUtils.UnparseableExtraField.SKIP;
+        ZipExtraField[] zipExtraFieldArray0 = ExtraFieldUtils.parse(byteArray0, false, extraFieldUtils_UnparseableExtraField0);
+        assertEquals(0, zipExtraFieldArray0.length);
+    }
+}
