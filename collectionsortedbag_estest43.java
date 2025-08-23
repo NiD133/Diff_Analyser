55fbe588153440919d7d43a7531637a42da3c9b3
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:44:37 +0800

    gemini processedAutoTestCases collectionsortedbag_estest43.java

--- collectionsortedbag_estest43.java
+++ collectionsortedbag_estest43.java
@@ -1,49 +1,30 @@
 package org.apache.commons.collections4.bag;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.ConcurrentModificationException;
-import java.util.LinkedList;
-import java.util.Locale;
-import java.util.PriorityQueue;
-import java.util.Set;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.Predicate;
 import org.apache.commons.collections4.SortedBag;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionPredicate;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.IfClosure;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.NOPClosure;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.TransformedPredicate;
-import org.apache.commons.collections4.functors.TransformerPredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class CollectionSortedBag_ESTestTest43 extends CollectionSortedBag_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
-    @Test(timeout = 4000)
-    public void test42() throws Throwable {
-        Comparator<Object> comparator0 = (Comparator<Object>) mock(Comparator.class, new ViolatedAssumptionAnswer());
-        TreeBag<Object> treeBag0 = new TreeBag<Object>(comparator0);
-        SynchronizedSortedBag<Object> synchronizedSortedBag0 = new SynchronizedSortedBag<Object>(treeBag0);
-        SortedBag<Object> sortedBag0 = CollectionSortedBag.collectionSortedBag((SortedBag<Object>) synchronizedSortedBag0);
-        assertEquals(0, sortedBag0.size());
+/**
+ * Tests for the {@link CollectionSortedBag} decorator.
+ */
+public class CollectionSortedBagTest {
+
+    /**
+     * Tests that the {@code collectionSortedBag()} factory method correctly
+     * decorates an empty bag, resulting in a new empty bag.
+     */
+    @Test
+    public void collectionSortedBagFactoryMethod_withEmptyBag_returnsEmptyBag() {
+        // Arrange: Create an empty source bag. A TreeBag is a standard SortedBag implementation.
+        final SortedBag<Object> emptySourceBag = new TreeBag<>();
+
+        // Act: Decorate the source bag using the factory method under test.
+        final SortedBag<Object> decoratedBag = CollectionSortedBag.collectionSortedBag(emptySourceBag);
+
+        // Assert: The resulting decorated bag should also be empty.
+        assertTrue("The decorated bag should be empty", decoratedBag.isEmpty());
+        assertEquals("The size of the decorated bag should be 0", 0, decoratedBag.size());
     }
 }
