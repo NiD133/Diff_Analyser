78b3a668f03a816a5504cdf5a3e875fba0471529
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:07:00 +0800

    gemini processedAutoTestCases pairedstatsaccumulator_estest23.java

--- pairedstatsaccumulator_estest23.java
+++ pairedstatsaccumulator_estest23.java
@@ -1,28 +1,20 @@
 package com.google.common.math;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.ArrayDeque;
-import java.util.Iterator;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class PairedStatsAccumulator_ESTestTest23 extends PairedStatsAccumulator_ESTest_scaffolding {
+/**
+ * Tests for {@link PairedStatsAccumulator}.
+ */
+public class PairedStatsAccumulatorTest {
 
-    @Test(timeout = 4000)
-    public void test22() throws Throwable {
-        PairedStatsAccumulator pairedStatsAccumulator0 = new PairedStatsAccumulator();
-        // Undeclared exception!
-        try {
-            pairedStatsAccumulator0.addAll((PairedStats) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("com.google.common.math.PairedStatsAccumulator", e);
-        }
+    @Test(expected = NullPointerException.class)
+    public void addAll_whenGivenNull_throwsNullPointerException() {
+        // Arrange
+        PairedStatsAccumulator accumulator = new PairedStatsAccumulator();
+
+        // Act
+        accumulator.addAll(null);
+
+        // Assert: NullPointerException is expected, as declared by the @Test annotation.
     }
 }
