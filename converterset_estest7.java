8f44e749fe72649bbf4fcc77c828854e394ebd65
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:45:49 +0800

    gemini processedAutoTestCases converterset_estest7.java

--- converterset_estest7.java
+++ converterset_estest7.java
@@ -1,33 +1,28 @@
 package org.joda.time.convert;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.joda.time.Hours;
-import org.joda.time.Interval;
-import org.joda.time.MutablePeriod;
-import org.joda.time.PeriodType;
-import org.joda.time.Seconds;
-import org.joda.time.chrono.CopticChronology;
-import org.junit.runner.RunWith;
 
-public class ConverterSet_ESTestTest7 extends ConverterSet_ESTest_scaffolding {
+/**
+ * Unit tests for {@link ConverterSet}.
+ */
+public class ConverterSetTest {
 
-    @Test(timeout = 4000)
-    public void test06() throws Throwable {
-        Converter[] converterArray0 = new Converter[1];
-        ConverterSet converterSet0 = new ConverterSet(converterArray0);
-        // Undeclared exception!
-        try {
-            converterSet0.remove(converterArray0[0], converterArray0);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.joda.time.convert.ConverterSet", e);
-        }
+    /**
+     * Tests that calling remove() with a null converter throws a NullPointerException,
+     * as specified by the method's contract.
+     */
+    @Test(expected = NullPointerException.class)
+    public void remove_whenConverterIsNull_throwsNullPointerException() {
+        // Arrange: Create a ConverterSet instance. Its initial content is irrelevant
+        // for this test, as the null check should happen before any processing.
+        // An empty set is the simplest valid state.
+        ConverterSet converterSet = new ConverterSet(new Converter[0]);
+
+        // Act: Attempt to remove a null converter.
+        // The second argument is for capturing the removed converter, which is not
+        // relevant to this exception test, so we can pass null.
+        converterSet.remove(null, null);
+
+        // Assert: The @Test(expected) annotation asserts that a NullPointerException is thrown.
     }
 }
