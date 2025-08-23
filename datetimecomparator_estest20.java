44aea9df0cefe12ee1d998a49301f48260b30d78
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:46:36 +0800

    Original processedAutoTestCases datetimecomparator_estest20.java

---
+++ datetimecomparator_estest20.java
@@ -0,0 +1,19 @@
+package org.joda.time;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class DateTimeComparator_ESTestTest20 extends DateTimeComparator_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test19() throws Throwable {
+        DateTimeFieldType dateTimeFieldType0 = DateTimeFieldType.dayOfYear();
+        DateTimeComparator dateTimeComparator0 = DateTimeComparator.getInstance(dateTimeFieldType0);
+        DateTimeFieldType dateTimeFieldType1 = dateTimeComparator0.getUpperLimit();
+        assertNull(dateTimeFieldType1);
+    }
+}
