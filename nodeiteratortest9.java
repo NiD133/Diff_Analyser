ee795699bb1f9927da2684606d2eac686d12ce99
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:31:32 +0800

    Original processedManualTestCases nodeiteratortest9.java

---
+++ nodeiteratortest9.java
@@ -0,0 +1,72 @@
+package org.jsoup.nodes;
+
+import org.jsoup.Jsoup;
+import org.junit.jupiter.api.Test;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class NodeIteratorTestTest9 {
+
+    String html = "<div id=1><p>One<p>Two</div><div id=2><p>Three<p>Four</div>";
+
+    static <T extends Node> void assertIterates(Iterator<T> it, String expected) {
+        Node previous = null;
+        StringBuilder actual = new StringBuilder();
+        while (it.hasNext()) {
+            Node node = it.next();
+            assertNotNull(node);
+            assertNotSame(previous, node);
+            trackSeen(node, actual);
+            previous = node;
+        }
+        assertEquals(expected, actual.toString());
+    }
+
+    static void assertContents(Element el, String expected) {
+        NodeIterator<Node> it = NodeIterator.from(el);
+        assertIterates(it, expected);
+    }
+
+    public static void trackSeen(Node node, StringBuilder actual) {
+        if (node instanceof Element) {
+            Element el = (Element) node;
+            actual.append(el.tagName());
+            if (el.hasAttr("id"))
+                actual.append("#").append(el.id());
+        } else if (node instanceof TextNode)
+            actual.append(((TextNode) node).text());
+        else
+            actual.append(node.nodeName());
+        actual.append(";");
+    }
+
+    @Test
+    void canReplace() {
+        String html = "<div id=out1><div id=1><p>One<p>Two</div><div id=2><p>Three<p>Four</div></div><div id=out2>Out2";
+        Document doc = Jsoup.parse(html);
+        NodeIterator<Node> it = NodeIterator.from(doc);
+        StringBuilder seen = new StringBuilder();
+        while (it.hasNext()) {
+            Node node = it.next();
+            trackSeen(node, seen);
+            if (node.attr("id").equals("1")) {
+                node.replaceWith(new Element("span").text("Foo"));
+            }
+        }
+        assertEquals("#root;html;head;body;div#out1;div#1;span;Foo;div#2;p;Three;p;Four;div#out2;Out2;", seen.toString());
+        // ^^ we don't see <p>One, do see the replaced in <span>, and the subsequent nodes
+        assertContents(doc, "#root;html;head;body;div#out1;span;Foo;div#2;p;Three;p;Four;div#out2;Out2;");
+        it = NodeIterator.from(doc);
+        seen = new StringBuilder();
+        while (it.hasNext()) {
+            Node node = it.next();
+            trackSeen(node, seen);
+            if (node.attr("id").equals("2")) {
+                node.replaceWith(new Element("span").text("Bar"));
+            }
+        }
+        assertEquals("#root;html;head;body;div#out1;span;Foo;div#2;span;Bar;div#out2;Out2;", seen.toString());
+        assertContents(doc, "#root;html;head;body;div#out1;span;Foo;span;Bar;div#out2;Out2;");
+    }
+}
