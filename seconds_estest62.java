013eb6488745fb62b249872dc04c648bace3efd4
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:13:11 +0800

    gemini processedAutoTestCases seconds_estest62.java

--- seconds_estest62.java
+++ seconds_estest62.java
@@ -1,18 +1,23 @@
 package org.joda.time;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class Seconds_ESTestTest62 extends Seconds_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link Seconds} class.
+ */
+public class SecondsTest {
 
-    @Test(timeout = 4000)
-    public void test61() throws Throwable {
-        Seconds seconds0 = Seconds.TWO;
-        Seconds seconds1 = seconds0.minus(seconds0);
-        assertEquals(0, seconds1.getSeconds());
+    @Test
+    public void minus_whenSubtractingAValueFromItself_shouldReturnZero() {
+        // Arrange
+        Seconds twoSeconds = Seconds.TWO;
+        Seconds expectedResult = Seconds.ZERO;
+
+        // Act
+        Seconds actualResult = twoSeconds.minus(twoSeconds);
+
+        // Assert
+        assertEquals(expectedResult, actualResult);
     }
 }
