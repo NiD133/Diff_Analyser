62e00ea72ea60287bcbca1d43ae6bab42a3c73dc
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:28:48 +0800

    Original processedAutoTestCases xnode_estest25.java

---
+++ xnode_estest25.java
@@ -0,0 +1,32 @@
+package org.apache.ibatis.parsing;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.List;
+import java.util.Locale;
+import java.util.Properties;
+import java.util.function.Supplier;
+import javax.imageio.metadata.IIOMetadataNode;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.xml.sax.ext.DefaultHandler2;
+
+public class XNode_ESTestTest25 extends XNode_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test024() throws Throwable {
+        Properties properties0 = new Properties();
+        IIOMetadataNode iIOMetadataNode0 = new IIOMetadataNode();
+        XPathParser xPathParser0 = new XPathParser((Document) null, true);
+        XNode xNode0 = new XNode(xPathParser0, iIOMetadataNode0, properties0);
+        Float float0 = new Float((double) 293L);
+        Float float1 = xNode0.getFloatBody(float0);
+        assertEquals(293.0F, (float) float1, 0.01F);
+    }
+}
