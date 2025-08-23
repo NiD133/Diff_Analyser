c21a27afcb8dda389a6dfb174de5f965a9f0d908
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:48:19 +0800

    gemini processedAutoTestCases elements_estest116.java

--- elements_estest116.java
+++ elements_estest116.java
@@ -1,36 +1,26 @@
 package org.jsoup.select;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.ConcurrentModificationException;
-import java.util.LinkedHashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.function.Predicate;
-import java.util.function.UnaryOperator;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.jsoup.nodes.Comment;
-import org.jsoup.nodes.DataNode;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.nodes.FormElement;
-import org.jsoup.nodes.TextNode;
 import org.jsoup.parser.Parser;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class Elements_ESTestTest116 extends Elements_ESTest_scaffolding {
+import static org.junit.Assert.assertSame;
 
-    @Test(timeout = 4000)
-    public void test115() throws Throwable {
-        Document document0 = Parser.parseBodyFragment("Bb,Y|aK", "Bb,Y|aK");
-        Elements elements0 = document0.getAllElements();
-        Elements elements1 = elements0.remove();
-        assertSame(elements1, elements0);
+/**
+ * Test suite for the {@link Elements} class.
+ */
+public class ElementsTest {
+
+    @Test
+    public void removeShouldReturnSameInstanceForChaining() {
+        // Arrange: Create a document and select a collection of elements.
+        Document doc = Parser.parseBodyFragment("<div><p>One</p><p>Two</p></div>", "");
+        Elements elements = doc.select("p");
+
+        // Act: Call the remove() method on the collection.
+        Elements returnedElements = elements.remove();
+
+        // Assert: The method should return the same Elements instance to allow for method chaining.
+        assertSame("The remove() method is expected to return the same instance.", elements, returnedElements);
     }
 }
