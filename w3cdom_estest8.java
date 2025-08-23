1ed2ba69836404bb8635816368c8f991daf8a0f3
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:27:02 +0800

    gemini processedAutoTestCases w3cdom_estest8.java

--- w3cdom_estest8.java
+++ w3cdom_estest8.java
@@ -1,49 +1,44 @@
 package org.jsoup.helper;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import com.sun.org.apache.xerces.internal.dom.DocumentImpl;
-import java.io.Reader;
-import java.io.StringReader;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
-import javax.imageio.metadata.IIOMetadataNode;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.jsoup.nodes.Attributes;
-import org.jsoup.nodes.DataNode;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.DocumentType;
-import org.jsoup.nodes.Element;
-import org.jsoup.nodes.FormElement;
-import org.jsoup.nodes.XmlDeclaration;
-import org.jsoup.parser.Parser;
-import org.jsoup.parser.Tag;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 import org.w3c.dom.DOMException;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
+import javax.imageio.metadata.IIOMetadataNode;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+// The original class structure is kept, as the focus is on improving the test case itself.
 public class W3CDom_ESTestTest8 extends W3CDom_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test07() throws Throwable {
-        W3CDom w3CDom0 = new W3CDom();
-        Class<Document> class0 = Document.class;
-        IIOMetadataNode iIOMetadataNode0 = new IIOMetadataNode("jsoupSource");
-        NodeList nodeList0 = iIOMetadataNode0.getElementsByTagName("jsoupSource");
-        // Undeclared exception!
+    /**
+     * Verifies that sourceNodes() throws a DOMException when given a NodeList
+     * containing nodes that do not support the getUserData() method. The method
+     * relies on getUserData() to retrieve the original jsoup node.
+     */
+    @Test
+    public void sourceNodesShouldThrowExceptionForUnsupportedNodeImplementation() {
+        // Arrange
+        W3CDom w3cDom = new W3CDom();
+
+        // We use IIOMetadataNode because it's a standard org.w3c.dom.Node implementation
+        // that is known to throw a DOMException on calls to getUserData(). This simulates
+        // passing a W3C node to sourceNodes() that was not created by W3CDom.
+        final String nodeName = "testNode";
+        IIOMetadataNode unsupportedNode = new IIOMetadataNode(nodeName);
+        NodeList nodeListWithUnsupportedNode = unsupportedNode.getElementsByTagName(nodeName);
+
+        // Act & Assert
         try {
-            w3CDom0.sourceNodes(nodeList0, class0);
-            fail("Expecting exception: DOMException");
+            w3cDom.sourceNodes(nodeListWithUnsupportedNode, Document.class);
+            fail("Expected a DOMException to be thrown because IIOMetadataNode does not support getUserData().");
         } catch (DOMException e) {
-            //
-            // Method not supported
-            //
-            verifyException("javax.imageio.metadata.IIOMetadataNode", e);
+            // Success: The expected exception was caught.
+            // We can further verify that the exception is of the expected type.
+            assertEquals("The exception code should indicate that the operation is not supported.",
+                DOMException.NOT_SUPPORTED_ERR, e.code);
         }
     }
 }
