b6f323f3be0b2ab50608b73540f6312e76b8d512
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:44:57 +0800

    Original processedAutoTestCases comparatorchain_estest18.java

---
+++ comparatorchain_estest18.java
@@ -0,0 +1,40 @@
+package org.apache.commons.collections4.comparators;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.nio.ByteBuffer;
+import java.nio.LongBuffer;
+import java.util.BitSet;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.ToIntFunction;
+import java.util.function.ToLongFunction;
+import org.apache.commons.collections4.Closure;
+import org.apache.commons.collections4.functors.ClosureTransformer;
+import org.apache.commons.collections4.functors.ComparatorPredicate;
+import org.apache.commons.collections4.functors.ExceptionClosure;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class ComparatorChain_ESTestTest18 extends ComparatorChain_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test17() throws Throwable {
+        ComparatorChain<ByteBuffer> comparatorChain0 = null;
+        try {
+            comparatorChain0 = new ComparatorChain<ByteBuffer>((List<Comparator<ByteBuffer>>) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.apache.commons.collections4.comparators.ComparatorChain", e);
+        }
+    }
+}
