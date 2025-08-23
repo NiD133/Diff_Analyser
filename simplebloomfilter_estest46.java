09dc370be3577ef3795f09d1c5af120b070076af
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:16:17 +0800

    gemini processedAutoTestCases simplebloomfilter_estest46.java

--- simplebloomfilter_estest46.java
+++ simplebloomfilter_estest46.java
@@ -1,23 +1,38 @@
 package org.apache.commons.collections4.bloomfilter;
 
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.function.IntPredicate;
-import java.util.function.LongPredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
 
-public class SimpleBloomFilter_ESTestTest46 extends SimpleBloomFilter_ESTest_scaffolding {
+/**
+ * Tests for the {@link SimpleBloomFilter}.
+ */
+public class SimpleBloomFilterTest {
 
-    @Test(timeout = 4000)
-    public void test45() throws Throwable {
-        Shape shape0 = Shape.fromNM(2147483605, 2147483605);
-        SimpleBloomFilter simpleBloomFilter0 = new SimpleBloomFilter(shape0);
-        long[] longArray0 = simpleBloomFilter0.asBitMapArray();
-        assertEquals(33554432, longArray0.length);
+    /**
+     * Tests that a SimpleBloomFilter is initialized with a correctly sized internal
+     * bitmap array. The size of this array is determined by the number of bits
+     * specified in the filter's Shape, where the array length must be sufficient
+     * to hold all the bits.
+     */
+    @Test
+    public void testConstructorAllocatesCorrectlySizedBitMapArray() {
+        // Arrange
+        // Define a shape with a large number of bits to test the allocation logic.
+        final int numberOfBits = 2_147_483_605;
+        final int numberOfItems = 1000; // This value is not critical for this test.
+        final Shape shape = Shape.fromNM(numberOfItems, numberOfBits);
+
+        // The expected size of the long array is the number of bits divided by 64
+        // (the number of bits in a long), rounded up to the nearest integer.
+        // This is equivalent to Math.ceil(numberOfBits / 64.0).
+        final int expectedBitMapArrayLength = (numberOfBits + 63) / 64;
+
+        // Act
+        final SimpleBloomFilter bloomFilter = new SimpleBloomFilter(shape);
+        final long[] bitMapArray = bloomFilter.asBitMapArray();
+
+        // Assert
+        assertEquals("The bitmap array should have the correct calculated size",
+                expectedBitMapArrayLength, bitMapArray.length);
     }
 }
