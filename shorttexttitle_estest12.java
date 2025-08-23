c185af7f260fe1db19a953ed34e2515815b79754
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:15:39 +0800

    gemini processedAutoTestCases shorttexttitle_estest12.java

--- shorttexttitle_estest12.java
+++ shorttexttitle_estest12.java
@@ -1,51 +1,26 @@
 package org.jfree.chart.title;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.awt.Font;
-import java.awt.Graphics2D;
-import java.awt.geom.Rectangle2D;
-import java.awt.image.BufferedImage;
-import java.time.chrono.ChronoLocalDate;
-import java.time.chrono.JapaneseDate;
-import java.util.Calendar;
-import java.util.List;
-import javax.swing.JTable;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.time.chrono.MockJapaneseDate;
-import org.evosuite.runtime.mock.java.util.MockCalendar;
-import org.jfree.chart.ChartRenderingInfo;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.axis.CyclicNumberAxis;
-import org.jfree.chart.axis.ValueAxis;
-import org.jfree.chart.block.RectangleConstraint;
-import org.jfree.chart.block.Size2D;
-import org.jfree.chart.plot.CombinedDomainXYPlot;
-import org.jfree.chart.plot.SpiderWebPlot;
-import org.jfree.chart.plot.pie.PiePlot;
 import org.jfree.data.Range;
-import org.jfree.data.statistics.DefaultBoxAndWhiskerCategoryDataset;
-import org.jfree.data.statistics.DefaultStatisticalCategoryDataset;
-import org.jfree.data.time.TimePeriodAnchor;
-import org.jfree.data.time.TimeSeries;
-import org.junit.runner.RunWith;
+import org.junit.Test;
+import java.awt.Graphics2D;
 
-public class ShortTextTitle_ESTestTest12 extends ShortTextTitle_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link ShortTextTitle} class.
+ */
+public class ShortTextTitleTest {
 
-    @Test(timeout = 4000)
-    public void test11() throws Throwable {
-        ShortTextTitle shortTextTitle0 = new ShortTextTitle("Fe`R4/pOsm9");
-        // Undeclared exception!
-        try {
-            shortTextTitle0.arrangeRN((Graphics2D) null, (Range) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.jfree.chart.title.ShortTextTitle", e);
-        }
+    /**
+     * Verifies that the arrangeRN() method throws a NullPointerException if the
+     * Graphics2D context is null. This is expected behavior as the method
+     * requires a graphics context to perform calculations, such as measuring text.
+     */
+    @Test(expected = NullPointerException.class)
+    public void arrangeRNShouldThrowNullPointerExceptionForNullGraphics() {
+        // Arrange: Create a title instance.
+        ShortTextTitle title = new ShortTextTitle("Test Title");
+
+        // Act & Assert: Call arrangeRN with a null Graphics2D object.
+        // This is expected to throw a NullPointerException.
+        title.arrangeRN((Graphics2D) null, (Range) null);
     }
 }
