e74698271b15e4cebafba5ec75d223d3ffae4eec
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:47:59 +0800

    Original processedAutoTestCases deviationrenderer_estest19.java

---
+++ deviationrenderer_estest19.java
@@ -0,0 +1,39 @@
+package org.jfree.chart.renderer.xy;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
+import java.util.Date;
+import java.util.Locale;
+import java.util.SimpleTimeZone;
+import java.util.TimeZone;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.jfree.chart.ChartRenderingInfo;
+import org.jfree.chart.axis.CategoryAnchor;
+import org.jfree.chart.axis.DateAxis;
+import org.jfree.chart.plot.DatasetRenderingOrder;
+import org.jfree.chart.plot.PlotRenderingInfo;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.data.time.TimeSeriesCollection;
+import org.jfree.data.xy.DefaultHighLowDataset;
+import org.jfree.data.xy.DefaultIntervalXYDataset;
+import org.jfree.data.xy.MatrixSeries;
+import org.jfree.data.xy.MatrixSeriesCollection;
+import org.junit.runner.RunWith;
+
+public class DeviationRenderer_ESTestTest19 extends DeviationRenderer_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test18() throws Throwable {
+        DeviationRenderer deviationRenderer0 = new DeviationRenderer(true, true);
+        Short short0 = new Short((short) 2396);
+        MatrixSeries<Short> matrixSeries0 = new MatrixSeries<Short>(short0, 17, 28);
+        MatrixSeriesCollection<Short> matrixSeriesCollection0 = new MatrixSeriesCollection<Short>(matrixSeries0);
+        deviationRenderer0.findRangeBounds(matrixSeriesCollection0);
+        assertTrue(deviationRenderer0.getDrawSeriesLineAsPath());
+        assertEquals(0.5F, deviationRenderer0.getAlpha(), 0.01F);
+    }
+}
