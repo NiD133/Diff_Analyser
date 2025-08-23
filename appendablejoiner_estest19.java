d8fb50ea5314ab337f5b7a44c8d63ceb5f35cf07
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:37:11 +0800

    Original processedAutoTestCases appendablejoiner_estest19.java

---
+++ appendablejoiner_estest19.java
@@ -0,0 +1,34 @@
+package org.apache.commons.lang3;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.IOException;
+import java.io.PipedWriter;
+import java.nio.BufferOverflowException;
+import java.nio.CharBuffer;
+import java.nio.ReadOnlyBufferException;
+import java.nio.charset.Charset;
+import java.sql.SQLNonTransientConnectionException;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.LinkedHashSet;
+import java.util.Locale;
+import org.apache.commons.lang3.function.FailableBiConsumer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class AppendableJoiner_ESTestTest19 extends AppendableJoiner_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test18() throws Throwable {
+        AppendableJoiner.Builder<SQLNonTransientConnectionException> appendableJoiner_Builder0 = new AppendableJoiner.Builder<SQLNonTransientConnectionException>();
+        StringBuffer stringBuffer0 = new StringBuffer(1078);
+        StringBuilder stringBuilder0 = new StringBuilder(stringBuffer0);
+        AppendableJoiner<SQLNonTransientConnectionException> appendableJoiner0 = appendableJoiner_Builder0.get();
+        StringBuilder stringBuilder1 = appendableJoiner0.joinA(stringBuilder0, (SQLNonTransientConnectionException[]) null);
+        assertEquals("", stringBuilder1.toString());
+    }
+}
