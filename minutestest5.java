c77a0e56bfcfad58592af575e18d8d67351020da
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:31:13 +0800

    Original processedManualTestCases minutestest5.java

---
+++ minutestest5.java
@@ -0,0 +1,41 @@
+package org.joda.time;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class MinutesTestTest5 extends TestCase {
+
+    // (before the late 90's they were all over the place)
+    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
+
+    public static TestSuite suite() {
+        return new TestSuite(TestMinutes.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+    }
+
+    public void testFactory_minutesIn_RInterval() {
+        DateTime start = new DateTime(2006, 6, 9, 12, 3, 0, 0, PARIS);
+        DateTime end1 = new DateTime(2006, 6, 9, 12, 6, 0, 0, PARIS);
+        DateTime end2 = new DateTime(2006, 6, 9, 12, 9, 0, 0, PARIS);
+        assertEquals(0, Minutes.minutesIn((ReadableInterval) null).getMinutes());
+        assertEquals(3, Minutes.minutesIn(new Interval(start, end1)).getMinutes());
+        assertEquals(0, Minutes.minutesIn(new Interval(start, start)).getMinutes());
+        assertEquals(0, Minutes.minutesIn(new Interval(end1, end1)).getMinutes());
+        assertEquals(6, Minutes.minutesIn(new Interval(start, end2)).getMinutes());
+    }
+}
