e1f21535908cd5578dca42bfd6e00bcfdcb8074f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:36:28 +0800

    Original processedManualTestCases yearstest17.java

---
+++ yearstest17.java
@@ -0,0 +1,47 @@
+package org.joda.time;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class YearsTestTest17 extends TestCase {
+
+    // (before the late 90's they were all over the place)
+    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
+
+    public static TestSuite suite() {
+        return new TestSuite(TestYears.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+    }
+
+    public void testMinus_Years() {
+        Years test2 = Years.years(2);
+        Years test3 = Years.years(3);
+        Years result = test2.minus(test3);
+        assertEquals(2, test2.getYears());
+        assertEquals(3, test3.getYears());
+        assertEquals(-1, result.getYears());
+        assertEquals(1, Years.ONE.minus(Years.ZERO).getYears());
+        assertEquals(1, Years.ONE.minus((Years) null).getYears());
+        try {
+            Years.MIN_VALUE.minus(Years.ONE);
+            fail();
+        } catch (ArithmeticException ex) {
+            // expected
+        }
+    }
+}
