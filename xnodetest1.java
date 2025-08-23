af61a6e9cb5673f2cab14db1354ddfbc210de4d1
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:36:21 +0800

    gemini processedManualTestCases xnodetest1.java

--- xnodetest1.java
+++ xnodetest1.java
@@ -1,68 +1,58 @@
 package org.apache.ibatis.parsing;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import java.util.Properties;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-public class XNodeTestTest1 {
+/**
+ * Tests for the XNode.toString() method.
+ * This class focuses on verifying that XNode correctly formats its XML content into a readable, indented string.
+ */
+@DisplayName("XNode toString()")
+class XNodeToStringTest {
 
-    @Test
-    void formatXNodeToString() {
-        XPathParser parser = new XPathParser("<users><user><id>100</id><name>Tom</name><age>30</age><cars><car index=\"1\">BMW</car><car index=\"2\">Audi</car><car index=\"3\">Benz</car></cars></user></users>");
-        String usersNodeToString = parser.evalNode("/users").toString();
-        String userNodeToString = parser.evalNode("/users/user").toString();
-        String carsNodeToString = parser.evalNode("/users/user/cars").toString();
-        String usersNodeToStringExpect = """
-            <users>
-              <user>
-                <id>
-                  100
-                </id>
-                <name>
-                  Tom
-                </name>
-                <age>
-                  30
-                </age>
-                <cars>
-                  <car index="1">
-                    BMW
-                  </car>
-                  <car index="2">
-                    Audi
-                  </car>
-                  <car index="3">
-                    Benz
-                  </car>
-                </cars>
-              </user>
-            </users>
-            """;
-        String userNodeToStringExpect = """
-            <user>
-              <id>
-                100
-              </id>
-              <name>
-                Tom
-              </name>
-              <age>
-                30
-              </age>
-              <cars>
-                <car index="1">
-                  BMW
-                </car>
-                <car index="2">
-                  Audi
-                </car>
-                <car index="3">
-                  Benz
-                </car>
-              </cars>
-            </user>
-            """;
-        String carsNodeToStringExpect = """
+  private static final String XML_DATA = """
+      <users>
+        <user>
+          <id>100</id>
+          <name>Tom</name>
+          <age>30</age>
+          <cars>
+            <car index="1">BMW</car>
+            <car index="2">Audi</car>
+            <car index="3">Benz</car>
+          </cars>
+        </user>
+      </users>""";
+
+  private XPathParser parser;
+
+  @BeforeEach
+  void setUp() {
+    // The parser is initialized with the same XML data for each test.
+    parser = new XPathParser(XML_DATA);
+  }
+
+  @Test
+  @DisplayName("should correctly format the root node")
+  void shouldFormatRootNode() {
+    // Arrange
+    XNode rootNode = parser.evalNode("/users");
+    String expectedXml = """
+        <users>
+          <user>
+            <id>
+              100
+            </id>
+            <name>
+              Tom
+            </name>
+            <age>
+              30
+            </age>
             <cars>
               <car index="1">
                 BMW
@@ -74,9 +64,77 @@ public class XNodeTestTest1 {
                 Benz
               </car>
             </cars>
-            """;
-        assertEquals(usersNodeToStringExpect, usersNodeToString);
-        assertEquals(userNodeToStringExpect, userNodeToString);
-        assertEquals(carsNodeToStringExpect, carsNodeToString);
-    }
+          </user>
+        </users>
+        """;
+
+    // Act
+    String actualXml = rootNode.toString();
+
+    // Assert
+    assertEquals(expectedXml, actualXml);
+  }
+
+  @Test
+  @DisplayName("should correctly format a direct child node")
+  void shouldFormatChildNode() {
+    // Arrange
+    XNode userNode = parser.evalNode("/users/user");
+    String expectedXml = """
+        <user>
+          <id>
+            100
+          </id>
+          <name>
+            Tom
+          </name>
+          <age>
+            30
+          </age>
+          <cars>
+            <car index="1">
+              BMW
+            </car>
+            <car index="2">
+              Audi
+            </car>
+            <car index="3">
+              Benz
+            </car>
+          </cars>
+        </user>
+        """;
+
+    // Act
+    String actualXml = userNode.toString();
+
+    // Assert
+    assertEquals(expectedXml, actualXml);
+  }
+
+  @Test
+  @DisplayName("should correctly format a nested child node")
+  void shouldFormatNestedChildNode() {
+    // Arrange
+    XNode carsNode = parser.evalNode("/users/user/cars");
+    String expectedXml = """
+        <cars>
+          <car index="1">
+            BMW
+          </car>
+          <car index="2">
+            Audi
+          </car>
+          <car index="3">
+            Benz
+          </car>
+        </cars>
+        """;
+
+    // Act
+    String actualXml = carsNode.toString();
+
+    // Assert
+    assertEquals(expectedXml, actualXml);
+  }
 }
