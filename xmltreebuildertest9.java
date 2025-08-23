448684766830cf8726ea1f24bd6c6e1fd7292856
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:36:20 +0800

    gemini processedManualTestCases xmltreebuildertest9.java

--- xmltreebuildertest9.java
+++ xmltreebuildertest9.java
@@ -1,31 +1,31 @@
 package org.jsoup.parser;
 
 import org.jsoup.Jsoup;
-import org.jsoup.TextUtil;
-import org.jsoup.nodes.*;
-import org.jsoup.select.Elements;
+import org.jsoup.nodes.Document;
 import org.junit.jupiter.api.Test;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URISyntaxException;
-import java.nio.charset.StandardCharsets;
-import java.util.List;
+
 import static org.jsoup.nodes.Document.OutputSettings.Syntax;
-import static org.jsoup.parser.Parser.NamespaceHtml;
-import static org.jsoup.parser.Parser.NamespaceXml;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class XmlTreeBuilderTestTest9 {
+/**
+ * Tests for the {@link XmlTreeBuilder}.
+ */
+public class XmlTreeBuilderTest {
 
-    private static void assertXmlNamespace(Element el) {
-        assertEquals(NamespaceXml, el.tag().namespace(), String.format("Element %s not in XML namespace", el.tagName()));
-    }
-
+    /**
+     * Verifies that when a document is parsed using the XML parser, its default
+     * output syntax is correctly set to {@code Syntax.xml}.
+     */
     @Test
-    public void xmlParseDefaultsToHtmlOutputSyntax() {
-        Document doc = Jsoup.parse("x", "", Parser.xmlParser());
+    void xmlParserSetsOutputSyntaxToXml() {
+        // Arrange
+        String simpleXml = "<data/>";
+        Parser xmlParser = Parser.xmlParser();
+
+        // Act
+        Document doc = Jsoup.parse(simpleXml, "", xmlParser);
+
+        // Assert
         assertEquals(Syntax.xml, doc.outputSettings().syntax());
     }
 }
