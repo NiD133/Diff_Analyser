6afa5bc952c761a653eaa0b5d853465d15e0aa7c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:02:32 +0800

    gemini processedAutoTestCases metaphone_estest35.java

--- metaphone_estest35.java
+++ metaphone_estest35.java
@@ -1,19 +1,44 @@
 package org.apache.commons.codec.language;
 
+import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class Metaphone_ESTestTest35 extends Metaphone_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link Metaphone} class.
+ */
+public class MetaphoneTest {
 
-    @Test(timeout = 4000)
-    public void test34() throws Throwable {
-        Metaphone metaphone0 = new Metaphone();
-        String string0 = metaphone0.metaphone("SC");
-        assertEquals("SK", string0);
-        assertEquals(4, metaphone0.getMaxCodeLen());
+    private Metaphone metaphone;
+
+    @Before
+    public void setUp() {
+        metaphone = new Metaphone();
+    }
+
+    /**
+     * Tests that the Metaphone algorithm correctly encodes the initial "SC" combination as "SK".
+     * This is a specific rule in the Metaphone encoding standard.
+     */
+    @Test
+    public void shouldEncodeInitialScAsSk() {
+        // Arrange
+        final String input = "SC";
+        final String expectedEncoding = "SK";
+
+        // Act
+        final String actualEncoding = metaphone.metaphone(input);
+
+        // Assert
+        assertEquals("The encoding for 'SC' should be 'SK'", expectedEncoding, actualEncoding);
+    }
+
+    /**
+     * Tests that a new Metaphone instance has a default maximum code length of 4.
+     */
+    @Test
+    public void shouldHaveDefaultMaxCodeLengthOf4() {
+        // Assert
+        assertEquals(4, metaphone.getMaxCodeLen());
     }
 }
