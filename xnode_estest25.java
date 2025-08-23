cff8c7a23b27aabbce5a0bfdcbb4a2d077f24750
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:28:48 +0800

    gemini processedAutoTestCases xnode_estest25.java

--- xnode_estest25.java
+++ xnode_estest25.java
@@ -1,32 +1,48 @@
 package org.apache.ibatis.parsing;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.List;
-import java.util.Locale;
-import java.util.Properties;
-import java.util.function.Supplier;
-import javax.imageio.metadata.IIOMetadataNode;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
-import org.xml.sax.ext.DefaultHandler2;
 
-public class XNode_ESTestTest25 extends XNode_ESTest_scaffolding {
+import javax.imageio.metadata.IIOMetadataNode;
+import java.util.Properties;
 
-    @Test(timeout = 4000)
-    public void test024() throws Throwable {
-        Properties properties0 = new Properties();
-        IIOMetadataNode iIOMetadataNode0 = new IIOMetadataNode();
-        XPathParser xPathParser0 = new XPathParser((Document) null, true);
-        XNode xNode0 = new XNode(xPathParser0, iIOMetadataNode0, properties0);
-        Float float0 = new Float((double) 293L);
-        Float float1 = xNode0.getFloatBody(float0);
-        assertEquals(293.0F, (float) float1, 0.01F);
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Test suite for the XNode class.
+ */
+public class XNodeTest {
+
+    /**
+     * This test verifies that getFloatBody(defaultValue) returns the provided
+     * default value when the XNode's body is null or empty.
+     */
+    @Test
+    public void getFloatBodyShouldReturnDefaultValueWhenBodyIsNull() {
+        // Arrange
+        // An XPathParser is required by the XNode constructor, but its internal state is not
+        // relevant for this test. We can pass a null document as the original test did.
+        XPathParser parser = new XPathParser((Document) null, false);
+
+        // Create a DOM node that results in a null body within the XNode.
+        // An IIOMetadataNode with no children serves this purpose perfectly.
+        Node emptyNode = new IIOMetadataNode();
+        
+        // The XNode constructor also requires a Properties object for variables.
+        Properties variables = new Properties();
+
+        XNode xnode = new XNode(parser, emptyNode, variables);
+
+        Float defaultValue = 293.0f;
+
+        // Act
+        // Call the method under test. Since the node's body is null,
+        // we expect the default value to be returned.
+        Float actualValue = xnode.getFloatBody(defaultValue);
+
+        // Assert
+        // Verify that the returned value is indeed the default value.
+        assertEquals(defaultValue, actualValue);
     }
 }
