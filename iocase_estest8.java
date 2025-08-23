406525b52915e1e59b3885129f6c3ff2a44a992f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:55:04 +0800

    gemini processedAutoTestCases iocase_estest8.java

--- iocase_estest8.java
+++ iocase_estest8.java
@@ -1,18 +1,45 @@
 package org.apache.commons.io;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertSame;
 
-public class IOCase_ESTestTest8 extends IOCase_ESTest_scaffolding {
+/**
+ * Tests for the static utility methods of {@link IOCase}.
+ */
+public class IOCaseTest {
 
-    @Test(timeout = 4000)
-    public void test07() throws Throwable {
-        IOCase iOCase0 = IOCase.INSENSITIVE;
-        IOCase iOCase1 = IOCase.value(iOCase0, iOCase0);
-        assertEquals(IOCase.INSENSITIVE, iOCase1);
+    /**
+     * Tests that {@link IOCase#value(IOCase, IOCase)} returns the first argument
+     * when it is not null.
+     */
+    @Test
+    public void value_whenFirstArgumentIsNotNull_shouldReturnFirstArgument() {
+        // Arrange
+        final IOCase inputValue = IOCase.INSENSITIVE;
+        final IOCase defaultValue = IOCase.SENSITIVE; // Use a different default to be explicit
+
+        // Act
+        final IOCase result = IOCase.value(inputValue, defaultValue);
+
+        // Assert
+        // The method should return the first argument, ignoring the default.
+        assertSame("Expected the non-null input value to be returned", inputValue, result);
+    }
+
+    /**
+     * Tests that {@link IOCase#value(IOCase, IOCase)} returns the default value
+     * when the first argument is null.
+     */
+    @Test
+    public void value_whenFirstArgumentIsNull_shouldReturnDefaultValue() {
+        // Arrange
+        final IOCase defaultValue = IOCase.SYSTEM;
+
+        // Act
+        final IOCase result = IOCase.value(null, defaultValue);
+
+        // Assert
+        // The method should fall back to the default value.
+        assertSame("Expected the default value to be returned when the input is null", defaultValue, result);
     }
 }
