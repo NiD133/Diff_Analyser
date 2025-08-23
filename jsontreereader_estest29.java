5818aa00a7a7b7f304e727a568285a7bf53e9a0c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:58:01 +0800

    Original processedAutoTestCases jsontreereader_estest29.java

---
+++ jsontreereader_estest29.java
@@ -0,0 +1,32 @@
+package com.google.gson.internal.bind;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonNull;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.Strictness;
+import com.google.gson.stream.JsonToken;
+import java.io.IOException;
+import java.util.ConcurrentModificationException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class JsonTreeReader_ESTestTest29 extends JsonTreeReader_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test028() throws Throwable {
+        JsonPrimitive jsonPrimitive0 = new JsonPrimitive("=7?D~zj~O\"YboE?a");
+        JsonTreeReader jsonTreeReader0 = new JsonTreeReader(jsonPrimitive0);
+        // Undeclared exception!
+        try {
+            jsonTreeReader0.nextDouble();
+            fail("Expecting exception: NumberFormatException");
+        } catch (NumberFormatException e) {
+        }
+    }
+}
