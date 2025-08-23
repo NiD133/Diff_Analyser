8d7c45f71872bb5da32e8ee2d598a441038086e7
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:56:08 +0800

    Original processedAutoTestCases jsonarray_estest43.java

---
+++ jsonarray_estest43.java
@@ -0,0 +1,31 @@
+package com.google.gson;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Iterator;
+import java.util.List;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class JsonArray_ESTestTest43 extends JsonArray_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test42() throws Throwable {
+        JsonArray jsonArray0 = new JsonArray();
+        jsonArray0.add((Number) null);
+        // Undeclared exception!
+        try {
+            jsonArray0.getAsBigInteger();
+            fail("Expecting exception: UnsupportedOperationException");
+        } catch (UnsupportedOperationException e) {
+            //
+            // JsonNull
+            //
+            verifyException("com.google.gson.JsonElement", e);
+        }
+    }
+}
