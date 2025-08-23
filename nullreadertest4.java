c5ffef28b1e0ffad7099e5b04d043d66b5dd927b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:31:34 +0800

    Original processedManualTestCases nullreadertest4.java

---
+++ nullreadertest4.java
@@ -0,0 +1,62 @@
+package org.apache.commons.io.input;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.Reader;
+import org.junit.jupiter.api.Test;
+
+public class NullReaderTestTest4 {
+
+    // Use the same message as in java.io.InputStream.reset() in OpenJDK 8.0.275-1.
+    private static final String MARK_RESET_NOT_SUPPORTED = "mark/reset not supported";
+
+    private static final class TestNullReader extends NullReader {
+
+        TestNullReader(final int size) {
+            super(size);
+        }
+
+        TestNullReader(final int size, final boolean markSupported, final boolean throwEofException) {
+            super(size, markSupported, throwEofException);
+        }
+
+        @Override
+        protected int processChar() {
+            return (int) getPosition() - 1;
+        }
+
+        @Override
+        protected void processChars(final char[] chars, final int offset, final int length) {
+            final int startPos = (int) getPosition() - length;
+            for (int i = offset; i < length; i++) {
+                chars[i] = (char) (startPos + i);
+            }
+        }
+    }
+
+    @Test
+    void testRead() throws Exception {
+        final int size = 5;
+        final TestNullReader reader = new TestNullReader(size);
+        for (int i = 0; i < size; i++) {
+            assertEquals(i, reader.read(), "Check Value [" + i + "]");
+        }
+        // Check End of File
+        assertEquals(-1, reader.read(), "End of File");
+        // Test reading after the end of file
+        try {
+            final int result = reader.read();
+            fail("Should have thrown an IOException, value=[" + result + "]");
+        } catch (final IOException e) {
+            assertEquals("Read after end of file", e.getMessage());
+        }
+        // Close - should reset
+        reader.close();
+        assertEquals(0, reader.getPosition(), "Available after close");
+    }
+}
