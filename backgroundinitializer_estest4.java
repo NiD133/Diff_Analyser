72a7402b56cd2d7ab01ee643a99954d75588c3dc
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:38:34 +0800

    gemini processedAutoTestCases backgroundinitializer_estest4.java

--- backgroundinitializer_estest4.java
+++ backgroundinitializer_estest4.java
@@ -1,44 +1,29 @@
 package org.apache.commons.lang3.concurrent;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.concurrent.Delayed;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.RejectedExecutionException;
-import java.util.concurrent.SynchronousQueue;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.evosuite.runtime.mock.java.lang.MockException;
-import org.evosuite.runtime.mock.java.lang.MockThrowable;
-import org.evosuite.runtime.testdata.FileSystemHandling;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertNull;
 
-public class BackgroundInitializer_ESTestTest4 extends BackgroundInitializer_ESTest_scaffolding {
+/**
+ * Contains tests for the {@link BackgroundInitializer} class, focusing on its
+ * executor management.
+ */
+public class BackgroundInitializerTest {
 
-    @Test(timeout = 4000)
-    public void test03() throws Throwable {
-        ForkJoinPool forkJoinPool0 = ForkJoinPool.commonPool();
-        BackgroundInitializer<Delayed> backgroundInitializer0 = new BackgroundInitializer<Delayed>(forkJoinPool0);
-        forkJoinPool0.getFactory();
-        backgroundInitializer0.start();
-        backgroundInitializer0.getFuture();
-        BackgroundInitializer<Exception> backgroundInitializer1 = new BackgroundInitializer<Exception>();
-        BackgroundInitializer.builder();
-        backgroundInitializer1.getActiveExecutor();
-        backgroundInitializer1.isInitialized();
-        BackgroundInitializer<BackgroundInitializer<Exception>> backgroundInitializer2 = new BackgroundInitializer<BackgroundInitializer<Exception>>();
-        ConcurrentException concurrentException0 = new ConcurrentException();
-        concurrentException0.getSuppressed();
-        backgroundInitializer2.getTypedException(concurrentException0);
-        backgroundInitializer2.setExternalExecutor((ExecutorService) null);
-        ExecutorService executorService0 = backgroundInitializer1.getExternalExecutor();
-        assertNull(executorService0);
+    /**
+     * Verifies that getExternalExecutor() returns null when the initializer is
+     * created with the default constructor (i.e., without providing an executor).
+     */
+    @Test
+    public void getExternalExecutorShouldReturnNullWhenNotProvided() {
+        // Arrange: Create a BackgroundInitializer using its default constructor.
+        // The generic type is not relevant for this test, so Object is a suitable choice.
+        BackgroundInitializer<Object> initializer = new BackgroundInitializer<>();
+
+        // Act: Retrieve the external executor service from the initializer.
+        final ExecutorService externalExecutor = initializer.getExternalExecutor();
+
+        // Assert: The external executor should be null, as none was configured.
+        assertNull("The external executor should be null by default", externalExecutor);
     }
 }
