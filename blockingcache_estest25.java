1e437126d22b6ea173611b42d85408f8b6a83cd9
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:39:31 +0800

    Original processedAutoTestCases blockingcache_estest25.java

---
+++ blockingcache_estest25.java
@@ -0,0 +1,23 @@
+package org.apache.ibatis.cache.decorators;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.concurrent.CountDownLatch;
+import org.apache.ibatis.cache.Cache;
+import org.apache.ibatis.cache.impl.PerpetualCache;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class BlockingCache_ESTestTest25 extends BlockingCache_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test24() throws Throwable {
+        PerpetualCache perpetualCache0 = new PerpetualCache("Detected an attempt at releasing unacquired lock. This should never happen.");
+        SoftCache softCache0 = new SoftCache(perpetualCache0);
+        BlockingCache blockingCache0 = new BlockingCache(softCache0);
+        String string0 = blockingCache0.getId();
+        assertEquals("Detected an attempt at releasing unacquired lock. This should never happen.", string0);
+    }
+}
