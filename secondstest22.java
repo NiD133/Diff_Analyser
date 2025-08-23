ef2600761ea04b5ab6e4c064f6fa2b2b1e45867a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:33:15 +0800

    Original processedManualTestCases secondstest22.java

---
+++ secondstest22.java
@@ -0,0 +1,44 @@
+package org.joda.time;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class SecondsTestTest22 extends TestCase {
+
+    // (before the late 90's they were all over the place)
+    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
+
+    public static TestSuite suite() {
+        return new TestSuite(TestSeconds.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+    }
+
+    public void testMinus_int() {
+        Seconds test2 = Seconds.seconds(2);
+        Seconds result = test2.minus(3);
+        assertEquals(2, test2.getSeconds());
+        assertEquals(-1, result.getSeconds());
+        assertEquals(1, Seconds.ONE.minus(0).getSeconds());
+        try {
+            Seconds.MIN_VALUE.minus(1);
+            fail();
+        } catch (ArithmeticException ex) {
+            // expected
+        }
+    }
+}
