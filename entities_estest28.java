23652e5bc515b10885a5f227a5f48820ca63457c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:49:26 +0800

    Original processedAutoTestCases entities_estest28.java

---
+++ entities_estest28.java
@@ -0,0 +1,28 @@
+package org.jsoup.nodes;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.File;
+import java.io.PipedWriter;
+import java.io.StringWriter;
+import java.nio.BufferOverflowException;
+import java.nio.CharBuffer;
+import java.nio.ReadOnlyBufferException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.evosuite.runtime.mock.java.io.MockFileWriter;
+import org.evosuite.runtime.mock.java.io.MockPrintStream;
+import org.jsoup.internal.QuietAppendable;
+import org.junit.runner.RunWith;
+
+public class Entities_ESTestTest28 extends Entities_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test27() throws Throwable {
+        Document.OutputSettings document_OutputSettings0 = new Document.OutputSettings();
+        String string0 = Entities.escape("\"PvE5H.,d+SC ,Q,}'xM", document_OutputSettings0);
+        assertEquals("&quot;PvE5H.,d+SC ,Q,}&apos;xM", string0);
+    }
+}
