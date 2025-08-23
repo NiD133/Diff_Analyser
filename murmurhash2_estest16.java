9925efc1d038ffdc3fd9fb551b9b7803736e6308
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:04:23 +0800

    Original processedAutoTestCases murmurhash2_estest16.java

---
+++ murmurhash2_estest16.java
@@ -0,0 +1,25 @@
+package org.apache.commons.codec.digest;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class MurmurHash2_ESTestTest16 extends MurmurHash2_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test15() throws Throwable {
+        // Undeclared exception!
+        try {
+            MurmurHash2.hash32((byte[]) null, 32);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.apache.commons.codec.digest.MurmurHash2", e);
+        }
+    }
+}
