595c5693faadb68b64fef44834097b52ce580d44
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:24:16 +0800

    Original processedAutoTestCases typehandler_estest17.java

---
+++ typehandler_estest17.java
@@ -0,0 +1,28 @@
+package org.apache.commons.cli;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.File;
+import java.util.Date;
+import java.util.Map;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.lang.MockThrowable;
+import org.junit.runner.RunWith;
+
+public class TypeHandler_ESTestTest17 extends TypeHandler_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test16() throws Throwable {
+        try {
+            TypeHandler.createNumber("I%5HIg?GFqZ!&");
+            fail("Expecting exception: Exception");
+        } catch (Exception e) {
+            //
+            // java.lang.NumberFormatException: For input string: \"I%5HIg?GFqZ!&\"
+            //
+            verifyException("org.apache.commons.cli.ParseException", e);
+        }
+    }
+}
