53e5147c61278f105172a523bf4b287c684bea9a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:58:00 +0800

    Original processedAutoTestCases jsontreereader_estest25.java

---
+++ jsontreereader_estest25.java
@@ -0,0 +1,38 @@
+package com.google.gson.internal.bind;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonNull;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.Strictness;
+import com.google.gson.stream.JsonToken;
+import java.io.IOException;
+import java.util.ConcurrentModificationException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class JsonTreeReader_ESTestTest25 extends JsonTreeReader_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test024() throws Throwable {
+        JsonArray jsonArray0 = new JsonArray();
+        JsonTreeReader jsonTreeReader0 = new JsonTreeReader(jsonArray0);
+        jsonTreeReader0.beginArray();
+        jsonArray0.add((String) null);
+        // Undeclared exception!
+        try {
+            jsonTreeReader0.nextInt();
+            fail("Expecting exception: ConcurrentModificationException");
+        } catch (ConcurrentModificationException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("java.util.ArrayList$Itr", e);
+        }
+    }
+}
