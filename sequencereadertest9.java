3262bc509712de1e970304326d68e049a4abd3b7
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:33:32 +0800

    Original processedManualTestCases sequencereadertest9.java

---
+++ sequencereadertest9.java
@@ -0,0 +1,77 @@
+package org.apache.commons.io.input;
+
+import static org.apache.commons.io.IOUtils.EOF;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.Test;
+
+public class SequenceReaderTestTest9 {
+
+    private static final char NUL = 0;
+
+    private void checkArray(final char[] expected, final char[] actual) {
+        for (int i = 0; i < expected.length; i++) {
+            assertEquals(expected[i], actual[i], "Compare[" + i + "]");
+        }
+    }
+
+    private void checkRead(final Reader reader, final String expected) throws IOException {
+        for (int i = 0; i < expected.length(); i++) {
+            assertEquals(expected.charAt(i), (char) reader.read(), "Read[" + i + "] of '" + expected + "'");
+        }
+    }
+
+    private void checkReadEof(final Reader reader) throws IOException {
+        for (int i = 0; i < 10; i++) {
+            assertEquals(-1, reader.read());
+        }
+    }
+
+    private static class CustomReader extends Reader {
+
+        boolean closed;
+
+        protected void checkOpen() throws IOException {
+            if (closed) {
+                throw new IOException("emptyReader already closed");
+            }
+        }
+
+        @Override
+        public void close() throws IOException {
+            closed = true;
+        }
+
+        public boolean isClosed() {
+            return closed;
+        }
+
+        @Override
+        public int read(final char[] cbuf, final int off, final int len) throws IOException {
+            checkOpen();
+            close();
+            return EOF;
+        }
+    }
+
+    @Test
+    void testReadCollection() throws IOException {
+        final Collection<Reader> readers = new ArrayList<>();
+        readers.add(new StringReader("F"));
+        readers.add(new StringReader("B"));
+        try (Reader reader = new SequenceReader(readers)) {
+            assertEquals('F', reader.read());
+            assertEquals('B', reader.read());
+            checkReadEof(reader);
+        }
+    }
+}
