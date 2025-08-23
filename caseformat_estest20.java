dd46e098e45720642c600e0acbb9e043eb7928c8
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:41:40 +0800

    Original processedAutoTestCases caseformat_estest20.java

---
+++ caseformat_estest20.java
@@ -0,0 +1,18 @@
+package com.google.common.base;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class CaseFormat_ESTestTest20 extends CaseFormat_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test19() throws Throwable {
+        CaseFormat caseFormat0 = CaseFormat.LOWER_UNDERSCORE;
+        String string0 = caseFormat0.convert(caseFormat0, "83pvzR?h!");
+        assertEquals("83pvzr?h!", string0);
+    }
+}
