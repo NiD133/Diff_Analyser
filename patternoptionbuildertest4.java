96d27294de955beab29d0aab6e659a38d0e0d063
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:32:10 +0800

    Original processedManualTestCases patternoptionbuildertest4.java

---
+++ patternoptionbuildertest4.java
@@ -0,0 +1,29 @@
+package org.apache.commons.cli;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Vector;
+import org.junit.jupiter.api.Test;
+
+public class PatternOptionBuilderTestTest4 {
+
+    @Test
+    void testExistingFilePatternFileNotExist() throws Exception {
+        final Options options = PatternOptionBuilder.parsePattern("f<");
+        final CommandLineParser parser = new PosixParser();
+        final CommandLine line = parser.parse(options, new String[] { "-f", "non-existing.file" });
+        assertNull(line.getOptionObject("f"), "option f parsed");
+    }
+}
