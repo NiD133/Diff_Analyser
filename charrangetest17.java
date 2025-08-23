16c2e86925c1fadf8caaf3df6be7e51f6a8025e0
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:26:31 +0800

    Original processedManualTestCases charrangetest17.java

---
+++ charrangetest17.java
@@ -0,0 +1,26 @@
+package org.apache.commons.lang3;
+
+import static org.apache.commons.lang3.LangAssertions.assertNullPointerException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.lang.reflect.Modifier;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import org.junit.jupiter.api.Test;
+
+public class CharRangeTestTest17 extends AbstractLangTest {
+
+    @Test
+    void testSerialization() {
+        CharRange range = CharRange.is('a');
+        assertEquals(range, SerializationUtils.clone(range));
+        range = CharRange.isIn('a', 'e');
+        assertEquals(range, SerializationUtils.clone(range));
+        range = CharRange.isNotIn('a', 'e');
+        assertEquals(range, SerializationUtils.clone(range));
+    }
+}
