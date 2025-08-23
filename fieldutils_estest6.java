86b71a0c949654f41672679cee63a44a4b49ff15
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:50:30 +0800

    gemini processedAutoTestCases fieldutils_estest6.java

--- fieldutils_estest6.java
+++ fieldutils_estest6.java
@@ -1,24 +1,29 @@
 package org.joda.time.field;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.math.RoundingMode;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.joda.time.DateTimeField;
 import org.joda.time.DateTimeFieldType;
-import org.joda.time.DateTimeZone;
-import org.joda.time.chrono.IslamicChronology;
-import org.joda.time.chrono.ZonedChronology;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class FieldUtils_ESTestTest6 extends FieldUtils_ESTest_scaffolding {
+/**
+ * Unit tests for {@link FieldUtils}.
+ * This class focuses on the verifyValueBounds method.
+ */
+public class FieldUtilsTest {
 
-    @Test(timeout = 4000)
-    public void test05() throws Throwable {
-        DateTimeFieldType dateTimeFieldType0 = DateTimeFieldType.minuteOfHour();
-        FieldUtils.verifyValueBounds(dateTimeFieldType0, (-5550), (-5550), (-5550));
-        assertEquals("minuteOfHour", dateTimeFieldType0.toString());
+    /**
+     * Tests that verifyValueBounds does not throw an exception when the value
+     * is equal to both the lower and upper bounds. This is a valid boundary condition.
+     */
+    @Test
+    public void verifyValueBounds_shouldNotThrowException_whenValueIsOnBoundary() {
+        // Arrange: Define the field type and a value that is on the boundary.
+        DateTimeFieldType fieldType = DateTimeFieldType.minuteOfHour();
+        int boundaryValue = -5550;
+        int lowerBound = -5550;
+        int upperBound = -5550;
+
+        // Act & Assert: The method should execute without throwing an exception.
+        // The test will pass if no IllegalFieldValueException is thrown, which is the
+        // expected behavior since the value is within the inclusive bounds.
+        FieldUtils.verifyValueBounds(fieldType, boundaryValue, lowerBound, upperBound);
     }
 }
