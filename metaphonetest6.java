95279cc6d6d4c1e4f3fa16e6bbf755be689cff92
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:31:05 +0800

    gemini processedManualTestCases metaphonetest6.java

--- metaphonetest6.java
+++ metaphonetest6.java
@@ -1,58 +1,50 @@
 package org.apache.commons.codec.language;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.stream.Stream;
 import org.apache.commons.codec.AbstractStringEncoderTest;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-public class MetaphoneTestTest6 extends AbstractStringEncoderTest<Metaphone> {
-
-    public void assertIsMetaphoneEqual(final String source, final String[] matches) {
-        // match source to all matches
-        for (final String matche : matches) {
-            assertTrue(getStringEncoder().isMetaphoneEqual(source, matche), "Source: " + source + ", should have same Metaphone as: " + matche);
-        }
-        // match to each other
-        for (final String matche : matches) {
-            for (final String matche2 : matches) {
-                assertTrue(getStringEncoder().isMetaphoneEqual(matche, matche2));
-            }
-        }
-    }
-
-    public void assertMetaphoneEqual(final String[][] pairs) {
-        validateFixture(pairs);
-        for (final String[] pair : pairs) {
-            final String name0 = pair[0];
-            final String name1 = pair[1];
-            final String failMsg = "Expected match between " + name0 + " and " + name1;
-            assertTrue(getStringEncoder().isMetaphoneEqual(name0, name1), failMsg);
-            assertTrue(getStringEncoder().isMetaphoneEqual(name1, name0), failMsg);
-        }
-    }
+/**
+ * Tests the isMetaphoneEqual method of the Metaphone class.
+ */
+public class MetaphoneIsMetaphoneEqualTest extends AbstractStringEncoderTest<Metaphone> {
 
     @Override
     protected Metaphone createStringEncoder() {
         return new Metaphone();
     }
 
-    public void validateFixture(final String[][] pairs) {
-        if (pairs.length == 0) {
-            fail("Test fixture is empty");
-        }
-        for (int i = 0; i < pairs.length; i++) {
-            if (pairs[i].length != 2) {
-                fail("Error in test fixture in the data array at index " + i);
-            }
-        }
+    /**
+     * Provides pairs of strings that are expected to have the same Metaphone encoding.
+     *
+     * @return A stream of arguments, where each argument is a pair of strings.
+     */
+    private static Stream<Arguments> metaphoneEqualPairs() {
+        // Matches computed from http://www.lanw.com/java/phonetic/default.htm
+        return Stream.of(
+            Arguments.of("Lawrence", "Lorenza"),
+            Arguments.of("Gary", "Cahra")
+        );
     }
 
     /**
-     * Matches computed from http://www.lanw.com/java/phonetic/default.htm
+     * Tests that isMetaphoneEqual returns true for words that should be phonetically equivalent.
+     * The isMetaphoneEqual method is expected to be symmetric, so checking both
+     * isMetaphoneEqual(str1, str2) and isMetaphoneEqual(str2, str1) is redundant.
+     *
+     * @param string1 the first string to compare
+     * @param string2 the second string to compare
      */
-    @Test
-    void testIsMetaphoneEqual2() {
-        assertMetaphoneEqual(new String[][] { { "Lawrence", "Lorenza" }, { "Gary", "Cahra" } });
+    @ParameterizedTest
+    @MethodSource("metaphoneEqualPairs")
+    void shouldReturnTrueForPhoneticallyEquivalentWords(final String string1, final String string2) {
+        assertTrue(
+            getStringEncoder().isMetaphoneEqual(string1, string2),
+            () -> String.format("Expected '%s' and '%s' to have the same Metaphone code", string1, string2)
+        );
     }
 }
