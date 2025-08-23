bee5200f8adccf70615a3b2a702c3028ad002aee
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:44:56 +0800

    Original processedAutoTestCases comparatorchain_estest15.java

---
+++ comparatorchain_estest15.java
@@ -0,0 +1,46 @@
+package org.apache.commons.collections4.comparators;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.nio.ByteBuffer;
+import java.nio.LongBuffer;
+import java.util.BitSet;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.ToIntFunction;
+import java.util.function.ToLongFunction;
+import org.apache.commons.collections4.Closure;
+import org.apache.commons.collections4.functors.ClosureTransformer;
+import org.apache.commons.collections4.functors.ComparatorPredicate;
+import org.apache.commons.collections4.functors.ExceptionClosure;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class ComparatorChain_ESTestTest15 extends ComparatorChain_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test14() throws Throwable {
+        LinkedList<Comparator<Object>> linkedList0 = new LinkedList<Comparator<Object>>();
+        ComparatorChain<Object> comparatorChain0 = new ComparatorChain<Object>(linkedList0);
+        Closure<Object> closure0 = ExceptionClosure.exceptionClosure();
+        ClosureTransformer<Object> closureTransformer0 = new ClosureTransformer<Object>(closure0);
+        Comparator<ComparatorChain<Object>> comparator0 = Comparator.comparing((Function<? super ComparatorChain<Object>, ?>) closureTransformer0, (Comparator<? super Object>) comparatorChain0);
+        ComparatorChain<ComparatorChain<Object>> comparatorChain1 = new ComparatorChain<ComparatorChain<Object>>(comparator0);
+        // Undeclared exception!
+        try {
+            comparatorChain1.compare(comparatorChain0, comparatorChain0);
+            fail("Expecting exception: RuntimeException");
+        } catch (RuntimeException e) {
+            //
+            // ExceptionClosure invoked
+            //
+            verifyException("org.apache.commons.collections4.functors.ExceptionClosure", e);
+        }
+    }
+}
