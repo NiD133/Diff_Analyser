1ba1f253004236e1d4abe6f40fc7f5543d8ac545
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:19:07 +0800

    gemini processedAutoTestCases stringutil_estest71.java

--- stringutil_estest71.java
+++ stringutil_estest71.java
@@ -1,28 +1,17 @@
 package org.jsoup.internal;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Collection;
-import java.util.ConcurrentModificationException;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.ListIterator;
-import java.util.stream.Collector;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.evosuite.runtime.mock.java.net.MockURL;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertTrue;
 
-public class StringUtil_ESTestTest71 extends StringUtil_ESTest_scaffolding {
+/**
+ * Test suite for {@link StringUtil}.
+ */
+public class StringUtilTest {
 
-    @Test(timeout = 4000)
-    public void test70() throws Throwable {
-        boolean boolean0 = StringUtil.isActuallyWhitespace(10);
-        assertTrue(boolean0);
+    @Test
+    public void isActuallyWhitespaceReturnsTrueForLineFeed() {
+        // The integer 10 is the ASCII code point for the Line Feed character ('\n').
+        // This test verifies that the method correctly identifies it as whitespace.
+        assertTrue("Line Feed character should be considered whitespace", StringUtil.isActuallyWhitespace('\n'));
     }
 }
