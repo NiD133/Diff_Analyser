9c1c9e109ee503c458df57e49296f6275f88c4e9
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:11:06 +0800

    Original processedAutoTestCases randomaccessfilemode_estest36.java

---
+++ randomaccessfilemode_estest36.java
@@ -0,0 +1,37 @@
+package org.apache.commons.io;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.file.OpenOption;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import org.apache.commons.io.function.IOConsumer;
+import org.apache.commons.io.function.IOFunction;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.evosuite.runtime.testdata.EvoSuiteFile;
+import org.evosuite.runtime.testdata.FileSystemHandling;
+import org.junit.runner.RunWith;
+
+public class RandomAccessFileMode_ESTestTest36 extends RandomAccessFileMode_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test35() throws Throwable {
+        RandomAccessFileMode randomAccessFileMode0 = RandomAccessFileMode.valueOfMode("r");
+        try {
+            randomAccessFileMode0.io("r");
+            fail("Expecting exception: FileNotFoundException");
+        } catch (FileNotFoundException e) {
+            //
+            // File does not exist, and RandomAccessFile is not open in write mode
+            //
+            verifyException("org.evosuite.runtime.mock.java.io.MockRandomAccessFile", e);
+        }
+    }
+}
