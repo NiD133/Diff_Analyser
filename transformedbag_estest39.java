b382c7d0656f0c6fefc2e0f785e13d670777daa9
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:23:06 +0800

    gemini processedAutoTestCases transformedbag_estest39.java

--- transformedbag_estest39.java
+++ transformedbag_estest39.java
@@ -1,63 +1,42 @@
 package org.apache.commons.collections4.bag;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Set;
-import java.util.Vector;
-import org.apache.commons.collections4.Bag;
-import org.apache.commons.collections4.Factory;
-import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.SortedBag;
 import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AndPredicate;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ChainedTransformer;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantFactory;
 import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.FactoryTransformer;
-import org.apache.commons.collections4.functors.FalsePredicate;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.InstantiateFactory;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.MapTransformer;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NotPredicate;
-import org.apache.commons.collections4.functors.OnePredicate;
-import org.apache.commons.collections4.functors.SwitchTransformer;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class TransformedBag_ESTestTest39 extends TransformedBag_ESTest_scaffolding {
+import static org.junit.Assert.fail;
 
-    @Test(timeout = 4000)
-    public void test38() throws Throwable {
-        Transformer<Object, Integer> transformer0 = ConstantTransformer.nullTransformer();
-        TreeBag<Object> treeBag0 = new TreeBag<Object>((Comparator<? super Object>) null);
-        TransformedSortedBag<Object> transformedSortedBag0 = new TransformedSortedBag<Object>(treeBag0, transformer0);
-        // Undeclared exception!
+/**
+ * Contains tests for the TransformedBag class, focusing on exception handling.
+ */
+public class TransformedBagTest {
+
+    /**
+     * Tests that calling remove() with a null object on a TransformedBag
+     * backed by a TreeBag throws a NullPointerException.
+     * <p>
+     * The TransformedBag should delegate the remove operation directly to the
+     * backing bag. A TreeBag, which uses natural ordering by default, does not
+     * support null elements and will throw a NullPointerException when an
+     * operation like remove() is attempted with null.
+     */
+    @Test
+    public void testRemoveWithNullOnUnderlyingTreeBagThrowsNPE() {
+        // Arrange
+        // The transformer is not used by the remove() method, so any transformer will do.
+        final Transformer<Object, Object> transformer = ConstantTransformer.nullTransformer();
+        
+        // A standard TreeBag does not allow null elements.
+        final TreeBag<Object> backingBag = new TreeBag<>();
+        final TransformedBag<Object> transformedBag = TransformedBag.transformingBag(backingBag, transformer);
+
+        // Act & Assert
         try {
-            transformedSortedBag0.remove((Object) null, (-2770));
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("java.util.TreeMap", e);
+            // The number of copies to remove is irrelevant; the null object causes the exception.
+            transformedBag.remove(null, 1);
+            fail("Expected a NullPointerException because the underlying TreeBag cannot handle null objects.");
+        } catch (final NullPointerException e) {
+            // This is the expected behavior. The test passes.
         }
     }
 }
