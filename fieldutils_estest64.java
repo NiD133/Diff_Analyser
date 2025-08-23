e3a04262accbd6d5e00494e8a9645b0144221a7d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:50:32 +0800

    Original processedAutoTestCases fieldutils_estest64.java

---
+++ fieldutils_estest64.java
@@ -0,0 +1,31 @@
+package org.joda.time.field;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.math.RoundingMode;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.joda.time.DateTimeField;
+import org.joda.time.DateTimeFieldType;
+import org.joda.time.DateTimeZone;
+import org.joda.time.chrono.IslamicChronology;
+import org.joda.time.chrono.ZonedChronology;
+import org.junit.runner.RunWith;
+
+public class FieldUtils_ESTestTest64 extends FieldUtils_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test63() throws Throwable {
+        // Undeclared exception!
+        try {
+            FieldUtils.safeMultiply((-9223372036854775808L), (-1L));
+            fail("Expecting exception: ArithmeticException");
+        } catch (ArithmeticException e) {
+            //
+            // Multiplication overflows a long: -9223372036854775808 * -1
+            //
+            verifyException("org.joda.time.field.FieldUtils", e);
+        }
+    }
+}
