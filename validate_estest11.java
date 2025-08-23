10b58574a4cf9dfdd3468441878069641d50394a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:25:51 +0800

    Original processedAutoTestCases validate_estest11.java

---
+++ validate_estest11.java
@@ -0,0 +1,33 @@
+package org.jsoup.helper;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.FormatFlagsConversionMismatchException;
+import java.util.IllegalFormatConversionException;
+import java.util.IllegalFormatFlagsException;
+import java.util.IllegalFormatWidthException;
+import java.util.MissingFormatArgumentException;
+import java.util.MissingFormatWidthException;
+import java.util.UnknownFormatConversionException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class Validate_ESTestTest11 extends Validate_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test10() throws Throwable {
+        Object[] objectArray0 = new Object[0];
+        // Undeclared exception!
+        try {
+            Validate.ensureNotNull((Object) null, "P>)7X)a:%'uj0", objectArray0);
+            fail("Expecting exception: UnknownFormatConversionException");
+        } catch (UnknownFormatConversionException e) {
+            //
+            // Conversion = '''
+            //
+            verifyException("java.util.Formatter", e);
+        }
+    }
+}
