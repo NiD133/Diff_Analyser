c86e6309d50b0caa351ef0a1361d7a8589bde513
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:53:56 +0800

    gemini processedAutoTestCases indexedcollection_estest23.java

--- indexedcollection_estest23.java
+++ indexedcollection_estest23.java
@@ -1,85 +1,83 @@
 package org.apache.commons.collections4.collection;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.ConcurrentModificationException;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Set;
-import org.apache.commons.collections4.Closure;
 import org.apache.commons.collections4.Predicate;
 import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AllPredicate;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ChainedTransformer;
-import org.apache.commons.collections4.functors.CloneTransformer;
-import org.apache.commons.collections4.functors.ClosureTransformer;
-import org.apache.commons.collections4.functors.ConstantFactory;
 import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.DefaultEquator;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.FactoryTransformer;
-import org.apache.commons.collections4.functors.FalsePredicate;
-import org.apache.commons.collections4.functors.ForClosure;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.NOPClosure;
-import org.apache.commons.collections4.functors.NOPTransformer;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NotNullPredicate;
-import org.apache.commons.collections4.functors.NullIsFalsePredicate;
-import org.apache.commons.collections4.functors.NullPredicate;
 import org.apache.commons.collections4.functors.SwitchTransformer;
-import org.apache.commons.collections4.functors.TransformedPredicate;
-import org.apache.commons.collections4.functors.TransformerClosure;
-import org.apache.commons.collections4.functors.TransformerPredicate;
 import org.apache.commons.collections4.functors.TruePredicate;
 import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class IndexedCollection_ESTestTest23 extends IndexedCollection_ESTest_scaffolding {
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
 
-    @Test(timeout = 4000)
-    public void test22() throws Throwable {
-        LinkedList<Object> linkedList0 = new LinkedList<Object>();
-        Predicate<Object>[] predicateArray0 = (Predicate<Object>[]) Array.newInstance(Predicate.class, 7);
-        Predicate<Object> predicate0 = FalsePredicate.falsePredicate();
-        predicateArray0[0] = predicate0;
-        Integer integer0 = new Integer((-521));
-        ConstantFactory<Integer> constantFactory0 = new ConstantFactory<Integer>(integer0);
-        FactoryTransformer<Object, Integer> factoryTransformer0 = new FactoryTransformer<Object, Integer>(constantFactory0);
-        TransformedPredicate<Object> transformedPredicate0 = new TransformedPredicate<Object>(factoryTransformer0, predicate0);
-        predicateArray0[1] = (Predicate<Object>) transformedPredicate0;
-        UniquePredicate<Object> uniquePredicate0 = new UniquePredicate<Object>();
-        predicateArray0[2] = (Predicate<Object>) uniquePredicate0;
-        predicateArray0[3] = predicate0;
-        DefaultEquator<Object> defaultEquator0 = DefaultEquator.defaultEquator();
-        EqualPredicate<Object> equalPredicate0 = new EqualPredicate<Object>(integer0, defaultEquator0);
-        predicateArray0[4] = (Predicate<Object>) equalPredicate0;
-        predicateArray0[5] = (Predicate<Object>) transformedPredicate0;
-        Predicate<Object> predicate1 = TransformedPredicate.transformedPredicate((Transformer<? super Object, ?>) factoryTransformer0, (Predicate<? super Object>) uniquePredicate0);
-        predicateArray0[6] = predicate1;
-        Transformer<Object, Integer>[] transformerArray0 = (Transformer<Object, Integer>[]) Array.newInstance(Transformer.class, 5);
-        transformerArray0[2] = (Transformer<Object, Integer>) factoryTransformer0;
-        SwitchTransformer<Object, Object> switchTransformer0 = new SwitchTransformer<Object, Object>(predicateArray0, transformerArray0, (Transformer<? super Object, ?>) null);
-        IndexedCollection<Object, Object> indexedCollection0 = IndexedCollection.nonUniqueIndexedCollection((Collection<Object>) linkedList0, (Transformer<Object, Object>) switchTransformer0);
-        indexedCollection0.add(defaultEquator0.HASHCODE_NULL);
-        // Undeclared exception!
+import static org.junit.Assert.fail;
+
+/**
+ * This class contains a test for the IndexedCollection class.
+ * The original test was auto-generated and has been rewritten for clarity.
+ */
+public class IndexedCollectionRefactoredTest {
+
+    /**
+     * Tests that {@link IndexedCollection#removeAll(Collection)} throws an exception
+     * if the provided key transformer is stateful and behaves inconsistently across calls.
+     * <p>
+     * This test configures a {@link SwitchTransformer} that successfully generates a key
+     * when an element is first added. However, when {@code removeAll} is called for the
+     * same element, the stateful nature of the predicates causes the transformer to select
+     * an index that is out of bounds for its internal transformers array, leading to an
+     * {@link ArrayIndexOutOfBoundsException}. This simulates a scenario where a key
+     * transformer is not a pure function, which can break the internal state of the
+     * IndexedCollection.
+     */
+    @Test
+    public void removeAllShouldThrowExceptionWhenKeyTransformerIsUnstable() {
+        // Arrange: Create a stateful key transformer that behaves differently on subsequent calls.
+
+        // A UniquePredicate is stateful: it returns true the first time it sees an object,
+        // and false for all subsequent calls with the same object.
+        Predicate<String> firstCallPredicate = new UniquePredicate<>();
+        Predicate<String> subsequentCallPredicate = TruePredicate.truePredicate();
+
+        Predicate<? super String>[] predicates = new Predicate[]{
+            firstCallPredicate,      // This will be true on the first call (during add).
+            subsequentCallPredicate  // This will be true on the second call (during removeAll).
+        };
+
+        // A transformer to be used on the first successful call.
+        Transformer<String, Integer> validTransformer = ConstantTransformer.constantTransformer(100);
+
+        // The array of transformers is intentionally made too short. It has one element,
+        // corresponding to the firstCallPredicate. There is no transformer at index 1
+        // for the subsequentCallPredicate.
+        Transformer<? super String, ?>[] transformers = new Transformer[]{validTransformer};
+
+        // The SwitchTransformer will use the first predicate that returns true to select a transformer.
+        // On add("element"): firstCallPredicate is true -> uses transformers[0].
+        // On removeAll("element"): firstCallPredicate is false, subsequentCallPredicate is true -> tries to use transformers[1].
+        Transformer<String, Integer> unstableKeyTransformer = new SwitchTransformer<>(predicates, transformers, null);
+
+        Collection<String> backingCollection = new LinkedList<>();
+        IndexedCollection<Integer, String> indexedCollection =
+                IndexedCollection.nonUniqueIndexedCollection(backingCollection, unstableKeyTransformer);
+
+        // Add an element. This works fine as it triggers the first predicate, which has a
+        // corresponding transformer at index 0.
+        final String element = "test_element";
+        indexedCollection.add(element);
+
+        final Collection<String> elementsToRemove = Collections.singletonList(element);
+
+        // Act & Assert: Expect an exception when removeAll is called.
         try {
-            indexedCollection0.removeAll(linkedList0);
-            fail("Expecting exception: ArrayIndexOutOfBoundsException");
-        } catch (ArrayIndexOutOfBoundsException e) {
-            //
-            // 6
-            //
-            verifyException("org.apache.commons.collections4.functors.SwitchTransformer", e);
+            indexedCollection.removeAll(elementsToRemove);
+            fail("Expected an ArrayIndexOutOfBoundsException due to the unstable key transformer.");
+        } catch (final ArrayIndexOutOfBoundsException e) {
+            // This is the expected outcome. The SwitchTransformer attempted to access an
+            // out-of-bounds index because the state of its predicates changed between the
+            // add and removeAll calls.
         }
     }
 }
