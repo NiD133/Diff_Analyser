e0e85be8acc360750b49fa88871532663be8963a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:24:31 +0800

    gemini processedAutoTestCases uncheck_estest25.java

--- uncheck_estest25.java
+++ uncheck_estest25.java
@@ -1,45 +1,26 @@
 package org.apache.commons.io.function;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.time.chrono.HijrahEra;
-import java.util.Comparator;
-import java.util.concurrent.ForkJoinTask;
-import java.util.function.BiConsumer;
-import java.util.function.BinaryOperator;
-import java.util.function.Function;
 import java.util.function.Supplier;
-import java.util.stream.Collector;
-import java.util.stream.LongStream;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
 
-public class Uncheck_ESTestTest25 extends Uncheck_ESTest_scaffolding {
+/**
+ * Tests for the {@link Uncheck} utility class.
+ */
+public class UncheckTest {
 
-    @Test(timeout = 4000)
-    public void test24() throws Throwable {
-        Supplier<String> supplier0 = (Supplier<String>) mock(Supplier.class, new ViolatedAssumptionAnswer());
-        IOBiConsumer<String, String> iOBiConsumer0 = IOBiConsumer.noop();
-        BiConsumer<String, String> biConsumer0 = iOBiConsumer0.asBiConsumer();
-        Comparator<String> comparator0 = (Comparator<String>) mock(Comparator.class, new ViolatedAssumptionAnswer());
-        BinaryOperator<String> binaryOperator0 = BinaryOperator.minBy((Comparator<? super String>) comparator0);
-        Collector.Characteristics[] collector_CharacteristicsArray0 = (Collector.Characteristics[]) Array.newInstance(Collector.Characteristics.class, 0);
-        Collector<String, String, String> collector0 = Collector.of(supplier0, biConsumer0, binaryOperator0, (Collector.Characteristics[]) collector_CharacteristicsArray0);
-        Supplier<String> supplier1 = collector0.supplier();
-        // Undeclared exception!
-        try {
-            Uncheck.run((IORunnable) null, supplier1);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.commons.io.function.Uncheck", e);
-        }
+    /**
+     * Tests that calling Uncheck.run() with a null IORunnable argument
+     * throws a NullPointerException.
+     */
+    @Test(expected = NullPointerException.class)
+    public void runWithNullRunnableShouldThrowNullPointerException() {
+        // Arrange: Create a dummy message supplier. This supplier is for wrapping
+        // IOExceptions and will not be used in this test case, as the
+        // NullPointerException is thrown before the runnable is executed.
+        final Supplier<String> dummyMessageSupplier = () -> "This message should not be used";
+
+        // Act & Assert: Call the method under test with a null runnable.
+        // The @Test(expected) annotation asserts that a NullPointerException is thrown.
+        Uncheck.run(null, dummyMessageSupplier);
     }
 }
