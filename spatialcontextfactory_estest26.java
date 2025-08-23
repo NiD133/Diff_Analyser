3de6481316ca7f75ffb944156e412183e2efb0a6
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:17:21 +0800

    Original processedAutoTestCases spatialcontextfactory_estest26.java

---
+++ spatialcontextfactory_estest26.java
@@ -0,0 +1,28 @@
+package org.locationtech.spatial4j.context;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.HashMap;
+import java.util.Map;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+import org.locationtech.spatial4j.io.PolyshapeReader;
+import org.locationtech.spatial4j.shape.ShapeFactory;
+
+public class SpatialContextFactory_ESTestTest26 extends SpatialContextFactory_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test25() throws Throwable {
+        SpatialContextFactory spatialContextFactory0 = new SpatialContextFactory();
+        HashMap<String, String> hashMap0 = new HashMap<String, String>();
+        ClassLoader classLoader0 = ClassLoader.getSystemClassLoader();
+        spatialContextFactory0.init(hashMap0, classLoader0);
+        Class<PolyshapeReader> class0 = PolyshapeReader.class;
+        spatialContextFactory0.addReaderIfNoggitExists(class0);
+        assertTrue(spatialContextFactory0.geo);
+        assertFalse(spatialContextFactory0.hasFormatConfig);
+        assertFalse(spatialContextFactory0.normWrapLongitude);
+    }
+}
