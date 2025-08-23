6e4a6ed5c2330cc64d70f1e7185251e339de564b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:07:31 +0800

    Original processedAutoTestCases patternoptionbuilder_estest18.java

---
+++ patternoptionbuilder_estest18.java
@@ -0,0 +1,18 @@
+package org.apache.commons.cli;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class PatternOptionBuilder_ESTestTest18 extends PatternOptionBuilder_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test17() throws Throwable {
+        Class<?> class0 = PatternOptionBuilder.getValueType('<');
+        assertNotNull(class0);
+        assertEquals("class java.io.FileInputStream", class0.toString());
+    }
+}
