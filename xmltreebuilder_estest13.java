efdc50a586a044bcd672a99827940b59d65dd9ad
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:27:55 +0800

    Original processedAutoTestCases xmltreebuilder_estest13.java

---
+++ xmltreebuilder_estest13.java
@@ -0,0 +1,38 @@
+package org.jsoup.parser;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.PipedReader;
+import java.io.PipedWriter;
+import java.io.Reader;
+import java.io.StringReader;
+import java.io.UncheckedIOException;
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.jsoup.nodes.Attributes;
+import org.jsoup.nodes.CDataNode;
+import org.jsoup.nodes.Comment;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.LeafNode;
+import org.jsoup.select.Elements;
+import org.junit.runner.RunWith;
+
+public class XmlTreeBuilder_ESTestTest13 extends XmlTreeBuilder_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test12() throws Throwable {
+        XmlTreeBuilder xmlTreeBuilder0 = new XmlTreeBuilder();
+        Parser parser0 = new Parser(xmlTreeBuilder0);
+        StreamParser streamParser0 = new StreamParser(parser0);
+        Element element0 = new Element("Doctype", "nfHa(P+.f");
+        streamParser0.parseFragment("$M{RJ]hM9&ek.Vtk$I", element0, "http://www.w3.org/1999/xhtml");
+        Token.Doctype token_Doctype0 = new Token.Doctype();
+        boolean boolean0 = xmlTreeBuilder0.process(token_Doctype0);
+        assertTrue(boolean0);
+        assertEquals("http://www.w3.org/XML/1998/namespace", xmlTreeBuilder0.defaultNamespace());
+    }
+}
