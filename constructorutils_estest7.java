5966b79f2ff043a5feba4460e2914d5c53ec7d52
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:45:40 +0800

    gemini processedAutoTestCases constructorutils_estest7.java

--- constructorutils_estest7.java
+++ constructorutils_estest7.java
@@ -1,31 +1,21 @@
 package org.apache.commons.lang3.reflect;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.annotation.Annotation;
-import java.lang.reflect.AccessibleObject;
-import java.lang.reflect.Array;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class ConstructorUtils_ESTestTest7 extends ConstructorUtils_ESTest_scaffolding {
+/**
+ * Unit tests for {@link org.apache.commons.lang3.reflect.ConstructorUtils}.
+ */
+public class ConstructorUtilsTest {
 
-    @Test(timeout = 4000)
-    public void test06() throws Throwable {
-        Class<Integer>[] classArray0 = (Class<Integer>[]) Array.newInstance(Class.class, 5);
-        // Undeclared exception!
-        try {
-            ConstructorUtils.invokeConstructor((Class<Integer>) null, (Object[]) classArray0, (Class<?>[]) classArray0);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // cls
-            //
-            verifyException("java.util.Objects", e);
-        }
+    /**
+     * Tests that {@link ConstructorUtils#invokeConstructor(Class, Object[], Class[])}
+     * throws a NullPointerException if the target class is null.
+     */
+    @Test(expected = NullPointerException.class)
+    public void testInvokeConstructorWithNullClassThrowsNPE() {
+        // The method under test should throw a NullPointerException before it
+        // processes the arguments or parameter types. Therefore, we can pass
+        // empty arrays for them.
+        ConstructorUtils.invokeConstructor(null, new Object[0], new Class<?>[0]);
     }
 }
