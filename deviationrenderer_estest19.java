82a3bd30ddae21e2da5c2ddef2dd25a662662bd4
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:48:00 +0800

    gemini processedAutoTestCases deviationrenderer_estest19.java

--- deviationrenderer_estest19.java
+++ deviationrenderer_estest19.java
@@ -1,39 +1,32 @@
 package org.jfree.chart.renderer.xy;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.awt.Graphics2D;
-import java.awt.geom.Rectangle2D;
-import java.util.Date;
-import java.util.Locale;
-import java.util.SimpleTimeZone;
-import java.util.TimeZone;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.jfree.chart.ChartRenderingInfo;
-import org.jfree.chart.axis.CategoryAnchor;
-import org.jfree.chart.axis.DateAxis;
-import org.jfree.chart.plot.DatasetRenderingOrder;
-import org.jfree.chart.plot.PlotRenderingInfo;
-import org.jfree.chart.plot.XYPlot;
-import org.jfree.data.time.TimeSeriesCollection;
-import org.jfree.data.xy.DefaultHighLowDataset;
-import org.jfree.data.xy.DefaultIntervalXYDataset;
-import org.jfree.data.xy.MatrixSeries;
-import org.jfree.data.xy.MatrixSeriesCollection;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
-public class DeviationRenderer_ESTestTest19 extends DeviationRenderer_ESTest_scaffolding {
+/**
+ * This test class contains tests for the DeviationRenderer.
+ * This specific test focuses on the initial state of a newly created renderer.
+ */
+public class DeviationRenderer_ESTestTest19 {
 
-    @Test(timeout = 4000)
-    public void test18() throws Throwable {
-        DeviationRenderer deviationRenderer0 = new DeviationRenderer(true, true);
-        Short short0 = new Short((short) 2396);
-        MatrixSeries<Short> matrixSeries0 = new MatrixSeries<Short>(short0, 17, 28);
-        MatrixSeriesCollection<Short> matrixSeriesCollection0 = new MatrixSeriesCollection<Short>(matrixSeries0);
-        deviationRenderer0.findRangeBounds(matrixSeriesCollection0);
-        assertTrue(deviationRenderer0.getDrawSeriesLineAsPath());
-        assertEquals(0.5F, deviationRenderer0.getAlpha(), 0.01F);
+    /**
+     * Verifies that the DeviationRenderer constructor correctly initializes
+     * the renderer with its default properties.
+     */
+    @Test
+    public void newRendererShouldInitializeWithDefaultSettings() {
+        // Arrange & Act: Create a new DeviationRenderer instance.
+        // The constructor is the "action" under test.
+        DeviationRenderer renderer = new DeviationRenderer(true, true);
+
+        // Assert: Check that the renderer's properties are set to their expected default values.
+
+        // The default alpha transparency for the deviation shading should be 0.5f.
+        assertEquals("Default alpha value should be 0.5f", 0.5F, renderer.getAlpha(), 0.001F);
+
+        // The DeviationRenderer is designed to draw series lines as a path to create the
+        // shaded area, so this property should always be true.
+        assertTrue("drawSeriesLineAsPath should be true by default", renderer.getDrawSeriesLineAsPath());
     }
 }
