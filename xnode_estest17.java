1d2d09604d080cd6cd171be19982741326ad1926
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:28:45 +0800

    gemini processedAutoTestCases xnode_estest17.java

--- xnode_estest17.java
+++ xnode_estest17.java
@@ -1,31 +1,43 @@
 package org.apache.ibatis.parsing;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.List;
-import java.util.Locale;
-import java.util.Properties;
-import java.util.function.Supplier;
-import javax.imageio.metadata.IIOMetadataNode;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
-import org.w3c.dom.Document;
 import org.w3c.dom.Node;
-import org.xml.sax.ext.DefaultHandler2;
 
-public class XNode_ESTestTest17 extends XNode_ESTest_scaffolding {
+import javax.imageio.metadata.IIOMetadataNode;
+import java.util.Properties;
 
-    @Test(timeout = 4000)
-    public void test016() throws Throwable {
-        Properties properties0 = new Properties();
-        IIOMetadataNode iIOMetadataNode0 = new IIOMetadataNode();
-        XNode xNode0 = new XNode((XPathParser) null, iIOMetadataNode0, properties0);
-        Long long0 = new Long((-2217L));
-        Long long1 = xNode0.getLongAttribute("%CjKdy/@j1\"{KuK'", long0);
-        assertEquals((-2217L), (long) long1);
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Test suite for the XNode class, focusing on attribute parsing.
+ */
+public class XNodeAttributeTest {
+
+    /**
+     * Verifies that getLongAttribute() returns the provided default value
+     * when the requested attribute does not exist on the XML node.
+     */
+    @Test
+    public void getLongAttributeShouldReturnDefaultValueForMissingAttribute() {
+        // Arrange
+        // 1. Define a default value and the name of an attribute that we know does not exist.
+        final Long expectedDefaultValue = -2217L;
+        final String nonExistentAttributeName = "port";
+
+        // 2. Create an empty DOM Node that has no attributes.
+        // IIOMetadataNode is a standard, concrete implementation of org.w3c.dom.Node.
+        Node emptyXmlNode = new IIOMetadataNode();
+
+        // 3. Create the XNode instance to be tested. The XPathParser is not used by
+        // the getLongAttribute method, so it can be null for this specific test.
+        XNode xNode = new XNode(null, emptyXmlNode, new Properties());
+
+        // Act
+        // Attempt to retrieve the long value of the non-existent attribute.
+        Long actualValue = xNode.getLongAttribute(nonExistentAttributeName, expectedDefaultValue);
+
+        // Assert
+        // The method should fall back to the provided default value.
+        assertEquals(expectedDefaultValue, actualValue);
     }
 }
