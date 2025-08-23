ea773d7fa43a21a4479d28421ade0bdffc837dd7
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:45:29 +0800

    gemini processedAutoTestCases compositeset_estest84.java

--- compositeset_estest84.java
+++ compositeset_estest84.java
@@ -1,53 +1,25 @@
 package org.apache.commons.collections4.set;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.LinkedHashSet;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Set;
-import java.util.Spliterator;
-import java.util.function.Consumer;
-import java.util.stream.Stream;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.Equator;
-import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ChainedClosure;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.DefaultEquator;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionPredicate;
-import org.apache.commons.collections4.functors.FalsePredicate;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.IfClosure;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NotNullPredicate;
-import org.apache.commons.collections4.functors.NotPredicate;
-import org.apache.commons.collections4.functors.NullIsExceptionPredicate;
-import org.apache.commons.collections4.functors.NullIsTruePredicate;
-import org.apache.commons.collections4.functors.OnePredicate;
-import org.apache.commons.collections4.functors.OrPredicate;
-import org.apache.commons.collections4.functors.TransformerClosure;
-import org.apache.commons.collections4.functors.TruePredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.apache.commons.collections4.functors.WhileClosure;
-import org.apache.commons.collections4.iterators.IteratorChain;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
 
-public class CompositeSet_ESTestTest84 extends CompositeSet_ESTest_scaffolding {
+/**
+ * Tests the constructor behavior of the {@link CompositeSet} class.
+ *
+ * Note: This class retains the original name for context, but a more
+ * descriptive name like {@code CompositeSetConstructorTest} would be preferable.
+ */
+public class CompositeSet_ESTestTest84 {
 
-    @Test(timeout = 4000)
-    public void test83() throws Throwable {
-        CompositeSet<Object> compositeSet0 = new CompositeSet<Object>((Set<Object>[]) null);
+    /**
+     * Tests that the varargs constructor {@code CompositeSet(Set...)} throws
+     * a NullPointerException when passed a null array.
+     */
+    @Test(expected = NullPointerException.class)
+    public void constructorWithNullSetArrayShouldThrowNullPointerException() {
+        // The constructor should not accept a null array for the composited sets.
+        // The cast to (Set<Object>[]) is necessary to avoid ambiguity between
+        // the varargs constructor and the single-set constructor.
+        new CompositeSet<Object>((Set<Object>[]) null);
     }
 }
