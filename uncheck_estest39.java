d0f25f8b38d045de49553e8530e6960f5e7d2b35
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:24:37 +0800

    Original processedAutoTestCases uncheck_estest39.java

---
+++ uncheck_estest39.java
@@ -0,0 +1,37 @@
+package org.apache.commons.io.function;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.lang.reflect.Array;
+import java.time.chrono.HijrahEra;
+import java.util.Comparator;
+import java.util.concurrent.ForkJoinTask;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+import java.util.stream.LongStream;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class Uncheck_ESTestTest39 extends Uncheck_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test38() throws Throwable {
+        // Undeclared exception!
+        try {
+            Uncheck.getAsInt((IOIntSupplier) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.apache.commons.io.function.Uncheck", e);
+        }
+    }
+}
