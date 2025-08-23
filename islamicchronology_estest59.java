1bb9cc56973242385ef360bb488f9c5ab7ad1e74
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:55:23 +0800

    gemini processedAutoTestCases islamicchronology_estest59.java

--- islamicchronology_estest59.java
+++ islamicchronology_estest59.java
@@ -1,22 +1,35 @@
 package org.joda.time.chrono;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.TimeZone;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
 import org.joda.time.Chronology;
 import org.joda.time.DateTimeZone;
-import org.joda.time.tz.UTCProvider;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class IslamicChronology_ESTestTest59 extends IslamicChronology_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
 
-    @Test(timeout = 4000)
-    public void test58() throws Throwable {
-        IslamicChronology islamicChronology0 = IslamicChronology.getInstanceUTC();
-        IslamicChronology islamicChronology1 = (IslamicChronology) islamicChronology0.withZone((DateTimeZone) null);
-        assertEquals(1, IslamicChronology.AH);
+/**
+ * Test suite for the IslamicChronology class.
+ */
+public class IslamicChronologyTest {
+
+    /**
+     * Tests that calling withZone() with a null argument returns a new
+     * Chronology instance configured with the default system time zone.
+     */
+    @Test
+    public void withZone_givenNull_shouldReturnChronologyInDefaultZone() {
+        // Arrange: Get a chronology in a specific zone (UTC) and the system's default zone.
+        // The Javadoc for withZone() states that a null input should result in using the default zone.
+        Chronology initialChronology = IslamicChronology.getInstanceUTC();
+        DateTimeZone defaultZone = DateTimeZone.getDefault();
+
+        // Act: Request a new chronology with a null time zone.
+        Chronology newChronology = initialChronology.withZone(null);
+
+        // Assert: The new chronology should have the default time zone and be a different instance.
+        assertEquals("The new chronology should have the default time zone",
+                defaultZone, newChronology.getZone());
+        assertNotSame("A new chronology instance should be returned",
+                initialChronology, newChronology);
     }
 }
