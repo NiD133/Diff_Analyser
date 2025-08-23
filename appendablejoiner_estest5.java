da443fc0bb76d0668992a062ed45d3e21b828af0
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:37:14 +0800

    Original processedAutoTestCases appendablejoiner_estest5.java

---
+++ appendablejoiner_estest5.java
@@ -0,0 +1,41 @@
+package org.apache.commons.lang3;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.IOException;
+import java.io.PipedWriter;
+import java.nio.BufferOverflowException;
+import java.nio.CharBuffer;
+import java.nio.ReadOnlyBufferException;
+import java.nio.charset.Charset;
+import java.sql.SQLNonTransientConnectionException;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.LinkedHashSet;
+import java.util.Locale;
+import org.apache.commons.lang3.function.FailableBiConsumer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class AppendableJoiner_ESTestTest5 extends AppendableJoiner_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test04() throws Throwable {
+        StringBuilder stringBuilder0 = new StringBuilder();
+        FailableBiConsumer<Appendable, StringBuilder, IOException> failableBiConsumer0 = FailableBiConsumer.nop();
+        StringBuilder[] stringBuilderArray0 = new StringBuilder[0];
+        // Undeclared exception!
+        try {
+            AppendableJoiner.joinSB((StringBuilder) null, (CharSequence) stringBuilder0, (CharSequence) null, (CharSequence) stringBuilder0, failableBiConsumer0, stringBuilderArray0);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.apache.commons.lang3.AppendableJoiner", e);
+        }
+    }
+}
