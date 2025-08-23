ceb5a53e218bf00a48c0bac9fb125ce074388e4b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:13:10 +0800

    gemini processedAutoTestCases seconds_estest61.java

--- seconds_estest61.java
+++ seconds_estest61.java
@@ -1,18 +1,31 @@
 package org.joda.time;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class Seconds_ESTestTest61 extends Seconds_ESTest_scaffolding {
+/**
+ * Test suite for the {@link Seconds} class.
+ */
+public class SecondsTest {
 
-    @Test(timeout = 4000)
-    public void test60() throws Throwable {
-        Seconds seconds0 = Seconds.MIN_VALUE;
-        Hours hours0 = seconds0.toStandardHours();
-        assertEquals((-596523), hours0.getHours());
+    /**
+     * Tests that converting the minimum possible number of seconds to hours
+     * results in the correct, truncated value.
+     */
+    @Test
+    public void toStandardHours_fromMinValue_calculatesCorrectly() {
+        // Arrange
+        final int SECONDS_PER_HOUR = 3600; // 60 seconds/minute * 60 minutes/hour
+        Seconds minSeconds = Seconds.MIN_VALUE;
+
+        // The conversion uses integer division, so any fractional part is truncated.
+        int expectedHours = Integer.MIN_VALUE / SECONDS_PER_HOUR;
+
+        // Act
+        Hours actualHours = minSeconds.toStandardHours();
+
+        // Assert
+        assertEquals("Conversion from minimum seconds to hours should be correct",
+                expectedHours, actualHours.getHours());
     }
 }
