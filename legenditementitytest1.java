764f27854f31198e27ef878e1a2c66fcf3f58852
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:30:34 +0800

    gemini processedManualTestCases legenditementitytest1.java

--- legenditementitytest1.java
+++ legenditementitytest1.java
@@ -1,41 +1,109 @@
 package org.jfree.chart.entity;
 
 import java.awt.geom.Rectangle2D;
-import org.jfree.chart.TestUtils;
-import org.jfree.chart.internal.CloneUtils;
 import org.jfree.data.category.DefaultCategoryDataset;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class LegendItemEntityTestTest1 {
+/**
+ * Tests for the {@link LegendItemEntity} class, focusing on the equals() method.
+ *
+ * The original monolithic test case was refactored into smaller, focused tests,
+ * each verifying a specific condition of the equals() contract. This approach
+ * improves readability and maintainability by making the purpose of each test
+ * clear and isolating failures to a specific property.
+ */
+public class LegendItemEntityTest {
 
     /**
-     * Confirm that the equals method can distinguish all the required fields.
+     * Creates a fully-populated LegendItemEntity for use as a baseline in tests.
+     *
+     * @return A new, fully-initialized LegendItemEntity instance.
      */
+    private LegendItemEntity<String> createPopulatedEntity() {
+        LegendItemEntity<String> entity = new LegendItemEntity<>(new Rectangle2D.Double(1.0, 2.0, 3.0, 4.0));
+        entity.setToolTipText("Test ToolTip");
+        entity.setURLText("http://www.jfree.org");
+        entity.setDataset(new DefaultCategoryDataset<String, String>());
+        entity.setSeriesKey("Series 1");
+        return entity;
+    }
+
     @Test
-    public void testEquals() {
-        LegendItemEntity<String> e1 = new LegendItemEntity<>(new Rectangle2D.Double(1.0, 2.0, 3.0, 4.0));
-        LegendItemEntity<String> e2 = new LegendItemEntity<>(new Rectangle2D.Double(1.0, 2.0, 3.0, 4.0));
-        assertEquals(e1, e2);
-        e1.setArea(new Rectangle2D.Double(4.0, 3.0, 2.0, 1.0));
-        assertNotEquals(e1, e2);
-        e2.setArea(new Rectangle2D.Double(4.0, 3.0, 2.0, 1.0));
-        assertEquals(e1, e2);
-        e1.setToolTipText("New ToolTip");
-        assertNotEquals(e1, e2);
-        e2.setToolTipText("New ToolTip");
-        assertEquals(e1, e2);
-        e1.setURLText("New URL");
-        assertNotEquals(e1, e2);
-        e2.setURLText("New URL");
-        assertEquals(e1, e2);
-        e1.setDataset(new DefaultCategoryDataset<String, String>());
-        assertNotEquals(e1, e2);
-        e2.setDataset(new DefaultCategoryDataset<String, String>());
-        assertEquals(e1, e2);
-        e1.setSeriesKey("A");
-        assertNotEquals(e1, e2);
-        e2.setSeriesKey("A");
-        assertEquals(e1, e2);
+    public void testEquals_withIdenticalInstances() {
+        // Arrange
+        LegendItemEntity<String> entity1 = createPopulatedEntity();
+        LegendItemEntity<String> entity2 = createPopulatedEntity();
+
+        // Act & Assert
+        assertEquals(entity1, entity2, "Two identical, fully-populated entities should be equal.");
+        assertEquals(entity1.hashCode(), entity2.hashCode(), "Hash codes should be equal for equal objects.");
+    }
+
+    @Test
+    public void testEquals_whenAreaIsDifferent() {
+        // Arrange
+        LegendItemEntity<String> entity1 = createPopulatedEntity();
+        LegendItemEntity<String> entity2 = createPopulatedEntity();
+        
+        // Act
+        entity2.setArea(new Rectangle2D.Double(4.0, 3.0, 2.0, 1.0));
+
+        // Assert
+        assertNotEquals(entity1, entity2, "Entities with different areas should not be equal.");
+    }
+
+    @Test
+    public void testEquals_whenToolTipTextIsDifferent() {
+        // Arrange
+        LegendItemEntity<String> entity1 = createPopulatedEntity();
+        LegendItemEntity<String> entity2 = createPopulatedEntity();
+        
+        // Act
+        entity2.setToolTipText("Different ToolTip");
+
+        // Assert
+        assertNotEquals(entity1, entity2, "Entities with different tooltips should not be equal.");
+    }
+
+    @Test
+    public void testEquals_whenURLTextIsDifferent() {
+        // Arrange
+        LegendItemEntity<String> entity1 = createPopulatedEntity();
+        LegendItemEntity<String> entity2 = createPopulatedEntity();
+        
+        // Act
+        entity2.setURLText("Different URL");
+
+        // Assert
+        assertNotEquals(entity1, entity2, "Entities with different URLs should not be equal.");
+    }
+
+    @Test
+    public void testEquals_whenDatasetIsDifferent() {
+        // Arrange
+        LegendItemEntity<String> entity1 = createPopulatedEntity();
+        LegendItemEntity<String> entity2 = createPopulatedEntity();
+        
+        // Act: Create a new dataset with different content
+        DefaultCategoryDataset<String, String> differentDataset = new DefaultCategoryDataset<>();
+        differentDataset.addValue(99.0, "R9", "C9");
+        entity2.setDataset(differentDataset);
+        
+        // Assert
+        assertNotEquals(entity1, entity2, "Entities with different datasets should not be equal.");
+    }
+
+    @Test
+    public void testEquals_whenSeriesKeyIsDifferent() {
+        // Arrange
+        LegendItemEntity<String> entity1 = createPopulatedEntity();
+        LegendItemEntity<String> entity2 = createPopulatedEntity();
+        
+        // Act
+        entity2.setSeriesKey("Different Key");
+
+        // Assert
+        assertNotEquals(entity1, entity2, "Entities with different series keys should not be equal.");
     }
 }
