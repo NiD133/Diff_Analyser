2bc3a3288e378cecae03833d74cd49bd7cafb5d5
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:38:40 +0800

    Original processedAutoTestCases base16_estest24.java

---
+++ base16_estest24.java
@@ -0,0 +1,31 @@
+package org.apache.commons.codec.binary;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.apache.commons.codec.CodecPolicy;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class Base16_ESTestTest24 extends Base16_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test23() throws Throwable {
+        CodecPolicy codecPolicy0 = CodecPolicy.LENIENT;
+        Base16 base16_0 = new Base16(true, codecPolicy0);
+        byte[] byteArray0 = new byte[19];
+        BaseNCodec.Context baseNCodec_Context0 = new BaseNCodec.Context();
+        baseNCodec_Context0.ibitWorkArea = 76;
+        // Undeclared exception!
+        try {
+            base16_0.decode(byteArray0, (int) (byte) 0, (int) (byte) 0, baseNCodec_Context0);
+            fail("Expecting exception: IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //
+            // Invalid octet in encoded value: 0
+            //
+            verifyException("org.apache.commons.codec.binary.Base16", e);
+        }
+    }
+}
