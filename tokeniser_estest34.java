ff6fbbae5535b529d086b01acaefcaa64201a2a3
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:22:13 +0800

    Original processedAutoTestCases tokeniser_estest34.java

---
+++ tokeniser_estest34.java
@@ -0,0 +1,24 @@
+package org.jsoup.parser;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.StringReader;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.jsoup.nodes.Comment;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.XmlDeclaration;
+import org.junit.runner.RunWith;
+
+public class Tokeniser_ESTestTest34 extends Tokeniser_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test33() throws Throwable {
+        XmlTreeBuilder xmlTreeBuilder0 = new XmlTreeBuilder();
+        xmlTreeBuilder0.parse("missing semicolon on [&#%s]", "missing semicolon on [&#%s]");
+        Tokeniser tokeniser0 = new Tokeniser(xmlTreeBuilder0);
+        tokeniser0.createBogusCommentPending();
+    }
+}
