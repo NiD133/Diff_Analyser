7f8d9a7cb323a9671b315a5e2d6f8c52424f9265
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:38:35 +0800

    Original processedAutoTestCases backgroundinitializer_estest7.java

---
+++ backgroundinitializer_estest7.java
@@ -0,0 +1,43 @@
+package org.apache.commons.lang3.concurrent;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.concurrent.Delayed;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.lang.MockException;
+import org.evosuite.runtime.mock.java.lang.MockThrowable;
+import org.evosuite.runtime.testdata.FileSystemHandling;
+import org.junit.runner.RunWith;
+
+public class BackgroundInitializer_ESTestTest7 extends BackgroundInitializer_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test06() throws Throwable {
+        BackgroundInitializer<Object> backgroundInitializer0 = new BackgroundInitializer<Object>();
+        backgroundInitializer0.getExternalExecutor();
+        BackgroundInitializer<Delayed> backgroundInitializer1 = new BackgroundInitializer<Delayed>((ExecutorService) null);
+        backgroundInitializer1.start();
+        backgroundInitializer1.start();
+        // Undeclared exception!
+        try {
+            backgroundInitializer1.setExternalExecutor((ExecutorService) null);
+            fail("Expecting exception: IllegalStateException");
+        } catch (IllegalStateException e) {
+            //
+            // Cannot set ExecutorService after start()!
+            //
+            verifyException("org.apache.commons.lang3.concurrent.BackgroundInitializer", e);
+        }
+    }
+}
