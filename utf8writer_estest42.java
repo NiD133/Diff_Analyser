7018d2b2a9dccf274b8fb0bb486cd3aba49fef7c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:25:45 +0800

    Original processedAutoTestCases utf8writer_estest42.java

---
+++ utf8writer_estest42.java
@@ -0,0 +1,48 @@
+package com.fasterxml.jackson.core.io;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.fasterxml.jackson.core.ErrorReportConfiguration;
+import com.fasterxml.jackson.core.StreamReadConstraints;
+import com.fasterxml.jackson.core.StreamWriteConstraints;
+import com.fasterxml.jackson.core.util.BufferRecycler;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PipedOutputStream;
+import java.io.Writer;
+import java.nio.CharBuffer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockFileOutputStream;
+import org.evosuite.runtime.mock.java.io.MockPrintStream;
+import org.junit.runner.RunWith;
+
+public class UTF8Writer_ESTestTest42 extends UTF8Writer_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test41() throws Throwable {
+        StreamWriteConstraints streamWriteConstraints0 = StreamWriteConstraints.defaults();
+        ErrorReportConfiguration errorReportConfiguration0 = ErrorReportConfiguration.defaults();
+        BufferRecycler bufferRecycler0 = new BufferRecycler();
+        ContentReference contentReference0 = ContentReference.REDACTED_CONTENT;
+        StreamReadConstraints streamReadConstraints0 = StreamReadConstraints.defaults();
+        IOContext iOContext0 = new IOContext(streamReadConstraints0, streamWriteConstraints0, errorReportConfiguration0, bufferRecycler0, contentReference0, true);
+        UTF8Writer uTF8Writer0 = new UTF8Writer(iOContext0, (OutputStream) null);
+        char[] charArray0 = new char[11];
+        charArray0[1] = '\u00D4';
+        // Undeclared exception!
+        try {
+            uTF8Writer0.write(charArray0, 1, 50000);
+            fail("Expecting exception: ArrayIndexOutOfBoundsException");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            //
+            // 11
+            //
+            verifyException("com.fasterxml.jackson.core.io.UTF8Writer", e);
+        }
+    }
+}
