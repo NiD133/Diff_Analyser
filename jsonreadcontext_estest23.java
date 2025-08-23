fd65b6673b4545535f1e0365fc09f4d6fc71cd30
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:57:23 +0800

    Original processedAutoTestCases jsonreadcontext_estest23.java

---
+++ jsonreadcontext_estest23.java
@@ -0,0 +1,28 @@
+package com.fasterxml.jackson.core.json;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.fasterxml.jackson.core.ErrorReportConfiguration;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonFactoryBuilder;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonLocation;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.io.ContentReference;
+import java.io.IOException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class JsonReadContext_ESTestTest23 extends JsonReadContext_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test22() throws Throwable {
+        JsonReadContext jsonReadContext0 = JsonReadContext.createRootContext((DupDetector) null);
+        jsonReadContext0.getDupDetector();
+        assertEquals(0, jsonReadContext0.getEntryCount());
+        assertTrue(jsonReadContext0.inRoot());
+        assertEquals(0, jsonReadContext0.getNestingDepth());
+    }
+}
