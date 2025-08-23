d20f66ad106f66228439854f7c53b89f88fd1177
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:23:29 +0800

    Original processedAutoTestCases transformedsortedmap_estest20.java

---
+++ transformedsortedmap_estest20.java
@@ -0,0 +1,53 @@
+package org.apache.commons.collections4.map;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.lang.reflect.Array;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.NoSuchElementException;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import org.apache.commons.collections4.Closure;
+import org.apache.commons.collections4.Factory;
+import org.apache.commons.collections4.Predicate;
+import org.apache.commons.collections4.Transformer;
+import org.apache.commons.collections4.functors.ChainedTransformer;
+import org.apache.commons.collections4.functors.ConstantFactory;
+import org.apache.commons.collections4.functors.ConstantTransformer;
+import org.apache.commons.collections4.functors.ExceptionTransformer;
+import org.apache.commons.collections4.functors.FactoryTransformer;
+import org.apache.commons.collections4.functors.InvokerTransformer;
+import org.apache.commons.collections4.functors.MapTransformer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class TransformedSortedMap_ESTestTest20 extends TransformedSortedMap_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test19() throws Throwable {
+        TreeMap<Object, Object> treeMap0 = new TreeMap<Object, Object>();
+        Transformer<Object, Object> transformer0 = MapTransformer.mapTransformer((Map<? super Object, ?>) treeMap0);
+        String string0 = "i]oNU\\u7I%x";
+        Transformer<Object, Integer> transformer1 = InvokerTransformer.invokerTransformer(string0);
+        TreeMap<Object, Integer> treeMap1 = new TreeMap<Object, Integer>();
+        TransformedSortedMap<Object, Integer> transformedSortedMap0 = TransformedSortedMap.transformingSortedMap((SortedMap<Object, Integer>) treeMap1, (Transformer<? super Object, ?>) transformer0, (Transformer<? super Integer, ? extends Integer>) transformer1);
+        Integer integer0 = new Integer(1);
+        // Undeclared exception!
+        try {
+            transformedSortedMap0.subMap(integer0, string0);
+            fail("Expecting exception: ClassCastException");
+        } catch (ClassCastException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+        }
+    }
+}
