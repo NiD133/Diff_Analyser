4229f6d5948463f56ad4996c24bc5301efed616a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:49:20 +0800

    gemini processedAutoTestCases entities_estest1.java

--- entities_estest1.java
+++ entities_estest1.java
@@ -1,28 +1,28 @@
 package org.jsoup.nodes;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.File;
-import java.io.PipedWriter;
-import java.io.StringWriter;
-import java.nio.BufferOverflowException;
-import java.nio.CharBuffer;
-import java.nio.ReadOnlyBufferException;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.io.MockFile;
-import org.evosuite.runtime.mock.java.io.MockFileWriter;
-import org.evosuite.runtime.mock.java.io.MockPrintStream;
-import org.jsoup.internal.QuietAppendable;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class Entities_ESTestTest1 extends Entities_ESTest_scaffolding {
+/**
+ * Tests for the {@link Entities.EscapeMode} enum.
+ */
+public class EntitiesEscapeModeTest {
 
-    @Test(timeout = 4000)
-    public void test00() throws Throwable {
-        Entities.EscapeMode entities_EscapeMode0 = Entities.EscapeMode.xhtml;
-        int int0 = entities_EscapeMode0.codepointForName("amp");
-        assertEquals(38, int0);
+    /**
+     * Verifies that codepointForName returns the correct codepoint for the "amp" entity
+     * when using the xhtml escape mode. The ampersand entity (&amp;) is one of the
+     * core entities defined in this mode.
+     */
+    @Test
+    public void codepointForName_inXhtmlMode_returnsCorrectCodepointForAmp() {
+        // Arrange
+        Entities.EscapeMode xhtmlMode = Entities.EscapeMode.xhtml;
+        final int expectedCodepoint = 38; // The ASCII codepoint for '&'
+
+        // Act
+        int actualCodepoint = xhtmlMode.codepointForName("amp");
+
+        // Assert
+        assertEquals("The codepoint for 'amp' in xhtml mode should be correct.", expectedCodepoint, actualCodepoint);
     }
 }
