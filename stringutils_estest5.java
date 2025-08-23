0f0c00b8752d5272a58f3badca11ada2ef2726c3
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:18:38 +0800

    Original processedAutoTestCases stringutils_estest5.java

---
+++ stringutils_estest5.java
@@ -0,0 +1,25 @@
+package com.itextpdf.text.pdf;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class StringUtils_ESTestTest5 extends StringUtils_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test04() throws Throwable {
+        // Undeclared exception!
+        try {
+            StringUtils.escapeString((byte[]) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("com.itextpdf.text.pdf.StringUtils", e);
+        }
+    }
+}
