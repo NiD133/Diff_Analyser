54c55b5f66a45780b2e45433e8f54690dacffe45
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:26:25 +0800

    Original processedManualTestCases caseformattest30.java

---
+++ caseformattest30.java
@@ -0,0 +1,28 @@
+package com.google.common.base;
+
+import static com.google.common.base.CaseFormat.LOWER_CAMEL;
+import static com.google.common.base.CaseFormat.LOWER_HYPHEN;
+import static com.google.common.base.CaseFormat.LOWER_UNDERSCORE;
+import static com.google.common.base.CaseFormat.UPPER_CAMEL;
+import static com.google.common.base.CaseFormat.UPPER_UNDERSCORE;
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.truth.Truth.assertWithMessage;
+import com.google.common.annotations.GwtCompatible;
+import com.google.common.annotations.GwtIncompatible;
+import com.google.common.annotations.J2ktIncompatible;
+import com.google.common.testing.NullPointerTester;
+import com.google.common.testing.SerializableTester;
+import junit.framework.TestCase;
+import org.jspecify.annotations.NullUnmarked;
+
+public class CaseFormatTestTest30 extends TestCase {
+
+    public void testConverter_nullConversions() {
+        for (CaseFormat outer : CaseFormat.values()) {
+            for (CaseFormat inner : CaseFormat.values()) {
+                assertThat(outer.converterTo(inner).convert(null)).isNull();
+                assertThat(outer.converterTo(inner).reverse().convert(null)).isNull();
+            }
+        }
+    }
+}
