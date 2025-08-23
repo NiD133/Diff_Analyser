e037a36981c2201b1c5d0dbe857288e711b9ee89
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:48:37 +0800

    Original processedAutoTestCases elements_estest169.java

---
+++ elements_estest169.java
@@ -0,0 +1,37 @@
+package org.jsoup.select;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.ConcurrentModificationException;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.jsoup.nodes.Comment;
+import org.jsoup.nodes.DataNode;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.FormElement;
+import org.jsoup.nodes.TextNode;
+import org.jsoup.parser.Parser;
+import org.junit.runner.RunWith;
+
+public class Elements_ESTestTest169 extends Elements_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test168() throws Throwable {
+        Document document0 = Parser.parse("Bb,Y|a8", "Bb,Y|a8");
+        Elements elements0 = document0.getAllElements();
+        Elements elements1 = elements0.clone();
+        assertNotSame(elements1, elements0);
+        assertEquals(4, elements1.size());
+    }
+}
