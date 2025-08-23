68e26b95bf9bceba763f7f7f61640e46ae6eda30
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:46:03 +0800

    gemini processedAutoTestCases coreoperation_estest5.java

--- coreoperation_estest5.java
+++ coreoperation_estest5.java
@@ -1,38 +1,31 @@
 package org.apache.commons.jxpath.ri.compiler;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.apache.commons.jxpath.JXPathContext;
-import org.apache.commons.jxpath.ri.EvalContext;
-import org.apache.commons.jxpath.ri.JXPathContextReferenceImpl;
-import org.apache.commons.jxpath.ri.QName;
-import org.apache.commons.jxpath.ri.axes.InitialContext;
-import org.apache.commons.jxpath.ri.axes.ParentContext;
-import org.apache.commons.jxpath.ri.axes.PrecedingOrFollowingContext;
-import org.apache.commons.jxpath.ri.axes.RootContext;
-import org.apache.commons.jxpath.ri.axes.SelfContext;
-import org.apache.commons.jxpath.ri.axes.UnionContext;
-import org.apache.commons.jxpath.ri.model.NodePointer;
-import org.apache.commons.jxpath.ri.model.VariablePointer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class CoreOperation_ESTestTest5 extends CoreOperation_ESTest_scaffolding {
+/**
+ * Contains tests for the abstract class {@link CoreOperation}.
+ * These tests verify the behavior of concrete methods implemented in the abstract base class.
+ */
+public class CoreOperationTest {
 
-    @Test(timeout = 4000)
-    public void test04() throws Throwable {
-        NameAttributeTest nameAttributeTest0 = new NameAttributeTest((Expression) null, (Expression) null);
-        // Undeclared exception!
-        try {
-            nameAttributeTest0.toString();
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.evosuite.runtime.System", e);
-        }
+    /**
+     * Tests that {@link CoreOperation#toString()} throws a NullPointerException
+     * when the operation is constructed with null arguments.
+     * <p>
+     * The {@code toString()} method in the abstract {@code CoreOperation} class
+     * is expected to access its internal expression arguments. If these arguments
+     * are null, a {@code NullPointerException} should be thrown, indicating
+     * that the operation was not properly initialized.
+     */
+    @Test(expected = NullPointerException.class)
+    public void toString_withNullArguments_throwsNullPointerException() {
+        // Since CoreOperation is abstract, we use a concrete subclass to test its
+        // non-abstract methods. NameAttributeTest is a simple two-argument operation.
+        // We pass null for its expression arguments to trigger the condition under test.
+        NameAttributeTest operationWithNullArgs = new NameAttributeTest(null, null);
+
+        // This call is expected to throw a NullPointerException because the
+        // toString() implementation in CoreOperation attempts to dereference its null arguments.
+        operationWithNullArgs.toString();
     }
 }
