e3e2248702a942f5efe792e9d4d70c14700be788
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:55:35 +0800

    gemini processedAutoTestCases iso8601utils_estest6.java

--- iso8601utils_estest6.java
+++ iso8601utils_estest6.java
@@ -1,31 +1,21 @@
 package com.google.gson.internal.bind.util;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.text.ParseException;
-import java.text.ParsePosition;
 import java.util.Date;
-import java.util.SimpleTimeZone;
-import java.util.TimeZone;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.util.MockDate;
-import org.junit.runner.RunWith;
 
-public class ISO8601Utils_ESTestTest6 extends ISO8601Utils_ESTest_scaffolding {
+/**
+ * Tests for {@link ISO8601Utils}.
+ */
+public class ISO8601UtilsTest {
 
-    @Test(timeout = 4000)
-    public void test05() throws Throwable {
-        // Undeclared exception!
-        try {
-            ISO8601Utils.format((Date) null, true);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("java.util.Calendar", e);
-        }
+    /**
+     * The format method should not accept a null Date object and must throw a
+     * NullPointerException.
+     */
+    @Test(expected = NullPointerException.class)
+    public void format_withNullDate_throwsNullPointerException() {
+        // Call the format method with a null Date, which is expected to fail.
+        // The second argument (include milliseconds) is irrelevant for this test case.
+        ISO8601Utils.format(null, true);
     }
 }
