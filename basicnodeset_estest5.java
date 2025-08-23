79b950f54ca4c311d762c997943fd75718255c07
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:38:52 +0800

    Original processedAutoTestCases basicnodeset_estest5.java

---
+++ basicnodeset_estest5.java
@@ -0,0 +1,31 @@
+package org.apache.commons.jxpath;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.List;
+import org.apache.commons.jxpath.ri.QName;
+import org.apache.commons.jxpath.ri.model.VariablePointer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class BasicNodeSet_ESTestTest5 extends BasicNodeSet_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test04() throws Throwable {
+        BasicNodeSet basicNodeSet0 = new BasicNodeSet();
+        VariablePointer variablePointer0 = new VariablePointer((QName) null);
+        basicNodeSet0.add((Pointer) variablePointer0);
+        // Undeclared exception!
+        try {
+            basicNodeSet0.getNodes();
+            fail("Expecting exception: RuntimeException");
+        } catch (RuntimeException e) {
+            //
+            // Undefined variable: null
+            //
+            verifyException("org.apache.commons.jxpath.ri.model.VariablePointer$1", e);
+        }
+    }
+}
