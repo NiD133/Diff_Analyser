55a95226c583517688d5e9a95c5892299e49651d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:44:37 +0800

    gemini processedAutoTestCases collectionsortedbag_estest42.java

--- collectionsortedbag_estest42.java
+++ collectionsortedbag_estest42.java
@@ -1,55 +1,35 @@
 package org.apache.commons.collections4.bag;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.ConcurrentModificationException;
-import java.util.LinkedList;
-import java.util.Locale;
-import java.util.PriorityQueue;
-import java.util.Set;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.Predicate;
 import org.apache.commons.collections4.SortedBag;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionPredicate;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.IfClosure;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.NOPClosure;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.TransformedPredicate;
-import org.apache.commons.collections4.functors.TransformerPredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class CollectionSortedBag_ESTestTest42 extends CollectionSortedBag_ESTest_scaffolding {
+/**
+ * Contains tests for {@link CollectionSortedBag}.
+ */
+public class CollectionSortedBagTest {
 
-    @Test(timeout = 4000)
-    public void test41() throws Throwable {
-        TreeBag<IfClosure<Object>> treeBag0 = new TreeBag<IfClosure<Object>>();
-        CollectionSortedBag<IfClosure<Object>> collectionSortedBag0 = new CollectionSortedBag<IfClosure<Object>>(treeBag0);
-        // Undeclared exception!
-        try {
-            collectionSortedBag0.remove((Object) treeBag0);
-            fail("Expecting exception: ClassCastException");
-        } catch (ClassCastException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-        }
+    /**
+     * Tests that calling remove() with an object of an incompatible type throws a ClassCastException.
+     * <p>
+     * The {@link CollectionSortedBag} delegates the remove operation to the decorated bag.
+     * In this test, we use a {@link TreeBag}, which requires its elements to be comparable.
+     * When attempting to remove an object that cannot be cast to the bag's element type,
+     * the underlying {@code TreeBag} will fail with a {@code ClassCastException} during its
+     * internal comparison logic.
+     */
+    @Test(expected = ClassCastException.class)
+    public void removeWithIncompatibleTypeShouldThrowClassCastException() {
+        // Arrange: Create a CollectionSortedBag decorating a TreeBag of Strings.
+        final SortedBag<String> decoratedBag = new TreeBag<>();
+        final SortedBag<String> collectionBag = new CollectionSortedBag<>(decoratedBag);
+
+        // An object that is not a String and cannot be cast to one.
+        final Object incompatibleObject = new Object();
+
+        // Act: Attempt to remove the incompatible object from the bag.
+        // This will trigger the ClassCastException from the underlying TreeBag.
+        collectionBag.remove(incompatibleObject);
+
+        // Assert: A ClassCastException is expected, as declared in the @Test annotation.
     }
 }
