9acb8bb34b9c2e5b6bef1120ac26c03c3945ea56
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:52:44 +0800

    gemini processedAutoTestCases groupedrandomaccesssource_estest16.java

--- groupedrandomaccesssource_estest16.java
+++ groupedrandomaccesssource_estest16.java
@@ -1,32 +1,41 @@
 package com.itextpdf.text.io;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
+/**
+ * This test class contains tests for the {@link GroupedRandomAccessSource}.
+ * Note: The original class name and inheritance were preserved from the auto-generated test.
+ * In a typical project, this might be renamed to "GroupedRandomAccessSourceTest".
+ */
 public class GroupedRandomAccessSource_ESTestTest16 extends GroupedRandomAccessSource_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test15() throws Throwable {
-        RandomAccessSource[] randomAccessSourceArray0 = new RandomAccessSource[1];
-        WindowRandomAccessSource windowRandomAccessSource0 = new WindowRandomAccessSource((RandomAccessSource) null, 328L, 328L);
-        randomAccessSourceArray0[0] = (RandomAccessSource) windowRandomAccessSource0;
-        GroupedRandomAccessSource groupedRandomAccessSource0 = new GroupedRandomAccessSource(randomAccessSourceArray0);
-        // Undeclared exception!
-        try {
-            groupedRandomAccessSource0.close();
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("com.itextpdf.text.io.WindowRandomAccessSource", e);
-        }
+    /**
+     * Verifies that calling close() on a GroupedRandomAccessSource propagates the call
+     * to its underlying sources and correctly throws a NullPointerException if one of
+     * those sources fails during its own close operation.
+     *
+     * This is tested by using a WindowRandomAccessSource that was intentionally
+     * created with a null delegate source. When the GroupedRandomAccessSource tries
+     * to close this member source, the attempt to close the null delegate results
+     * in the expected NullPointerException.
+     */
+    @Test(timeout = 4000, expected = NullPointerException.class)
+    public void closeShouldThrowNpeWhenUnderlyingSourceDelegateIsNull() throws IOException {
+        // Arrange: Create a source that is designed to fail on close().
+        // A WindowRandomAccessSource initialized with a null delegate will throw an NPE
+        // when its close() method is called.
+        RandomAccessSource sourceWithNullDelegate = new WindowRandomAccessSource(null, 328L, 328L);
+
+        // Arrange: Create the GroupedRandomAccessSource containing the faulty source.
+        RandomAccessSource[] sources = { sourceWithNullDelegate };
+        GroupedRandomAccessSource groupedSource = new GroupedRandomAccessSource(sources);
+
+        // Act: Attempt to close the grouped source. This action should trigger the NPE
+        // from the underlying sourceWithNullDelegate.
+        groupedSource.close();
+
+        // Assert: The test passes if a NullPointerException is thrown, as declared
+        // by the @Test(expected=...) annotation.
     }
 }
