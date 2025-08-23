f7ca660c67f8e6ef48b8d2592bd521f7d5cf3e83
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:53:55 +0800

    gemini processedAutoTestCases indexedcollection_estest2.java

--- indexedcollection_estest2.java
+++ indexedcollection_estest2.java
@@ -1,61 +1,50 @@
 package org.apache.commons.collections4.collection;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.ConcurrentModificationException;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Set;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.Predicate;
 import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AllPredicate;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ChainedTransformer;
-import org.apache.commons.collections4.functors.CloneTransformer;
-import org.apache.commons.collections4.functors.ClosureTransformer;
-import org.apache.commons.collections4.functors.ConstantFactory;
 import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.DefaultEquator;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.FactoryTransformer;
-import org.apache.commons.collections4.functors.FalsePredicate;
-import org.apache.commons.collections4.functors.ForClosure;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.NOPClosure;
-import org.apache.commons.collections4.functors.NOPTransformer;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NotNullPredicate;
-import org.apache.commons.collections4.functors.NullIsFalsePredicate;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.SwitchTransformer;
-import org.apache.commons.collections4.functors.TransformedPredicate;
-import org.apache.commons.collections4.functors.TransformerClosure;
-import org.apache.commons.collections4.functors.TransformerPredicate;
-import org.apache.commons.collections4.functors.TruePredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class IndexedCollection_ESTestTest2 extends IndexedCollection_ESTest_scaffolding {
+import java.util.Collection;
+import java.util.LinkedList;
 
-    @Test(timeout = 4000)
-    public void test01() throws Throwable {
-        LinkedList<LinkedList<Integer>> linkedList0 = new LinkedList<LinkedList<Integer>>();
-        Integer integer0 = new Integer(1);
-        Transformer<LinkedList<Integer>, Integer> transformer0 = ConstantTransformer.constantTransformer(integer0);
-        IndexedCollection<Integer, LinkedList<Integer>> indexedCollection0 = IndexedCollection.uniqueIndexedCollection((Collection<LinkedList<Integer>>) linkedList0, transformer0);
-        LinkedList<Integer> linkedList1 = new LinkedList<Integer>();
-        indexedCollection0.add(linkedList1);
-        Collection<LinkedList<Integer>> collection0 = indexedCollection0.values(integer0);
-        assertTrue(collection0.contains(linkedList1));
-        assertNotNull(collection0);
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Contains tests for the {@link IndexedCollection} class.
+ */
+public class IndexedCollectionTest {
+
+    /**
+     * Tests that an item added to a unique IndexedCollection can be successfully
+     * retrieved using the values(key) method.
+     */
+    @Test
+    public void values_shouldReturnCollectionContainingTheAddedItem_forUniqueIndex() {
+        // Arrange
+        // The transformer will always return a constant key (1) for any element added.
+        final Integer key = 1;
+        final Transformer<String, Integer> keyTransformer = ConstantTransformer.constantTransformer(key);
+
+        // Create a unique indexed collection, which will enforce that each key maps to only one value.
+        final Collection<String> sourceCollection = new LinkedList<>();
+        final IndexedCollection<Integer, String> indexedCollection =
+                IndexedCollection.uniqueIndexedCollection(sourceCollection, keyTransformer);
+
+        final String valueToAdd = "test-value";
+
+        // Act
+        // Add the element to the collection. It should be indexed under the key '1'.
+        indexedCollection.add(valueToAdd);
+
+        // Retrieve all values associated with the key.
+        final Collection<String> retrievedValues = indexedCollection.values(key);
+
+        // Assert
+        // Verify that the retrieved collection is not null and contains exactly the added element.
+        assertNotNull("The collection returned by values() should not be null.", retrievedValues);
+        assertEquals("There should be exactly one value for the given key.", 1, retrievedValues.size());
+        assertTrue("The retrieved collection should contain the added value.", retrievedValues.contains(valueToAdd));
     }
 }
