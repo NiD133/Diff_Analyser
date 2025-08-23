9b8f2329ce31802150a15993c8c342565ed55da1
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:26:23 +0800

    gemini processedManualTestCases caseformattest26.java

--- caseformattest26.java
+++ caseformattest26.java
@@ -1,25 +1,30 @@
 package com.google.common.base;
 
-import static com.google.common.base.CaseFormat.LOWER_CAMEL;
-import static com.google.common.base.CaseFormat.LOWER_HYPHEN;
-import static com.google.common.base.CaseFormat.LOWER_UNDERSCORE;
 import static com.google.common.base.CaseFormat.UPPER_CAMEL;
 import static com.google.common.base.CaseFormat.UPPER_UNDERSCORE;
 import static com.google.common.truth.Truth.assertThat;
-import static com.google.common.truth.Truth.assertWithMessage;
-import com.google.common.annotations.GwtCompatible;
-import com.google.common.annotations.GwtIncompatible;
-import com.google.common.annotations.J2ktIncompatible;
-import com.google.common.testing.NullPointerTester;
-import com.google.common.testing.SerializableTester;
-import junit.framework.TestCase;
-import org.jspecify.annotations.NullUnmarked;
 
-public class CaseFormatTestTest26 extends TestCase {
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
-    public void testUpperUnderscoreToUpperCamel() {
+/**
+ * Tests for the conversion from {@link CaseFormat#UPPER_UNDERSCORE} to
+ * {@link CaseFormat#UPPER_CAMEL}.
+ */
+@RunWith(JUnit4.class)
+public class CaseFormatTest {
+
+    @Test
+    public void upperUnderscore_to_upperCamel_convertsStringsCorrectly() {
+        // A single word should be converted to title case.
         assertThat(UPPER_UNDERSCORE.to(UPPER_CAMEL, "FOO")).isEqualTo("Foo");
+
+        // Multiple words should be joined, with each word converted to title case.
         assertThat(UPPER_UNDERSCORE.to(UPPER_CAMEL, "FOO_BAR")).isEqualTo("FooBar");
+
+        // An acronym-like string should be fully capitalized, which tests the
+        // special handling of single-letter words.
         assertThat(UPPER_UNDERSCORE.to(UPPER_CAMEL, "H_T_T_P")).isEqualTo("HTTP");
     }
 }
