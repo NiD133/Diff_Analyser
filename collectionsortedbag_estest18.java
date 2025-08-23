ac8306d8819f8eaa24fde858f3832012cfe63156
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:44:29 +0800

    gemini processedAutoTestCases collectionsortedbag_estest18.java

--- collectionsortedbag_estest18.java
+++ collectionsortedbag_estest18.java
@@ -1,56 +1,29 @@
 package org.apache.commons.collections4.bag;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.ConcurrentModificationException;
-import java.util.LinkedList;
-import java.util.Locale;
-import java.util.PriorityQueue;
-import java.util.Set;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.Predicate;
 import org.apache.commons.collections4.SortedBag;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionPredicate;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.IfClosure;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.NOPClosure;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.TransformedPredicate;
-import org.apache.commons.collections4.functors.TransformerPredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class CollectionSortedBag_ESTestTest18 extends CollectionSortedBag_ESTest_scaffolding {
+/**
+ * Tests for {@link CollectionSortedBag}, focusing on argument validation.
+ */
+public class CollectionSortedBag_ESTestTest18 {
 
-    @Test(timeout = 4000)
-    public void test17() throws Throwable {
-        TreeBag<Object> treeBag0 = new TreeBag<Object>();
-        CollectionSortedBag<Object> collectionSortedBag0 = new CollectionSortedBag<Object>(treeBag0);
-        // Undeclared exception!
-        try {
-            collectionSortedBag0.addAll((Collection<?>) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.commons.collections4.bag.CollectionSortedBag", e);
-        }
+    /**
+     * Verifies that calling addAll() with a null collection throws a NullPointerException.
+     * <p>
+     * The {@link CollectionSortedBag} is a decorator, so it is expected to delegate
+     * this call to the underlying bag, which should reject a null argument.
+     */
+    @Test(expected = NullPointerException.class)
+    public void addAllWithNullCollectionShouldThrowNullPointerException() {
+        // Arrange: Create a CollectionSortedBag decorating a standard TreeBag.
+        final SortedBag<Object> decoratedBag = new TreeBag<>();
+        final SortedBag<Object> collectionBag = new CollectionSortedBag<>(decoratedBag);
+
+        // Act: Attempt to add a null collection. This action is expected to throw.
+        collectionBag.addAll(null);
+
+        // Assert: The 'expected' parameter of the @Test annotation handles the assertion.
+        // The test will pass only if a NullPointerException is thrown.
     }
 }
