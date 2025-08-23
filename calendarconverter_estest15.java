7c8029c82261ca03497e55c0d633159f71ea7537
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:41:31 +0800

    gemini processedAutoTestCases calendarconverter_estest15.java

--- calendarconverter_estest15.java
+++ calendarconverter_estest15.java
@@ -1,38 +1,26 @@
 package org.joda.time.convert;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
-import java.util.GregorianCalendar;
-import java.util.TimeZone;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.System;
-import org.evosuite.runtime.mock.java.time.MockZonedDateTime;
-import org.evosuite.runtime.mock.java.util.MockDate;
-import org.evosuite.runtime.mock.java.util.MockGregorianCalendar;
 import org.joda.time.Chronology;
-import org.joda.time.DateTimeZone;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class CalendarConverter_ESTestTest15 extends CalendarConverter_ESTest_scaffolding {
+/**
+ * Unit tests for {@link CalendarConverter}.
+ */
+public class CalendarConverterTest {
 
-    @Test(timeout = 4000)
-    public void test14() throws Throwable {
-        CalendarConverter calendarConverter0 = new CalendarConverter();
-        Object object0 = new Object();
-        // Undeclared exception!
-        try {
-            calendarConverter0.getInstantMillis(object0, (Chronology) null);
-            fail("Expecting exception: ClassCastException");
-        } catch (ClassCastException e) {
-            //
-            // java.lang.Object cannot be cast to java.util.Calendar
-            //
-            verifyException("org.joda.time.convert.CalendarConverter", e);
-        }
+    /**
+     * Tests that getInstantMillis() throws a ClassCastException when the input object
+     * is not an instance of java.util.Calendar.
+     */
+    @Test(expected = ClassCastException.class)
+    public void getInstantMillis_whenObjectIsNotCalendar_shouldThrowClassCastException() {
+        // Arrange
+        CalendarConverter converter = CalendarConverter.INSTANCE;
+        Object invalidObject = new Object();
+
+        // Act & Assert
+        // This call is expected to throw a ClassCastException because the converter
+        // cannot handle a plain Object.
+        converter.getInstantMillis(invalidObject, (Chronology) null);
     }
 }
