0e6b43a5e320c1d185e7a93cfc85f0b20f80fb83
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:40:35 +0800

    Original processedAutoTestCases bytearraybuilder_estest28.java

---
+++ bytearraybuilder_estest28.java
@@ -0,0 +1,22 @@
+package com.fasterxml.jackson.core.util;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class ByteArrayBuilder_ESTestTest28 extends ByteArrayBuilder_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test27() throws Throwable {
+        ByteArrayBuilder byteArrayBuilder0 = ByteArrayBuilder.fromInitial((byte[]) null, 131050);
+        // Undeclared exception!
+        try {
+            byteArrayBuilder0.getClearAndRelease();
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+        }
+    }
+}
