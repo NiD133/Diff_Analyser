7e3f0f717a247d3c789c68ad18255512a485571c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:31:41 +0800

    gemini processedManualTestCases observableinputstreamtest1.java

--- observableinputstreamtest1.java
+++ observableinputstreamtest1.java
@@ -1,171 +1,63 @@
 package org.apache.commons.io.input;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import java.io.ByteArrayInputStream;
+
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.concurrent.atomic.AtomicBoolean;
+
 import org.apache.commons.io.IOUtils;
-import org.apache.commons.io.input.ObservableInputStream.Observer;
-import org.apache.commons.io.output.NullOutputStream;
 import org.apache.commons.io.test.CustomIOException;
 import org.junit.jupiter.api.Test;
 
-public class ObservableInputStreamTestTest1 {
+/**
+ * Tests for {@link ObservableInputStream.Builder}, focusing on the afterRead consumer.
+ */
+public class ObservableInputStreamBuilderTest {
 
-    private ObservableInputStream brokenObservableInputStream() {
-        return new ObservableInputStream(BrokenInputStream.INSTANCE);
-    }
-
-    private InputStream createInputStream() {
-        final byte[] buffer = MessageDigestInputStreamTest.generateRandomByteStream(IOUtils.DEFAULT_BUFFER_SIZE);
-        return createInputStream(new ByteArrayInputStream(buffer));
-    }
-
-    private ObservableInputStream createInputStream(final InputStream origin) {
-        return new ObservableInputStream(origin);
-    }
-
-    private void testNotificationCallbacks(final int bufferSize) throws IOException {
-        final byte[] buffer = IOUtils.byteArray();
-        final LengthObserver lengthObserver = new LengthObserver();
-        final MethodCountObserver methodCountObserver = new MethodCountObserver();
-        try (ObservableInputStream ois = new ObservableInputStream(new ByteArrayInputStream(buffer), lengthObserver, methodCountObserver)) {
-            assertEquals(IOUtils.DEFAULT_BUFFER_SIZE, IOUtils.copy(ois, NullOutputStream.INSTANCE, bufferSize));
-        }
-        assertEquals(IOUtils.DEFAULT_BUFFER_SIZE, lengthObserver.getTotal());
-        assertEquals(1, methodCountObserver.getClosedCount());
-        assertEquals(1, methodCountObserver.getFinishedCount());
-        assertEquals(0, methodCountObserver.getErrorCount());
-        assertEquals(0, methodCountObserver.getDataCount());
-        assertEquals(buffer.length / bufferSize, methodCountObserver.getDataBufferCount());
-    }
-
-    private static final class DataViewObserver extends MethodCountObserver {
-
-        private byte[] buffer;
-
-        private int lastValue = -1;
-
-        private int length = -1;
-
-        private int offset = -1;
-
-        @Override
-        public void data(final byte[] buffer, final int offset, final int length) throws IOException {
-            this.buffer = buffer;
-            this.offset = offset;
-            this.length = length;
-        }
-
-        @Override
-        public void data(final int value) throws IOException {
-            super.data(value);
-            lastValue = value;
-        }
-    }
-
-    private static final class LengthObserver extends Observer {
-
-        private long total;
-
-        @Override
-        public void data(final byte[] buffer, final int offset, final int length) throws IOException {
-            this.total += length;
-        }
-
-        @Override
-        public void data(final int value) throws IOException {
-            total++;
-        }
-
-        public long getTotal() {
-            return total;
-        }
-    }
-
-    private static class MethodCountObserver extends Observer {
-
-        private long closedCount;
-
-        private long dataBufferCount;
-
-        private long dataCount;
-
-        private long errorCount;
-
-        private long finishedCount;
-
-        @Override
-        public void closed() throws IOException {
-            closedCount++;
-        }
-
-        @Override
-        public void data(final byte[] buffer, final int offset, final int length) throws IOException {
-            dataBufferCount++;
-        }
-
-        @Override
-        public void data(final int value) throws IOException {
-            dataCount++;
-        }
-
-        @Override
-        public void error(final IOException exception) throws IOException {
-            errorCount++;
-        }
-
-        @Override
-        public void finished() throws IOException {
-            finishedCount++;
-        }
-
-        public long getClosedCount() {
-            return closedCount;
-        }
-
-        public long getDataBufferCount() {
-            return dataBufferCount;
-        }
-
-        public long getDataCount() {
-            return dataCount;
-        }
-
-        public long getErrorCount() {
-            return errorCount;
-        }
-
-        public long getFinishedCount() {
-            return finishedCount;
-        }
-    }
-
+    /**
+     * Tests that the afterRead consumer is successfully invoked after the stream is read.
+     */
     @Test
-    void testAfterReadConsumer() throws Exception {
-        final AtomicBoolean boolRef = new AtomicBoolean();
-        // @formatter:off
-        try (InputStream bounded = new ObservableInputStream.Builder().setCharSequence("Hi").setAfterRead(i -> boolRef.set(true)).get()) {
-            IOUtils.consume(bounded);
+    void testAfterReadConsumerIsCalledWhenStreamIsConsumed() throws IOException {
+        // Arrange
+        final AtomicBoolean consumerCalled = new AtomicBoolean(false);
+        final InputStream observableStream = new ObservableInputStream.Builder()
+            .setCharSequence("test data")
+            .setAfterRead(bytesRead -> consumerCalled.set(true))
+            .get();
+
+        // Act
+        try (InputStream stream = observableStream) {
+            IOUtils.consume(stream);
         }
-        // @formatter:on
-        assertTrue(boolRef.get());
-        // Throwing
-        final String message = "test exception message";
-        // @formatter:off
-        try (InputStream bounded = new ObservableInputStream.Builder().setCharSequence("Hi").setAfterRead(i -> {
-            throw new CustomIOException(message);
-        }).get()) {
-            assertEquals(message, assertThrowsExactly(CustomIOException.class, () -> IOUtils.consume(bounded)).getMessage());
+
+        // Assert
+        assertTrue(consumerCalled.get(), "The afterRead consumer should have been called.");
+    }
+
+    /**
+     * Tests that an IOException thrown by the afterRead consumer is propagated to the caller.
+     */
+    @Test
+    void testExceptionInAfterReadConsumerIsPropagated() throws IOException {
+        // Arrange
+        final String expectedMessage = "Test exception from afterRead consumer";
+        final InputStream observableStream = new ObservableInputStream.Builder()
+            .setCharSequence("test data")
+            .setAfterRead(bytesRead -> {
+                throw new CustomIOException(expectedMessage);
+            })
+            .get();
+
+        // Act & Assert
+        try (InputStream stream = observableStream) {
+            final CustomIOException thrown = assertThrowsExactly(CustomIOException.class,
+                () -> IOUtils.consume(stream),
+                "Consuming the stream should propagate the exception from the consumer.");
+            assertEquals(expectedMessage, thrown.getMessage(), "The exception message should match the expected message.");
         }
-        // @formatter:on
     }
 }
