2084325eca1ea1ae5d26e3b2231587f42b433458
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:49:33 +0800

    Original processedAutoTestCases entities_estest49.java

---
+++ entities_estest49.java
@@ -0,0 +1,27 @@
+package org.jsoup.nodes;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.File;
+import java.io.PipedWriter;
+import java.io.StringWriter;
+import java.nio.BufferOverflowException;
+import java.nio.CharBuffer;
+import java.nio.ReadOnlyBufferException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.evosuite.runtime.mock.java.io.MockFileWriter;
+import org.evosuite.runtime.mock.java.io.MockPrintStream;
+import org.jsoup.internal.QuietAppendable;
+import org.junit.runner.RunWith;
+
+public class Entities_ESTestTest49 extends Entities_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test48() throws Throwable {
+        String string0 = Entities.escape("/*<![CDATA[*/\n");
+        assertEquals("/*&lt;![CDATA[*/\n", string0);
+    }
+}
