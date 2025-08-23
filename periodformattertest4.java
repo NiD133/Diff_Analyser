e610f49a43707e957e07bdb05c253305081bf385
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:32:19 +0800

    gemini processedManualTestCases periodformattertest4.java

--- periodformattertest4.java
+++ periodformattertest4.java
@@ -1,90 +1,64 @@
 package org.joda.time.format;
 
-import java.io.CharArrayWriter;
 import java.util.Locale;
-import java.util.TimeZone;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
-import org.joda.time.Chronology;
-import org.joda.time.DateTimeConstants;
-import org.joda.time.DateTimeUtils;
-import org.joda.time.DateTimeZone;
-import org.joda.time.MutablePeriod;
-import org.joda.time.Period;
-import org.joda.time.PeriodType;
-import org.joda.time.chrono.BuddhistChronology;
-import org.joda.time.chrono.ISOChronology;
 
-public class PeriodFormatterTestTest4 extends TestCase {
+/**
+ * Tests the locale-related functionality of PeriodFormatter.
+ */
+public class PeriodFormatterLocaleTest extends TestCase {
 
-    private static final DateTimeZone UTC = DateTimeZone.UTC;
+    private static final PeriodFormatter BASE_FORMATTER = ISOPeriodFormat.standard();
 
-    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
-
-    private static final DateTimeZone LONDON = DateTimeZone.forID("Europe/London");
-
-    private static final DateTimeZone TOKYO = DateTimeZone.forID("Asia/Tokyo");
-
-    private static final DateTimeZone NEWYORK = DateTimeZone.forID("America/New_York");
-
-    private static final Chronology ISO_UTC = ISOChronology.getInstanceUTC();
-
-    private static final Chronology ISO_PARIS = ISOChronology.getInstance(PARIS);
-
-    private static final Chronology BUDDHIST_PARIS = BuddhistChronology.getInstance(PARIS);
-
-    long y2002days = 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365;
-
-    // 2002-06-09
-    private long TEST_TIME_NOW = (y2002days + 31L + 28L + 31L + 30L + 31L + 9L - 1L) * DateTimeConstants.MILLIS_PER_DAY;
-
-    private DateTimeZone originalDateTimeZone = null;
-
-    private TimeZone originalTimeZone = null;
-
-    private Locale originalLocale = null;
-
-    private PeriodFormatter f = null;
-
-    public static void main(String[] args) {
-        junit.textui.TestRunner.run(suite());
+    public void testGetLocale_isInitiallyNull() {
+        // A default formatter should not have a specific locale.
+        assertNull(BASE_FORMATTER.getLocale());
     }
 
-    public static TestSuite suite() {
-        return new TestSuite(TestPeriodFormatter.class);
+    public void testWithLocale_returnsNewFormatterWithSpecifiedLocale() {
+        // Arrange
+        assertNull("Pre-condition: base formatter should have no locale", BASE_FORMATTER.getLocale());
+
+        // Act
+        PeriodFormatter frenchFormatter = BASE_FORMATTER.withLocale(Locale.FRENCH);
+
+        // Assert
+        assertEquals(Locale.FRENCH, frenchFormatter.getLocale());
     }
 
-    @Override
-    protected void setUp() throws Exception {
-        DateTimeUtils.setCurrentMillisFixed(TEST_TIME_NOW);
-        originalDateTimeZone = DateTimeZone.getDefault();
-        originalTimeZone = TimeZone.getDefault();
-        originalLocale = Locale.getDefault();
-        DateTimeZone.setDefault(LONDON);
-        TimeZone.setDefault(TimeZone.getTimeZone("Europe/London"));
-        Locale.setDefault(Locale.UK);
-        f = ISOPeriodFormat.standard();
+    public void testWithLocale_doesNotMutateOriginalFormatter() {
+        // Arrange
+        PeriodFormatter originalFormatter = ISOPeriodFormat.standard();
+
+        // Act: The result of withLocale is intentionally ignored.
+        originalFormatter.withLocale(Locale.FRENCH);
+
+        // Assert: The original formatter remains unchanged, proving immutability.
+        assertNull(originalFormatter.getLocale());
     }
 
-    @Override
-    protected void tearDown() throws Exception {
-        DateTimeUtils.setCurrentMillisSystem();
-        DateTimeZone.setDefault(originalDateTimeZone);
-        TimeZone.setDefault(originalTimeZone);
-        Locale.setDefault(originalLocale);
-        originalDateTimeZone = null;
-        originalTimeZone = null;
-        originalLocale = null;
-        f = null;
+    public void testWithLocale_returnsSameInstanceIfLocaleIsUnchanged() {
+        // Arrange
+        PeriodFormatter frenchFormatter = BASE_FORMATTER.withLocale(Locale.FRENCH);
+        PeriodFormatter nullLocaleFormatter = ISOPeriodFormat.standard();
+
+        // Act & Assert: Calling withLocale with the same locale should return the same instance.
+        assertSame("Should return same instance for the same non-null locale",
+                frenchFormatter, frenchFormatter.withLocale(Locale.FRENCH));
+        
+        assertSame("Should return same instance for a null locale if already null",
+                nullLocaleFormatter, nullLocaleFormatter.withLocale(null));
     }
 
-    //-----------------------------------------------------------------------
-    public void testWithGetLocaleMethods() {
-        PeriodFormatter f2 = f.withLocale(Locale.FRENCH);
-        assertEquals(Locale.FRENCH, f2.getLocale());
-        assertSame(f2, f2.withLocale(Locale.FRENCH));
-        f2 = f.withLocale(null);
-        assertEquals(null, f2.getLocale());
-        assertSame(f2, f2.withLocale(null));
+    public void testWithLocale_canBeSetToNullToClearLocale() {
+        // Arrange
+        PeriodFormatter frenchFormatter = BASE_FORMATTER.withLocale(Locale.FRENCH);
+        assertEquals("Pre-condition: formatter should have French locale", Locale.FRENCH, frenchFormatter.getLocale());
+
+        // Act
+        PeriodFormatter clearedFormatter = frenchFormatter.withLocale(null);
+
+        // Assert
+        assertNull("Locale should be cleared to null", clearedFormatter.getLocale());
     }
 }
