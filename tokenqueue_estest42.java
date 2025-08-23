5e565dac0d04917d8c6d8b95cbda3cdb54542d70
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:22:32 +0800

    gemini processedAutoTestCases tokenqueue_estest42.java

--- tokenqueue_estest42.java
+++ tokenqueue_estest42.java
@@ -1,25 +1,18 @@
 package org.jsoup.parser;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class TokenQueue_ESTestTest42 extends TokenQueue_ESTest_scaffolding {
+/**
+ * Test suite for the {@link TokenQueue} class.
+ */
+public class TokenQueueTest {
 
-    @Test(timeout = 4000)
-    public void test41() throws Throwable {
-        TokenQueue tokenQueue0 = null;
-        try {
-            tokenQueue0 = new TokenQueue((String) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("java.io.StringReader", e);
-        }
+    /**
+     * Verifies that the TokenQueue constructor throws a NullPointerException
+     * when initialized with a null input string, as this is an invalid state.
+     */
+    @Test(expected = NullPointerException.class)
+    public void constructorShouldThrowNullPointerExceptionForNullInput() {
+        new TokenQueue(null);
     }
 }
