ac1cb63038fdddd40672b3bc51ea41f6328d38f7
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:00:41 +0800

    gemini processedAutoTestCases linkedtreemap_estest60.java

--- linkedtreemap_estest60.java
+++ linkedtreemap_estest60.java
@@ -2,30 +2,37 @@ package com.google.gson.internal;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.AbstractMap;
-import java.util.Comparator;
-import java.util.Map;
-import java.util.Set;
-import java.util.function.BiFunction;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
 
+/**
+ * This class contains the refactored test case.
+ * The original test was part of a larger, generated test suite (LinkedTreeMap_ESTestTest60).
+ * This version is functionally identical but has been improved for human readability.
+ */
 public class LinkedTreeMap_ESTestTest60 extends LinkedTreeMap_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test59() throws Throwable {
-        Integer integer0 = new Integer((-2139));
-        LinkedTreeMap<Integer, Integer> linkedTreeMap0 = new LinkedTreeMap<Integer, Integer>();
-        linkedTreeMap0.putIfAbsent(integer0, integer0);
-        Integer integer1 = new Integer((-1109));
-        Integer integer2 = new Integer((-36));
-        linkedTreeMap0.putIfAbsent(integer2, (Integer) null);
-        LinkedTreeMap.Node<Integer, Integer> linkedTreeMap_Node0 = linkedTreeMap0.find(integer1, true);
-        linkedTreeMap0.removeInternal(linkedTreeMap_Node0, true);
-        assertEquals(2, linkedTreeMap0.size());
+    /**
+     * Tests that a node created by {@link LinkedTreeMap#find(Object, boolean)}
+     * with create=true can be successfully removed by {@link LinkedTreeMap#removeInternal(Node, boolean)}.
+     */
+    @Test
+    public void removeInternal_removesNodeCreatedByFindWithCreateFlag() {
+        // Arrange
+        LinkedTreeMap<Integer, Integer> map = new LinkedTreeMap<>();
+        map.put(-2139, -2139);
+        map.put(-36, null);
+        assertEquals("Map should contain 2 elements initially", 2, map.size());
+
+        Integer keyToCreateAndRemove = -1109;
+
+        // Act
+        // 1. Call find() with create=true, which adds a new node for a non-existent key.
+        LinkedTreeMap.Node<Integer, Integer> nodeCreatedByFind = map.find(keyToCreateAndRemove, true);
+        assertEquals("Size should be 3 after find() creates a new node", 3, map.size());
+
+        // 2. Remove the newly created node using the internal remove method.
+        map.removeInternal(nodeCreatedByFind, true);
+
+        // Assert
+        assertEquals("Size should return to 2 after removing the node", 2, map.size());
     }
 }
