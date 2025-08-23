88ae1e9887f3eb851484dd35032286052fbf68cb
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:38:46 +0800

    Original processedAutoTestCases basetypehandler_estest18.java

---
+++ basetypehandler_estest18.java
@@ -0,0 +1,24 @@
+package org.apache.ibatis.type;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.time.YearMonth;
+import org.apache.ibatis.session.Configuration;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class BaseTypeHandler_ESTestTest18 extends BaseTypeHandler_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test17() throws Throwable {
+        ClobTypeHandler clobTypeHandler0 = new ClobTypeHandler();
+        clobTypeHandler0.setConfiguration((Configuration) null);
+    }
+}
