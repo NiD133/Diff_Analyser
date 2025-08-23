cfc8a15a435f71ed89de46f159fd8fa545c35477
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:29:14 +0800

    gemini processedAutoTestCases xnode_estest82.java

--- xnode_estest82.java
+++ xnode_estest82.java
@@ -1,31 +1,37 @@
 package org.apache.ibatis.parsing;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.List;
-import java.util.Locale;
+import static org.junit.Assert.assertEquals;
+
 import java.util.Properties;
-import java.util.function.Supplier;
 import javax.imageio.metadata.IIOMetadataNode;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
-import org.w3c.dom.Document;
+import org.junit.Test;
 import org.w3c.dom.Node;
-import org.xml.sax.ext.DefaultHandler2;
 
-public class XNode_ESTestTest82 extends XNode_ESTest_scaffolding {
+/**
+ * Test suite for the {@link XNode} class.
+ */
+public class XNodeTest {
 
-    @Test(timeout = 4000)
-    public void test081() throws Throwable {
-        Properties properties0 = new Properties();
-        IIOMetadataNode iIOMetadataNode0 = new IIOMetadataNode();
-        XNode xNode0 = new XNode((XPathParser) null, iIOMetadataNode0, properties0);
-        Long long0 = new Long((-1669L));
-        Long long1 = xNode0.getLongBody(long0);
-        assertEquals((-1669L), (long) long1);
+    /**
+     * Verifies that getLongBody(defaultValue) returns the provided default value
+     * when the XNode's body is null.
+     * <p>
+     * An XNode's body is considered null if its underlying DOM Node is empty,
+     * meaning it contains no text or CDATA content.
+     */
+    @Test
+    public void getLongBodyShouldReturnDefaultValueWhenBodyIsNull() {
+        // Arrange
+        // Create an XNode from an empty DOM node, which results in a null body.
+        Node emptyNode = new IIOMetadataNode();
+        XNode xNode = new XNode(null, emptyNode, new Properties());
+        Long defaultValue = -1669L;
+
+        // Act
+        Long actualValue = xNode.getLongBody(defaultValue);
+
+        // Assert
+        assertEquals("getLongBody should return the default value for a null body",
+                defaultValue, actualValue);
     }
 }
