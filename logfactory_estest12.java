fe0e0d41076375eab9c933790bd4385ec4945292
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:01:27 +0800

    Original processedAutoTestCases logfactory_estest12.java

---
+++ logfactory_estest12.java
@@ -0,0 +1,17 @@
+package org.apache.ibatis.logging;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.apache.ibatis.logging.commons.JakartaCommonsLoggingImpl;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class LogFactory_ESTestTest12 extends LogFactory_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test11() throws Throwable {
+        LogFactory.useNoLogging();
+    }
+}
