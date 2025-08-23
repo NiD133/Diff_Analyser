076eded6b3d95765496f173446c8abce088c10df
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:50:04 +0800

    Original processedAutoTestCases extrafieldutils_estest41.java

---
+++ extrafieldutils_estest41.java
@@ -0,0 +1,17 @@
+package org.apache.commons.compress.archivers.zip;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.zip.ZipException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class ExtraFieldUtils_ESTestTest41 extends ExtraFieldUtils_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test40() throws Throwable {
+        ExtraFieldUtils extraFieldUtils0 = new ExtraFieldUtils();
+    }
+}
