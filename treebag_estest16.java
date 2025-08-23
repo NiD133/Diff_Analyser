20768799415be6670d6d876576a28ad0be98b88e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:23:49 +0800

    Original processedAutoTestCases treebag_estest16.java

---
+++ treebag_estest16.java
@@ -0,0 +1,37 @@
+package org.apache.commons.collections4.bag;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.EnumSet;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.Locale;
+import java.util.NoSuchElementException;
+import java.util.SortedMap;
+import org.apache.commons.collections4.Predicate;
+import org.apache.commons.collections4.functors.AnyPredicate;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class TreeBag_ESTestTest16 extends TreeBag_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test15() throws Throwable {
+        TreeBag<AbstractMapBag.MutableInteger> treeBag0 = null;
+        try {
+            treeBag0 = new TreeBag<AbstractMapBag.MutableInteger>((Collection<? extends AbstractMapBag.MutableInteger>) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.apache.commons.collections4.bag.AbstractMapBag", e);
+        }
+    }
+}
