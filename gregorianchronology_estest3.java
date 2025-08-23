07e3503716d465c2002b37ee8b1d7964b08462aa
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:52:10 +0800

    gemini processedAutoTestCases gregorianchronology_estest3.java

--- gregorianchronology_estest3.java
+++ gregorianchronology_estest3.java
@@ -1,24 +1,34 @@
 package org.joda.time.chrono;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.TimeZone;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.joda.time.Chronology;
-import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
-import org.joda.time.LocalDateTime;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit tests for the {@link GregorianChronology} class, focusing on leap year calculations.
+ */
 public class GregorianChronology_ESTestTest3 extends GregorianChronology_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test02() throws Throwable {
-        DateTimeZone dateTimeZone0 = DateTimeZone.getDefault();
-        GregorianChronology gregorianChronology0 = GregorianChronology.getInstance(dateTimeZone0);
-        boolean boolean0 = gregorianChronology0.isLeapYear(0);
-        assertTrue(boolean0);
+    /**
+     * Tests that the proleptic Gregorian calendar correctly identifies year 0 as a leap year.
+     * <p>
+     * According to the Gregorian rules, a year is a leap year if it is divisible by 4,
+     * unless it is divisible by 100 but not by 400. Year 0 is divisible by 400,
+     * and therefore it is a leap year.
+     */
+    @Test
+    public void isLeapYear_shouldReturnTrue_forYearZero() {
+        // Arrange
+        // The isLeapYear calculation is independent of the time zone, so we can use a fixed
+        // zone like UTC to make the test more deterministic.
+        GregorianChronology gregorianChronology = GregorianChronology.getInstance(DateTimeZone.UTC);
+        int year = 0;
+
+        // Act
+        boolean isLeap = gregorianChronology.isLeapYear(year);
+
+        // Assert
+        assertTrue("Year 0 should be identified as a leap year.", isLeap);
     }
 }
