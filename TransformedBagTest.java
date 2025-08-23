c9862f01788f078c3f3c88080ece6fe09d2e1761
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 05:35:27 +0800

    Original manualTestSuite TransformedBagTest.java

--- TransformedBagTest.java
+++ TransformedBagTest.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.commons.collections4.bag;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -10,7 +26,8 @@ import org.apache.commons.collections4.collection.TransformedCollectionTest;
 import org.junit.jupiter.api.Test;
 
 /**
- * Test suite for the {@link TransformedBag} implementation.
+ * Extension of {@link AbstractBagTest} for exercising the {@link TransformedBag}
+ * implementation.
  */
 public class TransformedBagTest<T> extends AbstractBagTest<T> {
 
@@ -33,42 +50,48 @@ public class TransformedBagTest<T> extends AbstractBagTest<T> {
 
     @Test
     @SuppressWarnings("unchecked")
-    void testEmptyTransformedBag() {
-        Bag<T> transformedBag = TransformedBag.transformingBag(new HashBag<>(),
+    void testTransformedBag() {
+        //T had better be Object!
+        final Bag<T> bag = TransformedBag.transformingBag(new HashBag<>(),
                 (Transformer<T, T>) TransformedCollectionTest.STRING_TO_INTEGER_TRANSFORMER);
-        assertTrue(transformedBag.isEmpty());
-
-        Object[] elements = {"1", "3", "5", "7", "2", "4", "6"};
-        for (int i = 0; i < elements.length; i++) {
-            transformedBag.add((T) elements[i]);
-            assertEquals(i + 1, transformedBag.size());
-            assertTrue(transformedBag.contains(Integer.valueOf((String) elements[i])));
-            assertFalse(transformedBag.contains(elements[i]));
+        assertTrue(bag.isEmpty());
+        final Object[] els = {"1", "3", "5", "7", "2", "4", "6"};
+        for (int i = 0; i < els.length; i++) {
+            bag.add((T) els[i]);
+            assertEquals(i + 1, bag.size());
+            assertTrue(bag.contains(Integer.valueOf((String) els[i])));
+            assertFalse(bag.contains(els[i]));
         }
 
-        assertFalse(transformedBag.remove(elements[0]));
-        assertTrue(transformedBag.remove(Integer.valueOf((String) elements[0])));
+        assertFalse(bag.remove(els[0]));
+        assertTrue(bag.remove(Integer.valueOf((String) els[0])));
     }
 
     @Test
     @SuppressWarnings("unchecked")
-    void testDecorateAndTransformExistingBag() {
-        Bag<T> originalBag = new HashBag<>();
-        Object[] elements = {"1", "3", "5", "7", "2", "4", "6"};
-        for (Object element : elements) {
-            originalBag.add((T) element);
+    void testTransformedBag_decorateTransform() {
+        final Bag<T> originalBag = new HashBag<>();
+        final Object[] els = {"1", "3", "5", "7", "2", "4", "6"};
+        for (final Object el : els) {
+            originalBag.add((T) el);
         }
-
-        Bag<T> transformedBag = TransformedBag.transformedBag(originalBag,
+        final Bag<T> bag = TransformedBag.transformedBag(originalBag,
                 (Transformer<T, T>) TransformedCollectionTest.STRING_TO_INTEGER_TRANSFORMER);
-        assertEquals(elements.length, transformedBag.size());
-
-        for (Object element : elements) {
-            assertTrue(transformedBag.contains(Integer.valueOf((String) element)));
-            assertFalse(transformedBag.contains(element));
+        assertEquals(els.length, bag.size());
+        for (final Object el : els) {
+            assertTrue(bag.contains(Integer.valueOf((String) el)));
+            assertFalse(bag.contains(el));
         }
 
-        assertFalse(transformedBag.remove(elements[0]));
-        assertTrue(transformedBag.remove(Integer.valueOf((String) elements[0])));
+        assertFalse(bag.remove(els[0]));
+        assertTrue(bag.remove(Integer.valueOf((String) els[0])));
     }
+
+//    void testCreate() throws Exception {
+//        Bag<T> bag = makeObject();
+//        writeExternalFormToDisk((java.io.Serializable) bag, "src/test/resources/data/test/TransformedBag.emptyCollection.version4.obj");
+//        bag = makeFullCollection();
+//        writeExternalFormToDisk((java.io.Serializable) bag, "src/test/resources/data/test/TransformedBag.fullCollection.version4.obj");
+//    }
+
 }
