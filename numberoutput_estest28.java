165b9efa3a4c601c7d7c2255e8abf9597b9209a7
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:05:03 +0800

    gemini processedAutoTestCases numberoutput_estest28.java

--- numberoutput_estest28.java
+++ numberoutput_estest28.java
@@ -1,26 +1,34 @@
 package com.fasterxml.jackson.core.io;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class NumberOutput_ESTestTest28 extends NumberOutput_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link NumberOutput} class, focusing on buffer boundary conditions.
+ */
+public class NumberOutputTest {
 
-    @Test(timeout = 4000)
-    public void test27() throws Throwable {
-        byte[] byteArray0 = new byte[3];
-        // Undeclared exception!
-        try {
-            NumberOutput.outputLong((-424L), byteArray0, 0);
-            fail("Expecting exception: ArrayIndexOutOfBoundsException");
-        } catch (ArrayIndexOutOfBoundsException e) {
-            //
-            // 3
-            //
-            verifyException("com.fasterxml.jackson.core.io.NumberOutput", e);
-        }
+    /**
+     * Verifies that {@code outputLong} throws an {@link ArrayIndexOutOfBoundsException}
+     * when the destination byte array is too small to hold the complete string
+     * representation of the number.
+     */
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void outputLongShouldThrowExceptionForInsufficientBuffer() {
+        // Arrange:
+        // The number -424 requires 4 bytes for its string representation ("-424").
+        long number = -424L;
+        // We provide a buffer that is one byte too short to hold the result.
+        byte[] buffer = new byte[3];
+        int offset = 0;
+
+        // Act:
+        // This call should attempt to write the 4th byte into a 3-byte array,
+        // which will cause an exception.
+        NumberOutput.outputLong(number, buffer, offset);
+
+        // Assert:
+        // The test succeeds if an ArrayIndexOutOfBoundsException is thrown,
+        // as specified by the @Test annotation. If no exception is thrown,
+        // the test will automatically fail.
     }
 }
