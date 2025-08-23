f0051aa2620da288f5580a1981e721eb92b990cc
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:39:38 +0800

    gemini processedAutoTestCases boundedreader_estest11.java

--- boundedreader_estest11.java
+++ boundedreader_estest11.java
@@ -1,31 +1,37 @@
 package org.apache.commons.io.input;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
+import static org.junit.Assert.assertThrows;
+
 import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class BoundedReader_ESTestTest11 extends BoundedReader_ESTest_scaffolding {
+/**
+ * Unit tests for {@link BoundedReader}.
+ */
+public class BoundedReaderTest {
 
-    @Test(timeout = 4000)
-    public void test10() throws Throwable {
-        StringReader stringReader0 = new StringReader("4s");
-        BoundedReader boundedReader0 = new BoundedReader(stringReader0, 179);
-        char[] charArray0 = new char[0];
-        // Undeclared exception!
-        try {
-            boundedReader0.read(charArray0, 179, 179);
-            fail("Expecting exception: ArrayIndexOutOfBoundsException");
-        } catch (ArrayIndexOutOfBoundsException e) {
-            //
-            // 179
-            //
-            verifyException("org.apache.commons.io.input.BoundedReader", e);
-        }
+    /**
+     * Tests that the read(char[], int, int) method propagates an IndexOutOfBoundsException
+     * from the underlying reader when the offset is invalid for the given buffer.
+     */
+    @Test
+    public void readWithOffsetOutOfBoundsShouldPropagateException() throws IOException {
+        // Arrange
+        final Reader underlyingReader = new StringReader("This is the test data.");
+        final BoundedReader boundedReader = new BoundedReader(underlyingReader, 100);
+        
+        final char[] buffer = new char[10];
+        final int invalidOffset = 20; // An offset clearly outside the buffer's bounds
+        final int length = 1;
+
+        // Act & Assert
+        // The BoundedReader should delegate the read call, and the underlying reader
+        // will throw an exception because the offset is out of bounds.
+        assertThrows(
+            IndexOutOfBoundsException.class,
+            () -> boundedReader.read(buffer, invalidOffset, length)
+        );
     }
 }
