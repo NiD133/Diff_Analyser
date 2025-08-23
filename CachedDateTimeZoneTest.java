b2f744c9e4ecf6fd7f73ccae41edc36d77ea4f09
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 05:32:39 +0800

    gpt5 manualTestSuite CachedDateTimeZoneTest.java

--- CachedDateTimeZoneTest.java
+++ CachedDateTimeZoneTest.java
@@ -1,84 +1,90 @@
-/*
- *  Copyright 2001-2012 Stephen Colebourne
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
 package org.joda.time.tz;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import java.io.Serializable;
 
 import org.joda.time.DateTimeZone;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 /**
- * Test cases for FixedDateTimeZone.
+ * Readable tests for CachedDateTimeZone.
  *
- * @author Stephen Colebourne
+ * What these tests assert:
+ * - forZone returns the same cached instance when called repeatedly for the same zone ID
+ * - Serialization round-trip preserves equality
+ * - getUncachedZone returns the original wrapped zone
  */
-public class TestCachedDateTimeZone extends TestCase {
+public class CachedDateTimeZoneTest {
 
-    public static void main(String[] args) {
-        junit.textui.TestRunner.run(suite());
-    }
+    private static final String TZ_EUROPE_PARIS = "Europe/Paris";
 
-    public static TestSuite suite() {
-        return new TestSuite(TestCachedDateTimeZone.class);
-    }
+    private DateTimeZone originalDefault;
 
-    private DateTimeZone originalDateTimeZone = null;
-
-    public TestCachedDateTimeZone(String name) {
-        super(name);
-    }
-
-    @Override
-    protected void setUp() throws Exception {
-        originalDateTimeZone = DateTimeZone.getDefault();
+    @Before
+    public void setUp() {
+        // Ensure deterministic behavior across environments
+        originalDefault = DateTimeZone.getDefault();
         DateTimeZone.setDefault(DateTimeZone.UTC);
     }
 
-    @Override
-    protected void tearDown() throws Exception {
-        DateTimeZone.setDefault(originalDateTimeZone);
+    @After
+    public void tearDown() {
+        DateTimeZone.setDefault(originalDefault);
     }
 
-    public void test_caching() throws Exception {
-        CachedDateTimeZone zone1 = CachedDateTimeZone.forZone(DateTimeZone.forID("Europe/Paris"));
-        CachedDateTimeZone zone2 = CachedDateTimeZone.forZone(DateTimeZone.forID("Europe/Paris"));
-        assertSame(zone1, zone2);
+    @Test
+    public void forZoneReturnsSameInstanceForSameZoneId() {
+        CachedDateTimeZone zone1 = CachedDateTimeZone.forZone(DateTimeZone.forID(TZ_EUROPE_PARIS));
+        CachedDateTimeZone zone2 = CachedDateTimeZone.forZone(DateTimeZone.forID(TZ_EUROPE_PARIS));
+
+        assertSame(
+                "CachedDateTimeZone.forZone should return the same cached instance for the same underlying zone",
+                zone1, zone2
+        );
     }
 
-    //-----------------------------------------------------------------------
-    public void testSerialization() throws Exception {
-        CachedDateTimeZone test = CachedDateTimeZone.forZone(DateTimeZone.forID("Europe/Paris"));
-        
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        ObjectOutputStream oos = new ObjectOutputStream(baos);
-        oos.writeObject(test);
-        oos.close();
-        byte[] bytes = baos.toByteArray();
-        
-        ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
-        ObjectInputStream ois = new ObjectInputStream(bais);
-        CachedDateTimeZone result = (CachedDateTimeZone) ois.readObject();
-        ois.close();
-        
-        assertEquals(test, result);
+    @Test
+    public void serializationRoundTripPreservesEquality() throws Exception {
+        CachedDateTimeZone original = CachedDateTimeZone.forZone(DateTimeZone.forID(TZ_EUROPE_PARIS));
+
+        CachedDateTimeZone deserialized = serializeAndDeserialize(original);
+
+        assertEquals(
+                "A serialized/deserialized CachedDateTimeZone should be equal to the original",
+                original, deserialized
+        );
     }
 
+    @Test
+    public void getUncachedZoneReturnsOriginalZone() {
+        DateTimeZone base = DateTimeZone.forID(TZ_EUROPE_PARIS);
+        CachedDateTimeZone cached = CachedDateTimeZone.forZone(base);
+
+        assertSame("getUncachedZone should return the wrapped zone", base, cached.getUncachedZone());
+    }
+
+    private static <T extends Serializable> T serializeAndDeserialize(T obj) throws Exception {
+        byte[] bytes;
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream();
+             ObjectOutputStream oos = new ObjectOutputStream(baos)) {
+            oos.writeObject(obj);
+            oos.flush();
+            bytes = baos.toByteArray();
+        }
+
+        try (ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
+             ObjectInputStream ois = new ObjectInputStream(bais)) {
+            @SuppressWarnings("unchecked")
+            T result = (T) ois.readObject();
+            return result;
+        }
+    }
 }
