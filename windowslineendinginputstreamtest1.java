e4c4e708d65c749b3bc202dd75b5bf3d2481e351
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:35:57 +0800

    Original processedManualTestCases windowslineendinginputstreamtest1.java

---
+++ windowslineendinginputstreamtest1.java
@@ -0,0 +1,77 @@
+package org.apache.commons.io.input;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class WindowsLineEndingInputStreamTestTest1 {
+
+    private String roundtripReadByte(final String msg) throws IOException {
+        return roundtripReadByte(msg, true);
+    }
+
+    private String roundtripReadByte(final String msg, final boolean ensure) throws IOException {
+        // read(byte[])
+        try (WindowsLineEndingInputStream lf = new WindowsLineEndingInputStream(CharSequenceInputStream.builder().setCharSequence(msg).setCharset(StandardCharsets.UTF_8).get(), ensure)) {
+            final byte[] buf = new byte[100];
+            int i = 0;
+            while (i < buf.length) {
+                final int read = lf.read();
+                if (read < 0) {
+                    break;
+                }
+                buf[i++] = (byte) read;
+            }
+            return new String(buf, 0, i, StandardCharsets.UTF_8);
+        }
+    }
+
+    private String roundtripReadByteArray(final String msg) throws IOException {
+        return roundtripReadByteArray(msg, true);
+    }
+
+    private String roundtripReadByteArray(final String msg, final boolean ensure) throws IOException {
+        // read(byte[])
+        try (WindowsLineEndingInputStream lf = new WindowsLineEndingInputStream(CharSequenceInputStream.builder().setCharSequence(msg).setCharset(StandardCharsets.UTF_8).get(), ensure)) {
+            final byte[] buf = new byte[100];
+            final int read = lf.read(buf);
+            return new String(buf, 0, read, StandardCharsets.UTF_8);
+        }
+    }
+
+    private String roundtripReadByteArrayIndex(final String msg) throws IOException {
+        return roundtripReadByteArrayIndex(msg, true);
+    }
+
+    private String roundtripReadByteArrayIndex(final String msg, final boolean ensure) throws IOException {
+        // read(byte[])
+        try (WindowsLineEndingInputStream lf = new WindowsLineEndingInputStream(CharSequenceInputStream.builder().setCharSequence(msg).setCharset(StandardCharsets.UTF_8).get(), ensure)) {
+            final byte[] buf = new byte[100];
+            final int read = lf.read(buf, 0, 100);
+            return new String(buf, 0, read, StandardCharsets.UTF_8);
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(booleans = { false, true })
+    void testMark(final boolean ensureLineFeedAtEndOfFile) {
+        assertThrows(UnsupportedOperationException.class, () -> new WindowsLineEndingInputStream(new NullInputStream(), true).mark(1));
+    }
+
+    @SuppressWarnings("resource")
+    @ParameterizedTest
+    @ValueSource(booleans = { false, true })
+    void testMarkSupported(final boolean ensureLineFeedAtEndOfFile) {
+        assertFalse(new WindowsLineEndingInputStream(new NullInputStream(), true).markSupported());
+    }
+
+    @Test
+    void testInTheMiddleOfTheLine_Byte() throws Exception {
+        assertEquals("a\r\nbc\r\n", roundtripReadByte("a\r\nbc"));
+    }
+}
