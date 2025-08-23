96cd613c09ff06393d986504ed4948af6efaf2e7
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:41:13 +0800

    gemini processedAutoTestCases cachebuilder_estest5.java

--- cachebuilder_estest5.java
+++ cachebuilder_estest5.java
@@ -1,34 +1,38 @@
 package org.apache.ibatis.mapping;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
+
 import java.util.Properties;
-import org.apache.ibatis.cache.Cache;
-import org.apache.ibatis.cache.decorators.BlockingCache;
-import org.apache.ibatis.cache.decorators.SynchronizedCache;
-import org.apache.ibatis.cache.impl.PerpetualCache;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class CacheBuilder_ESTestTest5 extends CacheBuilder_ESTest_scaffolding {
+/**
+ * Test suite for {@link CacheBuilder}.
+ */
+public class CacheBuilderTest {
 
-    @Test(timeout = 4000)
-    public void test04() throws Throwable {
-        CacheBuilder cacheBuilder0 = new CacheBuilder("F");
-        Properties properties0 = new Properties();
-        properties0.put(cacheBuilder0, "F");
-        CacheBuilder cacheBuilder1 = cacheBuilder0.properties(properties0);
-        // Undeclared exception!
-        try {
-            cacheBuilder1.build();
-            fail("Expecting exception: ClassCastException");
-        } catch (ClassCastException e) {
-            //
-            // class org.apache.ibatis.mapping.CacheBuilder cannot be cast to class java.lang.String (org.apache.ibatis.mapping.CacheBuilder is in unnamed module of loader org.evosuite.instrumentation.InstrumentingClassLoader @5d8eae10; java.lang.String is in module java.base of loader 'bootstrap')
-            //
-            verifyException("org.apache.ibatis.mapping.CacheBuilder", e);
-        }
+    /**
+     * The CacheBuilder's build() method processes custom properties to configure the cache.
+     * It expects property keys to be Strings. This test verifies that if a non-String key
+     * is provided in the properties, the build() method correctly throws a
+     * ClassCastException.
+     */
+    @Test(expected = ClassCastException.class)
+    public void buildShouldThrowClassCastExceptionWhenPropertyKeyIsNotString() {
+        // Arrange: Create a CacheBuilder and provide it with a Properties object
+        // that contains a non-String key.
+        CacheBuilder cacheBuilder = new CacheBuilder("test-cache");
+        Properties invalidProperties = new Properties();
+
+        // The Properties object allows non-String keys, but CacheBuilder expects them to be Strings.
+        // We use a simple Object instance as the key to trigger the failure condition.
+        invalidProperties.put(new Object(), "any-value");
+
+        cacheBuilder.properties(invalidProperties);
+
+        // Act: Attempt to build the cache. This action is expected to throw the exception
+        // when it tries to cast the property key to a String.
+        cacheBuilder.build();
+
+        // Assert: The test will pass if a ClassCastException is thrown, as declared
+        // by the @Test(expected = ...) annotation. No explicit assert is needed.
     }
 }
