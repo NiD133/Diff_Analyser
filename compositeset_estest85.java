b6f8f469c4ac05be2a9211c7a988184b227d4567
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:45:29 +0800

    gemini processedAutoTestCases compositeset_estest85.java

--- compositeset_estest85.java
+++ compositeset_estest85.java
@@ -1,55 +1,28 @@
 package org.apache.commons.collections4.set;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.LinkedHashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Set;
-import java.util.Spliterator;
-import java.util.function.Consumer;
-import java.util.stream.Stream;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.Equator;
-import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ChainedClosure;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.DefaultEquator;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionPredicate;
-import org.apache.commons.collections4.functors.FalsePredicate;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.IfClosure;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NotNullPredicate;
-import org.apache.commons.collections4.functors.NotPredicate;
-import org.apache.commons.collections4.functors.NullIsExceptionPredicate;
-import org.apache.commons.collections4.functors.NullIsTruePredicate;
-import org.apache.commons.collections4.functors.OnePredicate;
-import org.apache.commons.collections4.functors.OrPredicate;
-import org.apache.commons.collections4.functors.TransformerClosure;
-import org.apache.commons.collections4.functors.TruePredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.apache.commons.collections4.functors.WhileClosure;
-import org.apache.commons.collections4.iterators.IteratorChain;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertNull;
 
+/**
+ * Contains tests for the {@link CompositeSet} class.
+ * This specific test focuses on the behavior of the getMutator() method.
+ */
+// The original class name is kept for context, but a name like 'CompositeSetTest' would be more standard.
 public class CompositeSet_ESTestTest85 extends CompositeSet_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test84() throws Throwable {
-        CompositeSet<Transformer<Boolean, Boolean>> compositeSet0 = new CompositeSet<Transformer<Boolean, Boolean>>();
-        CompositeSet.SetMutator<Transformer<Boolean, Boolean>> compositeSet_SetMutator0 = compositeSet0.getMutator();
-        assertNull(compositeSet_SetMutator0);
+    /**
+     * Tests that getMutator() returns null for a newly created CompositeSet
+     * that has not had a mutator explicitly set.
+     */
+    @Test
+    public void getMutatorShouldReturnNullWhenNotSet() {
+        // Arrange: Create a new CompositeSet. By default, it has no mutator.
+        final CompositeSet<String> compositeSet = new CompositeSet<>();
+
+        // Act: Retrieve the mutator from the newly created set.
+        final CompositeSet.SetMutator<String> mutator = compositeSet.getMutator();
+
+        // Assert: The retrieved mutator should be null.
+        assertNull("A new CompositeSet should have a null mutator by default", mutator);
     }
 }
