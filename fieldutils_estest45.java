2e0329f9098722511bdc5a299b6ff14ad4eec696
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:50:25 +0800

    gemini processedAutoTestCases fieldutils_estest45.java

--- fieldutils_estest45.java
+++ fieldutils_estest45.java
@@ -1,23 +1,38 @@
 package org.joda.time.field;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.math.RoundingMode;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.joda.time.DateTimeField;
-import org.joda.time.DateTimeFieldType;
-import org.joda.time.DateTimeZone;
-import org.joda.time.chrono.IslamicChronology;
-import org.joda.time.chrono.ZonedChronology;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class FieldUtils_ESTestTest45 extends FieldUtils_ESTest_scaffolding {
+/**
+ * Unit tests for the utility methods in {@link FieldUtils}.
+ */
+public class FieldUtilsTest {
 
-    @Test(timeout = 4000)
-    public void test44() throws Throwable {
-        int int0 = FieldUtils.getWrappedValue(2149, 1610612735, (-1), 0);
-        assertEquals(0, int0);
+    /**
+     * Tests that getWrappedValue correctly wraps a value that exceeds the maximum
+     * boundary of the specified range. The result should circle back to the start
+     * of the range.
+     */
+    @Test
+    public void getWrappedValue_shouldWrapAroundWhenSumExceedsMaximum() {
+        // Arrange
+        // Define a simple range of [-1, 0]. The total size of this range is 2.
+        final int minValue = -1;
+        final int maxValue = 0;
+
+        // The sum of the current value and the amount to add (1 + 3 = 4) is well outside the range.
+        final int currentValue = 1;
+        final int amountToAdd = 3;
+
+        // The expected result after wrapping the value 4 within the [-1, 0] range is 0.
+        // The wrapping calculation is: ((value - minValue) % rangeSize) + minValue
+        // ((4 - (-1)) % 2) + (-1)  =>  (5 % 2) - 1  =>  1 - 1 = 0
+        final int expectedValue = 0;
+
+        // Act
+        int actualValue = FieldUtils.getWrappedValue(currentValue, amountToAdd, minValue, maxValue);
+
+        // Assert
+        assertEquals("The wrapped value should be the maximum of the range", expectedValue, actualValue);
     }
 }
