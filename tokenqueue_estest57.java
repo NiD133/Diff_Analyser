a66d1e113108803ab2131ca245cbf5aa4930b0ca
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:22:38 +0800

    gemini processedAutoTestCases tokenqueue_estest57.java

--- tokenqueue_estest57.java
+++ tokenqueue_estest57.java
@@ -1,17 +1,30 @@
 package org.jsoup.parser;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class TokenQueue_ESTestTest57 extends TokenQueue_ESTest_scaffolding {
+/**
+ * Tests for the static utility methods in {@link TokenQueue}.
+ */
+public class TokenQueueTest {
 
-    @Test(timeout = 4000)
-    public void test56() throws Throwable {
-        String string0 = TokenQueue.escapeCssIdentifier("- %");
-        assertEquals("-\\ \\%", string0);
+    /**
+     * Tests that CSS meta-characters, which have special meaning in a selector,
+     * are correctly escaped with a backslash. This test case covers a leading hyphen,
+     * a space, and a percent sign.
+     *
+     * @see <a href="https://www.w3.org/TR/cssom-1/#serialize-an-identifier">CSSOM Spec: Serialize an identifier</a>
+     */
+    @Test
+    public void escapeCssIdentifierShouldEscapeSpecialCharacters() {
+        // Arrange
+        String identifierWithSpecialChars = "- %";
+        String expectedEscapedIdentifier = "-\\ \\%";
+
+        // Act
+        String actualEscapedIdentifier = TokenQueue.escapeCssIdentifier(identifierWithSpecialChars);
+
+        // Assert
+        assertEquals(expectedEscapedIdentifier, actualEscapedIdentifier);
     }
 }
