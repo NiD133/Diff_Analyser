63eae052f597bbb11998d4d4838d0c5f2015588f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:27:59 +0800

    Original processedAutoTestCases xmltreebuilder_estest21.java

---
+++ xmltreebuilder_estest21.java
@@ -0,0 +1,40 @@
+package org.jsoup.parser;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.PipedReader;
+import java.io.PipedWriter;
+import java.io.Reader;
+import java.io.StringReader;
+import java.io.UncheckedIOException;
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.jsoup.nodes.Attributes;
+import org.jsoup.nodes.CDataNode;
+import org.jsoup.nodes.Comment;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.LeafNode;
+import org.jsoup.select.Elements;
+import org.junit.runner.RunWith;
+
+public class XmlTreeBuilder_ESTestTest21 extends XmlTreeBuilder_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test20() throws Throwable {
+        XmlTreeBuilder xmlTreeBuilder0 = new XmlTreeBuilder();
+        // Undeclared exception!
+        try {
+            xmlTreeBuilder0.parse(",Ix<T(pi#>?mbGzH", (String) null);
+            fail("Expecting exception: IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //
+            // The parameter 'baseUri' must not be null.
+            //
+            verifyException("org.jsoup.helper.Validate", e);
+        }
+    }
+}
