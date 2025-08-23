c8e83182a1c04907c202ee8c709e9d545cf1034f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:29:38 +0800

    Original processedAutoTestCases xydrawableannotation_estest32.java

---
+++ xydrawableannotation_estest32.java
@@ -0,0 +1,58 @@
+package org.jfree.chart.annotations;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.RoundRectangle2D;
+import java.awt.image.BufferedImage;
+import java.math.BigInteger;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.jfree.chart.ChartRenderingInfo;
+import org.jfree.chart.Drawable;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.CyclicNumberAxis;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.axis.ValueAxis;
+import org.jfree.chart.block.BlockContainer;
+import org.jfree.chart.legend.LegendTitle;
+import org.jfree.chart.plot.MeterPlot;
+import org.jfree.chart.plot.PlotRenderingInfo;
+import org.jfree.chart.plot.PolarAxisLocation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.AbstractRenderer;
+import org.jfree.chart.renderer.category.ScatterRenderer;
+import org.jfree.chart.renderer.xy.XYBarRenderer;
+import org.jfree.chart.title.CompositeTitle;
+import org.jfree.chart.title.DateTitle;
+import org.jfree.chart.title.ShortTextTitle;
+import org.jfree.chart.title.TextTitle;
+import org.jfree.data.general.DefaultValueDataset;
+import org.junit.runner.RunWith;
+
+public class XYDrawableAnnotation_ESTestTest32 extends XYDrawableAnnotation_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test31() throws Throwable {
+        TextTitle textTitle0 = new TextTitle("x");
+        XYDrawableAnnotation xYDrawableAnnotation0 = new XYDrawableAnnotation(738.4170062070756, 738.4170062070756, 738.4170062070756, 738.4170062070756, textTitle0);
+        xYDrawableAnnotation0.setURL("x");
+        XYPlot<PolarAxisLocation> xYPlot0 = new XYPlot<PolarAxisLocation>();
+        RoundRectangle2D.Float roundRectangle2D_Float0 = new RoundRectangle2D.Float();
+        Rectangle2D rectangle2D0 = roundRectangle2D_Float0.getBounds2D();
+        NumberAxis numberAxis0 = new NumberAxis("x");
+        ChartRenderingInfo chartRenderingInfo0 = new ChartRenderingInfo();
+        PlotRenderingInfo plotRenderingInfo0 = chartRenderingInfo0.getPlotInfo();
+        JFreeChart jFreeChart0 = new JFreeChart(xYPlot0);
+        BufferedImage bufferedImage0 = jFreeChart0.createBufferedImage(1564, 1564, (double) 0.0F, 50.70624, chartRenderingInfo0);
+        Graphics2D graphics2D0 = bufferedImage0.createGraphics();
+        xYDrawableAnnotation0.draw(graphics2D0, xYPlot0, rectangle2D0, numberAxis0, numberAxis0, 3697, plotRenderingInfo0);
+        assertEquals(1.0, xYDrawableAnnotation0.getDrawScaleFactor(), 0.01);
+        assertEquals(738.4170062070756, xYDrawableAnnotation0.getDisplayWidth(), 0.01);
+        assertEquals(738.4170062070756, xYDrawableAnnotation0.getX(), 0.01);
+        assertEquals(738.4170062070756, xYDrawableAnnotation0.getDisplayHeight(), 0.01);
+    }
+}
