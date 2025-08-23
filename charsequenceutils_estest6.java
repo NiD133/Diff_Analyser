36e137fe36fdbea71c70c3309565e345ff7df5d4
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:42:36 +0800

    gemini processedAutoTestCases charsequenceutils_estest6.java

--- charsequenceutils_estest6.java
+++ charsequenceutils_estest6.java
@@ -1,21 +1,32 @@
 package org.apache.commons.lang3;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.nio.CharBuffer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class CharSequenceUtils_ESTestTest6 extends CharSequenceUtils_ESTest_scaffolding {
+/**
+ * This test case has been improved for better understandability.
+ */
+public class CharSequenceUtilsImprovedTest {
 
-    @Test(timeout = 4000)
-    public void test05() throws Throwable {
-        StringBuffer stringBuffer0 = new StringBuffer();
-        StringBuilder stringBuilder0 = new StringBuilder(stringBuffer0);
-        StringBuilder stringBuilder1 = stringBuilder0.insert(0, "6KY>-,V");
-        int int0 = CharSequenceUtils.lastIndexOf(stringBuilder1, 1403, 1403);
-        assertEquals((-1), int0);
+    /**
+     * Tests that lastIndexOf returns -1 when the starting search index is greater than the length of the CharSequence.
+     *
+     * According to the Javadoc, a starting index beyond the length of the string should be handled
+     * by starting the search from the end of the string. In this case, since the character
+     * is not found in the string, the expected result is -1.
+     */
+    @Test
+    public void lastIndexOfWithStartIndexGreaterThanLengthShouldReturnNotFound() {
+        // Arrange
+        final CharSequence text = "6KY>-,V";
+        final int searchCharNotInText = 1403; // A character not present in the text.
+        final int startIndexBeyondLength = text.length() + 100; // An index clearly out of bounds.
+
+        // Act
+        final int actualIndex = CharSequenceUtils.lastIndexOf(text, searchCharNotInText, startIndexBeyondLength);
+
+        // Assert
+        final int expectedIndex = -1;
+        assertEquals(expectedIndex, actualIndex);
     }
 }
