a12f196919cd838773e1fcf994fa76b8548c8bfc
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:00:44 +0800

    Original processedAutoTestCases linkedtreemap_estest67.java

---
+++ linkedtreemap_estest67.java
@@ -0,0 +1,35 @@
+package com.google.gson.internal;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.AbstractMap;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.BiFunction;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class LinkedTreeMap_ESTestTest67 extends LinkedTreeMap_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test66() throws Throwable {
+        LinkedTreeMap<Integer, Integer> linkedTreeMap0 = new LinkedTreeMap<Integer, Integer>();
+        Integer integer0 = new Integer((-1179));
+        linkedTreeMap0.putIfAbsent(integer0, integer0);
+        // Undeclared exception!
+        try {
+            linkedTreeMap0.find((Integer) null, true);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("com.google.gson.internal.LinkedTreeMap$1", e);
+        }
+    }
+}
