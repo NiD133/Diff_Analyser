1f5dffcf2b620b7a54c18058c6dce3ab089d8147
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:07:44 +0800

    Original processedAutoTestCases patternoptionbuilder_estest51.java

---
+++ patternoptionbuilder_estest51.java
@@ -0,0 +1,16 @@
+package org.apache.commons.cli;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class PatternOptionBuilder_ESTestTest51 extends PatternOptionBuilder_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test50() throws Throwable {
+        PatternOptionBuilder patternOptionBuilder0 = new PatternOptionBuilder();
+    }
+}
