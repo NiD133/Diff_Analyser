f5ae2325b0073121e108a67b13fd36eb505628cb
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:44:58 +0800

    Original processedAutoTestCases comparatorchain_estest22.java

---
+++ comparatorchain_estest22.java
@@ -0,0 +1,33 @@
+package org.apache.commons.collections4.comparators;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.nio.ByteBuffer;
+import java.nio.LongBuffer;
+import java.util.BitSet;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.ToIntFunction;
+import java.util.function.ToLongFunction;
+import org.apache.commons.collections4.Closure;
+import org.apache.commons.collections4.functors.ClosureTransformer;
+import org.apache.commons.collections4.functors.ComparatorPredicate;
+import org.apache.commons.collections4.functors.ExceptionClosure;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class ComparatorChain_ESTestTest22 extends ComparatorChain_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test21() throws Throwable {
+        ComparatorChain<Object> comparatorChain0 = new ComparatorChain<Object>((Comparator<Object>) null, false);
+        comparatorChain0.setComparator(0, (Comparator<Object>) null, true);
+        assertFalse(comparatorChain0.isLocked());
+    }
+}
