8be5864bd3b268572eb24dfd558a38edf411e188
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:26:41 +0800

    Original processedManualTestCases charsetutilstest5.java

---
+++ charsetutilstest5.java
@@ -0,0 +1,34 @@
+package org.apache.commons.lang3;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Modifier;
+import org.junit.jupiter.api.Test;
+
+public class CharSetUtilsTestTest5 extends AbstractLangTest {
+
+    @Test
+    void testCount_StringStringarray() {
+        assertEquals(0, CharSetUtils.count(null, (String[]) null));
+        assertEquals(0, CharSetUtils.count(null));
+        assertEquals(0, CharSetUtils.count(null, (String) null));
+        assertEquals(0, CharSetUtils.count(null, "a-e"));
+        assertEquals(0, CharSetUtils.count("", (String[]) null));
+        assertEquals(0, CharSetUtils.count(""));
+        assertEquals(0, CharSetUtils.count("", (String) null));
+        assertEquals(0, CharSetUtils.count("", "a-e"));
+        assertEquals(0, CharSetUtils.count("hello", (String[]) null));
+        assertEquals(0, CharSetUtils.count("hello"));
+        assertEquals(0, CharSetUtils.count("hello", (String) null));
+        assertEquals(1, CharSetUtils.count("hello", "a-e"));
+        assertEquals(3, CharSetUtils.count("hello", "el"));
+        assertEquals(0, CharSetUtils.count("hello", "x"));
+        assertEquals(2, CharSetUtils.count("hello", "e-i"));
+        assertEquals(5, CharSetUtils.count("hello", "a-z"));
+        assertEquals(0, CharSetUtils.count("hello", ""));
+    }
+}
