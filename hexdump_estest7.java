37aa3f330e36bb143406835509e2e3eb140d4bb2
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:53:18 +0800

    Original processedAutoTestCases hexdump_estest7.java

---
+++ hexdump_estest7.java
@@ -0,0 +1,42 @@
+package org.apache.commons.io;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PipedOutputStream;
+import java.io.PipedWriter;
+import java.io.StringWriter;
+import java.nio.BufferOverflowException;
+import java.nio.CharBuffer;
+import java.nio.ReadOnlyBufferException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.evosuite.runtime.mock.java.io.MockFileOutputStream;
+import org.evosuite.runtime.mock.java.io.MockFileWriter;
+import org.evosuite.runtime.mock.java.io.MockPrintStream;
+import org.junit.runner.RunWith;
+
+public class HexDump_ESTestTest7 extends HexDump_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test06() throws Throwable {
+        byte[] byteArray0 = new byte[5];
+        CharBuffer charBuffer0 = CharBuffer.allocate((byte) 3);
+        // Undeclared exception!
+        try {
+            HexDump.dump(byteArray0, (long) (byte) 17, (Appendable) charBuffer0, 1, (int) (byte) 2);
+            fail("Expecting exception: BufferOverflowException");
+        } catch (BufferOverflowException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("java.nio.CharBuffer", e);
+        }
+    }
+}
