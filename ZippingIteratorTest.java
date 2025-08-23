9719123d4b1e969bd9256452eb2a7e9afa77efcc
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 05:35:51 +0800

    gpt5 manualTestSuite ZippingIteratorTest.java

--- ZippingIteratorTest.java
+++ ZippingIteratorTest.java
@@ -1,44 +1,35 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.apache.commons.collections4.iterators;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.IntPredicate;
 
 import org.apache.commons.collections4.IteratorUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
- * Unit test suite for {@link ZippingIterator}.
+ * Tests for {@link ZippingIterator}.
+ * 
+ * The tests aim to clearly demonstrate the behavior of ZippingIterator:
+ * - When given one iterator, it yields the same sequence.
+ * - When given multiple iterators, it yields elements in round-robin order.
+ * - Removal delegates to the iterator that provided the last returned element.
  */
-@SuppressWarnings("boxing")
 class ZippingIteratorTest extends AbstractIteratorTest<Integer> {
 
-    private ArrayList<Integer> evens;
+    private static final int N = 20; // number of integers used to build evens/odds (0..19)
 
-    private ArrayList<Integer> odds;
-    private ArrayList<Integer> fib;
+    private List<Integer> evens;
+    private List<Integer> odds;
+    private List<Integer> fib;
 
     @Override
-    @SuppressWarnings("unchecked")
     public ZippingIterator<Integer> makeEmptyIterator() {
         return new ZippingIterator<>(IteratorUtils.<Integer>emptyIterator());
     }
@@ -49,142 +40,169 @@ class ZippingIteratorTest extends AbstractIteratorTest<Integer> {
     }
 
     @BeforeEach
-    public void setUp() throws Exception {
-        evens = new ArrayList<>();
-        odds = new ArrayList<>();
-        for (int i = 0; i < 20; i++) {
-            if (0 == i % 2) {
-                evens.add(i);
-            } else {
-                odds.add(i);
-            }
-        }
-        fib = new ArrayList<>();
-        fib.add(1);
-        fib.add(1);
-        fib.add(2);
-        fib.add(3);
-        fib.add(5);
-        fib.add(8);
-        fib.add(13);
-        fib.add(21);
+    void setUp() {
+        evens = buildRange(0, N, 2); // 0, 2, 4, ..., 18
+        odds  = buildRange(1, N, 2); // 1, 3, 5, ..., 19
+        fib   = Arrays.asList(1, 1, 2, 3, 5, 8, 13, 21);
     }
 
+    // ------------------------------
+    // Iteration behavior
+    // ------------------------------
+
     @Test
-    void testIterateEven() {
-        @SuppressWarnings("unchecked")
+    void singleIterator_yieldsSameOrder() {
         final ZippingIterator<Integer> iter = new ZippingIterator<>(evens.iterator());
-        for (final Integer even : evens) {
-            assertTrue(iter.hasNext());
-            assertEquals(even, iter.next());
-        }
-        assertFalse(iter.hasNext());
+
+        final List<Integer> actual = drain(iter);
+        assertEquals(evens, actual, "Zipping a single iterator should preserve its order");
+
+        assertFalse(iter.hasNext(), "Iterator should be exhausted");
     }
 
     @Test
-    void testIterateEvenEven() {
+    void twoIterators_sameIterator_roundRobinAlternates() {
         final ZippingIterator<Integer> iter = new ZippingIterator<>(evens.iterator(), evens.iterator());
-        for (final Integer even : evens) {
-            assertTrue(iter.hasNext());
-            assertEquals(even, iter.next());
-            assertTrue(iter.hasNext());
-            assertEquals(even, iter.next());
-        }
-        assertFalse(iter.hasNext());
+
+        final List<Integer> expected = duplicateEach(evens);
+        final List<Integer> actual = drain(iter);
+
+        assertEquals(expected, actual, "Elements should alternate from the two identical iterators");
+        assertFalse(iter.hasNext(), "Iterator should be exhausted");
     }
 
     @Test
-    void testIterateEvenOdd() {
+    void twoIterators_evenThenOdd_produces0Through19() {
         final ZippingIterator<Integer> iter = new ZippingIterator<>(evens.iterator(), odds.iterator());
-        for (int i = 0; i < 20; i++) {
-            assertTrue(iter.hasNext());
-            assertEquals(Integer.valueOf(i), iter.next());
-        }
-        assertFalse(iter.hasNext());
+
+        final List<Integer> expected = buildRange(0, N, 1);
+        final List<Integer> actual = drain(iter);
+
+        assertEquals(expected, actual, "Zipping evens then odds should yield 0..19 in order");
+        assertFalse(iter.hasNext(), "Iterator should be exhausted");
     }
 
     @Test
-    void testIterateFibEvenOdd() {
+    void twoIterators_oddThenEven_alternateStartingWithOdd() {
+        final ZippingIterator<Integer> iter = new ZippingIterator<>(odds.iterator(), evens.iterator());
+
+        final List<Integer> expected = alternate(odds, evens);
+        final List<Integer> actual = drain(iter);
+
+        assertEquals(expected, actual, "Zipping odds then evens should alternate [1,0,3,2,...,19,18]");
+        assertFalse(iter.hasNext(), "Iterator should be exhausted");
+    }
+
+    @Test
+    void threeIterators_fibEvenOdd_roundRobinUntilAllExhausted() {
         final ZippingIterator<Integer> iter = new ZippingIterator<>(fib.iterator(), evens.iterator(), odds.iterator());
 
-        assertEquals(Integer.valueOf(1), iter.next());  // fib    1
-        assertEquals(Integer.valueOf(0), iter.next());  // even   0
-        assertEquals(Integer.valueOf(1), iter.next());  // odd    1
-        assertEquals(Integer.valueOf(1), iter.next());  // fib    1
-        assertEquals(Integer.valueOf(2), iter.next());  // even   2
-        assertEquals(Integer.valueOf(3), iter.next());  // odd    3
-        assertEquals(Integer.valueOf(2), iter.next());  // fib    2
-        assertEquals(Integer.valueOf(4), iter.next());  // even   4
-        assertEquals(Integer.valueOf(5), iter.next());  // odd    5
-        assertEquals(Integer.valueOf(3), iter.next());  // fib    3
-        assertEquals(Integer.valueOf(6), iter.next());  // even   6
-        assertEquals(Integer.valueOf(7), iter.next());  // odd    7
-        assertEquals(Integer.valueOf(5), iter.next());  // fib    5
-        assertEquals(Integer.valueOf(8), iter.next());  // even   8
-        assertEquals(Integer.valueOf(9), iter.next());  // odd    9
-        assertEquals(Integer.valueOf(8), iter.next());  // fib    8
-        assertEquals(Integer.valueOf(10), iter.next()); // even  10
-        assertEquals(Integer.valueOf(11), iter.next()); // odd   11
-        assertEquals(Integer.valueOf(13), iter.next()); // fib   13
-        assertEquals(Integer.valueOf(12), iter.next()); // even  12
-        assertEquals(Integer.valueOf(13), iter.next()); // odd   13
-        assertEquals(Integer.valueOf(21), iter.next()); // fib   21
-        assertEquals(Integer.valueOf(14), iter.next()); // even  14
-        assertEquals(Integer.valueOf(15), iter.next()); // odd   15
-        assertEquals(Integer.valueOf(16), iter.next()); // even  16
-        assertEquals(Integer.valueOf(17), iter.next()); // odd   17
-        assertEquals(Integer.valueOf(18), iter.next()); // even  18
-        assertEquals(Integer.valueOf(19), iter.next()); // odd   19
+        final List<Integer> expected = roundRobinOf(fib, evens, odds);
+        final List<Integer> actual = drain(iter);
 
-        assertFalse(iter.hasNext());
+        assertEquals(expected, actual, "Round-robin across [fib, evens, odds] until all are exhausted");
+        assertFalse(iter.hasNext(), "Iterator should be exhausted");
     }
 
-    @Test
-    void testIterateOddEven() {
-        final ZippingIterator<Integer> iter = new ZippingIterator<>(odds.iterator(), evens.iterator());
-        for (int i = 0, j = 0; i < 20; i++) {
-            assertTrue(iter.hasNext());
-            final int val = iter.next();
-            if (i % 2 == 0) {
-                assertEquals(odds.get(j).intValue(), val);
-            } else {
-                assertEquals(evens.get(j).intValue(), val);
-                j++;
-            }
-        }
-        assertFalse(iter.hasNext());
-    }
+    // ------------------------------
+    // Removal behavior
+    // ------------------------------
 
     @Test
-    void testRemoveFromDouble() {
+    void removeFromTwoIterators_removesFromCorrectSource() {
         final ZippingIterator<Integer> iter = new ZippingIterator<>(evens.iterator(), odds.iterator());
-        int expectedSize = evens.size() + odds.size();
-        while (iter.hasNext()) {
-            final Object o = iter.next();
-            final Integer val = (Integer) o;
-            if (val.intValue() % 4 == 0 || val.intValue() % 3 == 0) {
-                expectedSize--;
-                iter.remove();
-            }
-        }
-        assertEquals(expectedSize, evens.size() + odds.size());
+
+        final int initialTotal = evens.size() + odds.size();
+        final int removed = removeMatchingAndCount(iter, v -> v % 4 == 0 || v % 3 == 0);
+
+        assertEquals(initialTotal - removed, evens.size() + odds.size(),
+                "Total size should reflect number of elements removed across both sources");
     }
 
     @Test
-    void testRemoveFromSingle() {
-        @SuppressWarnings("unchecked")
+    void removeFromSingleIterator_removesFromThatSource() {
         final ZippingIterator<Integer> iter = new ZippingIterator<>(evens.iterator());
-        int expectedSize = evens.size();
-        while (iter.hasNext()) {
-            final Object o = iter.next();
-            final Integer val = (Integer) o;
-            if (val.intValue() % 4 == 0) {
-                expectedSize--;
-                iter.remove();
-            }
-        }
-        assertEquals(expectedSize, evens.size());
+
+        final int initialSize = evens.size();
+        final int removed = removeMatchingAndCount(iter, v -> v % 4 == 0);
+
+        assertEquals(initialSize - removed, evens.size(),
+                "Evens list size should decrease by exactly the number of removed elements");
     }
 
-}
+    // ------------------------------
+    // Helpers
+    // ------------------------------
 
+    private static List<Integer> buildRange(final int startInclusive, final int endExclusive, final int step) {
+        final List<Integer> list = new ArrayList<>();
+        for (int i = startInclusive; i < endExclusive; i += step) {
+            list.add(i);
+        }
+        return list;
+    }
+
+    private static List<Integer> duplicateEach(final List<Integer> source) {
+        final List<Integer> out = new ArrayList<>(source.size() * 2);
+        for (final Integer value : source) {
+            out.add(value);
+            out.add(value);
+        }
+        return out;
+    }
+
+    private static List<Integer> alternate(final List<Integer> first, final List<Integer> second) {
+        final int size = Math.min(first.size(), second.size());
+        final List<Integer> out = new ArrayList<>(size * 2);
+        for (int i = 0; i < size; i++) {
+            out.add(first.get(i));
+            out.add(second.get(i));
+        }
+        return out;
+    }
+
+    /**
+     * Builds the round-robin sequence you would expect from a correct ZippingIterator
+     * without using ZippingIterator itself (to avoid circular testing).
+     */
+    @SafeVarargs
+    private static List<Integer> roundRobinOf(final List<Integer>... sources) {
+        final List<Iterator<Integer>> iterators = new ArrayList<>(sources.length);
+        for (final List<Integer> s : sources) {
+            iterators.add(s.iterator());
+        }
+
+        final List<Integer> out = new ArrayList<>();
+        boolean anyEmitted;
+        do {
+            anyEmitted = false;
+            for (final Iterator<Integer> it : iterators) {
+                if (it.hasNext()) {
+                    out.add(it.next());
+                    anyEmitted = true;
+                }
+            }
+        } while (anyEmitted);
+        return out;
+    }
+
+    private static List<Integer> drain(final Iterator<Integer> iterator) {
+        final List<Integer> out = new ArrayList<>();
+        while (iterator.hasNext()) {
+            out.add(iterator.next());
+        }
+        return out;
+    }
+
+    private static int removeMatchingAndCount(final ZippingIterator<Integer> iterator, final IntPredicate predicate) {
+        int removed = 0;
+        while (iterator.hasNext()) {
+            final int value = iterator.next();
+            if (predicate.test(value)) {
+                iterator.remove();
+                removed++;
+            }
+        }
+        return removed;
+    }
+}
