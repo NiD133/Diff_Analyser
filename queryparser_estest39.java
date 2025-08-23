a845cbe9d1d27626f012a52cfac60830d1fb44f1
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:10:08 +0800

    Original processedAutoTestCases queryparser_estest39.java

---
+++ queryparser_estest39.java
@@ -0,0 +1,26 @@
+package org.jsoup.select;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class QueryParser_ESTestTest39 extends QueryParser_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test38() throws Throwable {
+        // Undeclared exception!
+        try {
+            QueryParser.parse(":nth-last-of-type(-464n-464) ~ ");
+            //  fail("Expecting exception: IllegalStateException");
+            // Unstable assertion
+        } catch (IllegalStateException e) {
+            //
+            // No match found
+            //
+            verifyException("java.util.regex.Matcher", e);
+        }
+    }
+}
