7e64d00ea2bfeca4f910d20b7e75836527936587
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:01:31 +0800

    gemini processedAutoTestCases lookuptranslator_estest2.java

--- lookuptranslator_estest2.java
+++ lookuptranslator_estest2.java
@@ -1,39 +1,31 @@
 package org.apache.commons.lang3.text.translate;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.StringWriter;
-import java.io.Writer;
-import java.nio.CharBuffer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertThrows;
 
-public class LookupTranslator_ESTestTest2 extends LookupTranslator_ESTest_scaffolding {
+/**
+ * Tests for {@link LookupTranslator}.
+ */
+public class LookupTranslatorTest {
 
-    @Test(timeout = 4000)
-    public void test01() throws Throwable {
-        CharSequence[] charSequenceArray0 = new CharSequence[3];
-        char[] charArray0 = new char[2];
-        CharBuffer charBuffer0 = CharBuffer.wrap(charArray0);
-        charSequenceArray0[0] = (CharSequence) charBuffer0;
-        charSequenceArray0[1] = (CharSequence) "FFFFF15A";
-        CharSequence[][] charSequenceArray1 = new CharSequence[8][3];
-        charSequenceArray1[0] = charSequenceArray0;
-        CharSequence[] charSequenceArray2 = new CharSequence[5];
-        charSequenceArray2[0] = (CharSequence) "FFFFF15A";
-        charSequenceArray2[1] = (CharSequence) "FFFFF15A";
-        charSequenceArray1[1] = charSequenceArray2;
-        LookupTranslator lookupTranslator0 = null;
-        try {
-            lookupTranslator0 = new LookupTranslator(charSequenceArray1);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.commons.lang3.text.translate.LookupTranslator", e);
-        }
+    /**
+     * Tests that the constructor throws a NullPointerException if the main lookup array
+     * contains a null element. The constructor iterates through each entry, and a null
+     * entry will cause a NullPointerException when its elements are accessed.
+     */
+    @Test
+    public void constructorShouldThrowNPEWhenLookupArrayContainsNullEntry() {
+        // Arrange: Create a lookup table where one of the lookup entries is null.
+        // The constructor expects an array of [key, value] pairs.
+        final CharSequence[][] lookupTableWithNullEntry = new CharSequence[][]{
+                {"one", "1"}, // A valid entry
+                null          // An invalid, null entry that should cause an exception
+        };
+
+        // Act & Assert: Verify that creating a LookupTranslator with this table
+        // throws a NullPointerException.
+        assertThrows(NullPointerException.class, () -> {
+            new LookupTranslator(lookupTableWithNullEntry);
+        });
     }
 }
