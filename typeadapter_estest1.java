6837231b82dfe007994ed0996d40f2e4572e7c03
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:23:59 +0800

    Original processedAutoTestCases typeadapter_estest1.java

---
+++ typeadapter_estest1.java
@@ -0,0 +1,28 @@
+package com.google.gson;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.io.Writer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class TypeAdapter_ESTestTest1 extends TypeAdapter_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test00() throws Throwable {
+        Gson.FutureTypeAdapter<Object> gson_FutureTypeAdapter0 = new Gson.FutureTypeAdapter<Object>();
+        TypeAdapter<Object> typeAdapter0 = gson_FutureTypeAdapter0.nullSafe();
+        StringWriter stringWriter0 = new StringWriter();
+        typeAdapter0.toJson((Writer) stringWriter0, (Object) null);
+        assertEquals("null", stringWriter0.toString());
+    }
+}
