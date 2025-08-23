72bf6a2c9fbebf440bdf1e7babc844acbbace4c3
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:32:50 +0800

    Original processedManualTestCases quotedprintablecodectest19.java

---
+++ quotedprintablecodectest19.java
@@ -0,0 +1,39 @@
+package org.apache.commons.codec.net;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.nio.charset.StandardCharsets;
+import java.nio.charset.UnsupportedCharsetException;
+import org.apache.commons.codec.CharEncoding;
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.EncoderException;
+import org.junit.jupiter.api.Test;
+
+public class QuotedPrintableCodecTestTest19 {
+
+    static final int[] SWISS_GERMAN_STUFF_UNICODE = { 0x47, 0x72, 0xFC, 0x65, 0x7A, 0x69, 0x5F, 0x7A, 0xE4, 0x6D, 0xE4 };
+
+    static final int[] RUSSIAN_STUFF_UNICODE = { 0x412, 0x441, 0x435, 0x43C, 0x5F, 0x43F, 0x440, 0x438, 0x432, 0x435, 0x442 };
+
+    private String constructString(final int[] unicodeChars) {
+        final StringBuilder buffer = new StringBuilder();
+        if (unicodeChars != null) {
+            for (final int unicodeChar : unicodeChars) {
+                buffer.append((char) unicodeChar);
+            }
+        }
+        return buffer.toString();
+    }
+
+    @Test
+    void testTrailingSpecial() throws Exception {
+        final QuotedPrintableCodec qpcodec = new QuotedPrintableCodec(true);
+        String plain = "This is a example of a quoted-printable text file. This might contain sp=cial chars.";
+        String expected = "This is a example of a quoted-printable text file. This might contain sp=3D=\r\ncial chars.";
+        assertEquals(expected, qpcodec.encode(plain));
+        plain = "This is a example of a quoted-printable text file. This might contain ta\tbs as well.";
+        expected = "This is a example of a quoted-printable text file. This might contain ta=09=\r\nbs as well.";
+        assertEquals(expected, qpcodec.encode(plain));
+    }
+}
