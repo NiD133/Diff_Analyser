50f277e5eea7de7369f61a5b14f0ff3f195290f8
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:24:42 +0800

    gemini processedAutoTestCases uncheck_estest49.java

--- uncheck_estest49.java
+++ uncheck_estest49.java
@@ -1,37 +1,24 @@
 package org.apache.commons.io.function;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.time.chrono.HijrahEra;
-import java.util.Comparator;
-import java.util.concurrent.ForkJoinTask;
-import java.util.function.BiConsumer;
-import java.util.function.BinaryOperator;
-import java.util.function.Function;
-import java.util.function.Supplier;
-import java.util.stream.Collector;
-import java.util.stream.LongStream;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
 
-public class Uncheck_ESTestTest49 extends Uncheck_ESTest_scaffolding {
+/**
+ * Tests for the {@link Uncheck} utility class.
+ */
+public class UncheckTest {
 
-    @Test(timeout = 4000)
-    public void test48() throws Throwable {
-        // Undeclared exception!
-        try {
-            Uncheck.accept((IOConsumer<String>) null, (String) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.commons.io.function.Uncheck", e);
-        }
+    /**
+     * Tests that calling {@link Uncheck#accept(IOConsumer, Object)} with a null
+     * consumer throws a {@link NullPointerException}.
+     * <p>
+     * This behavior is expected because a null functional interface is a
+     * programming error (precondition violation) and should fail fast, rather
+     * than be wrapped in an {@link java.io.UncheckedIOException}.
+     * </p>
+     */
+    @Test(expected = NullPointerException.class)
+    public void testAcceptThrowsNpeForNullConsumer() {
+        // The cast to IOConsumer<String> is required to resolve the correct method overload.
+        Uncheck.accept((IOConsumer<String>) null, "test-input");
     }
 }
