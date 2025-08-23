0d2b8f589c715c750f220aaf5fb323ae96b122f7
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:53:57 +0800

    gemini processedAutoTestCases indexedcollection_estest25.java

--- indexedcollection_estest25.java
+++ indexedcollection_estest25.java
@@ -1,68 +1,53 @@
 package org.apache.commons.collections4.collection;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.ConcurrentModificationException;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Set;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.Predicate;
 import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AllPredicate;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ChainedTransformer;
-import org.apache.commons.collections4.functors.CloneTransformer;
-import org.apache.commons.collections4.functors.ClosureTransformer;
-import org.apache.commons.collections4.functors.ConstantFactory;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.DefaultEquator;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.FactoryTransformer;
-import org.apache.commons.collections4.functors.FalsePredicate;
-import org.apache.commons.collections4.functors.ForClosure;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
 import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.NOPClosure;
-import org.apache.commons.collections4.functors.NOPTransformer;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NotNullPredicate;
-import org.apache.commons.collections4.functors.NullIsFalsePredicate;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.SwitchTransformer;
-import org.apache.commons.collections4.functors.TransformedPredicate;
-import org.apache.commons.collections4.functors.TransformerClosure;
-import org.apache.commons.collections4.functors.TransformerPredicate;
-import org.apache.commons.collections4.functors.TruePredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
+import java.util.Collection;
+import java.util.LinkedList;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+/**
+ * This test class contains tests for the IndexedCollection.
+ * The original test was auto-generated and has been refactored for clarity.
+ */
 public class IndexedCollection_ESTestTest25 extends IndexedCollection_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test24() throws Throwable {
-        LinkedList<Integer> linkedList0 = new LinkedList<Integer>();
-        IndexedCollection<Integer, Integer> indexedCollection0 = IndexedCollection.uniqueIndexedCollection((Collection<Integer>) linkedList0, (Transformer<Integer, Integer>) null);
-        Transformer<Integer, Object> transformer0 = InvokerTransformer.invokerTransformer(".");
-        IndexedCollection<Object, Integer> indexedCollection1 = IndexedCollection.uniqueIndexedCollection((Collection<Integer>) indexedCollection0, transformer0);
-        Integer integer0 = new Integer((-1));
-        linkedList0.add(integer0);
-        // Undeclared exception!
+    /**
+     * Tests that reindex() propagates a RuntimeException thrown by the key transformer.
+     * This scenario occurs when the underlying collection is modified directly,
+     * and reindex() is called to synchronize the index.
+     */
+    @Test
+    public void reindexShouldPropagateExceptionFromFailingTransformer() {
+        // Arrange
+        // 1. A transformer designed to fail on Integer objects by invoking a non-existent method.
+        //    The original test used ".", which is a valid but unusual method name to test.
+        final Transformer<Integer, Object> failingTransformer = InvokerTransformer.invokerTransformer(".");
+
+        // 2. The underlying collection that the IndexedCollection will decorate.
+        final Collection<Integer> underlyingCollection = new LinkedList<>();
+
+        // 3. Create the IndexedCollection while the underlying collection is empty.
+        //    The initial reindex() in the constructor will do nothing and succeed.
+        final IndexedCollection<Object, Integer> indexedCollection =
+                IndexedCollection.uniqueIndexedCollection(underlyingCollection, failingTransformer);
+
+        // 4. Modify the underlying collection directly, making the index out of sync.
+        //    This is the scenario where an explicit call to reindex() is needed.
+        underlyingCollection.add(-1);
+
+        // Act & Assert
         try {
-            indexedCollection1.reindex();
-            fail("Expecting exception: RuntimeException");
-        } catch (RuntimeException e) {
-            //
-            // InvokerTransformer: The method '.' on 'class java.lang.Integer' does not exist
-            //
-            verifyException("org.apache.commons.collections4.functors.InvokerTransformer", e);
+            indexedCollection.reindex();
+            fail("Expected a RuntimeException because the transformer should fail.");
+        } catch (final RuntimeException e) {
+            // Verify that the exception from the InvokerTransformer was propagated correctly.
+            final String expectedMessage = "InvokerTransformer: The method '.' on 'class java.lang.Integer' does not exist";
+            assertEquals(expectedMessage, e.getMessage());
         }
     }
 }
