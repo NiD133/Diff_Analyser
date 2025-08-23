3bea2d7a23d9c22f2b90cf012d4079350314f1ce
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:25:04 +0800

    Original processedManualTestCases attributetest3.java

---
+++ attributetest3.java
@@ -0,0 +1,18 @@
+package org.jsoup.nodes;
+
+import org.jsoup.Jsoup;
+import org.jsoup.parser.ParseSettings;
+import org.jsoup.parser.Parser;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AttributeTestTest3 {
+
+    @Test
+    public void testWithSupplementaryCharacterInAttributeKeyAndValue() {
+        String s = new String(Character.toChars(135361));
+        Attribute attr = new Attribute(s, "A" + s + "B");
+        assertEquals(s + "=\"A" + s + "B\"", attr.html());
+        assertEquals(attr.html(), attr.toString());
+    }
+}
