118338955d4c29d545fe7e3df7d42bb0999f35d8
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:26:22 +0800

    Original processedAutoTestCases vector_estest8.java

---
+++ vector_estest8.java
@@ -0,0 +1,18 @@
+package com.itextpdf.text.pdf.parser;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class Vector_ESTestTest8 extends Vector_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test07() throws Throwable {
+        Vector vector0 = new Vector((-1465.0F), (-1465.0F), 1);
+        float float0 = vector0.lengthSquared();
+        assertEquals(4292451.0F, float0, 0.01F);
+    }
+}
