037d35aeb08124026f770b183acbd71d2d39509a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:18:07 +0800

    Original processedAutoTestCases streamcompressor_estest12.java

---
+++ streamcompressor_estest12.java
@@ -0,0 +1,44 @@
+package org.apache.commons.compress.archivers.zip;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.io.SequenceInputStream;
+import java.nio.channels.SeekableByteChannel;
+import java.util.Enumeration;
+import java.util.zip.Deflater;
+import org.apache.commons.compress.parallel.ScatterGatherBackingStore;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.io.MockFileInputStream;
+import org.evosuite.runtime.mock.java.io.MockFileOutputStream;
+import org.junit.runner.RunWith;
+
+public class StreamCompressor_ESTestTest12 extends StreamCompressor_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test11() throws Throwable {
+        StreamCompressor streamCompressor0 = StreamCompressor.create(8, (ScatterGatherBackingStore) null);
+        // Undeclared exception!
+        try {
+            streamCompressor0.writeCounted((byte[]) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.apache.commons.compress.archivers.zip.StreamCompressor", e);
+        }
+    }
+}
