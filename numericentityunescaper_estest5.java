1575159d7ceaee9e8bd08622f958656426730583
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:05:35 +0800

    gemini processedAutoTestCases numericentityunescaper_estest5.java

--- numericentityunescaper_estest5.java
+++ numericentityunescaper_estest5.java
@@ -1,30 +1,36 @@
 package org.apache.commons.lang3.text.translate;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
+import static org.junit.Assert.assertEquals;
+
 import java.io.StringWriter;
-import java.io.Writer;
-import java.nio.CharBuffer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import java.io.IOException;
 
-public class NumericEntityUnescaper_ESTestTest5 extends NumericEntityUnescaper_ESTest_scaffolding {
+/**
+ * Tests for {@link NumericEntityUnescaper}.
+ */
+public class NumericEntityUnescaperTest {
 
-    @Test(timeout = 4000)
-    public void test04() throws Throwable {
-        NumericEntityUnescaper.OPTION[] numericEntityUnescaper_OPTIONArray0 = new NumericEntityUnescaper.OPTION[0];
-        NumericEntityUnescaper numericEntityUnescaper0 = new NumericEntityUnescaper(numericEntityUnescaper_OPTIONArray0);
-        char[] charArray0 = new char[7];
-        charArray0[3] = '&';
-        charArray0[4] = '#';
-        charArray0[5] = '3';
-        charArray0[6] = ';';
-        CharBuffer charBuffer0 = CharBuffer.wrap(charArray0);
-        StringWriter stringWriter0 = new StringWriter();
-        int int0 = numericEntityUnescaper0.translate((CharSequence) charBuffer0, 3, (Writer) stringWriter0);
-        assertEquals("\u0003", stringWriter0.toString());
-        assertEquals(4, int0);
+    /**
+     * Verifies that a standard decimal numeric entity with a semicolon is correctly unescaped.
+     */
+    @Test
+    public void shouldUnescapeDecimalEntityWithSemicolon() throws IOException {
+        // Arrange: Set up the test objects and inputs.
+        // The default NumericEntityUnescaper requires a semicolon for numeric entities.
+        final NumericEntityUnescaper unescaper = new NumericEntityUnescaper();
+        final String input = "An example: &#3; is a control character.";
+        final StringWriter writer = new StringWriter();
+        final int entityStartIndex = input.indexOf('&');
+
+        // Act: Call the method under test.
+        final int consumedChars = unescaper.translate(input, entityStartIndex, writer);
+
+        // Assert: Verify the results.
+        // The entity "&#3;" should be translated to the character with code point 3.
+        assertEquals("The unescaped character should be correct", "\u0003", writer.toString());
+        
+        // The method should report consuming 4 characters: '&', '#', '3', ';'.
+        assertEquals("The number of consumed characters should be correct", 4, consumedChars);
     }
 }
