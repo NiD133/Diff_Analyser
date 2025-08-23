647daed7178e64395db6f72c029a2e60b3647f83
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:50:07 +0800

    Original processedAutoTestCases fieldnamingpolicy_estest10.java

---
+++ fieldnamingpolicy_estest10.java
@@ -0,0 +1,27 @@
+package com.google.gson;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.lang.reflect.Field;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class FieldNamingPolicy_ESTestTest10 extends FieldNamingPolicy_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test09() throws Throwable {
+        FieldNamingPolicy fieldNamingPolicy0 = FieldNamingPolicy.IDENTITY;
+        // Undeclared exception!
+        try {
+            fieldNamingPolicy0.translateName((Field) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("com.google.gson.FieldNamingPolicy$1", e);
+        }
+    }
+}
