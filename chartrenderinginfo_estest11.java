863ede8f8ea4be541bda76dee211b3ba639e116a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:43:09 +0800

    Original processedAutoTestCases chartrenderinginfo_estest11.java

---
+++ chartrenderinginfo_estest11.java
@@ -0,0 +1,32 @@
+package org.jfree.chart;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.awt.Rectangle;
+import java.awt.geom.Rectangle2D;
+import java.util.Locale;
+import java.util.SimpleTimeZone;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.jfree.chart.axis.DateAxis;
+import org.jfree.chart.entity.EntityCollection;
+import org.jfree.chart.entity.StandardEntityCollection;
+import org.jfree.chart.plot.CombinedRangeCategoryPlot;
+import org.jfree.chart.plot.PlotRenderingInfo;
+import org.jfree.data.xy.XYDatasetTableModel;
+import org.junit.runner.RunWith;
+
+public class ChartRenderingInfo_ESTestTest11 extends ChartRenderingInfo_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test10() throws Throwable {
+        ChartRenderingInfo chartRenderingInfo0 = new ChartRenderingInfo();
+        ChartRenderingInfo chartRenderingInfo1 = new ChartRenderingInfo();
+        assertTrue(chartRenderingInfo1.equals((Object) chartRenderingInfo0));
+        chartRenderingInfo1.setEntityCollection((EntityCollection) null);
+        boolean boolean0 = chartRenderingInfo0.equals(chartRenderingInfo1);
+        assertFalse(chartRenderingInfo1.equals((Object) chartRenderingInfo0));
+        assertFalse(boolean0);
+    }
+}
