d640fcc465c222b25442996359d556d932a2f41d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:26:15 +0800

    Original processedManualTestCases calendarconvertertest1.java

---
+++ calendarconvertertest1.java
@@ -0,0 +1,60 @@
+package org.joda.time.convert;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.joda.time.Chronology;
+import org.joda.time.DateTimeZone;
+import org.joda.time.TimeOfDay;
+import org.joda.time.chrono.BuddhistChronology;
+import org.joda.time.chrono.GJChronology;
+import org.joda.time.chrono.GregorianChronology;
+import org.joda.time.chrono.ISOChronology;
+import org.joda.time.chrono.JulianChronology;
+
+public class CalendarConverterTestTest1 extends TestCase {
+
+    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
+
+    private static final DateTimeZone MOSCOW = DateTimeZone.forID("Europe/Moscow");
+
+    private static Chronology JULIAN;
+
+    private static Chronology ISO;
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
+
+    public static TestSuite suite() {
+        return new TestSuite(TestCalendarConverter.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        JULIAN = JulianChronology.getInstance();
+        ISO = ISOChronology.getInstance();
+    }
+
+    //-----------------------------------------------------------------------
+    public void testSingleton() throws Exception {
+        Class cls = CalendarConverter.class;
+        assertEquals(false, Modifier.isPublic(cls.getModifiers()));
+        assertEquals(false, Modifier.isProtected(cls.getModifiers()));
+        assertEquals(false, Modifier.isPrivate(cls.getModifiers()));
+        Constructor con = cls.getDeclaredConstructor((Class[]) null);
+        assertEquals(1, cls.getDeclaredConstructors().length);
+        assertEquals(true, Modifier.isProtected(con.getModifiers()));
+        Field fld = cls.getDeclaredField("INSTANCE");
+        assertEquals(false, Modifier.isPublic(fld.getModifiers()));
+        assertEquals(false, Modifier.isProtected(fld.getModifiers()));
+        assertEquals(false, Modifier.isPrivate(fld.getModifiers()));
+    }
+}
