1337a0e97ab1614e9f1dc80077341edcb5569bdc
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:27:38 +0800

    Original processedAutoTestCases wktwriter_estest15.java

---
+++ wktwriter_estest15.java
@@ -0,0 +1,39 @@
+package org.locationtech.spatial4j.io;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.text.ChoiceFormat;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.LinkedList;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+import org.locationtech.spatial4j.context.SpatialContext;
+import org.locationtech.spatial4j.context.SpatialContextFactory;
+import org.locationtech.spatial4j.distance.GeodesicSphereDistCalc;
+import org.locationtech.spatial4j.shape.Circle;
+import org.locationtech.spatial4j.shape.Point;
+import org.locationtech.spatial4j.shape.Rectangle;
+import org.locationtech.spatial4j.shape.Shape;
+import org.locationtech.spatial4j.shape.ShapeCollection;
+import org.locationtech.spatial4j.shape.impl.BufferedLine;
+import org.locationtech.spatial4j.shape.impl.BufferedLineString;
+import org.locationtech.spatial4j.shape.impl.PointImpl;
+
+public class WKTWriter_ESTestTest15 extends WKTWriter_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test14() throws Throwable {
+        WKTWriter wKTWriter0 = new WKTWriter();
+        LinkedList<Point> linkedList0 = new LinkedList<Point>();
+        SpatialContext spatialContext0 = SpatialContext.GEO;
+        BufferedLineString bufferedLineString0 = new BufferedLineString(linkedList0, 360.0, spatialContext0);
+        Point point0 = bufferedLineString0.getCenter();
+        String string0 = wKTWriter0.toString((Shape) point0);
+        assertEquals("POINT EMPTY", string0);
+    }
+}
