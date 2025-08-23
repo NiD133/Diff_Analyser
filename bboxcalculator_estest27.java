68d7e9b39b0439caf6ba0c1061757643286d7387
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:38:59 +0800

    Original processedAutoTestCases bboxcalculator_estest27.java

---
+++ bboxcalculator_estest27.java
@@ -0,0 +1,27 @@
+package org.locationtech.spatial4j.shape.impl;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.HashMap;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+import org.locationtech.spatial4j.context.SpatialContext;
+import org.locationtech.spatial4j.context.SpatialContextFactory;
+import org.locationtech.spatial4j.distance.GeodesicSphereDistCalc;
+import org.locationtech.spatial4j.shape.Point;
+import org.locationtech.spatial4j.shape.Rectangle;
+
+public class BBoxCalculator_ESTestTest27 extends BBoxCalculator_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test26() throws Throwable {
+        SpatialContext spatialContext0 = SpatialContext.GEO;
+        BBoxCalculator bBoxCalculator0 = new BBoxCalculator(spatialContext0);
+        bBoxCalculator0.expandXRange(Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY);
+        bBoxCalculator0.expandRange((-5743.7453681), (-5743.7453681), Double.NEGATIVE_INFINITY, (-5743.7453681));
+        bBoxCalculator0.expandXRange((-5743.7453681), Double.NEGATIVE_INFINITY);
+        assertEquals(Double.NEGATIVE_INFINITY, bBoxCalculator0.getMinY(), 0.01);
+    }
+}
