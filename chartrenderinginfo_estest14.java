dd766ceef4b38ecc1b52430f315f325959e15548
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:43:10 +0800

    Original processedAutoTestCases chartrenderinginfo_estest14.java

---
+++ chartrenderinginfo_estest14.java
@@ -0,0 +1,28 @@
+package org.jfree.chart;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.awt.Rectangle;
+import java.awt.geom.Rectangle2D;
+import java.util.Locale;
+import java.util.SimpleTimeZone;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.jfree.chart.axis.DateAxis;
+import org.jfree.chart.entity.EntityCollection;
+import org.jfree.chart.entity.StandardEntityCollection;
+import org.jfree.chart.plot.CombinedRangeCategoryPlot;
+import org.jfree.chart.plot.PlotRenderingInfo;
+import org.jfree.data.xy.XYDatasetTableModel;
+import org.junit.runner.RunWith;
+
+public class ChartRenderingInfo_ESTestTest14 extends ChartRenderingInfo_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test13() throws Throwable {
+        ChartRenderingInfo chartRenderingInfo0 = new ChartRenderingInfo();
+        boolean boolean0 = chartRenderingInfo0.equals(chartRenderingInfo0);
+        assertTrue(boolean0);
+    }
+}
