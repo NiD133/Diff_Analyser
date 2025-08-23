599377032142856c0ffab0f1b97d41ddedcbfa52
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 05:34:27 +0800

    Original manualTestSuite NumberOutputTest.java

--- NumberOutputTest.java
+++ NumberOutputTest.java
@@ -1,129 +1,148 @@
 package com.fasterxml.jackson.core.io;
 
 import java.util.Random;
+
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
-class NumberOutputTest {
-
-    /**
-     * Test the conversion of integer values to their string representation.
-     */
+class NumberOutputTest
+{
     @Test
-    void testIntegerToStringConversion() throws Exception {
-        int[] testValues = {
-            0, -3, 1234, -1234, 56789, -56789, 999999, -999999,
-            1000000, -1000000, 10000001, -10000001, -100000012, 100000012,
-            1999888777, -1999888777, Integer.MAX_VALUE, Integer.MIN_VALUE
-        };
+    void intPrinting() throws Exception
+    {
+        assertIntPrint(0);
+        assertIntPrint(-3);
+        assertIntPrint(1234);
+        assertIntPrint(-1234);
+        assertIntPrint(56789);
+        assertIntPrint(-56789);
+        assertIntPrint(999999);
+        assertIntPrint(-999999);
+        assertIntPrint(1000000);
+        assertIntPrint(-1000000);
+        assertIntPrint(10000001);
+        assertIntPrint(-10000001);
+        assertIntPrint(-100000012);
+        assertIntPrint(100000012);
+        assertIntPrint(1999888777);
+        assertIntPrint(-1999888777);
+        assertIntPrint(Integer.MAX_VALUE);
+        assertIntPrint(Integer.MIN_VALUE);
 
-        for (int value : testValues) {
-            assertIntegerToStringConversion(value);
-        }
-
-        Random random = new Random(12345L);
+        Random rnd = new Random(12345L);
         for (int i = 0; i < 251000; ++i) {
-            assertIntegerToStringConversion(random.nextInt());
+            assertIntPrint(rnd.nextInt());
         }
     }
 
-    /**
-     * Test the conversion of long values to their string representation.
-     */
     @Test
-    void testLongToStringConversion() throws Exception {
-        long[] edgeCases = {
-            0L, 1L, -1L, Long.MAX_VALUE, Long.MIN_VALUE,
-            Long.MAX_VALUE - 1L, Long.MIN_VALUE + 1L
-        };
+    void longPrinting() throws Exception
+    {
+        // First, let's just cover couple of edge cases
+        assertLongPrint(0L, 0);
+        assertLongPrint(1L, 0);
+        assertLongPrint(-1L, 0);
+        assertLongPrint(Long.MAX_VALUE, 0);
+        assertLongPrint(Long.MIN_VALUE, 0);
+        assertLongPrint(Long.MAX_VALUE-1L, 0);
+        assertLongPrint(Long.MIN_VALUE+1L, 0);
 
-        for (long value : edgeCases) {
-            assertLongToStringConversion(value, 0);
-        }
-
-        Random random = new Random(12345L);
+        Random rnd = new Random(12345L);
+        // Bigger value space, need more iterations for long
         for (int i = 0; i < 678000; ++i) {
-            long randomLong = ((long) random.nextInt() << 32) | random.nextInt();
-            assertLongToStringConversion(randomLong, i);
+            long l = ((long) rnd.nextInt() << 32) | rnd.nextInt();
+            assertLongPrint(l, i);
         }
     }
 
-    /**
-     * Test the division of small integers by 1000.
-     */
+    // // // Tests for divBy1000
+    
     @Test
-    void testDivisionBy1000ForSmallNumbers() {
+    void divBy1000Small()
+    {
         for (int number = 0; number <= 999_999; ++number) {
-            assertDivisionBy1000(number);
+            int expected = number / 1000;
+            int actual = NumberOutput.divBy1000(number);
+            if (expected != actual) { // only construct String if fail
+                fail("With "+number+" should get "+expected+", got: "+actual);
+            }
         }
     }
 
-    /**
-     * Test the division of sampled integers by 1000.
-     */
     @Test
-    void testDivisionBy1000ForSampledNumbers() {
+    void divBy1000Sampled()
+    {
         for (int number = 1_000_000; number > 0; number += 7) {
-            assertDivisionBy1000(number);
+            int expected = number / 1000;
+            int actual = NumberOutput.divBy1000(number);
+            if (expected != actual) { // only construct String if fail
+                fail("With "+number+" should get "+expected+", got: "+actual);
+            }
         }
     }
 
-    /**
-     * Test the division of all integers by 1000.
-     * This test is disabled by default due to its extensive range.
-     */
+    // And then full range, not included in CI since code shouldn't change;
+    // but has been run to verify full range manually
     @Test
-    @Disabled("Disabled for CI. Run manually for full range verification.")
-    void testDivisionBy1000ForFullRange() {
+    // Comment out for manual testing:
+    @Disabled
+    void divBy1000FullRange() {
+        // To get to Integer.MAX_VALUE, need to check for overflow
         for (int number = 0; number >= 0; ++number) {
-            assertDivisionBy1000(number);
+            int expected = number / 1000;
+            int actual = NumberOutput.divBy1000(number);
+            if (expected != actual) { // only construct String if fail
+                fail("With "+number+" should get "+expected+", got: "+actual);
+            }
         }
     }
 
-    // Helper methods
+    /*
+    /**********************************************************
+    /* Internal methods
+    /**********************************************************
+     */
 
-    private void assertIntegerToStringConversion(int value) {
-        String expected = Integer.toString(value);
-        String actual = convertIntToString(value);
+    private void assertIntPrint(int value)
+    {
+        String exp = ""+value;
+        String act = printToString(value);
 
-        assertEquals(expected, actual, 
-            String.format("Mismatch for value %d: expected '%s', got '%s'", value, expected, actual));
-
-        String alternative = NumberOutput.toString(value);
-        assertEquals(expected, alternative, 
-            String.format("Mismatch for value %d: expected '%s', got '%s'", value, expected, alternative));
-    }
-
-    private void assertLongToStringConversion(long value, int index) {
-        String expected = Long.toString(value);
-        String actual = convertLongToString(value);
-
-        assertEquals(expected, actual, 
-            String.format("Mismatch for value %d at index %d: expected '%s', got '%s'", value, index, expected, actual));
-
-        String alternative = NumberOutput.toString(value);
-        assertEquals(expected, alternative, 
-            String.format("Mismatch for value %d at index %d: expected '%s', got '%s'", value, index, expected, alternative));
-    }
-
-    private void assertDivisionBy1000(int number) {
-        int expected = number / 1000;
-        int actual = NumberOutput.divBy1000(number);
-
-        if (expected != actual) {
-            fail(String.format("For number %d: expected %d, got %d", number, expected, actual));
+        if (!exp.equals(act)) {
+            assertEquals(exp, act, "Expected conversion (exp '"+exp+"', len "+exp.length()+"; act len "+act.length()+")");
+        }
+        String alt = NumberOutput.toString(value);
+        if (!exp.equals(alt)) {
+            assertEquals(exp, act, "Expected conversion (exp '"+exp+"', len "+exp.length()+"; act len "+act.length()+")");
         }
     }
 
-    private String convertIntToString(int value) {
+    private void assertLongPrint(long value, int index)
+    {
+        String exp = ""+value;
+        String act = printToString(value);
+
+        if (!exp.equals(act)) {
+            assertEquals(exp, act, "Expected conversion (exp '"+exp+"', len "+exp.length()+"; act len "+act.length()+"; number index "+index+")");
+        }
+        String alt = NumberOutput.toString(value);
+        if (!exp.equals(alt)) {
+            assertEquals(exp, act, "Expected conversion (exp '"+exp+"', len "+exp.length()+"; act len "+act.length()+"; number index "+index+")");
+        }
+    }
+
+    private String printToString(int value)
+    {
         char[] buffer = new char[12];
         int offset = NumberOutput.outputInt(value, buffer, 0);
         return new String(buffer, 0, offset);
     }
 
-    private String convertLongToString(long value) {
+    private String printToString(long value)
+    {
         char[] buffer = new char[22];
         int offset = NumberOutput.outputLong(value, buffer, 0);
         return new String(buffer, 0, offset);
