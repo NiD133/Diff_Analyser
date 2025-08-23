90717511d1b76832f820c839a7ccc6b15647648b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:03:18 +0800

    Original processedAutoTestCases minutes_estest52.java

---
+++ minutes_estest52.java
@@ -0,0 +1,18 @@
+package org.joda.time;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class Minutes_ESTestTest52 extends Minutes_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test51() throws Throwable {
+        Minutes minutes0 = Minutes.ZERO;
+        Minutes minutes1 = minutes0.minus((Minutes) null);
+        assertSame(minutes0, minutes1);
+    }
+}
