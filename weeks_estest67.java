c1d98348bac1570b7d5cd5dab879ac1f6bd57f88
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:27:28 +0800

    gemini processedAutoTestCases weeks_estest67.java

--- weeks_estest67.java
+++ weeks_estest67.java
@@ -1,18 +1,26 @@
 package org.joda.time;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class Weeks_ESTestTest67 extends Weeks_ESTest_scaffolding {
+/**
+ * A test suite for the {@link Weeks} class.
+ */
+public class WeeksTest {
 
-    @Test(timeout = 4000)
-    public void test66() throws Throwable {
-        Weeks weeks0 = Weeks.TWO;
-        PeriodType periodType0 = weeks0.getPeriodType();
-        assertEquals(1, periodType0.size());
+    /**
+     * Tests that getPeriodType() correctly returns the PeriodType for 'weeks'.
+     */
+    @Test
+    public void getPeriodType_shouldReturnWeeksPeriodType() {
+        // Arrange
+        Weeks twoWeeks = Weeks.TWO;
+        PeriodType expectedPeriodType = PeriodType.weeks();
+
+        // Act
+        PeriodType actualPeriodType = twoWeeks.getPeriodType();
+
+        // Assert
+        assertEquals("The period type for Weeks should be 'weeks'", expectedPeriodType, actualPeriodType);
     }
 }
