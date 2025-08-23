65606a591bb6d047595776746c4d692c02d0e996
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:53:26 +0800

    gemini processedAutoTestCases ieee754rutils_estest12.java

--- ieee754rutils_estest12.java
+++ ieee754rutils_estest12.java
@@ -1,17 +1,25 @@
 package org.apache.commons.lang3.math;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class IEEE754rUtils_ESTestTest12 extends IEEE754rUtils_ESTest_scaffolding {
+/**
+ * Test suite for the {@link IEEE754rUtils} class.
+ */
+public class IEEE754rUtilsTest {
 
-    @Test(timeout = 4000)
-    public void test11() throws Throwable {
-        double double0 = IEEE754rUtils.min((-855.02919), 0.0);
-        assertEquals((-855.02919), double0, 0.01);
+    @Test
+    public void min_shouldReturnSmallerValue_whenComparingNegativeAndZero() {
+        // Arrange
+        final double negativeNumber = -855.02919;
+        final double zero = 0.0;
+        final double expectedMinimum = -855.02919;
+
+        // Act
+        final double actualMinimum = IEEE754rUtils.min(negativeNumber, zero);
+
+        // Assert
+        assertEquals("The minimum of a negative number and zero should be the negative number",
+                expectedMinimum, actualMinimum, 0.0);
     }
 }
