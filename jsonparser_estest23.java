5f09f1772d91ba843e89e3146fd3e1a0a295eff1
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:57:13 +0800

    Original processedAutoTestCases jsonparser_estest23.java

---
+++ jsonparser_estest23.java
@@ -0,0 +1,31 @@
+package com.google.gson;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.google.gson.stream.JsonReader;
+import java.io.Reader;
+import java.io.StringReader;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class JsonParser_ESTestTest23 extends JsonParser_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test22() throws Throwable {
+        JsonParser jsonParser0 = new JsonParser();
+        StringReader stringReader0 = new StringReader("}pm");
+        JsonReader jsonReader0 = new JsonReader(stringReader0);
+        try {
+            jsonParser0.parse(jsonReader0);
+            fail("Expecting exception: RuntimeException");
+        } catch (RuntimeException e) {
+            //
+            // org.evosuite.runtime.mock.java.lang.MockThrowable: Expected value at line 1 column 1 path $
+            // See https://github.com/google/gson/blob/main/Troubleshooting.md#malformed-json
+            //
+            verifyException("com.google.gson.internal.Streams", e);
+        }
+    }
+}
