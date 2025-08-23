3aaace9d8c2bc86c0a06c3de336083bc64f4fdca
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:09:26 +0800

    Original processedAutoTestCases predicatedmap_estest22.java

---
+++ predicatedmap_estest22.java
@@ -0,0 +1,46 @@
+package org.apache.commons.collections4.map;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.lang.reflect.Array;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import org.apache.commons.collections4.Predicate;
+import org.apache.commons.collections4.Transformer;
+import org.apache.commons.collections4.functors.AnyPredicate;
+import org.apache.commons.collections4.functors.ComparatorPredicate;
+import org.apache.commons.collections4.functors.ConstantTransformer;
+import org.apache.commons.collections4.functors.EqualPredicate;
+import org.apache.commons.collections4.functors.ExceptionPredicate;
+import org.apache.commons.collections4.functors.IdentityPredicate;
+import org.apache.commons.collections4.functors.NonePredicate;
+import org.apache.commons.collections4.functors.NotNullPredicate;
+import org.apache.commons.collections4.functors.NullPredicate;
+import org.apache.commons.collections4.functors.OnePredicate;
+import org.apache.commons.collections4.functors.OrPredicate;
+import org.apache.commons.collections4.functors.PredicateTransformer;
+import org.apache.commons.collections4.functors.TransformerPredicate;
+import org.apache.commons.collections4.functors.TruePredicate;
+import org.apache.commons.collections4.functors.UniquePredicate;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class PredicatedMap_ESTestTest22 extends PredicatedMap_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test21() throws Throwable {
+        HashMap<Object, HashMap<Integer, Integer>> hashMap0 = new HashMap<Object, HashMap<Integer, Integer>>();
+        HashMap<Integer, Integer> hashMap1 = new HashMap<Integer, Integer>();
+        HashMap<Integer, Integer> hashMap2 = hashMap0.put((Object) null, hashMap1);
+        PredicatedMap<Object, HashMap<Integer, Integer>> predicatedMap0 = new PredicatedMap<Object, HashMap<Integer, Integer>>(hashMap0, (Predicate<? super Object>) null, (Predicate<? super HashMap<Integer, Integer>>) null);
+        HashMap<Integer, Integer> hashMap3 = predicatedMap0.put((Object) null, hashMap2);
+        assertEquals(0, hashMap3.size());
+    }
+}
