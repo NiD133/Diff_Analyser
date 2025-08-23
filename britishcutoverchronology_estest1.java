1bb60040b236f4023930b890d0719edca5fb3f0e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:39:43 +0800

    Original processedAutoTestCases britishcutoverchronology_estest1.java

---
+++ britishcutoverchronology_estest1.java
@@ -0,0 +1,56 @@
+package org.threeten.extra.chrono;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.time.Clock;
+import java.time.DateTimeException;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.chrono.ChronoLocalDateTime;
+import java.time.chrono.ChronoPeriod;
+import java.time.chrono.ChronoZonedDateTime;
+import java.time.chrono.Era;
+import java.time.chrono.IsoEra;
+import java.time.chrono.JapaneseEra;
+import java.time.chrono.MinguoEra;
+import java.time.chrono.ThaiBuddhistEra;
+import java.time.format.ResolverStyle;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAccessor;
+import java.time.temporal.TemporalAmount;
+import java.time.temporal.TemporalField;
+import java.time.temporal.ValueRange;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.time.MockClock;
+import org.evosuite.runtime.mock.java.time.MockInstant;
+import org.evosuite.runtime.mock.java.time.MockZonedDateTime;
+import org.junit.runner.RunWith;
+
+public class BritishCutoverChronology_ESTestTest1 extends BritishCutoverChronology_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test00() throws Throwable {
+        BritishCutoverChronology britishCutoverChronology0 = new BritishCutoverChronology();
+        JulianEra julianEra0 = JulianEra.AD;
+        // Undeclared exception!
+        try {
+            britishCutoverChronology0.date((Era) julianEra0, 6, 1000, 1);
+            fail("Expecting exception: DateTimeException");
+        } catch (DateTimeException e) {
+            //
+            // Invalid value for MonthOfYear (valid values 1 - 12): 1000
+            //
+            verifyException("java.time.temporal.ValueRange", e);
+        }
+    }
+}
