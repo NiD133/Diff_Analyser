ab9e9c503d321667908646df3e673846ba75bfe4
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:30:17 +0800

    gemini processedManualTestCases jsontreewritertest16.java

--- jsontreewritertest16.java
+++ jsontreewritertest16.java
@@ -1,24 +1,27 @@
 package com.google.gson.internal.bind;
 
 import static com.google.common.truth.Truth.assertThat;
-import static org.junit.Assert.assertThrows;
+
 import com.google.gson.JsonElement;
-import com.google.gson.JsonNull;
-import com.google.gson.Strictness;
-import com.google.gson.common.MoreAsserts;
-import com.google.gson.stream.JsonWriter;
+import com.google.gson.JsonPrimitive;
 import java.io.IOException;
-import java.io.Writer;
-import java.util.Arrays;
-import java.util.List;
 import org.junit.Test;
 
-public class JsonTreeWriterTestTest16 {
+/**
+ * Tests for {@link JsonTreeWriter}.
+ */
+public class JsonTreeWriterTest {
 
-    @Test
-    public void testBoolValue() throws Exception {
-        JsonTreeWriter writer = new JsonTreeWriter();
-        boolean bool = true;
-        assertThat(writer.value(bool)).isEqualTo(writer);
-    }
+  @Test
+  public void value_withBoolean_writesBooleanPrimitive() throws IOException {
+    // Arrange
+    JsonTreeWriter writer = new JsonTreeWriter();
+
+    // Act
+    writer.value(true);
+
+    // Assert
+    JsonElement result = writer.get();
+    assertThat(result).isEqualTo(new JsonPrimitive(true));
+  }
 }
