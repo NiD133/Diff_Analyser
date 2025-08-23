a8519e5708dab1e7d7a5f4a48ef6eefebd26f3e4
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:07:28 +0800

    gemini processedAutoTestCases parameter_estest8.java

--- parameter_estest8.java
+++ parameter_estest8.java
@@ -1,28 +1,27 @@
 package com.google.common.reflect;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
 import java.lang.annotation.Annotation;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class Parameter_ESTestTest8 extends Parameter_ESTest_scaffolding {
+/**
+ * Unit tests for {@link Parameter}.
+ */
+public class ParameterTest {
 
-    @Test(timeout = 4000)
-    public void test07() throws Throwable {
-        Parameter parameter0 = null;
-        try {
-            parameter0 = new Parameter((Invokable<?, ?>) null, 14, (TypeToken<?>) null, (Annotation[]) null, (Object) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("com.google.common.collect.ImmutableList", e);
-        }
+    /**
+     * Verifies that the Parameter constructor throws a NullPointerException
+     * when the provided annotations array is null. This is expected because the constructor
+     * uses this array to create an ImmutableList, which does not accept null collections.
+     */
+    @Test(expected = NullPointerException.class)
+    public void constructor_withNullAnnotationsArray_throwsNullPointerException() {
+        // The constructor is expected to fail immediately when processing the null annotations array.
+        // Therefore, the other arguments can be dummy values for this specific test case.
+        new Parameter(
+            /* declaration */ null,
+            /* position */ 0,
+            /* type */ null,
+            /* annotations */ (Annotation[]) null,
+            /* annotatedType */ null);
     }
 }
