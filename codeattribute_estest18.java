adb4f4b31322d4cc6262b0a5d7b422be3f7d803d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:43:48 +0800

    Original processedAutoTestCases codeattribute_estest18.java

---
+++ codeattribute_estest18.java
@@ -0,0 +1,39 @@
+package org.apache.commons.compress.harmony.unpack200.bytecode;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.PipedOutputStream;
+import java.util.LinkedList;
+import java.util.List;
+import org.apache.commons.compress.harmony.unpack200.Segment;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockFileOutputStream;
+import org.junit.runner.RunWith;
+
+public class CodeAttribute_ESTestTest18 extends CodeAttribute_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test17() throws Throwable {
+        byte[] byteArray0 = new byte[4];
+        OperandManager operandManager0 = new OperandManager((int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null);
+        LinkedList<ExceptionTableEntry> linkedList0 = new LinkedList<ExceptionTableEntry>();
+        CodeAttribute codeAttribute0 = new CodeAttribute((-2553), 85, byteArray0, (Segment) null, operandManager0, linkedList0);
+        CPUTF8 cPUTF8_0 = new CPUTF8("i2f", 38);
+        CPClass cPClass0 = new CPClass(cPUTF8_0, 85);
+        ExceptionTableEntry exceptionTableEntry0 = new ExceptionTableEntry(87, 38, 87, cPClass0);
+        linkedList0.add(exceptionTableEntry0);
+        ClassConstantPool classConstantPool0 = new ClassConstantPool();
+        classConstantPool0.resolve((Segment) null);
+        codeAttribute0.resolve(classConstantPool0);
+        MockFileOutputStream mockFileOutputStream0 = new MockFileOutputStream("$0'{");
+        DataOutputStream dataOutputStream0 = new DataOutputStream(mockFileOutputStream0);
+        codeAttribute0.writeBody(dataOutputStream0);
+        assertEquals(4, codeAttribute0.codeLength);
+        assertEquals(85, codeAttribute0.maxLocals);
+        assertEquals((-2553), codeAttribute0.maxStack);
+    }
+}
