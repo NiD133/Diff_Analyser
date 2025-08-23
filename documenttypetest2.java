979d30ddd5bbac3441c3f80c54cde5ffaec6ab12
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:28:04 +0800

    Original processedManualTestCases documenttypetest2.java

---
+++ documenttypetest2.java
@@ -0,0 +1,24 @@
+package org.jsoup.nodes;
+
+import org.jsoup.Jsoup;
+import org.jsoup.parser.Parser;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class DocumentTypeTestTest2 {
+
+    private String htmlOutput(String in) {
+        DocumentType type = (DocumentType) Jsoup.parse(in).childNode(0);
+        return type.outerHtml();
+    }
+
+    private String xmlOutput(String in) {
+        return Jsoup.parse(in, "", Parser.xmlParser()).childNode(0).outerHtml();
+    }
+
+    @Test
+    public void constructorValidationThrowsExceptionOnNulls() {
+        assertThrows(IllegalArgumentException.class, () -> new DocumentType("html", null, null));
+    }
+}
