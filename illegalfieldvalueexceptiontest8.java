067e74161f15da148eab597f1c83eed348d029d0
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:29:23 +0800

    gemini processedManualTestCases illegalfieldvalueexceptiontest8.java

--- illegalfieldvalueexceptiontest8.java
+++ illegalfieldvalueexceptiontest8.java
@@ -1,51 +1,81 @@
 package org.joda.time;
 
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
-import org.joda.time.chrono.GJChronology;
-import org.joda.time.chrono.ISOChronology;
-import org.joda.time.chrono.JulianChronology;
-import org.joda.time.field.FieldUtils;
-import org.joda.time.field.SkipDateTimeField;
 
-public class IllegalFieldValueExceptionTestTest8 extends TestCase {
+/**
+ * Unit tests for the constructors of {@link IllegalFieldValueException}.
+ * <p>
+ * This class focuses on constructors that are not commonly called by other parts of the Joda-Time library,
+ * ensuring they correctly initialize the exception's state.
+ */
+public class TestIllegalFieldValueException extends TestCase {
 
-    public static void main(String[] args) {
-        junit.textui.TestRunner.run(suite());
+    /**
+     * Tests the constructor that accepts a DurationFieldType and numeric bounds.
+     */
+    public void testConstructorWithDurationFieldTypeAndBounds() {
+        // Arrange
+        final DurationFieldType fieldType = DurationFieldType.days();
+        final Number illegalValue = Integer.valueOf(1);
+        final Number lowerBound = Integer.valueOf(2);
+        final Number upperBound = Integer.valueOf(3);
+
+        // Act
+        IllegalFieldValueException exception = new IllegalFieldValueException(
+                fieldType, illegalValue, lowerBound, upperBound);
+
+        // Assert
+        assertNull("DateTimeFieldType should be null", exception.getDateTimeFieldType());
+        assertEquals("DurationFieldType should be set correctly", fieldType, exception.getDurationFieldType());
+        assertEquals("Field name should be derived from the field type", "days", exception.getFieldName());
+        assertEquals("Illegal number value should be set correctly", illegalValue, exception.getIllegalNumberValue());
+        assertNull("Illegal string value should be null", exception.getIllegalStringValue());
+        assertEquals("Illegal value as string should be correct", "1", exception.getIllegalValueAsString());
+        assertEquals("Lower bound should be set correctly", lowerBound, exception.getLowerBound());
+        assertEquals("Upper bound should be set correctly", upperBound, exception.getUpperBound());
     }
 
-    public static TestSuite suite() {
-        return new TestSuite(TestIllegalFieldValueException.class);
+    /**
+     * Tests the constructor that accepts a DurationFieldType and a string value.
+     */
+    public void testConstructorWithDurationFieldTypeAndStringValue() {
+        // Arrange
+        final DurationFieldType fieldType = DurationFieldType.months();
+        final String illegalValue = "five";
+
+        // Act
+        IllegalFieldValueException exception = new IllegalFieldValueException(fieldType, illegalValue);
+
+        // Assert
+        assertNull("DateTimeFieldType should be null", exception.getDateTimeFieldType());
+        assertEquals("DurationFieldType should be set correctly", fieldType, exception.getDurationFieldType());
+        assertEquals("Field name should be derived from the field type", "months", exception.getFieldName());
+        assertNull("Illegal number value should be null", exception.getIllegalNumberValue());
+        assertEquals("Illegal string value should be set correctly", illegalValue, exception.getIllegalStringValue());
+        assertEquals("Illegal value as string should be correct", "five", exception.getIllegalValueAsString());
+        assertNull("Lower bound should be null", exception.getLowerBound());
+        assertNull("Upper bound should be null", exception.getUpperBound());
     }
 
-    // Test extra constructors not currently called by anything
-    public void testOtherConstructors() {
-        IllegalFieldValueException e = new IllegalFieldValueException(DurationFieldType.days(), new Integer(1), new Integer(2), new Integer(3));
-        assertEquals(null, e.getDateTimeFieldType());
-        assertEquals(DurationFieldType.days(), e.getDurationFieldType());
-        assertEquals("days", e.getFieldName());
-        assertEquals(new Integer(1), e.getIllegalNumberValue());
-        assertEquals(null, e.getIllegalStringValue());
-        assertEquals("1", e.getIllegalValueAsString());
-        assertEquals(new Integer(2), e.getLowerBound());
-        assertEquals(new Integer(3), e.getUpperBound());
-        e = new IllegalFieldValueException(DurationFieldType.months(), "five");
-        assertEquals(null, e.getDateTimeFieldType());
-        assertEquals(DurationFieldType.months(), e.getDurationFieldType());
-        assertEquals("months", e.getFieldName());
-        assertEquals(null, e.getIllegalNumberValue());
-        assertEquals("five", e.getIllegalStringValue());
-        assertEquals("five", e.getIllegalValueAsString());
-        assertEquals(null, e.getLowerBound());
-        assertEquals(null, e.getUpperBound());
-        e = new IllegalFieldValueException("months", "five");
-        assertEquals(null, e.getDateTimeFieldType());
-        assertEquals(null, e.getDurationFieldType());
-        assertEquals("months", e.getFieldName());
-        assertEquals(null, e.getIllegalNumberValue());
-        assertEquals("five", e.getIllegalStringValue());
-        assertEquals("five", e.getIllegalValueAsString());
-        assertEquals(null, e.getLowerBound());
-        assertEquals(null, e.getUpperBound());
+    /**
+     * Tests the constructor that accepts a field name and a string value.
+     */
+    public void testConstructorWithStringFieldNameAndStringValue() {
+        // Arrange
+        final String fieldName = "months";
+        final String illegalValue = "five";
+
+        // Act
+        IllegalFieldValueException exception = new IllegalFieldValueException(fieldName, illegalValue);
+
+        // Assert
+        assertNull("DateTimeFieldType should be null", exception.getDateTimeFieldType());
+        assertNull("DurationFieldType should be null", exception.getDurationFieldType());
+        assertEquals("Field name should be set correctly", fieldName, exception.getFieldName());
+        assertNull("Illegal number value should be null", exception.getIllegalNumberValue());
+        assertEquals("Illegal string value should be set correctly", illegalValue, exception.getIllegalStringValue());
+        assertEquals("Illegal value as string should be correct", "five", exception.getIllegalValueAsString());
+        assertNull("Lower bound should be null", exception.getLowerBound());
+        assertNull("Upper bound should be null", exception.getUpperBound());
     }
 }
