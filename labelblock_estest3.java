f9cbb23408acb56c5d71e9f2f26e28f9c48a30ec
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:59:41 +0800

    Original processedAutoTestCases labelblock_estest3.java

---
+++ labelblock_estest3.java
@@ -0,0 +1,38 @@
+package org.jfree.chart.block;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.Paint;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.SystemColor;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.text.AttributedCharacterIterator;
+import java.util.Hashtable;
+import javax.swing.text.DefaultStyledDocument;
+import javax.swing.text.StyleContext;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.jfree.chart.api.RectangleAnchor;
+import org.jfree.chart.text.TextBlockAnchor;
+import org.jfree.data.Range;
+import org.junit.runner.RunWith;
+
+public class LabelBlock_ESTestTest3 extends LabelBlock_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test02() throws Throwable {
+        LabelBlock labelBlock0 = new LabelBlock("/s");
+        labelBlock0.setToolTipText("mpAJ'YV3YX;OK");
+        String string0 = labelBlock0.getToolTipText();
+        assertEquals("mpAJ'YV3YX;OK", string0);
+    }
+}
