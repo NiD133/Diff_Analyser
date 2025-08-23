ec0b4346741f989ed30536b3fa9a027daade32cb
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:32:17 +0800

    Original processedManualTestCases percentcodectest9.java

---
+++ percentcodectest9.java
@@ -0,0 +1,22 @@
+package org.apache.commons.codec.net;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.EncoderException;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class PercentCodecTestTest9 {
+
+    @Test
+    void testInvalidByte() throws Exception {
+        final byte[] invalid = { (byte) -1, (byte) 'A' };
+        assertThrows(IllegalArgumentException.class, () -> new PercentCodec(invalid, true));
+    }
+}
