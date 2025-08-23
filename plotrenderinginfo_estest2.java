21733633149e8a8d5d00e4e1142355cecb2c1b4e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:08:59 +0800

    gemini processedAutoTestCases plotrenderinginfo_estest2.java

--- plotrenderinginfo_estest2.java
+++ plotrenderinginfo_estest2.java
@@ -1,31 +1,43 @@
 package org.jfree.chart.plot;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.awt.Dimension;
-import java.awt.Point;
-import java.awt.Rectangle;
-import java.awt.geom.Line2D;
-import java.awt.geom.Point2D;
-import java.awt.geom.Rectangle2D;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
 import org.jfree.chart.ChartRenderingInfo;
-import org.jfree.chart.entity.EntityCollection;
-import org.jfree.chart.entity.StandardEntityCollection;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class PlotRenderingInfo_ESTestTest2 extends PlotRenderingInfo_ESTest_scaffolding {
+import java.awt.geom.Rectangle2D;
 
-    @Test(timeout = 4000)
-    public void test01() throws Throwable {
-        ChartRenderingInfo chartRenderingInfo0 = new ChartRenderingInfo();
-        PlotRenderingInfo plotRenderingInfo0 = chartRenderingInfo0.getPlotInfo();
-        Point point0 = new Point(4047, 4047);
-        Line2D.Float line2D_Float0 = new Line2D.Float(point0, point0);
-        Rectangle2D rectangle2D0 = line2D_Float0.getBounds2D();
-        plotRenderingInfo0.setPlotArea(rectangle2D0);
-        plotRenderingInfo0.hashCode();
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+/**
+ * Unit tests for the hashCode() method in the {@link PlotRenderingInfo} class.
+ */
+public class PlotRenderingInfo_ESTestTest2 {
+
+    /**
+     * Verifies that the hashCode is consistent and changes when the plot area is modified.
+     * A consistent hashCode is essential for the correct functioning of hash-based collections
+     * like HashMap or HashSet.
+     */
+    @Test
+    public void hashCode_shouldBeConsistentAndReflectPlotAreaChanges() {
+        // Arrange: Create a PlotRenderingInfo instance and get its initial hashCode.
+        ChartRenderingInfo chartInfo = new ChartRenderingInfo();
+        PlotRenderingInfo plotInfo = chartInfo.getPlotInfo();
+        int initialHashCode = plotInfo.hashCode();
+
+        // Act: Modify the state of the object by setting a new plot area.
+        Rectangle2D plotArea = new Rectangle2D.Double(10.0, 20.0, 30.0, 40.0);
+        plotInfo.setPlotArea(plotArea);
+        int newHashCode = plotInfo.hashCode();
+
+        // Assert:
+        // 1. The hashCode must change when a property it depends on is modified.
+        assertNotEquals("HashCode should change after setting the plot area.",
+                initialHashCode, newHashCode);
+
+        // 2. The hashCode must be consistent (return the same value) across multiple calls
+        //    if the object's state has not changed.
+        assertEquals("HashCode should be consistent on subsequent calls.",
+                newHashCode, plotInfo.hashCode());
     }
 }
