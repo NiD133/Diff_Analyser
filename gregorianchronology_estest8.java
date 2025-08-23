e2f06bd96aefbf639b626e70b9d15cb24a2d89b4
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:52:12 +0800

    Original processedAutoTestCases gregorianchronology_estest8.java

---
+++ gregorianchronology_estest8.java
@@ -0,0 +1,31 @@
+package org.joda.time.chrono;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.TimeZone;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.joda.time.Chronology;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.LocalDateTime;
+import org.junit.runner.RunWith;
+
+public class GregorianChronology_ESTestTest8 extends GregorianChronology_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test07() throws Throwable {
+        GregorianChronology gregorianChronology0 = GregorianChronology.getInstanceUTC();
+        // Undeclared exception!
+        try {
+            gregorianChronology0.getDaysInYearMonth((-2764), (-2764));
+            fail("Expecting exception: ArrayIndexOutOfBoundsException");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            //
+            // -2765
+            //
+            verifyException("org.joda.time.chrono.BasicGJChronology", e);
+        }
+    }
+}
