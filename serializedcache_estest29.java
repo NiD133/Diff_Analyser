7570bf7b4f98624d7b96e1c5f1ab78b0a4c60beb
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:14:55 +0800

    Original processedAutoTestCases serializedcache_estest29.java

---
+++ serializedcache_estest29.java
@@ -0,0 +1,27 @@
+package org.apache.ibatis.cache.decorators;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.EOFException;
+import java.io.SequenceInputStream;
+import java.util.Enumeration;
+import org.apache.ibatis.cache.Cache;
+import org.apache.ibatis.cache.impl.PerpetualCache;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.io.MockFileInputStream;
+import org.junit.runner.RunWith;
+
+public class SerializedCache_ESTestTest29 extends SerializedCache_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test28() throws Throwable {
+        PerpetualCache perpetualCache0 = new PerpetualCache("org.apache.ibatis.exceptions.PersistenceException");
+        SerializedCache serializedCache0 = new SerializedCache(perpetualCache0);
+        Object object0 = serializedCache0.removeObject("org.apache.ibatis.exceptions.PersistenceException");
+        assertNull(object0);
+    }
+}
