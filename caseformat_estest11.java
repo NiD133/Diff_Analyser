5133d6d090a49682815eed504373906522413737
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:41:37 +0800

    gemini processedAutoTestCases caseformat_estest11.java

--- caseformat_estest11.java
+++ caseformat_estest11.java
@@ -1,26 +1,16 @@
 package com.google.common.base;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class CaseFormat_ESTestTest11 extends CaseFormat_ESTest_scaffolding {
+/**
+ * Tests for {@link CaseFormat}.
+ */
+public class CaseFormatTest {
 
-    @Test(timeout = 4000)
-    public void test10() throws Throwable {
-        CaseFormat caseFormat0 = CaseFormat.LOWER_CAMEL;
-        // Undeclared exception!
-        try {
-            caseFormat0.converterTo((CaseFormat) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("com.google.common.base.Preconditions", e);
-        }
+    @Test(expected = NullPointerException.class)
+    public void converterTo_shouldThrowNullPointerException_whenTargetFormatIsNull() {
+        // The converterTo method relies on Preconditions.checkNotNull for its target format.
+        // Therefore, passing null should result in a NullPointerException.
+        CaseFormat.LOWER_CAMEL.converterTo(null);
     }
 }
