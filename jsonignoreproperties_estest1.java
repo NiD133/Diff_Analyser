f2361ab00e5c599649d64cffa81cb60744294996
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:56:24 +0800

    gemini processedAutoTestCases jsonignoreproperties_estest1.java

--- jsonignoreproperties_estest1.java
+++ jsonignoreproperties_estest1.java
@@ -2,34 +2,59 @@ package com.fasterxml.jackson.annotation;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import java.util.LinkedHashSet;
-import java.util.Set;
-import java.util.function.Predicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
 
-public class JsonIgnoreProperties_ESTestTest1 extends JsonIgnoreProperties_ESTest_scaffolding {
+/**
+ * This test focuses on the merging behavior of {@link JsonIgnoreProperties.Value},
+ * specifically how properties are combined when using the {@code mergeAll} method.
+ */
+public class JsonIgnorePropertiesValueMergeTest {
 
-    @Test(timeout = 4000)
-    public void test00() throws Throwable {
-        String[] stringArray0 = new String[1];
-        JsonIgnoreProperties.Value jsonIgnoreProperties_Value0 = JsonIgnoreProperties.Value.forIgnoredProperties(stringArray0);
-        JsonIgnoreProperties.Value jsonIgnoreProperties_Value1 = jsonIgnoreProperties_Value0.withAllowSetters();
-        JsonIgnoreProperties.Value jsonIgnoreProperties_Value2 = jsonIgnoreProperties_Value0.withOverrides(jsonIgnoreProperties_Value1);
-        assertFalse(jsonIgnoreProperties_Value2.getIgnoreUnknown());
-        assertFalse(jsonIgnoreProperties_Value2.getAllowGetters());
-        JsonIgnoreProperties.Value jsonIgnoreProperties_Value3 = jsonIgnoreProperties_Value0.withoutMerge();
-        assertFalse(jsonIgnoreProperties_Value3.getMerge());
-        assertFalse(jsonIgnoreProperties_Value3.getAllowSetters());
-        JsonIgnoreProperties.Value[] jsonIgnoreProperties_ValueArray0 = new JsonIgnoreProperties.Value[2];
-        jsonIgnoreProperties_ValueArray0[0] = jsonIgnoreProperties_Value3;
-        jsonIgnoreProperties_ValueArray0[1] = jsonIgnoreProperties_Value2;
-        JsonIgnoreProperties.Value jsonIgnoreProperties_Value4 = JsonIgnoreProperties.Value.mergeAll(jsonIgnoreProperties_ValueArray0);
-        assertNotSame(jsonIgnoreProperties_Value4, jsonIgnoreProperties_Value1);
-        assertTrue(jsonIgnoreProperties_Value4.equals((Object) jsonIgnoreProperties_Value1));
-        assertTrue(jsonIgnoreProperties_Value4.getAllowSetters());
+    /**
+     * Tests that when merging multiple {@link JsonIgnoreProperties.Value} instances,
+     * the properties from later instances in the sequence override those from earlier ones.
+     *
+     * This test merges two configurations:
+     * 1. The first has 'merge' explicitly disabled.
+     * 2. The second has 'allowSetters' enabled.
+     *
+     * The expected result is a new configuration that reflects the properties of the second
+     * instance, effectively having 'allowSetters' enabled and the default 'merge' behavior (enabled).
+     */
+    @Test
+    public void mergeAllShouldPrioritizePropertiesFromLaterValues() {
+        // Arrange: Create two conflicting Value instances.
+        String[] ignored = {"testProp"};
+
+        // First value: has merging disabled.
+        JsonIgnoreProperties.Value valueWithMergeDisabled = JsonIgnoreProperties.Value
+                .forIgnoredProperties(ignored)
+                .withoutMerge();
+
+        // Second value: has allowSetters enabled (and default merge=true).
+        JsonIgnoreProperties.Value valueWithAllowSettersEnabled = JsonIgnoreProperties.Value
+                .forIgnoredProperties(ignored)
+                .withAllowSetters();
+
+        JsonIgnoreProperties.Value[] valuesToMerge = {valueWithMergeDisabled, valueWithAllowSettersEnabled};
+
+        // Act: Merge the two values.
+        JsonIgnoreProperties.Value mergedValue = JsonIgnoreProperties.Value.mergeAll(valuesToMerge);
+
+        // Assert: The merged result should be equivalent to the second value, as it came later.
+        // It should be a new instance but logically equal.
+        assertNotSame("A new Value instance should be created",
+                valueWithAllowSettersEnabled, mergedValue);
+        assertEquals("Merged value should be logically equal to the last value in the sequence",
+                valueWithAllowSettersEnabled, mergedValue);
+
+        // For clarity, explicitly verify the key properties of the merged result.
+        assertTrue("allowSetters should be true, as taken from the second value",
+                mergedValue.getAllowSetters());
+        assertTrue("merge should be true, as the second value's default overrides the first's",
+                mergedValue.getMerge());
+        assertFalse("ignoreUnknown should retain its default value of false",
+                mergedValue.getIgnoreUnknown());
+        assertFalse("allowGetters should retain its default value of false",
+                mergedValue.getAllowGetters());
     }
 }
