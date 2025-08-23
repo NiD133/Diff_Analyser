355b919ee1125a56883ad17808242c0fa9066e38
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:23:20 +0800

    gemini processedAutoTestCases transformedmultivaluedmap_estest32.java

--- transformedmultivaluedmap_estest32.java
+++ transformedmultivaluedmap_estest32.java
@@ -1,59 +1,37 @@
 package org.apache.commons.collections4.multimap;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.AbstractMap;
-import java.util.ArrayDeque;
-import java.util.Comparator;
-import java.util.ConcurrentModificationException;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
-import java.util.PriorityQueue;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.Factory;
 import org.apache.commons.collections4.MultiValuedMap;
-import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AllPredicate;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ChainedTransformer;
-import org.apache.commons.collections4.functors.CloneTransformer;
-import org.apache.commons.collections4.functors.ClosureTransformer;
-import org.apache.commons.collections4.functors.ConstantFactory;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.ExceptionFactory;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.FactoryTransformer;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.MapTransformer;
-import org.apache.commons.collections4.functors.NOPTransformer;
-import org.apache.commons.collections4.functors.NotPredicate;
-import org.apache.commons.collections4.functors.NullIsExceptionPredicate;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.SwitchTransformer;
-import org.apache.commons.collections4.functors.TransformerClosure;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class TransformedMultiValuedMap_ESTestTest32 extends TransformedMultiValuedMap_ESTest_scaffolding {
+import java.util.Collections;
+import java.util.Map;
 
-    @Test(timeout = 4000)
-    public void test31() throws Throwable {
-        LinkedHashSetValuedLinkedHashMap<Object, Integer> linkedHashSetValuedLinkedHashMap0 = new LinkedHashSetValuedLinkedHashMap<Object, Integer>(2566);
-        Predicate<Object>[] predicateArray0 = (Predicate<Object>[]) Array.newInstance(Predicate.class, 0);
-        Transformer<Object, Integer>[] transformerArray0 = (Transformer<Object, Integer>[]) Array.newInstance(Transformer.class, 1);
-        HashMap<Object, Integer> hashMap0 = new HashMap<Object, Integer>();
-        Transformer<Object, Integer> transformer0 = MapTransformer.mapTransformer((Map<? super Object, ? extends Integer>) hashMap0);
-        SwitchTransformer<Object, Integer> switchTransformer0 = new SwitchTransformer<Object, Integer>(predicateArray0, transformerArray0, transformer0);
-        TransformedMultiValuedMap<Object, Integer> transformedMultiValuedMap0 = TransformedMultiValuedMap.transformedMap((MultiValuedMap<Object, Integer>) linkedHashSetValuedLinkedHashMap0, (Transformer<? super Object, ?>) switchTransformer0, (Transformer<? super Integer, ? extends Integer>) transformer0);
-        boolean boolean0 = transformedMultiValuedMap0.putAll((Map<?, ? extends Integer>) hashMap0);
-        assertFalse(boolean0);
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Test suite for {@link TransformedMultiValuedMap}.
+ */
+public class TransformedMultiValuedMapTest {
+
+    /**
+     * Tests that calling putAll() with an empty Map does not modify the
+     * underlying map and returns false.
+     */
+    @Test
+    public void putAllWithEmptyMapShouldReturnFalse() {
+        // Arrange
+        final MultiValuedMap<String, Integer> baseMap = new ArrayListValuedHashMap<>();
+        final TransformedMultiValuedMap<String, Integer> transformedMap =
+                TransformedMultiValuedMap.transformingMap(baseMap, null, null);
+
+        final Map<String, Integer> emptyMapToPut = Collections.emptyMap();
+
+        // Act
+        final boolean wasModified = transformedMap.putAll(emptyMapToPut);
+
+        // Assert
+        assertFalse("putAll with an empty map should return false as no changes were made", wasModified);
+        assertTrue("The transformed map should remain empty", transformedMap.isEmpty());
     }
 }
