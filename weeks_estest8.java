c00a256fb06e42a1223ab37ea975b07c1e73cd71
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:27:31 +0800

    gemini processedAutoTestCases weeks_estest8.java

--- weeks_estest8.java
+++ weeks_estest8.java
@@ -1,19 +1,33 @@
 package org.joda.time;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class Weeks_ESTestTest8 extends Weeks_ESTest_scaffolding {
+/**
+ * Test suite for the {@link Weeks} class, focusing on conversion methods.
+ */
+public class WeeksTest {
 
-    @Test(timeout = 4000)
-    public void test07() throws Throwable {
-        Weeks weeks0 = Weeks.weeksIn((ReadableInterval) null);
-        Weeks weeks1 = weeks0.minus(3765);
-        Minutes minutes0 = weeks1.toStandardMinutes();
-        assertEquals((-37951200), minutes0.getMinutes());
+    /**
+     * Tests that converting a negative number of weeks to standard minutes
+     * produces the correct negative minute count.
+     */
+    @Test
+    public void toStandardMinutes_forNegativeWeeks_returnsCorrectlyCalculatedMinutes() {
+        // Arrange
+        final int numberOfWeeks = -3765;
+        Weeks negativeWeeks = Weeks.weeks(numberOfWeeks);
+
+        // The expected number of minutes is calculated based on standard conversions.
+        // 1 week = 7 days, 1 day = 24 hours, 1 hour = 60 minutes.
+        final int MINUTES_PER_WEEK = 7 * 24 * 60;
+        final int expectedMinutes = numberOfWeeks * MINUTES_PER_WEEK; // -3765 * 10080 = -37951200
+
+        // Act
+        Minutes actualMinutes = negativeWeeks.toStandardMinutes();
+
+        // Assert
+        assertEquals("The total number of minutes should be correctly calculated.",
+                expectedMinutes, actualMinutes.getMinutes());
     }
 }
