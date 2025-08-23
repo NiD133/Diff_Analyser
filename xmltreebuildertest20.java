9650b861f7c2a50047f60a6c19f44bd00a2fdbef
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:36:10 +0800

    gemini processedManualTestCases xmltreebuildertest20.java

--- xmltreebuildertest20.java
+++ xmltreebuildertest20.java
@@ -1,33 +1,31 @@
 package org.jsoup.parser;
 
 import org.jsoup.Jsoup;
-import org.jsoup.TextUtil;
-import org.jsoup.nodes.*;
-import org.jsoup.select.Elements;
+import org.jsoup.nodes.Document;
 import org.junit.jupiter.api.Test;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URISyntaxException;
-import java.nio.charset.StandardCharsets;
-import java.util.List;
-import static org.jsoup.nodes.Document.OutputSettings.Syntax;
-import static org.jsoup.parser.Parser.NamespaceHtml;
-import static org.jsoup.parser.Parser.NamespaceXml;
-import static org.junit.jupiter.api.Assertions.*;
 
-public class XmlTreeBuilderTestTest20 {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-    private static void assertXmlNamespace(Element el) {
-        assertEquals(NamespaceXml, el.tag().namespace(), String.format("Element %s not in XML namespace", el.tagName()));
-    }
+/**
+ * Tests for the {@link XmlTreeBuilder}.
+ */
+public class XmlTreeBuilderTest {
 
     @Test
-    public void normalizesDiscordantTags() {
+    void xmlParserWithHtmlSettingsNormalizesTagCase() {
+        // Arrange
+        String htmlWithMixedCaseTags = "<div>test</DIV><p></p>";
+        String expectedHtml = "<div>test</div><p></p>";
+
+        // This is a specific edge case: using an XML parser, but with HTML-style settings
+        // that normalize tag names to lowercase.
         Parser parser = Parser.xmlParser().settings(ParseSettings.htmlDefault);
-        Document document = Jsoup.parse("<div>test</DIV><p></p>", "", parser);
-        assertEquals("<div>test</div><p></p>", document.html());
-        // was failing -> toString() = "<div>\n test\n <p></p>\n</div>"
+
+        // Act
+        Document document = Jsoup.parse(htmlWithMixedCaseTags, "", parser);
+        String actualHtml = document.html();
+
+        // Assert
+        assertEquals(expectedHtml, actualHtml, "The mixed-case end tag </DIV> should be normalized to </div>");
     }
 }
