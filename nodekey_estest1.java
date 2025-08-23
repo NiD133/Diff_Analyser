5d4cb7f290c15e46af393af28c6479cefe92bdae
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:04:40 +0800

    gemini processedAutoTestCases nodekey_estest1.java

--- nodekey_estest1.java
+++ nodekey_estest1.java
@@ -2,22 +2,33 @@ package org.jfree.data.flow;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class NodeKey_ESTestTest1 extends NodeKey_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link NodeKey} class.
+ */
+public class NodeKeyTest {
 
-    @Test(timeout = 4000)
-    public void test00() throws Throwable {
-        Integer integer0 = new Integer(0);
-        NodeKey<Integer> nodeKey0 = new NodeKey<Integer>((-1934), integer0);
-        NodeKey<Integer> nodeKey1 = new NodeKey<Integer>((-3799), integer0);
-        NodeKey nodeKey2 = (NodeKey) nodeKey1.clone();
-        boolean boolean0 = nodeKey0.equals(nodeKey2);
-        assertFalse(boolean0);
-        assertEquals((-3799), nodeKey2.getStage());
-        assertFalse(nodeKey1.equals((Object) nodeKey0));
+    /**
+     * Verifies that two NodeKey instances are not considered equal if they have
+     * the same node but different stage values.
+     */
+    @Test
+    public void equals_shouldReturnFalse_whenStagesAreDifferent() throws CloneNotSupportedException {
+        // Arrange: Create two NodeKey instances with the same node but different stages.
+        Integer commonNode = 0;
+        NodeKey<Integer> key1 = new NodeKey<>(-1934, commonNode);
+        NodeKey<Integer> key2 = new NodeKey<>(-3799, commonNode);
+
+        // Act: For the sake of comparison, create a clone of the second key.
+        // This also helps verify that clone() produces an equal object.
+        NodeKey<Integer> key2Clone = (NodeKey<Integer>) key2.clone();
+
+        // Assert:
+        // First, confirm the clone is equal to its original.
+        assertEquals("A cloned key should be equal to its original.", key2, key2Clone);
+
+        // The main assertion: key1 should not be equal to the clone of key2,
+        // as their stages are different.
+        assertNotEquals("Keys with different stages should not be equal.", key1, key2Clone);
     }
 }
