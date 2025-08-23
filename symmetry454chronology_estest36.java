3671fb3f2b8aa0932247ddc0c1601b1d83e8848d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:19:58 +0800

    gemini processedAutoTestCases symmetry454chronology_estest36.java

--- symmetry454chronology_estest36.java
+++ symmetry454chronology_estest36.java
@@ -1,51 +1,23 @@
 package org.threeten.extra.chrono;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.time.Clock;
-import java.time.DateTimeException;
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.OffsetDateTime;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
-import java.time.chrono.ChronoLocalDateTime;
-import java.time.chrono.ChronoZonedDateTime;
-import java.time.chrono.Era;
-import java.time.chrono.HijrahEra;
-import java.time.chrono.IsoEra;
-import java.time.chrono.JapaneseEra;
-import java.time.temporal.ChronoField;
-import java.time.temporal.ChronoUnit;
-import java.time.temporal.TemporalAccessor;
-import java.time.temporal.TemporalUnit;
-import java.time.temporal.UnsupportedTemporalTypeException;
-import java.time.temporal.ValueRange;
-import java.util.List;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.System;
-import org.evosuite.runtime.mock.java.time.MockClock;
-import org.evosuite.runtime.mock.java.time.MockInstant;
-import org.evosuite.runtime.mock.java.time.MockLocalDateTime;
-import org.evosuite.runtime.mock.java.time.MockOffsetDateTime;
-import org.junit.runner.RunWith;
 
-public class Symmetry454Chronology_ESTestTest36 extends Symmetry454Chronology_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link Symmetry454Chronology} class.
+ */
+public class Symmetry454ChronologyTest {
 
-    @Test(timeout = 4000)
-    public void test35() throws Throwable {
-        Symmetry454Chronology symmetry454Chronology0 = new Symmetry454Chronology();
-        // Undeclared exception!
-        try {
-            symmetry454Chronology0.date((TemporalAccessor) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.threeten.extra.chrono.Symmetry454Date", e);
-        }
+    /**
+     * Tests that calling the date() method with a null TemporalAccessor
+     * argument correctly throws a NullPointerException.
+     */
+    @Test(expected = NullPointerException.class)
+    public void date_withNullTemporalAccessor_throwsNullPointerException() {
+        // Arrange: Get the singleton instance of the chronology.
+        Symmetry454Chronology chronology = Symmetry454Chronology.INSTANCE;
+
+        // Act & Assert: Calling date(null) should throw NullPointerException.
+        // The assertion is handled by the @Test(expected=...) annotation.
+        chronology.date(null);
     }
 }
