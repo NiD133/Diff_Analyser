4faf1bb4dcfb733ea2d97ec6fc3b058165af22a8
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:35:51 +0800

    Original processedManualTestCases weekstest15.java

---
+++ weekstest15.java
@@ -0,0 +1,43 @@
+package org.joda.time;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class WeeksTestTest15 extends TestCase {
+
+    // (before the late 90's they were all over the place)
+    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
+
+    public static TestSuite suite() {
+        return new TestSuite(TestWeeks.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+    }
+
+    //-----------------------------------------------------------------------
+    public void testToStandardDays() {
+        Weeks test = Weeks.weeks(2);
+        Days expected = Days.days(14);
+        assertEquals(expected, test.toStandardDays());
+        try {
+            Weeks.MAX_VALUE.toStandardDays();
+            fail();
+        } catch (ArithmeticException ex) {
+            // expected
+        }
+    }
+}
