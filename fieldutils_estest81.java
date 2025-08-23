2110e245b65585f2af69f3df39766079817e778c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:50:38 +0800

    Original processedAutoTestCases fieldutils_estest81.java

---
+++ fieldutils_estest81.java
@@ -0,0 +1,31 @@
+package org.joda.time.field;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.math.RoundingMode;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.joda.time.DateTimeField;
+import org.joda.time.DateTimeFieldType;
+import org.joda.time.DateTimeZone;
+import org.joda.time.chrono.IslamicChronology;
+import org.joda.time.chrono.ZonedChronology;
+import org.junit.runner.RunWith;
+
+public class FieldUtils_ESTestTest81 extends FieldUtils_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test80() throws Throwable {
+        // Undeclared exception!
+        try {
+            FieldUtils.safeAdd((-9223372036854775808L), (-9223372036854775808L));
+            fail("Expecting exception: ArithmeticException");
+        } catch (ArithmeticException e) {
+            //
+            // The calculation caused an overflow: -9223372036854775808 + -9223372036854775808
+            //
+            verifyException("org.joda.time.field.FieldUtils", e);
+        }
+    }
+}
