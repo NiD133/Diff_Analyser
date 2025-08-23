c077b791faee9ba7813d20463e06a473526a519d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:57:37 +0800

    Original processedAutoTestCases jsonsetter_estest13.java

---
+++ jsonsetter_estest13.java
@@ -0,0 +1,26 @@
+package com.fasterxml.jackson.annotation;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class JsonSetter_ESTestTest13 extends JsonSetter_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test12() throws Throwable {
+        JsonSetter jsonSetter0 = mock(JsonSetter.class, CALLS_REAL_METHODS);
+        doReturn((Nulls) null).when(jsonSetter0).contentNulls();
+        doReturn((Nulls) null).when(jsonSetter0).nulls();
+        JsonSetter.Value jsonSetter_Value0 = JsonSetter.Value.from(jsonSetter0);
+        Nulls nulls0 = Nulls.DEFAULT;
+        Nulls nulls1 = Nulls.SKIP;
+        JsonSetter.Value jsonSetter_Value1 = jsonSetter_Value0.withValueNulls(nulls0, nulls1);
+        jsonSetter_Value1.nonDefaultContentNulls();
+        assertEquals(Nulls.DEFAULT, jsonSetter_Value1.getValueNulls());
+        assertEquals(Nulls.SKIP, jsonSetter_Value1.getContentNulls());
+    }
+}
