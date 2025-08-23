74001b5b4f22ceeeaf91954492574d00f6fa167a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:29:47 +0800

    gemini processedManualTestCases islamicchronologytest9.java

--- islamicchronologytest9.java
+++ islamicchronologytest9.java
@@ -6,38 +6,24 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 import org.joda.time.Chronology;
 import org.joda.time.DateTime;
-import org.joda.time.DateTimeConstants;
 import org.joda.time.DateTimeField;
 import org.joda.time.DateTimeUtils;
 import org.joda.time.DateTimeZone;
-import org.joda.time.DurationFieldType;
-import org.joda.time.DateTime.Property;
+import org.joda.time.DurationField;
 
-public class IslamicChronologyTestTest9 extends TestCase {
+/**
+ * Tests the properties of the date fields in IslamicChronology.
+ * This test verifies field names, support, and associated duration fields.
+ */
+public class IslamicChronologyFieldsTest extends TestCase {
 
-    private static long SKIP = 1 * DateTimeConstants.MILLIS_PER_DAY;
-
-    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
+    // A specific date to fix the "current" time for tests, corresponds to 2002-06-09Z.
+    private static final long TEST_TIME_NOW = new DateTime(2002, 6, 9, 0, 0, ISOChronology.getInstanceUTC()).getMillis();
 
     private static final DateTimeZone LONDON = DateTimeZone.forID("Europe/London");
 
-    private static final DateTimeZone TOKYO = DateTimeZone.forID("Asia/Tokyo");
-
-    private static final Chronology ISLAMIC_UTC = IslamicChronology.getInstanceUTC();
-
-    private static final Chronology JULIAN_UTC = JulianChronology.getInstanceUTC();
-
-    private static final Chronology ISO_UTC = ISOChronology.getInstanceUTC();
-
-    long y2002days = 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365;
-
-    // 2002-06-09
-    private long TEST_TIME_NOW = (y2002days + 31L + 28L + 31L + 30L + 31L + 9L - 1L) * DateTimeConstants.MILLIS_PER_DAY;
-
     private DateTimeZone originalDateTimeZone = null;
-
     private TimeZone originalTimeZone = null;
-
     private Locale originalLocale = null;
 
     public static void main(String[] args) {
@@ -45,8 +31,7 @@ public class IslamicChronologyTestTest9 extends TestCase {
     }
 
     public static TestSuite suite() {
-        SKIP = 1 * DateTimeConstants.MILLIS_PER_DAY;
-        return new TestSuite(TestIslamicChronology.class);
+        return new TestSuite(IslamicChronologyFieldsTest.class);
     }
 
     @Override
@@ -71,55 +56,46 @@ public class IslamicChronologyTestTest9 extends TestCase {
         originalLocale = null;
     }
 
+    /**
+     * Tests that the date fields of IslamicChronology are correctly configured.
+     * It checks the name, supported status, duration field, and range duration field for each date-related field.
+     */
     public void testDateFields() {
-        final IslamicChronology islamic = IslamicChronology.getInstance();
-        assertEquals("era", islamic.era().getName());
-        assertEquals("centuryOfEra", islamic.centuryOfEra().getName());
-        assertEquals("yearOfCentury", islamic.yearOfCentury().getName());
-        assertEquals("yearOfEra", islamic.yearOfEra().getName());
-        assertEquals("year", islamic.year().getName());
-        assertEquals("monthOfYear", islamic.monthOfYear().getName());
-        assertEquals("weekyearOfCentury", islamic.weekyearOfCentury().getName());
-        assertEquals("weekyear", islamic.weekyear().getName());
-        assertEquals("weekOfWeekyear", islamic.weekOfWeekyear().getName());
-        assertEquals("dayOfYear", islamic.dayOfYear().getName());
-        assertEquals("dayOfMonth", islamic.dayOfMonth().getName());
-        assertEquals("dayOfWeek", islamic.dayOfWeek().getName());
-        assertEquals(true, islamic.era().isSupported());
-        assertEquals(true, islamic.centuryOfEra().isSupported());
-        assertEquals(true, islamic.yearOfCentury().isSupported());
-        assertEquals(true, islamic.yearOfEra().isSupported());
-        assertEquals(true, islamic.year().isSupported());
-        assertEquals(true, islamic.monthOfYear().isSupported());
-        assertEquals(true, islamic.weekyearOfCentury().isSupported());
-        assertEquals(true, islamic.weekyear().isSupported());
-        assertEquals(true, islamic.weekOfWeekyear().isSupported());
-        assertEquals(true, islamic.dayOfYear().isSupported());
-        assertEquals(true, islamic.dayOfMonth().isSupported());
-        assertEquals(true, islamic.dayOfWeek().isSupported());
-        assertEquals(islamic.eras(), islamic.era().getDurationField());
-        assertEquals(islamic.centuries(), islamic.centuryOfEra().getDurationField());
-        assertEquals(islamic.years(), islamic.yearOfCentury().getDurationField());
-        assertEquals(islamic.years(), islamic.yearOfEra().getDurationField());
-        assertEquals(islamic.years(), islamic.year().getDurationField());
-        assertEquals(islamic.months(), islamic.monthOfYear().getDurationField());
-        assertEquals(islamic.weekyears(), islamic.weekyearOfCentury().getDurationField());
-        assertEquals(islamic.weekyears(), islamic.weekyear().getDurationField());
-        assertEquals(islamic.weeks(), islamic.weekOfWeekyear().getDurationField());
-        assertEquals(islamic.days(), islamic.dayOfYear().getDurationField());
-        assertEquals(islamic.days(), islamic.dayOfMonth().getDurationField());
-        assertEquals(islamic.days(), islamic.dayOfWeek().getDurationField());
-        assertEquals(null, islamic.era().getRangeDurationField());
-        assertEquals(islamic.eras(), islamic.centuryOfEra().getRangeDurationField());
-        assertEquals(islamic.centuries(), islamic.yearOfCentury().getRangeDurationField());
-        assertEquals(islamic.eras(), islamic.yearOfEra().getRangeDurationField());
-        assertEquals(null, islamic.year().getRangeDurationField());
-        assertEquals(islamic.years(), islamic.monthOfYear().getRangeDurationField());
-        assertEquals(islamic.centuries(), islamic.weekyearOfCentury().getRangeDurationField());
-        assertEquals(null, islamic.weekyear().getRangeDurationField());
-        assertEquals(islamic.weekyears(), islamic.weekOfWeekyear().getRangeDurationField());
-        assertEquals(islamic.years(), islamic.dayOfYear().getRangeDurationField());
-        assertEquals(islamic.months(), islamic.dayOfMonth().getRangeDurationField());
-        assertEquals(islamic.weeks(), islamic.dayOfWeek().getRangeDurationField());
+        final IslamicChronology islamic = IslamicChronology.getInstance(LONDON);
+
+        assertField(islamic.era(), "era", islamic.eras(), null);
+        assertField(islamic.centuryOfEra(), "centuryOfEra", islamic.centuries(), islamic.eras());
+        assertField(islamic.yearOfCentury(), "yearOfCentury", islamic.years(), islamic.centuries());
+        assertField(islamic.yearOfEra(), "yearOfEra", islamic.years(), islamic.eras());
+        assertField(islamic.year(), "year", islamic.years(), null);
+        assertField(islamic.monthOfYear(), "monthOfYear", islamic.months(), islamic.years());
+        assertField(islamic.weekyearOfCentury(), "weekyearOfCentury", islamic.weekyears(), islamic.centuries());
+        assertField(islamic.weekyear(), "weekyear", islamic.weekyears(), null);
+        assertField(islamic.weekOfWeekyear(), "weekOfWeekyear", islamic.weeks(), islamic.weekyears());
+        assertField(islamic.dayOfYear(), "dayOfYear", islamic.days(), islamic.years());
+        assertField(islamic.dayOfMonth(), "dayOfMonth", islamic.days(), islamic.months());
+        assertField(islamic.dayOfWeek(), "dayOfWeek", islamic.days(), islamic.weeks());
+    }
+
+    /**
+     * Helper method to assert the properties of a DateTimeField.
+     *
+     * @param field the field to test
+     * @param expectedName the expected name of the field
+     * @param expectedDurationField the expected duration field
+     * @param expectedRangeDurationField the expected range duration field
+     */
+    private void assertField(
+            DateTimeField field,
+            String expectedName,
+            DurationField expectedDurationField,
+            DurationField expectedRangeDurationField) {
+
+        // All date fields in IslamicChronology are expected to be supported.
+        assertTrue("Field should be supported: " + expectedName, field.isSupported());
+
+        assertEquals("Field name mismatch for " + expectedName, expectedName, field.getName());
+        assertEquals("Duration field mismatch for " + expectedName, expectedDurationField, field.getDurationField());
+        assertEquals("Range duration field mismatch for " + expectedName, expectedRangeDurationField, field.getRangeDurationField());
     }
 }
