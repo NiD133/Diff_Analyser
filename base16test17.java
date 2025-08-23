a37bcee5d0887f4ecb022ba650c9111deae76dad
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:25:11 +0800

    Original processedManualTestCases base16test17.java

---
+++ base16test17.java
@@ -0,0 +1,62 @@
+package org.apache.commons.codec.binary;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Random;
+import org.apache.commons.codec.CodecPolicy;
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.EncoderException;
+import org.apache.commons.lang3.ArrayUtils;
+import org.junit.jupiter.api.Test;
+
+public class Base16TestTest17 {
+
+    private static final Charset CHARSET_UTF8 = StandardCharsets.UTF_8;
+
+    private final Random random = new Random();
+
+    /**
+     * @return the random.
+     */
+    public Random getRandom() {
+        return this.random;
+    }
+
+    private void testBase16InBuffer(final int startPasSize, final int endPadSize) {
+        final String content = "Hello World";
+        final String encodedContent;
+        final byte[] bytesUtf8 = StringUtils.getBytesUtf8(content);
+        byte[] buffer = ArrayUtils.addAll(bytesUtf8, new byte[endPadSize]);
+        buffer = ArrayUtils.addAll(new byte[startPasSize], buffer);
+        final byte[] encodedBytes = new Base16().encode(buffer, startPasSize, bytesUtf8.length);
+        encodedContent = StringUtils.newStringUtf8(encodedBytes);
+        assertEquals("48656C6C6F20576F726C64", encodedContent, "encoding hello world");
+    }
+
+    private String toString(final byte[] data) {
+        final StringBuilder buf = new StringBuilder();
+        for (int i = 0; i < data.length; i++) {
+            buf.append(data[i]);
+            if (i != data.length - 1) {
+                buf.append(",");
+            }
+        }
+        return buf.toString();
+    }
+
+    @Test
+    void testKnownDecodings() {
+        assertEquals("The quick brown fox jumped over the lazy dogs.", new String(new Base16(true).decode("54686520717569636b2062726f776e20666f78206a756d706564206f76657220746865206c617a7920646f67732e".getBytes(CHARSET_UTF8))));
+        assertEquals("It was the best of times, it was the worst of times.", new String(new Base16(true).decode("497420776173207468652062657374206f662074696d65732c206974207761732074686520776f727374206f662074696d65732e".getBytes(CHARSET_UTF8))));
+        assertEquals("http://jakarta.apache.org/commmons", new String(new Base16(true).decode("687474703a2f2f6a616b617274612e6170616368652e6f72672f636f6d6d6d6f6e73".getBytes(CHARSET_UTF8))));
+        assertEquals("AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz", new String(new Base16(true).decode("4161426243634464456546664767486849694a6a4b6b4c6c4d6d4e6e4f6f50705171527253735474557556765777587859795a7a".getBytes(CHARSET_UTF8))));
+        assertEquals("{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }", new String(new Base16(true).decode("7b20302c20312c20322c20332c20342c20352c20362c20372c20382c2039207d".getBytes(CHARSET_UTF8))));
+        assertEquals("xyzzy!", new String(new Base16(true).decode("78797a7a7921".getBytes(CHARSET_UTF8))));
+    }
+}
