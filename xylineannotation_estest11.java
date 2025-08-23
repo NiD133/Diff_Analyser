dd64744888f46f0905e62e749ba7c7fb4f5dadb9
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:29:56 +0800

    Original processedAutoTestCases xylineannotation_estest11.java

---
+++ xylineannotation_estest11.java
@@ -0,0 +1,47 @@
+package org.jfree.chart.annotations;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Paint;
+import java.awt.Polygon;
+import java.awt.Rectangle;
+import java.awt.Stroke;
+import java.awt.image.BufferedImage;
+import java.time.chrono.ChronoLocalDate;
+import javax.swing.text.DefaultCaret;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.CyclicNumberAxis;
+import org.jfree.chart.axis.DateAxis;
+import org.jfree.chart.axis.PeriodAxis;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.plot.CombinedRangeXYPlot;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.PlotRenderingInfo;
+import org.jfree.chart.plot.RingPlot;
+import org.jfree.chart.plot.SpiderWebPlot;
+import org.jfree.chart.plot.WaferMapPlot;
+import org.jfree.chart.plot.pie.PiePlot;
+import org.jfree.chart.renderer.WaferMapRenderer;
+import org.jfree.chart.renderer.xy.SamplingXYLineRenderer;
+import org.jfree.data.time.Day;
+import org.junit.runner.RunWith;
+
+public class XYLineAnnotation_ESTestTest11 extends XYLineAnnotation_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test10() throws Throwable {
+        SamplingXYLineRenderer samplingXYLineRenderer0 = new SamplingXYLineRenderer();
+        XYLineAnnotation xYLineAnnotation0 = new XYLineAnnotation(2545.5, (-909.0), (-1423.676), (double) samplingXYLineRenderer0.ZERO, samplingXYLineRenderer0.DEFAULT_OUTLINE_STROKE, samplingXYLineRenderer0.DEFAULT_OUTLINE_PAINT);
+        double double0 = xYLineAnnotation0.getY1();
+        assertEquals((-1423.676), xYLineAnnotation0.getX2(), 0.01);
+        assertEquals(2545.5, xYLineAnnotation0.getX1(), 0.01);
+        assertEquals((-909.0), double0, 0.01);
+        assertEquals(0.0, xYLineAnnotation0.getY2(), 0.01);
+    }
+}
