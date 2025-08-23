cab74c7da83a38d869bf95f7fc2a21dc7ebb31a0
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:00:04 +0800

    Original processedAutoTestCases leafnode_estest25.java

---
+++ leafnode_estest25.java
@@ -0,0 +1,30 @@
+package org.jsoup.nodes;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.List;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.jsoup.internal.QuietAppendable;
+import org.jsoup.parser.Parser;
+import org.junit.runner.RunWith;
+
+public class LeafNode_ESTestTest25 extends LeafNode_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test24() throws Throwable {
+        CDataNode cDataNode0 = new CDataNode("org.jsoup.nodes.LeafNode");
+        cDataNode0.value = (Object) cDataNode0;
+        // Undeclared exception!
+        try {
+            cDataNode0.removeAttr("org.jsoup.nodes.LeafNode");
+            fail("Expecting exception: ClassCastException");
+        } catch (ClassCastException e) {
+            //
+            // org.jsoup.nodes.CDataNode cannot be cast to java.lang.String
+            //
+            verifyException("org.jsoup.nodes.LeafNode", e);
+        }
+    }
+}
