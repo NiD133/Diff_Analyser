3c601ebd7c539b286089b1e238cfc5c8e01e6ec6
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:26:26 +0800

    gemini processedManualTestCases caseformattest6.java

--- caseformattest6.java
+++ caseformattest6.java
@@ -1,24 +1,23 @@
 package com.google.common.base;
 
-import static com.google.common.base.CaseFormat.LOWER_CAMEL;
 import static com.google.common.base.CaseFormat.LOWER_HYPHEN;
-import static com.google.common.base.CaseFormat.LOWER_UNDERSCORE;
 import static com.google.common.base.CaseFormat.UPPER_CAMEL;
-import static com.google.common.base.CaseFormat.UPPER_UNDERSCORE;
 import static com.google.common.truth.Truth.assertThat;
-import static com.google.common.truth.Truth.assertWithMessage;
-import com.google.common.annotations.GwtCompatible;
-import com.google.common.annotations.GwtIncompatible;
-import com.google.common.annotations.J2ktIncompatible;
-import com.google.common.testing.NullPointerTester;
-import com.google.common.testing.SerializableTester;
-import junit.framework.TestCase;
-import org.jspecify.annotations.NullUnmarked;
 
+import junit.framework.TestCase;
+
+/**
+ * Tests for {@link CaseFormat} conversions from {@code LOWER_HYPHEN} to {@code UPPER_CAMEL}.
+ */
 public class CaseFormatTestTest6 extends TestCase {
 
-    public void testLowerHyphenToUpperCamel() {
-        assertThat(LOWER_HYPHEN.to(UPPER_CAMEL, "foo")).isEqualTo("Foo");
-        assertThat(LOWER_HYPHEN.to(UPPER_CAMEL, "foo-bar")).isEqualTo("FooBar");
-    }
+  public void testLowerHyphenToUpperCamel_singleWord() {
+    String converted = LOWER_HYPHEN.to(UPPER_CAMEL, "foo");
+    assertThat(converted).isEqualTo("Foo");
+  }
+
+  public void testLowerHyphenToUpperCamel_multipleWords() {
+    String converted = LOWER_HYPHEN.to(UPPER_CAMEL, "foo-bar");
+    assertThat(converted).isEqualTo("FooBar");
+  }
 }
