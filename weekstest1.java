d7054e20eb0f56e27f2c46ad30d5185297dd17c8
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:35:49 +0800

    gemini processedManualTestCases weekstest1.java

--- weekstest1.java
+++ weekstest1.java
@@ -1,35 +1,15 @@
 package org.joda.time;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class WeeksTestTest1 extends TestCase {
+/**
+ * Unit tests for the {@link Weeks} class.
+ */
+class WeeksTest {
 
-    // (before the late 90's they were all over the place)
-    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
-
-    public static void main(String[] args) {
-        junit.textui.TestRunner.run(suite());
-    }
-
-    public static TestSuite suite() {
-        return new TestSuite(TestWeeks.class);
-    }
-
-    @Override
-    protected void setUp() throws Exception {
-    }
-
-    @Override
-    protected void tearDown() throws Exception {
-    }
-
-    //-----------------------------------------------------------------------
-    public void testConstants() {
+    @Test
+    void constants_shouldHoldCorrectWeekValues() {
         assertEquals(0, Weeks.ZERO.getWeeks());
         assertEquals(1, Weeks.ONE.getWeeks());
         assertEquals(2, Weeks.TWO.getWeeks());
