cc24e5e84f2cc183efe84889dfe273b4bfc44951
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:23:58 +0800

    gemini processedAutoTestCases typeadapterruntimetypewrapper_estest8.java

--- typeadapterruntimetypewrapper_estest8.java
+++ typeadapterruntimetypewrapper_estest8.java
@@ -1,56 +1,43 @@
 package com.google.gson.internal.bind;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
 import com.google.gson.Gson;
-import com.google.gson.JsonDeserializer;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonSerializer;
-import com.google.gson.ToNumberPolicy;
 import com.google.gson.TypeAdapter;
-import com.google.gson.TypeAdapterFactory;
-import com.google.gson.internal.Excluder;
-import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
-import com.google.gson.stream.JsonWriter;
-import java.io.BufferedWriter;
-import java.io.CharArrayWriter;
+import org.junit.Test;
+
 import java.io.EOFException;
 import java.io.IOException;
-import java.io.PipedReader;
-import java.io.PipedWriter;
 import java.io.StringReader;
-import java.io.StringWriter;
-import java.time.temporal.ChronoField;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.evosuite.runtime.mock.java.io.MockPrintWriter;
-import org.junit.runner.RunWith;
 
-public class TypeAdapterRuntimeTypeWrapper_ESTestTest8 extends TypeAdapterRuntimeTypeWrapper_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
-    @Test(timeout = 4000)
-    public void test07() throws Throwable {
-        JsonSerializer<Object> jsonSerializer0 = (JsonSerializer<Object>) mock(JsonSerializer.class, new ViolatedAssumptionAnswer());
-        Gson gson0 = new Gson();
-        Class<Object> class0 = Object.class;
-        TypeToken<Object> typeToken0 = TypeToken.get(class0);
-        TypeAdapterFactory typeAdapterFactory0 = TreeTypeAdapter.newFactoryWithMatchRawType(typeToken0, jsonSerializer0);
-        TypeAdapter<Object> typeAdapter0 = gson0.getDelegateAdapter(typeAdapterFactory0, typeToken0);
-        TypeAdapterRuntimeTypeWrapper<Object> typeAdapterRuntimeTypeWrapper0 = new TypeAdapterRuntimeTypeWrapper<Object>(gson0, typeAdapter0, class0);
-        StringReader stringReader0 = new StringReader("");
-        JsonReader jsonReader0 = gson0.newJsonReader(stringReader0);
-        try {
-            typeAdapterRuntimeTypeWrapper0.read(jsonReader0);
-            fail("Expecting exception: EOFException");
-        } catch (EOFException e) {
-            //
-            // End of input at line 1 column 1 path $
-            //
-            verifyException("com.google.gson.stream.JsonReader", e);
-        }
+public class TypeAdapterRuntimeTypeWrapper_ESTestTest8 {
+
+    /**
+     * Tests that calling read() on a wrapper with an empty JSON input
+     * correctly propagates the EOFException from the underlying reader.
+     */
+    @Test
+    public void read_withEmptyJsonInput_throwsEofException() throws IOException {
+        // Arrange
+        Gson gson = new Gson();
+        // The specific delegate adapter doesn't matter, as long as it attempts to read.
+        // The standard Object adapter is a simple and realistic choice.
+        TypeAdapter<Object> delegateAdapter = gson.getAdapter(Object.class);
+        TypeAdapterRuntimeTypeWrapper<Object> typeAdapterWrapper =
+                new TypeAdapterRuntimeTypeWrapper<>(gson, delegateAdapter, Object.class);
+
+        StringReader emptyInput = new StringReader("");
+        JsonReader jsonReader = gson.newJsonReader(emptyInput);
+
+        // Act & Assert
+        // Reading from an empty stream should result in an End of File exception.
+        EOFException exception = assertThrows(EOFException.class, () -> {
+            typeAdapterWrapper.read(jsonReader);
+        });
+
+        // Verify the exception message for more precise testing.
+        assertEquals("End of input at line 1 column 1 path $", exception.getMessage());
     }
 }
