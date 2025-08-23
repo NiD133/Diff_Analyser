e02dce910afd61d0be31d090cc1e97bd31046a8c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:15:40 +0800

    Original processedAutoTestCases shorttexttitle_estest14.java

---
+++ shorttexttitle_estest14.java
@@ -0,0 +1,51 @@
+package org.jfree.chart.title;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.time.chrono.ChronoLocalDate;
+import java.time.chrono.JapaneseDate;
+import java.util.Calendar;
+import java.util.List;
+import javax.swing.JTable;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.time.chrono.MockJapaneseDate;
+import org.evosuite.runtime.mock.java.util.MockCalendar;
+import org.jfree.chart.ChartRenderingInfo;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.CyclicNumberAxis;
+import org.jfree.chart.axis.ValueAxis;
+import org.jfree.chart.block.RectangleConstraint;
+import org.jfree.chart.block.Size2D;
+import org.jfree.chart.plot.CombinedDomainXYPlot;
+import org.jfree.chart.plot.SpiderWebPlot;
+import org.jfree.chart.plot.pie.PiePlot;
+import org.jfree.data.Range;
+import org.jfree.data.statistics.DefaultBoxAndWhiskerCategoryDataset;
+import org.jfree.data.statistics.DefaultStatisticalCategoryDataset;
+import org.jfree.data.time.TimePeriodAnchor;
+import org.jfree.data.time.TimeSeries;
+import org.junit.runner.RunWith;
+
+public class ShortTextTitle_ESTestTest14 extends ShortTextTitle_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test13() throws Throwable {
+        ShortTextTitle shortTextTitle0 = new ShortTextTitle("Polar Zoom Out");
+        // Undeclared exception!
+        try {
+            shortTextTitle0.arrangeFN((Graphics2D) null, (-1393.4121));
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.jfree.chart.title.ShortTextTitle", e);
+        }
+    }
+}
