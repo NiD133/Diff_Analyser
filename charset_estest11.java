a3d18e75f25ff4b7375a636b590f37e4ca9107c0
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:42:50 +0800

    Original processedAutoTestCases charset_estest11.java

---
+++ charset_estest11.java
@@ -0,0 +1,19 @@
+package org.apache.commons.lang3;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class CharSet_ESTestTest11 extends CharSet_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test10() throws Throwable {
+        String[] stringArray0 = new String[1];
+        stringArray0[0] = "]ZOr9";
+        CharSet charSet0 = CharSet.getInstance(stringArray0);
+        assertNotNull(charSet0);
+    }
+}
