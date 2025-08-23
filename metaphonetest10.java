8a3fa320b077c9b102f719aa35484d38ce732aac
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:30:59 +0800

    gemini processedManualTestCases metaphonetest10.java

--- metaphonetest10.java
+++ metaphonetest10.java
@@ -1,58 +1,52 @@
 package org.apache.commons.codec.language;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.stream.Stream;
 import org.apache.commons.codec.AbstractStringEncoderTest;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
-public class MetaphoneTestTest10 extends AbstractStringEncoderTest<Metaphone> {
-
-    public void assertIsMetaphoneEqual(final String source, final String[] matches) {
-        // match source to all matches
-        for (final String matche : matches) {
-            assertTrue(getStringEncoder().isMetaphoneEqual(source, matche), "Source: " + source + ", should have same Metaphone as: " + matche);
-        }
-        // match to each other
-        for (final String matche : matches) {
-            for (final String matche2 : matches) {
-                assertTrue(getStringEncoder().isMetaphoneEqual(matche, matche2));
-            }
-        }
-    }
-
-    public void assertMetaphoneEqual(final String[][] pairs) {
-        validateFixture(pairs);
-        for (final String[] pair : pairs) {
-            final String name0 = pair[0];
-            final String name1 = pair[1];
-            final String failMsg = "Expected match between " + name0 + " and " + name1;
-            assertTrue(getStringEncoder().isMetaphoneEqual(name0, name1), failMsg);
-            assertTrue(getStringEncoder().isMetaphoneEqual(name1, name0), failMsg);
-        }
-    }
+/**
+ * Tests the isMetaphoneEqual method of the Metaphone class.
+ */
+@DisplayName("Metaphone.isMetaphoneEqual")
+class MetaphoneIsEqualTest extends AbstractStringEncoderTest<Metaphone> {
 
     @Override
     protected Metaphone createStringEncoder() {
         return new Metaphone();
     }
 
-    public void validateFixture(final String[][] pairs) {
-        if (pairs.length == 0) {
-            fail("Test fixture is empty");
-        }
-        for (int i = 0; i < pairs.length; i++) {
-            if (pairs[i].length != 2) {
-                fail("Error in test fixture in the data array at index " + i);
-            }
-        }
+    /**
+     * Provides a stream of names that should be phonetically equivalent to "John".
+     * Match data was originally computed from http://www.lanw.com/java/phonetic/default.htm
+     *
+     * @return A stream of names.
+     */
+    private static Stream<String> phoneticMatchesForJohn() {
+        return Stream.of(
+            "Gena", "Gene", "Genia", "Genna", "Genni", "Gennie", "Genny", "Giana",
+            "Gianna", "Gina", "Ginni", "Ginnie", "Ginny", "Jaine", "Jan", "Jana",
+            "Jane", "Janey", "Jania", "Janie", "Janna", "Jany", "Jayne", "Jean",
+            "Jeana", "Jeane", "Jeanie", "Jeanna", "Jeanne", "Jeannie", "Jen",
+            "Jena", "Jeni", "Jenn", "Jenna", "Jennee", "Jenni", "Jennie", "Jenny",
+            "Jinny", "Jo Ann", "Jo-Ann", "Jo-Anne", "Joan", "Joana", "Joane",
+            "Joanie", "Joann", "Joanna", "Joanne", "Joeann", "Johna", "Johnna",
+            "Joni", "Jonie", "Juana", "June", "Junia", "Junie"
+        );
     }
 
-    /**
-     * Match data computed from http://www.lanw.com/java/phonetic/default.htm
-     */
-    @Test
-    void testIsMetaphoneEqualJohn() {
-        assertIsMetaphoneEqual("John", new String[] { "Gena", "Gene", "Genia", "Genna", "Genni", "Gennie", "Genny", "Giana", "Gianna", "Gina", "Ginni", "Ginnie", "Ginny", "Jaine", "Jan", "Jana", "Jane", "Janey", "Jania", "Janie", "Janna", "Jany", "Jayne", "Jean", "Jeana", "Jeane", "Jeanie", "Jeanna", "Jeanne", "Jeannie", "Jen", "Jena", "Jeni", "Jenn", "Jenna", "Jennee", "Jenni", "Jennie", "Jenny", "Jinny", "Jo Ann", "Jo-Ann", "Jo-Anne", "Joan", "Joana", "Joane", "Joanie", "Joann", "Joanna", "Joanne", "Joeann", "Johna", "Johnna", "Joni", "Jonie", "Juana", "June", "Junia", "Junie" });
+    @DisplayName("should return true for names phonetically similar to 'John'")
+    @ParameterizedTest(name = "\"{0}\" should be metaphone-equal to \"John\"")
+    @MethodSource("phoneticMatchesForJohn")
+    void returnsTrueForSimilarSoundingNames(final String similarName) {
+        final String sourceName = "John";
+        
+        assertTrue(
+            getStringEncoder().isMetaphoneEqual(sourceName, similarName),
+            () -> "Expected '" + sourceName + "' to be metaphone-equal to '" + similarName + "'"
+        );
     }
 }
