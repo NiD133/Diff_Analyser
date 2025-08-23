81a45dd04f07690ddce7aa7d60798e5cb4a99436
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:25:48 +0800

    Original processedManualTestCases boundediteratortest9.java

---
+++ boundediteratortest9.java
@@ -0,0 +1,69 @@
+package org.apache.commons.collections4.iterators;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class BoundedIteratorTestTest9<E> extends AbstractIteratorTest<E> {
+
+    /**
+     * Test array of size 7
+     */
+    private final String[] testArray = { "a", "b", "c", "d", "e", "f", "g" };
+
+    private List<E> testList;
+
+    @Override
+    public Iterator<E> makeEmptyIterator() {
+        return new BoundedIterator<>(Collections.<E>emptyList().iterator(), 0, 10);
+    }
+
+    @Override
+    public Iterator<E> makeObject() {
+        return new BoundedIterator<>(new ArrayList<>(testList).iterator(), 1, testList.size() - 1);
+    }
+
+    @SuppressWarnings("unchecked")
+    @BeforeEach
+    public void setUp() throws Exception {
+        testList = Arrays.asList((E[]) testArray);
+    }
+
+    /**
+     * Test removing the last element. Verify that the element is removed from
+     * the underlying collection.
+     */
+    @Test
+    void testRemoveLast() {
+        final List<E> testListCopy = new ArrayList<>(testList);
+        final Iterator<E> iter = new BoundedIterator<>(testListCopy.iterator(), 1, 5);
+        assertTrue(iter.hasNext());
+        assertEquals("b", iter.next());
+        assertTrue(iter.hasNext());
+        assertEquals("c", iter.next());
+        assertTrue(iter.hasNext());
+        assertEquals("d", iter.next());
+        assertTrue(iter.hasNext());
+        assertEquals("e", iter.next());
+        assertTrue(iter.hasNext());
+        assertEquals("f", iter.next());
+        assertFalse(iter.hasNext());
+        final NoSuchElementException thrown = assertThrows(NoSuchElementException.class, () -> iter.next());
+        assertNull(thrown.getMessage());
+        iter.remove();
+        assertFalse(testListCopy.contains("f"));
+        assertFalse(iter.hasNext());
+        final NoSuchElementException thrown1 = assertThrows(NoSuchElementException.class, () -> iter.next());
+        assertNull(thrown1.getMessage());
+    }
+}
