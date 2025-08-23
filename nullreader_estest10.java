60e9f16f37caf9bdf5e5742c102ead765b445e81
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:04:46 +0800

    Original processedAutoTestCases nullreader_estest10.java

---
+++ nullreader_estest10.java
@@ -0,0 +1,27 @@
+package org.apache.commons.io.input;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.EOFException;
+import java.io.IOException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class NullReader_ESTestTest10 extends NullReader_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test09() throws Throwable {
+        NullReader nullReader0 = new NullReader(0, true, true);
+        try {
+            nullReader0.skip(512L);
+            fail("Expecting exception: EOFException");
+        } catch (EOFException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.apache.commons.io.input.NullReader", e);
+        }
+    }
+}
