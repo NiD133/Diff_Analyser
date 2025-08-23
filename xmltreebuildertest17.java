477d0e04d1a7740cb7db3dc3b74f7b3a15edf713
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:36:09 +0800

    gemini processedManualTestCases xmltreebuildertest17.java

--- xmltreebuildertest17.java
+++ xmltreebuildertest17.java
@@ -2,33 +2,35 @@ package org.jsoup.parser;
 
 import org.jsoup.Jsoup;
 import org.jsoup.TextUtil;
-import org.jsoup.nodes.*;
+import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URISyntaxException;
-import java.nio.charset.StandardCharsets;
-import java.util.List;
-import static org.jsoup.nodes.Document.OutputSettings.Syntax;
-import static org.jsoup.parser.Parser.NamespaceHtml;
-import static org.jsoup.parser.Parser.NamespaceXml;
-import static org.junit.jupiter.api.Assertions.*;
 
-public class XmlTreeBuilderTestTest17 {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-    private static void assertXmlNamespace(Element el) {
-        assertEquals(NamespaceXml, el.tag().namespace(), String.format("Element %s not in XML namespace", el.tagName()));
-    }
+/**
+ * Test suite for the XmlTreeBuilder.
+ * This test focuses on how the XML parser handles appending fragments, specifically regarding case sensitivity.
+ */
+public class XmlTreeBuilderTest {
 
     @Test
-    public void appendPreservesCaseByDefault() {
-        String xml = "<One>One</One>";
-        Document doc = Jsoup.parse(xml, "", Parser.xmlParser());
-        Elements one = doc.select("One");
-        one.append("<Two ID=2>Two</Two>");
-        assertEquals("<One>One<Two ID=\"2\">Two</Two></One>", TextUtil.stripNewlines(doc.html()));
+    @DisplayName("Appending an XML fragment should preserve the case of tags and attributes")
+    public void appendXmlFragmentPreservesTagAndAttributeCase() {
+        // Arrange: Create an XML document using the case-sensitive XML parser.
+        String initialXml = "<One>One</One>";
+        Document doc = Jsoup.parse(initialXml, "", Parser.xmlParser());
+        Elements parentElement = doc.select("One");
+        String fragmentToAppend = "<Two ID=2>Two</Two>";
+
+        // Act: Append the new XML fragment to the selected element.
+        parentElement.append(fragmentToAppend);
+
+        // Assert: Verify that the tag ('Two') and attribute ('ID') cases are preserved in the final document.
+        // The HTML parser, by contrast, would have lowercased them.
+        String expectedXml = "<One>One<Two ID=\"2\">Two</Two></One>";
+        String actualXml = TextUtil.stripNewlines(doc.html());
+        assertEquals(expectedXml, actualXml);
     }
 }
