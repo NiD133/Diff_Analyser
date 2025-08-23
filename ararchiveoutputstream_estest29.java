7cf63e65e18a10caa861edd279fb2a89fdbb13f3
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:37:21 +0800

    Original processedAutoTestCases ararchiveoutputstream_estest29.java

---
+++ ararchiveoutputstream_estest29.java
@@ -0,0 +1,41 @@
+package org.apache.commons.compress.archivers.ar;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.LinkOption;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.System;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.evosuite.runtime.mock.java.io.MockFileOutputStream;
+import org.evosuite.runtime.mock.java.io.MockPrintStream;
+import org.evosuite.runtime.testdata.FileSystemHandling;
+import org.junit.runner.RunWith;
+
+public class ArArchiveOutputStream_ESTestTest29 extends ArArchiveOutputStream_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test28() throws Throwable {
+        ByteArrayOutputStream byteArrayOutputStream0 = new ByteArrayOutputStream();
+        ArArchiveOutputStream arArchiveOutputStream0 = new ArArchiveOutputStream(byteArrayOutputStream0);
+        ArArchiveEntry arArchiveEntry0 = new ArArchiveEntry("jl^;TJ]T9ZU<Vh", 0);
+        arArchiveOutputStream0.putArchiveEntry(arArchiveEntry0);
+        arArchiveOutputStream0.closeArchiveEntry();
+        try {
+            arArchiveOutputStream0.closeArchiveEntry();
+            fail("Expecting exception: IOException");
+        } catch (IOException e) {
+            //
+            // No current entry to close
+            //
+            verifyException("org.apache.commons.compress.archivers.ar.ArArchiveOutputStream", e);
+        }
+    }
+}
