44e0dbe2bfc6ae3f657a01c8b189d631cd019c8c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:01:52 +0800

    Original processedAutoTestCases lzmautils_estest27.java

---
+++ lzmautils_estest27.java
@@ -0,0 +1,17 @@
+package org.apache.commons.compress.compressors.lzma;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class LZMAUtils_ESTestTest27 extends LZMAUtils_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test26() throws Throwable {
+        String string0 = LZMAUtils.getUncompressedFileName("Kj-M[Oi{g@l@");
+        assertEquals("Kj-M[Oi{g@l@", string0);
+    }
+}
