f03547cfae2b256e04d2d812ab7d27a885727f24
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:26:04 +0800

    Original processedAutoTestCases validate_estest40.java

---
+++ validate_estest40.java
@@ -0,0 +1,25 @@
+package org.jsoup.helper;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.FormatFlagsConversionMismatchException;
+import java.util.IllegalFormatConversionException;
+import java.util.IllegalFormatFlagsException;
+import java.util.IllegalFormatWidthException;
+import java.util.MissingFormatArgumentException;
+import java.util.MissingFormatWidthException;
+import java.util.UnknownFormatConversionException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class Validate_ESTestTest40 extends Validate_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test39() throws Throwable {
+        Object object0 = new Object();
+        Object object1 = Validate.ensureNotNull(object0);
+        assertSame(object1, object0);
+    }
+}
