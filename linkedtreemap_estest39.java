68e9ef9ddfc95ed9dc3cc0396c9c2f1009fb7c11
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:00:33 +0800

    gemini processedAutoTestCases linkedtreemap_estest39.java

--- linkedtreemap_estest39.java
+++ linkedtreemap_estest39.java
@@ -1,29 +1,37 @@
 package com.google.gson.internal;
 
+import com.google.gson.internal.LinkedTreeMap.Node;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.AbstractMap;
-import java.util.Comparator;
-import java.util.Map;
-import java.util.Set;
-import java.util.function.BiFunction;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertFalse;
 
+/**
+ * This test class contains tests for the {@link LinkedTreeMap.Node#equals(Object)} method.
+ */
 public class LinkedTreeMap_ESTestTest39 extends LinkedTreeMap_ESTest_scaffolding {
 
+    /**
+     * Tests that the Node.equals() method returns false when two nodes have the
+     * same key but different values.
+     */
     @Test(timeout = 4000)
-    public void test38() throws Throwable {
-        LinkedTreeMap.Node<Integer, Integer> linkedTreeMap_Node0 = new LinkedTreeMap.Node<Integer, Integer>(false);
-        Integer integer0 = Integer.valueOf((-2));
-        linkedTreeMap_Node0.value = integer0;
-        LinkedTreeMap.Node<Integer, Integer> linkedTreeMap_Node1 = new LinkedTreeMap.Node<Integer, Integer>(true);
-        LinkedTreeMap.Node<Integer, Integer> linkedTreeMap_Node2 = new LinkedTreeMap.Node<Integer, Integer>(true, linkedTreeMap_Node0, (Integer) null, linkedTreeMap_Node1, linkedTreeMap_Node0);
-        boolean boolean0 = linkedTreeMap_Node2.equals(linkedTreeMap_Node0);
-        assertFalse(boolean0);
+    public void nodeEquals_shouldReturnFalse_whenNodesHaveSameKeyButDifferentValues() {
+        // Arrange
+        // The Node.equals() method compares nodes based on their key and value.
+        // We will create two nodes that have the same key (null) but different values.
+
+        // Create a node with a null key and a specific value.
+        // The simple Node constructor initializes the key to null.
+        Node<Integer, Integer> nodeWithAValue = new Node<>(false);
+        nodeWithAValue.value = -2;
+
+        // Create another node with a null key and no assigned value (i.e., value is null).
+        Node<Integer, Integer> nodeWithoutAValue = new Node<>(false);
+
+        // Act
+        // Compare the two nodes. The expected result is false because their values differ.
+        boolean areEqual = nodeWithoutAValue.equals(nodeWithAValue);
+
+        // Assert
+        assertFalse("Nodes with different values should not be considered equal.", areEqual);
     }
 }
