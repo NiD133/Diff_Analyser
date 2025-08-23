1fd32d4ba6d38322a2c403d55437f3a4599a0827
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:45:32 +0800

    gemini processedAutoTestCases constructorinstantiator_estest2.java

--- constructorinstantiator_estest2.java
+++ constructorinstantiator_estest2.java
@@ -1,27 +1,21 @@
 package org.mockito.internal.creation.instance;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class ConstructorInstantiator_ESTestTest2 extends ConstructorInstantiator_ESTest_scaffolding {
+/**
+ * Tests for {@link ConstructorInstantiator}.
+ */
+public class ConstructorInstantiatorTest {
 
-    @Test(timeout = 4000)
-    public void test1() throws Throwable {
-        ConstructorInstantiator constructorInstantiator0 = new ConstructorInstantiator(false, (Object[]) null);
-        Class<Object> class0 = Object.class;
-        // Undeclared exception!
-        try {
-            constructorInstantiator0.newInstance(class0);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.mockito.internal.creation.instance.ConstructorInstantiator", e);
-        }
+    @Test(expected = NullPointerException.class)
+    public void newInstance_shouldThrowNullPointerException_whenConstructedWithNullArguments() {
+        // Arrange: Create an instantiator with a null array for constructor arguments.
+        // The varargs parameter requires an explicit cast to null to avoid ambiguity.
+        Object[] nullConstructorArgs = null;
+        ConstructorInstantiator instantiator = new ConstructorInstantiator(false, nullConstructorArgs);
+
+        // Act & Assert: Attempting to create a new instance should fail with an NPE
+        // because the internal logic will try to access the null arguments array.
+        instantiator.newInstance(Object.class);
     }
 }
