812246bec11b4624a1497d235e7cd1403b93ffeb
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:06:35 +0800

    gemini processedAutoTestCases optiongroup_estest10.java

--- optiongroup_estest10.java
+++ optiongroup_estest10.java
@@ -1,21 +1,32 @@
 package org.apache.commons.cli;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.Collection;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class OptionGroup_ESTestTest10 extends OptionGroup_ESTest_scaffolding {
+/**
+ * Tests for the {@link OptionGroup} class.
+ */
+public class OptionGroupTest {
 
-    @Test(timeout = 4000)
-    public void test09() throws Throwable {
-        OptionGroup optionGroup0 = new OptionGroup();
-        Option option0 = new Option("oQxw", (String) null, true, "[]");
-        optionGroup0.setSelected(option0);
-        optionGroup0.setSelected(option0);
-        assertFalse(option0.isRequired());
+    /**
+     * Tests that calling setSelected() multiple times with the same option
+     * does not throw an exception and remains idempotent.
+     */
+    @Test
+    public void setSelectedShouldNotThrowExceptionWhenCalledMultipleTimesWithSameOption() throws AlreadySelectedException {
+        // Arrange: Create an option group and an option to be selected.
+        OptionGroup optionGroup = new OptionGroup();
+        Option option = new Option("s", "select", false, "The selected option");
+
+        // Act: Select the same option twice.
+        // The first call sets the selected option.
+        optionGroup.setSelected(option);
+        
+        // The second call with the same option should be a no-op and not throw an exception.
+        optionGroup.setSelected(option);
+
+        // Assert: Verify that the option is still correctly selected.
+        // The primary test is that the second call above did not throw an AlreadySelectedException.
+        assertEquals("The selected option name should be 's'", "s", optionGroup.getSelected());
     }
 }
