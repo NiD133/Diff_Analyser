4fb6c8494639adcc00581bd5a6404aa8b6d5406d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:00:06 +0800

    Original processedAutoTestCases leafnode_estest3.java

---
+++ leafnode_estest3.java
@@ -0,0 +1,21 @@
+package org.jsoup.nodes;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.List;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.jsoup.internal.QuietAppendable;
+import org.jsoup.parser.Parser;
+import org.junit.runner.RunWith;
+
+public class LeafNode_ESTestTest3 extends LeafNode_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test02() throws Throwable {
+        CDataNode cDataNode0 = new CDataNode("~p1SkcF]Q'5D");
+        cDataNode0.coreValue("");
+        assertEquals(0, cDataNode0.siblingIndex());
+    }
+}
