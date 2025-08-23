7bbc2f7190f8bee0eadff8d02c0a897abf17c003
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:40:46 +0800

    gemini processedAutoTestCases bytearraybuilder_estest62.java

--- bytearraybuilder_estest62.java
+++ bytearraybuilder_estest62.java
@@ -1,19 +1,39 @@
 package com.fasterxml.jackson.core.util;
 
+import com.fasterxml.jackson.core.io.BufferRecycler;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class ByteArrayBuilder_ESTestTest62 extends ByteArrayBuilder_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
-    @Test(timeout = 4000)
-    public void test61() throws Throwable {
-        BufferRecycler bufferRecycler0 = new BufferRecycler();
-        ByteArrayBuilder byteArrayBuilder0 = new ByteArrayBuilder(bufferRecycler0, (-3320));
-        byte[] byteArray0 = byteArrayBuilder0.getCurrentSegment();
-        assertEquals(2000, byteArray0.length);
+/**
+ * Unit tests for the {@link ByteArrayBuilder} class.
+ */
+public class ByteArrayBuilderTest {
+
+    /**
+     * Verifies that the ByteArrayBuilder constructor, when provided with a BufferRecycler,
+     * ignores the 'firstBlockSize' hint and instead requests a default-sized buffer
+     * from the recycler.
+     */
+    @Test
+    public void constructorWithBufferRecycler_shouldUseDefaultRecyclerBufferSize() {
+        // Arrange
+        BufferRecycler recycler = new BufferRecycler();
+        // Use a nonsensical size to emphasize that this argument should be ignored.
+        int ignoredInitialSize = -1;
+
+        // Act
+        // This constructor should request a BYTE_WRITE_CONCAT_BUFFER from the recycler.
+        ByteArrayBuilder builder = new ByteArrayBuilder(recycler, ignoredInitialSize);
+        byte[] currentSegment = builder.getCurrentSegment();
+
+        // Assert
+        assertNotNull("The initial segment should not be null", currentSegment);
+
+        // The default size for a BYTE_WRITE_CONCAT_BUFFER from a standard BufferRecycler is 2000 bytes.
+        final int expectedDefaultBufferSize = 2000;
+        assertEquals("Initial buffer size should match the recycler's default",
+                expectedDefaultBufferSize, currentSegment.length);
     }
 }
