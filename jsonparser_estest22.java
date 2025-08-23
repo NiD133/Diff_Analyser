4c5c19a9dd29158f7867ba54d417010f6d0fd0dc
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:57:12 +0800

    Original processedAutoTestCases jsonparser_estest22.java

---
+++ jsonparser_estest22.java
@@ -0,0 +1,32 @@
+package com.google.gson;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.google.gson.stream.JsonReader;
+import java.io.Reader;
+import java.io.StringReader;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class JsonParser_ESTestTest22 extends JsonParser_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test21() throws Throwable {
+        JsonParser jsonParser0 = new JsonParser();
+        StringReader stringReader0 = new StringReader("?3p");
+        JsonReader jsonReader0 = new JsonReader(stringReader0);
+        jsonParser0.parse(jsonReader0);
+        // Undeclared exception!
+        try {
+            jsonParser0.parse(jsonReader0);
+            fail("Expecting exception: IllegalStateException");
+        } catch (IllegalStateException e) {
+            //
+            // Unexpected token: END_DOCUMENT
+            //
+            verifyException("com.google.gson.internal.bind.JsonElementTypeAdapter", e);
+        }
+    }
+}
