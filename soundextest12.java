dad3b0bc00551d2f4eec4eeb92b289afa4b68be3
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:33:51 +0800

    gemini processedManualTestCases soundextest12.java

--- soundextest12.java
+++ soundextest12.java
@@ -1,31 +1,45 @@
 package org.apache.commons.codec.language;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import org.apache.commons.codec.AbstractStringEncoderTest;
-import org.apache.commons.codec.EncoderException;
-import org.junit.jupiter.api.Test;
 
-public class SoundexTestTest12 extends AbstractStringEncoderTest<Soundex> {
+import org.apache.commons.codec.AbstractStringEncoderTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+/**
+ * Tests the Soundex algorithm's special rule for the letters 'H' and 'W'.
+ * This rule applies when using the default {@link Soundex#US_ENGLISH} mapping.
+ */
+public class SoundexHWRuleTest extends AbstractStringEncoderTest<Soundex> {
 
     @Override
     protected Soundex createStringEncoder() {
+        // Creates a Soundex instance with the default US English mapping,
+        // where 'H' and 'W' are handled specially.
         return new Soundex();
     }
 
     /**
-     * Consonants from the same code group separated by W or H are treated as one.
+     * The standard Soundex algorithm specifies that consonants with the same Soundex code
+     * separated by 'H' or 'W' should be treated as a single consonant.
+     * <p>
+     * This test is based on the official Soundex guidelines from the U.S. National Archives:
+     * "Ashcraft is coded A-261 (A, 2 for the S, C ignored, 6 for the R, 1 for the F). It is not coded A-226."
+     * (Source: http://www.archives.gov/research_room/genealogy/census/soundex.html)
+     * </p>
      */
-    @Test
-    void testHWRuleEx1() {
-        // From
-        // http://www.archives.gov/research_room/genealogy/census/soundex.html:
-        // Ashcraft is coded A-261 (A, 2 for the S, C ignored, 6 for the R, 1
-        // for the F). It is not coded A-226.
-        assertEquals("A261", getStringEncoder().encode("Ashcraft"));
-        assertEquals("A261", getStringEncoder().encode("Ashcroft"));
-        assertEquals("Y330", getStringEncoder().encode("yehudit"));
-        assertEquals("Y330", getStringEncoder().encode("yhwdyt"));
+    @DisplayName("Consonants from the same code group separated by 'H' or 'W' should be treated as one")
+    @ParameterizedTest
+    @CsvSource({
+        // The Ashcraft example from the US National Archives and Records Administration
+        "Ashcraft, A261",
+        "Ashcroft, A261",
+        // Examples demonstrating the H/W rule with different letters
+        "yehudit,  Y330",
+        "yhwdyt,   Y330"
+    })
+    void shouldTreatConsonantsWithSameCodeSeparatedByHOrWAsOne(final String name, final String expectedSoundex) {
+        assertEquals(expectedSoundex, getStringEncoder().encode(name));
     }
 }
