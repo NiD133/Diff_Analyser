dad601b78f3873265e371df9f0e3679b0cb75903
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:20:13 +0800

    gemini processedAutoTestCases tagset_estest14.java

--- tagset_estest14.java
+++ tagset_estest14.java
@@ -1,29 +1,26 @@
 package org.jsoup.parser;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.function.Consumer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
 
-public class TagSet_ESTestTest14 extends TagSet_ESTest_scaffolding {
+/**
+ * Tests for the {@link TagSet} class, focusing on exception handling.
+ */
+public class TagSetTest {
 
-    @Test(timeout = 4000)
-    public void test13() throws Throwable {
-        TagSet tagSet0 = new TagSet();
-        // Undeclared exception!
-        try {
-            tagSet0.valueOf("org.jsoup.select.Evaluator$AttributeWithValueMatching", "org.jsoup.select.Evaluator$AttributeWithValueMatching", (ParseSettings) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.jsoup.parser.TagSet", e);
-        }
+    /**
+     * Verifies that calling valueOf() with a null ParseSettings object
+     * throws a NullPointerException, as the settings are required to
+     * determine tag name case sensitivity.
+     */
+    @Test(expected = NullPointerException.class)
+    public void valueOfWithNullParseSettingsThrowsNullPointerException() {
+        // Arrange
+        TagSet tagSet = new TagSet();
+        String tagName = "div";
+        String namespace = Parser.NamespaceHtml;
+
+        // Act & Assert
+        // This call is expected to throw a NullPointerException because the settings argument is null.
+        tagSet.valueOf(tagName, namespace, null);
     }
 }
