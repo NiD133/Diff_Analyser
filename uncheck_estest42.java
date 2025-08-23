fd7fa0e65febc3df9b991c3f8e3c7f80481a4c24
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:24:39 +0800

    Original processedAutoTestCases uncheck_estest42.java

---
+++ uncheck_estest42.java
@@ -0,0 +1,30 @@
+package org.apache.commons.io.function;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.lang.reflect.Array;
+import java.time.chrono.HijrahEra;
+import java.util.Comparator;
+import java.util.concurrent.ForkJoinTask;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+import java.util.stream.LongStream;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class Uncheck_ESTestTest42 extends Uncheck_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test41() throws Throwable {
+        IOFunction<String, String> iOFunction0 = IOFunction.identity();
+        String string0 = Uncheck.apply(iOFunction0, "http");
+        assertEquals("http", string0);
+    }
+}
