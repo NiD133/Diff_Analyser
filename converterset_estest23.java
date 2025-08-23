caae02ab769a5dc3d180a7fee778d27fbfd4adbd
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:45:45 +0800

    gemini processedAutoTestCases converterset_estest23.java

--- converterset_estest23.java
+++ converterset_estest23.java
@@ -1,27 +1,43 @@
 package org.joda.time.convert;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.joda.time.Hours;
-import org.joda.time.Interval;
-import org.joda.time.MutablePeriod;
-import org.joda.time.PeriodType;
-import org.joda.time.Seconds;
-import org.joda.time.chrono.CopticChronology;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertSame;
 
-public class ConverterSet_ESTestTest23 extends ConverterSet_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link ConverterSet} class.
+ */
+public class ConverterSetTest {
 
-    @Test(timeout = 4000)
-    public void test22() throws Throwable {
-        Converter[] converterArray0 = new Converter[2];
-        ReadablePartialConverter readablePartialConverter0 = ReadablePartialConverter.INSTANCE;
-        converterArray0[1] = (Converter) readablePartialConverter0;
-        ConverterSet converterSet0 = new ConverterSet(converterArray0);
-        ConverterSet converterSet1 = converterSet0.remove(converterArray0[1], converterArray0);
-        assertNotSame(converterSet1, converterSet0);
+    /**
+     * Tests that removing an existing converter from a ConverterSet returns a new,
+     * modified set instance, confirming the class's immutability.
+     */
+    @Test
+    public void remove_whenConverterExists_shouldReturnNewSetInstance() {
+        // Arrange: Create a ConverterSet containing a specific converter and a null element.
+        // This setup mirrors the original test's structure.
+        Converter converterToRemove = ReadablePartialConverter.INSTANCE;
+        Converter[] initialConverters = {null, converterToRemove};
+        ConverterSet initialSet = new ConverterSet(initialConverters);
+
+        // The 'remove' method can optionally return the removed converter in this array.
+        Converter[] removedConverterContainer = new Converter[1];
+
+        // Act: Remove the specific converter from the set.
+        ConverterSet resultSet = initialSet.remove(converterToRemove, removedConverterContainer);
+
+        // Assert: Verify the results of the remove operation.
+
+        // 1. A new instance must be returned, as ConverterSet is immutable.
+        assertNotSame("Removing a converter should return a new set instance.", initialSet, resultSet);
+
+        // 2. The size of the new set should be one less than the original.
+        assertEquals("The new set should contain one less converter.", 1, resultSet.size());
+
+        // 3. The container array should now hold the converter that was removed.
+        assertSame("The removed converter should be captured in the provided array.",
+                   converterToRemove, removedConverterContainer[0]);
     }
 }
