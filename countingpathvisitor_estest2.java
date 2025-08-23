b784b490b58fafda99ed0bbf17f52b039f66ad63
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:46:08 +0800

    Original processedAutoTestCases countingpathvisitor_estest2.java

---
+++ countingpathvisitor_estest2.java
@@ -0,0 +1,39 @@
+package org.apache.commons.io.file;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.function.UnaryOperator;
+import org.apache.commons.io.filefilter.CanWriteFileFilter;
+import org.apache.commons.io.filefilter.EmptyFileFilter;
+import org.apache.commons.io.filefilter.FileFileFilter;
+import org.apache.commons.io.filefilter.HiddenFileFilter;
+import org.apache.commons.io.filefilter.IOFileFilter;
+import org.apache.commons.io.filefilter.NotFileFilter;
+import org.apache.commons.io.filefilter.PathEqualsFileFilter;
+import org.apache.commons.io.filefilter.SuffixFileFilter;
+import org.apache.commons.io.function.IOBiFunction;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.evosuite.runtime.mock.java.io.MockIOException;
+import org.junit.runner.RunWith;
+
+public class CountingPathVisitor_ESTestTest2 extends CountingPathVisitor_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test01() throws Throwable {
+        CountingPathVisitor.Builder countingPathVisitor_Builder0 = new CountingPathVisitor.Builder();
+        Counters.PathCounters counters_PathCounters0 = countingPathVisitor_Builder0.getPathCounters();
+        CountingPathVisitor.Builder countingPathVisitor_Builder1 = countingPathVisitor_Builder0.setPathCounters(counters_PathCounters0);
+        assertSame(countingPathVisitor_Builder1, countingPathVisitor_Builder0);
+    }
+}
