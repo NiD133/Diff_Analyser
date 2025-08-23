aba6de0c04309e32f28d17dc0002fa777a7426d3
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:04:20 +0800

    gemini processedAutoTestCases multireadonlyseekablebytechannel_estest8.java

--- multireadonlyseekablebytechannel_estest8.java
+++ multireadonlyseekablebytechannel_estest8.java
@@ -1,39 +1,36 @@
 package org.apache.commons.compress.utils;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.mock.java.io.MockFile;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.nio.channels.ClosedChannelException;
-import java.nio.channels.FileChannel;
-import java.nio.channels.NonWritableChannelException;
 import java.nio.channels.SeekableByteChannel;
-import java.nio.file.NoSuchFileException;
-import java.nio.file.OpenOption;
-import java.nio.file.Path;
-import java.util.LinkedList;
-import java.util.List;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.io.MockFile;
-import org.junit.runner.RunWith;
 
+/**
+ * This test class is a placeholder to contain the improved test case.
+ * The original test was part of an auto-generated suite.
+ */
 public class MultiReadOnlySeekableByteChannel_ESTestTest8 extends MultiReadOnlySeekableByteChannel_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test07() throws Throwable {
-        File[] fileArray0 = new File[2];
-        MockFile mockFile0 = new MockFile("");
-        fileArray0[0] = (File) mockFile0;
-        fileArray0[1] = (File) mockFile0;
-        SeekableByteChannel seekableByteChannel0 = MultiReadOnlySeekableByteChannel.forFiles(fileArray0);
-        ByteBuffer byteBuffer0 = ByteBuffer.allocateDirect(5);
-        try {
-            seekableByteChannel0.read(byteBuffer0);
-            fail("Expecting exception: IOException");
-        } catch (IOException e) {
-        }
+    /**
+     * Tests that attempting to read from a MultiReadOnlySeekableByteChannel
+     * created from directories (instead of regular files) throws an IOException.
+     * The underlying FileChannel cannot be read from a directory.
+     */
+    @Test(timeout = 4000, expected = IOException.class)
+    public void readFromChannelOfDirectoriesShouldThrowIOException() throws IOException {
+        // Arrange: Create a channel from one or more directories.
+        // MockFile("") represents the current working directory.
+        File directory = new MockFile("");
+        File[] directories = {directory, directory};
+
+        SeekableByteChannel channel = MultiReadOnlySeekableByteChannel.forFiles(directories);
+        ByteBuffer buffer = ByteBuffer.allocate(16);
+
+        // Act & Assert: Attempting to read from the channel should throw an IOException
+        // because the underlying resources are directories, not readable files.
+        channel.read(buffer);
     }
 }
