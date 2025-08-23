bd828dd8f05ead41f506c652f0664cbfb4ed5bbb
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:06:52 +0800

    gemini processedAutoTestCases orderedproperties_estest5.java

--- orderedproperties_estest5.java
+++ orderedproperties_estest5.java
@@ -1,53 +1,30 @@
 package org.apache.commons.collections4.properties;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.Reader;
-import java.io.StringReader;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.LinkedHashSet;
-import java.util.Map;
-import java.util.NoSuchElementException;
+import static org.junit.Assert.assertTrue;
 import java.util.Set;
-import java.util.function.BiConsumer;
-import java.util.function.BiFunction;
-import java.util.function.Function;
-import org.apache.commons.collections4.Equator;
-import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AllPredicate;
-import org.apache.commons.collections4.functors.CloneTransformer;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.DefaultEquator;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.NOPTransformer;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NotNullPredicate;
-import org.apache.commons.collections4.functors.NullIsTruePredicate;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.SwitchTransformer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
 
-public class OrderedProperties_ESTestTest5 extends OrderedProperties_ESTest_scaffolding {
+/**
+ * Unit tests for {@link OrderedProperties}.
+ * This class focuses on testing the keySet() method.
+ */
+public class OrderedPropertiesTest {
 
-    @Test(timeout = 4000)
-    public void test04() throws Throwable {
-        OrderedProperties orderedProperties0 = new OrderedProperties();
-        Integer integer0 = new Integer(61);
-        orderedProperties0.put(integer0, integer0);
-        Set<Object> set0 = orderedProperties0.keySet();
-        assertTrue(set0.contains(61));
+    /**
+     * Tests that a key added via put() is correctly reported in the keySet().
+     */
+    @Test
+    public void keySetShouldContainAddedKey() {
+        // Arrange
+        OrderedProperties properties = new OrderedProperties();
+        Integer testKey = 61;
+        Integer testValue = 61;
+
+        // Act
+        properties.put(testKey, testValue);
+        Set<Object> keySet = properties.keySet();
+
+        // Assert
+        assertTrue("The key set should contain the key that was just added.", keySet.contains(testKey));
     }
 }
