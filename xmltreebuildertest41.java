9db522fdfc15a6501ae16f23e650fdd84984e4fd
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:36:16 +0800

    gemini processedManualTestCases xmltreebuildertest41.java

--- xmltreebuildertest41.java
+++ xmltreebuildertest41.java
@@ -1,41 +1,68 @@
 package org.jsoup.parser;
 
 import org.jsoup.Jsoup;
-import org.jsoup.TextUtil;
-import org.jsoup.nodes.*;
-import org.jsoup.select.Elements;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.parser.Tag;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URISyntaxException;
-import java.nio.charset.StandardCharsets;
-import java.util.List;
-import static org.jsoup.nodes.Document.OutputSettings.Syntax;
-import static org.jsoup.parser.Parser.NamespaceHtml;
-import static org.jsoup.parser.Parser.NamespaceXml;
-import static org.junit.jupiter.api.Assertions.*;
 
-public class XmlTreeBuilderTestTest41 {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-    private static void assertXmlNamespace(Element el) {
-        assertEquals(NamespaceXml, el.tag().namespace(), String.format("Element %s not in XML namespace", el.tagName()));
-    }
+/**
+ * Test suite for the XmlTreeBuilder, focusing on pretty-printing behavior.
+ */
+public class XmlTreeBuilderTest {
 
     @Test
-    void prettyFormatsTextInline() {
-        // https://github.com/jhy/jsoup/issues/2141
-        String xml = "<package><metadata xmlns:dc=\"http://purl.org/dc/elements/1.1/\">\n" + "<dc:identifier id=\"pub-id\">id</dc:identifier>\n" + "<dc:title>title</dc:title>\n" + "<dc:language>ja</dc:language>\n" + "<dc:description>desc</dc:description>\n" + "</metadata></package>";
-        Document doc = Jsoup.parse(xml, Parser.xmlParser());
+    @DisplayName("XML pretty-printing of text-only elements should default to inline but can be customized to block format")
+    void xmlPrettyPrintingForTextOnlyElementsCanBeCustomized() {
+        // This test case addresses the behavior reported in: https://github.com/jhy/jsoup/issues/2141
+        String inputXml = """
+            <package>
+                <metadata xmlns:dc="http://purl.org/dc/elements/1.1/">
+                    <dc:identifier id="pub-id">id</dc:identifier>
+                    <dc:title>title</dc:title>
+                    <dc:language>ja</dc:language>
+                    <dc:description>desc</dc:description>
+                </metadata>
+            </package>""";
+
+        // Arrange: Parse the XML and enable pretty-printing.
+        Document doc = Jsoup.parse(inputXml, Parser.xmlParser());
         doc.outputSettings().prettyPrint(true);
-        assertEquals("<package>\n" + " <metadata xmlns:dc=\"http://purl.org/dc/elements/1.1/\">\n" + "  <dc:identifier id=\"pub-id\">id</dc:identifier> <dc:title>title</dc:title> <dc:language>ja</dc:language> <dc:description>desc</dc:description>\n" + " </metadata>\n" + "</package>", doc.html());
-        // can customize
-        Element meta = doc.expectFirst("metadata");
-        Tag metaTag = meta.tag();
-        metaTag.set(Tag.Block);
-        // set all the inner els of meta to be blocks
-        for (Element inner : meta) inner.tag().set(Tag.Block);
-        assertEquals("<package>\n" + " <metadata xmlns:dc=\"http://purl.org/dc/elements/1.1/\">\n" + "  <dc:identifier id=\"pub-id\">id</dc:identifier>\n" + "  <dc:title>title</dc:title>\n" + "  <dc:language>ja</dc:language>\n" + "  <dc:description>desc</dc:description>\n" + " </metadata>\n" + "</package>", doc.html());
+
+        // --- Part 1: Verify the default pretty-print behavior ---
+        // By default, elements containing only text are formatted inline to save space.
+        String expectedInlineOutput = """
+            <package>
+             <metadata xmlns:dc="http://purl.org/dc/elements/1.1/">
+              <dc:identifier id="pub-id">id</dc:identifier> <dc:title>title</dc:title> <dc:language>ja</dc:language> <dc:description>desc</dc:description>
+             </metadata>
+            </package>""";
+
+        // Act & Assert: Check the initial output.
+        assertEquals(expectedInlineOutput, doc.html());
+
+        // --- Part 2: Customize tag definitions to force block formatting ---
+        // By changing the tags to be block-level, we force each element onto a new, indented line.
+        Element metadataElement = doc.expectFirst("metadata");
+        metadataElement.tag().set(Tag.Block); // Treat <metadata> as a block element
+        for (Element child : metadataElement.children()) {
+            child.tag().set(Tag.Block); // Treat each child element as a block element
+        }
+
+        String expectedBlockOutput = """
+            <package>
+             <metadata xmlns:dc="http://purl.org/dc/elements/1.1/">
+              <dc:identifier id="pub-id">id</dc:identifier>
+              <dc:title>title</dc:title>
+              <dc:language>ja</dc:language>
+              <dc:description>desc</dc:description>
+             </metadata>
+            </package>""";
+
+        // Act & Assert: Check the output after customization.
+        assertEquals(expectedBlockOutput, doc.html());
     }
 }
