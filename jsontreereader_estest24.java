50c6328ede0bc8f1842169aba561ec5d4a745eb7
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:57:59 +0800

    Original processedAutoTestCases jsontreereader_estest24.java

---
+++ jsontreereader_estest24.java
@@ -0,0 +1,34 @@
+package com.google.gson.internal.bind;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonNull;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.Strictness;
+import com.google.gson.stream.JsonToken;
+import java.io.IOException;
+import java.util.ConcurrentModificationException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class JsonTreeReader_ESTestTest24 extends JsonTreeReader_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test023() throws Throwable {
+        JsonTreeReader jsonTreeReader0 = new JsonTreeReader((JsonElement) null);
+        // Undeclared exception!
+        try {
+            jsonTreeReader0.nextJsonElement();
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+        }
+    }
+}
