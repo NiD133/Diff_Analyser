80f2d730407bec38ff898756bba129ca104e9242
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:44:32 +0800

    gemini processedAutoTestCases collectionsortedbag_estest26.java

--- collectionsortedbag_estest26.java
+++ collectionsortedbag_estest26.java
@@ -1,56 +1,32 @@
 package org.apache.commons.collections4.bag;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.ConcurrentModificationException;
-import java.util.LinkedList;
-import java.util.Locale;
-import java.util.PriorityQueue;
-import java.util.Set;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.Predicate;
 import org.apache.commons.collections4.SortedBag;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionPredicate;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.IfClosure;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.NOPClosure;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.TransformedPredicate;
-import org.apache.commons.collections4.functors.TransformerPredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
+/**
+ * Tests for {@link CollectionSortedBag} focusing on exception handling with incompatible types.
+ */
 public class CollectionSortedBag_ESTestTest26 extends CollectionSortedBag_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test25() throws Throwable {
-        TreeBag<ComparatorPredicate.Criterion> treeBag0 = new TreeBag<ComparatorPredicate.Criterion>();
-        TreeBag<Object> treeBag1 = new TreeBag<Object>((Collection<?>) treeBag0);
-        CollectionSortedBag<Object> collectionSortedBag0 = new CollectionSortedBag<Object>(treeBag1);
-        // Undeclared exception!
-        try {
-            collectionSortedBag0.add((Object) treeBag0, 1000);
-            fail("Expecting exception: ClassCastException");
-        } catch (ClassCastException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-        }
+    /**
+     * Verifies that adding a non-comparable element to a CollectionSortedBag
+     * that is backed by a natural-ordering SortedBag (like TreeBag)
+     * throws a ClassCastException.
+     */
+    @Test(expected = ClassCastException.class)
+    public void addWithCount_whenElementIsNotComparable_shouldThrowClassCastException() {
+        // Arrange: Create a CollectionSortedBag decorating a TreeBag.
+        // A TreeBag requires its elements to be Comparable or to be configured with a Comparator.
+        SortedBag<Object> underlyingBag = new TreeBag<>();
+        SortedBag<Object> collectionSortedBag = new CollectionSortedBag<>(underlyingBag);
+
+        // Create an element that does not implement the Comparable interface.
+        // A new Object() or, as in the original test, another Bag instance works well.
+        Object nonComparableElement = new TreeBag<>();
+
+        // Act & Assert: Attempting to add the non-comparable element should fail.
+        // The underlying TreeBag will throw a ClassCastException when it tries to
+        // compare the new element to place it in sorted order.
+        collectionSortedBag.add(nonComparableElement, 1000);
     }
 }
