fa13095f50bab11aa43f27a6dac0013d172dca98
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:02:04 +0800

    Original processedAutoTestCases mergedstream_estest10.java

---
+++ mergedstream_estest10.java
@@ -0,0 +1,54 @@
+package com.fasterxml.jackson.core.io;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.fasterxml.jackson.core.ErrorReportConfiguration;
+import com.fasterxml.jackson.core.StreamReadConstraints;
+import com.fasterxml.jackson.core.StreamWriteConstraints;
+import com.fasterxml.jackson.core.util.BufferRecycler;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.io.PushbackInputStream;
+import java.io.SequenceInputStream;
+import java.util.Enumeration;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.evosuite.runtime.mock.java.io.MockFileInputStream;
+import org.junit.runner.RunWith;
+
+public class MergedStream_ESTestTest10 extends MergedStream_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test09() throws Throwable {
+        StreamReadConstraints streamReadConstraints0 = StreamReadConstraints.defaults();
+        StreamWriteConstraints streamWriteConstraints0 = StreamWriteConstraints.defaults();
+        ErrorReportConfiguration errorReportConfiguration0 = ErrorReportConfiguration.defaults();
+        BufferRecycler bufferRecycler0 = new BufferRecycler();
+        ContentReference contentReference0 = ContentReference.REDACTED_CONTENT;
+        IOContext iOContext0 = new IOContext(streamReadConstraints0, streamWriteConstraints0, errorReportConfiguration0, bufferRecycler0, contentReference0, false);
+        byte[] byteArray0 = new byte[0];
+        ByteArrayInputStream byteArrayInputStream0 = new ByteArrayInputStream(byteArray0);
+        PushbackInputStream pushbackInputStream0 = new PushbackInputStream(byteArrayInputStream0);
+        MergedStream mergedStream0 = new MergedStream(iOContext0, pushbackInputStream0, byteArray0, 1000, 200);
+        // Undeclared exception!
+        try {
+            mergedStream0.skip((-240L));
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("com.fasterxml.jackson.core.io.IOContext", e);
+        }
+    }
+}
