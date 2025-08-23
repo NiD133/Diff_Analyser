b3f33a9b32f2692cbfa1a132174af0de827edeb4
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:12:09 +0800

    gemini processedAutoTestCases randomaccessfileorarray_estest96.java

--- randomaccessfileorarray_estest96.java
+++ randomaccessfileorarray_estest96.java
@@ -1,41 +1,28 @@
 package com.itextpdf.text.pdf;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import com.itextpdf.text.io.GetBufferedRandomAccessSource;
-import com.itextpdf.text.io.IndependentRandomAccessSource;
-import com.itextpdf.text.io.RandomAccessSource;
-import com.itextpdf.text.io.WindowRandomAccessSource;
-import java.io.ByteArrayInputStream;
-import java.io.EOFException;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.PipedInputStream;
-import java.net.URL;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.net.MockURL;
-import org.evosuite.runtime.testdata.EvoSuiteFile;
-import org.evosuite.runtime.testdata.FileSystemHandling;
-import org.junit.runner.RunWith;
 
-public class RandomAccessFileOrArray_ESTestTest96 extends RandomAccessFileOrArray_ESTest_scaffolding {
+/**
+ * Tests for the {@link RandomAccessFileOrArray} class.
+ */
+public class RandomAccessFileOrArrayTest {
 
-    @Test(timeout = 4000)
-    public void test095() throws Throwable {
-        byte[] byteArray0 = new byte[1];
-        RandomAccessFileOrArray randomAccessFileOrArray0 = new RandomAccessFileOrArray(byteArray0);
-        // Undeclared exception!
-        try {
-            randomAccessFileOrArray0.read((byte[]) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("com.itextpdf.text.pdf.RandomAccessFileOrArray", e);
-        }
+    /**
+     * Verifies that calling the read(byte[]) method with a null buffer
+     * correctly throws a NullPointerException.
+     */
+    @Test(expected = NullPointerException.class)
+    public void readWithNullBufferShouldThrowNullPointerException() throws Exception {
+        // Arrange: Create an instance of RandomAccessFileOrArray with some dummy data.
+        // The content of the data is irrelevant for this test.
+        byte[] dummyData = new byte[1];
+        RandomAccessFileOrArray fileOrArray = new RandomAccessFileOrArray(dummyData);
+
+        // Act: Call the method under test with a null argument.
+        // This action is expected to throw the NullPointerException.
+        fileOrArray.read((byte[]) null);
+
+        // Assert: The test framework verifies that a NullPointerException was thrown.
+        // If no exception is thrown, the test will fail automatically.
     }
 }
