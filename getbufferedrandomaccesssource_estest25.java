d84f02e1b5d2060c50c68295ccfa34d1550a216e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:51:56 +0800

    Original processedAutoTestCases getbufferedrandomaccesssource_estest25.java

---
+++ getbufferedrandomaccesssource_estest25.java
@@ -0,0 +1,23 @@
+package com.itextpdf.text.io;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class GetBufferedRandomAccessSource_ESTestTest25 extends GetBufferedRandomAccessSource_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test24() throws Throwable {
+        byte[] byteArray0 = new byte[15];
+        ArrayRandomAccessSource arrayRandomAccessSource0 = new ArrayRandomAccessSource(byteArray0);
+        GetBufferedRandomAccessSource getBufferedRandomAccessSource0 = new GetBufferedRandomAccessSource(arrayRandomAccessSource0);
+        int int0 = getBufferedRandomAccessSource0.get((long) (byte) 0, byteArray0, (int) (byte) 0, (int) (byte) 0);
+        assertEquals(0, int0);
+    }
+}
