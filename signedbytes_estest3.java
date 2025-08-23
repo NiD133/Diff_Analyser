41abb473c93ed7537e4456459363073ee13bbff9
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:15:57 +0800

    Original processedAutoTestCases signedbytes_estest3.java

---
+++ signedbytes_estest3.java
@@ -0,0 +1,26 @@
+package com.google.common.primitives;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.Comparator;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class SignedBytes_ESTestTest3 extends SignedBytes_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test02() throws Throwable {
+        // Undeclared exception!
+        try {
+            SignedBytes.checkedCast(1940L);
+            fail("Expecting exception: IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //
+            // Out of range: java.lang.Long@0000000001
+            //
+            verifyException("com.google.common.base.Preconditions", e);
+        }
+    }
+}
