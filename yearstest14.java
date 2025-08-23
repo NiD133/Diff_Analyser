202e058056f7b024c8f95185546d5a6365bcba6e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:36:27 +0800

    Original processedManualTestCases yearstest14.java

---
+++ yearstest14.java
@@ -0,0 +1,45 @@
+package org.joda.time;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class YearsTestTest14 extends TestCase {
+
+    // (before the late 90's they were all over the place)
+    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
+
+    public static TestSuite suite() {
+        return new TestSuite(TestYears.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+    }
+
+    //-----------------------------------------------------------------------
+    public void testPlus_int() {
+        Years test2 = Years.years(2);
+        Years result = test2.plus(3);
+        assertEquals(2, test2.getYears());
+        assertEquals(5, result.getYears());
+        assertEquals(1, Years.ONE.plus(0).getYears());
+        try {
+            Years.MAX_VALUE.plus(1);
+            fail();
+        } catch (ArithmeticException ex) {
+            // expected
+        }
+    }
+}
