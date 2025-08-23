219e79b02e17cf5b9dfe1894cd7244625b510d14
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:30:27 +0800

    gemini processedAutoTestCases years_estest47.java

--- years_estest47.java
+++ years_estest47.java
@@ -1,17 +1,22 @@
 package org.joda.time;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class Years_ESTestTest47 extends Years_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link Years} class.
+ */
+public class YearsTest {
 
-    @Test(timeout = 4000)
-    public void test46() throws Throwable {
-        Years years0 = Years.years(Integer.MIN_VALUE);
-        assertEquals(Integer.MIN_VALUE, years0.getYears());
+    @Test
+    public void yearsFactory_shouldCreateYearsWithMinimumValue() {
+        // Arrange: Define the minimum possible integer value for years.
+        final int minValue = Integer.MIN_VALUE;
+
+        // Act: Create a Years instance using the factory method.
+        Years years = Years.years(minValue);
+
+        // Assert: Verify that the created instance holds the correct minimum value.
+        assertEquals(minValue, years.getYears());
     }
 }
