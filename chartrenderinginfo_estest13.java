a708dbfd168b8461bef08835fc3f09a30498e58e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:43:10 +0800

    gemini processedAutoTestCases chartrenderinginfo_estest13.java

--- chartrenderinginfo_estest13.java
+++ chartrenderinginfo_estest13.java
@@ -1,29 +1,25 @@
 package org.jfree.chart;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.awt.Rectangle;
-import java.awt.geom.Rectangle2D;
-import java.util.Locale;
-import java.util.SimpleTimeZone;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.jfree.chart.axis.DateAxis;
-import org.jfree.chart.entity.EntityCollection;
-import org.jfree.chart.entity.StandardEntityCollection;
-import org.jfree.chart.plot.CombinedRangeCategoryPlot;
-import org.jfree.chart.plot.PlotRenderingInfo;
-import org.jfree.data.xy.XYDatasetTableModel;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertFalse;
 
-public class ChartRenderingInfo_ESTestTest13 extends ChartRenderingInfo_ESTest_scaffolding {
+/**
+ * Contains tests for the equals() method of the {@link ChartRenderingInfo} class.
+ */
+public class ChartRenderingInfoTest {
 
-    @Test(timeout = 4000)
-    public void test12() throws Throwable {
-        ChartRenderingInfo chartRenderingInfo0 = new ChartRenderingInfo();
-        XYDatasetTableModel xYDatasetTableModel0 = new XYDatasetTableModel();
-        boolean boolean0 = chartRenderingInfo0.equals(xYDatasetTableModel0);
-        assertFalse(boolean0);
+    /**
+     * Verifies that the equals() method returns false when a ChartRenderingInfo
+     * object is compared to an object of a completely different type.
+     */
+    @Test
+    public void equals_shouldReturnFalse_whenComparedWithDifferentType() {
+        // Arrange
+        ChartRenderingInfo info = new ChartRenderingInfo();
+        Object unrelatedObject = new Object(); // An object of a different class
+
+        // Act & Assert
+        // The equals method must return false for objects of different types.
+        assertFalse(info.equals(unrelatedObject));
     }
 }
