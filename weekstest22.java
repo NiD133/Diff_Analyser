7a037dc500e0946904b311c87ea2437145980bc3
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:35:53 +0800

    Original processedManualTestCases weekstest22.java

---
+++ weekstest22.java
@@ -0,0 +1,44 @@
+package org.joda.time;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class WeeksTestTest22 extends TestCase {
+
+    // (before the late 90's they were all over the place)
+    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
+
+    public static TestSuite suite() {
+        return new TestSuite(TestWeeks.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+    }
+
+    public void testMinus_int() {
+        Weeks test2 = Weeks.weeks(2);
+        Weeks result = test2.minus(3);
+        assertEquals(2, test2.getWeeks());
+        assertEquals(-1, result.getWeeks());
+        assertEquals(1, Weeks.ONE.minus(0).getWeeks());
+        try {
+            Weeks.MIN_VALUE.minus(1);
+            fail();
+        } catch (ArithmeticException ex) {
+            // expected
+        }
+    }
+}
