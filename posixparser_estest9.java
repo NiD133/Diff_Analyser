e4d5e2c9696c25d717ddd4126bd7bc18acda3db7
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:09:12 +0800

    gemini processedAutoTestCases posixparser_estest9.java

--- posixparser_estest9.java
+++ posixparser_estest9.java
@@ -1,22 +1,33 @@
 package org.apache.commons.cli;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.Locale;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertArrayEquals;
 
-public class PosixParser_ESTestTest9 extends PosixParser_ESTest_scaffolding {
+/**
+ * Test case for the PosixParser class, focusing on the flatten method's behavior.
+ */
+public class PosixParserTest {
 
-    @Test(timeout = 4000)
-    public void test08() throws Throwable {
-        Options options0 = new Options();
-        PosixParser posixParser0 = new PosixParser();
-        String[] stringArray0 = new String[9];
-        stringArray0[5] = "--=<q;n";
-        String[] stringArray1 = posixParser0.flatten(options0, stringArray0, false);
-        assertEquals(1, stringArray1.length);
+    /**
+     * Tests that a token resembling a long option with a value (e.g., "--opt=value"),
+     * but which is not a defined option, is treated as a single argument when
+     * stopAtNonOption is false.
+     */
+    @Test
+    public void flattenShouldTreatUnrecognizedLongOptionWithEqualsAsSingleArgument() throws ParseException {
+        // Arrange
+        PosixParser parser = new PosixParser();
+        Options options = new Options(); // No options are defined.
+        String[] arguments = {"--=<q;n"}; // The input token is an unrecognized long option with a value.
+
+        // Act
+        // The flatten method is called with stopAtNonOption set to false.
+        String[] flattenedArgs = parser.flatten(options, arguments, false);
+
+        // Assert
+        // The parser should treat the entire token as a single, regular argument because
+        // "--" is not a registered option and we are not stopping at non-options.
+        String[] expected = {"--=<q;n"};
+        assertArrayEquals("The unrecognized token should be returned as a single argument", expected, flattenedArgs);
     }
 }
