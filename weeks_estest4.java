f29d57aff0321e1976eb5c800b04533d9d45a6b3
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:27:16 +0800

    gemini processedAutoTestCases weeks_estest4.java

--- weeks_estest4.java
+++ weeks_estest4.java
@@ -1,18 +1,25 @@
 package org.joda.time;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertFalse;
 
-public class Weeks_ESTestTest4 extends Weeks_ESTest_scaffolding {
+/**
+ * Test suite for the {@link Weeks} class.
+ */
+public class WeeksTest {
 
-    @Test(timeout = 4000)
-    public void test03() throws Throwable {
-        Weeks weeks0 = Weeks.MIN_VALUE;
-        boolean boolean0 = weeks0.isGreaterThan((Weeks) null);
-        assertFalse(boolean0);
+    @Test
+    public void isGreaterThan_withNullOperand_shouldReturnFalseForMinValue() {
+        // The Javadoc for isGreaterThan states that a null operand is treated as zero.
+        // This test verifies that the minimum possible Weeks value is not greater than zero.
+
+        // Arrange
+        Weeks minWeeks = Weeks.MIN_VALUE;
+
+        // Act
+        boolean isGreater = minWeeks.isGreaterThan(null);
+
+        // Assert
+        assertFalse("Weeks.MIN_VALUE should not be greater than null (treated as zero)", isGreater);
     }
 }
