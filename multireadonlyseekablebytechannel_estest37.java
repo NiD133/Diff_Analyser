a690cd1b771bddfbd029f8b1c50d204c3529a472
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:04:18 +0800

    gemini processedAutoTestCases multireadonlyseekablebytechannel_estest37.java

--- multireadonlyseekablebytechannel_estest37.java
+++ multireadonlyseekablebytechannel_estest37.java
@@ -1,35 +1,42 @@
 package org.apache.commons.compress.utils;
 
+import org.junit.Rule;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
+import org.junit.rules.TemporaryFolder;
+
 import java.io.File;
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.channels.ClosedChannelException;
-import java.nio.channels.FileChannel;
-import java.nio.channels.NonWritableChannelException;
 import java.nio.channels.SeekableByteChannel;
-import java.nio.file.NoSuchFileException;
-import java.nio.file.OpenOption;
-import java.nio.file.Path;
-import java.util.LinkedList;
-import java.util.List;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.io.MockFile;
-import org.junit.runner.RunWith;
 
-public class MultiReadOnlySeekableByteChannel_ESTestTest37 extends MultiReadOnlySeekableByteChannel_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
 
-    @Test(timeout = 4000)
-    public void test36() throws Throwable {
-        File[] fileArray0 = new File[2];
-        MockFile mockFile0 = new MockFile("");
-        fileArray0[0] = (File) mockFile0;
-        fileArray0[1] = (File) mockFile0;
-        SeekableByteChannel seekableByteChannel0 = MultiReadOnlySeekableByteChannel.forFiles(fileArray0);
-        long long0 = seekableByteChannel0.position();
-        assertEquals(0L, long0);
+/**
+ * Tests for {@link MultiReadOnlySeekableByteChannel}.
+ */
+public class MultiReadOnlySeekableByteChannelTest {
+
+    @Rule
+    public TemporaryFolder tempFolder = new TemporaryFolder();
+
+    /**
+     * Verifies that a newly created MultiReadOnlySeekableByteChannel
+     * has an initial position of 0.
+     */
+    @Test
+    public void initialPositionShouldBeZero() throws IOException {
+        // Arrange: Create a set of empty files to be concatenated.
+        // The content of the files is irrelevant for this test.
+        File file1 = tempFolder.newFile("test1.txt");
+        File file2 = tempFolder.newFile("test2.txt");
+        File[] inputFiles = {file1, file2};
+
+        // Act: Create the multi-channel and get its initial position.
+        // A try-with-resources statement ensures the channel is closed automatically.
+        try (SeekableByteChannel multiChannel = MultiReadOnlySeekableByteChannel.forFiles(inputFiles)) {
+            long initialPosition = multiChannel.position();
+
+            // Assert: The initial position should be zero.
+            assertEquals("A new channel's initial position should be 0.", 0L, initialPosition);
+        }
     }
 }
