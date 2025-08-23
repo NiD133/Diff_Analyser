ad7e4b52234c5f4809570e889099f2f70ae37dc4
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:29:20 +0800

    Original processedManualTestCases hostspecifiertest4.java

---
+++ hostspecifiertest4.java
@@ -0,0 +1,52 @@
+package com.google.common.net;
+
+import static com.google.common.truth.Truth.assertThat;
+import com.google.common.collect.ImmutableList;
+import com.google.common.testing.EqualsTester;
+import com.google.common.testing.NullPointerTester;
+import java.text.ParseException;
+import junit.framework.TestCase;
+import org.jspecify.annotations.NullUnmarked;
+
+public class HostSpecifierTestTest4 extends TestCase {
+
+    private static final ImmutableList<String> GOOD_IPS = ImmutableList.of("1.2.3.4", "2001:db8::1", "[2001:db8::1]");
+
+    private static final ImmutableList<String> BAD_IPS = ImmutableList.of("1.2.3", "2001:db8::1::::::0", "[2001:db8::1", "[::]:80");
+
+    private static final ImmutableList<String> GOOD_DOMAINS = ImmutableList.of("com", "google.com", "foo.co.uk");
+
+    private static final ImmutableList<String> BAD_DOMAINS = ImmutableList.of("foo.blah", "", "[google.com]");
+
+    private static HostSpecifier spec(String specifier) {
+        return HostSpecifier.fromValid(specifier);
+    }
+
+    private void assertGood(String spec) throws ParseException {
+        // Throws exception if not working correctly
+        HostSpecifier unused = HostSpecifier.fromValid(spec);
+        unused = HostSpecifier.from(spec);
+        assertTrue(HostSpecifier.isValid(spec));
+    }
+
+    private void assertBad(String spec) {
+        try {
+            HostSpecifier.fromValid(spec);
+            fail("Should have thrown IllegalArgumentException: " + spec);
+        } catch (IllegalArgumentException expected) {
+        }
+        try {
+            HostSpecifier.from(spec);
+            fail("Should have thrown ParseException: " + spec);
+        } catch (ParseException expected) {
+            assertThat(expected).hasCauseThat().isInstanceOf(IllegalArgumentException.class);
+        }
+        assertFalse(HostSpecifier.isValid(spec));
+    }
+
+    public void testBadDomains() {
+        for (String spec : BAD_DOMAINS) {
+            assertBad(spec);
+        }
+    }
+}
