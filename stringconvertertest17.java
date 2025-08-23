fcf838c3171c02ee9f1b8eb5d13f1a14be042f97
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:34:08 +0800

    gemini processedManualTestCases stringconvertertest17.java

--- stringconvertertest17.java
+++ stringconvertertest17.java
@@ -1,97 +1,48 @@
 package org.joda.time.convert;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-import java.util.Arrays;
-import java.util.Locale;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-import org.joda.time.Chronology;
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
-import org.joda.time.MutableInterval;
-import org.joda.time.MutablePeriod;
-import org.joda.time.PeriodType;
-import org.joda.time.TimeOfDay;
-import org.joda.time.chrono.BuddhistChronology;
-import org.joda.time.chrono.ISOChronology;
-import org.joda.time.chrono.JulianChronology;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
-public class StringConverterTestTest17 extends TestCase {
+/**
+ * Test suite for the getDurationMillis method in {@link StringConverter}.
+ * <p>
+ * This suite focuses on verifying the parsing of ISO 8601 duration strings
+ * that contain seconds and fractional seconds (e.g., "PT12.345S").
+ */
+public class StringConverterGetDurationMillisTest {
 
-    private static final DateTimeZone ONE_HOUR = DateTimeZone.forOffsetHours(1);
+    private final StringConverter converter = StringConverter.INSTANCE;
 
-    private static final DateTimeZone SIX = DateTimeZone.forOffsetHours(6);
-
-    private static final DateTimeZone SEVEN = DateTimeZone.forOffsetHours(7);
-
-    private static final DateTimeZone EIGHT = DateTimeZone.forOffsetHours(8);
-
-    private static final DateTimeZone UTC = DateTimeZone.UTC;
-
-    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
-
-    private static final DateTimeZone LONDON = DateTimeZone.forID("Europe/London");
-
-    private static final Chronology ISO_EIGHT = ISOChronology.getInstance(EIGHT);
-
-    private static final Chronology ISO_PARIS = ISOChronology.getInstance(PARIS);
-
-    private static final Chronology ISO_LONDON = ISOChronology.getInstance(LONDON);
-
-    private static Chronology ISO;
-
-    private static Chronology JULIAN;
-
-    private DateTimeZone zone = null;
-
-    private Locale locale = null;
-
-    public static void main(String[] args) {
-        junit.textui.TestRunner.run(suite());
+    @Test
+    public void getDurationMillis_shouldParsePositiveDurations() {
+        // Standard format with 3 decimal places
+        assertEquals(12345L, converter.getDurationMillis("PT12.345S"));
+        
+        // Lowercase format should also be accepted
+        assertEquals(12345L, converter.getDurationMillis("pt12.345s"));
+        
+        // Whole seconds without a decimal part
+        assertEquals(12000L, converter.getDurationMillis("pt12s"));
+        
+        // Whole seconds with a trailing decimal point
+        assertEquals(12000L, converter.getDurationMillis("pt12.s"));
     }
 
-    public static TestSuite suite() {
-        return new TestSuite(TestStringConverter.class);
+    @Test
+    public void getDurationMillis_shouldParseNegativeDurations() {
+        assertEquals(-12320L, converter.getDurationMillis("pt-12.32s"));
+        assertEquals(-320L, converter.getDurationMillis("pt-0.32s"));
     }
 
-    @Override
-    protected void setUp() throws Exception {
-        zone = DateTimeZone.getDefault();
-        locale = Locale.getDefault();
-        DateTimeZone.setDefault(LONDON);
-        Locale.setDefault(Locale.UK);
-        JULIAN = JulianChronology.getInstance();
-        ISO = ISOChronology.getInstance();
+    @Test
+    public void getDurationMillis_shouldParseZeroDuration() {
+        assertEquals(0L, converter.getDurationMillis("pt-0.0s"));
+        assertEquals(0L, converter.getDurationMillis("pt0.0s"));
     }
 
-    @Override
-    protected void tearDown() throws Exception {
-        DateTimeZone.setDefault(zone);
-        Locale.setDefault(locale);
-        zone = null;
-    }
-
-    //-----------------------------------------------------------------------
-    public void testGetDurationMillis_Object1() throws Exception {
-        long millis = StringConverter.INSTANCE.getDurationMillis("PT12.345S");
-        assertEquals(12345, millis);
-        millis = StringConverter.INSTANCE.getDurationMillis("pt12.345s");
-        assertEquals(12345, millis);
-        millis = StringConverter.INSTANCE.getDurationMillis("pt12s");
-        assertEquals(12000, millis);
-        millis = StringConverter.INSTANCE.getDurationMillis("pt12.s");
-        assertEquals(12000, millis);
-        millis = StringConverter.INSTANCE.getDurationMillis("pt-12.32s");
-        assertEquals(-12320, millis);
-        millis = StringConverter.INSTANCE.getDurationMillis("pt-0.32s");
-        assertEquals(-320, millis);
-        millis = StringConverter.INSTANCE.getDurationMillis("pt-0.0s");
-        assertEquals(0, millis);
-        millis = StringConverter.INSTANCE.getDurationMillis("pt0.0s");
-        assertEquals(0, millis);
-        millis = StringConverter.INSTANCE.getDurationMillis("pt12.3456s");
-        assertEquals(12345, millis);
+    @Test
+    public void getDurationMillis_shouldTruncateSubMillisecondPrecision() {
+        // The parser should ignore digits beyond the third decimal place (milliseconds)
+        assertEquals(12345L, converter.getDurationMillis("pt12.3456s"));
     }
 }
