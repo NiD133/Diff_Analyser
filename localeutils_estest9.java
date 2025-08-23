1339781d58ac76ffbbf36fcda08df758a06ded1a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:01:11 +0800

    Original processedAutoTestCases localeutils_estest9.java

---
+++ localeutils_estest9.java
@@ -0,0 +1,28 @@
+package org.apache.commons.lang3;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.List;
+import java.util.Locale;
+import java.util.Set;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class LocaleUtils_ESTestTest9 extends LocaleUtils_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test08() throws Throwable {
+        // Undeclared exception!
+        try {
+            LocaleUtils.toLocale("-CIIN");
+            fail("Expecting exception: IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //
+            // Invalid locale format: -CIIN
+            //
+            verifyException("org.apache.commons.lang3.LocaleUtils", e);
+        }
+    }
+}
