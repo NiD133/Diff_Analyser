27ca3392a75397c926c50ad31a2f65340da113bc
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:44:31 +0800

    gemini processedAutoTestCases collectionsortedbag_estest24.java

--- collectionsortedbag_estest24.java
+++ collectionsortedbag_estest24.java
@@ -1,59 +1,30 @@
 package org.apache.commons.collections4.bag;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.ConcurrentModificationException;
-import java.util.LinkedList;
-import java.util.Locale;
-import java.util.PriorityQueue;
-import java.util.Set;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.Predicate;
 import org.apache.commons.collections4.SortedBag;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionPredicate;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.IfClosure;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.NOPClosure;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.TransformedPredicate;
-import org.apache.commons.collections4.functors.TransformerPredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class CollectionSortedBag_ESTestTest24 extends CollectionSortedBag_ESTest_scaffolding {
+/**
+ * This test class demonstrates the behavior of the CollectionSortedBag decorator.
+ * The original test case was refactored for improved understandability.
+ */
+public class CollectionSortedBagTest {
 
-    @Test(timeout = 4000)
-    public void test23() throws Throwable {
-        TreeBag<IfTransformer<Object, Object>> treeBag0 = new TreeBag<IfTransformer<Object, Object>>();
-        SynchronizedSortedBag<IfTransformer<Object, Object>> synchronizedSortedBag0 = new SynchronizedSortedBag<IfTransformer<Object, Object>>(treeBag0);
-        Transformer<Object, IfTransformer<Object, Object>> transformer0 = ConstantTransformer.nullTransformer();
-        TransformedSortedBag<IfTransformer<Object, Object>> transformedSortedBag0 = TransformedSortedBag.transformingSortedBag((SortedBag<IfTransformer<Object, Object>>) synchronizedSortedBag0, (Transformer<? super IfTransformer<Object, Object>, ? extends IfTransformer<Object, Object>>) transformer0);
-        CollectionSortedBag<IfTransformer<Object, Object>> collectionSortedBag0 = new CollectionSortedBag<IfTransformer<Object, Object>>(transformedSortedBag0);
-        // Undeclared exception!
-        try {
-            collectionSortedBag0.add((IfTransformer<Object, Object>) null, 1020);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("java.util.TreeMap", e);
-        }
+    /**
+     * Tests that calling add(element, count) on a CollectionSortedBag throws a
+     * NullPointerException if the element is null and the underlying decorated
+     * bag (in this case, a TreeBag) does not support null elements.
+     */
+    @Test(expected = NullPointerException.class)
+    public void addWithCount_whenElementIsNullAndUnderlyingBagDisallowsNulls_throwsNullPointerException() {
+        // Arrange: Create a CollectionSortedBag decorating a TreeBag.
+        // A TreeBag is a SortedBag implementation that does not permit null elements.
+        final SortedBag<String> treeBag = new TreeBag<>();
+        final SortedBag<String> collectionSortedBag = new CollectionSortedBag<>(treeBag);
+        final int count = 10;
+
+        // Act & Assert: Attempt to add a null element.
+        // This call should be forwarded to the underlying TreeBag, which will
+        // throw a NullPointerException, which is then expected by the test.
+        collectionSortedBag.add(null, count);
     }
 }
