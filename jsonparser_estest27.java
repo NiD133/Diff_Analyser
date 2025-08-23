2b0a0a50cc8d89a174d000aa71ac11743da6b934
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:57:14 +0800

    Original processedAutoTestCases jsonparser_estest27.java

---
+++ jsonparser_estest27.java
@@ -0,0 +1,32 @@
+package com.google.gson;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.google.gson.stream.JsonReader;
+import java.io.Reader;
+import java.io.StringReader;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class JsonParser_ESTestTest27 extends JsonParser_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test26() throws Throwable {
+        StringReader stringReader0 = new StringReader("MUB_qz@d");
+        JsonReader jsonReader0 = new JsonReader(stringReader0);
+        Strictness strictness0 = Strictness.STRICT;
+        jsonReader0.setStrictness(strictness0);
+        try {
+            JsonParser.parseReader(jsonReader0);
+            fail("Expecting exception: RuntimeException");
+        } catch (RuntimeException e) {
+            //
+            // org.evosuite.runtime.mock.java.lang.MockThrowable: Use JsonReader.setStrictness(Strictness.LENIENT) to accept malformed JSON at line 1 column 1 path $
+            // See https://github.com/google/gson/blob/main/Troubleshooting.md#malformed-json
+            //
+            verifyException("com.google.gson.internal.Streams", e);
+        }
+    }
+}
