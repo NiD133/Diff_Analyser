286aba795ee5ad8406ea73bd13c57e337df351ef
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:55:01 +0800

    Original processedAutoTestCases iocase_estest41.java

---
+++ iocase_estest41.java
@@ -0,0 +1,18 @@
+package org.apache.commons.io;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class IOCase_ESTestTest41 extends IOCase_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test40() throws Throwable {
+        IOCase iOCase0 = IOCase.SYSTEM;
+        IOCase iOCase1 = IOCase.value((IOCase) null, iOCase0);
+        assertEquals(IOCase.SYSTEM, iOCase1);
+    }
+}
