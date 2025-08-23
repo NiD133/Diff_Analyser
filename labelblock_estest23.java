2cc16dfe7a3c813694c4e22970b2e2e7694a59dd
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:59:39 +0800

    Original processedAutoTestCases labelblock_estest23.java

---
+++ labelblock_estest23.java
@@ -0,0 +1,43 @@
+package org.jfree.chart.block;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.Paint;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.SystemColor;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.text.AttributedCharacterIterator;
+import java.util.Hashtable;
+import javax.swing.text.DefaultStyledDocument;
+import javax.swing.text.StyleContext;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.jfree.chart.api.RectangleAnchor;
+import org.jfree.chart.text.TextBlockAnchor;
+import org.jfree.data.Range;
+import org.junit.runner.RunWith;
+
+public class LabelBlock_ESTestTest23 extends LabelBlock_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test22() throws Throwable {
+        LabelBlock labelBlock0 = new LabelBlock("F\"M5 ; ws");
+        Font font0 = Font.decode("F\"M5 ; ws");
+        DefaultStyledDocument defaultStyledDocument0 = new DefaultStyledDocument();
+        StyleContext styleContext0 = StyleContext.getDefaultStyleContext();
+        StyleContext.NamedStyle styleContext_NamedStyle0 = styleContext0.new NamedStyle();
+        Color color0 = defaultStyledDocument0.getBackground(styleContext_NamedStyle0);
+        LabelBlock labelBlock1 = new LabelBlock("SansSerif", font0, color0);
+        boolean boolean0 = labelBlock0.equals(labelBlock1);
+        assertFalse(boolean0);
+    }
+}
