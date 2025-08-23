2935c25b87a54e6b166d46ecce7105d4af653cb4
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:05:39 +0800

    gemini processedAutoTestCases objectgraphiterator_estest14.java

--- objectgraphiterator_estest14.java
+++ objectgraphiterator_estest14.java
@@ -1,55 +1,38 @@
 package org.apache.commons.collections4.iterators;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Comparator;
-import java.util.ConcurrentModificationException;
-import java.util.HashMap;
+
+import java.util.Collections;
 import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.ListIterator;
-import java.util.Map;
-import java.util.NoSuchElementException;
-import java.util.function.Function;
-import org.apache.commons.collections4.Factory;
-import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AndPredicate;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantFactory;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionPredicate;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.FactoryTransformer;
-import org.apache.commons.collections4.functors.FalsePredicate;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.MapTransformer;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NullIsFalsePredicate;
-import org.apache.commons.collections4.functors.NullIsTruePredicate;
-import org.apache.commons.collections4.functors.OrPredicate;
-import org.apache.commons.collections4.functors.PredicateTransformer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
 
-public class ObjectGraphIterator_ESTestTest14 extends ObjectGraphIterator_ESTest_scaffolding {
+import static org.junit.Assert.assertFalse;
 
-    @Test(timeout = 4000)
-    public void test13() throws Throwable {
-        LinkedList<Integer> linkedList0 = new LinkedList<Integer>();
-        Iterator<Integer> iterator0 = linkedList0.descendingIterator();
-        ObjectGraphIterator<Integer> objectGraphIterator0 = new ObjectGraphIterator<Integer>(iterator0);
-        ListIterator<Integer> listIterator0 = linkedList0.listIterator();
-        objectGraphIterator0.findNextByIterator(listIterator0);
-        assertFalse(listIterator0.hasNext());
+/**
+ * Contains tests for the protected method findNextByIterator in {@link ObjectGraphIterator}.
+ */
+public class ObjectGraphIterator_ESTestTest14 {
+
+    /**
+     * Tests that calling findNextByIterator with an empty iterator correctly
+     * handles it by simply exhausting it.
+     */
+    @Test
+    public void findNextByIteratorWithEmptyIteratorShouldResultInExhaustedIterator() {
+        // Arrange
+        // An ObjectGraphIterator instance is needed to call the protected method.
+        // Its initial state is not critical for this test, so we use an empty root iterator.
+        final ObjectGraphIterator<Object> graphIterator = new ObjectGraphIterator<>(Collections.emptyIterator());
+
+        // This is the empty iterator that will be passed to the method under test.
+        final Iterator<Object> emptyIteratorToSearch = Collections.emptyIterator();
+
+        // Act
+        // The method should attempt to find a next element by consuming the provided iterator.
+        // Since the iterator is empty, it will be immediately exhausted.
+        graphIterator.findNextByIterator(emptyIteratorToSearch);
+
+        // Assert
+        // Verify that the iterator passed to the method has been fully traversed.
+        assertFalse("The empty iterator should be exhausted after the call", emptyIteratorToSearch.hasNext());
     }
 }
