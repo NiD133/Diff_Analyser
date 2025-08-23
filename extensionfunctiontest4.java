2e50bd1ef5db7664924df85fdd0103804ce482ea
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:28:38 +0800

    Original processedManualTestCases extensionfunctiontest4.java

---
+++ extensionfunctiontest4.java
@@ -0,0 +1,109 @@
+package org.apache.commons.jxpath.ri.compiler;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Locale;
+import org.apache.commons.jxpath.AbstractJXPathTest;
+import org.apache.commons.jxpath.ClassFunctions;
+import org.apache.commons.jxpath.ExpressionContext;
+import org.apache.commons.jxpath.Function;
+import org.apache.commons.jxpath.FunctionLibrary;
+import org.apache.commons.jxpath.Functions;
+import org.apache.commons.jxpath.JXPathContext;
+import org.apache.commons.jxpath.NodeSet;
+import org.apache.commons.jxpath.PackageFunctions;
+import org.apache.commons.jxpath.Pointer;
+import org.apache.commons.jxpath.TestBean;
+import org.apache.commons.jxpath.Variables;
+import org.apache.commons.jxpath.ri.model.NodePointer;
+import org.apache.commons.jxpath.util.JXPath11CompatibleTypeConverter;
+import org.apache.commons.jxpath.util.TypeConverter;
+import org.apache.commons.jxpath.util.TypeUtils;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ExtensionFunctionTestTest4 extends AbstractJXPathTest {
+
+    private Functions functions;
+
+    private JXPathContext context;
+
+    private TestBean testBean;
+
+    private TypeConverter typeConverter;
+
+    @Override
+    @BeforeEach
+    public void setUp() {
+        if (context == null) {
+            testBean = new TestBean();
+            context = JXPathContext.newContext(testBean);
+            final Variables vars = context.getVariables();
+            vars.declareVariable("test", new TestFunctions(4, "test"));
+            final FunctionLibrary lib = new FunctionLibrary();
+            lib.addFunctions(new ClassFunctions(TestFunctions.class, "test"));
+            lib.addFunctions(new ClassFunctions(TestFunctions2.class, "test"));
+            lib.addFunctions(new PackageFunctions("", "call"));
+            lib.addFunctions(new PackageFunctions("org.apache.commons.jxpath.ri.compiler.", "jxpathtest"));
+            lib.addFunctions(new PackageFunctions("", null));
+            context.setFunctions(lib);
+            context.getVariables().declareVariable("List.class", List.class);
+            context.getVariables().declareVariable("NodeSet.class", NodeSet.class);
+        }
+        functions = new ClassFunctions(TestFunctions.class, "test");
+        typeConverter = TypeUtils.getTypeConverter();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        TypeUtils.setTypeConverter(typeConverter);
+    }
+
+    private static final class Context implements ExpressionContext {
+
+        private final Object object;
+
+        public Context(final Object object) {
+            this.object = object;
+        }
+
+        @Override
+        public List<Pointer> getContextNodeList() {
+            return null;
+        }
+
+        @Override
+        public Pointer getContextNodePointer() {
+            return NodePointer.newNodePointer(null, object, Locale.getDefault());
+        }
+
+        @Override
+        public JXPathContext getJXPathContext() {
+            return null;
+        }
+
+        @Override
+        public int getPosition() {
+            return 0;
+        }
+    }
+
+    @Test
+    void testCollectionReturn() {
+        assertXPathValueIterator(context, "test:collection()/name", list("foo", "bar"));
+        assertXPathPointerIterator(context, "test:collection()/name", list("/.[1]/name", "/.[2]/name"));
+        assertXPathValue(context, "test:collection()/name", "foo");
+        assertXPathValue(context, "test:collection()/@name", "foo");
+        final List list = new ArrayList();
+        list.add("foo");
+        list.add("bar");
+        context.getVariables().declareVariable("list", list);
+        final Object values = context.getValue("test:items($list)");
+        assertInstanceOf(Collection.class, values, "Return type: ");
+        assertEquals(list, new ArrayList((Collection) values), "Return values: ");
+    }
+}
