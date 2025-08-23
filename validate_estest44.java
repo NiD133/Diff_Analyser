e27ace3c80e4b0ff450282f5d1820eb595786f3a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:26:06 +0800

    gemini processedAutoTestCases validate_estest44.java

--- validate_estest44.java
+++ validate_estest44.java
@@ -1,26 +1,24 @@
 package org.jsoup.helper;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.FormatFlagsConversionMismatchException;
-import java.util.IllegalFormatConversionException;
-import java.util.IllegalFormatFlagsException;
-import java.util.IllegalFormatWidthException;
-import java.util.MissingFormatArgumentException;
-import java.util.MissingFormatWidthException;
-import java.util.UnknownFormatConversionException;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class Validate_ESTestTest44 extends Validate_ESTest_scaffolding {
+/**
+ * Test suite for the Validate helper class.
+ */
+public class ValidateTest {
 
-    @Test(timeout = 4000)
-    public void test43() throws Throwable {
-        Object[] objectArray0 = new Object[2];
-        objectArray0[1] = (Object) "";
-        Validate.notNullParam(objectArray0[1], "p4%]]3_}o");
-        assertEquals(2, objectArray0.length);
+    /**
+     * Verifies that notNullParam() does not throw an exception when the provided object is not null.
+     */
+    @Test
+    public void notNullParamShouldNotThrowExceptionForNonNullObject() {
+        // Arrange
+        Object nonNullObject = "I am not null";
+        String parameterName = "testParameter";
+
+        // Act & Assert
+        // The test's purpose is to ensure this method call completes without throwing an exception.
+        // JUnit will automatically fail the test if any unexpected exception is thrown.
+        Validate.notNullParam(nonNullObject, parameterName);
     }
 }
