83fa7ab738868763fe7ded0b68c766941fa96f8d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:46:07 +0800

    Original processedAutoTestCases countingpathvisitor_estest18.java

---
+++ countingpathvisitor_estest18.java
@@ -0,0 +1,48 @@
+package org.apache.commons.io.file;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.function.UnaryOperator;
+import org.apache.commons.io.filefilter.CanWriteFileFilter;
+import org.apache.commons.io.filefilter.EmptyFileFilter;
+import org.apache.commons.io.filefilter.FileFileFilter;
+import org.apache.commons.io.filefilter.HiddenFileFilter;
+import org.apache.commons.io.filefilter.IOFileFilter;
+import org.apache.commons.io.filefilter.NotFileFilter;
+import org.apache.commons.io.filefilter.PathEqualsFileFilter;
+import org.apache.commons.io.filefilter.SuffixFileFilter;
+import org.apache.commons.io.function.IOBiFunction;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.evosuite.runtime.mock.java.io.MockIOException;
+import org.junit.runner.RunWith;
+
+public class CountingPathVisitor_ESTestTest18 extends CountingPathVisitor_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test17() throws Throwable {
+        CountingPathVisitor countingPathVisitor0 = CountingPathVisitor.withLongCounters();
+        MockFile mockFile0 = new MockFile("");
+        Path path0 = mockFile0.toPath();
+        // Undeclared exception!
+        try {
+            countingPathVisitor0.visitFile(path0, (BasicFileAttributes) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.apache.commons.io.file.CountingPathVisitor", e);
+        }
+    }
+}
