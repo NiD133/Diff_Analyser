be992afacb78bd49359596b079c93665a5299b8a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:49:46 +0800

    Original processedAutoTestCases ethiopicchronology_estest4.java

---
+++ ethiopicchronology_estest4.java
@@ -0,0 +1,20 @@
+package org.joda.time.chrono;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.joda.time.Chronology;
+import org.joda.time.DateTimeZone;
+import org.junit.runner.RunWith;
+
+public class EthiopicChronology_ESTestTest4 extends EthiopicChronology_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test03() throws Throwable {
+        EthiopicChronology ethiopicChronology0 = EthiopicChronology.getInstance();
+        long long0 = ethiopicChronology0.getApproxMillisAtEpochDividedByTwo();
+        assertEquals(30962844000000L, long0);
+    }
+}
