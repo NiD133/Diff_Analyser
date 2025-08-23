fe197b8489e947bdcd5ba505ab6c8f4a16c99ceb
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:24:37 +0800

    gemini processedManualTestCases abstractiteratortest4.java

--- abstractiteratortest4.java
+++ abstractiteratortest4.java
@@ -1,42 +1,34 @@
 package com.google.common.collect;
 
 import static com.google.common.collect.ReflectionFreeAssertThrows.assertThrows;
-import static com.google.common.collect.SneakyThrows.sneakyThrow;
-import com.google.common.annotations.GwtCompatible;
-import com.google.common.annotations.GwtIncompatible;
-import com.google.common.annotations.J2ktIncompatible;
-import com.google.common.collect.TestExceptions.SomeCheckedException;
-import com.google.common.collect.TestExceptions.SomeUncheckedException;
-import com.google.common.testing.GcFinalization;
-import java.lang.ref.WeakReference;
-import java.util.Iterator;
+
 import java.util.NoSuchElementException;
 import junit.framework.TestCase;
-import org.jspecify.annotations.NullMarked;
-import org.jspecify.annotations.Nullable;
 
 public class AbstractIteratorTestTest4 extends TestCase {
 
-    public void testDefaultBehaviorOfPeekForEmptyIteration() {
-        AbstractIterator<Integer> empty = new AbstractIterator<Integer>() {
-
-            private boolean alreadyCalledEndOfData;
-
-            @Override
-            @Nullable
-            public Integer computeNext() {
-                if (alreadyCalledEndOfData) {
-                    fail("Should not have been invoked again");
-                }
-                alreadyCalledEndOfData = true;
-                return endOfData();
-            }
+  /**
+   * Tests that calling peek() on an iterator that is immediately exhausted throws a {@link
+   * NoSuchElementException}.
+   */
+  public void testPeek_onEmptyIterator_throwsNoSuchElementException() {
+    // Arrange: Create an iterator that is immediately exhausted. Its computeNext() will be called
+    // once (the first time hasNext() or peek() is called) and will signal the end of data.
+    AbstractIterator<Integer> emptyIterator =
+        new AbstractIterator<Integer>() {
+          @Override
+          protected Integer computeNext() {
+            return endOfData();
+          }
         };
-        /*
-     * We test multiple calls to peek() to make sure that one bad call doesn't interfere with its
-     * ability to throw the correct exception in the future.
-     */
-        assertThrows(NoSuchElementException.class, empty::peek);
-        assertThrows(NoSuchElementException.class, empty::peek);
-    }
+
+    // Act & Assert:
+    // The first call to peek() should trigger computeNext(), find the end of data,
+    // and throw NoSuchElementException.
+    assertThrows(NoSuchElementException.class, emptyIterator::peek);
+
+    // A subsequent call to peek() should also throw. This ensures that the iterator
+    // remains in a terminal state and consistently reports that it's empty.
+    assertThrows(NoSuchElementException.class, emptyIterator::peek);
+  }
 }
