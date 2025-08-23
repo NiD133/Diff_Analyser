1285ebadcde379d7e11940aa3a01a2a2302ca197
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:16:21 +0800

    gemini processedAutoTestCases simpleobjectidresolver_estest1.java

--- simpleobjectidresolver_estest1.java
+++ simpleobjectidresolver_estest1.java
@@ -2,20 +2,39 @@ package com.fasterxml.jackson.annotation;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class SimpleObjectIdResolver_ESTestTest1 extends SimpleObjectIdResolver_ESTest_scaffolding {
+/**
+ * Tests for {@link SimpleObjectIdResolver}.
+ */
+public class SimpleObjectIdResolverTest {
 
-    @Test(timeout = 4000)
-    public void test0() throws Throwable {
-        SimpleObjectIdResolver simpleObjectIdResolver0 = new SimpleObjectIdResolver();
-        Class<Object> class0 = Object.class;
-        ObjectIdGenerator.IdKey objectIdGenerator_IdKey0 = new ObjectIdGenerator.IdKey(class0, class0, simpleObjectIdResolver0);
-        simpleObjectIdResolver0.bindItem(objectIdGenerator_IdKey0, class0);
-        Class class1 = (Class) simpleObjectIdResolver0.resolveId(objectIdGenerator_IdKey0);
-        assertFalse(class1.isEnum());
+    /**
+     * Verifies that an object bound to an ID can be successfully retrieved
+     * using that same ID.
+     */
+    @Test
+    public void shouldReturnBoundObject_whenResolvingId() {
+        // Arrange
+        SimpleObjectIdResolver resolver = new SimpleObjectIdResolver();
+        
+        // Create a unique key and an object to associate with it.
+        Object objectToBind = new Object();
+        ObjectIdGenerator.IdKey idKey = new ObjectIdGenerator.IdKey(
+                Object.class, // type
+                null,         // scope
+                "test-id-123" // key
+        );
+
+        // Act
+        // Bind the object to the ID key.
+        resolver.bindItem(idKey, objectToBind);
+        
+        // Attempt to resolve the ID key to get the object back.
+        Object resolvedObject = resolver.resolveId(idKey);
+
+        // Assert
+        // The resolved object should be the exact same instance that was bound.
+        assertSame("The resolved object should be the same instance that was originally bound.",
+                   objectToBind, resolvedObject);
     }
 }
