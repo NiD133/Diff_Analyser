bed83846040c9b75928a25f8da1e62c9e2280039
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:27:03 +0800

    gemini processedManualTestCases constructorinstantiatortest5.java

--- constructorinstantiatortest5.java
+++ constructorinstantiatortest5.java
@@ -1,36 +1,37 @@
 package org.mockito.internal.creation.instance;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.Assert.fail;
 import org.junit.Test;
+import org.mockito.creation.instance.Instantiator;
 import org.mockitoutil.TestBase;
 
-public class ConstructorInstantiatorTestTest5 extends TestBase {
+import static org.assertj.core.api.Assertions.assertThat;
 
-    static class SomeClass {
-    }
+/**
+ * Tests for {@link ConstructorInstantiator} focusing on its ability to handle
+ * constructors with primitive type arguments.
+ */
+public class ConstructorInstantiatorTest extends TestBase {
 
-    class SomeInnerClass {
-    }
-
-    class ChildOfThis extends ConstructorInstantiatorTest {
-    }
-
-    static class SomeClass2 {
-
-        SomeClass2(String x) {
-        }
-    }
-
-    static class SomeClass3 {
-
-        SomeClass3(int i) {
+    // A simple class with a constructor that accepts a primitive int.
+    static class ClassWithPrimitiveConstructor {
+        ClassWithPrimitiveConstructor(int value) {
+            // This constructor is the target for the test.
         }
     }
 
     @Test
-    public void creates_instances_with_primitive_arguments() {
-        assertThat(new ConstructorInstantiator(false, 123).newInstance(SomeClass3.class).getClass()).isEqualTo(SomeClass3.class);
+    public void shouldInstantiateClassWithPrimitiveArgument() {
+        // given
+        // An instantiator configured with an argument that can be unboxed to a primitive 'int'.
+        int constructorArg = 123;
+        Instantiator instantiator = new ConstructorInstantiator(false, constructorArg);
+
+        // when
+        // We request a new instance of a class that has a matching primitive constructor.
+        Object instance = instantiator.newInstance(ClassWithPrimitiveConstructor.class);
+
+        // then
+        // The instance is successfully created and is of the correct type.
+        assertThat(instance).isInstanceOf(ClassWithPrimitiveConstructor.class);
     }
 }
