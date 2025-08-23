d4ac55ca6c4f444ab7892b413b1d12008036161c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:46:31 +0800

    gemini processedAutoTestCases dataformatmatcher_estest5.java

--- dataformatmatcher_estest5.java
+++ dataformatmatcher_estest5.java
@@ -1,41 +1,38 @@
 package com.fasterxml.jackson.core.format;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
 import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.ObjectCodec;
+import org.junit.Test;
+
 import java.io.ByteArrayInputStream;
-import java.io.CharConversionException;
-import java.io.FileDescriptor;
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.PipedInputStream;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.evosuite.runtime.mock.java.io.MockFileInputStream;
-import org.junit.runner.RunWith;
 
-public class DataFormatMatcher_ESTestTest5 extends DataFormatMatcher_ESTest_scaffolding {
+/**
+ * Contains tests for the {@link DataFormatMatcher} constructor.
+ */
+public class DataFormatMatcher_ESTestTest5 { // NOTE: Renaming to DataFormatMatcherTest is recommended
 
-    @Test(timeout = 4000)
-    public void test04() throws Throwable {
-        byte[] byteArray0 = new byte[1];
-        ByteArrayInputStream byteArrayInputStream0 = new ByteArrayInputStream(byteArray0);
-        JsonFactory jsonFactory0 = new JsonFactory();
-        MatchStrength matchStrength0 = MatchStrength.INCONCLUSIVE;
-        DataFormatMatcher dataFormatMatcher0 = null;
-        try {
-            dataFormatMatcher0 = new DataFormatMatcher(byteArrayInputStream0, (byte[]) null, '\"', '\"', jsonFactory0, matchStrength0);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("com.fasterxml.jackson.core.format.DataFormatMatcher", e);
-        }
+    /**
+     * Verifies that the DataFormatMatcher constructor throws a NullPointerException
+     * when the provided byte buffer is null, as it needs to access the buffer's length.
+     */
+    @Test(expected = NullPointerException.class)
+    public void constructorShouldThrowNPEWhenBufferIsNull() {
+        // Arrange: Create dummy arguments for the constructor.
+        // The actual values are not important, as the test focuses on the null buffer.
+        InputStream dummyInputStream = new ByteArrayInputStream(new byte[0]);
+        JsonFactory dummyJsonFactory = new JsonFactory();
+        MatchStrength dummyMatchStrength = MatchStrength.INCONCLUSIVE;
+        int bufferStart = 0;
+        int bufferLength = 0;
+
+        // Act & Assert: Instantiating with a null buffer should throw a NullPointerException.
+        new DataFormatMatcher(
+                dummyInputStream,
+                null, // The null buffer that should cause the exception
+                bufferStart,
+                bufferLength,
+                dummyJsonFactory,
+                dummyMatchStrength
+        );
     }
 }
