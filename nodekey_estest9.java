1691d39f3f9e5484ecfb6732d7c05b19f2ba6a00
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:04:45 +0800

    gemini processedAutoTestCases nodekey_estest9.java

--- nodekey_estest9.java
+++ nodekey_estest9.java
@@ -2,21 +2,31 @@ package org.jfree.data.flow;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class NodeKey_ESTestTest9 extends NodeKey_ESTest_scaffolding {
+/**
+ * Unit tests for the NodeKey class.
+ */
+public class NodeKeyTest {
 
-    @Test(timeout = 4000)
-    public void test08() throws Throwable {
-        Integer integer0 = new Integer(3);
-        NodeKey<Integer> nodeKey0 = new NodeKey<Integer>(2281, integer0);
-        NodeKey<Integer> nodeKey1 = new NodeKey<Integer>(2725, integer0);
-        boolean boolean0 = nodeKey0.equals(nodeKey1);
-        assertEquals(2725, nodeKey1.getStage());
-        assertFalse(nodeKey1.equals((Object) nodeKey0));
-        assertFalse(boolean0);
+    /**
+     * Verifies that two NodeKey instances are not considered equal if they have
+     * the same node but different stage values. The equals() method should be
+     * symmetric.
+     */
+    @Test
+    public void equals_shouldReturnFalse_whenStagesDifferAndNodesAreSame() {
+        // Arrange: Create two NodeKey instances with the same node but different stages.
+        Integer commonNode = 100;
+        NodeKey<Integer> keyWithStage1 = new NodeKey<>(1, commonNode);
+        NodeKey<Integer> keyWithStage2 = new NodeKey<>(2, commonNode);
+
+        // Act & Assert: The keys should not be equal to each other.
+        // Test for equality
+        assertFalse("Keys with different stages should not be equal.",
+                keyWithStage1.equals(keyWithStage2));
+
+        // Test for symmetry of the equals method
+        assertFalse("Symmetry check: Keys with different stages should not be equal.",
+                keyWithStage2.equals(keyWithStage1));
     }
 }
