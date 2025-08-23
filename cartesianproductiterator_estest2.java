0ea3c0323fc449426c517f589f05a0528f0e006a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:41:34 +0800

    gemini processedAutoTestCases cartesianproductiterator_estest2.java

--- cartesianproductiterator_estest2.java
+++ cartesianproductiterator_estest2.java
@@ -1,41 +1,38 @@
 package org.apache.commons.collections4.iterators;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.sql.SQLNonTransientConnectionException;
+
+import java.util.ArrayList;
 import java.util.ConcurrentModificationException;
-import java.util.EnumSet;
 import java.util.List;
-import java.util.Locale;
-import java.util.NoSuchElementException;
-import java.util.PriorityQueue;
-import java.util.Stack;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class CartesianProductIterator_ESTestTest2 extends CartesianProductIterator_ESTest_scaffolding {
+/**
+ * Tests for {@link CartesianProductIterator} focusing on failure scenarios.
+ */
+public class CartesianProductIteratorTest {
 
-    @Test(timeout = 4000)
-    public void test1() throws Throwable {
-        Stack<Locale.Category> stack0 = new Stack<Locale.Category>();
-        Locale.Category locale_Category0 = Locale.Category.DISPLAY;
-        stack0.add(locale_Category0);
-        Iterable<Locale.Category>[] iterableArray0 = (Iterable<Locale.Category>[]) Array.newInstance(Iterable.class, 1);
-        iterableArray0[0] = (Iterable<Locale.Category>) stack0;
-        CartesianProductIterator<Locale.Category> cartesianProductIterator0 = new CartesianProductIterator<Locale.Category>(iterableArray0);
-        stack0.add(locale_Category0);
-        // Undeclared exception!
-        try {
-            cartesianProductIterator0.next();
-            fail("Expecting exception: ConcurrentModificationException");
-        } catch (ConcurrentModificationException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("java.util.Vector$Itr", e);
-        }
+    /**
+     * Tests that the iterator fails fast with a ConcurrentModificationException
+     * if an underlying collection is modified after the iterator is created.
+     * This is the expected behavior of iterators that follow the Java Collections Framework contract.
+     */
+    @Test(expected = ConcurrentModificationException.class)
+    public void testNextThrowsExceptionWhenUnderlyingCollectionIsModified() {
+        // Arrange: Create an iterator from a single list.
+        final List<String> sourceList = new ArrayList<>();
+        sourceList.add("A");
+
+        // The CartesianProductIterator takes an array of Iterables.
+        @SuppressWarnings("unchecked") // Safe as we are creating and populating the array ourselves.
+        final Iterable<String>[] iterables = new Iterable[]{sourceList};
+        final CartesianProductIterator<String> iterator = new CartesianProductIterator<>(iterables);
+
+        // Act: Modify the source collection after the iterator has been created.
+        // This invalidates the internal iterator state.
+        sourceList.add("B");
+
+        // Assert: Calling next() is now expected to throw a ConcurrentModificationException.
+        // The @Test(expected = ...) annotation handles the assertion.
+        iterator.next();
     }
 }
