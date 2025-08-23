b0b809da0eabcca9147948146dec7ab72ce81b5e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:52:27 +0800

    Original processedAutoTestCases gridarrangement_estest5.java

---
+++ gridarrangement_estest5.java
@@ -0,0 +1,56 @@
+package org.jfree.chart.block;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.awt.Graphics2D;
+import java.awt.SystemColor;
+import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.util.MockGregorianCalendar;
+import org.jfree.chart.api.HorizontalAlignment;
+import org.jfree.chart.api.RectangleAnchor;
+import org.jfree.chart.api.VerticalAlignment;
+import org.jfree.chart.text.TextBlockAnchor;
+import org.jfree.data.Range;
+import org.jfree.data.time.TimePeriodAnchor;
+import org.jfree.data.time.TimeSeries;
+import org.junit.runner.RunWith;
+
+public class GridArrangement_ESTestTest5 extends GridArrangement_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test04() throws Throwable {
+        GridArrangement gridArrangement0 = new GridArrangement((-873), (-873));
+        assertNotNull(gridArrangement0);
+        BlockContainer blockContainer0 = new BlockContainer(gridArrangement0);
+        assertEquals(0.0, blockContainer0.getContentXOffset(), 0.01);
+        assertEquals(0.0, blockContainer0.getContentYOffset(), 0.01);
+        assertNull(blockContainer0.getID());
+        assertEquals(0.0, blockContainer0.getHeight(), 0.01);
+        assertEquals(0.0, blockContainer0.getWidth(), 0.01);
+        assertTrue(blockContainer0.isEmpty());
+        assertNotNull(blockContainer0);
+        GridArrangement gridArrangement1 = new GridArrangement(212, (-297));
+        assertNotNull(gridArrangement1);
+        assertFalse(gridArrangement1.equals((Object) gridArrangement0));
+        RectangleConstraint rectangleConstraint0 = new RectangleConstraint(212, (Range) null);
+        assertEquals(LengthConstraintType.RANGE, rectangleConstraint0.getHeightConstraintType());
+        assertEquals(0.0, rectangleConstraint0.getHeight(), 0.01);
+        assertEquals(212.0, rectangleConstraint0.getWidth(), 0.01);
+        assertEquals(LengthConstraintType.FIXED, rectangleConstraint0.getWidthConstraintType());
+        assertNotNull(rectangleConstraint0);
+        // Undeclared exception!
+        try {
+            gridArrangement1.arrange(blockContainer0, (Graphics2D) null, rectangleConstraint0);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.jfree.chart.block.GridArrangement", e);
+        }
+    }
+}
