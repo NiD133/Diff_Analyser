90160ee40192b5bc3c2057dfa99ffdf288d3a697
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:27:47 +0800

    Original processedAutoTestCases xmlstreamwriter_estest19.java

---
+++ xmlstreamwriter_estest19.java
@@ -0,0 +1,41 @@
+package org.apache.commons.io.output;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.io.Writer;
+import java.nio.CharBuffer;
+import java.nio.charset.IllegalCharsetNameException;
+import java.nio.charset.UnsupportedCharsetException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.evosuite.runtime.mock.java.io.MockPrintStream;
+import org.junit.runner.RunWith;
+
+public class XmlStreamWriter_ESTestTest19 extends XmlStreamWriter_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test18() throws Throwable {
+        java.io.ByteArrayOutputStream byteArrayOutputStream0 = new java.io.ByteArrayOutputStream();
+        XmlStreamWriter xmlStreamWriter0 = new XmlStreamWriter(byteArrayOutputStream0);
+        Writer writer0 = xmlStreamWriter0.append((CharSequence) null);
+        char[] charArray0 = new char[1];
+        writer0.write(charArray0);
+        // Undeclared exception!
+        try {
+            xmlStreamWriter0.write((char[]) null, 2049, 2049);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+        }
+    }
+}
