f209c81d80187158b5a64011c1d9f72a5f1310bc
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:16:09 +0800

    Original processedAutoTestCases simplebloomfilter_estest27.java

---
+++ simplebloomfilter_estest27.java
@@ -0,0 +1,24 @@
+package org.apache.commons.collections4.bloomfilter;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.function.IntPredicate;
+import java.util.function.LongPredicate;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class SimpleBloomFilter_ESTestTest27 extends SimpleBloomFilter_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test26() throws Throwable {
+        Shape shape0 = Shape.fromNM(2147483602, 2147483602);
+        SimpleBloomFilter simpleBloomFilter0 = new SimpleBloomFilter(shape0);
+        SparseBloomFilter sparseBloomFilter0 = new SparseBloomFilter(shape0);
+        // Undeclared exception!
+        simpleBloomFilter0.merge((BitMapExtractor) sparseBloomFilter0);
+    }
+}
