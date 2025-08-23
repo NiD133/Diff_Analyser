d024499b4cb6eef91aa8446d1514ed81a39ccbf2
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:23:32 +0800

    gemini processedAutoTestCases transformedsortedmap_estest27.java

--- transformedsortedmap_estest27.java
+++ transformedsortedmap_estest27.java
@@ -1,49 +1,33 @@
 package org.apache.commons.collections4.map;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.NavigableMap;
-import java.util.NoSuchElementException;
+
 import java.util.SortedMap;
 import java.util.TreeMap;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.Factory;
-import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.ChainedTransformer;
-import org.apache.commons.collections4.functors.ConstantFactory;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.FactoryTransformer;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.MapTransformer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
 
-public class TransformedSortedMap_ESTestTest27 extends TransformedSortedMap_ESTest_scaffolding {
+/**
+ * Contains tests for the sub-map functionality of {@link TransformedSortedMap}.
+ */
+public class TransformedSortedMapTest {
 
-    @Test(timeout = 4000)
-    public void test26() throws Throwable {
-        TreeMap<Object, Object> treeMap0 = new TreeMap<Object, Object>();
-        TransformedSortedMap<Object, Object> transformedSortedMap0 = TransformedSortedMap.transformingSortedMap((SortedMap<Object, Object>) treeMap0, (Transformer<? super Object, ?>) null, (Transformer<? super Object, ?>) null);
-        // Undeclared exception!
-        try {
-            transformedSortedMap0.subMap((Object) null, treeMap0);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("java.util.TreeMap", e);
-        }
+    /**
+     * Tests that calling subMap() with a null 'fromKey' throws a NullPointerException.
+     * This behavior is expected because the method delegates to the underlying TreeMap,
+     * which does not permit null keys by default.
+     */
+    @Test(expected = NullPointerException.class)
+    public void subMapWithNullFromKeyShouldThrowNullPointerException() {
+        // Arrange
+        // Create a TransformedSortedMap backed by a standard TreeMap.
+        // No transformers are used, so it behaves like a regular SortedMap.
+        final SortedMap<String, String> backingMap = new TreeMap<>();
+        final SortedMap<String, String> transformedMap =
+            TransformedSortedMap.transformingSortedMap(backingMap, null, null);
+
+        // Act & Assert
+        // Attempt to create a sub-map with a null 'fromKey'.
+        // This should fail and throw a NullPointerException, which is asserted
+        // by the @Test(expected=...) annotation.
+        transformedMap.subMap(null, "anyKey");
     }
 }
