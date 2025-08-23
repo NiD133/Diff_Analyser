4e0521ad5bf1d931117311fa7b3b998a411358fc
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:24:33 +0800

    gemini processedAutoTestCases uncheck_estest29.java

--- uncheck_estest29.java
+++ uncheck_estest29.java
@@ -1,37 +1,22 @@
 package org.apache.commons.io.function;
 
+import static org.junit.Assert.assertThrows;
+
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.time.chrono.HijrahEra;
-import java.util.Comparator;
-import java.util.concurrent.ForkJoinTask;
-import java.util.function.BiConsumer;
-import java.util.function.BinaryOperator;
-import java.util.function.Function;
-import java.util.function.Supplier;
-import java.util.stream.Collector;
-import java.util.stream.LongStream;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
 
-public class Uncheck_ESTestTest29 extends Uncheck_ESTest_scaffolding {
+/**
+ * Tests for {@link Uncheck}.
+ */
+public class UncheckTest {
 
-    @Test(timeout = 4000)
-    public void test28() throws Throwable {
-        // Undeclared exception!
-        try {
-            Uncheck.getAsBoolean((IOBooleanSupplier) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.commons.io.function.Uncheck", e);
-        }
+    /**
+     * Tests that calling {@link Uncheck#getAsBoolean(IOBooleanSupplier)} with a null
+     * supplier throws a {@link NullPointerException}.
+     */
+    @Test
+    public void getAsBoolean_withNullSupplier_throwsNullPointerException() {
+        // The method under test is expected to throw a NullPointerException
+        // when its argument is null.
+        assertThrows(NullPointerException.class, () -> Uncheck.getAsBoolean(null));
     }
 }
