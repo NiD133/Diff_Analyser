982046f9213b91b3c6f2e3452ba8bf1009773d5b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:49:11 +0800

    Original processedAutoTestCases endpointpair_estest16.java

---
+++ endpointpair_estest16.java
@@ -0,0 +1,22 @@
+package com.google.common.graph;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.google.common.collect.UnmodifiableIterator;
+import java.util.Locale;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class EndpointPair_ESTestTest16 extends EndpointPair_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test15() throws Throwable {
+        Locale.Category locale_Category0 = Locale.Category.FORMAT;
+        EndpointPair<Object> endpointPair0 = EndpointPair.unordered((Object) locale_Category0, (Object) locale_Category0);
+        EndpointPair<Object> endpointPair1 = EndpointPair.unordered((Object) endpointPair0, (Object) locale_Category0);
+        boolean boolean0 = endpointPair0.equals(endpointPair1);
+        assertFalse(boolean0);
+    }
+}
