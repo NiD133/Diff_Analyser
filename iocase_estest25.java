3eb50e202888a4e0046d4c6b059b25e6f078a098
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:54:55 +0800

    Original processedAutoTestCases iocase_estest25.java

---
+++ iocase_estest25.java
@@ -0,0 +1,18 @@
+package org.apache.commons.io;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class IOCase_ESTestTest25 extends IOCase_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test24() throws Throwable {
+        IOCase iOCase0 = IOCase.SYSTEM;
+        boolean boolean0 = iOCase0.checkRegionMatches((String) null, 493, (String) null);
+        assertFalse(boolean0);
+    }
+}
