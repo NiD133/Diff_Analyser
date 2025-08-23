9f110c4e891c2da2603fd0f877b095b31183cda7
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:45:47 +0800

    gemini processedAutoTestCases converterset_estest29.java

--- converterset_estest29.java
+++ converterset_estest29.java
@@ -1,33 +1,24 @@
 package org.joda.time.convert;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.joda.time.Hours;
-import org.joda.time.Interval;
-import org.joda.time.MutablePeriod;
-import org.joda.time.PeriodType;
-import org.joda.time.Seconds;
-import org.joda.time.chrono.CopticChronology;
-import org.junit.runner.RunWith;
 
-public class ConverterSet_ESTestTest29 extends ConverterSet_ESTest_scaffolding {
+/**
+ * Test suite for the {@link ConverterSet#select(Class)} method.
+ */
+public class ConverterSetTest {
 
-    @Test(timeout = 4000)
-    public void test28() throws Throwable {
-        Converter[] converterArray0 = new Converter[1];
-        ConverterSet converterSet0 = new ConverterSet(converterArray0);
-        // Undeclared exception!
-        try {
-            converterSet0.select((Class<?>) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.joda.time.convert.ConverterSet", e);
-        }
+    /**
+     * Tests that calling select() with a null type argument throws a NullPointerException.
+     * The implementation is expected to perform a non-null check on its input.
+     */
+    @Test(expected = NullPointerException.class)
+    public void select_whenTypeIsNull_throwsNullPointerException() {
+        // Arrange: Create a ConverterSet. Its internal state is not relevant for this test.
+        // An empty set is sufficient to test the null-handling of the select method.
+        ConverterSet converterSet = new ConverterSet(new Converter[0]);
+
+        // Act: Call the select method with a null argument.
+        // The @Test(expected) annotation will assert that a NullPointerException is thrown.
+        converterSet.select(null);
     }
 }
