4d294450f79b2481a35eafb4a1a3d31ddd44f053
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:06:44 +0800

    gemini processedAutoTestCases orderedproperties_estest20.java

--- orderedproperties_estest20.java
+++ orderedproperties_estest20.java
@@ -1,59 +1,38 @@
 package org.apache.commons.collections4.properties;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.Reader;
-import java.io.StringReader;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.LinkedHashSet;
-import java.util.Map;
-import java.util.NoSuchElementException;
-import java.util.Set;
-import java.util.function.BiConsumer;
-import java.util.function.BiFunction;
 import java.util.function.Function;
-import org.apache.commons.collections4.Equator;
-import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AllPredicate;
-import org.apache.commons.collections4.functors.CloneTransformer;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.DefaultEquator;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.NOPTransformer;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NotNullPredicate;
-import org.apache.commons.collections4.functors.NullIsTruePredicate;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.SwitchTransformer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
 
-public class OrderedProperties_ESTestTest20 extends OrderedProperties_ESTest_scaffolding {
+/**
+ * Tests for {@link OrderedProperties} focusing on exception handling in compute methods.
+ */
+public class OrderedProperties_ESTestTest20 {
 
-    @Test(timeout = 4000)
-    public void test19() throws Throwable {
-        OrderedProperties orderedProperties0 = new OrderedProperties();
-        Integer integer0 = new Integer(95);
-        Predicate<Object>[] predicateArray0 = (Predicate<Object>[]) Array.newInstance(Predicate.class, 5);
-        Predicate<Object> predicate0 = NullPredicate.nullPredicate();
-        predicateArray0[0] = predicate0;
-        NonePredicate<Object> nonePredicate0 = new NonePredicate<Object>(predicateArray0);
-        predicateArray0[1] = (Predicate<Object>) nonePredicate0;
-        predicateArray0[4] = predicateArray0[1];
-        IfTransformer<Object, Integer> ifTransformer0 = new IfTransformer<Object, Integer>(predicateArray0[4], (Transformer<? super Object, ? extends Integer>) null, (Transformer<? super Object, ? extends Integer>) null);
-        // Undeclared exception!
-        orderedProperties0.computeIfAbsent(integer0, ifTransformer0);
+    /**
+     * Tests that {@link OrderedProperties#computeIfAbsent(Object, Function)} propagates a
+     * StackOverflowError thrown by the mapping function. This ensures that the method
+     * correctly handles severe runtime errors originating from user-provided logic without
+     * swallowing them or entering an inconsistent state.
+     */
+    @Test(expected = StackOverflowError.class)
+    public void computeIfAbsentShouldPropagateStackOverflowErrorFromMappingFunction() {
+        // Arrange
+        OrderedProperties orderedProperties = new OrderedProperties();
+        Object key = "anyKey";
+
+        // Create a mapping function that will cause a StackOverflowError by calling itself indefinitely.
+        // This simulates a faulty mapping function provided by a user.
+        Function<Object, Object> recursiveMappingFunction = new Function<Object, Object>() {
+            @Override
+            public Object apply(Object o) {
+                // This infinite recursion will trigger a StackOverflowError.
+                return this.apply(o);
+            }
+        };
+
+        // Act & Assert
+        // The call to computeIfAbsent should execute the faulty mapping function because the key
+        // is not present. The test expects the resulting StackOverflowError to be thrown.
+        orderedProperties.computeIfAbsent(key, recursiveMappingFunction);
     }
 }
