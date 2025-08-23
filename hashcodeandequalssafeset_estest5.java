8e03f1e150f72fd7a90ce58722caf3c78a1e827d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:53:10 +0800

    Original processedAutoTestCases hashcodeandequalssafeset_estest5.java

---
+++ hashcodeandequalssafeset_estest5.java
@@ -0,0 +1,30 @@
+package org.mockito.internal.util.collections;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Vector;
+import java.util.function.Predicate;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class HashCodeAndEqualsSafeSet_ESTestTest5 extends HashCodeAndEqualsSafeSet_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test04() throws Throwable {
+        HashCodeAndEqualsSafeSet hashCodeAndEqualsSafeSet0 = new HashCodeAndEqualsSafeSet();
+        Object object0 = new Object();
+        hashCodeAndEqualsSafeSet0.add(object0);
+        ArrayDeque<HashCodeAndEqualsMockWrapper> arrayDeque0 = new ArrayDeque<HashCodeAndEqualsMockWrapper>();
+        boolean boolean0 = hashCodeAndEqualsSafeSet0.retainAll(arrayDeque0);
+        assertTrue(boolean0);
+    }
+}
