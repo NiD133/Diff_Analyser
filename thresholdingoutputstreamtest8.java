a370cfd6da86dbc3b4a5ad8af53919e8dd74c187
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:34:53 +0800

    Original processedManualTestCases thresholdingoutputstreamtest8.java

---
+++ thresholdingoutputstreamtest8.java
@@ -0,0 +1,52 @@
+package org.apache.commons.io.output;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.junit.jupiter.api.Test;
+
+public class ThresholdingOutputStreamTestTest8 {
+
+    /**
+     * Asserts initial state without changing it.
+     *
+     * @param out the stream to test.
+     * @param expectedThreshold the expected threshold.
+     * @param expectedByeCount the expected byte count.
+     */
+    static void assertThresholdingInitialState(final ThresholdingOutputStream out, final int expectedThreshold, final int expectedByeCount) {
+        assertFalse(out.isThresholdExceeded());
+        assertEquals(expectedThreshold, out.getThreshold());
+        assertEquals(expectedByeCount, out.getByteCount());
+    }
+
+    /**
+     * Tests the case where the threshold is negative.
+     * The threshold is not reached until something is written to the stream.
+     */
+    @Test
+    void testThresholdLessThanZero() throws IOException {
+        final AtomicBoolean reached = new AtomicBoolean();
+        try (ThresholdingOutputStream out = new ThresholdingOutputStream(-1) {
+
+            @Override
+            protected void thresholdReached() throws IOException {
+                reached.set(true);
+            }
+        }) {
+            assertThresholdingInitialState(out, 0, 0);
+            assertFalse(reached.get());
+            out.write(89);
+            assertTrue(reached.get());
+            assertTrue(out.isThresholdExceeded());
+            assertInstanceOf(NullOutputStream.class, out.getOutputStream());
+            assertTrue(out.isThresholdExceeded());
+        }
+    }
+}
