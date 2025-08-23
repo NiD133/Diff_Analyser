e0d92255d142b565a622792837aa28a3e3bbea50
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:36:33 +0800

    gemini processedManualTestCases zippingiteratortest5.java

--- zippingiteratortest5.java
+++ zippingiteratortest5.java
@@ -3,17 +3,23 @@ package org.apache.commons.collections4.iterators;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.apache.commons.collections4.IteratorUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class ZippingIteratorTestTest5 extends AbstractIteratorTest<Integer> {
+/**
+ * Tests for ZippingIterator.
+ * This class name has been improved from ZippingIteratorTestTest5 for clarity.
+ */
+public class ZippingIteratorTest extends AbstractIteratorTest<Integer> {
 
     private ArrayList<Integer> evens;
-
     private ArrayList<Integer> odds;
-
     private ArrayList<Integer> fib;
 
     @Override
@@ -24,15 +30,18 @@ public class ZippingIteratorTestTest5 extends AbstractIteratorTest<Integer> {
 
     @Override
     public ZippingIterator<Integer> makeObject() {
+        // This iterator is used by the tests in the parent AbstractIteratorTest class.
         return new ZippingIterator<>(evens.iterator(), odds.iterator(), fib.iterator());
     }
 
     @BeforeEach
-    public void setUp() throws Exception {
+    public void setUp() {
         evens = new ArrayList<>();
         odds = new ArrayList<>();
+        // evens will be [0, 2, 4, ..., 18]
+        // odds will be  [1, 3, 5, ..., 19]
         for (int i = 0; i < 20; i++) {
-            if (0 == i % 2) {
+            if (i % 2 == 0) {
                 evens.add(i);
             } else {
                 odds.add(i);
@@ -49,19 +58,29 @@ public class ZippingIteratorTestTest5 extends AbstractIteratorTest<Integer> {
         fib.add(21);
     }
 
+    /**
+     * Tests that the iterator correctly interleaves elements from two iterators of the same length.
+     */
     @Test
-    void testIterateOddEven() {
-        final ZippingIterator<Integer> iter = new ZippingIterator<>(odds.iterator(), evens.iterator());
-        for (int i = 0, j = 0; i < 20; i++) {
-            assertTrue(iter.hasNext());
-            final int val = iter.next();
-            if (i % 2 == 0) {
-                assertEquals(odds.get(j).intValue(), val);
-            } else {
-                assertEquals(evens.get(j).intValue(), val);
-                j++;
-            }
+    void testZippingWithTwoIteratorsOfSameLength() {
+        // Arrange
+        // The ZippingIterator should alternate between the odds and evens iterators, starting with odds.
+        final ZippingIterator<Integer> zippingIterator = new ZippingIterator<>(odds.iterator(), evens.iterator());
+
+        // The expected sequence is an explicit interleaving of the 'odds' and 'evens' lists.
+        final List<Integer> expectedSequence = Arrays.asList(
+            1, 0, 3, 2, 5, 4, 7, 6, 9, 8,
+            11, 10, 13, 12, 15, 14, 17, 16, 19, 18
+        );
+
+        // Act & Assert
+        // Iterate through the expected sequence and verify each element from the zipping iterator.
+        for (final Integer expectedValue : expectedSequence) {
+            assertTrue(zippingIterator.hasNext(), "Iterator should have more elements");
+            assertEquals(expectedValue, zippingIterator.next(), "Iterator should return the next value in the zipped sequence");
         }
-        assertFalse(iter.hasNext());
+
+        // After consuming all elements, the iterator should be exhausted.
+        assertFalse(zippingIterator.hasNext(), "Iterator should be exhausted after the loop");
     }
 }
