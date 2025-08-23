2580a57eba6783d049f6a71947153b9513f2c8d8
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:46:43 +0800

    Original processedAutoTestCases dayofmonth_estest23.java

---
+++ dayofmonth_estest23.java
@@ -0,0 +1,45 @@
+package org.threeten.extra;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.time.Clock;
+import java.time.DateTimeException;
+import java.time.Duration;
+import java.time.Month;
+import java.time.YearMonth;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.chrono.Chronology;
+import java.time.chrono.HijrahDate;
+import java.time.chrono.MinguoDate;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.Temporal;
+import java.time.temporal.TemporalAccessor;
+import java.time.temporal.TemporalField;
+import java.time.temporal.TemporalQuery;
+import java.time.temporal.UnsupportedTemporalTypeException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.time.MockClock;
+import org.evosuite.runtime.mock.java.time.MockYearMonth;
+import org.evosuite.runtime.mock.java.time.MockZonedDateTime;
+import org.evosuite.runtime.mock.java.time.chrono.MockHijrahDate;
+import org.evosuite.runtime.mock.java.time.chrono.MockMinguoDate;
+import org.junit.runner.RunWith;
+
+public class DayOfMonth_ESTestTest23 extends DayOfMonth_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test22() throws Throwable {
+        DayOfMonth dayOfMonth0 = DayOfMonth.now();
+        ChronoField chronoField0 = ChronoField.AMPM_OF_DAY;
+        boolean boolean0 = dayOfMonth0.isSupported(chronoField0);
+        assertEquals(14, dayOfMonth0.getValue());
+        assertFalse(boolean0);
+    }
+}
