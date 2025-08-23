a518baea59881151d2dd82e548e7da2f088f0ded
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:14:42 +0800

    Original processedAutoTestCases serializationutils_estest15.java

---
+++ serializationutils_estest15.java
@@ -0,0 +1,39 @@
+package org.apache.commons.lang3;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.io.SequenceInputStream;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Locale;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.evosuite.runtime.mock.java.io.MockFileInputStream;
+import org.evosuite.runtime.mock.java.io.MockPrintStream;
+import org.junit.runner.RunWith;
+
+public class SerializationUtils_ESTestTest15 extends SerializationUtils_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test14() throws Throwable {
+        Integer integer0 = new Integer((-106));
+        // Undeclared exception!
+        try {
+            SerializationUtils.serialize((Serializable) integer0, (OutputStream) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // outputStream
+            //
+            verifyException("java.util.Objects", e);
+        }
+    }
+}
