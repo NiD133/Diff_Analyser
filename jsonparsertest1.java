969d9965fa7a1171da3431e7568a96c788204614
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:30:06 +0800

    Original processedManualTestCases jsonparsertest1.java

---
+++ jsonparsertest1.java
@@ -0,0 +1,20 @@
+package com.google.gson;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertThrows;
+import com.google.gson.common.TestTypes.BagOfPrimitives;
+import com.google.gson.internal.Streams;
+import com.google.gson.stream.JsonReader;
+import java.io.CharArrayReader;
+import java.io.CharArrayWriter;
+import java.io.IOException;
+import java.io.StringReader;
+import org.junit.Test;
+
+public class JsonParserTestTest1 {
+
+    @Test
+    public void testParseInvalidJson() {
+        assertThrows(JsonSyntaxException.class, () -> JsonParser.parseString("[[]"));
+    }
+}
