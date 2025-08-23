698183311c0ed373d47180bdea396481f550c391
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:37:22 +0800

    gemini processedAutoTestCases ararchiveoutputstream_estest31.java

--- ararchiveoutputstream_estest31.java
+++ ararchiveoutputstream_estest31.java
@@ -1,32 +1,32 @@
 package org.apache.commons.compress.archivers.ar;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
+
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.nio.file.LinkOption;
-import java.nio.file.NoSuchFileException;
-import java.nio.file.Path;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.System;
-import org.evosuite.runtime.mock.java.io.MockFile;
-import org.evosuite.runtime.mock.java.io.MockFileOutputStream;
-import org.evosuite.runtime.mock.java.io.MockPrintStream;
-import org.evosuite.runtime.testdata.FileSystemHandling;
-import org.junit.runner.RunWith;
 
-public class ArArchiveOutputStream_ESTestTest31 extends ArArchiveOutputStream_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link ArArchiveOutputStream} class.
+ */
+public class ArArchiveOutputStreamTest {
 
-    @Test(timeout = 4000)
-    public void test30() throws Throwable {
-        ByteArrayOutputStream byteArrayOutputStream0 = new ByteArrayOutputStream();
-        ArArchiveOutputStream arArchiveOutputStream0 = new ArArchiveOutputStream(byteArrayOutputStream0);
-        arArchiveOutputStream0.close();
-        arArchiveOutputStream0.close();
-        assertEquals(1, ArArchiveOutputStream.LONGFILE_BSD);
+    /**
+     * Verifies that calling close() on an already closed ArArchiveOutputStream
+     * is an idempotent operation and does not throw an exception.
+     *
+     * @throws IOException if an I/O error occurs during the first close.
+     */
+    @Test
+    public void closeCanBeCalledMultipleTimesWithoutError() throws IOException {
+        // Arrange: Create an ArArchiveOutputStream and close it.
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        ArArchiveOutputStream arOutputStream = new ArArchiveOutputStream(outputStream);
+        arOutputStream.close();
+
+        // Act: Call close() again on the already closed stream.
+        arOutputStream.close();
+
+        // Assert: The test succeeds if no exception is thrown during the second call to close().
+        // This confirms the idempotency of the close() method.
     }
 }
