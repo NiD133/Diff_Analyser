2594a3b1bc5914869769de892c6a46a72273cf3e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:50:31 +0800

    gemini processedAutoTestCases fieldutils_estest61.java

--- fieldutils_estest61.java
+++ fieldutils_estest61.java
@@ -1,23 +1,29 @@
 package org.joda.time.field;
 
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.math.RoundingMode;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.joda.time.DateTimeField;
-import org.joda.time.DateTimeFieldType;
-import org.joda.time.DateTimeZone;
-import org.joda.time.chrono.IslamicChronology;
-import org.joda.time.chrono.ZonedChronology;
-import org.junit.runner.RunWith;
 
-public class FieldUtils_ESTestTest61 extends FieldUtils_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link FieldUtils} class.
+ */
+public class FieldUtilsTest {
 
-    @Test(timeout = 4000)
-    public void test60() throws Throwable {
-        long long0 = FieldUtils.safeDivide((-9223372036854775765L), (-2123L));
-        assertEquals(4344499310812423L, long0);
+    /**
+     * Tests that safeDivide correctly calculates the quotient of two negative long values.
+     * This serves as a standard case for division involving large negative numbers.
+     */
+    @Test
+    public void safeDivide_withTwoNegativeLongs_returnsCorrectQuotient() {
+        // Arrange
+        // A dividend close to Long.MIN_VALUE to test with large numbers.
+        final long dividend = -9_223_372_036_854_775_765L;
+        final long divisor = -2_123L;
+        final long expectedQuotient = 4_344_499_310_812_423L;
+
+        // Act
+        final long actualQuotient = FieldUtils.safeDivide(dividend, divisor);
+
+        // Assert
+        assertEquals(expectedQuotient, actualQuotient);
     }
 }
