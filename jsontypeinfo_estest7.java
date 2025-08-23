ee73258cd7bdbdd7c9b0e4b2c0eef95d01a2800a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:58:59 +0800

    Original processedAutoTestCases jsontypeinfo_estest7.java

---
+++ jsontypeinfo_estest7.java
@@ -0,0 +1,20 @@
+package com.fasterxml.jackson.annotation;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class JsonTypeInfo_ESTestTest7 extends JsonTypeInfo_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test06() throws Throwable {
+        JsonTypeInfo.Value jsonTypeInfo_Value0 = JsonTypeInfo.Value.EMPTY;
+        boolean boolean0 = jsonTypeInfo_Value0.equals("EXTERNAL_PROPERTY");
+        assertFalse(boolean0);
+    }
+}
