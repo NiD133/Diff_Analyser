327bfc210ac5bc0e1cf9d74496f6c0cfb17ef86b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:59:04 +0800

    Original processedAutoTestCases jsonwritecontext_estest2.java

---
+++ jsonwritecontext_estest2.java
@@ -0,0 +1,31 @@
+package com.fasterxml.jackson.core.json;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.filter.FilteringGeneratorDelegate;
+import com.fasterxml.jackson.core.filter.TokenFilter;
+import com.fasterxml.jackson.core.util.JsonGeneratorDelegate;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.PipedInputStream;
+import java.io.StringWriter;
+import java.io.Writer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class JsonWriteContext_ESTestTest2 extends JsonWriteContext_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test01() throws Throwable {
+        JsonWriteContext jsonWriteContext0 = JsonWriteContext.createRootContext();
+        jsonWriteContext0.reset(50000, (Object) null);
+        int int0 = jsonWriteContext0.writeValue();
+        assertEquals(1, jsonWriteContext0.getEntryCount());
+        assertEquals(0, int0);
+    }
+}
