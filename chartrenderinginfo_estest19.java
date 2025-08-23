929b2a3aee77e6f658eeca790cf9ae32344dbe2c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:43:12 +0800

    gemini processedAutoTestCases chartrenderinginfo_estest19.java

--- chartrenderinginfo_estest19.java
+++ chartrenderinginfo_estest19.java
@@ -1,34 +1,35 @@
 package org.jfree.chart;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.awt.Rectangle;
-import java.awt.geom.Rectangle2D;
-import java.util.Locale;
-import java.util.SimpleTimeZone;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.jfree.chart.axis.DateAxis;
-import org.jfree.chart.entity.EntityCollection;
-import org.jfree.chart.entity.StandardEntityCollection;
-import org.jfree.chart.plot.CombinedRangeCategoryPlot;
 import org.jfree.chart.plot.PlotRenderingInfo;
-import org.jfree.data.xy.XYDatasetTableModel;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class ChartRenderingInfo_ESTestTest19 extends ChartRenderingInfo_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
-    @Test(timeout = 4000)
-    public void test18() throws Throwable {
-        StandardEntityCollection standardEntityCollection0 = new StandardEntityCollection();
-        ChartRenderingInfo chartRenderingInfo0 = new ChartRenderingInfo(standardEntityCollection0);
-        PlotRenderingInfo plotRenderingInfo0 = chartRenderingInfo0.getPlotInfo();
-        ChartRenderingInfo chartRenderingInfo1 = new ChartRenderingInfo();
-        assertTrue(chartRenderingInfo1.equals((Object) chartRenderingInfo0));
-        plotRenderingInfo0.addSubplotInfo(plotRenderingInfo0);
-        boolean boolean0 = chartRenderingInfo0.equals(chartRenderingInfo1);
-        assertFalse(chartRenderingInfo1.equals((Object) chartRenderingInfo0));
-        assertFalse(boolean0);
+/**
+ * A test suite for the equals() method of the {@link ChartRenderingInfo} class.
+ */
+public class ChartRenderingInfoEqualsTest {
+
+    /**
+     * Verifies that the equals() method returns false when the PlotRenderingInfo
+     * of one object is modified, making it different from the other.
+     */
+    @Test
+    public void equals_shouldReturnFalse_whenPlotInfoIsModified() {
+        // Arrange: Create two identical ChartRenderingInfo objects.
+        ChartRenderingInfo info1 = new ChartRenderingInfo();
+        ChartRenderingInfo info2 = new ChartRenderingInfo();
+
+        // Sanity check: The two objects should be equal initially.
+        assertEquals("Initially, the two ChartRenderingInfo objects should be equal.", info1, info2);
+
+        // Act: Modify the plot information of the first object by adding a subplot.
+        // This change should cause the parent ChartRenderingInfo objects to become unequal.
+        PlotRenderingInfo plotInfoForInfo1 = info1.getPlotInfo();
+        plotInfoForInfo1.addSubplotInfo(new PlotRenderingInfo(info1));
+
+        // Assert: The modified object should no longer be equal to the original.
+        assertNotEquals("After modification, the objects should no longer be equal.", info1, info2);
     }
 }
