a77bdbc0cdda8b4f52f620990c49995ca46f43e5
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:39:31 +0800

    gemini processedAutoTestCases blockingcache_estest23.java

--- blockingcache_estest23.java
+++ blockingcache_estest23.java
@@ -1,23 +1,30 @@
 package org.apache.ibatis.cache.decorators;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.concurrent.CountDownLatch;
 import org.apache.ibatis.cache.Cache;
 import org.apache.ibatis.cache.impl.PerpetualCache;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class BlockingCache_ESTestTest23 extends BlockingCache_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
 
-    @Test(timeout = 4000)
-    public void test22() throws Throwable {
-        PerpetualCache perpetualCache0 = new PerpetualCache((String) null);
-        SoftCache softCache0 = new SoftCache(perpetualCache0);
-        BlockingCache blockingCache0 = new BlockingCache(softCache0);
-        blockingCache0.clear();
-        assertEquals(0L, blockingCache0.getTimeout());
+/**
+ * Tests for the {@link BlockingCache} decorator.
+ */
+public class BlockingCacheTest {
+
+    /**
+     * Verifies that a newly created BlockingCache has a default timeout of 0.
+     */
+    @Test
+    public void shouldHaveZeroTimeoutByDefault() {
+        // Arrange
+        // A BlockingCache requires a delegate cache. A simple PerpetualCache is used here.
+        Cache delegateCache = new PerpetualCache("default-cache");
+        BlockingCache blockingCache = new BlockingCache(delegateCache);
+
+        // Act
+        long timeout = blockingCache.getTimeout();
+
+        // Assert
+        assertEquals("A new BlockingCache should have a default timeout of 0", 0L, timeout);
     }
 }
