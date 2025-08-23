b58fd9b26b200e101ee02f557b672e7fcfd4bb0f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:08:35 +0800

    Original processedAutoTestCases periodformatter_estest37.java

---
+++ periodformatter_estest37.java
@@ -0,0 +1,44 @@
+package org.joda.time.format;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.IOException;
+import java.io.PipedWriter;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.LinkedList;
+import java.util.Locale;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.joda.time.Duration;
+import org.joda.time.Hours;
+import org.joda.time.Minutes;
+import org.joda.time.MutablePeriod;
+import org.joda.time.Period;
+import org.joda.time.PeriodType;
+import org.joda.time.ReadWritablePeriod;
+import org.joda.time.ReadablePeriod;
+import org.joda.time.Seconds;
+import org.joda.time.Weeks;
+import org.joda.time.Years;
+import org.junit.runner.RunWith;
+
+public class PeriodFormatter_ESTestTest37 extends PeriodFormatter_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test36() throws Throwable {
+        PeriodFormatter periodFormatter0 = new PeriodFormatter((PeriodPrinter) null, (PeriodParser) null);
+        Seconds seconds0 = Seconds.ZERO;
+        // Undeclared exception!
+        try {
+            periodFormatter0.print(seconds0);
+            fail("Expecting exception: UnsupportedOperationException");
+        } catch (UnsupportedOperationException e) {
+            //
+            // Printing not supported
+            //
+            verifyException("org.joda.time.format.PeriodFormatter", e);
+        }
+    }
+}
