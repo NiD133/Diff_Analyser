e76ed4de2fa1f6fcc0f415a1c87acb7563e525ff
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:59:54 +0800

    Original processedAutoTestCases lazilyparsednumber_estest24.java

---
+++ lazilyparsednumber_estest24.java
@@ -0,0 +1,26 @@
+package com.google.gson.internal;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class LazilyParsedNumber_ESTestTest24 extends LazilyParsedNumber_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test23() throws Throwable {
+        LazilyParsedNumber lazilyParsedNumber0 = new LazilyParsedNumber((String) null);
+        // Undeclared exception!
+        try {
+            lazilyParsedNumber0.longValue();
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("com.google.gson.internal.NumberLimits", e);
+        }
+    }
+}
