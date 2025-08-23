2834e4e43050329f664a617b891f0160a9204f37
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:02:35 +0800

    gemini processedAutoTestCases metaphone_estest43.java

--- metaphone_estest43.java
+++ metaphone_estest43.java
@@ -1,19 +1,45 @@
 package org.apache.commons.codec.language;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class Metaphone_ESTestTest43 extends Metaphone_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link Metaphone} class.
+ */
+public class MetaphoneTest {
 
-    @Test(timeout = 4000)
-    public void test42() throws Throwable {
-        Metaphone metaphone0 = new Metaphone();
-        String string0 = metaphone0.metaphone("AEIOU");
-        assertEquals("E", string0);
-        assertEquals(4, metaphone0.getMaxCodeLen());
+    private final Metaphone metaphone = new Metaphone();
+
+    /**
+     * Tests the Metaphone algorithm's special rule for words starting with "AE",
+     * which should be encoded as "E".
+     */
+    @Test
+    public void shouldEncodeWordStartingWithAEAsE() {
+        // Arrange
+        String input = "AEIOU";
+        String expectedCode = "E";
+
+        // Act
+        String actualCode = metaphone.metaphone(input);
+
+        // Assert
+        assertEquals("The word 'AEIOU' should be encoded as 'E'", expectedCode, actualCode);
+    }
+
+    /**
+     * Tests that a new Metaphone instance is created with the correct default
+     * maximum code length.
+     */
+    @Test
+    public void shouldHaveDefaultMaxCodeLenOfFour() {
+        // Arrange
+        int expectedMaxCodeLen = 4;
+
+        // Act
+        int actualMaxCodeLen = metaphone.getMaxCodeLen();
+
+        // Assert
+        assertEquals("Default max code length should be 4", expectedMaxCodeLen, actualMaxCodeLen);
     }
 }
