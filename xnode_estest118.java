aea3aadcaf1c20c011954f187f144657bc72fa30
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:28:40 +0800

    gemini processedAutoTestCases xnode_estest118.java

--- xnode_estest118.java
+++ xnode_estest118.java
@@ -1,30 +1,34 @@
 package org.apache.ibatis.parsing;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.List;
-import java.util.Locale;
+import static org.junit.Assert.assertSame;
+
 import java.util.Properties;
-import java.util.function.Supplier;
 import javax.imageio.metadata.IIOMetadataNode;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
-import org.w3c.dom.Document;
+import org.junit.Test;
 import org.w3c.dom.Node;
-import org.xml.sax.ext.DefaultHandler2;
 
-public class XNode_ESTestTest118 extends XNode_ESTest_scaffolding {
+/**
+ * Test suite for the {@link XNode} class.
+ */
+public class XNodeTest {
 
-    @Test(timeout = 4000)
-    public void test117() throws Throwable {
-        IIOMetadataNode iIOMetadataNode0 = new IIOMetadataNode();
-        Properties properties0 = new Properties();
-        XNode xNode0 = new XNode((XPathParser) null, iIOMetadataNode0, properties0);
-        Node node0 = xNode0.getNode();
-        assertSame(node0, iIOMetadataNode0);
+    /**
+     * Verifies that the getNode() method returns the exact same Node instance
+     * that was passed to the XNode constructor.
+     */
+    @Test
+    public void getNodeShouldReturnTheNodePassedToConstructor() {
+        // Arrange
+        Node originalNode = new IIOMetadataNode(); // A concrete implementation of Node for testing
+        Properties variables = new Properties();
+        // The XPathParser is not used by the constructor or getNode(), so it can be null for this test.
+        XNode xNode = new XNode(null, originalNode, variables);
+
+        // Act
+        Node retrievedNode = xNode.getNode();
+
+        // Assert
+        assertSame("The retrieved node should be the same instance as the one used for construction.",
+                originalNode, retrievedNode);
     }
 }
