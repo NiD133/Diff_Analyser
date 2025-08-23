c95a12a713be1688eba800bf3c534b44ca5e9206
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:46:47 +0800

    Original processedAutoTestCases dayofmonth_estest35.java

---
+++ dayofmonth_estest35.java
@@ -0,0 +1,52 @@
+package org.threeten.extra;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.time.Clock;
+import java.time.DateTimeException;
+import java.time.Duration;
+import java.time.Month;
+import java.time.YearMonth;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.chrono.Chronology;
+import java.time.chrono.HijrahDate;
+import java.time.chrono.MinguoDate;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.Temporal;
+import java.time.temporal.TemporalAccessor;
+import java.time.temporal.TemporalField;
+import java.time.temporal.TemporalQuery;
+import java.time.temporal.UnsupportedTemporalTypeException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.time.MockClock;
+import org.evosuite.runtime.mock.java.time.MockYearMonth;
+import org.evosuite.runtime.mock.java.time.MockZonedDateTime;
+import org.evosuite.runtime.mock.java.time.chrono.MockHijrahDate;
+import org.evosuite.runtime.mock.java.time.chrono.MockMinguoDate;
+import org.junit.runner.RunWith;
+
+public class DayOfMonth_ESTestTest35 extends DayOfMonth_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test34() throws Throwable {
+        DayOfMonth dayOfMonth0 = DayOfMonth.now();
+        ChronoField chronoField0 = ChronoField.INSTANT_SECONDS;
+        // Undeclared exception!
+        try {
+            dayOfMonth0.getLong(chronoField0);
+            fail("Expecting exception: UnsupportedTemporalTypeException");
+        } catch (UnsupportedTemporalTypeException e) {
+            //
+            // Unsupported field: InstantSeconds
+            //
+            verifyException("org.threeten.extra.DayOfMonth", e);
+        }
+    }
+}
