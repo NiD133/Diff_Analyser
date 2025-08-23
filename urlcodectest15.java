d01313dee22954c796f691e7aeb9ea12c20b908a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:35:22 +0800

    Original processedManualTestCases urlcodectest15.java

---
+++ urlcodectest15.java
@@ -0,0 +1,41 @@
+package org.apache.commons.codec.net;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.nio.charset.StandardCharsets;
+import org.apache.commons.codec.CharEncoding;
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.EncoderException;
+import org.junit.jupiter.api.Test;
+
+public class URLCodecTestTest15 {
+
+    static final int[] SWISS_GERMAN_STUFF_UNICODE = { 0x47, 0x72, 0xFC, 0x65, 0x7A, 0x69, 0x5F, 0x7A, 0xE4, 0x6D, 0xE4 };
+
+    static final int[] RUSSIAN_STUFF_UNICODE = { 0x412, 0x441, 0x435, 0x43C, 0x5F, 0x43F, 0x440, 0x438, 0x432, 0x435, 0x442 };
+
+    private String constructString(final int[] unicodeChars) {
+        final StringBuilder buffer = new StringBuilder();
+        if (unicodeChars != null) {
+            for (final int unicodeChar : unicodeChars) {
+                buffer.append((char) unicodeChar);
+            }
+        }
+        return buffer.toString();
+    }
+
+    private void validateState(final URLCodec urlCodec) {
+        // no tests for now.
+    }
+
+    @Test
+    void testUnsafeEncodeDecode() throws Exception {
+        final URLCodec urlCodec = new URLCodec();
+        final String plain = "~!@#$%^&()+{}\"\\;:`,/[]";
+        final String encoded = urlCodec.encode(plain);
+        assertEquals("%7E%21%40%23%24%25%5E%26%28%29%2B%7B%7D%22%5C%3B%3A%60%2C%2F%5B%5D", encoded, "Unsafe chars URL encoding test");
+        assertEquals(plain, urlCodec.decode(encoded), "Unsafe chars URL decoding test");
+        validateState(urlCodec);
+    }
+}
