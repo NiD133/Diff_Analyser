cb7e705d6c68ad127b8e3ff408fe98a6893f4120
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:51:07 +0800

    gemini processedAutoTestCases filterlistiterator_estest3.java

--- filterlistiterator_estest3.java
+++ filterlistiterator_estest3.java
@@ -1,74 +1,49 @@
 package org.apache.commons.collections4.iterators;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.ConcurrentModificationException;
-import java.util.LinkedList;
-import java.util.ListIterator;
-import java.util.Map;
-import java.util.NoSuchElementException;
-import java.util.function.Consumer;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.Equator;
 import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ClosureTransformer;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.DefaultEquator;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionClosure;
 import org.apache.commons.collections4.functors.ExceptionPredicate;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.IfClosure;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.MapTransformer;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NotNullPredicate;
-import org.apache.commons.collections4.functors.NotPredicate;
-import org.apache.commons.collections4.functors.NullIsExceptionPredicate;
-import org.apache.commons.collections4.functors.NullIsFalsePredicate;
-import org.apache.commons.collections4.functors.NullIsTruePredicate;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.OnePredicate;
-import org.apache.commons.collections4.functors.TransformedPredicate;
-import org.apache.commons.collections4.functors.TransformerPredicate;
-import org.apache.commons.collections4.functors.TruePredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.apache.commons.collections4.functors.WhileClosure;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class FilterListIterator_ESTestTest3 extends FilterListIterator_ESTest_scaffolding {
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
 
-    @Test(timeout = 4000)
-    public void test02() throws Throwable {
-        FilterListIterator<Boolean> filterListIterator0 = new FilterListIterator<Boolean>();
-        LinkedList<Integer> linkedList0 = new LinkedList<Integer>();
-        linkedList0.add((Integer) null);
-        ListIterator<Integer> listIterator0 = linkedList0.listIterator(1);
-        Predicate<Object> predicate0 = ExceptionPredicate.exceptionPredicate();
-        FilterListIterator<Integer> filterListIterator1 = new FilterListIterator<Integer>(listIterator0, predicate0);
-        filterListIterator1.setListIterator(listIterator0);
-        IdentityPredicate<Integer> identityPredicate0 = new IdentityPredicate<Integer>((Integer) null);
-        FilterListIterator<Integer> filterListIterator2 = new FilterListIterator<Integer>(filterListIterator1, predicate0);
-        // Undeclared exception!
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+/**
+ * Contains tests for the {@link FilterListIterator} class, focusing on exception handling.
+ */
+public class FilterListIteratorTest {
+
+    /**
+     * Tests that hasPrevious() propagates the RuntimeException thrown by the predicate.
+     *
+     * The hasPrevious() method needs to evaluate the previous element to see if it
+     * matches the predicate. This test ensures that if the predicate throws an
+     * exception during this evaluation, the exception is not caught but is instead
+     * passed up to the caller.
+     */
+    @Test
+    public void testHasPreviousPropagatesExceptionFromPredicate() {
+        // Arrange: Create a list with one element and an iterator positioned at the end.
+        // This ensures that the underlying iterator's hasPrevious() returns true.
+        final List<String> sourceList = new LinkedList<>();
+        sourceList.add("A");
+        final ListIterator<String> baseIterator = sourceList.listIterator(1);
+
+        // Arrange: Use a predicate that is guaranteed to throw an exception.
+        final Predicate<String> exceptionPredicate = ExceptionPredicate.exceptionPredicate();
+        final FilterListIterator<String> filteredIterator = new FilterListIterator<>(baseIterator, exceptionPredicate);
+
+        // Act & Assert
         try {
-            filterListIterator2.hasPrevious();
-            fail("Expecting exception: RuntimeException");
-        } catch (RuntimeException e) {
-            //
-            // ExceptionPredicate invoked
-            //
-            verifyException("org.apache.commons.collections4.functors.ExceptionPredicate", e);
+            filteredIterator.hasPrevious();
+            fail("A RuntimeException should have been thrown by the predicate.");
+        } catch (final RuntimeException e) {
+            // Assert: Verify that the correct exception was propagated.
+            final String expectedMessage = "ExceptionPredicate invoked";
+            assertEquals(expectedMessage, e.getMessage());
         }
     }
 }
