aafe6e8e76f5cffa51e192e5f98b3ad4665a39ce
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:22:31 +0800

    gemini processedAutoTestCases tokenqueue_estest40.java

--- tokenqueue_estest40.java
+++ tokenqueue_estest40.java
@@ -1,26 +1,26 @@
 package org.jsoup.parser;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class TokenQueue_ESTestTest40 extends TokenQueue_ESTest_scaffolding {
+/**
+ * Test suite for the TokenQueue class.
+ */
+public class TokenQueueTest {
 
-    @Test(timeout = 4000)
-    public void test39() throws Throwable {
-        TokenQueue tokenQueue0 = new TokenQueue("Om`o!");
-        tokenQueue0.close();
-        // Undeclared exception!
-        try {
-            tokenQueue0.consume();
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-        }
+    /**
+     * Verifies that attempting to consume from a queue after it has been closed
+     * results in a NullPointerException. This is expected because closing the queue
+     * releases its internal resources.
+     */
+    @Test(expected = NullPointerException.class)
+    public void consumeAfterCloseThrowsNullPointerException() {
+        // Arrange: Create a queue and then close it.
+        TokenQueue queue = new TokenQueue("Some data");
+        queue.close();
+
+        // Act: Attempt to consume from the closed queue.
+        queue.consume();
+
+        // Assert: A NullPointerException is expected, as declared in the @Test annotation.
     }
 }
