06fafea80c75596b4fb9df657d4dd24f839deec3
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:02:26 +0800

    Original processedAutoTestCases metaphone_estest21.java

---
+++ metaphone_estest21.java
@@ -0,0 +1,18 @@
+package org.apache.commons.codec.language;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class Metaphone_ESTestTest21 extends Metaphone_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test20() throws Throwable {
+        Metaphone metaphone0 = new Metaphone();
+        metaphone0.metaphone((String) null);
+        assertEquals(4, metaphone0.getMaxCodeLen());
+    }
+}
