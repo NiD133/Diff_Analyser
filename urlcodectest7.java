4cea34f78bb57c7ef6bc0c8f1111e9787cf989dc
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:35:24 +0800

    Original processedManualTestCases urlcodectest7.java

---
+++ urlcodectest7.java
@@ -0,0 +1,43 @@
+package org.apache.commons.codec.net;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.nio.charset.StandardCharsets;
+import org.apache.commons.codec.CharEncoding;
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.EncoderException;
+import org.junit.jupiter.api.Test;
+
+public class URLCodecTestTest7 {
+
+    static final int[] SWISS_GERMAN_STUFF_UNICODE = { 0x47, 0x72, 0xFC, 0x65, 0x7A, 0x69, 0x5F, 0x7A, 0xE4, 0x6D, 0xE4 };
+
+    static final int[] RUSSIAN_STUFF_UNICODE = { 0x412, 0x441, 0x435, 0x43C, 0x5F, 0x43F, 0x440, 0x438, 0x432, 0x435, 0x442 };
+
+    private String constructString(final int[] unicodeChars) {
+        final StringBuilder buffer = new StringBuilder();
+        if (unicodeChars != null) {
+            for (final int unicodeChar : unicodeChars) {
+                buffer.append((char) unicodeChar);
+            }
+        }
+        return buffer.toString();
+    }
+
+    private void validateState(final URLCodec urlCodec) {
+        // no tests for now.
+    }
+
+    @Test
+    void testDefaultEncoding() throws Exception {
+        final String plain = "Hello there!";
+        final URLCodec urlCodec = new URLCodec("UnicodeBig");
+        // To work around a weird quirk in Java 1.2.2
+        urlCodec.encode(plain);
+        final String encoded1 = urlCodec.encode(plain, "UnicodeBig");
+        final String encoded2 = urlCodec.encode(plain);
+        assertEquals(encoded1, encoded2);
+        validateState(urlCodec);
+    }
+}
