c92b06c8902d34153499d079ed84751b2c5ec60e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:18:33 +0800

    gemini processedAutoTestCases stringconverter_estest40.java

--- stringconverter_estest40.java
+++ stringconverter_estest40.java
@@ -1,54 +1,32 @@
 package org.joda.time.convert;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.joda.time.Chronology;
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeFieldType;
-import org.joda.time.DateTimeZone;
-import org.joda.time.LocalDate;
-import org.joda.time.MonthDay;
-import org.joda.time.MutableDateTime;
-import org.joda.time.MutableInterval;
-import org.joda.time.MutablePeriod;
-import org.joda.time.Partial;
-import org.joda.time.PeriodType;
-import org.joda.time.ReadWritableInterval;
-import org.joda.time.ReadWritablePeriod;
-import org.joda.time.ReadableInstant;
-import org.joda.time.ReadablePartial;
-import org.joda.time.chrono.CopticChronology;
-import org.joda.time.chrono.EthiopicChronology;
-import org.joda.time.chrono.GJChronology;
-import org.joda.time.chrono.GregorianChronology;
-import org.joda.time.chrono.ISOChronology;
-import org.joda.time.chrono.IslamicChronology;
-import org.joda.time.chrono.JulianChronology;
-import org.joda.time.chrono.ZonedChronology;
-import org.joda.time.format.DateTimeFormatter;
-import org.joda.time.format.DateTimeParser;
-import org.joda.time.format.DateTimePrinter;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
-public class StringConverter_ESTestTest40 extends StringConverter_ESTest_scaffolding {
+/**
+ * Unit tests for the duration parsing functionality of {@link StringConverter}.
+ */
+public class StringConverterTest {
 
-    @Test(timeout = 4000)
-    public void test39() throws Throwable {
-        StringConverter stringConverter0 = new StringConverter();
-        // Undeclared exception!
+    /**
+     * Tests that getDurationMillis() throws an IllegalArgumentException when given a string
+     * in an ISO 8601 interval format, as it expects a duration format.
+     */
+    @Test
+    public void getDurationMillis_shouldThrowException_forInvalidIntervalFormatString() {
+        // Arrange
+        StringConverter converter = new StringConverter();
+        // This string represents an ISO 8601 interval, not a duration.
+        String invalidIntervalString = "6/P7m";
+
+        // Act & Assert
         try {
-            stringConverter0.getDurationMillis("6/P7m");
-            fail("Expecting exception: IllegalArgumentException");
+            converter.getDurationMillis(invalidIntervalString);
+            fail("Expected an IllegalArgumentException to be thrown for invalid format.");
         } catch (IllegalArgumentException e) {
-            //
-            // Invalid format: \"6/P7m\"
-            //
-            verifyException("org.joda.time.convert.StringConverter", e);
+            // Verify that the exception message correctly identifies the invalid format.
+            assertEquals("Invalid format: \"" + invalidIntervalString + "\"", e.getMessage());
         }
     }
 }
