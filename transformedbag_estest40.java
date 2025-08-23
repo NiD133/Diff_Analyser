0493c4036772f67d7da010fabbbd8b59d87ca894
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:23:07 +0800

    gemini processedAutoTestCases transformedbag_estest40.java

--- transformedbag_estest40.java
+++ transformedbag_estest40.java
@@ -1,56 +1,38 @@
 package org.apache.commons.collections4.bag;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Set;
-import java.util.Vector;
 import org.apache.commons.collections4.Bag;
-import org.apache.commons.collections4.Factory;
-import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.SortedBag;
 import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AndPredicate;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ChainedTransformer;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantFactory;
 import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.FactoryTransformer;
-import org.apache.commons.collections4.functors.FalsePredicate;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.InstantiateFactory;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.MapTransformer;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NotPredicate;
-import org.apache.commons.collections4.functors.OnePredicate;
-import org.apache.commons.collections4.functors.SwitchTransformer;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class TransformedBag_ESTestTest40 extends TransformedBag_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
 
-    @Test(timeout = 4000)
-    public void test39() throws Throwable {
-        Vector<Integer> vector0 = new Vector<Integer>(0, 0);
-        TreeBag<Object> treeBag0 = new TreeBag<Object>((Collection<?>) vector0);
-        HashMap<Object, Locale.FilteringMode> hashMap0 = new HashMap<Object, Locale.FilteringMode>();
-        Transformer<Object, Locale.FilteringMode> transformer0 = MapTransformer.mapTransformer((Map<? super Object, ? extends Locale.FilteringMode>) hashMap0);
-        TransformedSortedBag<Object> transformedSortedBag0 = TransformedSortedBag.transformedSortedBag((SortedBag<Object>) treeBag0, (Transformer<? super Object, ?>) transformer0);
-        transformedSortedBag0.hashCode();
+/**
+ * Contains tests for the TransformedBag class, focusing on specific behaviors.
+ */
+public class TransformedBagTest {
+
+    /**
+     * Tests that calling hashCode() on an empty TransformedBag returns the
+     * correct value (0) and does not throw an exception.
+     *
+     * The hashCode of a TransformedBag is delegated to the decorated bag.
+     * For an empty bag, this is expected to be 0.
+     */
+    @Test
+    public void testHashCodeOnEmptyBagIsZero() {
+        // Arrange: Create an empty bag to be decorated.
+        final Bag<Object> emptySourceBag = new HashBag<>();
+        
+        // The specific transformer is irrelevant for this test, as hashCode() on an
+        // empty bag does not involve transforming elements.
+        final Transformer<Object, Object> dummyTransformer = ConstantTransformer.constantTransformer("ignored");
+
+        // Act: Decorate the empty bag.
+        final Bag<Object> transformedBag = TransformedBag.transformingBag(emptySourceBag, dummyTransformer);
+
+        // Assert: The hashCode of an empty collection should be 0.
+        // This also implicitly verifies that the method call completes without an exception.
+        assertEquals("The hashCode of an empty bag should be 0", 0, transformedBag.hashCode());
     }
 }
