5ae657d0e66f5ebf433d110885cdb407bad8285a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:30:40 +0800

    Original processedManualTestCases linkedtreemaptest10.java

---
+++ linkedtreemaptest10.java
@@ -0,0 +1,27 @@
+package com.google.gson.internal;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertThrows;
+import com.google.gson.common.MoreAsserts;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Random;
+import org.junit.Test;
+
+public class LinkedTreeMapTestTest10 {
+
+    @Test
+    public void testContainsNullKeyIsAlwaysFalse() {
+        LinkedTreeMap<String, String> map = new LinkedTreeMap<>();
+        assertThat(map.containsKey(null)).isFalse();
+        map.put("a", "android");
+        assertThat(map.containsKey(null)).isFalse();
+    }
+}
