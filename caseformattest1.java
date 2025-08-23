f22f8d483a0fb1d857400736cf3b9041c62371c4
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:26:19 +0800

    Original processedManualTestCases caseformattest1.java

---
+++ caseformattest1.java
@@ -0,0 +1,29 @@
+package com.google.common.base;
+
+import static com.google.common.base.CaseFormat.LOWER_CAMEL;
+import static com.google.common.base.CaseFormat.LOWER_HYPHEN;
+import static com.google.common.base.CaseFormat.LOWER_UNDERSCORE;
+import static com.google.common.base.CaseFormat.UPPER_CAMEL;
+import static com.google.common.base.CaseFormat.UPPER_UNDERSCORE;
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.truth.Truth.assertWithMessage;
+import com.google.common.annotations.GwtCompatible;
+import com.google.common.annotations.GwtIncompatible;
+import com.google.common.annotations.J2ktIncompatible;
+import com.google.common.testing.NullPointerTester;
+import com.google.common.testing.SerializableTester;
+import junit.framework.TestCase;
+import org.jspecify.annotations.NullUnmarked;
+
+public class CaseFormatTestTest1 extends TestCase {
+
+    public void testIdentity() {
+        for (CaseFormat from : CaseFormat.values()) {
+            assertWithMessage("%s to %s", from, from).that(from.to(from, "foo")).isSameInstanceAs("foo");
+            for (CaseFormat to : CaseFormat.values()) {
+                assertWithMessage("%s to %s", from, to).that(from.to(to, "")).isEmpty();
+                assertWithMessage("%s to %s", from, to).that(from.to(to, " ")).isEqualTo(" ");
+            }
+        }
+    }
+}
