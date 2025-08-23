4f4f883db647d803f1be58eb3582ee2941873624
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:16:21 +0800

    gemini processedAutoTestCases simpleobjectidresolver_estest2.java

--- simpleobjectidresolver_estest2.java
+++ simpleobjectidresolver_estest2.java
@@ -1,26 +1,23 @@
 package com.fasterxml.jackson.annotation;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class SimpleObjectIdResolver_ESTestTest2 extends SimpleObjectIdResolver_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link SimpleObjectIdResolver} class.
+ */
+public class SimpleObjectIdResolverTest {
 
-    @Test(timeout = 4000)
-    public void test1() throws Throwable {
-        SimpleObjectIdResolver simpleObjectIdResolver0 = new SimpleObjectIdResolver();
-        // Undeclared exception!
-        try {
-            simpleObjectIdResolver0.canUseFor((ObjectIdResolver) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("com.fasterxml.jackson.annotation.SimpleObjectIdResolver", e);
-        }
+    /**
+     * Verifies that the canUseFor() method throws a NullPointerException
+     * when passed a null argument, as this is considered invalid input.
+     */
+    @Test(expected = NullPointerException.class, timeout = 4000)
+    public void canUseForShouldThrowNullPointerExceptionWhenResolverIsNull() {
+        // Arrange: Create an instance of the class under test.
+        SimpleObjectIdResolver resolver = new SimpleObjectIdResolver();
+
+        // Act: Call the method with a null argument.
+        // The test framework will assert that a NullPointerException is thrown.
+        resolver.canUseFor(null);
     }
 }
