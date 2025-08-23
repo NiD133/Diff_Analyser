de07d2ddd039364bfc89001a439d647b09706e76
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:26:33 +0800

    Original processedAutoTestCases versionutil_estest13.java

---
+++ versionutil_estest13.java
@@ -0,0 +1,19 @@
+package com.fasterxml.jackson.core.util;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.Version;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class VersionUtil_ESTestTest13 extends VersionUtil_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test12() throws Throwable {
+        Version version0 = VersionUtil.parseVersion("", "", "");
+        assertTrue(version0.isUknownVersion());
+    }
+}
