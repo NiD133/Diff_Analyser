cddccfd44550568c2542caf357f1a63c238804e8
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:51:46 +0800

    Original processedAutoTestCases geohashutils_estest20.java

---
+++ geohashutils_estest20.java
@@ -0,0 +1,31 @@
+package org.locationtech.spatial4j.io;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.HashMap;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+import org.locationtech.spatial4j.context.SpatialContext;
+import org.locationtech.spatial4j.context.SpatialContextFactory;
+import org.locationtech.spatial4j.shape.Point;
+import org.locationtech.spatial4j.shape.Rectangle;
+
+public class GeohashUtils_ESTestTest20 extends GeohashUtils_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test19() throws Throwable {
+        SpatialContext spatialContext0 = SpatialContext.GEO;
+        // Undeclared exception!
+        try {
+            GeohashUtils.decodeBoundary("J-", spatialContext0);
+            fail("Expecting exception: ArrayIndexOutOfBoundsException");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            //
+            // -3
+            //
+            verifyException("org.locationtech.spatial4j.io.GeohashUtils", e);
+        }
+    }
+}
