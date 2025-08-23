9da66154a0b9bcfd797c8df6fae7e324fa991a43
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:11:23 +0800

    gemini processedAutoTestCases randomaccessfileorarray_estest135.java

--- randomaccessfileorarray_estest135.java
+++ randomaccessfileorarray_estest135.java
@@ -1,40 +1,26 @@
 package com.itextpdf.text.pdf;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import com.itextpdf.text.io.GetBufferedRandomAccessSource;
-import com.itextpdf.text.io.IndependentRandomAccessSource;
-import com.itextpdf.text.io.RandomAccessSource;
-import com.itextpdf.text.io.WindowRandomAccessSource;
-import java.io.ByteArrayInputStream;
 import java.io.EOFException;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.PipedInputStream;
-import java.net.URL;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.net.MockURL;
-import org.evosuite.runtime.testdata.EvoSuiteFile;
-import org.evosuite.runtime.testdata.FileSystemHandling;
-import org.junit.runner.RunWith;
 
-public class RandomAccessFileOrArray_ESTestTest135 extends RandomAccessFileOrArray_ESTest_scaffolding {
+/**
+ * Test suite for the {@link RandomAccessFileOrArray} class.
+ */
+public class RandomAccessFileOrArrayTest {
 
-    @Test(timeout = 4000)
-    public void test134() throws Throwable {
-        byte[] byteArray0 = new byte[1];
-        RandomAccessFileOrArray randomAccessFileOrArray0 = new RandomAccessFileOrArray(byteArray0);
-        try {
-            randomAccessFileOrArray0.readUnsignedInt();
-            fail("Expecting exception: EOFException");
-        } catch (EOFException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("com.itextpdf.text.pdf.RandomAccessFileOrArray", e);
-        }
+    /**
+     * Verifies that readUnsignedInt() throws an EOFException when there are
+     * not enough bytes available in the source to read a full 4-byte integer.
+     */
+    @Test(expected = EOFException.class)
+    public void readUnsignedInt_withInsufficientBytes_throwsEOFException() throws IOException {
+        // Arrange: Create a data source with only 1 byte. Reading an unsigned int requires 4 bytes.
+        byte[] insufficientData = new byte[1];
+        RandomAccessFileOrArray fileOrArray = new RandomAccessFileOrArray(insufficientData);
+
+        // Act & Assert: Attempting to read an unsigned int from the truncated source
+        // should throw an EOFException. The expectation is handled by the @Test annotation.
+        fileOrArray.readUnsignedInt();
     }
 }
