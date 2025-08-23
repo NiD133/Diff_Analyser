eba05b0bdf2cd20f7fb8e577308b5d92d2c06904
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:59:51 +0800

    Original processedAutoTestCases lazilyparsednumber_estest18.java

---
+++ lazilyparsednumber_estest18.java
@@ -0,0 +1,22 @@
+package com.google.gson.internal;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class LazilyParsedNumber_ESTestTest18 extends LazilyParsedNumber_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test17() throws Throwable {
+        LazilyParsedNumber lazilyParsedNumber0 = new LazilyParsedNumber("...");
+        // Undeclared exception!
+        try {
+            lazilyParsedNumber0.doubleValue();
+            fail("Expecting exception: NumberFormatException");
+        } catch (NumberFormatException e) {
+        }
+    }
+}
