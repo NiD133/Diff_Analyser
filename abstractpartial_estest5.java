1617c00ce6a61f8baaff3465bd92862bf3944ab2
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:36:50 +0800

    Original processedAutoTestCases abstractpartial_estest5.java

---
+++ abstractpartial_estest5.java
@@ -0,0 +1,52 @@
+package org.joda.time.base;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.Date;
+import java.util.Locale;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.util.MockGregorianCalendar;
+import org.joda.time.Chronology;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeField;
+import org.joda.time.DateTimeFieldType;
+import org.joda.time.DateTimeZone;
+import org.joda.time.DurationFieldType;
+import org.joda.time.Instant;
+import org.joda.time.LocalDate;
+import org.joda.time.LocalDateTime;
+import org.joda.time.LocalTime;
+import org.joda.time.MonthDay;
+import org.joda.time.Partial;
+import org.joda.time.ReadablePartial;
+import org.joda.time.Weeks;
+import org.joda.time.YearMonth;
+import org.joda.time.Years;
+import org.joda.time.chrono.CopticChronology;
+import org.joda.time.chrono.GJChronology;
+import org.joda.time.chrono.GregorianChronology;
+import org.joda.time.chrono.IslamicChronology;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.DateTimeParser;
+import org.joda.time.format.DateTimePrinter;
+import org.junit.runner.RunWith;
+
+public class AbstractPartial_ESTestTest5 extends AbstractPartial_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test04() throws Throwable {
+        Instant instant0 = Instant.now();
+        DateTimeZone dateTimeZone0 = instant0.getZone();
+        LocalDate localDate0 = new LocalDate(dateTimeZone0);
+        DateTimePrinter dateTimePrinter0 = mock(DateTimePrinter.class, new ViolatedAssumptionAnswer());
+        doReturn(0, 0).when(dateTimePrinter0).estimatePrintedLength();
+        DateTimeParser dateTimeParser0 = mock(DateTimeParser.class, new ViolatedAssumptionAnswer());
+        DateTimeFormatter dateTimeFormatter0 = new DateTimeFormatter(dateTimePrinter0, dateTimeParser0);
+        String string0 = localDate0.toString(dateTimeFormatter0);
+        assertEquals("", string0);
+    }
+}
