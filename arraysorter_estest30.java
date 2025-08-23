1014df419e3f74de31b0a5816ae8b0ad3ad083cd
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:37:41 +0800

    Original processedAutoTestCases arraysorter_estest30.java

---
+++ arraysorter_estest30.java
@@ -0,0 +1,19 @@
+package org.apache.commons.lang3;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.Comparator;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class ArraySorter_ESTestTest30 extends ArraySorter_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test29() throws Throwable {
+        ArraySorter arraySorter0 = new ArraySorter();
+    }
+}
