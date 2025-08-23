0b36ae1ad7d8ad7887e3fcaddef8a43f5ae3cd58
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:29:55 +0800

    gemini processedManualTestCases javaversiontest3.java

--- javaversiontest3.java
+++ javaversiontest3.java
@@ -3,11 +3,26 @@ package com.google.gson.internal;
 import static com.google.common.truth.Truth.assertThat;
 import org.junit.Test;
 
-public class JavaVersionTestTest3 {
+/**
+ * Tests for {@link JavaVersion}.
+ */
+public class JavaVersionTest {
 
+    /**
+     * Tests parsing of the legacy Java version string format (e.g., "1.x.y").
+     * Before Java 9, versions were reported as "1.7", "1.8", etc. The major
+     * version in this scheme is the second number (e.g., 7 for "1.7.0").
+     */
     @Test
-    public void testJava7() {
-        // http://www.oracle.com/technetwork/java/javase/jdk7-naming-418744.html
-        assertThat(JavaVersion.parseMajorJavaVersion("1.7.0")).isEqualTo(7);
+    public void parseMajorJavaVersion_legacyFormat_returnsMajorVersion() {
+        // Arrange
+        String legacyJavaVersionString = "1.7.0";
+        int expectedMajorVersion = 7;
+
+        // Act
+        int actualMajorVersion = JavaVersion.parseMajorJavaVersion(legacyJavaVersionString);
+
+        // Assert
+        assertThat(actualMajorVersion).isEqualTo(expectedMajorVersion);
     }
 }
