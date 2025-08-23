e1c89acae95cad6285a46eb2d904a52d10bcc981
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:01:33 +0800

    gemini processedAutoTestCases lookuptranslator_estest8.java

--- lookuptranslator_estest8.java
+++ lookuptranslator_estest8.java
@@ -1,35 +1,22 @@
 package org.apache.commons.lang3.text.translate;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.StringWriter;
-import java.io.Writer;
-import java.nio.CharBuffer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class LookupTranslator_ESTestTest8 extends LookupTranslator_ESTest_scaffolding {
+/**
+ * Tests for {@link LookupTranslator}.
+ */
+public class LookupTranslatorTest {
 
-    @Test(timeout = 4000)
-    public void test07() throws Throwable {
-        CharSequence[][] charSequenceArray0 = new CharSequence[1][2];
-        CharSequence[] charSequenceArray1 = new CharSequence[2];
-        char[] charArray0 = new char[0];
-        CharBuffer charBuffer0 = CharBuffer.wrap(charArray0);
-        charSequenceArray1[0] = (CharSequence) charBuffer0;
-        charSequenceArray1[1] = (CharSequence) charBuffer0;
-        charSequenceArray0[0] = charSequenceArray1;
-        LookupTranslator lookupTranslator0 = null;
-        try {
-            lookupTranslator0 = new LookupTranslator(charSequenceArray0);
-            fail("Expecting exception: IndexOutOfBoundsException");
-        } catch (IndexOutOfBoundsException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("java.nio.Buffer", e);
-        }
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void constructorShouldThrowExceptionForEmptyLookupKey() {
+        // Arrange: Create a lookup table where one of the keys is an empty string.
+        final CharSequence[][] lookupTable = {
+            {"", "value"} // The key is empty, the value is arbitrary.
+        };
+
+        // Act & Assert: The constructor is expected to throw an IndexOutOfBoundsException
+        // because it attempts to access the first character of the empty key to
+        // populate its internal prefix set.
+        new LookupTranslator(lookupTable);
     }
 }
