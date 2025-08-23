2a8604b32deaffdac27621383a341631b44ace7d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:10:28 +0800

    Original processedAutoTestCases quotedprintablecodec_estest20.java

---
+++ quotedprintablecodec_estest20.java
@@ -0,0 +1,24 @@
+package org.apache.commons.codec.net;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.IllegalCharsetNameException;
+import java.nio.charset.UnsupportedCharsetException;
+import java.util.BitSet;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class QuotedPrintableCodec_ESTestTest20 extends QuotedPrintableCodec_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test19() throws Throwable {
+        Charset charset0 = Charset.defaultCharset();
+        QuotedPrintableCodec quotedPrintableCodec0 = new QuotedPrintableCodec();
+        String string0 = quotedPrintableCodec0.decode("", charset0);
+        assertEquals("", string0);
+    }
+}
