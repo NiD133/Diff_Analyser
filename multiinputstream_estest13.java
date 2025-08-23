53b0e0c12c816796c1428edd04a6f838200cf70c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:04:00 +0800

    Original processedAutoTestCases multiinputstream_estest13.java

---
+++ multiinputstream_estest13.java
@@ -0,0 +1,30 @@
+package com.google.common.io;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.ListIterator;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class MultiInputStream_ESTestTest13 extends MultiInputStream_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test12() throws Throwable {
+        ByteSource byteSource0 = ByteSource.empty();
+        ArrayDeque<ByteSource> arrayDeque0 = new ArrayDeque<ByteSource>();
+        arrayDeque0.add(byteSource0);
+        Iterator<ByteSource> iterator0 = arrayDeque0.iterator();
+        MultiInputStream multiInputStream0 = new MultiInputStream(iterator0);
+        multiInputStream0.close();
+        assertFalse(multiInputStream0.markSupported());
+    }
+}
