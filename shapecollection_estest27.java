8e8b04710ae841114c801723fdbeff707d0d7005
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:15:28 +0800

    Original processedAutoTestCases shapecollection_estest27.java

---
+++ shapecollection_estest27.java
@@ -0,0 +1,32 @@
+package org.locationtech.spatial4j.shape;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Stack;
+import java.util.Vector;
+import java.util.function.Predicate;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+import org.locationtech.spatial4j.context.SpatialContext;
+import org.locationtech.spatial4j.context.SpatialContextFactory;
+import org.locationtech.spatial4j.distance.GeodesicSphereDistCalc;
+import org.locationtech.spatial4j.shape.impl.PointImpl;
+import org.locationtech.spatial4j.shape.jts.JtsPoint;
+
+public class ShapeCollection_ESTestTest27 extends ShapeCollection_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test26() throws Throwable {
+        Vector<JtsPoint> vector0 = new Vector<JtsPoint>();
+        SpatialContext spatialContext0 = SpatialContext.GEO;
+        ShapeCollection<JtsPoint> shapeCollection0 = new ShapeCollection<JtsPoint>(vector0, spatialContext0);
+        boolean boolean0 = shapeCollection0.equals(shapeCollection0);
+        assertTrue(boolean0);
+    }
+}
