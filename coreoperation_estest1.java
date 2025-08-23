82287a5242be96c19cc4e4e3596f5b2eeab025b7
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:46:00 +0800

    Original processedAutoTestCases coreoperation_estest1.java

---
+++ coreoperation_estest1.java
@@ -0,0 +1,31 @@
+package org.apache.commons.jxpath.ri.compiler;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.apache.commons.jxpath.JXPathContext;
+import org.apache.commons.jxpath.ri.EvalContext;
+import org.apache.commons.jxpath.ri.JXPathContextReferenceImpl;
+import org.apache.commons.jxpath.ri.QName;
+import org.apache.commons.jxpath.ri.axes.InitialContext;
+import org.apache.commons.jxpath.ri.axes.ParentContext;
+import org.apache.commons.jxpath.ri.axes.PrecedingOrFollowingContext;
+import org.apache.commons.jxpath.ri.axes.RootContext;
+import org.apache.commons.jxpath.ri.axes.SelfContext;
+import org.apache.commons.jxpath.ri.axes.UnionContext;
+import org.apache.commons.jxpath.ri.model.NodePointer;
+import org.apache.commons.jxpath.ri.model.VariablePointer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class CoreOperation_ESTestTest1 extends CoreOperation_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test00() throws Throwable {
+        Expression[] expressionArray0 = new Expression[0];
+        CoreOperationUnion coreOperationUnion0 = new CoreOperationUnion(expressionArray0);
+        String string0 = coreOperationUnion0.toString();
+        assertEquals("", string0);
+    }
+}
