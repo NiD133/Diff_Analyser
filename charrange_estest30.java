3b9dc1ed36a7b3f8443119c1c4d3cc6afa022384
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:42:04 +0800

    Original processedAutoTestCases charrange_estest30.java

---
+++ charrange_estest30.java
@@ -0,0 +1,23 @@
+package org.apache.commons.lang3;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.function.Consumer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class CharRange_ESTestTest30 extends CharRange_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test29() throws Throwable {
+        CharRange charRange0 = CharRange.isIn('8', 'A');
+        boolean boolean0 = charRange0.contains(charRange0);
+        assertEquals('8', charRange0.getStart());
+        assertEquals('A', charRange0.getEnd());
+        assertTrue(boolean0);
+    }
+}
