9c96a600b560a7c25206e2de665e61524e22589b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:31:09 +0800

    Original processedManualTestCases minutestest17.java

---
+++ minutestest17.java
@@ -0,0 +1,36 @@
+package org.joda.time;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class MinutesTestTest17 extends TestCase {
+
+    // (before the late 90's they were all over the place)
+    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
+
+    public static TestSuite suite() {
+        return new TestSuite(TestMinutes.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+    }
+
+    public void testToStandardHours() {
+        Minutes test = Minutes.minutes(3 * 60);
+        Hours expected = Hours.hours(3);
+        assertEquals(expected, test.toStandardHours());
+    }
+}
