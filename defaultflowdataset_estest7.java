236d5b5822a60ed040f3d3c2c0b740e03eff42be
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:47:22 +0800

    Original processedAutoTestCases defaultflowdataset_estest7.java

---
+++ defaultflowdataset_estest7.java
@@ -0,0 +1,26 @@
+package org.jfree.data.flow;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.time.chrono.HijrahEra;
+import java.util.List;
+import java.util.Set;
+import javax.swing.Icon;
+import javax.swing.JLayeredPane;
+import javax.swing.JRadioButtonMenuItem;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class DefaultFlowDataset_ESTestTest7 extends DefaultFlowDataset_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test06() throws Throwable {
+        DefaultFlowDataset<Integer> defaultFlowDataset0 = new DefaultFlowDataset<Integer>();
+        Integer integer0 = JLayeredPane.PALETTE_LAYER;
+        defaultFlowDataset0.setFlow(1, integer0, integer0, (-1670.283700956413));
+        defaultFlowDataset0.setFlow(0, integer0, integer0, (-2177.79472));
+        assertEquals(2, defaultFlowDataset0.getStageCount());
+    }
+}
