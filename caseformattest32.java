dd896414d7f71c0ee43e43505d6ea9f78b3f24af
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:26:25 +0800

    Original processedManualTestCases caseformattest32.java

---
+++ caseformattest32.java
@@ -0,0 +1,27 @@
+package com.google.common.base;
+
+import static com.google.common.base.CaseFormat.LOWER_CAMEL;
+import static com.google.common.base.CaseFormat.LOWER_HYPHEN;
+import static com.google.common.base.CaseFormat.LOWER_UNDERSCORE;
+import static com.google.common.base.CaseFormat.UPPER_CAMEL;
+import static com.google.common.base.CaseFormat.UPPER_UNDERSCORE;
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.truth.Truth.assertWithMessage;
+import com.google.common.annotations.GwtCompatible;
+import com.google.common.annotations.GwtIncompatible;
+import com.google.common.annotations.J2ktIncompatible;
+import com.google.common.testing.NullPointerTester;
+import com.google.common.testing.SerializableTester;
+import junit.framework.TestCase;
+import org.jspecify.annotations.NullUnmarked;
+
+public class CaseFormatTestTest32 extends TestCase {
+
+    public void testConverter_serialization() {
+        for (CaseFormat outer : CaseFormat.values()) {
+            for (CaseFormat inner : CaseFormat.values()) {
+                SerializableTester.reserializeAndAssert(outer.converterTo(inner));
+            }
+        }
+    }
+}
