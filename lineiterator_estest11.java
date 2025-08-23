81e8d07acc1a5cb04c5e567b4f373b5ca62ef155
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:00:17 +0800

    Original processedAutoTestCases lineiterator_estest11.java

---
+++ lineiterator_estest11.java
@@ -0,0 +1,23 @@
+package org.apache.commons.io;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.BufferedReader;
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.NoSuchElementException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class LineIterator_ESTestTest11 extends LineIterator_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test10() throws Throwable {
+        StringReader stringReader0 = new StringReader("remove not supported");
+        BufferedReader bufferedReader0 = new BufferedReader(stringReader0, 2745);
+        LineIterator lineIterator0 = new LineIterator(bufferedReader0);
+        lineIterator0.close();
+    }
+}
