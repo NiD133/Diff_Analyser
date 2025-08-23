c1f36b460374ca6bab6a22b117d48f5bb0d29eca
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:22:12 +0800

    gemini processedAutoTestCases tokeniser_estest31.java

--- tokeniser_estest31.java
+++ tokeniser_estest31.java
@@ -2,24 +2,35 @@ package org.jsoup.parser;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.StringReader;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.jsoup.nodes.Comment;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.nodes.XmlDeclaration;
-import org.junit.runner.RunWith;
 
-public class Tokeniser_ESTestTest31 extends Tokeniser_ESTest_scaffolding {
+import org.jsoup.parser.Token.XmlDecl;
 
-    @Test(timeout = 4000)
-    public void test30() throws Throwable {
-        XmlTreeBuilder xmlTreeBuilder0 = new XmlTreeBuilder();
-        xmlTreeBuilder0.parse("http://www.w3.org/1999/xhtml", "http://www.w3.org/1999/xhtml");
-        Tokeniser tokeniser0 = new Tokeniser(xmlTreeBuilder0);
-        Token.XmlDecl token_XmlDecl0 = tokeniser0.createXmlDeclPending(false);
-        assertNotNull(token_XmlDecl0);
+/**
+ * This test class contains an improved version of a test for the Tokeniser.
+ * The original class name `Tokeniser_ESTestTest31` and its scaffolding are omitted
+ * for clarity, as they are artifacts of a test generation tool.
+ */
+public class TokeniserTest {
+
+    /**
+     * Verifies that createXmlDeclPending() returns a non-null, correctly initialized
+     * XML declaration token.
+     */
+    @Test
+    public void createXmlDeclPendingShouldReturnInitializedToken() {
+        // Arrange: Create a Tokeniser configured for XML parsing.
+        // An XmlTreeBuilder is required to instantiate a Tokeniser. It must be
+        // initialized by parsing some content, though the content itself is not
+        // relevant to this specific test.
+        XmlTreeBuilder xmlTreeBuilder = new XmlTreeBuilder();
+        xmlTreeBuilder.parse("<root/>", "http://example.com/");
+        Tokeniser tokeniser = new Tokeniser(xmlTreeBuilder);
+
+        // Act: Create a pending XML declaration token, setting its 'isDeclaration' flag to false.
+        XmlDecl xmlDeclToken = tokeniser.createXmlDeclPending(false);
+
+        // Assert: The method should return a valid token instance with the specified state.
+        assertNotNull("The created XML declaration token should not be null.", xmlDeclToken);
+        assertFalse("The 'isDeclaration' flag should be correctly set to false.", xmlDeclToken.isDeclaration);
     }
 }
