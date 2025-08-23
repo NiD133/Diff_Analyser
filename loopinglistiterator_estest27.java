1b678eb3986e3c997a371b244b92a040e4a5f018
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:01:41 +0800

    Original processedAutoTestCases loopinglistiterator_estest27.java

---
+++ loopinglistiterator_estest27.java
@@ -0,0 +1,27 @@
+package org.apache.commons.collections4.iterators;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.ConcurrentModificationException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import org.apache.commons.collections4.Closure;
+import org.apache.commons.collections4.functors.InstanceofPredicate;
+import org.apache.commons.collections4.functors.UniquePredicate;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class LoopingListIterator_ESTestTest27 extends LoopingListIterator_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test26() throws Throwable {
+        LinkedList<Integer> linkedList0 = new LinkedList<Integer>();
+        linkedList0.add((Integer) null);
+        LoopingListIterator<Integer> loopingListIterator0 = new LoopingListIterator<Integer>(linkedList0);
+        boolean boolean0 = loopingListIterator0.hasPrevious();
+        assertTrue(boolean0);
+    }
+}
