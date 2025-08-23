ab9bc0c39f564eff372f8d3c8f166dfed282e436
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:39:47 +0800

    gemini processedAutoTestCases britishcutoverchronology_estest22.java

--- britishcutoverchronology_estest22.java
+++ britishcutoverchronology_estest22.java
@@ -1,55 +1,24 @@
 package org.threeten.extra.chrono;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
 import java.time.Clock;
-import java.time.DateTimeException;
-import java.time.Duration;
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
-import java.time.chrono.ChronoLocalDateTime;
-import java.time.chrono.ChronoPeriod;
-import java.time.chrono.ChronoZonedDateTime;
-import java.time.chrono.Era;
-import java.time.chrono.IsoEra;
-import java.time.chrono.JapaneseEra;
-import java.time.chrono.MinguoEra;
-import java.time.chrono.ThaiBuddhistEra;
-import java.time.format.ResolverStyle;
-import java.time.temporal.ChronoField;
-import java.time.temporal.ChronoUnit;
-import java.time.temporal.TemporalAccessor;
-import java.time.temporal.TemporalAmount;
-import java.time.temporal.TemporalField;
-import java.time.temporal.ValueRange;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.time.MockClock;
-import org.evosuite.runtime.mock.java.time.MockInstant;
-import org.evosuite.runtime.mock.java.time.MockZonedDateTime;
-import org.junit.runner.RunWith;
 
-public class BritishCutoverChronology_ESTestTest22 extends BritishCutoverChronology_ESTest_scaffolding {
+/**
+ * Tests for {@link BritishCutoverChronology}.
+ */
+public class BritishCutoverChronologyTest {
 
-    @Test(timeout = 4000)
-    public void test21() throws Throwable {
-        BritishCutoverChronology britishCutoverChronology0 = new BritishCutoverChronology();
-        // Undeclared exception!
-        try {
-            britishCutoverChronology0.dateNow((Clock) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // clock
-            //
-            verifyException("java.util.Objects", e);
-        }
+    /**
+     * Tests that dateNow(Clock) throws a NullPointerException when the clock is null.
+     * The method contract requires a non-null clock.
+     */
+    @Test(expected = NullPointerException.class)
+    public void dateNow_withNullClock_throwsNullPointerException() {
+        // Arrange: Get the singleton instance of the chronology.
+        BritishCutoverChronology chronology = BritishCutoverChronology.INSTANCE;
+
+        // Act & Assert: Call dateNow with a null clock.
+        // The cast to (Clock) is necessary to resolve method overload ambiguity.
+        chronology.dateNow((Clock) null);
     }
 }
