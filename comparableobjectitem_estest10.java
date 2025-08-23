c3941e2026d7f2a71d1e663cb02ef27ba9220b7d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:44:50 +0800

    gemini processedAutoTestCases comparableobjectitem_estest10.java

--- comparableobjectitem_estest10.java
+++ comparableobjectitem_estest10.java
@@ -2,21 +2,30 @@ package org.jfree.data;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
 
-public class ComparableObjectItem_ESTestTest10 extends ComparableObjectItem_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link ComparableObjectItem} class.
+ */
+public class ComparableObjectItemTest {
 
-    @Test(timeout = 4000)
-    public void test09() throws Throwable {
-        Comparable<Object> comparable0 = (Comparable<Object>) mock(Comparable.class, new ViolatedAssumptionAnswer());
-        ComparableObjectItem comparableObjectItem0 = new ComparableObjectItem(comparable0, (Object) null);
-        Object object0 = new Object();
-        comparableObjectItem0.setObject(object0);
-        comparableObjectItem0.hashCode();
+    /**
+     * Verifies that the hashCode is correctly recalculated when the object
+     * component is updated from null to a non-null value.
+     */
+    @Test
+    public void hashCode_shouldChange_whenObjectIsUpdatedFromNull() {
+        // Arrange: Create an item with a null object and calculate its initial hash code.
+        String comparable = "TestKey";
+        ComparableObjectItem item = new ComparableObjectItem(comparable, null);
+        int initialHashCode = item.hashCode();
+
+        // Act: Update the item with a non-null object and get the new hash code.
+        Object newObject = "TestObject";
+        item.setObject(newObject);
+        int updatedHashCode = item.hashCode();
+
+        // Assert: The hash code should be different after the update.
+        assertNotEquals("The hash code should change when the object is updated from null to a non-null value.",
+                        initialHashCode, updatedHashCode);
     }
 }
