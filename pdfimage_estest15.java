342b447a4330416f5bc01ee55ccfa0b265d3f405
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:08:11 +0800

    gemini processedAutoTestCases pdfimage_estest15.java

--- pdfimage_estest15.java
+++ pdfimage_estest15.java
@@ -1,39 +1,30 @@
 package com.itextpdf.text.pdf;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
+import com.itextpdf.text.BadPdfFormatException;
 import com.itextpdf.text.Image;
-import com.itextpdf.text.ImgJBIG2;
-import com.itextpdf.text.ImgTemplate;
-import com.itextpdf.text.Rectangle;
-import java.io.BufferedInputStream;
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.PipedInputStream;
-import java.io.PipedOutputStream;
-import java.io.PrintStream;
-import java.util.Locale;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.io.MockPrintStream;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class PdfImage_ESTestTest15 extends PdfImage_ESTest_scaffolding {
+/**
+ * This test case focuses on the constructor behavior of the {@link PdfImage} class.
+ */
+public class PdfImage_ESTestTest15 {
 
-    @Test(timeout = 4000)
-    public void test14() throws Throwable {
-        PdfIndirectReference pdfIndirectReference0 = new PdfIndirectReference(508, 508);
-        PdfImage pdfImage0 = null;
-        try {
-            pdfImage0 = new PdfImage((Image) null, "", pdfIndirectReference0);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("com.itextpdf.text.pdf.PdfImage", e);
-        }
+    /**
+     * Verifies that the PdfImage constructor throws a NullPointerException
+     * when the provided Image object is null.
+     */
+    @Test(expected = NullPointerException.class)
+    public void constructorShouldThrowNullPointerExceptionForNullImage() throws BadPdfFormatException {
+        // Arrange
+        Image nullImage = null;
+        String dummyName = "ImageName";
+        // The indirect reference is a required parameter, but its value is irrelevant for this test.
+        PdfIndirectReference dummyMaskRef = new PdfIndirectReference(0, 0);
+
+        // Act
+        // This call is expected to throw a NullPointerException because the first argument is null.
+        new PdfImage(nullImage, dummyName, dummyMaskRef);
+
+        // Assert is handled by the @Test(expected=...) annotation.
     }
 }
