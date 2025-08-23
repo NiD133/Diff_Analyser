20f7de7ace30f3e695e01dd99aedef2d844b9a24
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:52:27 +0800

    Original processedAutoTestCases gridarrangement_estest51.java

---
+++ gridarrangement_estest51.java
@@ -0,0 +1,39 @@
+package org.jfree.chart.block;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.awt.Graphics2D;
+import java.awt.SystemColor;
+import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.util.MockGregorianCalendar;
+import org.jfree.chart.api.HorizontalAlignment;
+import org.jfree.chart.api.RectangleAnchor;
+import org.jfree.chart.api.VerticalAlignment;
+import org.jfree.chart.text.TextBlockAnchor;
+import org.jfree.data.Range;
+import org.jfree.data.time.TimePeriodAnchor;
+import org.jfree.data.time.TimeSeries;
+import org.junit.runner.RunWith;
+
+public class GridArrangement_ESTestTest51 extends GridArrangement_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test50() throws Throwable {
+        GridArrangement gridArrangement0 = new GridArrangement((-653), (-653));
+        assertNotNull(gridArrangement0);
+        // Undeclared exception!
+        try {
+            gridArrangement0.arrangeNN((BlockContainer) null, (Graphics2D) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.jfree.chart.block.GridArrangement", e);
+        }
+    }
+}
