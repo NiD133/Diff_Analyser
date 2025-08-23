5b2d9f16170fd698af22a5f1d88b70fa9fa44d2c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:16:27 +0800

    Original processedAutoTestCases simplepathinterpreter_estest15.java

---
+++ simplepathinterpreter_estest15.java
@@ -0,0 +1,75 @@
+package org.apache.commons.jxpath.ri.axes;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.Locale;
+import org.apache.commons.jxpath.BasicVariables;
+import org.apache.commons.jxpath.JXPathBasicBeanInfo;
+import org.apache.commons.jxpath.JXPathContext;
+import org.apache.commons.jxpath.ri.EvalContext;
+import org.apache.commons.jxpath.ri.JXPathContextReferenceImpl;
+import org.apache.commons.jxpath.ri.QName;
+import org.apache.commons.jxpath.ri.compiler.Constant;
+import org.apache.commons.jxpath.ri.compiler.CoreFunction;
+import org.apache.commons.jxpath.ri.compiler.CoreOperationAnd;
+import org.apache.commons.jxpath.ri.compiler.CoreOperationEqual;
+import org.apache.commons.jxpath.ri.compiler.CoreOperationGreaterThanOrEqual;
+import org.apache.commons.jxpath.ri.compiler.CoreOperationLessThanOrEqual;
+import org.apache.commons.jxpath.ri.compiler.CoreOperationMod;
+import org.apache.commons.jxpath.ri.compiler.CoreOperationMultiply;
+import org.apache.commons.jxpath.ri.compiler.CoreOperationNegate;
+import org.apache.commons.jxpath.ri.compiler.CoreOperationNotEqual;
+import org.apache.commons.jxpath.ri.compiler.CoreOperationOr;
+import org.apache.commons.jxpath.ri.compiler.CoreOperationSubtract;
+import org.apache.commons.jxpath.ri.compiler.CoreOperationUnion;
+import org.apache.commons.jxpath.ri.compiler.Expression;
+import org.apache.commons.jxpath.ri.compiler.NameAttributeTest;
+import org.apache.commons.jxpath.ri.compiler.NodeNameTest;
+import org.apache.commons.jxpath.ri.compiler.NodeTest;
+import org.apache.commons.jxpath.ri.compiler.NodeTypeTest;
+import org.apache.commons.jxpath.ri.compiler.ProcessingInstructionTest;
+import org.apache.commons.jxpath.ri.compiler.Step;
+import org.apache.commons.jxpath.ri.compiler.VariableReference;
+import org.apache.commons.jxpath.ri.model.NodePointer;
+import org.apache.commons.jxpath.ri.model.VariablePointer;
+import org.apache.commons.jxpath.ri.model.beans.BeanPointer;
+import org.apache.commons.jxpath.ri.model.beans.BeanPropertyPointer;
+import org.apache.commons.jxpath.ri.model.beans.NullPointer;
+import org.apache.commons.jxpath.ri.model.beans.NullPropertyPointer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class SimplePathInterpreter_ESTestTest15 extends SimplePathInterpreter_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test14() throws Throwable {
+        QName qName0 = new QName("#{WI2%=9byI>t{^<");
+        Locale locale0 = Locale.JAPAN;
+        NodePointer nodePointer0 = NodePointer.newNodePointer(qName0, locale0, locale0);
+        Step[] stepArray0 = new Step[1];
+        Expression[] expressionArray0 = new Expression[1];
+        CoreFunction coreFunction0 = new CoreFunction(Integer.MIN_VALUE, expressionArray0);
+        CoreOperationLessThanOrEqual coreOperationLessThanOrEqual0 = new CoreOperationLessThanOrEqual(coreFunction0, coreFunction0);
+        CoreOperationMod coreOperationMod0 = new CoreOperationMod(coreOperationLessThanOrEqual0, coreOperationLessThanOrEqual0);
+        CoreOperationEqual coreOperationEqual0 = new CoreOperationEqual(coreOperationMod0, coreFunction0);
+        expressionArray0[0] = (Expression) coreOperationEqual0;
+        Step step0 = mock(Step.class, new ViolatedAssumptionAnswer());
+        doReturn(Integer.MIN_VALUE).when(step0).getAxis();
+        doReturn((Object) expressionArray0, (Object) expressionArray0).when(step0).getPredicates();
+        stepArray0[0] = step0;
+        // Undeclared exception!
+        try {
+            SimplePathInterpreter.interpretSimpleLocationPath((EvalContext) null, nodePointer0, stepArray0);
+            fail("Expecting exception: ArithmeticException");
+        } catch (ArithmeticException e) {
+            //
+            // / by zero
+            //
+            verifyException("org.apache.commons.jxpath.ri.compiler.CoreOperationMod", e);
+        }
+    }
+}
