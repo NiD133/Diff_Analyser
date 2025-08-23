2fa240b035cae657482d40b93474913eef58ff9a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:55:13 +0800

    Original processedAutoTestCases islamicchronology_estest31.java

---
+++ islamicchronology_estest31.java
@@ -0,0 +1,30 @@
+package org.joda.time.chrono;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.TimeZone;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.joda.time.Chronology;
+import org.joda.time.DateTimeZone;
+import org.joda.time.tz.UTCProvider;
+import org.junit.runner.RunWith;
+
+public class IslamicChronology_ESTestTest31 extends IslamicChronology_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test30() throws Throwable {
+        CopticChronology copticChronology0 = CopticChronology.getInstance();
+        IslamicChronology islamicChronology0 = new IslamicChronology(copticChronology0, copticChronology0, (IslamicChronology.LeapYearPatternType) null);
+        // Undeclared exception!
+        try {
+            islamicChronology0.getDayOfMonth(1);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+        }
+    }
+}
