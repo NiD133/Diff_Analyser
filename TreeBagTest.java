81315e19b023e2ea5f019e60afd0a9438145a676
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 05:35:29 +0800

    gpt manualTestSuite TreeBagTest.java

--- TreeBagTest.java
+++ TreeBagTest.java
@@ -24,23 +24,31 @@ import org.apache.commons.collections4.SortedBag;
 import org.junit.jupiter.api.Test;
 
 /**
- * Extension of {@link AbstractBagTest} for exercising the {@link TreeBag}
- * implementation.
+ * Test suite for {@link TreeBag} implementation, extending {@link AbstractSortedBagTest}.
  */
 public class TreeBagTest<T> extends AbstractSortedBagTest<T> {
 
+    /**
+     * Returns the compatibility version for serialization tests.
+     */
     @Override
     public String getCompatibilityVersion() {
         return "4";
     }
 
+    /**
+     * Creates and returns a new instance of {@link TreeBag}.
+     */
     @Override
     public SortedBag<T> makeObject() {
         return new TreeBag<>();
     }
 
+    /**
+     * Sets up a sample {@link SortedBag} with predefined elements for testing.
+     */
     @SuppressWarnings("unchecked")
-    public SortedBag<T> setupBag() {
+    private SortedBag<T> createSampleBag() {
         final SortedBag<T> bag = makeObject();
         bag.add((T) "C");
         bag.add((T) "A");
@@ -49,42 +57,48 @@ public class TreeBagTest<T> extends AbstractSortedBagTest<T> {
         return bag;
     }
 
+    /**
+     * Tests that adding a non-comparable object to a {@link TreeBag} throws an {@link IllegalArgumentException}.
+     */
     @Test
-    void testCollections265() {
+    void testAddNonComparableObject() {
         final Bag<Object> bag = new TreeBag<>();
-
         assertThrows(IllegalArgumentException.class, () -> bag.add(new Object()));
     }
 
+    /**
+     * Tests that adding a null element to a {@link TreeBag} throws a {@link NullPointerException}.
+     */
     @Test
-    void testCollections555() {
+    void testAddNullElement() {
         final Bag<Object> bag = new TreeBag<>();
-
         assertThrows(NullPointerException.class, () -> bag.add(null));
 
-        final Bag<String> bag2 = new TreeBag<>(String::compareTo);
-        // jdk bug: adding null to an empty TreeMap works
-        // thus ensure that the bag is not empty before adding null
-        bag2.add("a");
-
-        assertThrows(NullPointerException.class, () -> bag2.add(null));
+        final Bag<String> bagWithComparator = new TreeBag<>(String::compareTo);
+        // Ensure the bag is not empty before adding null to avoid JDK bug with TreeMap
+        bagWithComparator.add("a");
+        assertThrows(NullPointerException.class, () -> bagWithComparator.add(null));
     }
 
+    /**
+     * Tests the ordering of elements in a {@link TreeBag}.
+     */
     @Test
-    void testOrdering() {
-        final Bag<T> bag = setupBag();
-        assertEquals("A", bag.toArray()[0], "Should get elements in correct order");
-        assertEquals("B", bag.toArray()[1], "Should get elements in correct order");
-        assertEquals("C", bag.toArray()[2], "Should get elements in correct order");
-        assertEquals("A", ((SortedBag<T>) bag).first(), "Should get first key");
-        assertEquals("D", ((SortedBag<T>) bag).last(), "Should get last key");
+    void testElementOrdering() {
+        final Bag<T> bag = createSampleBag();
+        assertEquals("A", bag.toArray()[0], "First element should be 'A'");
+        assertEquals("B", bag.toArray()[1], "Second element should be 'B'");
+        assertEquals("C", bag.toArray()[2], "Third element should be 'C'");
+        assertEquals("A", ((SortedBag<T>) bag).first(), "First element should be 'A'");
+        assertEquals("D", ((SortedBag<T>) bag).last(), "Last element should be 'D'");
     }
 
-//    void testCreate() throws Exception {
-//        Bag<T> bag = makeObject();
-//        writeExternalFormToDisk((java.io.Serializable) bag, "src/test/resources/data/test/TreeBag.emptyCollection.version4.obj");
-//        bag = makeFullCollection();
-//        writeExternalFormToDisk((java.io.Serializable) bag, "src/test/resources/data/test/TreeBag.fullCollection.version4.obj");
-//    }
-
+    // Uncomment and implement if serialization tests are needed
+    // @Test
+    // void testSerialization() throws Exception {
+    //     Bag<T> emptyBag = makeObject();
+    //     writeExternalFormToDisk((java.io.Serializable) emptyBag, "src/test/resources/data/test/TreeBag.emptyCollection.version4.obj");
+    //     Bag<T> fullBag = makeFullCollection();
+    //     writeExternalFormToDisk((java.io.Serializable) fullBag, "src/test/resources/data/test/TreeBag.fullCollection.version4.obj");
+    // }
 }
