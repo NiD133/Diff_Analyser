f4e93e790e138a3e7bb6668b0d6cd9ab23e0d50f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:00:17 +0800

    Original processedAutoTestCases lineiterator_estest12.java

---
+++ lineiterator_estest12.java
@@ -0,0 +1,31 @@
+package org.apache.commons.io;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.BufferedReader;
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.NoSuchElementException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class LineIterator_ESTestTest12 extends LineIterator_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test11() throws Throwable {
+        StringReader stringReader0 = new StringReader("");
+        LineIterator lineIterator0 = new LineIterator(stringReader0);
+        // Undeclared exception!
+        try {
+            lineIterator0.nextLine();
+            fail("Expecting exception: NoSuchElementException");
+        } catch (NoSuchElementException e) {
+            //
+            // No more lines
+            //
+            verifyException("org.apache.commons.io.LineIterator", e);
+        }
+    }
+}
