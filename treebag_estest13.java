f55cfd7e00cb90df4a98be0da105981434c29a9b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:23:48 +0800

    gemini processedAutoTestCases treebag_estest13.java

--- treebag_estest13.java
+++ treebag_estest13.java
@@ -1,38 +1,24 @@
 package org.apache.commons.collections4.bag;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.EnumSet;
-import java.util.LinkedHashSet;
-import java.util.LinkedList;
-import java.util.Locale;
-import java.util.NoSuchElementException;
-import java.util.SortedMap;
-import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
 
-public class TreeBag_ESTestTest13 extends TreeBag_ESTest_scaffolding {
+/**
+ * Unit tests for {@link TreeBag}.
+ */
+public class TreeBagTest {
 
-    @Test(timeout = 4000)
-    public void test12() throws Throwable {
-        TreeBag<Predicate<Object>> treeBag0 = new TreeBag<Predicate<Object>>();
-        // Undeclared exception!
-        try {
-            treeBag0.add((Predicate<Object>) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // object
-            //
-            verifyException("java.util.Objects", e);
-        }
+    /**
+     * Tests that adding a null element to a TreeBag throws a NullPointerException,
+     * as the underlying TreeMap does not permit null keys by default.
+     */
+    @Test(expected = NullPointerException.class)
+    public void add_withNullObject_shouldThrowNullPointerException() {
+        // Given: An empty TreeBag that uses natural ordering.
+        final TreeBag<String> treeBag = new TreeBag<>();
+
+        // When: A null element is added.
+        treeBag.add(null);
+
+        // Then: A NullPointerException is expected.
     }
 }
