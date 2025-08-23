941b8d5eecb294d8791ba2fe4c71871611d22f6e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:41:25 +0800

    gemini processedAutoTestCases cachekey_estest13.java

--- cachekey_estest13.java
+++ cachekey_estest13.java
@@ -1,21 +1,28 @@
 package org.apache.ibatis.cache;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class CacheKey_ESTestTest13 extends CacheKey_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
 
-    @Test(timeout = 4000)
-    public void test12() throws Throwable {
-        CacheKey cacheKey0 = new CacheKey();
-        cacheKey0.update(cacheKey0);
-        CacheKey cacheKey1 = cacheKey0.clone();
-        boolean boolean0 = cacheKey0.equals(cacheKey1);
-        assertNotSame(cacheKey1, cacheKey0);
-        assertTrue(boolean0);
+/**
+ * Tests for {@link CacheKey}.
+ */
+public class CacheKeyTest {
+
+    @Test
+    public void cloneShouldCreateEqualButNotSameInstance() throws CloneNotSupportedException {
+        // Arrange: Create an original CacheKey and populate it with some data.
+        CacheKey originalKey = new CacheKey();
+        originalKey.update("first-item");
+        originalKey.update(12345);
+
+        // Act: Clone the original key.
+        CacheKey clonedKey = originalKey.clone();
+
+        // Assert: The cloned key should be a different object instance,
+        // but it must be logically equal to the original.
+        assertNotSame("The cloned key should be a new object instance.", originalKey, clonedKey);
+        assertEquals("The cloned key should be equal to the original key.", originalKey, clonedKey);
     }
 }
