113c6926cde368a7ff7a8b596566e4066298b429
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:23:13 +0800

    gemini processedAutoTestCases transformedmultivaluedmap_estest17.java

--- transformedmultivaluedmap_estest17.java
+++ transformedmultivaluedmap_estest17.java
@@ -1,66 +1,38 @@
 package org.apache.commons.collections4.multimap;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.AbstractMap;
-import java.util.ArrayDeque;
-import java.util.Comparator;
-import java.util.ConcurrentModificationException;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
-import java.util.PriorityQueue;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.Factory;
 import org.apache.commons.collections4.MultiValuedMap;
-import org.apache.commons.collections4.Predicate;
 import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AllPredicate;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ChainedTransformer;
-import org.apache.commons.collections4.functors.CloneTransformer;
-import org.apache.commons.collections4.functors.ClosureTransformer;
-import org.apache.commons.collections4.functors.ConstantFactory;
 import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.ExceptionFactory;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.FactoryTransformer;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.MapTransformer;
-import org.apache.commons.collections4.functors.NOPTransformer;
-import org.apache.commons.collections4.functors.NotPredicate;
-import org.apache.commons.collections4.functors.NullIsExceptionPredicate;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.SwitchTransformer;
-import org.apache.commons.collections4.functors.TransformerClosure;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class TransformedMultiValuedMap_ESTestTest17 extends TransformedMultiValuedMap_ESTest_scaffolding {
+import java.util.ConcurrentModificationException;
 
-    @Test(timeout = 4000)
-    public void test16() throws Throwable {
-        Integer integer0 = new Integer(0);
-        ConstantFactory<Integer> constantFactory0 = new ConstantFactory<Integer>(integer0);
-        FactoryTransformer<Object, Integer> factoryTransformer0 = new FactoryTransformer<Object, Integer>(constantFactory0);
-        ArrayListValuedHashMap<Integer, Integer> arrayListValuedHashMap0 = new ArrayListValuedHashMap<Integer, Integer>(450, 0);
-        TransformedMultiValuedMap<Integer, Integer> transformedMultiValuedMap0 = new TransformedMultiValuedMap<Integer, Integer>(arrayListValuedHashMap0, factoryTransformer0, factoryTransformer0);
-        transformedMultiValuedMap0.put(integer0, (Integer) null);
-        // Undeclared exception!
-        try {
-            transformedMultiValuedMap0.putAll((MultiValuedMap<? extends Integer, ? extends Integer>) transformedMultiValuedMap0);
-            fail("Expecting exception: ConcurrentModificationException");
-        } catch (ConcurrentModificationException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("java.util.ArrayList$Itr", e);
-        }
+/**
+ * Tests for {@link TransformedMultiValuedMap}.
+ */
+public class TransformedMultiValuedMapTest {
+
+    /**
+     * Tests that calling putAll with the map itself as the source results in a
+     * ConcurrentModificationException. This occurs because the method attempts
+     * to modify the map while iterating over it.
+     */
+    @Test(expected = ConcurrentModificationException.class)
+    public void putAll_whenSourceIsTheSameMap_throwsConcurrentModificationException() {
+        // Arrange
+        // A transformer that converts any input key or value to the integer 0.
+        final Transformer<Object, Integer> transformer = ConstantTransformer.constantTransformer(0);
+        final MultiValuedMap<Integer, Integer> backingMap = new ArrayListValuedHashMap<>();
+        final TransformedMultiValuedMap<Integer, Integer> transformedMap =
+                new TransformedMultiValuedMap<>(backingMap, transformer, transformer);
+
+        // Add an initial entry to ensure the map is not empty, so that iteration occurs.
+        transformedMap.put(1, 2); // This will be stored as {0=[0]}
+
+        // Act
+        // Attempt to add all entries from the map back into itself.
+        // This will fail because the underlying iterator detects a modification
+        // during iteration.
+        transformedMap.putAll(transformedMap);
     }
 }
