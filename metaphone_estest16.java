d0b36b792422a9d053d190b96a8da6fba81423d9
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:02:24 +0800

    gemini processedAutoTestCases metaphone_estest16.java

--- metaphone_estest16.java
+++ metaphone_estest16.java
@@ -1,19 +1,44 @@
 package org.apache.commons.codec.language;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class Metaphone_ESTestTest16 extends Metaphone_ESTest_scaffolding {
+/**
+ * Tests for the {@link Metaphone} class.
+ */
+public class MetaphoneTest {
 
-    @Test(timeout = 4000)
-    public void test15() throws Throwable {
-        Metaphone metaphone0 = new Metaphone();
-        String string0 = metaphone0.metaphone("SCH");
-        assertEquals(4, metaphone0.getMaxCodeLen());
-        assertEquals("SK", string0);
+    /**
+     * Tests that the Metaphone algorithm correctly encodes the string "SCH" to "SK".
+     * This is a specific encoding rule for Metaphone.
+     */
+    @Test
+    public void metaphoneShouldEncodeSchAsSk() {
+        // Arrange
+        Metaphone metaphone = new Metaphone();
+        String input = "SCH";
+        String expectedCode = "SK";
+
+        // Act
+        String actualCode = metaphone.metaphone(input);
+
+        // Assert
+        assertEquals("The metaphone code for 'SCH' should be 'SK'", expectedCode, actualCode);
+    }
+
+    /**
+     * Tests that a new Metaphone instance is created with the default max code length of 4.
+     */
+    @Test
+    public void constructorShouldSetDefaultMaxCodeLength() {
+        // Arrange
+        Metaphone metaphone = new Metaphone();
+        int expectedMaxCodeLen = 4;
+
+        // Act
+        int actualMaxCodeLen = metaphone.getMaxCodeLen();
+
+        // Assert
+        assertEquals("Default max code length should be 4", expectedMaxCodeLen, actualMaxCodeLen);
     }
 }
