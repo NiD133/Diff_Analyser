0a7543e9d0cadebb1998b9fdbc52b03fb85f18c5
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:29:17 +0800

    gemini processedAutoTestCases xnode_estest88.java

--- xnode_estest88.java
+++ xnode_estest88.java
@@ -1,30 +1,37 @@
 package org.apache.ibatis.parsing;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.List;
-import java.util.Locale;
-import java.util.Properties;
-import java.util.function.Supplier;
-import javax.imageio.metadata.IIOMetadataNode;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
-import org.w3c.dom.Document;
 import org.w3c.dom.Node;
-import org.xml.sax.ext.DefaultHandler2;
 
-public class XNode_ESTestTest88 extends XNode_ESTest_scaffolding {
+import javax.imageio.metadata.IIOMetadataNode;
+import java.util.Properties;
 
-    @Test(timeout = 4000)
-    public void test087() throws Throwable {
-        Properties properties0 = new Properties();
-        IIOMetadataNode iIOMetadataNode0 = new IIOMetadataNode();
-        XNode xNode0 = new XNode((XPathParser) null, iIOMetadataNode0, properties0);
-        String string0 = xNode0.getStringAttribute("g\"7}o`");
-        assertNull(string0);
+import static org.junit.Assert.assertNull;
+
+/**
+ * Test suite for the XNode class.
+ * This refactored test focuses on a specific behavior of the getStringAttribute method.
+ */
+public class XNodeTest {
+
+    /**
+     * Verifies that getStringAttribute() returns null when the requested attribute does not exist.
+     */
+    @Test
+    public void getStringAttributeShouldReturnNullForNonExistentAttribute() {
+        // Arrange
+        // Create an empty DOM node that has no attributes.
+        // IIOMetadataNode is a standard, concrete implementation of the org.w3c.dom.Node interface.
+        Node nodeWithoutAttributes = new IIOMetadataNode();
+        Properties variables = new Properties();
+
+        // The XPathParser is not used by the getStringAttribute method, so it can be null for this test.
+        XNode xNode = new XNode(null, nodeWithoutAttributes, variables);
+
+        // Act
+        String attributeValue = xNode.getStringAttribute("nonExistentAttribute");
+
+        // Assert
+        assertNull("Expected a null value for an attribute that does not exist.", attributeValue);
     }
 }
