f6224f702f3b25020cd620548fcba2901474258a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:33:13 +0800

    Original processedManualTestCases secondstest18.java

---
+++ secondstest18.java
@@ -0,0 +1,36 @@
+package org.joda.time;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class SecondsTestTest18 extends TestCase {
+
+    // (before the late 90's they were all over the place)
+    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
+
+    public static TestSuite suite() {
+        return new TestSuite(TestSeconds.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+    }
+
+    public void testToStandardMinutes() {
+        Seconds test = Seconds.seconds(60 * 2);
+        Minutes expected = Minutes.minutes(2);
+        assertEquals(expected, test.toStandardMinutes());
+    }
+}
