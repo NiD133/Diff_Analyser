2397838955254b8bfebab4141b9ecf37ab382b88
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:58:26 +0800

    gemini processedAutoTestCases jsontreereader_estest98.java

--- jsontreereader_estest98.java
+++ jsontreereader_estest98.java
@@ -1,34 +1,25 @@
 package com.google.gson.internal.bind;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
-import com.google.gson.JsonNull;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonPrimitive;
-import com.google.gson.Strictness;
-import com.google.gson.stream.JsonToken;
-import java.io.IOException;
-import java.util.ConcurrentModificationException;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class JsonTreeReader_ESTestTest98 extends JsonTreeReader_ESTest_scaffolding {
+/**
+ * Unit tests for {@link JsonTreeReader}.
+ */
+public class JsonTreeReaderTest {
 
-    @Test(timeout = 4000)
-    public void test097() throws Throwable {
-        JsonTreeReader jsonTreeReader0 = new JsonTreeReader((JsonElement) null);
-        // Undeclared exception!
-        try {
-            jsonTreeReader0.nextName();
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-        }
+    /**
+     * Verifies that calling nextName() on a JsonTreeReader initialized with a null
+     * JsonElement throws a NullPointerException. The constructor itself accepts null,
+     * but subsequent read operations are expected to fail.
+     */
+    @Test(expected = NullPointerException.class)
+    public void nextName_whenInitializedWithNull_throwsNullPointerException() {
+        // Arrange: Create a reader with a null JsonElement.
+        JsonTreeReader reader = new JsonTreeReader((JsonElement) null);
+
+        // Act & Assert: Attempting to read the next name should throw a NullPointerException.
+        // The assertion is handled by the @Test(expected=...) annotation.
+        reader.nextName();
     }
 }
