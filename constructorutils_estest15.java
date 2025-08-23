d1be8059c0a2647ee854e08f5617ebedbb967e5b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:45:35 +0800

    gemini processedAutoTestCases constructorutils_estest15.java

--- constructorutils_estest15.java
+++ constructorutils_estest15.java
@@ -1,29 +1,32 @@
 package org.apache.commons.lang3.reflect;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.annotation.Annotation;
-import java.lang.reflect.AccessibleObject;
-import java.lang.reflect.Array;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.InstantiationException;
+
+/**
+ * This test class contains tests for {@link ConstructorUtils}.
+ * The original test class name and inheritance are kept as per the problem description.
+ */
 public class ConstructorUtils_ESTestTest15 extends ConstructorUtils_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test14() throws Throwable {
-        Class<Integer>[] classArray0 = (Class<Integer>[]) Array.newInstance(Class.class, 19);
-        Class<Object> class0 = Object.class;
-        Class<Integer>[] classArray1 = (Class<Integer>[]) Array.newInstance(Class.class, 0);
-        // Undeclared exception!
-        try {
-            ConstructorUtils.invokeExactConstructor(class0, (Object[]) classArray0, (Class<?>[]) classArray1);
-            fail("Expecting exception: IllegalArgumentException");
-        } catch (IllegalArgumentException e) {
-        }
+    /**
+     * Tests that {@link ConstructorUtils#invokeExactConstructor(Class, Object[], Class[])}
+     * throws an IllegalArgumentException when the number of arguments provided does not
+     * match the number of parameter types specified.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void invokeExactConstructorShouldThrowExceptionForMismatchedArgumentAndParameterCounts()
+            throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        // Arrange: Define the class to instantiate and create arguments and parameter
+        // types with different lengths. The method's contract states this is an illegal state.
+        final Class<Object> classToInstantiate = Object.class;
+        final Object[] constructorArgs = new Object[19];
+        final Class<?>[] parameterTypes = new Class<?>[0]; // An empty array
+
+        // Act & Assert: Call the method and expect an IllegalArgumentException.
+        // The assertion is handled declaratively by the `expected` attribute of the @Test annotation.
+        ConstructorUtils.invokeExactConstructor(classToInstantiate, constructorArgs, parameterTypes);
     }
 }
