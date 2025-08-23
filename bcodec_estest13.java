92ec9ff2634ba6719360337dd6d0504699c4e9e9
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:39:05 +0800

    Original processedAutoTestCases bcodec_estest13.java

---
+++ bcodec_estest13.java
@@ -0,0 +1,30 @@
+package org.apache.commons.codec.net;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.nio.charset.Charset;
+import java.nio.charset.IllegalCharsetNameException;
+import java.nio.charset.UnsupportedCharsetException;
+import org.apache.commons.codec.CodecPolicy;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class BCodec_ESTestTest13 extends BCodec_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test12() throws Throwable {
+        BCodec bCodec0 = new BCodec();
+        // Undeclared exception!
+        try {
+            bCodec0.encode(" encoded content", (String) null);
+            fail("Expecting exception: IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //
+            // Null charset name
+            //
+            verifyException("java.nio.charset.Charset", e);
+        }
+    }
+}
