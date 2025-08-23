3208b1fd84a963fe69636e0572e527c4000ea319
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:00:05 +0800

    gemini processedAutoTestCases leafnode_estest26.java

--- leafnode_estest26.java
+++ leafnode_estest26.java
@@ -1,30 +1,38 @@
 package org.jsoup.nodes;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.List;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.jsoup.internal.QuietAppendable;
-import org.jsoup.parser.Parser;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
-public class LeafNode_ESTestTest26 extends LeafNode_ESTest_scaffolding {
+/**
+ * Test suite for the LeafNode abstract class.
+ */
+public class LeafNodeTest {
 
-    @Test(timeout = 4000)
-    public void test25() throws Throwable {
-        CDataNode cDataNode0 = new CDataNode("-/9fsuZ");
-        cDataNode0.value = (Object) cDataNode0;
-        // Undeclared exception!
+    /**
+     * Tests that calling attributes() on a LeafNode with a corrupted internal 'value'
+     * field throws a ClassCastException. This is a white-box test verifying the
+     * method's behavior under unexpected internal states.
+     */
+    @Test
+    public void attributesThrowsClassCastExceptionWhenInternalValueIsInvalidType() {
+        // Arrange: Create a CDataNode, which is a subclass of LeafNode.
+        CDataNode node = new CDataNode("Initial content");
+
+        // Act: Directly manipulate the internal 'value' field to simulate a corrupted state.
+        // The 'value' field is expected to be a String or an Attributes object,
+        // but here we set it to the node itself to create an invalid state.
+        node.value = node;
+
+        // Assert: Calling attributes() should result in a ClassCastException because
+        // it cannot cast the invalid object type to a String.
         try {
-            cDataNode0.attributes();
-            fail("Expecting exception: ClassCastException");
+            node.attributes();
+            fail("A ClassCastException should have been thrown due to the invalid internal state.");
         } catch (ClassCastException e) {
-            //
-            // org.jsoup.nodes.CDataNode cannot be cast to java.lang.String
-            //
-            verifyException("org.jsoup.nodes.LeafNode", e);
+            // The exception is expected. We verify the message to ensure it's the correct failure.
+            String expectedMessage = "org.jsoup.nodes.CDataNode cannot be cast to java.lang.String";
+            assertEquals(expectedMessage, e.getMessage());
         }
     }
 }
