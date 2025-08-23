ad6b429bb8a317046919c5d37dffec88be6d3c4f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:42:46 +0800

    Original processedAutoTestCases charsetutils_estest20.java

---
+++ charsetutils_estest20.java
@@ -0,0 +1,18 @@
+package org.apache.commons.lang3;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class CharSetUtils_ESTestTest20 extends CharSetUtils_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test19() throws Throwable {
+        String[] stringArray0 = new String[7];
+        stringArray0[0] = "Xii8KJ";
+        boolean boolean0 = CharSetUtils.containsAny("A-Za-z", stringArray0);
+        assertFalse(boolean0);
+    }
+}
