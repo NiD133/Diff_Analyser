f52a07cdb6f30e80ec8784239b69a5d4d997e5d3
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:03:53 +0800

    Original processedAutoTestCases multibackgroundinitializer_estest6.java

---
+++ multibackgroundinitializer_estest6.java
@@ -0,0 +1,32 @@
+package org.apache.commons.lang3.concurrent;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.NoSuchElementException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.lang.MockException;
+import org.junit.runner.RunWith;
+
+public class MultiBackgroundInitializer_ESTestTest6 extends MultiBackgroundInitializer_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test05() throws Throwable {
+        MultiBackgroundInitializer multiBackgroundInitializer0 = new MultiBackgroundInitializer();
+        ScheduledThreadPoolExecutor scheduledThreadPoolExecutor0 = new ScheduledThreadPoolExecutor(3230);
+        BackgroundInitializer<Object> backgroundInitializer0 = new BackgroundInitializer<Object>(scheduledThreadPoolExecutor0);
+        multiBackgroundInitializer0.addInitializer("", backgroundInitializer0);
+        multiBackgroundInitializer0.close();
+        multiBackgroundInitializer0.isInitialized();
+    }
+}
