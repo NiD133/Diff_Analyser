8fe5d49d944f2af0ee3dd86959094c70d8a1910d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:28:44 +0800

    gemini processedAutoTestCases xnode_estest16.java

--- xnode_estest16.java
+++ xnode_estest16.java
@@ -1,31 +1,35 @@
 package org.apache.ibatis.parsing;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.List;
-import java.util.Locale;
+import static org.junit.Assert.assertEquals;
+
 import java.util.Properties;
-import java.util.function.Supplier;
 import javax.imageio.metadata.IIOMetadataNode;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
-import org.w3c.dom.Document;
+import org.junit.Test;
 import org.w3c.dom.Node;
-import org.xml.sax.ext.DefaultHandler2;
 
-public class XNode_ESTestTest16 extends XNode_ESTest_scaffolding {
+/**
+ * Test suite for the XNode class, focusing on attribute retrieval methods.
+ */
+public class XNodeGetAttributeTest {
 
-    @Test(timeout = 4000)
-    public void test015() throws Throwable {
-        Properties properties0 = new Properties();
-        IIOMetadataNode iIOMetadataNode0 = new IIOMetadataNode();
-        XNode xNode0 = new XNode((XPathParser) null, iIOMetadataNode0, properties0);
-        Long long0 = new Long(2541L);
-        Long long1 = xNode0.getLongAttribute("%CjKdy/@j1\"{KuK'", long0);
-        assertEquals(2541L, (long) long1);
+    /**
+     * Verifies that getLongAttribute() returns the provided default value
+     * when the requested attribute does not exist on the XML node.
+     */
+    @Test
+    public void getLongAttribute_shouldReturnDefaultValue_whenAttributeIsMissing() {
+        // Arrange
+        // Create an XML node that has no attributes.
+        Node nodeWithoutAttributes = new IIOMetadataNode();
+        XNode xNode = new XNode(null, nodeWithoutAttributes, new Properties());
+
+        String nonExistentAttributeName = "timeout";
+        Long defaultValue = 5000L;
+
+        // Act
+        Long actualValue = xNode.getLongAttribute(nonExistentAttributeName, defaultValue);
+
+        // Assert
+        assertEquals(defaultValue, actualValue);
     }
 }
