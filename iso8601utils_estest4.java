cbc4e08b9358c5936ce8739bc71c7d93bf8b1e3e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:55:34 +0800

    Original processedAutoTestCases iso8601utils_estest4.java

---
+++ iso8601utils_estest4.java
@@ -0,0 +1,31 @@
+package com.google.gson.internal.bind.util;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.text.ParseException;
+import java.text.ParsePosition;
+import java.util.Date;
+import java.util.SimpleTimeZone;
+import java.util.TimeZone;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.util.MockDate;
+import org.junit.runner.RunWith;
+
+public class ISO8601Utils_ESTestTest4 extends ISO8601Utils_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test03() throws Throwable {
+        // Undeclared exception!
+        try {
+            ISO8601Utils.parse("H0tU&_9';1S)", (ParsePosition) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("com.google.gson.internal.bind.util.ISO8601Utils", e);
+        }
+    }
+}
