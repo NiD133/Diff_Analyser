dc4e03779bbff76e275a0f5a254d3b98b09cb678
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:06:50 +0800

    gemini processedAutoTestCases orderedproperties_estest34.java

--- orderedproperties_estest34.java
+++ orderedproperties_estest34.java
@@ -1,52 +1,30 @@
 package org.apache.commons.collections4.properties;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.Reader;
-import java.io.StringReader;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.LinkedHashSet;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Collections;
 import java.util.Map;
-import java.util.NoSuchElementException;
-import java.util.Set;
-import java.util.function.BiConsumer;
-import java.util.function.BiFunction;
-import java.util.function.Function;
-import org.apache.commons.collections4.Equator;
-import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AllPredicate;
-import org.apache.commons.collections4.functors.CloneTransformer;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.DefaultEquator;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.NOPTransformer;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NotNullPredicate;
-import org.apache.commons.collections4.functors.NullIsTruePredicate;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.SwitchTransformer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class OrderedProperties_ESTestTest34 extends OrderedProperties_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link OrderedProperties} class.
+ */
+public class OrderedPropertiesTest {
 
-    @Test(timeout = 4000)
-    public void test33() throws Throwable {
-        OrderedProperties orderedProperties0 = new OrderedProperties();
-        HashMap<Integer, Integer> hashMap0 = new HashMap<Integer, Integer>();
-        orderedProperties0.putAll(hashMap0);
-        assertTrue(orderedProperties0.isEmpty());
+    /**
+     * Tests that calling putAll() with an empty map on an empty OrderedProperties
+     * instance does not change its state.
+     */
+    @Test
+    public void putAllWithEmptyMapOnEmptyPropertiesShouldRemainEmpty() {
+        // Arrange: Create an empty OrderedProperties instance.
+        final OrderedProperties properties = new OrderedProperties();
+        final Map<Object, Object> emptyMap = Collections.emptyMap();
+
+        // Act: Call the method under test with an empty map.
+        properties.putAll(emptyMap);
+
+        // Assert: Verify that the properties object is still empty.
+        assertTrue("Calling putAll with an empty map should not add any elements", properties.isEmpty());
     }
 }
