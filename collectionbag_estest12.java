095ac171423555efa887be50ad63a8661a5e30b8
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:44:05 +0800

    Original processedAutoTestCases collectionbag_estest12.java

---
+++ collectionbag_estest12.java
@@ -0,0 +1,51 @@
+package org.apache.commons.collections4.bag;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.lang.reflect.Array;
+import java.util.Collection;
+import java.util.ConcurrentModificationException;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Stack;
+import java.util.TreeSet;
+import org.apache.commons.collections4.Bag;
+import org.apache.commons.collections4.Predicate;
+import org.apache.commons.collections4.SortedBag;
+import org.apache.commons.collections4.Transformer;
+import org.apache.commons.collections4.functors.ComparatorPredicate;
+import org.apache.commons.collections4.functors.ConstantTransformer;
+import org.apache.commons.collections4.functors.FalsePredicate;
+import org.apache.commons.collections4.functors.IdentityPredicate;
+import org.apache.commons.collections4.functors.IfTransformer;
+import org.apache.commons.collections4.functors.InvokerTransformer;
+import org.apache.commons.collections4.functors.MapTransformer;
+import org.apache.commons.collections4.functors.NullIsExceptionPredicate;
+import org.apache.commons.collections4.functors.TransformerPredicate;
+import org.apache.commons.collections4.functors.UniquePredicate;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class CollectionBag_ESTestTest12 extends CollectionBag_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test11() throws Throwable {
+        Stack<LinkedList<Integer>> stack0 = new Stack<LinkedList<Integer>>();
+        TreeBag<LinkedList<Integer>> treeBag0 = new TreeBag<LinkedList<Integer>>((Iterable<? extends LinkedList<Integer>>) stack0);
+        SynchronizedSortedBag<LinkedList<Integer>> synchronizedSortedBag0 = SynchronizedSortedBag.synchronizedSortedBag((SortedBag<LinkedList<Integer>>) treeBag0);
+        Transformer<LinkedList<Integer>, LinkedList<Integer>> transformer0 = InvokerTransformer.invokerTransformer("org.apache.commons.collections4.bag.CollectionBag");
+        CollectionBag<LinkedList<Integer>> collectionBag0 = new CollectionBag<LinkedList<Integer>>(synchronizedSortedBag0);
+        // Undeclared exception!
+        try {
+            collectionBag0.remove((Object) transformer0);
+            fail("Expecting exception: ClassCastException");
+        } catch (ClassCastException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+        }
+    }
+}
