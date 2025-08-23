de7eeb7fb55ac58b268f7346f0692e3bcff55522
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:53:07 +0800

    gemini processedAutoTestCases hashcodeandequalssafeset_estest34.java

--- hashcodeandequalssafeset_estest34.java
+++ hashcodeandequalssafeset_estest34.java
@@ -1,29 +1,23 @@
 package org.mockito.internal.util.collections;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.LinkedHashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Vector;
-import java.util.function.Predicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertFalse;
 
-public class HashCodeAndEqualsSafeSet_ESTestTest34 extends HashCodeAndEqualsSafeSet_ESTest_scaffolding {
+/**
+ * Test suite for {@link HashCodeAndEqualsSafeSet}.
+ */
+public class HashCodeAndEqualsSafeSetTest {
 
-    @Test(timeout = 4000)
-    public void test33() throws Throwable {
-        HashCodeAndEqualsSafeSet hashCodeAndEqualsSafeSet0 = new HashCodeAndEqualsSafeSet();
-        Predicate<Object> predicate0 = Predicate.isEqual((Object) hashCodeAndEqualsSafeSet0);
-        HashCodeAndEqualsMockWrapper hashCodeAndEqualsMockWrapper0 = HashCodeAndEqualsMockWrapper.of(predicate0);
-        boolean boolean0 = hashCodeAndEqualsSafeSet0.remove(hashCodeAndEqualsMockWrapper0);
-        assertFalse(boolean0);
+    @Test
+    public void shouldReturnFalseWhenRemovingNonExistentElement() {
+        // Arrange
+        HashCodeAndEqualsSafeSet safeSet = new HashCodeAndEqualsSafeSet();
+        Object nonExistentElement = new Object();
+
+        // Act
+        boolean wasRemoved = safeSet.remove(nonExistentElement);
+
+        // Assert
+        assertFalse("Removing an element that is not in the set should return false.", wasRemoved);
     }
 }
