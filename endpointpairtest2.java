cca16c84507678ec3151c78771f3f5f1179224ea
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:28:20 +0800

    Original processedManualTestCases endpointpairtest2.java

---
+++ endpointpairtest2.java
@@ -0,0 +1,56 @@
+package com.google.common.graph;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertThrows;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.testing.EqualsTester;
+import java.util.Collection;
+import java.util.Set;
+import org.jspecify.annotations.NullUnmarked;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+public class EndpointPairTestTest2 {
+
+    private static final Integer N0 = 0;
+
+    private static final Integer N1 = 1;
+
+    private static final Integer N2 = 2;
+
+    private static final Integer N3 = 3;
+
+    private static final Integer N4 = 4;
+
+    private static final String E12 = "1-2";
+
+    private static final String E12_A = "1-2a";
+
+    private static final String E21 = "2-1";
+
+    private static final String E13 = "1-3";
+
+    private static final String E44 = "4-4";
+
+    private static void containsExactlySanityCheck(Collection<?> collection, Object... varargs) {
+        assertThat(collection).hasSize(varargs.length);
+        for (Object obj : varargs) {
+            assertThat(collection).contains(obj);
+        }
+        assertThat(collection).containsExactly(varargs);
+    }
+
+    @Test
+    public void testUnorderedEndpointPair() {
+        EndpointPair<String> unordered = EndpointPair.unordered("chicken", "egg");
+        assertThat(unordered.isOrdered()).isFalse();
+        assertThat(unordered).containsExactly("chicken", "egg");
+        assertThat(ImmutableSet.of(unordered.nodeU(), unordered.nodeV())).containsExactly("chicken", "egg");
+        assertThat(unordered.adjacentNode(unordered.nodeU())).isEqualTo(unordered.nodeV());
+        assertThat(unordered.adjacentNode(unordered.nodeV())).isEqualTo(unordered.nodeU());
+        assertThat(unordered.toString()).contains("chicken");
+        assertThat(unordered.toString()).contains("egg");
+    }
+}
