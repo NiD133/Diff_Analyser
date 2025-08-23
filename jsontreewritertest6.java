e468b82bf5a8a1b41ecc08585a3705b65f453fdf
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:30:19 +0800

    Original processedManualTestCases jsontreewritertest6.java

---
+++ jsontreewritertest6.java
@@ -0,0 +1,26 @@
+package com.google.gson.internal.bind;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertThrows;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonNull;
+import com.google.gson.Strictness;
+import com.google.gson.common.MoreAsserts;
+import com.google.gson.stream.JsonWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Test;
+
+public class JsonTreeWriterTestTest6 {
+
+    @Test
+    public void testPrematureClose() throws Exception {
+        JsonTreeWriter writer = new JsonTreeWriter();
+        writer.setStrictness(Strictness.LENIENT);
+        writer.beginArray();
+        var e = assertThrows(IOException.class, () -> writer.close());
+        assertThat(e).hasMessageThat().isEqualTo("Incomplete document");
+    }
+}
