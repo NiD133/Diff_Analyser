d8e2f26e785c6e712772ed858f59b046c2524ff7
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:04:37 +0800

    Original processedAutoTestCases nodeiterator_estest2.java

---
+++ nodeiterator_estest2.java
@@ -0,0 +1,21 @@
+package org.jsoup.nodes;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.NoSuchElementException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.jsoup.parser.Parser;
+import org.junit.runner.RunWith;
+
+public class NodeIterator_ESTestTest2 extends NodeIterator_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test01() throws Throwable {
+        Document document0 = Parser.parseBodyFragment("org.jsoup.nodes.NodeIterator", "org.jsoup.nodes.NodeIterator");
+        NodeIterator<Node> nodeIterator0 = NodeIterator.from(document0);
+        boolean boolean0 = nodeIterator0.hasNext();
+        assertTrue(boolean0);
+    }
+}
