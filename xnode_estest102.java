bdbea864ac2741e63e4e19100b68a0558179932b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:28:32 +0800

    Original processedAutoTestCases xnode_estest102.java

---
+++ xnode_estest102.java
@@ -0,0 +1,33 @@
+package org.apache.ibatis.parsing;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.List;
+import java.util.Locale;
+import java.util.Properties;
+import java.util.function.Supplier;
+import javax.imageio.metadata.IIOMetadataNode;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.xml.sax.ext.DefaultHandler2;
+
+public class XNode_ESTestTest102 extends XNode_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test101() throws Throwable {
+        Properties properties0 = new Properties();
+        IIOMetadataNode iIOMetadataNode0 = new IIOMetadataNode();
+        IIOMetadataNode iIOMetadataNode1 = new IIOMetadataNode();
+        iIOMetadataNode0.appendChild(iIOMetadataNode1);
+        XPathParser xPathParser0 = new XPathParser((Document) null, false);
+        XNode xNode0 = new XNode(xPathParser0, iIOMetadataNode1, properties0);
+        String string0 = xNode0.getValueBasedIdentifier();
+        assertEquals("null_null", string0);
+    }
+}
