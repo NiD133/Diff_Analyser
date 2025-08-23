1cccd0e0401960e594642acd1e6922939dd20e6a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:18:47 +0800

    gemini processedAutoTestCases stringutil_estest24.java

--- stringutil_estest24.java
+++ stringutil_estest24.java
@@ -1,36 +1,18 @@
 package org.jsoup.internal;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Collection;
-import java.util.ConcurrentModificationException;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.ListIterator;
-import java.util.stream.Collector;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.evosuite.runtime.mock.java.net.MockURL;
-import org.junit.runner.RunWith;
 
+/**
+ * Tests for the {@link StringUtil#releaseBuilderVoid(StringBuilder)} method.
+ */
 public class StringUtil_ESTestTest24 extends StringUtil_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test23() throws Throwable {
-        // Undeclared exception!
-        try {
-            StringUtil.releaseBuilderVoid((StringBuilder) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.jsoup.internal.StringUtil", e);
-        }
+    /**
+     * Verifies that passing a null StringBuilder to releaseBuilderVoid
+     * results in a NullPointerException, as expected.
+     */
+    @Test(expected = NullPointerException.class)
+    public void releaseBuilderVoidShouldThrowNullPointerExceptionWhenBuilderIsNull() {
+        StringUtil.releaseBuilderVoid(null);
     }
 }
