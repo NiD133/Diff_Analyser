f80903f71670b74c3cada5072a5563c0bc9ce99e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:28:42 +0800

    gemini processedManualTestCases fieldnamingpolicytest2.java

--- fieldnamingpolicytest2.java
+++ fieldnamingpolicytest2.java
@@ -1,21 +1,57 @@
 package com.google.gson;
 
 import static com.google.common.truth.Truth.assertThat;
-import static com.google.common.truth.Truth.assertWithMessage;
-import java.lang.reflect.Field;
-import java.util.Locale;
+
+import java.util.Arrays;
+import java.util.Collection;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
-public class FieldNamingPolicyTestTest2 {
+/**
+ * Tests for the helper method {@link FieldNamingPolicy#upperCaseFirstLetter(String)}.
+ */
+@RunWith(Parameterized.class)
+public class FieldNamingPolicyTest {
 
-    @Test
-    public void testUpperCaseFirstLetter() {
-        // Map from original -> expected
-        String[][] argumentPairs = { { "a", "A" }, { "ab", "Ab" }, { "AB", "AB" }, { "_a", "_A" }, { "_ab", "_Ab" }, { "__", "__" }, { "_1", "_1" }, // Not a letter, but has uppercase variant (should not be uppercased)
-        // See https://github.com/google/gson/issues/1965
-        { "\u2170", "\u2170" }, { "_\u2170", "_\u2170" }, { "\u2170a", "\u2170A" } };
-        for (String[] pair : argumentPairs) {
-            assertThat(FieldNamingPolicy.upperCaseFirstLetter(pair[0])).isEqualTo(pair[1]);
-        }
-    }
+  @Parameters(name = "{index}: upperCaseFirstLetter(\"{0}\") => \"{1}\"")
+  public static Collection<Object[]> data() {
+    return Arrays.asList(
+        new Object[][] {
+          // Standard cases
+          {"a", "A"},
+          {"ab", "Ab"},
+          {"AB", "AB"},
+
+          // Cases with leading underscores
+          {"_a", "_A"},
+          {"_ab", "_Ab"},
+          {"__", "__"},
+
+          // Cases with non-letter characters
+          {"_1", "_1"}, // Digits should not be uppercased
+
+          // Test cases for characters which have an uppercase variant but are not
+          // considered letters by Character.isLetter().
+          // See https://github.com/google/gson/issues/1965
+          {"\u2170", "\u2170"}, // Small Roman Numeral One
+          {"_\u2170", "_\u2170"},
+          {"\u2170a", "\u2170A"} // First letter 'a' should be uppercased
+        });
+  }
+
+  private final String input;
+  private final String expectedOutput;
+
+  public FieldNamingPolicyTest(String input, String expectedOutput) {
+    this.input = input;
+    this.expectedOutput = expectedOutput;
+  }
+
+  @Test
+  public void testUpperCaseFirstLetter() {
+    String actual = FieldNamingPolicy.upperCaseFirstLetter(input);
+    assertThat(actual).isEqualTo(expectedOutput);
+  }
 }
