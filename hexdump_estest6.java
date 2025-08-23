445ffb431f14a84b633c6dee20a4da429bc6115c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:53:18 +0800

    Original processedAutoTestCases hexdump_estest6.java

---
+++ hexdump_estest6.java
@@ -0,0 +1,42 @@
+package org.apache.commons.io;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PipedOutputStream;
+import java.io.PipedWriter;
+import java.io.StringWriter;
+import java.nio.BufferOverflowException;
+import java.nio.CharBuffer;
+import java.nio.ReadOnlyBufferException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.evosuite.runtime.mock.java.io.MockFileOutputStream;
+import org.evosuite.runtime.mock.java.io.MockFileWriter;
+import org.evosuite.runtime.mock.java.io.MockPrintStream;
+import org.junit.runner.RunWith;
+
+public class HexDump_ESTestTest6 extends HexDump_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test05() throws Throwable {
+        byte[] byteArray0 = new byte[12];
+        CharBuffer charBuffer0 = CharBuffer.wrap((CharSequence) "\n");
+        // Undeclared exception!
+        try {
+            HexDump.dump(byteArray0, (long) 5, (Appendable) charBuffer0, 5, 6);
+            fail("Expecting exception: ReadOnlyBufferException");
+        } catch (ReadOnlyBufferException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("java.nio.CharBuffer", e);
+        }
+    }
+}
