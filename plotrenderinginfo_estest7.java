0d0054ec4da2c59acd1ef2f6534d70969a1cd707
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:09:05 +0800

    gemini processedAutoTestCases plotrenderinginfo_estest7.java

--- plotrenderinginfo_estest7.java
+++ plotrenderinginfo_estest7.java
@@ -1,27 +1,27 @@
 package org.jfree.chart.plot;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.awt.Dimension;
-import java.awt.Point;
-import java.awt.Rectangle;
-import java.awt.geom.Line2D;
-import java.awt.geom.Point2D;
-import java.awt.geom.Rectangle2D;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
 import org.jfree.chart.ChartRenderingInfo;
-import org.jfree.chart.entity.EntityCollection;
-import org.jfree.chart.entity.StandardEntityCollection;
-import org.junit.runner.RunWith;
+import org.junit.Test;
+import static org.junit.Assert.assertNull;
 
-public class PlotRenderingInfo_ESTestTest7 extends PlotRenderingInfo_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link PlotRenderingInfo} class.
+ */
+public class PlotRenderingInfoTest {
 
-    @Test(timeout = 4000)
-    public void test06() throws Throwable {
-        PlotRenderingInfo plotRenderingInfo0 = new PlotRenderingInfo((ChartRenderingInfo) null);
-        ChartRenderingInfo chartRenderingInfo0 = plotRenderingInfo0.getOwner();
-        assertNull(chartRenderingInfo0);
+    /**
+     * Verifies that getOwner() returns null when the PlotRenderingInfo
+     * is constructed with a null owner.
+     */
+    @Test
+    public void getOwnerShouldReturnNullWhenConstructedWithNullOwner() {
+        // Arrange: Create a PlotRenderingInfo instance with a null owner.
+        PlotRenderingInfo plotRenderingInfo = new PlotRenderingInfo(null);
+
+        // Act: Retrieve the owner from the instance.
+        ChartRenderingInfo owner = plotRenderingInfo.getOwner();
+
+        // Assert: The retrieved owner should be null.
+        assertNull("The owner should be null as it was set to null in the constructor.", owner);
     }
 }
