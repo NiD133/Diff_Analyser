89a66c91af1b4e63bc550ed38b5d7d28dd40cfe3
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:56:22 +0800

    gemini processedAutoTestCases jsonarray_estest8.java

--- jsonarray_estest8.java
+++ jsonarray_estest8.java
@@ -1,24 +1,24 @@
 package com.google.gson;
 
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.util.Iterator;
-import java.util.List;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class JsonArray_ESTestTest8 extends JsonArray_ESTest_scaffolding {
+/**
+ * Tests for {@link JsonArray}.
+ */
+public class JsonArrayTest {
 
-    @Test(timeout = 4000)
-    public void test07() throws Throwable {
-        JsonArray jsonArray0 = new JsonArray();
-        Character character0 = Character.valueOf('6');
-        jsonArray0.add(character0);
-        short short0 = jsonArray0.getAsShort();
-        assertEquals((short) 6, short0);
+    @Test
+    public void getAsShort_whenArrayContainsSingleNumericCharacter_returnsCorrespondingShort() {
+        // Arrange
+        JsonArray jsonArray = new JsonArray();
+        jsonArray.add('6');
+
+        // Act
+        short actualShort = jsonArray.getAsShort();
+
+        // Assert
+        short expectedShort = 6;
+        assertEquals(expectedShort, actualShort);
     }
 }
