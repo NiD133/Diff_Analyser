cefab5d2faee06f60508e22ff60d3c74d864bb3c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:20:17 +0800

    gemini processedAutoTestCases tagset_estest25.java

--- tagset_estest25.java
+++ tagset_estest25.java
@@ -1,22 +1,23 @@
 package org.jsoup.parser;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.function.Consumer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertFalse;
 
-public class TagSet_ESTestTest25 extends TagSet_ESTest_scaffolding {
+/**
+ * Tests for the {@link TagSet} class, focusing on the equals() method contract.
+ */
+public class TagSetTest {
 
-    @Test(timeout = 4000)
-    public void test24() throws Throwable {
-        TagSet tagSet0 = new TagSet();
-        Object object0 = new Object();
-        boolean boolean0 = tagSet0.equals(object0);
-        assertFalse(boolean0);
+    @Test
+    public void equals_shouldReturnFalse_whenComparedWithDifferentObjectType() {
+        // Arrange: Create a TagSet instance and an instance of a different class (Object).
+        TagSet tagSet = new TagSet();
+        Object nonTagSetObject = new Object();
+
+        // Act: Compare the TagSet instance with the other object.
+        boolean areEqual = tagSet.equals(nonTagSetObject);
+
+        // Assert: The result should be false, as they are of incompatible types.
+        assertFalse("A TagSet instance should not be equal to an instance of a different class.", areEqual);
     }
 }
