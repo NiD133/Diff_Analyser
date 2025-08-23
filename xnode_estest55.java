b8a183711e43652ffe00c4a4e8dd22c191fb2a6c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:29:02 +0800

    gemini processedAutoTestCases xnode_estest55.java

--- xnode_estest55.java
+++ xnode_estest55.java
@@ -1,38 +1,34 @@
 package org.apache.ibatis.parsing;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.List;
 import java.util.Locale;
 import java.util.Properties;
-import java.util.function.Supplier;
 import javax.imageio.metadata.IIOMetadataNode;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
-import org.w3c.dom.Document;
+import org.junit.Test;
 import org.w3c.dom.Node;
-import org.xml.sax.ext.DefaultHandler2;
 
-public class XNode_ESTestTest55 extends XNode_ESTest_scaffolding {
+/**
+ * Test suite for the {@link XNode} class.
+ */
+public class XNodeTest {
 
-    @Test(timeout = 4000)
-    public void test054() throws Throwable {
-        IIOMetadataNode iIOMetadataNode0 = new IIOMetadataNode();
-        Properties properties0 = new Properties();
-        XNode xNode0 = new XNode((XPathParser) null, iIOMetadataNode0, properties0);
-        Class<Locale.Category> class0 = Locale.Category.class;
-        // Undeclared exception!
-        try {
-            xNode0.getEnumAttribute(class0, (String) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-        }
+    /**
+     * Verifies that calling getEnumAttribute with a null attribute name
+     * results in a NullPointerException.
+     */
+    @Test(expected = NullPointerException.class)
+    public void getEnumAttributeShouldThrowNPEWhenNameIsNull() {
+        // Arrange: Create an XNode instance. An empty node and properties are sufficient
+        // for this test, and the XPathParser can be null as it's not used by the
+        // method under test.
+        Node node = new IIOMetadataNode();
+        Properties variables = new Properties();
+        XNode xNode = new XNode(null, node, variables);
+        Class<Locale.Category> anyEnumType = Locale.Category.class;
+
+        // Act: Attempt to get an enum attribute using a null name.
+        xNode.getEnumAttribute(anyEnumType, null);
+
+        // Assert: The test expects a NullPointerException, which is handled
+        // by the @Test(expected) annotation. No further assertion is needed.
     }
 }
