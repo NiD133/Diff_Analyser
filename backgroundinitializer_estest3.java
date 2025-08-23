d8ac4ee7e726d86c7e833b2fb109147fc8bea7a7
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:38:33 +0800

    Original processedAutoTestCases backgroundinitializer_estest3.java

---
+++ backgroundinitializer_estest3.java
@@ -0,0 +1,33 @@
+package org.apache.commons.lang3.concurrent;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.concurrent.Delayed;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.lang.MockException;
+import org.evosuite.runtime.mock.java.lang.MockThrowable;
+import org.evosuite.runtime.testdata.FileSystemHandling;
+import org.junit.runner.RunWith;
+
+public class BackgroundInitializer_ESTestTest3 extends BackgroundInitializer_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test02() throws Throwable {
+        BackgroundInitializer<Delayed> backgroundInitializer0 = new BackgroundInitializer<Delayed>();
+        boolean boolean0 = backgroundInitializer0.start();
+        boolean boolean1 = backgroundInitializer0.isInitialized();
+        assertFalse(boolean1 == boolean0);
+        assertFalse(boolean1);
+    }
+}
