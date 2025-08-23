fa5346616db2e738cc9c636ecd5ac6e53ce572b2
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:09:51 +0800

    Original processedAutoTestCases quarterdateformat_estest13.java

---
+++ quarterdateformat_estest13.java
@@ -0,0 +1,34 @@
+package org.jfree.chart.axis;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.StringWriter;
+import java.text.FieldPosition;
+import java.text.Format;
+import java.text.NumberFormat;
+import java.text.ParsePosition;
+import java.util.Date;
+import java.util.SimpleTimeZone;
+import java.util.TimeZone;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.util.MockDate;
+import org.junit.runner.RunWith;
+
+public class QuarterDateFormat_ESTestTest13 extends QuarterDateFormat_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test12() throws Throwable {
+        TimeZone timeZone0 = TimeZone.getDefault();
+        QuarterDateFormat quarterDateFormat0 = new QuarterDateFormat(timeZone0);
+        MockDate mockDate0 = new MockDate((-2355), 0, 2123, 584, 0, (-797));
+        StringWriter stringWriter0 = new StringWriter();
+        StringBuffer stringBuffer0 = stringWriter0.getBuffer();
+        quarterDateFormat0.format((Date) mockDate0, stringBuffer0, (FieldPosition) null);
+        assertEquals(5, stringBuffer0.length());
+        assertEquals("451 4", stringWriter0.toString());
+    }
+}
