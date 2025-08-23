d7cacd0d7fb6a4a433781a1419aca6d9ee41d9b6
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:00:06 +0800

    gemini processedAutoTestCases leafnode_estest29.java

--- leafnode_estest29.java
+++ leafnode_estest29.java
@@ -1,31 +1,43 @@
 package org.jsoup.nodes;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.List;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.jsoup.internal.QuietAppendable;
-import org.jsoup.parser.Parser;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.fail;
+import static org.junit.Assert.assertTrue;
 
-public class LeafNode_ESTestTest29 extends LeafNode_ESTest_scaffolding {
+/**
+ * Test suite for the abstract LeafNode class, using CDataNode as a concrete implementation.
+ */
+public class LeafNodeTest {
 
-    @Test(timeout = 4000)
-    public void test28() throws Throwable {
-        CDataNode cDataNode0 = new CDataNode(">eaHxh");
-        Object object0 = new Object();
-        cDataNode0.value = object0;
-        // Undeclared exception!
+    /**
+     * Tests that calling attr() throws a ClassCastException if the node's internal
+     * 'value' field has been corrupted to hold a non-String type.
+     */
+    @Test
+    public void attrThrowsClassCastExceptionWhenInternalValueIsInvalid() {
+        // Arrange: Use CDataNode as a concrete instance of the abstract LeafNode.
+        CDataNode node = new CDataNode("some text");
+
+        // This is a white-box test. We intentionally corrupt the internal state
+        // of the LeafNode by directly setting its 'value' field to a plain Object.
+        // The 'value' field is expected to be either a String or an Attributes map.
+        Object invalidValue = new Object();
+        node.value = invalidValue;
+
+        // Act & Assert
         try {
-            cDataNode0.attr(">eaHxh", ">eaHxh");
-            fail("Expecting exception: ClassCastException");
+            // Attempting to set an attribute should trigger an internal cast of the
+            // 'value' field to a String, which will fail.
+            node.attr("key", "value");
+            fail("Expected a ClassCastException to be thrown due to the invalid internal state.");
         } catch (ClassCastException e) {
-            //
-            // java.lang.Object cannot be cast to java.lang.String
-            //
-            verifyException("org.jsoup.nodes.LeafNode", e);
+            // Verify that the exception is the one we expect.
+            // This confirms that the internal type safety is the cause of the failure.
+            String expectedMessage = "java.lang.Object cannot be cast to java.lang.String";
+            assertTrue(
+                "The exception message should indicate a failed cast to String.",
+                e.getMessage().contains(expectedMessage)
+            );
         }
     }
 }
