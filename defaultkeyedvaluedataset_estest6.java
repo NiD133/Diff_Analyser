c48be693148e6ef346a3f1438f7371db718e18d7
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:47:35 +0800

    Original processedAutoTestCases defaultkeyedvaluedataset_estest6.java

---
+++ defaultkeyedvaluedataset_estest6.java
@@ -0,0 +1,32 @@
+package org.jfree.data.general;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.math.BigInteger;
+import javax.swing.JLayeredPane;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.util.MockDate;
+import org.jfree.chart.date.SerialDate;
+import org.jfree.chart.date.SpreadsheetDate;
+import org.jfree.data.statistics.SimpleHistogramBin;
+import org.jfree.data.xy.OHLCDataItem;
+import org.junit.runner.RunWith;
+
+public class DefaultKeyedValueDataset_ESTestTest6 extends DefaultKeyedValueDataset_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test05() throws Throwable {
+        DefaultKeyedValueDataset defaultKeyedValueDataset0 = null;
+        try {
+            defaultKeyedValueDataset0 = new DefaultKeyedValueDataset((Comparable) null, (Number) null);
+            fail("Expecting exception: IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //
+            // Null 'key' argument.
+            //
+            verifyException("org.jfree.chart.internal.Args", e);
+        }
+    }
+}
