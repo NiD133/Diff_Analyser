2589ce59c0d5ca053dff77850ec96b2ed44fee1f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:04:16 +0800

    gemini processedAutoTestCases multireadonlyseekablebytechannel_estest33.java

--- multireadonlyseekablebytechannel_estest33.java
+++ multireadonlyseekablebytechannel_estest33.java
@@ -1,43 +1,35 @@
 package org.apache.commons.compress.utils;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
+import static org.junit.Assert.assertThrows;
+
 import java.io.File;
 import java.io.IOException;
-import java.nio.ByteBuffer;
 import java.nio.channels.ClosedChannelException;
-import java.nio.channels.FileChannel;
-import java.nio.channels.NonWritableChannelException;
 import java.nio.channels.SeekableByteChannel;
-import java.nio.file.NoSuchFileException;
-import java.nio.file.OpenOption;
-import java.nio.file.Path;
-import java.util.LinkedList;
-import java.util.List;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.io.MockFile;
-import org.junit.runner.RunWith;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 public class MultiReadOnlySeekableByteChannel_ESTestTest33 extends MultiReadOnlySeekableByteChannel_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test32() throws Throwable {
-        File[] fileArray0 = new File[2];
-        MockFile mockFile0 = new MockFile("");
-        fileArray0[0] = (File) mockFile0;
-        fileArray0[1] = (File) mockFile0;
-        SeekableByteChannel seekableByteChannel0 = MultiReadOnlySeekableByteChannel.forFiles(fileArray0);
-        ((MultiReadOnlySeekableByteChannel) seekableByteChannel0).close();
-        try {
-            seekableByteChannel0.position(0L);
-            fail("Expecting exception: ClosedChannelException");
-        } catch (ClosedChannelException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.commons.compress.utils.MultiReadOnlySeekableByteChannel", e);
-        }
+    @Rule
+    public TemporaryFolder tempFolder = new TemporaryFolder();
+
+    /**
+     * Verifies that attempting to change the position of a closed
+     * MultiReadOnlySeekableByteChannel throws a ClosedChannelException.
+     */
+    @Test
+    public void positionShouldThrowExceptionWhenChannelIsClosed() throws IOException {
+        // Arrange: Create a multi-channel from two temporary files.
+        File file1 = tempFolder.newFile("test1.bin");
+        File file2 = tempFolder.newFile("test2.bin");
+        final SeekableByteChannel channel = MultiReadOnlySeekableByteChannel.forFiles(file1, file2);
+
+        // Act: Close the channel before attempting to use it.
+        channel.close();
+
+        // Assert: Expect a ClosedChannelException when trying to set the position.
+        assertThrows(ClosedChannelException.class, () -> channel.position(0L));
     }
 }
