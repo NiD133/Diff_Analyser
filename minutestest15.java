b39491a0f7398e72ff2944af4daf20c393fb8542
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:31:09 +0800

    Original processedManualTestCases minutestest15.java

---
+++ minutestest15.java
@@ -0,0 +1,37 @@
+package org.joda.time;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class MinutesTestTest15 extends TestCase {
+
+    // (before the late 90's they were all over the place)
+    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
+
+    public static TestSuite suite() {
+        return new TestSuite(TestMinutes.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+    }
+
+    //-----------------------------------------------------------------------
+    public void testToStandardWeeks() {
+        Minutes test = Minutes.minutes(60 * 24 * 7 * 2);
+        Weeks expected = Weeks.weeks(2);
+        assertEquals(expected, test.toStandardWeeks());
+    }
+}
