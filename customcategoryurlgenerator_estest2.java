e6a27903de6a0a1f6f688b59b57b0087c2904285
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:46:23 +0800

    gemini processedAutoTestCases customcategoryurlgenerator_estest2.java

--- customcategoryurlgenerator_estest2.java
+++ customcategoryurlgenerator_estest2.java
@@ -1,29 +1,36 @@
 package org.jfree.chart.urls;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.ArrayList;
+import static org.junit.Assert.assertNotEquals;
+
 import java.util.List;
-import java.util.Stack;
-import java.util.Vector;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.jfree.data.category.DefaultCategoryDataset;
-import org.jfree.data.general.DefaultKeyedValues2DDataset;
-import org.junit.runner.RunWith;
 
-public class CustomCategoryURLGenerator_ESTestTest2 extends CustomCategoryURLGenerator_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link CustomCategoryURLGenerator} class.
+ */
+public class CustomCategoryURLGeneratorTest {
 
-    @Test(timeout = 4000)
-    public void test01() throws Throwable {
-        CustomCategoryURLGenerator customCategoryURLGenerator0 = new CustomCategoryURLGenerator();
-        List<String> list0 = List.of("");
-        customCategoryURLGenerator0.addURLSeries(list0);
-        CustomCategoryURLGenerator customCategoryURLGenerator1 = new CustomCategoryURLGenerator();
-        customCategoryURLGenerator1.addURLSeries((List<String>) null);
-        boolean boolean0 = customCategoryURLGenerator0.equals(customCategoryURLGenerator1);
-        assertFalse(boolean0);
-        assertFalse(customCategoryURLGenerator1.equals((Object) customCategoryURLGenerator0));
+    /**
+     * Verifies that the equals() method correctly identifies two generators
+     * as unequal when they have been configured with different URL series.
+     */
+    @Test
+    public void equals_shouldReturnFalse_whenUrlSeriesAreDifferent() {
+        // Arrange: Create two generators with different URL series.
+        // The first generator has a list containing one empty URL.
+        CustomCategoryURLGenerator generatorWithUrlList = new CustomCategoryURLGenerator();
+        List<String> urlList = List.of("");
+        generatorWithUrlList.addURLSeries(urlList);
+
+        // The second generator has a null list added as its series.
+        CustomCategoryURLGenerator generatorWithNullList = new CustomCategoryURLGenerator();
+        generatorWithNullList.addURLSeries(null);
+
+        // Act & Assert: The generators should not be considered equal.
+        // We also test for symmetry, which is a requirement of the equals() contract.
+        assertNotEquals("Generators with different URL series should not be equal",
+                generatorWithUrlList, generatorWithNullList);
+        assertNotEquals("Symmetry check: Generators should still not be equal",
+                generatorWithNullList, generatorWithUrlList);
     }
 }
