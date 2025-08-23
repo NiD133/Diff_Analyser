de1b602ac06793bd1aa9dc62d5eb2db3c4b5a265
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:26:23 +0800

    gemini processedManualTestCases caseformattest24.java

--- caseformattest24.java
+++ caseformattest24.java
@@ -1,24 +1,23 @@
 package com.google.common.base;
 
-import static com.google.common.base.CaseFormat.LOWER_CAMEL;
-import static com.google.common.base.CaseFormat.LOWER_HYPHEN;
 import static com.google.common.base.CaseFormat.LOWER_UNDERSCORE;
-import static com.google.common.base.CaseFormat.UPPER_CAMEL;
 import static com.google.common.base.CaseFormat.UPPER_UNDERSCORE;
 import static com.google.common.truth.Truth.assertThat;
-import static com.google.common.truth.Truth.assertWithMessage;
-import com.google.common.annotations.GwtCompatible;
-import com.google.common.annotations.GwtIncompatible;
-import com.google.common.annotations.J2ktIncompatible;
-import com.google.common.testing.NullPointerTester;
-import com.google.common.testing.SerializableTester;
+
 import junit.framework.TestCase;
-import org.jspecify.annotations.NullUnmarked;
 
-public class CaseFormatTestTest24 extends TestCase {
+/**
+ * Tests for {@link CaseFormat}.
+ */
+public class CaseFormatTest extends TestCase {
 
-    public void testUpperUnderscoreToLowerUnderscore() {
-        assertThat(UPPER_UNDERSCORE.to(LOWER_UNDERSCORE, "FOO")).isEqualTo("foo");
-        assertThat(UPPER_UNDERSCORE.to(LOWER_UNDERSCORE, "FOO_BAR")).isEqualTo("foo_bar");
-    }
+  public void testTo_fromUpperUnderscoreToLowerUnderscore_singleWord() {
+    String converted = UPPER_UNDERSCORE.to(LOWER_UNDERSCORE, "FOO");
+    assertThat(converted).isEqualTo("foo");
+  }
+
+  public void testTo_fromUpperUnderscoreToLowerUnderscore_multipleWords() {
+    String converted = UPPER_UNDERSCORE.to(LOWER_UNDERSCORE, "FOO_BAR");
+    assertThat(converted).isEqualTo("foo_bar");
+  }
 }
