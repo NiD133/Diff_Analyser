e5819f97b9dc90263137b3c07d0ff888055ef03e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:21:21 +0800

    Original processedAutoTestCases texthelpappendable_estest91.java

---
+++ texthelpappendable_estest91.java
@@ -0,0 +1,47 @@
+package org.apache.commons.cli.help;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.IOException;
+import java.io.PipedWriter;
+import java.io.StringWriter;
+import java.nio.BufferOverflowException;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.ReadOnlyBufferException;
+import java.nio.charset.Charset;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.SortedSet;
+import java.util.Stack;
+import java.util.TreeSet;
+import java.util.Vector;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class TextHelpAppendable_ESTestTest91 extends TextHelpAppendable_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test90() throws Throwable {
+        byte[] byteArray0 = new byte[3];
+        byteArray0[1] = (byte) 11;
+        ByteBuffer byteBuffer0 = ByteBuffer.wrap(byteArray0);
+        Charset charset0 = Charset.defaultCharset();
+        CharBuffer charBuffer0 = charset0.decode(byteBuffer0);
+        int int0 = TextHelpAppendable.indexOfWrap(charBuffer0, 74, 1);
+        assertEquals(1, int0);
+    }
+}
