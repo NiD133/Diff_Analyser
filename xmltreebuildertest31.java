69aecda8f25ecec4ab11d4d05bfeda98d53d253c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:36:13 +0800

    gemini processedManualTestCases xmltreebuildertest31.java

--- xmltreebuildertest31.java
+++ xmltreebuildertest31.java
@@ -1,40 +1,47 @@
 package org.jsoup.parser;
 
 import org.jsoup.Jsoup;
-import org.jsoup.TextUtil;
-import org.jsoup.nodes.*;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Tag;
 import org.jsoup.select.Elements;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URISyntaxException;
-import java.nio.charset.StandardCharsets;
-import java.util.List;
-import static org.jsoup.nodes.Document.OutputSettings.Syntax;
-import static org.jsoup.parser.Parser.NamespaceHtml;
-import static org.jsoup.parser.Parser.NamespaceXml;
-import static org.junit.jupiter.api.Assertions.*;
 
-public class XmlTreeBuilderTestTest31 {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
-    private static void assertXmlNamespace(Element el) {
-        assertEquals(NamespaceXml, el.tag().namespace(), String.format("Element %s not in XML namespace", el.tagName()));
-    }
+/**
+ * Tests for the {@link XmlTreeBuilder}.
+ * This test focuses on the caching and reuse of Tag objects.
+ */
+public class XmlTreeBuilderTest {
 
     @Test
-    void customTagsAreFlyweights() {
+    @DisplayName("XML parser should reuse Tag objects for identical tag names (Flyweight pattern)")
+    void xmlParserReusesTagInstancesForSameTagName() {
+        // Arrange: Create an XML string with duplicate tags, differing only by case.
         String xml = "<foo>Foo</foo><foo>Foo</foo><FOO>FOO</FOO><FOO>FOO</FOO>";
+
+        // Act: Parse the XML and retrieve the Tag object for each element.
         Document doc = Jsoup.parse(xml, Parser.xmlParser());
-        Elements els = doc.children();
-        Tag t1 = els.get(0).tag();
-        Tag t2 = els.get(1).tag();
-        Tag t3 = els.get(2).tag();
-        Tag t4 = els.get(3).tag();
-        assertEquals("foo", t1.getName());
-        assertEquals("FOO", t3.getName());
-        assertSame(t1, t2);
-        assertSame(t3, t4);
+        Elements elements = doc.children();
+
+        Tag firstFooTag = elements.get(0).tag();
+        Tag secondFooTag = elements.get(1).tag();
+        Tag firstUpperFooTag = elements.get(2).tag();
+        Tag secondUpperFooTag = elements.get(3).tag();
+
+        // Assert: Verify that for each distinct tag name, the same Tag object instance is used.
+        // This confirms the Flyweight pattern is being used to conserve memory.
+
+        // First, confirm we have the right tags and that they are case-sensitive.
+        assertEquals("foo", firstFooTag.getName());
+        assertEquals("FOO", firstUpperFooTag.getName());
+
+        // Now, assert that identical tags point to the same object instance.
+        assertSame(firstFooTag, secondFooTag,
+            "Tag instances for lowercase 'foo' should be the same object.");
+        assertSame(firstUpperFooTag, secondUpperFooTag,
+            "Tag instances for uppercase 'FOO' should be the same object.");
     }
 }
