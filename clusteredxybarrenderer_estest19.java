998b1b726f3269468de4e1536a61b8e4598be0f9
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:43:41 +0800

    gemini processedAutoTestCases clusteredxybarrenderer_estest19.java

--- clusteredxybarrenderer_estest19.java
+++ clusteredxybarrenderer_estest19.java
@@ -1,45 +1,23 @@
 package org.jfree.chart.renderer.xy;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.awt.Graphics2D;
-import java.awt.geom.Rectangle2D;
-import java.text.DateFormatSymbols;
-import java.util.Date;
-import java.util.Locale;
-import javax.swing.JLayeredPane;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.text.MockSimpleDateFormat;
-import org.jfree.chart.ChartRenderingInfo;
-import org.jfree.chart.axis.CyclicNumberAxis;
-import org.jfree.chart.plot.CategoryCrosshairState;
-import org.jfree.chart.plot.CombinedRangeXYPlot;
-import org.jfree.chart.plot.PlotRenderingInfo;
-import org.jfree.chart.util.DirectionalGradientPaintTransformer;
-import org.jfree.data.Range;
-import org.jfree.data.statistics.DefaultBoxAndWhiskerXYDataset;
-import org.jfree.data.statistics.SimpleHistogramBin;
-import org.jfree.data.statistics.SimpleHistogramDataset;
-import org.jfree.data.time.TimeSeriesDataItem;
-import org.jfree.data.xy.CategoryTableXYDataset;
-import org.jfree.data.xy.DefaultOHLCDataset;
-import org.jfree.data.xy.DefaultWindDataset;
-import org.jfree.data.xy.DefaultXYZDataset;
-import org.jfree.data.xy.IntervalXYDataset;
-import org.jfree.data.xy.OHLCDataItem;
-import org.jfree.data.xy.XYDataset;
-import org.jfree.data.xy.XYSeries;
-import org.jfree.data.xy.XYSeriesCollection;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertTrue;
 
+/**
+ * This test class focuses on the behavior of the equals() method in ClusteredXYBarRenderer.
+ */
 public class ClusteredXYBarRenderer_ESTestTest19 extends ClusteredXYBarRenderer_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test18() throws Throwable {
-        ClusteredXYBarRenderer clusteredXYBarRenderer0 = new ClusteredXYBarRenderer();
-        boolean boolean0 = clusteredXYBarRenderer0.equals(clusteredXYBarRenderer0);
-        assertTrue(boolean0);
+    /**
+     * Verifies the reflexive property of the equals() method.
+     * An instance of ClusteredXYBarRenderer should always be equal to itself.
+     */
+    @Test
+    public void equals_onSameInstance_shouldReturnTrue() {
+        // Arrange
+        ClusteredXYBarRenderer renderer = new ClusteredXYBarRenderer();
+
+        // Act & Assert: An object must be equal to itself.
+        assertTrue("An object should be equal to itself", renderer.equals(renderer));
     }
 }
