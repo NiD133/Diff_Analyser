10e9fe3906c26c05fba9d5eef09c93d1cf337004
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:28:53 +0800

    gemini processedAutoTestCases xnode_estest35.java

--- xnode_estest35.java
+++ xnode_estest35.java
@@ -1,32 +1,45 @@
 package org.apache.ibatis.parsing;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.List;
-import java.util.Locale;
+import static org.junit.Assert.assertEquals;
+
 import java.util.Properties;
-import java.util.function.Supplier;
 import javax.imageio.metadata.IIOMetadataNode;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
-import org.xml.sax.ext.DefaultHandler2;
 
-public class XNode_ESTestTest35 extends XNode_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link XNode} class, focusing on attribute parsing.
+ */
+public class XNodeTest {
 
-    @Test(timeout = 4000)
-    public void test034() throws Throwable {
-        Properties properties0 = new Properties();
-        IIOMetadataNode iIOMetadataNode0 = new IIOMetadataNode("M>/QTFx#Et:2opz");
-        XPathParser xPathParser0 = new XPathParser((Document) null, true);
-        XNode xNode0 = new XNode(xPathParser0, iIOMetadataNode0, properties0);
-        Double double0 = new Double(0.0);
-        Double double1 = xNode0.getDoubleAttribute("<M>/QTFx#Et:2opz />\n", double0);
-        assertEquals(0.0, (double) double1, 0.01);
+    /**
+     * Verifies that getDoubleAttribute() returns the provided default value
+     * when the requested attribute does not exist on the XML node.
+     */
+    @Test
+    public void getDoubleAttributeShouldReturnDefaultValueWhenAttributeIsMissing() {
+        // Arrange
+        // 1. Create a DOM node that has a name but no attributes.
+        //    IIOMetadataNode is a standard implementation of org.w3c.dom.Node.
+        Node nodeWithoutAttributes = new IIOMetadataNode("root");
+
+        // 2. Instantiate XNode. The XPathParser and variables are required for the
+        //    constructor but are not central to this test's logic.
+        XPathParser parser = new XPathParser((Document) null, true);
+        Properties variables = new Properties();
+        XNode xNode = new XNode(parser, nodeWithoutAttributes, variables);
+
+        // 3. Define a non-existent attribute name and a default value.
+        String nonExistentAttributeName = "age";
+        Double defaultValue = 30.0;
+
+        // Act
+        // Attempt to get the double value of an attribute that isn't there.
+        Double actualValue = xNode.getDoubleAttribute(nonExistentAttributeName, defaultValue);
+
+        // Assert
+        // The method should return the default value.
+        assertEquals(defaultValue, actualValue, 0.0);
     }
 }
