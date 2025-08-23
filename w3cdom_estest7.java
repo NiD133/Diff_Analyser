9012a11110c8a3a03de705bce02e618b5d7f2fc6
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:27:02 +0800

    gemini processedAutoTestCases w3cdom_estest7.java

--- w3cdom_estest7.java
+++ w3cdom_estest7.java
@@ -1,38 +1,26 @@
 package org.jsoup.helper;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import com.sun.org.apache.xerces.internal.dom.DocumentImpl;
-import java.io.Reader;
-import java.io.StringReader;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
-import javax.imageio.metadata.IIOMetadataNode;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.jsoup.nodes.Attributes;
-import org.jsoup.nodes.DataNode;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.DocumentType;
-import org.jsoup.nodes.Element;
-import org.jsoup.nodes.FormElement;
-import org.jsoup.nodes.XmlDeclaration;
-import org.jsoup.parser.Parser;
-import org.jsoup.parser.Tag;
-import org.junit.runner.RunWith;
-import org.w3c.dom.DOMException;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import static org.junit.Assert.assertTrue;
 
-public class W3CDom_ESTestTest7 extends W3CDom_ESTest_scaffolding {
+/**
+ * Test suite for the {@link W3CDom} helper class.
+ */
+public class W3CDomTest {
 
-    @Test(timeout = 4000)
-    public void test06() throws Throwable {
-        W3CDom w3CDom0 = new W3CDom();
-        w3CDom0.namespaceAware(true);
-        assertTrue(w3CDom0.namespaceAware());
+    /**
+     * Verifies that the namespaceAware property can be set to true and its state
+     * is correctly retrieved by the corresponding getter.
+     */
+    @Test
+    public void namespaceAwarePropertyIsSettableAndGettable() {
+        // Arrange: Create a new W3CDom instance.
+        W3CDom w3cDom = new W3CDom();
+
+        // Act: Set the namespaceAware property to true.
+        w3cDom.namespaceAware(true);
+
+        // Assert: Verify that the getter returns true.
+        assertTrue("The namespaceAware property should be true after being set.", w3cDom.namespaceAware());
     }
 }
