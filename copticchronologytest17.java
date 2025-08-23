8cf049788cd199e4c7348c71a53dbbe632d763f2
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:27:10 +0800

    Original processedManualTestCases copticchronologytest17.java

---
+++ copticchronologytest17.java
@@ -0,0 +1,118 @@
+package org.joda.time.chrono;
+
+import java.util.Locale;
+import java.util.TimeZone;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.joda.time.Chronology;
+import org.joda.time.DateTime;
+import org.joda.time.DateTime.Property;
+import org.joda.time.DateTimeConstants;
+import org.joda.time.DateTimeField;
+import org.joda.time.DateTimeUtils;
+import org.joda.time.DateTimeZone;
+import org.joda.time.DurationField;
+import org.joda.time.DurationFieldType;
+
+public class CopticChronologyTestTest17 extends TestCase {
+
+    private static final int MILLIS_PER_DAY = DateTimeConstants.MILLIS_PER_DAY;
+
+    private static long SKIP = 1 * MILLIS_PER_DAY;
+
+    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
+
+    private static final DateTimeZone LONDON = DateTimeZone.forID("Europe/London");
+
+    private static final DateTimeZone TOKYO = DateTimeZone.forID("Asia/Tokyo");
+
+    private static final Chronology COPTIC_UTC = CopticChronology.getInstanceUTC();
+
+    private static final Chronology JULIAN_UTC = JulianChronology.getInstanceUTC();
+
+    private static final Chronology ISO_UTC = ISOChronology.getInstanceUTC();
+
+    long y2002days = 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365;
+
+    // 2002-06-09
+    private long TEST_TIME_NOW = (y2002days + 31L + 28L + 31L + 30L + 31L + 9L - 1L) * MILLIS_PER_DAY;
+
+    private DateTimeZone originalDateTimeZone = null;
+
+    private TimeZone originalTimeZone = null;
+
+    private Locale originalLocale = null;
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
+
+    public static TestSuite suite() {
+        SKIP = 1 * MILLIS_PER_DAY;
+        return new TestSuite(TestCopticChronology.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        DateTimeUtils.setCurrentMillisFixed(TEST_TIME_NOW);
+        originalDateTimeZone = DateTimeZone.getDefault();
+        originalTimeZone = TimeZone.getDefault();
+        originalLocale = Locale.getDefault();
+        DateTimeZone.setDefault(LONDON);
+        TimeZone.setDefault(TimeZone.getTimeZone("Europe/London"));
+        Locale.setDefault(Locale.UK);
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        DateTimeUtils.setCurrentMillisSystem();
+        DateTimeZone.setDefault(originalDateTimeZone);
+        TimeZone.setDefault(originalTimeZone);
+        Locale.setDefault(originalLocale);
+        originalDateTimeZone = null;
+        originalTimeZone = null;
+        originalLocale = null;
+    }
+
+    public void testDurationMonth() {
+        // Leap 1723
+        DateTime dt11 = new DateTime(1723, 11, 2, 0, 0, 0, 0, COPTIC_UTC);
+        DateTime dt12 = new DateTime(1723, 12, 2, 0, 0, 0, 0, COPTIC_UTC);
+        DateTime dt13 = new DateTime(1723, 13, 2, 0, 0, 0, 0, COPTIC_UTC);
+        DateTime dt01 = new DateTime(1724, 1, 2, 0, 0, 0, 0, COPTIC_UTC);
+        DurationField fld = dt11.monthOfYear().getDurationField();
+        assertEquals(COPTIC_UTC.months(), fld);
+        assertEquals(1L * 30L * MILLIS_PER_DAY, fld.getMillis(1, dt11.getMillis()));
+        assertEquals(2L * 30L * MILLIS_PER_DAY, fld.getMillis(2, dt11.getMillis()));
+        assertEquals((2L * 30L + 6L) * MILLIS_PER_DAY, fld.getMillis(3, dt11.getMillis()));
+        assertEquals((3L * 30L + 6L) * MILLIS_PER_DAY, fld.getMillis(4, dt11.getMillis()));
+        assertEquals(1L * 30L * MILLIS_PER_DAY, fld.getMillis(1));
+        assertEquals(2L * 30L * MILLIS_PER_DAY, fld.getMillis(2));
+        assertEquals(13L * 30L * MILLIS_PER_DAY, fld.getMillis(13));
+        assertEquals(1L * 30L * MILLIS_PER_DAY, fld.getMillis(1L, dt11.getMillis()));
+        assertEquals(2L * 30L * MILLIS_PER_DAY, fld.getMillis(2L, dt11.getMillis()));
+        assertEquals((2L * 30L + 6L) * MILLIS_PER_DAY, fld.getMillis(3L, dt11.getMillis()));
+        assertEquals((3L * 30L + 6L) * MILLIS_PER_DAY, fld.getMillis(4L, dt11.getMillis()));
+        assertEquals(1L * 30L * MILLIS_PER_DAY, fld.getMillis(1L));
+        assertEquals(2L * 30L * MILLIS_PER_DAY, fld.getMillis(2L));
+        assertEquals(13L * 30L * MILLIS_PER_DAY, fld.getMillis(13L));
+        assertEquals(0, fld.getValue(1L * 30L * MILLIS_PER_DAY - 1L, dt11.getMillis()));
+        assertEquals(1, fld.getValue(1L * 30L * MILLIS_PER_DAY, dt11.getMillis()));
+        assertEquals(1, fld.getValue(1L * 30L * MILLIS_PER_DAY + 1L, dt11.getMillis()));
+        assertEquals(1, fld.getValue(2L * 30L * MILLIS_PER_DAY - 1L, dt11.getMillis()));
+        assertEquals(2, fld.getValue(2L * 30L * MILLIS_PER_DAY, dt11.getMillis()));
+        assertEquals(2, fld.getValue(2L * 30L * MILLIS_PER_DAY + 1L, dt11.getMillis()));
+        assertEquals(2, fld.getValue((2L * 30L + 6L) * MILLIS_PER_DAY - 1L, dt11.getMillis()));
+        assertEquals(3, fld.getValue((2L * 30L + 6L) * MILLIS_PER_DAY, dt11.getMillis()));
+        assertEquals(3, fld.getValue((2L * 30L + 6L) * MILLIS_PER_DAY + 1L, dt11.getMillis()));
+        assertEquals(3, fld.getValue((3L * 30L + 6L) * MILLIS_PER_DAY - 1L, dt11.getMillis()));
+        assertEquals(4, fld.getValue((3L * 30L + 6L) * MILLIS_PER_DAY, dt11.getMillis()));
+        assertEquals(4, fld.getValue((3L * 30L + 6L) * MILLIS_PER_DAY + 1L, dt11.getMillis()));
+        assertEquals(dt12.getMillis(), fld.add(dt11.getMillis(), 1));
+        assertEquals(dt13.getMillis(), fld.add(dt11.getMillis(), 2));
+        assertEquals(dt01.getMillis(), fld.add(dt11.getMillis(), 3));
+        assertEquals(dt12.getMillis(), fld.add(dt11.getMillis(), 1L));
+        assertEquals(dt13.getMillis(), fld.add(dt11.getMillis(), 2L));
+        assertEquals(dt01.getMillis(), fld.add(dt11.getMillis(), 3L));
+    }
+}
