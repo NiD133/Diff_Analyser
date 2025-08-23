98374874dbd2133fd4b65a506a28caf9e14b25d7
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:09:21 +0800

    gemini processedAutoTestCases predicatedmap_estest1.java

--- predicatedmap_estest1.java
+++ predicatedmap_estest1.java
@@ -1,56 +1,36 @@
 package org.apache.commons.collections4.map;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
 import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionPredicate;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NotNullPredicate;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.OnePredicate;
-import org.apache.commons.collections4.functors.OrPredicate;
-import org.apache.commons.collections4.functors.PredicateTransformer;
-import org.apache.commons.collections4.functors.TransformerPredicate;
 import org.apache.commons.collections4.functors.TruePredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class PredicatedMap_ESTestTest1 extends PredicatedMap_ESTest_scaffolding {
+import java.util.HashMap;
+import java.util.Map;
 
-    @Test(timeout = 4000)
-    public void test00() throws Throwable {
-        HashMap<HashMap<Integer, Object>, Integer> hashMap0 = new HashMap<HashMap<Integer, Object>, Integer>();
-        Predicate<Object>[] predicateArray0 = (Predicate<Object>[]) Array.newInstance(Predicate.class, 2);
-        AnyPredicate<Object> anyPredicate0 = new AnyPredicate<Object>(predicateArray0);
-        OnePredicate<Object> onePredicate0 = new OnePredicate<Object>(predicateArray0);
-        PredicatedMap<HashMap<Integer, Object>, Integer> predicatedMap0 = PredicatedMap.predicatedMap((Map<HashMap<Integer, Object>, Integer>) hashMap0, (Predicate<? super HashMap<Integer, Object>>) onePredicate0, (Predicate<? super Integer>) onePredicate0);
-        PredicatedMap<HashMap<Integer, Object>, Integer> predicatedMap1 = new PredicatedMap<HashMap<Integer, Object>, Integer>(predicatedMap0, anyPredicate0, predicateArray0[0]);
-        // Undeclared exception!
-        try {
-            predicatedMap1.checkSetValue((Integer) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.commons.collections4.map.PredicatedMap", e);
-        }
+/**
+ * Unit tests for {@link PredicatedMap}.
+ */
+public class PredicatedMapTest {
+
+    /**
+     * Tests that checkSetValue() throws a NullPointerException if the map
+     * was constructed with a null valuePredicate. The method internally
+     * calls evaluate() on the predicate without a null check.
+     */
+    @Test(expected = NullPointerException.class)
+    public void checkSetValueShouldThrowNullPointerExceptionWhenValuePredicateIsNull() {
+        // Arrange: Create a PredicatedMap with a null value predicate.
+        // The key predicate can be any valid predicate; it's not relevant to this test.
+        final Map<String, String> map = new HashMap<>();
+        final Predicate<String> keyPredicate = TruePredicate.truePredicate();
+        final Predicate<String> nullValuePredicate = null;
+
+        // The constructor is protected, but this test is in the same package, so it's accessible.
+        final PredicatedMap<String, String> predicatedMap =
+            new PredicatedMap<>(map, keyPredicate, nullValuePredicate);
+
+        // Act: Call the method under test. This is expected to throw a NullPointerException
+        // because it will attempt to call a method on the null valuePredicate.
+        predicatedMap.checkSetValue("anyValue");
     }
 }
