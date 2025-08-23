04f749cd05066eb1e967f20fc661ac736422d2b9
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:32:51 +0800

    Original processedManualTestCases quotedprintablecodectest5.java

---
+++ quotedprintablecodectest5.java
@@ -0,0 +1,35 @@
+package org.apache.commons.codec.net;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.nio.charset.StandardCharsets;
+import java.nio.charset.UnsupportedCharsetException;
+import org.apache.commons.codec.CharEncoding;
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.EncoderException;
+import org.junit.jupiter.api.Test;
+
+public class QuotedPrintableCodecTestTest5 {
+
+    static final int[] SWISS_GERMAN_STUFF_UNICODE = { 0x47, 0x72, 0xFC, 0x65, 0x7A, 0x69, 0x5F, 0x7A, 0xE4, 0x6D, 0xE4 };
+
+    static final int[] RUSSIAN_STUFF_UNICODE = { 0x412, 0x441, 0x435, 0x43C, 0x5F, 0x43F, 0x440, 0x438, 0x432, 0x435, 0x442 };
+
+    private String constructString(final int[] unicodeChars) {
+        final StringBuilder buffer = new StringBuilder();
+        if (unicodeChars != null) {
+            for (final int unicodeChar : unicodeChars) {
+                buffer.append((char) unicodeChar);
+            }
+        }
+        return buffer.toString();
+    }
+
+    @Test
+    void testDecodeWithNullArray() throws Exception {
+        final byte[] plain = null;
+        final byte[] result = QuotedPrintableCodec.decodeQuotedPrintable(plain);
+        assertNull(result, "Result should be null");
+    }
+}
