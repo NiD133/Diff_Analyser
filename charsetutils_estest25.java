a995419c3602f8955e898a79064b4df55a820512
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:42:47 +0800

    gemini processedAutoTestCases charsetutils_estest25.java

--- charsetutils_estest25.java
+++ charsetutils_estest25.java
@@ -1,15 +1,29 @@
 package org.apache.commons.lang3;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertNotNull;
 
-public class CharSetUtils_ESTestTest25 extends CharSetUtils_ESTest_scaffolding {
+/**
+ * Unit tests for {@link CharSetUtils}.
+ */
+public class CharSetUtilsTest {
 
-    @Test(timeout = 4000)
-    public void test24() throws Throwable {
-        CharSetUtils charSetUtils0 = new CharSetUtils();
+    /**
+     * Tests the public, deprecated constructor.
+     *
+     * <p>The constructor is not intended for general use, as all methods in CharSetUtils are static.
+     * However, it is public to allow instantiation by tools that require a JavaBean instance.
+     * This test confirms that the class can be instantiated, primarily for code coverage purposes
+     * and to ensure JavaBean compatibility is maintained until the constructor is made private.</p>
+     */
+    @Test
+    public void testConstructorForJavaBeanCompatibility() {
+        // The CharSetUtils class has a public constructor for tools that require a
+        // JavaBean instance, even though all its methods are static.
+        final CharSetUtils instance = new CharSetUtils();
+
+        // Assert that the instance is created successfully. This confirms that the
+        // constructor works as expected for tools that might rely on it.
+        assertNotNull("The instance should not be null.", instance);
     }
 }
