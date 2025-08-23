51fdacfe45a7e420bb3724817b11fb105f4f2b6e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:19:02 +0800

    Original processedAutoTestCases stringutil_estest60.java

---
+++ stringutil_estest60.java
@@ -0,0 +1,30 @@
+package org.jsoup.internal;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Collection;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.stream.Collector;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.net.MockURL;
+import org.junit.runner.RunWith;
+
+public class StringUtil_ESTestTest60 extends StringUtil_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test59() throws Throwable {
+        String[] stringArray0 = new String[1];
+        stringArray0[0] = "{3f\"nUAQw7TH,Y-";
+        boolean boolean0 = StringUtil.inSorted("{3f\"nUAQw7TH,Y-", stringArray0);
+        assertTrue(boolean0);
+    }
+}
