08163fc8748788c7c6dbc37501d3d24cc6b4b02d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:49:09 +0800

    gemini processedAutoTestCases endpointpair_estest10.java

--- endpointpair_estest10.java
+++ endpointpair_estest10.java
@@ -1,29 +1,22 @@
 package com.google.common.graph;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import com.google.common.collect.UnmodifiableIterator;
-import java.util.Locale;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class EndpointPair_ESTestTest10 extends EndpointPair_ESTest_scaffolding {
+/**
+ * Tests for {@link EndpointPair}.
+ */
+public class EndpointPairTest {
 
-    @Test(timeout = 4000)
-    public void test09() throws Throwable {
-        NetworkBuilder<Object, Object> networkBuilder0 = NetworkBuilder.directed();
-        StandardNetwork<Object, Object> standardNetwork0 = new StandardNetwork<Object, Object>(networkBuilder0);
-        // Undeclared exception!
-        try {
-            EndpointPair.of((Network<?, ?>) standardNetwork0, (Comparable<Comparable>) null, (Comparable<Comparable>) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("com.google.common.base.Preconditions", e);
-        }
+    @Test(expected = NullPointerException.class)
+    public void of_network_withNullNodes_throwsNullPointerException() {
+        // Arrange: Create a network instance. Its configuration is not important for this test.
+        Network<String, Integer> network = NetworkBuilder.directed().build();
+
+        // Act: Call the method under test with null arguments for the nodes.
+        // According to the contract, this should trigger a Preconditions check.
+        EndpointPair.of(network, null, null);
+
+        // Assert: The test passes if a NullPointerException is thrown,
+        // which is handled by the `expected` attribute of the @Test annotation.
     }
 }
