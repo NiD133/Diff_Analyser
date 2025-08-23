7cacc28e5323f14c4bd2369cf3d54dcc55f8a472
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:39:30 +0800

    gemini processedAutoTestCases blockingcache_estest21.java

--- blockingcache_estest21.java
+++ blockingcache_estest21.java
@@ -1,23 +1,31 @@
 package org.apache.ibatis.cache.decorators;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.concurrent.CountDownLatch;
 import org.apache.ibatis.cache.Cache;
 import org.apache.ibatis.cache.impl.PerpetualCache;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class BlockingCache_ESTestTest21 extends BlockingCache_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
 
-    @Test(timeout = 4000)
-    public void test20() throws Throwable {
-        PerpetualCache perpetualCache0 = new PerpetualCache("x");
-        SoftCache softCache0 = new SoftCache(perpetualCache0);
-        BlockingCache blockingCache0 = new BlockingCache(softCache0);
-        long long0 = blockingCache0.getTimeout();
-        assertEquals(0L, long0);
+/**
+ * Test suite for BlockingCache to ensure its core functionalities work as expected.
+ */
+public class BlockingCacheTest {
+
+    /**
+     * Verifies that a newly created BlockingCache instance has a default timeout of 0.
+     * A timeout of 0 indicates that a thread will wait indefinitely for a lock to be released.
+     */
+    @Test
+    public void shouldHaveDefaultTimeoutOfZeroOnInitialization() {
+        // Arrange: Create a BlockingCache with a standard delegate cache.
+        // The specific type of delegate is not important for this test.
+        Cache delegateCache = new PerpetualCache("default-cache");
+        BlockingCache blockingCache = new BlockingCache(delegateCache);
+
+        // Act: Retrieve the timeout value from the new instance.
+        long timeout = blockingCache.getTimeout();
+
+        // Assert: The default timeout should be 0L.
+        assertEquals("Default timeout should be 0", 0L, timeout);
     }
 }
