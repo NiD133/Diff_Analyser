9adfbcaac571800f70cb6ea7ec6643204a8ed4f7
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:06:31 +0800

    gemini processedAutoTestCases optionformatter_estest39.java

--- optionformatter_estest39.java
+++ optionformatter_estest39.java
@@ -1,24 +1,31 @@
 package org.apache.commons.cli.help;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.function.BiFunction;
 import org.apache.commons.cli.Option;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class OptionFormatter_ESTestTest39 extends OptionFormatter_ESTest_scaffolding {
+import static org.junit.Assert.assertSame;
 
-    @Test(timeout = 4000)
-    public void test38() throws Throwable {
-        Option option0 = new Option((String) null, (String) null);
-        OptionFormatter optionFormatter0 = OptionFormatter.from(option0);
-        OptionFormatter.Builder optionFormatter_Builder0 = new OptionFormatter.Builder(optionFormatter0);
-        OptionFormatter.Builder optionFormatter_Builder1 = optionFormatter_Builder0.setOptPrefix("-");
-        assertSame(optionFormatter_Builder1, optionFormatter_Builder0);
+/**
+ * Tests for the fluent interface of {@link OptionFormatter.Builder}.
+ */
+public class OptionFormatterBuilderTest {
+
+    /**
+     * Verifies that the setter methods in the Builder return the same instance,
+     * allowing for fluent call chaining.
+     */
+    @Test
+    public void setOptPrefixShouldReturnSameBuilderInstanceForChaining() {
+        // Arrange: A builder instance is required to test its methods.
+        // Creating an Option and Formatter is the standard way to get a Builder.
+        Option dummyOption = new Option("o", "option");
+        OptionFormatter formatter = OptionFormatter.from(dummyOption);
+        OptionFormatter.Builder builder = new OptionFormatter.Builder(formatter);
+
+        // Act: Call the method under test.
+        OptionFormatter.Builder returnedBuilder = builder.setOptPrefix("-");
+
+        // Assert: The method should return the same instance for fluent chaining.
+        assertSame("The builder instance should be returned to allow for a fluent API.", builder, returnedBuilder);
     }
 }
