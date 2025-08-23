b06306b7b531cc621572e642a44efb3f43244f5b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:11:00 +0800

    gemini processedAutoTestCases randomaccessfilemode_estest2.java

--- randomaccessfilemode_estest2.java
+++ randomaccessfilemode_estest2.java
@@ -1,29 +1,26 @@
 package org.apache.commons.io;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.RandomAccessFile;
-import java.nio.file.OpenOption;
-import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
-import org.apache.commons.io.function.IOConsumer;
-import org.apache.commons.io.function.IOFunction;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.io.MockFile;
-import org.evosuite.runtime.testdata.EvoSuiteFile;
-import org.evosuite.runtime.testdata.FileSystemHandling;
-import org.junit.runner.RunWith;
 
-public class RandomAccessFileMode_ESTestTest2 extends RandomAccessFileMode_ESTest_scaffolding {
+/**
+ * Tests for the {@link RandomAccessFileMode} enum.
+ */
+public class RandomAccessFileModeTest {
 
-    @Test(timeout = 4000)
-    public void test01() throws Throwable {
-        RandomAccessFileMode[] randomAccessFileModeArray0 = RandomAccessFileMode.values();
-        assertEquals(4, randomAccessFileModeArray0.length);
+    /**
+     * Tests that the values() method returns all expected enum constants.
+     * This acts as a safeguard to ensure that if a new mode is added, this test
+     * will fail, prompting a review of related logic.
+     */
+    @Test
+    public void testValuesReturnsAllDefinedModes() {
+        // The RandomAccessFileMode enum is expected to have four constants:
+        // READ_ONLY, READ_WRITE, READ_WRITE_SYNC_ALL, and READ_WRITE_SYNC_CONTENT.
+        final RandomAccessFileMode[] modes = RandomAccessFileMode.values();
+
+        // Verify that the number of enum constants is correct.
+        assertEquals("The RandomAccessFileMode enum should contain exactly 4 constants.", 4, modes.length);
     }
 }
