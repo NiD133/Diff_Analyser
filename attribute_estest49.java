9b2bdadbf22ed0ee9c2c3cbef8b5c30a4cd688cf
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:38:19 +0800

    Original processedAutoTestCases attribute_estest49.java

---
+++ attribute_estest49.java
@@ -0,0 +1,30 @@
+package org.jsoup.nodes;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.ByteArrayOutputStream;
+import java.io.FilterOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PipedWriter;
+import java.io.StringWriter;
+import java.nio.BufferOverflowException;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetEncoder;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockPrintWriter;
+import org.jsoup.internal.QuietAppendable;
+import org.junit.runner.RunWith;
+
+public class Attribute_ESTestTest49 extends Attribute_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test48() throws Throwable {
+        Document.OutputSettings.Syntax document_OutputSettings_Syntax0 = Document.OutputSettings.Syntax.html;
+        String string0 = Attribute.getValidKey("AkY'Y", document_OutputSettings_Syntax0);
+        assertNotNull(string0);
+        assertEquals("AkY_Y", string0);
+    }
+}
