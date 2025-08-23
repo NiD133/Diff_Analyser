d546716722ea973598d73c4aeabb911429a39188
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:53:31 +0800

    gemini processedAutoTestCases ieee754rutils_estest27.java

--- ieee754rutils_estest27.java
+++ ieee754rutils_estest27.java
@@ -1,25 +1,22 @@
 package org.apache.commons.lang3.math;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class IEEE754rUtils_ESTestTest27 extends IEEE754rUtils_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link IEEE754rUtils} class.
+ */
+public class IEEE754rUtilsTest {
 
-    @Test(timeout = 4000)
-    public void test26() throws Throwable {
-        // Undeclared exception!
-        try {
-            IEEE754rUtils.max((float[]) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // array
-            //
-            verifyException("java.util.Objects", e);
-        }
+    /**
+     * Tests that calling max() with a null float array throws a NullPointerException.
+     * This verifies the contract that the input array must not be null.
+     */
+    @Test(expected = NullPointerException.class)
+    public void maxFloatArray_withNullArray_shouldThrowNullPointerException() {
+        // When the max method is called with a null float array
+        IEEE754rUtils.max((float[]) null);
+
+        // Then a NullPointerException is expected to be thrown.
+        // This is asserted by the 'expected' attribute of the @Test annotation.
     }
 }
