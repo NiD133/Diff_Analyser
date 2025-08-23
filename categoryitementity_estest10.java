7a227e668c2b462522dcc30dbaa143c7487dd475
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:41:44 +0800

    gemini processedAutoTestCases categoryitementity_estest10.java

--- categoryitementity_estest10.java
+++ categoryitementity_estest10.java
@@ -1,36 +1,41 @@
 package org.jfree.chart.entity;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.awt.Polygon;
-import java.awt.Rectangle;
-import java.awt.geom.Rectangle2D;
-import java.time.Clock;
-import java.time.LocalDate;
-import java.time.chrono.ChronoLocalDate;
-import java.time.chrono.JapaneseDate;
-import javax.swing.JLayeredPane;
-import javax.swing.text.DefaultCaret;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.time.MockClock;
-import org.evosuite.runtime.mock.java.time.MockLocalDate;
-import org.evosuite.runtime.mock.java.time.chrono.MockJapaneseDate;
-import org.jfree.data.category.CategoryDataset;
-import org.jfree.data.statistics.DefaultBoxAndWhiskerCategoryDataset;
 import org.jfree.data.statistics.DefaultMultiValueCategoryDataset;
-import org.jfree.data.statistics.DefaultStatisticalCategoryDataset;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class CategoryItemEntity_ESTestTest10 extends CategoryItemEntity_ESTest_scaffolding {
+import javax.swing.text.DefaultCaret;
+import java.awt.Shape;
 
-    @Test(timeout = 4000)
-    public void test09() throws Throwable {
-        DefaultCaret defaultCaret0 = new DefaultCaret();
-        DefaultMultiValueCategoryDataset<Integer, Integer> defaultMultiValueCategoryDataset0 = new DefaultMultiValueCategoryDataset<Integer, Integer>();
-        CategoryItemEntity<Integer, Integer> categoryItemEntity0 = new CategoryItemEntity<Integer, Integer>(defaultCaret0, "", "", defaultMultiValueCategoryDataset0, (Integer) 0, (Integer) 0);
-        boolean boolean0 = categoryItemEntity0.equals("");
-        assertFalse(boolean0);
+import static org.junit.Assert.assertFalse;
+
+/**
+ * Tests for the {@link CategoryItemEntity} class, focusing on the equals() method.
+ */
+public class CategoryItemEntityTest {
+
+    /**
+     * Verifies that the equals() method returns false when a CategoryItemEntity
+     * is compared to an object of a different type.
+     */
+    @Test
+    public void equals_whenComparedWithDifferentObjectType_shouldReturnFalse() {
+        // Arrange
+        Shape area = new DefaultCaret();
+        DefaultMultiValueCategoryDataset<Integer, Integer> dataset = new DefaultMultiValueCategoryDataset<>();
+        
+        CategoryItemEntity<Integer, Integer> entity = new CategoryItemEntity<>(
+                area,
+                "Test ToolTip",
+                "Test URL",
+                dataset,
+                0, // rowKey
+                0  // columnKey
+        );
+
+        Object objectOfDifferentType = "";
+
+        // Act & Assert
+        // The result should be false because the entity is being compared to a String.
+        assertFalse(entity.equals(objectOfDifferentType));
     }
 }
