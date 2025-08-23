592087b1ac8e8e3860c492baa797c12dd4e57478
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:55:30 +0800

    Original processedAutoTestCases iso8601utils_estest14.java

---
+++ iso8601utils_estest14.java
@@ -0,0 +1,31 @@
+package com.google.gson.internal.bind.util;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.text.ParseException;
+import java.text.ParsePosition;
+import java.util.Date;
+import java.util.SimpleTimeZone;
+import java.util.TimeZone;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.util.MockDate;
+import org.junit.runner.RunWith;
+
+public class ISO8601Utils_ESTestTest14 extends ISO8601Utils_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test13() throws Throwable {
+        ParsePosition parsePosition0 = new ParsePosition((-844));
+        try {
+            ISO8601Utils.parse("", parsePosition0);
+            fail("Expecting exception: ParseException");
+        } catch (ParseException e) {
+            //
+            // Failed to parse date [\"\"]: (java.lang.NumberFormatException)
+            //
+            verifyException("com.google.gson.internal.bind.util.ISO8601Utils", e);
+        }
+    }
+}
