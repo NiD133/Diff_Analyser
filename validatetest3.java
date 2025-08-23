da0e1fef6679bae8054a27ce4a15c44c86a86fb8
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:35:37 +0800

    Original processedManualTestCases validatetest3.java

---
+++ validatetest3.java
@@ -0,0 +1,19 @@
+package org.jsoup.helper;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ValidateTestTest3 {
+
+    @Test
+    void nonnullParam() {
+        boolean threw = true;
+        try {
+            Validate.notNullParam(null, "foo");
+        } catch (ValidationException e) {
+            assertEquals("The parameter 'foo' must not be null.", e.getMessage());
+        }
+        assertTrue(threw);
+    }
+}
