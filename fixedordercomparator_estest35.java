b8287c09251fb799f72514d8236c78aae1eb3f41
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:51:30 +0800

    gemini processedAutoTestCases fixedordercomparator_estest35.java

--- fixedordercomparator_estest35.java
+++ fixedordercomparator_estest35.java
@@ -1,27 +1,26 @@
 package org.apache.commons.collections4.comparators;
 
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.function.Function;
-import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.PredicateTransformer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class FixedOrderComparator_ESTestTest35 extends FixedOrderComparator_ESTest_scaffolding {
+/**
+ * Unit tests for {@link FixedOrderComparator}.
+ */
+public class FixedOrderComparatorTest {
 
-    @Test(timeout = 4000)
-    public void test34() throws Throwable {
-        FixedOrderComparator<Predicate<Object>> fixedOrderComparator0 = new FixedOrderComparator<Predicate<Object>>();
-        FixedOrderComparator.UnknownObjectBehavior fixedOrderComparator_UnknownObjectBehavior0 = fixedOrderComparator0.getUnknownObjectBehavior();
-        assertEquals(FixedOrderComparator.UnknownObjectBehavior.EXCEPTION, fixedOrderComparator_UnknownObjectBehavior0);
+    /**
+     * Tests that a newly created FixedOrderComparator, using the default constructor,
+     * is configured to throw an exception when an unknown object is encountered.
+     */
+    @Test
+    public void shouldDefaultToExceptionForUnknownObjectBehavior() {
+        // Arrange: Create a comparator with the default constructor.
+        final FixedOrderComparator<Object> comparator = new FixedOrderComparator<>();
+
+        // Act: Retrieve the configured behavior for unknown objects.
+        final FixedOrderComparator.UnknownObjectBehavior behavior = comparator.getUnknownObjectBehavior();
+
+        // Assert: Verify that the default behavior is EXCEPTION.
+        assertEquals(FixedOrderComparator.UnknownObjectBehavior.EXCEPTION, behavior);
     }
 }
