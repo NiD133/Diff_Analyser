e29049538366f217693f584d3a87b50885d101bf
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:48:25 +0800

    Original processedAutoTestCases elements_estest133.java

---
+++ elements_estest133.java
@@ -0,0 +1,37 @@
+package org.jsoup.select;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.ConcurrentModificationException;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.jsoup.nodes.Comment;
+import org.jsoup.nodes.DataNode;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.FormElement;
+import org.jsoup.nodes.TextNode;
+import org.jsoup.parser.Parser;
+import org.junit.runner.RunWith;
+
+public class Elements_ESTestTest133 extends Elements_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test132() throws Throwable {
+        Document document0 = Parser.parse("nly http & htps prtocols supported", "nly http & htps prtocols supported");
+        Elements elements0 = document0.getAllElements();
+        elements0.val("*mxmNg");
+        String string0 = elements0.val();
+        assertEquals("*mxmNg", string0);
+    }
+}
