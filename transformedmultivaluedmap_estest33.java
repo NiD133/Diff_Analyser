89d63919041e09370e583dd0dbb89306c5e27c93
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:23:21 +0800

    gemini processedAutoTestCases transformedmultivaluedmap_estest33.java

--- transformedmultivaluedmap_estest33.java
+++ transformedmultivaluedmap_estest33.java
@@ -1,61 +1,53 @@
 package org.apache.commons.collections4.multimap;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.AbstractMap;
-import java.util.ArrayDeque;
-import java.util.Comparator;
-import java.util.ConcurrentModificationException;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
-import java.util.PriorityQueue;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.Factory;
 import org.apache.commons.collections4.MultiValuedMap;
-import org.apache.commons.collections4.Predicate;
 import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AllPredicate;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ChainedTransformer;
-import org.apache.commons.collections4.functors.CloneTransformer;
-import org.apache.commons.collections4.functors.ClosureTransformer;
-import org.apache.commons.collections4.functors.ConstantFactory;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.ExceptionFactory;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.FactoryTransformer;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.MapTransformer;
-import org.apache.commons.collections4.functors.NOPTransformer;
-import org.apache.commons.collections4.functors.NotPredicate;
-import org.apache.commons.collections4.functors.NullIsExceptionPredicate;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.SwitchTransformer;
-import org.apache.commons.collections4.functors.TransformerClosure;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class TransformedMultiValuedMap_ESTestTest33 extends TransformedMultiValuedMap_ESTest_scaffolding {
+import java.util.Collections;
 
-    @Test(timeout = 4000)
-    public void test32() throws Throwable {
-        LinkedHashSetValuedLinkedHashMap<Integer, Object> linkedHashSetValuedLinkedHashMap0 = new LinkedHashSetValuedLinkedHashMap<Integer, Object>(0);
-        Integer integer0 = new Integer(0);
-        Factory<Integer> factory0 = ConstantFactory.constantFactory(integer0);
-        FactoryTransformer<Object, Integer> factoryTransformer0 = new FactoryTransformer<Object, Integer>(factory0);
-        TransformedMultiValuedMap<Integer, Object> transformedMultiValuedMap0 = new TransformedMultiValuedMap<Integer, Object>(linkedHashSetValuedLinkedHashMap0, factoryTransformer0, factoryTransformer0);
-        ArrayDeque<Integer> arrayDeque0 = new ArrayDeque<Integer>();
-        arrayDeque0.offerFirst(integer0);
-        boolean boolean0 = transformedMultiValuedMap0.putAll(integer0, (Iterable<?>) arrayDeque0);
-        assertTrue(boolean0);
-        boolean boolean1 = transformedMultiValuedMap0.putAll(integer0, (Iterable<?>) arrayDeque0);
-        assertFalse(boolean1);
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Contains tests for {@link TransformedMultiValuedMap}.
+ */
+public class TransformedMultiValuedMapTest {
+
+    /**
+     * Tests that putAll() returns false when adding a key-value pair that, after
+     * transformation, already exists in a map backed by a Set. The putAll() method
+     * should only return true if the underlying collection is actually modified.
+     */
+    @Test
+    public void putAllShouldReturnFalseWhenAddingDuplicateTransformedValueToSetBasedMap() {
+        // Arrange
+        // The backing map uses a Set to store values, which prevents duplicate entries for a given key.
+        final MultiValuedMap<Integer, Object> backingMap = new LinkedHashSetValuedLinkedHashMap<>();
+
+        // A transformer that converts any input key or value to the integer 0.
+        // This ensures we are always attempting to add the same transformed key and value.
+        final Transformer<Object, Object> constantTransformer = ignored -> 0;
+
+        final TransformedMultiValuedMap<Integer, Object> transformedMap =
+                TransformedMultiValuedMap.transformingMap(backingMap, constantTransformer, constantTransformer);
+
+        final Integer key = 1; // An arbitrary key, will be transformed to 0.
+        final Iterable<Integer> valuesToAdd = Collections.singletonList(100); // An arbitrary value, will be transformed to 0.
+
+        // Act & Assert: First call
+        // The first call should modify the map by adding the transformed key and value.
+        // The map's state becomes {0=[0]}.
+        boolean wasModified = transformedMap.putAll(key, valuesToAdd);
+        assertTrue("putAll should return true when the map is modified for the first time.", wasModified);
+
+        // Act & Assert: Second call
+        // The second call uses a different key and value, but they transform to the same results.
+        // Since the backing map is Set-based, adding the existing value '0' for key '0'
+        // does not change the map. Therefore, putAll should return false.
+        final Integer anotherKey = 2;
+        final Iterable<Integer> anotherValue = Collections.singletonList(200);
+        wasModified = transformedMap.putAll(anotherKey, anotherValue);
+        assertFalse("putAll should return false when adding a pre-existing transformed value.", wasModified);
     }
 }
