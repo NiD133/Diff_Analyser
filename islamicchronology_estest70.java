cc1a85d3994848106466a2eb969d23cfa3fe090e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:55:27 +0800

    gemini processedAutoTestCases islamicchronology_estest70.java

--- islamicchronology_estest70.java
+++ islamicchronology_estest70.java
@@ -1,22 +1,29 @@
 package org.joda.time.chrono;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.TimeZone;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.joda.time.Chronology;
-import org.joda.time.DateTimeZone;
-import org.joda.time.tz.UTCProvider;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class IslamicChronology_ESTestTest70 extends IslamicChronology_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link IslamicChronology} class.
+ */
+public class IslamicChronologyTest {
 
-    @Test(timeout = 4000)
-    public void test69() throws Throwable {
-        IslamicChronology islamicChronology0 = IslamicChronology.getInstance();
-        int int0 = islamicChronology0.getDaysInMonthMax();
-        assertEquals(30, int0);
+    /**
+     * Tests that the maximum number of days in any month is correctly reported as 30.
+     *
+     * According to the Islamic calendar's tabular form, months have either 29 or 30 days.
+     * Therefore, the maximum number of days possible in a month is 30.
+     */
+    @Test
+    public void getDaysInMonthMax_shouldReturn30() {
+        // Arrange: Get an instance of the IslamicChronology.
+        IslamicChronology islamicChronology = IslamicChronology.getInstance();
+        int expectedMaxDays = 30;
+
+        // Act: Call the method under test.
+        int actualMaxDays = islamicChronology.getDaysInMonthMax();
+
+        // Assert: Verify that the result is 30.
+        assertEquals("The maximum days in any Islamic month should be 30.", expectedMaxDays, actualMaxDays);
     }
 }
