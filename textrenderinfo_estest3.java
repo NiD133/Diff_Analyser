b0ac5c7ae0f966595ddd6b14e2ca30138ddfe23f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:21:32 +0800

    Original processedAutoTestCases textrenderinfo_estest3.java

---
+++ textrenderinfo_estest3.java
@@ -0,0 +1,43 @@
+package com.itextpdf.text.pdf.parser;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.pdf.BaseFont;
+import com.itextpdf.text.pdf.CMapAwareDocumentFont;
+import com.itextpdf.text.pdf.DocumentFont;
+import com.itextpdf.text.pdf.PdfDate;
+import com.itextpdf.text.pdf.PdfGState;
+import com.itextpdf.text.pdf.PdfString;
+import java.nio.charset.IllegalCharsetNameException;
+import java.nio.charset.UnsupportedCharsetException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Stack;
+import java.util.TreeSet;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class TextRenderInfo_ESTestTest3 extends TextRenderInfo_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test02() throws Throwable {
+        PdfDate pdfDate0 = new PdfDate();
+        GraphicsState graphicsState0 = new GraphicsState();
+        PdfGState pdfGState0 = new PdfGState();
+        graphicsState0.fontSize = (float) 4;
+        CMapAwareDocumentFont cMapAwareDocumentFont0 = new CMapAwareDocumentFont(pdfGState0);
+        graphicsState0.font = cMapAwareDocumentFont0;
+        Stack<MarkedContentInfo> stack0 = new Stack<MarkedContentInfo>();
+        Matrix matrix0 = graphicsState0.getCtm();
+        TextRenderInfo textRenderInfo0 = new TextRenderInfo(pdfDate0, graphicsState0, matrix0, stack0);
+        List<TextRenderInfo> list0 = textRenderInfo0.getCharacterRenderInfos();
+        assertEquals(9, list0.size());
+        assertFalse(list0.contains(textRenderInfo0));
+    }
+}
