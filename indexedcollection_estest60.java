52dfb221f3ad67ced5f169cd262bcac5708eb0b0
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:54:09 +0800

    gemini processedAutoTestCases indexedcollection_estest60.java

--- indexedcollection_estest60.java
+++ indexedcollection_estest60.java
@@ -1,66 +1,51 @@
 package org.apache.commons.collections4.collection;
 
+import org.apache.commons.collections4.Transformer;
+import org.apache.commons.collections4.functors.ConstantTransformer;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
+
 import java.util.Collection;
 import java.util.ConcurrentModificationException;
-import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.Set;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AllPredicate;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ChainedTransformer;
-import org.apache.commons.collections4.functors.CloneTransformer;
-import org.apache.commons.collections4.functors.ClosureTransformer;
-import org.apache.commons.collections4.functors.ConstantFactory;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.DefaultEquator;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.FactoryTransformer;
-import org.apache.commons.collections4.functors.FalsePredicate;
-import org.apache.commons.collections4.functors.ForClosure;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.NOPClosure;
-import org.apache.commons.collections4.functors.NOPTransformer;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NotNullPredicate;
-import org.apache.commons.collections4.functors.NullIsFalsePredicate;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.SwitchTransformer;
-import org.apache.commons.collections4.functors.TransformedPredicate;
-import org.apache.commons.collections4.functors.TransformerClosure;
-import org.apache.commons.collections4.functors.TransformerPredicate;
-import org.apache.commons.collections4.functors.TruePredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class IndexedCollection_ESTestTest60 extends IndexedCollection_ESTest_scaffolding {
+/**
+ * Contains tests for the {@link IndexedCollection} class, focusing on modification scenarios.
+ */
+public class IndexedCollectionTest {
 
-    @Test(timeout = 4000)
-    public void test59() throws Throwable {
-        LinkedList<Object> linkedList0 = new LinkedList<Object>();
-        Transformer<Object, Object> transformer0 = ConstantTransformer.nullTransformer();
-        IndexedCollection<Object, Object> indexedCollection0 = IndexedCollection.nonUniqueIndexedCollection((Collection<Object>) linkedList0, transformer0);
-        indexedCollection0.add(transformer0);
-        // Undeclared exception!
-        try {
-            indexedCollection0.addAll(linkedList0);
-            fail("Expecting exception: ConcurrentModificationException");
-        } catch (ConcurrentModificationException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("java.util.LinkedList$ListItr", e);
-        }
+    /**
+     * Tests that a ConcurrentModificationException is thrown when calling addAll()
+     * with the collection's own underlying collection as the source.
+     * <p>
+     * This scenario causes a failure because the addAll() method iterates over the
+     * underlying collection while the add() method, called internally, modifies it.
+     * This is a classic example of concurrent modification.
+     */
+    @Test(expected = ConcurrentModificationException.class)
+    public void addAll_whenSourceIsTheUnderlyingCollection_shouldThrowCME() {
+        // Arrange
+        // The collection that will be decorated and also used as the source for addAll()
+        Collection<String> underlyingCollection = new LinkedList<>();
+
+        // A transformer to generate keys (the specific key doesn't matter for this test)
+        Transformer<String, Object> keyTransformer = ConstantTransformer.nullTransformer();
+
+        // The IndexedCollection under test, which decorates the underlying collection
+        IndexedCollection<Object, String> indexedCollection =
+            IndexedCollection.nonUniqueIndexedCollection(underlyingCollection, keyTransformer);
+
+        // Add an initial element to ensure the collection is not empty before addAll is called.
+        // This is necessary to trigger the iteration that leads to the exception.
+        indexedCollection.add("initial element");
+
+        // Act
+        // Attempt to add all elements from the underlying collection back into itself
+        // via the decorator. This will cause an iteration over underlyingCollection
+        // while simultaneously modifying it.
+        indexedCollection.addAll(underlyingCollection);
+
+        // Assert
+        // The test will pass if a ConcurrentModificationException is thrown,
+        // as specified by the @Test(expected=...) annotation.
     }
 }
