d19b8b9b679ae15a9daa51dc22bcf610b8e8052b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:26:08 +0800

    gemini processedManualTestCases byteutilstest22.java

--- byteutilstest22.java
+++ byteutilstest22.java
@@ -1,34 +1,32 @@
 package org.apache.commons.compress.utils;
 
-import static org.apache.commons.compress.utils.ByteUtils.fromLittleEndian;
 import static org.apache.commons.compress.utils.ByteUtils.toLittleEndian;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import java.io.ByteArrayInputStream;
+
 import java.io.ByteArrayOutputStream;
-import java.io.DataInput;
-import java.io.DataInputStream;
-import java.io.DataOutput;
-import java.io.DataOutputStream;
-import java.io.EOFException;
 import java.io.IOException;
-import java.util.Arrays;
-import org.apache.commons.compress.utils.ByteUtils.InputStreamByteSupplier;
 import org.apache.commons.compress.utils.ByteUtils.OutputStreamByteConsumer;
 import org.junit.jupiter.api.Test;
 
 public class ByteUtilsTestTest22 {
 
     @Test
-    void testToLittleEndianToConsumerUnsignedInt32() throws IOException {
-        final byte[] byteArray;
-        final byte[] expected = { 2, 3, 4, (byte) 128 };
-        try (ByteArrayOutputStream bos = new ByteArrayOutputStream()) {
-            toLittleEndian(new OutputStreamByteConsumer(bos), 2 + 3 * 256 + 4 * 256 * 256 + 128L * 256 * 256 * 256, 4);
-            byteArray = bos.toByteArray();
-            assertArrayEquals(expected, byteArray);
-        }
-        assertArrayEquals(expected, byteArray);
+    void toLittleEndianWithConsumerWritesUnsignedIntCorrectly() throws IOException {
+        // Arrange
+        // The test value is 0x80040302, an unsigned 32-bit integer that is larger
+        // than Integer.MAX_VALUE. This helps verify the correct handling of values
+        // that might otherwise be misinterpreted as negative.
+        final long valueToWrite = 0x80040302L;
+        final byte[] expectedBytes = { 2, 3, 4, (byte) 128 }; // Little-endian for 0x80040302
+
+        final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        final OutputStreamByteConsumer consumer = new OutputStreamByteConsumer(outputStream);
+
+        // Act
+        toLittleEndian(consumer, valueToWrite, 4);
+
+        // Assert
+        final byte[] actualBytes = outputStream.toByteArray();
+        assertArrayEquals(expectedBytes, actualBytes);
     }
 }
