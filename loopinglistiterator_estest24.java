6d7523f1ba2937d83fc7d427859a3f6e685712e3
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:01:40 +0800

    Original processedAutoTestCases loopinglistiterator_estest24.java

---
+++ loopinglistiterator_estest24.java
@@ -0,0 +1,34 @@
+package org.apache.commons.collections4.iterators;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.ConcurrentModificationException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import org.apache.commons.collections4.Closure;
+import org.apache.commons.collections4.functors.InstanceofPredicate;
+import org.apache.commons.collections4.functors.UniquePredicate;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class LoopingListIterator_ESTestTest24 extends LoopingListIterator_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test23() throws Throwable {
+        LinkedList<Integer> linkedList0 = new LinkedList<Integer>();
+        LoopingListIterator<Integer> loopingListIterator0 = new LoopingListIterator<Integer>(linkedList0);
+        // Undeclared exception!
+        try {
+            loopingListIterator0.next();
+            fail("Expecting exception: NoSuchElementException");
+        } catch (NoSuchElementException e) {
+            //
+            // There are no elements for this iterator to loop on
+            //
+            verifyException("org.apache.commons.collections4.iterators.LoopingListIterator", e);
+        }
+    }
+}
