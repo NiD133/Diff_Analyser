c3abdffb9077578d9eb163c9851d5376e0ef7d5e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:09:46 +0800

    Original processedAutoTestCases proxyreader_estest44.java

---
+++ proxyreader_estest44.java
@@ -0,0 +1,33 @@
+package org.apache.commons.io.input;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.CharArrayWriter;
+import java.io.IOException;
+import java.io.PipedReader;
+import java.io.StringReader;
+import java.nio.CharBuffer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockIOException;
+import org.junit.runner.RunWith;
+
+public class ProxyReader_ESTestTest44 extends ProxyReader_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test43() throws Throwable {
+        StringReader stringReader0 = new StringReader("6+pe[XK?~jcz*N&o]");
+        TaggedReader taggedReader0 = new TaggedReader(stringReader0);
+        // Undeclared exception!
+        try {
+            taggedReader0.mark((-1520));
+            fail("Expecting exception: IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //
+            // Read-ahead limit < 0
+            //
+            verifyException("java.io.StringReader", e);
+        }
+    }
+}
