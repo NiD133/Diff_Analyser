32534f0070240b213476dfda6fe1ec47c3b43fa1
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:07:22 +0800

    Original processedAutoTestCases parameter_estest16.java

---
+++ parameter_estest16.java
@@ -0,0 +1,30 @@
+package com.google.common.reflect;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.lang.annotation.Annotation;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class Parameter_ESTestTest16 extends Parameter_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test15() throws Throwable {
+        Annotation[] annotationArray0 = new Annotation[0];
+        Parameter parameter0 = new Parameter((Invokable<?, ?>) null, (-3558), (TypeToken<?>) null, annotationArray0, (Object) null);
+        // Undeclared exception!
+        try {
+            parameter0.equals(parameter0);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("com.google.common.reflect.Parameter", e);
+        }
+    }
+}
