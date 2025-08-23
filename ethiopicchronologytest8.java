a94446522d63e385d9bf286c87dab55f905a1616
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:28:33 +0800

    gemini processedManualTestCases ethiopicchronologytest8.java

--- ethiopicchronologytest8.java
+++ ethiopicchronologytest8.java
@@ -1,141 +1,97 @@
 package org.joda.time.chrono;
 
-import java.util.Locale;
-import java.util.TimeZone;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.joda.time.Chronology;
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeConstants;
-import org.joda.time.DateTimeField;
-import org.joda.time.DateTimeUtils;
 import org.joda.time.DateTimeZone;
-import org.joda.time.DurationField;
-import org.joda.time.DurationFieldType;
-import org.joda.time.DateTime.Property;
+import org.junit.Test;
 
-public class EthiopicChronologyTestTest8 extends TestCase {
+/**
+ * Unit tests for the duration fields of {@link EthiopicChronology}.
+ * <p>
+ * This test class focuses on verifying the name, support status, and precision
+ * of each duration field, and how precision is affected by the time zone.
+ */
+public class EthiopicChronologyDurationFieldsTest {
 
-    private static final int MILLIS_PER_DAY = DateTimeConstants.MILLIS_PER_DAY;
-
-    private static long SKIP = 1 * MILLIS_PER_DAY;
-
-    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
-
-    private static final DateTimeZone LONDON = DateTimeZone.forID("Europe/London");
-
-    private static final DateTimeZone TOKYO = DateTimeZone.forID("Asia/Tokyo");
-
+    private static final DateTimeZone ZONE_LONDON = DateTimeZone.forID("Europe/London");
     private static final Chronology ETHIOPIC_UTC = EthiopicChronology.getInstanceUTC();
+    private static final Chronology ETHIOPIC_LONDON = EthiopicChronology.getInstance(ZONE_LONDON);
 
-    private static final Chronology JULIAN_UTC = JulianChronology.getInstanceUTC();
-
-    private static final Chronology ISO_UTC = ISOChronology.getInstanceUTC();
-
-    long y2002days = 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365;
-
-    // 2002-06-09
-    private long TEST_TIME_NOW = (y2002days + 31L + 28L + 31L + 30L + 31L + 9L - 1L) * MILLIS_PER_DAY;
-
-    private DateTimeZone originalDateTimeZone = null;
-
-    private TimeZone originalTimeZone = null;
-
-    private Locale originalLocale = null;
-
-    public static void main(String[] args) {
-        junit.textui.TestRunner.run(suite());
+    @Test
+    public void testFieldNamesAreCorrect() {
+        assertEquals("eras", ETHIOPIC_UTC.eras().getName());
+        assertEquals("centuries", ETHIOPIC_UTC.centuries().getName());
+        assertEquals("years", ETHIOPIC_UTC.years().getName());
+        assertEquals("weekyears", ETHIOPIC_UTC.weekyears().getName());
+        assertEquals("months", ETHIOPIC_UTC.months().getName());
+        assertEquals("weeks", ETHIOPIC_UTC.weeks().getName());
+        assertEquals("days", ETHIOPIC_UTC.days().getName());
+        assertEquals("halfdays", ETHIOPIC_UTC.halfdays().getName());
+        assertEquals("hours", ETHIOPIC_UTC.hours().getName());
+        assertEquals("minutes", ETHIOPIC_UTC.minutes().getName());
+        assertEquals("seconds", ETHIOPIC_UTC.seconds().getName());
+        assertEquals("millis", ETHIOPIC_UTC.millis().getName());
     }
 
-    public static TestSuite suite() {
-        SKIP = 1 * MILLIS_PER_DAY;
-        return new TestSuite(TestEthiopicChronology.class);
+    @Test
+    public void testFieldSupport() {
+        assertFalse("Eras field should not be supported", ETHIOPIC_UTC.eras().isSupported());
+        
+        assertTrue("Centuries field should be supported", ETHIOPIC_UTC.centuries().isSupported());
+        assertTrue("Years field should be supported", ETHIOPIC_UTC.years().isSupported());
+        assertTrue("Weekyears field should be supported", ETHIOPIC_UTC.weekyears().isSupported());
+        assertTrue("Months field should be supported", ETHIOPIC_UTC.months().isSupported());
+        assertTrue("Weeks field should be supported", ETHIOPIC_UTC.weeks().isSupported());
+        assertTrue("Days field should be supported", ETHIOPIC_UTC.days().isSupported());
+        assertTrue("Halfdays field should be supported", ETHIOPIC_UTC.halfdays().isSupported());
+        assertTrue("Hours field should be supported", ETHIOPIC_UTC.hours().isSupported());
+        assertTrue("Minutes field should be supported", ETHIOPIC_UTC.minutes().isSupported());
+        assertTrue("Seconds field should be supported", ETHIOPIC_UTC.seconds().isSupported());
+        assertTrue("Millis field should be supported", ETHIOPIC_UTC.millis().isSupported());
     }
 
-    @Override
-    protected void setUp() throws Exception {
-        DateTimeUtils.setCurrentMillisFixed(TEST_TIME_NOW);
-        originalDateTimeZone = DateTimeZone.getDefault();
-        originalTimeZone = TimeZone.getDefault();
-        originalLocale = Locale.getDefault();
-        DateTimeZone.setDefault(LONDON);
-        TimeZone.setDefault(TimeZone.getTimeZone("Europe/London"));
-        Locale.setDefault(Locale.UK);
+    @Test
+    public void testPrecisionInFixedOffsetZone() {
+        // In a fixed-offset zone like UTC, date-based fields are precise if their
+        // length in milliseconds is constant.
+        assertTrue("In UTC, weeks are precise", ETHIOPIC_UTC.weeks().isPrecise());
+        assertTrue("In UTC, days are precise", ETHIOPIC_UTC.days().isPrecise());
+        assertTrue("In UTC, halfdays are precise", ETHIOPIC_UTC.halfdays().isPrecise());
+
+        // Time-based fields are always precise.
+        assertTrue("Hours are always precise", ETHIOPIC_UTC.hours().isPrecise());
+        assertTrue("Minutes are always precise", ETHIOPIC_UTC.minutes().isPrecise());
+        assertTrue("Seconds are always precise", ETHIOPIC_UTC.seconds().isPrecise());
+        assertTrue("Millis are always precise", ETHIOPIC_UTC.millis().isPrecise());
+
+        // Fields with variable length (due to leap years) are never precise.
+        assertFalse("Centuries are not precise", ETHIOPIC_UTC.centuries().isPrecise());
+        assertFalse("Years are not precise", ETHIOPIC_UTC.years().isPrecise());
+        assertFalse("Weekyears are not precise", ETHIOPIC_UTC.weekyears().isPrecise());
+        assertFalse("Months are not precise", ETHIOPIC_UTC.months().isPrecise());
     }
 
-    @Override
-    protected void tearDown() throws Exception {
-        DateTimeUtils.setCurrentMillisSystem();
-        DateTimeZone.setDefault(originalDateTimeZone);
-        TimeZone.setDefault(originalTimeZone);
-        Locale.setDefault(originalLocale);
-        originalDateTimeZone = null;
-        originalTimeZone = null;
-        originalLocale = null;
-    }
+    @Test
+    public void testPrecisionInVariableOffsetZone() {
+        // In a zone with DST (a variable offset), date-based fields are imprecise
+        // because a "day" can have a different number of hours across transitions.
+        assertFalse("In a DST zone, weeks are not precise", ETHIOPIC_LONDON.weeks().isPrecise());
+        assertFalse("In a DST zone, days are not precise", ETHIOPIC_LONDON.days().isPrecise());
+        assertFalse("In a DST zone, halfdays are not precise", ETHIOPIC_LONDON.halfdays().isPrecise());
 
-    //-----------------------------------------------------------------------
-    public void testDurationFields() {
-        final EthiopicChronology ethiopic = EthiopicChronology.getInstance();
-        assertEquals("eras", ethiopic.eras().getName());
-        assertEquals("centuries", ethiopic.centuries().getName());
-        assertEquals("years", ethiopic.years().getName());
-        assertEquals("weekyears", ethiopic.weekyears().getName());
-        assertEquals("months", ethiopic.months().getName());
-        assertEquals("weeks", ethiopic.weeks().getName());
-        assertEquals("days", ethiopic.days().getName());
-        assertEquals("halfdays", ethiopic.halfdays().getName());
-        assertEquals("hours", ethiopic.hours().getName());
-        assertEquals("minutes", ethiopic.minutes().getName());
-        assertEquals("seconds", ethiopic.seconds().getName());
-        assertEquals("millis", ethiopic.millis().getName());
-        assertEquals(false, ethiopic.eras().isSupported());
-        assertEquals(true, ethiopic.centuries().isSupported());
-        assertEquals(true, ethiopic.years().isSupported());
-        assertEquals(true, ethiopic.weekyears().isSupported());
-        assertEquals(true, ethiopic.months().isSupported());
-        assertEquals(true, ethiopic.weeks().isSupported());
-        assertEquals(true, ethiopic.days().isSupported());
-        assertEquals(true, ethiopic.halfdays().isSupported());
-        assertEquals(true, ethiopic.hours().isSupported());
-        assertEquals(true, ethiopic.minutes().isSupported());
-        assertEquals(true, ethiopic.seconds().isSupported());
-        assertEquals(true, ethiopic.millis().isSupported());
-        assertEquals(false, ethiopic.centuries().isPrecise());
-        assertEquals(false, ethiopic.years().isPrecise());
-        assertEquals(false, ethiopic.weekyears().isPrecise());
-        assertEquals(false, ethiopic.months().isPrecise());
-        assertEquals(false, ethiopic.weeks().isPrecise());
-        assertEquals(false, ethiopic.days().isPrecise());
-        assertEquals(false, ethiopic.halfdays().isPrecise());
-        assertEquals(true, ethiopic.hours().isPrecise());
-        assertEquals(true, ethiopic.minutes().isPrecise());
-        assertEquals(true, ethiopic.seconds().isPrecise());
-        assertEquals(true, ethiopic.millis().isPrecise());
-        final EthiopicChronology ethiopicUTC = EthiopicChronology.getInstanceUTC();
-        assertEquals(false, ethiopicUTC.centuries().isPrecise());
-        assertEquals(false, ethiopicUTC.years().isPrecise());
-        assertEquals(false, ethiopicUTC.weekyears().isPrecise());
-        assertEquals(false, ethiopicUTC.months().isPrecise());
-        assertEquals(true, ethiopicUTC.weeks().isPrecise());
-        assertEquals(true, ethiopicUTC.days().isPrecise());
-        assertEquals(true, ethiopicUTC.halfdays().isPrecise());
-        assertEquals(true, ethiopicUTC.hours().isPrecise());
-        assertEquals(true, ethiopicUTC.minutes().isPrecise());
-        assertEquals(true, ethiopicUTC.seconds().isPrecise());
-        assertEquals(true, ethiopicUTC.millis().isPrecise());
-        final DateTimeZone gmt = DateTimeZone.forID("Etc/GMT");
-        final EthiopicChronology ethiopicGMT = EthiopicChronology.getInstance(gmt);
-        assertEquals(false, ethiopicGMT.centuries().isPrecise());
-        assertEquals(false, ethiopicGMT.years().isPrecise());
-        assertEquals(false, ethiopicGMT.weekyears().isPrecise());
-        assertEquals(false, ethiopicGMT.months().isPrecise());
-        assertEquals(true, ethiopicGMT.weeks().isPrecise());
-        assertEquals(true, ethiopicGMT.days().isPrecise());
-        assertEquals(true, ethiopicGMT.halfdays().isPrecise());
-        assertEquals(true, ethiopicGMT.hours().isPrecise());
-        assertEquals(true, ethiopicGMT.minutes().isPrecise());
-        assertEquals(true, ethiopicGMT.seconds().isPrecise());
-        assertEquals(true, ethiopicGMT.millis().isPrecise());
+        // Time-based fields are always precise.
+        assertTrue("Hours are always precise", ETHIOPIC_LONDON.hours().isPrecise());
+        assertTrue("Minutes are always precise", ETHIOPIC_LONDON.minutes().isPrecise());
+        assertTrue("Seconds are always precise", ETHIOPIC_LONDON.seconds().isPrecise());
+        assertTrue("Millis are always precise", ETHIOPIC_LONDON.millis().isPrecise());
+
+        // Fields with variable length (due to leap years) are never precise.
+        assertFalse("Centuries are not precise", ETHIOPIC_LONDON.centuries().isPrecise());
+        assertFalse("Years are not precise", ETHIOPIC_LONDON.years().isPrecise());
+        assertFalse("Weekyears are not precise", ETHIOPIC_LONDON.weekyears().isPrecise());
+        assertFalse("Months are not precise", ETHIOPIC_LONDON.months().isPrecise());
     }
 }
