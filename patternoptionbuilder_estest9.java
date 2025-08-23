200fbc397c9c2d46701a0117c41408415533197f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:07:46 +0800

    gemini processedAutoTestCases patternoptionbuilder_estest9.java

--- patternoptionbuilder_estest9.java
+++ patternoptionbuilder_estest9.java
@@ -1,17 +1,21 @@
 package org.apache.commons.cli;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertTrue;
 
-public class PatternOptionBuilder_ESTestTest9 extends PatternOptionBuilder_ESTest_scaffolding {
+/**
+ * Tests for {@link PatternOptionBuilder}.
+ */
+public class PatternOptionBuilderTest {
 
-    @Test(timeout = 4000)
-    public void test08() throws Throwable {
-        boolean boolean0 = PatternOptionBuilder.isValueCode('@');
-        assertTrue(boolean0);
+    /**
+     * Tests that the '@' character is correctly identified as a value code.
+     * According to the PatternOptionBuilder documentation, '@' represents an object type.
+     */
+    @Test
+    public void isValueCode_shouldReturnTrue_forAtSymbol() {
+        // The '@' character is a value code representing an Object type argument.
+        assertTrue("The '@' character should be recognized as a value code.",
+                   PatternOptionBuilder.isValueCode('@'));
     }
 }
