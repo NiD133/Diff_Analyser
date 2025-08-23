9a441eb4f40d41a5e389ca75c685ae3f7bdc0687
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:10:46 +0800

    Original processedAutoTestCases quotedprintablecodec_estest64.java

---
+++ quotedprintablecodec_estest64.java
@@ -0,0 +1,26 @@
+package org.apache.commons.codec.net;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.IllegalCharsetNameException;
+import java.nio.charset.UnsupportedCharsetException;
+import java.util.BitSet;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class QuotedPrintableCodec_ESTestTest64 extends QuotedPrintableCodec_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test63() throws Throwable {
+        byte[] byteArray0 = new byte[27];
+        byte[] byteArray1 = QuotedPrintableCodec.encodeQuotedPrintable((BitSet) null, byteArray0, true);
+        byte[] byteArray2 = QuotedPrintableCodec.decodeQuotedPrintable(byteArray1);
+        assertEquals(27, byteArray2.length);
+        assertNotNull(byteArray2);
+        assertEquals(84, byteArray1.length);
+    }
+}
