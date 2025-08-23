dc30de6bab8279449cb2b0cd955b38020645cda3
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:01:09 +0800

    gemini processedAutoTestCases localeutils_estest43.java

--- localeutils_estest43.java
+++ localeutils_estest43.java
@@ -1,19 +1,31 @@
 package org.apache.commons.lang3;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.List;
-import java.util.Locale;
-import java.util.Set;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertNotNull;
 
-public class LocaleUtils_ESTestTest43 extends LocaleUtils_ESTest_scaffolding {
+/**
+ * Unit tests for {@link org.apache.commons.lang3.LocaleUtils}.
+ */
+public class LocaleUtilsTest {
 
-    @Test(timeout = 4000)
-    public void test42() throws Throwable {
-        LocaleUtils localeUtils0 = new LocaleUtils();
+    /**
+     * Tests the public, deprecated constructor.
+     * <p>
+     * The {@link LocaleUtils} class is a utility class with static methods and is not
+     * intended to be instantiated. However, it has a public constructor for compatibility
+     * with tools that require a JavaBean instance.
+     * </p>
+     * <p>
+     * This test confirms that the constructor can be called without throwing an exception,
+     * primarily for code coverage purposes.
+     * </p>
+     */
+    @Test
+    public void testConstructor() {
+        // Create an instance to ensure the public constructor is covered.
+        final LocaleUtils instance = new LocaleUtils();
+        
+        // Assert that the instance is created successfully.
+        assertNotNull("A new instance should have been created.", instance);
     }
 }
