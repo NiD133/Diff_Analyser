c5003200e7966207631ada883ac92bf33300594b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:37:40 +0800

    Original processedAutoTestCases arraysorter_estest25.java

---
+++ arraysorter_estest25.java
@@ -0,0 +1,20 @@
+package org.apache.commons.lang3;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.Comparator;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class ArraySorter_ESTestTest25 extends ArraySorter_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test24() throws Throwable {
+        double[] doubleArray0 = ArraySorter.sort((double[]) null);
+        assertNull(doubleArray0);
+    }
+}
