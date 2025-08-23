8c578d02ed4171e733081dcf7486d6c3eef5ef27
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:52:37 +0800

    gemini processedAutoTestCases gridarrangement_estest79.java

--- gridarrangement_estest79.java
+++ gridarrangement_estest79.java
@@ -1,38 +1,29 @@
 package org.jfree.chart.block;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.awt.Graphics2D;
-import java.awt.SystemColor;
-import java.time.temporal.ChronoUnit;
-import java.util.Calendar;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.util.MockGregorianCalendar;
-import org.jfree.chart.api.HorizontalAlignment;
-import org.jfree.chart.api.RectangleAnchor;
-import org.jfree.chart.api.VerticalAlignment;
-import org.jfree.chart.text.TextBlockAnchor;
-import org.jfree.data.Range;
-import org.jfree.data.time.TimePeriodAnchor;
-import org.jfree.data.time.TimeSeries;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
 
-public class GridArrangement_ESTestTest79 extends GridArrangement_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link GridArrangement} class, focusing on object equality.
+ */
+public class GridArrangementEqualityTest {
 
-    @Test(timeout = 4000)
-    public void test78() throws Throwable {
-        GridArrangement gridArrangement0 = new GridArrangement(0, 0);
-        assertNotNull(gridArrangement0);
-        GridArrangement gridArrangement1 = new GridArrangement(0, 0);
-        assertNotNull(gridArrangement1);
-        assertTrue(gridArrangement1.equals((Object) gridArrangement0));
-        boolean boolean0 = gridArrangement0.equals(gridArrangement1);
-        assertNotSame(gridArrangement0, gridArrangement1);
-        assertNotSame(gridArrangement1, gridArrangement0);
-        assertTrue(boolean0);
-        assertTrue(gridArrangement0.equals((Object) gridArrangement1));
-        assertTrue(gridArrangement1.equals((Object) gridArrangement0));
+    /**
+     * Verifies that two GridArrangement instances created with the same number of rows
+     * and columns are considered equal in value.
+     */
+    @Test
+    public void equals_shouldReturnTrue_whenObjectsHaveSameDimensions() {
+        // Arrange: Create two separate GridArrangement objects with identical dimensions.
+        GridArrangement arrangement1 = new GridArrangement(0, 0);
+        GridArrangement arrangement2 = new GridArrangement(0, 0);
+
+        // Assert: The objects should be equal in value but must be different instances.
+        assertNotSame("The two instances should be different objects in memory.", arrangement1, arrangement2);
+        assertEquals("Instances with the same dimensions should be considered equal.", arrangement1, arrangement2);
+        
+        // As per the equals() contract, equal objects must also have equal hash codes.
+        assertEquals("Equal objects must have the same hash code.", arrangement1.hashCode(), arrangement2.hashCode());
     }
 }
