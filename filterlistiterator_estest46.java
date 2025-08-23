d16b770910f607345c3786d605cb8301c49611ff
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:51:13 +0800

    Original processedAutoTestCases filterlistiterator_estest46.java

---
+++ filterlistiterator_estest46.java
@@ -0,0 +1,69 @@
+package org.apache.commons.collections4.iterators;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.lang.reflect.Array;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.ConcurrentModificationException;
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.function.Consumer;
+import org.apache.commons.collections4.Closure;
+import org.apache.commons.collections4.Equator;
+import org.apache.commons.collections4.Predicate;
+import org.apache.commons.collections4.Transformer;
+import org.apache.commons.collections4.functors.AnyPredicate;
+import org.apache.commons.collections4.functors.ClosureTransformer;
+import org.apache.commons.collections4.functors.ComparatorPredicate;
+import org.apache.commons.collections4.functors.DefaultEquator;
+import org.apache.commons.collections4.functors.EqualPredicate;
+import org.apache.commons.collections4.functors.ExceptionClosure;
+import org.apache.commons.collections4.functors.ExceptionPredicate;
+import org.apache.commons.collections4.functors.IdentityPredicate;
+import org.apache.commons.collections4.functors.IfClosure;
+import org.apache.commons.collections4.functors.InstanceofPredicate;
+import org.apache.commons.collections4.functors.InvokerTransformer;
+import org.apache.commons.collections4.functors.MapTransformer;
+import org.apache.commons.collections4.functors.NonePredicate;
+import org.apache.commons.collections4.functors.NotNullPredicate;
+import org.apache.commons.collections4.functors.NotPredicate;
+import org.apache.commons.collections4.functors.NullIsExceptionPredicate;
+import org.apache.commons.collections4.functors.NullIsFalsePredicate;
+import org.apache.commons.collections4.functors.NullIsTruePredicate;
+import org.apache.commons.collections4.functors.NullPredicate;
+import org.apache.commons.collections4.functors.OnePredicate;
+import org.apache.commons.collections4.functors.TransformedPredicate;
+import org.apache.commons.collections4.functors.TransformerPredicate;
+import org.apache.commons.collections4.functors.TruePredicate;
+import org.apache.commons.collections4.functors.UniquePredicate;
+import org.apache.commons.collections4.functors.WhileClosure;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class FilterListIterator_ESTestTest46 extends FilterListIterator_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test45() throws Throwable {
+        FilterListIterator<Integer> filterListIterator0 = new FilterListIterator<Integer>();
+        assertEquals((-1), filterListIterator0.previousIndex());
+        FilterListIterator<Object> filterListIterator1 = new FilterListIterator<Object>(filterListIterator0);
+        assertEquals((-1), filterListIterator1.previousIndex());
+        // Undeclared exception!
+        try {
+            filterListIterator1.next();
+            fail("Expecting exception: NoSuchElementException");
+        } catch (NoSuchElementException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.apache.commons.collections4.iterators.FilterListIterator", e);
+        }
+    }
+}
