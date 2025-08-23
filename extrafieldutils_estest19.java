0ff05af70ab356c16b882dd409bd243f2433d516
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:49:56 +0800

    Original processedAutoTestCases extrafieldutils_estest19.java

---
+++ extrafieldutils_estest19.java
@@ -0,0 +1,27 @@
+package org.apache.commons.compress.archivers.zip;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.zip.ZipException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class ExtraFieldUtils_ESTestTest19 extends ExtraFieldUtils_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test18() throws Throwable {
+        ZipExtraField[] zipExtraFieldArray0 = new ZipExtraField[2];
+        // Undeclared exception!
+        try {
+            ExtraFieldUtils.mergeCentralDirectoryData(zipExtraFieldArray0);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.apache.commons.compress.archivers.zip.ExtraFieldUtils", e);
+        }
+    }
+}
