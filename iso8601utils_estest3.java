9f5c491cef051a79924b91b66b536f5a4cb7f9dd
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:55:34 +0800

    gemini processedAutoTestCases iso8601utils_estest3.java

--- iso8601utils_estest3.java
+++ iso8601utils_estest3.java
@@ -1,31 +1,39 @@
 package com.google.gson.internal.bind.util;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
+
 import java.text.ParseException;
 import java.text.ParsePosition;
-import java.util.Date;
-import java.util.SimpleTimeZone;
-import java.util.TimeZone;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.util.MockDate;
-import org.junit.runner.RunWith;
 
-public class ISO8601Utils_ESTestTest3 extends ISO8601Utils_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
-    @Test(timeout = 4000)
-    public void test02() throws Throwable {
-        ParsePosition parsePosition0 = new ParsePosition(1);
+/**
+ * Tests for {@link ISO8601Utils}.
+ */
+public class ISO8601UtilsTest {
+
+    /**
+     * Verifies that parsing a string with an invalid numeric component
+     * throws a ParseException with a descriptive message.
+     */
+    @Test
+    public void parse_withInvalidNumericComponent_throwsParseException() {
+        // Arrange
+        String invalidDateString = "p;72<&YyPko{%";
+        // The test will start parsing from index 1, attempting to read ";72<" as a year.
+        ParsePosition position = new ParsePosition(1);
+
+        // Act & Assert
         try {
-            ISO8601Utils.parse("p;72<&YyPko{%", parsePosition0);
-            fail("Expecting exception: ParseException");
+            ISO8601Utils.parse(invalidDateString, position);
+            fail("A ParseException was expected but was not thrown.");
         } catch (ParseException e) {
-            //
-            // Failed to parse date [\"p;72<&YyPko{%\"]: Invalid number: ;72<
-            //
-            verifyException("com.google.gson.internal.bind.util.ISO8601Utils", e);
+            // The parser should fail because it expects a number but finds an invalid sequence.
+            String expectedMessage = "Failed to parse date [\"" + invalidDateString + "\"]: Invalid number: ;72<";
+            
+            assertEquals("The exception message should detail the parsing failure.", expectedMessage, e.getMessage());
+            assertEquals("The error offset should point to the start of the invalid token.", 1, e.getErrorOffset());
         }
     }
 }
