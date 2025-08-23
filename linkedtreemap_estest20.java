eb987443c6f8be8fd6b63797b7f553a52048e4c2
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:00:26 +0800

    gemini processedAutoTestCases linkedtreemap_estest20.java

--- linkedtreemap_estest20.java
+++ linkedtreemap_estest20.java
@@ -1,37 +1,39 @@
 package com.google.gson.internal;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.AbstractMap;
-import java.util.Comparator;
-import java.util.Map;
-import java.util.Set;
-import java.util.function.BiFunction;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertThrows;
 
-public class LinkedTreeMap_ESTestTest20 extends LinkedTreeMap_ESTest_scaffolding {
+/**
+ * This test class contains an improved version of the original test case.
+ */
+public class LinkedTreeMapImprovedTest {
 
-    @Test(timeout = 4000)
-    public void test19() throws Throwable {
-        LinkedTreeMap<Object, Object> linkedTreeMap0 = new LinkedTreeMap<Object, Object>();
-        LinkedTreeMap.Node<Object, Object> linkedTreeMap_Node0 = new LinkedTreeMap.Node<Object, Object>(false);
-        LinkedTreeMap.Node<Object, Object> linkedTreeMap_Node1 = new LinkedTreeMap.Node<Object, Object>(false, linkedTreeMap_Node0, linkedTreeMap0, linkedTreeMap_Node0, linkedTreeMap_Node0);
-        LinkedTreeMap.Node<Object, Object> linkedTreeMap_Node2 = linkedTreeMap_Node1.first();
-        linkedTreeMap0.root = linkedTreeMap_Node2;
-        Integer integer0 = new Integer((-1));
-        // Undeclared exception!
-        try {
-            linkedTreeMap0.put(integer0, integer0);
-            fail("Expecting exception: ClassCastException");
-        } catch (ClassCastException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-        }
+    /**
+     * Tests that a ClassCastException is thrown when attempting to insert a key
+     * that is not mutually comparable with existing keys in the map.
+     *
+     * A LinkedTreeMap created with the default constructor uses natural ordering,
+     * which requires all keys to implement the Comparable interface and be
+     * comparable to one another.
+     */
+    @Test
+    public void putWithIncompatibleKeyTypeThrowsClassCastException() {
+        // Arrange: Create a map that uses natural ordering for its keys.
+        // This requires all keys to be of a compatible, comparable type.
+        LinkedTreeMap<Object, String> map = new LinkedTreeMap<>();
+
+        // Add an initial entry with a String key. All subsequent keys will be
+        // compared to this key.
+        map.put("a string key", "value1");
+
+        // Act & Assert: Attempt to add an entry with an incompatible key type (Integer).
+        // This should fail because an Integer cannot be compared to a String.
+        Integer incompatibleKey = 123;
+
+        // The map's internal logic will try to execute `((Comparable)incompatibleKey).compareTo("a string key")`,
+        // which results in a ClassCastException.
+        assertThrows(ClassCastException.class, () -> {
+            map.put(incompatibleKey, "value2");
+        });
     }
 }
