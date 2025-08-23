9d72bea12302439992181aa9d770b21491bc9a52
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:27:55 +0800

    Original processedManualTestCases defaultexceptioncontexttest3.java

---
+++ defaultexceptioncontexttest3.java
@@ -0,0 +1,30 @@
+package org.apache.commons.lang3.exception;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.apache.commons.lang3.ObjectToStringRuntimeException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class DefaultExceptionContextTestTest3 extends AbstractExceptionContextTest<DefaultExceptionContext> {
+
+    @Override
+    @BeforeEach
+    public void setUp() throws Exception {
+        exceptionContext = new DefaultExceptionContext();
+        super.setUp();
+    }
+
+    @Test
+    void testFormattedExceptionMessageNullValue() {
+        exceptionContext = new DefaultExceptionContext();
+        final String label1 = "throws 1";
+        final String label2 = "throws 2";
+        exceptionContext.addContextValue(label1, null);
+        exceptionContext.addContextValue(label2, null);
+        final String message = exceptionContext.getFormattedExceptionMessage(TEST_MESSAGE);
+        assertTrue(message.startsWith(TEST_MESSAGE));
+        assertTrue(message.contains(label1));
+        assertTrue(message.contains(label2));
+    }
+}
