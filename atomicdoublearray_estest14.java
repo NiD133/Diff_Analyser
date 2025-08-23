f6ebaf9bd8feba27ba5c985812618282ac70bec3
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:37:48 +0800

    Original processedAutoTestCases atomicdoublearray_estest14.java

---
+++ atomicdoublearray_estest14.java
@@ -0,0 +1,27 @@
+package com.google.common.util.concurrent;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.function.DoubleBinaryOperator;
+import java.util.function.DoubleUnaryOperator;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class AtomicDoubleArray_ESTestTest14 extends AtomicDoubleArray_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test13() throws Throwable {
+        double[] doubleArray0 = new double[6];
+        AtomicDoubleArray atomicDoubleArray0 = new AtomicDoubleArray(doubleArray0);
+        DoubleBinaryOperator doubleBinaryOperator0 = mock(DoubleBinaryOperator.class, new ViolatedAssumptionAnswer());
+        doReturn((-1.0)).when(doubleBinaryOperator0).applyAsDouble(anyDouble(), anyDouble());
+        double double0 = atomicDoubleArray0.getAndAccumulate(1, (-2032.2), doubleBinaryOperator0);
+        assertEquals(0.0, double0, 0.01);
+        double double1 = atomicDoubleArray0.get(1);
+        assertEquals((-1.0), double1, 0.01);
+    }
+}
