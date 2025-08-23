4652f64227a53db3ec3f24aa58b589f68cc145e8
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:47:06 +0800

    Original processedAutoTestCases dayofyear_estest5.java

---
+++ dayofyear_estest5.java
@@ -0,0 +1,48 @@
+package org.threeten.extra;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.time.Clock;
+import java.time.DateTimeException;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Year;
+import java.time.YearMonth;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.chrono.ChronoLocalDate;
+import java.time.chrono.HijrahDate;
+import java.time.chrono.ThaiBuddhistDate;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.Temporal;
+import java.time.temporal.TemporalAccessor;
+import java.time.temporal.TemporalField;
+import java.time.temporal.TemporalQuery;
+import java.time.temporal.UnsupportedTemporalTypeException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.time.MockClock;
+import org.evosuite.runtime.mock.java.time.MockYear;
+import org.evosuite.runtime.mock.java.time.MockYearMonth;
+import org.evosuite.runtime.mock.java.time.MockZonedDateTime;
+import org.evosuite.runtime.mock.java.time.chrono.MockHijrahDate;
+import org.evosuite.runtime.mock.java.time.chrono.MockThaiBuddhistDate;
+import org.junit.runner.RunWith;
+
+public class DayOfYear_ESTestTest5 extends DayOfYear_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test04() throws Throwable {
+        DayOfYear dayOfYear0 = DayOfYear.of(1);
+        Clock clock0 = MockClock.systemDefaultZone();
+        DayOfYear dayOfYear1 = DayOfYear.now(clock0);
+        int int0 = dayOfYear0.compareTo(dayOfYear1);
+        assertEquals((-44), int0);
+    }
+}
