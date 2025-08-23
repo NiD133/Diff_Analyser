af495c7bc07cb9d7dd23276a4d891642bd35c57d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:00:48 +0800

    Original processedAutoTestCases listeningexecutorservice_estest2.java

---
+++ listeningexecutorservice_estest2.java
@@ -0,0 +1,40 @@
+package com.google.common.util.concurrent;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.time.Duration;
+import java.time.temporal.ChronoField;
+import java.time.temporal.TemporalUnit;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.LinkedTransferQueue;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class ListeningExecutorService_ESTestTest2 extends ListeningExecutorService_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test1() throws Throwable {
+        DirectExecutorService directExecutorService0 = new DirectExecutorService();
+        LinkedBlockingQueue<Callable<Object>> linkedBlockingQueue0 = new LinkedBlockingQueue<Callable<Object>>();
+        // Undeclared exception!
+        try {
+            directExecutorService0.invokeAny((Collection<? extends Callable<Object>>) linkedBlockingQueue0, (Duration) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("com.google.common.util.concurrent.Internal", e);
+        }
+    }
+}
