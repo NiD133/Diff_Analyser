3150e83c94336e1b369542ff1a24ea70afbc0432
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:06:13 +0800

    Original processedAutoTestCases optionbuilder_estest2.java

---
+++ optionbuilder_estest2.java
@@ -0,0 +1,18 @@
+package org.apache.commons.cli;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class OptionBuilder_ESTestTest2 extends OptionBuilder_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test01() throws Throwable {
+        OptionBuilder.isRequired(true);
+        Option option0 = OptionBuilder.create("converterMap");
+        assertEquals((-1), option0.getArgs());
+    }
+}
