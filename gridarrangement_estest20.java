8eeec38c2f434176ca3c44e2071eca4d45373152
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:52:17 +0800

    Original processedAutoTestCases gridarrangement_estest20.java

---
+++ gridarrangement_estest20.java
@@ -0,0 +1,50 @@
+package org.jfree.chart.block;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.awt.Graphics2D;
+import java.awt.SystemColor;
+import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.util.MockGregorianCalendar;
+import org.jfree.chart.api.HorizontalAlignment;
+import org.jfree.chart.api.RectangleAnchor;
+import org.jfree.chart.api.VerticalAlignment;
+import org.jfree.chart.text.TextBlockAnchor;
+import org.jfree.data.Range;
+import org.jfree.data.time.TimePeriodAnchor;
+import org.jfree.data.time.TimeSeries;
+import org.junit.runner.RunWith;
+
+public class GridArrangement_ESTestTest20 extends GridArrangement_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test19() throws Throwable {
+        GridArrangement gridArrangement0 = new GridArrangement(123, 123);
+        assertNotNull(gridArrangement0);
+        BlockContainer blockContainer0 = new BlockContainer(gridArrangement0);
+        assertEquals(0.0, blockContainer0.getWidth(), 0.01);
+        assertTrue(blockContainer0.isEmpty());
+        assertNull(blockContainer0.getID());
+        assertEquals(0.0, blockContainer0.getContentYOffset(), 0.01);
+        assertEquals(0.0, blockContainer0.getHeight(), 0.01);
+        assertEquals(0.0, blockContainer0.getContentXOffset(), 0.01);
+        assertNotNull(blockContainer0);
+        Size2D size2D0 = gridArrangement0.arrangeNN(blockContainer0, (Graphics2D) null);
+        assertEquals(0.0, blockContainer0.getWidth(), 0.01);
+        assertTrue(blockContainer0.isEmpty());
+        assertNull(blockContainer0.getID());
+        assertEquals(0.0, blockContainer0.getContentYOffset(), 0.01);
+        assertEquals(0.0, blockContainer0.getHeight(), 0.01);
+        assertEquals(0.0, blockContainer0.getContentXOffset(), 0.01);
+        assertEquals(0.0, size2D0.getWidth(), 0.01);
+        assertEquals("Size2D[width=0.0, height=0.0]", size2D0.toString());
+        assertEquals(0.0, size2D0.getHeight(), 0.01);
+        assertNotNull(size2D0);
+        assertEquals(0.0, size2D0.height, 0.01);
+        assertEquals(0.0, size2D0.width, 0.01);
+    }
+}
