ab89e9c1414d248329358b7233a0a12bf93f79a2
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:41:59 +0800

    gemini processedAutoTestCases charrange_estest16.java

--- charrange_estest16.java
+++ charrange_estest16.java
@@ -1,25 +1,28 @@
 package org.apache.commons.lang3;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.function.Consumer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
-public class CharRange_ESTestTest16 extends CharRange_ESTest_scaffolding {
+/**
+ * Unit tests for {@link CharRange}.
+ */
+public class CharRangeTest {
 
-    @Test(timeout = 4000)
-    public void test15() throws Throwable {
-        CharRange charRange0 = CharRange.isNot('O');
-        CharRange charRange1 = CharRange.isNot('O');
-        boolean boolean0 = charRange1.equals(charRange0);
-        assertEquals('O', charRange1.getEnd());
-        assertEquals('O', charRange1.getStart());
-        assertTrue(charRange1.isNegated());
-        assertTrue(boolean0);
+    @Test
+    public void testEqualsReturnsTrueForIdenticalNegatedRanges() {
+        // Arrange: Create two identical negated character ranges.
+        // A negated range includes all characters EXCEPT the one specified.
+        final CharRange rangeA = CharRange.isNot('O');
+        final CharRange rangeB = CharRange.isNot('O');
+
+        // Assert: First, confirm the properties of a created range are as expected.
+        assertTrue("Range should be negated", rangeA.isNegated());
+        assertEquals("Start character should be 'O'", 'O', rangeA.getStart());
+        assertEquals("End character should be 'O'", 'O', rangeA.getEnd());
+
+        // Assert: The two identical ranges should be considered equal.
+        // This is the primary goal of this test.
+        assertEquals("Identical negated ranges should be equal", rangeA, rangeB);
     }
 }
