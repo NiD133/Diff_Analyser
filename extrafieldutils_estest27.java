0dd58b3b64a501596c22be37b76ba001f266a2de
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:49:59 +0800

    gemini processedAutoTestCases extrafieldutils_estest27.java

--- extrafieldutils_estest27.java
+++ extrafieldutils_estest27.java
@@ -1,27 +1,32 @@
 package org.apache.commons.compress.archivers.zip;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.zip.ZipException;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class ExtraFieldUtils_ESTestTest27 extends ExtraFieldUtils_ESTest_scaffolding {
+/**
+ * Unit tests for the ExtraFieldUtils class.
+ */
+public class ExtraFieldUtilsTest {
 
-    @Test(timeout = 4000)
-    public void test26() throws Throwable {
-        Zip64ExtendedInformationExtraField zip64ExtendedInformationExtraField0 = new Zip64ExtendedInformationExtraField((ZipEightByteInteger) null, (ZipEightByteInteger) null);
-        // Undeclared exception!
-        try {
-            ExtraFieldUtils.fillExtraField(zip64ExtendedInformationExtraField0, (byte[]) null, (-1811), (-1811), false);
-            fail("Expecting exception: NegativeArraySizeException");
-        } catch (NegativeArraySizeException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.commons.compress.archivers.zip.Zip64ExtendedInformationExtraField", e);
-        }
+    /**
+     * Tests that fillExtraField throws a NegativeArraySizeException when a negative length is provided.
+     * This exception is expected because the underlying ZipExtraField implementation (in this case,
+     * Zip64ExtendedInformationExtraField) will fail when trying to process a buffer with a negative size.
+     */
+    @Test(expected = NegativeArraySizeException.class)
+    public void fillExtraFieldShouldThrowNegativeArraySizeExceptionForNegativeLength() throws ZipException {
+        // Arrange: Create an extra field instance and define invalid (negative) parameters.
+        // The specific field type is important as its parse method will throw the exception.
+        final ZipExtraField extraField = new Zip64ExtendedInformationExtraField(null, null);
+        final byte[] data = null; // The data buffer is not accessed before the length check fails.
+        final int offset = 0;
+        final int negativeLength = -1;
+        final boolean isForCentralDirectory = false;
+
+        // Act: Attempt to fill the extra field with a negative data length.
+        // This call is expected to throw a NegativeArraySizeException from within the extraField's parsing logic.
+        ExtraFieldUtils.fillExtraField(extraField, data, offset, negativeLength, isForCentralDirectory);
+
+        // Assert: The test passes if the expected NegativeArraySizeException is thrown.
+        // This is handled declaratively by the @Test(expected=...) annotation.
     }
 }
