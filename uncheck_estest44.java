ddb3e14b9e450971be19f14da3f9a65885abbf09
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:24:40 +0800

    gemini processedAutoTestCases uncheck_estest44.java

--- uncheck_estest44.java
+++ uncheck_estest44.java
@@ -1,37 +1,20 @@
 package org.apache.commons.io.function;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.time.chrono.HijrahEra;
-import java.util.Comparator;
-import java.util.concurrent.ForkJoinTask;
-import java.util.function.BiConsumer;
-import java.util.function.BinaryOperator;
-import java.util.function.Function;
-import java.util.function.Supplier;
-import java.util.stream.Collector;
-import java.util.stream.LongStream;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
 
-public class Uncheck_ESTestTest44 extends Uncheck_ESTest_scaffolding {
+/**
+ * Tests for {@link Uncheck}.
+ */
+public class UncheckTest {
 
-    @Test(timeout = 4000)
-    public void test43() throws Throwable {
-        // Undeclared exception!
-        try {
-            Uncheck.accept((IOBiConsumer<String, String>) null, " exists", " exists");
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.commons.io.function.Uncheck", e);
-        }
+    /**
+     * Tests that calling {@link Uncheck#accept(IOBiConsumer, Object, Object)} with a null consumer
+     * throws a {@link NullPointerException}.
+     */
+    @Test(expected = NullPointerException.class)
+    public void acceptWithNullBiConsumerShouldThrowNullPointerException() {
+        // The IOBiConsumer argument is null, which is expected to cause the exception.
+        // The other two arguments are arbitrary non-null values.
+        Uncheck.accept((IOBiConsumer<String, String>) null, "input1", "input2");
     }
 }
