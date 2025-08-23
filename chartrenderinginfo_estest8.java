2a76b952c2f7dbd47764a365f9b2b3b60ef59c10
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:43:14 +0800

    Original processedAutoTestCases chartrenderinginfo_estest8.java

---
+++ chartrenderinginfo_estest8.java
@@ -0,0 +1,30 @@
+package org.jfree.chart;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.awt.Rectangle;
+import java.awt.geom.Rectangle2D;
+import java.util.Locale;
+import java.util.SimpleTimeZone;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.jfree.chart.axis.DateAxis;
+import org.jfree.chart.entity.EntityCollection;
+import org.jfree.chart.entity.StandardEntityCollection;
+import org.jfree.chart.plot.CombinedRangeCategoryPlot;
+import org.jfree.chart.plot.PlotRenderingInfo;
+import org.jfree.data.xy.XYDatasetTableModel;
+import org.junit.runner.RunWith;
+
+public class ChartRenderingInfo_ESTestTest8 extends ChartRenderingInfo_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test07() throws Throwable {
+        ChartRenderingInfo chartRenderingInfo0 = new ChartRenderingInfo();
+        Rectangle rectangle0 = new Rectangle((-1), (-1), 1, 7);
+        chartRenderingInfo0.setChartArea(rectangle0);
+        Rectangle2D rectangle2D0 = chartRenderingInfo0.getChartArea();
+        assertEquals(0.0, rectangle2D0.getMaxX(), 0.01);
+    }
+}
