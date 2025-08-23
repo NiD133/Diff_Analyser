26b77990fdb461e2403eae160a6141d84a151d37
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:13:08 +0800

    gemini processedAutoTestCases seconds_estest57.java

--- seconds_estest57.java
+++ seconds_estest57.java
@@ -1,17 +1,27 @@
 package org.joda.time;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class Seconds_ESTestTest57 extends Seconds_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link Seconds} class.
+ */
+public class SecondsTest {
 
-    @Test(timeout = 4000)
-    public void test56() throws Throwable {
-        Seconds seconds0 = Seconds.seconds(Integer.MIN_VALUE);
-        assertEquals(Integer.MIN_VALUE, seconds0.getSeconds());
+    /**
+     * Tests that the {@link Seconds#seconds(int)} factory method correctly handles
+     * the minimum possible integer value.
+     */
+    @Test
+    public void secondsFactory_withMinValue_createsInstanceWithCorrectValue() {
+        // Arrange
+        final int minValue = Integer.MIN_VALUE;
+
+        // Act
+        Seconds minSeconds = Seconds.seconds(minValue);
+
+        // Assert
+        assertEquals("The number of seconds should match the minimum integer value",
+                minValue, minSeconds.getSeconds());
     }
 }
