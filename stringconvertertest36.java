c21328a6f11980ae6bfc15c828c30d8651e2c079
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:34:13 +0800

    gemini processedManualTestCases stringconvertertest36.java

--- stringconvertertest36.java
+++ stringconvertertest36.java
@@ -1,83 +1,75 @@
 package org.joda.time.convert;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-import java.util.Arrays;
-import java.util.Locale;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
 import org.joda.time.Chronology;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.joda.time.MutableInterval;
-import org.joda.time.MutablePeriod;
-import org.joda.time.PeriodType;
-import org.joda.time.TimeOfDay;
 import org.joda.time.chrono.BuddhistChronology;
-import org.joda.time.chrono.ISOChronology;
-import org.joda.time.chrono.JulianChronology;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
-public class StringConverterTestTest36 extends TestCase {
+import java.util.Locale;
 
-    private static final DateTimeZone ONE_HOUR = DateTimeZone.forOffsetHours(1);
+import static org.junit.Assert.assertEquals;
 
-    private static final DateTimeZone SIX = DateTimeZone.forOffsetHours(6);
+/**
+ * Tests for {@link StringConverter#setInto(ReadWritableInterval, Object, Chronology)}.
+ */
+public class StringConverterSetIntoIntervalTest {
 
-    private static final DateTimeZone SEVEN = DateTimeZone.forOffsetHours(7);
+    // Time zones used to construct the test data
+    private static final DateTimeZone ZONE_PLUS_6 = DateTimeZone.forOffsetHours(6);
+    private static final DateTimeZone ZONE_PLUS_7 = DateTimeZone.forOffsetHours(7);
+    private static final DateTimeZone ZONE_PLUS_8 = DateTimeZone.forOffsetHours(8);
 
-    private static final DateTimeZone EIGHT = DateTimeZone.forOffsetHours(8);
+    // Keep track of original default zone and locale to restore them after tests
+    private DateTimeZone originalDefaultZone;
+    private Locale originalDefaultLocale;
 
-    private static final DateTimeZone UTC = DateTimeZone.UTC;
-
-    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
-
-    private static final DateTimeZone LONDON = DateTimeZone.forID("Europe/London");
-
-    private static final Chronology ISO_EIGHT = ISOChronology.getInstance(EIGHT);
-
-    private static final Chronology ISO_PARIS = ISOChronology.getInstance(PARIS);
-
-    private static final Chronology ISO_LONDON = ISOChronology.getInstance(LONDON);
-
-    private static Chronology ISO;
-
-    private static Chronology JULIAN;
-
-    private DateTimeZone zone = null;
-
-    private Locale locale = null;
-
-    public static void main(String[] args) {
-        junit.textui.TestRunner.run(suite());
-    }
-
-    public static TestSuite suite() {
-        return new TestSuite(TestStringConverter.class);
-    }
-
-    @Override
-    protected void setUp() throws Exception {
-        zone = DateTimeZone.getDefault();
-        locale = Locale.getDefault();
-        DateTimeZone.setDefault(LONDON);
+    @Before
+    public void setUp() {
+        originalDefaultZone = DateTimeZone.getDefault();
+        originalDefaultLocale = Locale.getDefault();
+        DateTimeZone.setDefault(DateTimeZone.forID("Europe/London"));
         Locale.setDefault(Locale.UK);
-        JULIAN = JulianChronology.getInstance();
-        ISO = ISOChronology.getInstance();
     }
 
-    @Override
-    protected void tearDown() throws Exception {
-        DateTimeZone.setDefault(zone);
-        Locale.setDefault(locale);
-        zone = null;
+    @After
+    public void tearDown() {
+        DateTimeZone.setDefault(originalDefaultZone);
+        Locale.setDefault(originalDefaultLocale);
     }
 
-    public void testSetIntoInterval_Object_Chronology8() throws Exception {
-        MutableInterval m = new MutableInterval(-1000L, 1000L);
-        StringConverter.INSTANCE.setInto(m, "2003-08-09T+06:00/2004-06-09T+07:00", BuddhistChronology.getInstance(EIGHT));
-        assertEquals(new DateTime(2003, 8, 9, 0, 0, 0, 0, BuddhistChronology.getInstance(SIX)).withZone(EIGHT), m.getStart());
-        assertEquals(new DateTime(2004, 6, 9, 0, 0, 0, 0, BuddhistChronology.getInstance(SEVEN)).withZone(EIGHT), m.getEnd());
-        assertEquals(BuddhistChronology.getInstance(EIGHT), m.getChronology());
+    @Test
+    public void setIntoInterval_whenStringHasOffsets_respectsStringOffsetsAndAssignsChronology() {
+        // Arrange
+        // The interval string contains start/end date-times with specific UTC offsets (+06:00 and +07:00).
+        final String intervalString = "2003-08-09T+06:00/2004-06-09T+07:00";
+
+        // The target chronology for the interval has a different time zone (+08:00).
+        final Chronology targetChronology = BuddhistChronology.getInstance(ZONE_PLUS_8);
+
+        // An existing interval to be modified. Its initial state is irrelevant.
+        MutableInterval interval = new MutableInterval(0L, 0L);
+
+        // Act
+        StringConverter.INSTANCE.setInto(interval, intervalString, targetChronology);
+
+        // Assert
+        // The converter should parse the start and end times using the offsets from the string,
+        // creating instants that are independent of the target chronology's time zone.
+        DateTime expectedStartInstant = new DateTime(2003, 8, 9, 0, 0, 0, 0, BuddhistChronology.getInstance(ZONE_PLUS_6));
+        DateTime expectedEndInstant = new DateTime(2004, 6, 9, 0, 0, 0, 0, BuddhistChronology.getInstance(ZONE_PLUS_7));
+
+        // Verify that the interval's start and end represent the correct absolute moments in time.
+        assertEquals("Interval start millis should match parsed instant",
+                expectedStartInstant.getMillis(), interval.getStartMillis());
+        assertEquals("Interval end millis should match parsed instant",
+                expectedEndInstant.getMillis(), interval.getEndMillis());
+
+        // Verify that the interval's chronology was set to the one provided.
+        assertEquals("Interval chronology should be the one provided",
+                targetChronology, interval.getChronology());
     }
 }
