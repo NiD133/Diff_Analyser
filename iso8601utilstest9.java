ce79409bcd67df5e8f90cbfd6573b374182dff9e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:29:49 +0800

    gemini processedManualTestCases iso8601utilstest9.java

--- iso8601utilstest9.java
+++ iso8601utilstest9.java
@@ -1,33 +1,31 @@
 package com.google.gson.internal.bind.util;
 
-import static com.google.common.truth.Truth.assertThat;
 import static org.junit.Assert.assertThrows;
+
 import java.text.ParseException;
 import java.text.ParsePosition;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.Locale;
-import java.util.TimeZone;
 import org.junit.Test;
 
-public class ISO8601UtilsTestTest9 {
-
-    private static TimeZone utcTimeZone() {
-        return TimeZone.getTimeZone("UTC");
-    }
-
-    private static GregorianCalendar createUtcCalendar() {
-        TimeZone utc = utcTimeZone();
-        GregorianCalendar calendar = new GregorianCalendar(utc);
-        // Calendar was created with current time, must clear it
-        calendar.clear();
-        return calendar;
-    }
+/**
+ * Tests for {@link ISO8601Utils}, focusing on parsing invalid date-time strings.
+ */
+public class ISO8601UtilsTest {
 
     @Test
-    public void testDateParseInvalidTime() {
-        String dateStr = "2018-06-25T61:60:62-03:00";
-        assertThrows(ParseException.class, () -> ISO8601Utils.parse(dateStr, new ParsePosition(0)));
+    public void parse_withInvalidTime_throwsParseException() {
+        // This string contains time components that are out of their valid ranges:
+        // Hour: 61 (valid: 0-23)
+        // Minute: 60 (valid: 0-59)
+        // Second: 62 (valid: 0-59)
+        String invalidIsoString = "2018-06-25T61:60:62-03:00";
+
+        // The position from which parsing should start.
+        ParsePosition position = new ParsePosition(0);
+
+        // Verify that parsing this string throws a ParseException.
+        assertThrows(
+                "Parsing a string with out-of-range time components should fail",
+                ParseException.class,
+                () -> ISO8601Utils.parse(invalidIsoString, position));
     }
 }
