e3e0da04d4bcfd3845047e687fceed60baae6bdb
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:40:09 +0800

    Original processedAutoTestCases bufferedlinestring_estest41.java

---
+++ bufferedlinestring_estest41.java
@@ -0,0 +1,30 @@
+package org.locationtech.spatial4j.shape.impl;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.HashMap;
+import java.util.LinkedList;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+import org.locationtech.spatial4j.context.SpatialContext;
+import org.locationtech.spatial4j.context.SpatialContextFactory;
+import org.locationtech.spatial4j.distance.GeodesicSphereDistCalc;
+import org.locationtech.spatial4j.shape.Point;
+import org.locationtech.spatial4j.shape.Rectangle;
+import org.locationtech.spatial4j.shape.Shape;
+import org.locationtech.spatial4j.shape.ShapeCollection;
+import org.locationtech.spatial4j.shape.SpatialRelation;
+
+public class BufferedLineString_ESTestTest41 extends BufferedLineString_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test40() throws Throwable {
+        LinkedList<Point> linkedList0 = new LinkedList<Point>();
+        SpatialContext spatialContext0 = SpatialContext.GEO;
+        BufferedLineString bufferedLineString0 = new BufferedLineString(linkedList0, 16.128532143886176, spatialContext0);
+        bufferedLineString0.relate(bufferedLineString0);
+        assertEquals(16.128532143886176, bufferedLineString0.getBuf(), 0.01);
+    }
+}
