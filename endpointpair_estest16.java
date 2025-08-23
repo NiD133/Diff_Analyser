5a3b0f5a760bc49afb4a102f4afe5069a436f702
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:49:11 +0800

    gemini processedAutoTestCases endpointpair_estest16.java

--- endpointpair_estest16.java
+++ endpointpair_estest16.java
@@ -1,22 +1,31 @@
 package com.google.common.graph;
 
+import static com.google.common.graph.EndpointPair.unordered;
+import static org.junit.Assert.assertNotEquals;
+
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import com.google.common.collect.UnmodifiableIterator;
-import java.util.Locale;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class EndpointPair_ESTestTest16 extends EndpointPair_ESTest_scaffolding {
+/**
+ * Tests for the equality logic in {@link EndpointPair}.
+ */
+public class EndpointPairEqualityTest {
 
-    @Test(timeout = 4000)
-    public void test15() throws Throwable {
-        Locale.Category locale_Category0 = Locale.Category.FORMAT;
-        EndpointPair<Object> endpointPair0 = EndpointPair.unordered((Object) locale_Category0, (Object) locale_Category0);
-        EndpointPair<Object> endpointPair1 = EndpointPair.unordered((Object) endpointPair0, (Object) locale_Category0);
-        boolean boolean0 = endpointPair0.equals(endpointPair1);
-        assertFalse(boolean0);
+    @Test
+    public void equals_withNestedPairAsNode_returnsFalse() {
+        // Arrange
+        // Create a simple self-loop pair using a string node.
+        String nodeA = "A";
+        EndpointPair<String> selfLoopPair = unordered(nodeA, nodeA);
+
+        // Create a second, different pair where one of its nodes is the first pair.
+        // This tests that an EndpointPair is not equal to another EndpointPair
+        // that contains the first one as a node.
+        EndpointPair<Object> nestedPair = unordered(selfLoopPair, nodeA);
+
+        // Act & Assert
+        // The two pairs should not be equal because their sets of nodes are different.
+        // selfLoopPair contains the nodes {"A", "A"}.
+        // nestedPair contains the nodes {<EndpointPair {"A", "A"}>, "A"}.
+        assertNotEquals(selfLoopPair, nestedPair);
     }
 }
