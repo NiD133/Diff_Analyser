367b9d4990449a88984bcd8958d2f5c64e899546
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:48:45 +0800

    gemini processedAutoTestCases elements_estest33.java

--- elements_estest33.java
+++ elements_estest33.java
@@ -1,36 +1,38 @@
 package org.jsoup.select;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.ConcurrentModificationException;
-import java.util.LinkedHashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.function.Predicate;
-import java.util.function.UnaryOperator;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.jsoup.nodes.Comment;
-import org.jsoup.nodes.DataNode;
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.nodes.FormElement;
-import org.jsoup.nodes.TextNode;
-import org.jsoup.parser.Parser;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class Elements_ESTestTest33 extends Elements_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
-    @Test(timeout = 4000)
-    public void test032() throws Throwable {
-        Document document0 = Parser.parse("*Vq9|=C*", "ch}9X");
-        Elements elements0 = document0.getElementsByAttributeValueNot("*Vq9|=C*", "*Vq9|=C*");
-        Elements elements1 = elements0.next();
-        assertEquals(1, elements1.size());
+/**
+ * Test suite for {@link Elements}.
+ */
+public class ElementsTest {
+
+    @Test
+    public void nextShouldReturnTheImmediateNextSiblingForEachElementInTheSet() {
+        // Arrange
+        // Jsoup.parse creates a full HTML document: <html><head></head><body>Some text</body></html>
+        Document doc = Jsoup.parse("Some text");
+
+        // Select the html, head, and body elements to test the .next() method on a collection.
+        Elements elements = doc.select("html, head, body");
+        assertEquals(3, elements.size()); // Sanity check: we have our 3 elements
+
+        // Act
+        // The .next() method finds the next sibling for each element in the collection.
+        // - <html> has no parent, thus no siblings.
+        // - <head>'s next sibling is <body>.
+        // - <body> has no next sibling.
+        // The resulting collection should contain only the unique siblings found, which is just the <body> element.
+        Elements nextSiblings = elements.next();
+
+        // Assert
+        assertNotNull(nextSiblings);
+        assertEquals(1, nextSiblings.size());
+        assertEquals("body", nextSiblings.first().tagName());
     }
 }
