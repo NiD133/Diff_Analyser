52b4e829ebee1e5878bcbc710dc98d203ce01fbe
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:20:29 +0800

    Original processedAutoTestCases taiinstant_estest25.java

---
+++ taiinstant_estest25.java
@@ -0,0 +1,31 @@
+package org.threeten.extra.scale;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.format.DateTimeParseException;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAmount;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.time.MockClock;
+import org.evosuite.runtime.mock.java.time.MockInstant;
+import org.junit.runner.RunWith;
+
+public class TaiInstant_ESTestTest25 extends TaiInstant_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test24() throws Throwable {
+        Instant instant0 = MockInstant.now();
+        TaiInstant taiInstant0 = TaiInstant.of(instant0);
+        UtcInstant utcInstant0 = UtcInstant.of(taiInstant0);
+        UtcInstant utcInstant1 = utcInstant0.withModifiedJulianDay(0L);
+        TaiInstant taiInstant1 = TaiInstant.of(utcInstant1);
+        long long0 = taiInstant1.getTaiSeconds();
+        assertEquals(73281320000000L, utcInstant1.getNanoOfDay());
+        assertEquals((-3127952309L), long0);
+    }
+}
