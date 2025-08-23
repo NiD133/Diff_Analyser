99f1da16cdad42d4189f4b699351cc64716dfcb3
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:27:37 +0800

    gemini processedManualTestCases dayofmonthtest39.java

--- dayofmonthtest39.java
+++ dayofmonthtest39.java
@@ -1,157 +1,32 @@
 package org.threeten.extra;
 
-import static java.time.Month.APRIL;
-import static java.time.Month.AUGUST;
-import static java.time.Month.DECEMBER;
-import static java.time.Month.FEBRUARY;
-import static java.time.Month.JANUARY;
-import static java.time.Month.JULY;
-import static java.time.Month.JUNE;
-import static java.time.Month.MARCH;
-import static java.time.Month.MAY;
-import static java.time.Month.NOVEMBER;
-import static java.time.Month.OCTOBER;
-import static java.time.Month.SEPTEMBER;
-import static java.time.temporal.ChronoField.ALIGNED_DAY_OF_WEEK_IN_MONTH;
-import static java.time.temporal.ChronoField.ALIGNED_DAY_OF_WEEK_IN_YEAR;
-import static java.time.temporal.ChronoField.ALIGNED_WEEK_OF_MONTH;
-import static java.time.temporal.ChronoField.ALIGNED_WEEK_OF_YEAR;
-import static java.time.temporal.ChronoField.AMPM_OF_DAY;
-import static java.time.temporal.ChronoField.CLOCK_HOUR_OF_AMPM;
-import static java.time.temporal.ChronoField.CLOCK_HOUR_OF_DAY;
-import static java.time.temporal.ChronoField.DAY_OF_MONTH;
-import static java.time.temporal.ChronoField.DAY_OF_WEEK;
-import static java.time.temporal.ChronoField.DAY_OF_YEAR;
-import static java.time.temporal.ChronoField.EPOCH_DAY;
-import static java.time.temporal.ChronoField.ERA;
-import static java.time.temporal.ChronoField.HOUR_OF_AMPM;
-import static java.time.temporal.ChronoField.HOUR_OF_DAY;
-import static java.time.temporal.ChronoField.INSTANT_SECONDS;
-import static java.time.temporal.ChronoField.MICRO_OF_DAY;
-import static java.time.temporal.ChronoField.MICRO_OF_SECOND;
-import static java.time.temporal.ChronoField.MILLI_OF_DAY;
-import static java.time.temporal.ChronoField.MILLI_OF_SECOND;
-import static java.time.temporal.ChronoField.MINUTE_OF_DAY;
-import static java.time.temporal.ChronoField.MINUTE_OF_HOUR;
-import static java.time.temporal.ChronoField.MONTH_OF_YEAR;
-import static java.time.temporal.ChronoField.NANO_OF_DAY;
-import static java.time.temporal.ChronoField.NANO_OF_SECOND;
-import static java.time.temporal.ChronoField.OFFSET_SECONDS;
-import static java.time.temporal.ChronoField.PROLEPTIC_MONTH;
-import static java.time.temporal.ChronoField.SECOND_OF_DAY;
-import static java.time.temporal.ChronoField.SECOND_OF_MINUTE;
-import static java.time.temporal.ChronoField.YEAR;
-import static java.time.temporal.ChronoField.YEAR_OF_ERA;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertSame;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
-import java.time.Clock;
-import java.time.DateTimeException;
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.LocalTime;
+
 import java.time.Month;
 import java.time.MonthDay;
-import java.time.YearMonth;
-import java.time.ZoneId;
-import java.time.chrono.IsoChronology;
-import java.time.chrono.JapaneseDate;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
-import java.time.temporal.IsoFields;
-import java.time.temporal.Temporal;
-import java.time.temporal.TemporalAccessor;
-import java.time.temporal.TemporalAdjuster;
-import java.time.temporal.TemporalField;
-import java.time.temporal.TemporalQueries;
-import java.time.temporal.TemporalUnit;
-import java.time.temporal.UnsupportedTemporalTypeException;
-import java.time.temporal.ValueRange;
-import org.junit.jupiter.api.Test;
-import org.junitpioneer.jupiter.RetryingTest;
-import com.google.common.testing.EqualsTester;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
-public class DayOfMonthTestTest39 {
+/**
+ * Tests for {@link DayOfMonth#atMonth(Month)}.
+ */
+class DayOfMonthAtMonthTest {
 
-    private static final int MAX_LENGTH = 31;
+    @DisplayName("atMonth() should create a correct MonthDay for a day valid in all months")
+    @ParameterizedTest(name = "for {0}")
+    @EnumSource(Month.class)
+    void atMonth_whenDayIsValidForAllMonths_createsCorrectMonthDay(Month month) {
+        // Day 28 is chosen because it is a valid day in every month of the year.
+        int dayValue = 28;
+        DayOfMonth dayOfMonth = DayOfMonth.of(dayValue);
 
-    private static final DayOfMonth TEST = DayOfMonth.of(12);
+        // Act
+        MonthDay actualMonthDay = dayOfMonth.atMonth(month);
 
-    private static final ZoneId PARIS = ZoneId.of("Europe/Paris");
-
-    private static class TestingField implements TemporalField {
-
-        public static final TestingField INSTANCE = new TestingField();
-
-        @Override
-        public TemporalUnit getBaseUnit() {
-            return ChronoUnit.DAYS;
-        }
-
-        @Override
-        public TemporalUnit getRangeUnit() {
-            return ChronoUnit.MONTHS;
-        }
-
-        @Override
-        public ValueRange range() {
-            return ValueRange.of(1, 28, 31);
-        }
-
-        @Override
-        public boolean isDateBased() {
-            return true;
-        }
-
-        @Override
-        public boolean isTimeBased() {
-            return false;
-        }
-
-        @Override
-        public boolean isSupportedBy(TemporalAccessor temporal) {
-            return temporal.isSupported(DAY_OF_MONTH);
-        }
-
-        @Override
-        public ValueRange rangeRefinedBy(TemporalAccessor temporal) {
-            return range();
-        }
-
-        @Override
-        public long getFrom(TemporalAccessor temporal) {
-            return temporal.getLong(DAY_OF_MONTH);
-        }
-
-        @Override
-        @SuppressWarnings("unchecked")
-        public <R extends Temporal> R adjustInto(R temporal, long newValue) {
-            return (R) temporal.with(DAY_OF_MONTH, newValue);
-        }
-    }
-
-    @Test
-    public void test_atMonth_Month_28() {
-        DayOfMonth test = DayOfMonth.of(28);
-        assertEquals(MonthDay.of(1, 28), test.atMonth(JANUARY));
-        assertEquals(MonthDay.of(2, 28), test.atMonth(FEBRUARY));
-        assertEquals(MonthDay.of(3, 28), test.atMonth(MARCH));
-        assertEquals(MonthDay.of(4, 28), test.atMonth(APRIL));
-        assertEquals(MonthDay.of(5, 28), test.atMonth(MAY));
-        assertEquals(MonthDay.of(6, 28), test.atMonth(JUNE));
-        assertEquals(MonthDay.of(7, 28), test.atMonth(JULY));
-        assertEquals(MonthDay.of(8, 28), test.atMonth(AUGUST));
-        assertEquals(MonthDay.of(9, 28), test.atMonth(SEPTEMBER));
-        assertEquals(MonthDay.of(10, 28), test.atMonth(OCTOBER));
-        assertEquals(MonthDay.of(11, 28), test.atMonth(NOVEMBER));
-        assertEquals(MonthDay.of(12, 28), test.atMonth(DECEMBER));
+        // Assert
+        MonthDay expectedMonthDay = MonthDay.of(month, dayValue);
+        assertEquals(expectedMonthDay, actualMonthDay);
     }
 }
