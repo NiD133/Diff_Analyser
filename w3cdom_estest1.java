21b4b2efa3bec1098343e787c17c6bfbec02198f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:26:39 +0800

    gemini processedAutoTestCases w3cdom_estest1.java

--- w3cdom_estest1.java
+++ w3cdom_estest1.java
@@ -1,42 +1,59 @@
 package org.jsoup.helper;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import com.sun.org.apache.xerces.internal.dom.DocumentImpl;
-import java.io.Reader;
-import java.io.StringReader;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
-import javax.imageio.metadata.IIOMetadataNode;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.jsoup.nodes.Attributes;
-import org.jsoup.nodes.DataNode;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.DocumentType;
-import org.jsoup.nodes.Element;
-import org.jsoup.nodes.FormElement;
-import org.jsoup.nodes.XmlDeclaration;
 import org.jsoup.parser.Parser;
-import org.jsoup.parser.Tag;
-import org.junit.runner.RunWith;
-import org.w3c.dom.DOMException;
+import org.junit.Test;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-public class W3CDom_ESTestTest1 extends W3CDom_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
-    @Test(timeout = 4000)
-    public void test00() throws Throwable {
-        Document document0 = Parser.parse("", "V5");
-        Document document1 = document0.clone();
-        Element element0 = document1.attr("http://www.w3.org/1998/Math/MathML", "http://www.w3.org/2000/svg");
-        W3CDom w3CDom0 = new W3CDom();
-        Element element1 = document0.prependChild(element0);
-        w3CDom0.fromJsoup(element1);
-        assertTrue(w3CDom0.namespaceAware());
+/**
+ * Tests for {@link W3CDom} to verify the conversion of jsoup documents
+ * with namespaces into W3C DOM documents.
+ */
+public class W3CDomTest {
+
+    private static final String SVG_NAMESPACE_URI = "http://www.w3.org/2000/svg";
+
+    /**
+     * Verifies that when a jsoup Document containing namespaced elements (like SVG)
+     * is converted to a W3C Document, the namespaces are correctly preserved.
+     */
+    @Test
+    public void fromJsoupConvertsNamespacedElementsCorrectly() {
+        // Arrange: Create a jsoup document with an embedded SVG element, which uses namespaces.
+        String htmlWithSvg = "<html><body><svg xmlns='" + SVG_NAMESPACE_URI + "' width='100' height='100'>" +
+                             "<circle cx='50' cy='50' r='40' fill='red' />" +
+                             "</svg></body></html>";
+        Document jsoupDoc = Parser.parse(htmlWithSvg, "");
+        W3CDom w3cDomConverter = new W3CDom();
+
+        // Act: Convert the jsoup document to a W3C DOM document.
+        org.w3c.dom.Document w3cDoc = w3cDomConverter.fromJsoup(jsoupDoc);
+
+        // Assert: Verify that the SVG and circle elements exist in the W3C DOM
+        // and have the correct namespace URI.
+        NodeList svgElements = w3cDoc.getElementsByTagNameNS(SVG_NAMESPACE_URI, "svg");
+        assertEquals("Should find one SVG element", 1, svgElements.getLength());
+        
+        Node svgNode = svgElements.item(0);
+        assertNotNull("SVG node should not be null", svgNode);
+        assertEquals("SVG node should have the correct namespace URI", SVG_NAMESPACE_URI, svgNode.getNamespaceURI());
+        assertEquals("SVG node should have the correct local name", "svg", svgNode.getLocalName());
+
+        NodeList circleElements = ((Element) svgNode).getElementsByTagNameNS(SVG_NAMESPACE_URI, "circle");
+        assertEquals("Should find one circle element within the SVG element", 1, circleElements.getLength());
+
+        Node circleNode = circleElements.item(0);
+        assertNotNull("Circle node should not be null", circleNode);
+        assertEquals("Circle node should have the correct namespace URI", SVG_NAMESPACE_URI, circleNode.getNamespaceURI());
+        assertEquals("Circle node should have the correct local name", "circle", circleNode.getLocalName());
+        
+        // Also verify an attribute on the namespaced element
+        Element circleElement = (Element) circleNode;
+        assertEquals("Circle element should have correct 'cx' attribute", "50", circleElement.getAttribute("cx"));
     }
 }
