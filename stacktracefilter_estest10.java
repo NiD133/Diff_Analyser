aa49606beebca685a36417cbacfaa542971b8ca2
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:17:40 +0800

    gemini processedAutoTestCases stacktracefilter_estest10.java

--- stacktracefilter_estest10.java
+++ stacktracefilter_estest10.java
@@ -1,22 +1,47 @@
 package org.mockito.internal.exceptions.stacktrace;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.lang.MockThrowable;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
 
-public class StackTraceFilter_ESTestTest10 extends StackTraceFilter_ESTest_scaffolding {
+/**
+ * Tests for {@link StackTraceFilter}.
+ */
+public class StackTraceFilterTest {
 
-    @Test(timeout = 4000)
-    public void test09() throws Throwable {
-        StackTraceFilter stackTraceFilter0 = new StackTraceFilter();
-        MockThrowable mockThrowable0 = new MockThrowable();
-        StackTraceElement stackTraceElement0 = new StackTraceElement("/34KVo", "org.mockito.internal.configuration.plugins.DefaultPluginSwitch", "/34KVo", "org.mockito.internal.configuration.plugins.DefaultPluginSwitch", "/34KVo", "org.mockito.internal.configuration.plugins.DefaultPluginSwitch", 1);
-        mockThrowable0.setOriginForDelegate(stackTraceElement0);
-        StackTraceElement stackTraceElement1 = stackTraceFilter0.filterFirst(mockThrowable0, true);
-        assertEquals("<evosuite>", stackTraceElement1.getFileName());
+    /**
+     * This test verifies that the filterFirst() method correctly skips over
+     * Mockito-internal stack frames and returns the first frame that belongs
+     * to user code.
+     */
+    @Test
+    public void filterFirst_shouldSkipInternalMockitoFrames_andReturnFirstUserFrame() {
+        // Arrange
+        StackTraceFilter stackTraceFilter = new StackTraceFilter();
+        Throwable throwable = new Throwable();
+
+        // A stack trace element from a Mockito-internal class that should be filtered out.
+        StackTraceElement mockitoInternalFrame = new StackTraceElement(
+                "org.mockito.internal.runners.Runner", "run", "Runner.java", 50);
+
+        // A stack trace element from typical user test code that should be preserved.
+        StackTraceElement userCodeFrame = new StackTraceElement(
+                "com.mycompany.MyTest", "myFailingTest", "MyTest.java", 23);
+
+        // A subsequent frame that should be ignored since we only want the first user frame.
+        StackTraceElement anotherUserFrame = new StackTraceElement(
+                "com.mycompany.MyService", "doSomething", "MyService.java", 101);
+
+        throwable.setStackTrace(new StackTraceElement[]{
+                mockitoInternalFrame,
+                userCodeFrame,
+                anotherUserFrame
+        });
+
+        // Act: Filter the stack trace to find the first relevant frame.
+        StackTraceElement firstRelevantFrame = stackTraceFilter.filterFirst(throwable, true);
+
+        // Assert: The returned frame should be the user's code, not the internal Mockito frame.
+        assertSame("The first user code frame should be returned", userCodeFrame, firstRelevantFrame);
     }
 }
