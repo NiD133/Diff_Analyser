26a5ddc925801ce7beb1fbfc29068e0801cb2d95
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:28:59 +0800

    gemini processedAutoTestCases xnode_estest49.java

--- xnode_estest49.java
+++ xnode_estest49.java
@@ -1,38 +1,36 @@
 package org.apache.ibatis.parsing;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.List;
-import java.util.Locale;
-import java.util.Properties;
-import java.util.function.Supplier;
 import javax.imageio.metadata.IIOMetadataNode;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
-import org.w3c.dom.Document;
+import java.util.Properties;
 import org.w3c.dom.Node;
-import org.xml.sax.ext.DefaultHandler2;
 
+/**
+ * Test suite for the {@link XNode} class.
+ * This specific test focuses on the behavior of attribute-retrieval methods.
+ */
+// The class name is kept to match the original context, though in a real-world
+// scenario, it would likely be renamed to something like XNodeTest.
 public class XNode_ESTestTest49 extends XNode_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test048() throws Throwable {
-        IIOMetadataNode iIOMetadataNode0 = new IIOMetadataNode();
-        Properties properties0 = new Properties();
-        XNode xNode0 = new XNode((XPathParser) null, iIOMetadataNode0, properties0);
-        Long long0 = new Long(94L);
-        // Undeclared exception!
-        try {
-            xNode0.getLongAttribute((String) null, long0);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-        }
+    /**
+     * Verifies that calling getLongAttribute with a null attribute name throws a NullPointerException.
+     * This is the expected behavior because the underlying Properties object, which stores
+     * the attributes, does not permit null keys.
+     */
+    @Test(expected = NullPointerException.class)
+    public void getLongAttributeShouldThrowNullPointerExceptionWhenNameIsNull() {
+        // Arrange: Create an XNode with a dummy node and no variables.
+        Node dummyNode = new IIOMetadataNode();
+        Properties emptyVariables = new Properties();
+        XNode xNode = new XNode(null, dummyNode, emptyVariables);
+        Long defaultValue = 94L;
+
+        // Act: Attempt to get an attribute using a null name.
+        // The test expects this line to throw a NullPointerException.
+        xNode.getLongAttribute(null, defaultValue);
+
+        // Assert: The test framework verifies that a NullPointerException was thrown,
+        // as specified by the @Test(expected = ...) annotation.
     }
 }
