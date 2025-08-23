6eb9a7e3c6b8122fd7dc7aed9a9ae818582ef20b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:26:24 +0800

    Original processedAutoTestCases verificationovertimeimpl_estest12.java

---
+++ verificationovertimeimpl_estest12.java
@@ -0,0 +1,30 @@
+package org.mockito.internal.verification;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.NoSuchElementException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+import org.mockito.internal.creation.MockSettingsImpl;
+import org.mockito.internal.invocation.InvocationMatcher;
+import org.mockito.internal.stubbing.InvocationContainerImpl;
+import org.mockito.internal.util.Timer;
+import org.mockito.internal.verification.api.VerificationData;
+import org.mockito.verification.After;
+import org.mockito.verification.Timeout;
+import org.mockito.verification.VerificationMode;
+
+public class VerificationOverTimeImpl_ESTestTest12 extends VerificationOverTimeImpl_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test11() throws Throwable {
+        NoMoreInteractions noMoreInteractions0 = new NoMoreInteractions();
+        After after0 = new After(2416L, noMoreInteractions0);
+        VerificationOverTimeImpl verificationOverTimeImpl0 = new VerificationOverTimeImpl(5157L, 1L, after0, false);
+        boolean boolean0 = verificationOverTimeImpl0.canRecoverFromFailure(after0);
+        assertEquals(5157L, verificationOverTimeImpl0.getPollingPeriodMillis());
+        assertTrue(boolean0);
+    }
+}
