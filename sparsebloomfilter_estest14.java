b7784814964784142b10f044d695129fb97916e7
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:16:57 +0800

    Original processedAutoTestCases sparsebloomfilter_estest14.java

---
+++ sparsebloomfilter_estest14.java
@@ -0,0 +1,24 @@
+package org.apache.commons.collections4.bloomfilter;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.function.IntPredicate;
+import java.util.function.LongPredicate;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class SparseBloomFilter_ESTestTest14 extends SparseBloomFilter_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test13() throws Throwable {
+        Shape shape0 = Shape.fromKM(Integer.MAX_VALUE, Integer.MAX_VALUE);
+        SimpleBloomFilter simpleBloomFilter0 = new SimpleBloomFilter(shape0);
+        SparseBloomFilter sparseBloomFilter0 = new SparseBloomFilter(shape0);
+        // Undeclared exception!
+        sparseBloomFilter0.merge((IndexExtractor) simpleBloomFilter0);
+    }
+}
