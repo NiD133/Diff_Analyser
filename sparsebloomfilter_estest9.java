4390fe3f6ac85ab0514e37c79f6c38b35c32d959
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:17:13 +0800

    Original processedAutoTestCases sparsebloomfilter_estest9.java

---
+++ sparsebloomfilter_estest9.java
@@ -0,0 +1,26 @@
+package org.apache.commons.collections4.bloomfilter;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.function.IntPredicate;
+import java.util.function.LongPredicate;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class SparseBloomFilter_ESTestTest9 extends SparseBloomFilter_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test08() throws Throwable {
+        Shape shape0 = Shape.fromNM(64, 64);
+        SparseBloomFilter sparseBloomFilter0 = new SparseBloomFilter(shape0);
+        byte[] byteArray0 = new byte[6];
+        EnhancedDoubleHasher enhancedDoubleHasher0 = new EnhancedDoubleHasher(byteArray0);
+        sparseBloomFilter0.merge((Hasher) enhancedDoubleHasher0);
+        int int0 = sparseBloomFilter0.cardinality();
+        assertEquals(1, int0);
+    }
+}
