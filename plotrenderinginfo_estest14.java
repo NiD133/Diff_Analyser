74ff67c4b55fd6b249ce5696ec1c00781f9d0efe
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:08:56 +0800

    gemini processedAutoTestCases plotrenderinginfo_estest14.java

--- plotrenderinginfo_estest14.java
+++ plotrenderinginfo_estest14.java
@@ -1,38 +1,30 @@
 package org.jfree.chart.plot;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.awt.Dimension;
-import java.awt.Point;
-import java.awt.Rectangle;
-import java.awt.geom.Line2D;
-import java.awt.geom.Point2D;
-import java.awt.geom.Rectangle2D;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
 import org.jfree.chart.ChartRenderingInfo;
-import org.jfree.chart.entity.EntityCollection;
-import org.jfree.chart.entity.StandardEntityCollection;
-import org.junit.runner.RunWith;
+import org.junit.Test;
+import java.awt.geom.Point2D;
 
-public class PlotRenderingInfo_ESTestTest14 extends PlotRenderingInfo_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link PlotRenderingInfo} class.
+ */
+public class PlotRenderingInfoTest {
 
-    @Test(timeout = 4000)
-    public void test13() throws Throwable {
-        ChartRenderingInfo chartRenderingInfo0 = new ChartRenderingInfo();
-        PlotRenderingInfo plotRenderingInfo0 = new PlotRenderingInfo(chartRenderingInfo0);
-        plotRenderingInfo0.addSubplotInfo((PlotRenderingInfo) null);
-        Point2D.Double point2D_Double0 = new Point2D.Double();
-        // Undeclared exception!
-        try {
-            plotRenderingInfo0.getSubplotIndex(point2D_Double0);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.jfree.chart.plot.PlotRenderingInfo", e);
-        }
+    /**
+     * Verifies that getSubplotIndex() throws a NullPointerException if the list
+     * of subplots contains a null entry. The method iterates through the subplots
+     * and attempts to access their data area, which will fail for a null element.
+     */
+    @Test(expected = NullPointerException.class)
+    public void getSubplotIndexShouldThrowNPEForNullSubplotInfo() {
+        // Arrange: Create a PlotRenderingInfo and add a null subplot to it.
+        ChartRenderingInfo chartInfo = new ChartRenderingInfo();
+        PlotRenderingInfo plotInfo = new PlotRenderingInfo(chartInfo);
+        plotInfo.addSubplotInfo(null);
+
+        Point2D.Double point = new Point2D.Double(10.0, 20.0);
+
+        // Act & Assert: Calling getSubplotIndex should trigger a NullPointerException
+        // when it tries to access the data area of the null subplot.
+        plotInfo.getSubplotIndex(point);
     }
 }
