12c3b95035709a2bbefe7e4111593ec1d5fb87ed
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:44:08 +0800

    Original processedAutoTestCases collectionbag_estest19.java

---
+++ collectionbag_estest19.java
@@ -0,0 +1,54 @@
+package org.apache.commons.collections4.bag;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.lang.reflect.Array;
+import java.util.Collection;
+import java.util.ConcurrentModificationException;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Stack;
+import java.util.TreeSet;
+import org.apache.commons.collections4.Bag;
+import org.apache.commons.collections4.Predicate;
+import org.apache.commons.collections4.SortedBag;
+import org.apache.commons.collections4.Transformer;
+import org.apache.commons.collections4.functors.ComparatorPredicate;
+import org.apache.commons.collections4.functors.ConstantTransformer;
+import org.apache.commons.collections4.functors.FalsePredicate;
+import org.apache.commons.collections4.functors.IdentityPredicate;
+import org.apache.commons.collections4.functors.IfTransformer;
+import org.apache.commons.collections4.functors.InvokerTransformer;
+import org.apache.commons.collections4.functors.MapTransformer;
+import org.apache.commons.collections4.functors.NullIsExceptionPredicate;
+import org.apache.commons.collections4.functors.TransformerPredicate;
+import org.apache.commons.collections4.functors.UniquePredicate;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class CollectionBag_ESTestTest19 extends CollectionBag_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test18() throws Throwable {
+        TreeBag<Integer> treeBag0 = new TreeBag<Integer>();
+        Integer integer0 = new Integer(977);
+        treeBag0.add(integer0);
+        UniquePredicate<Integer> uniquePredicate0 = new UniquePredicate<Integer>();
+        NullIsExceptionPredicate<Integer> nullIsExceptionPredicate0 = new NullIsExceptionPredicate<Integer>(uniquePredicate0);
+        PredicatedSortedBag<Integer> predicatedSortedBag0 = PredicatedSortedBag.predicatedSortedBag((SortedBag<Integer>) treeBag0, (Predicate<? super Integer>) nullIsExceptionPredicate0);
+        CollectionBag<Integer> collectionBag0 = new CollectionBag<Integer>(predicatedSortedBag0);
+        // Undeclared exception!
+        try {
+            collectionBag0.addAll(predicatedSortedBag0);
+            fail("Expecting exception: IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //
+            // Cannot add Object '977' - Predicate 'org.apache.commons.collections4.functors.NullIsExceptionPredicate@4' rejected it
+            //
+            verifyException("org.apache.commons.collections4.collection.PredicatedCollection", e);
+        }
+    }
+}
