51732897d0f794f8a9e60c739e702b81b7cce68c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:28:13 +0800

    gemini processedAutoTestCases xmltreebuilder_estest9.java

--- xmltreebuilder_estest9.java
+++ xmltreebuilder_estest9.java
@@ -1,35 +1,45 @@
 package org.jsoup.parser;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.PipedReader;
-import java.io.PipedWriter;
-import java.io.Reader;
+import static org.junit.Assert.assertEquals;
 import java.io.StringReader;
-import java.io.UncheckedIOException;
-import java.util.ArrayList;
-import java.util.NoSuchElementException;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.jsoup.nodes.Attributes;
-import org.jsoup.nodes.CDataNode;
-import org.jsoup.nodes.Comment;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.nodes.LeafNode;
-import org.jsoup.select.Elements;
-import org.junit.runner.RunWith;
 
+/**
+ * This test class contains tests for the XmlTreeBuilder.
+ * This specific test focuses on the behavior of the initialiseParse method.
+ */
+// The original class name is kept to match the context provided.
 public class XmlTreeBuilder_ESTestTest9 extends XmlTreeBuilder_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test08() throws Throwable {
-        XmlTreeBuilder xmlTreeBuilder0 = new XmlTreeBuilder();
-        StringReader stringReader0 = new StringReader("xmlns:#text");
-        Parser parser0 = Parser.xmlParser();
-        xmlTreeBuilder0.initialiseParse(stringReader0, "xmlns:#text", parser0);
-        assertEquals("http://www.w3.org/XML/1998/namespace", parser0.defaultNamespace());
-        assertEquals("http://www.w3.org/XML/1998/namespace", xmlTreeBuilder0.defaultNamespace());
+    /**
+     * Verifies that initialiseParse correctly sets the default XML namespace
+     * on both the builder and its associated parser, even when given unusual input.
+     * The standard XML namespace should always be set by default for an XML parse.
+     */
+    @Test
+    public void initialiseParseSetsDefaultXmlNamespaceRegardlessOfInput() {
+        // Arrange
+        XmlTreeBuilder xmlTreeBuilder = new XmlTreeBuilder();
+        Parser xmlParser = Parser.xmlParser();
+
+        // Use an input string that resembles a namespace attribute but is not a valid one.
+        // This ensures the default namespace setting is robust and not affected by input content.
+        String trickyInput = "xmlns:#text";
+        StringReader inputReader = new StringReader(trickyInput);
+        String baseUri = trickyInput; // The original test used the input as the base URI.
+
+        String expectedDefaultNamespace = "http://www.w3.org/XML/1998/namespace";
+
+        // Act
+        xmlTreeBuilder.initialiseParse(inputReader, baseUri, xmlParser);
+
+        // Assert
+        // The tree builder should adopt the standard XML default namespace upon initialization.
+        assertEquals("The XmlTreeBuilder should have the default XML namespace.",
+                expectedDefaultNamespace, xmlTreeBuilder.defaultNamespace());
+
+        // The parser configured by the builder should also have the correct default namespace.
+        assertEquals("The Parser should have the default XML namespace.",
+                expectedDefaultNamespace, xmlParser.defaultNamespace());
     }
 }
