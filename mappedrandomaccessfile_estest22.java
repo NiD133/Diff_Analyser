0b5304df449a49d2277a7bea4c6eb4491cc855fb
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:02:00 +0800

    Original processedAutoTestCases mappedrandomaccessfile_estest22.java

---
+++ mappedrandomaccessfile_estest22.java
@@ -0,0 +1,22 @@
+package com.itextpdf.text.pdf;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.testdata.EvoSuiteFile;
+import org.evosuite.runtime.testdata.FileSystemHandling;
+import org.junit.runner.RunWith;
+
+public class MappedRandomAccessFile_ESTestTest22 extends MappedRandomAccessFile_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test21() throws Throwable {
+        MappedRandomAccessFile mappedRandomAccessFile0 = new MappedRandomAccessFile("rw", "rw");
+        long long0 = mappedRandomAccessFile0.getFilePointer();
+        assertEquals(0L, long0);
+    }
+}
