cdf78e4ee7ccf6855975e8465b771c9e98cd29c7
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:52:40 +0800

    Original processedAutoTestCases gridarrangement_estest9.java

---
+++ gridarrangement_estest9.java
@@ -0,0 +1,30 @@
+package org.jfree.chart.block;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.awt.Graphics2D;
+import java.awt.SystemColor;
+import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.util.MockGregorianCalendar;
+import org.jfree.chart.api.HorizontalAlignment;
+import org.jfree.chart.api.RectangleAnchor;
+import org.jfree.chart.api.VerticalAlignment;
+import org.jfree.chart.text.TextBlockAnchor;
+import org.jfree.data.Range;
+import org.jfree.data.time.TimePeriodAnchor;
+import org.jfree.data.time.TimeSeries;
+import org.junit.runner.RunWith;
+
+public class GridArrangement_ESTestTest9 extends GridArrangement_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test08() throws Throwable {
+        GridArrangement gridArrangement0 = new GridArrangement((-3031), (-3031));
+        assertNotNull(gridArrangement0);
+        gridArrangement0.clear();
+    }
+}
