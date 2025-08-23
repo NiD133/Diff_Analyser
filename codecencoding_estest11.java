390f5cf6adc371e5696d9a36cafa414f0749b4f4
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:43:55 +0800

    Original processedAutoTestCases codecencoding_estest11.java

---
+++ codecencoding_estest11.java
@@ -0,0 +1,30 @@
+package org.apache.commons.compress.harmony.pack200;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.io.PushbackInputStream;
+import java.io.SequenceInputStream;
+import java.util.Enumeration;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class CodecEncoding_ESTestTest11 extends CodecEncoding_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test10() throws Throwable {
+        BHSDCodec bHSDCodec0 = Codec.CHAR3;
+        int[] intArray0 = CodecEncoding.getSpecifier(bHSDCodec0, bHSDCodec0);
+        assertArrayEquals(new int[] { 116, 16, 127 }, intArray0);
+    }
+}
