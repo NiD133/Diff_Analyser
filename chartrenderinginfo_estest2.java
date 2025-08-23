60e50b4d965537bf98a6f5ac5bfb5e60540cbeb6
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:43:12 +0800

    Original processedAutoTestCases chartrenderinginfo_estest2.java

---
+++ chartrenderinginfo_estest2.java
@@ -0,0 +1,31 @@
+package org.jfree.chart;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.awt.Rectangle;
+import java.awt.geom.Rectangle2D;
+import java.util.Locale;
+import java.util.SimpleTimeZone;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.jfree.chart.axis.DateAxis;
+import org.jfree.chart.entity.EntityCollection;
+import org.jfree.chart.entity.StandardEntityCollection;
+import org.jfree.chart.plot.CombinedRangeCategoryPlot;
+import org.jfree.chart.plot.PlotRenderingInfo;
+import org.jfree.data.xy.XYDatasetTableModel;
+import org.junit.runner.RunWith;
+
+public class ChartRenderingInfo_ESTestTest2 extends ChartRenderingInfo_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test01() throws Throwable {
+        StandardEntityCollection standardEntityCollection0 = new StandardEntityCollection();
+        ChartRenderingInfo chartRenderingInfo0 = new ChartRenderingInfo(standardEntityCollection0);
+        PlotRenderingInfo plotRenderingInfo0 = chartRenderingInfo0.getPlotInfo();
+        plotRenderingInfo0.addSubplotInfo(plotRenderingInfo0);
+        PlotRenderingInfo plotRenderingInfo1 = chartRenderingInfo0.getPlotInfo();
+        assertEquals(1, plotRenderingInfo1.getSubplotCount());
+    }
+}
