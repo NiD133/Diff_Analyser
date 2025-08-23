75454a44058e3a9004b834e7b6baa8f6f3b93a0c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:38:28 +0800

    Original processedAutoTestCases attribute_estest79.java

---
+++ attribute_estest79.java
@@ -0,0 +1,30 @@
+package org.jsoup.nodes;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.ByteArrayOutputStream;
+import java.io.FilterOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PipedWriter;
+import java.io.StringWriter;
+import java.nio.BufferOverflowException;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetEncoder;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockPrintWriter;
+import org.jsoup.internal.QuietAppendable;
+import org.junit.runner.RunWith;
+
+public class Attribute_ESTestTest79 extends Attribute_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test78() throws Throwable {
+        Attribute attribute0 = Attribute.createFromEncoded("_Tr_2_", "ope3n");
+        String string0 = attribute0.getKey();
+        assertEquals("ope3n", attribute0.getValue());
+        assertEquals("_Tr_2_", string0);
+    }
+}
