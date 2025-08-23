1576d321ed3930e6b4ca8ec2a321cd3dbce44180
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:44:33 +0800

    gemini processedAutoTestCases collectionsortedbag_estest31.java

--- collectionsortedbag_estest31.java
+++ collectionsortedbag_estest31.java
@@ -1,57 +1,30 @@
 package org.apache.commons.collections4.bag;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.ConcurrentModificationException;
-import java.util.LinkedList;
-import java.util.Locale;
-import java.util.PriorityQueue;
-import java.util.Set;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.Predicate;
 import org.apache.commons.collections4.SortedBag;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionPredicate;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.IfClosure;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.NOPClosure;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.TransformedPredicate;
-import org.apache.commons.collections4.functors.TransformerPredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class CollectionSortedBag_ESTestTest31 extends CollectionSortedBag_ESTest_scaffolding {
+/**
+ * Contains tests for {@link CollectionSortedBag} to ensure it correctly
+ * propagates behavior from the decorated bag.
+ */
+public class CollectionSortedBagTest {
 
-    @Test(timeout = 4000)
-    public void test30() throws Throwable {
-        TreeBag<Object> treeBag0 = new TreeBag<Object>((Comparator<? super Object>) null);
-        SynchronizedSortedBag<Object> synchronizedSortedBag0 = new SynchronizedSortedBag<Object>(treeBag0);
-        CollectionSortedBag<Object> collectionSortedBag0 = new CollectionSortedBag<Object>(synchronizedSortedBag0);
-        // Undeclared exception!
-        try {
-            collectionSortedBag0.add((Object) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("java.util.TreeMap", e);
-        }
+    /**
+     * Tests that add(null) throws a NullPointerException if the underlying
+     * bag does not support null elements.
+     */
+    @Test(expected = NullPointerException.class)
+    public void add_whenUnderlyingBagRejectsNull_throwsNullPointerException() {
+        // Arrange: Create a CollectionSortedBag that decorates a TreeBag.
+        // A standard TreeBag uses natural ordering and does not allow nulls.
+        final SortedBag<Object> underlyingBag = new TreeBag<>();
+        final SortedBag<Object> bagUnderTest = new CollectionSortedBag<>(underlyingBag);
+
+        // Act: Attempt to add a null element. This call is delegated to the
+        // underlying TreeBag, which is expected to throw the exception.
+        bagUnderTest.add(null);
+
+        // Assert: The test passes if a NullPointerException is thrown,
+        // as specified by the @Test(expected=...) annotation.
     }
 }
