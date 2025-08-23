e7d2599128cc5f06caf5887726ec82633348f520
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:22:53 +0800

    Original processedAutoTestCases tonumberpolicy_estest6.java

---
+++ tonumberpolicy_estest6.java
@@ -0,0 +1,28 @@
+package com.google.gson;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.google.gson.stream.JsonReader;
+import java.io.StringReader;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class ToNumberPolicy_ESTestTest6 extends ToNumberPolicy_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test5() throws Throwable {
+        ToNumberPolicy toNumberPolicy0 = ToNumberPolicy.DOUBLE;
+        // Undeclared exception!
+        try {
+            toNumberPolicy0.readNumber((JsonReader) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("com.google.gson.ToNumberPolicy$1", e);
+        }
+    }
+}
