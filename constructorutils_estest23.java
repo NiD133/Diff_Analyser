83c51d82b53b966b40837586f97b210c5699e378
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:45:38 +0800

    Original processedAutoTestCases constructorutils_estest23.java

---
+++ constructorutils_estest23.java
@@ -0,0 +1,21 @@
+package org.apache.commons.lang3.reflect;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.AccessibleObject;
+import java.lang.reflect.Array;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class ConstructorUtils_ESTestTest23 extends ConstructorUtils_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test22() throws Throwable {
+        ConstructorUtils constructorUtils0 = new ConstructorUtils();
+    }
+}
