8d98038012c4e3040b7cf4bd6a7a4dfcabde8aa0
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:30:56 +0800

    Original processedManualTestCases loopinglistiteratortest6.java

---
+++ loopinglistiteratortest6.java
@@ -0,0 +1,43 @@
+package org.apache.commons.collections4.iterators;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.NoSuchElementException;
+import org.junit.jupiter.api.Test;
+
+public class LoopingListIteratorTestTest6 {
+
+    /**
+     * Tests whether a looping list iterator works on a list with only
+     * one element.
+     */
+    @Test
+    void testLooping1() {
+        final List<String> list = Arrays.asList("a");
+        // <a>
+        final LoopingListIterator<String> loop = new LoopingListIterator<>(list);
+        assertTrue(loop.hasNext());
+        // <a>
+        assertEquals("a", loop.next());
+        assertTrue(loop.hasNext());
+        // <a>
+        assertEquals("a", loop.next());
+        assertTrue(loop.hasNext());
+        // <a>
+        assertEquals("a", loop.next());
+        assertTrue(loop.hasPrevious());
+        // <a>
+        assertEquals("a", loop.previous());
+        assertTrue(loop.hasPrevious());
+        // <a>
+        assertEquals("a", loop.previous());
+        assertTrue(loop.hasPrevious());
+        // <a>
+        assertEquals("a", loop.previous());
+    }
+}
