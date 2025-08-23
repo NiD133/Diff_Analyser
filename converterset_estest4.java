21e165c5bfaf2d36d2f7f7c992ca770ff3e66396
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:45:48 +0800

    Original processedAutoTestCases converterset_estest4.java

---
+++ converterset_estest4.java
@@ -0,0 +1,28 @@
+package org.joda.time.convert;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.joda.time.Hours;
+import org.joda.time.Interval;
+import org.joda.time.MutablePeriod;
+import org.joda.time.PeriodType;
+import org.joda.time.Seconds;
+import org.joda.time.chrono.CopticChronology;
+import org.junit.runner.RunWith;
+
+public class ConverterSet_ESTestTest4 extends ConverterSet_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test03() throws Throwable {
+        Converter[] converterArray0 = new Converter[1];
+        StringConverter stringConverter0 = StringConverter.INSTANCE;
+        converterArray0[0] = (Converter) stringConverter0;
+        ConverterSet converterSet0 = new ConverterSet(converterArray0);
+        ConverterSet converterSet1 = converterSet0.remove(converterArray0[0], converterArray0);
+        int int0 = converterSet1.size();
+        assertEquals(0, int0);
+    }
+}
