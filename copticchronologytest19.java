c6bfb96e074078a4342f0320706d21ede18db86d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:27:11 +0800

    gemini processedManualTestCases copticchronologytest19.java

--- copticchronologytest19.java
+++ copticchronologytest19.java
@@ -1,85 +1,35 @@
 package org.joda.time.chrono;
 
-import java.util.Locale;
-import java.util.TimeZone;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertTrue;
+
 import org.joda.time.Chronology;
 import org.joda.time.DateTime;
-import org.joda.time.DateTime.Property;
-import org.joda.time.DateTimeConstants;
-import org.joda.time.DateTimeField;
-import org.joda.time.DateTimeUtils;
-import org.joda.time.DateTimeZone;
-import org.joda.time.DurationField;
-import org.joda.time.DurationFieldType;
+import org.junit.Test;
 
-public class CopticChronologyTestTest19 extends TestCase {
+/**
+ * Unit tests for {@link CopticChronology}.
+ */
+public class CopticChronologyTest {
 
-    private static final int MILLIS_PER_DAY = DateTimeConstants.MILLIS_PER_DAY;
+    @Test
+    public void isLeap_forDateOnCopticLeapDay_returnsTrueForAllProperties() {
+        // The Coptic calendar has 12 months of 30 days, plus a 13th "intercalary"
+        // month of 5 days in a common year and 6 days in a leap year.
+        // The 6th day of the 13th month is the leap day and only exists in a leap year.
+        // Coptic year 3 is a leap year.
+        final Chronology copticChronology = CopticChronology.getInstance();
+        final DateTime copticLeapDate = new DateTime(3, 13, 6, 0, 0, copticChronology);
 
-    private static long SKIP = 1 * MILLIS_PER_DAY;
+        // Assert that the year property correctly identifies a leap year
+        assertTrue("Year 3 should be a leap year", copticLeapDate.year().isLeap());
 
-    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
+        // Assert that the month property identifies the 13th month as a leap month
+        assertTrue("Month 13 in a leap year should be a leap month", copticLeapDate.monthOfYear().isLeap());
 
-    private static final DateTimeZone LONDON = DateTimeZone.forID("Europe/London");
+        // Assert that the day of month property identifies the 6th day as a leap day
+        assertTrue("Day 6 of month 13 should be a leap day", copticLeapDate.dayOfMonth().isLeap());
 
-    private static final DateTimeZone TOKYO = DateTimeZone.forID("Asia/Tokyo");
-
-    private static final Chronology COPTIC_UTC = CopticChronology.getInstanceUTC();
-
-    private static final Chronology JULIAN_UTC = JulianChronology.getInstanceUTC();
-
-    private static final Chronology ISO_UTC = ISOChronology.getInstanceUTC();
-
-    long y2002days = 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365;
-
-    // 2002-06-09
-    private long TEST_TIME_NOW = (y2002days + 31L + 28L + 31L + 30L + 31L + 9L - 1L) * MILLIS_PER_DAY;
-
-    private DateTimeZone originalDateTimeZone = null;
-
-    private TimeZone originalTimeZone = null;
-
-    private Locale originalLocale = null;
-
-    public static void main(String[] args) {
-        junit.textui.TestRunner.run(suite());
-    }
-
-    public static TestSuite suite() {
-        SKIP = 1 * MILLIS_PER_DAY;
-        return new TestSuite(TestCopticChronology.class);
-    }
-
-    @Override
-    protected void setUp() throws Exception {
-        DateTimeUtils.setCurrentMillisFixed(TEST_TIME_NOW);
-        originalDateTimeZone = DateTimeZone.getDefault();
-        originalTimeZone = TimeZone.getDefault();
-        originalLocale = Locale.getDefault();
-        DateTimeZone.setDefault(LONDON);
-        TimeZone.setDefault(TimeZone.getTimeZone("Europe/London"));
-        Locale.setDefault(Locale.UK);
-    }
-
-    @Override
-    protected void tearDown() throws Exception {
-        DateTimeUtils.setCurrentMillisSystem();
-        DateTimeZone.setDefault(originalDateTimeZone);
-        TimeZone.setDefault(originalTimeZone);
-        Locale.setDefault(originalLocale);
-        originalDateTimeZone = null;
-        originalTimeZone = null;
-        originalLocale = null;
-    }
-
-    public void testLeap_6_13() {
-        Chronology chrono = CopticChronology.getInstance();
-        DateTime dt = new DateTime(3, 13, 6, 0, 0, chrono);
-        assertEquals(true, dt.year().isLeap());
-        assertEquals(true, dt.monthOfYear().isLeap());
-        assertEquals(true, dt.dayOfMonth().isLeap());
-        assertEquals(true, dt.dayOfYear().isLeap());
+        // Assert that the day of year property identifies the last day as a leap day
+        assertTrue("The 366th day of the year should be a leap day", copticLeapDate.dayOfYear().isLeap());
     }
 }
