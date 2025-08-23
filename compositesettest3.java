38b07e5dbdb59d92992cbc822e28c953fed599ed
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:27:00 +0800

    Original processedManualTestCases compositesettest3.java

---
+++ compositesettest3.java
@@ -0,0 +1,56 @@
+package org.apache.commons.collections4.set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.apache.commons.collections4.set.CompositeSet.SetMutator;
+import org.junit.jupiter.api.Test;
+
+public class CompositeSetTestTest3<E> extends AbstractSetTest<E> {
+
+    @SuppressWarnings("unchecked")
+    public Set<E> buildOne() {
+        final HashSet<E> set = new HashSet<>();
+        set.add((E) "1");
+        set.add((E) "2");
+        return set;
+    }
+
+    @SuppressWarnings("unchecked")
+    public Set<E> buildTwo() {
+        final HashSet<E> set = new HashSet<>();
+        set.add((E) "3");
+        set.add((E) "4");
+        return set;
+    }
+
+    @Override
+    public String getCompatibilityVersion() {
+        return "4";
+    }
+
+    @Override
+    protected int getIterationBehaviour() {
+        return UNORDERED;
+    }
+
+    @Override
+    public CompositeSet<E> makeObject() {
+        final HashSet<E> contained = new HashSet<>();
+        final CompositeSet<E> set = new CompositeSet<>(contained);
+        set.setMutator(new EmptySetMutator<>(contained));
+        return set;
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    void testContains() {
+        final CompositeSet<E> set = new CompositeSet<>(buildOne(), buildTwo());
+        assertTrue(set.contains("1"));
+    }
+}
