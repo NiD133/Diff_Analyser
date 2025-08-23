f3ae4f8082c12ec7b79f0a1dd9d4d0cc1d863927
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:29:48 +0800

    Original processedManualTestCases iso8601utilstest6.java

---
+++ iso8601utilstest6.java
@@ -0,0 +1,33 @@
+package com.google.gson.internal.bind.util;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertThrows;
+import java.text.ParseException;
+import java.text.ParsePosition;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+import java.util.TimeZone;
+import org.junit.Test;
+
+public class ISO8601UtilsTestTest6 {
+
+    private static TimeZone utcTimeZone() {
+        return TimeZone.getTimeZone("UTC");
+    }
+
+    private static GregorianCalendar createUtcCalendar() {
+        TimeZone utc = utcTimeZone();
+        GregorianCalendar calendar = new GregorianCalendar(utc);
+        // Calendar was created with current time, must clear it
+        calendar.clear();
+        return calendar;
+    }
+
+    @Test
+    public void testDateParseInvalidMonth() {
+        String dateStr = "2022-14-30";
+        assertThrows(ParseException.class, () -> ISO8601Utils.parse(dateStr, new ParsePosition(0)));
+    }
+}
