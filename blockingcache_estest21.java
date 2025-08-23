2832e3ad23fdb5fb46f1e9176eb1c7669b1ab6c2
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:39:30 +0800

    Original processedAutoTestCases blockingcache_estest21.java

---
+++ blockingcache_estest21.java
@@ -0,0 +1,23 @@
+package org.apache.ibatis.cache.decorators;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.concurrent.CountDownLatch;
+import org.apache.ibatis.cache.Cache;
+import org.apache.ibatis.cache.impl.PerpetualCache;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class BlockingCache_ESTestTest21 extends BlockingCache_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test20() throws Throwable {
+        PerpetualCache perpetualCache0 = new PerpetualCache("x");
+        SoftCache softCache0 = new SoftCache(perpetualCache0);
+        BlockingCache blockingCache0 = new BlockingCache(softCache0);
+        long long0 = blockingCache0.getTimeout();
+        assertEquals(0L, long0);
+    }
+}
