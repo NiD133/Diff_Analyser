6f0687b9ef98cc0a6926edfd9d8073d755758f33
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:37:57 +0800

    Original processedAutoTestCases atomicdoublearray_estest39.java

---
+++ atomicdoublearray_estest39.java
@@ -0,0 +1,23 @@
+package com.google.common.util.concurrent;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.function.DoubleBinaryOperator;
+import java.util.function.DoubleUnaryOperator;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class AtomicDoubleArray_ESTestTest39 extends AtomicDoubleArray_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test38() throws Throwable {
+        double[] doubleArray0 = new double[2];
+        AtomicDoubleArray atomicDoubleArray0 = new AtomicDoubleArray(doubleArray0);
+        int int0 = atomicDoubleArray0.length();
+        assertEquals(2, int0);
+    }
+}
