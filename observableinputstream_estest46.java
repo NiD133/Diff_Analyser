d1684e9c768598471dec2b34385ca9ea9811b27d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:06:04 +0800

    Original processedAutoTestCases observableinputstream_estest46.java

---
+++ observableinputstream_estest46.java
@@ -0,0 +1,54 @@
+package org.apache.commons.io.input;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.FileDescriptor;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.io.PushbackInputStream;
+import java.io.SequenceInputStream;
+import java.io.StringWriter;
+import java.nio.CharBuffer;
+import java.nio.file.NoSuchFileException;
+import java.security.MessageDigest;
+import java.util.Enumeration;
+import java.util.LinkedList;
+import java.util.List;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.io.MockFileInputStream;
+import org.evosuite.runtime.mock.java.io.MockIOException;
+import org.junit.runner.RunWith;
+
+public class ObservableInputStream_ESTestTest46 extends ObservableInputStream_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test45() throws Throwable {
+        PipedInputStream pipedInputStream0 = new PipedInputStream(3);
+        ObservableInputStream.Observer[] observableInputStream_ObserverArray0 = new ObservableInputStream.Observer[1];
+        ObservableInputStream observableInputStream0 = new ObservableInputStream(pipedInputStream0, observableInputStream_ObserverArray0);
+        PushbackInputStream pushbackInputStream0 = new PushbackInputStream(observableInputStream0);
+        BufferedInputStream bufferedInputStream0 = new BufferedInputStream(pushbackInputStream0);
+        ObservableInputStream observableInputStream1 = new ObservableInputStream(bufferedInputStream0);
+        byte[] byteArray0 = new byte[1];
+        // Undeclared exception!
+        try {
+            observableInputStream1.read(byteArray0);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.apache.commons.io.input.ObservableInputStream", e);
+        }
+    }
+}
