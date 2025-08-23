e8dd6df4a96bf61e55fd9c762f3af544adaee023
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:16:32 +0800

    gemini processedAutoTestCases simplepathinterpreter_estest28.java

--- simplepathinterpreter_estest28.java
+++ simplepathinterpreter_estest28.java
@@ -1,73 +1,56 @@
 package org.apache.commons.jxpath.ri.axes;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.Locale;
-import org.apache.commons.jxpath.BasicVariables;
-import org.apache.commons.jxpath.JXPathBasicBeanInfo;
 import org.apache.commons.jxpath.JXPathContext;
 import org.apache.commons.jxpath.ri.EvalContext;
 import org.apache.commons.jxpath.ri.JXPathContextReferenceImpl;
 import org.apache.commons.jxpath.ri.QName;
-import org.apache.commons.jxpath.ri.compiler.Constant;
-import org.apache.commons.jxpath.ri.compiler.CoreFunction;
-import org.apache.commons.jxpath.ri.compiler.CoreOperationAnd;
-import org.apache.commons.jxpath.ri.compiler.CoreOperationEqual;
-import org.apache.commons.jxpath.ri.compiler.CoreOperationGreaterThanOrEqual;
-import org.apache.commons.jxpath.ri.compiler.CoreOperationLessThanOrEqual;
-import org.apache.commons.jxpath.ri.compiler.CoreOperationMod;
-import org.apache.commons.jxpath.ri.compiler.CoreOperationMultiply;
-import org.apache.commons.jxpath.ri.compiler.CoreOperationNegate;
-import org.apache.commons.jxpath.ri.compiler.CoreOperationNotEqual;
-import org.apache.commons.jxpath.ri.compiler.CoreOperationOr;
-import org.apache.commons.jxpath.ri.compiler.CoreOperationSubtract;
-import org.apache.commons.jxpath.ri.compiler.CoreOperationUnion;
 import org.apache.commons.jxpath.ri.compiler.Expression;
-import org.apache.commons.jxpath.ri.compiler.NameAttributeTest;
-import org.apache.commons.jxpath.ri.compiler.NodeNameTest;
-import org.apache.commons.jxpath.ri.compiler.NodeTest;
-import org.apache.commons.jxpath.ri.compiler.NodeTypeTest;
-import org.apache.commons.jxpath.ri.compiler.ProcessingInstructionTest;
 import org.apache.commons.jxpath.ri.compiler.Step;
 import org.apache.commons.jxpath.ri.compiler.VariableReference;
 import org.apache.commons.jxpath.ri.model.NodePointer;
-import org.apache.commons.jxpath.ri.model.VariablePointer;
-import org.apache.commons.jxpath.ri.model.beans.BeanPointer;
-import org.apache.commons.jxpath.ri.model.beans.BeanPropertyPointer;
-import org.apache.commons.jxpath.ri.model.beans.NullPointer;
-import org.apache.commons.jxpath.ri.model.beans.NullPropertyPointer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
+import java.util.Locale;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+// The test class name is kept as is from the original file.
 public class SimplePathInterpreter_ESTestTest28 extends SimplePathInterpreter_ESTest_scaffolding {
 
+    /**
+     * Tests that interpretSimpleExpressionPath throws a RuntimeException when an expression
+     * contains a reference to a variable that is not defined in the JXPathContext.
+     */
     @Test(timeout = 4000)
-    public void test27() throws Throwable {
-        Locale locale0 = Locale.JAPANESE;
-        QName qName0 = new QName("<<unknown namespace>>");
-        NodePointer nodePointer0 = NodePointer.newNodePointer(qName0, qName0, locale0);
-        JXPathContextReferenceImpl jXPathContextReferenceImpl0 = new JXPathContextReferenceImpl((JXPathContext) null, nodePointer0, nodePointer0);
-        Constant constant0 = new Constant("string");
-        NameAttributeTest nameAttributeTest0 = new NameAttributeTest(constant0, constant0);
-        RootContext rootContext0 = new RootContext(jXPathContextReferenceImpl0, nodePointer0);
-        Expression[] expressionArray0 = new Expression[2];
-        CoreOperationGreaterThanOrEqual coreOperationGreaterThanOrEqual0 = new CoreOperationGreaterThanOrEqual(nameAttributeTest0, constant0);
-        expressionArray0[0] = (Expression) coreOperationGreaterThanOrEqual0;
-        VariableReference variableReference0 = new VariableReference(qName0);
-        expressionArray0[1] = (Expression) variableReference0;
-        // Undeclared exception!
+    public void interpretSimpleExpressionPathWithUndefinedVariableThrowsRuntimeException() {
+        // ARRANGE: Set up a context and an expression path with a predicate
+        // that references an undefined variable.
+
+        // 1. Define the name for the variable that will be undefined.
+        String undefinedVariableName = "<<unknown namespace>>";
+        QName undefinedVariableQName = new QName(undefinedVariableName);
+
+        // 2. Create a basic JXPath context. The root object is arbitrary as it's not
+        // relevant to the variable resolution logic being tested.
+        NodePointer rootPointer = NodePointer.newNodePointer(new QName("root"), new Object(), Locale.getDefault());
+        JXPathContextReferenceImpl jxpathContext = new JXPathContextReferenceImpl(null, rootPointer, rootPointer);
+        EvalContext evalContext = new RootContext(jxpathContext, rootPointer);
+
+        // 3. Create a predicate array containing only the reference to the undefined variable.
+        // This focuses the test on the specific failure condition.
+        Expression undefinedVariableRef = new VariableReference(undefinedVariableQName);
+        Expression[] predicates = {undefinedVariableRef};
+        Step[] steps = null; // The path has no steps, only predicates on the root.
+
+        // ACT & ASSERT: Attempting to interpret the path should throw a RuntimeException.
         try {
-            SimplePathInterpreter.interpretSimpleExpressionPath(rootContext0, nodePointer0, expressionArray0, (Step[]) null);
-            fail("Expecting exception: RuntimeException");
+            SimplePathInterpreter.interpretSimpleExpressionPath(evalContext, rootPointer, predicates, steps);
+            fail("Expected a RuntimeException for an undefined variable, but none was thrown.");
         } catch (RuntimeException e) {
-            //
-            // Undefined variable: <<unknown namespace>>
-            //
-            verifyException("org.apache.commons.jxpath.ri.model.VariablePointer$1", e);
+            // Verify that the exception message clearly indicates the undefined variable.
+            String expectedMessage = "Undefined variable: " + undefinedVariableName;
+            assertEquals(expectedMessage, e.getMessage());
         }
     }
 }
