e8fc4bef2318208b219de47a35fee5685c805e35
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:24:38 +0800

    Original processedAutoTestCases uncheck_estest4.java

---
+++ uncheck_estest4.java
@@ -0,0 +1,30 @@
+package org.apache.commons.io.function;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.lang.reflect.Array;
+import java.time.chrono.HijrahEra;
+import java.util.Comparator;
+import java.util.concurrent.ForkJoinTask;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+import java.util.stream.LongStream;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class Uncheck_ESTestTest4 extends Uncheck_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test03() throws Throwable {
+        IOPredicate<String> iOPredicate0 = IOPredicate.alwaysTrue();
+        boolean boolean0 = Uncheck.test(iOPredicate0, "after");
+        assertTrue(boolean0);
+    }
+}
