7e1ef093c08d77358367cfbc77e81eade283f699
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:32:21 +0800

    gemini processedManualTestCases posixparsertest1.java

--- posixparsertest1.java
+++ posixparsertest1.java
@@ -1,10 +1,33 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.commons.cli;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-public class PosixParserTestTest1 extends AbstractParserTestCase {
+/**
+ * Tests for the deprecated {@link PosixParser}.
+ * <p>
+ * This class inherits from {@link AbstractParserTestCase} and selectively disables
+ * tests for features that the {@code PosixParser} does not support.
+ * </p>
+ */
+public class PosixParserTest extends AbstractParserTestCase {
 
     @Override
     @SuppressWarnings("deprecation")
@@ -14,9 +37,16 @@ public class PosixParserTestTest1 extends AbstractParserTestCase {
         parser = new PosixParser();
     }
 
+    /**
+     * Overrides and disables the superclass test for ambiguous long options
+     * specified with a single dash (e.g., {@code -o-ambiguous}). The
+     * {@code PosixParser} does not support this syntax.
+     */
     @Override
     @Test
-    @Disabled("not supported by the PosixParser")
-    void testAmbiguousLongWithoutEqualSingleDash() throws Exception {
+    @Disabled("Not supported by PosixParser: ambiguous long options with a single dash.")
+    void testAmbiguousLongWithoutEqualSingleDash() {
+        // This test is intentionally left blank to disable the inherited test case.
+        // The PosixParser's behavior for this scenario is not applicable.
     }
 }
