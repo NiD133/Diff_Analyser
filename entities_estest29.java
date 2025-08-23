cf56b31865d5a42625122eb97d9822b410b2c393
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:49:26 +0800

    Original processedAutoTestCases entities_estest29.java

---
+++ entities_estest29.java
@@ -0,0 +1,35 @@
+package org.jsoup.nodes;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.File;
+import java.io.PipedWriter;
+import java.io.StringWriter;
+import java.nio.BufferOverflowException;
+import java.nio.CharBuffer;
+import java.nio.ReadOnlyBufferException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.evosuite.runtime.mock.java.io.MockFileWriter;
+import org.evosuite.runtime.mock.java.io.MockPrintStream;
+import org.jsoup.internal.QuietAppendable;
+import org.junit.runner.RunWith;
+
+public class Entities_ESTestTest29 extends Entities_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test28() throws Throwable {
+        Document.OutputSettings document_OutputSettings0 = new Document.OutputSettings();
+        // Undeclared exception!
+        try {
+            Entities.escape((QuietAppendable) null, "></", document_OutputSettings0, 108);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+        }
+    }
+}
