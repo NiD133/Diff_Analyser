d0cff2c614de09460f61e7b74bf2514efad8b5cf
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:46:17 +0800

    Original processedAutoTestCases cpioutil_estest15.java

---
+++ cpioutil_estest15.java
@@ -0,0 +1,16 @@
+package org.apache.commons.compress.archivers.cpio;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class CpioUtil_ESTestTest15 extends CpioUtil_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test14() throws Throwable {
+        CpioUtil cpioUtil0 = new CpioUtil();
+    }
+}
