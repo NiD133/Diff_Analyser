5ca6e0b4e8fd3510a59e422652afcf9785b34487
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:36:30 +0800

    Original processedManualTestCases yearstest5.java

---
+++ yearstest5.java
@@ -0,0 +1,41 @@
+package org.joda.time;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class YearsTestTest5 extends TestCase {
+
+    // (before the late 90's they were all over the place)
+    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
+
+    public static TestSuite suite() {
+        return new TestSuite(TestYears.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+    }
+
+    public void testFactory_yearsIn_RInterval() {
+        DateTime start = new DateTime(2006, 6, 9, 12, 0, 0, 0, PARIS);
+        DateTime end1 = new DateTime(2009, 6, 9, 12, 0, 0, 0, PARIS);
+        DateTime end2 = new DateTime(2012, 6, 9, 12, 0, 0, 0, PARIS);
+        assertEquals(0, Years.yearsIn((ReadableInterval) null).getYears());
+        assertEquals(3, Years.yearsIn(new Interval(start, end1)).getYears());
+        assertEquals(0, Years.yearsIn(new Interval(start, start)).getYears());
+        assertEquals(0, Years.yearsIn(new Interval(end1, end1)).getYears());
+        assertEquals(6, Years.yearsIn(new Interval(start, end2)).getYears());
+    }
+}
