4724f7a01e35424d18c96293000173411c486ec0
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 05:35:12 +0800

    Original manualTestSuite StandardBarPainterTest.java

--- StandardBarPainterTest.java
+++ StandardBarPainterTest.java
@@ -1,57 +1,97 @@
+/* ======================================================
+ * JFreeChart : a chart library for the Java(tm) platform
+ * ======================================================
+ *
+ * (C) Copyright 2000-present, by David Gilbert and Contributors.
+ *
+ * Project Info:  https://www.jfree.org/jfreechart/index.html
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ *
+ * [Oracle and Java are registered trademarks of Oracle and/or its affiliates. 
+ * Other names may be trademarks of their respective owners.]
+ *
+ * ---------------------------
+ * StandardBarPainterTest.java
+ * ---------------------------
+ * (C) Copyright 2008-present, by David Gilbert and Contributors.
+ *
+ * Original Author:  David Gilbert;
+ * Contributor(s):   -;
+ *
+ */
+
 package org.jfree.chart.renderer.category;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.jfree.chart.TestUtils;
 import org.jfree.chart.api.PublicCloneable;
+
 import org.junit.jupiter.api.Test;
 
 /**
- * Unit tests for the {@link StandardBarPainter} class.
+ * Tests for the {@link StandardBarPainter} class.
  */
 public class StandardBarPainterTest {
 
     /**
-     * Tests that two newly created {@link StandardBarPainter} objects are equal.
+     * Check that the equals() method distinguishes all fields.
      */
     @Test
     public void testEquals() {
-        StandardBarPainter painter1 = new StandardBarPainter();
-        StandardBarPainter painter2 = new StandardBarPainter();
-        assertEquals(painter1, painter2, "Two new StandardBarPainter instances should be equal.");
+        StandardBarPainter p1 = new StandardBarPainter();
+        StandardBarPainter p2 = new StandardBarPainter();
+        assertEquals(p1, p2);
     }
 
     /**
-     * Tests that two equal {@link StandardBarPainter} objects have the same hash code.
+     * Two objects that are equal are required to return the same hashCode.
      */
     @Test
-    public void testHashCode() {
-        StandardBarPainter painter1 = new StandardBarPainter();
-        StandardBarPainter painter2 = new StandardBarPainter();
-        assertEquals(painter1, painter2, "Two new StandardBarPainter instances should be equal.");
-        assertEquals(painter1.hashCode(), painter2.hashCode(), "Equal StandardBarPainter instances should have the same hash code.");
+    public void testHashcode() {
+        StandardBarPainter p1 = new StandardBarPainter();
+        StandardBarPainter p2 = new StandardBarPainter();
+        assertEquals(p1, p2);
+        int h1 = p1.hashCode();
+        int h2 = p2.hashCode();
+        assertEquals(h1, h2);
     }
 
     /**
-     * Tests that {@link StandardBarPainter} does not implement cloning interfaces.
-     * Since instances are immutable, cloning is not required.
+     * Confirm that cloning isn't implemented (it isn't required, because
+     * instances of the class are immutable).
      */
     @Test
     public void testCloning() {
-        StandardBarPainter painter = new StandardBarPainter();
-        assertFalse(painter instanceof Cloneable, "StandardBarPainter should not be cloneable.");
-        assertFalse(painter instanceof PublicCloneable, "StandardBarPainter should not implement PublicCloneable.");
+        StandardBarPainter p1 = new StandardBarPainter();
+        assertFalse(p1 instanceof Cloneable);
+        assertFalse(p1 instanceof PublicCloneable);
     }
 
     /**
-     * Tests serialization and deserialization of a {@link StandardBarPainter} instance.
-     * The deserialized instance should be equal to the original.
+     * Serialize an instance, restore it, and check for equality.
      */
     @Test
     public void testSerialization() {
-        StandardBarPainter originalPainter = new StandardBarPainter();
-        StandardBarPainter deserializedPainter = TestUtils.serialised(originalPainter);
-        assertEquals(originalPainter, deserializedPainter, "Deserialized StandardBarPainter should be equal to the original.");
+        StandardBarPainter p1 = new StandardBarPainter();
+        StandardBarPainter p2 = TestUtils.serialised(p1);
+        assertEquals(p1, p2);
     }
+
 }
