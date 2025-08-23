359065b6fadccdb22f3ab7911eb44f7fd7207a93
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:15:05 +0800

    Original processedAutoTestCases serializedstring_estest24.java

---
+++ serializedstring_estest24.java
@@ -0,0 +1,35 @@
+package com.fasterxml.jackson.core.io;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PipedOutputStream;
+import java.nio.ByteBuffer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.evosuite.runtime.mock.java.io.MockFileOutputStream;
+import org.evosuite.runtime.mock.java.io.MockPrintStream;
+import org.junit.runner.RunWith;
+
+public class SerializedString_ESTestTest24 extends SerializedString_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test23() throws Throwable {
+        SerializedString serializedString0 = new SerializedString("wSQ ");
+        // Undeclared exception!
+        try {
+            serializedString0.appendUnquoted((char[]) null, (-1666));
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("com.fasterxml.jackson.core.io.SerializedString", e);
+        }
+    }
+}
