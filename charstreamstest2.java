e0f157541741e233daa7969520adf03d1e11ead2
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:26:44 +0800

    gemini processedManualTestCases charstreamstest2.java

--- charstreamstest2.java
+++ charstreamstest2.java
@@ -1,85 +1,72 @@
 package com.google.common.io;
 
-import static org.junit.Assert.assertThrows;
-import com.google.common.base.Strings;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import com.google.common.collect.ImmutableList;
-import java.io.EOFException;
-import java.io.FilterReader;
 import java.io.IOException;
-import java.io.Reader;
 import java.io.StringReader;
-import java.io.StringWriter;
-import java.io.Writer;
-import java.nio.CharBuffer;
 import java.util.List;
-import org.jspecify.annotations.NullUnmarked;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-public class CharStreamsTestTest2 extends IoTestCase {
+/**
+ * Tests for {@link CharStreams#readLines(Readable)}.
+ */
+@DisplayName("CharStreams.readLines()")
+class CharStreamsReadLinesTest {
 
-    private static final String TEXT = "The quick brown fox jumped over the lazy dog.";
+    @Test
+    void readLines_withNewlineSeparatedString_returnsListOfLines() throws IOException {
+        // Arrange
+        String inputWithNewlines = "a\nb\nc";
+        StringReader reader = new StringReader(inputWithNewlines);
+        List<String> expectedLines = ImmutableList.of("a", "b", "c");
 
-    /**
-     * Returns a reader wrapping the given reader that only reads half of the maximum number of
-     * characters that it could read in read(char[], int, int).
-     */
-    private static Reader newNonBufferFillingReader(Reader reader) {
-        return new FilterReader(reader) {
+        // Act
+        List<String> actualLines = CharStreams.readLines(reader);
 
-            @Override
-            public int read(char[] cbuf, int off, int len) throws IOException {
-                // if a buffer isn't being cleared correctly, this method will eventually start being called
-                // with a len of 0 forever
-                if (len <= 0) {
-                    fail("read called with a len of " + len);
-                }
-                // read fewer than the max number of chars to read
-                // shouldn't be a problem unless the buffer is shrinking each call
-                return in.read(cbuf, off, Math.max(len - 1024, 0));
-            }
-        };
+        // Assert
+        assertEquals(expectedLines, actualLines);
     }
 
-    /**
-     * Wrap an appendable in an appendable to defeat any type specific optimizations.
-     */
-    private static Appendable wrapAsGenericAppendable(Appendable a) {
-        return new Appendable() {
+    @Test
+    void readLines_withTrailingNewline_doesNotIncludeEmptyStringAtEnd() throws IOException {
+        // Arrange
+        String inputWithTrailingNewline = "a\nb\n";
+        StringReader reader = new StringReader(inputWithTrailingNewline);
+        List<String> expectedLines = ImmutableList.of("a", "b");
 
-            @Override
-            public Appendable append(CharSequence csq) throws IOException {
-                a.append(csq);
-                return this;
-            }
+        // Act
+        List<String> actualLines = CharStreams.readLines(reader);
 
-            @Override
-            public Appendable append(CharSequence csq, int start, int end) throws IOException {
-                a.append(csq, start, end);
-                return this;
-            }
-
-            @Override
-            public Appendable append(char c) throws IOException {
-                a.append(c);
-                return this;
-            }
-        };
+        // Assert
+        assertEquals(expectedLines, actualLines);
     }
 
-    /**
-     * Wrap a readable in a readable to defeat any type specific optimizations.
-     */
-    private static Readable wrapAsGenericReadable(Readable a) {
-        return new Readable() {
+    @Test
+    void readLines_withEmptyInput_returnsEmptyList() throws IOException {
+        // Arrange
+        StringReader emptyReader = new StringReader("");
+        List<String> expectedLines = ImmutableList.of();
 
-            @Override
-            public int read(CharBuffer cb) throws IOException {
-                return a.read(cb);
-            }
-        };
+        // Act
+        List<String> actualLines = CharStreams.readLines(emptyReader);
+
+        // Assert
+        assertEquals(expectedLines, actualLines);
     }
 
-    public void testReadLines() throws IOException {
-        List<String> lines = CharStreams.readLines(new StringReader("a\nb\nc"));
-        assertEquals(ImmutableList.of("a", "b", "c"), lines);
+    @Test
+    void readLines_withCarriageReturnLineFeed_returnsListOfLines() throws IOException {
+        // Arrange
+        String inputWithCrLf = "a\r\nb\r\nc";
+        StringReader reader = new StringReader(inputWithCrLf);
+        List<String> expectedLines = ImmutableList.of("a", "b", "c");
+
+        // Act
+        List<String> actualLines = CharStreams.readLines(reader);
+
+        // Assert
+        assertEquals(expectedLines, actualLines);
     }
 }
