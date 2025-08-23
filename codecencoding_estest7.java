c97b691501a0c3266a8057d7cd7d504d5571a0c8
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:44:04 +0800

    Original processedAutoTestCases codecencoding_estest7.java

---
+++ codecencoding_estest7.java
@@ -0,0 +1,29 @@
+package org.apache.commons.compress.harmony.pack200;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.io.PushbackInputStream;
+import java.io.SequenceInputStream;
+import java.util.Enumeration;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class CodecEncoding_ESTestTest7 extends CodecEncoding_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test06() throws Throwable {
+        BHSDCodec bHSDCodec0 = CodecEncoding.getCanonicalCodec(9);
+        assertFalse(bHSDCodec0.isSigned());
+    }
+}
