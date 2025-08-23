226fcabfe8cb149100ddd0d8513bc4680913310e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:28:36 +0800

    gemini processedManualTestCases extensionfunctiontest12.java

--- extensionfunctiontest12.java
+++ extensionfunctiontest12.java
@@ -1,101 +1,109 @@
 package org.apache.commons.jxpath.ri.compiler;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertInstanceOf;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Locale;
-import org.apache.commons.jxpath.AbstractJXPathTest;
 import org.apache.commons.jxpath.ClassFunctions;
 import org.apache.commons.jxpath.ExpressionContext;
 import org.apache.commons.jxpath.Function;
-import org.apache.commons.jxpath.FunctionLibrary;
-import org.apache.commons.jxpath.Functions;
 import org.apache.commons.jxpath.JXPathContext;
-import org.apache.commons.jxpath.NodeSet;
-import org.apache.commons.jxpath.PackageFunctions;
 import org.apache.commons.jxpath.Pointer;
-import org.apache.commons.jxpath.TestBean;
-import org.apache.commons.jxpath.Variables;
 import org.apache.commons.jxpath.ri.model.NodePointer;
-import org.apache.commons.jxpath.util.JXPath11CompatibleTypeConverter;
-import org.apache.commons.jxpath.util.TypeConverter;
-import org.apache.commons.jxpath.util.TypeUtils;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class ExtensionFunctionTestTest12 extends AbstractJXPathTest {
+import java.util.List;
+import java.util.Locale;
 
-    private Functions functions;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-    private JXPathContext context;
+/**
+ * Tests the behavior of ClassFunctions when dealing with methods that accept an ExpressionContext.
+ *
+ * This test focuses on verifying that JXPath can correctly find and invoke an instance method
+ * that has an ExpressionContext as one of its parameters.
+ */
+@DisplayName("ClassFunctions with ExpressionContext")
+class ClassFunctionsWithExpressionContextTest {
 
-    private TestBean testBean;
+    /**
+     * A mock implementation of ExpressionContext used to supply a specific
+     * context value for the test.
+     */
+    private static class TestExpressionContext implements ExpressionContext {
+        private final Object contextObject;
 
-    private TypeConverter typeConverter;
-
-    @Override
-    @BeforeEach
-    public void setUp() {
-        if (context == null) {
-            testBean = new TestBean();
-            context = JXPathContext.newContext(testBean);
-            final Variables vars = context.getVariables();
-            vars.declareVariable("test", new TestFunctions(4, "test"));
-            final FunctionLibrary lib = new FunctionLibrary();
-            lib.addFunctions(new ClassFunctions(TestFunctions.class, "test"));
-            lib.addFunctions(new ClassFunctions(TestFunctions2.class, "test"));
-            lib.addFunctions(new PackageFunctions("", "call"));
-            lib.addFunctions(new PackageFunctions("org.apache.commons.jxpath.ri.compiler.", "jxpathtest"));
-            lib.addFunctions(new PackageFunctions("", null));
-            context.setFunctions(lib);
-            context.getVariables().declareVariable("List.class", List.class);
-            context.getVariables().declareVariable("NodeSet.class", NodeSet.class);
-        }
-        functions = new ClassFunctions(TestFunctions.class, "test");
-        typeConverter = TypeUtils.getTypeConverter();
-    }
-
-    @AfterEach
-    public void tearDown() {
-        TypeUtils.setTypeConverter(typeConverter);
-    }
-
-    private static final class Context implements ExpressionContext {
-
-        private final Object object;
-
-        public Context(final Object object) {
-            this.object = object;
-        }
-
-        @Override
-        public List<Pointer> getContextNodeList() {
-            return null;
+        public TestExpressionContext(Object contextObject) {
+            this.contextObject = contextObject;
         }
 
         @Override
         public Pointer getContextNodePointer() {
-            return NodePointer.newNodePointer(null, object, Locale.getDefault());
+            // The value of the context is extracted from this pointer.
+            return NodePointer.newNodePointer(null, contextObject, Locale.getDefault());
         }
 
+        // Unused methods for this test
         @Override
-        public JXPathContext getJXPathContext() {
-            return null;
-        }
+        public JXPathContext getJXPathContext() { return null; }
+        @Override
+        public List<Pointer> getContextNodeList() { return null; }
+        @Override
+        public int getPosition() { return 0; }
+    }
 
-        @Override
-        public int getPosition() {
-            return 0;
+    /**
+     * A helper class containing the target method for this test.
+     * In the original test suite, this was a more complex, shared class.
+     */
+    public static class TestFunctions {
+        /**
+         * An instance method that takes an ExpressionContext and a String.
+         * JXPath's invoke mechanism will extract the value from the context.
+         */
+        public String pathWithSuffix(ExpressionContext context, String suffix) {
+            Object contextValue = context.getContextNodePointer().getValue();
+            return contextValue + suffix;
         }
     }
 
+    private ClassFunctions classFunctions;
+
+    @BeforeEach
+    void setUp() {
+        // The System Under Test (SUT) is ClassFunctions, configured to find
+        // methods within the TestFunctions class under the "test" namespace.
+        classFunctions = new ClassFunctions(TestFunctions.class, "test");
+    }
+
     @Test
-    void testMethodLookupWithExpressionContextAndArgument() {
-        final Object[] args = { new TestFunctions(), "*" };
-        final Function func = functions.getFunction("test", "pathWithSuffix", args);
-        assertEquals("1*", func.invoke(new Context(Integer.valueOf(1)), args), "test:pathWithSuffix('*')");
+    @DisplayName("should find and invoke an instance method accepting an ExpressionContext")
+    void shouldFindAndInvokeMethodAcceptingExpressionContext() {
+        // ARRANGE
+        // The target method is `TestFunctions.pathWithSuffix(ExpressionContext, String)`.
+        // Since this is an instance method, the first element of the arguments array
+        // must be the instance on which the method will be called.
+        final TestFunctions targetInstance = new TestFunctions();
+        final String suffixArgument = "*";
+        final Object[] lookupArgs = {targetInstance, suffixArgument};
+
+        // The function will be invoked with a custom ExpressionContext that provides an Integer value.
+        final Integer contextValue = 1;
+        final ExpressionContext invocationContext = new TestExpressionContext(contextValue);
+
+        // ACT
+        // 1. Find the function. JXPath's method lookup is able to find a method that
+        // accepts an ExpressionContext, even though one is not provided in the lookup arguments.
+        final Function function = classFunctions.getFunction("test", "pathWithSuffix", lookupArgs);
+        assertNotNull(function, "Function 'test:pathWithSuffix' should be found.");
+
+        // 2. Invoke the function. The `invoke` implementation correctly passes the `invocationContext`
+        // to the method, using `targetInstance` from `lookupArgs[0]` as the target object
+        // and `suffixArgument` from `lookupArgs[1]` as the second parameter.
+        final Object result = function.invoke(invocationContext, lookupArgs);
+
+        // ASSERT
+        // The method is expected to concatenate the context value (1) and the suffix ("*").
+        final String expectedResult = "1*";
+        assertEquals(expectedResult, result, "Method should combine the context value and the string argument.");
     }
 }
