4b6fd55c3caf922835452cb42f4b3ab82b05ff38
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:38:26 +0800

    Original processedAutoTestCases attribute_estest71.java

---
+++ attribute_estest71.java
@@ -0,0 +1,31 @@
+package org.jsoup.nodes;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.ByteArrayOutputStream;
+import java.io.FilterOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PipedWriter;
+import java.io.StringWriter;
+import java.nio.BufferOverflowException;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetEncoder;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockPrintWriter;
+import org.jsoup.internal.QuietAppendable;
+import org.junit.runner.RunWith;
+
+public class Attribute_ESTestTest71 extends Attribute_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test70() throws Throwable {
+        Attribute attribute0 = Attribute.createFromEncoded("data-(i7=", "dvy}i6(v)yl6hca_8");
+        attribute0.setValue((String) null);
+        boolean boolean0 = attribute0.hasDeclaredValue();
+        assertEquals("", attribute0.getValue());
+        assertFalse(boolean0);
+    }
+}
