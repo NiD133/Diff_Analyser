06b36f3073500458f75d605e8524561ea4d6d2a6
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:01:15 +0800

    gemini processedAutoTestCases locationtextextractionstrategy_estest19.java

--- locationtextextractionstrategy_estest19.java
+++ locationtextextractionstrategy_estest19.java
@@ -1,38 +1,27 @@
 package com.itextpdf.text.pdf.parser;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import com.itextpdf.text.pdf.CMapAwareDocumentFont;
-import com.itextpdf.text.pdf.PdfDate;
-import com.itextpdf.text.pdf.PdfDictionary;
-import com.itextpdf.text.pdf.PdfIndirectReference;
-import com.itextpdf.text.pdf.PdfOCProperties;
-import com.itextpdf.text.pdf.PdfSigLockDictionary;
-import com.itextpdf.text.pdf.PdfString;
-import java.nio.charset.IllegalCharsetNameException;
-import java.util.Collection;
-import java.util.LinkedList;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
 
-public class LocationTextExtractionStrategy_ESTestTest19 extends LocationTextExtractionStrategy_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link LocationTextExtractionStrategy} class.
+ */
+public class LocationTextExtractionStrategyTest {
 
-    @Test(timeout = 4000)
-    public void test18() throws Throwable {
-        LocationTextExtractionStrategy locationTextExtractionStrategy0 = new LocationTextExtractionStrategy();
-        // Undeclared exception!
-        try {
-            locationTextExtractionStrategy0.isChunkAtWordBoundary((LocationTextExtractionStrategy.TextChunk) null, (LocationTextExtractionStrategy.TextChunk) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("com.itextpdf.text.pdf.parser.LocationTextExtractionStrategy", e);
-        }
+    /**
+     * Verifies that calling {@link LocationTextExtractionStrategy#isChunkAtWordBoundary(LocationTextExtractionStrategy.TextChunk, LocationTextExtractionStrategy.TextChunk)}
+     * with null arguments throws a {@link NullPointerException}.
+     *
+     * The method is expected to attempt to access methods on the first null parameter,
+     * which will trigger the exception.
+     */
+    @Test(expected = NullPointerException.class)
+    public void isChunkAtWordBoundary_whenCalledWithNullChunks_throwsNullPointerException() {
+        // Arrange
+        LocationTextExtractionStrategy strategy = new LocationTextExtractionStrategy();
+
+        // Act & Assert
+        // This call is expected to throw a NullPointerException.
+        // The assertion is handled by the 'expected' attribute of the @Test annotation.
+        strategy.isChunkAtWordBoundary(null, null);
     }
 }
