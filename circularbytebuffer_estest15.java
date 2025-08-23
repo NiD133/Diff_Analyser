4ecdd6d932911337cf5baf7f5e913df87604fef0
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:43:17 +0800

    gemini processedAutoTestCases circularbytebuffer_estest15.java

--- circularbytebuffer_estest15.java
+++ circularbytebuffer_estest15.java
@@ -1,20 +1,35 @@
 package org.apache.commons.io.input.buffer;
 
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class CircularByteBuffer_ESTestTest15 extends CircularByteBuffer_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
 
-    @Test(timeout = 4000)
-    public void test14() throws Throwable {
-        CircularByteBuffer circularByteBuffer0 = new CircularByteBuffer();
-        circularByteBuffer0.add((byte) 0);
-        int int0 = circularByteBuffer0.getCurrentNumberOfBytes();
-        assertEquals(8191, circularByteBuffer0.getSpace());
-        assertEquals(1, int0);
+/**
+ * Tests for {@link CircularByteBuffer}.
+ */
+public class CircularByteBufferTest {
+
+    /**
+     * Tests that adding a single byte to a new buffer correctly updates the
+     * number of available bytes and the remaining space.
+     */
+    @Test
+    public void addSingleByteShouldIncreaseCountAndDecreaseSpace() {
+        // Arrange: Create a buffer with the default size.
+        // The default size is IOUtils.DEFAULT_BUFFER_SIZE (8192 bytes).
+        final CircularByteBuffer buffer = new CircularByteBuffer();
+
+        // Act: Add a single byte to the buffer.
+        buffer.add((byte) 0);
+
+        // Assert: Check that the byte count is 1 and the available space has decreased by 1.
+        final int expectedCount = 1;
+        final int expectedSpace = IOUtils.DEFAULT_BUFFER_SIZE - 1; // 8192 - 1 = 8191
+
+        assertEquals("The number of bytes in the buffer should be 1.",
+                     expectedCount, buffer.getCurrentNumberOfBytes());
+        assertEquals("The available space should be the total size minus 1.",
+                     expectedSpace, buffer.getSpace());
     }
 }
