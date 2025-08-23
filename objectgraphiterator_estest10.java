7ed18d16fc42a0270a77178982ca83e77d78283c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:05:37 +0800

    gemini processedAutoTestCases objectgraphiterator_estest10.java

--- objectgraphiterator_estest10.java
+++ objectgraphiterator_estest10.java
@@ -1,55 +1,32 @@
 package org.apache.commons.collections4.iterators;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Comparator;
-import java.util.ConcurrentModificationException;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.ListIterator;
-import java.util.Map;
-import java.util.NoSuchElementException;
-import java.util.function.Function;
-import org.apache.commons.collections4.Factory;
-import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AndPredicate;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantFactory;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionPredicate;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.FactoryTransformer;
-import org.apache.commons.collections4.functors.FalsePredicate;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.MapTransformer;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NullIsFalsePredicate;
-import org.apache.commons.collections4.functors.NullIsTruePredicate;
-import org.apache.commons.collections4.functors.OrPredicate;
-import org.apache.commons.collections4.functors.PredicateTransformer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertNotSame;
 
-public class ObjectGraphIterator_ESTestTest10 extends ObjectGraphIterator_ESTest_scaffolding {
+/**
+ * Tests for the {@link ObjectGraphIterator} class, focusing on its constructors.
+ */
+public class ObjectGraphIteratorTest {
 
-    @Test(timeout = 4000)
-    public void test09() throws Throwable {
-        LinkedList<Integer> linkedList0 = new LinkedList<Integer>();
-        Integer integer0 = new Integer((-12));
-        ObjectGraphIterator<Integer> objectGraphIterator0 = new ObjectGraphIterator<Integer>(integer0, (Transformer<? super Integer, ? extends Integer>) null);
-        ObjectGraphIterator<Object> objectGraphIterator1 = new ObjectGraphIterator<Object>(objectGraphIterator0, (Transformer<? super Object, ?>) null);
-        ObjectGraphIterator<Object> objectGraphIterator2 = new ObjectGraphIterator<Object>(objectGraphIterator1);
-        assertFalse(objectGraphIterator2.equals((Object) objectGraphIterator1));
+    /**
+     * Tests that the constructor taking an iterator as a root creates a new,
+     * distinct iterator instance.
+     */
+    @Test
+    public void constructorWithRootIteratorShouldCreateDistinctInstance() {
+        // Arrange
+        // Create a base iterator that will serve as the root for another iterator.
+        ObjectGraphIterator<String> originalIterator = new ObjectGraphIterator<>("root element");
+
+        // Act
+        // Create a new iterator using the original iterator as its root.
+        // This invokes the constructor: public ObjectGraphIterator(final Iterator<? extends E> rootIterator)
+        ObjectGraphIterator<String> newIterator = new ObjectGraphIterator<>(originalIterator);
+
+        // Assert
+        // The new iterator should be a distinct object instance from the original.
+        // We use assertNotSame because ObjectGraphIterator does not override equals(),
+        // and we want to explicitly test for reference inequality.
+        assertNotSame(originalIterator, newIterator);
     }
 }
