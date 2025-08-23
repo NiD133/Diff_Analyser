28ae033cbe706cf96c7c509a158cbfb836a5db3a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:18:27 +0800

    gemini processedAutoTestCases stringconverter_estest27.java

--- stringconverter_estest27.java
+++ stringconverter_estest27.java
@@ -1,56 +1,32 @@
 package org.joda.time.convert;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
 import org.joda.time.Chronology;
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeFieldType;
-import org.joda.time.DateTimeZone;
-import org.joda.time.LocalDate;
-import org.joda.time.MonthDay;
-import org.joda.time.MutableDateTime;
-import org.joda.time.MutableInterval;
 import org.joda.time.MutablePeriod;
-import org.joda.time.Partial;
-import org.joda.time.PeriodType;
-import org.joda.time.ReadWritableInterval;
 import org.joda.time.ReadWritablePeriod;
-import org.joda.time.ReadableInstant;
-import org.joda.time.ReadablePartial;
-import org.joda.time.chrono.CopticChronology;
-import org.joda.time.chrono.EthiopicChronology;
-import org.joda.time.chrono.GJChronology;
 import org.joda.time.chrono.GregorianChronology;
-import org.joda.time.chrono.ISOChronology;
-import org.joda.time.chrono.IslamicChronology;
-import org.joda.time.chrono.JulianChronology;
-import org.joda.time.chrono.ZonedChronology;
-import org.joda.time.format.DateTimeFormatter;
-import org.joda.time.format.DateTimeParser;
-import org.joda.time.format.DateTimePrinter;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
+/**
+ * Unit tests for {@link StringConverter}.
+ */
+// The original test class name and inheritance are preserved.
 public class StringConverter_ESTestTest27 extends StringConverter_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test26() throws Throwable {
-        StringConverter stringConverter0 = new StringConverter();
-        GregorianChronology gregorianChronology0 = GregorianChronology.getInstanceUTC();
-        MutablePeriod mutablePeriod0 = new MutablePeriod();
-        // Undeclared exception!
-        try {
-            stringConverter0.setInto((ReadWritablePeriod) mutablePeriod0, (Object) "", (Chronology) gregorianChronology0);
-            fail("Expecting exception: IllegalArgumentException");
-        } catch (IllegalArgumentException e) {
-            //
-            // Invalid format: \"\"
-            //
-            verifyException("org.joda.time.format.PeriodFormatter", e);
-        }
+    /**
+     * Tests that calling setInto() with an empty string for a period
+     * results in an IllegalArgumentException, as it's an invalid format.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void setIntoPeriod_withEmptyString_shouldThrowIllegalArgumentException() {
+        // Arrange
+        StringConverter converter = new StringConverter();
+        ReadWritablePeriod period = new MutablePeriod();
+        Chronology chronology = GregorianChronology.getInstanceUTC();
+        String invalidPeriodString = "";
+
+        // Act
+        converter.setInto(period, invalidPeriodString, chronology);
+
+        // Assert: Exception is expected and handled by the @Test annotation.
     }
 }
