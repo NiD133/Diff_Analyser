cab6668baeae867f6ee687a38bc6b92d5a63def6
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:56:33 +0800

    gemini processedAutoTestCases jsonignoreproperties_estest34.java

--- jsonignoreproperties_estest34.java
+++ jsonignoreproperties_estest34.java
@@ -2,29 +2,35 @@ package com.fasterxml.jackson.annotation;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import java.util.LinkedHashSet;
-import java.util.Set;
-import java.util.function.Predicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
 
-public class JsonIgnoreProperties_ESTestTest34 extends JsonIgnoreProperties_ESTest_scaffolding {
+/**
+ * This test class focuses on the behavior of the {@link JsonIgnoreProperties.Value} class,
+ * specifically its equals() method.
+ */
+public class JsonIgnorePropertiesValueTest {
 
-    @Test(timeout = 4000)
-    public void test33() throws Throwable {
-        JsonIgnoreProperties.Value[] jsonIgnoreProperties_ValueArray0 = new JsonIgnoreProperties.Value[8];
-        String[] stringArray0 = new String[0];
-        JsonIgnoreProperties.Value jsonIgnoreProperties_Value0 = JsonIgnoreProperties.Value.forIgnoredProperties(stringArray0);
-        jsonIgnoreProperties_ValueArray0[0] = jsonIgnoreProperties_Value0;
-        JsonIgnoreProperties.Value jsonIgnoreProperties_Value1 = JsonIgnoreProperties.Value.forIgnoreUnknown(true);
-        assertFalse(jsonIgnoreProperties_Value1.getAllowSetters());
-        assertFalse(jsonIgnoreProperties_Value1.getAllowGetters());
-        assertTrue(jsonIgnoreProperties_Value1.getMerge());
-        jsonIgnoreProperties_ValueArray0[2] = jsonIgnoreProperties_Value1;
-        boolean boolean0 = jsonIgnoreProperties_ValueArray0[2].equals(jsonIgnoreProperties_ValueArray0[0]);
-        assertFalse(boolean0);
+    /**
+     * Tests that two {@link JsonIgnoreProperties.Value} instances created with different
+     * configurations are correctly identified as not being equal.
+     */
+    @Test
+    public void equals_shouldReturnFalse_forDifferentConfigurations() {
+        // Arrange
+        // Create a value configured to ignore a specific (but empty) set of properties.
+        JsonIgnoreProperties.Value valueWithNoIgnoredProperties = JsonIgnoreProperties.Value.forIgnoredProperties();
+
+        // Create another value configured to ignore any unknown properties.
+        JsonIgnoreProperties.Value valueWithIgnoreUnknown = JsonIgnoreProperties.Value.forIgnoreUnknown(true);
+
+        // Sanity check: Verify the default properties of the 'ignoreUnknown' value.
+        // The factory method should set these defaults consistently.
+        assertFalse("allowSetters should be false by default", valueWithIgnoreUnknown.getAllowSetters());
+        assertFalse("allowGetters should be false by default", valueWithIgnoreUnknown.getAllowGetters());
+        assertTrue("merge should be true by default", valueWithIgnoreUnknown.getMerge());
+
+        // Act & Assert
+        // The two instances have different settings (the _ignoreUnknown flag differs),
+        // so they should not be equal.
+        assertNotEquals(valueWithNoIgnoredProperties, valueWithIgnoreUnknown);
     }
 }
