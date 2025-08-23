f404f240dbdf053d53398e6a63da812ce28315fb
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:36:58 +0800

    Original processedAutoTestCases abstractstreaminghasher_estest23.java

---
+++ abstractstreaminghasher_estest23.java
@@ -0,0 +1,22 @@
+package com.google.common.hash;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.nio.BufferOverflowException;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class AbstractStreamingHasher_ESTestTest23 extends AbstractStreamingHasher_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test22() throws Throwable {
+        Crc32cHashFunction.Crc32cHasher crc32cHashFunction_Crc32cHasher0 = new Crc32cHashFunction.Crc32cHasher();
+        byte[] byteArray0 = new byte[2];
+        Hasher hasher0 = crc32cHashFunction_Crc32cHasher0.putBytes(byteArray0, 1, 1);
+        assertSame(hasher0, crc32cHashFunction_Crc32cHasher0);
+    }
+}
