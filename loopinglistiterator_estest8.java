a62cb4d34e405e966438f3780eaf6b38849ab84f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:01:45 +0800

    gemini processedAutoTestCases loopinglistiterator_estest8.java

--- loopinglistiterator_estest8.java
+++ loopinglistiterator_estest8.java
@@ -1,37 +1,31 @@
 package org.apache.commons.collections4.iterators;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
+
 import java.util.ConcurrentModificationException;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.NoSuchElementException;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class LoopingListIterator_ESTestTest8 extends LoopingListIterator_ESTest_scaffolding {
+import static org.junit.Assert.assertThrows;
 
-    @Test(timeout = 4000)
-    public void test07() throws Throwable {
-        LinkedList<Integer> linkedList0 = new LinkedList<Integer>();
-        List<Integer> list0 = linkedList0.subList(0, 0);
-        LoopingListIterator<Integer> loopingListIterator0 = new LoopingListIterator<Integer>(list0);
-        Integer integer0 = new Integer(0);
-        linkedList0.add(integer0);
-        // Undeclared exception!
-        try {
-            loopingListIterator0.previousIndex();
-            fail("Expecting exception: ConcurrentModificationException");
-        } catch (ConcurrentModificationException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("java.util.SubList", e);
-        }
+/**
+ * Test class for LoopingListIterator focusing on concurrent modification scenarios.
+ */
+public class LoopingListIteratorConcurrentModificationTest {
+
+    @Test
+    public void iteratorShouldThrowConcurrentModificationExceptionWhenBackingListOfSubListIsModified() {
+        // Arrange: Create an iterator from a sublist view of a backing list.
+        final LinkedList<Integer> backingList = new LinkedList<>();
+        final List<Integer> subListView = backingList.subList(0, 0);
+        final LoopingListIterator<Integer> iterator = new LoopingListIterator<>(subListView);
+
+        // Act: Modify the original backing list. This action is expected to invalidate
+        // any iterators created from the sublist view.
+        backingList.add(42);
+
+        // Assert: Any subsequent operation on the iterator should fail fast by throwing
+        // a ConcurrentModificationException.
+        assertThrows(ConcurrentModificationException.class, iterator::previousIndex);
     }
 }
