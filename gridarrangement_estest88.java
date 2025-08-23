fba42535f2449243b7afddc6f018e3d6bd9da2c1
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:52:40 +0800

    Original processedAutoTestCases gridarrangement_estest88.java

---
+++ gridarrangement_estest88.java
@@ -0,0 +1,33 @@
+package org.jfree.chart.block;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.awt.Graphics2D;
+import java.awt.SystemColor;
+import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.util.MockGregorianCalendar;
+import org.jfree.chart.api.HorizontalAlignment;
+import org.jfree.chart.api.RectangleAnchor;
+import org.jfree.chart.api.VerticalAlignment;
+import org.jfree.chart.text.TextBlockAnchor;
+import org.jfree.data.Range;
+import org.jfree.data.time.TimePeriodAnchor;
+import org.jfree.data.time.TimeSeries;
+import org.junit.runner.RunWith;
+
+public class GridArrangement_ESTestTest88 extends GridArrangement_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test87() throws Throwable {
+        GridArrangement gridArrangement0 = new GridArrangement(105, 105);
+        Range range0 = new Range(0.0, 0.0);
+        RectangleConstraint rectangleConstraint0 = new RectangleConstraint(range0, range0);
+        BlockContainer blockContainer0 = new BlockContainer();
+        Size2D size2D0 = gridArrangement0.arrangeRF(blockContainer0, (Graphics2D) null, rectangleConstraint0);
+        assertEquals("Size2D[width=0.0, height=0.0]", size2D0.toString());
+    }
+}
