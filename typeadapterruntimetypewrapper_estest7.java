d7ee3e5ab70521c058b58051efb0a9720ebf34ce
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:23:58 +0800

    gemini processedAutoTestCases typeadapterruntimetypewrapper_estest7.java

--- typeadapterruntimetypewrapper_estest7.java
+++ typeadapterruntimetypewrapper_estest7.java
@@ -1,52 +1,38 @@
 package com.google.gson.internal.bind;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
 import com.google.gson.Gson;
-import com.google.gson.JsonDeserializer;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonSerializer;
-import com.google.gson.ToNumberPolicy;
 import com.google.gson.TypeAdapter;
-import com.google.gson.TypeAdapterFactory;
-import com.google.gson.internal.Excluder;
-import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
-import com.google.gson.stream.JsonWriter;
-import java.io.BufferedWriter;
-import java.io.CharArrayWriter;
-import java.io.EOFException;
-import java.io.IOException;
-import java.io.PipedReader;
-import java.io.PipedWriter;
-import java.io.StringReader;
-import java.io.StringWriter;
-import java.time.temporal.ChronoField;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.evosuite.runtime.mock.java.io.MockPrintWriter;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class TypeAdapterRuntimeTypeWrapper_ESTestTest7 extends TypeAdapterRuntimeTypeWrapper_ESTest_scaffolding {
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.fail;
 
-    @Test(timeout = 4000)
-    public void test06() throws Throwable {
-        Gson gson0 = new Gson();
-        Class<Object> class0 = Object.class;
-        TypeAdapter<Object> typeAdapter0 = gson0.getAdapter(class0);
-        TypeAdapterRuntimeTypeWrapper<Object> typeAdapterRuntimeTypeWrapper0 = new TypeAdapterRuntimeTypeWrapper<Object>(gson0, typeAdapter0, class0);
-        // Undeclared exception!
+/**
+ * Tests for {@link TypeAdapterRuntimeTypeWrapper}.
+ */
+public class TypeAdapterRuntimeTypeWrapperTest {
+
+    @Test
+    public void read_whenReaderIsNull_throwsNullPointerException() {
+        // Arrange
+        Gson gson = new Gson();
+        Class<Object> objectType = Object.class;
+
+        // Get the delegate adapter that the wrapper will use. For Object.class, this is an ObjectTypeAdapter.
+        TypeAdapter<Object> delegateAdapter = gson.getAdapter(objectType);
+        TypeAdapter<Object> wrapperAdapter = new TypeAdapterRuntimeTypeWrapper<>(gson, delegateAdapter, objectType);
+
+        // Act & Assert
         try {
-            typeAdapterRuntimeTypeWrapper0.read((JsonReader) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("com.google.gson.internal.bind.ObjectTypeAdapter", e);
+            // The read method is expected to delegate to the underlying adapter,
+            // which will throw a NullPointerException if the reader is null.
+            wrapperAdapter.read(null);
+            fail("A NullPointerException should have been thrown.");
+        } catch (NullPointerException expected) {
+            // The test passes if this exception is caught.
+            // We can also assert on the message if it's part of the contract.
+            assertNull("The NPE from the underlying adapter has no message.", expected.getMessage());
         }
     }
 }
