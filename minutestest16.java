fbefd114354fc0285145a7bf9bd787156ce27ccf
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:31:09 +0800

    gemini processedManualTestCases minutestest16.java

--- minutestest16.java
+++ minutestest16.java
@@ -1,36 +1,29 @@
 package org.joda.time;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
-public class MinutesTestTest16 extends TestCase {
+/**
+ * Contains tests for the {@link Minutes} class.
+ */
+public class MinutesTest {
 
-    // (before the late 90's they were all over the place)
-    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
+    /**
+     * Tests that a {@link Minutes} value representing a whole number of standard days
+     * is correctly converted to a {@link Days} object.
+     */
+    @Test
+    public void toStandardDays_convertsMinutesEquivalentToTwoDays() {
+        // Arrange: Create a Minutes instance equivalent to 2 standard days.
+        // A standard day has a fixed number of minutes (24 hours * 60 minutes).
+        final int twoDaysInMinutes = DateTimeConstants.MINUTES_PER_DAY * 2;
+        Minutes periodInMinutes = Minutes.minutes(twoDaysInMinutes);
+        Days expectedPeriodInDays = Days.days(2);
 
-    public static void main(String[] args) {
-        junit.textui.TestRunner.run(suite());
-    }
+        // Act: Convert the Minutes object to Days.
+        Days actualPeriodInDays = periodInMinutes.toStandardDays();
 
-    public static TestSuite suite() {
-        return new TestSuite(TestMinutes.class);
-    }
-
-    @Override
-    protected void setUp() throws Exception {
-    }
-
-    @Override
-    protected void tearDown() throws Exception {
-    }
-
-    public void testToStandardDays() {
-        Minutes test = Minutes.minutes(60 * 24 * 2);
-        Days expected = Days.days(2);
-        assertEquals(expected, test.toStandardDays());
+        // Assert: The result should be exactly 2 days.
+        assertEquals(expectedPeriodInDays, actualPeriodInDays);
     }
 }
