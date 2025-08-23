10cc71eca79bdb066966c64d257e7ed8e0c40475
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:24:26 +0800

    gemini processedAutoTestCases uncheck_estest13.java

--- uncheck_estest13.java
+++ uncheck_estest13.java
@@ -1,31 +1,27 @@
 package org.apache.commons.io.function;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.time.chrono.HijrahEra;
-import java.util.Comparator;
-import java.util.concurrent.ForkJoinTask;
-import java.util.function.BiConsumer;
-import java.util.function.BinaryOperator;
-import java.util.function.Function;
-import java.util.function.Supplier;
-import java.util.stream.Collector;
-import java.util.stream.LongStream;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class Uncheck_ESTestTest13 extends Uncheck_ESTest_scaffolding {
+/**
+ * Tests for the {@link Uncheck} utility class.
+ */
+public class UncheckTest {
 
-    @Test(timeout = 4000)
-    public void test12() throws Throwable {
-        IOIntSupplier iOIntSupplier0 = mock(IOIntSupplier.class, new ViolatedAssumptionAnswer());
-        doReturn(1165).when(iOIntSupplier0).getAsInt();
-        int int0 = Uncheck.getAsInt(iOIntSupplier0);
-        assertEquals(1165, int0);
+    /**
+     * Tests that Uncheck.getAsInt() successfully returns the integer value
+     * from a given IOIntSupplier when no exception is thrown.
+     */
+    @Test
+    public void testGetAsIntReturnsValueFromSupplier() {
+        // Arrange
+        final int expectedValue = 1165;
+        final IOIntSupplier supplier = () -> expectedValue;
+
+        // Act
+        final int actualValue = Uncheck.getAsInt(supplier);
+
+        // Assert
+        assertEquals(expectedValue, actualValue);
     }
 }
