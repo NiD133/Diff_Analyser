54c3347077bc61a70f68e9dbca25dbd27181a4ba
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:01:24 +0800

    Original processedAutoTestCases locationtextextractionstrategy_estest42.java

---
+++ locationtextextractionstrategy_estest42.java
@@ -0,0 +1,40 @@
+package com.itextpdf.text.pdf.parser;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.itextpdf.text.pdf.CMapAwareDocumentFont;
+import com.itextpdf.text.pdf.PdfDate;
+import com.itextpdf.text.pdf.PdfDictionary;
+import com.itextpdf.text.pdf.PdfIndirectReference;
+import com.itextpdf.text.pdf.PdfOCProperties;
+import com.itextpdf.text.pdf.PdfSigLockDictionary;
+import com.itextpdf.text.pdf.PdfString;
+import java.nio.charset.IllegalCharsetNameException;
+import java.util.Collection;
+import java.util.LinkedList;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class LocationTextExtractionStrategy_ESTestTest42 extends LocationTextExtractionStrategy_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test41() throws Throwable {
+        PdfDate pdfDate0 = new PdfDate();
+        GraphicsState graphicsState0 = new GraphicsState();
+        Matrix matrix0 = new Matrix(8, 2);
+        PdfOCProperties pdfOCProperties0 = new PdfOCProperties();
+        CMapAwareDocumentFont cMapAwareDocumentFont0 = new CMapAwareDocumentFont(pdfOCProperties0);
+        LinkedList<MarkedContentInfo> linkedList0 = new LinkedList<MarkedContentInfo>();
+        graphicsState0.font = cMapAwareDocumentFont0;
+        TextRenderInfo textRenderInfo0 = new TextRenderInfo(pdfDate0, graphicsState0, matrix0, linkedList0);
+        LocationTextExtractionStrategy locationTextExtractionStrategy0 = new LocationTextExtractionStrategy();
+        locationTextExtractionStrategy0.renderText(textRenderInfo0);
+        LocationTextExtractionStrategy.DUMP_STATE = true;
+        String string0 = locationTextExtractionStrategy0.getResultantText();
+        assertEquals("", string0);
+    }
+}
