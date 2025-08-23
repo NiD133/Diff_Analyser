5b4e9f9a64f455434f9f3f0c401fc6444eaf552d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:07:22 +0800

    gemini processedAutoTestCases parameter_estest18.java

--- parameter_estest18.java
+++ parameter_estest18.java
@@ -1,25 +1,40 @@
 package com.google.common.reflect;
 
+import com.google.common.reflect.Invokable;
+import com.google.common.reflect.Parameter;
+import com.google.common.reflect.TypeToken;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
+import javax.annotation.Nullable; // A standard, simple annotation for the test case
 import java.lang.annotation.Annotation;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertFalse;
+import static org.mockito.Mockito.mock;
 
 public class Parameter_ESTestTest18 extends Parameter_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test17() throws Throwable {
-        Invokable<Object, Annotation> invokable0 = (Invokable<Object, Annotation>) mock(Invokable.class, new ViolatedAssumptionAnswer());
-        Class<Annotation> class0 = Annotation.class;
-        TypeToken<Annotation> typeToken0 = TypeToken.of(class0);
-        Annotation[] annotationArray0 = new Annotation[0];
-        Parameter parameter0 = new Parameter(invokable0, 0, typeToken0, annotationArray0, invokable0);
-        boolean boolean0 = parameter0.isAnnotationPresent(class0);
-        assertFalse(boolean0);
+    /**
+     * This test was refactored to improve its clarity and maintainability.
+     * Original name: test17
+     */
+    @Test
+    public void isAnnotationPresent_whenParameterHasNoAnnotations_returnsFalse() {
+        // Arrange: Create a Parameter instance that has an empty array of annotations.
+        Invokable<?, ?> mockDeclaringInvokable = mock(Invokable.class);
+        TypeToken<?> parameterType = TypeToken.of(String.class);
+        Annotation[] noAnnotations = new Annotation[0];
+
+        // The constructor is package-private. The last argument is 'annotatedType'.
+        Parameter parameter = new Parameter(
+                mockDeclaringInvokable,
+                0, // position
+                parameterType,
+                noAnnotations,
+                null // annotatedType
+        );
+
+        // Act: Check for the presence of an arbitrary annotation.
+        boolean isPresent = parameter.isAnnotationPresent(Nullable.class);
+
+        // Assert: The method should return false because no annotations were provided.
+        assertFalse("Expected isAnnotationPresent to return false for a parameter with no annotations.", isPresent);
     }
 }
