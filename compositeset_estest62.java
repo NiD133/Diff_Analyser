1691aeee033d00daca37d9753b961e605d697956
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:45:22 +0800

    gemini processedAutoTestCases compositeset_estest62.java

--- compositeset_estest62.java
+++ compositeset_estest62.java
@@ -1,57 +1,31 @@
 package org.apache.commons.collections4.set;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.LinkedHashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Set;
-import java.util.Spliterator;
-import java.util.function.Consumer;
-import java.util.stream.Stream;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.Equator;
-import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ChainedClosure;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.DefaultEquator;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionPredicate;
-import org.apache.commons.collections4.functors.FalsePredicate;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.IfClosure;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NotNullPredicate;
-import org.apache.commons.collections4.functors.NotPredicate;
-import org.apache.commons.collections4.functors.NullIsExceptionPredicate;
-import org.apache.commons.collections4.functors.NullIsTruePredicate;
-import org.apache.commons.collections4.functors.OnePredicate;
-import org.apache.commons.collections4.functors.OrPredicate;
-import org.apache.commons.collections4.functors.TransformerClosure;
-import org.apache.commons.collections4.functors.TruePredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.apache.commons.collections4.functors.WhileClosure;
-import org.apache.commons.collections4.iterators.IteratorChain;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import java.util.HashSet;
+import static org.junit.Assert.assertEquals;
 
-public class CompositeSet_ESTestTest62 extends CompositeSet_ESTest_scaffolding {
+/**
+ * This class contains test cases for the {@link CompositeSet#equals(Object)} method.
+ */
+public class CompositeSetEqualityTest {
 
-    @Test(timeout = 4000)
-    public void test61() throws Throwable {
-        Set<Integer>[] setArray0 = (Set<Integer>[]) Array.newInstance(Set.class, 6);
-        CompositeSet<Integer> compositeSet0 = new CompositeSet<Integer>(setArray0);
-        CompositeSet<Object> compositeSet1 = new CompositeSet<Object>();
-        boolean boolean0 = compositeSet0.equals(compositeSet1);
-        assertTrue(boolean0);
+    @Test
+    public void equals_shouldReturnTrue_whenComparingTwoEmptyCompositeSets() {
+        // Arrange
+        // Create an empty CompositeSet using the default constructor.
+        final CompositeSet<String> set1 = new CompositeSet<>();
+
+        // Create another empty CompositeSet by composing two empty component sets.
+        // This set should also be considered empty and thus equal to the first one.
+        final CompositeSet<String> set2 = new CompositeSet<>(new HashSet<>(), new HashSet<>());
+
+        // Act & Assert
+        // According to the Set contract, two sets are equal if they have the same size
+        // and contain the same elements. Both sets are empty, so they should be equal.
+        assertEquals(set1, set2);
+
+        // For completeness, verify that the hashCode contract is met and equality is symmetric.
+        assertEquals("Equal objects must have equal hash codes", set1.hashCode(), set2.hashCode());
+        assertEquals("Equality must be symmetric", set2, set1);
     }
 }
