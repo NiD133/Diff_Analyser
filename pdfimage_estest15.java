c2dd4b7d7a14266520c279d6d62662ae8927b6e8
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:08:11 +0800

    Original processedAutoTestCases pdfimage_estest15.java

---
+++ pdfimage_estest15.java
@@ -0,0 +1,39 @@
+package com.itextpdf.text.pdf;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.ImgJBIG2;
+import com.itextpdf.text.ImgTemplate;
+import com.itextpdf.text.Rectangle;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.io.PrintStream;
+import java.util.Locale;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockPrintStream;
+import org.junit.runner.RunWith;
+
+public class PdfImage_ESTestTest15 extends PdfImage_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test14() throws Throwable {
+        PdfIndirectReference pdfIndirectReference0 = new PdfIndirectReference(508, 508);
+        PdfImage pdfImage0 = null;
+        try {
+            pdfImage0 = new PdfImage((Image) null, "", pdfIndirectReference0);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("com.itextpdf.text.pdf.PdfImage", e);
+        }
+    }
+}
