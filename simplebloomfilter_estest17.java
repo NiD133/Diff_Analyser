298271b4a3ebd770f71873d948aecb1823f8fea8
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:16:04 +0800

    Original processedAutoTestCases simplebloomfilter_estest17.java

---
+++ simplebloomfilter_estest17.java
@@ -0,0 +1,28 @@
+package org.apache.commons.collections4.bloomfilter;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.function.IntPredicate;
+import java.util.function.LongPredicate;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class SimpleBloomFilter_ESTestTest17 extends SimpleBloomFilter_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test16() throws Throwable {
+        Shape shape0 = Shape.fromNM(2147483639, 2147483639);
+        SimpleBloomFilter simpleBloomFilter0 = new SimpleBloomFilter(shape0);
+        long[] longArray0 = new long[0];
+        LongBiPredicate longBiPredicate0 = mock(LongBiPredicate.class, new ViolatedAssumptionAnswer());
+        doReturn(false, false, false, false, false).when(longBiPredicate0).test(anyLong(), anyLong());
+        CountingLongPredicate countingLongPredicate0 = new CountingLongPredicate(longArray0, longBiPredicate0);
+        LongPredicate longPredicate0 = countingLongPredicate0.negate();
+        // Undeclared exception!
+        simpleBloomFilter0.processBitMaps(longPredicate0);
+    }
+}
