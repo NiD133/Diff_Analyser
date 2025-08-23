6721a3651d1a741c7db24d1e8bb72396486f6923
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:47:44 +0800

    gemini processedAutoTestCases defaultpiedataset_estest32.java

--- defaultpiedataset_estest32.java
+++ defaultpiedataset_estest32.java
@@ -1,32 +1,27 @@
 package org.jfree.data.general;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.time.chrono.ChronoLocalDate;
-import java.util.List;
-import java.util.Vector;
-import javax.swing.JLayeredPane;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.jfree.chart.api.SortOrder;
-import org.jfree.chart.api.TableOrder;
-import org.jfree.data.DefaultKeyedValues;
-import org.jfree.data.KeyedValues;
-import org.jfree.data.category.CategoryDataset;
-import org.jfree.data.category.CategoryToPieDataset;
-import org.jfree.data.category.DefaultIntervalCategoryDataset;
-import org.jfree.data.category.SlidingCategoryDataset;
-import org.jfree.data.statistics.DefaultBoxAndWhiskerCategoryDataset;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertFalse;
 
-public class DefaultPieDataset_ESTestTest32 extends DefaultPieDataset_ESTest_scaffolding {
+/**
+ * Provides tests for the {@code equals()} method in the {@link DefaultPieDataset} class.
+ */
+public class DefaultPieDatasetTest {
 
-    @Test(timeout = 4000)
-    public void test31() throws Throwable {
-        DefaultPieDataset<Integer> defaultPieDataset0 = new DefaultPieDataset<Integer>();
-        Integer integer0 = JLayeredPane.DRAG_LAYER;
-        boolean boolean0 = defaultPieDataset0.equals(integer0);
-        assertFalse(boolean0);
+    /**
+     * Verifies that the equals() method returns false when a DefaultPieDataset
+     * is compared with an object of an incompatible type.
+     */
+    @Test
+    public void equals_shouldReturnFalse_whenComparedWithIncompatibleType() {
+        // Arrange: Create an empty pie dataset and an object of a different type.
+        DefaultPieDataset<String> pieDataset = new DefaultPieDataset<>();
+        Object incompatibleObject = "a simple string";
+
+        // Act: Compare the dataset with the incompatible object.
+        boolean result = pieDataset.equals(incompatibleObject);
+
+        // Assert: The result should be false, as the types are different.
+        assertFalse("A DefaultPieDataset should not be equal to a String.", result);
     }
 }
