4bbca8f952898f55c7d507664599124ba83f6005
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:29:22 +0800

    gemini processedManualTestCases illegalfieldvalueexceptiontest4.java

--- illegalfieldvalueexceptiontest4.java
+++ illegalfieldvalueexceptiontest4.java
@@ -1,37 +1,46 @@
 package org.joda.time;
 
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-import org.joda.time.chrono.GJChronology;
-import org.joda.time.chrono.ISOChronology;
-import org.joda.time.chrono.JulianChronology;
-import org.joda.time.field.FieldUtils;
-import org.joda.time.field.SkipDateTimeField;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-public class IllegalFieldValueExceptionTestTest4 extends TestCase {
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-    public static void main(String[] args) {
-        junit.textui.TestRunner.run(suite());
-    }
+/**
+ * Tests for {@link IllegalFieldValueException}.
+ */
+class IllegalFieldValueExceptionTest {
 
-    public static TestSuite suite() {
-        return new TestSuite(TestIllegalFieldValueException.class);
-    }
+    @Test
+    @DisplayName("Setting an hour that falls into a DST gap should throw IllegalFieldValueException")
+    void setHourOfDay_toNonExistentHourInDstGap_throwsException() {
+        // Arrange: Define a date and time just before a Daylight Saving Time (DST) transition.
+        // In the "America/Los_Angeles" timezone, on April 3, 2005, the clocks jumped
+        // forward from 2:00 AM to 3:00 AM. The 2:xx hour did not exist on this day.
+        final DateTimeZone losAngelesZone = DateTimeZone.forID("America/Los_Angeles");
+        final DateTime dateTimeBeforeDstGap = new DateTime(2005, 4, 3, 1, 0, losAngelesZone);
+        final int nonExistentHour = 2;
 
-    public void testZoneTransition() {
-        DateTime dt = new DateTime(2005, 4, 3, 1, 0, 0, 0, DateTimeZone.forID("America/Los_Angeles"));
-        try {
-            dt.hourOfDay().setCopy(2);
-            fail();
-        } catch (IllegalFieldValueException e) {
-            assertEquals(DateTimeFieldType.hourOfDay(), e.getDateTimeFieldType());
-            assertEquals(null, e.getDurationFieldType());
-            assertEquals("hourOfDay", e.getFieldName());
-            assertEquals(new Integer(2), e.getIllegalNumberValue());
-            assertEquals(null, e.getIllegalStringValue());
-            assertEquals("2", e.getIllegalValueAsString());
-            assertEquals(null, e.getLowerBound());
-            assertEquals(null, e.getUpperBound());
-        }
+        // Act & Assert: Attempting to set the hour to a non-existent value should throw an exception.
+        // We use assertThrows to verify that the correct exception is thrown and to capture it.
+        IllegalFieldValueException thrownException = assertThrows(
+            IllegalFieldValueException.class,
+            () -> dateTimeBeforeDstGap.hourOfDay().setCopy(nonExistentHour)
+        );
+
+        // Assert: Verify the properties of the thrown exception are correct.
+        // Using assertAll to group related assertions and report all failures at once.
+        assertAll("Exception properties",
+            () -> assertEquals(DateTimeFieldType.hourOfDay(), thrownException.getDateTimeFieldType(), "DateTimeFieldType should be hourOfDay"),
+            () -> assertNull(thrownException.getDurationFieldType(), "DurationFieldType should be null"),
+            () -> assertEquals("hourOfDay", thrownException.getFieldName(), "Field name should be 'hourOfDay'"),
+            () -> assertEquals(nonExistentHour, thrownException.getIllegalNumberValue(), "IllegalNumberValue should be the attempted hour"),
+            () -> assertNull(thrownException.getIllegalStringValue(), "IllegalStringValue should be null as a number was provided"),
+            () -> assertEquals(String.valueOf(nonExistentHour), thrownException.getIllegalValueAsString(), "IllegalValueAsString should be the string representation of the attempted hour"),
+            () -> assertNull(thrownException.getLowerBound(), "LowerBound should be null for a DST gap scenario"),
+            () -> assertNull(thrownException.getUpperBound(), "UpperBound should be null for a DST gap scenario")
+        );
     }
 }
