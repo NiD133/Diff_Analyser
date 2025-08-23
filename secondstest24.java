aef4263b7b1016fbe7aeeaafa4ff21f26e0211d9
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:33:15 +0800

    Original processedManualTestCases secondstest24.java

---
+++ secondstest24.java
@@ -0,0 +1,45 @@
+package org.joda.time;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class SecondsTestTest24 extends TestCase {
+
+    // (before the late 90's they were all over the place)
+    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
+
+    public static TestSuite suite() {
+        return new TestSuite(TestSeconds.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+    }
+
+    public void testMultipliedBy_int() {
+        Seconds test = Seconds.seconds(2);
+        assertEquals(6, test.multipliedBy(3).getSeconds());
+        assertEquals(2, test.getSeconds());
+        assertEquals(-6, test.multipliedBy(-3).getSeconds());
+        assertSame(test, test.multipliedBy(1));
+        Seconds halfMax = Seconds.seconds(Integer.MAX_VALUE / 2 + 1);
+        try {
+            halfMax.multipliedBy(2);
+            fail();
+        } catch (ArithmeticException ex) {
+            // expected
+        }
+    }
+}
