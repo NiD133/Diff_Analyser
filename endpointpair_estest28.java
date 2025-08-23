bf77d4ca79232a24e73cb674183dab9d40172ce4
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:49:15 +0800

    gemini processedAutoTestCases endpointpair_estest28.java

--- endpointpair_estest28.java
+++ endpointpair_estest28.java
@@ -1,31 +1,37 @@
 package com.google.common.graph;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import com.google.common.collect.UnmodifiableIterator;
-import java.util.Locale;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class EndpointPair_ESTestTest28 extends EndpointPair_ESTest_scaffolding {
+/**
+ * Tests for the interaction between EndpointPair and the Network class.
+ */
+public class EndpointPairInteractionTest {
 
-    @Test(timeout = 4000)
-    public void test27() throws Throwable {
-        Locale.Category locale_Category0 = Locale.Category.FORMAT;
-        EndpointPair<Object> endpointPair0 = EndpointPair.ordered((Object) locale_Category0, (Object) locale_Category0);
-        NetworkBuilder<Object, Object> networkBuilder0 = NetworkBuilder.undirected();
-        StandardMutableNetwork<Object, Object> standardMutableNetwork0 = new StandardMutableNetwork<Object, Object>(networkBuilder0);
-        // Undeclared exception!
+    /**
+     * Verifies that querying for the incident nodes of an edge that is not in the network
+     * throws an IllegalArgumentException.
+     */
+    @Test
+    public void incidentNodes_whenEdgeNotInNetwork_throwsIllegalArgumentException() {
+        // Arrange: Create a network and an EndpointPair representing an edge.
+        // In this setup, the EndpointPair itself is used as the edge object.
+        MutableNetwork<String, EndpointPair<String>> network = NetworkBuilder.undirected().build();
+
+        // This EndpointPair represents an edge that has NOT been added to the network.
+        EndpointPair<String> nonExistentEdge = EndpointPair.ordered("A", "B");
+
+        // Act & Assert: Verify that the expected exception is thrown.
         try {
-            standardMutableNetwork0.incidentNodes(endpointPair0);
-            fail("Expecting exception: IllegalArgumentException");
-        } catch (IllegalArgumentException e) {
-            //
-            // Edge <FORMAT -> FORMAT> is not an element of this graph.
-            //
-            verifyException("com.google.common.graph.StandardNetwork", e);
+            network.incidentNodes(nonExistentEdge);
+            fail("Expected an IllegalArgumentException for an edge not present in the network.");
+        } catch (IllegalArgumentException expected) {
+            // The exception message should clearly state that the edge is not in the graph.
+            // The message uses the toString() representation of the EndpointPair.
+            String expectedMessage = "Edge <A -> B> is not an element of this graph.";
+            assertEquals(expectedMessage, expected.getMessage());
         }
     }
 }
