c9d21d60bdcaab12d9917db0c889d61086290098
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:29:27 +0800

    Original processedAutoTestCases xxhash32_estest8.java

---
+++ xxhash32_estest8.java
@@ -0,0 +1,17 @@
+package org.apache.commons.codec.digest;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class XXHash32_ESTestTest8 extends XXHash32_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test07() throws Throwable {
+        XXHash32 xXHash32_0 = new XXHash32(97);
+        assertEquals(3659767818L, xXHash32_0.getValue());
+    }
+}
