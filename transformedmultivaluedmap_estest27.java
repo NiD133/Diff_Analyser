a1c95f3cc19d888ddc053ccbc9ecc45a9e519a8d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:23:18 +0800

    gemini processedAutoTestCases transformedmultivaluedmap_estest27.java

--- transformedmultivaluedmap_estest27.java
+++ transformedmultivaluedmap_estest27.java
@@ -1,65 +1,41 @@
 package org.apache.commons.collections4.multimap;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.AbstractMap;
-import java.util.ArrayDeque;
-import java.util.Comparator;
-import java.util.ConcurrentModificationException;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
-import java.util.PriorityQueue;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.Factory;
 import org.apache.commons.collections4.MultiValuedMap;
-import org.apache.commons.collections4.Predicate;
 import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AllPredicate;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ChainedTransformer;
-import org.apache.commons.collections4.functors.CloneTransformer;
-import org.apache.commons.collections4.functors.ClosureTransformer;
-import org.apache.commons.collections4.functors.ConstantFactory;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.ExceptionFactory;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.FactoryTransformer;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.MapTransformer;
 import org.apache.commons.collections4.functors.NOPTransformer;
-import org.apache.commons.collections4.functors.NotPredicate;
-import org.apache.commons.collections4.functors.NullIsExceptionPredicate;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.SwitchTransformer;
-import org.apache.commons.collections4.functors.TransformerClosure;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class TransformedMultiValuedMap_ESTestTest27 extends TransformedMultiValuedMap_ESTest_scaffolding {
+import static org.junit.Assert.assertThrows;
 
-    @Test(timeout = 4000)
-    public void test26() throws Throwable {
-        ArrayListValuedLinkedHashMap<Integer, Integer> arrayListValuedLinkedHashMap0 = new ArrayListValuedLinkedHashMap<Integer, Integer>();
-        UnmodifiableMultiValuedMap<Integer, Object> unmodifiableMultiValuedMap0 = UnmodifiableMultiValuedMap.unmodifiableMultiValuedMap((MultiValuedMap<? extends Integer, ?>) arrayListValuedLinkedHashMap0);
-        Integer integer0 = new Integer(0);
-        Transformer<Object, Integer> transformer0 = ConstantTransformer.constantTransformer(integer0);
-        TransformedMultiValuedMap<Integer, Object> transformedMultiValuedMap0 = new TransformedMultiValuedMap<Integer, Object>(unmodifiableMultiValuedMap0, (Transformer<? super Integer, ? extends Integer>) null, transformer0);
-        // Undeclared exception!
-        try {
-            transformedMultiValuedMap0.put(integer0, integer0);
-            fail("Expecting exception: UnsupportedOperationException");
-        } catch (UnsupportedOperationException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.commons.collections4.multimap.UnmodifiableMultiValuedMap", e);
-        }
+/**
+ * Contains tests for {@link TransformedMultiValuedMap}.
+ */
+public class TransformedMultiValuedMapTest {
+
+    /**
+     * Tests that calling put() on a TransformedMultiValuedMap throws
+     * an UnsupportedOperationException if the map is decorating an unmodifiable map.
+     * The put() operation should be delegated to the decorated map, which then
+     * throws the exception.
+     */
+    @Test
+    public void putOnUnmodifiableDecoratedMapShouldThrowException() {
+        // Arrange
+        // Create a base map and wrap it to make it unmodifiable.
+        final MultiValuedMap<String, String> baseMap = new ArrayListValuedLinkedHashMap<>();
+        final MultiValuedMap<String, String> unmodifiableMap =
+                UnmodifiableMultiValuedMap.unmodifiableMultiValuedMap(baseMap);
+
+        // Use a no-op transformer, as the transformation logic is not the focus of this test.
+        final Transformer<String, String> keyTransformer = NOPTransformer.nopTransformer();
+        final Transformer<String, String> valueTransformer = NOPTransformer.nopTransformer();
+
+        // Create the TransformedMultiValuedMap decorating the unmodifiable map.
+        final MultiValuedMap<String, String> transformedMap =
+                TransformedMultiValuedMap.transformingMap(unmodifiableMap, keyTransformer, valueTransformer);
+
+        // Act & Assert
+        // An attempt to add an element should fail because the underlying map is unmodifiable.
+        assertThrows(UnsupportedOperationException.class, () -> transformedMap.put("key", "value"));
     }
 }
