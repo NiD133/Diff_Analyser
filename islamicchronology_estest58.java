c9f6b11ad4c943cd7d765113ed33e6887fd47373
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:55:22 +0800

    gemini processedAutoTestCases islamicchronology_estest58.java

--- islamicchronology_estest58.java
+++ islamicchronology_estest58.java
@@ -1,23 +1,35 @@
 package org.joda.time.chrono;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.TimeZone;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
 import org.joda.time.Chronology;
 import org.joda.time.DateTimeZone;
-import org.joda.time.tz.UTCProvider;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class IslamicChronology_ESTestTest58 extends IslamicChronology_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertNotNull;
 
-    @Test(timeout = 4000)
-    public void test57() throws Throwable {
-        IslamicChronology islamicChronology0 = IslamicChronology.getInstanceUTC();
-        DateTimeZone dateTimeZone0 = DateTimeZone.forOffsetHoursMinutes(1, 1);
-        Chronology chronology0 = islamicChronology0.withZone(dateTimeZone0);
-        assertNotSame(chronology0, islamicChronology0);
+/**
+ * Unit tests for {@link IslamicChronology}.
+ */
+public class IslamicChronologyTest {
+
+    @Test
+    public void withZone_givenDifferentZone_returnsNewInstance() {
+        // Arrange
+        // Get the singleton instance of IslamicChronology in UTC.
+        final IslamicChronology utcChronology = IslamicChronology.getInstanceUTC();
+        final DateTimeZone differentZone = DateTimeZone.forOffsetHoursMinutes(1, 1);
+
+        // Act
+        // Request a new Chronology instance with a different time zone.
+        final Chronology zonedChronology = utcChronology.withZone(differentZone);
+
+        // Assert
+        // The returned object should not be the same instance as the original UTC one.
+        assertNotNull(zonedChronology);
+        assertNotSame("A new Chronology instance should be returned for a different zone.", utcChronology, zonedChronology);
+        
+        // Also, verify that the new instance has the correct time zone.
+        assertEquals("The new instance should have the specified time zone.", differentZone, zonedChronology.getZone());
     }
 }
