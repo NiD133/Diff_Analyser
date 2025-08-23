89465293508548c121bc5cd351bc1cec85513356
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:16:18 +0800

    gemini processedAutoTestCases simplebloomfilter_estest48.java

--- simplebloomfilter_estest48.java
+++ simplebloomfilter_estest48.java
@@ -1,23 +1,27 @@
 package org.apache.commons.collections4.bloomfilter;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.function.IntPredicate;
-import java.util.function.LongPredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class SimpleBloomFilter_ESTestTest48 extends SimpleBloomFilter_ESTest_scaffolding {
+/**
+ * Test suite for the {@link SimpleBloomFilter}.
+ */
+public class SimpleBloomFilterTest {
 
-    @Test(timeout = 4000)
-    public void test47() throws Throwable {
-        Shape shape0 = Shape.fromKM(23, 23);
-        SimpleBloomFilter simpleBloomFilter0 = new SimpleBloomFilter(shape0);
-        simpleBloomFilter0.clear();
-        assertEquals(0, simpleBloomFilter0.characteristics());
+    /**
+     * Tests that calling clear() on a newly created (and thus empty)
+     * filter results in a filter with zero characteristics.
+     */
+    @Test
+    public void testClearOnNewEmptyFilterShouldResultInZeroCharacteristics() {
+        // Arrange: Create a new, empty Bloom filter.
+        final Shape shape = Shape.fromKM(23, 23);
+        final SimpleBloomFilter bloomFilter = new SimpleBloomFilter(shape);
+
+        // Act: Clear the filter.
+        bloomFilter.clear();
+
+        // Assert: The number of set bits (characteristics) should be zero.
+        assertEquals("A cleared empty filter should have zero characteristics", 0, bloomFilter.characteristics());
     }
 }
