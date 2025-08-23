f5d28803f598c6b95e797d05b81cf039b70385b1
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:14:32 +0800

    Original processedAutoTestCases sequencereader_estest5.java

---
+++ sequencereader_estest5.java
@@ -0,0 +1,40 @@
+package org.apache.commons.io.input;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.IOException;
+import java.io.PipedReader;
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.ConcurrentModificationException;
+import java.util.List;
+import java.util.Vector;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class SequenceReader_ESTestTest5 extends SequenceReader_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test04() throws Throwable {
+        ArrayDeque<StringReader> arrayDeque0 = new ArrayDeque<StringReader>();
+        StringReader stringReader0 = new StringReader("");
+        arrayDeque0.add(stringReader0);
+        arrayDeque0.add(stringReader0);
+        SequenceReader sequenceReader0 = new SequenceReader(arrayDeque0);
+        arrayDeque0.add(stringReader0);
+        // Undeclared exception!
+        try {
+            sequenceReader0.read();
+            fail("Expecting exception: ConcurrentModificationException");
+        } catch (ConcurrentModificationException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("java.util.ArrayDeque$DeqIterator", e);
+        }
+    }
+}
