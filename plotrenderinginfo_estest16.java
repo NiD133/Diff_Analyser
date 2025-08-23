ffe36978395f7ae60d83f2c279b3a51ffc702f3c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:08:57 +0800

    Original processedAutoTestCases plotrenderinginfo_estest16.java

---
+++ plotrenderinginfo_estest16.java
@@ -0,0 +1,28 @@
+package org.jfree.chart.plot;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.jfree.chart.ChartRenderingInfo;
+import org.jfree.chart.entity.EntityCollection;
+import org.jfree.chart.entity.StandardEntityCollection;
+import org.junit.runner.RunWith;
+
+public class PlotRenderingInfo_ESTestTest16 extends PlotRenderingInfo_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test15() throws Throwable {
+        ChartRenderingInfo chartRenderingInfo0 = new ChartRenderingInfo();
+        PlotRenderingInfo plotRenderingInfo0 = new PlotRenderingInfo(chartRenderingInfo0);
+        int int0 = plotRenderingInfo0.getSubplotCount();
+        assertEquals(0, int0);
+    }
+}
