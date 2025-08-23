4bd87274e99116115b3069f2a18c3ce06e411cff
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:10:21 +0800

    gemini processedAutoTestCases queueinputstream_estest2.java

--- queueinputstream_estest2.java
+++ queueinputstream_estest2.java
@@ -1,25 +1,26 @@
 package org.apache.commons.io.input;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.time.Duration;
-import java.time.temporal.ChronoUnit;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingDeque;
-import java.util.concurrent.PriorityBlockingQueue;
-import org.apache.commons.io.output.QueueOutputStream;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertSame;
 
-public class QueueInputStream_ESTestTest2 extends QueueInputStream_ESTest_scaffolding {
+/**
+ * Tests for the fluent interface of {@link QueueInputStream.Builder}.
+ */
+public class QueueInputStreamBuilderTest {
 
-    @Test(timeout = 4000)
-    public void test01() throws Throwable {
-        QueueInputStream.Builder queueInputStream_Builder0 = new QueueInputStream.Builder();
-        LinkedBlockingDeque<Integer> linkedBlockingDeque0 = new LinkedBlockingDeque<Integer>();
-        QueueInputStream.Builder queueInputStream_Builder1 = queueInputStream_Builder0.setBlockingQueue(linkedBlockingDeque0);
-        assertSame(queueInputStream_Builder0, queueInputStream_Builder1);
+    @Test
+    public void setBlockingQueueShouldReturnSameBuilderInstanceForFluentChaining() {
+        // Arrange
+        final QueueInputStream.Builder builder = new QueueInputStream.Builder();
+        final BlockingQueue<Integer> blockingQueue = new LinkedBlockingDeque<>();
+
+        // Act
+        final QueueInputStream.Builder returnedBuilder = builder.setBlockingQueue(blockingQueue);
+
+        // Assert
+        assertSame("The method should return the same instance to support a fluent API.",
+                builder, returnedBuilder);
     }
 }
