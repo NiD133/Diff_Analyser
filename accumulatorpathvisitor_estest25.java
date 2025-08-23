12aa6c7d71316b5f00d3e8dedd1c2a451b9a72d4
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:37:06 +0800

    gemini processedAutoTestCases accumulatorpathvisitor_estest25.java

--- accumulatorpathvisitor_estest25.java
+++ accumulatorpathvisitor_estest25.java
@@ -1,44 +1,31 @@
 package org.apache.commons.io.file;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.IOException;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Path;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.Comparator;
-import java.util.List;
-import java.util.regex.Pattern;
-import org.apache.commons.io.filefilter.CanExecuteFileFilter;
-import org.apache.commons.io.filefilter.CanReadFileFilter;
-import org.apache.commons.io.filefilter.CanWriteFileFilter;
-import org.apache.commons.io.filefilter.HiddenFileFilter;
-import org.apache.commons.io.filefilter.PrefixFileFilter;
-import org.apache.commons.io.filefilter.RegexFileFilter;
-import org.apache.commons.io.function.IOBiFunction;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.evosuite.runtime.mock.java.io.MockFile;
-import org.evosuite.runtime.mock.java.io.MockIOException;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertThrows;
 
+import java.nio.file.Path;
+import java.util.Comparator;
+import org.junit.Test;
+
+/**
+ * Tests for {@link AccumulatorPathVisitor}.
+ * This class contains a specific test case that was improved for understandability.
+ */
 public class AccumulatorPathVisitor_ESTestTest25 extends AccumulatorPathVisitor_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test24() throws Throwable {
-        AccumulatorPathVisitor accumulatorPathVisitor0 = AccumulatorPathVisitor.withLongCounters();
-        // Undeclared exception!
-        try {
-            accumulatorPathVisitor0.relativizeFiles((Path) null, true, (Comparator<? super Path>) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("java.util.Objects", e);
-        }
+    /**
+     * Tests that relativizeFiles() throws a NullPointerException when the parent path argument is null.
+     */
+    @Test
+    public void relativizeFiles_whenParentPathIsNull_throwsNullPointerException() {
+        // Arrange
+        final AccumulatorPathVisitor visitor = AccumulatorPathVisitor.withLongCounters();
+        final Path nullParentPath = null;
+        final Comparator<Path> nullComparator = null;
+
+        // Act & Assert
+        // The method is expected to fail fast when the essential 'parent' argument is null.
+        assertThrows(NullPointerException.class, () -> {
+            visitor.relativizeFiles(nullParentPath, true, nullComparator);
+        });
     }
 }
