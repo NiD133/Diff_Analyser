6637a082784576a66bb199f39066f30c0d970f4c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:56:12 +0800

    gemini processedAutoTestCases jsonarray_estest52.java

--- jsonarray_estest52.java
+++ jsonarray_estest52.java
@@ -1,22 +1,24 @@
 package com.google.gson;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.util.Iterator;
-import java.util.List;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class JsonArray_ESTestTest52 extends JsonArray_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link JsonArray} class, focusing on the equals method.
+ */
+public class JsonArrayTest {
 
-    @Test(timeout = 4000)
-    public void test51() throws Throwable {
-        JsonArray jsonArray0 = new JsonArray();
-        boolean boolean0 = jsonArray0.equals(jsonArray0);
-        assertTrue(boolean0);
+    /**
+     * Tests the reflexive property of the equals method, ensuring that a
+     * JsonArray instance is always equal to itself.
+     */
+    @Test
+    public void equals_onSameInstance_returnsTrue() {
+        // Arrange
+        JsonArray jsonArray = new JsonArray();
+
+        // Assert
+        // According to the contract of Object.equals(), an object must be equal to itself.
+        assertEquals(jsonArray, jsonArray);
     }
 }
