636c9721f7356fa79badf5487ed1b939df97d424
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:20:46 +0800

    Original processedAutoTestCases texthelpappendable_estest1.java

---
+++ texthelpappendable_estest1.java
@@ -0,0 +1,49 @@
+package org.apache.commons.cli.help;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.IOException;
+import java.io.PipedWriter;
+import java.io.StringWriter;
+import java.nio.BufferOverflowException;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.ReadOnlyBufferException;
+import java.nio.charset.Charset;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.SortedSet;
+import java.util.Stack;
+import java.util.TreeSet;
+import java.util.Vector;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class TextHelpAppendable_ESTestTest1 extends TextHelpAppendable_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test00() throws Throwable {
+        TextHelpAppendable textHelpAppendable0 = TextHelpAppendable.systemOut();
+        CharBuffer charBuffer0 = CharBuffer.allocate(3);
+        ArrayDeque<CharSequence> arrayDeque0 = new ArrayDeque<CharSequence>();
+        arrayDeque0.add(charBuffer0);
+        arrayDeque0.add(charBuffer0);
+        textHelpAppendable0.appendList(true, arrayDeque0);
+        assertEquals(1, textHelpAppendable0.getLeftPad());
+        assertEquals(3, textHelpAppendable0.getIndent());
+        assertEquals(74, textHelpAppendable0.getMaxWidth());
+    }
+}
