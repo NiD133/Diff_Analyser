a7a8ca1e56b2c916acb5922398264a69106f74e0
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:56:17 +0800

    Original processedAutoTestCases jsonarray_estest69.java

---
+++ jsonarray_estest69.java
@@ -0,0 +1,30 @@
+package com.google.gson;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Iterator;
+import java.util.List;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class JsonArray_ESTestTest69 extends JsonArray_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test68() throws Throwable {
+        JsonArray jsonArray0 = new JsonArray();
+        // Undeclared exception!
+        try {
+            jsonArray0.get(0);
+            fail("Expecting exception: IndexOutOfBoundsException");
+        } catch (IndexOutOfBoundsException e) {
+            //
+            // Index: 0, Size: 0
+            //
+            verifyException("java.util.ArrayList", e);
+        }
+    }
+}
