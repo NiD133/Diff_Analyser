9fc37748b166220cb4f65013a2218399bddc9290
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:06:58 +0800

    Original processedAutoTestCases pairedstatsaccumulator_estest19.java

---
+++ pairedstatsaccumulator_estest19.java
@@ -0,0 +1,22 @@
+package com.google.common.math;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.ArrayDeque;
+import java.util.Iterator;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class PairedStatsAccumulator_ESTestTest19 extends PairedStatsAccumulator_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test18() throws Throwable {
+        PairedStatsAccumulator pairedStatsAccumulator0 = new PairedStatsAccumulator();
+        pairedStatsAccumulator0.add((-1.0), (-1.0));
+        pairedStatsAccumulator0.add((-1313.1264), 1.0);
+        double double0 = pairedStatsAccumulator0.pearsonsCorrelationCoefficient();
+        assertEquals((-1.0), double0, 0.01);
+    }
+}
