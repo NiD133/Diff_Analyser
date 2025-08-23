257e61935069b54b5d6336f969022147f7920220
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:18:08 +0800

    Original processedAutoTestCases streamcompressor_estest14.java

---
+++ streamcompressor_estest14.java
@@ -0,0 +1,46 @@
+package org.apache.commons.compress.archivers.zip;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.io.SequenceInputStream;
+import java.nio.channels.SeekableByteChannel;
+import java.util.Enumeration;
+import java.util.zip.Deflater;
+import org.apache.commons.compress.parallel.ScatterGatherBackingStore;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.io.MockFileInputStream;
+import org.evosuite.runtime.mock.java.io.MockFileOutputStream;
+import org.junit.runner.RunWith;
+
+public class StreamCompressor_ESTestTest14 extends StreamCompressor_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test13() throws Throwable {
+        PipedOutputStream pipedOutputStream0 = new PipedOutputStream();
+        StreamCompressor streamCompressor0 = StreamCompressor.create((OutputStream) pipedOutputStream0);
+        byte[] byteArray0 = new byte[0];
+        // Undeclared exception!
+        try {
+            streamCompressor0.write(byteArray0, (-822), (-822), (-822));
+            fail("Expecting exception: ArrayIndexOutOfBoundsException");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("java.util.zip.CRC32", e);
+        }
+    }
+}
