9ceb4b6bbd17f5018a358d69f55d5b8fac2f1a4b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:47:35 +0800

    Original processedAutoTestCases defaultkeyedvaluedataset_estest8.java

---
+++ defaultkeyedvaluedataset_estest8.java
@@ -0,0 +1,25 @@
+package org.jfree.data.general;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.math.BigInteger;
+import javax.swing.JLayeredPane;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.util.MockDate;
+import org.jfree.chart.date.SerialDate;
+import org.jfree.chart.date.SpreadsheetDate;
+import org.jfree.data.statistics.SimpleHistogramBin;
+import org.jfree.data.xy.OHLCDataItem;
+import org.junit.runner.RunWith;
+
+public class DefaultKeyedValueDataset_ESTestTest8 extends DefaultKeyedValueDataset_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test07() throws Throwable {
+        DefaultKeyedValueDataset defaultKeyedValueDataset0 = new DefaultKeyedValueDataset();
+        Comparable comparable0 = defaultKeyedValueDataset0.getKey();
+        assertNull(comparable0);
+    }
+}
