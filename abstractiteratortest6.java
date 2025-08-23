a044d6ccae3f564bb50a4018775cb59f56298cb1
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:24:38 +0800

    gemini processedManualTestCases abstractiteratortest6.java

--- abstractiteratortest6.java
+++ abstractiteratortest6.java
@@ -1,33 +1,40 @@
 package com.google.common.collect;
 
 import static com.google.common.collect.ReflectionFreeAssertThrows.assertThrows;
-import static com.google.common.collect.SneakyThrows.sneakyThrow;
-import com.google.common.annotations.GwtCompatible;
-import com.google.common.annotations.GwtIncompatible;
-import com.google.common.annotations.J2ktIncompatible;
-import com.google.common.collect.TestExceptions.SomeCheckedException;
-import com.google.common.collect.TestExceptions.SomeUncheckedException;
-import com.google.common.testing.GcFinalization;
-import java.lang.ref.WeakReference;
-import java.util.Iterator;
-import java.util.NoSuchElementException;
-import junit.framework.TestCase;
-import org.jspecify.annotations.NullMarked;
-import org.jspecify.annotations.Nullable;
 
+import com.google.common.collect.TestExceptions.SomeUncheckedException;
+import java.util.Iterator;
+import junit.framework.TestCase;
+
+/**
+ * Tests for {@link AbstractIterator}.
+ */
 public class AbstractIteratorTestTest6 extends TestCase {
 
-    public void testException() {
-        SomeUncheckedException exception = new SomeUncheckedException();
-        Iterator<Integer> iter = new AbstractIterator<Integer>() {
-
-            @Override
-            public Integer computeNext() {
-                throw exception;
-            }
+  /**
+   * Tests that an unchecked exception thrown from {@link AbstractIterator#computeNext()} is
+   * propagated without modification when {@link Iterator#hasNext()} is called.
+   */
+  public void testHasNext_propagatesUncheckedExceptionFromComputeNext() {
+    // ARRANGE: Create an exception and an iterator that is designed to throw it.
+    SomeUncheckedException expectedException = new SomeUncheckedException();
+    Iterator<Integer> iterator =
+        new AbstractIterator<Integer>() {
+          @Override
+          protected Integer computeNext() {
+            throw expectedException;
+          }
         };
-        // It should pass through untouched
-        SomeUncheckedException e = assertThrows(SomeUncheckedException.class, iter::hasNext);
-        assertSame(exception, e);
-    }
+
+    // ACT: Call hasNext(), which should trigger computeNext() and throw the exception.
+    SomeUncheckedException thrownException =
+        assertThrows(SomeUncheckedException.class, iterator::hasNext);
+
+    // ASSERT: Verify that the caught exception is the exact same instance we arranged to be thrown,
+    // confirming it was propagated without being wrapped or altered.
+    assertSame(
+        "The propagated exception should be the same instance thrown from computeNext()",
+        expectedException,
+        thrownException);
+  }
 }
