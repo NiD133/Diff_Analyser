390dc5f8165c9b86c4630c2063211a6882ba61c7
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:00:50 +0800

    Original processedAutoTestCases listeningexecutorservice_estest8.java

---
+++ listeningexecutorservice_estest8.java
@@ -0,0 +1,33 @@
+package com.google.common.util.concurrent;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.time.Duration;
+import java.time.temporal.ChronoField;
+import java.time.temporal.TemporalUnit;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.LinkedTransferQueue;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class ListeningExecutorService_ESTestTest8 extends ListeningExecutorService_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test7() throws Throwable {
+        DirectExecutorService directExecutorService0 = new DirectExecutorService();
+        Duration duration0 = Duration.ZERO;
+        LinkedTransferQueue<Callable<Object>> linkedTransferQueue0 = new LinkedTransferQueue<Callable<Object>>();
+        List<Future<Object>> list0 = directExecutorService0.invokeAll((Collection<? extends Callable<Object>>) linkedTransferQueue0, duration0);
+        assertEquals(0, list0.size());
+    }
+}
