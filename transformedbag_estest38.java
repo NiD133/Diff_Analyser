baf55621452909afbda8f514b89fe563eeb1f483
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:23:06 +0800

    gemini processedAutoTestCases transformedbag_estest38.java

--- transformedbag_estest38.java
+++ transformedbag_estest38.java
@@ -1,54 +1,34 @@
 package org.apache.commons.collections4.bag;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Set;
-import java.util.Vector;
 import org.apache.commons.collections4.Bag;
-import org.apache.commons.collections4.Factory;
-import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.SortedBag;
 import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AndPredicate;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ChainedTransformer;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantFactory;
 import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.FactoryTransformer;
-import org.apache.commons.collections4.functors.FalsePredicate;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.InstantiateFactory;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.MapTransformer;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NotPredicate;
-import org.apache.commons.collections4.functors.OnePredicate;
-import org.apache.commons.collections4.functors.SwitchTransformer;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class TransformedBag_ESTestTest38 extends TransformedBag_ESTest_scaffolding {
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
-    @Test(timeout = 4000)
-    public void test37() throws Throwable {
-        HashBag<Integer> hashBag0 = new HashBag<Integer>();
-        Transformer<Object, Integer> transformer0 = ConstantTransformer.nullTransformer();
-        Bag<Integer> bag0 = TransformedBag.transformedBag((Bag<Integer>) hashBag0, (Transformer<? super Integer, ? extends Integer>) transformer0);
-        assertEquals(0, bag0.size());
+/**
+ * Contains tests for the factory methods of {@link TransformedBag}.
+ */
+public class TransformedBagTest {
+
+    /**
+     * Tests that the transformedBag() factory method correctly wraps an empty bag,
+     * resulting in a new empty bag.
+     */
+    @Test
+    public void testFactoryMethodCreatesEmptyBagFromEmptySource() {
+        // Arrange: Create an empty source bag and a transformer.
+        final Bag<Integer> sourceBag = new HashBag<>();
+        // The specific transformer doesn't matter here, as there are no elements to transform.
+        final Transformer<Integer, Integer> transformer = ConstantTransformer.nullTransformer();
+
+        // Act: Create a transformed bag from the empty source bag.
+        final Bag<Integer> transformedBag = TransformedBag.transformedBag(sourceBag, transformer);
+
+        // Assert: The resulting bag should be non-null and empty.
+        assertNotNull("The factory method should not return a null bag.", transformedBag);
+        assertTrue("A transformed bag created from an empty bag should also be empty.", transformedBag.isEmpty());
     }
 }
