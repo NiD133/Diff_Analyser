549bbd3910ede41bbbf3d118088d66b03fddf471
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:17:21 +0800

    Original processedAutoTestCases spatialcontextfactory_estest25.java

---
+++ spatialcontextfactory_estest25.java
@@ -0,0 +1,31 @@
+package org.locationtech.spatial4j.context;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.HashMap;
+import java.util.Map;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+import org.locationtech.spatial4j.io.PolyshapeReader;
+import org.locationtech.spatial4j.shape.ShapeFactory;
+
+public class SpatialContextFactory_ESTestTest25 extends SpatialContextFactory_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test24() throws Throwable {
+        SpatialContextFactory spatialContextFactory0 = new SpatialContextFactory();
+        ClassLoader classLoader0 = ClassLoader.getSystemClassLoader();
+        // Undeclared exception!
+        try {
+            spatialContextFactory0.init((Map<String, String>) null, classLoader0);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.locationtech.spatial4j.context.SpatialContextFactory", e);
+        }
+    }
+}
