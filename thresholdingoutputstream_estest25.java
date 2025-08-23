7ddb773c40e279eb30354e43a68d59596a40faf4
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:21:58 +0800

    Original processedAutoTestCases thresholdingoutputstream_estest25.java

---
+++ thresholdingoutputstream_estest25.java
@@ -0,0 +1,23 @@
+package org.apache.commons.io.output;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.OutputStream;
+import org.apache.commons.io.function.IOConsumer;
+import org.apache.commons.io.function.IOFunction;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class ThresholdingOutputStream_ESTestTest25 extends ThresholdingOutputStream_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test24() throws Throwable {
+        ThresholdingOutputStream thresholdingOutputStream0 = new ThresholdingOutputStream((-2596));
+        byte[] byteArray0 = new byte[1];
+        thresholdingOutputStream0.write(byteArray0);
+        thresholdingOutputStream0.checkThreshold(0);
+        assertEquals(1L, thresholdingOutputStream0.getByteCount());
+    }
+}
