aa7e41fe507d834cab74308dde254e0b4c18330f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:13:25 +0800

    gemini processedAutoTestCases seekableinmemorybytechannel_estest32.java

--- seekableinmemorybytechannel_estest32.java
+++ seekableinmemorybytechannel_estest32.java
@@ -1,31 +1,32 @@
 package org.apache.commons.compress.utils;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
+
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.ClosedChannelException;
-import java.nio.channels.SeekableByteChannel;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class SeekableInMemoryByteChannel_ESTestTest32 extends SeekableInMemoryByteChannel_ESTest_scaffolding {
+/**
+ * Unit tests for the SeekableInMemoryByteChannel class.
+ */
+public class SeekableInMemoryByteChannelTest {
 
-    @Test(timeout = 4000)
-    public void test31() throws Throwable {
-        SeekableInMemoryByteChannel seekableInMemoryByteChannel0 = new SeekableInMemoryByteChannel();
-        seekableInMemoryByteChannel0.close();
-        ByteBuffer byteBuffer0 = ByteBuffer.allocate(1);
-        try {
-            seekableInMemoryByteChannel0.write(byteBuffer0);
-            fail("Expecting exception: ClosedChannelException");
-        } catch (ClosedChannelException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.commons.compress.utils.SeekableInMemoryByteChannel", e);
-        }
+    /**
+     * Verifies that attempting to write to a channel that has been closed
+     * results in a ClosedChannelException.
+     */
+    @Test(expected = ClosedChannelException.class)
+    public void writeToClosedChannelShouldThrowException() throws IOException {
+        // Arrange: Create a channel, close it, and prepare a buffer for writing.
+        final SeekableInMemoryByteChannel channel = new SeekableInMemoryByteChannel();
+        channel.close();
+        final ByteBuffer buffer = ByteBuffer.allocate(1);
+
+        // Act: Attempt to write to the closed channel.
+        // This action is expected to throw the ClosedChannelException.
+        channel.write(buffer);
+
+        // Assert: The test passes if the expected exception is thrown.
+        // This is handled automatically by the @Test(expected=...) annotation.
     }
 }
