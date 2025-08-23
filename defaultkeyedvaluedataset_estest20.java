6ea202f81626659fba09619f95d6452af5b574b3
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:47:33 +0800

    Original processedAutoTestCases defaultkeyedvaluedataset_estest20.java

---
+++ defaultkeyedvaluedataset_estest20.java
@@ -0,0 +1,27 @@
+package org.jfree.data.general;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.math.BigInteger;
+import javax.swing.JLayeredPane;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.util.MockDate;
+import org.jfree.chart.date.SerialDate;
+import org.jfree.chart.date.SpreadsheetDate;
+import org.jfree.data.statistics.SimpleHistogramBin;
+import org.jfree.data.xy.OHLCDataItem;
+import org.junit.runner.RunWith;
+
+public class DefaultKeyedValueDataset_ESTestTest20 extends DefaultKeyedValueDataset_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test19() throws Throwable {
+        BigInteger bigInteger0 = BigInteger.TEN;
+        DefaultKeyedValueDataset defaultKeyedValueDataset0 = new DefaultKeyedValueDataset(bigInteger0, bigInteger0);
+        DefaultKeyedValueDataset defaultKeyedValueDataset1 = new DefaultKeyedValueDataset();
+        boolean boolean0 = defaultKeyedValueDataset0.equals(defaultKeyedValueDataset1);
+        assertFalse(boolean0);
+    }
+}
