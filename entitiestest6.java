1c3003c1d6a4d384aa3b23f4ecd0e35b2c5f02f5
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:28:26 +0800

    gemini processedManualTestCases entitiestest6.java

--- entitiestest6.java
+++ entitiestest6.java
@@ -1,20 +1,46 @@
 package org.jsoup.nodes;
 
-import org.jsoup.Jsoup;
-import org.jsoup.parser.Parser;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import static org.jsoup.nodes.Document.OutputSettings;
-import static org.jsoup.nodes.Entities.EscapeMode.*;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class EntitiesTestTest6 {
+/**
+ * A more understandable test suite for the {@link Entities} class.
+ */
+class EntitiesTest {
 
-    @Test
-    public void getByName() {
-        assertEquals("≫⃒", Entities.getByName("nGt"));
-        assertEquals("fj", Entities.getByName("fjlig"));
-        assertEquals("≫", Entities.getByName("gg"));
-        assertEquals("©", Entities.getByName("copy"));
+    @Nested
+    @DisplayName("getByName")
+    class GetByName {
+
+        @Test
+        @DisplayName("should resolve a common named entity")
+        void resolvesCommonEntity() {
+            // "copy" is a well-known entity for the copyright symbol.
+            assertEquals("©", Entities.getByName("copy"));
+        }
+
+        @Test
+        @DisplayName("should resolve a symbolic named entity")
+        void resolvesSymbolicEntity() {
+            // "gg" is an entity for the "much greater-than" symbol.
+            assertEquals("≫", Entities.getByName("gg"));
+        }
+
+        @Test
+        @DisplayName("should resolve a multi-codepoint named entity")
+        void resolvesMultiCodepointEntity() {
+            // "nGt" ("not Greater-Than") is an example of an entity that resolves to multiple characters.
+            assertEquals("≫⃒", Entities.getByName("nGt"));
+        }
+
+        @Test
+        @DisplayName("should resolve a ligature named entity")
+        void resolvesLigatureEntity() {
+            // "fjlig" is an example of a ligature entity, which represents two characters.
+            assertEquals("fj", Entities.getByName("fjlig"));
+        }
     }
 }
