67d8e452f251f6717c13598cd362c9545a82bfb0
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:59:39 +0800

    Original processedAutoTestCases labelblock_estest24.java

---
+++ labelblock_estest24.java
@@ -0,0 +1,40 @@
+package org.jfree.chart.block;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.Paint;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.SystemColor;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.text.AttributedCharacterIterator;
+import java.util.Hashtable;
+import javax.swing.text.DefaultStyledDocument;
+import javax.swing.text.StyleContext;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.jfree.chart.api.RectangleAnchor;
+import org.jfree.chart.text.TextBlockAnchor;
+import org.jfree.data.Range;
+import org.junit.runner.RunWith;
+
+public class LabelBlock_ESTestTest24 extends LabelBlock_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test23() throws Throwable {
+        StyleContext styleContext0 = StyleContext.getDefaultStyleContext();
+        Font font0 = styleContext0.getFont("\"IAf&@`u", 1447, 1447);
+        Color color0 = Color.cyan;
+        LabelBlock labelBlock0 = new LabelBlock("\"IAf&@`u", font0, color0);
+        boolean boolean0 = labelBlock0.equals(color0);
+        assertFalse(boolean0);
+    }
+}
