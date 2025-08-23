9c5e2dca5ecdec462b24aad90550bb859324525f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:18:56 +0800

    gemini processedAutoTestCases stringutil_estest46.java

--- stringutil_estest46.java
+++ stringutil_estest46.java
@@ -1,39 +1,33 @@
 package org.jsoup.internal;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Collection;
+
 import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.ListIterator;
-import java.util.stream.Collector;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.evosuite.runtime.mock.java.net.MockURL;
-import org.junit.runner.RunWith;
+import java.util.List;
 
-public class StringUtil_ESTestTest46 extends StringUtil_ESTest_scaffolding {
+/**
+ * Test for {@link StringUtil#join(Iterator, String)} to ensure it handles concurrent modifications correctly.
+ */
+public class StringUtilJoinIteratorTest {
 
-    @Test(timeout = 4000)
-    public void test45() throws Throwable {
-        LinkedList<Object> linkedList0 = new LinkedList<Object>();
-        ListIterator<Object> listIterator0 = linkedList0.listIterator();
-        linkedList0.add((Object) listIterator0);
-        // Undeclared exception!
-        try {
-            StringUtil.join((Iterator<?>) listIterator0, "?4");
-            fail("Expecting exception: ConcurrentModificationException");
-        } catch (ConcurrentModificationException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("java.util.LinkedList$ListItr", e);
-        }
+    /**
+     * Verifies that StringUtil.join() throws a ConcurrentModificationException
+     * when the underlying collection is modified after an iterator has been created from it.
+     */
+    @Test(expected = ConcurrentModificationException.class)
+    public void joinWithIteratorThrowsExceptionWhenCollectionIsModified() {
+        // Arrange: Create a list and an iterator for it.
+        List<String> list = new LinkedList<>();
+        Iterator<String> iterator = list.iterator();
+
+        // Act: Modify the list after the iterator has been created. This action
+        // invalidates the iterator, which should cause any subsequent use to fail.
+        list.add("one");
+
+        // Assert: Calling join with the now-invalidated iterator must throw
+        // a ConcurrentModificationException. The @Test(expected=...) annotation handles this check.
+        StringUtil.join(iterator, ", ");
     }
 }
