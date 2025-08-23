14747775efe9a1ea82229457a5f283b2b84029df
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:39:25 +0800

    Original processedAutoTestCases binarycodec_estest5.java

---
+++ binarycodec_estest5.java
@@ -0,0 +1,18 @@
+package org.apache.commons.codec.binary;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class BinaryCodec_ESTestTest5 extends BinaryCodec_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test04() throws Throwable {
+        BinaryCodec binaryCodec0 = new BinaryCodec();
+        byte[] byteArray0 = binaryCodec0.encode((byte[]) null);
+        assertArrayEquals(new byte[] {}, byteArray0);
+    }
+}
