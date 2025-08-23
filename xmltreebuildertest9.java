31d2e95731338729bbf1c384875b7fcb230cce6d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:36:19 +0800

    Original processedManualTestCases xmltreebuildertest9.java

---
+++ xmltreebuildertest9.java
@@ -0,0 +1,31 @@
+package org.jsoup.parser;
+
+import org.jsoup.Jsoup;
+import org.jsoup.TextUtil;
+import org.jsoup.nodes.*;
+import org.jsoup.select.Elements;
+import org.junit.jupiter.api.Test;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import static org.jsoup.nodes.Document.OutputSettings.Syntax;
+import static org.jsoup.parser.Parser.NamespaceHtml;
+import static org.jsoup.parser.Parser.NamespaceXml;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class XmlTreeBuilderTestTest9 {
+
+    private static void assertXmlNamespace(Element el) {
+        assertEquals(NamespaceXml, el.tag().namespace(), String.format("Element %s not in XML namespace", el.tagName()));
+    }
+
+    @Test
+    public void xmlParseDefaultsToHtmlOutputSyntax() {
+        Document doc = Jsoup.parse("x", "", Parser.xmlParser());
+        assertEquals(Syntax.xml, doc.outputSettings().syntax());
+    }
+}
