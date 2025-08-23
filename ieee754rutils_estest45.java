5092470f8178d5ae36591b85ca73f0bb4ab108d5
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:53:37 +0800

    gemini processedAutoTestCases ieee754rutils_estest45.java

--- ieee754rutils_estest45.java
+++ ieee754rutils_estest45.java
@@ -1,16 +1,28 @@
 package org.apache.commons.lang3.math;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertNotNull;
 
-public class IEEE754rUtils_ESTestTest45 extends IEEE754rUtils_ESTest_scaffolding {
+/**
+ * Test case for the {@link IEEE754rUtils} class.
+ */
+public class IEEE754rUtilsTest {
 
-    @Test(timeout = 4000)
-    public void test44() throws Throwable {
-        IEEE754rUtils iEEE754rUtils0 = new IEEE754rUtils();
+    /**
+     * Tests that the public constructor of the utility class can be instantiated.
+     *
+     * <p>
+     * Although {@link IEEE754rUtils} is a utility class with only static methods,
+     * its public constructor is maintained for backward compatibility. This test ensures
+     * the constructor can be called without error, primarily for code coverage purposes.
+     * </p>
+     */
+    @Test
+    public void constructorShouldCreateInstanceForCoverage() {
+        // Act: Instantiate the utility class
+        final IEEE754rUtils instance = new IEEE754rUtils();
+
+        // Assert: The instance should be successfully created and not null.
+        assertNotNull("The constructor should create a non-null instance.", instance);
     }
 }
