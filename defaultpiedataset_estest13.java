f404d39538e6ccb886234d29e13eff478353e5c0
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:47:37 +0800

    gemini processedAutoTestCases defaultpiedataset_estest13.java

--- defaultpiedataset_estest13.java
+++ defaultpiedataset_estest13.java
@@ -1,39 +1,31 @@
 package org.jfree.data.general;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.time.chrono.ChronoLocalDate;
-import java.util.List;
-import java.util.Vector;
-import javax.swing.JLayeredPane;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.jfree.chart.api.SortOrder;
-import org.jfree.chart.api.TableOrder;
-import org.jfree.data.DefaultKeyedValues;
-import org.jfree.data.KeyedValues;
-import org.jfree.data.category.CategoryDataset;
-import org.jfree.data.category.CategoryToPieDataset;
-import org.jfree.data.category.DefaultIntervalCategoryDataset;
-import org.jfree.data.category.SlidingCategoryDataset;
-import org.jfree.data.statistics.DefaultBoxAndWhiskerCategoryDataset;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
-public class DefaultPieDataset_ESTestTest13 extends DefaultPieDataset_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link DefaultPieDataset} class.
+ */
+public class DefaultPieDatasetTest {
 
-    @Test(timeout = 4000)
-    public void test12() throws Throwable {
-        DefaultKeyedValuesDataset<Integer> defaultKeyedValuesDataset0 = new DefaultKeyedValuesDataset<Integer>();
-        // Undeclared exception!
+    /**
+     * Verifies that calling setValue() with a null key throws an
+     * IllegalArgumentException, as null keys are not permitted.
+     */
+    @Test
+    public void setValue_withNullKey_shouldThrowIllegalArgumentException() {
+        // Arrange: Create an empty dataset
+        DefaultPieDataset<String> dataset = new DefaultPieDataset<>();
+        final Double value = 10.0;
+
+        // Act & Assert: Attempt to set a value with a null key and expect an exception
         try {
-            defaultKeyedValuesDataset0.setValue((Integer) null, (Number) null);
-            fail("Expecting exception: IllegalArgumentException");
+            dataset.setValue(null, value);
+            fail("Expected an IllegalArgumentException to be thrown for a null key, but it was not.");
         } catch (IllegalArgumentException e) {
-            //
-            // Null 'key' argument.
-            //
-            verifyException("org.jfree.chart.internal.Args", e);
+            // Verify that the exception message is correct
+            assertEquals("Null 'key' argument.", e.getMessage());
         }
     }
 }
