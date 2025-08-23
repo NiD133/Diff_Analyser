c1b6f3f5f46b5dfb1543b000b73e2069cb4e7c78
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:45:35 +0800

    Original processedAutoTestCases constructorutils_estest13.java

---
+++ constructorutils_estest13.java
@@ -0,0 +1,31 @@
+package org.apache.commons.lang3.reflect;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.AccessibleObject;
+import java.lang.reflect.Array;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class ConstructorUtils_ESTestTest13 extends ConstructorUtils_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test12() throws Throwable {
+        Class<Integer>[] classArray0 = (Class<Integer>[]) Array.newInstance(Class.class, 1);
+        // Undeclared exception!
+        try {
+            ConstructorUtils.getAccessibleConstructor((Class<Integer>) null, (Class<?>[]) classArray0);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // cls
+            //
+            verifyException("java.util.Objects", e);
+        }
+    }
+}
