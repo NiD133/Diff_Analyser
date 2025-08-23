63a3ab261c2d547948b9d5b86ce41ec3deddcb60
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:36:20 +0800

    gemini processedManualTestCases xmpwritertest2.java

--- xmpwritertest2.java
+++ xmpwritertest2.java
@@ -4,48 +4,66 @@ import com.itextpdf.testutils.CompareTool;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Paragraph;
-import com.itextpdf.text.pdf.*;
-import com.itextpdf.xmp.XMPException;
+import com.itextpdf.text.pdf.PdfWriter;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import java.io.ByteArrayOutputStream;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.HashMap;
 
-public class XmpWriterTestTest2 {
+/**
+ * Tests the automatic creation of XMP metadata from the standard PDF Info dictionary.
+ */
+public class XmpWriterAutomaticCreationTest {
 
-    public static final String OUT_FOLDER = "./target/com/itextpdf/text/xml/xmp/";
+    private static final String OUT_FOLDER = "./target/test-output/com/itextpdf/text/xml/xmp/";
+    private static final String CMP_FOLDER = "./src/test/resources/com/itextpdf/text/xml/xmp/";
 
-    public static final String CMP_FOLDER = "./src/test/resources/com/itextpdf/text/xml/xmp/";
+    private static final String FILE_NAME = "xmp_metadata_automatic.pdf";
+    private static final String DEST_PATH = OUT_FOLDER + FILE_NAME;
+    private static final String CMP_PATH = CMP_FOLDER + FILE_NAME;
 
     @Before
-    public void init() {
+    public void setUp() {
         new File(OUT_FOLDER).mkdirs();
     }
 
+    /**
+     * Verifies that when createXmpMetadata() is called, the document's Info dictionary
+     * properties (Title, Author, etc.) are correctly converted into an XMP stream in the
+     * resulting PDF.
+     */
     @Test
-    public void createPdfAutomaticTest() throws IOException, DocumentException {
-        String fileName = "xmp_metadata_automatic.pdf";
-        // step 1
+    public void testAutomaticXmpCreationFromDocumentInfo() throws IOException, DocumentException {
+        // Arrange
         Document document = new Document();
-        // step 2
-        PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(OUT_FOLDER + fileName));
+        PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(DEST_PATH));
+
+        // Add standard document metadata, which populates the PDF Info dictionary.
         document.addTitle("Hello World example");
         document.addSubject("This example shows how to add metadata & XMP");
         document.addKeywords("Metadata, iText, step 3");
         document.addCreator("My program using 'iText'");
         document.addAuthor("Bruno Lowagie & Paulo Soares");
+
+        // This method call instructs the writer to automatically generate
+        // an XMP stream from the Info dictionary metadata upon closing the document.
         writer.createXmpMetadata();
-        // step 3
+
+        // Act
         document.open();
-        // step 4
         document.add(new Paragraph("Hello World"));
-        // step 5
         document.close();
-        CompareTool ct = new CompareTool();
-        Assert.assertNull(ct.compareXmp(OUT_FOLDER + fileName, CMP_FOLDER + fileName, true));
+
+        // Assert
+        CompareTool compareTool = new CompareTool();
+        String comparisonResult = compareTool.compareXmp(DEST_PATH, CMP_PATH, true);
+
+        Assert.assertNull(
+            "The generated XMP metadata should match the reference file. Differences found: " + comparisonResult,
+            comparisonResult
+        );
     }
 }
