ec25bb9a5fce8e5c9f2cc8dda1236202aa07d454
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:27:20 +0800

    gemini processedManualTestCases datetimecomparatortest15.java

--- datetimecomparatortest15.java
+++ datetimecomparatortest15.java
@@ -1,242 +1,45 @@
-package org.joda.time;
+package com.fasterxml.jackson.core.format;
 
+import com.fasterxml.jackson.core.JsonFactory;
+import org.junit.Test;
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.lang.reflect.Modifier;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Date;
-import java.util.List;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-import org.joda.time.chrono.ISOChronology;
+import java.io.InputStream;
+import static org.junit.Assert.assertNull;
 
-public class DateTimeComparatorTestTest15 extends TestCase {
-
-    private static final Chronology ISO = ISOChronology.getInstance();
+/**
+ * Contains tests for the {@link DataFormatMatcher} class.
+ */
+public class DataFormatMatcherTest {
 
     /**
-     * A reference to a DateTime object.
+     * Tests that {@link DataFormatMatcher#getMatchedFormatName()} returns null
+     * when no data format has been matched.
+     *
+     * This scenario is simulated by creating a DataFormatMatcher instance
+     * with a null JsonFactory, which represents the absence of a successful match.
      */
-    DateTime aDateTime = null;
+    @Test
+    public void getMatchedFormatNameShouldReturnNullWhenNoMatchIsFound() {
+        // Arrange: Set up a DataFormatMatcher representing a state where no format was matched.
+        // An empty input stream and a null JsonFactory are used to simulate this case.
+        byte[] inputData = new byte[10];
+        InputStream inputStream = new ByteArrayInputStream(inputData);
+        JsonFactory noMatchingFactory = null; // A null factory indicates no format was matched.
+        MatchStrength strength = MatchStrength.INCONCLUSIVE;
 
-    /**
-     * A reference to a DateTime object.
-     */
-    DateTime bDateTime = null;
+        DataFormatMatcher matcher = new DataFormatMatcher(
+                inputStream,
+                inputData,
+                0, // bufferStart
+                0, // bufferLength
+                noMatchingFactory,
+                strength
+        );
 
-    /**
-     * A reference to a DateTimeComparator object
-     * (a Comparator) for millis of seconds.
-     */
-    Comparator cMillis = null;
+        // Act: Call the method to get the name of the matched format.
+        String formatName = matcher.getMatchedFormatName();
 
-    /**
-     * A reference to a DateTimeComparator object
-     * (a Comparator) for seconds.
-     */
-    Comparator cSecond = null;
-
-    /**
-     * A reference to a DateTimeComparator object
-     * (a Comparator) for minutes.
-     */
-    Comparator cMinute = null;
-
-    /**
-     * A reference to a DateTimeComparator object
-     * (a Comparator) for hours.
-     */
-    Comparator cHour = null;
-
-    /**
-     * A reference to a DateTimeComparator object
-     * (a Comparator) for day of the week.
-     */
-    Comparator cDayOfWeek = null;
-
-    /**
-     * A reference to a DateTimeComparator object
-     * (a Comparator) for day of the month.
-     */
-    Comparator cDayOfMonth = null;
-
-    /**
-     * A reference to a DateTimeComparator object
-     * (a Comparator) for day of the year.
-     */
-    Comparator cDayOfYear = null;
-
-    /**
-     * A reference to a DateTimeComparator object
-     * (a Comparator) for week of the weekyear.
-     */
-    Comparator cWeekOfWeekyear = null;
-
-    /**
-     * A reference to a DateTimeComparator object
-     * (a Comparator) for year given a week of the year.
-     */
-    Comparator cWeekyear = null;
-
-    /**
-     * A reference to a DateTimeComparator object
-     * (a Comparator) for months.
-     */
-    Comparator cMonth = null;
-
-    /**
-     * A reference to a DateTimeComparator object
-     * (a Comparator) for year.
-     */
-    Comparator cYear = null;
-
-    /**
-     * A reference to a DateTimeComparator object
-     * (a Comparator) for the date portion of an
-     * object.
-     */
-    Comparator cDate = null;
-
-    /**
-     * A reference to a DateTimeComparator object
-     * (a Comparator) for the time portion of an
-     * object.
-     */
-    Comparator cTime = null;
-
-    public static void main(String[] args) {
-        junit.textui.TestRunner.run(suite());
-    }
-
-    public static TestSuite suite() {
-        return new TestSuite(TestDateTimeComparator.class);
-    }
-
-    /**
-     * Junit <code>setUp()</code> method.
-     */
-    @Override
-    public void setUp() /* throws Exception */
-    {
-        Chronology chrono = ISOChronology.getInstanceUTC();
-        // super.setUp();
-        // Obtain comparator's
-        cMillis = DateTimeComparator.getInstance(null, DateTimeFieldType.secondOfMinute());
-        cSecond = DateTimeComparator.getInstance(DateTimeFieldType.secondOfMinute(), DateTimeFieldType.minuteOfHour());
-        cMinute = DateTimeComparator.getInstance(DateTimeFieldType.minuteOfHour(), DateTimeFieldType.hourOfDay());
-        cHour = DateTimeComparator.getInstance(DateTimeFieldType.hourOfDay(), DateTimeFieldType.dayOfYear());
-        cDayOfWeek = DateTimeComparator.getInstance(DateTimeFieldType.dayOfWeek(), DateTimeFieldType.weekOfWeekyear());
-        cDayOfMonth = DateTimeComparator.getInstance(DateTimeFieldType.dayOfMonth(), DateTimeFieldType.monthOfYear());
-        cDayOfYear = DateTimeComparator.getInstance(DateTimeFieldType.dayOfYear(), DateTimeFieldType.year());
-        cWeekOfWeekyear = DateTimeComparator.getInstance(DateTimeFieldType.weekOfWeekyear(), DateTimeFieldType.weekyear());
-        cWeekyear = DateTimeComparator.getInstance(DateTimeFieldType.weekyear());
-        cMonth = DateTimeComparator.getInstance(DateTimeFieldType.monthOfYear(), DateTimeFieldType.year());
-        cYear = DateTimeComparator.getInstance(DateTimeFieldType.year());
-        cDate = DateTimeComparator.getDateOnlyInstance();
-        cTime = DateTimeComparator.getTimeOnlyInstance();
-    }
-
-    /**
-     * Junit <code>tearDown()</code> method.
-     */
-    @Override
-    protected void tearDown() /* throws Exception */
-    {
-        // super.tearDown();
-        aDateTime = null;
-        bDateTime = null;
-        //
-        cMillis = null;
-        cSecond = null;
-        cMinute = null;
-        cHour = null;
-        cDayOfWeek = null;
-        cDayOfMonth = null;
-        cDayOfYear = null;
-        cWeekOfWeekyear = null;
-        cWeekyear = null;
-        cMonth = null;
-        cYear = null;
-        cDate = null;
-        cTime = null;
-    }
-
-    /**
-     * Creates a date to test with.
-     */
-    private DateTime getADate(String s) {
-        DateTime retDT = null;
-        try {
-            retDT = new DateTime(s, DateTimeZone.UTC);
-        } catch (IllegalArgumentException pe) {
-            pe.printStackTrace();
-        }
-        return retDT;
-    }
-
-    /**
-     * Load a string array.
-     */
-    private List loadAList(String[] someStrs) {
-        List newList = new ArrayList();
-        try {
-            for (int i = 0; i < someStrs.length; ++i) {
-                newList.add(new DateTime(someStrs[i], DateTimeZone.UTC));
-            }
-            // end of the for
-        } catch (IllegalArgumentException pe) {
-            pe.printStackTrace();
-        }
-        return newList;
-    }
-
-    /**
-     * Check if the list is sorted.
-     */
-    private boolean isListSorted(List tl) {
-        // tl must be populated with DateTime objects.
-        DateTime lhDT = (DateTime) tl.get(0);
-        DateTime rhDT = null;
-        Long lhVal = new Long(lhDT.getMillis());
-        Long rhVal = null;
-        for (int i = 1; i < tl.size(); ++i) {
-            rhDT = (DateTime) tl.get(i);
-            rhVal = new Long(rhDT.getMillis());
-            if (lhVal.compareTo(rhVal) > 0)
-                return false;
-            //
-            // swap for next iteration
-            lhVal = rhVal;
-            // swap for next iteration
-            lhDT = rhDT;
-        }
-        return true;
-    }
-
-    /**
-     * Test all basic comparator operation with Calendar objects.
-     */
-    public void testBasicComps5() {
-        Calendar aDateTime = // right now
-        Calendar.getInstance();
-        Calendar bDateTime = aDateTime;
-        assertEquals("MILLIS", 0, cMillis.compare(aDateTime, bDateTime));
-        assertEquals("SECOND", 0, cSecond.compare(aDateTime, bDateTime));
-        assertEquals("MINUTE", 0, cMinute.compare(aDateTime, bDateTime));
-        assertEquals("HOUR", 0, cHour.compare(aDateTime, bDateTime));
-        assertEquals("DOW", 0, cDayOfWeek.compare(aDateTime, bDateTime));
-        assertEquals("DOM", 0, cDayOfMonth.compare(aDateTime, bDateTime));
-        assertEquals("DOY", 0, cDayOfYear.compare(aDateTime, bDateTime));
-        assertEquals("WOW", 0, cWeekOfWeekyear.compare(aDateTime, bDateTime));
-        assertEquals("WY", 0, cWeekyear.compare(aDateTime, bDateTime));
-        assertEquals("MONTH", 0, cMonth.compare(aDateTime, bDateTime));
-        assertEquals("YEAR", 0, cYear.compare(aDateTime, bDateTime));
-        assertEquals("DATE", 0, cDate.compare(aDateTime, bDateTime));
-        assertEquals("TIME", 0, cTime.compare(aDateTime, bDateTime));
+        // Assert: Verify that the returned format name is null, as expected.
+        assertNull("The format name should be null when no JsonFactory is matched.", formatName);
     }
 }
