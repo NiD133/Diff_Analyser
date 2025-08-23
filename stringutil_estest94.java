445d8cd081ad159551fbd8966f49892b592e49a8
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:19:16 +0800

    gemini processedAutoTestCases stringutil_estest94.java

--- stringutil_estest94.java
+++ stringutil_estest94.java
@@ -1,29 +1,26 @@
 package org.jsoup.internal;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Collection;
-import java.util.ConcurrentModificationException;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.ListIterator;
-import java.util.stream.Collector;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.evosuite.runtime.mock.java.net.MockURL;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertSame;
 
-public class StringUtil_ESTestTest94 extends StringUtil_ESTest_scaffolding {
+/**
+ * Tests for the inner class {@link StringUtil.StringJoiner}.
+ */
+public class StringUtilStringJoinerTest {
 
-    @Test(timeout = 4000)
-    public void test93() throws Throwable {
-        StringUtil.StringJoiner stringUtil_StringJoiner0 = new StringUtil.StringJoiner("auzz./f?g8Z@iU:MH");
-        StringUtil.StringJoiner stringUtil_StringJoiner1 = stringUtil_StringJoiner0.append("auzz./f?g8Z@iU:MH");
-        assertSame(stringUtil_StringJoiner1, stringUtil_StringJoiner0);
+    /**
+     * Verifies that the append() method returns the same StringJoiner instance
+     * to allow for fluent method chaining.
+     */
+    @Test
+    public void appendReturnsSameInstanceForChaining() {
+        // Arrange: Create a StringJoiner with a simple separator.
+        StringUtil.StringJoiner joiner = new StringUtil.StringJoiner(", ");
+
+        // Act: Call the append method.
+        StringUtil.StringJoiner result = joiner.append("some text");
+
+        // Assert: The returned object should be the exact same instance as the original.
+        assertSame("The append() method must return 'this' to support a fluent API.", joiner, result);
     }
 }
