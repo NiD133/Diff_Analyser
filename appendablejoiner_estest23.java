6687a3a2e027e81ca26b5f1a3a9d9a9a1a9e72d2
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:37:13 +0800

    Original processedAutoTestCases appendablejoiner_estest23.java

---
+++ appendablejoiner_estest23.java
@@ -0,0 +1,42 @@
+package org.apache.commons.lang3;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.IOException;
+import java.io.PipedWriter;
+import java.nio.BufferOverflowException;
+import java.nio.CharBuffer;
+import java.nio.ReadOnlyBufferException;
+import java.nio.charset.Charset;
+import java.sql.SQLNonTransientConnectionException;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.LinkedHashSet;
+import java.util.Locale;
+import org.apache.commons.lang3.function.FailableBiConsumer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class AppendableJoiner_ESTestTest23 extends AppendableJoiner_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test22() throws Throwable {
+        AppendableJoiner.Builder<StringBuilder> appendableJoiner_Builder0 = new AppendableJoiner.Builder<StringBuilder>();
+        CharBuffer charBuffer0 = CharBuffer.allocate(0);
+        AppendableJoiner<StringBuilder> appendableJoiner0 = appendableJoiner_Builder0.get();
+        StringBuilder[] stringBuilderArray0 = new StringBuilder[6];
+        // Undeclared exception!
+        try {
+            appendableJoiner0.joinA((Appendable) charBuffer0, stringBuilderArray0);
+            fail("Expecting exception: BufferOverflowException");
+        } catch (BufferOverflowException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("java.nio.CharBuffer", e);
+        }
+    }
+}
