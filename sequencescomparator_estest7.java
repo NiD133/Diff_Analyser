15534fca8a4f6cdecabde82a69ce5d2ac8953ba4
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:14:39 +0800

    Original processedAutoTestCases sequencescomparator_estest7.java

---
+++ sequencescomparator_estest7.java
@@ -0,0 +1,36 @@
+package org.apache.commons.collections4.sequence;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.ConcurrentModificationException;
+import java.util.LinkedList;
+import java.util.List;
+import org.apache.commons.collections4.Equator;
+import org.apache.commons.collections4.Predicate;
+import org.apache.commons.collections4.functors.DefaultEquator;
+import org.apache.commons.collections4.functors.NotNullPredicate;
+import org.apache.commons.collections4.functors.PredicateTransformer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class SequencesComparator_ESTestTest7 extends SequencesComparator_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test06() throws Throwable {
+        LinkedList<Object> linkedList0 = new LinkedList<Object>();
+        linkedList0.add((Object) null);
+        SequencesComparator<Object> sequencesComparator0 = new SequencesComparator<Object>(linkedList0, linkedList0, (Equator<? super Object>) null);
+        // Undeclared exception!
+        try {
+            sequencesComparator0.getScript();
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.apache.commons.collections4.sequence.SequencesComparator", e);
+        }
+    }
+}
