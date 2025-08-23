a2a37929acb8bb7e24ac07bb98b4b4f08fd7a02b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:09:00 +0800

    gemini processedAutoTestCases plotrenderinginfo_estest24.java

--- plotrenderinginfo_estest24.java
+++ plotrenderinginfo_estest24.java
@@ -1,28 +1,27 @@
 package org.jfree.chart.plot;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.awt.Dimension;
-import java.awt.Point;
-import java.awt.Rectangle;
-import java.awt.geom.Line2D;
-import java.awt.geom.Point2D;
-import java.awt.geom.Rectangle2D;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
 import org.jfree.chart.ChartRenderingInfo;
-import org.jfree.chart.entity.EntityCollection;
-import org.jfree.chart.entity.StandardEntityCollection;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class PlotRenderingInfo_ESTestTest24 extends PlotRenderingInfo_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
 
-    @Test(timeout = 4000)
-    public void test23() throws Throwable {
-        ChartRenderingInfo chartRenderingInfo0 = new ChartRenderingInfo();
-        PlotRenderingInfo plotRenderingInfo0 = chartRenderingInfo0.getPlotInfo();
-        boolean boolean0 = plotRenderingInfo0.equals(plotRenderingInfo0);
-        assertTrue(boolean0);
+/**
+ * Unit tests for the {@link PlotRenderingInfo} class.
+ */
+public class PlotRenderingInfoTest {
+
+    /**
+     * Verifies the reflexive property of the equals() method.
+     * An object must always be equal to itself.
+     */
+    @Test
+    public void equals_shouldReturnTrue_whenComparedToItself() {
+        // Arrange: Create a PlotRenderingInfo instance.
+        ChartRenderingInfo chartInfo = new ChartRenderingInfo();
+        PlotRenderingInfo plotInfo = chartInfo.getPlotInfo();
+
+        // Act & Assert: The object should be equal to itself.
+        // Using assertEquals is more semantic for equality checks.
+        assertEquals(plotInfo, plotInfo);
     }
 }
