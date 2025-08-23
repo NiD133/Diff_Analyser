4c43e6a24a0619db78786fc1b8cc8e5131cf3b86
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:03:03 +0800

    gemini processedAutoTestCases minutes_estest13.java

--- minutes_estest13.java
+++ minutes_estest13.java
@@ -2,17 +2,31 @@ package org.joda.time;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class Minutes_ESTestTest13 extends Minutes_ESTest_scaffolding {
+/**
+ * Contains tests for the {@link Minutes} class.
+ */
+public class MinutesTest {
 
-    @Test(timeout = 4000)
-    public void test12() throws Throwable {
-        Minutes minutes0 = Minutes.minutesIn((ReadableInterval) null);
-        Days days0 = minutes0.MAX_VALUE.toStandardDays();
-        assertEquals(1491308, days0.getDays());
+    /**
+     * Tests that converting the maximum possible number of minutes (Integer.MAX_VALUE)
+     * to standard days produces the correct number of whole days.
+     */
+    @Test
+    public void toStandardDays_fromMaxValue_returnsCorrectNumberOfDays() {
+        // Arrange
+        // A standard day is defined as 24 hours * 60 minutes/hour = 1440 minutes.
+        // The expected result is the integer division of the maximum minutes value
+        // by the number of minutes in a day.
+        final int minutesInStandardDay = 24 * 60;
+        final int expectedDays = Integer.MAX_VALUE / minutesInStandardDay; // 2,147,483,647 / 1440 = 1,491,308
+
+        // Act
+        // Convert the maximum Minutes value to Days.
+        Days actualDays = Minutes.MAX_VALUE.toStandardDays();
+
+        // Assert
+        assertEquals("The number of days should be Integer.MAX_VALUE / (24 * 60)",
+                     expectedDays, actualDays.getDays());
     }
 }
