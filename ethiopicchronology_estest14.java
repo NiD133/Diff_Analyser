eb709936288e08d439c28174dd6ad69e877ff94a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:49:43 +0800

    gemini processedAutoTestCases ethiopicchronology_estest14.java

--- ethiopicchronology_estest14.java
+++ ethiopicchronology_estest14.java
@@ -1,21 +1,38 @@
 package org.joda.time.chrono;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.joda.time.Chronology;
+import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class EthiopicChronology_ESTestTest14 extends EthiopicChronology_ESTest_scaffolding {
+import static org.junit.Assert.assertTrue;
 
-    @Test(timeout = 4000)
-    public void test13() throws Throwable {
-        DateTimeZone dateTimeZone0 = DateTimeZone.forOffsetMillis((-833));
-        EthiopicChronology ethiopicChronology0 = EthiopicChronology.getInstance(dateTimeZone0);
-        boolean boolean0 = ethiopicChronology0.isLeapDay(3461702400000L);
-        assertTrue(boolean0);
+/**
+ * A more understandable test for the {@link EthiopicChronology} class.
+ */
+public class EthiopicChronologyTest {
+
+    /**
+     * Tests that isLeapDay() correctly identifies the 6th day of the 13th month
+     * in an Ethiopic leap year.
+     */
+    @Test
+    public void isLeapDay_shouldReturnTrue_forLeapDayInEthiopicLeapYear() {
+        // Arrange:
+        // In the Ethiopic calendar, a leap year occurs when (year % 4) == 3.
+        // The year 2071 is a leap year because 2071 % 4 = 3.
+        // The leap day is the 6th day of the 13th month (Pagume).
+        final EthiopicChronology ethiopicChronology = EthiopicChronology.getInstanceUTC();
+        
+        // Construct the specific date for the leap day instead of using a magic number.
+        final DateTime ethiopicLeapDay = new DateTime(2071, 13, 6, 0, 0, ethiopicChronology);
+        final long leapDayInstant = ethiopicLeapDay.getMillis();
+
+        // Act:
+        // Check if the calculated instant is considered a leap day.
+        boolean isLeapDay = ethiopicChronology.isLeapDay(leapDayInstant);
+
+        // Assert:
+        // The result should be true for this specific day.
+        assertTrue("The 6th day of the 13th month in Ethiopic year 2071 should be a leap day.", isLeapDay);
     }
 }
