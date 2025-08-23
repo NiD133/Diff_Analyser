520a2b50efd0ce3857ce6daf3d82c1feeaecfbb8
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:59:43 +0800

    Original processedAutoTestCases labelblock_estest36.java

---
+++ labelblock_estest36.java
@@ -0,0 +1,37 @@
+package org.jfree.chart.block;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.Paint;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.SystemColor;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.text.AttributedCharacterIterator;
+import java.util.Hashtable;
+import javax.swing.text.DefaultStyledDocument;
+import javax.swing.text.StyleContext;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.jfree.chart.api.RectangleAnchor;
+import org.jfree.chart.text.TextBlockAnchor;
+import org.jfree.data.Range;
+import org.junit.runner.RunWith;
+
+public class LabelBlock_ESTestTest36 extends LabelBlock_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test35() throws Throwable {
+        LabelBlock labelBlock0 = new LabelBlock("");
+        LabelBlock labelBlock1 = (LabelBlock) labelBlock0.clone();
+        assertEquals(0.0, labelBlock1.getContentXOffset(), 0.01);
+    }
+}
