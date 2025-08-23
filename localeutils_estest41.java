3cc33cf830a29e49978e6a2433c0bbc9146ebc51
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:01:08 +0800

    Original processedAutoTestCases localeutils_estest41.java

---
+++ localeutils_estest41.java
@@ -0,0 +1,20 @@
+package org.apache.commons.lang3;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.List;
+import java.util.Locale;
+import java.util.Set;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class LocaleUtils_ESTestTest41 extends LocaleUtils_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test40() throws Throwable {
+        Set<Locale> set0 = LocaleUtils.availableLocaleSet();
+        assertFalse(set0.isEmpty());
+    }
+}
