75ed0396b34fe58a84529e15e5caf10ceaccccae
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:12:48 +0800

    Original processedAutoTestCases scriptrunner_estest7.java

---
+++ scriptrunner_estest7.java
@@ -0,0 +1,56 @@
+package org.apache.ibatis.jdbc;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.PrintWriter;
+import java.io.Reader;
+import java.io.StringReader;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLWarning;
+import java.sql.Statement;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.testdata.EvoSuiteFile;
+import org.evosuite.runtime.testdata.FileSystemHandling;
+import org.junit.runner.RunWith;
+
+public class ScriptRunner_ESTestTest7 extends ScriptRunner_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test06() throws Throwable {
+        ResultSetMetaData resultSetMetaData0 = mock(ResultSetMetaData.class, new ViolatedAssumptionAnswer());
+        doReturn(245).when(resultSetMetaData0).getColumnCount();
+        doReturn((String) null, (String) null, (String) null, (String) null, (String) null).when(resultSetMetaData0).getColumnLabel(anyInt());
+        ResultSet resultSet0 = mock(ResultSet.class, new ViolatedAssumptionAnswer());
+        doReturn(resultSetMetaData0).when(resultSet0).getMetaData();
+        doReturn(false).when(resultSet0).next();
+        Statement statement0 = mock(Statement.class, new ViolatedAssumptionAnswer());
+        doReturn(true).when(statement0).execute(anyString());
+        doReturn(false, true).when(statement0).getMoreResults();
+        doReturn(resultSet0, (ResultSet) null).when(statement0).getResultSet();
+        doReturn(245).when(statement0).getUpdateCount();
+        Connection connection0 = mock(Connection.class, new ViolatedAssumptionAnswer());
+        doReturn(statement0).when(connection0).createStatement();
+        doReturn(true, true).when(connection0).getAutoCommit();
+        ScriptRunner scriptRunner0 = new ScriptRunner(connection0);
+        scriptRunner0.setSendFullScript(true);
+        scriptRunner0.setLogWriter((PrintWriter) null);
+        StringReader stringReader0 = new StringReader("UVo%w/2W`*&");
+        // Undeclared exception!
+        try {
+            scriptRunner0.runScript(stringReader0);
+            fail("Expecting exception: RuntimeException");
+        } catch (RuntimeException e) {
+            //
+            // Error executing: UVo%w/2W`*&
+            // .  Cause: java.lang.NullPointerException
+            //
+            verifyException("org.apache.ibatis.jdbc.ScriptRunner", e);
+        }
+    }
+}
