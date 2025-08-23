353a3f3914306b9fc0889b45546693a2e19ddffc
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:29:06 +0800

    gemini processedAutoTestCases xnode_estest63.java

--- xnode_estest63.java
+++ xnode_estest63.java
@@ -1,38 +1,31 @@
 package org.apache.ibatis.parsing;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.List;
-import java.util.Locale;
-import java.util.Properties;
-import java.util.function.Supplier;
 import javax.imageio.metadata.IIOMetadataNode;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
-import org.w3c.dom.Document;
+import java.util.Properties;
 import org.w3c.dom.Node;
-import org.xml.sax.ext.DefaultHandler2;
 
-public class XNode_ESTestTest63 extends XNode_ESTest_scaffolding {
+/**
+ * Test suite for the {@link XNode} class.
+ */
+public class XNodeTest {
 
-    @Test(timeout = 4000)
-    public void test062() throws Throwable {
-        IIOMetadataNode iIOMetadataNode0 = new IIOMetadataNode();
-        Properties properties0 = new Properties();
-        XNode xNode0 = new XNode((XPathParser) null, iIOMetadataNode0, properties0);
-        // Undeclared exception!
-        try {
-            xNode0.evalNode("qY1AVLeE|y?n8Y<");
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.ibatis.parsing.XNode", e);
-        }
+    /**
+     * Verifies that calling an evaluation method (e.g., evalNode) on an XNode
+     * instance created with a null XPathParser results in a NullPointerException.
+     * This is an important contract check for the constructor.
+     */
+    @Test(expected = NullPointerException.class)
+    public void shouldThrowNullPointerExceptionWhenEvaluatingNodeWithNullParser() {
+        // Arrange: Create an XNode with a null XPathParser.
+        // This is the specific condition we want to test.
+        Node dummyNode = new IIOMetadataNode();
+        Properties emptyVariables = new Properties();
+        XNode xNode = new XNode(null, dummyNode, emptyVariables);
+
+        // Act & Assert: Attempt to evaluate an expression.
+        // This action is expected to throw a NullPointerException because the internal
+        // XPathParser, which is required for evaluation, is null.
+        xNode.evalNode("/any/expression");
     }
 }
