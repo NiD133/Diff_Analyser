ccd45680367339300b7a13c4c6d4abb1460616f2
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:15:11 +0800

    Original processedAutoTestCases serializedstring_estest38.java

---
+++ serializedstring_estest38.java
@@ -0,0 +1,31 @@
+package com.fasterxml.jackson.core.io;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PipedOutputStream;
+import java.nio.ByteBuffer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.evosuite.runtime.mock.java.io.MockFileOutputStream;
+import org.evosuite.runtime.mock.java.io.MockPrintStream;
+import org.junit.runner.RunWith;
+
+public class SerializedString_ESTestTest38 extends SerializedString_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test37() throws Throwable {
+        SerializedString serializedString0 = new SerializedString("");
+        byte[] byteArray0 = new byte[0];
+        MockPrintStream mockPrintStream0 = new MockPrintStream("I?4|?BKE|");
+        int int0 = serializedString0.appendUnquotedUTF8(byteArray0, 1);
+        assertEquals((-1), int0);
+        int int1 = serializedString0.writeUnquotedUTF8(mockPrintStream0);
+        assertEquals(0, int1);
+    }
+}
