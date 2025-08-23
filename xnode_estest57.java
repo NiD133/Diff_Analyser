218fe7e6d445d0a06353da0edfcb6505810e1e32
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:29:03 +0800

    gemini processedAutoTestCases xnode_estest57.java

--- xnode_estest57.java
+++ xnode_estest57.java
@@ -1,37 +1,29 @@
 package org.apache.ibatis.parsing;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.List;
-import java.util.Locale;
 import java.util.Properties;
-import java.util.function.Supplier;
 import javax.imageio.metadata.IIOMetadataNode;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
-import org.w3c.dom.Document;
+import org.junit.Test;
 import org.w3c.dom.Node;
-import org.xml.sax.ext.DefaultHandler2;
 
-public class XNode_ESTestTest57 extends XNode_ESTest_scaffolding {
+/**
+ * Test suite for the {@link XNode} class, focusing on attribute parsing behavior.
+ */
+public class XNodeTest {
 
-    @Test(timeout = 4000)
-    public void test056() throws Throwable {
-        Properties properties0 = new Properties();
-        IIOMetadataNode iIOMetadataNode0 = new IIOMetadataNode();
-        XNode xNode0 = new XNode((XPathParser) null, iIOMetadataNode0, properties0);
-        // Undeclared exception!
-        try {
-            xNode0.getDoubleAttribute((String) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-        }
+    /**
+     * Verifies that getDoubleAttribute throws a NullPointerException
+     * when the attribute name provided is null.
+     */
+    @Test(expected = NullPointerException.class)
+    public void getDoubleAttributeShouldThrowNullPointerExceptionWhenNameIsNull() {
+        // Arrange: Create an XNode with a basic DOM node and empty properties.
+        // The XPathParser is not needed for this attribute-related test.
+        Node domNode = new IIOMetadataNode();
+        Properties variables = new Properties();
+        XNode xNode = new XNode(null, domNode, variables);
+
+        // Act: Call the method under test with a null argument.
+        // The @Test(expected=...) annotation will assert that the correct exception is thrown.
+        xNode.getDoubleAttribute(null);
     }
 }
