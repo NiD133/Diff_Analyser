bc74a8dd3c858152e26ee63278956ce17da2caf0
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:24:53 +0800

    Original processedAutoTestCases urlcodec_estest25.java

---
+++ urlcodec_estest25.java
@@ -0,0 +1,29 @@
+package org.apache.commons.codec.net;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.UnsupportedEncodingException;
+import java.util.BitSet;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class URLCodec_ESTestTest25 extends URLCodec_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test24() throws Throwable {
+        URLCodec uRLCodec0 = new URLCodec();
+        byte[] byteArray0 = new byte[2];
+        byteArray0[0] = (byte) 37;
+        try {
+            uRLCodec0.decode(byteArray0);
+            fail("Expecting exception: Exception");
+        } catch (Exception e) {
+            //
+            // Invalid URL encoding: not a valid digit (radix 16): 0
+            //
+            verifyException("org.apache.commons.codec.net.Utils", e);
+        }
+    }
+}
