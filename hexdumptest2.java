f213b954ca936f9a20a86678ca7b3dcce581cc60
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:29:19 +0800

    Original processedManualTestCases hexdumptest2.java

---
+++ hexdumptest2.java
@@ -0,0 +1,168 @@
+package org.apache.commons.io;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.io.IOException;
+import org.apache.commons.io.output.ByteArrayOutputStream;
+import org.apache.commons.io.test.ThrowOnCloseOutputStream;
+import org.junit.jupiter.api.Test;
+
+public class HexDumpTestTest2 {
+
+    private char toAscii(final int c) {
+        char rval = '.';
+        if (c >= 32 && c <= 126) {
+            rval = (char) c;
+        }
+        return rval;
+    }
+
+    private char toHex(final int n) {
+        final char[] hexChars = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+        return hexChars[n % 16];
+    }
+
+    @Test
+    void testDumpOutputStream() throws IOException {
+        final byte[] testArray = new byte[256];
+        for (int j = 0; j < 256; j++) {
+            testArray[j] = (byte) j;
+        }
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        HexDump.dump(testArray, 0, stream, 0);
+        byte[] outputArray = new byte[16 * (73 + System.lineSeparator().length())];
+        for (int j = 0; j < 16; j++) {
+            int offset = (73 + System.lineSeparator().length()) * j;
+            outputArray[offset++] = (byte) '0';
+            outputArray[offset++] = (byte) '0';
+            outputArray[offset++] = (byte) '0';
+            outputArray[offset++] = (byte) '0';
+            outputArray[offset++] = (byte) '0';
+            outputArray[offset++] = (byte) '0';
+            outputArray[offset++] = (byte) toHex(j);
+            outputArray[offset++] = (byte) '0';
+            outputArray[offset++] = (byte) ' ';
+            for (int k = 0; k < 16; k++) {
+                outputArray[offset++] = (byte) toHex(j);
+                outputArray[offset++] = (byte) toHex(k);
+                outputArray[offset++] = (byte) ' ';
+            }
+            for (int k = 0; k < 16; k++) {
+                outputArray[offset++] = (byte) toAscii(j * 16 + k);
+            }
+            System.arraycopy(System.lineSeparator().getBytes(), 0, outputArray, offset, System.lineSeparator().getBytes().length);
+        }
+        byte[] actualOutput = stream.toByteArray();
+        assertEquals(outputArray.length, actualOutput.length, "array size mismatch");
+        for (int j = 0; j < outputArray.length; j++) {
+            assertEquals(outputArray[j], actualOutput[j], "array[ " + j + "] mismatch");
+        }
+        // verify proper behavior with non-zero offset
+        stream = new ByteArrayOutputStream();
+        HexDump.dump(testArray, 0x10000000, stream, 0);
+        outputArray = new byte[16 * (73 + System.lineSeparator().length())];
+        for (int j = 0; j < 16; j++) {
+            int offset = (73 + System.lineSeparator().length()) * j;
+            outputArray[offset++] = (byte) '1';
+            outputArray[offset++] = (byte) '0';
+            outputArray[offset++] = (byte) '0';
+            outputArray[offset++] = (byte) '0';
+            outputArray[offset++] = (byte) '0';
+            outputArray[offset++] = (byte) '0';
+            outputArray[offset++] = (byte) toHex(j);
+            outputArray[offset++] = (byte) '0';
+            outputArray[offset++] = (byte) ' ';
+            for (int k = 0; k < 16; k++) {
+                outputArray[offset++] = (byte) toHex(j);
+                outputArray[offset++] = (byte) toHex(k);
+                outputArray[offset++] = (byte) ' ';
+            }
+            for (int k = 0; k < 16; k++) {
+                outputArray[offset++] = (byte) toAscii(j * 16 + k);
+            }
+            System.arraycopy(System.lineSeparator().getBytes(), 0, outputArray, offset, System.lineSeparator().getBytes().length);
+        }
+        actualOutput = stream.toByteArray();
+        assertEquals(outputArray.length, actualOutput.length, "array size mismatch");
+        for (int j = 0; j < outputArray.length; j++) {
+            assertEquals(outputArray[j], actualOutput[j], "array[ " + j + "] mismatch");
+        }
+        // verify proper behavior with negative offset
+        stream = new ByteArrayOutputStream();
+        HexDump.dump(testArray, 0xFF000000, stream, 0);
+        outputArray = new byte[16 * (73 + System.lineSeparator().length())];
+        for (int j = 0; j < 16; j++) {
+            int offset = (73 + System.lineSeparator().length()) * j;
+            outputArray[offset++] = (byte) 'F';
+            outputArray[offset++] = (byte) 'F';
+            outputArray[offset++] = (byte) '0';
+            outputArray[offset++] = (byte) '0';
+            outputArray[offset++] = (byte) '0';
+            outputArray[offset++] = (byte) '0';
+            outputArray[offset++] = (byte) toHex(j);
+            outputArray[offset++] = (byte) '0';
+            outputArray[offset++] = (byte) ' ';
+            for (int k = 0; k < 16; k++) {
+                outputArray[offset++] = (byte) toHex(j);
+                outputArray[offset++] = (byte) toHex(k);
+                outputArray[offset++] = (byte) ' ';
+            }
+            for (int k = 0; k < 16; k++) {
+                outputArray[offset++] = (byte) toAscii(j * 16 + k);
+            }
+            System.arraycopy(System.lineSeparator().getBytes(), 0, outputArray, offset, System.lineSeparator().getBytes().length);
+        }
+        actualOutput = stream.toByteArray();
+        assertEquals(outputArray.length, actualOutput.length, "array size mismatch");
+        for (int j = 0; j < outputArray.length; j++) {
+            assertEquals(outputArray[j], actualOutput[j], "array[ " + j + "] mismatch");
+        }
+        // verify proper behavior with non-zero index
+        stream = new ByteArrayOutputStream();
+        HexDump.dump(testArray, 0x10000000, stream, 0x81);
+        outputArray = new byte[8 * (73 + System.lineSeparator().length()) - 1];
+        for (int j = 0; j < 8; j++) {
+            int offset = (73 + System.lineSeparator().length()) * j;
+            outputArray[offset++] = (byte) '1';
+            outputArray[offset++] = (byte) '0';
+            outputArray[offset++] = (byte) '0';
+            outputArray[offset++] = (byte) '0';
+            outputArray[offset++] = (byte) '0';
+            outputArray[offset++] = (byte) '0';
+            outputArray[offset++] = (byte) toHex(j + 8);
+            outputArray[offset++] = (byte) '1';
+            outputArray[offset++] = (byte) ' ';
+            for (int k = 0; k < 16; k++) {
+                final int index = 0x81 + j * 16 + k;
+                if (index < 0x100) {
+                    outputArray[offset++] = (byte) toHex(index / 16);
+                    outputArray[offset++] = (byte) toHex(index);
+                } else {
+                    outputArray[offset++] = (byte) ' ';
+                    outputArray[offset++] = (byte) ' ';
+                }
+                outputArray[offset++] = (byte) ' ';
+            }
+            for (int k = 0; k < 16; k++) {
+                final int index = 0x81 + j * 16 + k;
+                if (index < 0x100) {
+                    outputArray[offset++] = (byte) toAscii(index);
+                }
+            }
+            System.arraycopy(System.lineSeparator().getBytes(), 0, outputArray, offset, System.lineSeparator().getBytes().length);
+        }
+        actualOutput = stream.toByteArray();
+        assertEquals(outputArray.length, actualOutput.length, "array size mismatch");
+        for (int j = 0; j < outputArray.length; j++) {
+            assertEquals(outputArray[j], actualOutput[j], "array[ " + j + "] mismatch");
+        }
+        // verify proper behavior with negative index
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> HexDump.dump(testArray, 0x10000000, new ByteArrayOutputStream(), -1));
+        // verify proper behavior with index that is too large
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> HexDump.dump(testArray, 0x10000000, new ByteArrayOutputStream(), testArray.length));
+        // verify proper behavior with null stream
+        assertThrows(NullPointerException.class, () -> HexDump.dump(testArray, 0x10000000, null, 0));
+        // verify output stream is not closed by the dump method
+        HexDump.dump(testArray, 0, new ThrowOnCloseOutputStream(new ByteArrayOutputStream()), 0);
+    }
+}
