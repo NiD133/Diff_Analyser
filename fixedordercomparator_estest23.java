909856f79355dbb9b6920be5ee01d5609ae6c0fa
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:51:25 +0800

    Original processedAutoTestCases fixedordercomparator_estest23.java

---
+++ fixedordercomparator_estest23.java
@@ -0,0 +1,28 @@
+package org.apache.commons.collections4.comparators;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.lang.reflect.Array;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Function;
+import org.apache.commons.collections4.Predicate;
+import org.apache.commons.collections4.functors.ConstantTransformer;
+import org.apache.commons.collections4.functors.IdentityPredicate;
+import org.apache.commons.collections4.functors.InstanceofPredicate;
+import org.apache.commons.collections4.functors.PredicateTransformer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class FixedOrderComparator_ESTestTest23 extends FixedOrderComparator_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test22() throws Throwable {
+        Class<Object> class0 = Object.class;
+        FixedOrderComparator<FixedOrderComparator.UnknownObjectBehavior> fixedOrderComparator0 = new FixedOrderComparator<FixedOrderComparator.UnknownObjectBehavior>();
+        boolean boolean0 = fixedOrderComparator0.equals(class0);
+        assertFalse(boolean0);
+    }
+}
