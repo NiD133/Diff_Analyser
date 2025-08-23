8724978609d4b0072f66b777c32c08360142d8cc
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:34:19 +0800

    gemini processedManualTestCases stringutiltest6.java

--- stringutiltest6.java
+++ stringutiltest6.java
@@ -1,24 +1,38 @@
 package org.jsoup.internal;
 
-import org.jsoup.Jsoup;
-import org.junit.jupiter.api.Test;
-import java.util.Arrays;
-import java.util.Collections;
-import static org.jsoup.internal.StringUtil.normaliseWhitespace;
-import static org.jsoup.internal.StringUtil.resolve;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-public class StringUtilTestTest6 {
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-    @Test
-    public void isWhitespace() {
-        assertTrue(StringUtil.isWhitespace('\t'));
-        assertTrue(StringUtil.isWhitespace('\n'));
-        assertTrue(StringUtil.isWhitespace('\r'));
-        assertTrue(StringUtil.isWhitespace('\f'));
-        assertTrue(StringUtil.isWhitespace(' '));
-        assertFalse(StringUtil.isWhitespace('\u00a0'));
-        assertFalse(StringUtil.isWhitespace('\u2000'));
-        assertFalse(StringUtil.isWhitespace('\u3000'));
+/**
+ * Tests for the StringUtil class, focusing on the isWhitespace() method.
+ */
+// The class was renamed from StringUtilTestTest6 for clarity and convention.
+public class StringUtilTest {
+
+    @DisplayName("isWhitespace() should return true for characters defined as whitespace in the HTML spec")
+    @ParameterizedTest(name = "Recognizes character ''{0}'' as whitespace")
+    @ValueSource(chars = {' ', '\t', '\n', '\f', '\r'})
+    void isWhitespace_ReturnsTrue_ForHtmlSpecWhitespace(char c) {
+        // According to the StringUtil#isWhitespace Javadoc, this method checks for
+        // whitespace as defined by the HTML spec.
+        assertTrue(StringUtil.isWhitespace(c));
+    }
+
+    @DisplayName("isWhitespace() should return false for other space-like, non-HTML-spec characters")
+    @ParameterizedTest(name = "Rejects character ''{0}'' as whitespace")
+    @ValueSource(chars = {
+        '\u00a0', // non-breaking space
+        '\u2000', // en quad
+        '\u3000'  // ideographic space
+    })
+    void isWhitespace_ReturnsFalse_ForNonHtmlSpecWhitespace(char c) {
+        // These characters are visually similar to spaces but are not considered
+        // whitespace by the HTML spec definition, which this method implements.
+        // The method isActuallyWhitespace() handles these cases differently.
+        assertFalse(StringUtil.isWhitespace(c));
     }
 }
