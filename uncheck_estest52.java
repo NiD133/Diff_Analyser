aa32d61209998ba84a452c215e1ec9059f654650
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:24:43 +0800

    Original processedAutoTestCases uncheck_estest52.java

---
+++ uncheck_estest52.java
@@ -0,0 +1,31 @@
+package org.apache.commons.io.function;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.lang.reflect.Array;
+import java.time.chrono.HijrahEra;
+import java.util.Comparator;
+import java.util.concurrent.ForkJoinTask;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+import java.util.stream.LongStream;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class Uncheck_ESTestTest52 extends Uncheck_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test51() throws Throwable {
+        IOSupplier<ForkJoinTask<String>> iOSupplier0 = (IOSupplier<ForkJoinTask<String>>) mock(IOSupplier.class, new ViolatedAssumptionAnswer());
+        doReturn((Object) null).when(iOSupplier0).get();
+        ForkJoinTask<String> forkJoinTask0 = Uncheck.get(iOSupplier0);
+        assertNull(forkJoinTask0);
+    }
+}
