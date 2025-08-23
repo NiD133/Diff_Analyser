1c1ac9d5ba578d687e615577150beb4e972d0155
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:49:27 +0800

    gemini processedAutoTestCases entities_estest30.java

--- entities_estest30.java
+++ entities_estest30.java
@@ -1,28 +1,25 @@
 package org.jsoup.nodes;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.File;
-import java.io.PipedWriter;
-import java.io.StringWriter;
-import java.nio.BufferOverflowException;
-import java.nio.CharBuffer;
-import java.nio.ReadOnlyBufferException;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.io.MockFile;
-import org.evosuite.runtime.mock.java.io.MockFileWriter;
-import org.evosuite.runtime.mock.java.io.MockPrintStream;
-import org.jsoup.internal.QuietAppendable;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class Entities_ESTestTest30 extends Entities_ESTest_scaffolding {
+/**
+ * Tests for the {@link Entities#escape(String, Document.OutputSettings)} method.
+ */
+public class EntitiesEscapeTest {
 
-    @Test(timeout = 4000)
-    public void test29() throws Throwable {
-        Document.OutputSettings document_OutputSettings0 = new Document.OutputSettings();
-        String string0 = Entities.escape("yen\u00A0", document_OutputSettings0);
-        assertEquals("yen&nbsp;", string0);
+    @Test
+    public void escapeShouldConvertNonBreakingSpaceToNbspEntity() {
+        // Arrange
+        // The input string contains "yen" followed by a non-breaking space character (\u00A0).
+        String inputWithNonBreakingSpace = "yen\u00A0";
+        String expectedOutput = "yen&nbsp;";
+        Document.OutputSettings outputSettings = new Document.OutputSettings();
+
+        // Act
+        String actualOutput = Entities.escape(inputWithNonBreakingSpace, outputSettings);
+
+        // Assert
+        assertEquals(expectedOutput, actualOutput);
     }
 }
