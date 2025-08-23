98ef8111e5a068a3cd0f9de85be998c3e6585783
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:33:52 +0800

    gemini processedManualTestCases soundextest13.java

--- soundextest13.java
+++ soundextest13.java
@@ -1,13 +1,17 @@
 package org.apache.commons.codec.language;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.apache.commons.codec.AbstractStringEncoderTest;
 import org.apache.commons.codec.EncoderException;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-public class SoundexTestTest13 extends AbstractStringEncoderTest<Soundex> {
+/**
+ * Contains specific test cases for the {@link Soundex} encoder, focusing on its unique rules.
+ */
+class SoundexTest extends AbstractStringEncoderTest<Soundex> {
 
     @Override
     protected Soundex createStringEncoder() {
@@ -15,13 +19,31 @@ public class SoundexTestTest13 extends AbstractStringEncoderTest<Soundex> {
     }
 
     /**
-     * Consonants from the same code group separated by W or H are treated as one.
+     * Tests the Soundex rule where consonants from the same code group separated by 'H' or 'W'
+     * are treated as a single unit. For example, in "BOOTHDAVIS", 'T' and 'D' share code '3'
+     * and are separated by 'H', so they are encoded as a single '3'.
      *
-     * Test data from http://www.myatt.demon.co.uk/sxalg.htm
+     * <p>Test data from http://www.myatt.demon.co.uk/sxalg.htm</p>
+     *
+     * @param name The input string to be encoded.
      */
-    @Test
-    void testHWRuleEx2() {
-        assertEquals("B312", getStringEncoder().encode("BOOTHDAVIS"));
-        assertEquals("B312", getStringEncoder().encode("BOOTH-DAVIS"));
+    @DisplayName("Consonants with the same code separated by 'H' should be encoded as a single code")
+    @ParameterizedTest
+    @ValueSource(strings = {"BOOTHDAVIS", "BOOTH-DAVIS"})
+    void shouldTreatConsonantsSeparatedByHAsSingleUnit(final String name) throws EncoderException {
+        // The expected Soundex code for both "BOOTHDAVIS" and "BOOTH-DAVIS" is B312.
+        // B -> B (first letter)
+        // O, O -> Vowels, ignored
+        // T -> 3
+        // H -> Separator, ignored
+        // D -> 3 (same code as previous consonant 'T', so 'D' is dropped)
+        // A -> Vowel, ignored
+        // V -> 1
+        // I -> Vowel, ignored
+        // S -> 2
+        // Final code: B312
+        final String expectedCode = "B312";
+
+        assertEquals(expectedCode, getStringEncoder().encode(name));
     }
 }
