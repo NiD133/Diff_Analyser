a0b3c14ae6358952f8c3d140100bc18c109def69
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:19:41 +0800

    Original processedAutoTestCases symmetry010chronology_estest55.java

---
+++ symmetry010chronology_estest55.java
@@ -0,0 +1,52 @@
+package org.threeten.extra.chrono;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.time.Clock;
+import java.time.DateTimeException;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.time.Year;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.chrono.ChronoLocalDateTime;
+import java.time.chrono.ChronoZonedDateTime;
+import java.time.chrono.Era;
+import java.time.chrono.IsoEra;
+import java.time.chrono.JapaneseEra;
+import java.time.chrono.ThaiBuddhistEra;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAccessor;
+import java.time.temporal.UnsupportedTemporalTypeException;
+import java.time.temporal.ValueRange;
+import java.util.List;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.time.MockClock;
+import org.evosuite.runtime.mock.java.time.MockInstant;
+import org.evosuite.runtime.mock.java.time.MockLocalDate;
+import org.evosuite.runtime.mock.java.time.MockOffsetDateTime;
+import org.evosuite.runtime.mock.java.time.MockYear;
+import org.junit.runner.RunWith;
+
+public class Symmetry010Chronology_ESTestTest55 extends Symmetry010Chronology_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test54() throws Throwable {
+        Symmetry010Chronology symmetry010Chronology0 = new Symmetry010Chronology();
+        // Undeclared exception!
+        try {
+            symmetry010Chronology0.date(5, 5, 35);
+            fail("Expecting exception: DateTimeException");
+        } catch (DateTimeException e) {
+            //
+            // Invalid date: 5/5/35
+            //
+            verifyException("org.threeten.extra.chrono.Symmetry010Date", e);
+        }
+    }
+}
