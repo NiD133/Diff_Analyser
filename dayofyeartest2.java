80a7d7c677a834107130f46c35c1c062a79c9a49
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:27:45 +0800

    gemini processedManualTestCases dayofyeartest2.java

--- dayofyeartest2.java
+++ dayofyeartest2.java
@@ -1,143 +1,33 @@
 package org.threeten.extra;
 
-import static java.time.temporal.ChronoField.ALIGNED_DAY_OF_WEEK_IN_MONTH;
-import static java.time.temporal.ChronoField.ALIGNED_DAY_OF_WEEK_IN_YEAR;
-import static java.time.temporal.ChronoField.ALIGNED_WEEK_OF_MONTH;
-import static java.time.temporal.ChronoField.ALIGNED_WEEK_OF_YEAR;
-import static java.time.temporal.ChronoField.AMPM_OF_DAY;
-import static java.time.temporal.ChronoField.CLOCK_HOUR_OF_AMPM;
-import static java.time.temporal.ChronoField.CLOCK_HOUR_OF_DAY;
-import static java.time.temporal.ChronoField.DAY_OF_MONTH;
-import static java.time.temporal.ChronoField.DAY_OF_WEEK;
-import static java.time.temporal.ChronoField.DAY_OF_YEAR;
-import static java.time.temporal.ChronoField.EPOCH_DAY;
-import static java.time.temporal.ChronoField.ERA;
-import static java.time.temporal.ChronoField.HOUR_OF_AMPM;
-import static java.time.temporal.ChronoField.HOUR_OF_DAY;
-import static java.time.temporal.ChronoField.INSTANT_SECONDS;
-import static java.time.temporal.ChronoField.MICRO_OF_DAY;
-import static java.time.temporal.ChronoField.MICRO_OF_SECOND;
-import static java.time.temporal.ChronoField.MILLI_OF_DAY;
-import static java.time.temporal.ChronoField.MILLI_OF_SECOND;
-import static java.time.temporal.ChronoField.MINUTE_OF_DAY;
-import static java.time.temporal.ChronoField.MINUTE_OF_HOUR;
-import static java.time.temporal.ChronoField.MONTH_OF_YEAR;
-import static java.time.temporal.ChronoField.NANO_OF_DAY;
-import static java.time.temporal.ChronoField.NANO_OF_SECOND;
-import static java.time.temporal.ChronoField.OFFSET_SECONDS;
-import static java.time.temporal.ChronoField.PROLEPTIC_MONTH;
-import static java.time.temporal.ChronoField.SECOND_OF_DAY;
-import static java.time.temporal.ChronoField.SECOND_OF_MINUTE;
-import static java.time.temporal.ChronoField.YEAR;
-import static java.time.temporal.ChronoField.YEAR_OF_ERA;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertSame;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
-import java.time.Clock;
-import java.time.DateTimeException;
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.Year;
-import java.time.ZoneId;
-import java.time.chrono.IsoChronology;
-import java.time.chrono.JapaneseDate;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
-import java.time.temporal.IsoFields;
-import java.time.temporal.Temporal;
-import java.time.temporal.TemporalAccessor;
-import java.time.temporal.TemporalAdjuster;
-import java.time.temporal.TemporalField;
-import java.time.temporal.TemporalQueries;
-import java.time.temporal.TemporalUnit;
-import java.time.temporal.UnsupportedTemporalTypeException;
+import org.junit.Test;
+import java.time.temporal.ChronoField;
 import java.time.temporal.ValueRange;
-import org.junit.jupiter.api.Test;
-import org.junitpioneer.jupiter.RetryingTest;
-import com.google.common.testing.EqualsTester;
+import static org.junit.Assert.assertEquals;
 
-public class DayOfYearTestTest2 {
+/**
+ * This test suite is for the {@link DayOfYear} class.
+ * It focuses on improving the understandability of tests related to the range of temporal fields.
+ */
+public class DayOfYearImprovedTest {
 
-    private static final Year YEAR_STANDARD = Year.of(2007);
-
-    private static final Year YEAR_LEAP = Year.of(2008);
-
-    private static final int STANDARD_YEAR_LENGTH = 365;
-
-    private static final int LEAP_YEAR_LENGTH = 366;
-
-    private static final DayOfYear TEST = DayOfYear.of(12);
-
-    private static final ZoneId PARIS = ZoneId.of("Europe/Paris");
-
-    private static class TestingField implements TemporalField {
-
-        public static final TestingField INSTANCE = new TestingField();
-
-        @Override
-        public TemporalUnit getBaseUnit() {
-            return ChronoUnit.DAYS;
-        }
-
-        @Override
-        public TemporalUnit getRangeUnit() {
-            return ChronoUnit.YEARS;
-        }
-
-        @Override
-        public ValueRange range() {
-            return ValueRange.of(1, 365, 366);
-        }
-
-        @Override
-        public boolean isDateBased() {
-            return true;
-        }
-
-        @Override
-        public boolean isTimeBased() {
-            return false;
-        }
-
-        @Override
-        public boolean isSupportedBy(TemporalAccessor temporal) {
-            return temporal.isSupported(DAY_OF_YEAR);
-        }
-
-        @Override
-        public ValueRange rangeRefinedBy(TemporalAccessor temporal) {
-            return range();
-        }
-
-        @Override
-        public long getFrom(TemporalAccessor temporal) {
-            return temporal.getLong(DAY_OF_YEAR);
-        }
-
-        @Override
-        @SuppressWarnings("unchecked")
-        public <R extends Temporal> R adjustInto(R temporal, long newValue) {
-            return (R) temporal.with(DAY_OF_YEAR, newValue);
-        }
-    }
-
+    /**
+     * Tests that the {@code range(TemporalField)} method returns the correct
+     * value range for the {@code DAY_OF_YEAR} field.
+     * The valid range for DAY_OF_YEAR is from 1 to 366, inclusive,
+     * as DayOfYear can represent days in a leap year.
+     */
     @Test
-    public void test_serialization() throws IOException, ClassNotFoundException {
-        DayOfYear test = DayOfYear.of(1);
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        try (ObjectOutputStream oos = new ObjectOutputStream(baos)) {
-            oos.writeObject(test);
-        }
-        try (ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(baos.toByteArray()))) {
-            assertSame(test, ois.readObject());
-        }
+    public void testRangeForDayOfYearFieldReturnsCorrectRange() {
+        // Arrange: Create a DayOfYear instance and define the expected range.
+        // The specific day (e.g., 45) does not affect the range of the DAY_OF_YEAR field.
+        DayOfYear dayOfYear = DayOfYear.of(45);
+        ValueRange expectedRange = ValueRange.of(1, 366);
+
+        // Act: Get the actual range for the DAY_OF_YEAR field.
+        ValueRange actualRange = dayOfYear.range(ChronoField.DAY_OF_YEAR);
+
+        // Assert: Verify that the actual range matches the expected range.
+        assertEquals("The range for DAY_OF_YEAR should be 1-366", expectedRange, actualRange);
     }
 }
