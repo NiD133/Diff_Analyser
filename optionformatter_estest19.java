df7c04b3f11ed1164219301786bf63b2114eb329
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:06:23 +0800

    Original processedAutoTestCases optionformatter_estest19.java

---
+++ optionformatter_estest19.java
@@ -0,0 +1,22 @@
+package org.apache.commons.cli.help;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.function.BiFunction;
+import org.apache.commons.cli.Option;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class OptionFormatter_ESTestTest19 extends OptionFormatter_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test18() throws Throwable {
+        OptionFormatter optionFormatter0 = OptionFormatter.from((Option) null);
+        String string0 = optionFormatter0.toOptional(", ");
+        assertEquals("[, ]", string0);
+    }
+}
