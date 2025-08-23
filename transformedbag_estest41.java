485d8ff24dedd2e80c00f4941e0886308a498a54
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:23:07 +0800

    gemini processedAutoTestCases transformedbag_estest41.java

--- transformedbag_estest41.java
+++ transformedbag_estest41.java
@@ -1,59 +1,39 @@
 package org.apache.commons.collections4.bag;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Set;
-import java.util.Vector;
 import org.apache.commons.collections4.Bag;
-import org.apache.commons.collections4.Factory;
-import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.SortedBag;
 import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AndPredicate;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ChainedTransformer;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantFactory;
 import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.FactoryTransformer;
-import org.apache.commons.collections4.functors.FalsePredicate;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.InstantiateFactory;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.MapTransformer;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NotPredicate;
-import org.apache.commons.collections4.functors.OnePredicate;
-import org.apache.commons.collections4.functors.SwitchTransformer;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class TransformedBag_ESTestTest41 extends TransformedBag_ESTest_scaffolding {
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-    @Test(timeout = 4000)
-    public void test40() throws Throwable {
-        Comparator<Object> comparator0 = (Comparator<Object>) mock(Comparator.class, new ViolatedAssumptionAnswer());
-        TreeBag<Object> treeBag0 = new TreeBag<Object>(comparator0);
-        CollectionSortedBag<Object> collectionSortedBag0 = new CollectionSortedBag<Object>(treeBag0);
-        Integer integer0 = new Integer(2);
-        ConstantFactory<Integer> constantFactory0 = new ConstantFactory<Integer>(integer0);
-        Transformer<Object, Integer> transformer0 = FactoryTransformer.factoryTransformer((Factory<? extends Integer>) constantFactory0);
-        TransformedSortedBag<Object> transformedSortedBag0 = new TransformedSortedBag<Object>(collectionSortedBag0, transformer0);
-        Bag<Object> bag0 = TransformedBag.transformingBag((Bag<Object>) transformedSortedBag0, (Transformer<? super Object, ?>) transformer0);
-        assertFalse(bag0.contains(2));
+/**
+ * Contains tests for the static factory method {@link TransformedBag#transformingBag(Bag, Transformer)}.
+ */
+public class TransformedBagCreationTest {
+
+    /**
+     * Tests that creating a transforming bag from an empty source bag results in an empty bag.
+     *
+     * The {@code transformingBag} factory method does not transform existing elements,
+     * so an empty source bag should produce an empty transformed bag.
+     */
+    @Test
+    public void transformingBagFromEmptyBagShouldBeEmpty() {
+        // Arrange
+        final Bag<Object> emptySourceBag = new HashBag<>();
+        final String transformedValue = "always this value";
+        final Transformer<Object, Object> transformer = ConstantTransformer.constantTransformer(transformedValue);
+
+        // Act
+        // Create a TransformedBag using the factory method.
+        final Bag<Object> transformedBag = TransformedBag.transformingBag(emptySourceBag, transformer);
+
+        // Assert
+        // The resulting bag should be empty because the source bag was empty.
+        assertTrue("A transformed bag created from an empty bag should be empty", transformedBag.isEmpty());
+        assertFalse("An empty transformed bag should not contain the value the transformer would produce",
+                transformedBag.contains(transformedValue));
     }
 }
