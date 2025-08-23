6147a8fbfaab2aaae209cc73b38a848f56885e51
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:22:30 +0800

    gemini processedAutoTestCases tokenqueue_estest39.java

--- tokenqueue_estest39.java
+++ tokenqueue_estest39.java
@@ -1,27 +1,24 @@
 package org.jsoup.parser;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class TokenQueue_ESTestTest39 extends TokenQueue_ESTest_scaffolding {
+/**
+ * Test suite for {@link TokenQueue}.
+ */
+public class TokenQueueTest {
 
-    @Test(timeout = 4000)
-    public void test38() throws Throwable {
-        TokenQueue tokenQueue0 = new TokenQueue("@vPEF9_+&M<,U");
-        tokenQueue0.close();
-        // Undeclared exception!
-        try {
-            tokenQueue0.consume("@vPEF9_+&M<,U");
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.jsoup.parser.CharacterReader", e);
-        }
+    /**
+     * Verifies that attempting to use a TokenQueue after it has been closed
+     * results in a NullPointerException.
+     */
+    @Test(expected = NullPointerException.class)
+    public void consumeOnClosedQueueThrowsException() {
+        // Arrange: Create a queue and then close it.
+        TokenQueue queue = new TokenQueue("some data");
+        queue.close();
+
+        // Act & Assert: Attempting to consume from the closed queue should throw.
+        // The @Test(expected=...) annotation handles the assertion.
+        queue.consume("some data");
     }
 }
