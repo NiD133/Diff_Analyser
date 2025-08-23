a82f39ec24ef125339ff39102b570a7cb49c6089
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:12:42 +0800

    gemini processedAutoTestCases rfc1522codec_estest9.java

--- rfc1522codec_estest9.java
+++ rfc1522codec_estest9.java
@@ -1,31 +1,40 @@
 package org.apache.commons.codec.net;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.UnsupportedEncodingException;
-import java.nio.charset.Charset;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
 import java.nio.charset.IllegalCharsetNameException;
-import java.nio.charset.UnsupportedCharsetException;
-import org.apache.commons.codec.CodecPolicy;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class RFC1522Codec_ESTestTest9 extends RFC1522Codec_ESTest_scaffolding {
+/**
+ * Tests for the RFC1522Codec class, focusing on exception handling for invalid inputs.
+ */
+public class RFC1522CodecTest {
 
-    @Test(timeout = 4000)
-    public void test08() throws Throwable {
-        BCodec bCodec0 = new BCodec();
-        // Undeclared exception!
+    /**
+     * Verifies that the encodeText method throws an IllegalCharsetNameException
+     * when provided with a syntactically invalid charset name.
+     */
+    @Test
+    public void encodeTextWithInvalidCharsetNameShouldThrowException() {
+        // Arrange
+        final BCodec codec = new BCodec();
+        final String inputText = "Any text can be used here";
+        // A charset name containing spaces is syntactically invalid.
+        final String invalidCharsetName = "This is an invalid charset name";
+
+        // Act & Assert
         try {
-            bCodec0.encodeText("This codec cannot decode ", "This codec cannot decode ");
-            fail("Expecting exception: IllegalCharsetNameException");
-        } catch (IllegalCharsetNameException e) {
-            //
-            // This codec cannot decode
-            //
-            verifyException("java.nio.charset.Charset", e);
+            codec.encodeText(inputText, invalidCharsetName);
+            fail("Expected an IllegalCharsetNameException to be thrown due to the invalid charset name.");
+        } catch (final IllegalCharsetNameException e) {
+            // This is the expected outcome.
+            // We can optionally assert that the exception message contains the invalid name
+            // to make the test more robust.
+            assertEquals(invalidCharsetName, e.getMessage());
+        } catch (final Exception e) {
+            // Fail the test if any other unexpected exception is thrown.
+            fail("Expected IllegalCharsetNameException, but caught " + e.getClass().getSimpleName());
         }
     }
 }
