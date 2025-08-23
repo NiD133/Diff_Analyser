b37a562a1d2f5b49cd27a00f2467c1ebc0cae90c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:28:00 +0800

    Original processedManualTestCases defaultsplitcharactertest2.java

---
+++ defaultsplitcharactertest2.java
@@ -0,0 +1,18 @@
+package com.itextpdf.text.pdf;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DefaultSplitCharacterTestTest2 {
+
+    private final String[] INPUT_TEXT = new String[] { "tha111-is one that should-be-splitted-right-herel-2018-12-18", "anddate format2 01-01-1920" };
+
+    private boolean isPsplitCharacter(int current, String text) {
+        return new DefaultSplitCharacter().isSplitCharacter(75, current, text.length() + 1, text.toCharArray(), null);
+    }
+
+    @Test
+    public void hypenInsideDateTest() {
+        Assert.assertFalse(isPsplitCharacter(21, INPUT_TEXT[1]));
+    }
+}
