3a5fb766399639ad8a8369bd0a61ea069c8cef6f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:43:08 +0800

    Original processedAutoTestCases charstreams_estest8.java

---
+++ charstreams_estest8.java
@@ -0,0 +1,51 @@
+package com.google.common.io;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.CharArrayReader;
+import java.io.EOFException;
+import java.io.FileDescriptor;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PipedInputStream;
+import java.io.PipedReader;
+import java.io.PipedWriter;
+import java.io.PushbackReader;
+import java.io.Reader;
+import java.io.StringReader;
+import java.io.Writer;
+import java.nio.BufferOverflowException;
+import java.nio.CharBuffer;
+import java.nio.ReadOnlyBufferException;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.MalformedInputException;
+import java.util.List;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.io.MockFileReader;
+import org.evosuite.runtime.mock.java.io.MockFileWriter;
+import org.evosuite.runtime.mock.java.io.MockPrintWriter;
+import org.junit.runner.RunWith;
+
+public class CharStreams_ESTestTest8 extends CharStreams_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test07() throws Throwable {
+        PipedReader pipedReader0 = new PipedReader();
+        try {
+            CharStreams.toString((Readable) pipedReader0);
+            fail("Expecting exception: IOException");
+        } catch (IOException e) {
+            //
+            // Pipe not connected
+            //
+            verifyException("java.io.PipedReader", e);
+        }
+    }
+}
