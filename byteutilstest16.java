d00597fce32929f7d19ef9c16fd89ac6667314e2
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:26:06 +0800

    Original processedManualTestCases byteutilstest16.java

---
+++ byteutilstest16.java
@@ -0,0 +1,27 @@
+package org.apache.commons.compress.utils;
+
+import static org.apache.commons.compress.utils.ByteUtils.fromLittleEndian;
+import static org.apache.commons.compress.utils.ByteUtils.toLittleEndian;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInput;
+import java.io.DataInputStream;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.util.Arrays;
+import org.apache.commons.compress.utils.ByteUtils.InputStreamByteSupplier;
+import org.apache.commons.compress.utils.ByteUtils.OutputStreamByteConsumer;
+import org.junit.jupiter.api.Test;
+
+public class ByteUtilsTestTest16 {
+
+    @Test
+    void testFromLittleEndianFromSupplierThrowsForLengthTooBig() {
+        assertThrows(IllegalArgumentException.class, () -> fromLittleEndian(new InputStreamByteSupplier(new ByteArrayInputStream(ByteUtils.EMPTY_BYTE_ARRAY)), 9));
+    }
+}
