9bd7a0c3e7f66f0a42aad0ba1a72af76c8e5d8a8
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:40:35 +0800

    Original processedAutoTestCases bytearraybuilder_estest27.java

---
+++ bytearraybuilder_estest27.java
@@ -0,0 +1,23 @@
+package com.fasterxml.jackson.core.util;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class ByteArrayBuilder_ESTestTest27 extends ByteArrayBuilder_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test26() throws Throwable {
+        byte[] byteArray0 = new byte[0];
+        ByteArrayBuilder byteArrayBuilder0 = ByteArrayBuilder.fromInitial(byteArray0, 294912);
+        // Undeclared exception!
+        try {
+            byteArrayBuilder0.toByteArray();
+            fail("Expecting exception: ArrayIndexOutOfBoundsException");
+        } catch (ArrayIndexOutOfBoundsException e) {
+        }
+    }
+}
