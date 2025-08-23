f6547809175138081ed992a1e70b180bd321de75
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:27:59 +0800

    Original processedManualTestCases defaultpiedatasettest4.java

---
+++ defaultpiedatasettest4.java
@@ -0,0 +1,37 @@
+package org.jfree.data.general;
+
+import org.jfree.chart.TestUtils;
+import org.jfree.chart.internal.CloneUtils;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DefaultPieDatasetTestTest4 implements DatasetChangeListener {
+
+    private DatasetChangeEvent lastEvent;
+
+    /**
+     * Records the last event.
+     *
+     * @param event  the last event.
+     */
+    @Override
+    public void datasetChanged(DatasetChangeEvent event) {
+        this.lastEvent = event;
+    }
+
+    /**
+     * Confirm that cloning works.
+     * @throws java.lang.CloneNotSupportedException
+     */
+    @Test
+    public void testCloning() throws CloneNotSupportedException {
+        DefaultPieDataset<String> d1 = new DefaultPieDataset<>();
+        d1.setValue("V1", 1);
+        d1.setValue("V2", null);
+        d1.setValue("V3", 3);
+        DefaultPieDataset<String> d2 = CloneUtils.clone(d1);
+        assertNotSame(d1, d2);
+        assertSame(d1.getClass(), d2.getClass());
+        assertEquals(d1, d2);
+    }
+}
