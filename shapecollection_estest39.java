ec2a026d612a9efc656a0474c62dc73d8982e7ed
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:15:33 +0800

    Original processedAutoTestCases shapecollection_estest39.java

---
+++ shapecollection_estest39.java
@@ -0,0 +1,34 @@
+package org.locationtech.spatial4j.shape;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Stack;
+import java.util.Vector;
+import java.util.function.Predicate;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+import org.locationtech.spatial4j.context.SpatialContext;
+import org.locationtech.spatial4j.context.SpatialContextFactory;
+import org.locationtech.spatial4j.distance.GeodesicSphereDistCalc;
+import org.locationtech.spatial4j.shape.impl.PointImpl;
+import org.locationtech.spatial4j.shape.jts.JtsPoint;
+
+public class ShapeCollection_ESTestTest39 extends ShapeCollection_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test38() throws Throwable {
+        ArrayList<JtsPoint> arrayList0 = new ArrayList<JtsPoint>();
+        HashMap<String, String> hashMap0 = new HashMap<String, String>();
+        ClassLoader classLoader0 = ClassLoader.getSystemClassLoader();
+        SpatialContext spatialContext0 = SpatialContextFactory.makeSpatialContext(hashMap0, classLoader0);
+        ShapeCollection<JtsPoint> shapeCollection0 = new ShapeCollection<JtsPoint>(arrayList0, spatialContext0);
+        SpatialContext spatialContext1 = shapeCollection0.getContext();
+        assertFalse(spatialContext1.isNormWrapLongitude());
+    }
+}
