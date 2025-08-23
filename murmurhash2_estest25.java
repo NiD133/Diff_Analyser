b7afbdd7fb322fb4fb2d606b19486cadf401a57b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:04:27 +0800

    gemini processedAutoTestCases murmurhash2_estest25.java

--- murmurhash2_estest25.java
+++ murmurhash2_estest25.java
@@ -1,25 +1,24 @@
 package org.apache.commons.codec.digest;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class MurmurHash2_ESTestTest25 extends MurmurHash2_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link MurmurHash2} class.
+ */
+public class MurmurHash2Test {
 
-    @Test(timeout = 4000)
-    public void test24() throws Throwable {
-        // Undeclared exception!
-        try {
-            MurmurHash2.hash64((byte[]) null, (-420), (-420));
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.commons.codec.digest.MurmurHash2", e);
-        }
+    /**
+     * Tests that the hash64 method throws a NullPointerException when the input
+     * byte array is null.
+     */
+    @Test(expected = NullPointerException.class)
+    public void hash64ShouldThrowNullPointerExceptionForNullData() {
+        // Define irrelevant parameters for length and seed, as the method should
+        // fail on the null data array before these are used.
+        final int length = 10;
+        final int seed = 123;
+
+        // This call is expected to throw a NullPointerException.
+        MurmurHash2.hash64(null, length, seed);
     }
 }
