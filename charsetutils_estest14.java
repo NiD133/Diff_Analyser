82dee5ef98f8ea20218246ee04816b85b8b54a51
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:42:43 +0800

    Original processedAutoTestCases charsetutils_estest14.java

---
+++ charsetutils_estest14.java
@@ -0,0 +1,17 @@
+package org.apache.commons.lang3;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class CharSetUtils_ESTestTest14 extends CharSetUtils_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test13() throws Throwable {
+        String[] stringArray0 = new String[8];
+        String string0 = CharSetUtils.keep("A-Z", stringArray0);
+        assertEquals("", string0);
+    }
+}
