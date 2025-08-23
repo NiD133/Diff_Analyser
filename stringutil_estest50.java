0c24fe0702b5c5c073b62852a7428270b3ac2cea
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:18:58 +0800

    Original processedAutoTestCases stringutil_estest50.java

---
+++ stringutil_estest50.java
@@ -0,0 +1,28 @@
+package org.jsoup.internal;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Collection;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.stream.Collector;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.net.MockURL;
+import org.junit.runner.RunWith;
+
+public class StringUtil_ESTestTest50 extends StringUtil_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test49() throws Throwable {
+        boolean boolean0 = StringUtil.isHexDigit('\u008F');
+        assertFalse(boolean0);
+    }
+}
