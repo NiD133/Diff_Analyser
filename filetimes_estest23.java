3280c08d926ec46e196bbb1536c12e5a455f0c8e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:50:46 +0800

    Original processedAutoTestCases filetimes_estest23.java

---
+++ filetimes_estest23.java
@@ -0,0 +1,33 @@
+package org.apache.commons.io.file.attribute;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.math.BigDecimal;
+import java.nio.file.Path;
+import java.nio.file.attribute.FileTime;
+import java.time.DateTimeException;
+import java.time.Instant;
+import java.util.Date;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.time.MockInstant;
+import org.junit.runner.RunWith;
+
+public class FileTimes_ESTestTest23 extends FileTimes_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test22() throws Throwable {
+        FileTime fileTime0 = FileTimes.ntfsTimeToFileTime(116444736024990000L);
+        // Undeclared exception!
+        try {
+            FileTimes.plusSeconds(fileTime0, 116444736024990000L);
+            fail("Expecting exception: DateTimeException");
+        } catch (DateTimeException e) {
+            //
+            // Instant exceeds minimum or maximum instant
+            //
+            verifyException("java.time.Instant", e);
+        }
+    }
+}
