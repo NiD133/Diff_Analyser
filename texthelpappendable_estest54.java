79f42ec5998df07c404b7e54475438109db09999
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:21:05 +0800

    Original processedAutoTestCases texthelpappendable_estest54.java

---
+++ texthelpappendable_estest54.java
@@ -0,0 +1,56 @@
+package org.apache.commons.cli.help;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.IOException;
+import java.io.PipedWriter;
+import java.io.StringWriter;
+import java.nio.BufferOverflowException;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.ReadOnlyBufferException;
+import java.nio.charset.Charset;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.SortedSet;
+import java.util.Stack;
+import java.util.TreeSet;
+import java.util.Vector;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class TextHelpAppendable_ESTestTest54 extends TextHelpAppendable_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test53() throws Throwable {
+        ArrayList<List<String>> arrayList0 = new ArrayList<List<String>>();
+        LinkedList<String> linkedList0 = new LinkedList<String>();
+        Stack<TextStyle> stack0 = new Stack<TextStyle>();
+        TableDefinition tableDefinition0 = TableDefinition.from(".rg.apache.commons.cli.help.TextStylejBuilder", stack0, linkedList0, arrayList0);
+        CharBuffer charBuffer0 = CharBuffer.wrap((CharSequence) ".rg.apache.commons.cli.help.TextStylejBuilder");
+        TextHelpAppendable textHelpAppendable0 = new TextHelpAppendable(charBuffer0);
+        // Undeclared exception!
+        try {
+            textHelpAppendable0.appendTable(tableDefinition0);
+            fail("Expecting exception: ReadOnlyBufferException");
+        } catch (ReadOnlyBufferException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("java.nio.CharBuffer", e);
+        }
+    }
+}
