a7323d4980966e725b1d281e905208ec4da39d50
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:25:13 +0800

    Original processedAutoTestCases utcinstant_estest24.java

---
+++ utcinstant_estest24.java
@@ -0,0 +1,26 @@
+package org.threeten.extra.scale;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.nio.CharBuffer;
+import java.time.DateTimeException;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.format.DateTimeParseException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.time.MockInstant;
+import org.junit.runner.RunWith;
+
+public class UtcInstant_ESTestTest24 extends UtcInstant_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test23() throws Throwable {
+        Instant instant0 = MockInstant.ofEpochSecond(0L);
+        UtcInstant utcInstant0 = UtcInstant.of(instant0);
+        long long0 = utcInstant0.getNanoOfDay();
+        assertEquals(0L, long0);
+        assertEquals(40587L, utcInstant0.getModifiedJulianDay());
+    }
+}
