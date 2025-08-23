158e76f7e0d98b0fc68f2766925337422491d5ce
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:06:25 +0800

    Original processedAutoTestCases optionformatter_estest23.java

---
+++ optionformatter_estest23.java
@@ -0,0 +1,23 @@
+package org.apache.commons.cli.help;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.function.BiFunction;
+import org.apache.commons.cli.Option;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class OptionFormatter_ESTestTest23 extends OptionFormatter_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test22() throws Throwable {
+        Option option0 = new Option((String) null, (String) null, true, "");
+        OptionFormatter optionFormatter0 = OptionFormatter.from(option0);
+        String string0 = optionFormatter0.getArgName();
+        assertEquals("<arg>", string0);
+    }
+}
