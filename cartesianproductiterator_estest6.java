ca7f939492ee14d8bda88acf2458e355e0b3eeca
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:41:35 +0800

    gemini processedAutoTestCases cartesianproductiterator_estest6.java

--- cartesianproductiterator_estest6.java
+++ cartesianproductiterator_estest6.java
@@ -1,36 +1,25 @@
 package org.apache.commons.collections4.iterators;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.sql.SQLNonTransientConnectionException;
-import java.util.ConcurrentModificationException;
-import java.util.EnumSet;
-import java.util.List;
-import java.util.Locale;
+import static org.junit.Assert.assertFalse;
+
 import java.util.NoSuchElementException;
-import java.util.PriorityQueue;
-import java.util.Stack;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class CartesianProductIterator_ESTestTest6 extends CartesianProductIterator_ESTest_scaffolding {
+/**
+ * Unit tests for {@link CartesianProductIterator}.
+ */
+public class CartesianProductIteratorTest {
 
-    @Test(timeout = 4000)
-    public void test5() throws Throwable {
-        Iterable<SQLNonTransientConnectionException>[] iterableArray0 = (Iterable<SQLNonTransientConnectionException>[]) Array.newInstance(Iterable.class, 0);
-        CartesianProductIterator<SQLNonTransientConnectionException> cartesianProductIterator0 = new CartesianProductIterator<SQLNonTransientConnectionException>(iterableArray0);
-        // Undeclared exception!
-        try {
-            cartesianProductIterator0.next();
-            fail("Expecting exception: NoSuchElementException");
-        } catch (NoSuchElementException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.commons.collections4.iterators.CartesianProductIterator", e);
-        }
+    @Test(expected = NoSuchElementException.class)
+    public void nextOnIteratorWithNoIterablesShouldThrowException() {
+        // Arrange: Create an iterator with no input iterables.
+        // This results in an empty Cartesian product.
+        final CartesianProductIterator<Object> iterator = new CartesianProductIterator<>();
+
+        // Assert: The iterator should correctly report that it has no elements.
+        assertFalse("Iterator should be empty when no iterables are provided", iterator.hasNext());
+
+        // Act: Calling next() on an empty iterator should throw the expected exception.
+        iterator.next();
     }
 }
