bc698a7fc2b76a807f9f315d13c499ab93b11c24
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:25:17 +0800

    gemini processedManualTestCases basicnodesettest3.java

--- basicnodesettest3.java
+++ basicnodesettest3.java
@@ -1,86 +1,21 @@
 package org.apache.commons.jxpath;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import java.util.Iterator;
+
 import java.util.List;
+import java.util.Iterator;
+import java.util.stream.Collectors;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.w3c.dom.Element;
 
-public class BasicNodeSetTestTest3 extends AbstractJXPathTest {
+/**
+ * Tests for the {@link BasicNodeSet} class, focusing on modification operations like add/remove.
+ */
+public class BasicNodeSetTest extends AbstractJXPathTest {
 
-    /**
-     * JXPathContext
-     */
-    protected JXPathContext context;
-
-    /**
-     * BasicNodeSet
-     */
-    protected BasicNodeSet nodeSet;
-
-    /**
-     * Add the pointers for the specified path to {@code nodeSet}.
-     *
-     * @param xpath
-     */
-    protected void addPointers(final String xpath) {
-        for (final Iterator<Pointer> iter = context.iteratePointers(xpath); iter.hasNext(); ) {
-            nodeSet.add(iter.next());
-        }
-        nudge();
-    }
-
-    /**
-     * Do assertions on DOM element names.
-     *
-     * @param names    List of expected names
-     * @param elements List of DOM elements
-     */
-    protected void assertElementNames(final List names, final List elements) {
-        assertEquals(names.size(), elements.size());
-        final Iterator nameIter = names.iterator();
-        final Iterator elementIter = elements.iterator();
-        while (elementIter.hasNext()) {
-            assertEquals(nameIter.next(), ((Element) elementIter.next()).getTagName());
-        }
-    }
-
-    /**
-     * Do assertions on DOM element values.
-     *
-     * @param values   List of expected values
-     * @param elements List of DOM elements
-     */
-    protected void assertElementValues(final List values, final List elements) {
-        assertEquals(values.size(), elements.size());
-        final Iterator valueIter = values.iterator();
-        final Iterator elementIter = elements.iterator();
-        while (elementIter.hasNext()) {
-            assertEquals(valueIter.next(), ((Element) elementIter.next()).getFirstChild().getNodeValue());
-        }
-    }
-
-    /**
-     * "Nudge" the nodeSet.
-     */
-    protected void nudge() {
-        nodeSet.getPointers();
-        nodeSet.getValues();
-        nodeSet.getNodes();
-    }
-
-    /**
-     * Remove the pointers for the specified path from {@code nodeSet}.
-     *
-     * @param xpath
-     */
-    protected void removePointers(final String xpath) {
-        for (final Iterator<Pointer> iter = context.iteratePointers(xpath); iter.hasNext(); ) {
-            nodeSet.remove(iter.next());
-        }
-        nudge();
-    }
+    private JXPathContext context;
+    private BasicNodeSet nodeSet;
 
     @Override
     @BeforeEach
@@ -91,14 +26,69 @@ public class BasicNodeSetTestTest3 extends AbstractJXPathTest {
     }
 
     /**
-     * Test removing a pointer.
+     * Tests that removing a pointer correctly updates the node set's internal state.
      */
     @Test
-    void testRemove() {
+    void removingPointerUpdatesTheNodeSet() {
+        // Arrange: Populate the node set with pointers to all integers in the bean.
         addPointers("/bean/integers");
+
+        // Act: Remove the pointer to the fourth integer.
         removePointers("/bean/integers[4]");
-        assertEquals(list("/bean/integers[1]", "/bean/integers[2]", "/bean/integers[3]").toString(), nodeSet.getPointers().toString());
-        assertEquals(list(Integer.valueOf(1), Integer.valueOf(2), Integer.valueOf(3)), nodeSet.getValues());
-        assertEquals(list(Integer.valueOf(1), Integer.valueOf(2), Integer.valueOf(3)), nodeSet.getNodes());
+
+        // Assert: The node set should now only contain the first three integers and their corresponding pointers.
+
+        // 1. Verify that the list of pointers is correct.
+        List<String> expectedPointerPaths = list(
+            "/bean/integers[1]",
+            "/bean/integers[2]",
+            "/bean/integers[3]"
+        );
+        List<String> actualPointerPaths = nodeSet.getPointers().stream()
+                .map(Pointer::asPath)
+                .collect(Collectors.toList());
+        assertEquals(expectedPointerPaths, actualPointerPaths, "Pointers should be updated after removal");
+
+        // 2. Verify that the lists of values and nodes are correct.
+        List<Integer> expectedValues = list(1, 2, 3);
+        assertEquals(expectedValues, nodeSet.getValues(), "Values should be updated after removal");
+        assertEquals(expectedValues, nodeSet.getNodes(), "Nodes should be updated after removal");
+    }
+
+    /**
+     * Adds all pointers matching the given XPath to the node set.
+     *
+     * @param xpath The XPath expression to find pointers for.
+     */
+    private void addPointers(final String xpath) {
+        for (final Iterator<Pointer> iter = context.iteratePointers(xpath); iter.hasNext(); ) {
+            nodeSet.add(iter.next());
+        }
+        forceCacheUpdate();
+    }
+
+    /**
+     * Removes all pointers matching the given XPath from the node set.
+     *
+     * @param xpath The XPath expression to find pointers for.
+     */
+    private void removePointers(final String xpath) {
+        for (final Iterator<Pointer> iter = context.iteratePointers(xpath); iter.hasNext(); ) {
+            nodeSet.remove(iter.next());
+        }
+        forceCacheUpdate();
+    }
+
+    /**
+     * Forces the BasicNodeSet to re-compute its internal cached lists.
+     * <p>
+     * The BasicNodeSet lazily computes its lists of pointers, values, and nodes.
+     * This method forces an immediate update, which is necessary for making assertions
+     * after the set has been modified via {@code add()} or {@code remove()}.
+     */
+    private void forceCacheUpdate() {
+        nodeSet.getPointers();
+        nodeSet.getValues();
+        nodeSet.getNodes();
     }
 }
