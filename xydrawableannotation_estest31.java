bb6209da0eb826969451fa256fbc312fd53fd5ff
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:29:38 +0800

    gemini processedAutoTestCases xydrawableannotation_estest31.java

--- xydrawableannotation_estest31.java
+++ xydrawableannotation_estest31.java
@@ -1,51 +1,40 @@
 package org.jfree.chart.annotations;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.awt.Font;
-import java.awt.Graphics2D;
-import java.awt.geom.Rectangle2D;
-import java.awt.geom.RoundRectangle2D;
-import java.awt.image.BufferedImage;
-import java.math.BigInteger;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.jfree.chart.ChartRenderingInfo;
 import org.jfree.chart.Drawable;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.axis.CyclicNumberAxis;
-import org.jfree.chart.axis.NumberAxis;
-import org.jfree.chart.axis.ValueAxis;
-import org.jfree.chart.block.BlockContainer;
-import org.jfree.chart.legend.LegendTitle;
-import org.jfree.chart.plot.MeterPlot;
-import org.jfree.chart.plot.PlotRenderingInfo;
-import org.jfree.chart.plot.PolarAxisLocation;
-import org.jfree.chart.plot.XYPlot;
-import org.jfree.chart.renderer.AbstractRenderer;
-import org.jfree.chart.renderer.category.ScatterRenderer;
-import org.jfree.chart.renderer.xy.XYBarRenderer;
-import org.jfree.chart.title.CompositeTitle;
-import org.jfree.chart.title.DateTitle;
-import org.jfree.chart.title.ShortTextTitle;
 import org.jfree.chart.title.TextTitle;
-import org.jfree.data.general.DefaultValueDataset;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class XYDrawableAnnotation_ESTestTest31 extends XYDrawableAnnotation_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
 
-    @Test(timeout = 4000)
-    public void test30() throws Throwable {
-        TextTitle textTitle0 = new TextTitle("x");
-        XYDrawableAnnotation xYDrawableAnnotation0 = new XYDrawableAnnotation(738.4170062070756, 738.4170062070756, 738.4170062070756, 738.4170062070756, textTitle0);
-        XYDrawableAnnotation xYDrawableAnnotation1 = (XYDrawableAnnotation) xYDrawableAnnotation0.clone();
-        boolean boolean0 = xYDrawableAnnotation0.equals(xYDrawableAnnotation1);
-        assertEquals(738.4170062070756, xYDrawableAnnotation1.getDisplayHeight(), 0.01);
-        assertEquals(1.0, xYDrawableAnnotation1.getDrawScaleFactor(), 0.01);
-        assertEquals(738.4170062070756, xYDrawableAnnotation1.getDisplayWidth(), 0.01);
-        assertTrue(boolean0);
-        assertEquals(738.4170062070756, xYDrawableAnnotation1.getY(), 0.01);
-        assertEquals(738.4170062070756, xYDrawableAnnotation1.getX(), 0.01);
+/**
+ * Unit tests for the {@link XYDrawableAnnotation} class, focusing on cloning and equality.
+ */
+public class XYDrawableAnnotationTest {
+
+    /**
+     * This test verifies that the clone() method creates a new instance
+     * that is a deep copy and is equal to the original object.
+     */
+    @Test
+    public void clone_shouldCreateEqualButSeparateInstance() throws CloneNotSupportedException {
+        // Arrange: Create an original annotation with clear, simple values.
+        Drawable drawable = new TextTitle("Test Annotation");
+        double x = 10.0;
+        double y = 20.0;
+        double width = 100.0;
+        double height = 50.0;
+        XYDrawableAnnotation originalAnnotation = new XYDrawableAnnotation(x, y, width, height, drawable);
+
+        // Act: Create a clone of the original annotation.
+        XYDrawableAnnotation clonedAnnotation = (XYDrawableAnnotation) originalAnnotation.clone();
+
+        // Assert: The clone should be a different object instance but equal in value to the original.
+        assertNotSame("A clone must be a different object in memory.", originalAnnotation, clonedAnnotation);
+        assertEquals("A clone should be equal to the original object.", originalAnnotation, clonedAnnotation);
+        
+        // As per the equals/hashCode contract, equal objects must have the same hash code.
+        assertEquals("Equal objects must have the same hash code.", 
+                     originalAnnotation.hashCode(), clonedAnnotation.hashCode());
     }
 }
