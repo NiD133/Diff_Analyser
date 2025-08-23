81c81c0d5f7766095edd5ef6bf3fb37d199f34ac
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:39:08 +0800

    gemini processedAutoTestCases bcodec_estest22.java

--- bcodec_estest22.java
+++ bcodec_estest22.java
@@ -1,26 +1,30 @@
 package org.apache.commons.codec.net;
 
+import org.apache.commons.codec.EncoderException;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
+
 import java.nio.charset.Charset;
-import java.nio.charset.IllegalCharsetNameException;
-import java.nio.charset.UnsupportedCharsetException;
-import org.apache.commons.codec.CodecPolicy;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class BCodec_ESTestTest22 extends BCodec_ESTest_scaffolding {
+/**
+ * Tests for {@link BCodec}.
+ */
+public class BCodecTest {
 
-    @Test(timeout = 4000)
-    public void test21() throws Throwable {
-        BCodec bCodec0 = new BCodec();
-        // Undeclared exception!
-        try {
-            bCodec0.encode(".LC", (Charset) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-        }
+    /**
+     * Tests that the encode method throws a NullPointerException when the provided
+     * Charset is null. The underlying implementation requires a non-null Charset
+     * to convert the input string into bytes for encoding.
+     */
+    @Test(expected = NullPointerException.class)
+    public void encodeWithNullCharsetShouldThrowNullPointerException() throws EncoderException {
+        // Arrange
+        final BCodec bCodec = new BCodec();
+        final String input = "any string";
+        final Charset nullCharset = null;
+
+        // Act: This call is expected to throw a NullPointerException.
+        bCodec.encode(input, nullCharset);
+
+        // Assert: The test passes if a NullPointerException is thrown.
     }
 }
