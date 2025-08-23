147fa4ab004fd54608394018ac657ecdcee0e779
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:04:06 +0800

    gemini processedAutoTestCases multiinputstream_estest9.java

--- multiinputstream_estest9.java
+++ multiinputstream_estest9.java
@@ -1,36 +1,28 @@
 package com.google.common.io;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.ConcurrentModificationException;
+
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.Iterator;
-import java.util.LinkedHashSet;
-import java.util.LinkedList;
-import java.util.ListIterator;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class MultiInputStream_ESTestTest9 extends MultiInputStream_ESTest_scaffolding {
+/**
+ * Unit tests for {@link MultiInputStream}.
+ */
+public class MultiInputStreamTest {
 
-    @Test(timeout = 4000)
-    public void test08() throws Throwable {
-        ArrayList<ByteSource> arrayList0 = new ArrayList<ByteSource>();
-        arrayList0.add((ByteSource) null);
-        Iterator<ByteSource> iterator0 = arrayList0.iterator();
-        MultiInputStream multiInputStream0 = null;
-        try {
-            multiInputStream0 = new MultiInputStream(iterator0);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("com.google.common.io.MultiInputStream", e);
-        }
+    /**
+     * Verifies that the constructor throws a NullPointerException if the provided
+     * iterator contains a null ByteSource. The constructor attempts to advance to the
+     * first stream immediately, which will fail when it encounters the null element.
+     */
+    @Test(expected = NullPointerException.class)
+    public void constructor_whenIteratorContainsNullByteSource_throwsNullPointerException() throws IOException {
+        // ARRANGE: Create an iterator that contains a single null ByteSource.
+        Iterator<ByteSource> sources = Arrays.asList((ByteSource) null).iterator();
+
+        // ACT & ASSERT: Constructing a MultiInputStream with this iterator should
+        // immediately throw a NullPointerException.
+        new MultiInputStream(sources);
     }
 }
