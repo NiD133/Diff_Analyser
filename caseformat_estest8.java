98cb96949f88cb415c95565b4057a435d73d02ff
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:41:43 +0800

    Original processedAutoTestCases caseformat_estest8.java

---
+++ caseformat_estest8.java
@@ -0,0 +1,18 @@
+package com.google.common.base;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class CaseFormat_ESTestTest8 extends CaseFormat_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test07() throws Throwable {
+        CaseFormat caseFormat0 = CaseFormat.UPPER_CAMEL;
+        String string0 = caseFormat0.convert(caseFormat0, "");
+        assertEquals("", string0);
+    }
+}
