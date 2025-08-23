9bc9a82e49563028bfc075ce40abd68a9af5b504
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:14:00 +0800

    Original processedAutoTestCases segmentutils_estest13.java

---
+++ segmentutils_estest13.java
@@ -0,0 +1,17 @@
+package org.apache.commons.compress.harmony.unpack200;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class SegmentUtils_ESTestTest13 extends SegmentUtils_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test12() throws Throwable {
+        int int0 = SegmentUtils.countArgs("_l((aDZ!yS3=O)=z/R2Z", 10);
+        assertEquals(19, int0);
+    }
+}
