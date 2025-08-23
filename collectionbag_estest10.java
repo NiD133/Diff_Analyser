d38da0bba7d90b763f8208a21d4ece7603cef9e7
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:44:05 +0800

    gemini processedAutoTestCases collectionbag_estest10.java

--- collectionbag_estest10.java
+++ collectionbag_estest10.java
@@ -1,49 +1,30 @@
 package org.apache.commons.collections4.bag;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.ConcurrentModificationException;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Stack;
-import java.util.TreeSet;
 import org.apache.commons.collections4.Bag;
-import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.SortedBag;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.FalsePredicate;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.MapTransformer;
-import org.apache.commons.collections4.functors.NullIsExceptionPredicate;
-import org.apache.commons.collections4.functors.TransformerPredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class CollectionBag_ESTestTest10 extends CollectionBag_ESTest_scaffolding {
+/**
+ * Tests for {@link CollectionBag} focusing on its decorator behavior.
+ */
+public class CollectionBagTest {
 
-    @Test(timeout = 4000)
-    public void test09() throws Throwable {
-        TreeBag<Integer> treeBag0 = new TreeBag<Integer>();
-        CollectionBag<Integer> collectionBag0 = new CollectionBag<Integer>(treeBag0);
-        // Undeclared exception!
-        try {
-            collectionBag0.remove((Object) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("java.util.TreeMap", e);
-        }
+    /**
+     * Tests that calling remove(null) on a CollectionBag that decorates a TreeBag
+     * correctly propagates the expected NullPointerException.
+     * <p>
+     * The test verifies that the decorator passes the method call to the underlying
+     * bag, and that exceptions from the underlying bag are not swallowed. A TreeBag
+     * is used as the decorated instance because it is known to not support null elements.
+     */
+    @Test(expected = NullPointerException.class)
+    public void removeNullFromCollectionBagDecoratingTreeBagThrowsNullPointerException() {
+        // Arrange: Create a CollectionBag that decorates a TreeBag.
+        // TreeBag does not support null elements.
+        final Bag<Integer> decoratedBag = new TreeBag<>();
+        final Bag<Integer> collectionBag = CollectionBag.collectionBag(decoratedBag);
+
+        // Act & Assert: Attempting to remove a null element should throw a
+        // NullPointerException, which is the expected behavior of the underlying TreeBag.
+        collectionBag.remove(null);
     }
 }
