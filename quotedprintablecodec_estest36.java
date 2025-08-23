185716226a371af7a154adf1d56eb1ac655c5d81
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:10:34 +0800

    Original processedAutoTestCases quotedprintablecodec_estest36.java

---
+++ quotedprintablecodec_estest36.java
@@ -0,0 +1,30 @@
+package org.apache.commons.codec.net;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.IllegalCharsetNameException;
+import java.nio.charset.UnsupportedCharsetException;
+import java.util.BitSet;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class QuotedPrintableCodec_ESTestTest36 extends QuotedPrintableCodec_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test35() throws Throwable {
+        QuotedPrintableCodec quotedPrintableCodec0 = null;
+        try {
+            quotedPrintableCodec0 = new QuotedPrintableCodec("4>&$1^)O:\"");
+            fail("Expecting exception: IllegalCharsetNameException");
+        } catch (IllegalCharsetNameException e) {
+            //
+            // 4>&$1^)O:\"
+            //
+            verifyException("java.nio.charset.Charset", e);
+        }
+    }
+}
