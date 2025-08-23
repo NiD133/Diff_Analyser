fc63fdba180a1f06e1fd7c677a47393a212a02a3
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:45:37 +0800

    Original processedAutoTestCases constructorutils_estest2.java

---
+++ constructorutils_estest2.java
@@ -0,0 +1,25 @@
+package org.apache.commons.lang3.reflect;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.AccessibleObject;
+import java.lang.reflect.Array;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class ConstructorUtils_ESTestTest2 extends ConstructorUtils_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test01() throws Throwable {
+        Class<Object> class0 = Object.class;
+        Object[] objectArray0 = new Object[0];
+        Class<Integer>[] classArray0 = (Class<Integer>[]) Array.newInstance(Class.class, 0);
+        Object object0 = ConstructorUtils.invokeConstructor(class0, objectArray0, (Class<?>[]) classArray0);
+        assertNotNull(object0);
+    }
+}
