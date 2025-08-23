3137f29050c7d4958de8ffa52d62ad3e48fc542e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:27:29 +0800

    gemini processedAutoTestCases weeks_estest69.java

--- weeks_estest69.java
+++ weeks_estest69.java
@@ -1,26 +1,20 @@
 package org.joda.time;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class Weeks_ESTestTest69 extends Weeks_ESTest_scaffolding {
+/**
+ * Test suite for the {@link Weeks} class.
+ */
+public class WeeksTest {
 
-    @Test(timeout = 4000)
-    public void test68() throws Throwable {
-        Weeks weeks0 = Weeks.MAX_VALUE;
-        // Undeclared exception!
-        try {
-            weeks0.multipliedBy(7);
-            fail("Expecting exception: ArithmeticException");
-        } catch (ArithmeticException e) {
-            //
-            // Multiplication overflows an int: 2147483647 * 7
-            //
-            verifyException("org.joda.time.field.FieldUtils", e);
-        }
+    @Test(expected = ArithmeticException.class)
+    public void multipliedBy_whenResultOverflows_throwsArithmeticException() {
+        // Arrange: Create a Weeks instance with the maximum integer value.
+        Weeks maxWeeks = Weeks.MAX_VALUE;
+        int multiplier = 7;
+
+        // Act: Attempt to multiply by a value that will cause an integer overflow.
+        // The @Test(expected) annotation asserts that this line throws an ArithmeticException.
+        maxWeeks.multipliedBy(multiplier);
     }
 }
