5998be4b9c28070396baccf2cb3038b452da61cf
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:22:54 +0800

    gemini processedAutoTestCases transformedbag_estest11.java

--- transformedbag_estest11.java
+++ transformedbag_estest11.java
@@ -1,61 +1,35 @@
 package org.apache.commons.collections4.bag;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Set;
-import java.util.Vector;
 import org.apache.commons.collections4.Bag;
-import org.apache.commons.collections4.Factory;
-import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.SortedBag;
 import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AndPredicate;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ChainedTransformer;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantFactory;
 import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.FactoryTransformer;
-import org.apache.commons.collections4.functors.FalsePredicate;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.InstantiateFactory;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.MapTransformer;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NotPredicate;
-import org.apache.commons.collections4.functors.OnePredicate;
-import org.apache.commons.collections4.functors.SwitchTransformer;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class TransformedBag_ESTestTest11 extends TransformedBag_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
-    @Test(timeout = 4000)
-    public void test10() throws Throwable {
-        Transformer<Object, Integer> transformer0 = ConstantTransformer.nullTransformer();
-        // Undeclared exception!
-        try {
-            TransformedBag.transformedBag((Bag<Integer>) null, (Transformer<? super Integer, ? extends Integer>) transformer0);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // collection
-            //
-            verifyException("java.util.Objects", e);
-        }
+/**
+ * Tests for {@link TransformedBag}.
+ */
+public class TransformedBagTest {
+
+    /**
+     * Tests that the transformedBag factory method throws a NullPointerException
+     * when the bag to be decorated is null.
+     */
+    @Test
+    public void transformedBag_whenDecoratedBagIsNull_shouldThrowNullPointerException() {
+        // Arrange: Create a valid transformer. The bag to be decorated is null.
+        final Transformer<Object, Object> transformer = ConstantTransformer.constantTransformer("ignored");
+        final Bag<Object> nullBag = null;
+
+        // Act & Assert: Call the factory method and verify the exception.
+        final NullPointerException exception = assertThrows(
+            NullPointerException.class,
+            () -> TransformedBag.transformedBag(nullBag, transformer)
+        );
+
+        // Verify that the exception message is as expected from the underlying implementation.
+        assertEquals("collection", exception.getMessage());
     }
 }
