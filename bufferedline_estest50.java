1975f16cace91bbb76c9e73ebbb45101d3dbd6bf
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:40:24 +0800

    gemini processedAutoTestCases bufferedline_estest50.java

--- bufferedline_estest50.java
+++ bufferedline_estest50.java
@@ -1,36 +1,36 @@
 package org.locationtech.spatial4j.shape.impl;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.HashMap;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 import org.locationtech.spatial4j.context.SpatialContext;
 import org.locationtech.spatial4j.context.SpatialContextFactory;
-import org.locationtech.spatial4j.distance.CartesianDistCalc;
 import org.locationtech.spatial4j.shape.Point;
-import org.locationtech.spatial4j.shape.Rectangle;
-import org.locationtech.spatial4j.shape.Shape;
-import org.locationtech.spatial4j.shape.SpatialRelation;
 
-public class BufferedLine_ESTestTest50 extends BufferedLine_ESTest_scaffolding {
+/**
+ * Test suite for {@link BufferedLine}.
+ */
+public class BufferedLineTest {
 
-    @Test(timeout = 4000)
-    public void test49() throws Throwable {
-        SpatialContextFactory spatialContextFactory0 = new SpatialContextFactory();
-        SpatialContext spatialContext0 = new SpatialContext(spatialContextFactory0);
-        PointImpl pointImpl0 = new PointImpl(57.29577951308232, 57.29577951308232, spatialContext0);
-        PointImpl pointImpl1 = new PointImpl(57.29577951308232, 5181.5, spatialContext0);
-        // Undeclared exception!
-        try {
-            BufferedLine.expandBufForLongitudeSkew(pointImpl1, pointImpl0, 0.7853981633974483);
-            fail("Expecting exception: AssertionError");
-        } catch (AssertionError e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-        }
+    private final SpatialContext spatialContext = new SpatialContext(new SpatialContextFactory());
+
+    /**
+     * Tests that {@link BufferedLine#expandBufForLongitudeSkew(Point, Point, double)}
+     * throws an AssertionError when a point has a latitude outside the valid [-90, 90] range.
+     * The method is expected to validate its inputs, and an invalid latitude should trigger
+     * a precondition failure.
+     */
+    @Test(expected = AssertionError.class)
+    public void expandBufForLongitudeSkew_whenLatitudeIsInvalid_throwsAssertionError() {
+        // Arrange
+        final double longitude = 57.3; // An arbitrary valid longitude
+        final double validLatitude = 45.0; // An arbitrary valid latitude
+        final double invalidLatitude = 5181.5; // Well outside the valid [-90, 90] range
+        final double buffer = Math.PI / 4;
+
+        Point validPoint = new PointImpl(longitude, validLatitude, spatialContext);
+        Point pointWithInvalidLatitude = new PointImpl(longitude, invalidLatitude, spatialContext);
+
+        // Act & Assert
+        // This call should fail because pointWithInvalidLatitude has an out-of-range Y-coordinate.
+        BufferedLine.expandBufForLongitudeSkew(pointWithInvalidLatitude, validPoint, buffer);
     }
 }
