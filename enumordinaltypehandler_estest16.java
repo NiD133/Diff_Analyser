4b3a4cfef5ae12ae09019d0dc87787832493f96c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:49:38 +0800

    Original processedAutoTestCases enumordinaltypehandler_estest16.java

---
+++ enumordinaltypehandler_estest16.java
@@ -0,0 +1,33 @@
+package org.apache.ibatis.type;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class EnumOrdinalTypeHandler_ESTestTest16 extends EnumOrdinalTypeHandler_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test15() throws Throwable {
+        Class<JdbcType> class0 = JdbcType.class;
+        EnumOrdinalTypeHandler<JdbcType> enumOrdinalTypeHandler0 = new EnumOrdinalTypeHandler<JdbcType>(class0);
+        JdbcType jdbcType0 = JdbcType.LONGVARBINARY;
+        // Undeclared exception!
+        try {
+            enumOrdinalTypeHandler0.setNonNullParameter((PreparedStatement) null, (-2025), jdbcType0, jdbcType0);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.apache.ibatis.type.EnumOrdinalTypeHandler", e);
+        }
+    }
+}
