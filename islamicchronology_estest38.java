75c4d8fbbb5f7d24086e90b8428d744bf9743ee7
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:55:15 +0800

    gemini processedAutoTestCases islamicchronology_estest38.java

--- islamicchronology_estest38.java
+++ islamicchronology_estest38.java
@@ -1,22 +1,23 @@
 package org.joda.time.chrono;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.TimeZone;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.joda.time.Chronology;
-import org.joda.time.DateTimeZone;
-import org.joda.time.tz.UTCProvider;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class IslamicChronology_ESTestTest38 extends IslamicChronology_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link IslamicChronology.LeapYearPatternType} inner class.
+ */
+public class IslamicChronologyLeapYearPatternTypeTest {
 
-    @Test(timeout = 4000)
-    public void test37() throws Throwable {
-        IslamicChronology.LeapYearPatternType islamicChronology_LeapYearPatternType0 = IslamicChronology.LEAP_YEAR_15_BASED;
-        boolean boolean0 = islamicChronology_LeapYearPatternType0.equals(islamicChronology_LeapYearPatternType0);
-        assertTrue(boolean0);
+    /**
+     * Verifies the reflexive property of the equals() method.
+     * According to the contract of Object.equals(), an object must be equal to itself.
+     */
+    @Test
+    public void testEqualsIsReflexive() {
+        // Arrange
+        final IslamicChronology.LeapYearPatternType pattern = IslamicChronology.LEAP_YEAR_15_BASED;
+
+        // Assert: An object must be equal to itself.
+        assertEquals(pattern, pattern);
     }
 }
