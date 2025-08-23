fda5efb051cfd929b9bfe0072a258861ce0be7a8
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:25:17 +0800

    Original processedAutoTestCases utcinstant_estest34.java

---
+++ utcinstant_estest34.java
@@ -0,0 +1,32 @@
+package org.threeten.extra.scale;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.nio.CharBuffer;
+import java.time.DateTimeException;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.format.DateTimeParseException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.time.MockInstant;
+import org.junit.runner.RunWith;
+
+public class UtcInstant_ESTestTest34 extends UtcInstant_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test33() throws Throwable {
+        CharBuffer charBuffer0 = CharBuffer.allocate(2278);
+        // Undeclared exception!
+        try {
+            UtcInstant.parse(charBuffer0);
+            fail("Expecting exception: DateTimeParseException");
+        } catch (DateTimeParseException e) {
+            //
+            // Text '\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000...' could not be parsed at index 0
+            //
+            verifyException("java.time.format.DateTimeFormatter", e);
+        }
+    }
+}
