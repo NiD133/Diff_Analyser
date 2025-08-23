54f806dcab580fa17d9d4cc56cfe2203e8a0bc7d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:34:01 +0800

    gemini processedManualTestCases stacktracefiltertest8.java

--- stacktracefiltertest8.java
+++ stacktracefiltertest8.java
@@ -1,23 +1,36 @@
 package org.mockito.internal.exceptions.stacktrace;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockitoutil.Conditions.onlyThoseClasses;
-import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.mockito.exceptions.base.TraceBuilder;
 import org.mockitoutil.TestBase;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class StackTraceFilterTestTest8 extends TestBase {
 
     private final StackTraceFilter filter = new StackTraceFilter();
 
     @Test
-    public void shouldStartFilteringAndKeepTop() {
-        // given
-        StackTraceElement[] t = new TraceBuilder().classes("org.test.Good", "org.mockito.internal.Bad", "org.test.MockitoSampleTest").toTraceArray();
-        // when
-        StackTraceElement[] filtered = filter.filter(t, true);
-        // then
-        Assertions.assertThat(filtered).has(onlyThoseClasses("org.test.MockitoSampleTest", "org.test.Good"));
+    public void shouldRemoveInternalFramesLocatedBetweenUserCodeFrames() {
+        // Given: A stack trace where an internal Mockito frame is surrounded by user code frames.
+        final String userCodeClass = "org.test.Good";
+        final String mockitoInternalClass = "org.mockito.internal.Bad"; // This class should be filtered out.
+        final String userTestClass = "org.test.MockitoSampleTest";
+
+        // The TraceBuilder creates a stack trace where the last class is the top of the stack.
+        // Stack trace order: userTestClass -> mockitoInternalClass -> userCodeClass
+        StackTraceElement[] originalStackTrace = new TraceBuilder()
+            .classes(userCodeClass, mockitoInternalClass, userTestClass)
+            .toTraceArray();
+
+        // When: The stack trace is filtered.
+        // The 'keepTop' parameter being true enables the standard filtering behavior.
+        StackTraceElement[] filteredStackTrace = filter.filter(originalStackTrace, true);
+
+        // Then: The internal Mockito frame should be removed, while preserving the user code frames.
+        assertThat(filteredStackTrace)
+            .extracting(StackTraceElement::getClassName)
+            .as("The filtered stack trace should only contain user code classes in the correct order")
+            .containsExactly(userTestClass, userCodeClass);
     }
 }
