fd922ce9ae3d93cfda6cecd970279fe87e42b4f9
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:41:24 +0800

    gemini processedAutoTestCases cachekey_estest11.java

--- cachekey_estest11.java
+++ cachekey_estest11.java
@@ -1,20 +1,32 @@
 package org.apache.ibatis.cache;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class CacheKey_ESTestTest11 extends CacheKey_ESTest_scaffolding {
+/**
+ * Test suite for the {@link CacheKey} class.
+ */
+public class CacheKeyTest {
 
-    @Test(timeout = 4000)
-    public void test10() throws Throwable {
-        CacheKey cacheKey0 = new CacheKey();
-        Object[] objectArray0 = new Object[0];
-        CacheKey cacheKey1 = new CacheKey(objectArray0);
-        boolean boolean0 = cacheKey1.equals(cacheKey0);
-        assertTrue(boolean0);
+    /**
+     * Verifies that a CacheKey created via the default constructor is considered
+     * equal to a CacheKey initialized with an empty object array. Both represent
+     * an empty key and should be interchangeable.
+     */
+    @Test
+    public void shouldConsiderTwoEmptyKeysAsEqual() {
+        // Arrange: Create two CacheKey instances that should be logically equivalent.
+        // One is created with the default constructor.
+        CacheKey keyFromDefaultConstructor = new CacheKey();
+        // The other is created with an empty array of objects.
+        CacheKey keyFromEmptyArray = new CacheKey(new Object[0]);
+
+        // Act & Assert: The two keys should be equal.
+        // We use assertEquals to check for logical equality.
+        assertEquals(keyFromDefaultConstructor, keyFromEmptyArray);
+
+        // As per the Java contract for equals(), if two objects are equal,
+        // their hash codes must also be equal.
+        assertEquals(keyFromDefaultConstructor.hashCode(), keyFromEmptyArray.hashCode());
     }
 }
