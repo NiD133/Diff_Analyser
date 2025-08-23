1bb640ec106a61ed0bb84c98add393592c5976b8
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:27:04 +0800

    Original processedManualTestCases constructorutilstest5.java

---
+++ constructorutilstest5.java
@@ -0,0 +1,153 @@
+package org.apache.commons.lang3.reflect;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.lang.reflect.Constructor;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.commons.lang3.AbstractLangTest;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+import org.apache.commons.lang3.mutable.MutableObject;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ConstructorUtilsTestTest5 extends AbstractLangTest {
+
+    private final Map<Class<?>, Class<?>[]> classCache;
+
+    private void expectMatchingAccessibleConstructorParameterTypes(final Class<?> cls, final Class<?>[] requestTypes, final Class<?>[] actualTypes) {
+        final Constructor<?> c = ConstructorUtils.getMatchingAccessibleConstructor(cls, requestTypes);
+        assertArrayEquals(actualTypes, c.getParameterTypes(), toString(c.getParameterTypes()) + " not equals " + toString(actualTypes));
+    }
+
+    @BeforeEach
+    public void setUp() {
+        classCache.clear();
+    }
+
+    private Class<?>[] singletonArray(final Class<?> c) {
+        Class<?>[] result = classCache.get(c);
+        if (result == null) {
+            result = new Class[] { c };
+            classCache.put(c, result);
+        }
+        return result;
+    }
+
+    private String toString(final Class<?>[] c) {
+        return Arrays.asList(c).toString();
+    }
+
+    private static class BaseClass {
+    }
+
+    static class PrivateClass {
+
+        @SuppressWarnings("unused")
+        public static class PublicInnerClass {
+
+            public PublicInnerClass() {
+            }
+        }
+
+        @SuppressWarnings("unused")
+        public PrivateClass() {
+        }
+    }
+
+    private static final class SubClass extends BaseClass {
+    }
+
+    public static class TestBean {
+
+        private final String toString;
+
+        final String[] varArgs;
+
+        public TestBean() {
+            toString = "()";
+            varArgs = null;
+        }
+
+        public TestBean(final BaseClass bc, final String... s) {
+            toString = "(BaseClass, String...)";
+            varArgs = s;
+        }
+
+        public TestBean(final double d) {
+            toString = "(double)";
+            varArgs = null;
+        }
+
+        public TestBean(final int i) {
+            toString = "(int)";
+            varArgs = null;
+        }
+
+        public TestBean(final Integer i) {
+            toString = "(Integer)";
+            varArgs = null;
+        }
+
+        public TestBean(final Integer first, final int... args) {
+            toString = "(Integer, String...)";
+            varArgs = new String[args.length];
+            for (int i = 0; i < args.length; ++i) {
+                varArgs[i] = Integer.toString(args[i]);
+            }
+        }
+
+        public TestBean(final Integer i, final String... s) {
+            toString = "(Integer, String...)";
+            varArgs = s;
+        }
+
+        public TestBean(final Object o) {
+            toString = "(Object)";
+            varArgs = null;
+        }
+
+        public TestBean(final String s) {
+            toString = "(String)";
+            varArgs = null;
+        }
+
+        public TestBean(final String... s) {
+            toString = "(String...)";
+            varArgs = s;
+        }
+
+        @Override
+        public String toString() {
+            return toString;
+        }
+
+        void verify(final String str, final String[] args) {
+            assertEquals(str, toString);
+            assertArrayEquals(args, varArgs);
+        }
+    }
+
+    @Test
+    void testInvokeConstructor() throws Exception {
+        assertEquals("()", ConstructorUtils.invokeConstructor(TestBean.class, (Object[]) ArrayUtils.EMPTY_CLASS_ARRAY).toString());
+        assertEquals("()", ConstructorUtils.invokeConstructor(TestBean.class, (Object[]) null).toString());
+        assertEquals("()", ConstructorUtils.invokeConstructor(TestBean.class).toString());
+        assertEquals("(String)", ConstructorUtils.invokeConstructor(TestBean.class, "").toString());
+        assertEquals("(Object)", ConstructorUtils.invokeConstructor(TestBean.class, new Object()).toString());
+        assertEquals("(Object)", ConstructorUtils.invokeConstructor(TestBean.class, Boolean.TRUE).toString());
+        assertEquals("(Integer)", ConstructorUtils.invokeConstructor(TestBean.class, NumberUtils.INTEGER_ONE).toString());
+        assertEquals("(int)", ConstructorUtils.invokeConstructor(TestBean.class, NumberUtils.BYTE_ONE).toString());
+        assertEquals("(double)", ConstructorUtils.invokeConstructor(TestBean.class, NumberUtils.LONG_ONE).toString());
+        assertEquals("(double)", ConstructorUtils.invokeConstructor(TestBean.class, NumberUtils.DOUBLE_ONE).toString());
+        ConstructorUtils.invokeConstructor(TestBean.class, NumberUtils.INTEGER_ONE).verify("(Integer)", null);
+        ConstructorUtils.invokeConstructor(TestBean.class, "a", "b").verify("(String...)", new String[] { "a", "b" });
+        ConstructorUtils.invokeConstructor(TestBean.class, NumberUtils.INTEGER_ONE, "a", "b").verify("(Integer, String...)", new String[] { "a", "b" });
+        ConstructorUtils.invokeConstructor(TestBean.class, new SubClass(), new String[] { "a", "b" }).verify("(BaseClass, String...)", new String[] { "a", "b" });
+    }
+}
