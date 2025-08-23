f72194b4f504a4d1f552ea13bbad5fa0e3fcfb80
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:09:37 +0800

    Original processedAutoTestCases proxyreader_estest22.java

---
+++ proxyreader_estest22.java
@@ -0,0 +1,33 @@
+package org.apache.commons.io.input;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.CharArrayWriter;
+import java.io.IOException;
+import java.io.PipedReader;
+import java.io.StringReader;
+import java.nio.CharBuffer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockIOException;
+import org.junit.runner.RunWith;
+
+public class ProxyReader_ESTestTest22 extends ProxyReader_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test21() throws Throwable {
+        StringReader stringReader0 = new StringReader("");
+        CloseShieldReader closeShieldReader0 = CloseShieldReader.wrap(stringReader0);
+        // Undeclared exception!
+        try {
+            closeShieldReader0.read((char[]) null, 63, 63);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("java.io.StringReader", e);
+        }
+    }
+}
