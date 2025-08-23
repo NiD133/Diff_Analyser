7cb1ceaf8383d747c1f915655e8287cc698014a0
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:16:50 +0800

    gemini processedAutoTestCases soundex_estest19.java

--- soundex_estest19.java
+++ soundex_estest19.java
@@ -1,17 +1,32 @@
 package org.apache.commons.codec.language;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class Soundex_ESTestTest19 extends Soundex_ESTest_scaffolding {
+/**
+ * Tests for the {@link Soundex} class.
+ */
+public class SoundexTest {
 
-    @Test(timeout = 4000)
-    public void test18() throws Throwable {
-        Soundex soundex0 = new Soundex("/HU+\"}{6.):e:-&!R");
-        assertEquals(4, soundex0.getMaxLength());
+    /**
+     * Tests that the default maximum length of a Soundex code is 4, as per the standard algorithm definition.
+     * This behavior should hold true regardless of the mapping used for the constructor.
+     * Note: The getMaxLength() method is deprecated.
+     */
+    @Test
+    public void getMaxLengthShouldReturnDefaultValue() {
+        // Arrange
+        // The standard Soundex algorithm produces a 4-character code.
+        final int expectedMaxLength = 4;
+        
+        // The constructor accepts a custom mapping, but this should not affect the default max length.
+        // We use the standard US English mapping for a realistic and clear example.
+        final Soundex soundex = new Soundex(Soundex.US_ENGLISH_MAPPING_STRING);
+
+        // Act
+        final int actualMaxLength = soundex.getMaxLength();
+
+        // Assert
+        assertEquals("The default max length of a Soundex code should be 4.", expectedMaxLength, actualMaxLength);
     }
 }
