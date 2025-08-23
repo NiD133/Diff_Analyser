4ae20060c64cc69b7cc406615c97b061acfc16be
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:08:41 +0800

    Original processedAutoTestCases periodformatter_estest6.java

---
+++ periodformatter_estest6.java
@@ -0,0 +1,39 @@
+package org.joda.time.format;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.IOException;
+import java.io.PipedWriter;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.LinkedList;
+import java.util.Locale;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.joda.time.Duration;
+import org.joda.time.Hours;
+import org.joda.time.Minutes;
+import org.joda.time.MutablePeriod;
+import org.joda.time.Period;
+import org.joda.time.PeriodType;
+import org.joda.time.ReadWritablePeriod;
+import org.joda.time.ReadablePeriod;
+import org.joda.time.Seconds;
+import org.joda.time.Weeks;
+import org.joda.time.Years;
+import org.junit.runner.RunWith;
+
+public class PeriodFormatter_ESTestTest6 extends PeriodFormatter_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test05() throws Throwable {
+        PeriodFormatterBuilder.Literal periodFormatterBuilder_Literal0 = PeriodFormatterBuilder.Literal.EMPTY;
+        PeriodFormatter periodFormatter0 = new PeriodFormatter(periodFormatterBuilder_Literal0, periodFormatterBuilder_Literal0);
+        Weeks weeks0 = Weeks.weeks(1);
+        Minutes minutes0 = weeks0.toStandardMinutes();
+        MutablePeriod mutablePeriod0 = minutes0.toMutablePeriod();
+        int int0 = periodFormatter0.parseInto(mutablePeriod0, "/M:TICzBQXt^", 0);
+        assertEquals(0, int0);
+    }
+}
