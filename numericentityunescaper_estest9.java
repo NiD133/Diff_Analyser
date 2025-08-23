d5996fe8b354b5266a4d812e54ef66f2c12061e7
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:05:37 +0800

    gemini processedAutoTestCases numericentityunescaper_estest9.java

--- numericentityunescaper_estest9.java
+++ numericentityunescaper_estest9.java
@@ -1,28 +1,29 @@
 package org.apache.commons.lang3.text.translate;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.StringWriter;
-import java.io.Writer;
-import java.nio.CharBuffer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class NumericEntityUnescaper_ESTestTest9 extends NumericEntityUnescaper_ESTest_scaffolding {
+/**
+ * Tests for {@link NumericEntityUnescaper}.
+ * This class provides a more understandable version of the original test case.
+ */
+public class NumericEntityUnescaperTest {
 
-    @Test(timeout = 4000)
-    public void test08() throws Throwable {
-        NumericEntityUnescaper.OPTION[] numericEntityUnescaper_OPTIONArray0 = new NumericEntityUnescaper.OPTION[0];
-        NumericEntityUnescaper numericEntityUnescaper0 = new NumericEntityUnescaper(numericEntityUnescaper_OPTIONArray0);
-        char[] charArray0 = new char[6];
-        charArray0[2] = '&';
-        charArray0[3] = '#';
-        charArray0[4] = 'x';
-        charArray0[5] = ';';
-        CharBuffer charBuffer0 = CharBuffer.wrap(charArray0);
-        String string0 = numericEntityUnescaper0.translate((CharSequence) charBuffer0);
-        assertEquals("\u0000\u0000&#x;", string0);
+    @Test
+    public void shouldNotTranslateIncompleteHexadecimalEntity() {
+        // Arrange
+        // Create an unescaper with default options. The default behavior is to require
+        // a semicolon at the end of an entity.
+        final NumericEntityUnescaper unescaper = new NumericEntityUnescaper();
+        final String malformedInput = "&#x;";
+
+        // Act
+        final String result = unescaper.translate(malformedInput);
+
+        // Assert
+        // The input string is an incomplete hexadecimal entity because it lacks any hex digits
+        // between 'x' and ';'. The unescaper should not perform any translation and should
+        // return the original string.
+        assertEquals("Incomplete hexadecimal entity should remain unchanged.", malformedInput, result);
     }
 }
