f158b3cd4405ec7c59ec4626a2c50603c518962d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:27:02 +0800

    gemini processedManualTestCases constructorinstantiatortest2.java

--- constructorinstantiatortest2.java
+++ constructorinstantiatortest2.java
@@ -1,37 +1,53 @@
 package org.mockito.internal.creation.instance;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.Assert.fail;
 import org.junit.Test;
+import org.mockito.creation.instance.Instantiator;
 import org.mockitoutil.TestBase;
 
-public class ConstructorInstantiatorTestTest2 extends TestBase {
+import static org.assertj.core.api.Assertions.assertThat;
 
-    static class SomeClass {
-    }
+/**
+ * Tests for {@link ConstructorInstantiator} focused on creating instances of non-static inner classes.
+ */
+public class ConstructorInstantiatorInnerClassTest extends TestBase {
 
-    class SomeInnerClass {
-    }
-
-    class ChildOfThis extends ConstructorInstantiatorTest {
-    }
-
-    static class SomeClass2 {
-
-        SomeClass2(String x) {
+    // A clear, self-contained outer class for testing purposes.
+    static class Outer {
+        // A non-static inner class that requires an instance of Outer to be created.
+        class Inner {
+            // The implicit constructor is Inner(Outer outer)
         }
     }
 
-    static class SomeClass3 {
-
-        SomeClass3(int i) {
-        }
+    // A subclass of Outer to test polymorphism during instantiation.
+    static class SubclassOfOuter extends Outer {
     }
 
     @Test
-    public void creates_instances_of_inner_classes() {
-        assertThat(new ConstructorInstantiator(true, this).newInstance(SomeInnerClass.class).getClass()).isEqualTo(SomeInnerClass.class);
-        assertThat(new ConstructorInstantiator(true, new ChildOfThis()).newInstance(SomeInnerClass.class).getClass()).isEqualTo(SomeInnerClass.class);
+    public void shouldInstantiateInnerClass_whenCorrectOuterInstanceIsProvided() {
+        // Arrange
+        Outer outerInstance = new Outer();
+        // The 'true' flag indicates that an outer instance is being provided as the first constructor argument.
+        Instantiator instantiator = new ConstructorInstantiator(true, outerInstance);
+
+        // Act
+        Outer.Inner instance = instantiator.newInstance(Outer.Inner.class);
+
+        // Assert
+        assertThat(instance).isInstanceOf(Outer.Inner.class);
+    }
+
+    @Test
+    public void shouldInstantiateInnerClass_whenSubclassOfOuterInstanceIsProvided() {
+        // Arrange
+        SubclassOfOuter subclassInstance = new SubclassOfOuter();
+        // The instantiator should be able to use a subclass of the required outer class.
+        Instantiator instantiator = new ConstructorInstantiator(true, subclassInstance);
+
+        // Act
+        Outer.Inner instance = instantiator.newInstance(Outer.Inner.class);
+
+        // Assert
+        assertThat(instance).isInstanceOf(Outer.Inner.class);
     }
 }
