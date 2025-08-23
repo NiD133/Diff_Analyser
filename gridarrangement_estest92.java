03c23b0348f003e245c2186d5ac0e0c2827ba720
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:52:41 +0800

    Original processedAutoTestCases gridarrangement_estest92.java

---
+++ gridarrangement_estest92.java
@@ -0,0 +1,42 @@
+package org.jfree.chart.block;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.awt.Graphics2D;
+import java.awt.SystemColor;
+import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.util.MockGregorianCalendar;
+import org.jfree.chart.api.HorizontalAlignment;
+import org.jfree.chart.api.RectangleAnchor;
+import org.jfree.chart.api.VerticalAlignment;
+import org.jfree.chart.text.TextBlockAnchor;
+import org.jfree.data.Range;
+import org.jfree.data.time.TimePeriodAnchor;
+import org.jfree.data.time.TimeSeries;
+import org.junit.runner.RunWith;
+
+public class GridArrangement_ESTestTest92 extends GridArrangement_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test91() throws Throwable {
+        GridArrangement gridArrangement0 = new GridArrangement(105, 105);
+        BlockContainer blockContainer0 = new BlockContainer();
+        LengthConstraintType lengthConstraintType0 = LengthConstraintType.RANGE;
+        LengthConstraintType lengthConstraintType1 = LengthConstraintType.NONE;
+        RectangleConstraint rectangleConstraint0 = new RectangleConstraint(0.0, (Range) null, lengthConstraintType0, (-1.0), (Range) null, lengthConstraintType1);
+        // Undeclared exception!
+        try {
+            gridArrangement0.arrange(blockContainer0, (Graphics2D) null, rectangleConstraint0);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.jfree.chart.block.GridArrangement", e);
+        }
+    }
+}
