20b717474f6163ba65da7cbf1cfe4cc51607ab50
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:12:33 +0800

    Original processedAutoTestCases refinedsoundex_estest4.java

---
+++ refinedsoundex_estest4.java
@@ -0,0 +1,18 @@
+package org.apache.commons.codec.language;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class RefinedSoundex_ESTestTest4 extends RefinedSoundex_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test03() throws Throwable {
+        RefinedSoundex refinedSoundex0 = new RefinedSoundex("org.apache.commons.codec.language.RefinedSoundex");
+        char char0 = refinedSoundex0.getMappingCode('X');
+        assertEquals('c', char0);
+    }
+}
