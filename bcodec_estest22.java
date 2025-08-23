02d59a2ac813a0c466b49fc6be4fa194f4599faa
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:39:08 +0800

    Original processedAutoTestCases bcodec_estest22.java

---
+++ bcodec_estest22.java
@@ -0,0 +1,26 @@
+package org.apache.commons.codec.net;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.nio.charset.Charset;
+import java.nio.charset.IllegalCharsetNameException;
+import java.nio.charset.UnsupportedCharsetException;
+import org.apache.commons.codec.CodecPolicy;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class BCodec_ESTestTest22 extends BCodec_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test21() throws Throwable {
+        BCodec bCodec0 = new BCodec();
+        // Undeclared exception!
+        try {
+            bCodec0.encode(".LC", (Charset) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+        }
+    }
+}
