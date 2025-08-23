4f454de7cb7ebcbf01a7d3807ccd5851881654ee
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:16:50 +0800

    Original processedAutoTestCases soundex_estest19.java

---
+++ soundex_estest19.java
@@ -0,0 +1,17 @@
+package org.apache.commons.codec.language;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class Soundex_ESTestTest19 extends Soundex_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test18() throws Throwable {
+        Soundex soundex0 = new Soundex("/HU+\"}{6.):e:-&!R");
+        assertEquals(4, soundex0.getMaxLength());
+    }
+}
