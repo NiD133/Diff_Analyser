433373d97808c4af0d1eb836e736af8b13c955ec
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:43:46 +0800

    Original processedAutoTestCases codeattribute_estest12.java

---
+++ codeattribute_estest12.java
@@ -0,0 +1,35 @@
+package org.apache.commons.compress.harmony.unpack200.bytecode;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.PipedOutputStream;
+import java.util.LinkedList;
+import java.util.List;
+import org.apache.commons.compress.harmony.unpack200.Segment;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockFileOutputStream;
+import org.junit.runner.RunWith;
+
+public class CodeAttribute_ESTestTest12 extends CodeAttribute_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test11() throws Throwable {
+        byte[] byteArray0 = new byte[17];
+        OperandManager operandManager0 = new OperandManager((int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null);
+        CodeAttribute codeAttribute0 = new CodeAttribute(295, 295, byteArray0, (Segment) null, operandManager0, (List<ExceptionTableEntry>) null);
+        // Undeclared exception!
+        try {
+            codeAttribute0.getLength();
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.apache.commons.compress.harmony.unpack200.bytecode.CodeAttribute", e);
+        }
+    }
+}
