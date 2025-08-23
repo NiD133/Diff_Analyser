53b426447d6e4382d30372527ac0478ba2763920
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:38:14 +0800

    Original processedAutoTestCases attribute_estest35.java

---
+++ attribute_estest35.java
@@ -0,0 +1,29 @@
+package org.jsoup.nodes;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.ByteArrayOutputStream;
+import java.io.FilterOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PipedWriter;
+import java.io.StringWriter;
+import java.nio.BufferOverflowException;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetEncoder;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockPrintWriter;
+import org.jsoup.internal.QuietAppendable;
+import org.junit.runner.RunWith;
+
+public class Attribute_ESTestTest35 extends Attribute_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test34() throws Throwable {
+        Attribute attribute0 = new Attribute("QSy4iJ5y", "QSy4iJ5y");
+        String string0 = attribute0.prefix();
+        assertEquals("", string0);
+    }
+}
