7e500da478a0d2d5984ad428c1807ea4938bc636
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:14:39 +0800

    Original processedAutoTestCases sequencescomparator_estest8.java

---
+++ sequencescomparator_estest8.java
@@ -0,0 +1,36 @@
+package org.apache.commons.collections4.sequence;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.ConcurrentModificationException;
+import java.util.LinkedList;
+import java.util.List;
+import org.apache.commons.collections4.Equator;
+import org.apache.commons.collections4.Predicate;
+import org.apache.commons.collections4.functors.DefaultEquator;
+import org.apache.commons.collections4.functors.NotNullPredicate;
+import org.apache.commons.collections4.functors.PredicateTransformer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class SequencesComparator_ESTestTest8 extends SequencesComparator_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test07() throws Throwable {
+        LinkedList<Object> linkedList0 = new LinkedList<Object>();
+        SequencesComparator<Object> sequencesComparator0 = new SequencesComparator<Object>(linkedList0, linkedList0);
+        linkedList0.add((Object) sequencesComparator0);
+        // Undeclared exception!
+        try {
+            sequencesComparator0.getScript();
+            fail("Expecting exception: ArrayIndexOutOfBoundsException");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            //
+            // 2
+            //
+            verifyException("org.apache.commons.collections4.sequence.SequencesComparator", e);
+        }
+    }
+}
