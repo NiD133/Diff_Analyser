22b9a8938bbc29ebf0547c5953249eaa4060f839
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:47:40 +0800

    gemini processedAutoTestCases defaultpiedataset_estest21.java

--- defaultpiedataset_estest21.java
+++ defaultpiedataset_estest21.java
@@ -1,40 +1,40 @@
 package org.jfree.data.general;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.time.chrono.ChronoLocalDate;
-import java.util.List;
-import java.util.Vector;
-import javax.swing.JLayeredPane;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.jfree.chart.api.SortOrder;
 import org.jfree.chart.api.TableOrder;
-import org.jfree.data.DefaultKeyedValues;
-import org.jfree.data.KeyedValues;
-import org.jfree.data.category.CategoryDataset;
 import org.jfree.data.category.CategoryToPieDataset;
-import org.jfree.data.category.DefaultIntervalCategoryDataset;
-import org.jfree.data.category.SlidingCategoryDataset;
-import org.jfree.data.statistics.DefaultBoxAndWhiskerCategoryDataset;
-import org.junit.runner.RunWith;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.junit.Test;
 
-public class DefaultPieDataset_ESTestTest21 extends DefaultPieDataset_ESTest_scaffolding {
+/**
+ * This test class focuses on the DefaultPieDataset.
+ * This specific test case verifies the behavior of the constructor when it receives
+ * a PieDataset adapter (CategoryToPieDataset) that is configured with an invalid index.
+ */
+public class DefaultPieDataset_ESTestTest21 {
 
-    @Test(timeout = 4000)
-    public void test20() throws Throwable {
-        DefaultBoxAndWhiskerCategoryDataset<Integer, Integer> defaultBoxAndWhiskerCategoryDataset0 = new DefaultBoxAndWhiskerCategoryDataset<Integer, Integer>();
-        Vector<Integer> vector0 = new Vector<Integer>();
-        Integer integer0 = JLayeredPane.MODAL_LAYER;
-        defaultBoxAndWhiskerCategoryDataset0.add((List<? extends Number>) vector0, integer0, integer0);
-        TableOrder tableOrder0 = TableOrder.BY_COLUMN;
-        CategoryToPieDataset categoryToPieDataset0 = new CategoryToPieDataset(defaultBoxAndWhiskerCategoryDataset0, tableOrder0, (-374));
-        DefaultPieDataset<Integer> defaultPieDataset0 = null;
-        try {
-            defaultPieDataset0 = new DefaultPieDataset<Integer>(categoryToPieDataset0);
-            fail("Expecting exception: IndexOutOfBoundsException");
-        } catch (IndexOutOfBoundsException e) {
-        }
+    /**
+     * Tests that the DefaultPieDataset constructor throws an IndexOutOfBoundsException
+     * when initialized with a CategoryToPieDataset that has an invalid 'extract' index.
+     * The CategoryToPieDataset acts as an adapter, and when the DefaultPieDataset
+     * constructor tries to read data from it, the invalid index causes the exception.
+     */
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void constructorWithAdapterThrowsExceptionForInvalidExtractIndex() {
+        // Arrange: Create a source CategoryDataset with one data point.
+        // This serves as the underlying data for the adapter.
+        DefaultCategoryDataset sourceCategoryDataset = new DefaultCategoryDataset();
+        sourceCategoryDataset.addValue(150.0, "RowKey", "ColumnKey");
+
+        // Arrange: Create a CategoryToPieDataset adapter. This adapter is configured
+        // to extract data from a specific column of the source dataset. We intentionally
+        // provide an invalid column index (-1) to trigger an error.
+        final int invalidColumnIndex = -1;
+        CategoryToPieDataset adapterDataset = new CategoryToPieDataset(
+                sourceCategoryDataset, TableOrder.BY_COLUMN, invalidColumnIndex);
+
+        // Act & Assert: Attempt to create a DefaultPieDataset from the misconfigured
+        // adapter. The constructor will try to access data using the invalid index,
+        // which is expected to throw an IndexOutOfBoundsException.
+        new DefaultPieDataset<>(adapterDataset);
     }
 }
