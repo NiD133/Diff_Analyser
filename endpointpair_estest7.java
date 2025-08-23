ab3d62c0c19b93d650a730a0318034a536c7cab5
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:49:19 +0800

    gemini processedAutoTestCases endpointpair_estest7.java

--- endpointpair_estest7.java
+++ endpointpair_estest7.java
@@ -1,27 +1,28 @@
 package com.google.common.graph;
 
+import static org.junit.Assert.assertThrows;
+
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import com.google.common.collect.UnmodifiableIterator;
-import java.util.Locale;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class EndpointPair_ESTestTest7 extends EndpointPair_ESTest_scaffolding {
+/**
+ * Tests for {@link EndpointPair}.
+ */
+public class EndpointPairTest {
 
-    @Test(timeout = 4000)
-    public void test06() throws Throwable {
-        // Undeclared exception!
-        try {
-            EndpointPair.unordered((Object) null, (Object) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("com.google.common.base.Preconditions", e);
-        }
+    /**
+     * Verifies that creating an unordered EndpointPair with a null node is not allowed.
+     * The factory method should throw a NullPointerException if any of its arguments are null,
+     * as enforced by Guava's Preconditions.checkNotNull.
+     */
+    @Test
+    public void unordered_withNullNode_throwsNullPointerException() {
+        // Test case 1: The first node is null.
+        assertThrows(NullPointerException.class, () -> EndpointPair.unordered(null, "nodeV"));
+
+        // Test case 2: The second node is null.
+        assertThrows(NullPointerException.class, () -> EndpointPair.unordered("nodeU", null));
+
+        // Test case 3: Both nodes are null (the original test's scenario).
+        assertThrows(NullPointerException.class, () -> EndpointPair.unordered(null, null));
     }
 }
