1238e75982ce14889be68665749ee7df69728d30
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:10:04 +0800

    Original processedAutoTestCases queryparser_estest30.java

---
+++ queryparser_estest30.java
@@ -0,0 +1,17 @@
+package org.jsoup.select;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class QueryParser_ESTestTest30 extends QueryParser_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test29() throws Throwable {
+        Evaluator evaluator0 = QueryParser.parse(":last-of-type");
+        assertEquals(":last-of-type", evaluator0.toString());
+    }
+}
