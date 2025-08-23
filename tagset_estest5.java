857cc277dc7c376c45636d3be83a2bddfa03a1b7
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:20:20 +0800

    Original processedAutoTestCases tagset_estest5.java

---
+++ tagset_estest5.java
@@ -0,0 +1,26 @@
+package org.jsoup.parser;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.function.Consumer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class TagSet_ESTestTest5 extends TagSet_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test04() throws Throwable {
+        TagSet tagSet0 = new TagSet();
+        ParseSettings parseSettings0 = ParseSettings.preserveCase;
+        Tag tag0 = tagSet0.valueOf(":_{P!x%}X,o'^63]{>", ":_{P!x%}X,o'^63]{>", parseSettings0);
+        Tag.Known = 55;
+        TagSet tagSet1 = tagSet0.add(tag0);
+        Tag tag1 = tagSet1.valueOf(":_{P!x%}X,o'^63]{>", ":_{P!x%}X,o'^63]{>", ":_{P!x%}X,o'^63]{>", true);
+        assertFalse(tag1.formatAsBlock());
+        assertEquals(":_{p!x%}x,o'^63]{>", tag1.normalName());
+    }
+}
