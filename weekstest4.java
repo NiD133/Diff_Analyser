55de78416584695cd2685a36555e719447e5562f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:35:55 +0800

    Original processedManualTestCases weekstest4.java

---
+++ weekstest4.java
@@ -0,0 +1,42 @@
+package org.joda.time;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class WeeksTestTest4 extends TestCase {
+
+    // (before the late 90's they were all over the place)
+    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
+
+    public static TestSuite suite() {
+        return new TestSuite(TestWeeks.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+    }
+
+    @SuppressWarnings("deprecation")
+    public void testFactory_weeksBetween_RPartial() {
+        LocalDate start = new LocalDate(2006, 6, 9);
+        LocalDate end1 = new LocalDate(2006, 6, 30);
+        YearMonthDay end2 = new YearMonthDay(2006, 7, 21);
+        assertEquals(3, Weeks.weeksBetween(start, end1).getWeeks());
+        assertEquals(0, Weeks.weeksBetween(start, start).getWeeks());
+        assertEquals(0, Weeks.weeksBetween(end1, end1).getWeeks());
+        assertEquals(-3, Weeks.weeksBetween(end1, start).getWeeks());
+        assertEquals(6, Weeks.weeksBetween(start, end2).getWeeks());
+    }
+}
