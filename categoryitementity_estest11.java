8bdae0c422d434f68ba1f24a5b850df5daaaa256
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:41:45 +0800

    gemini processedAutoTestCases categoryitementity_estest11.java

--- categoryitementity_estest11.java
+++ categoryitementity_estest11.java
@@ -1,36 +1,38 @@
 package org.jfree.chart.entity;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.awt.Polygon;
-import java.awt.Rectangle;
-import java.awt.geom.Rectangle2D;
-import java.time.Clock;
-import java.time.LocalDate;
-import java.time.chrono.ChronoLocalDate;
-import java.time.chrono.JapaneseDate;
-import javax.swing.JLayeredPane;
-import javax.swing.text.DefaultCaret;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.time.MockClock;
-import org.evosuite.runtime.mock.java.time.MockLocalDate;
-import org.evosuite.runtime.mock.java.time.chrono.MockJapaneseDate;
 import org.jfree.data.category.CategoryDataset;
-import org.jfree.data.statistics.DefaultBoxAndWhiskerCategoryDataset;
 import org.jfree.data.statistics.DefaultMultiValueCategoryDataset;
-import org.jfree.data.statistics.DefaultStatisticalCategoryDataset;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class CategoryItemEntity_ESTestTest11 extends CategoryItemEntity_ESTest_scaffolding {
+import java.awt.Rectangle;
+import java.awt.Shape;
 
-    @Test(timeout = 4000)
-    public void test10() throws Throwable {
-        DefaultCaret defaultCaret0 = new DefaultCaret();
-        DefaultMultiValueCategoryDataset<Integer, Integer> defaultMultiValueCategoryDataset0 = new DefaultMultiValueCategoryDataset<Integer, Integer>();
-        CategoryItemEntity<Integer, Integer> categoryItemEntity0 = new CategoryItemEntity<Integer, Integer>(defaultCaret0, "N`", "N`", defaultMultiValueCategoryDataset0, (Integer) 0, (Integer) 0);
-        boolean boolean0 = categoryItemEntity0.equals(categoryItemEntity0);
-        assertTrue(boolean0);
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit tests for the {@link CategoryItemEntity} class.
+ */
+public class CategoryItemEntityTest {
+
+    /**
+     * Verifies the reflexivity of the equals() method.
+     * An object must always be equal to itself.
+     */
+    @Test
+    public void testEquals_onSameInstance_shouldReturnTrue() {
+        // Arrange: Create a CategoryItemEntity instance with typical test data.
+        Shape area = new Rectangle(10, 20, 30, 40);
+        CategoryDataset<String, String> dataset = new DefaultMultiValueCategoryDataset<>();
+        
+        CategoryItemEntity<String, String> entity = new CategoryItemEntity<>(
+                area,
+                "Test ToolTip",
+                "test.html",
+                dataset,
+                "Row 1",
+                "Column 1");
+
+        // Act & Assert: An object must be equal to itself.
+        assertTrue("An entity instance should be equal to itself.", entity.equals(entity));
     }
 }
