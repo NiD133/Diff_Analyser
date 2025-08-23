8e3126edb78b00ba47c57a6ccb39e7ad7334a730
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:43:59 +0800

    Original processedAutoTestCases codecencoding_estest25.java

---
+++ codecencoding_estest25.java
@@ -0,0 +1,33 @@
+package org.apache.commons.compress.harmony.pack200;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.io.PushbackInputStream;
+import java.io.SequenceInputStream;
+import java.util.Enumeration;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class CodecEncoding_ESTestTest25 extends CodecEncoding_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test24() throws Throwable {
+        BHSDCodec bHSDCodec0 = Codec.MDELTA5;
+        byte[] byteArray0 = new byte[6];
+        byteArray0[0] = (byte) (-88);
+        ByteArrayInputStream byteArrayInputStream0 = new ByteArrayInputStream(byteArray0);
+        CodecEncoding.getCodec(141, byteArrayInputStream0, bHSDCodec0);
+        assertEquals(3, byteArrayInputStream0.available());
+    }
+}
