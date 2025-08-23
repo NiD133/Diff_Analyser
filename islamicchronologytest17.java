232abf596296fa03478400df14c0712f97b2c901
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:29:44 +0800

    gemini processedManualTestCases islamicchronologytest17.java

--- islamicchronologytest17.java
+++ islamicchronologytest17.java
@@ -1,123 +1,108 @@
 package org.joda.time.chrono;
 
-import java.util.Locale;
-import java.util.TimeZone;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
 import org.joda.time.Chronology;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeConstants;
-import org.joda.time.DateTimeField;
-import org.joda.time.DateTimeUtils;
-import org.joda.time.DateTimeZone;
-import org.joda.time.DurationFieldType;
 import org.joda.time.DateTime.Property;
+import org.joda.time.DurationFieldType;
+import org.junit.Before;
+import org.junit.Test;
 
-public class IslamicChronologyTestTest17 extends TestCase {
-
-    private static long SKIP = 1 * DateTimeConstants.MILLIS_PER_DAY;
-
-    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
-
-    private static final DateTimeZone LONDON = DateTimeZone.forID("Europe/London");
-
-    private static final DateTimeZone TOKYO = DateTimeZone.forID("Asia/Tokyo");
+/**
+ * Tests various properties of date fields in the IslamicChronology.
+ * This test focuses on a specific leap year date to verify field behaviors.
+ */
+public class IslamicChronologyDateFieldsTest {
 
     private static final Chronology ISLAMIC_UTC = IslamicChronology.getInstanceUTC();
 
-    private static final Chronology JULIAN_UTC = JulianChronology.getInstanceUTC();
+    // Test date is 1426-12-24 in the Islamic calendar.
+    // The year 1426 is a leap year in the default (16-based) pattern,
+    // as 1426 % 30 = 16, and 16 is a leap year in that pattern.
+    private static final int TEST_YEAR = 1426;
+    private static final int TEST_MONTH = 12;
+    private static final int TEST_DAY = 24;
 
-    private static final Chronology ISO_UTC = ISOChronology.getInstanceUTC();
+    private DateTime testDate;
 
-    long y2002days = 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365;
-
-    // 2002-06-09
-    private long TEST_TIME_NOW = (y2002days + 31L + 28L + 31L + 30L + 31L + 9L - 1L) * DateTimeConstants.MILLIS_PER_DAY;
-
-    private DateTimeZone originalDateTimeZone = null;
-
-    private TimeZone originalTimeZone = null;
-
-    private Locale originalLocale = null;
-
-    public static void main(String[] args) {
-        junit.textui.TestRunner.run(suite());
+    @Before
+    public void setUp() {
+        testDate = new DateTime(TEST_YEAR, TEST_MONTH, TEST_DAY, 0, 0, 0, 0, ISLAMIC_UTC);
     }
 
-    public static TestSuite suite() {
-        SKIP = 1 * DateTimeConstants.MILLIS_PER_DAY;
-        return new TestSuite(TestIslamicChronology.class);
+    @Test
+    public void testDateCreationAndBasicComponents() {
+        assertEquals(IslamicChronology.AH, testDate.getEra());
+        assertEquals(TEST_YEAR, testDate.getYear());
+        assertEquals(TEST_MONTH, testDate.getMonthOfYear());
+        assertEquals(TEST_DAY, testDate.getDayOfMonth());
+        assertEquals(DateTimeConstants.TUESDAY, testDate.getDayOfWeek());
     }
 
-    @Override
-    protected void setUp() throws Exception {
-        DateTimeUtils.setCurrentMillisFixed(TEST_TIME_NOW);
-        originalDateTimeZone = DateTimeZone.getDefault();
-        originalTimeZone = TimeZone.getDefault();
-        originalLocale = Locale.getDefault();
-        DateTimeZone.setDefault(LONDON);
-        TimeZone.setDefault(TimeZone.getTimeZone("Europe/London"));
-        Locale.setDefault(Locale.UK);
+    @Test
+    public void testTimeComponents() {
+        assertEquals(0, testDate.getHourOfDay());
+        assertEquals(0, testDate.getMinuteOfHour());
+        assertEquals(0, testDate.getSecondOfMinute());
+        assertEquals(0, testDate.getMillisOfSecond());
     }
 
-    @Override
-    protected void tearDown() throws Exception {
-        DateTimeUtils.setCurrentMillisSystem();
-        DateTimeZone.setDefault(originalDateTimeZone);
-        TimeZone.setDefault(originalTimeZone);
-        Locale.setDefault(originalLocale);
-        originalDateTimeZone = null;
-        originalTimeZone = null;
-        originalLocale = null;
+    @Test
+    public void testYearField_inLeapYear() {
+        Property yearProperty = testDate.year();
+        assertTrue("Year 1426 should be a leap year", yearProperty.isLeap());
+        assertEquals(1, yearProperty.getLeapAmount());
+        assertEquals(DurationFieldType.days(), yearProperty.getLeapDurationField().getType());
     }
 
-    public void testSampleDate3() {
-        DateTime dt = new DateTime(1426, 12, 24, 0, 0, 0, 0, ISLAMIC_UTC);
-        assertEquals(IslamicChronology.AH, dt.getEra());
-        assertEquals(1426, dt.getYear());
-        Property fld = dt.year();
-        assertEquals(true, fld.isLeap());
-        assertEquals(1, fld.getLeapAmount());
-        assertEquals(DurationFieldType.days(), fld.getLeapDurationField().getType());
-        assertEquals(12, dt.getMonthOfYear());
-        fld = dt.monthOfYear();
-        assertEquals(true, fld.isLeap());
-        assertEquals(1, fld.getLeapAmount());
-        assertEquals(DurationFieldType.days(), fld.getLeapDurationField().getType());
-        assertEquals(1, fld.getMinimumValue());
-        assertEquals(1, fld.getMinimumValueOverall());
-        assertEquals(12, fld.getMaximumValue());
-        assertEquals(12, fld.getMaximumValueOverall());
-        assertEquals(24, dt.getDayOfMonth());
-        fld = dt.dayOfMonth();
-        assertEquals(false, fld.isLeap());
-        assertEquals(0, fld.getLeapAmount());
-        assertEquals(null, fld.getLeapDurationField());
-        assertEquals(1, fld.getMinimumValue());
-        assertEquals(1, fld.getMinimumValueOverall());
-        assertEquals(30, fld.getMaximumValue());
-        assertEquals(30, fld.getMaximumValueOverall());
-        assertEquals(DateTimeConstants.TUESDAY, dt.getDayOfWeek());
-        fld = dt.dayOfWeek();
-        assertEquals(false, fld.isLeap());
-        assertEquals(0, fld.getLeapAmount());
-        assertEquals(null, fld.getLeapDurationField());
-        assertEquals(1, fld.getMinimumValue());
-        assertEquals(1, fld.getMinimumValueOverall());
-        assertEquals(7, fld.getMaximumValue());
-        assertEquals(7, fld.getMaximumValueOverall());
-        assertEquals(6 * 30 + 5 * 29 + 24, dt.getDayOfYear());
-        fld = dt.dayOfYear();
-        assertEquals(false, fld.isLeap());
-        assertEquals(0, fld.getLeapAmount());
-        assertEquals(null, fld.getLeapDurationField());
-        assertEquals(1, fld.getMinimumValue());
-        assertEquals(1, fld.getMinimumValueOverall());
-        assertEquals(355, fld.getMaximumValue());
-        assertEquals(355, fld.getMaximumValueOverall());
-        assertEquals(0, dt.getHourOfDay());
-        assertEquals(0, dt.getMinuteOfHour());
-        assertEquals(0, dt.getSecondOfMinute());
-        assertEquals(0, dt.getMillisOfSecond());
+    @Test
+    public void testMonthOfYearField_inLeapYear() {
+        Property monthProperty = testDate.monthOfYear();
+        // The last month of a leap year is also considered "leap" as it has an extra day.
+        assertTrue("Month 12 in a leap year should be considered leap", monthProperty.isLeap());
+        assertEquals(1, monthProperty.getLeapAmount());
+        assertEquals(DurationFieldType.days(), monthProperty.getLeapDurationField().getType());
+        assertEquals(1, monthProperty.getMinimumValue());
+        assertEquals(12, monthProperty.getMaximumValue());
+    }
+
+    @Test
+    public void testDayOfMonthField_inLeapYear() {
+        Property dayOfMonthProperty = testDate.dayOfMonth();
+        assertFalse(dayOfMonthProperty.isLeap());
+        assertNull(dayOfMonthProperty.getLeapDurationField());
+        assertEquals(1, dayOfMonthProperty.getMinimumValue());
+        // In a leap year, the 12th month (Dhu al-Hijjah) has 30 days instead of 29.
+        assertEquals(30, dayOfMonthProperty.getMaximumValue());
+        assertEquals(30, dayOfMonthProperty.getMaximumValueOverall());
+    }
+
+    @Test
+    public void testDayOfWeekField() {
+        Property dayOfWeekProperty = testDate.dayOfWeek();
+        assertFalse(dayOfWeekProperty.isLeap());
+        assertNull(dayOfWeekProperty.getLeapDurationField());
+        assertEquals(1, dayOfWeekProperty.getMinimumValue());
+        assertEquals(7, dayOfWeekProperty.getMaximumValue());
+    }
+
+    @Test
+    public void testDayOfYearField_inLeapYear() {
+        // In the Islamic calendar, odd months have 30 days and even months have 29.
+        // For the first 11 months, there are 6 odd (30 days) and 5 even (29 days).
+        // Day of year = (6 * 30) + (5 * 29) + day_of_12th_month
+        int expectedDayOfYear = (6 * 30) + (5 * 29) + TEST_DAY;
+        assertEquals(expectedDayOfYear, testDate.getDayOfYear());
+
+        Property dayOfYearProperty = testDate.dayOfYear();
+        assertFalse(dayOfYearProperty.isLeap());
+        // A leap year in the Islamic calendar has 355 days.
+        assertEquals(355, dayOfYearProperty.getMaximumValue());
+        assertEquals(355, dayOfYearProperty.getMaximumValueOverall());
     }
 }
