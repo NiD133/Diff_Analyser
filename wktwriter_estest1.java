689c296c3b304bec02d75a07c21d4b95838fe1bd
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:27:36 +0800

    Original processedAutoTestCases wktwriter_estest1.java

---
+++ wktwriter_estest1.java
@@ -0,0 +1,35 @@
+package org.locationtech.spatial4j.io;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.text.ChoiceFormat;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.LinkedList;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+import org.locationtech.spatial4j.context.SpatialContext;
+import org.locationtech.spatial4j.context.SpatialContextFactory;
+import org.locationtech.spatial4j.distance.GeodesicSphereDistCalc;
+import org.locationtech.spatial4j.shape.Circle;
+import org.locationtech.spatial4j.shape.Point;
+import org.locationtech.spatial4j.shape.Rectangle;
+import org.locationtech.spatial4j.shape.Shape;
+import org.locationtech.spatial4j.shape.ShapeCollection;
+import org.locationtech.spatial4j.shape.impl.BufferedLine;
+import org.locationtech.spatial4j.shape.impl.BufferedLineString;
+import org.locationtech.spatial4j.shape.impl.PointImpl;
+
+public class WKTWriter_ESTestTest1 extends WKTWriter_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test00() throws Throwable {
+        WKTWriter wKTWriter0 = new WKTWriter();
+        DecimalFormat decimalFormat0 = (DecimalFormat) wKTWriter0.getNumberFormat();
+        assertEquals("###0.######", decimalFormat0.toLocalizedPattern());
+    }
+}
