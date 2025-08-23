726dc01021286ca0cb82c339bd0f61b9d168da5e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:51:51 +0800

    Original processedAutoTestCases getbufferedrandomaccesssource_estest14.java

---
+++ getbufferedrandomaccesssource_estest14.java
@@ -0,0 +1,30 @@
+package com.itextpdf.text.io;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class GetBufferedRandomAccessSource_ESTestTest14 extends GetBufferedRandomAccessSource_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test13() throws Throwable {
+        WindowRandomAccessSource windowRandomAccessSource0 = new WindowRandomAccessSource((RandomAccessSource) null, (-685L), (-685L));
+        GetBufferedRandomAccessSource getBufferedRandomAccessSource0 = new GetBufferedRandomAccessSource(windowRandomAccessSource0);
+        // Undeclared exception!
+        try {
+            getBufferedRandomAccessSource0.get((-960L));
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("com.itextpdf.text.io.WindowRandomAccessSource", e);
+        }
+    }
+}
