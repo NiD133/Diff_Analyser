709cf38e510a46e933b3a31571197139ae61a063
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:59:10 +0800

    Original processedAutoTestCases jsonwritecontext_estest37.java

---
+++ jsonwritecontext_estest37.java
@@ -0,0 +1,32 @@
+package com.fasterxml.jackson.core.json;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.filter.FilteringGeneratorDelegate;
+import com.fasterxml.jackson.core.filter.TokenFilter;
+import com.fasterxml.jackson.core.util.JsonGeneratorDelegate;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.PipedInputStream;
+import java.io.StringWriter;
+import java.io.Writer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class JsonWriteContext_ESTestTest37 extends JsonWriteContext_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test36() throws Throwable {
+        JsonWriteContext jsonWriteContext0 = JsonWriteContext.createRootContext();
+        JsonWriteContext jsonWriteContext1 = jsonWriteContext0.createChildObjectContext();
+        jsonWriteContext1.writeFieldName("NRN=M&Q");
+        int int0 = jsonWriteContext1.writeFieldName("):<z");
+        assertTrue(jsonWriteContext1.hasCurrentName());
+        assertEquals(4, int0);
+    }
+}
