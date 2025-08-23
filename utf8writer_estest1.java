4e5b1ee5216ae2a87bc0aa98eec42332e4171d40
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:25:31 +0800

    Original processedAutoTestCases utf8writer_estest1.java

---
+++ utf8writer_estest1.java
@@ -0,0 +1,31 @@
+package com.fasterxml.jackson.core.io;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.fasterxml.jackson.core.ErrorReportConfiguration;
+import com.fasterxml.jackson.core.StreamReadConstraints;
+import com.fasterxml.jackson.core.StreamWriteConstraints;
+import com.fasterxml.jackson.core.util.BufferRecycler;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PipedOutputStream;
+import java.io.Writer;
+import java.nio.CharBuffer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockFileOutputStream;
+import org.evosuite.runtime.mock.java.io.MockPrintStream;
+import org.junit.runner.RunWith;
+
+public class UTF8Writer_ESTestTest1 extends UTF8Writer_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test00() throws Throwable {
+        String string0 = UTF8Writer.illegalSurrogateDesc(56319);
+        assertEquals("Unmatched first part of surrogate pair (0xdbff)", string0);
+    }
+}
