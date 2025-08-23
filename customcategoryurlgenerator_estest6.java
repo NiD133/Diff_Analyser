3b555fce031c692b2fc314ddec679ea0164035f6
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:46:26 +0800

    Original processedAutoTestCases customcategoryurlgenerator_estest6.java

---
+++ customcategoryurlgenerator_estest6.java
@@ -0,0 +1,30 @@
+package org.jfree.chart.urls;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+import java.util.Vector;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.general.DefaultKeyedValues2DDataset;
+import org.junit.runner.RunWith;
+
+public class CustomCategoryURLGenerator_ESTestTest6 extends CustomCategoryURLGenerator_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test05() throws Throwable {
+        CustomCategoryURLGenerator customCategoryURLGenerator0 = new CustomCategoryURLGenerator();
+        ArrayList<String> arrayList0 = new ArrayList<String>();
+        customCategoryURLGenerator0.addURLSeries(arrayList0);
+        customCategoryURLGenerator0.addURLSeries((List<String>) null);
+        customCategoryURLGenerator0.addURLSeries((List<String>) null);
+        customCategoryURLGenerator0.addURLSeries((List<String>) null);
+        customCategoryURLGenerator0.addURLSeries(arrayList0);
+        String string0 = customCategoryURLGenerator0.getURL(5, (-1785));
+        assertNull(string0);
+    }
+}
