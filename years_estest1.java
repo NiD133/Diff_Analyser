79b7e181ced0e083466773afa7aba94ec42e69a3
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:30:10 +0800

    gemini processedAutoTestCases years_estest1.java

--- years_estest1.java
+++ years_estest1.java
@@ -1,19 +1,21 @@
 package org.joda.time;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertFalse;
 
-public class Years_ESTestTest1 extends Years_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link Years} class.
+ */
+public class YearsTest {
 
-    @Test(timeout = 4000)
-    public void test00() throws Throwable {
-        Years years0 = Years.THREE;
-        Years years1 = Years.MAX_VALUE;
-        boolean boolean0 = years1.isLessThan(years0);
-        assertFalse(boolean0);
+    @Test
+    public void isLessThan_shouldReturnFalse_whenComparingLargerToSmaller() {
+        // Arrange
+        Years largerYears = Years.MAX_VALUE;
+        Years smallerYears = Years.THREE;
+
+        // Act & Assert
+        assertFalse("MAX_VALUE years should not be considered less than THREE years",
+                largerYears.isLessThan(smallerYears));
     }
 }
