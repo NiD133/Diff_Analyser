d6a28cc85dae95db8513134077ecb4aac36e052c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:09:23 +0800

    gemini processedAutoTestCases predicatedmap_estest14.java

--- predicatedmap_estest14.java
+++ predicatedmap_estest14.java
@@ -1,54 +1,46 @@
 package org.apache.commons.collections4.map;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.Comparator;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.Map;
 import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionPredicate;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NotNullPredicate;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.OnePredicate;
 import org.apache.commons.collections4.functors.OrPredicate;
-import org.apache.commons.collections4.functors.PredicateTransformer;
-import org.apache.commons.collections4.functors.TransformerPredicate;
-import org.apache.commons.collections4.functors.TruePredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class PredicatedMap_ESTestTest14 extends PredicatedMap_ESTest_scaffolding {
+/**
+ * Tests for {@link PredicatedMap} focusing on its behavior when predicates throw exceptions.
+ */
+public class PredicatedMapTest {
 
-    @Test(timeout = 4000)
-    public void test13() throws Throwable {
-        HashMap<Object, HashMap<Integer, Integer>> hashMap0 = new HashMap<Object, HashMap<Integer, Integer>>();
-        HashMap<Predicate<Object>, Object> hashMap1 = new HashMap<Predicate<Object>, Object>();
-        OrPredicate<Object> orPredicate0 = new OrPredicate<Object>((Predicate<? super Object>) null, (Predicate<? super Object>) null);
-        PredicatedMap<Predicate<Object>, Object> predicatedMap0 = new PredicatedMap<Predicate<Object>, Object>(hashMap1, orPredicate0, orPredicate0);
-        // Undeclared exception!
+    /**
+     * Tests that PredicatedMap.put() propagates a NullPointerException thrown by a faulty key predicate.
+     */
+    @Test
+    public void putWithFaultyKeyPredicateShouldPropagateNPE() {
+        // Arrange
+        // An OrPredicate constructed with null sub-predicates will throw a NullPointerException
+        // when its `evaluate` method is called. This simulates a faulty predicate.
+        final Predicate<Object> faultyKeyPredicate = new OrPredicate<>(null, null);
+        final Map<Object, Object> baseMap = new HashMap<>();
+
+        // Create a PredicatedMap that uses the faulty predicate for key validation.
+        // The value predicate is not relevant for this test, so it can be null.
+        final Map<Object, Object> predicatedMap = new PredicatedMap<>(baseMap, faultyKeyPredicate, null);
+
+        // Act & Assert
         try {
-            predicatedMap0.put(orPredicate0, hashMap0);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.commons.collections4.functors.OrPredicate", e);
+            // Attempting to add any entry should trigger the faulty key predicate.
+            predicatedMap.put("any key", "any value");
+            fail("A NullPointerException should have been thrown by the faulty key predicate.");
+        } catch (final NullPointerException e) {
+            // This is the expected outcome.
+            // For a more robust test, we verify that the exception originates from the
+            // OrPredicate, confirming the predicate's internal error is being propagated.
+            // This check replaces the original test's non-standard `verifyException` call.
+            final String throwingClassName = e.getStackTrace()[0].getClassName();
+            assertEquals("org.apache.commons.collections4.functors.OrPredicate", throwingClassName);
         }
     }
 }
