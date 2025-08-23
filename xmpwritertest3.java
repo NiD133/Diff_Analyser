c391388547da19deef43ae93508feca35c4a87e0
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:36:20 +0800

    Original processedManualTestCases xmpwritertest3.java

---
+++ xmpwritertest3.java
@@ -0,0 +1,44 @@
+package com.itextpdf.text.xml.xmp;
+
+import com.itextpdf.testutils.CompareTool;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.*;
+import com.itextpdf.xmp.XMPException;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class XmpWriterTestTest3 {
+
+    public static final String OUT_FOLDER = "./target/com/itextpdf/text/xml/xmp/";
+
+    public static final String CMP_FOLDER = "./src/test/resources/com/itextpdf/text/xml/xmp/";
+
+    @Before
+    public void init() {
+        new File(OUT_FOLDER).mkdirs();
+    }
+
+    @Test
+    public void manipulatePdfTest() throws IOException, DocumentException {
+        String fileName = "xmp_metadata_added.pdf";
+        PdfReader reader = new PdfReader(CMP_FOLDER + "pdf_metadata.pdf");
+        PdfStamper stamper = new PdfStamper(reader, new FileOutputStream(OUT_FOLDER + fileName));
+        HashMap<String, String> info = reader.getInfo();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        XmpWriter xmp = new XmpWriter(baos, info);
+        xmp.close();
+        stamper.setXmpMetadata(baos.toByteArray());
+        stamper.close();
+        reader.close();
+        CompareTool ct = new CompareTool();
+        Assert.assertNull(ct.compareXmp(OUT_FOLDER + fileName, CMP_FOLDER + fileName, true));
+    }
+}
