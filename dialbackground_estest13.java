95c4cdc707f4695a4846ecf0af4038d2c2a5a111
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:48:04 +0800

    gemini processedAutoTestCases dialbackground_estest13.java

--- dialbackground_estest13.java
+++ dialbackground_estest13.java
@@ -1,34 +1,35 @@
 package org.jfree.chart.plot.dial;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.awt.Color;
-import java.awt.GradientPaint;
-import java.awt.Graphics2D;
-import java.awt.Paint;
-import java.awt.Rectangle;
-import java.awt.geom.Rectangle2D;
-import java.awt.image.BufferedImage;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.jfree.chart.ChartRenderingInfo;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.entity.EntityCollection;
-import org.jfree.chart.plot.FastScatterPlot;
-import org.jfree.chart.plot.pie.PiePlot;
 import org.jfree.chart.util.GradientPaintTransformType;
 import org.jfree.chart.util.GradientPaintTransformer;
 import org.jfree.chart.util.StandardGradientPaintTransformer;
-import org.jfree.data.general.DefaultValueDataset;
-import org.junit.runner.RunWith;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+/**
+ * Tests for the {@link DialBackground} class, focusing on its default state.
+ */
 public class DialBackground_ESTestTest13 extends DialBackground_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test12() throws Throwable {
-        DialBackground dialBackground0 = new DialBackground();
-        StandardGradientPaintTransformer standardGradientPaintTransformer0 = (StandardGradientPaintTransformer) dialBackground0.getGradientPaintTransformer();
-        assertEquals(GradientPaintTransformType.VERTICAL, standardGradientPaintTransformer0.getType());
+    /**
+     * Verifies that the default constructor initializes a StandardGradientPaintTransformer
+     * with a VERTICAL transform type.
+     */
+    @Test
+    public void constructor_shouldSetDefaultGradientPaintTransformerToVertical() {
+        // Arrange: Create a DialBackground instance using its default constructor.
+        DialBackground dialBackground = new DialBackground();
+
+        // Act: Retrieve the gradient paint transformer created by default.
+        GradientPaintTransformer transformer = dialBackground.getGradientPaintTransformer();
+
+        // Assert: Check that the transformer is the expected type and has the correct default setting.
+        assertTrue("The transformer should be an instance of StandardGradientPaintTransformer.",
+                transformer instanceof StandardGradientPaintTransformer);
+
+        StandardGradientPaintTransformer standardTransformer = (StandardGradientPaintTransformer) transformer;
+        assertEquals("The default transformer type should be VERTICAL.",
+                GradientPaintTransformType.VERTICAL, standardTransformer.getType());
     }
 }
