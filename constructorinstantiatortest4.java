80ea7c2526c417c8be9a399d45da8cabd2010ad0
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:27:03 +0800

    gemini processedManualTestCases constructorinstantiatortest4.java

--- constructorinstantiatortest4.java
+++ constructorinstantiatortest4.java
@@ -1,36 +1,36 @@
 package org.mockito.internal.creation.instance;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.Assert.fail;
 import org.junit.Test;
+import org.mockito.creation.instance.Instantiator;
 import org.mockitoutil.TestBase;
 
-public class ConstructorInstantiatorTestTest4 extends TestBase {
+import static org.assertj.core.api.Assertions.assertThat;
 
-    static class SomeClass {
-    }
+/**
+ * Tests for {@link ConstructorInstantiator}.
+ */
+public class ConstructorInstantiatorTest extends TestBase {
 
-    class SomeInnerClass {
-    }
-
-    class ChildOfThis extends ConstructorInstantiatorTest {
-    }
-
-    static class SomeClass2 {
-
-        SomeClass2(String x) {
-        }
-    }
-
-    static class SomeClass3 {
-
-        SomeClass3(int i) {
+    // A helper class with a constructor that accepts a single reference type.
+    // This is used to verify that the instantiator can match it with a null argument.
+    static class ClassWithSingleReferenceConstructor {
+        ClassWithSingleReferenceConstructor(String anyString) {
+            // The constructor logic is not relevant for this test.
         }
     }
 
     @Test
-    public void creates_instances_with_null_arguments() {
-        assertThat(new ConstructorInstantiator(false, new Object[] { null }).newInstance(SomeClass2.class).getClass()).isEqualTo(SomeClass2.class);
+    public void shouldInstantiateClassWhenConstructorArgumentIsNull() {
+        // Arrange: Create an instantiator configured to use a single 'null' argument.
+        // The 'false' flag indicates that the target class is not an inner class
+        // that requires an outer instance.
+        Instantiator instantiator = new ConstructorInstantiator(false, new Object[]{null});
+
+        // Act: Attempt to create an instance of a class whose constructor accepts a reference type.
+        Object instance = instantiator.newInstance(ClassWithSingleReferenceConstructor.class);
+
+        // Assert: The instantiator should successfully create the instance because 'null'
+        // is a valid value for any reference type parameter (like String).
+        assertThat(instance).isInstanceOf(ClassWithSingleReferenceConstructor.class);
     }
 }
