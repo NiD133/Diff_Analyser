72808b6ae2181a4ce1662a685e8448305738e004
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:10:07 +0800

    Original processedAutoTestCases queryparser_estest37.java

---
+++ queryparser_estest37.java
@@ -0,0 +1,25 @@
+package org.jsoup.select;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class QueryParser_ESTestTest37 extends QueryParser_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test36() throws Throwable {
+        // Undeclared exception!
+        try {
+            QueryParser.parse(":lt(%d)");
+            fail("Expecting exception: IllegalStateException");
+        } catch (IllegalStateException e) {
+            //
+            // Index must be numeric
+            //
+            verifyException("org.jsoup.select.QueryParser", e);
+        }
+    }
+}
