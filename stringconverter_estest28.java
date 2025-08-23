745554a283e760063ba2002956d50b99c975a218
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:18:27 +0800

    Original processedAutoTestCases stringconverter_estest28.java

---
+++ stringconverter_estest28.java
@@ -0,0 +1,51 @@
+package org.joda.time.convert;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.joda.time.Chronology;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeFieldType;
+import org.joda.time.DateTimeZone;
+import org.joda.time.LocalDate;
+import org.joda.time.MonthDay;
+import org.joda.time.MutableDateTime;
+import org.joda.time.MutableInterval;
+import org.joda.time.MutablePeriod;
+import org.joda.time.Partial;
+import org.joda.time.PeriodType;
+import org.joda.time.ReadWritableInterval;
+import org.joda.time.ReadWritablePeriod;
+import org.joda.time.ReadableInstant;
+import org.joda.time.ReadablePartial;
+import org.joda.time.chrono.CopticChronology;
+import org.joda.time.chrono.EthiopicChronology;
+import org.joda.time.chrono.GJChronology;
+import org.joda.time.chrono.GregorianChronology;
+import org.joda.time.chrono.ISOChronology;
+import org.joda.time.chrono.IslamicChronology;
+import org.joda.time.chrono.JulianChronology;
+import org.joda.time.chrono.ZonedChronology;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.DateTimeParser;
+import org.joda.time.format.DateTimePrinter;
+import org.junit.runner.RunWith;
+
+public class StringConverter_ESTestTest28 extends StringConverter_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test27() throws Throwable {
+        StringConverter stringConverter0 = new StringConverter();
+        MutableDateTime mutableDateTime0 = new MutableDateTime(0L);
+        DateTime dateTime0 = mutableDateTime0.toDateTimeISO();
+        MutablePeriod mutablePeriod0 = new MutablePeriod(dateTime0, dateTime0, (PeriodType) null);
+        DateTimeZone dateTimeZone0 = DateTimeZone.UTC;
+        GJChronology gJChronology0 = GJChronology.getInstance(dateTimeZone0, (ReadableInstant) mutableDateTime0);
+        ZonedChronology zonedChronology0 = ZonedChronology.getInstance(gJChronology0, dateTimeZone0);
+        stringConverter0.setInto((ReadWritablePeriod) mutablePeriod0, (Object) "Pt2s", (Chronology) zonedChronology0);
+    }
+}
