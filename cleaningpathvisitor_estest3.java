5227c0b78483a596c329aa7106dd910b6ad24190
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:43:35 +0800

    Original processedAutoTestCases cleaningpathvisitor_estest3.java

---
+++ cleaningpathvisitor_estest3.java
@@ -0,0 +1,38 @@
+package org.apache.commons.io.file;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.junit.runner.RunWith;
+
+public class CleaningPathVisitor_ESTestTest3 extends CleaningPathVisitor_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test02() throws Throwable {
+        Counters.PathCounters counters_PathCounters0 = CountingPathVisitor.defaultPathCounters();
+        DeleteOption[] deleteOptionArray0 = new DeleteOption[2];
+        String[] stringArray0 = new String[3];
+        CleaningPathVisitor cleaningPathVisitor0 = null;
+        try {
+            cleaningPathVisitor0 = new CleaningPathVisitor(counters_PathCounters0, deleteOptionArray0, stringArray0);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+        }
+    }
+}
