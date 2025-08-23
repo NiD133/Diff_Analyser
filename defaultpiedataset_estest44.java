29b017279d70b79c2f8f6ab4588f4d0e855ba898
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:47:48 +0800

    Original processedAutoTestCases defaultpiedataset_estest44.java

---
+++ defaultpiedataset_estest44.java
@@ -0,0 +1,31 @@
+package org.jfree.data.general;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.time.chrono.ChronoLocalDate;
+import java.util.List;
+import java.util.Vector;
+import javax.swing.JLayeredPane;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.jfree.chart.api.SortOrder;
+import org.jfree.chart.api.TableOrder;
+import org.jfree.data.DefaultKeyedValues;
+import org.jfree.data.KeyedValues;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.category.CategoryToPieDataset;
+import org.jfree.data.category.DefaultIntervalCategoryDataset;
+import org.jfree.data.category.SlidingCategoryDataset;
+import org.jfree.data.statistics.DefaultBoxAndWhiskerCategoryDataset;
+import org.junit.runner.RunWith;
+
+public class DefaultPieDataset_ESTestTest44 extends DefaultPieDataset_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test43() throws Throwable {
+        DefaultKeyedValuesDataset<Integer> defaultKeyedValuesDataset0 = new DefaultKeyedValuesDataset<Integer>();
+        DefaultPieDataset defaultPieDataset0 = (DefaultPieDataset) defaultKeyedValuesDataset0.clone();
+        assertTrue(defaultPieDataset0.getNotify());
+    }
+}
