7aa31819fff8e3b1f3d84ac2f89b668bd5cee366
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:03:21 +0800

    gemini processedAutoTestCases minutes_estest6.java

--- minutes_estest6.java
+++ minutes_estest6.java
@@ -1,18 +1,35 @@
 package org.joda.time;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class Minutes_ESTestTest6 extends Minutes_ESTest_scaffolding {
+/**
+ * Test suite for the {@link Minutes} class.
+ */
+public class MinutesTest {
 
-    @Test(timeout = 4000)
-    public void test05() throws Throwable {
-        Minutes minutes0 = Minutes.MIN_VALUE;
-        Weeks weeks0 = minutes0.toStandardWeeks();
-        assertEquals((-213044), weeks0.getWeeks());
+    /**
+     * Tests that converting the minimum possible number of minutes to weeks
+     * results in the correct, large negative number of weeks.
+     */
+    @Test
+    public void toStandardWeeks_whenConvertingMinValue_calculatesCorrectNegativeWeeks() {
+        // Arrange
+        // The number of minutes in a standard week is 7 days * 24 hours * 60 minutes.
+        final int MINUTES_PER_WEEK = 10080;
+        
+        // The expected result is Integer.MIN_VALUE / MINUTES_PER_WEEK.
+        // Joda-Time uses integer division, so the fractional part is truncated.
+        // -2,147,483,648 / 10,080 = -213,044
+        final int expectedWeeks = Integer.MIN_VALUE / MINUTES_PER_WEEK;
+        
+        Minutes minMinutes = Minutes.MIN_VALUE;
+
+        // Act
+        Weeks actualWeeks = minMinutes.toStandardWeeks();
+
+        // Assert
+        assertEquals("The number of weeks should be the result of integer division.",
+                expectedWeeks, actualWeeks.getWeeks());
     }
 }
