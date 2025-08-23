bd6f7e2cc59c83f7a19adbf3a8c801e015ded77d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:26:28 +0800

    Original processedManualTestCases charrangetest1.java

---
+++ charrangetest1.java
@@ -0,0 +1,23 @@
+package org.apache.commons.lang3;
+
+import static org.apache.commons.lang3.LangAssertions.assertNullPointerException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.lang.reflect.Modifier;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import org.junit.jupiter.api.Test;
+
+public class CharRangeTestTest1 extends AbstractLangTest {
+
+    @Test
+    void testClass() {
+        // class changed to non-public in 3.0
+        assertFalse(Modifier.isPublic(CharRange.class.getModifiers()));
+        assertTrue(Modifier.isFinal(CharRange.class.getModifiers()));
+    }
+}
