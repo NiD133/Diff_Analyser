c4b07ef17a79b35c1e3f264628dc626f44d1b946
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:13:12 +0800

    gemini processedAutoTestCases seconds_estest67.java

--- seconds_estest67.java
+++ seconds_estest67.java
@@ -1,18 +1,28 @@
 package org.joda.time;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class Seconds_ESTestTest67 extends Seconds_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link Seconds} class, focusing on period conversions.
+ */
+public class SecondsTest {
 
-    @Test(timeout = 4000)
-    public void test66() throws Throwable {
-        Seconds seconds0 = Seconds.TWO;
-        Weeks weeks0 = seconds0.toStandardWeeks();
-        assertEquals(0, weeks0.getWeeks());
+    /**
+     * Tests that converting a Seconds value that is less than a full week
+     * into standard weeks results in zero weeks.
+     */
+    @Test
+    public void toStandardWeeks_whenSecondsIsLessThanAWeek_returnsZeroWeeks() {
+        // Arrange: A standard week contains 604,800 seconds. We use a value of two seconds,
+        // which is clearly less than a full week.
+        Seconds twoSeconds = Seconds.TWO;
+        Weeks expectedWeeks = Weeks.ZERO;
+
+        // Act: Convert the Seconds object to standard weeks.
+        Weeks actualWeeks = twoSeconds.toStandardWeeks();
+
+        // Assert: The result should be equivalent to Weeks.ZERO.
+        assertEquals(expectedWeeks, actualWeeks);
     }
 }
