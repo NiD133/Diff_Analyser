75f38c44cabc6177fa63c9dff4a59792a94adf85
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:37:14 +0800

    gemini processedAutoTestCases appendablejoiner_estest5.java

--- appendablejoiner_estest5.java
+++ appendablejoiner_estest5.java
@@ -1,41 +1,31 @@
 package org.apache.commons.lang3;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.IOException;
-import java.io.PipedWriter;
-import java.nio.BufferOverflowException;
-import java.nio.CharBuffer;
-import java.nio.ReadOnlyBufferException;
-import java.nio.charset.Charset;
-import java.sql.SQLNonTransientConnectionException;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.EnumSet;
-import java.util.LinkedHashSet;
-import java.util.Locale;
 import org.apache.commons.lang3.function.FailableBiConsumer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class AppendableJoiner_ESTestTest5 extends AppendableJoiner_ESTest_scaffolding {
+import java.io.IOException;
 
-    @Test(timeout = 4000)
-    public void test04() throws Throwable {
-        StringBuilder stringBuilder0 = new StringBuilder();
-        FailableBiConsumer<Appendable, StringBuilder, IOException> failableBiConsumer0 = FailableBiConsumer.nop();
-        StringBuilder[] stringBuilderArray0 = new StringBuilder[0];
-        // Undeclared exception!
-        try {
-            AppendableJoiner.joinSB((StringBuilder) null, (CharSequence) stringBuilder0, (CharSequence) null, (CharSequence) stringBuilder0, failableBiConsumer0, stringBuilderArray0);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.commons.lang3.AppendableJoiner", e);
-        }
+/**
+ * Unit tests for the static helper methods in {@link AppendableJoiner}.
+ */
+public class AppendableJoinerTest {
+
+    /**
+     * Tests that {@link AppendableJoiner#joinSB(StringBuilder, CharSequence, CharSequence, CharSequence, FailableBiConsumer, Object...)}
+     * throws a NullPointerException when the target StringBuilder is null.
+     */
+    @Test(expected = NullPointerException.class)
+    public void joinSBShouldThrowNullPointerExceptionWhenTargetStringBuilderIsNull() {
+        // Arrange: Prepare arguments for the method call. The specific values of the
+        // prefix, suffix, etc., are not relevant for this test.
+        final FailableBiConsumer<Appendable, String, IOException> noOpAppender = FailableBiConsumer.nop();
+        final String[] emptyElements = {};
+
+        // Act: Call the method with a null StringBuilder as the target.
+        // This is expected to throw a NullPointerException.
+        AppendableJoiner.joinSB(null, "", "", "", noOpAppender, emptyElements);
+
+        // Assert: The test succeeds if a NullPointerException is thrown,
+        // as specified by the @Test(expected=...) annotation.
     }
 }
