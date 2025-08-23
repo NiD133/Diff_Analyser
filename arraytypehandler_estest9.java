382daa331f4ebf7dc36b1a5b80135a51e7545cea
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:37:47 +0800

    Original processedAutoTestCases arraytypehandler_estest9.java

---
+++ arraytypehandler_estest9.java
@@ -0,0 +1,33 @@
+package org.apache.ibatis.type;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.sql.Array;
+import java.sql.CallableStatement;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.Month;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.time.MockInstant;
+import org.evosuite.runtime.mock.java.time.MockLocalDate;
+import org.junit.runner.RunWith;
+
+public class ArrayTypeHandler_ESTestTest9 extends ArrayTypeHandler_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test08() throws Throwable {
+        ArrayTypeHandler arrayTypeHandler0 = new ArrayTypeHandler();
+        Class<java.util.Date> class0 = java.util.Date.class;
+        String string0 = arrayTypeHandler0.resolveTypeName(class0);
+        assertEquals("TIMESTAMP", string0);
+    }
+}
