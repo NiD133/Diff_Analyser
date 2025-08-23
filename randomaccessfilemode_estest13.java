d8d4d49e535bf31ba6020202f65266f71832bcc9
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:10:57 +0800

    gemini processedAutoTestCases randomaccessfilemode_estest13.java

--- randomaccessfilemode_estest13.java
+++ randomaccessfilemode_estest13.java
@@ -1,37 +1,19 @@
 package org.apache.commons.io;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.RandomAccessFile;
-import java.nio.file.OpenOption;
-import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
-import org.apache.commons.io.function.IOConsumer;
-import org.apache.commons.io.function.IOFunction;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.io.MockFile;
-import org.evosuite.runtime.testdata.EvoSuiteFile;
-import org.evosuite.runtime.testdata.FileSystemHandling;
-import org.junit.runner.RunWith;
 
-public class RandomAccessFileMode_ESTestTest13 extends RandomAccessFileMode_ESTest_scaffolding {
+/**
+ * Tests for {@link RandomAccessFileMode#valueOfMode(String)}.
+ */
+public class RandomAccessFileMode_ESTestTest13 { // Note: Class name kept for consistency with the original file.
 
-    @Test(timeout = 4000)
-    public void test12() throws Throwable {
-        // Undeclared exception!
-        try {
-            RandomAccessFileMode.valueOfMode((String) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.commons.io.RandomAccessFileMode", e);
-        }
+    /**
+     * Tests that {@link RandomAccessFileMode#valueOfMode(String)} throws a
+     * {@link NullPointerException} when the input string is null.
+     */
+    @Test(expected = NullPointerException.class, timeout = 4000)
+    public void valueOfModeShouldThrowNullPointerExceptionForNullInput() {
+        // This call is expected to throw a NullPointerException.
+        RandomAccessFileMode.valueOfMode(null);
     }
 }
