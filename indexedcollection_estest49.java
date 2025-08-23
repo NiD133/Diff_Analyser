af52cb9e3ca5351db3a8a5ddcb61841039d76b7a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:54:05 +0800

    gemini processedAutoTestCases indexedcollection_estest49.java

--- indexedcollection_estest49.java
+++ indexedcollection_estest49.java
@@ -1,66 +1,44 @@
 package org.apache.commons.collections4.collection;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.ConcurrentModificationException;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Set;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.Predicate;
 import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AllPredicate;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ChainedTransformer;
-import org.apache.commons.collections4.functors.CloneTransformer;
-import org.apache.commons.collections4.functors.ClosureTransformer;
-import org.apache.commons.collections4.functors.ConstantFactory;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.DefaultEquator;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.FactoryTransformer;
-import org.apache.commons.collections4.functors.FalsePredicate;
-import org.apache.commons.collections4.functors.ForClosure;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.NOPClosure;
-import org.apache.commons.collections4.functors.NOPTransformer;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NotNullPredicate;
-import org.apache.commons.collections4.functors.NullIsFalsePredicate;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.SwitchTransformer;
-import org.apache.commons.collections4.functors.TransformedPredicate;
-import org.apache.commons.collections4.functors.TransformerClosure;
-import org.apache.commons.collections4.functors.TransformerPredicate;
-import org.apache.commons.collections4.functors.TruePredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class IndexedCollection_ESTestTest49 extends IndexedCollection_ESTest_scaffolding {
+import java.util.Collection;
+import java.util.LinkedList;
 
-    @Test(timeout = 4000)
-    public void test48() throws Throwable {
-        LinkedList<Object> linkedList0 = new LinkedList<Object>();
-        Transformer<Object, Object> transformer0 = ConstantTransformer.nullTransformer();
-        linkedList0.add((Object) transformer0);
-        IndexedCollection<Object, Object> indexedCollection0 = IndexedCollection.uniqueIndexedCollection((Collection<Object>) linkedList0, transformer0);
-        // Undeclared exception!
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+/**
+ * Test suite for {@link IndexedCollection}.
+ */
+public class IndexedCollectionTest {
+
+    /**
+     * Tests that adding an element which maps to an already existing key
+     * in a unique IndexedCollection throws an IllegalArgumentException.
+     */
+    @Test
+    public void addShouldThrowExceptionOnDuplicateKeyInUniqueCollection() {
+        // Arrange
+        // Use a transformer that generates a key from an object's property (e.g., String length).
+        final Transformer<String, Integer> keyTransformer = String::length;
+        final Collection<String> sourceCollection = new LinkedList<>();
+        sourceCollection.add("Apple"); // This element will have a key of 5.
+
+        final IndexedCollection<Integer, String> uniqueIndexedCollection =
+                IndexedCollection.uniqueIndexedCollection(sourceCollection, keyTransformer);
+
+        // This new element also has a length of 5, which creates a key collision.
+        final String elementWithDuplicateKey = "Grape";
+
+        // Act & Assert
         try {
-            indexedCollection0.add(linkedList0);
-            fail("Expecting exception: IllegalArgumentException");
-        } catch (IllegalArgumentException e) {
-            //
-            // Duplicate key in uniquely indexed collection.
-            //
-            verifyException("org.apache.commons.collections4.collection.IndexedCollection", e);
+            uniqueIndexedCollection.add(elementWithDuplicateKey);
+            fail("Expected an IllegalArgumentException to be thrown for a duplicate key.");
+        } catch (final IllegalArgumentException e) {
+            // Verify the exception and its message are correct.
+            assertEquals("Duplicate key in uniquely indexed collection.", e.getMessage());
         }
     }
 }
