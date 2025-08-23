96c41b69e548b2f184ae8b6fef428e58f78868be
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:09:05 +0800

    gemini processedAutoTestCases plotrenderinginfo_estest6.java

--- plotrenderinginfo_estest6.java
+++ plotrenderinginfo_estest6.java
@@ -1,31 +1,36 @@
 package org.jfree.chart.plot;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.awt.Dimension;
-import java.awt.Point;
-import java.awt.Rectangle;
-import java.awt.geom.Line2D;
-import java.awt.geom.Point2D;
-import java.awt.geom.Rectangle2D;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
 import org.jfree.chart.ChartRenderingInfo;
-import org.jfree.chart.entity.EntityCollection;
-import org.jfree.chart.entity.StandardEntityCollection;
-import org.junit.runner.RunWith;
+import org.junit.Test;
+import java.awt.Rectangle;
+import java.awt.geom.Rectangle2D;
+import static org.junit.Assert.assertEquals;
 
-public class PlotRenderingInfo_ESTestTest6 extends PlotRenderingInfo_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link PlotRenderingInfo} class.
+ */
+public class PlotRenderingInfoTest {
 
-    @Test(timeout = 4000)
-    public void test05() throws Throwable {
-        ChartRenderingInfo chartRenderingInfo0 = new ChartRenderingInfo((EntityCollection) null);
-        PlotRenderingInfo plotRenderingInfo0 = chartRenderingInfo0.getPlotInfo();
-        Dimension dimension0 = new Dimension(1595, 2);
-        Rectangle rectangle0 = new Rectangle(dimension0);
-        plotRenderingInfo0.setPlotArea(rectangle0);
-        Rectangle2D rectangle2D0 = plotRenderingInfo0.getPlotArea();
-        assertEquals(1595.0, rectangle2D0.getWidth(), 0.01);
+    /**
+     * Verifies that getPlotArea() correctly returns the Rectangle2D object
+     * that was previously set via setPlotArea().
+     */
+    @Test
+    public void getPlotArea_shouldReturnRectangleSetBySetPlotArea() {
+        // Arrange
+        final int plotWidth = 1595;
+        final int plotHeight = 2;
+        
+        ChartRenderingInfo chartInfo = new ChartRenderingInfo(null);
+        PlotRenderingInfo plotInfo = chartInfo.getPlotInfo();
+        Rectangle2D expectedPlotArea = new Rectangle(plotWidth, plotHeight);
+
+        // Act
+        plotInfo.setPlotArea(expectedPlotArea);
+        Rectangle2D actualPlotArea = plotInfo.getPlotArea();
+
+        // Assert
+        assertEquals("The retrieved plot area should be identical to the one that was set.",
+                expectedPlotArea, actualPlotArea);
     }
 }
