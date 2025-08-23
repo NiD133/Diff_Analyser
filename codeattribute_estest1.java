9cecc25f331d683d157f4dd502aa3a46462a4b54
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:43:45 +0800

    gemini processedAutoTestCases codeattribute_estest1.java

--- codeattribute_estest1.java
+++ codeattribute_estest1.java
@@ -1,34 +1,75 @@
 package org.apache.commons.compress.harmony.unpack200.bytecode;
 
+import org.apache.commons.compress.harmony.pack200.Pack200Exception;
+import org.apache.commons.compress.harmony.unpack200.Segment;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.PipedOutputStream;
+
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
-import org.apache.commons.compress.harmony.unpack200.Segment;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.io.MockFileOutputStream;
-import org.junit.runner.RunWith;
 
-public class CodeAttribute_ESTestTest1 extends CodeAttribute_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
 
-    @Test(timeout = 4000)
-    public void test00() throws Throwable {
-        byte[] byteArray0 = new byte[4];
-        OperandManager operandManager0 = new OperandManager((int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null, (int[]) null);
-        LinkedList<ExceptionTableEntry> linkedList0 = new LinkedList<ExceptionTableEntry>();
-        CodeAttribute codeAttribute0 = new CodeAttribute((-2553), 85, byteArray0, (Segment) null, operandManager0, linkedList0);
-        CPUTF8 cPUTF8_0 = new CPUTF8("i2f", 38);
-        CPClass cPClass0 = new CPClass(cPUTF8_0, 85);
-        ExceptionTableEntry exceptionTableEntry0 = new ExceptionTableEntry(87, 38, 87, cPClass0);
-        linkedList0.add(exceptionTableEntry0);
-        int int0 = codeAttribute0.getLength();
-        assertEquals(85, codeAttribute0.maxLocals);
-        assertEquals((-2553), codeAttribute0.maxStack);
-        assertEquals(24, int0);
+/**
+ * Tests for {@link CodeAttribute}.
+ */
+public class CodeAttributeTest {
+
+    /**
+     * Tests that the getLength() method correctly calculates the total length of the
+     * CodeAttribute, including the header, the bytecode, and a non-empty exception table.
+     */
+    @Test
+    public void getLengthShouldCalculateCorrectlyForCodeWithExceptionTable() throws Pack200Exception {
+        // Arrange
+        final int maxStack = -2553; // An arbitrary value for testing.
+        final int maxLocals = 85;
+        // A simple bytecode sequence (4 NOP instructions).
+        final byte[] byteCodeBytes = {0x00, 0x00, 0x00, 0x00};
+
+        // The OperandManager is required by the constructor. For simple bytecodes like NOP,
+        // it is not used, so providing nulls is sufficient for this test.
+        final OperandManager operandManager = new OperandManager(
+            null, null, null, null, null, null, null, null, null, null,
+            null, null, null, null, null, null, null, null, null, null, null);
+
+        // Create an exception table with a single entry.
+        final List<ExceptionTableEntry> exceptionTable = new LinkedList<>();
+        final CPUTF8 exceptionName = new CPUTF8("java/lang/Exception", 0);
+        final CPClass exceptionClass = new CPClass(exceptionName, 0);
+        exceptionTable.add(new ExceptionTableEntry(87, 38, 87, exceptionClass));
+
+        final CodeAttribute codeAttribute = new CodeAttribute(
+            maxStack,
+            maxLocals,
+            byteCodeBytes,
+            null, // Segment is not needed for this test.
+            operandManager,
+            exceptionTable
+        );
+
+        /*
+         * The expected length of a Code attribute is calculated based on the JVM specification:
+         *   - max_stack (u2)                  :  2 bytes
+         *   - max_locals (u2)                 :  2 bytes
+         *   - code_length (u4)                :  4 bytes
+         *   - code (byte[code_length])        :  4 bytes (for our 4 NOPs)
+         *   - exception_table_length (u2)     :  2 bytes
+         *   - exception_table (1 entry * 8B)  :  8 bytes
+         *   - attributes_count (u2)           :  2 bytes (for sub-attributes, none in this case)
+         *   --------------------------------------------------
+         *   Total                             : 24 bytes
+         */
+        final int expectedLength = 24;
+
+        // Act
+        final int actualLength = codeAttribute.getLength();
+
+        // Assert
+        assertEquals("The calculated attribute length should be correct.", expectedLength, actualLength);
+
+        // Also verify that constructor parameters were stored correctly.
+        assertEquals("Max stack should be set correctly.", maxStack, codeAttribute.maxStack);
+        assertEquals("Max locals should be set correctly.", maxLocals, codeAttribute.maxLocals);
     }
 }
