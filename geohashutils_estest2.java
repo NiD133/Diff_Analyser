79c34deda1377928460b75e10fbbe6209f8a14e5
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:51:46 +0800

    Original processedAutoTestCases geohashutils_estest2.java

---
+++ geohashutils_estest2.java
@@ -0,0 +1,21 @@
+package org.locationtech.spatial4j.io;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.HashMap;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+import org.locationtech.spatial4j.context.SpatialContext;
+import org.locationtech.spatial4j.context.SpatialContextFactory;
+import org.locationtech.spatial4j.shape.Point;
+import org.locationtech.spatial4j.shape.Rectangle;
+
+public class GeohashUtils_ESTestTest2 extends GeohashUtils_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test01() throws Throwable {
+        GeohashUtils.encodeLatLon((double) 13, (double) 13, 0);
+    }
+}
