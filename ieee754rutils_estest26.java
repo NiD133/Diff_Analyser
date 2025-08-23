c87b176fa5f7f70ba009204cc9a783d9d5351f6f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:53:30 +0800

    Original processedAutoTestCases ieee754rutils_estest26.java

---
+++ ieee754rutils_estest26.java
@@ -0,0 +1,25 @@
+package org.apache.commons.lang3.math;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class IEEE754rUtils_ESTestTest26 extends IEEE754rUtils_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test25() throws Throwable {
+        // Undeclared exception!
+        try {
+            IEEE754rUtils.min((double[]) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // array
+            //
+            verifyException("java.util.Objects", e);
+        }
+    }
+}
