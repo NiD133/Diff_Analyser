2b993f98f73aeb1f85b14e2ceee6f136ac0d1a27
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:04:41 +0800

    gemini processedAutoTestCases nodekey_estest11.java

--- nodekey_estest11.java
+++ nodekey_estest11.java
@@ -2,20 +2,32 @@ package org.jfree.data.flow;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class NodeKey_ESTestTest11 extends NodeKey_ESTest_scaffolding {
+/**
+ * A test suite for the {@link NodeKey} class.
+ */
+public class NodeKeyTest {
 
-    @Test(timeout = 4000)
-    public void test10() throws Throwable {
-        Integer integer0 = new Integer(3);
-        NodeKey<Integer> nodeKey0 = new NodeKey<Integer>(2281, integer0);
-        NodeKey nodeKey1 = (NodeKey) nodeKey0.clone();
-        boolean boolean0 = nodeKey0.equals(nodeKey1);
-        assertTrue(boolean0);
-        assertEquals(2281, nodeKey1.getStage());
+    /**
+     * Verifies that cloning a NodeKey results in a new instance that is equal
+     * to the original but is a distinct object in memory.
+     */
+    @Test
+    public void clone_shouldCreateEqualButDistinctInstance() throws CloneNotSupportedException {
+        // Arrange: Create an original NodeKey instance.
+        int stage = 2281;
+        Integer node = 3;
+        NodeKey<Integer> originalKey = new NodeKey<>(stage, node);
+
+        // Act: Clone the original instance.
+        NodeKey<Integer> clonedKey = (NodeKey<Integer>) originalKey.clone();
+
+        // Assert: The cloned instance should be equal to the original, but not the same instance.
+        assertNotSame("A cloned object should be a new instance in memory.", originalKey, clonedKey);
+        assertEquals("A cloned object should be equal to the original.", originalKey, clonedKey);
+
+        // Explicitly verify that the internal state was copied correctly.
+        assertEquals("The stage of the cloned key should match the original.", stage, clonedKey.getStage());
+        assertEquals("The node of the cloned key should match the original.", node, clonedKey.getNode());
     }
 }
