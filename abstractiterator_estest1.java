13568b8ebcfce0592f0a43ac15c96aff78845fc6
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:36:34 +0800

    Original processedAutoTestCases abstractiterator_estest1.java

---
+++ abstractiterator_estest1.java
@@ -0,0 +1,29 @@
+package com.google.common.collect;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.Locale;
+import java.util.NoSuchElementException;
+import java.util.PriorityQueue;
+import java.util.function.Consumer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class AbstractIterator_ESTestTest1 extends AbstractIterator_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test0() throws Throwable {
+        PriorityQueue<Locale.FilteringMode> priorityQueue0 = new PriorityQueue<Locale.FilteringMode>();
+        Locale.FilteringMode locale_FilteringMode0 = Locale.FilteringMode.REJECT_EXTENDED_RANGES;
+        priorityQueue0.add(locale_FilteringMode0);
+        ConsumingQueueIterator<Locale.FilteringMode> consumingQueueIterator0 = new ConsumingQueueIterator<Locale.FilteringMode>(priorityQueue0);
+        Locale.FilteringMode locale_FilteringMode1 = consumingQueueIterator0.next();
+        assertSame(locale_FilteringMode1, locale_FilteringMode0);
+    }
+}
