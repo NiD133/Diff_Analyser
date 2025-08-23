85c73941de9ca833953c4b6614f43eae5df1e9e1
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:11:05 +0800

    gemini processedAutoTestCases randomaccessfilemode_estest33.java

--- randomaccessfilemode_estest33.java
+++ randomaccessfilemode_estest33.java
@@ -1,34 +1,36 @@
 package org.apache.commons.io;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
+import static org.junit.Assert.assertEquals;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.RandomAccessFile;
 import java.nio.file.OpenOption;
-import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
-import org.apache.commons.io.function.IOConsumer;
-import org.apache.commons.io.function.IOFunction;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
 import org.evosuite.runtime.mock.java.io.MockFile;
-import org.evosuite.runtime.testdata.EvoSuiteFile;
-import org.evosuite.runtime.testdata.FileSystemHandling;
-import org.junit.runner.RunWith;
 
+// Note: Unused imports from the original file have been removed for clarity.
+// The original test class structure is preserved.
 public class RandomAccessFileMode_ESTestTest33 extends RandomAccessFileMode_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test32() throws Throwable {
-        OpenOption[] openOptionArray0 = new OpenOption[1];
-        StandardOpenOption standardOpenOption0 = StandardOpenOption.WRITE;
-        openOptionArray0[0] = (OpenOption) standardOpenOption0;
-        RandomAccessFileMode randomAccessFileMode0 = RandomAccessFileMode.valueOf(openOptionArray0);
-        MockFile mockFile0 = new MockFile("6UOySR)&C(+0\"]F]DS");
-        IORandomAccessFile iORandomAccessFile0 = (IORandomAccessFile) randomAccessFileMode0.create((File) mockFile0);
-        assertEquals("rw", iORandomAccessFile0.getMode());
+    /**
+     * Tests that RandomAccessFileMode.valueOf() correctly interprets the
+     * StandardOpenOption.WRITE option as READ_WRITE mode.
+     */
+    @Test
+    public void valueOfWithWriteOptionShouldReturnReadWriteMode() throws FileNotFoundException {
+        // Arrange: Define the input options for creating the file mode.
+        final OpenOption[] openOptions = { StandardOpenOption.WRITE };
+        final File testFile = new MockFile("test-file.txt");
+
+        // Act: Get the RandomAccessFileMode from the open options.
+        final RandomAccessFileMode fileMode = RandomAccessFileMode.valueOf(openOptions);
+        
+        // Create a file instance using the determined mode.
+        // The create() method returns an IORandomAccessFile, which has the getMode() method.
+        final IORandomAccessFile randomAccessFile = (IORandomAccessFile) fileMode.create(testFile);
+
+        // Assert: Verify that the correct mode was selected and applied.
+        assertEquals("The file mode enum should be READ_WRITE", RandomAccessFileMode.READ_WRITE, fileMode);
+        assertEquals("The underlying file mode string should be 'rw'", "rw", randomAccessFile.getMode());
     }
 }
