e78a61b09a8bf21c2718b86e04eab8684db80d06
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:56:33 +0800

    gemini processedAutoTestCases jsonignoreproperties_estest35.java

--- jsonignoreproperties_estest35.java
+++ jsonignoreproperties_estest35.java
@@ -1,22 +1,23 @@
 package com.fasterxml.jackson.annotation;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import java.util.LinkedHashSet;
-import java.util.Set;
-import java.util.function.Predicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertFalse;
 
-public class JsonIgnoreProperties_ESTestTest35 extends JsonIgnoreProperties_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link JsonIgnoreProperties.Value} class.
+ */
+public class JsonIgnorePropertiesValueTest {
 
-    @Test(timeout = 4000)
-    public void test34() throws Throwable {
-        JsonIgnoreProperties.Value jsonIgnoreProperties_Value0 = JsonIgnoreProperties.Value.empty();
-        boolean boolean0 = jsonIgnoreProperties_Value0.equals((Object) null);
-        assertFalse(boolean0);
+    /**
+     * Verifies that an instance of JsonIgnoreProperties.Value is not equal to null,
+     * adhering to the general contract of the Object.equals() method.
+     */
+    @Test
+    public void equalsShouldReturnFalseWhenComparedWithNull() {
+        // Arrange: Create an empty instance of JsonIgnoreProperties.Value
+        JsonIgnoreProperties.Value emptyValue = JsonIgnoreProperties.Value.empty();
+
+        // Act & Assert: The equals() method must return false for a null argument.
+        assertFalse("An object instance should never be equal to null.", emptyValue.equals(null));
     }
 }
