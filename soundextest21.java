4e9574418e8b04bf44b0e4b0ecebba4b37e89853
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:33:54 +0800

    gemini processedManualTestCases soundextest21.java

--- soundextest21.java
+++ soundextest21.java
@@ -1,34 +1,63 @@
 package org.apache.commons.codec.language;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import org.apache.commons.codec.AbstractStringEncoderTest;
-import org.apache.commons.codec.EncoderException;
-import org.junit.jupiter.api.Test;
 
-public class SoundexTestTest21 extends AbstractStringEncoderTest<Soundex> {
+import org.apache.commons.codec.AbstractStringEncoderTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+/**
+ * Tests for the {@link Soundex} encoder.
+ */
+@DisplayName("Tests for Soundex")
+class SoundexTest extends AbstractStringEncoderTest<Soundex> {
 
     @Override
     protected Soundex createStringEncoder() {
+        // This provides the standard Soundex instance for tests in AbstractStringEncoderTest
         return new Soundex();
     }
 
-    @Test
-    // examples and algorithm rules from:  http://west-penwith.org.uk/misc/soundex.htm
-    void testSimplifiedSoundex() {
-        // treat vowels and HW as separators
-        final Soundex s = Soundex.US_ENGLISH_SIMPLIFIED;
-        assertEquals("W452", s.encode("WILLIAMS"));
-        assertEquals("B625", s.encode("BARAGWANATH"));
-        assertEquals("D540", s.encode("DONNELL"));
-        assertEquals("L300", s.encode("LLOYD"));
-        assertEquals("W422", s.encode("WOOLCOCK"));
-        // Additional local examples
-        assertEquals("D320", s.encode("Dodds"));
-        // w is a separator
-        assertEquals("D320", s.encode("Dwdds"));
-        // h is a separator
-        assertEquals("D320", s.encode("Dhdds"));
+    @Nested
+    @DisplayName("US_ENGLISH_SIMPLIFIED Algorithm")
+    class SimplifiedSoundexTest {
+
+        // The simplified algorithm is documented at http://west-penwith.org.uk/misc/soundex.htm (link is now defunct).
+        // Its key feature is treating vowels (A, E, I, O, U, Y) and the letters 'H' and 'W' as separators.
+        // This differs from the standard Soundex, where 'H' and 'W' are typically ignored.
+
+        @DisplayName("should correctly encode reference examples")
+        @ParameterizedTest(name = "encode(\"{0}\") -> \"{1}\"")
+        @CsvSource({
+            "WILLIAMS,    W452",
+            "BARAGWANATH, B625",
+            "DONNELL,     D540",
+            "LLOYD,       L300",
+            "WOOLCOCK,    W422"
+        })
+        void shouldEncodeReferenceExamples(String input, String expected) {
+            final Soundex soundexEncoder = Soundex.US_ENGLISH_SIMPLIFIED;
+            assertEquals(expected, soundexEncoder.encode(input));
+        }
+
+        @Test
+        @DisplayName("should treat 'H' and 'W' as code separators, like vowels")
+        void shouldTreatHAndWAsSeparators() {
+            final Soundex soundexEncoder = Soundex.US_ENGLISH_SIMPLIFIED;
+
+            // In "Dodds", the vowel 'o' separates the first 'D' from the subsequent 'd'.
+            // This allows the code for the second 'd' to be included.
+            // D-o-d-d-s -> D-3-2-0
+            final String doddsCode = soundexEncoder.encode("Dodds");
+            assertEquals("D320", doddsCode, "Vowel 'o' should separate the two 'd' sounds");
+
+            // In the simplified algorithm, 'W' and 'H' act as separators, just like 'o'.
+            // Therefore, "Dwdds" and "Dhdds" should produce the same Soundex code as "Dodds".
+            assertEquals(doddsCode, soundexEncoder.encode("Dwdds"), "'W' should act as a separator, like a vowel");
+            assertEquals(doddsCode, soundexEncoder.encode("Dhdds"), "'H' should act as a separator, like a vowel");
+        }
     }
 }
