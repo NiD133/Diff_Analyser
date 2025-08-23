e532cced5b8fd8d0c2f6793fcb2c4d69ce724932
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:44:57 +0800

    Original processedAutoTestCases comparatorchain_estest2.java

---
+++ comparatorchain_estest2.java
@@ -0,0 +1,34 @@
+package org.apache.commons.collections4.comparators;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.nio.ByteBuffer;
+import java.nio.LongBuffer;
+import java.util.BitSet;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.ToIntFunction;
+import java.util.function.ToLongFunction;
+import org.apache.commons.collections4.Closure;
+import org.apache.commons.collections4.functors.ClosureTransformer;
+import org.apache.commons.collections4.functors.ComparatorPredicate;
+import org.apache.commons.collections4.functors.ExceptionClosure;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class ComparatorChain_ESTestTest2 extends ComparatorChain_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test01() throws Throwable {
+        ComparatorChain<Integer> comparatorChain0 = new ComparatorChain<Integer>();
+        comparatorChain0.addComparator((Comparator<Integer>) comparatorChain0, true);
+        comparatorChain0.addComparator((Comparator<Integer>) comparatorChain0, true);
+        assertFalse(comparatorChain0.isLocked());
+    }
+}
