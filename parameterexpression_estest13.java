08752fc3ddbc37be0996e1198ed7da8a360efbab
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:07:16 +0800

    Original processedAutoTestCases parameterexpression_estest13.java

---
+++ parameterexpression_estest13.java
@@ -0,0 +1,17 @@
+package org.apache.ibatis.builder;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class ParameterExpression_ESTestTest13 extends ParameterExpression_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test12() throws Throwable {
+        ParameterExpression parameterExpression0 = new ParameterExpression(" ;|l, =R=kNH");
+        assertEquals(2, parameterExpression0.size());
+    }
+}
