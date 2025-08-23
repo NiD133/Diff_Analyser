2c55a71d9d859fd76a0382e9ab42326edbd88b5a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:30:52 +0800

    Original processedManualTestCases logfactorytest2.java

---
+++ logfactorytest2.java
@@ -0,0 +1,38 @@
+package org.apache.ibatis.logging;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.io.Reader;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.logging.commons.JakartaCommonsLoggingImpl;
+import org.apache.ibatis.logging.jdk14.Jdk14LoggingImpl;
+import org.apache.ibatis.logging.log4j.Log4jImpl;
+import org.apache.ibatis.logging.log4j2.Log4j2Impl;
+import org.apache.ibatis.logging.nologging.NoLoggingImpl;
+import org.apache.ibatis.logging.slf4j.Slf4jImpl;
+import org.apache.ibatis.logging.stdout.StdOutImpl;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+
+public class LogFactoryTestTest2 {
+
+    @AfterAll
+    static void restore() {
+        LogFactory.useSlf4jLogging();
+    }
+
+    private void logSomething(Log log) {
+        log.warn("Warning message.");
+        log.debug("Debug message.");
+        log.error("Error message.");
+        log.error("Error with Exception.", new Exception("Test exception."));
+    }
+
+    @Test
+    void shouldUseLog4J() {
+        LogFactory.useLog4JLogging();
+        Log log = LogFactory.getLog(Object.class);
+        logSomething(log);
+        assertEquals(log.getClass().getName(), Log4jImpl.class.getName());
+    }
+}
