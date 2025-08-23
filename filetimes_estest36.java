f75bdb70da8c30c9a7a0326dd928af59cc27c4ce
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:50:51 +0800

    gemini processedAutoTestCases filetimes_estest36.java

--- filetimes_estest36.java
+++ filetimes_estest36.java
@@ -1,32 +1,23 @@
 package org.apache.commons.io.file.attribute;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.math.BigDecimal;
-import java.nio.file.Path;
 import java.nio.file.attribute.FileTime;
-import java.time.DateTimeException;
-import java.time.Instant;
-import java.util.Date;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.time.MockInstant;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class FileTimes_ESTestTest36 extends FileTimes_ESTest_scaffolding {
+/**
+ * Tests for {@link FileTimes}.
+ */
+public class FileTimesTest {
 
-    @Test(timeout = 4000)
-    public void test35() throws Throwable {
-        // Undeclared exception!
-        try {
-            FileTimes.minusMillis((FileTime) null, 2332L);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.commons.io.file.attribute.FileTimes", e);
-        }
+    /**
+     * Tests that FileTimes.minusMillis() throws a NullPointerException when the
+     * input FileTime is null.
+     */
+    @Test(expected = NullPointerException.class)
+    public void minusMillisShouldThrowNullPointerExceptionForNullInput() {
+        // The value for millisToSubtract is arbitrary as it's not relevant to the null check.
+        final long arbitraryMillis = 2332L;
+
+        // Act & Assert: Call the method with a null FileTime, which is expected to throw.
+        FileTimes.minusMillis(null, arbitraryMillis);
     }
 }
