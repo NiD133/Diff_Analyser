2dbaae76f8780fb4c9e739336dd09a362368457d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:09:00 +0800

    gemini processedAutoTestCases plotrenderinginfo_estest22.java

--- plotrenderinginfo_estest22.java
+++ plotrenderinginfo_estest22.java
@@ -1,33 +1,35 @@
 package org.jfree.chart.plot;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.awt.Dimension;
-import java.awt.Point;
-import java.awt.Rectangle;
-import java.awt.geom.Line2D;
-import java.awt.geom.Point2D;
-import java.awt.geom.Rectangle2D;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
 import org.jfree.chart.ChartRenderingInfo;
-import org.jfree.chart.entity.EntityCollection;
-import org.jfree.chart.entity.StandardEntityCollection;
-import org.junit.runner.RunWith;
+import org.junit.Test;
+import java.awt.Rectangle;
+import static org.junit.Assert.assertFalse;
 
-public class PlotRenderingInfo_ESTestTest22 extends PlotRenderingInfo_ESTest_scaffolding {
+/**
+ * Unit tests for the equals() method in the {@link PlotRenderingInfo} class.
+ */
+public class PlotRenderingInfoTest {
 
-    @Test(timeout = 4000)
-    public void test21() throws Throwable {
-        ChartRenderingInfo chartRenderingInfo0 = new ChartRenderingInfo();
-        PlotRenderingInfo plotRenderingInfo0 = new PlotRenderingInfo(chartRenderingInfo0);
-        Point point0 = new Point(7, 0);
-        Dimension dimension0 = new Dimension();
-        Rectangle rectangle0 = new Rectangle(point0, dimension0);
-        plotRenderingInfo0.setDataArea(rectangle0);
-        PlotRenderingInfo plotRenderingInfo1 = chartRenderingInfo0.getPlotInfo();
-        boolean boolean0 = plotRenderingInfo0.equals(plotRenderingInfo1);
-        assertFalse(boolean0);
+    /**
+     * Verifies that two PlotRenderingInfo instances are not considered equal
+     * if they have different dataArea properties.
+     */
+    @Test
+    public void equals_shouldReturnFalse_whenDataAreasAreDifferent() {
+        // Arrange: Create two PlotRenderingInfo instances with different data areas.
+        ChartRenderingInfo chartInfo = new ChartRenderingInfo();
+
+        // Create the first instance and assign it a custom data area.
+        PlotRenderingInfo info1 = new PlotRenderingInfo(chartInfo);
+        info1.setDataArea(new Rectangle(10, 20, 30, 40));
+
+        // The second instance has the default data area (an empty rectangle at 0,0).
+        PlotRenderingInfo info2 = new PlotRenderingInfo(chartInfo);
+
+        // Act: Compare the two instances for equality.
+        boolean areEqual = info1.equals(info2);
+
+        // Assert: The instances should not be equal.
+        assertFalse("PlotRenderingInfo objects with different data areas should not be equal.", areEqual);
     }
 }
