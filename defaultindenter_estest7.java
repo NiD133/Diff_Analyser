e42674a54bc921bbde4b5c8d9cee0e88dadab4d9
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:47:28 +0800

    gemini processedAutoTestCases defaultindenter_estest7.java

--- defaultindenter_estest7.java
+++ defaultindenter_estest7.java
@@ -1,50 +1,28 @@
 package com.fasterxml.jackson.core.util;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import com.fasterxml.jackson.core.ErrorReportConfiguration;
-import com.fasterxml.jackson.core.JsonEncoding;
-import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.core.JsonFactoryBuilder;
 import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.ObjectCodec;
-import com.fasterxml.jackson.core.StreamReadConstraints;
-import com.fasterxml.jackson.core.StreamWriteConstraints;
-import com.fasterxml.jackson.core.filter.FilteringGeneratorDelegate;
-import com.fasterxml.jackson.core.filter.TokenFilter;
-import com.fasterxml.jackson.core.io.ContentReference;
-import com.fasterxml.jackson.core.io.IOContext;
-import com.fasterxml.jackson.core.json.UTF8JsonGenerator;
-import com.fasterxml.jackson.core.json.WriterBasedJsonGenerator;
-import java.io.File;
+import org.junit.Test;
+
 import java.io.IOException;
-import java.io.OutputStream;
-import java.io.StringWriter;
-import java.io.Writer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.io.MockFile;
-import org.evosuite.runtime.mock.java.io.MockFileOutputStream;
-import org.evosuite.runtime.mock.java.io.MockPrintStream;
-import org.evosuite.runtime.testdata.EvoSuiteFile;
-import org.evosuite.runtime.testdata.FileSystemHandling;
-import org.junit.runner.RunWith;
 
-public class DefaultIndenter_ESTestTest7 extends DefaultIndenter_ESTest_scaffolding {
+/**
+ * This test verifies the behavior of the {@link DefaultIndenter#writeIndentation} method.
+ */
+public class DefaultIndenterTest {
 
-    @Test(timeout = 4000)
-    public void test06() throws Throwable {
-        DefaultIndenter defaultIndenter0 = new DefaultIndenter();
-        // Undeclared exception!
-        try {
-            defaultIndenter0.SYSTEM_LINEFEED_INSTANCE.writeIndentation((JsonGenerator) null, (-110));
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("com.fasterxml.jackson.core.util.DefaultIndenter", e);
-        }
+    /**
+     * Verifies that calling writeIndentation with a null JsonGenerator
+     * results in a NullPointerException, as the method cannot operate
+     * without a generator to write to.
+     */
+    @Test(expected = NullPointerException.class)
+    public void writeIndentation_whenGeneratorIsNull_shouldThrowNullPointerException() throws IOException {
+        // Arrange: Get a standard indenter instance. The indentation level is arbitrary
+        // as the null generator will cause the exception before the level is used.
+        DefaultIndenter indenter = DefaultIndenter.SYSTEM_LINEFEED_INSTANCE;
+        int anyIndentLevel = 1;
+
+        // Act & Assert: This call is expected to throw a NullPointerException.
+        indenter.writeIndentation(null, anyIndentLevel);
     }
 }
