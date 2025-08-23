585516da2e6640daafd37935d3bbb3ea98fb7270
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:58:34 +0800

    gemini processedAutoTestCases jsontreewriter_estest3.java

--- jsontreewriter_estest3.java
+++ jsontreewriter_estest3.java
@@ -1,25 +1,20 @@
 package com.google.gson.internal.bind;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonPrimitive;
-import com.google.gson.Strictness;
-import com.google.gson.stream.JsonWriter;
-import java.io.IOException;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertFalse;
 
-public class JsonTreeWriter_ESTestTest3 extends JsonTreeWriter_ESTest_scaffolding {
+/**
+ * Tests for {@link JsonTreeWriter}.
+ */
+public class JsonTreeWriterTest {
 
-    @Test(timeout = 4000)
-    public void test02() throws Throwable {
-        JsonTreeWriter jsonTreeWriter0 = new JsonTreeWriter();
-        jsonTreeWriter0.setHtmlSafe(true);
-        JsonWriter jsonWriter0 = jsonTreeWriter0.value(true);
-        assertFalse(jsonWriter0.isLenient());
+    @Test
+    public void newWriterIsNotLenientByDefault() {
+        // Arrange
+        JsonTreeWriter writer = new JsonTreeWriter();
+
+        // Act & Assert
+        // By default, a writer should adhere to strict JSON rules.
+        assertFalse("A newly created JsonTreeWriter should not be lenient by default", writer.isLenient());
     }
 }
