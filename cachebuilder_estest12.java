0525e18e50083d6874d511ac2ecdadf92bfc2f50
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:41:11 +0800

    Original processedAutoTestCases cachebuilder_estest12.java

---
+++ cachebuilder_estest12.java
@@ -0,0 +1,25 @@
+package org.apache.ibatis.mapping;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.Properties;
+import org.apache.ibatis.cache.Cache;
+import org.apache.ibatis.cache.decorators.BlockingCache;
+import org.apache.ibatis.cache.decorators.SynchronizedCache;
+import org.apache.ibatis.cache.impl.PerpetualCache;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class CacheBuilder_ESTestTest12 extends CacheBuilder_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test11() throws Throwable {
+        CacheBuilder cacheBuilder0 = new CacheBuilder("");
+        Integer integer0 = new Integer(1);
+        CacheBuilder cacheBuilder1 = cacheBuilder0.size(integer0);
+        Cache cache0 = cacheBuilder1.build();
+        assertEquals("", cache0.getId());
+    }
+}
