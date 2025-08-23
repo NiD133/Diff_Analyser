c9a4a5f8098aed6bebbf2a341c2094dbfe973289
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:07:54 +0800

    Original processedAutoTestCases pdfdictionary_estest3.java

---
+++ pdfdictionary_estest3.java
@@ -0,0 +1,32 @@
+package com.itextpdf.text.pdf;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.collection.PdfCollectionField;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PipedOutputStream;
+import java.util.LinkedHashMap;
+import java.util.Set;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockPrintStream;
+import org.junit.runner.RunWith;
+
+public class PdfDictionary_ESTestTest3 extends PdfDictionary_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test02() throws Throwable {
+        PdfName pdfName0 = PdfName.LINEHEIGHT;
+        PdfDictionary pdfDictionary0 = new PdfDictionary(pdfName0);
+        PdfWriter pdfWriter0 = new PdfWriter();
+        PdfRectangle pdfRectangle0 = new PdfRectangle(0.0F, 642.05963F, 8388608, (-424.6F));
+        Rectangle rectangle0 = PdfReader.getNormalizedRectangle(pdfRectangle0);
+        PdfAnnotation pdfAnnotation0 = PdfAnnotation.createLink(pdfWriter0, rectangle0, pdfDictionary0.CATALOG);
+        pdfAnnotation0.putAll(pdfDictionary0);
+        assertEquals(3, PdfAnnotation.MARKUP_SQUIGGLY);
+    }
+}
