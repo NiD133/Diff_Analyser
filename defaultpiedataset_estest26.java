ac12e0cf725eaba7c0f69bafa65348248b4fc817
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:47:41 +0800

    Original processedAutoTestCases defaultpiedataset_estest26.java

---
+++ defaultpiedataset_estest26.java
@@ -0,0 +1,35 @@
+package org.jfree.data.general;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.time.chrono.ChronoLocalDate;
+import java.util.List;
+import java.util.Vector;
+import javax.swing.JLayeredPane;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.jfree.chart.api.SortOrder;
+import org.jfree.chart.api.TableOrder;
+import org.jfree.data.DefaultKeyedValues;
+import org.jfree.data.KeyedValues;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.category.CategoryToPieDataset;
+import org.jfree.data.category.DefaultIntervalCategoryDataset;
+import org.jfree.data.category.SlidingCategoryDataset;
+import org.jfree.data.statistics.DefaultBoxAndWhiskerCategoryDataset;
+import org.junit.runner.RunWith;
+
+public class DefaultPieDataset_ESTestTest26 extends DefaultPieDataset_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test25() throws Throwable {
+        DefaultPieDataset<Integer> defaultPieDataset0 = new DefaultPieDataset<Integer>();
+        // Undeclared exception!
+        try {
+            defaultPieDataset0.getValue(0);
+            fail("Expecting exception: IndexOutOfBoundsException");
+        } catch (IndexOutOfBoundsException e) {
+        }
+    }
+}
