824ad1df1d0a557572af23308ac24106f7b588e3
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:53:24 +0800

    Original processedAutoTestCases hostspecifier_estest6.java

---
+++ hostspecifier_estest6.java
@@ -0,0 +1,26 @@
+package com.google.common.net;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.text.ParseException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class HostSpecifier_ESTestTest6 extends HostSpecifier_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test05() throws Throwable {
+        // Undeclared exception!
+        try {
+            HostSpecifier.from((String) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("com.google.common.base.Preconditions", e);
+        }
+    }
+}
