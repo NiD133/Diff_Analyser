101751c221b26537c9e5d25ab606c2417a945c91
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:29:17 +0800

    Original processedAutoTestCases xnode_estest9.java

---
+++ xnode_estest9.java
@@ -0,0 +1,31 @@
+package org.apache.ibatis.parsing;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.List;
+import java.util.Locale;
+import java.util.Properties;
+import java.util.function.Supplier;
+import javax.imageio.metadata.IIOMetadataNode;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.xml.sax.ext.DefaultHandler2;
+
+public class XNode_ESTestTest9 extends XNode_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test008() throws Throwable {
+        IIOMetadataNode iIOMetadataNode0 = new IIOMetadataNode(" />\n");
+        XPathParser xPathParser0 = new XPathParser((Document) null, true);
+        Properties properties0 = new Properties();
+        XNode xNode0 = new XNode(xPathParser0, iIOMetadataNode0, properties0);
+        String string0 = xNode0.getStringAttribute(" ", "");
+        assertEquals("", string0);
+    }
+}
