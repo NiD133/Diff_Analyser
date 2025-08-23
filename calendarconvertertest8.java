a8bbc64aa6dff2f9caa890f680bf43fb8e6689cc
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:26:16 +0800

    gemini processedManualTestCases calendarconvertertest8.java

--- calendarconvertertest8.java
+++ calendarconvertertest8.java
@@ -1,50 +1,22 @@
 package org.joda.time.convert;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-import java.util.Arrays;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.TimeZone;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-import org.joda.time.Chronology;
-import org.joda.time.DateTimeZone;
-import org.joda.time.TimeOfDay;
-import org.joda.time.chrono.BuddhistChronology;
-import org.joda.time.chrono.GJChronology;
-import org.joda.time.chrono.GregorianChronology;
-import org.joda.time.chrono.ISOChronology;
-import org.joda.time.chrono.JulianChronology;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
-public class CalendarConverterTestTest8 extends TestCase {
+/**
+ * Unit tests for {@link CalendarConverter}.
+ */
+public class CalendarConverterTest {
 
-    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
-
-    private static final DateTimeZone MOSCOW = DateTimeZone.forID("Europe/Moscow");
-
-    private static Chronology JULIAN;
-
-    private static Chronology ISO;
-
-    public static void main(String[] args) {
-        junit.textui.TestRunner.run(suite());
-    }
-
-    public static TestSuite suite() {
-        return new TestSuite(TestCalendarConverter.class);
-    }
-
-    @Override
-    protected void setUp() throws Exception {
-        JULIAN = JulianChronology.getInstance();
-        ISO = ISOChronology.getInstance();
-    }
-
-    //-----------------------------------------------------------------------
-    public void testToString() {
-        assertEquals("Converter[java.util.Calendar]", CalendarConverter.INSTANCE.toString());
+    /**
+     * Tests that the toString() method returns a standard representation,
+     * including the name of the type it converts.
+     */
+    @Test
+    public void toString_shouldReturnCorrectRepresentation() {
+        // The expected format is "Converter[<supported-class-name>]"
+        String expected = "Converter[java.util.Calendar]";
+        
+        assertEquals(expected, CalendarConverter.INSTANCE.toString());
     }
 }
