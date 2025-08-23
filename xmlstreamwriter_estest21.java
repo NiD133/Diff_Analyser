50884e6cb085a7545f57617ea1a2591dee292238
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:27:48 +0800

    Original processedAutoTestCases xmlstreamwriter_estest21.java

---
+++ xmlstreamwriter_estest21.java
@@ -0,0 +1,31 @@
+package org.apache.commons.io.output;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.io.Writer;
+import java.nio.CharBuffer;
+import java.nio.charset.IllegalCharsetNameException;
+import java.nio.charset.UnsupportedCharsetException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.evosuite.runtime.mock.java.io.MockPrintStream;
+import org.junit.runner.RunWith;
+
+public class XmlStreamWriter_ESTestTest21 extends XmlStreamWriter_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test20() throws Throwable {
+        java.io.ByteArrayOutputStream byteArrayOutputStream0 = new java.io.ByteArrayOutputStream();
+        XmlStreamWriter xmlStreamWriter0 = new XmlStreamWriter(byteArrayOutputStream0);
+        xmlStreamWriter0.flush();
+        assertEquals("UTF-8", xmlStreamWriter0.getDefaultEncoding());
+    }
+}
