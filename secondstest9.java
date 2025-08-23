d8a82a6fbe3f5a5dd4f044c6e8f875b5cd40c7d4
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:33:18 +0800

    Original processedManualTestCases secondstest9.java

---
+++ secondstest9.java
@@ -0,0 +1,35 @@
+package org.joda.time;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class SecondsTestTest9 extends TestCase {
+
+    // (before the late 90's they were all over the place)
+    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
+
+    public static TestSuite suite() {
+        return new TestSuite(TestSeconds.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+    }
+
+    public void testGetFieldType() {
+        Seconds test = Seconds.seconds(20);
+        assertEquals(DurationFieldType.seconds(), test.getFieldType());
+    }
+}
