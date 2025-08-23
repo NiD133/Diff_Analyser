12cdddc48f0329f7bf6ef0eebc7fc05b6d9c6c35
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:03:23 +0800

    gemini processedAutoTestCases minutes_estest65.java

--- minutes_estest65.java
+++ minutes_estest65.java
@@ -2,17 +2,26 @@ package org.joda.time;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class Minutes_ESTestTest65 extends Minutes_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link Minutes} class.
+ */
+public class MinutesTest {
 
-    @Test(timeout = 4000)
-    public void test64() throws Throwable {
-        Minutes minutes0 = Minutes.MIN_VALUE;
-        DurationFieldType durationFieldType0 = minutes0.getFieldType();
-        assertEquals("minutes", durationFieldType0.toString());
+    /**
+     * Tests that getFieldType() consistently returns the 'minutes' duration field type,
+     * regardless of the specific Minutes value.
+     */
+    @Test
+    public void testGetFieldType_shouldReturnMinutesType() {
+        // Arrange: Create an instance of the Minutes class.
+        // The specific value does not matter for this test.
+        Minutes testMinutes = Minutes.ONE;
+
+        // Act: Call the method under test.
+        DurationFieldType fieldType = testMinutes.getFieldType();
+
+        // Assert: Verify that the returned type is the expected 'minutes' type.
+        assertEquals(DurationFieldType.minutes(), fieldType);
     }
 }
