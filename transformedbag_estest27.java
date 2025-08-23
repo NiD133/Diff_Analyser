25707b577c538658ba2358491dd58af39ae25de1
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:23:01 +0800

    gemini processedAutoTestCases transformedbag_estest27.java

--- transformedbag_estest27.java
+++ transformedbag_estest27.java
@@ -1,65 +1,36 @@
 package org.apache.commons.collections4.bag;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Set;
-import java.util.Vector;
 import org.apache.commons.collections4.Bag;
-import org.apache.commons.collections4.Factory;
-import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.SortedBag;
 import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AndPredicate;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ChainedTransformer;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantFactory;
 import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.FactoryTransformer;
-import org.apache.commons.collections4.functors.FalsePredicate;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.InstantiateFactory;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.MapTransformer;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NotPredicate;
-import org.apache.commons.collections4.functors.OnePredicate;
-import org.apache.commons.collections4.functors.SwitchTransformer;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class TransformedBag_ESTestTest27 extends TransformedBag_ESTest_scaffolding {
+/**
+ * Tests for {@link TransformedBag}.
+ */
+public class TransformedBagTest {
 
-    @Test(timeout = 4000)
-    public void test26() throws Throwable {
-        Transformer<Integer, Integer> transformer0 = ConstantTransformer.nullTransformer();
-        TreeBag<Integer> treeBag0 = new TreeBag<Integer>();
-        SynchronizedSortedBag<Integer> synchronizedSortedBag0 = new SynchronizedSortedBag<Integer>(treeBag0);
-        TransformedSortedBag<Integer> transformedSortedBag0 = new TransformedSortedBag<Integer>(synchronizedSortedBag0, transformer0);
-        Integer integer0 = new Integer(414);
-        // Undeclared exception!
-        try {
-            transformedSortedBag0.add(integer0, 414);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("java.util.TreeMap", e);
-        }
+    /**
+     * Tests that adding an element to a TransformedBag throws a NullPointerException
+     * if the transformer converts the element to null and the underlying decorated bag
+     * does not support null elements.
+     */
+    @Test(expected = NullPointerException.class)
+    public void testAddTransformedNullToBagThatDisallowsNulls() {
+        // Arrange
+        // 1. The decorated bag (TreeBag) does not allow null elements.
+        final Bag<Integer> underlyingBag = new TreeBag<>();
+
+        // 2. The transformer converts any input element to null.
+        final Transformer<Integer, Integer> nullTransformer = ConstantTransformer.nullTransformer();
+
+        // 3. The TransformedBag uses the null-disallowing bag and the null-transformer.
+        final Bag<Integer> transformedBag = TransformedBag.transformingBag(underlyingBag, nullTransformer);
+
+        // Act & Assert
+        // Attempting to add an element will first transform it to null.
+        // The subsequent call to the underlying TreeBag's add(null, count) will
+        // throw a NullPointerException.
+        transformedBag.add(414, 10);
     }
 }
