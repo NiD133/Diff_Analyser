628d13c5e6cdd15e0a5704a47ccd5c707455134c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:54:03 +0800

    Original processedAutoTestCases indexedcollection_estest43.java

---
+++ indexedcollection_estest43.java
@@ -0,0 +1,67 @@
+package org.apache.commons.collections4.collection;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.lang.reflect.Array;
+import java.util.Collection;
+import java.util.ConcurrentModificationException;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Set;
+import org.apache.commons.collections4.Closure;
+import org.apache.commons.collections4.Predicate;
+import org.apache.commons.collections4.Transformer;
+import org.apache.commons.collections4.functors.AllPredicate;
+import org.apache.commons.collections4.functors.AnyPredicate;
+import org.apache.commons.collections4.functors.ChainedTransformer;
+import org.apache.commons.collections4.functors.CloneTransformer;
+import org.apache.commons.collections4.functors.ClosureTransformer;
+import org.apache.commons.collections4.functors.ConstantFactory;
+import org.apache.commons.collections4.functors.ConstantTransformer;
+import org.apache.commons.collections4.functors.DefaultEquator;
+import org.apache.commons.collections4.functors.EqualPredicate;
+import org.apache.commons.collections4.functors.ExceptionTransformer;
+import org.apache.commons.collections4.functors.FactoryTransformer;
+import org.apache.commons.collections4.functors.FalsePredicate;
+import org.apache.commons.collections4.functors.ForClosure;
+import org.apache.commons.collections4.functors.IfTransformer;
+import org.apache.commons.collections4.functors.InstanceofPredicate;
+import org.apache.commons.collections4.functors.InvokerTransformer;
+import org.apache.commons.collections4.functors.NOPClosure;
+import org.apache.commons.collections4.functors.NOPTransformer;
+import org.apache.commons.collections4.functors.NonePredicate;
+import org.apache.commons.collections4.functors.NotNullPredicate;
+import org.apache.commons.collections4.functors.NullIsFalsePredicate;
+import org.apache.commons.collections4.functors.NullPredicate;
+import org.apache.commons.collections4.functors.SwitchTransformer;
+import org.apache.commons.collections4.functors.TransformedPredicate;
+import org.apache.commons.collections4.functors.TransformerClosure;
+import org.apache.commons.collections4.functors.TransformerPredicate;
+import org.apache.commons.collections4.functors.TruePredicate;
+import org.apache.commons.collections4.functors.UniquePredicate;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class IndexedCollection_ESTestTest43 extends IndexedCollection_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test42() throws Throwable {
+        HashMap<Integer, Integer> hashMap0 = new HashMap<Integer, Integer>();
+        Collection<Integer> collection0 = hashMap0.values();
+        Transformer<Integer, Object> transformer0 = ExceptionTransformer.exceptionTransformer();
+        IndexedCollection<Object, Integer> indexedCollection0 = IndexedCollection.nonUniqueIndexedCollection(collection0, transformer0);
+        Integer integer0 = new Integer((-2641));
+        // Undeclared exception!
+        try {
+            indexedCollection0.add(integer0);
+            fail("Expecting exception: UnsupportedOperationException");
+        } catch (UnsupportedOperationException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("java.util.AbstractCollection", e);
+        }
+    }
+}
