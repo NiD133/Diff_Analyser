f67ad2c0291df4546203b27b9fec7b87f699b957
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:07:11 +0800

    Original processedAutoTestCases parallelscatterzipcreator_estest14.java

---
+++ parallelscatterzipcreator_estest14.java
@@ -0,0 +1,56 @@
+package org.apache.commons.compress.archivers.zip;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.File;
+import java.net.URI;
+import java.nio.file.Path;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import org.apache.commons.compress.archivers.jar.JarArchiveEntry;
+import org.apache.commons.compress.parallel.InputStreamSupplier;
+import org.apache.commons.compress.parallel.ScatterGatherBackingStoreSupplier;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.evosuite.runtime.mock.java.net.MockURI;
+import org.junit.runner.RunWith;
+
+public class ParallelScatterZipCreator_ESTestTest14 extends ParallelScatterZipCreator_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test13() throws Throwable {
+        Future<?> future = executor.submit(new Runnable() {
+
+            @Override
+            public void run() {
+                try {
+                    ParallelScatterZipCreator parallelScatterZipCreator0 = new ParallelScatterZipCreator();
+                    parallelScatterZipCreator0.addArchiveEntry((ZipArchiveEntryRequestSupplier) null);
+                    try {
+                        parallelScatterZipCreator0.writeTo((ZipArchiveOutputStream) null);
+                        fail("Expecting exception: ExecutionException");
+                    } catch (ExecutionException e) {
+                        //
+                        // java.lang.SecurityException: Unable to create temporary file or directory
+                        //
+                        verifyException("java.util.concurrent.FutureTask", e);
+                    }
+                } catch (Throwable t) {
+                    // Need to catch declared exceptions
+                }
+            }
+        });
+        future.get(4000, TimeUnit.MILLISECONDS);
+    }
+}
