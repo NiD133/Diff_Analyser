b1bebddb39a6863034b8231ae231431b83aa42d5
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:51:36 +0800

    Original processedAutoTestCases forwardingqueue_estest7.java

---
+++ forwardingqueue_estest7.java
@@ -0,0 +1,23 @@
+package com.google.common.collect;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.nio.CharBuffer;
+import java.util.Locale;
+import java.util.NoSuchElementException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class ForwardingQueue_ESTestTest7 extends ForwardingQueue_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test06() throws Throwable {
+        EvictingQueue<Object> evictingQueue0 = EvictingQueue.create(1937);
+        Object object0 = new Object();
+        evictingQueue0.standardOffer(object0);
+        Object object1 = evictingQueue0.element();
+        assertTrue(evictingQueue0.contains(object1));
+    }
+}
