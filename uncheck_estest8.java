f1316f3f196e762a266b02be2376d5a82e6e1f01
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:24:45 +0800

    gemini processedAutoTestCases uncheck_estest8.java

--- uncheck_estest8.java
+++ uncheck_estest8.java
@@ -1,31 +1,35 @@
 package org.apache.commons.io.function;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.time.chrono.HijrahEra;
-import java.util.Comparator;
-import java.util.concurrent.ForkJoinTask;
-import java.util.function.BiConsumer;
-import java.util.function.BinaryOperator;
-import java.util.function.Function;
-import java.util.function.Supplier;
-import java.util.stream.Collector;
-import java.util.stream.LongStream;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
 
-public class Uncheck_ESTestTest8 extends Uncheck_ESTest_scaffolding {
+/**
+ * Tests for {@link Uncheck}.
+ */
+public class UncheckTest {
 
-    @Test(timeout = 4000)
-    public void test07() throws Throwable {
-        IOLongSupplier iOLongSupplier0 = mock(IOLongSupplier.class, new ViolatedAssumptionAnswer());
-        doReturn(0L).when(iOLongSupplier0).getAsLong();
-        long long0 = Uncheck.getAsLong(iOLongSupplier0);
-        assertEquals(0L, long0);
+    /**
+     * Tests that {@link Uncheck#getAsLong(IOLongSupplier)} correctly returns the value
+     * from the supplier when the underlying operation succeeds without an exception.
+     */
+    @Test
+    public void testGetAsLongReturnsValueOnSuccess() throws IOException {
+        // Arrange: Create a mock IOLongSupplier that returns a specific value.
+        final IOLongSupplier mockSupplier = mock(IOLongSupplier.class);
+        final long expectedValue = 0L;
+        
+        // Stub the getAsLong() method to return our expected value.
+        // We add 'throws IOException' to the test method signature to allow this clean syntax.
+        when(mockSupplier.getAsLong()).thenReturn(expectedValue);
+
+        // Act: Call the method under test.
+        final long actualValue = Uncheck.getAsLong(mockSupplier);
+
+        // Assert: Verify that the returned value is the one we expect.
+        assertEquals(expectedValue, actualValue);
     }
 }
