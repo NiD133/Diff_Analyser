8f6f98b348f3aa1bcafd7b4bb5ae5ebd8c0293f1
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:43:32 +0800

    gemini processedAutoTestCases cleaningpathvisitor_estest10.java

--- cleaningpathvisitor_estest10.java
+++ cleaningpathvisitor_estest10.java
@@ -1,35 +1,30 @@
 package org.apache.commons.io.file;
 
+import static org.junit.Assert.assertNotEquals;
+
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.IOException;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Path;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.evosuite.runtime.mock.java.io.MockFile;
-import org.junit.runner.RunWith;
 
-public class CleaningPathVisitor_ESTestTest10 extends CleaningPathVisitor_ESTest_scaffolding {
+/**
+ * Tests for the equals method in {@link CleaningPathVisitor}.
+ */
+public class CleaningPathVisitorEqualsTest {
 
-    @Test(timeout = 4000)
-    public void test09() throws Throwable {
-        CountingPathVisitor countingPathVisitor0 = CleaningPathVisitor.withBigIntegerCounters();
-        Counters.PathCounters counters_PathCounters0 = CountingPathVisitor.defaultPathCounters();
-        DeleteOption[] deleteOptionArray0 = new DeleteOption[4];
-        StandardDeleteOption standardDeleteOption0 = StandardDeleteOption.OVERRIDE_READ_ONLY;
-        deleteOptionArray0[1] = (DeleteOption) standardDeleteOption0;
-        String[] stringArray0 = new String[0];
-        CleaningPathVisitor cleaningPathVisitor0 = new CleaningPathVisitor(counters_PathCounters0, deleteOptionArray0, stringArray0);
-        boolean boolean0 = countingPathVisitor0.equals(cleaningPathVisitor0);
-        assertFalse(boolean0);
+    @Test
+    public void equals_shouldReturnFalse_whenVisitorsHaveDifferentConfigurations() {
+        // Arrange
+        // Visitor A: Created using a factory method. It uses BigInteger counters
+        // and has default delete options (i.e., 'overrideReadOnly' is false).
+        final CleaningPathVisitor visitorA = (CleaningPathVisitor) CleaningPathVisitor.withBigIntegerCounters();
+
+        // Visitor B: Created with an explicit configuration that differs from Visitor A.
+        // It uses standard 'long' counters and is set to override read-only files.
+        final Counters.PathCounters longCounters = CountingPathVisitor.defaultPathCounters();
+        final DeleteOption[] deleteOptions = {StandardDeleteOption.OVERRIDE_READ_ONLY};
+        final CleaningPathVisitor visitorB = new CleaningPathVisitor(longCounters, deleteOptions);
+
+        // Act & Assert
+        // The visitors are expected to be unequal because their configurations differ
+        // in both the counter type and the 'overrideReadOnly' setting.
+        assertNotEquals(visitorA, visitorB);
     }
 }
