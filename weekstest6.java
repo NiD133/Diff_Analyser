f6ea9757f38efc14a0259b45fef4d50c1ea12daa
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:35:55 +0800

    Original processedManualTestCases weekstest6.java

---
+++ weekstest6.java
@@ -0,0 +1,47 @@
+package org.joda.time;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class WeeksTestTest6 extends TestCase {
+
+    // (before the late 90's they were all over the place)
+    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
+
+    public static TestSuite suite() {
+        return new TestSuite(TestWeeks.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+    }
+
+    public void testFactory_standardWeeksIn_RPeriod() {
+        assertEquals(0, Weeks.standardWeeksIn((ReadablePeriod) null).getWeeks());
+        assertEquals(0, Weeks.standardWeeksIn(Period.ZERO).getWeeks());
+        assertEquals(1, Weeks.standardWeeksIn(new Period(0, 0, 1, 0, 0, 0, 0, 0)).getWeeks());
+        assertEquals(123, Weeks.standardWeeksIn(Period.weeks(123)).getWeeks());
+        assertEquals(-987, Weeks.standardWeeksIn(Period.weeks(-987)).getWeeks());
+        assertEquals(1, Weeks.standardWeeksIn(Period.days(13)).getWeeks());
+        assertEquals(2, Weeks.standardWeeksIn(Period.days(14)).getWeeks());
+        assertEquals(2, Weeks.standardWeeksIn(Period.days(15)).getWeeks());
+        try {
+            Weeks.standardWeeksIn(Period.months(1));
+            fail();
+        } catch (IllegalArgumentException ex) {
+            // expected
+        }
+    }
+}
