f45bcf3a7ff28d7786bb31d0dedfc26377b2d9a0
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:25:24 +0800

    Original processedAutoTestCases utcinstant_estest5.java

---
+++ utcinstant_estest5.java
@@ -0,0 +1,27 @@
+package org.threeten.extra.scale;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.nio.CharBuffer;
+import java.time.DateTimeException;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.format.DateTimeParseException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.time.MockInstant;
+import org.junit.runner.RunWith;
+
+public class UtcInstant_ESTestTest5 extends UtcInstant_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test04() throws Throwable {
+        UtcInstant utcInstant0 = UtcInstant.ofModifiedJulianDay(9223372036854775807L, 73281320003515L);
+        assertFalse(utcInstant0.isLeapSecond());
+        UtcInstant utcInstant1 = utcInstant0.withModifiedJulianDay(6400000041317L);
+        boolean boolean0 = utcInstant0.isBefore(utcInstant1);
+        assertFalse(boolean0);
+        assertEquals(6400000041317L, utcInstant1.getModifiedJulianDay());
+    }
+}
