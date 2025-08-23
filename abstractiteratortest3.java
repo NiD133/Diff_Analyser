a915175681f6fe84f52e568937d120de10282381
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:24:37 +0800

    gemini processedManualTestCases abstractiteratortest3.java

--- abstractiteratortest3.java
+++ abstractiteratortest3.java
@@ -17,21 +17,34 @@ import org.jspecify.annotations.Nullable;
 
 public class AbstractIteratorTestTest3 extends TestCase {
 
-    // weak references, details of GC
-    @J2ktIncompatible
-    // weak references
-    @GwtIncompatible
-    // depends on details of GC
-    @AndroidIncompatible
-    public void testFreesNextReference() {
-        Iterator<Object> itr = new AbstractIterator<Object>() {
-
+    /**
+     * Tests that the iterator does not hold a strong reference to the object returned by {@code
+     * next()} after the call completes. This is important to prevent memory leaks, as it allows the
+     * returned object to be garbage collected if no other strong references to it exist.
+     *
+     * <p>We verify this by holding only a {@link WeakReference} to the returned object and then
+     * asserting that the garbage collector eventually clears this reference.
+     */
+    @J2ktIncompatible // weak references
+    @GwtIncompatible // weak references
+    @AndroidIncompatible // depends on details of GC
+    public void testNext_doesNotRetainReferenceToReturnedObject() {
+        // Arrange: Create an iterator that produces a new object on each call.
+        Iterator<Object> iterator = new AbstractIterator<Object>() {
             @Override
-            public Object computeNext() {
+            protected Object computeNext() {
                 return new Object();
             }
         };
-        WeakReference<Object> ref = new WeakReference<>(itr.next());
-        GcFinalization.awaitClear(ref);
+
+        // Act: Retrieve an object from the iterator and hold only a weak reference to it.
+        Object returnedObject = iterator.next();
+        WeakReference<Object> weakReference = new WeakReference<>(returnedObject);
+
+        // Assert: The object should be garbage-collectable because the iterator has released its
+        // internal reference. GcFinalization.awaitClear() will time out and fail the test if the
+        // weak reference is not cleared, which would happen if the iterator still held a strong
+        // reference.
+        GcFinalization.awaitClear(weakReference);
     }
 }
