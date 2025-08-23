857d139719ab6dc5ffeba847dfa8228c31217dd5
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:27:48 +0800

    gemini processedManualTestCases dayofyeartest31.java

--- dayofyeartest31.java
+++ dayofyeartest31.java
@@ -1,142 +1,52 @@
 package org.threeten.extra;
 
-import static java.time.temporal.ChronoField.ALIGNED_DAY_OF_WEEK_IN_MONTH;
-import static java.time.temporal.ChronoField.ALIGNED_DAY_OF_WEEK_IN_YEAR;
-import static java.time.temporal.ChronoField.ALIGNED_WEEK_OF_MONTH;
-import static java.time.temporal.ChronoField.ALIGNED_WEEK_OF_YEAR;
-import static java.time.temporal.ChronoField.AMPM_OF_DAY;
-import static java.time.temporal.ChronoField.CLOCK_HOUR_OF_AMPM;
-import static java.time.temporal.ChronoField.CLOCK_HOUR_OF_DAY;
-import static java.time.temporal.ChronoField.DAY_OF_MONTH;
-import static java.time.temporal.ChronoField.DAY_OF_WEEK;
-import static java.time.temporal.ChronoField.DAY_OF_YEAR;
-import static java.time.temporal.ChronoField.EPOCH_DAY;
-import static java.time.temporal.ChronoField.ERA;
-import static java.time.temporal.ChronoField.HOUR_OF_AMPM;
-import static java.time.temporal.ChronoField.HOUR_OF_DAY;
-import static java.time.temporal.ChronoField.INSTANT_SECONDS;
-import static java.time.temporal.ChronoField.MICRO_OF_DAY;
-import static java.time.temporal.ChronoField.MICRO_OF_SECOND;
-import static java.time.temporal.ChronoField.MILLI_OF_DAY;
-import static java.time.temporal.ChronoField.MILLI_OF_SECOND;
-import static java.time.temporal.ChronoField.MINUTE_OF_DAY;
-import static java.time.temporal.ChronoField.MINUTE_OF_HOUR;
-import static java.time.temporal.ChronoField.MONTH_OF_YEAR;
-import static java.time.temporal.ChronoField.NANO_OF_DAY;
-import static java.time.temporal.ChronoField.NANO_OF_SECOND;
-import static java.time.temporal.ChronoField.OFFSET_SECONDS;
-import static java.time.temporal.ChronoField.PROLEPTIC_MONTH;
-import static java.time.temporal.ChronoField.SECOND_OF_DAY;
-import static java.time.temporal.ChronoField.SECOND_OF_MINUTE;
-import static java.time.temporal.ChronoField.YEAR;
-import static java.time.temporal.ChronoField.YEAR_OF_ERA;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertSame;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
-import java.time.Clock;
-import java.time.DateTimeException;
-import java.time.Instant;
+
 import java.time.LocalDate;
-import java.time.LocalTime;
 import java.time.Year;
-import java.time.ZoneId;
-import java.time.chrono.IsoChronology;
-import java.time.chrono.JapaneseDate;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
-import java.time.temporal.IsoFields;
-import java.time.temporal.Temporal;
-import java.time.temporal.TemporalAccessor;
-import java.time.temporal.TemporalAdjuster;
-import java.time.temporal.TemporalField;
-import java.time.temporal.TemporalQueries;
-import java.time.temporal.TemporalUnit;
-import java.time.temporal.UnsupportedTemporalTypeException;
-import java.time.temporal.ValueRange;
-import org.junit.jupiter.api.Test;
-import org.junitpioneer.jupiter.RetryingTest;
-import com.google.common.testing.EqualsTester;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-public class DayOfYearTestTest31 {
+/**
+ * Tests for {@link DayOfYear#adjustInto(Temporal)}.
+ */
+class DayOfYearAdjustIntoTest {
 
-    private static final Year YEAR_STANDARD = Year.of(2007);
+    private static final Year NON_LEAP_YEAR = Year.of(2007);
 
-    private static final Year YEAR_LEAP = Year.of(2008);
+    // An arbitrary date within the non-leap year, used as the base for adjustment.
+    // The specific day doesn't matter, as adjustInto() should overwrite it.
+    private static final LocalDate A_DATE_IN_NON_LEAP_YEAR = NON_LEAP_YEAR.atDay(100); // 2007-04-10
 
-    private static final int STANDARD_YEAR_LENGTH = 365;
-
-    private static final int LEAP_YEAR_LENGTH = 366;
-
-    private static final DayOfYear TEST = DayOfYear.of(12);
-
-    private static final ZoneId PARIS = ZoneId.of("Europe/Paris");
-
-    private static class TestingField implements TemporalField {
-
-        public static final TestingField INSTANCE = new TestingField();
-
-        @Override
-        public TemporalUnit getBaseUnit() {
-            return ChronoUnit.DAYS;
-        }
-
-        @Override
-        public TemporalUnit getRangeUnit() {
-            return ChronoUnit.YEARS;
-        }
-
-        @Override
-        public ValueRange range() {
-            return ValueRange.of(1, 365, 366);
-        }
-
-        @Override
-        public boolean isDateBased() {
-            return true;
-        }
-
-        @Override
-        public boolean isTimeBased() {
-            return false;
-        }
-
-        @Override
-        public boolean isSupportedBy(TemporalAccessor temporal) {
-            return temporal.isSupported(DAY_OF_YEAR);
-        }
-
-        @Override
-        public ValueRange rangeRefinedBy(TemporalAccessor temporal) {
-            return range();
-        }
-
-        @Override
-        public long getFrom(TemporalAccessor temporal) {
-            return temporal.getLong(DAY_OF_YEAR);
-        }
-
-        @Override
-        @SuppressWarnings("unchecked")
-        public <R extends Temporal> R adjustInto(R temporal, long newValue) {
-            return (R) temporal.with(DAY_OF_YEAR, newValue);
-        }
+    /**
+     * Provides test cases for adjusting a date in a non-leap year.
+     * @return A stream of arguments, each containing a day-of-year value and the expected LocalDate result.
+     */
+    private static Stream<Arguments> nonLeapYearAdjustmentCases() {
+        return Stream.of(
+            // Case 1: First day of the year
+            Arguments.of(1, NON_LEAP_YEAR.atDay(1)),   // Jan 1st
+            // Case 2: The day before where a leap day would be in a leap year
+            Arguments.of(59, NON_LEAP_YEAR.atDay(59)),  // Feb 28th
+            // Case 3: The day immediately after the "skipped" leap day
+            Arguments.of(60, NON_LEAP_YEAR.atDay(60)),  // Mar 1st
+            // Case 4: Last day of the year
+            Arguments.of(365, NON_LEAP_YEAR.atDay(365)) // Dec 31st
+        );
     }
 
-    @Test
-    public void test_adjustInto_fromEndOfYear_notLeapYear() {
-        LocalDate base = LocalDate.of(2007, 12, 31);
-        LocalDate expected = LocalDate.of(2007, 1, 1);
-        for (int i = 1; i <= STANDARD_YEAR_LENGTH; i++) {
-            DayOfYear test = DayOfYear.of(i);
-            assertEquals(expected, test.adjustInto(base));
-            expected = expected.plusDays(1);
-        }
+    @ParameterizedTest(name = "Day {0} in a non-leap year should adjust date to {1}")
+    @MethodSource("nonLeapYearAdjustmentCases")
+    void adjustInto_shouldSetCorrectDayInNonLeapYear(int dayOfYearValue, LocalDate expectedDate) {
+        // Arrange
+        DayOfYear dayOfYearToAdjustTo = DayOfYear.of(dayOfYearValue);
+
+        // Act
+        LocalDate adjustedDate = dayOfYearToAdjustTo.adjustInto(A_DATE_IN_NON_LEAP_YEAR);
+
+        // Assert
+        assertEquals(expectedDate, adjustedDate);
     }
 }
