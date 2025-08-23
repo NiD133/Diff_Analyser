d114b74dda0db08b173605394e8936b76e409be6
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:57:48 +0800

    Original processedAutoTestCases jsontreereader_estest101.java

---
+++ jsontreereader_estest101.java
@@ -0,0 +1,36 @@
+package com.google.gson.internal.bind;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonNull;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.Strictness;
+import com.google.gson.stream.JsonToken;
+import java.io.IOException;
+import java.util.ConcurrentModificationException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class JsonTreeReader_ESTestTest101 extends JsonTreeReader_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test100() throws Throwable {
+        JsonArray jsonArray0 = new JsonArray();
+        JsonTreeReader jsonTreeReader0 = new JsonTreeReader(jsonArray0);
+        // Undeclared exception!
+        try {
+            jsonTreeReader0.endObject();
+            fail("Expecting exception: IllegalStateException");
+        } catch (IllegalStateException e) {
+            //
+            // Expected END_OBJECT but was BEGIN_ARRAY at path $
+            //
+            verifyException("com.google.gson.internal.bind.JsonTreeReader", e);
+        }
+    }
+}
