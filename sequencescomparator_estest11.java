f26133798f9400f15d3026ce75d078282904019a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:14:34 +0800

    Original processedAutoTestCases sequencescomparator_estest11.java

---
+++ sequencescomparator_estest11.java
@@ -0,0 +1,33 @@
+package org.apache.commons.collections4.sequence;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.ConcurrentModificationException;
+import java.util.LinkedList;
+import java.util.List;
+import org.apache.commons.collections4.Equator;
+import org.apache.commons.collections4.Predicate;
+import org.apache.commons.collections4.functors.DefaultEquator;
+import org.apache.commons.collections4.functors.NotNullPredicate;
+import org.apache.commons.collections4.functors.PredicateTransformer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class SequencesComparator_ESTestTest11 extends SequencesComparator_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test10() throws Throwable {
+        LinkedList<Object> linkedList0 = new LinkedList<Object>();
+        LinkedList<Object> linkedList1 = new LinkedList<Object>();
+        linkedList0.offerFirst(linkedList1);
+        Object object0 = new Object();
+        linkedList1.add(object0);
+        linkedList1.add((Object) linkedList0);
+        linkedList1.add(object0);
+        SequencesComparator<Object> sequencesComparator0 = new SequencesComparator<Object>(linkedList0, linkedList1);
+        EditScript<Object> editScript0 = sequencesComparator0.getScript();
+        assertEquals(4, editScript0.getModifications());
+    }
+}
