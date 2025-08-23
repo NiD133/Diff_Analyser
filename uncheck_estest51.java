892b291d936779269962a351293f22cf8d795e79
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:24:43 +0800

    gemini processedAutoTestCases uncheck_estest51.java

--- uncheck_estest51.java
+++ uncheck_estest51.java
@@ -1,37 +1,28 @@
 package org.apache.commons.io.function;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.time.chrono.HijrahEra;
-import java.util.Comparator;
-import java.util.concurrent.ForkJoinTask;
-import java.util.function.BiConsumer;
-import java.util.function.BinaryOperator;
-import java.util.function.Function;
 import java.util.function.Supplier;
-import java.util.stream.Collector;
-import java.util.stream.LongStream;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
 
+/**
+ * Contains tests for the {@link Uncheck} utility class.
+ * Note: The original class name and inheritance were preserved for context.
+ * In a typical project, a more descriptive name like "UncheckTest" would be preferred.
+ */
 public class Uncheck_ESTestTest51 extends Uncheck_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test50() throws Throwable {
-        // Undeclared exception!
-        try {
-            Uncheck.getAsLong((IOLongSupplier) null, (Supplier<String>) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.commons.io.function.Uncheck", e);
-        }
+    /**
+     * Tests that calling {@link Uncheck#getAsLong(IOLongSupplier, Supplier)} with a null
+     * {@link IOLongSupplier} throws a {@link NullPointerException}.
+     */
+    @Test(expected = NullPointerException.class)
+    public void getAsLongWithNullSupplierShouldThrowNullPointerException() {
+        // Arrange: The IOLongSupplier is null. The message supplier is not relevant
+        // for this test and can also be null.
+        IOLongSupplier nullSupplier = null;
+        Supplier<String> nullMessageSupplier = null;
+
+        // Act & Assert: Call the method under test.
+        // The @Test(expected) annotation asserts that a NullPointerException is thrown.
+        Uncheck.getAsLong(nullSupplier, nullMessageSupplier);
     }
 }
