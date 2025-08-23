cb280b2839911812fc92f1e516233ef9f5d9a725
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:26:25 +0800

    gemini processedManualTestCases caseformattest30.java

--- caseformattest30.java
+++ caseformattest30.java
@@ -1,28 +1,28 @@
 package com.google.common.base;
 
-import static com.google.common.base.CaseFormat.LOWER_CAMEL;
 import static com.google.common.base.CaseFormat.LOWER_HYPHEN;
-import static com.google.common.base.CaseFormat.LOWER_UNDERSCORE;
 import static com.google.common.base.CaseFormat.UPPER_CAMEL;
-import static com.google.common.base.CaseFormat.UPPER_UNDERSCORE;
 import static com.google.common.truth.Truth.assertThat;
-import static com.google.common.truth.Truth.assertWithMessage;
-import com.google.common.annotations.GwtCompatible;
-import com.google.common.annotations.GwtIncompatible;
-import com.google.common.annotations.J2ktIncompatible;
-import com.google.common.testing.NullPointerTester;
-import com.google.common.testing.SerializableTester;
-import junit.framework.TestCase;
-import org.jspecify.annotations.NullUnmarked;
 
+import com.google.common.annotations.GwtCompatible;
+import junit.framework.TestCase;
+
+@GwtCompatible // All CaseFormat methods are GwtCompatible
 public class CaseFormatTestTest30 extends TestCase {
 
-    public void testConverter_nullConversions() {
-        for (CaseFormat outer : CaseFormat.values()) {
-            for (CaseFormat inner : CaseFormat.values()) {
-                assertThat(outer.converterTo(inner).convert(null)).isNull();
-                assertThat(outer.converterTo(inner).reverse().convert(null)).isNull();
-            }
-        }
-    }
+  /**
+   * The contract of a Guava Converter is to return null when given a null input. This test
+   * verifies this behavior for CaseFormat converters.
+   */
+  public void testConverter_givenNullInput_returnsNull() {
+    // The null-handling behavior is defined in the base Converter class and is the same for all
+    // format combinations. We use one representative example to verify this contract.
+    Converter<String, String> converter = LOWER_HYPHEN.converterTo(UPPER_CAMEL);
+
+    // Test that a forward conversion of null results in null.
+    assertThat(converter.convert(null)).isNull();
+
+    // Test that a reverse conversion of null also results in null.
+    assertThat(converter.reverse().convert(null)).isNull();
+  }
 }
