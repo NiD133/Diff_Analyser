853b7459bf782fbdea07ea2beee7f6a205e60e77
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:24:20 +0800

    Original processedAutoTestCases typehandler_estest3.java

---
+++ typehandler_estest3.java
@@ -0,0 +1,22 @@
+package org.apache.commons.cli;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.File;
+import java.util.Date;
+import java.util.Map;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.lang.MockThrowable;
+import org.junit.runner.RunWith;
+
+public class TypeHandler_ESTestTest3 extends TypeHandler_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test02() throws Throwable {
+        Class<MockThrowable> class0 = MockThrowable.class;
+        MockThrowable mockThrowable0 = TypeHandler.createValue((String) null, class0);
+        assertNull(mockThrowable0);
+    }
+}
