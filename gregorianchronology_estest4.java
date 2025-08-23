d0fab13c96d0f8291d78f72cda403d692fad429b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:52:11 +0800

    gemini processedAutoTestCases gregorianchronology_estest4.java

--- gregorianchronology_estest4.java
+++ gregorianchronology_estest4.java
@@ -1,23 +1,32 @@
 package org.joda.time.chrono;
 
+import static org.junit.Assert.assertFalse;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.TimeZone;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.joda.time.Chronology;
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
-import org.joda.time.LocalDateTime;
-import org.junit.runner.RunWith;
 
-public class GregorianChronology_ESTestTest4 extends GregorianChronology_ESTest_scaffolding {
+/**
+ * Unit tests for {@link GregorianChronology}.
+ */
+public class GregorianChronologyTest {
 
-    @Test(timeout = 4000)
-    public void test03() throws Throwable {
-        GregorianChronology gregorianChronology0 = GregorianChronology.getInstance();
-        boolean boolean0 = gregorianChronology0.isLeapYear((-900));
-        assertFalse(boolean0);
+    /**
+     * Tests that a year divisible by 100 but not by 400 is correctly identified
+     * as a common (non-leap) year.
+     */
+    @Test
+    public void isLeapYear_shouldReturnFalse_forYearDivisibleBy100ButNot400() {
+        // The Gregorian calendar rule states a year is a leap year if it is
+        // divisible by 4, except for end-of-century years, which must be
+        // divisible by 400 to be a leap year.
+        // The year -900 is divisible by 100 but not by 400, so it is not a leap year.
+
+        // Arrange
+        GregorianChronology chronology = GregorianChronology.getInstance();
+        int year = -900;
+
+        // Act
+        boolean isLeap = chronology.isLeapYear(year);
+
+        // Assert
+        assertFalse("A year divisible by 100 but not 400 should not be a leap year.", isLeap);
     }
 }
