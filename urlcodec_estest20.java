6c04b3add5a43575c67b399906c642b305cf96e5
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:24:51 +0800

    Original processedAutoTestCases urlcodec_estest20.java

---
+++ urlcodec_estest20.java
@@ -0,0 +1,21 @@
+package org.apache.commons.codec.net;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.UnsupportedEncodingException;
+import java.util.BitSet;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class URLCodec_ESTestTest20 extends URLCodec_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test19() throws Throwable {
+        URLCodec uRLCodec0 = new URLCodec();
+        Object object0 = uRLCodec0.decode((Object) "UTF-8");
+        assertEquals("UTF-8", object0);
+        assertNotNull(object0);
+    }
+}
