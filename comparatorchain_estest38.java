9edd708e5e06e2588a12d3d7fdf8dc7bd51b4382
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:45:03 +0800

    gemini processedAutoTestCases comparatorchain_estest38.java

--- comparatorchain_estest38.java
+++ comparatorchain_estest38.java
@@ -1,35 +1,23 @@
 package org.apache.commons.collections4.comparators;
 
+import static org.junit.Assert.assertFalse;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.nio.ByteBuffer;
-import java.nio.LongBuffer;
-import java.util.BitSet;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.function.Function;
-import java.util.function.ToIntFunction;
-import java.util.function.ToLongFunction;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.functors.ClosureTransformer;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ExceptionClosure;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
 
-public class ComparatorChain_ESTestTest38 extends ComparatorChain_ESTest_scaffolding {
+/**
+ * Unit tests for {@link org.apache.commons.collections4.comparators.ComparatorChain}.
+ */
+public class ComparatorChainTest {
 
-    @Test(timeout = 4000)
-    public void test37() throws Throwable {
-        LinkedList<Comparator<Object>> linkedList0 = new LinkedList<Comparator<Object>>();
-        BitSet bitSet0 = new BitSet();
-        ComparatorChain<Object> comparatorChain0 = new ComparatorChain<Object>(linkedList0, bitSet0);
-        boolean boolean0 = comparatorChain0.isLocked();
-        assertFalse(boolean0);
+    @Test
+    public void newComparatorChainShouldNotBeLocked() {
+        // Arrange: Create a new, empty ComparatorChain.
+        // A new chain should be modifiable, and therefore not "locked".
+        final ComparatorChain<Object> chain = new ComparatorChain<>();
+
+        // Act: Check if the chain is locked.
+        final boolean isLocked = chain.isLocked();
+
+        // Assert: The chain should not be locked, allowing for further configuration.
+        assertFalse("A newly created ComparatorChain should not be locked", isLocked);
     }
 }
