1a3cb306d1849c92e483fa2ed421d5578628b24a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:44:30 +0800

    gemini processedAutoTestCases collectionsortedbag_estest22.java

--- collectionsortedbag_estest22.java
+++ collectionsortedbag_estest22.java
@@ -1,58 +1,35 @@
 package org.apache.commons.collections4.bag;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.ConcurrentModificationException;
-import java.util.LinkedList;
-import java.util.Locale;
-import java.util.PriorityQueue;
-import java.util.Set;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.Predicate;
 import org.apache.commons.collections4.SortedBag;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionPredicate;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.IfClosure;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.NOPClosure;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.TransformedPredicate;
-import org.apache.commons.collections4.functors.TransformerPredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class CollectionSortedBag_ESTestTest22 extends CollectionSortedBag_ESTest_scaffolding {
+import java.util.Locale;
 
-    @Test(timeout = 4000)
-    public void test21() throws Throwable {
-        TreeBag<Locale.FilteringMode> treeBag0 = new TreeBag<Locale.FilteringMode>();
-        SortedBag<Locale.FilteringMode> sortedBag0 = UnmodifiableSortedBag.unmodifiableSortedBag((SortedBag<Locale.FilteringMode>) treeBag0);
-        CollectionSortedBag<Locale.FilteringMode> collectionSortedBag0 = new CollectionSortedBag<Locale.FilteringMode>(sortedBag0);
-        Locale.FilteringMode locale_FilteringMode0 = Locale.FilteringMode.EXTENDED_FILTERING;
-        // Undeclared exception!
-        try {
-            collectionSortedBag0.add(locale_FilteringMode0, 4463);
-            fail("Expecting exception: UnsupportedOperationException");
-        } catch (UnsupportedOperationException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.commons.collections4.bag.UnmodifiableSortedBag", e);
-        }
+/**
+ * Contains tests for {@link CollectionSortedBag}, focusing on its decorator behavior.
+ */
+public class CollectionSortedBagTest {
+
+    /**
+     * Tests that a modification attempt on a CollectionSortedBag that wraps an unmodifiable bag
+     * correctly throws an UnsupportedOperationException.
+     *
+     * This verifies that the decorator correctly delegates the modification call,
+     * preserving the unmodifiable nature of the decorated bag.
+     */
+    @Test(expected = UnsupportedOperationException.class)
+    public void addWithCountShouldThrowExceptionWhenDecoratingUnmodifiableBag() {
+        // Arrange: Create a CollectionSortedBag that decorates an unmodifiable bag.
+        final SortedBag<Locale.FilteringMode> unmodifiableBag =
+                UnmodifiableSortedBag.unmodifiableSortedBag(new TreeBag<>());
+
+        final SortedBag<Locale.FilteringMode> collectionBag = new CollectionSortedBag<>(unmodifiableBag);
+
+        final Locale.FilteringMode elementToAdd = Locale.FilteringMode.EXTENDED_FILTERING;
+        final int count = 5;
+
+        // Act: Attempt to add an element to the decorated bag.
+        // Assert: The test expects an UnsupportedOperationException to be thrown by the line below.
+        collectionBag.add(elementToAdd, count);
     }
 }
