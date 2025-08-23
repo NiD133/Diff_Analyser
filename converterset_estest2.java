4e9fe7c8b67974ebeaf4fb78b7a53df2627e6d3e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:45:44 +0800

    gemini processedAutoTestCases converterset_estest2.java

--- converterset_estest2.java
+++ converterset_estest2.java
@@ -2,33 +2,37 @@ package org.joda.time.convert;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.joda.time.Hours;
-import org.joda.time.Interval;
-import org.joda.time.MutablePeriod;
-import org.joda.time.PeriodType;
-import org.joda.time.Seconds;
-import org.joda.time.chrono.CopticChronology;
-import org.junit.runner.RunWith;
 
-public class ConverterSet_ESTestTest2 extends ConverterSet_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link ConverterSet} class.
+ */
+public class ConverterSetTest {
 
-    @Test(timeout = 4000)
-    public void test01() throws Throwable {
-        Converter[] converterArray0 = new Converter[8];
-        ReadableInstantConverter readableInstantConverter0 = new ReadableInstantConverter();
-        converterArray0[0] = (Converter) readableInstantConverter0;
-        ReadableIntervalConverter readableIntervalConverter0 = new ReadableIntervalConverter();
-        converterArray0[1] = (Converter) readableIntervalConverter0;
-        ReadablePeriodConverter readablePeriodConverter0 = new ReadablePeriodConverter();
-        converterArray0[2] = (Converter) readablePeriodConverter0;
-        CalendarConverter calendarConverter0 = new CalendarConverter();
-        converterArray0[3] = (Converter) calendarConverter0;
-        CalendarConverter calendarConverter1 = new CalendarConverter();
-        ConverterSet converterSet0 = new ConverterSet(converterArray0);
-        ConverterSet converterSet1 = converterSet0.add(calendarConverter1, converterArray0);
-        assertNotSame(converterSet1, converterSet0);
+    /**
+     * Tests that the add() method returns a new ConverterSet instance when a
+     * converter is replaced with a new instance of the same type.
+     * The ConverterSet is immutable, so modifications should always result in a new object.
+     */
+    @Test
+    public void add_whenReplacingConverter_returnsNewInstance() {
+        // Arrange: Create a set containing an initial converter.
+        Converter existingConverter = new CalendarConverter();
+        ConverterSet initialSet = new ConverterSet(new Converter[]{existingConverter});
+
+        // The converter to add is a new instance of a type that's already in the set.
+        Converter newConverter = new CalendarConverter();
+
+        // Act: Add the new converter, which should replace the existing one.
+        // We pass 'null' for the 'removed' parameter as we don't need to inspect it.
+        ConverterSet updatedSet = initialSet.add(newConverter, null);
+
+        // Assert: The operation should return a new ConverterSet instance because
+        // the contents of the set have changed.
+        assertNotNull("The updated set should not be null.", updatedSet);
+        assertNotSame(
+            "A new ConverterSet instance should be returned when a converter is replaced.",
+            initialSet,
+            updatedSet
+        );
     }
 }
