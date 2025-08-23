be3491e490db70e7a969d0849c90b93acbcf2774
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:21:53 +0800

    Original processedAutoTestCases thresholdingoutputstream_estest14.java

---
+++ thresholdingoutputstream_estest14.java
@@ -0,0 +1,30 @@
+package org.apache.commons.io.output;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.OutputStream;
+import org.apache.commons.io.function.IOConsumer;
+import org.apache.commons.io.function.IOFunction;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class ThresholdingOutputStream_ESTestTest14 extends ThresholdingOutputStream_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test13() throws Throwable {
+        DeferredFileOutputStream.Builder deferredFileOutputStream_Builder0 = new DeferredFileOutputStream.Builder();
+        DeferredFileOutputStream deferredFileOutputStream0 = deferredFileOutputStream_Builder0.get();
+        // Undeclared exception!
+        try {
+            deferredFileOutputStream0.checkThreshold(1);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("java.nio.file.Files", e);
+        }
+    }
+}
