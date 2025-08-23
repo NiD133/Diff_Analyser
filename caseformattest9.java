7461148043ffb2c4fe487b5f6f25d31c7059ac44
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:26:27 +0800

    gemini processedManualTestCases caseformattest9.java

--- caseformattest9.java
+++ caseformattest9.java
@@ -1,24 +1,26 @@
 package com.google.common.base;
 
-import static com.google.common.base.CaseFormat.LOWER_CAMEL;
-import static com.google.common.base.CaseFormat.LOWER_HYPHEN;
 import static com.google.common.base.CaseFormat.LOWER_UNDERSCORE;
-import static com.google.common.base.CaseFormat.UPPER_CAMEL;
-import static com.google.common.base.CaseFormat.UPPER_UNDERSCORE;
 import static com.google.common.truth.Truth.assertThat;
-import static com.google.common.truth.Truth.assertWithMessage;
-import com.google.common.annotations.GwtCompatible;
-import com.google.common.annotations.GwtIncompatible;
-import com.google.common.annotations.J2ktIncompatible;
-import com.google.common.testing.NullPointerTester;
-import com.google.common.testing.SerializableTester;
-import junit.framework.TestCase;
-import org.jspecify.annotations.NullUnmarked;
 
+import junit.framework.TestCase;
+
+/**
+ * Tests for {@link CaseFormat}, focusing on identity conversions.
+ */
 public class CaseFormatTestTest9 extends TestCase {
 
-    public void testLowerUnderscoreToLowerUnderscore() {
-        assertThat(LOWER_UNDERSCORE.to(LOWER_UNDERSCORE, "foo")).isEqualTo("foo");
-        assertThat(LOWER_UNDERSCORE.to(LOWER_UNDERSCORE, "foo_bar")).isEqualTo("foo_bar");
-    }
+  /**
+   * Tests that converting a string from a format to itself is an identity operation,
+   * meaning the output string is identical to the input.
+   */
+  public void testLowerUnderscore_convertToSelf_isIdentity() {
+    // A string with a single "word"
+    String singleWord = "foo";
+    assertThat(LOWER_UNDERSCORE.to(LOWER_UNDERSCORE, singleWord)).isEqualTo(singleWord);
+
+    // A string with multiple "words" separated by the format's delimiter
+    String multiWord = "foo_bar";
+    assertThat(LOWER_UNDERSCORE.to(LOWER_UNDERSCORE, multiWord)).isEqualTo(multiWord);
+  }
 }
