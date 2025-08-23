d627934fccdea63aec422c9d7816c52c57dff49c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:31:14 +0800

    Original processedManualTestCases minutestest7.java

---
+++ minutestest7.java
@@ -0,0 +1,51 @@
+package org.joda.time;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class MinutesTestTest7 extends TestCase {
+
+    // (before the late 90's they were all over the place)
+    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
+
+    public static TestSuite suite() {
+        return new TestSuite(TestMinutes.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+    }
+
+    public void testFactory_parseMinutes_String() {
+        assertEquals(0, Minutes.parseMinutes((String) null).getMinutes());
+        assertEquals(0, Minutes.parseMinutes("PT0M").getMinutes());
+        assertEquals(1, Minutes.parseMinutes("PT1M").getMinutes());
+        assertEquals(-3, Minutes.parseMinutes("PT-3M").getMinutes());
+        assertEquals(2, Minutes.parseMinutes("P0Y0M0DT2M").getMinutes());
+        assertEquals(2, Minutes.parseMinutes("PT0H2M").getMinutes());
+        try {
+            Minutes.parseMinutes("P1Y1D");
+            fail();
+        } catch (IllegalArgumentException ex) {
+            // expected
+        }
+        try {
+            Minutes.parseMinutes("P1DT1M");
+            fail();
+        } catch (IllegalArgumentException ex) {
+            // expected
+        }
+    }
+}
