5a3e0cb3727713c9263d6990bc7fb46bf3895b4e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:18:28 +0800

    gemini processedAutoTestCases stringconverter_estest29.java

--- stringconverter_estest29.java
+++ stringconverter_estest29.java
@@ -1,46 +1,28 @@
 package org.joda.time.convert;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.joda.time.Chronology;
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeFieldType;
-import org.joda.time.DateTimeZone;
-import org.joda.time.LocalDate;
-import org.joda.time.MonthDay;
-import org.joda.time.MutableDateTime;
-import org.joda.time.MutableInterval;
-import org.joda.time.MutablePeriod;
-import org.joda.time.Partial;
-import org.joda.time.PeriodType;
-import org.joda.time.ReadWritableInterval;
-import org.joda.time.ReadWritablePeriod;
-import org.joda.time.ReadableInstant;
-import org.joda.time.ReadablePartial;
-import org.joda.time.chrono.CopticChronology;
-import org.joda.time.chrono.EthiopicChronology;
-import org.joda.time.chrono.GJChronology;
-import org.joda.time.chrono.GregorianChronology;
-import org.joda.time.chrono.ISOChronology;
-import org.joda.time.chrono.IslamicChronology;
-import org.joda.time.chrono.JulianChronology;
-import org.joda.time.chrono.ZonedChronology;
-import org.joda.time.format.DateTimeFormatter;
-import org.joda.time.format.DateTimeParser;
-import org.joda.time.format.DateTimePrinter;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class StringConverter_ESTestTest29 extends StringConverter_ESTest_scaffolding {
+/**
+ * Unit tests for {@link StringConverter}.
+ */
+public class StringConverterTest {
 
-    @Test(timeout = 4000)
-    public void test28() throws Throwable {
-        StringConverter stringConverter0 = new StringConverter();
-        long long0 = stringConverter0.getDurationMillis("Pt2s");
-        assertEquals(2000L, long0);
+    /**
+     * Tests that getDurationMillis() correctly parses an ISO 8601 period string
+     * representing a duration of seconds.
+     */
+    @Test
+    public void getDurationMillis_shouldParseISOPeriodStringWithSeconds() {
+        // Arrange
+        final StringConverter converter = new StringConverter();
+        final String isoPeriodString = "Pt2s"; // ISO 8601 format for a 2-second period
+        final long expectedMillis = 2000L;
+
+        // Act
+        final long actualMillis = converter.getDurationMillis(isoPeriodString);
+
+        // Assert
+        assertEquals(expectedMillis, actualMillis);
     }
 }
