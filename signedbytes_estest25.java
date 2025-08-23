3f79eee79ccb1f49480069f1aa652e8ba0a7ade0
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:15:55 +0800

    gemini processedAutoTestCases signedbytes_estest25.java

--- signedbytes_estest25.java
+++ signedbytes_estest25.java
@@ -1,27 +1,16 @@
 package com.google.common.primitives;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.Comparator;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class SignedBytes_ESTestTest25 extends SignedBytes_ESTest_scaffolding {
+/**
+ * Unit tests for {@link SignedBytes}.
+ */
+public class SignedBytesTest {
 
-    @Test(timeout = 4000)
-    public void test24() throws Throwable {
-        byte[] byteArray0 = new byte[0];
-        // Undeclared exception!
-        try {
-            SignedBytes.min(byteArray0);
-            fail("Expecting exception: IllegalArgumentException");
-        } catch (IllegalArgumentException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("com.google.common.base.Preconditions", e);
-        }
+    @Test(expected = IllegalArgumentException.class)
+    public void min_onEmptyArray_throwsIllegalArgumentException() {
+        // The min() method is specified to throw an exception for an empty array,
+        // as there is no minimum value to return.
+        SignedBytes.min(new byte[0]);
     }
 }
