f7682c238c3c5d63a080c330c52615f2fe772a39
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:44:28 +0800

    gemini processedAutoTestCases collectionsortedbag_estest16.java

--- collectionsortedbag_estest16.java
+++ collectionsortedbag_estest16.java
@@ -1,60 +1,44 @@
 package org.apache.commons.collections4.bag;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.Comparator;
+import static org.junit.Assert.fail;
+
 import java.util.ConcurrentModificationException;
-import java.util.LinkedList;
-import java.util.Locale;
-import java.util.PriorityQueue;
-import java.util.Set;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.Predicate;
 import org.apache.commons.collections4.SortedBag;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionPredicate;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.IfClosure;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.NOPClosure;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.TransformedPredicate;
-import org.apache.commons.collections4.functors.TransformerPredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class CollectionSortedBag_ESTestTest16 extends CollectionSortedBag_ESTest_scaffolding {
+/**
+ * Tests for {@link CollectionSortedBag} focusing on concurrent modification scenarios.
+ */
+public class CollectionSortedBagTest {
 
-    @Test(timeout = 4000)
-    public void test15() throws Throwable {
-        HashBag<Integer> hashBag0 = new HashBag<Integer>();
-        Integer integer0 = new Integer((-219));
-        hashBag0.add(integer0);
-        SynchronizedSortedBag<Integer> synchronizedSortedBag0 = new SynchronizedSortedBag<Integer>(hashBag0, hashBag0);
-        hashBag0.add(integer0);
-        CollectionSortedBag<Integer> collectionSortedBag0 = new CollectionSortedBag<Integer>(synchronizedSortedBag0);
-        // Undeclared exception!
+    /**
+     * Tests that a ConcurrentModificationException is thrown when attempting to add a bag's
+     * contents to itself using addAll().
+     * <p>
+     * This is the expected behavior for collections based on the fail-fast iterator
+     * principle. The addAll() method iterates over the source collection while adding
+     * elements to the destination. When the source and destination are the same, the
+     * underlying collection is modified during iteration, which the iterator detects
+     * and signals by throwing a ConcurrentModificationException.
+     */
+    @Test
+    public void addAllWithSelfAsSourceShouldThrowConcurrentModificationException() {
+        // Arrange
+        // Use TreeBag as a concrete implementation of SortedBag.
+        final SortedBag<String> underlyingBag = new TreeBag<>();
+        underlyingBag.add("one");
+        underlyingBag.add("two");
+
+        // Decorate the bag with CollectionSortedBag, the class under test.
+        final SortedBag<String> collectionSortedBag = CollectionSortedBag.collectionSortedBag(underlyingBag);
+
+        // Act & Assert
         try {
-            collectionSortedBag0.addAll(synchronizedSortedBag0);
-            fail("Expecting exception: ConcurrentModificationException");
-        } catch (ConcurrentModificationException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.commons.collections4.bag.AbstractMapBag$BagIterator", e);
+            // Attempt to add the bag to itself. This operation should fail fast.
+            collectionSortedBag.addAll(collectionSortedBag);
+            fail("Expected a ConcurrentModificationException to be thrown.");
+        } catch (final ConcurrentModificationException e) {
+            // This exception is expected. The test passes.
         }
     }
 }
