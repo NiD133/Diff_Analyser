cf0b58d0d9ef291a328240b0c4a98061028fa982
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:41:12 +0800

    Original processedAutoTestCases cachebuilder_estest4.java

---
+++ cachebuilder_estest4.java
@@ -0,0 +1,30 @@
+package org.apache.ibatis.mapping;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.Properties;
+import org.apache.ibatis.cache.Cache;
+import org.apache.ibatis.cache.decorators.BlockingCache;
+import org.apache.ibatis.cache.decorators.SynchronizedCache;
+import org.apache.ibatis.cache.impl.PerpetualCache;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class CacheBuilder_ESTestTest4 extends CacheBuilder_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test03() throws Throwable {
+        Properties properties0 = new Properties();
+        properties0.setProperty("xPxjnuTbn[", "xPxjnuTbn[");
+        CacheBuilder cacheBuilder0 = new CacheBuilder("xPxjnuTbn[");
+        cacheBuilder0.properties(properties0);
+        // Undeclared exception!
+        try {
+            cacheBuilder0.build();
+            fail("Expecting exception: StringIndexOutOfBoundsException");
+        } catch (StringIndexOutOfBoundsException e) {
+        }
+    }
+}
