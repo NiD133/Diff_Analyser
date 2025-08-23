bc96abdf6bf6f9fe51ff2c8859826b72b5cb09db
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:44:59 +0800

    Original processedAutoTestCases comparatorchain_estest25.java

---
+++ comparatorchain_estest25.java
@@ -0,0 +1,35 @@
+package org.apache.commons.collections4.comparators;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.nio.ByteBuffer;
+import java.nio.LongBuffer;
+import java.util.BitSet;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.ToIntFunction;
+import java.util.function.ToLongFunction;
+import org.apache.commons.collections4.Closure;
+import org.apache.commons.collections4.functors.ClosureTransformer;
+import org.apache.commons.collections4.functors.ComparatorPredicate;
+import org.apache.commons.collections4.functors.ExceptionClosure;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class ComparatorChain_ESTestTest25 extends ComparatorChain_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test24() throws Throwable {
+        LinkedList<Comparator<Object>> linkedList0 = new LinkedList<Comparator<Object>>();
+        ComparatorChain<Object> comparatorChain0 = new ComparatorChain<Object>(linkedList0);
+        ComparatorChain<Object> comparatorChain1 = new ComparatorChain<Object>(comparatorChain0);
+        boolean boolean0 = comparatorChain0.equals(comparatorChain1);
+        assertFalse(boolean0);
+    }
+}
