4bc5eab89f69bec9f7c142c0edc6236e3471cf18
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:24:56 +0800

    Original processedManualTestCases atomicdoublearraytest12.java

---
+++ atomicdoublearraytest12.java
@@ -0,0 +1,79 @@
+package com.google.common.util.concurrent;
+
+import static java.lang.Math.max;
+import static org.junit.Assert.assertThrows;
+import com.google.common.annotations.GwtIncompatible;
+import com.google.common.annotations.J2ktIncompatible;
+import com.google.common.testing.NullPointerTester;
+import java.util.Arrays;
+import org.jspecify.annotations.NullUnmarked;
+
+public class AtomicDoubleArrayTestTest12 extends JSR166TestCase {
+
+    private static final double[] VALUES = { Double.NEGATIVE_INFINITY, -Double.MAX_VALUE, (double) Long.MIN_VALUE, (double) Integer.MIN_VALUE, -Math.PI, -1.0, -Double.MIN_VALUE, -0.0, +0.0, Double.MIN_VALUE, 1.0, Math.PI, (double) Integer.MAX_VALUE, (double) Long.MAX_VALUE, Double.MAX_VALUE, Double.POSITIVE_INFINITY, Double.NaN, Float.MAX_VALUE };
+
+    static final long COUNTDOWN = 100000;
+
+    /**
+     * The notion of equality used by AtomicDoubleArray
+     */
+    static boolean bitEquals(double x, double y) {
+        return Double.doubleToRawLongBits(x) == Double.doubleToRawLongBits(y);
+    }
+
+    static void assertBitEquals(double x, double y) {
+        assertEquals(Double.doubleToRawLongBits(x), Double.doubleToRawLongBits(y));
+    }
+
+    class Counter extends CheckedRunnable {
+
+        final AtomicDoubleArray aa;
+
+        volatile long counts;
+
+        Counter(AtomicDoubleArray a) {
+            aa = a;
+        }
+
+        @Override
+        public void realRun() {
+            for (; ; ) {
+                boolean done = true;
+                for (int i = 0; i < aa.length(); i++) {
+                    double v = aa.get(i);
+                    assertTrue(v >= 0);
+                    if (v != 0) {
+                        done = false;
+                        if (aa.compareAndSet(i, v, v - 1.0)) {
+                            ++counts;
+                        }
+                    }
+                }
+                if (done) {
+                    break;
+                }
+            }
+        }
+    }
+
+    /**
+     * repeated weakCompareAndSet succeeds in changing value when equal to expected
+     */
+    public void testWeakCompareAndSet() {
+        AtomicDoubleArray aa = new AtomicDoubleArray(SIZE);
+        for (int i : new int[] { 0, SIZE - 1 }) {
+            double prev = 0.0;
+            double unused = Math.E + Math.PI;
+            for (double x : VALUES) {
+                assertBitEquals(prev, aa.get(i));
+                assertFalse(aa.weakCompareAndSet(i, unused, x));
+                assertBitEquals(prev, aa.get(i));
+                while (!aa.weakCompareAndSet(i, prev, x)) {
+                    ;
+                }
+                assertBitEquals(x, aa.get(i));
+                prev = x;
+            }
+        }
+    }
+}
