c2ff65249a8bbf8320931ceae2c5c386fd12e45a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:53:49 +0800

    gemini processedAutoTestCases immutablenetwork_estest3.java

--- immutablenetwork_estest3.java
+++ immutablenetwork_estest3.java
@@ -1,25 +1,20 @@
 package com.google.common.graph;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class ImmutableNetwork_ESTestTest3 extends ImmutableNetwork_ESTest_scaffolding {
+/**
+ * Tests for {@link ImmutableNetwork}.
+ */
+public class ImmutableNetworkTest {
 
-    @Test(timeout = 4000)
-    public void test2() throws Throwable {
-        // Undeclared exception!
-        try {
-            ImmutableNetwork.copyOf((ImmutableNetwork<Object, Integer>) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("com.google.common.base.Preconditions", e);
-        }
+    /**
+     * Tests that calling the deprecated {@code copyOf(ImmutableNetwork)} with a null argument
+     * throws a {@link NullPointerException}, as is standard for Guava's static factory methods.
+     */
+    @Test(expected = NullPointerException.class)
+    public void copyOf_withNullImmutableNetwork_throwsNullPointerException() {
+        // The cast to ImmutableNetwork is necessary to resolve the correct overloaded method,
+        // as there is also a copyOf(Network).
+        ImmutableNetwork.copyOf((ImmutableNetwork<?, ?>) null);
     }
 }
