3c078d4a5b3a8120690981eefde060c6fd026666
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:02:09 +0800

    Original processedAutoTestCases mergedstream_estest21.java

---
+++ mergedstream_estest21.java
@@ -0,0 +1,45 @@
+package com.fasterxml.jackson.core.io;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.fasterxml.jackson.core.ErrorReportConfiguration;
+import com.fasterxml.jackson.core.StreamReadConstraints;
+import com.fasterxml.jackson.core.StreamWriteConstraints;
+import com.fasterxml.jackson.core.util.BufferRecycler;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.io.PushbackInputStream;
+import java.io.SequenceInputStream;
+import java.util.Enumeration;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.evosuite.runtime.mock.java.io.MockFileInputStream;
+import org.junit.runner.RunWith;
+
+public class MergedStream_ESTestTest21 extends MergedStream_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test20() throws Throwable {
+        StreamReadConstraints streamReadConstraints0 = StreamReadConstraints.defaults();
+        StreamWriteConstraints streamWriteConstraints0 = StreamWriteConstraints.defaults();
+        ErrorReportConfiguration errorReportConfiguration0 = ErrorReportConfiguration.defaults();
+        BufferRecycler bufferRecycler0 = new BufferRecycler();
+        ContentReference contentReference0 = ContentReference.redacted();
+        IOContext iOContext0 = new IOContext(streamReadConstraints0, streamWriteConstraints0, errorReportConfiguration0, bufferRecycler0, contentReference0, true);
+        Enumeration<FilterInputStream> enumeration0 = (Enumeration<FilterInputStream>) mock(Enumeration.class, new ViolatedAssumptionAnswer());
+        doReturn(false, false).when(enumeration0).hasMoreElements();
+        SequenceInputStream sequenceInputStream0 = new SequenceInputStream(enumeration0);
+        MergedStream mergedStream0 = new MergedStream(iOContext0, sequenceInputStream0, (byte[]) null, 1, 999);
+        mergedStream0.close();
+    }
+}
