682fa69b3fbc6e79a66e5eb8e44045c8545c943e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:30:00 +0800

    gemini processedAutoTestCases xylineannotation_estest21.java

--- xylineannotation_estest21.java
+++ xylineannotation_estest21.java
@@ -2,49 +2,38 @@ package org.jfree.chart.annotations;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
+
 import java.awt.BasicStroke;
 import java.awt.Color;
-import java.awt.Graphics2D;
 import java.awt.Paint;
-import java.awt.Polygon;
-import java.awt.Rectangle;
 import java.awt.Stroke;
-import java.awt.image.BufferedImage;
-import java.time.chrono.ChronoLocalDate;
-import javax.swing.text.DefaultCaret;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.axis.CyclicNumberAxis;
-import org.jfree.chart.axis.DateAxis;
-import org.jfree.chart.axis.PeriodAxis;
-import org.jfree.chart.plot.CategoryPlot;
-import org.jfree.chart.plot.CombinedRangeXYPlot;
-import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.chart.plot.PlotRenderingInfo;
-import org.jfree.chart.plot.RingPlot;
-import org.jfree.chart.plot.SpiderWebPlot;
-import org.jfree.chart.plot.WaferMapPlot;
-import org.jfree.chart.plot.pie.PiePlot;
-import org.jfree.chart.renderer.WaferMapRenderer;
-import org.jfree.chart.renderer.xy.SamplingXYLineRenderer;
-import org.jfree.data.time.Day;
-import org.junit.runner.RunWith;
 
+/**
+ * Tests for the equals() method in the {@link XYLineAnnotation} class.
+ */
 public class XYLineAnnotation_ESTestTest21 extends XYLineAnnotation_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test20() throws Throwable {
-        XYLineAnnotation xYLineAnnotation0 = new XYLineAnnotation((-1433.6156858015083), (-1433.6156858015083), (-1433.6156858015083), (-1433.6156858015083));
-        CyclicNumberAxis cyclicNumberAxis0 = new CyclicNumberAxis(135.0);
-        Stroke stroke0 = cyclicNumberAxis0.getAdvanceLineStroke();
-        XYLineAnnotation xYLineAnnotation1 = new XYLineAnnotation((-1433.6156858015083), (-1433.6156858015083), 1505.073663087376, 2995.807896, stroke0, cyclicNumberAxis0.DEFAULT_TICK_LABEL_PAINT);
-        boolean boolean0 = xYLineAnnotation0.equals(xYLineAnnotation1);
-        assertEquals(2995.807896, xYLineAnnotation1.getY2(), 0.01);
-        assertEquals((-1433.6156858015083), xYLineAnnotation1.getX1(), 0.01);
-        assertFalse(boolean0);
-        assertEquals((-1433.6156858015083), xYLineAnnotation1.getY1(), 0.01);
-        assertEquals(1505.073663087376, xYLineAnnotation1.getX2(), 0.01);
+    /**
+     * Tests that two XYLineAnnotation objects are not considered equal if their
+     * coordinates or styling (stroke and paint) are different.
+     */
+    @Test
+    public void equals_shouldReturnFalse_whenAnnotationsHaveDifferentAttributes() {
+        // Arrange
+        // Create a standard annotation with default styling.
+        XYLineAnnotation annotation1 = new XYLineAnnotation(10.0, 20.0, 30.0, 40.0);
+
+        // Create a second annotation with different end coordinates (x2, y2)
+        // and custom styling.
+        Stroke customStroke = new BasicStroke(2.0f);
+        Paint customPaint = Color.RED;
+        XYLineAnnotation annotation2 = new XYLineAnnotation(
+                10.0, 20.0, 50.0, 60.0, customStroke, customPaint);
+
+        // Act
+        boolean areEqual = annotation1.equals(annotation2);
+
+        // Assert
+        assertFalse("Annotations with different coordinates and styles should not be equal", areEqual);
     }
 }
