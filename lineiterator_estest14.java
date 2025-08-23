0b2a4bd955d014df844231c6e8e0b7d98b57bc60
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:00:18 +0800

    Original processedAutoTestCases lineiterator_estest14.java

---
+++ lineiterator_estest14.java
@@ -0,0 +1,23 @@
+package org.apache.commons.io;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.BufferedReader;
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.NoSuchElementException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class LineIterator_ESTestTest14 extends LineIterator_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test13() throws Throwable {
+        StringReader stringReader0 = new StringReader("\n");
+        LineIterator lineIterator0 = new LineIterator(stringReader0);
+        String string0 = lineIterator0.next();
+        assertEquals("", string0);
+    }
+}
