c413ff7c0d57e92b044fa33195b1c52c16cbce2f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:42:27 +0800

    Original processedAutoTestCases charsequenceutils_estest31.java

---
+++ charsequenceutils_estest31.java
@@ -0,0 +1,19 @@
+package org.apache.commons.lang3;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.nio.CharBuffer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class CharSequenceUtils_ESTestTest31 extends CharSequenceUtils_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test30() throws Throwable {
+        StringBuilder stringBuilder0 = new StringBuilder("', is neither of type Map.Entry nor an Array");
+        char[] charArray0 = CharSequenceUtils.toCharArray(stringBuilder0);
+        assertEquals(44, charArray0.length);
+    }
+}
