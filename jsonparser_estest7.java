be89095f428f36ab2ddf280e4df852c59ef8b9cd
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:57:17 +0800

    Original processedAutoTestCases jsonparser_estest7.java

---
+++ jsonparser_estest7.java
@@ -0,0 +1,21 @@
+package com.google.gson;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.google.gson.stream.JsonReader;
+import java.io.Reader;
+import java.io.StringReader;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class JsonParser_ESTestTest7 extends JsonParser_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test06() throws Throwable {
+        JsonParser jsonParser0 = new JsonParser();
+        JsonPrimitive jsonPrimitive0 = (JsonPrimitive) jsonParser0.parse("N");
+        assertFalse(jsonPrimitive0.isNumber());
+    }
+}
