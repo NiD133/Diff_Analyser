1f9686e997bb129a2aef42cffb00021cc3985151
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:07:07 +0800

    Original processedAutoTestCases pairedstatsaccumulator_estest5.java

---
+++ pairedstatsaccumulator_estest5.java
@@ -0,0 +1,29 @@
+package com.google.common.math;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.ArrayDeque;
+import java.util.Iterator;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class PairedStatsAccumulator_ESTestTest5 extends PairedStatsAccumulator_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test04() throws Throwable {
+        PairedStatsAccumulator pairedStatsAccumulator0 = new PairedStatsAccumulator();
+        pairedStatsAccumulator0.add(0.0, 0.0);
+        // Undeclared exception!
+        try {
+            pairedStatsAccumulator0.sampleCovariance();
+            fail("Expecting exception: IllegalStateException");
+        } catch (IllegalStateException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("com.google.common.base.Preconditions", e);
+        }
+    }
+}
