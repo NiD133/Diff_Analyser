22060f3891345e1b4e55cc0cff85367f5549d320
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:47:31 +0800

    gemini processedAutoTestCases defaultkeyedvaluedataset_estest15.java

--- defaultkeyedvaluedataset_estest15.java
+++ defaultkeyedvaluedataset_estest15.java
@@ -1,25 +1,23 @@
 package org.jfree.data.general;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.math.BigInteger;
-import javax.swing.JLayeredPane;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.util.MockDate;
-import org.jfree.chart.date.SerialDate;
-import org.jfree.chart.date.SpreadsheetDate;
-import org.jfree.data.statistics.SimpleHistogramBin;
-import org.jfree.data.xy.OHLCDataItem;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertTrue;
 
-public class DefaultKeyedValueDataset_ESTestTest15 extends DefaultKeyedValueDataset_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link DefaultKeyedValueDataset} class.
+ */
+public class DefaultKeyedValueDatasetTest {
 
-    @Test(timeout = 4000)
-    public void test14() throws Throwable {
-        DefaultKeyedValueDataset defaultKeyedValueDataset0 = new DefaultKeyedValueDataset();
-        boolean boolean0 = defaultKeyedValueDataset0.equals(defaultKeyedValueDataset0);
-        assertTrue(boolean0);
+    /**
+     * Verifies the reflexive property of the equals() method.
+     * An object must always be equal to itself.
+     */
+    @Test
+    public void equals_whenComparedToItself_returnsTrue() {
+        // Arrange: Create an instance of the dataset.
+        DefaultKeyedValueDataset dataset = new DefaultKeyedValueDataset();
+
+        // Act & Assert: The object must be equal to itself.
+        assertTrue("A dataset instance should be equal to itself.", dataset.equals(dataset));
     }
 }
