c1152983500cf58d660740ba75a51fb835a45b0e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:44:05 +0800

    Original processedAutoTestCases collectionbag_estest1.java

---
+++ collectionbag_estest1.java
@@ -0,0 +1,43 @@
+package org.apache.commons.collections4.bag;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.lang.reflect.Array;
+import java.util.Collection;
+import java.util.ConcurrentModificationException;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Stack;
+import java.util.TreeSet;
+import org.apache.commons.collections4.Bag;
+import org.apache.commons.collections4.Predicate;
+import org.apache.commons.collections4.SortedBag;
+import org.apache.commons.collections4.Transformer;
+import org.apache.commons.collections4.functors.ComparatorPredicate;
+import org.apache.commons.collections4.functors.ConstantTransformer;
+import org.apache.commons.collections4.functors.FalsePredicate;
+import org.apache.commons.collections4.functors.IdentityPredicate;
+import org.apache.commons.collections4.functors.IfTransformer;
+import org.apache.commons.collections4.functors.InvokerTransformer;
+import org.apache.commons.collections4.functors.MapTransformer;
+import org.apache.commons.collections4.functors.NullIsExceptionPredicate;
+import org.apache.commons.collections4.functors.TransformerPredicate;
+import org.apache.commons.collections4.functors.UniquePredicate;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class CollectionBag_ESTestTest1 extends CollectionBag_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test00() throws Throwable {
+        HashBag<Integer> hashBag0 = new HashBag<Integer>();
+        CollectionBag<Integer> collectionBag0 = new CollectionBag<Integer>(hashBag0);
+        Integer integer0 = new Integer(389);
+        hashBag0.add(integer0);
+        boolean boolean0 = collectionBag0.remove((Object) integer0);
+        assertTrue(boolean0);
+    }
+}
