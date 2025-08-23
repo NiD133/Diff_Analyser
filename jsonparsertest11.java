f253728ac8f417a0c0c4cad8b960b1290d412b47
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:30:07 +0800

    Original processedManualTestCases jsonparsertest11.java

---
+++ jsonparsertest11.java
@@ -0,0 +1,24 @@
+package com.google.gson;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertThrows;
+import com.google.gson.common.TestTypes.BagOfPrimitives;
+import com.google.gson.internal.Streams;
+import com.google.gson.stream.JsonReader;
+import java.io.CharArrayReader;
+import java.io.CharArrayWriter;
+import java.io.IOException;
+import java.io.StringReader;
+import org.junit.Test;
+
+public class JsonParserTestTest11 {
+
+    @Test
+    public void testParseReader() {
+        StringReader reader = new StringReader("{a:10,b:'c'}");
+        JsonElement e = JsonParser.parseReader(reader);
+        assertThat(e.isJsonObject()).isTrue();
+        assertThat(e.getAsJsonObject().get("a").getAsInt()).isEqualTo(10);
+        assertThat(e.getAsJsonObject().get("b").getAsString()).isEqualTo("c");
+    }
+}
