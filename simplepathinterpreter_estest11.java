81218fa50f2db1b060ada88e2a5749adba30d610
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:16:25 +0800

    gemini processedAutoTestCases simplepathinterpreter_estest11.java

--- simplepathinterpreter_estest11.java
+++ simplepathinterpreter_estest11.java
@@ -1,73 +1,75 @@
 package org.apache.commons.jxpath.ri.axes;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.Locale;
-import org.apache.commons.jxpath.BasicVariables;
-import org.apache.commons.jxpath.JXPathBasicBeanInfo;
-import org.apache.commons.jxpath.JXPathContext;
+import org.apache.commons.jxpath.ri.Compiler;
 import org.apache.commons.jxpath.ri.EvalContext;
-import org.apache.commons.jxpath.ri.JXPathContextReferenceImpl;
 import org.apache.commons.jxpath.ri.QName;
-import org.apache.commons.jxpath.ri.compiler.Constant;
 import org.apache.commons.jxpath.ri.compiler.CoreFunction;
-import org.apache.commons.jxpath.ri.compiler.CoreOperationAnd;
-import org.apache.commons.jxpath.ri.compiler.CoreOperationEqual;
-import org.apache.commons.jxpath.ri.compiler.CoreOperationGreaterThanOrEqual;
-import org.apache.commons.jxpath.ri.compiler.CoreOperationLessThanOrEqual;
-import org.apache.commons.jxpath.ri.compiler.CoreOperationMod;
-import org.apache.commons.jxpath.ri.compiler.CoreOperationMultiply;
-import org.apache.commons.jxpath.ri.compiler.CoreOperationNegate;
-import org.apache.commons.jxpath.ri.compiler.CoreOperationNotEqual;
-import org.apache.commons.jxpath.ri.compiler.CoreOperationOr;
 import org.apache.commons.jxpath.ri.compiler.CoreOperationSubtract;
-import org.apache.commons.jxpath.ri.compiler.CoreOperationUnion;
 import org.apache.commons.jxpath.ri.compiler.Expression;
-import org.apache.commons.jxpath.ri.compiler.NameAttributeTest;
-import org.apache.commons.jxpath.ri.compiler.NodeNameTest;
-import org.apache.commons.jxpath.ri.compiler.NodeTest;
-import org.apache.commons.jxpath.ri.compiler.NodeTypeTest;
-import org.apache.commons.jxpath.ri.compiler.ProcessingInstructionTest;
 import org.apache.commons.jxpath.ri.compiler.Step;
-import org.apache.commons.jxpath.ri.compiler.VariableReference;
 import org.apache.commons.jxpath.ri.model.NodePointer;
-import org.apache.commons.jxpath.ri.model.VariablePointer;
-import org.apache.commons.jxpath.ri.model.beans.BeanPointer;
-import org.apache.commons.jxpath.ri.model.beans.BeanPropertyPointer;
-import org.apache.commons.jxpath.ri.model.beans.NullPointer;
-import org.apache.commons.jxpath.ri.model.beans.NullPropertyPointer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class SimplePathInterpreter_ESTestTest11 extends SimplePathInterpreter_ESTest_scaffolding {
+import java.util.Locale;
 
-    @Test(timeout = 4000)
-    public void test10() throws Throwable {
-        Locale locale0 = Locale.JAPANESE;
-        QName qName0 = new QName("[nme)SR=1'");
-        NodePointer nodePointer0 = NodePointer.newNodePointer(qName0, locale0, locale0);
-        Step[] stepArray0 = new Step[2];
-        Expression[] expressionArray0 = new Expression[1];
-        CoreFunction coreFunction0 = new CoreFunction(13, expressionArray0);
-        CoreOperationSubtract coreOperationSubtract0 = new CoreOperationSubtract(coreFunction0, coreFunction0);
-        expressionArray0[0] = (Expression) coreOperationSubtract0;
-        Step step0 = mock(Step.class, new ViolatedAssumptionAnswer());
-        doReturn(2013265920).when(step0).getAxis();
-        doReturn((Object) expressionArray0, (Object) expressionArray0).when(step0).getPredicates();
-        stepArray0[0] = step0;
-        // Undeclared exception!
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class SimplePathInterpreterTest {
+
+    /**
+     * Tests that interpretSimpleLocationPath throws a RuntimeException when a step
+     * contains a predicate with a function that has an incorrect number of arguments.
+     */
+    @Test
+    public void interpretSimpleLocationPathThrowsExceptionForPredicateWithInvalidFunctionArguments() {
+        // ARRANGE
+        // 1. Set up a root node pointer for the path evaluation to start from.
+        QName rootQName = new QName("root");
+        NodePointer rootPointer = NodePointer.newNodePointer(rootQName, new Object(), Locale.getDefault());
+
+        // 2. Create a deliberately invalid predicate expression to trigger an error.
+        // We will use the 'substring-after' function, which requires two arguments,
+        // but construct the expression tree to provide only one.
+        Expression[] predicates = new Expression[1];
+
+        // The function code 13 corresponds to 'substring-after'.
+        final int SUBSTRING_AFTER_FUNCTION_CODE = 13;
+        CoreFunction substringAfterFunction = new CoreFunction(SUBSTRING_AFTER_FUNCTION_CODE, predicates);
+
+        // Create a subtraction operation. This operation will serve as the single argument
+        // to the 'substring-after' function.
+        CoreOperationSubtract subtractOperation = new CoreOperationSubtract(substringAfterFunction, substringAfterFunction);
+
+        // Set the step's predicate to be this operation. When the path interpreter evaluates
+        // this predicate, it will first compute the subtraction, then pass the single
+        // result to 'substring-after', causing an "Incorrect number of arguments" error.
+        predicates[0] = subtractOperation;
+
+        // 3. Mock a path Step that uses this invalid predicate.
+        Step mockStepWithInvalidPredicate = mock(Step.class);
+        when(mockStepWithInvalidPredicate.getAxis()).thenReturn(Compiler.AXIS_CHILD);
+        when(mockStepWithInvalidPredicate.getPredicates()).thenReturn(predicates);
+
+        // The path consists of our mocked step. The second step is not needed as the
+        // evaluation will fail on the first step's predicate.
+        Step[] steps = {mockStepWithInvalidPredicate, null};
+
+        // ACT & ASSERT
         try {
-            SimplePathInterpreter.interpretSimpleLocationPath((EvalContext) null, nodePointer0, stepArray0);
-            fail("Expecting exception: RuntimeException");
+            // The method under test is expected to evaluate the predicate and throw an exception.
+            SimplePathInterpreter.interpretSimpleLocationPath((EvalContext) null, rootPointer, steps);
+            fail("Expected a RuntimeException due to an invalid function call in the predicate.");
         } catch (RuntimeException e) {
-            //
-            // Incorrect number of arguments: substring-after(org.apache.commons.jxpath.ri.compiler.CoreFunction@0000000003 - org.apache.commons.jxpath.ri.compiler.CoreFunction@0000000003)
-            //
-            verifyException("org.apache.commons.jxpath.ri.compiler.CoreFunction", e);
+            // VERIFY
+            // Check that the exception is the one we expect from CoreFunction's argument validation.
+            String expectedMessagePrefix = "Incorrect number of arguments: substring-after";
+            assertTrue(
+                "Exception message should indicate incorrect argument count for the function.",
+                e.getMessage().startsWith(expectedMessagePrefix)
+            );
         }
     }
 }
