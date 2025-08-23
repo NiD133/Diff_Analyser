103b50995aa153ee990ecb88f543f42b73cd8ac9
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:27:20 +0800

    gemini processedAutoTestCases weeks_estest5.java

--- weeks_estest5.java
+++ weeks_estest5.java
@@ -1,18 +1,33 @@
 package org.joda.time;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class Weeks_ESTestTest5 extends Weeks_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link Weeks} class.
+ */
+public class WeeksTest {
 
-    @Test(timeout = 4000)
-    public void test04() throws Throwable {
-        Weeks weeks0 = Weeks.weeks((-2820));
-        Seconds seconds0 = weeks0.toStandardSeconds();
-        assertEquals((-1705536000), seconds0.getSeconds());
+    /**
+     * Tests that converting a negative number of weeks to standard seconds
+     * yields the correct negative number of seconds.
+     */
+    @Test
+    public void toStandardSeconds_withNegativeWeeks_returnsCorrectSeconds() {
+        // Arrange
+        final int numberOfWeeks = -2820;
+        Weeks weeks = Weeks.weeks(numberOfWeeks);
+
+        // The expected number of seconds is calculated based on the standard definition:
+        // -2820 weeks * (7 days/week * 24 hours/day * 60 minutes/hour * 60 seconds/minute)
+        final int secondsInAWeek = 7 * 24 * 60 * 60;
+        final int expectedSeconds = numberOfWeeks * secondsInAWeek;
+
+        // Act
+        Seconds actualSeconds = weeks.toStandardSeconds();
+
+        // Assert
+        assertEquals("The number of seconds should match the expected calculation.",
+                     expectedSeconds, actualSeconds.getSeconds());
     }
 }
