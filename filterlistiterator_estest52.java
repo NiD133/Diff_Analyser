32759ee87eb85a6f893ce0d8f7c99ed69ccea2d7
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:51:15 +0800

    gemini processedAutoTestCases filterlistiterator_estest52.java

--- filterlistiterator_estest52.java
+++ filterlistiterator_estest52.java
@@ -1,67 +1,33 @@
 package org.apache.commons.collections4.iterators;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.ConcurrentModificationException;
-import java.util.LinkedList;
-import java.util.ListIterator;
-import java.util.Map;
-import java.util.NoSuchElementException;
-import java.util.function.Consumer;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.Equator;
-import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ClosureTransformer;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.DefaultEquator;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionClosure;
-import org.apache.commons.collections4.functors.ExceptionPredicate;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.IfClosure;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.MapTransformer;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NotNullPredicate;
-import org.apache.commons.collections4.functors.NotPredicate;
-import org.apache.commons.collections4.functors.NullIsExceptionPredicate;
-import org.apache.commons.collections4.functors.NullIsFalsePredicate;
-import org.apache.commons.collections4.functors.NullIsTruePredicate;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.OnePredicate;
-import org.apache.commons.collections4.functors.TransformedPredicate;
-import org.apache.commons.collections4.functors.TransformerPredicate;
-import org.apache.commons.collections4.functors.TruePredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.apache.commons.collections4.functors.WhileClosure;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
 
-public class FilterListIterator_ESTestTest52 extends FilterListIterator_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
-    @Test(timeout = 4000)
-    public void test51() throws Throwable {
-        Predicate<Object> predicate0 = null;
-        FilterListIterator<Object> filterListIterator0 = new FilterListIterator<Object>((Predicate<? super Object>) null);
-        // Undeclared exception!
+/**
+ * Tests for the {@link FilterListIterator} class, focusing on its modification methods.
+ */
+public class FilterListIteratorTest {
+
+    /**
+     * Verifies that the add() method is unsupported and throws an UnsupportedOperationException.
+     * The FilterListIterator is a read-only decorator for an iterator and should not
+     * allow adding elements.
+     */
+    @Test
+    public void addShouldThrowUnsupportedOperationException() {
+        // Given: A default FilterListIterator instance.
+        // No underlying iterator or predicate is needed to test this behavior.
+        final FilterListIterator<Object> filterIterator = new FilterListIterator<>();
+
+        // When & Then: Calling add() should throw an exception.
         try {
-            filterListIterator0.add((Object) null);
-            fail("Expecting exception: UnsupportedOperationException");
-        } catch (UnsupportedOperationException e) {
-            //
-            // FilterListIterator.add(Object) is not supported.
-            //
-            verifyException("org.apache.commons.collections4.iterators.FilterListIterator", e);
+            filterIterator.add("any element");
+            fail("Expected an UnsupportedOperationException to be thrown.");
+        } catch (final UnsupportedOperationException e) {
+            // Verify the exception message to ensure it's the one we expect.
+            assertEquals("FilterListIterator.add(Object) is not supported.", e.getMessage());
         }
     }
 }
