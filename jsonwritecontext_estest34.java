490aa82f425a6c17ec871e124d8b92527d16c3d8
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:59:09 +0800

    Original processedAutoTestCases jsonwritecontext_estest34.java

---
+++ jsonwritecontext_estest34.java
@@ -0,0 +1,32 @@
+package com.fasterxml.jackson.core.json;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.filter.FilteringGeneratorDelegate;
+import com.fasterxml.jackson.core.filter.TokenFilter;
+import com.fasterxml.jackson.core.util.JsonGeneratorDelegate;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.PipedInputStream;
+import java.io.StringWriter;
+import java.io.Writer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class JsonWriteContext_ESTestTest34 extends JsonWriteContext_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test33() throws Throwable {
+        DupDetector dupDetector0 = DupDetector.rootDetector((JsonGenerator) null);
+        JsonWriteContext jsonWriteContext0 = JsonWriteContext.createRootContext(dupDetector0);
+        jsonWriteContext0.writeValue();
+        jsonWriteContext0.writeValue();
+        jsonWriteContext0.withDupDetector(dupDetector0);
+        assertEquals(2, jsonWriteContext0.getEntryCount());
+    }
+}
