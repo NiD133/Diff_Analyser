4790429b57b588fc0e52426ddc9dfc3694598a2d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:26:38 +0800

    gemini processedAutoTestCases versionutil_estest7.java

--- versionutil_estest7.java
+++ versionutil_estest7.java
@@ -1,27 +1,25 @@
 package com.fasterxml.jackson.core.util;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.core.Version;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class VersionUtil_ESTestTest7 extends VersionUtil_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link VersionUtil} class, focusing on edge cases for version string parsing.
+ */
+public class VersionUtilTest {
 
-    @Test(timeout = 4000)
-    public void test06() throws Throwable {
-        // Undeclared exception!
-        try {
-            VersionUtil.parseVersion(";", ";", ";");
-            fail("Expecting exception: ArrayIndexOutOfBoundsException");
-        } catch (ArrayIndexOutOfBoundsException e) {
-            //
-            // 0
-            //
-            verifyException("com.fasterxml.jackson.core.util.VersionUtil", e);
-        }
+    /**
+     * Verifies that parseVersion() throws an ArrayIndexOutOfBoundsException
+     * when the version string consists only of a separator character.
+     * <p>
+     * The method is expected to split the version string by separators. An input
+     * like ";" results in an empty array of version components. Attempting to
+     * access the first component (major version) from this empty array causes the
+     * exception.
+     */
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void parseVersion_whenVersionStringIsOnlyASeparator_throwsException() {
+        // Act: Call parseVersion with a version string that is only a separator.
+        // The groupId and artifactId are arbitrary for this test case.
+        VersionUtil.parseVersion(";", "some.group.id", "some-artifact-id");
     }
 }
