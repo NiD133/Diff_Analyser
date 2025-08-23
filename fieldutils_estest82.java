6f0b1f8c9a0ac33e7ea0413c641048edd9530cad
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:50:38 +0800

    gemini processedAutoTestCases fieldutils_estest82.java

--- fieldutils_estest82.java
+++ fieldutils_estest82.java
@@ -1,23 +1,25 @@
 package org.joda.time.field;
 
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.math.RoundingMode;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.joda.time.DateTimeField;
-import org.joda.time.DateTimeFieldType;
-import org.joda.time.DateTimeZone;
-import org.joda.time.chrono.IslamicChronology;
-import org.joda.time.chrono.ZonedChronology;
-import org.junit.runner.RunWith;
 
-public class FieldUtils_ESTestTest82 extends FieldUtils_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link FieldUtils} class.
+ */
+public class FieldUtilsTest {
 
-    @Test(timeout = 4000)
-    public void test81() throws Throwable {
-        long long0 = FieldUtils.safeAdd((-275L), 9223372036854775800L);
-        assertEquals(9223372036854775525L, long0);
+    @Test
+    public void safeAdd_shouldReturnCorrectSum_whenAddingNearMaxValueWithoutOverflow() {
+        // Arrange
+        // Use a value very close to Long.MAX_VALUE to test boundary conditions.
+        final long largePositiveValue = Long.MAX_VALUE - 7L;
+        final long negativeValue = -275L;
+        final long expectedSum = Long.MAX_VALUE - 282L; // -275L + (Long.MAX_VALUE - 7L)
+
+        // Act
+        long actualSum = FieldUtils.safeAdd(negativeValue, largePositiveValue);
+
+        // Assert
+        assertEquals(expectedSum, actualSum);
     }
 }
