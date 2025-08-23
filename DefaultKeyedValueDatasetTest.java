ab8920110e7b6fa2a51f9a1a4c20be1df912b28b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 05:33:13 +0800

    gpt5 manualTestSuite DefaultKeyedValueDatasetTest.java

--- DefaultKeyedValueDatasetTest.java
+++ DefaultKeyedValueDatasetTest.java
@@ -1,111 +1,144 @@
-/* ======================================================
- * JFreeChart : a chart library for the Java(tm) platform
- * ======================================================
- *
- * (C) Copyright 2000-present, by David Gilbert and Contributors.
- *
- * Project Info:  https://www.jfree.org/jfreechart/index.html
- *
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 2.1 of the License, or
- * (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
- * License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
- * USA.
- *
- * [Oracle and Java are registered trademarks of Oracle and/or its affiliates. 
- * Other names may be trademarks of their respective owners.]
- *
- * ---------------------------------
- * DefaultKeyedValueDatasetTest.java
- * ---------------------------------
- * (C) Copyright 2003-present, by David Gilbert and Contributors.
- *
- * Original Author:  David Gilbert;
- * Contributor(s):   -;
- *
- */
-
 package org.jfree.data.general;
 
 import org.jfree.chart.TestUtils;
 import org.jfree.chart.internal.CloneUtils;
-
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * Tests for the {@link DefaultKeyedValueDataset} class.
+ * Readable, intention-revealing tests for DefaultKeyedValueDataset.
  */
+@DisplayName("DefaultKeyedValueDataset")
 public class DefaultKeyedValueDatasetTest {
 
-    /**
-     * Confirm that the equals method can distinguish all the required fields.
-     */
-    @Test
-    public void testEquals() {
+    private static final String KEY_A = "Key A";
+    private static final String KEY_B = "Key B";
+    private static final double VALUE_1 = 45.5;
+    private static final double VALUE_2 = 45.6;
 
-        DefaultKeyedValueDataset d1 = new DefaultKeyedValueDataset("Test", 45.5);
-        DefaultKeyedValueDataset d2 = new DefaultKeyedValueDataset("Test", 45.5);
-        assertEquals(d1, d2);
-        assertEquals(d2, d1);
-
-        d1 = new DefaultKeyedValueDataset("Test 1", 45.5);
-        d2 = new DefaultKeyedValueDataset("Test 2", 45.5);
-        assertNotEquals(d1, d2);
-
-        d1 = new DefaultKeyedValueDataset("Test", 45.5);
-        d2 = new DefaultKeyedValueDataset("Test", 45.6);
-        assertNotEquals(d1, d2);
-
+    private static DefaultKeyedValueDataset dataset(String key, double value) {
+        return new DefaultKeyedValueDataset(key, value);
     }
 
-    /**
-     * Confirm that cloning works.
-     * @throws java.lang.CloneNotSupportedException
-     */
+    // equals()
+
     @Test
-    public void testCloning() throws CloneNotSupportedException {
-        DefaultKeyedValueDataset d1 = new DefaultKeyedValueDataset("Test", 45.5);
-        DefaultKeyedValueDataset d2 = (DefaultKeyedValueDataset) d1.clone();
-        assertNotSame(d1, d2);
-        assertSame(d1.getClass(), d2.getClass());
-        assertEquals(d1, d2);
+    @DisplayName("equals(): two datasets with same key and value are equal (symmetry)")
+    public void equals_sameState_true_andSymmetric() {
+        // arrange
+        DefaultKeyedValueDataset d1 = dataset(KEY_A, VALUE_1);
+        DefaultKeyedValueDataset d2 = dataset(KEY_A, VALUE_1);
+
+        // assert
+        assertAll(
+                () -> assertEquals(d1, d2, "Expected equality for same key/value"),
+                () -> assertEquals(d2, d1, "Symmetry must hold")
+        );
     }
 
-    /**
-     * Confirm that the clone is independent of the original.
-     * @throws java.lang.CloneNotSupportedException
-     */
     @Test
-    public void testCloneIndependence() throws CloneNotSupportedException {
-        DefaultKeyedValueDataset d1
-            = new DefaultKeyedValueDataset("Key", 10.0);
-        DefaultKeyedValueDataset d2 = CloneUtils.clone(d1);
-        assertEquals(d1, d2);
-        d2.updateValue(99.9);
-        assertNotEquals(d1, d2);
-        d2.updateValue(10.0);
-        assertEquals(d1, d2);
+    @DisplayName("equals(): different key -> not equal")
+    public void equals_differentKey_false() {
+        // arrange
+        DefaultKeyedValueDataset d1 = dataset(KEY_A, VALUE_1);
+        DefaultKeyedValueDataset d2 = dataset(KEY_B, VALUE_1);
+
+        // assert
+        assertNotEquals(d1, d2, "Datasets with different keys must not be equal");
     }
 
-    /**
-     * Serialize an instance, restore it, and check for equality.
-     */
     @Test
-    public void testSerialization() {
-        DefaultKeyedValueDataset d1 = new DefaultKeyedValueDataset("Test", 25.3);
-        DefaultKeyedValueDataset d2 = TestUtils.serialised(d1);
-        assertEquals(d1, d2);
+    @DisplayName("equals(): different value -> not equal")
+    public void equals_differentValue_false() {
+        // arrange
+        DefaultKeyedValueDataset d1 = dataset(KEY_A, VALUE_1);
+        DefaultKeyedValueDataset d2 = dataset(KEY_A, VALUE_2);
+
+        // assert
+        assertNotEquals(d1, d2, "Datasets with different values must not be equal");
     }
 
+    @Test
+    @DisplayName("equals(): null and different type -> not equal")
+    public void equals_nullAndDifferentType_false() {
+        // arrange
+        DefaultKeyedValueDataset d1 = dataset(KEY_A, VALUE_1);
+
+        // assert
+        assertAll(
+                () -> assertNotEquals(d1, null, "Never equal to null"),
+                () -> assertNotEquals(d1, "not a dataset", "Never equal to another type")
+        );
+    }
+
+    @Test
+    @DisplayName("hashCode(): equal objects have equal hash codes")
+    public void hashCode_consistentWithEquals() {
+        // arrange
+        DefaultKeyedValueDataset d1 = dataset(KEY_A, VALUE_1);
+        DefaultKeyedValueDataset d2 = dataset(KEY_A, VALUE_1);
+
+        // assert
+        assertEquals(d1.hashCode(), d2.hashCode(),
+                "Equal objects must produce equal hash codes");
+    }
+
+    // clone()
+
+    @Test
+    @DisplayName("clone(): produces a distinct but equal copy")
+    public void cloning_createsEqualButDistinctInstance() throws CloneNotSupportedException {
+        // arrange
+        DefaultKeyedValueDataset original = dataset(KEY_A, VALUE_1);
+
+        // act
+        DefaultKeyedValueDataset clone = (DefaultKeyedValueDataset) original.clone();
+
+        // assert
+        assertAll(
+                () -> assertNotSame(original, clone, "Clone must be a different instance"),
+                () -> assertSame(original.getClass(), clone.getClass(), "Clone must have same runtime type"),
+                () -> assertEquals(original, clone, "Clone must be equal to the original")
+        );
+    }
+
+    @Test
+    @DisplayName("clone(): modifying the clone does not affect the original")
+    public void clone_isIndependentFromOriginal() throws CloneNotSupportedException {
+        // arrange
+        DefaultKeyedValueDataset original = dataset("Key", 10.0);
+
+        // act
+        DefaultKeyedValueDataset clone = CloneUtils.clone(original);
+
+        // assert precondition
+        assertEquals(original, clone, "Sanity check: clone should start equal to original");
+
+        // mutate clone only
+        clone.updateValue(99.9);
+
+        // assert independence
+        assertNotEquals(original, clone, "Changing the clone must not affect the original");
+
+        // revert and confirm equality again
+        clone.updateValue(10.0);
+        assertEquals(original, clone, "After reverting, objects should be equal again");
+    }
+
+    // serialization
+
+    @Test
+    @DisplayName("serialization: round-trip preserves equality")
+    public void serialization_roundTrip_preservesEquality() {
+        // arrange
+        DefaultKeyedValueDataset original = dataset("Test", 25.3);
+
+        // act
+        DefaultKeyedValueDataset deserialized = TestUtils.serialised(original);
+
+        // assert
+        assertEquals(original, deserialized, "Serialized-then-deserialized object should be equal");
+    }
 }
