e317c0b5cdaacb7f9356126aeb9d6e8a371dd54c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:53:01 +0800

    Original processedAutoTestCases hashcodeandequalssafeset_estest19.java

---
+++ hashcodeandequalssafeset_estest19.java
@@ -0,0 +1,35 @@
+package org.mockito.internal.util.collections;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Vector;
+import java.util.function.Predicate;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class HashCodeAndEqualsSafeSet_ESTestTest19 extends HashCodeAndEqualsSafeSet_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test18() throws Throwable {
+        HashCodeAndEqualsSafeSet hashCodeAndEqualsSafeSet0 = new HashCodeAndEqualsSafeSet();
+        // Undeclared exception!
+        try {
+            hashCodeAndEqualsSafeSet0.toArray((HashCodeAndEqualsMockWrapper[]) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.mockito.internal.util.collections.HashCodeAndEqualsSafeSet", e);
+        }
+    }
+}
