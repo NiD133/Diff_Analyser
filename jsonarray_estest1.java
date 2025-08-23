e421a34802c3321e24428b3b2ad11dbf466138d6
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:55:56 +0800

    gemini processedAutoTestCases jsonarray_estest1.java

--- jsonarray_estest1.java
+++ jsonarray_estest1.java
@@ -1,24 +1,22 @@
 package com.google.gson;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.util.Iterator;
-import java.util.List;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class JsonArray_ESTestTest1 extends JsonArray_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link JsonArray} class.
+ */
+public class JsonArrayTest {
 
-    @Test(timeout = 4000)
-    public void test00() throws Throwable {
-        JsonArray jsonArray0 = new JsonArray();
-        Float float0 = new Float(0.0);
-        jsonArray0.add((Number) float0);
-        int int0 = jsonArray0.size();
-        assertEquals(1, int0);
+    @Test
+    public void addNumber_increasesArraySizeByOne() {
+        // Arrange
+        JsonArray jsonArray = new JsonArray();
+
+        // Act
+        jsonArray.add(0.0f);
+
+        // Assert
+        assertEquals("The size of the array should be 1 after adding a number.", 1, jsonArray.size());
     }
 }
