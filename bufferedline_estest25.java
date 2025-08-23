ff48e2030509c1565437684048e61f7667456599
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:40:16 +0800

    Original processedAutoTestCases bufferedline_estest25.java

---
+++ bufferedline_estest25.java
@@ -0,0 +1,33 @@
+package org.locationtech.spatial4j.shape.impl;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.HashMap;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+import org.locationtech.spatial4j.context.SpatialContext;
+import org.locationtech.spatial4j.context.SpatialContextFactory;
+import org.locationtech.spatial4j.distance.CartesianDistCalc;
+import org.locationtech.spatial4j.shape.Point;
+import org.locationtech.spatial4j.shape.Rectangle;
+import org.locationtech.spatial4j.shape.Shape;
+import org.locationtech.spatial4j.shape.SpatialRelation;
+
+public class BufferedLine_ESTestTest25 extends BufferedLine_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test24() throws Throwable {
+        SpatialContext spatialContext0 = SpatialContext.GEO;
+        BufferedLine bufferedLine0 = null;
+        try {
+            bufferedLine0 = new BufferedLine((Point) null, (Point) null, 2693.6085181, spatialContext0);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+        }
+    }
+}
