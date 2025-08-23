46e4fd055cdc5b1d1ad0ee6c5af0d44c8d8bb386
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:58:45 +0800

    Original processedAutoTestCases jsontreewriter_estest60.java

---
+++ jsontreewriter_estest60.java
@@ -0,0 +1,33 @@
+package com.google.gson.internal.bind;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.Strictness;
+import com.google.gson.stream.JsonWriter;
+import java.io.IOException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class JsonTreeWriter_ESTestTest60 extends JsonTreeWriter_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test59() throws Throwable {
+        JsonTreeWriter jsonTreeWriter0 = new JsonTreeWriter();
+        jsonTreeWriter0.beginObject();
+        // Undeclared exception!
+        try {
+            jsonTreeWriter0.value((Boolean) null);
+            fail("Expecting exception: IllegalStateException");
+        } catch (IllegalStateException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("com.google.gson.internal.bind.JsonTreeWriter", e);
+        }
+    }
+}
