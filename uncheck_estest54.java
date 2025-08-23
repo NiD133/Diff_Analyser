fccb96f21e06e24afe6d7553f48d7c8cbbab8e92
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:24:44 +0800

    gemini processedAutoTestCases uncheck_estest54.java

--- uncheck_estest54.java
+++ uncheck_estest54.java
@@ -1,29 +1,29 @@
 package org.apache.commons.io.function;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.time.chrono.HijrahEra;
-import java.util.Comparator;
-import java.util.concurrent.ForkJoinTask;
-import java.util.function.BiConsumer;
-import java.util.function.BinaryOperator;
-import java.util.function.Function;
+
 import java.util.function.Supplier;
-import java.util.stream.Collector;
-import java.util.stream.LongStream;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
 
-public class Uncheck_ESTestTest54 extends Uncheck_ESTest_scaffolding {
+/**
+ * Tests for the {@link Uncheck} utility class.
+ */
+public class UncheckTest {
 
-    @Test(timeout = 4000)
-    public void test53() throws Throwable {
-        IORunnable iORunnable0 = IORunnable.noop();
-        Uncheck.run(iORunnable0, (Supplier<String>) null);
+    /**
+     * Tests that {@link Uncheck#run(IORunnable, Supplier)} completes normally
+     * when the provided runnable succeeds, even if the message supplier is null.
+     * The message supplier should only be evaluated if an exception is thrown.
+     */
+    @Test
+    public void runWithNullMessageSupplierShouldNotThrowExceptionWhenRunnableSucceeds() {
+        // Arrange: Create a runnable that does nothing and will not throw an exception.
+        // The message supplier is explicitly null to test this edge case.
+        final IORunnable successfulRunnable = IORunnable.noop();
+        final Supplier<String> nullMessageSupplier = null;
+
+        // Act & Assert: The call to Uncheck.run should execute the runnable
+        // and complete without throwing any exceptions. The absence of an
+        // exception is the success condition for this test.
+        Uncheck.run(successfulRunnable, nullMessageSupplier);
     }
 }
