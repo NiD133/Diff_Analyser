67c671aa3672f43806a0958b5af33ad1fc9ad155
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:55:57 +0800

    gemini processedAutoTestCases jsonarray_estest12.java

--- jsonarray_estest12.java
+++ jsonarray_estest12.java
@@ -1,24 +1,23 @@
 package com.google.gson;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.util.Iterator;
-import java.util.List;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class JsonArray_ESTestTest12 extends JsonArray_ESTest_scaffolding {
+/**
+ * Tests for {@link JsonArray}.
+ */
+public class JsonArrayTest {
 
-    @Test(timeout = 4000)
-    public void test11() throws Throwable {
-        JsonArray jsonArray0 = new JsonArray();
-        Character character0 = Character.valueOf('6');
-        jsonArray0.add(character0);
-        int int0 = jsonArray0.getAsInt();
-        assertEquals(6, int0);
+    @Test
+    public void getAsInt_whenArrayContainsSingleNumericCharacter_returnsCorrespondingInt() {
+        // Arrange
+        JsonArray jsonArray = new JsonArray();
+        jsonArray.add('6');
+
+        // Act
+        int result = jsonArray.getAsInt();
+
+        // Assert
+        assertEquals(6, result);
     }
 }
