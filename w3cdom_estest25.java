cea3f373e423fd3be71004866aaeeea9b6466fc2
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:26:46 +0800

    gemini processedAutoTestCases w3cdom_estest25.java

--- w3cdom_estest25.java
+++ w3cdom_estest25.java
@@ -1,48 +1,33 @@
 package org.jsoup.helper;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import com.sun.org.apache.xerces.internal.dom.DocumentImpl;
-import java.io.Reader;
-import java.io.StringReader;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
-import javax.imageio.metadata.IIOMetadataNode;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.jsoup.nodes.Attributes;
-import org.jsoup.nodes.DataNode;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.DocumentType;
 import org.jsoup.nodes.Element;
-import org.jsoup.nodes.FormElement;
-import org.jsoup.nodes.XmlDeclaration;
-import org.jsoup.parser.Parser;
-import org.jsoup.parser.Tag;
-import org.junit.runner.RunWith;
-import org.w3c.dom.DOMException;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import org.junit.Test;
 
-public class W3CDom_ESTestTest25 extends W3CDom_ESTest_scaffolding {
+/**
+ * Test suite for the {@link W3CDom} class, focusing on exception handling.
+ */
+public class W3CDomTest {
 
-    @Test(timeout = 4000)
-    public void test24() throws Throwable {
-        W3CDom w3CDom0 = new W3CDom();
-        Element element0 = new Element("javax.xml.xpath.XPathFactory:jsoup");
-        org.w3c.dom.Document document0 = w3CDom0.fromJsoup(element0);
-        // Undeclared exception!
-        try {
-            w3CDom0.convert((Document) null, document0);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.jsoup.helper.W3CDom", e);
-        }
+    /**
+     * Verifies that the convert method throws a NullPointerException when the
+     * source jsoup Document is null.
+     */
+    @Test(expected = NullPointerException.class)
+    public void convertWithNullJsoupDocumentThrowsNullPointerException() {
+        // Arrange: Create a W3CDom instance and a non-null destination W3C document.
+        W3CDom w3cDom = new W3CDom();
+        
+        // The convert method requires a destination document. We can create one from a dummy element.
+        // The content of this document is irrelevant to this test.
+        Element dummyJsoupElement = new Element("html");
+        org.w3c.dom.Document destinationW3cDoc = w3cDom.fromJsoup(dummyJsoupElement);
+
+        // Act: Call the method under test with a null jsoup document.
+        // This call is expected to throw a NullPointerException.
+        w3cDom.convert((Document) null, destinationW3cDoc);
+
+        // Assert: The test will pass if a NullPointerException is thrown, as declared
+        // by the @Test(expected = ...) annotation.
     }
 }
