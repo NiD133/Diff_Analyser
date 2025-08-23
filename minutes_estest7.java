b38abd4dd7b6b202672c6fbc030a229fb4b5ab04
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:03:25 +0800

    gemini processedAutoTestCases minutes_estest7.java

--- minutes_estest7.java
+++ minutes_estest7.java
@@ -1,18 +1,23 @@
 package org.joda.time;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class Minutes_ESTestTest7 extends Minutes_ESTest_scaffolding {
+/**
+ * Unit tests for the conversion methods of the {@link Minutes} class.
+ */
+public class MinutesConversionTest {
 
-    @Test(timeout = 4000)
-    public void test06() throws Throwable {
-        Minutes minutes0 = Minutes.ZERO;
-        Seconds seconds0 = minutes0.toStandardSeconds();
-        assertEquals(1, seconds0.size());
+    @Test
+    public void toStandardSeconds_whenZeroMinutes_returnsZeroSeconds() {
+        // Arrange
+        Minutes zeroMinutes = Minutes.ZERO;
+        Seconds expectedSeconds = Seconds.ZERO;
+
+        // Act
+        Seconds actualSeconds = zeroMinutes.toStandardSeconds();
+
+        // Assert
+        assertEquals(expectedSeconds, actualSeconds);
     }
 }
