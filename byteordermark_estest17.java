1c18297ac5a192d13efdf140739f53b61100f776
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:40:53 +0800

    gemini processedAutoTestCases byteordermark_estest17.java

--- byteordermark_estest17.java
+++ byteordermark_estest17.java
@@ -1,20 +1,29 @@
 package org.apache.commons.io;
 
+import static org.junit.Assert.assertFalse;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class ByteOrderMark_ESTestTest17 extends ByteOrderMark_ESTest_scaffolding {
+/**
+ * Contains tests for the {@link ByteOrderMark#equals(Object)} method.
+ */
+public class ByteOrderMarkTest {
 
-    @Test(timeout = 4000)
-    public void test16() throws Throwable {
-        ByteOrderMark byteOrderMark0 = ByteOrderMark.UTF_32LE;
-        ByteOrderMark byteOrderMark1 = ByteOrderMark.UTF_32BE;
-        boolean boolean0 = byteOrderMark0.equals(byteOrderMark1);
-        assertFalse(boolean0);
-        assertFalse(byteOrderMark1.equals((Object) byteOrderMark0));
+    /**
+     * Tests that the equals() method correctly returns false when comparing two
+     * different ByteOrderMark instances.
+     */
+    @Test
+    public void testEqualsIsFalseForDifferentBoms() {
+        // Arrange: Define two different ByteOrderMark constants.
+        final ByteOrderMark utf32le = ByteOrderMark.UTF_32LE;
+        final ByteOrderMark utf32be = ByteOrderMark.UTF_32BE;
+
+        // Act & Assert: Verify that the instances are not equal to each other.
+        
+        // Test for inequality
+        assertFalse("UTF-32LE should not be equal to UTF-32BE", utf32le.equals(utf32be));
+
+        // Test for symmetry (b.equals(a) should also be false)
+        assertFalse("Symmetry check: UTF-32BE should not be equal to UTF-32LE", utf32be.equals(utf32le));
     }
 }
