2cd86002ee59be62d17ba436c8953e717ed65e05
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:45:48 +0800

    Original processedAutoTestCases converterset_estest30.java

---
+++ converterset_estest30.java
@@ -0,0 +1,32 @@
+package org.joda.time.convert;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.joda.time.Hours;
+import org.joda.time.Interval;
+import org.joda.time.MutablePeriod;
+import org.joda.time.PeriodType;
+import org.joda.time.Seconds;
+import org.joda.time.chrono.CopticChronology;
+import org.junit.runner.RunWith;
+
+public class ConverterSet_ESTestTest30 extends ConverterSet_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test29() throws Throwable {
+        ConverterSet converterSet0 = new ConverterSet((Converter[]) null);
+        // Undeclared exception!
+        try {
+            converterSet0.size();
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.joda.time.convert.ConverterSet", e);
+        }
+    }
+}
