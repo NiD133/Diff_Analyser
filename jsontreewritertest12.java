a2bb1b4f21ed56192403df4ac89230b816c033bf
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:30:16 +0800

    gemini processedManualTestCases jsontreewritertest12.java

--- jsontreewritertest12.java
+++ jsontreewritertest12.java
@@ -1,23 +1,25 @@
 package com.google.gson.internal.bind;
 
 import static com.google.common.truth.Truth.assertThat;
-import static org.junit.Assert.assertThrows;
-import com.google.gson.JsonElement;
+
 import com.google.gson.JsonNull;
-import com.google.gson.Strictness;
-import com.google.gson.common.MoreAsserts;
-import com.google.gson.stream.JsonWriter;
-import java.io.IOException;
-import java.io.Writer;
-import java.util.Arrays;
-import java.util.List;
 import org.junit.Test;
 
-public class JsonTreeWriterTestTest12 {
+/**
+ * Tests for {@link JsonTreeWriter}.
+ */
+public final class JsonTreeWriterTest {
 
-    @Test
-    public void testEmptyWriter() {
-        JsonTreeWriter writer = new JsonTreeWriter();
-        assertThat(writer.get()).isEqualTo(JsonNull.INSTANCE);
-    }
+  /**
+   * Verifies that a new {@link JsonTreeWriter} on which no write operations have been performed
+   * represents {@link JsonNull#INSTANCE}.
+   */
+  @Test
+  public void get_onNewWriter_returnsJsonNull() {
+    // Arrange: Create a new writer.
+    JsonTreeWriter writer = new JsonTreeWriter();
+
+    // Act & Assert: The initial state of the writer should be a JSON null.
+    assertThat(writer.get()).isSameInstanceAs(JsonNull.INSTANCE);
+  }
 }
