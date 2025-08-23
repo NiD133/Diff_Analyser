feff4e7334847ffc7cb90905715a462d441f7bf2
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:52:00 +0800

    gemini processedAutoTestCases gregorianchronology_estest13.java

--- gregorianchronology_estest13.java
+++ gregorianchronology_estest13.java
@@ -1,23 +1,32 @@
 package org.joda.time.chrono;
 
+import org.joda.time.DateTimeConstants;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.TimeZone;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.joda.time.Chronology;
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
-import org.joda.time.LocalDateTime;
-import org.junit.runner.RunWith;
 
-public class GregorianChronology_ESTestTest13 extends GregorianChronology_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
 
-    @Test(timeout = 4000)
-    public void test12() throws Throwable {
-        GregorianChronology gregorianChronology0 = GregorianChronology.getInstanceUTC();
-        long long0 = gregorianChronology0.getAverageMillisPerMonth();
-        assertEquals(2629746000L, long0);
+/**
+ * Unit tests for the GregorianChronology class.
+ */
+public class GregorianChronologyTest {
+
+    /**
+     * Verifies that getAverageMillisPerMonth() returns the correct, pre-calculated average.
+     * The average is based on a Gregorian year having 365.2425 days.
+     */
+    @Test
+    public void getAverageMillisPerMonth_returnsCorrectConstantValue() {
+        // Arrange
+        GregorianChronology chronology = GregorianChronology.getInstanceUTC();
+
+        // The expected value is derived from the average number of days in a Gregorian year.
+        // Calculation: (365.2425 days/year * MILLIS_PER_DAY) / 12 months/year
+        long expectedAverageMillis = (long) (365.2425 * DateTimeConstants.MILLIS_PER_DAY / 12);
+
+        // Act
+        long actualAverageMillis = chronology.getAverageMillisPerMonth();
+
+        // Assert
+        assertEquals(expectedAverageMillis, actualAverageMillis);
     }
 }
