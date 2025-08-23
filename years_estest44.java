727b5eae9af331e77f92fd8c73207002b86b119c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:30:26 +0800

    gemini processedAutoTestCases years_estest44.java

--- years_estest44.java
+++ years_estest44.java
@@ -1,17 +1,26 @@
 package org.joda.time;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class Years_ESTestTest44 extends Years_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link Years} class.
+ */
+public class YearsTest {
 
-    @Test(timeout = 4000)
-    public void test43() throws Throwable {
-        Years years0 = Years.years(3);
-        assertEquals(3, years0.getYears());
+    /**
+     * Tests that the factory method {@link Years#years(int)} creates an instance
+     * holding the specified number of years.
+     */
+    @Test
+    public void shouldCreateYearsInstanceWithCorrectValue() {
+        // Define the number of years to test with
+        final int expectedYears = 3;
+
+        // Create a Years instance using the factory method
+        Years years = Years.years(expectedYears);
+
+        // Verify that the created instance holds the correct value
+        assertEquals(expectedYears, years.getYears());
     }
 }
