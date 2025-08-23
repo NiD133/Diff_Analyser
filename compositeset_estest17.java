6d3dd455b6af4f6a2017f73036465cc9324a1604
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:45:08 +0800

    Original processedAutoTestCases compositeset_estest17.java

---
+++ compositeset_estest17.java
@@ -0,0 +1,85 @@
+package org.apache.commons.collections4.set;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.lang.reflect.Array;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+import org.apache.commons.collections4.Closure;
+import org.apache.commons.collections4.Equator;
+import org.apache.commons.collections4.Predicate;
+import org.apache.commons.collections4.Transformer;
+import org.apache.commons.collections4.functors.AnyPredicate;
+import org.apache.commons.collections4.functors.ChainedClosure;
+import org.apache.commons.collections4.functors.ConstantTransformer;
+import org.apache.commons.collections4.functors.DefaultEquator;
+import org.apache.commons.collections4.functors.EqualPredicate;
+import org.apache.commons.collections4.functors.ExceptionPredicate;
+import org.apache.commons.collections4.functors.FalsePredicate;
+import org.apache.commons.collections4.functors.IdentityPredicate;
+import org.apache.commons.collections4.functors.IfClosure;
+import org.apache.commons.collections4.functors.NonePredicate;
+import org.apache.commons.collections4.functors.NotNullPredicate;
+import org.apache.commons.collections4.functors.NotPredicate;
+import org.apache.commons.collections4.functors.NullIsExceptionPredicate;
+import org.apache.commons.collections4.functors.NullIsTruePredicate;
+import org.apache.commons.collections4.functors.OnePredicate;
+import org.apache.commons.collections4.functors.OrPredicate;
+import org.apache.commons.collections4.functors.TransformerClosure;
+import org.apache.commons.collections4.functors.TruePredicate;
+import org.apache.commons.collections4.functors.UniquePredicate;
+import org.apache.commons.collections4.functors.WhileClosure;
+import org.apache.commons.collections4.iterators.IteratorChain;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class CompositeSet_ESTestTest17 extends CompositeSet_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test16() throws Throwable {
+        LinkedHashSet<Integer> linkedHashSet0 = new LinkedHashSet<Integer>();
+        assertNotNull(linkedHashSet0);
+        assertTrue(linkedHashSet0.isEmpty());
+        assertEquals(0, linkedHashSet0.size());
+        Integer integer0 = new Integer(512);
+        assertNotNull(integer0);
+        assertEquals(512, (int) integer0);
+        boolean boolean0 = linkedHashSet0.add(integer0);
+        assertTrue(linkedHashSet0.contains(512));
+        assertEquals(1, linkedHashSet0.size());
+        assertFalse(linkedHashSet0.isEmpty());
+        assertTrue(boolean0);
+        CompositeSet<Object> compositeSet0 = new CompositeSet<Object>();
+        assertNotNull(compositeSet0);
+        assertFalse(compositeSet0.contains(512));
+        Set<Object> set0 = compositeSet0.toSet();
+        assertNotNull(set0);
+        assertFalse(compositeSet0.contains(512));
+        assertFalse(set0.contains(512));
+        assertEquals(0, set0.size());
+        assertTrue(set0.isEmpty());
+        compositeSet0.addComposited(set0);
+        assertFalse(compositeSet0.contains(512));
+        assertFalse(set0.contains(512));
+        assertEquals(0, set0.size());
+        assertTrue(set0.isEmpty());
+        boolean boolean1 = compositeSet0.containsAll(linkedHashSet0);
+        assertTrue(linkedHashSet0.contains(512));
+        assertFalse(compositeSet0.contains(512));
+        assertEquals(1, linkedHashSet0.size());
+        assertFalse(linkedHashSet0.isEmpty());
+        assertFalse(boolean1 == boolean0);
+        assertFalse(boolean1);
+    }
+}
