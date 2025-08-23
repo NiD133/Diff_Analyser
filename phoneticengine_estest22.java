1a9a9f1489dbdf71d744f7a9a291f7dee5ed6450
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:08:47 +0800

    Original processedAutoTestCases phoneticengine_estest22.java

---
+++ phoneticengine_estest22.java
@@ -0,0 +1,23 @@
+package org.apache.commons.codec.language.bm;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.nio.CharBuffer;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class PhoneticEngine_ESTestTest22 extends PhoneticEngine_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test21() throws Throwable {
+        RuleType ruleType0 = RuleType.EXACT;
+        NameType nameType0 = NameType.SEPHARDIC;
+        PhoneticEngine phoneticEngine0 = new PhoneticEngine(nameType0, ruleType0, false);
+        String string0 = phoneticEngine0.encode("du della");
+        assertEquals("dudela", string0);
+    }
+}
