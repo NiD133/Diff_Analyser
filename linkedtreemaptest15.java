a5e91445b75c0e0efa0aaeee97af481ec40c41ff
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:30:41 +0800

    gemini processedManualTestCases linkedtreemaptest15.java

--- linkedtreemaptest15.java
+++ linkedtreemaptest15.java
@@ -1,34 +1,43 @@
 package com.google.gson.internal;
 
-import static com.google.common.truth.Truth.assertThat;
-import static org.junit.Assert.assertThrows;
-import com.google.gson.common.MoreAsserts;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Random;
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
-public class LinkedTreeMapTestTest15 {
+/**
+ * Contains tests for the equals() and hashCode() contracts of {@link LinkedTreeMap}.
+ */
+public class LinkedTreeMapTest {
 
+    /**
+     * Verifies that two maps with the same entries are equal, regardless of insertion order,
+     * and consequently have the same hash code. This behavior is required by the
+     * {@link java.util.Map} interface contract.
+     */
     @Test
-    public void testEqualsAndHashCode() throws Exception {
+    public void mapsWithSameEntriesButDifferentInsertionOrderShouldBeEqualAndHaveSameHashCode() {
+        // Arrange: Create two maps with identical key-value pairs but inserted in a different sequence.
         LinkedTreeMap<String, Integer> map1 = new LinkedTreeMap<>();
         map1.put("A", 1);
         map1.put("B", 2);
         map1.put("C", 3);
         map1.put("D", 4);
+
         LinkedTreeMap<String, Integer> map2 = new LinkedTreeMap<>();
         map2.put("C", 3);
         map2.put("B", 2);
         map2.put("D", 4);
         map2.put("A", 1);
-        MoreAsserts.assertEqualsAndHashCode(map1, map2);
+
+        // Assert: The maps should be considered equal and must have the same hash code.
+        // We use standard JUnit assertions for clarity and better failure messages.
+
+        // 1. Test for equality.
+        // The Map.equals() contract is based on the entry sets being equal, not insertion order.
+        assertEquals(map1, map2);
+
+        // 2. Test for hash code consistency.
+        // If two objects are equal, their hash codes must also be equal.
+        assertEquals(map1.hashCode(), map2.hashCode());
     }
 }
