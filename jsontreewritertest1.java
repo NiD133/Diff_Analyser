1c0b4b4a44e9e1f5fd4789e316daf228a0ea8d67
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:30:15 +0800

    gemini processedManualTestCases jsontreewritertest1.java

--- jsontreewritertest1.java
+++ jsontreewritertest1.java
@@ -1,28 +1,35 @@
 package com.google.gson.internal.bind;
 
 import static com.google.common.truth.Truth.assertThat;
-import static org.junit.Assert.assertThrows;
+
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
-import com.google.gson.JsonNull;
-import com.google.gson.Strictness;
-import com.google.gson.common.MoreAsserts;
-import com.google.gson.stream.JsonWriter;
 import java.io.IOException;
-import java.io.Writer;
-import java.util.Arrays;
-import java.util.List;
 import org.junit.Test;
 
-public class JsonTreeWriterTestTest1 {
+/**
+ * Tests for {@link JsonTreeWriter}.
+ */
+public class JsonTreeWriterTest {
 
     @Test
-    public void testArray() throws IOException {
+    public void writeArray_withNumericValues_buildsCorrectJsonArray() throws IOException {
+        // Arrange
         JsonTreeWriter writer = new JsonTreeWriter();
+        JsonArray expectedJson = new JsonArray();
+        expectedJson.add(1);
+        expectedJson.add(2);
+        expectedJson.add(3);
+
+        // Act
         writer.beginArray();
         writer.value(1);
         writer.value(2);
         writer.value(3);
         writer.endArray();
-        assertThat(writer.get().toString()).isEqualTo("[1,2,3]");
+        JsonElement actualJson = writer.get();
+
+        // Assert
+        assertThat(actualJson).isEqualTo(expectedJson);
     }
 }
