ed33ad809dd068dfe8133481a7cc38be3b947411
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:05:34 +0800

    gemini processedAutoTestCases numericentityunescaper_estest14.java

--- numericentityunescaper_estest14.java
+++ numericentityunescaper_estest14.java
@@ -1,23 +1,30 @@
 package org.apache.commons.lang3.text.translate;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
+import static org.junit.Assert.assertEquals;
+
 import java.io.StringWriter;
-import java.io.Writer;
-import java.nio.CharBuffer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import java.io.IOException;
 
-public class NumericEntityUnescaper_ESTestTest14 extends NumericEntityUnescaper_ESTest_scaffolding {
+/**
+ * Tests for {@link NumericEntityUnescaper}.
+ */
+public class NumericEntityUnescaperTest {
 
-    @Test(timeout = 4000)
-    public void test13() throws Throwable {
-        NumericEntityUnescaper.OPTION[] numericEntityUnescaper_OPTIONArray0 = new NumericEntityUnescaper.OPTION[0];
-        StringWriter stringWriter0 = new StringWriter();
-        NumericEntityUnescaper numericEntityUnescaper0 = new NumericEntityUnescaper(numericEntityUnescaper_OPTIONArray0);
-        numericEntityUnescaper0.translate((CharSequence) "D+RTgb,eb:&s", (Writer) stringWriter0);
-        assertEquals("D+RTgb,eb:&s", stringWriter0.toString());
+    @Test
+    public void translateShouldNotAlterStringWithoutNumericEntities() throws IOException {
+        // Arrange
+        // The default constructor creates an unescaper that requires a semicolon for entities.
+        final NumericEntityUnescaper unescaper = new NumericEntityUnescaper();
+        final String input = "D+RTgb,eb:&s";
+        final StringWriter writer = new StringWriter();
+
+        // Act
+        unescaper.translate(input, writer);
+
+        // Assert
+        final String expectedOutput = "D+RTgb,eb:&s";
+        assertEquals("The string should remain unchanged as it contains no numeric entities to unescape.",
+                     expectedOutput, writer.toString());
     }
 }
