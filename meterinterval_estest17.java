d8ffb751c5313e395a283fc9e5d3e00c49b9a4c7
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:02:42 +0800

    Original processedAutoTestCases meterinterval_estest17.java

---
+++ meterinterval_estest17.java
@@ -0,0 +1,28 @@
+package org.jfree.chart.plot;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.GradientPaint;
+import java.awt.Paint;
+import java.awt.Stroke;
+import java.awt.SystemColor;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.jfree.data.Range;
+import org.jfree.data.statistics.DefaultBoxAndWhiskerCategoryDataset;
+import org.jfree.data.statistics.HistogramType;
+import org.junit.runner.RunWith;
+
+public class MeterInterval_ESTestTest17 extends MeterInterval_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test16() throws Throwable {
+        Range range0 = new Range((-229.41607020985958), (-229.41607020985958));
+        MeterInterval meterInterval0 = new MeterInterval(",", range0);
+        boolean boolean0 = meterInterval0.equals(meterInterval0);
+        assertTrue(boolean0);
+    }
+}
