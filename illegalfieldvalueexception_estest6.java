dd608525f285e624a66dce4e439e677248781bab
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:53:47 +0800

    gemini processedAutoTestCases illegalfieldvalueexception_estest6.java

--- illegalfieldvalueexception_estest6.java
+++ illegalfieldvalueexception_estest6.java
@@ -2,18 +2,47 @@ package org.joda.time;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class IllegalFieldValueException_ESTestTest6 extends IllegalFieldValueException_ESTest_scaffolding {
+/**
+ * Unit tests for {@link IllegalFieldValueException}.
+ */
+public class IllegalFieldValueExceptionTest {
 
-    @Test(timeout = 4000)
-    public void test05() throws Throwable {
-        DateTimeFieldType dateTimeFieldType0 = DateTimeFieldType.secondOfMinute();
-        IllegalFieldValueException illegalFieldValueException0 = new IllegalFieldValueException(dateTimeFieldType0, "");
-        illegalFieldValueException0.getDateTimeFieldType();
-        assertEquals("Value \"\" for secondOfMinute is not supported", illegalFieldValueException0.getMessage());
+    /**
+     * Tests the constructor that accepts a DateTimeFieldType and an illegal String value.
+     * This test verifies that all properties of the created exception are set correctly.
+     */
+    @Test
+    public void constructor_withDateTimeFieldTypeAndStringValue_setsAllPropertiesCorrectly() {
+        // Arrange
+        DateTimeFieldType fieldType = DateTimeFieldType.secondOfMinute();
+        String illegalValue = "";
+        String expectedMessage = "Value \"\" for secondOfMinute is not supported";
+
+        // Act
+        IllegalFieldValueException exception = new IllegalFieldValueException(fieldType, illegalValue);
+
+        // Assert
+        // Verify the primary properties are set as expected
+        assertEquals("The exception message should be correctly formatted.",
+                expectedMessage, exception.getMessage());
+        assertSame("The DateTimeFieldType should be the one provided in the constructor.",
+                fieldType, exception.getDateTimeFieldType());
+        assertEquals("The field name should be derived from the DateTimeFieldType.",
+                "secondOfMinute", exception.getFieldName());
+        assertSame("The illegal string value should be stored.",
+                illegalValue, exception.getIllegalStringValue());
+        assertEquals("The illegal value should be available as a string.",
+                illegalValue, exception.getIllegalValueAsString());
+
+        // Verify that properties related to other constructor overloads are null
+        assertNull("DurationFieldType should be null for this constructor.",
+                exception.getDurationFieldType());
+        assertNull("IllegalNumberValue should be null when a string value is provided.",
+                exception.getIllegalNumberValue());
+        assertNull("LowerBound should be null as it was not provided.",
+                exception.getLowerBound());
+        assertNull("UpperBound should be null as it was not provided.",
+                exception.getUpperBound());
     }
 }
