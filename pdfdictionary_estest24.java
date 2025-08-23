17aa93e2162168e6bcb96a4ede53509327722689
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:07:52 +0800

    Original processedAutoTestCases pdfdictionary_estest24.java

---
+++ pdfdictionary_estest24.java
@@ -0,0 +1,28 @@
+package com.itextpdf.text.pdf;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.collection.PdfCollectionField;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PipedOutputStream;
+import java.util.LinkedHashMap;
+import java.util.Set;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockPrintStream;
+import org.junit.runner.RunWith;
+
+public class PdfDictionary_ESTestTest24 extends PdfDictionary_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test23() throws Throwable {
+        PdfResources pdfResources0 = new PdfResources();
+        PdfCollectionField pdfCollectionField0 = new PdfCollectionField("UnicodeBig", 8);
+        boolean boolean0 = pdfCollectionField0.checkType(pdfResources0.CATALOG);
+        assertFalse(boolean0);
+    }
+}
