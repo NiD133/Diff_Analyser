11726ae36b5badb35cfdbb42642e9f439e3300fc
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:31:36 +0800

    gemini processedManualTestCases numericentityunescapertest1.java

--- numericentityunescapertest1.java
+++ numericentityunescapertest1.java
@@ -1,18 +1,42 @@
 package org.apache.commons.lang3.text.translate;
 
-import static org.apache.commons.lang3.LangAssertions.assertIllegalArgumentException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.apache.commons.lang3.AbstractLangTest;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-public class NumericEntityUnescaperTestTest1 extends AbstractLangTest {
+/**
+ * Tests for {@link NumericEntityUnescaper}.
+ */
+// The class name has been improved from "NumericEntityUnescaperTestTest1"
+// to the standard convention "NumericEntityUnescaperTest".
+public class NumericEntityUnescaperTest extends AbstractLangTest {
 
-    @Test
-    void testOutOfBounds() {
-        final NumericEntityUnescaper neu = new NumericEntityUnescaper();
-        assertEquals("Test &", neu.translate("Test &"), "Failed to ignore when last character is &");
-        assertEquals("Test &#", neu.translate("Test &#"), "Failed to ignore when last character is &");
-        assertEquals("Test &#x", neu.translate("Test &#x"), "Failed to ignore when last character is &");
-        assertEquals("Test &#X", neu.translate("Test &#X"), "Failed to ignore when last character is &");
+    private NumericEntityUnescaper unescaper;
+
+    @BeforeEach
+    void setUp() {
+        // The unescaper is created with default options (semicolon required).
+        unescaper = new NumericEntityUnescaper();
+    }
+
+    @DisplayName("Should not translate incomplete numeric entities at the end of the input")
+    @ParameterizedTest(name = "Input: \"{0}\"")
+    @ValueSource(strings = {
+        "Test &",    // Ampersand alone
+        "Test &#",   // Incomplete decimal entity
+        "Test &#x",  // Incomplete hex entity (lowercase)
+        "Test &#X"   // Incomplete hex entity (uppercase)
+    })
+    void translateShouldIgnoreIncompleteEntityAtEndOfInput(final String input) {
+        // Act
+        final String result = unescaper.translate(input);
+
+        // Assert
+        // The unescaper should not change the input string if the entity is incomplete.
+        assertEquals(input, result);
     }
 }
