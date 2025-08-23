25d414a2ad9a810ef50b92586a7d65b47d65d487
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:45:46 +0800

    Original processedAutoTestCases converterset_estest24.java

---
+++ converterset_estest24.java
@@ -0,0 +1,26 @@
+package org.joda.time.convert;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.joda.time.Hours;
+import org.joda.time.Interval;
+import org.joda.time.MutablePeriod;
+import org.joda.time.PeriodType;
+import org.joda.time.Seconds;
+import org.joda.time.chrono.CopticChronology;
+import org.junit.runner.RunWith;
+
+public class ConverterSet_ESTestTest24 extends ConverterSet_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test23() throws Throwable {
+        Converter[] converterArray0 = new Converter[0];
+        ConverterSet converterSet0 = new ConverterSet(converterArray0);
+        ReadableInstantConverter readableInstantConverter0 = ReadableInstantConverter.INSTANCE;
+        ConverterSet converterSet1 = converterSet0.add(readableInstantConverter0, (Converter[]) null);
+        assertNotSame(converterSet0, converterSet1);
+    }
+}
