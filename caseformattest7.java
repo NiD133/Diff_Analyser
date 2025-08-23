06278346610e72d87d602d24d3d1807c06f4386b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:26:26 +0800

    gemini processedManualTestCases caseformattest7.java

--- caseformattest7.java
+++ caseformattest7.java
@@ -1,24 +1,56 @@
 package com.google.common.base;
 
-import static com.google.common.base.CaseFormat.LOWER_CAMEL;
 import static com.google.common.base.CaseFormat.LOWER_HYPHEN;
-import static com.google.common.base.CaseFormat.LOWER_UNDERSCORE;
-import static com.google.common.base.CaseFormat.UPPER_CAMEL;
 import static com.google.common.base.CaseFormat.UPPER_UNDERSCORE;
 import static com.google.common.truth.Truth.assertThat;
-import static com.google.common.truth.Truth.assertWithMessage;
-import com.google.common.annotations.GwtCompatible;
-import com.google.common.annotations.GwtIncompatible;
-import com.google.common.annotations.J2ktIncompatible;
-import com.google.common.testing.NullPointerTester;
-import com.google.common.testing.SerializableTester;
-import junit.framework.TestCase;
-import org.jspecify.annotations.NullUnmarked;
 
-public class CaseFormatTestTest7 extends TestCase {
+import java.util.Arrays;
+import java.util.Collection;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
-    public void testLowerHyphenToUpperUnderscore() {
-        assertThat(LOWER_HYPHEN.to(UPPER_UNDERSCORE, "foo")).isEqualTo("FOO");
-        assertThat(LOWER_HYPHEN.to(UPPER_UNDERSCORE, "foo-bar")).isEqualTo("FOO_BAR");
-    }
+/**
+ * Parameterized tests for conversions from {@link CaseFormat#LOWER_HYPHEN} to {@link
+ * CaseFormat#UPPER_UNDERSCORE}.
+ */
+@RunWith(Parameterized.class)
+public class LowerHyphenToUpperUnderscoreTest {
+
+  private final String input;
+  private final String expectedOutput;
+
+  public LowerHyphenToUpperUnderscoreTest(String input, String expectedOutput) {
+    this.input = input;
+    this.expectedOutput = expectedOutput;
+  }
+
+  @Parameters(name = "\"{0}\" -> \"{1}\")
+  public static Collection<Object[]> data() {
+    return Arrays.asList(
+        new Object[][] {
+          // Test case 1: Single word
+          {"foo", "FOO"},
+          // Test case 2: Multiple words
+          {"foo-bar", "FOO_BAR"},
+          // Test case 3: Empty string (edge case)
+          {"", ""},
+          // Test case 4: String with leading hyphen (edge case)
+          {"-foo", "_FOO"},
+          // Test case 5: String with trailing hyphen (edge case)
+          {"foo-", "FOO_"}
+        });
+  }
+
+  @Test
+  public void to_fromLowerHyphenToUpperUnderscore_convertsCorrectly() {
+    // Arrange: The input and expectedOutput are provided by the test parameters.
+
+    // Act: Perform the case conversion.
+    String actualOutput = LOWER_HYPHEN.to(UPPER_UNDERSCORE, input);
+
+    // Assert: Verify the result matches the expected output.
+    assertThat(actualOutput).isEqualTo(expectedOutput);
+  }
 }
