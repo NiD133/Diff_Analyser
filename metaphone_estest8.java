da208726e88143bbda7b1121524bc32fbd3a365b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:02:38 +0800

    Original processedAutoTestCases metaphone_estest8.java

---
+++ metaphone_estest8.java
@@ -0,0 +1,18 @@
+package org.apache.commons.codec.language;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class Metaphone_ESTestTest8 extends Metaphone_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test07() throws Throwable {
+        Metaphone metaphone0 = new Metaphone();
+        String string0 = metaphone0.metaphone("XMBX");
+        //  // Unstable assertion: assertEquals("XMBK", string0);
+    }
+}
