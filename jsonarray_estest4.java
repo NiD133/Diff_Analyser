ebb5a3d45e15cd11e57c430b71184f9c76907d1b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:56:07 +0800

    gemini processedAutoTestCases jsonarray_estest4.java

--- jsonarray_estest4.java
+++ jsonarray_estest4.java
@@ -2,23 +2,26 @@ package com.google.gson;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.util.Iterator;
-import java.util.List;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class JsonArray_ESTestTest4 extends JsonArray_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link JsonArray} class.
+ */
+public class JsonArrayTest {
 
-    @Test(timeout = 4000)
-    public void test03() throws Throwable {
-        JsonArray jsonArray0 = new JsonArray(846);
-        Character character0 = Character.valueOf('\\');
-        jsonArray0.add(character0);
-        JsonElement jsonElement0 = jsonArray0.remove(0);
-        assertFalse(jsonElement0.isJsonNull());
+    @Test
+    public void removeAtIndex_returnsTheRemovedElement() {
+        // Arrange
+        JsonArray jsonArray = new JsonArray();
+        Character characterToAdd = 'a';
+        jsonArray.add(characterToAdd);
+
+        JsonElement expectedElement = new JsonPrimitive(characterToAdd);
+
+        // Act
+        JsonElement removedElement = jsonArray.remove(0);
+
+        // Assert
+        assertEquals("The removed element should be the one that was added.", expectedElement, removedElement);
+        assertTrue("The array should be empty after removing its only element.", jsonArray.isEmpty());
     }
 }
