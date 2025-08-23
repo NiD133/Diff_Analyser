b3d99dc73b8b697e47a6c950810be80490d4527e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:06:31 +0800

    Original processedAutoTestCases optionformatter_estest38.java

---
+++ optionformatter_estest38.java
@@ -0,0 +1,24 @@
+package org.apache.commons.cli.help;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.function.BiFunction;
+import org.apache.commons.cli.Option;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class OptionFormatter_ESTestTest38 extends OptionFormatter_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test37() throws Throwable {
+        Option option0 = new Option((String) null, (String) null);
+        OptionFormatter optionFormatter0 = OptionFormatter.from(option0);
+        OptionFormatter.Builder optionFormatter_Builder0 = new OptionFormatter.Builder(optionFormatter0);
+        OptionFormatter.Builder optionFormatter_Builder1 = optionFormatter_Builder0.setDeprecatedFormatFunction(optionFormatter0.COMPLEX_DEPRECATED_FORMAT);
+        assertSame(optionFormatter_Builder0, optionFormatter_Builder1);
+    }
+}
