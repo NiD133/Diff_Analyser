1ce66f0e2e9c9571e697a10a7a5c7ff7c947072d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:09:50 +0800

    gemini processedAutoTestCases quarterdateformat_estest10.java

--- quarterdateformat_estest10.java
+++ quarterdateformat_estest10.java
@@ -1,29 +1,24 @@
 package org.jfree.chart.axis;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.StringWriter;
-import java.text.FieldPosition;
-import java.text.Format;
-import java.text.NumberFormat;
-import java.text.ParsePosition;
-import java.util.Date;
-import java.util.SimpleTimeZone;
-import java.util.TimeZone;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.evosuite.runtime.mock.java.util.MockDate;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class QuarterDateFormat_ESTestTest10 extends QuarterDateFormat_ESTest_scaffolding {
+/**
+ * Unit tests for the equals() method in the {@link QuarterDateFormat} class.
+ */
+public class QuarterDateFormatTest {
 
-    @Test(timeout = 4000)
-    public void test09() throws Throwable {
-        QuarterDateFormat quarterDateFormat0 = new QuarterDateFormat();
-        boolean boolean0 = quarterDateFormat0.equals(quarterDateFormat0);
-        assertTrue(boolean0);
+    /**
+     * Verifies the reflexive property of the equals() method.
+     * An object must always be equal to itself.
+     */
+    @Test
+    public void equals_givenSameInstance_returnsTrue() {
+        // Arrange: Create an instance of the class.
+        QuarterDateFormat formatter = new QuarterDateFormat();
+
+        // Assert: The object must be equal to itself.
+        // This is a standard contract of the Object.equals() method.
+        assertEquals(formatter, formatter);
     }
 }
