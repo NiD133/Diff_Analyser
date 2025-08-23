756d96c65f866cc8273c173487f33fbd4885af76
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:29:16 +0800

    Original processedManualTestCases hashcodeandequalssafesettest16.java

---
+++ hashcodeandequalssafesettest16.java
@@ -0,0 +1,43 @@
+package org.mockito.internal.util.collections;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.mock;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Observer;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+public class HashCodeAndEqualsSafeSetTestTest16 {
+
+    @Rule
+    public MockitoRule r = MockitoJUnit.rule();
+
+    @Mock
+    private UnmockableHashCodeAndEquals mock1;
+
+    private static class UnmockableHashCodeAndEquals {
+
+        @Override
+        public final int hashCode() {
+            throw new NullPointerException("I'm failing on hashCode and I don't care");
+        }
+
+        @Override
+        public final boolean equals(Object obj) {
+            throw new NullPointerException("I'm failing on equals and I don't care");
+        }
+    }
+
+    @Test
+    public void toStringIsNotNullOrEmpty() throws Exception {
+        HashCodeAndEqualsSafeSet set = HashCodeAndEqualsSafeSet.of(mock1);
+        assertThat(set.toString()).isNotEmpty();
+    }
+}
