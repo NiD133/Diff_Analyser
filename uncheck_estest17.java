561dba9c74f71836f12f3631b95e6122991ffc76
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:24:28 +0800

    Original processedAutoTestCases uncheck_estest17.java

---
+++ uncheck_estest17.java
@@ -0,0 +1,31 @@
+package org.apache.commons.io.function;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.lang.reflect.Array;
+import java.time.chrono.HijrahEra;
+import java.util.Comparator;
+import java.util.concurrent.ForkJoinTask;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+import java.util.stream.LongStream;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class Uncheck_ESTestTest17 extends Uncheck_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test16() throws Throwable {
+        IOComparator<String> iOComparator0 = (IOComparator<String>) mock(IOComparator.class, new ViolatedAssumptionAnswer());
+        doReturn(553).when(iOComparator0).compare(anyString(), anyString());
+        int int0 = Uncheck.compare(iOComparator0, "I8ucG1|7Y@", "I8ucG1|7Y@");
+        assertEquals(553, int0);
+    }
+}
