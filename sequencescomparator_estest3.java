a42164da1aa160fdc80e7331dd5e64921d4ab4e6
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:14:37 +0800

    gemini processedAutoTestCases sequencescomparator_estest3.java

--- sequencescomparator_estest3.java
+++ sequencescomparator_estest3.java
@@ -1,31 +1,50 @@
 package org.apache.commons.collections4.sequence;
 
+import org.apache.commons.collections4.Equator;
+import org.apache.commons.collections4.functors.DefaultEquator;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.ConcurrentModificationException;
+
 import java.util.LinkedList;
 import java.util.List;
-import org.apache.commons.collections4.Equator;
-import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.functors.DefaultEquator;
-import org.apache.commons.collections4.functors.NotNullPredicate;
-import org.apache.commons.collections4.functors.PredicateTransformer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class SequencesComparator_ESTestTest3 extends SequencesComparator_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
 
-    @Test(timeout = 4000)
-    public void test02() throws Throwable {
-        LinkedList<Object> linkedList0 = new LinkedList<Object>();
-        linkedList0.push((Object) null);
-        DefaultEquator<Object> defaultEquator0 = DefaultEquator.defaultEquator();
-        SequencesComparator<Object> sequencesComparator0 = new SequencesComparator<Object>(linkedList0, linkedList0, defaultEquator0);
-        Object object0 = new Object();
-        linkedList0.add(object0);
-        EditScript<Object> editScript0 = sequencesComparator0.getScript();
-        assertEquals(0, editScript0.getModifications());
+/**
+ * Contains tests for the {@link SequencesComparator} class.
+ */
+public class SequencesComparatorTest {
+
+    /**
+     * Tests that the comparison is based on the state of the lists at the time
+     * getScript() is called, not at the time of the comparator's construction.
+     * This confirms that the comparator uses live references to the lists.
+     */
+    @Test
+    public void testComparisonUsesListStateAtTimeOfExecutionNotConstruction() {
+        // Arrange
+        // Create a single list instance that will be used for both sequences.
+        List<Object> sequence = new LinkedList<>();
+        sequence.add("initial element");
+
+        // Instantiate the comparator. At this point, it holds a reference
+        // to the list containing one element.
+        SequencesComparator<Object> comparator = new SequencesComparator<>(sequence, sequence);
+
+        // Act
+        // Modify the list *after* the comparator has been constructed.
+        sequence.add("added element");
+
+        // Now, when getScript() is called, the comparator will see the modified list
+        // for both the 'left' and 'right' sequences. It will be comparing
+        // ["initial element", "added element"] to itself.
+        EditScript<Object> script = comparator.getScript();
+
+        // Assert
+        // Since the comparator is effectively comparing a list to itself,
+        // there should be no differences and therefore no modifications.
+        final int expectedModifications = 0;
+        assertEquals("There should be no modifications when comparing a list to itself, " +
+                     "even if it was modified after the comparator was created.",
+                     expectedModifications, script.getModifications());
     }
 }
