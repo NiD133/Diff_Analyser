6fd0988a9dd8a3abf97e14697bf2a813d94346c2
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:20:34 +0800

    gemini processedAutoTestCases taiinstant_estest36.java

--- taiinstant_estest36.java
+++ taiinstant_estest36.java
@@ -1,37 +1,27 @@
 package org.threeten.extra.scale;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.time.Clock;
 import java.time.Duration;
-import java.time.Instant;
-import java.time.format.DateTimeParseException;
 import java.time.temporal.ChronoUnit;
-import java.time.temporal.TemporalAmount;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.time.MockClock;
-import org.evosuite.runtime.mock.java.time.MockInstant;
-import org.junit.runner.RunWith;
 
-public class TaiInstant_ESTestTest36 extends TaiInstant_ESTest_scaffolding {
+/**
+ * Tests for the {@link TaiInstant#minus(Duration)} method.
+ */
+public class TaiInstantMinusTest {
 
-    @Test(timeout = 4000)
-    public void test35() throws Throwable {
-        UtcInstant utcInstant0 = UtcInstant.ofModifiedJulianDay(14L, 14L);
-        TaiInstant taiInstant0 = TaiInstant.of(utcInstant0);
-        ChronoUnit chronoUnit0 = ChronoUnit.FOREVER;
-        Duration duration0 = chronoUnit0.getDuration();
-        // Undeclared exception!
-        try {
-            taiInstant0.minus(duration0);
-            fail("Expecting exception: ArithmeticException");
-        } catch (ArithmeticException e) {
-            //
-            // long overflow
-            //
-            verifyException("java.lang.Math", e);
-        }
+    @Test(expected = ArithmeticException.class)
+    public void minus_whenDurationIsForever_throwsArithmeticException() {
+        // Arrange
+        // A duration of "forever" is the largest possible duration and will cause an overflow
+        // when used in subtraction, as documented by the minus() method.
+        Duration foreverDuration = ChronoUnit.FOREVER.getDuration();
+        TaiInstant anyInstant = TaiInstant.ofTaiSeconds(0, 0);
+
+        // Act
+        // Attempt to subtract this massive duration from an instant.
+        anyInstant.minus(foreverDuration);
+
+        // Assert
+        // The @Test(expected) annotation verifies that an ArithmeticException is thrown.
     }
 }
