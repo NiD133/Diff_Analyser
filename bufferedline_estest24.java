78bec72bda8419f64593f6aea0f2a729c3376082
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:40:15 +0800

    Original processedAutoTestCases bufferedline_estest24.java

---
+++ bufferedline_estest24.java
@@ -0,0 +1,36 @@
+package org.locationtech.spatial4j.shape.impl;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.HashMap;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+import org.locationtech.spatial4j.context.SpatialContext;
+import org.locationtech.spatial4j.context.SpatialContextFactory;
+import org.locationtech.spatial4j.distance.CartesianDistCalc;
+import org.locationtech.spatial4j.shape.Point;
+import org.locationtech.spatial4j.shape.Rectangle;
+import org.locationtech.spatial4j.shape.Shape;
+import org.locationtech.spatial4j.shape.SpatialRelation;
+
+public class BufferedLine_ESTestTest24 extends BufferedLine_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test23() throws Throwable {
+        SpatialContext spatialContext0 = SpatialContext.GEO;
+        PointImpl pointImpl0 = new PointImpl(0.0, 0.0, spatialContext0);
+        BufferedLine bufferedLine0 = new BufferedLine(pointImpl0, pointImpl0, 0.0, spatialContext0);
+        // Undeclared exception!
+        try {
+            bufferedLine0.contains((Point) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.locationtech.spatial4j.shape.impl.InfBufLine", e);
+        }
+    }
+}
