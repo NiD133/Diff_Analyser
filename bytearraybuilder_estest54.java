d8beb5cfab1370072edf52e2d1b628709a0f876c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:40:44 +0800

    Original processedAutoTestCases bytearraybuilder_estest54.java

---
+++ bytearraybuilder_estest54.java
@@ -0,0 +1,18 @@
+package com.fasterxml.jackson.core.util;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class ByteArrayBuilder_ESTestTest54 extends ByteArrayBuilder_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test53() throws Throwable {
+        ByteArrayBuilder byteArrayBuilder0 = new ByteArrayBuilder();
+        byteArrayBuilder0.reset();
+        assertEquals(0, byteArrayBuilder0.size());
+    }
+}
