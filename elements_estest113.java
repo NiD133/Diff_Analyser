23a72347eef30ebf13c56b21d1d880a1525d3db0
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:48:18 +0800

    Original processedAutoTestCases elements_estest113.java

---
+++ elements_estest113.java
@@ -0,0 +1,36 @@
+package org.jsoup.select;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.ConcurrentModificationException;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.jsoup.nodes.Comment;
+import org.jsoup.nodes.DataNode;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.FormElement;
+import org.jsoup.nodes.TextNode;
+import org.jsoup.parser.Parser;
+import org.junit.runner.RunWith;
+
+public class Elements_ESTestTest113 extends Elements_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test112() throws Throwable {
+        Document document0 = Parser.parseBodyFragment("Bb,Y6", "Bb,Y6");
+        Elements elements0 = document0.getAllElements();
+        boolean boolean0 = elements0.is("Bb,Y6");
+        assertFalse(boolean0);
+    }
+}
