9e169c4d7256d10a1bced6fda7543ef9dff77bbe
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:36:10 +0800

    Original processedManualTestCases xmltreebuildertest20.java

---
+++ xmltreebuildertest20.java
@@ -0,0 +1,33 @@
+package org.jsoup.parser;
+
+import org.jsoup.Jsoup;
+import org.jsoup.TextUtil;
+import org.jsoup.nodes.*;
+import org.jsoup.select.Elements;
+import org.junit.jupiter.api.Test;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import static org.jsoup.nodes.Document.OutputSettings.Syntax;
+import static org.jsoup.parser.Parser.NamespaceHtml;
+import static org.jsoup.parser.Parser.NamespaceXml;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class XmlTreeBuilderTestTest20 {
+
+    private static void assertXmlNamespace(Element el) {
+        assertEquals(NamespaceXml, el.tag().namespace(), String.format("Element %s not in XML namespace", el.tagName()));
+    }
+
+    @Test
+    public void normalizesDiscordantTags() {
+        Parser parser = Parser.xmlParser().settings(ParseSettings.htmlDefault);
+        Document document = Jsoup.parse("<div>test</DIV><p></p>", "", parser);
+        assertEquals("<div>test</div><p></p>", document.html());
+        // was failing -> toString() = "<div>\n test\n <p></p>\n</div>"
+    }
+}
