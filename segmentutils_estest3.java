5ccde24766cdc75807f218aca207cca8edb9b013
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:14:07 +0800

    Original processedAutoTestCases segmentutils_estest3.java

---
+++ segmentutils_estest3.java
@@ -0,0 +1,17 @@
+package org.apache.commons.compress.harmony.unpack200;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class SegmentUtils_ESTestTest3 extends SegmentUtils_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test02() throws Throwable {
+        int int0 = SegmentUtils.countInvokeInterfaceArgs("1.8()JbKnQPeYyNxq!");
+        assertEquals(0, int0);
+    }
+}
