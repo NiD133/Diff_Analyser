223452033de17b3b350985364749554b51736095
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 05:35:43 +0800

    Original manualTestSuite WKTWriterTest.java

--- WKTWriterTest.java
+++ WKTWriterTest.java
@@ -1,52 +1,37 @@
 package org.locationtech.spatial4j.io;
 
 import static org.junit.Assert.assertEquals;
-
-import java.util.Collections;
-import org.junit.Before;
+import java.util.ArrayList;
 import org.junit.Test;
 import org.locationtech.spatial4j.context.SpatialContext;
 import org.locationtech.spatial4j.shape.Point;
 import org.locationtech.spatial4j.shape.ShapeCollection;
 
-/**
- * Tests for {@link WKTWriter} to ensure it correctly formats shapes into
- * Well-Known Text, particularly for empty geometries.
- */
 public class WKTWriterTest {
 
-  private WKTWriter writer;
   private SpatialContext ctx;
 
-  @Before
-  public void setUp() {
-    ctx = SpatialContext.GEO;
-    writer = (WKTWriter) ctx.getFormats().getWktWriter();
+  protected WKTWriterTest(SpatialContext ctx) {
+    this.ctx = ctx;
+  }
+
+  public WKTWriterTest() {
+    this(SpatialContext.GEO);
   }
 
   @Test
-  public void toString_givenEmptyPoint_returnsPointEmptyWkt() {
-    // Arrange
+  public void testToStringOnEmptyPoint() throws Exception {
+    ShapeWriter writer = ctx.getFormats().getWktWriter();
     Point emptyPoint = ctx.makePoint(Double.NaN, Double.NaN);
-    String expectedWkt = "POINT EMPTY";
 
-    // Act
-    String actualWkt = writer.toString(emptyPoint);
-
-    // Assert
-    assertEquals(expectedWkt, actualWkt);
+    assertEquals("POINT EMPTY", writer.toString(emptyPoint));
   }
 
   @Test
-  public void toString_givenEmptyShapeCollection_returnsGeometryCollectionEmptyWkt() {
-    // Arrange
-    ShapeCollection<?> emptyCollection = ctx.makeCollection(Collections.emptyList());
-    String expectedWkt = "GEOMETRYCOLLECTION EMPTY";
+  public void testToStringOnEmptyShapeCollection() throws Exception {
+    ShapeWriter writer = ctx.getFormats().getWktWriter();
+    ShapeCollection<Point> emptyCollection = ctx.makeCollection(new ArrayList<>());
 
-    // Act
-    String actualWkt = writer.toString(emptyCollection);
-
-    // Assert
-    assertEquals(expectedWkt, actualWkt);
+    assertEquals("GEOMETRYCOLLECTION EMPTY", writer.toString(emptyCollection));
   }
 }
