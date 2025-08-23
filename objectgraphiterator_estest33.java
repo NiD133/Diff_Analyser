1660ebbd636d03dfd16ead22084845cb76a7b242
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:05:46 +0800

    gemini processedAutoTestCases objectgraphiterator_estest33.java

--- objectgraphiterator_estest33.java
+++ objectgraphiterator_estest33.java
@@ -1,62 +1,28 @@
 package org.apache.commons.collections4.iterators;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Comparator;
-import java.util.ConcurrentModificationException;
-import java.util.HashMap;
+
+import java.util.Collections;
 import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.ListIterator;
-import java.util.Map;
 import java.util.NoSuchElementException;
-import java.util.function.Function;
-import org.apache.commons.collections4.Factory;
-import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AndPredicate;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantFactory;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionPredicate;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.FactoryTransformer;
-import org.apache.commons.collections4.functors.FalsePredicate;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.MapTransformer;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NullIsFalsePredicate;
-import org.apache.commons.collections4.functors.NullIsTruePredicate;
-import org.apache.commons.collections4.functors.OrPredicate;
-import org.apache.commons.collections4.functors.PredicateTransformer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
 
-public class ObjectGraphIterator_ESTestTest33 extends ObjectGraphIterator_ESTest_scaffolding {
+/**
+ * Test cases for {@link ObjectGraphIterator}.
+ * This class focuses on specific behaviors of the iterator.
+ */
+public class ObjectGraphIteratorTest {
 
-    @Test(timeout = 4000)
-    public void test32() throws Throwable {
-        LinkedList<Integer> linkedList0 = new LinkedList<Integer>();
-        Iterator<Integer> iterator0 = linkedList0.descendingIterator();
-        ObjectGraphIterator<Object> objectGraphIterator0 = new ObjectGraphIterator<Object>(iterator0);
-        // Undeclared exception!
-        try {
-            objectGraphIterator0.next();
-            fail("Expecting exception: NoSuchElementException");
-        } catch (NoSuchElementException e) {
-            //
-            // No more elements in the iteration
-            //
-            verifyException("org.apache.commons.collections4.iterators.ObjectGraphIterator", e);
-        }
+    /**
+     * Verifies that calling next() on an ObjectGraphIterator initialized with an
+     * empty root iterator correctly throws a NoSuchElementException.
+     */
+    @Test(expected = NoSuchElementException.class)
+    public void nextShouldThrowExceptionWhenRootIteratorIsEmpty() {
+        // Arrange: Create an ObjectGraphIterator with an empty iterator as its root.
+        final Iterator<Object> emptyRootIterator = Collections.emptyIterator();
+        final ObjectGraphIterator<Object> graphIterator = new ObjectGraphIterator<>(emptyRootIterator);
+
+        // Act & Assert: Calling next() on the iterator is expected to throw.
+        graphIterator.next();
     }
 }
