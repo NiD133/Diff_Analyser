a65f8c8e3a07a8e4b4c6f53701c98aae57f52ee8
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:46:45 +0800

    gemini processedAutoTestCases dayofmonth_estest28.java

--- dayofmonth_estest28.java
+++ dayofmonth_estest28.java
@@ -1,45 +1,28 @@
 package org.threeten.extra;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.time.Clock;
-import java.time.DateTimeException;
-import java.time.Duration;
-import java.time.Month;
-import java.time.YearMonth;
-import java.time.ZoneId;
 import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
-import java.time.chrono.Chronology;
-import java.time.chrono.HijrahDate;
-import java.time.chrono.MinguoDate;
-import java.time.temporal.ChronoField;
-import java.time.temporal.ChronoUnit;
-import java.time.temporal.Temporal;
-import java.time.temporal.TemporalAccessor;
-import java.time.temporal.TemporalField;
-import java.time.temporal.TemporalQuery;
-import java.time.temporal.UnsupportedTemporalTypeException;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.evosuite.runtime.mock.java.time.MockClock;
-import org.evosuite.runtime.mock.java.time.MockYearMonth;
-import org.evosuite.runtime.mock.java.time.MockZonedDateTime;
-import org.evosuite.runtime.mock.java.time.chrono.MockHijrahDate;
-import org.evosuite.runtime.mock.java.time.chrono.MockMinguoDate;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertFalse;
 
+/**
+ * Tests for the {@link DayOfMonth#equals(Object)} method.
+ */
 public class DayOfMonth_ESTestTest28 extends DayOfMonth_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test27() throws Throwable {
-        ZoneOffset zoneOffset0 = ZoneOffset.MAX;
-        DayOfMonth dayOfMonth0 = DayOfMonth.now();
-        boolean boolean0 = dayOfMonth0.equals(zoneOffset0);
-        assertEquals(14, dayOfMonth0.getValue());
-        assertFalse(boolean0);
+    /**
+     * Tests that the equals() method returns false when comparing a DayOfMonth
+     * instance with an object of a completely different type.
+     */
+    @Test
+    public void equals_returnsFalseWhenComparedWithObjectOfDifferentType() {
+        // Arrange
+        DayOfMonth dayOfMonth = DayOfMonth.of(15);
+        Object differentTypeObject = ZoneOffset.MAX;
+
+        // Act
+        boolean isEqual = dayOfMonth.equals(differentTypeObject);
+
+        // Assert
+        assertFalse("DayOfMonth should not be equal to an object of a different type.", isEqual);
     }
 }
