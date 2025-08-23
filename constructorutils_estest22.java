85a7b3db16a66205a9fee289702a17ee240e4ee9
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:45:38 +0800

    Original processedAutoTestCases constructorutils_estest22.java

---
+++ constructorutils_estest22.java
@@ -0,0 +1,27 @@
+package org.apache.commons.lang3.reflect;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.AccessibleObject;
+import java.lang.reflect.Array;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class ConstructorUtils_ESTestTest22 extends ConstructorUtils_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test21() throws Throwable {
+        Class<Integer>[] classArray0 = (Class<Integer>[]) Array.newInstance(Class.class, 1);
+        Class<Integer> class0 = Integer.class;
+        try {
+            ConstructorUtils.invokeConstructor(class0, (Object[]) classArray0);
+            fail("Expecting exception: InvocationTargetException");
+        } catch (InvocationTargetException e) {
+        }
+    }
+}
