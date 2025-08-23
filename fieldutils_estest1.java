c2050878f348cd2e5d887a9ce2efeb7b5f9922d2
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:50:12 +0800

    gemini processedAutoTestCases fieldutils_estest1.java

--- fieldutils_estest1.java
+++ fieldutils_estest1.java
@@ -1,23 +1,33 @@
 package org.joda.time.field;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.math.RoundingMode;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.joda.time.DateTimeField;
-import org.joda.time.DateTimeFieldType;
-import org.joda.time.DateTimeZone;
-import org.joda.time.chrono.IslamicChronology;
-import org.joda.time.chrono.ZonedChronology;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class FieldUtils_ESTestTest1 extends FieldUtils_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link FieldUtils} class.
+ */
+public class FieldUtilsTest {
 
-    @Test(timeout = 4000)
-    public void test00() throws Throwable {
-        int int0 = FieldUtils.getWrappedValue((-2147483647), 0, (-1), 0);
-        assertEquals((-1), int0);
+    /**
+     * Tests that getWrappedValue correctly handles wrapping a large negative number.
+     * The wrapping logic should work correctly even for values near Integer.MIN_VALUE.
+     */
+    @Test
+    public void getWrappedValue_withLargeNegativeValue_wrapsIntoRangeCorrectly() {
+        // Arrange: Define the inputs for the test.
+        // We are testing the wrapping of a large negative number.
+        final int currentValue = -2147483647;
+        final int amountToAdd = 0;
+        final int minValue = -1;
+        final int maxValue = 0;
+        
+        // The expected result after wrapping the currentValue into the range [-1, 0] is -1.
+        final int expectedValue = -1;
+
+        // Act: Call the method under test.
+        int actualValue = FieldUtils.getWrappedValue(currentValue, amountToAdd, minValue, maxValue);
+
+        // Assert: Verify the result is as expected.
+        assertEquals(expectedValue, actualValue);
     }
 }
