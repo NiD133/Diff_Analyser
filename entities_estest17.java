0f3f83176177ba9c9cd240b7a706ede7f899a26a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:49:22 +0800

    gemini processedAutoTestCases entities_estest17.java

--- entities_estest17.java
+++ entities_estest17.java
@@ -1,36 +1,24 @@
 package org.jsoup.nodes;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.File;
-import java.io.PipedWriter;
-import java.io.StringWriter;
-import java.nio.BufferOverflowException;
-import java.nio.CharBuffer;
-import java.nio.ReadOnlyBufferException;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.io.MockFile;
-import org.evosuite.runtime.mock.java.io.MockFileWriter;
-import org.evosuite.runtime.mock.java.io.MockPrintStream;
-import org.jsoup.internal.QuietAppendable;
-import org.junit.runner.RunWith;
 
+/**
+ * Tests for the {@link Entities#codepointsForName(String, int[])} method.
+ */
 public class Entities_ESTestTest17 extends Entities_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test16() throws Throwable {
-        int[] intArray0 = new int[0];
-        // Undeclared exception!
-        try {
-            Entities.codepointsForName("amp", intArray0);
-            fail("Expecting exception: ArrayIndexOutOfBoundsException");
-        } catch (ArrayIndexOutOfBoundsException e) {
-            //
-            // 0
-            //
-            verifyException("org.jsoup.nodes.Entities", e);
-        }
+    /**
+     * Verifies that codepointsForName throws an ArrayIndexOutOfBoundsException
+     * when the provided output array is too small to hold the resulting codepoints.
+     */
+    @Test(timeout = 4000, expected = ArrayIndexOutOfBoundsException.class)
+    public void codepointsForNameThrowsExceptionWhenOutputArrayIsTooSmall() {
+        // Arrange: The entity "amp" has one codepoint, but the output array has a size of zero.
+        String entityName = "amp";
+        int[] codepointsOutput = new int[0];
+
+        // Act: Attempt to write the codepoint for "amp" into the undersized array.
+        // The method is expected to throw an ArrayIndexOutOfBoundsException.
+        Entities.codepointsForName(entityName, codepointsOutput);
     }
 }
