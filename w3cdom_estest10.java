a6439a59757e5bb0380dd59fca8ba0bb5d69c33c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:26:40 +0800

    Original processedAutoTestCases w3cdom_estest10.java

---
+++ w3cdom_estest10.java
@@ -0,0 +1,49 @@
+package org.jsoup.helper;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.sun.org.apache.xerces.internal.dom.DocumentImpl;
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import javax.imageio.metadata.IIOMetadataNode;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.jsoup.nodes.Attributes;
+import org.jsoup.nodes.DataNode;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.DocumentType;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.FormElement;
+import org.jsoup.nodes.XmlDeclaration;
+import org.jsoup.parser.Parser;
+import org.jsoup.parser.Tag;
+import org.junit.runner.RunWith;
+import org.w3c.dom.DOMException;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class W3CDom_ESTestTest10 extends W3CDom_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test09() throws Throwable {
+        W3CDom w3CDom0 = new W3CDom();
+        IIOMetadataNode iIOMetadataNode0 = new IIOMetadataNode();
+        iIOMetadataNode0.insertBefore(iIOMetadataNode0, iIOMetadataNode0);
+        Class<XmlDeclaration> class0 = XmlDeclaration.class;
+        // Undeclared exception!
+        try {
+            w3CDom0.sourceNodes((NodeList) iIOMetadataNode0, class0);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.jsoup.helper.W3CDom", e);
+        }
+    }
+}
