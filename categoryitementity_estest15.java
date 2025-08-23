dc276fb0b104e1b8448dab5ef3a5160edab788dc
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:41:46 +0800

    Original processedAutoTestCases categoryitementity_estest15.java

---
+++ categoryitementity_estest15.java
@@ -0,0 +1,36 @@
+package org.jfree.chart.entity;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.awt.Polygon;
+import java.awt.Rectangle;
+import java.awt.geom.Rectangle2D;
+import java.time.Clock;
+import java.time.LocalDate;
+import java.time.chrono.ChronoLocalDate;
+import java.time.chrono.JapaneseDate;
+import javax.swing.JLayeredPane;
+import javax.swing.text.DefaultCaret;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.time.MockClock;
+import org.evosuite.runtime.mock.java.time.MockLocalDate;
+import org.evosuite.runtime.mock.java.time.chrono.MockJapaneseDate;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.statistics.DefaultBoxAndWhiskerCategoryDataset;
+import org.jfree.data.statistics.DefaultMultiValueCategoryDataset;
+import org.jfree.data.statistics.DefaultStatisticalCategoryDataset;
+import org.junit.runner.RunWith;
+
+public class CategoryItemEntity_ESTestTest15 extends CategoryItemEntity_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test14() throws Throwable {
+        DefaultCaret defaultCaret0 = new DefaultCaret();
+        DefaultMultiValueCategoryDataset<Integer, Integer> defaultMultiValueCategoryDataset0 = new DefaultMultiValueCategoryDataset<Integer, Integer>();
+        CategoryItemEntity<Integer, Integer> categoryItemEntity0 = new CategoryItemEntity<Integer, Integer>(defaultCaret0, "", "", defaultMultiValueCategoryDataset0, (Integer) 0, (Integer) 0);
+        String string0 = categoryItemEntity0.toString();
+        assertNotNull(string0);
+    }
+}
