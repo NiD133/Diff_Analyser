fe3eca92a8035124971fa758d57b6bc62478128e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:38:04 +0800

    Original processedAutoTestCases atomicdoublearray_estest7.java

---
+++ atomicdoublearray_estest7.java
@@ -0,0 +1,25 @@
+package com.google.common.util.concurrent;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.function.DoubleBinaryOperator;
+import java.util.function.DoubleUnaryOperator;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class AtomicDoubleArray_ESTestTest7 extends AtomicDoubleArray_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test06() throws Throwable {
+        double[] doubleArray0 = new double[9];
+        doubleArray0[0] = (-1583.803774);
+        AtomicDoubleArray atomicDoubleArray0 = new AtomicDoubleArray(doubleArray0);
+        DoubleUnaryOperator doubleUnaryOperator0 = DoubleUnaryOperator.identity();
+        double double0 = atomicDoubleArray0.getAndUpdate(0, doubleUnaryOperator0);
+        assertEquals((-1583.803774), double0, 0.01);
+    }
+}
