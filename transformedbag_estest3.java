d79c3fc773fdd9553a4e87302d8ed1f39f220626
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:23:02 +0800

    gemini processedAutoTestCases transformedbag_estest3.java

--- transformedbag_estest3.java
+++ transformedbag_estest3.java
@@ -1,57 +1,37 @@
 package org.apache.commons.collections4.bag;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Set;
-import java.util.Vector;
 import org.apache.commons.collections4.Bag;
-import org.apache.commons.collections4.Factory;
-import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.SortedBag;
 import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AndPredicate;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ChainedTransformer;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantFactory;
 import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.FactoryTransformer;
-import org.apache.commons.collections4.functors.FalsePredicate;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.InstantiateFactory;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.MapTransformer;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NotPredicate;
-import org.apache.commons.collections4.functors.OnePredicate;
-import org.apache.commons.collections4.functors.SwitchTransformer;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class TransformedBag_ESTestTest3 extends TransformedBag_ESTest_scaffolding {
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-    @Test(timeout = 4000)
-    public void test02() throws Throwable {
-        HashBag<Integer> hashBag0 = new HashBag<Integer>();
-        Transformer<Object, Integer> transformer0 = ConstantTransformer.nullTransformer();
-        TreeBag<Object> treeBag0 = new TreeBag<Object>();
-        SynchronizedSortedBag<Object> synchronizedSortedBag0 = SynchronizedSortedBag.synchronizedSortedBag((SortedBag<Object>) treeBag0);
-        TransformedBag<Integer> transformedBag0 = new TransformedBag<Integer>(hashBag0, transformer0);
-        boolean boolean0 = transformedBag0.remove((Object) synchronizedSortedBag0, 0);
-        assertFalse(boolean0);
+/**
+ * Unit tests for {@link TransformedBag}.
+ */
+public class TransformedBagTest {
+
+    /**
+     * Tests that calling remove() with a count of zero does not modify the bag
+     * and returns false, as specified by the Bag interface contract.
+     */
+    @Test
+    public void removeWithZeroCopiesShouldReturnFalseAndNotModifyBag() {
+        // Arrange
+        final Bag<Integer> underlyingBag = new HashBag<>();
+        final Transformer<Object, Integer> transformer = ConstantTransformer.nullTransformer();
+        final Bag<Integer> transformedBag = new TransformedBag<>(underlyingBag, transformer);
+
+        final String objectToRemove = "any object";
+        final int copiesToRemove = 0;
+
+        // Act
+        final boolean wasModified = transformedBag.remove(objectToRemove, copiesToRemove);
+
+        // Assert
+        assertFalse("Removing zero copies of an element should return false.", wasModified);
+        assertTrue("The bag should not be modified when removing zero copies.", transformedBag.isEmpty());
     }
 }
