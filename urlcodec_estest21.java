6151f18e937f9c54759bd2415a5a69a175a28f9d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:24:51 +0800

    Original processedAutoTestCases urlcodec_estest21.java

---
+++ urlcodec_estest21.java
@@ -0,0 +1,24 @@
+package org.apache.commons.codec.net;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.UnsupportedEncodingException;
+import java.util.BitSet;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class URLCodec_ESTestTest21 extends URLCodec_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test20() throws Throwable {
+        URLCodec uRLCodec0 = new URLCodec();
+        // Undeclared exception!
+        try {
+            uRLCodec0.encode("org.apache.commons.codec.net.URLCodec", (String) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+        }
+    }
+}
