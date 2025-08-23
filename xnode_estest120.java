cd61cb11f5abb711ac48032be4651f7fa4b27dfe
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:28:41 +0800

    gemini processedAutoTestCases xnode_estest120.java

--- xnode_estest120.java
+++ xnode_estest120.java
@@ -1,31 +1,34 @@
 package org.apache.ibatis.parsing;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.List;
-import java.util.Locale;
+import static org.junit.Assert.assertNull;
+
 import java.util.Properties;
-import java.util.function.Supplier;
 import javax.imageio.metadata.IIOMetadataNode;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
-import org.xml.sax.ext.DefaultHandler2;
 
-public class XNode_ESTestTest120 extends XNode_ESTest_scaffolding {
+/**
+ * Contains tests for the {@link XNode} class.
+ */
+public class XNodeTest {
 
-    @Test(timeout = 4000)
-    public void test119() throws Throwable {
-        IIOMetadataNode iIOMetadataNode0 = new IIOMetadataNode();
-        XPathParser xPathParser0 = new XPathParser((Document) null, false);
-        Properties properties0 = new Properties();
-        XNode xNode0 = new XNode(xPathParser0, iIOMetadataNode0, properties0);
-        Double double0 = xNode0.getDoubleAttribute("name");
-        assertNull(double0);
+    @Test
+    public void getDoubleAttributeShouldReturnNullWhenAttributeIsMissing() {
+        // Arrange
+        // Create a DOM Node that has no attributes. IIOMetadataNode is a convenient
+        // concrete implementation of org.w3c.dom.Node for this purpose.
+        Node nodeWithoutAttributes = new IIOMetadataNode();
+        XPathParser parser = new XPathParser((Document) null, false);
+        Properties variables = new Properties();
+        XNode xNode = new XNode(parser, nodeWithoutAttributes, variables);
+
+        // Act
+        // Attempt to retrieve a non-existent attribute as a Double.
+        Double attributeValue = xNode.getDoubleAttribute("nonExistentAttribute");
+
+        // Assert
+        // The result should be null, as the attribute is not defined on the node.
+        assertNull(attributeValue);
     }
 }
