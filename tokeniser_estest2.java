dd40ad45318d0a2dc28dc63d1762027c493da60b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:22:06 +0800

    Original processedAutoTestCases tokeniser_estest2.java

---
+++ tokeniser_estest2.java
@@ -0,0 +1,25 @@
+package org.jsoup.parser;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.StringReader;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.jsoup.nodes.Comment;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.XmlDeclaration;
+import org.junit.runner.RunWith;
+
+public class Tokeniser_ESTestTest2 extends Tokeniser_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test01() throws Throwable {
+        XmlTreeBuilder xmlTreeBuilder0 = new XmlTreeBuilder();
+        xmlTreeBuilder0.parse("{O_&7vi9I", "{O_&7vi9I");
+        Tokeniser tokeniser0 = new Tokeniser(xmlTreeBuilder0);
+        TokeniserState tokeniserState0 = TokeniserState.ScriptDataEscapedLessthanSign;
+        tokeniser0.eofError(tokeniserState0);
+    }
+}
