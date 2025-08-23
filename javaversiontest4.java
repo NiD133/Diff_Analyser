1fcc07722270781a3ed3ae076f43f4af2cd99a05
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:29:56 +0800

    gemini processedManualTestCases javaversiontest4.java

--- javaversiontest4.java
+++ javaversiontest4.java
@@ -3,16 +3,26 @@ package com.google.gson.internal;
 import static com.google.common.truth.Truth.assertThat;
 import org.junit.Test;
 
-public class JavaVersionTestTest4 {
+/**
+ * Tests for {@link JavaVersion#parseMajorJavaVersion(String)}.
+ */
+public class JavaVersionTest {
 
     @Test
-    public void testJava8() {
+    public void parseMajorVersion_forJava8_returns8() {
+        // Test standard "1.8" formats
         assertThat(JavaVersion.parseMajorJavaVersion("1.8")).isEqualTo(8);
         assertThat(JavaVersion.parseMajorJavaVersion("1.8.0")).isEqualTo(8);
+
+        // Test format with update version
         assertThat(JavaVersion.parseMajorJavaVersion("1.8.0_131")).isEqualTo(8);
+
+        // Test formats with suffixes, e.g., for early access or internal builds
         assertThat(JavaVersion.parseMajorJavaVersion("1.8.0_60-ea")).isEqualTo(8);
         assertThat(JavaVersion.parseMajorJavaVersion("1.8.0_111-internal")).isEqualTo(8);
-        // openjdk8 per https://github.com/AdoptOpenJDK/openjdk-build/issues/93
+
+        // Test specific OpenJDK formats
+        // See: https://github.com/AdoptOpenJDK/openjdk-build/issues/93
         assertThat(JavaVersion.parseMajorJavaVersion("1.8.0-internal")).isEqualTo(8);
         assertThat(JavaVersion.parseMajorJavaVersion("1.8.0_131-adoptopenjdk")).isEqualTo(8);
     }
