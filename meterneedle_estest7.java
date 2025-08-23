a1767f0dba5ed44a9e1904ef9aa4d3a79226a122
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:03:01 +0800

    Original processedAutoTestCases meterneedle_estest7.java

---
+++ meterneedle_estest7.java
@@ -0,0 +1,33 @@
+package org.jfree.chart.plot.compass;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Paint;
+import java.awt.Rectangle;
+import java.awt.Stroke;
+import java.awt.SystemColor;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.text.AttributedCharacterIterator;
+import javax.swing.JScrollPane;
+import javax.swing.text.DefaultCaret;
+import javax.swing.tree.DefaultTreeCellRenderer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class MeterNeedle_ESTestTest7 extends MeterNeedle_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test06() throws Throwable {
+        ShipNeedle shipNeedle0 = new ShipNeedle();
+        shipNeedle0.setSize((-1083));
+        int int0 = shipNeedle0.getSize();
+        assertEquals((-1083), int0);
+    }
+}
