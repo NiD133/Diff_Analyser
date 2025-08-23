cdd7d0817122b3ab973db52f94bd5020c868ed7c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:35:09 +0800

    Original processedManualTestCases typeadaptertest6.java

---
+++ typeadaptertest6.java
@@ -0,0 +1,49 @@
+package com.google.gson;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertThrows;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+import java.io.IOException;
+import java.io.StringReader;
+import org.junit.Test;
+
+public class TypeAdapterTestTest6 {
+
+    private static final TypeAdapter<String> assertionErrorAdapter = new TypeAdapter<>() {
+
+        @Override
+        public void write(JsonWriter out, String value) {
+            throw new AssertionError("unexpected call");
+        }
+
+        @Override
+        public String read(JsonReader in) {
+            throw new AssertionError("unexpected call");
+        }
+
+        @Override
+        public String toString() {
+            return "assertionErrorAdapter";
+        }
+    };
+
+    private static final TypeAdapter<String> adapter = new TypeAdapter<>() {
+
+        @Override
+        public void write(JsonWriter out, String value) throws IOException {
+            out.value(value);
+        }
+
+        @Override
+        public String read(JsonReader in) throws IOException {
+            return in.nextString();
+        }
+    };
+
+    // whether that behavior is actually desired
+    @Test
+    public void testFromJson_String_TrailingData() throws IOException {
+        assertThat(adapter.fromJson("\"a\"1")).isEqualTo("a");
+    }
+}
