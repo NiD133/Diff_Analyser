3010f7f86de072bde95e505da004cf35181fc4e6
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:12:36 +0800

    Original processedAutoTestCases rfc1522codec_estest11.java

---
+++ rfc1522codec_estest11.java
@@ -0,0 +1,27 @@
+package org.apache.commons.codec.net;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.IllegalCharsetNameException;
+import java.nio.charset.UnsupportedCharsetException;
+import org.apache.commons.codec.CodecPolicy;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class RFC1522Codec_ESTestTest11 extends RFC1522Codec_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test10() throws Throwable {
+        BCodec bCodec0 = new BCodec();
+        // Undeclared exception!
+        try {
+            bCodec0.decodeText("=?=");
+            fail("Expecting exception: StringIndexOutOfBoundsException");
+        } catch (StringIndexOutOfBoundsException e) {
+        }
+    }
+}
