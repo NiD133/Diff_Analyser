fe235766b86891cdee81c063273594207ab6265d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:01:29 +0800

    gemini processedAutoTestCases logfactory_estest7.java

--- logfactory_estest7.java
+++ logfactory_estest7.java
@@ -1,26 +1,37 @@
 package org.apache.ibatis.logging;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.apache.ibatis.logging.commons.JakartaCommonsLoggingImpl;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class LogFactory_ESTestTest7 extends LogFactory_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
-    @Test(timeout = 4000)
-    public void test06() throws Throwable {
-        // Undeclared exception!
+/**
+ * Test suite for the {@link LogFactory} class.
+ * Note: This class name is retained from the original, but a name like 'LogFactoryTest' would be more conventional.
+ */
+public class LogFactory_ESTestTest7 {
+
+    /**
+     * Verifies that calling getLog() with a null logger name throws a RuntimeException.
+     *
+     * The LogFactory is expected to wrap the underlying constructor failure (an InvocationTargetException)
+     * in a descriptive RuntimeException.
+     */
+    @Test
+    public void shouldThrowRuntimeExceptionWhenLoggerNameIsNull() {
+        // Arrange
+        String expectedErrorMessage = "Error creating logger for logger null.  Cause: java.lang.reflect.InvocationTargetException";
+
         try {
+            // Act
             LogFactory.getLog((String) null);
-            fail("Expecting exception: RuntimeException");
+
+            // Assert: Fail the test if the expected exception was not thrown.
+            fail("A RuntimeException should have been thrown for a null logger name.");
+
         } catch (RuntimeException e) {
-            //
-            // Error creating logger for logger null.  Cause: java.lang.reflect.InvocationTargetException
-            //
-            verifyException("org.apache.ibatis.logging.LogFactory", e);
+            // Assert: Verify the exception is of the expected type and has the correct message.
+            assertEquals(expectedErrorMessage, e.getMessage());
         }
     }
 }
