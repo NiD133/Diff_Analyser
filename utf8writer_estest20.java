bb61a9a5daae90a76692dd423ca1888fef91de94
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:25:36 +0800

    Original processedAutoTestCases utf8writer_estest20.java

---
+++ utf8writer_estest20.java
@@ -0,0 +1,39 @@
+package com.fasterxml.jackson.core.io;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.fasterxml.jackson.core.ErrorReportConfiguration;
+import com.fasterxml.jackson.core.StreamReadConstraints;
+import com.fasterxml.jackson.core.StreamWriteConstraints;
+import com.fasterxml.jackson.core.util.BufferRecycler;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PipedOutputStream;
+import java.io.Writer;
+import java.nio.CharBuffer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockFileOutputStream;
+import org.evosuite.runtime.mock.java.io.MockPrintStream;
+import org.junit.runner.RunWith;
+
+public class UTF8Writer_ESTestTest20 extends UTF8Writer_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test19() throws Throwable {
+        UTF8Writer uTF8Writer0 = null;
+        try {
+            uTF8Writer0 = new UTF8Writer((IOContext) null, (OutputStream) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("com.fasterxml.jackson.core.io.UTF8Writer", e);
+        }
+    }
+}
