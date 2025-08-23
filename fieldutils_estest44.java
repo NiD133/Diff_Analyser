ec3d9170eedab192513de990e7c51bb037bad31b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:50:24 +0800

    gemini processedAutoTestCases fieldutils_estest44.java

--- fieldutils_estest44.java
+++ fieldutils_estest44.java
@@ -1,23 +1,33 @@
 package org.joda.time.field;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.math.RoundingMode;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.joda.time.DateTimeField;
-import org.joda.time.DateTimeFieldType;
-import org.joda.time.DateTimeZone;
-import org.joda.time.chrono.IslamicChronology;
-import org.joda.time.chrono.ZonedChronology;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class FieldUtils_ESTestTest44 extends FieldUtils_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link FieldUtils} class.
+ */
+public class FieldUtilsTest {
 
-    @Test(timeout = 4000)
-    public void test43() throws Throwable {
-        int int0 = FieldUtils.getWrappedValue(Integer.MIN_VALUE, 0, 1);
-        assertEquals(0, int0);
+    /**
+     * Tests that getWrappedValue correctly handles the edge case of Integer.MIN_VALUE.
+     *
+     * The value Integer.MIN_VALUE is a special case in integer arithmetic because its
+     * absolute value cannot be represented as a positive integer. This test verifies
+     * that when wrapping this value within a range of [0, 1], it correctly maps to 0,
+     * as Integer.MIN_VALUE is an even number.
+     */
+    @Test
+    public void getWrappedValue_withIntegerMinValue_shouldWrapCorrectly() {
+        // Arrange
+        int valueToWrap = Integer.MIN_VALUE;
+        int minValue = 0;
+        int maxValue = 1;
+        int expectedValue = 0; // Since MIN_VALUE is even, it should wrap to the start of the [0, 1] range.
+
+        // Act
+        int wrappedValue = FieldUtils.getWrappedValue(valueToWrap, minValue, maxValue);
+
+        // Assert
+        assertEquals(expectedValue, wrappedValue);
     }
 }
