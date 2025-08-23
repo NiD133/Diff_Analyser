417d0cb6d71cd9f0d0fcd83855ebb634681ff3df
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:17:58 +0800

    Original processedAutoTestCases standardentitycollection_estest9.java

---
+++ standardentitycollection_estest9.java
@@ -0,0 +1,25 @@
+package org.jfree.chart.entity;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.geom.Line2D;
+import java.util.Collection;
+import java.util.Iterator;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class StandardEntityCollection_ESTestTest9 extends StandardEntityCollection_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test08() throws Throwable {
+        StandardEntityCollection standardEntityCollection0 = new StandardEntityCollection();
+        StandardEntityCollection standardEntityCollection1 = new StandardEntityCollection();
+        boolean boolean0 = standardEntityCollection0.equals(standardEntityCollection1);
+        assertTrue(boolean0);
+    }
+}
