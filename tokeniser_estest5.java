8d4df25d87a407786bb14c38d8748d1a00429f70
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:22:15 +0800

    gemini processedAutoTestCases tokeniser_estest5.java

--- tokeniser_estest5.java
+++ tokeniser_estest5.java
@@ -2,24 +2,25 @@ package org.jsoup.parser;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.StringReader;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.jsoup.nodes.Comment;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.nodes.XmlDeclaration;
-import org.junit.runner.RunWith;
 
-public class Tokeniser_ESTestTest5 extends Tokeniser_ESTest_scaffolding {
+/**
+ * Tests for the {@link Tokeniser} class, focusing on tag creation.
+ */
+public class TokeniserTest {
 
-    @Test(timeout = 4000)
-    public void test04() throws Throwable {
-        XmlTreeBuilder xmlTreeBuilder0 = new XmlTreeBuilder();
-        xmlTreeBuilder0.parse("{_&vi9I", "{_&vi9I");
-        Tokeniser tokeniser0 = new Tokeniser(xmlTreeBuilder0);
-        Token.Tag token_Tag0 = tokeniser0.createTagPending(false);
-        assertNotNull(token_Tag0);
+    @Test
+    public void createTagPendingWithFalseArgumentShouldCreateAnEndTag() {
+        // Arrange: A Tokeniser requires a TreeBuilder for its construction.
+        // For this test, an XmlTreeBuilder is used, but no input needs to be parsed.
+        XmlTreeBuilder xmlTreeBuilder = new XmlTreeBuilder();
+        Tokeniser tokeniser = new Tokeniser(xmlTreeBuilder);
+
+        // Act: Call createTagPending with 'false' to request an end tag.
+        Token.Tag pendingTag = tokeniser.createTagPending(false);
+
+        // Assert: Verify that a non-null end tag was created.
+        assertNotNull("The created pending tag should not be null.", pendingTag);
+        assertTrue("The tag should be an end tag.", pendingTag.isEndTag());
+        assertFalse("The tag should not be a start tag.", pendingTag.isStartTag());
     }
 }
