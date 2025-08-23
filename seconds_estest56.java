c77b253feec5f52ee0b62859ecb1604a59389baf
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:13:08 +0800

    Original processedAutoTestCases seconds_estest56.java

---
+++ seconds_estest56.java
@@ -0,0 +1,17 @@
+package org.joda.time;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class Seconds_ESTestTest56 extends Seconds_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test55() throws Throwable {
+        Seconds seconds0 = Seconds.seconds(3);
+        assertEquals(3, seconds0.getSeconds());
+    }
+}
