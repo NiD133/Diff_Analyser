a36cfdad0fd82e5e860bcb01325f334dd4ae4f2c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:47:43 +0800

    gemini processedAutoTestCases defaultpiedataset_estest30.java

--- defaultpiedataset_estest30.java
+++ defaultpiedataset_estest30.java
@@ -2,37 +2,28 @@ package org.jfree.data.general;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.time.chrono.ChronoLocalDate;
-import java.util.List;
-import java.util.Vector;
-import javax.swing.JLayeredPane;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.jfree.chart.api.SortOrder;
-import org.jfree.chart.api.TableOrder;
-import org.jfree.data.DefaultKeyedValues;
-import org.jfree.data.KeyedValues;
-import org.jfree.data.category.CategoryDataset;
-import org.jfree.data.category.CategoryToPieDataset;
-import org.jfree.data.category.DefaultIntervalCategoryDataset;
-import org.jfree.data.category.SlidingCategoryDataset;
-import org.jfree.data.statistics.DefaultBoxAndWhiskerCategoryDataset;
-import org.junit.runner.RunWith;
 
-public class DefaultPieDataset_ESTestTest30 extends DefaultPieDataset_ESTest_scaffolding {
+/**
+ * Tests for the equals() method in the DefaultPieDataset class.
+ */
+public class DefaultPieDataset_ESTestTest30 {
 
-    @Test(timeout = 4000)
-    public void test29() throws Throwable {
-        DefaultPieDataset<Integer> defaultPieDataset0 = new DefaultPieDataset<Integer>();
-        Integer integer0 = JLayeredPane.DRAG_LAYER;
-        defaultPieDataset0.setValue(integer0, (Number) null);
-        DefaultPieDataset<Integer> defaultPieDataset1 = new DefaultPieDataset<Integer>(defaultPieDataset0);
-        Integer integer1 = JLayeredPane.POPUP_LAYER;
-        defaultPieDataset1.clear();
-        defaultPieDataset1.setValue(integer1, (Number) integer0);
-        boolean boolean0 = defaultPieDataset0.equals(defaultPieDataset1);
-        assertEquals(1, defaultPieDataset1.getItemCount());
-        assertFalse(boolean0);
+    /**
+     * Verifies that the equals() method returns false when comparing two datasets
+     * that contain different keys and values.
+     */
+    @Test
+    public void equals_shouldReturnFalse_forDatasetsWithDifferentContent() {
+        // Arrange: Create two distinct datasets.
+        // The first dataset contains a key with a null value.
+        DefaultPieDataset<Integer> dataset1 = new DefaultPieDataset<>();
+        dataset1.setValue(100, null);
+
+        // The second dataset contains a different key and a non-null value.
+        DefaultPieDataset<Integer> dataset2 = new DefaultPieDataset<>();
+        dataset2.setValue(200, 100);
+
+        // Act & Assert: The two datasets have different content and should not be equal.
+        assertNotEquals("Datasets with different key-value pairs should not be equal", dataset1, dataset2);
     }
 }
