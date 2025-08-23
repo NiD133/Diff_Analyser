f92c01817fab51bc8228eed25208731985a382ae
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:16:09 +0800

    Original processedAutoTestCases simplebloomfilter_estest28.java

---
+++ simplebloomfilter_estest28.java
@@ -0,0 +1,31 @@
+package org.apache.commons.collections4.bloomfilter;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.function.IntPredicate;
+import java.util.function.LongPredicate;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class SimpleBloomFilter_ESTestTest28 extends SimpleBloomFilter_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test27() throws Throwable {
+        Shape shape0 = Shape.fromKM(1, 1);
+        SimpleBloomFilter simpleBloomFilter0 = new SimpleBloomFilter(shape0);
+        // Undeclared exception!
+        try {
+            simpleBloomFilter0.merge((BitMapExtractor) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // bitMapExtractor
+            //
+            verifyException("java.util.Objects", e);
+        }
+    }
+}
