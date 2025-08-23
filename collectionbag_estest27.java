10f41cc972a55206c6846c87f50a2dcf3469b0a3
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:44:11 +0800

    gemini processedAutoTestCases collectionbag_estest27.java

--- collectionbag_estest27.java
+++ collectionbag_estest27.java
@@ -1,51 +1,29 @@
 package org.apache.commons.collections4.bag;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.ConcurrentModificationException;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Stack;
-import java.util.TreeSet;
 import org.apache.commons.collections4.Bag;
-import org.apache.commons.collections4.Predicate;
 import org.apache.commons.collections4.SortedBag;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.FalsePredicate;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.MapTransformer;
-import org.apache.commons.collections4.functors.NullIsExceptionPredicate;
-import org.apache.commons.collections4.functors.TransformerPredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
+/**
+ * Tests the behavior of {@link CollectionBag} when it decorates an unmodifiable bag.
+ */
+// The original test extended a scaffolding class, which is preserved here.
 public class CollectionBag_ESTestTest27 extends CollectionBag_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test26() throws Throwable {
-        TreeBag<Integer> treeBag0 = new TreeBag<Integer>();
-        Integer integer0 = new Integer((-300));
-        SortedBag<Integer> sortedBag0 = UnmodifiableSortedBag.unmodifiableSortedBag((SortedBag<Integer>) treeBag0);
-        CollectionBag<Integer> collectionBag0 = new CollectionBag<Integer>(sortedBag0);
-        // Undeclared exception!
-        try {
-            collectionBag0.add(integer0);
-            fail("Expecting exception: UnsupportedOperationException");
-        } catch (UnsupportedOperationException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.commons.collections4.bag.UnmodifiableSortedBag", e);
-        }
+    /**
+     * Verifies that add() throws an UnsupportedOperationException when the CollectionBag
+     * is backed by an unmodifiable bag.
+     */
+    @Test(expected = UnsupportedOperationException.class)
+    public void addShouldThrowExceptionWhenDecoratingUnmodifiableBag() {
+        // Arrange: Create a CollectionBag that wraps an unmodifiable bag.
+        final SortedBag<Integer> underlyingBag = new TreeBag<>();
+        final SortedBag<Integer> unmodifiableBag = UnmodifiableSortedBag.unmodifiableSortedBag(underlyingBag);
+        final Bag<Integer> collectionBag = new CollectionBag<>(unmodifiableBag);
+
+        // Act: Attempt to add an element to the decorated bag.
+        // This action is expected to throw an UnsupportedOperationException,
+        // which is handled by the @Test(expected=...) annotation.
+        collectionBag.add(-300);
     }
 }
