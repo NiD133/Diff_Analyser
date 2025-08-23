294489ea24ed7a71f6e288649712f2061b4995f7
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:36:07 +0800

    Original processedManualTestCases xmltreebuildertest11.java

---
+++ xmltreebuildertest11.java
@@ -0,0 +1,34 @@
+package org.jsoup.parser;
+
+import org.jsoup.Jsoup;
+import org.jsoup.TextUtil;
+import org.jsoup.nodes.*;
+import org.jsoup.select.Elements;
+import org.junit.jupiter.api.Test;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import static org.jsoup.nodes.Document.OutputSettings.Syntax;
+import static org.jsoup.parser.Parser.NamespaceHtml;
+import static org.jsoup.parser.Parser.NamespaceXml;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class XmlTreeBuilderTestTest11 {
+
+    private static void assertXmlNamespace(Element el) {
+        assertEquals(NamespaceXml, el.tag().namespace(), String.format("Element %s not in XML namespace", el.tagName()));
+    }
+
+    @Test
+    public void testDetectCharsetEncodingDeclaration() throws IOException, URISyntaxException {
+        File xmlFile = new File(XmlTreeBuilder.class.getResource("/htmltests/xml-charset.xml").toURI());
+        InputStream inStream = new FileInputStream(xmlFile);
+        Document doc = Jsoup.parse(inStream, null, "http://example.com/", Parser.xmlParser());
+        assertEquals("ISO-8859-1", doc.charset().name());
+        assertEquals("<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?><data>äöåéü</data>", TextUtil.stripNewlines(doc.html()));
+    }
+}
