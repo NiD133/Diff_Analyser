9917bd074491fa4ff7b34bba4a4df0df92ac1959
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:36:59 +0800

    Original processedAutoTestCases abstractstreaminghasher_estest3.java

---
+++ abstractstreaminghasher_estest3.java
@@ -0,0 +1,30 @@
+package com.google.common.hash;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.nio.BufferOverflowException;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class AbstractStreamingHasher_ESTestTest3 extends AbstractStreamingHasher_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test02() throws Throwable {
+        Crc32cHashFunction.Crc32cHasher crc32cHashFunction_Crc32cHasher0 = new Crc32cHashFunction.Crc32cHasher();
+        crc32cHashFunction_Crc32cHasher0.hash();
+        // Undeclared exception!
+        try {
+            crc32cHashFunction_Crc32cHasher0.putShort((short) 24366);
+            fail("Expecting exception: BufferOverflowException");
+        } catch (BufferOverflowException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("java.nio.Buffer", e);
+        }
+    }
+}
