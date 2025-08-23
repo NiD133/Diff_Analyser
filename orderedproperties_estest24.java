9e648be470301d76cffb41e5ca273c1d03cf14d1
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:06:46 +0800

    gemini processedAutoTestCases orderedproperties_estest24.java

--- orderedproperties_estest24.java
+++ orderedproperties_estest24.java
@@ -1,53 +1,52 @@
 package org.apache.commons.collections4.properties;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.Reader;
-import java.io.StringReader;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.LinkedHashSet;
+
 import java.util.Map;
-import java.util.NoSuchElementException;
 import java.util.Set;
-import java.util.function.BiConsumer;
-import java.util.function.BiFunction;
 import java.util.function.Function;
-import org.apache.commons.collections4.Equator;
-import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AllPredicate;
-import org.apache.commons.collections4.functors.CloneTransformer;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.DefaultEquator;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.NOPTransformer;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NotNullPredicate;
-import org.apache.commons.collections4.functors.NullIsTruePredicate;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.SwitchTransformer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
 
-public class OrderedProperties_ESTestTest24 extends OrderedProperties_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
 
-    @Test(timeout = 4000)
-    public void test23() throws Throwable {
-        OrderedProperties orderedProperties0 = new OrderedProperties();
-        Set<Map.Entry<Object, Object>> set0 = (Set<Map.Entry<Object, Object>>) orderedProperties0.entrySet();
-        Transformer<Object, Object> transformer0 = NOPTransformer.nopTransformer();
-        LinkedHashSet linkedHashSet0 = (LinkedHashSet) orderedProperties0.computeIfAbsent(set0, transformer0);
-        assertEquals(0, linkedHashSet0.size());
+/**
+ * Tests for the {@code computeIfAbsent} method in {@link OrderedProperties}.
+ */
+public class OrderedPropertiesTest {
+
+    /**
+     * Tests that computeIfAbsent correctly adds a new key-value pair and returns
+     * the new value when the key is not already present in the properties.
+     */
+    @Test
+    public void computeIfAbsentShouldAddAndReturnValueWhenKeyIsMissing() {
+        // Arrange
+        final OrderedProperties properties = new OrderedProperties();
+        
+        // For this test, we use the properties' own (initially empty) entry set as a key.
+        // This is an unusual but valid object to use as a key.
+        final Set<Map.Entry<Object, Object>> missingKey = properties.entrySet();
+        assertTrue("Precondition: The key (the entry set) should be empty", missingKey.isEmpty());
+        
+        // The mapping function will simply return its input (an identity function).
+        // This means the value associated with the key will be the key itself.
+        final Function<Object, Object> identityFunction = key -> key;
+
+        // Act
+        // Since the key is not present, the mapping function is called.
+        // The result of the function is then put into the map and returned.
+        final Object computedValue = properties.computeIfAbsent(missingKey, identityFunction);
+
+        // Assert
+        // 1. Verify the returned value is correct.
+        // The NOPTransformer in the original test is an identity function, so the returned
+        // value should be the same instance as the key.
+        assertSame("The returned value should be the same instance as the key", missingKey, computedValue);
+
+        // 2. Verify the state of the properties map has been updated correctly.
+        assertEquals("Properties should now contain one entry", 1, properties.size());
+        assertTrue("The key should now be present in the properties", properties.containsKey(missingKey));
+        assertSame("The value for the new key should be the key itself", missingKey, properties.get(missingKey));
     }
 }
