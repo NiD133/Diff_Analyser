11432b1851975157bd6b5356941720f9b3214e17
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:14:31 +0800

    gemini processedAutoTestCases sequencereader_estest3.java

--- sequencereader_estest3.java
+++ sequencereader_estest3.java
@@ -1,36 +1,33 @@
 package org.apache.commons.io.input;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
+
 import java.io.IOException;
-import java.io.PipedReader;
 import java.io.Reader;
-import java.io.StringReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.ConcurrentModificationException;
-import java.util.List;
-import java.util.Vector;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import java.util.Collections;
 
-public class SequenceReader_ESTestTest3 extends SequenceReader_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link SequenceReader} class, focusing on input validation.
+ */
+public class SequenceReaderTest {
 
-    @Test(timeout = 4000)
-    public void test02() throws Throwable {
-        ArrayDeque<StringReader> arrayDeque0 = new ArrayDeque<StringReader>();
-        SequenceReader sequenceReader0 = new SequenceReader(arrayDeque0);
-        // Undeclared exception!
-        try {
-            sequenceReader0.read((char[]) null, (-1), (-1));
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // cbuf
-            //
-            verifyException("java.util.Objects", e);
-        }
+    /**
+     * Tests that read(char[], int, int) throws a NullPointerException when the
+     * provided character buffer is null. This behavior is part of the general
+     * contract of the {@link java.io.Reader} class.
+     */
+    @Test(expected = NullPointerException.class)
+    public void testReadWithNullBufferShouldThrowNullPointerException() throws IOException {
+        // Arrange: Create a SequenceReader. The source of the reader is not important
+        // for this test, so we use an empty collection.
+        final SequenceReader sequenceReader = new SequenceReader(Collections.<Reader>emptyList());
+
+        // Act: Attempt to read into a null buffer. The test is expected to throw
+        // a NullPointerException at this point. The offset and length values are
+        // irrelevant as the null check on the buffer should occur first.
+        sequenceReader.read(null, 0, 0);
+
+        // Assert: The test will pass if a NullPointerException is thrown, as
+        // specified by the @Test(expected) annotation.
     }
 }
