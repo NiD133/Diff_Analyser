edc918ecf2351bcbe96f4012eaccdfd32fe597d3
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:08:01 +0800

    Original processedAutoTestCases pdfdictionary_estest48.java

---
+++ pdfdictionary_estest48.java
@@ -0,0 +1,35 @@
+package com.itextpdf.text.pdf;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.collection.PdfCollectionField;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PipedOutputStream;
+import java.util.LinkedHashMap;
+import java.util.Set;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockPrintStream;
+import org.junit.runner.RunWith;
+
+public class PdfDictionary_ESTestTest48 extends PdfDictionary_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test47() throws Throwable {
+        PdfFileSpecification pdfFileSpecification0 = new PdfFileSpecification();
+        // Undeclared exception!
+        try {
+            pdfFileSpecification0.put((PdfName) null, (PdfObject) null);
+            fail("Expecting exception: IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //
+            // key is null.
+            //
+            verifyException("com.itextpdf.text.pdf.PdfDictionary", e);
+        }
+    }
+}
