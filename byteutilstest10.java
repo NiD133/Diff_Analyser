4442f98d47aa0d664058c1aab4c3e25ed620378a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:26:05 +0800

    Original processedManualTestCases byteutilstest10.java

---
+++ byteutilstest10.java
@@ -0,0 +1,28 @@
+package org.apache.commons.compress.utils;
+
+import static org.apache.commons.compress.utils.ByteUtils.fromLittleEndian;
+import static org.apache.commons.compress.utils.ByteUtils.toLittleEndian;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInput;
+import java.io.DataInputStream;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.util.Arrays;
+import org.apache.commons.compress.utils.ByteUtils.InputStreamByteSupplier;
+import org.apache.commons.compress.utils.ByteUtils.OutputStreamByteConsumer;
+import org.junit.jupiter.api.Test;
+
+public class ByteUtilsTestTest10 {
+
+    @Test
+    void testFromLittleEndianFromDataInputUnsignedInt32() throws IOException {
+        final DataInput din = new DataInputStream(new ByteArrayInputStream(new byte[] { 2, 3, 4, (byte) 128 }));
+        assertEquals(2 + 3 * 256 + 4 * 256 * 256 + 128L * 256 * 256 * 256, fromLittleEndian(din, 4));
+    }
+}
