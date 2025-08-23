4b814e80d328408504a22516f082b2a6b5c35680
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:08:19 +0800

    gemini processedAutoTestCases percentcodec_estest14.java

--- percentcodec_estest14.java
+++ percentcodec_estest14.java
@@ -1,25 +1,36 @@
 package org.apache.commons.codec.net;
 
+import org.apache.commons.codec.DecoderException;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class PercentCodec_ESTestTest14 extends PercentCodec_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
-    @Test(timeout = 4000)
-    public void test13() throws Throwable {
-        PercentCodec percentCodec0 = new PercentCodec();
+/**
+ * Contains tests for the {@link PercentCodec} class.
+ * This class focuses on improving a specific auto-generated test case.
+ */
+public class PercentCodecTest {
+
+    /**
+     * Verifies that the decode(Object) method throws a DecoderException when passed an object
+     * that is not a byte array, as the method is only designed to handle byte arrays.
+     */
+    @Test
+    public void decodeWithUnsupportedObjectTypeShouldThrowDecoderException() {
+        // Arrange: Create a codec and an input object of an unsupported type.
+        // In this case, we use the codec instance itself as the invalid input.
+        final PercentCodec percentCodec = new PercentCodec();
+        final Object invalidInput = percentCodec;
+        final String expectedMessage = "Objects of type " + invalidInput.getClass().getName() + " cannot be Percent decoded";
+
+        // Act & Assert
         try {
-            percentCodec0.decode((Object) percentCodec0);
-            fail("Expecting exception: Exception");
-        } catch (Exception e) {
-            //
-            // Objects of type org.apache.commons.codec.net.PercentCodec cannot be Percent decoded
-            //
-            verifyException("org.apache.commons.codec.net.PercentCodec", e);
+            percentCodec.decode(invalidInput);
+            fail("Expected a DecoderException to be thrown for an unsupported object type.");
+        } catch (final DecoderException e) {
+            // Verify that the correct exception was thrown with the expected message.
+            assertEquals(expectedMessage, e.getMessage());
         }
     }
 }
