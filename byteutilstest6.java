96e6ba9fc068c59aa9fe55e9dc142df8c2d8e342
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:26:10 +0800

    gemini processedManualTestCases byteutilstest6.java

--- byteutilstest6.java
+++ byteutilstest6.java
@@ -1,28 +1,34 @@
 package org.apache.commons.compress.utils;
 
 import static org.apache.commons.compress.utils.ByteUtils.fromLittleEndian;
-import static org.apache.commons.compress.utils.ByteUtils.toLittleEndian;
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.DataInput;
-import java.io.DataInputStream;
-import java.io.DataOutput;
-import java.io.DataOutputStream;
-import java.io.EOFException;
-import java.io.IOException;
-import java.util.Arrays;
-import org.apache.commons.compress.utils.ByteUtils.InputStreamByteSupplier;
-import org.apache.commons.compress.utils.ByteUtils.OutputStreamByteConsumer;
+
 import org.junit.jupiter.api.Test;
 
-public class ByteUtilsTestTest6 {
+/**
+ * Tests for {@link ByteUtils}.
+ */
+class ByteUtilsTest {
 
     @Test
-    void testFromLittleEndianFromArrayUnsignedInt32() {
-        final byte[] b = { 1, 2, 3, 4, (byte) 128 };
-        assertEquals(2 + 3 * 256 + 4 * 256 * 256 + 128L * 256 * 256 * 256, fromLittleEndian(b, 1, 4));
+    void fromLittleEndianWithOffsetShouldReadUnsigned32BitValue() {
+        // Arrange
+        // The relevant bytes {0x02, 0x03, 0x04, 0x80} are used to form the little-endian value.
+        // The first byte (0x01) is ignored due to the offset, testing the offset functionality.
+        final byte[] sourceBytes = {0x01, 0x02, 0x03, 0x04, (byte) 0x80};
+        final int offset = 1;
+        final int length = 4;
+
+        // The little-endian bytes {0x02, 0x03, 0x04, 0x80} represent the number 0x80040302.
+        // This value (2,147,746,818) is larger than Integer.MAX_VALUE, which ensures
+        // that the method correctly handles values that would be negative if treated
+        // as a signed 32-bit integer.
+        final long expectedValue = 0x80040302L;
+
+        // Act
+        final long actualValue = fromLittleEndian(sourceBytes, offset, length);
+
+        // Assert
+        assertEquals(expectedValue, actualValue);
     }
 }
