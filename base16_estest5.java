e3126816fcfea120bfdf11e52ad02e04af8e155e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:38:41 +0800

    Original processedAutoTestCases base16_estest5.java

---
+++ base16_estest5.java
@@ -0,0 +1,27 @@
+package org.apache.commons.codec.binary;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.apache.commons.codec.CodecPolicy;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class Base16_ESTestTest5 extends Base16_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test04() throws Throwable {
+        Base16 base16_0 = new Base16();
+        // Undeclared exception!
+        try {
+            base16_0.decode("Gw");
+            fail("Expecting exception: IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //
+            // Invalid octet in encoded value: 71
+            //
+            verifyException("org.apache.commons.codec.binary.Base16", e);
+        }
+    }
+}
