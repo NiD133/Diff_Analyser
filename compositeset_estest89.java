2365654211278188611237110a6a0530b11c2cee
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:45:30 +0800

    gemini processedAutoTestCases compositeset_estest89.java

--- compositeset_estest89.java
+++ compositeset_estest89.java
@@ -1,56 +1,35 @@
 package org.apache.commons.collections4.set;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.Iterator;
 import java.util.LinkedHashSet;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Set;
-import java.util.Spliterator;
-import java.util.function.Consumer;
-import java.util.stream.Stream;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.Equator;
-import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ChainedClosure;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.DefaultEquator;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionPredicate;
-import org.apache.commons.collections4.functors.FalsePredicate;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.IfClosure;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NotNullPredicate;
-import org.apache.commons.collections4.functors.NotPredicate;
-import org.apache.commons.collections4.functors.NullIsExceptionPredicate;
-import org.apache.commons.collections4.functors.NullIsTruePredicate;
-import org.apache.commons.collections4.functors.OnePredicate;
-import org.apache.commons.collections4.functors.OrPredicate;
-import org.apache.commons.collections4.functors.TransformerClosure;
-import org.apache.commons.collections4.functors.TruePredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.apache.commons.collections4.functors.WhileClosure;
-import org.apache.commons.collections4.iterators.IteratorChain;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertFalse;
 
-public class CompositeSet_ESTestTest89 extends CompositeSet_ESTest_scaffolding {
+/**
+ * Contains tests for the {@link CompositeSet} class.
+ */
+public class CompositeSetTest {
 
-    @Test(timeout = 4000)
-    public void test88() throws Throwable {
-        LinkedHashSet<Integer> linkedHashSet0 = new LinkedHashSet<Integer>();
-        CompositeSet<Integer> compositeSet0 = new CompositeSet<Integer>(linkedHashSet0);
-        boolean boolean0 = compositeSet0.retainAll(compositeSet0);
-        assertFalse(boolean0);
+    /**
+     * Tests that calling retainAll() with the set itself as an argument
+     * results in no modification and correctly returns false.
+     *
+     * According to the Collection API, retainAll should return true only if
+     * the collection was modified as a result of the call. Retaining all
+     * elements from itself should never modify the collection.
+     */
+    @Test
+    public void retainAllWithSelfShouldNotModifySetAndReturnFalse() {
+        // Arrange: Create an empty CompositeSet.
+        // The behavior should be the same whether the set is empty or not,
+        // but an empty set is a simple and effective case to test.
+        final Set<Integer> componentSet = new LinkedHashSet<>();
+        final CompositeSet<Integer> compositeSet = new CompositeSet<>(componentSet);
+
+        // Act: Call retainAll with the set itself.
+        final boolean wasModified = compositeSet.retainAll(compositeSet);
+
+        // Assert: Verify that the set was not modified.
+        assertFalse("retainAll(this) should always return false as it cannot modify the collection.", wasModified);
     }
 }
