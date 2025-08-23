e0da56b20c8aa6ff5f82d18b07364c1ebfa7b8d1
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:30:34 +0800

    gemini processedManualTestCases lazilyparsednumbertest3.java

--- lazilyparsednumbertest3.java
+++ lazilyparsednumbertest3.java
@@ -1,6 +1,7 @@
 package com.google.gson.internal;
 
 import static com.google.common.truth.Truth.assertThat;
+
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -9,16 +10,47 @@ import java.io.ObjectOutputStream;
 import java.math.BigDecimal;
 import org.junit.Test;
 
-public class LazilyParsedNumberTestTest3 {
+/**
+ * Test for the Java serialization behavior of {@link LazilyParsedNumber}.
+ */
+public class LazilyParsedNumberTest {
 
+    /**
+     * The {@code writeReplace()} method in {@code LazilyParsedNumber} is designed to
+     * serialize the object as a {@code BigDecimal}. This test verifies that after
+     * a serialization-deserialization round trip, the resulting object is indeed a
+     * {@code BigDecimal} with the correct value.
+     */
     @Test
-    public void testJavaSerialization() throws IOException, ClassNotFoundException {
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        ObjectOutputStream objOut = new ObjectOutputStream(out);
-        objOut.writeObject(new LazilyParsedNumber("123"));
-        objOut.close();
-        ObjectInputStream objIn = new ObjectInputStream(new ByteArrayInputStream(out.toByteArray()));
-        Number deserialized = (Number) objIn.readObject();
-        assertThat(deserialized).isEqualTo(new BigDecimal("123"));
+    public void javaSerialization_writesAsBigDecimal() throws IOException, ClassNotFoundException {
+        // Arrange
+        LazilyParsedNumber originalNumber = new LazilyParsedNumber("123");
+        BigDecimal expectedValue = new BigDecimal("123");
+
+        // Act
+        Number deserializedNumber = serializeAndDeserialize(originalNumber);
+
+        // Assert
+        assertThat(deserializedNumber).isInstanceOf(BigDecimal.class);
+        assertThat(deserializedNumber).isEqualTo(expectedValue);
+    }
+
+    /**
+     * Serializes an object to a byte array and then deserializes it back.
+     *
+     * @return The deserialized object.
+     */
+    private static <T> T serializeAndDeserialize(T object) throws IOException, ClassNotFoundException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try (ObjectOutputStream oos = new ObjectOutputStream(baos)) {
+            oos.writeObject(object);
+        }
+
+        ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+        try (ObjectInputStream ois = new ObjectInputStream(bais)) {
+            @SuppressWarnings("unchecked")
+            T deserialized = (T) ois.readObject();
+            return deserialized;
+        }
     }
 }
