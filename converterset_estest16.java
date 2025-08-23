a5a223e2efab855fc715f0f8b9484d8c19ec56d4
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:45:43 +0800

    gemini processedAutoTestCases converterset_estest16.java

--- converterset_estest16.java
+++ converterset_estest16.java
@@ -1,28 +1,29 @@
 package org.joda.time.convert;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.joda.time.Hours;
-import org.joda.time.Interval;
-import org.joda.time.MutablePeriod;
-import org.joda.time.PeriodType;
-import org.joda.time.Seconds;
-import org.joda.time.chrono.CopticChronology;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertNull;
 
-public class ConverterSet_ESTestTest16 extends ConverterSet_ESTest_scaffolding {
+/**
+ * Unit tests for {@link ConverterSet}.
+ */
+public class ConverterSetTest {
 
-    @Test(timeout = 4000)
-    public void test15() throws Throwable {
-        Converter[] converterArray0 = new Converter[1];
-        NullConverter nullConverter0 = NullConverter.INSTANCE;
-        converterArray0[0] = (Converter) nullConverter0;
-        ConverterSet converterSet0 = new ConverterSet(converterArray0);
-        Class<ConverterSet.Entry> class0 = ConverterSet.Entry.class;
-        Converter converter0 = converterSet0.select(class0);
-        assertNull(converter0);
+    /**
+     * Tests that the select() method returns null when no converter in the set
+     * can handle the requested type.
+     */
+    @Test
+    public void selectShouldReturnNullWhenNoConverterSupportsTheGivenType() {
+        // Arrange: Create a ConverterSet containing only the NullConverter,
+        // which has a supported type of 'null'.
+        Converter[] converters = { NullConverter.INSTANCE };
+        ConverterSet converterSet = new ConverterSet(converters);
+
+        // Act: Attempt to select a converter for a non-null type (String.class)
+        // that is not supported by any converter in the set.
+        Converter result = converterSet.select(String.class);
+
+        // Assert: Verify that no converter was found.
+        assertNull("Expected select() to return null for an unsupported type", result);
     }
 }
