a53fa35bb487569405f29a93b39ffeebcd3dc911
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:31:10 +0800

    Original processedManualTestCases minutestest20.java

---
+++ minutestest20.java
@@ -0,0 +1,45 @@
+package org.joda.time;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class MinutesTestTest20 extends TestCase {
+
+    // (before the late 90's they were all over the place)
+    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
+
+    public static TestSuite suite() {
+        return new TestSuite(TestMinutes.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+    }
+
+    //-----------------------------------------------------------------------
+    public void testPlus_int() {
+        Minutes test2 = Minutes.minutes(2);
+        Minutes result = test2.plus(3);
+        assertEquals(2, test2.getMinutes());
+        assertEquals(5, result.getMinutes());
+        assertEquals(1, Minutes.ONE.plus(0).getMinutes());
+        try {
+            Minutes.MAX_VALUE.plus(1);
+            fail();
+        } catch (ArithmeticException ex) {
+            // expected
+        }
+    }
+}
