90208a18739f500a52682cb8f2b65ce938001465
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:01:27 +0800

    gemini processedAutoTestCases logfactory_estest2.java

--- logfactory_estest2.java
+++ logfactory_estest2.java
@@ -1,26 +1,22 @@
 package org.apache.ibatis.logging;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.apache.ibatis.logging.commons.JakartaCommonsLoggingImpl;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class LogFactory_ESTestTest2 extends LogFactory_ESTest_scaffolding {
+/**
+ * Test suite for the {@link LogFactory} class.
+ * This class verifies the correct behavior of the factory, especially in edge cases.
+ */
+public class LogFactoryTest {
 
-    @Test(timeout = 4000)
-    public void test01() throws Throwable {
-        // Undeclared exception!
-        try {
-            LogFactory.getLog((Class<?>) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.ibatis.logging.LogFactory", e);
-        }
+    /**
+     * Verifies that calling getLog(Class) with a null argument
+     * correctly throws a NullPointerException.
+     *
+     * A logger instance is intrinsically tied to a class, so a null class
+     * is an invalid input that should be rejected.
+     */
+    @Test(expected = NullPointerException.class)
+    public void getLogWithNullClassShouldThrowNullPointerException() {
+        LogFactory.getLog((Class<?>) null);
     }
 }
