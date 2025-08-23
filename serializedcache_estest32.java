17478157362c36f4776d3d97fd0f930a188085ed
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:14:56 +0800

    Original processedAutoTestCases serializedcache_estest32.java

---
+++ serializedcache_estest32.java
@@ -0,0 +1,34 @@
+package org.apache.ibatis.cache.decorators;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.EOFException;
+import java.io.SequenceInputStream;
+import java.util.Enumeration;
+import org.apache.ibatis.cache.Cache;
+import org.apache.ibatis.cache.impl.PerpetualCache;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.io.MockFileInputStream;
+import org.junit.runner.RunWith;
+
+public class SerializedCache_ESTestTest32 extends SerializedCache_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test31() throws Throwable {
+        SerializedCache serializedCache0 = new SerializedCache((Cache) null);
+        // Undeclared exception!
+        try {
+            serializedCache0.clear();
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.apache.ibatis.cache.decorators.SerializedCache", e);
+        }
+    }
+}
