4460d8cbec8cf9d38aee2a050d411e6f169c74f4
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:58:00 +0800

    Original processedAutoTestCases jsontreereader_estest26.java

---
+++ jsontreereader_estest26.java
@@ -0,0 +1,36 @@
+package com.google.gson.internal.bind;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonNull;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.Strictness;
+import com.google.gson.stream.JsonToken;
+import java.io.IOException;
+import java.util.ConcurrentModificationException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class JsonTreeReader_ESTestTest26 extends JsonTreeReader_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test025() throws Throwable {
+        JsonPrimitive jsonPrimitive0 = new JsonPrimitive("||9{dvk.\"Ana");
+        JsonTreeReader jsonTreeReader0 = new JsonTreeReader(jsonPrimitive0);
+        // Undeclared exception!
+        try {
+            jsonTreeReader0.nextInt();
+            fail("Expecting exception: NumberFormatException");
+        } catch (NumberFormatException e) {
+            //
+            // For input string: \"||9{dvk.\"Ana\"
+            //
+            verifyException("java.lang.NumberFormatException", e);
+        }
+    }
+}
