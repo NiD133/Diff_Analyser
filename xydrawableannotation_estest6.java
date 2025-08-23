b0b77ea54e0b613213344855c9ccbdd0099eaee8
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:29:42 +0800

    gemini processedAutoTestCases xydrawableannotation_estest6.java

--- xydrawableannotation_estest6.java
+++ xydrawableannotation_estest6.java
@@ -1,51 +1,48 @@
 package org.jfree.chart.annotations;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.awt.Font;
-import java.awt.Graphics2D;
-import java.awt.geom.Rectangle2D;
-import java.awt.geom.RoundRectangle2D;
-import java.awt.image.BufferedImage;
-import java.math.BigInteger;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.jfree.chart.ChartRenderingInfo;
 import org.jfree.chart.Drawable;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.axis.CyclicNumberAxis;
-import org.jfree.chart.axis.NumberAxis;
-import org.jfree.chart.axis.ValueAxis;
 import org.jfree.chart.block.BlockContainer;
-import org.jfree.chart.legend.LegendTitle;
-import org.jfree.chart.plot.MeterPlot;
-import org.jfree.chart.plot.PlotRenderingInfo;
-import org.jfree.chart.plot.PolarAxisLocation;
-import org.jfree.chart.plot.XYPlot;
-import org.jfree.chart.renderer.AbstractRenderer;
-import org.jfree.chart.renderer.category.ScatterRenderer;
-import org.jfree.chart.renderer.xy.XYBarRenderer;
-import org.jfree.chart.title.CompositeTitle;
-import org.jfree.chart.title.DateTitle;
-import org.jfree.chart.title.ShortTextTitle;
-import org.jfree.chart.title.TextTitle;
-import org.jfree.data.general.DefaultValueDataset;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class XYDrawableAnnotation_ESTestTest6 extends XYDrawableAnnotation_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
-    @Test(timeout = 4000)
-    public void test05() throws Throwable {
-        BlockContainer blockContainer0 = new BlockContainer();
-        XYDrawableAnnotation xYDrawableAnnotation0 = new XYDrawableAnnotation((-2655.096265552367), (-2655.096265552367), (-2655.096265552367), (-2655.096265552367), (-2655.096265552367), blockContainer0);
-        XYDrawableAnnotation xYDrawableAnnotation1 = new XYDrawableAnnotation(0.0F, 0.0F, 0.0F, 0.0F, blockContainer0);
-        boolean boolean0 = xYDrawableAnnotation0.equals(xYDrawableAnnotation1);
-        assertEquals(1.0, xYDrawableAnnotation1.getDrawScaleFactor(), 0.01);
-        assertEquals(0.0, xYDrawableAnnotation1.getDisplayHeight(), 0.01);
-        assertFalse(boolean0);
-        assertEquals(0.0, xYDrawableAnnotation1.getDisplayWidth(), 0.01);
-        assertEquals(0.0, xYDrawableAnnotation1.getY(), 0.01);
-        assertEquals(0.0, xYDrawableAnnotation1.getX(), 0.01);
+/**
+ * Unit tests for the {@link XYDrawableAnnotation} class.
+ */
+public class XYDrawableAnnotationTest {
+
+    /**
+     * Verifies that two XYDrawableAnnotation instances with different properties
+     * are not considered equal.
+     */
+    @Test
+    public void equals_whenAnnotationsHaveDifferentProperties_shouldReturnFalse() {
+        // Arrange
+        Drawable drawable = new BlockContainer();
+
+        // Create an annotation with specific, non-zero properties.
+        XYDrawableAnnotation annotation1 = new XYDrawableAnnotation(
+                -10.0, -20.0, 100.0, 50.0, 2.0, drawable);
+
+        // Create a second, different annotation using the constructor that
+        // sets a default scale factor of 1.0.
+        XYDrawableAnnotation annotation2 = new XYDrawableAnnotation(
+                0.0, 0.0, 0.0, 0.0, drawable);
+
+        // Act
+        boolean areEqual = annotation1.equals(annotation2);
+
+        // Assert
+        // The primary goal is to test that the two different annotations are not equal.
+        assertFalse("Annotations with different properties should not be equal", areEqual);
+
+        // The original test also verified the state of the second annotation,
+        // which is a good check for its constructor. We'll keep that here for completeness.
+        assertEquals("Default scale factor should be 1.0", 1.0, annotation2.getDrawScaleFactor(), 0.001);
+        assertEquals("X coordinate should be 0.0", 0.0, annotation2.getX(), 0.001);
+        assertEquals("Y coordinate should be 0.0", 0.0, annotation2.getY(), 0.001);
+        assertEquals("Display width should be 0.0", 0.0, annotation2.getDisplayWidth(), 0.001);
+        assertEquals("Display height should be 0.0", 0.0, annotation2.getDisplayHeight(), 0.001);
     }
 }
