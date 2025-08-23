9351e371f30aabe56b81e36d85eed047aa1ffe88
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:24:55 +0800

    Original processedManualTestCases arraytypehandlertest8.java

---
+++ arraytypehandlertest8.java
@@ -0,0 +1,30 @@
+package org.apache.ibatis.type;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import java.sql.Array;
+import java.sql.Connection;
+import java.sql.Types;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+public class ArrayTypeHandlerTestTest8 extends BaseTypeHandlerTest {
+
+    private static final TypeHandler<Object> TYPE_HANDLER = new ArrayTypeHandler();
+
+    @Mock
+    Array mockArray;
+
+    @Override
+    @Test
+    public void shouldGetResultNullFromResultSetByPosition() throws Exception {
+        when(rs.getArray(1)).thenReturn(null);
+        assertNull(TYPE_HANDLER.getResult(rs, 1));
+    }
+}
