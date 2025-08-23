1001bd40a3a53be6e34bd8673111fd3f5525d846
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:38:38 +0800

    gemini processedAutoTestCases base16_estest18.java

--- base16_estest18.java
+++ base16_estest18.java
@@ -1,26 +1,30 @@
 package org.apache.commons.codec.binary;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
 import org.apache.commons.codec.CodecPolicy;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
-public class Base16_ESTestTest18 extends Base16_ESTest_scaffolding {
+/**
+ * Tests the constructor of the {@link Base16} class.
+ */
+public class Base16ConstructorTest {
 
-    @Test(timeout = 4000)
-    public void test17() throws Throwable {
-        Base16 base16_0 = null;
-        try {
-            base16_0 = new Base16(true, (CodecPolicy) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // codecPolicy
-            //
-            verifyException("java.util.Objects", e);
-        }
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    /**
+     * Tests that the constructor throws a NullPointerException when a null CodecPolicy is provided.
+     * The constructor's contract requires a non-null policy.
+     */
+    @Test
+    public void constructorShouldThrowNullPointerExceptionWhenCodecPolicyIsNull() {
+        // Arrange: Define the expected exception and its message.
+        // The underlying BaseNCodec constructor enforces this with Objects.requireNonNull.
+        thrown.expect(NullPointerException.class);
+        thrown.expectMessage("codecPolicy");
+
+        // Act: Call the constructor with a null CodecPolicy, which should trigger the exception.
+        new Base16(true, null);
     }
 }
