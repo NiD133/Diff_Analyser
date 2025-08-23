1dc1b491af153a6b4b17b62d91c6c1262dab9b95
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:08:32 +0800

    gemini processedAutoTestCases periodformatter_estest3.java

--- periodformatter_estest3.java
+++ periodformatter_estest3.java
@@ -1,37 +1,33 @@
 package org.joda.time.format;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.IOException;
-import java.io.PipedWriter;
-import java.io.StringWriter;
-import java.io.Writer;
-import java.util.LinkedList;
 import java.util.Locale;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.joda.time.Duration;
-import org.joda.time.Hours;
-import org.joda.time.Minutes;
-import org.joda.time.MutablePeriod;
-import org.joda.time.Period;
 import org.joda.time.PeriodType;
-import org.joda.time.ReadWritablePeriod;
-import org.joda.time.ReadablePeriod;
-import org.joda.time.Seconds;
-import org.joda.time.Weeks;
-import org.joda.time.Years;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertSame;
 
-public class PeriodFormatter_ESTestTest3 extends PeriodFormatter_ESTest_scaffolding {
+/**
+ * Unit tests for {@link PeriodFormatter}.
+ */
+public class PeriodFormatterTest {
 
-    @Test(timeout = 4000)
-    public void test02() throws Throwable {
-        PeriodType periodType0 = PeriodType.minutes();
-        Locale locale0 = Locale.SIMPLIFIED_CHINESE;
-        PeriodFormatter periodFormatter0 = new PeriodFormatter((PeriodPrinter) null, (PeriodParser) null, locale0, periodType0);
-        PeriodFormatter periodFormatter1 = periodFormatter0.withLocale(locale0);
-        assertSame(periodFormatter0, periodFormatter1);
+    /**
+     * Tests that calling withLocale() with the same locale returns the same
+     * PeriodFormatter instance, confirming an immutability optimization.
+     */
+    @Test
+    public void withLocale_whenLocaleIsUnchanged_shouldReturnSameInstance() {
+        // Arrange
+        Locale chineseLocale = Locale.SIMPLIFIED_CHINESE;
+        PeriodType periodType = PeriodType.minutes();
+        PeriodFormatter originalFormatter = new PeriodFormatter(null, null, chineseLocale, periodType);
+
+        // Act
+        PeriodFormatter newFormatter = originalFormatter.withLocale(chineseLocale);
+
+        // Assert
+        // Since PeriodFormatter is immutable, calling a "with" method with the same value
+        // should be optimized to return the original instance.
+        assertSame("Expected the same instance when withLocale() is called with the same locale",
+                originalFormatter, newFormatter);
     }
 }
