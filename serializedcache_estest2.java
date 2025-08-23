a4839208a91d866d944cbceb6c5d0c244e1a9547
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:14:51 +0800

    gemini processedAutoTestCases serializedcache_estest2.java

--- serializedcache_estest2.java
+++ serializedcache_estest2.java
@@ -1,26 +1,34 @@
 package org.apache.ibatis.cache.decorators;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.EOFException;
-import java.io.SequenceInputStream;
-import java.util.Enumeration;
 import org.apache.ibatis.cache.Cache;
 import org.apache.ibatis.cache.impl.PerpetualCache;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.evosuite.runtime.mock.java.io.MockFileInputStream;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class SerializedCache_ESTestTest2 extends SerializedCache_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
 
-    @Test(timeout = 4000)
-    public void test01() throws Throwable {
-        PerpetualCache perpetualCache0 = new PerpetualCache("");
-        SerializedCache serializedCache0 = new SerializedCache(perpetualCache0);
-        serializedCache0.hashCode();
+/**
+ * Test suite for the SerializedCache decorator.
+ * This class focuses on verifying the decorator's behavior, such as delegation.
+ */
+public class SerializedCacheTest {
+
+    /**
+     * Verifies that the hashCode() method of SerializedCache correctly
+     * delegates the call to the underlying (decorated) cache instance.
+     * The hash code of the decorator should be the same as the hash code
+     * of the cache it wraps.
+     */
+    @Test
+    public void shouldDelegateHashCodeToDecoratedCache() {
+        // Arrange: Create a delegate cache and wrap it with SerializedCache.
+        Cache delegateCache = new PerpetualCache("test-delegate-cache");
+        Cache serializedCache = new SerializedCache(delegateCache);
+
+        // Act: Get the hash code from both the delegate and the decorator.
+        int expectedHashCode = delegateCache.hashCode();
+        int actualHashCode = serializedCache.hashCode();
+
+        // Assert: The hash codes should be identical, confirming delegation.
+        assertEquals(expectedHashCode, actualHashCode);
     }
 }
