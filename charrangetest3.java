629706bad3a6711aa31295fa195574084092b863
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:26:31 +0800

    Original processedManualTestCases charrangetest3.java

---
+++ charrangetest3.java
@@ -0,0 +1,25 @@
+package org.apache.commons.lang3;
+
+import static org.apache.commons.lang3.LangAssertions.assertNullPointerException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.lang.reflect.Modifier;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import org.junit.jupiter.api.Test;
+
+public class CharRangeTestTest3 extends AbstractLangTest {
+
+    @Test
+    void testConstructorAccessors_isIn_Normal() {
+        final CharRange rangea = CharRange.isIn('a', 'e');
+        assertEquals('a', rangea.getStart());
+        assertEquals('e', rangea.getEnd());
+        assertFalse(rangea.isNegated());
+        assertEquals("a-e", rangea.toString());
+    }
+}
