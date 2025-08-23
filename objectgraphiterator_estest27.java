14356df25702505d9dd1bb1ace506318948b3405
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:05:44 +0800

    gemini processedAutoTestCases objectgraphiterator_estest27.java

--- objectgraphiterator_estest27.java
+++ objectgraphiterator_estest27.java
@@ -1,62 +1,36 @@
 package org.apache.commons.collections4.iterators;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Comparator;
-import java.util.ConcurrentModificationException;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.ListIterator;
-import java.util.Map;
-import java.util.NoSuchElementException;
-import java.util.function.Function;
-import org.apache.commons.collections4.Factory;
-import org.apache.commons.collections4.Predicate;
 import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AndPredicate;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantFactory;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionPredicate;
 import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.FactoryTransformer;
-import org.apache.commons.collections4.functors.FalsePredicate;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.MapTransformer;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NullIsFalsePredicate;
-import org.apache.commons.collections4.functors.NullIsTruePredicate;
-import org.apache.commons.collections4.functors.OrPredicate;
-import org.apache.commons.collections4.functors.PredicateTransformer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class ObjectGraphIterator_ESTestTest27 extends ObjectGraphIterator_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
-    @Test(timeout = 4000)
-    public void test26() throws Throwable {
-        Object object0 = new Object();
-        Transformer<Object, Integer> transformer0 = ExceptionTransformer.exceptionTransformer();
-        ObjectGraphIterator<Object> objectGraphIterator0 = new ObjectGraphIterator<Object>(object0, transformer0);
-        // Undeclared exception!
+/**
+ * Contains tests for the {@link ObjectGraphIterator}.
+ */
+public class ObjectGraphIteratorTest {
+
+    /**
+     * Tests that if the provided transformer throws an exception during iteration,
+     * the call to {@link ObjectGraphIterator#next()} correctly propagates that exception.
+     */
+    @Test
+    public void nextShouldPropagateExceptionFromTransformer() {
+        // Arrange: Create an iterator with a transformer that is designed to always throw an exception.
+        final Object root = new Object();
+        final Transformer<Object, ?> exceptionThrowingTransformer = ExceptionTransformer.exceptionTransformer();
+        final ObjectGraphIterator<Object> iterator = new ObjectGraphIterator<>(root, exceptionThrowingTransformer);
+
+        // Act & Assert: Verify that calling next() results in the expected RuntimeException.
         try {
-            objectGraphIterator0.next();
-            fail("Expecting exception: RuntimeException");
-        } catch (RuntimeException e) {
-            //
-            // ExceptionTransformer invoked
-            //
-            verifyException("org.apache.commons.collections4.functors.ExceptionTransformer", e);
+            iterator.next();
+            fail("A RuntimeException was expected but not thrown.");
+        } catch (final RuntimeException e) {
+            // The iterator should propagate the exception from the transformer without modification.
+            final String expectedMessage = "ExceptionTransformer invoked";
+            assertEquals(expectedMessage, e.getMessage());
         }
     }
 }
