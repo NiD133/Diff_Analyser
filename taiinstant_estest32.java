1bfc6419e9cbe6d5a50a52633b5195ccb9f0d596
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:20:32 +0800

    gemini processedAutoTestCases taiinstant_estest32.java

--- taiinstant_estest32.java
+++ taiinstant_estest32.java
@@ -1,33 +1,19 @@
 package org.threeten.extra.scale;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.time.Clock;
-import java.time.Duration;
-import java.time.Instant;
-import java.time.format.DateTimeParseException;
-import java.time.temporal.ChronoUnit;
-import java.time.temporal.TemporalAmount;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.time.MockClock;
-import org.evosuite.runtime.mock.java.time.MockInstant;
-import org.junit.runner.RunWith;
 
-public class TaiInstant_ESTestTest32 extends TaiInstant_ESTest_scaffolding {
+/**
+ * Test class for the {@link TaiInstant} factory methods.
+ */
+public class TaiInstantTest {
 
-    @Test(timeout = 4000)
-    public void test31() throws Throwable {
-        // Undeclared exception!
-        try {
-            TaiInstant.of((UtcInstant) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.threeten.extra.scale.UtcRules", e);
-        }
+    /**
+     * Tests that the of(UtcInstant) factory method throws a NullPointerException
+     * when passed a null argument, as required by its contract.
+     */
+    @Test(expected = NullPointerException.class)
+    public void of_utcInstant_withNullInput_throwsNullPointerException() {
+        // The cast to UtcInstant is necessary to resolve method ambiguity.
+        TaiInstant.of((UtcInstant) null);
     }
 }
