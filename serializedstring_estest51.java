8d51ac3b0370fc9b5fac8547b12f3d2412118fb4
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:15:16 +0800

    Original processedAutoTestCases serializedstring_estest51.java

---
+++ serializedstring_estest51.java
@@ -0,0 +1,29 @@
+package com.fasterxml.jackson.core.io;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PipedOutputStream;
+import java.nio.ByteBuffer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.evosuite.runtime.mock.java.io.MockFileOutputStream;
+import org.evosuite.runtime.mock.java.io.MockPrintStream;
+import org.junit.runner.RunWith;
+
+public class SerializedString_ESTestTest51 extends SerializedString_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test50() throws Throwable {
+        SerializedString serializedString0 = new SerializedString("Null String illegal for SerializedString");
+        byte[] byteArray0 = serializedString0.asQuotedUTF8();
+        byte[] byteArray1 = serializedString0.asQuotedUTF8();
+        assertSame(byteArray1, byteArray0);
+        assertNotNull(byteArray1);
+    }
+}
