32ea6ac7927ef4715bc170be4373f42a1246a2ee
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:29:15 +0800

    gemini processedAutoTestCases xnode_estest83.java

--- xnode_estest83.java
+++ xnode_estest83.java
@@ -1,31 +1,34 @@
 package org.apache.ibatis.parsing;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.List;
-import java.util.Locale;
+import static org.junit.Assert.assertEquals;
+
 import java.util.Properties;
-import java.util.function.Supplier;
 import javax.imageio.metadata.IIOMetadataNode;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
-import org.w3c.dom.Document;
+import org.junit.Test;
 import org.w3c.dom.Node;
-import org.xml.sax.ext.DefaultHandler2;
 
-public class XNode_ESTestTest83 extends XNode_ESTest_scaffolding {
+/**
+ * Test suite for the XNode class, focusing on body parsing methods.
+ */
+public class XNodeTest {
 
-    @Test(timeout = 4000)
-    public void test082() throws Throwable {
-        Properties properties0 = new Properties();
-        IIOMetadataNode iIOMetadataNode0 = new IIOMetadataNode();
-        XNode xNode0 = new XNode((XPathParser) null, iIOMetadataNode0, properties0);
-        Integer integer0 = new Integer(97);
-        Integer integer1 = xNode0.getIntBody(integer0);
-        assertEquals(97, (int) integer1);
+    /**
+     * Verifies that getIntBody(defaultValue) returns the provided default value
+     * when the XML node's body is empty.
+     */
+    @Test
+    public void getIntBodyShouldReturnDefaultWhenBodyIsEmpty() {
+        // Arrange: Create an XNode with an empty body.
+        // We use IIOMetadataNode as a standard, empty implementation of org.w3c.dom.Node.
+        Node emptyNode = new IIOMetadataNode();
+        XNode xNode = new XNode(null, emptyNode, new Properties());
+
+        Integer defaultValue = 97;
+
+        // Act: Call the method under test.
+        Integer result = xNode.getIntBody(defaultValue);
+
+        // Assert: The result should be the default value.
+        assertEquals(defaultValue, result);
     }
 }
