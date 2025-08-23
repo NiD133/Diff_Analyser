d9c6840a0d529911416665f557adadfd28821143
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:46:39 +0800

    gemini processedAutoTestCases datetimecomparator_estest8.java

--- datetimecomparator_estest8.java
+++ datetimecomparator_estest8.java
@@ -1,17 +1,37 @@
 package org.joda.time;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class DateTimeComparator_ESTestTest8 extends DateTimeComparator_ESTest_scaffolding {
+/**
+ * Tests for the hashCode() method of the {@link DateTimeComparator} class.
+ */
+public class DateTimeComparatorHashCodeTest {
 
-    @Test(timeout = 4000)
-    public void test07() throws Throwable {
-        DateTimeComparator dateTimeComparator0 = DateTimeComparator.getInstance();
-        dateTimeComparator0.hashCode();
+    /**
+     * Tests that the hashCode() method for the default comparator instance
+     * adheres to the general contract of Object.hashCode().
+     * Specifically, it checks for consistency (multiple calls return the same value)
+     * and that equal objects have equal hash codes.
+     */
+    @Test
+    public void hashCode_forDefaultInstance_isConsistentAndEqualsBased() {
+        // Arrange: Get two references to the default singleton instance.
+        // DateTimeComparator.getInstance() returns a singleton, so comparator1 and comparator2
+        // will refer to the same object.
+        DateTimeComparator comparator1 = DateTimeComparator.getInstance();
+        DateTimeComparator comparator2 = DateTimeComparator.getInstance();
+
+        // Assert:
+        // 1. Consistency: Calling hashCode() multiple times on the same object
+        //    should return the same value.
+        assertEquals("hashCode must be consistent across multiple calls",
+                comparator1.hashCode(), comparator1.hashCode());
+
+        // 2. Equality: If two objects are equal according to the equals() method,
+        //    they must have the same hash code.
+        assertEquals("Two default instances should be equal", comparator1, comparator2);
+        assertEquals("Equal objects must have the same hash code",
+                comparator1.hashCode(), comparator2.hashCode());
     }
 }
