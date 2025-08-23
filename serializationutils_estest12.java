d3456f5c017400196fb18b76474b69975075fbc9
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:14:41 +0800

    Original processedAutoTestCases serializationutils_estest12.java

---
+++ serializationutils_estest12.java
@@ -0,0 +1,39 @@
+package org.apache.commons.lang3;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.io.SequenceInputStream;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Locale;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.evosuite.runtime.mock.java.io.MockFileInputStream;
+import org.evosuite.runtime.mock.java.io.MockPrintStream;
+import org.junit.runner.RunWith;
+
+public class SerializationUtils_ESTestTest12 extends SerializationUtils_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test11() throws Throwable {
+        PipedInputStream pipedInputStream0 = new PipedInputStream();
+        // Undeclared exception!
+        try {
+            SerializationUtils.deserialize((InputStream) pipedInputStream0);
+            fail("Expecting exception: RuntimeException");
+        } catch (RuntimeException e) {
+            //
+            // java.io.IOException: Pipe not connected
+            //
+            verifyException("org.apache.commons.lang3.SerializationUtils", e);
+        }
+    }
+}
