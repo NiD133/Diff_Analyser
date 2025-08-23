c5b6cfb4b1a0ffb7d339ea356d5b03e15c7c4485
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:17:23 +0800

    Original processedAutoTestCases spatialcontextfactory_estest31.java

---
+++ spatialcontextfactory_estest31.java
@@ -0,0 +1,25 @@
+package org.locationtech.spatial4j.context;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.HashMap;
+import java.util.Map;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+import org.locationtech.spatial4j.io.PolyshapeReader;
+import org.locationtech.spatial4j.shape.ShapeFactory;
+
+public class SpatialContextFactory_ESTestTest31 extends SpatialContextFactory_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test30() throws Throwable {
+        SpatialContextFactory spatialContextFactory0 = new SpatialContextFactory();
+        SpatialContext spatialContext0 = new SpatialContext(spatialContextFactory0);
+        spatialContextFactory0.makeFormats(spatialContext0);
+        assertFalse(spatialContextFactory0.normWrapLongitude);
+        assertFalse(spatialContextFactory0.hasFormatConfig);
+        assertTrue(spatialContextFactory0.geo);
+    }
+}
