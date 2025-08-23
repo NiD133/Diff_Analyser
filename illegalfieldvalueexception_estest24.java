158617ed66ea57d4ac1b81893ef015fd108a9b71
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:53:44 +0800

    Original processedAutoTestCases illegalfieldvalueexception_estest24.java

---
+++ illegalfieldvalueexception_estest24.java
@@ -0,0 +1,17 @@
+package org.joda.time;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class IllegalFieldValueException_ESTestTest24 extends IllegalFieldValueException_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test23() throws Throwable {
+        DurationFieldType durationFieldType0 = DurationFieldType.SECONDS_TYPE;
+        IllegalFieldValueException illegalFieldValueException0 = new IllegalFieldValueException(durationFieldType0, (Number) null, (Number) null, (Number) null);
+    }
+}
