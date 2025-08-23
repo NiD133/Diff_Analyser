c6c1e27cad7eb37b77f38a4862941d5a38591281
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:41:42 +0800

    Original processedAutoTestCases caseformat_estest5.java

---
+++ caseformat_estest5.java
@@ -0,0 +1,17 @@
+package com.google.common.base;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class CaseFormat_ESTestTest5 extends CaseFormat_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test04() throws Throwable {
+        CaseFormat[] caseFormatArray0 = CaseFormat.values();
+        assertEquals(5, caseFormatArray0.length);
+    }
+}
