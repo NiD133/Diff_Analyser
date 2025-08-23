c25de36752585a35f3a3a6a3f6221783baae02f9
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:45:50 +0800

    gemini processedAutoTestCases cookieutil_estest1.java

--- cookieutil_estest1.java
+++ cookieutil_estest1.java
@@ -1,32 +1,32 @@
 package org.jsoup.helper;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.net.MalformedURLException;
+
+import java.io.IOException;
 import java.net.URI;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.function.BiConsumer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.evosuite.runtime.mock.java.io.MockFile;
-import org.evosuite.runtime.mock.java.net.MockURL;
-import org.jsoup.nodes.Attributes;
-import org.jsoup.nodes.TextNode;
-import org.junit.runner.RunWith;
 
-public class CookieUtil_ESTestTest1 extends CookieUtil_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
-    @Test(timeout = 4000)
-    public void test00() throws Throwable {
-        URL uRL0 = MockURL.getFtpExample();
-        URI uRI0 = CookieUtil.asUri(uRL0);
-        assertEquals("ftp", uRI0.getScheme());
+/**
+ * Tests for the {@link CookieUtil} helper class.
+ */
+public class CookieUtilTest {
+
+    @Test
+    public void asUriShouldCorrectlyConvertUrlToUri() throws IOException {
+        // Arrange: Create a sample URL to be converted.
+        // We use a standard URL constructor to make the test self-contained and clear.
+        URL sourceUrl = new URL("ftp://ftp.example.com/resource/file.txt");
+
+        // Act: Call the method under test.
+        URI resultUri = CookieUtil.asUri(sourceUrl);
+
+        // Assert: Verify that the resulting URI has the correct components.
+        assertNotNull("The converted URI should not be null.", resultUri);
+        assertEquals("The scheme should be correctly transferred.", "ftp", resultUri.getScheme());
+        assertEquals("The host should be correctly transferred.", "ftp.example.com", resultUri.getHost());
+        assertEquals("The path should be correctly transferred.", "/resource/file.txt", resultUri.getPath());
     }
 }
