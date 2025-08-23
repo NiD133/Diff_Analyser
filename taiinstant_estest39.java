0293393702eb23f7b5ca5e5e974370060e28c51c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:20:35 +0800

    Original processedAutoTestCases taiinstant_estest39.java

---
+++ taiinstant_estest39.java
@@ -0,0 +1,34 @@
+package org.threeten.extra.scale;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.format.DateTimeParseException;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAmount;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.time.MockClock;
+import org.evosuite.runtime.mock.java.time.MockInstant;
+import org.junit.runner.RunWith;
+
+public class TaiInstant_ESTestTest39 extends TaiInstant_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test38() throws Throwable {
+        TaiInstant taiInstant0 = TaiInstant.ofTaiSeconds(2784L, 2784L);
+        // Undeclared exception!
+        try {
+            taiInstant0.durationUntil((TaiInstant) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.threeten.extra.scale.TaiInstant", e);
+        }
+    }
+}
