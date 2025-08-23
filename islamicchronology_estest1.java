f0c8e2c5a5c77c233a09b26dc4a065aaa2e46a1f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:55:05 +0800

    Original processedAutoTestCases islamicchronology_estest1.java

---
+++ islamicchronology_estest1.java
@@ -0,0 +1,23 @@
+package org.joda.time.chrono;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.TimeZone;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.joda.time.Chronology;
+import org.joda.time.DateTimeZone;
+import org.joda.time.tz.UTCProvider;
+import org.junit.runner.RunWith;
+
+public class IslamicChronology_ESTestTest1 extends IslamicChronology_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test00() throws Throwable {
+        IslamicChronology.LeapYearPatternType islamicChronology_LeapYearPatternType0 = IslamicChronology.LEAP_YEAR_15_BASED;
+        IslamicChronology islamicChronology0 = IslamicChronology.getInstance();
+        boolean boolean0 = islamicChronology0.LEAP_YEAR_HABASH_AL_HASIB.equals(islamicChronology_LeapYearPatternType0);
+        assertFalse(boolean0);
+    }
+}
