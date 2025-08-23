af155ecb77b9661c35a95b9e9ca7afe6432e5210
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:46:33 +0800

    gemini processedAutoTestCases datetimecomparator_estest12.java

--- datetimecomparator_estest12.java
+++ datetimecomparator_estest12.java
@@ -2,17 +2,24 @@ package org.joda.time;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class DateTimeComparator_ESTestTest12 extends DateTimeComparator_ESTest_scaffolding {
+/**
+ * Unit tests for {@link DateTimeComparator}.
+ */
+public class DateTimeComparatorTest {
 
-    @Test(timeout = 4000)
-    public void test11() throws Throwable {
-        DateTimeComparator dateTimeComparator0 = DateTimeComparator.getTimeOnlyInstance();
-        boolean boolean0 = dateTimeComparator0.equals(dateTimeComparator0);
-        assertTrue(boolean0);
+    /**
+     * Tests that the equals() method is reflexive for the time-only comparator.
+     * According to the Java contract for Object.equals(), an object must be equal to itself.
+     */
+    @Test
+    public void timeOnlyInstance_equals_isReflexive() {
+        // Arrange
+        DateTimeComparator timeOnlyComparator = DateTimeComparator.getTimeOnlyInstance();
+
+        // Act & Assert
+        // The reflexive property of the equals contract states that x.equals(x) must be true.
+        // We use assertEquals here as it's the idiomatic way to test this contract.
+        assertEquals("A comparator instance should be equal to itself.", timeOnlyComparator, timeOnlyComparator);
     }
 }
