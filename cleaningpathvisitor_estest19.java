247824f5fcab724c97b33f4604661cff9cbe81ac
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:43:35 +0800

    gemini processedAutoTestCases cleaningpathvisitor_estest19.java

--- cleaningpathvisitor_estest19.java
+++ cleaningpathvisitor_estest19.java
@@ -1,37 +1,34 @@
 package org.apache.commons.io.file;
 
+import static org.junit.Assert.assertEquals;
+
+import org.apache.commons.io.file.Counters.PathCounters;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.IOException;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Path;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.evosuite.runtime.mock.java.io.MockFile;
-import org.junit.runner.RunWith;
 
-public class CleaningPathVisitor_ESTestTest19 extends CleaningPathVisitor_ESTest_scaffolding {
+/**
+ * Tests for {@link CleaningPathVisitor}.
+ */
+public class CleaningPathVisitorTest {
 
-    @Test(timeout = 4000)
-    public void test18() throws Throwable {
-        CountingPathVisitor.Builder countingPathVisitor_Builder0 = new CountingPathVisitor.Builder();
-        Counters.PathCounters counters_PathCounters0 = countingPathVisitor_Builder0.getPathCounters();
-        String[] stringArray0 = new String[6];
-        stringArray0[0] = "";
-        stringArray0[1] = "";
-        stringArray0[2] = "";
-        stringArray0[3] = "";
-        stringArray0[4] = "";
-        stringArray0[5] = "";
-        CleaningPathVisitor cleaningPathVisitor0 = new CleaningPathVisitor(counters_PathCounters0, stringArray0);
-        cleaningPathVisitor0.hashCode();
+    /**
+     * Tests that two identical CleaningPathVisitor instances produce the same hash code,
+     * adhering to the general contract of Object.hashCode().
+     */
+    @Test
+    public void testHashCodeContract() {
+        // Arrange
+        // 1. Define the configuration for the visitors.
+        final PathCounters pathCounters = Counters.longPathCounters();
+        final String[] pathsToSkip = {"", "", "", "", "", ""};
+
+        // 2. Create two separate but identical instances.
+        final CleaningPathVisitor visitor1 = new CleaningPathVisitor(pathCounters, pathsToSkip);
+        final CleaningPathVisitor visitor2 = new CleaningPathVisitor(pathCounters, pathsToSkip);
+
+        // Act & Assert
+        // The hashCode() contract states that if two objects are equal according to the
+        // equals() method, then calling hashCode() on each must produce the same integer result.
+        assertEquals("Identical visitor instances should be equal.", visitor1, visitor2);
+        assertEquals("Hash codes of equal visitor instances should be the same.", visitor1.hashCode(), visitor2.hashCode());
     }
 }
