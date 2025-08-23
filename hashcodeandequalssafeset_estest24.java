dbf2685fd4efdab7592393070b5834e1c1dca560
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:53:03 +0800

    Original processedAutoTestCases hashcodeandequalssafeset_estest24.java

---
+++ hashcodeandequalssafeset_estest24.java
@@ -0,0 +1,35 @@
+package org.mockito.internal.util.collections;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Vector;
+import java.util.function.Predicate;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class HashCodeAndEqualsSafeSet_ESTestTest24 extends HashCodeAndEqualsSafeSet_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test23() throws Throwable {
+        HashCodeAndEqualsSafeSet hashCodeAndEqualsSafeSet0 = new HashCodeAndEqualsSafeSet();
+        // Undeclared exception!
+        try {
+            hashCodeAndEqualsSafeSet0.containsAll((Collection<?>) null);
+            fail("Expecting exception: IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //
+            // Passed collection should notify() be null should not be null
+            //
+            verifyException("org.mockito.internal.util.Checks", e);
+        }
+    }
+}
