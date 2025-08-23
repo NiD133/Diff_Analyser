517d8f0410473f80a8b361c0e01e768492c4159a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:13:08 +0800

    gemini processedAutoTestCases seconds_estest56.java

--- seconds_estest56.java
+++ seconds_estest56.java
@@ -1,17 +1,26 @@
 package org.joda.time;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class Seconds_ESTestTest56 extends Seconds_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link Seconds} class.
+ */
+public class SecondsTest {
 
-    @Test(timeout = 4000)
-    public void test55() throws Throwable {
-        Seconds seconds0 = Seconds.seconds(3);
-        assertEquals(3, seconds0.getSeconds());
+    /**
+     * Tests that the factory method {@code Seconds.seconds(int)} creates an instance
+     * holding the correct value, which can be retrieved via {@code getSeconds()}.
+     */
+    @Test
+    public void secondsFactory_createsInstanceWithCorrectValue() {
+        // Arrange
+        final int expectedSeconds = 3;
+
+        // Act
+        Seconds seconds = Seconds.seconds(expectedSeconds);
+
+        // Assert
+        assertEquals(expectedSeconds, seconds.getSeconds());
     }
 }
