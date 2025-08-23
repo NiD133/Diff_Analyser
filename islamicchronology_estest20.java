d31bc0f77f7d566510f7e74b0febd84ad1468d11
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:55:09 +0800

    Original processedAutoTestCases islamicchronology_estest20.java

---
+++ islamicchronology_estest20.java
@@ -0,0 +1,22 @@
+package org.joda.time.chrono;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.TimeZone;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.joda.time.Chronology;
+import org.joda.time.DateTimeZone;
+import org.joda.time.tz.UTCProvider;
+import org.junit.runner.RunWith;
+
+public class IslamicChronology_ESTestTest20 extends IslamicChronology_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test19() throws Throwable {
+        IslamicChronology islamicChronology0 = IslamicChronology.getInstance();
+        int int0 = islamicChronology0.getMonthOfYear(0L, (-2992));
+        assertEquals(52360, int0);
+    }
+}
