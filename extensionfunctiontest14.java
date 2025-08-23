800aa96fa198fa30916729ebf2c9d37c6e9ac73b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:28:37 +0800

    Original processedManualTestCases extensionfunctiontest14.java

---
+++ extensionfunctiontest14.java
@@ -0,0 +1,108 @@
+package org.apache.commons.jxpath.ri.compiler;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Locale;
+import org.apache.commons.jxpath.AbstractJXPathTest;
+import org.apache.commons.jxpath.ClassFunctions;
+import org.apache.commons.jxpath.ExpressionContext;
+import org.apache.commons.jxpath.Function;
+import org.apache.commons.jxpath.FunctionLibrary;
+import org.apache.commons.jxpath.Functions;
+import org.apache.commons.jxpath.JXPathContext;
+import org.apache.commons.jxpath.NodeSet;
+import org.apache.commons.jxpath.PackageFunctions;
+import org.apache.commons.jxpath.Pointer;
+import org.apache.commons.jxpath.TestBean;
+import org.apache.commons.jxpath.Variables;
+import org.apache.commons.jxpath.ri.model.NodePointer;
+import org.apache.commons.jxpath.util.JXPath11CompatibleTypeConverter;
+import org.apache.commons.jxpath.util.TypeConverter;
+import org.apache.commons.jxpath.util.TypeUtils;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ExtensionFunctionTestTest14 extends AbstractJXPathTest {
+
+    private Functions functions;
+
+    private JXPathContext context;
+
+    private TestBean testBean;
+
+    private TypeConverter typeConverter;
+
+    @Override
+    @BeforeEach
+    public void setUp() {
+        if (context == null) {
+            testBean = new TestBean();
+            context = JXPathContext.newContext(testBean);
+            final Variables vars = context.getVariables();
+            vars.declareVariable("test", new TestFunctions(4, "test"));
+            final FunctionLibrary lib = new FunctionLibrary();
+            lib.addFunctions(new ClassFunctions(TestFunctions.class, "test"));
+            lib.addFunctions(new ClassFunctions(TestFunctions2.class, "test"));
+            lib.addFunctions(new PackageFunctions("", "call"));
+            lib.addFunctions(new PackageFunctions("org.apache.commons.jxpath.ri.compiler.", "jxpathtest"));
+            lib.addFunctions(new PackageFunctions("", null));
+            context.setFunctions(lib);
+            context.getVariables().declareVariable("List.class", List.class);
+            context.getVariables().declareVariable("NodeSet.class", NodeSet.class);
+        }
+        functions = new ClassFunctions(TestFunctions.class, "test");
+        typeConverter = TypeUtils.getTypeConverter();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        TypeUtils.setTypeConverter(typeConverter);
+    }
+
+    private static final class Context implements ExpressionContext {
+
+        private final Object object;
+
+        public Context(final Object object) {
+            this.object = object;
+        }
+
+        @Override
+        public List<Pointer> getContextNodeList() {
+            return null;
+        }
+
+        @Override
+        public Pointer getContextNodePointer() {
+            return NodePointer.newNodePointer(null, object, Locale.getDefault());
+        }
+
+        @Override
+        public JXPathContext getJXPathContext() {
+            return null;
+        }
+
+        @Override
+        public int getPosition() {
+            return 0;
+        }
+    }
+
+    @Test
+    void testStaticMethodCall() {
+        assertXPathValue(context, "string(test:build(8, 'goober'))", "foo=8; bar=goober");
+        // Call a static method using PackageFunctions and class name
+        assertXPathValue(context, "string(jxpathtest:TestFunctions.build(8, 'goober'))", "foo=8; bar=goober");
+        // Call a static method with a fully qualified class name
+        assertXPathValue(context, "string(" + TestFunctions.class.getName() + ".build(8, 'goober'))", "foo=8; bar=goober");
+        // Two ClassFunctions are sharing the same prefix.
+        // This is TestFunctions2
+        assertXPathValue(context, "string(test:increment(8))", "9");
+        // See that a NodeSet gets properly converted to a string
+        assertXPathValue(context, "test:string(/beans/name)", "Name 1");
+    }
+}
