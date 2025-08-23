c0c1714a1cf5bebd644e8c43b60d0a8088255b06
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:28:11 +0800

    gemini processedManualTestCases elementstest29.java

--- elementstest29.java
+++ elementstest29.java
@@ -1,37 +1,46 @@
 package org.jsoup.select;
 
 import org.jsoup.Jsoup;
-import org.jsoup.TextUtil;
-import org.jsoup.nodes.Comment;
-import org.jsoup.nodes.DataNode;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.nodes.FormElement;
 import org.jsoup.nodes.Node;
-import org.jsoup.nodes.TextNode;
 import org.junit.jupiter.api.Test;
-import java.util.Iterator;
-import java.util.List;
-import static org.junit.jupiter.api.Assertions.*;
 
-public class ElementsTestTest29 {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Tests for the {@link Elements#traverse(NodeVisitor)} method.
+ */
+public class ElementsTest {
 
     @Test
-    public void traverse() {
-        Document doc = Jsoup.parse("<div><p>Hello</p></div><div>There</div>");
-        final StringBuilder accum = new StringBuilder();
-        doc.select("div").traverse(new NodeVisitor() {
+    void traverseShouldVisitAllNodesWithinEachSelectedElement() {
+        // Arrange
+        String html = "<div><p>Hello</p></div><div>There</div>";
+        Document doc = Jsoup.parse(html);
 
+        // The expected string represents a depth-first traversal of both selected 'div' elements.
+        // The NodeVisitor builds this log by appending node names during the head and tail visits.
+        String expectedLog = "<div><p><#text></#text></p></div><div><#text></#text></div>";
+
+        final StringBuilder traversalLog = new StringBuilder();
+        NodeVisitor loggingVisitor = new NodeVisitor() {
             @Override
             public void head(Node node, int depth) {
-                accum.append("<").append(node.nodeName()).append(">");
+                traversalLog.append("<").append(node.nodeName()).append(">");
             }
 
             @Override
             public void tail(Node node, int depth) {
-                accum.append("</").append(node.nodeName()).append(">");
+                traversalLog.append("</").append(node.nodeName()).append(">");
             }
-        });
-        assertEquals("<div><p><#text></#text></p></div><div><#text></#text></div>", accum.toString());
+        };
+
+        Elements selectedElements = doc.select("div");
+
+        // Act
+        selectedElements.traverse(loggingVisitor);
+
+        // Assert
+        assertEquals(expectedLog, traversalLog.toString());
     }
 }
