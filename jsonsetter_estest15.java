e41473dcdfda975f77f8460a51b2be68535e3cd4
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:57:38 +0800

    Original processedAutoTestCases jsonsetter_estest15.java

---
+++ jsonsetter_estest15.java
@@ -0,0 +1,20 @@
+package com.fasterxml.jackson.annotation;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class JsonSetter_ESTestTest15 extends JsonSetter_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test14() throws Throwable {
+        Nulls nulls0 = Nulls.DEFAULT;
+        JsonSetter.Value jsonSetter_Value0 = JsonSetter.Value.construct(nulls0, nulls0);
+        JsonSetter.Value jsonSetter_Value1 = jsonSetter_Value0.withValueNulls(nulls0, (Nulls) null);
+        assertSame(jsonSetter_Value1, jsonSetter_Value0);
+    }
+}
