738a939a6b85faa0af3870897620d3af362bff1f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:26:22 +0800

    gemini processedManualTestCases caseformattest20.java

--- caseformattest20.java
+++ caseformattest20.java
@@ -1,25 +1,54 @@
 package com.google.common.base;
 
 import static com.google.common.base.CaseFormat.LOWER_CAMEL;
-import static com.google.common.base.CaseFormat.LOWER_HYPHEN;
-import static com.google.common.base.CaseFormat.LOWER_UNDERSCORE;
 import static com.google.common.base.CaseFormat.UPPER_CAMEL;
-import static com.google.common.base.CaseFormat.UPPER_UNDERSCORE;
 import static com.google.common.truth.Truth.assertThat;
-import static com.google.common.truth.Truth.assertWithMessage;
-import com.google.common.annotations.GwtCompatible;
-import com.google.common.annotations.GwtIncompatible;
-import com.google.common.annotations.J2ktIncompatible;
-import com.google.common.testing.NullPointerTester;
-import com.google.common.testing.SerializableTester;
+
 import junit.framework.TestCase;
-import org.jspecify.annotations.NullUnmarked;
 
-public class CaseFormatTestTest20 extends TestCase {
+/**
+ * Tests for {@link CaseFormat} conversion from UPPER_CAMEL to LOWER_CAMEL.
+ */
+public class CaseFormatTest extends TestCase {
 
-    public void testUpperCamelToLowerCamel() {
-        assertThat(UPPER_CAMEL.to(LOWER_CAMEL, "Foo")).isEqualTo("foo");
-        assertThat(UPPER_CAMEL.to(LOWER_CAMEL, "FooBar")).isEqualTo("fooBar");
-        assertThat(UPPER_CAMEL.to(LOWER_CAMEL, "HTTP")).isEqualTo("hTTP");
+    public void testUpperCamelToLowerCamel_withSingleWord() {
+        // Arrange
+        String input = "Foo";
+        String expectedOutput = "foo";
+
+        // Act
+        String result = UPPER_CAMEL.to(LOWER_CAMEL, input);
+
+        // Assert
+        assertThat(result).isEqualTo(expectedOutput);
+    }
+
+    public void testUpperCamelToLowerCamel_withMultipleWords() {
+        // Arrange
+        String input = "FooBar";
+        String expectedOutput = "fooBar";
+
+        // Act
+        String result = UPPER_CAMEL.to(LOWER_CAMEL, input);
+
+        // Assert
+        assertThat(result).isEqualTo(expectedOutput);
+    }
+
+    public void testUpperCamelToLowerCamel_withAcronymLikeString() {
+        // Arrange
+        String input = "HTTP";
+        String expectedOutput = "hTTP";
+
+        // This behavior is by design. The UPPER_CAMEL format does not define a word
+        // boundary between consecutive capital letters. Therefore, "HTTP" is treated
+        // as a single word, and the conversion to LOWER_CAMEL only lowercases the
+        // first character of the entire string.
+        
+        // Act
+        String result = UPPER_CAMEL.to(LOWER_CAMEL, input);
+
+        // Assert
+        assertThat(result).isEqualTo(expectedOutput);
     }
 }
