d9061c47af79272279b70205dec6f0a56d7b33ec
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:07:24 +0800

    Original processedAutoTestCases parameter_estest22.java

---
+++ parameter_estest22.java
@@ -0,0 +1,22 @@
+package com.google.common.reflect;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.lang.annotation.Annotation;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class Parameter_ESTestTest22 extends Parameter_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test21() throws Throwable {
+        Annotation[] annotationArray0 = new Annotation[0];
+        Parameter parameter0 = new Parameter((Invokable<?, ?>) null, (-3591), (TypeToken<?>) null, annotationArray0, (Object) null);
+        TypeToken<?> typeToken0 = parameter0.getType();
+        assertNull(typeToken0);
+    }
+}
