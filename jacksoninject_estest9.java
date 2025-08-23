1c2a9fb7571cf9dfa17fae381ba82cbdc45e947b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:55:50 +0800

    Original processedAutoTestCases jacksoninject_estest9.java

---
+++ jacksoninject_estest9.java
@@ -0,0 +1,25 @@
+package com.fasterxml.jackson.annotation;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class JacksonInject_ESTestTest9 extends JacksonInject_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test08() throws Throwable {
+        JacksonInject.Value jacksonInject_Value0 = JacksonInject.Value.EMPTY;
+        Boolean boolean0 = Boolean.FALSE;
+        JacksonInject.Value jacksonInject_Value1 = jacksonInject_Value0.withUseInput(boolean0);
+        Object object0 = new Object();
+        JacksonInject.Value jacksonInject_Value2 = jacksonInject_Value1.withId(object0);
+        assertTrue(jacksonInject_Value2.hasId());
+        assertFalse(jacksonInject_Value1.equals((Object) jacksonInject_Value0));
+        assertNotSame(jacksonInject_Value1, jacksonInject_Value0);
+    }
+}
