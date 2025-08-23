cd0d9521ee2a5ff39eff1911d7de212180aba90a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:36:30 +0800

    Original processedManualTestCases yearstest4.java

---
+++ yearstest4.java
@@ -0,0 +1,42 @@
+package org.joda.time;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class YearsTestTest4 extends TestCase {
+
+    // (before the late 90's they were all over the place)
+    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
+
+    public static TestSuite suite() {
+        return new TestSuite(TestYears.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+    }
+
+    @SuppressWarnings("deprecation")
+    public void testFactory_yearsBetween_RPartial() {
+        LocalDate start = new LocalDate(2006, 6, 9);
+        LocalDate end1 = new LocalDate(2009, 6, 9);
+        YearMonthDay end2 = new YearMonthDay(2012, 6, 9);
+        assertEquals(3, Years.yearsBetween(start, end1).getYears());
+        assertEquals(0, Years.yearsBetween(start, start).getYears());
+        assertEquals(0, Years.yearsBetween(end1, end1).getYears());
+        assertEquals(-3, Years.yearsBetween(end1, start).getYears());
+        assertEquals(6, Years.yearsBetween(start, end2).getYears());
+    }
+}
