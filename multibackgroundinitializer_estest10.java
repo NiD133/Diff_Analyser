9c9f238c33f692a870aceac58dd1effab340d451
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:03:45 +0800

    Original processedAutoTestCases multibackgroundinitializer_estest10.java

---
+++ multibackgroundinitializer_estest10.java
@@ -0,0 +1,35 @@
+package org.apache.commons.lang3.concurrent;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.NoSuchElementException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.lang.MockException;
+import org.junit.runner.RunWith;
+
+public class MultiBackgroundInitializer_ESTestTest10 extends MultiBackgroundInitializer_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test09() throws Throwable {
+        ForkJoinPool forkJoinPool0 = ForkJoinPool.commonPool();
+        forkJoinPool0.getActiveThreadCount();
+        forkJoinPool0.getAsyncMode();
+        MultiBackgroundInitializer multiBackgroundInitializer0 = new MultiBackgroundInitializer();
+        multiBackgroundInitializer0.isInitialized();
+        BackgroundInitializer<MultiBackgroundInitializer.MultiBackgroundInitializerResults> backgroundInitializer0 = new BackgroundInitializer<MultiBackgroundInitializer.MultiBackgroundInitializerResults>();
+        multiBackgroundInitializer0.addInitializer("org.apache.commons.lang3.concurrent.MultiBackgroundInitializer$1", backgroundInitializer0);
+        multiBackgroundInitializer0.initialize();
+        multiBackgroundInitializer0.close();
+    }
+}
