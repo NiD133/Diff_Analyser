7f099b68aa5d17282c460ae52bfea16f38886e70
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:16:15 +0800

    Original processedAutoTestCases simplebloomfilter_estest42.java

---
+++ simplebloomfilter_estest42.java
@@ -0,0 +1,24 @@
+package org.apache.commons.collections4.bloomfilter;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.function.IntPredicate;
+import java.util.function.LongPredicate;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class SimpleBloomFilter_ESTestTest42 extends SimpleBloomFilter_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test41() throws Throwable {
+        Shape shape0 = Shape.fromNM(2, 2);
+        SimpleBloomFilter simpleBloomFilter0 = new SimpleBloomFilter(shape0);
+        SparseBloomFilter sparseBloomFilter0 = new SparseBloomFilter(shape0);
+        boolean boolean0 = simpleBloomFilter0.merge((BloomFilter<?>) sparseBloomFilter0);
+        assertTrue(boolean0);
+    }
+}
