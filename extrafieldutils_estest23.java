78c307e30d840c36f6e90937f845237d01fcfd3f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:49:57 +0800

    Original processedAutoTestCases extrafieldutils_estest23.java

---
+++ extrafieldutils_estest23.java
@@ -0,0 +1,25 @@
+package org.apache.commons.compress.archivers.zip;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.zip.ZipException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class ExtraFieldUtils_ESTestTest23 extends ExtraFieldUtils_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test22() throws Throwable {
+        // Undeclared exception!
+        try {
+            ExtraFieldUtils.createExtraFieldNoDefault((ZipShort) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+        }
+    }
+}
