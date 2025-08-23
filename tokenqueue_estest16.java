982e8a1178308625c51aba82f11ccdb58e914dab
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:22:20 +0800

    gemini processedAutoTestCases tokenqueue_estest16.java

--- tokenqueue_estest16.java
+++ tokenqueue_estest16.java
@@ -1,18 +1,23 @@
 package org.jsoup.parser;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class TokenQueue_ESTestTest16 extends TokenQueue_ESTest_scaffolding {
+/**
+ * Test suite for the {@link TokenQueue} class.
+ */
+public class TokenQueueTest {
 
-    @Test(timeout = 4000)
-    public void test15() throws Throwable {
-        TokenQueue tokenQueue0 = new TokenQueue("8Nw@t]UFUu54QXB.");
-        char char0 = tokenQueue0.current();
-        assertEquals('8', char0);
+    @Test
+    public void current_shouldReturnFirstCharacter_whenQueueIsNotEmpty() {
+        // Arrange
+        String inputData = "8Nw@t]UFUu54QXB.";
+        TokenQueue queue = new TokenQueue(inputData);
+
+        // Act
+        char firstChar = queue.current();
+
+        // Assert
+        assertEquals("The current() method should peek at the first character without consuming it.", '8', firstChar);
     }
 }
