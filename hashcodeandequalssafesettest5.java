f6345177e2bc7f77d8c31e2bb7f5da3df4b81a9f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:29:18 +0800

    gemini processedManualTestCases hashcodeandequalssafesettest5.java

--- hashcodeandequalssafesettest5.java
+++ hashcodeandequalssafesettest5.java
@@ -1,12 +1,8 @@
 package org.mockito.internal.util.collections;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.mock;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
+
 import java.util.Observer;
 import org.junit.Rule;
 import org.junit.Test;
@@ -14,32 +10,50 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
-public class HashCodeAndEqualsSafeSetTestTest5 {
+public class HashCodeAndEqualsSafeSetTest {
 
     @Rule
-    public MockitoRule r = MockitoJUnit.rule();
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
 
     @Mock
-    private UnmockableHashCodeAndEquals mock1;
+    private ObjectWithFailingHashCodeAndEquals mockWithFailingHashCode;
 
-    private static class UnmockableHashCodeAndEquals {
-
+    /**
+     * A helper class whose hashCode() and equals() methods are designed to fail.
+     * This simulates a mock object where these methods have not been stubbed and
+     * would throw an exception if called.
+     */
+    private static class ObjectWithFailingHashCodeAndEquals {
         @Override
         public final int hashCode() {
-            throw new NullPointerException("I'm failing on hashCode and I don't care");
+            throw new NullPointerException("hashCode() has failed");
         }
 
         @Override
         public final boolean equals(Object obj) {
-            throw new NullPointerException("I'm failing on equals and I don't care");
+            throw new NullPointerException("equals() has failed");
         }
     }
 
+    /**
+     * This test verifies that the addAll() method of HashCodeAndEqualsSafeSet
+     * works correctly even when the source collection contains an object
+     * whose hashCode() and equals() methods throw exceptions. A standard
+     * java.util.HashSet would fail in this scenario.
+     */
     @Test
-    public void can_add_a_collection() throws Exception {
-        HashCodeAndEqualsSafeSet mocks = HashCodeAndEqualsSafeSet.of(mock1, mock(Observer.class));
-        HashCodeAndEqualsSafeSet workingSet = new HashCodeAndEqualsSafeSet();
-        workingSet.addAll(mocks);
-        assertThat(workingSet.containsAll(mocks)).isTrue();
+    public void addAllShouldSucceedForCollectionContainingObjectWithFailingHashCode() {
+        // Arrange
+        Observer regularMock = mock(Observer.class);
+        HashCodeAndEqualsSafeSet sourceSet = HashCodeAndEqualsSafeSet.of(mockWithFailingHashCode, regularMock);
+        HashCodeAndEqualsSafeSet safeSet = new HashCodeAndEqualsSafeSet();
+
+        // Act
+        safeSet.addAll(sourceSet);
+
+        // Assert
+        assertThat(safeSet)
+            .hasSize(2)
+            .containsAll(sourceSet);
     }
 }
