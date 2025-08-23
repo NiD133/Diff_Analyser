c0fecd162235af779973ae10b21c829ece2dc0d2
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:46:46 +0800

    gemini processedAutoTestCases dayofmonth_estest3.java

--- dayofmonth_estest3.java
+++ dayofmonth_estest3.java
@@ -1,44 +1,30 @@
 package org.threeten.extra;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.time.Clock;
-import java.time.DateTimeException;
-import java.time.Duration;
-import java.time.Month;
-import java.time.YearMonth;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
-import java.time.chrono.Chronology;
-import java.time.chrono.HijrahDate;
-import java.time.chrono.MinguoDate;
+
 import java.time.temporal.ChronoField;
-import java.time.temporal.ChronoUnit;
-import java.time.temporal.Temporal;
-import java.time.temporal.TemporalAccessor;
-import java.time.temporal.TemporalField;
-import java.time.temporal.TemporalQuery;
-import java.time.temporal.UnsupportedTemporalTypeException;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.evosuite.runtime.mock.java.time.MockClock;
-import org.evosuite.runtime.mock.java.time.MockYearMonth;
-import org.evosuite.runtime.mock.java.time.MockZonedDateTime;
-import org.evosuite.runtime.mock.java.time.chrono.MockHijrahDate;
-import org.evosuite.runtime.mock.java.time.chrono.MockMinguoDate;
-import org.junit.runner.RunWith;
+import java.time.temporal.ValueRange;
 
-public class DayOfMonth_ESTestTest3 extends DayOfMonth_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
 
-    @Test(timeout = 4000)
-    public void test02() throws Throwable {
-        DayOfMonth dayOfMonth0 = DayOfMonth.now();
-        ChronoField chronoField0 = ChronoField.DAY_OF_MONTH;
-        dayOfMonth0.range(chronoField0);
-        assertEquals(14, dayOfMonth0.getValue());
+/**
+ * Tests for the range() method of the DayOfMonth class.
+ */
+public class DayOfMonthTest {
+
+    /**
+     * Verifies that the range() method returns the correct, fixed range for the DAY_OF_MONTH field.
+     */
+    @Test
+    public void range_forDayOfMonthField_returnsCorrectRange() {
+        // Arrange: Create an arbitrary DayOfMonth instance. The specific value does not affect the range.
+        DayOfMonth dayOfMonth = DayOfMonth.of(15);
+        ValueRange expectedRange = ValueRange.of(1, 31);
+
+        // Act: Get the range for the DAY_OF_MONTH field.
+        ValueRange actualRange = dayOfMonth.range(ChronoField.DAY_OF_MONTH);
+
+        // Assert: The valid range for a day of the month is always 1 to 31.
+        assertEquals("The range for DAY_OF_MONTH should be 1-31", expectedRange, actualRange);
     }
 }
