208f5b9d5522624fb4b47fced2fff75b6c01fbfb
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:39:11 +0800

    gemini processedAutoTestCases bcodec_estest30.java

--- bcodec_estest30.java
+++ bcodec_estest30.java
@@ -1,29 +1,35 @@
 package org.apache.commons.codec.net;
 
+import org.apache.commons.codec.DecoderException;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.nio.charset.Charset;
-import java.nio.charset.IllegalCharsetNameException;
-import java.nio.charset.UnsupportedCharsetException;
-import org.apache.commons.codec.CodecPolicy;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+// The test class name and inheritance are kept from the original for context.
 public class BCodec_ESTestTest30 extends BCodec_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test29() throws Throwable {
-        BCodec bCodec0 = new BCodec();
+    /**
+     * Tests that the decode(Object) method throws a DecoderException when passed
+     * an object that is not a String, as specified by its contract.
+     */
+    @Test
+    public void decode_withUnsupportedObjectType_shouldThrowDecoderException() {
+        // Arrange: Create a codec instance and an input object of an unsupported type.
+        // The decode(Object) method is designed to only accept Strings.
+        BCodec codec = new BCodec();
+        Object unsupportedInput = new BCodec(); // Any non-String object will do.
+
         try {
-            bCodec0.decode((Object) bCodec0);
-            fail("Expecting exception: Exception");
-        } catch (Exception e) {
-            //
-            // Objects of type org.apache.commons.codec.net.BCodec cannot be decoded using BCodec
-            //
-            verifyException("org.apache.commons.codec.net.BCodec", e);
+            // Act: Attempt to decode the unsupported object.
+            codec.decode(unsupportedInput);
+            fail("Expected a DecoderException to be thrown for an unsupported object type.");
+        } catch (final DecoderException e) {
+            // Assert: Verify that the correct exception was thrown with the expected message.
+            final String expectedMessage = "Objects of type " +
+                                           unsupportedInput.getClass().getName() +
+                                           " cannot be decoded using BCodec";
+            assertEquals(expectedMessage, e.getMessage());
         }
     }
 }
