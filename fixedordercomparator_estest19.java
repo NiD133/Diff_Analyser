adfff2d2679360dfe1dd41c91627c0b5c3c65910
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:51:23 +0800

    Original processedAutoTestCases fixedordercomparator_estest19.java

---
+++ fixedordercomparator_estest19.java
@@ -0,0 +1,29 @@
+package org.apache.commons.collections4.comparators;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.lang.reflect.Array;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Function;
+import org.apache.commons.collections4.Predicate;
+import org.apache.commons.collections4.functors.ConstantTransformer;
+import org.apache.commons.collections4.functors.IdentityPredicate;
+import org.apache.commons.collections4.functors.InstanceofPredicate;
+import org.apache.commons.collections4.functors.PredicateTransformer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class FixedOrderComparator_ESTestTest19 extends FixedOrderComparator_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test18() throws Throwable {
+        FixedOrderComparator<Object> fixedOrderComparator0 = new FixedOrderComparator<Object>();
+        FixedOrderComparator<Object>[] fixedOrderComparatorArray0 = (FixedOrderComparator<Object>[]) Array.newInstance(FixedOrderComparator.class, 1);
+        FixedOrderComparator<FixedOrderComparator<Object>> fixedOrderComparator1 = new FixedOrderComparator<FixedOrderComparator<Object>>(fixedOrderComparatorArray0);
+        boolean boolean0 = fixedOrderComparator1.equals(fixedOrderComparator0);
+        assertFalse(boolean0);
+    }
+}
