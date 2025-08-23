2ca5905665cab5f86f973abe995139a476cf2cc2
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:29:03 +0800

    gemini processedManualTestCases gregorianchronologytest12.java

--- gregorianchronologytest12.java
+++ gregorianchronologytest12.java
@@ -1,71 +1,47 @@
 package org.joda.time.chrono;
 
-import java.util.Locale;
-import java.util.TimeZone;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
+
 import org.joda.time.Chronology;
 import org.joda.time.DateMidnight;
-import org.joda.time.DateTime;
 import org.joda.time.DateTimeConstants;
-import org.joda.time.DateTimeUtils;
-import org.joda.time.DateTimeZone;
 import org.joda.time.YearMonthDay;
+import org.junit.Test;
 
-public class GregorianChronologyTestTest12 extends TestCase {
+/**
+ * Tests for GregorianChronology, focusing on date field calculations.
+ */
+public class GregorianChronologyTest {
 
-    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
+    // A stateless, thread-safe instance of the chronology under test.
+    private static final Chronology GREGORIAN_CHRONOLOGY = GregorianChronology.getInstance();
 
-    private static final DateTimeZone LONDON = DateTimeZone.forID("Europe/London");
+    @Test
+    public void dayOfMonth_getMaximumValue_returns28ForFebruaryInNonLeapYear_usingPartial() {
+        // Arrange: 1999 is a non-leap year, so February has 28 days.
+        // This test provides the date context as a ReadablePartial.
+        final int expectedDaysInFebruary = 28;
+        YearMonthDay february1999 = new YearMonthDay(1999, DateTimeConstants.FEBRUARY, 1);
 
-    private static final DateTimeZone TOKYO = DateTimeZone.forID("Asia/Tokyo");
+        // Act
+        int actualMaxDays = GREGORIAN_CHRONOLOGY.dayOfMonth().getMaximumValue(february1999);
 
-    long y2002days = 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365;
-
-    // 2002-06-09
-    private long TEST_TIME_NOW = (y2002days + 31L + 28L + 31L + 30L + 31L + 9L - 1L) * DateTimeConstants.MILLIS_PER_DAY;
-
-    private DateTimeZone originalDateTimeZone = null;
-
-    private TimeZone originalTimeZone = null;
-
-    private Locale originalLocale = null;
-
-    public static void main(String[] args) {
-        junit.textui.TestRunner.run(suite());
+        // Assert
+        assertEquals(expectedDaysInFebruary, actualMaxDays);
     }
 
-    public static TestSuite suite() {
-        return new TestSuite(TestGregorianChronology.class);
-    }
+    @Test
+    public void dayOfMonth_getMaximumValue_returns28ForFebruaryInNonLeapYear_usingInstant() {
+        // Arrange: 1999 is a non-leap year, so February has 28 days.
+        // This test provides the date context as an instant in milliseconds.
+        final int expectedDaysInFebruary = 28;
+        DateMidnight february1999 = new DateMidnight(1999, DateTimeConstants.FEBRUARY, 1);
+        long instantInMillis = february1999.getMillis();
 
-    @Override
-    protected void setUp() throws Exception {
-        DateTimeUtils.setCurrentMillisFixed(TEST_TIME_NOW);
-        originalDateTimeZone = DateTimeZone.getDefault();
-        originalTimeZone = TimeZone.getDefault();
-        originalLocale = Locale.getDefault();
-        DateTimeZone.setDefault(LONDON);
-        TimeZone.setDefault(TimeZone.getTimeZone("Europe/London"));
-        Locale.setDefault(Locale.UK);
-    }
+        // Act
+        int actualMaxDays = GREGORIAN_CHRONOLOGY.dayOfMonth().getMaximumValue(instantInMillis);
 
-    @Override
-    protected void tearDown() throws Exception {
-        DateTimeUtils.setCurrentMillisSystem();
-        DateTimeZone.setDefault(originalDateTimeZone);
-        TimeZone.setDefault(originalTimeZone);
-        Locale.setDefault(originalLocale);
-        originalDateTimeZone = null;
-        originalTimeZone = null;
-        originalLocale = null;
-    }
-
-    public void testMaximumValue() {
-        YearMonthDay ymd1 = new YearMonthDay(1999, DateTimeConstants.FEBRUARY, 1);
-        DateMidnight dm1 = new DateMidnight(1999, DateTimeConstants.FEBRUARY, 1);
-        Chronology chrono = GregorianChronology.getInstance();
-        assertEquals(28, chrono.dayOfMonth().getMaximumValue(ymd1));
-        assertEquals(28, chrono.dayOfMonth().getMaximumValue(dm1.getMillis()));
+        // Assert
+        assertEquals(expectedDaysInFebruary, actualMaxDays);
     }
 }
