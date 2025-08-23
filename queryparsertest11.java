43cf938c4083a13bc2094fae32ba542462316bc9
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:32:41 +0800

    Original processedManualTestCases queryparsertest11.java

---
+++ queryparsertest11.java
@@ -0,0 +1,19 @@
+package org.jsoup.select;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.junit.jupiter.api.Test;
+import static org.jsoup.select.EvaluatorDebug.asElement;
+import static org.jsoup.select.EvaluatorDebug.sexpr;
+import static org.jsoup.select.Selector.SelectorParseException;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class QueryParserTestTest11 {
+
+    @Test
+    public void okOnSpacesForeAndAft() {
+        Evaluator parse = QueryParser.parse(" span div  ");
+        assertEquals("span div", parse.toString());
+    }
+}
