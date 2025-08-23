6b1ab98830ff4fce374104ca9ed6ec00ac4b9368
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:50:28 +0800

    gemini processedAutoTestCases fieldutils_estest53.java

--- fieldutils_estest53.java
+++ fieldutils_estest53.java
@@ -1,35 +1,44 @@
 package org.joda.time.field;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.math.RoundingMode;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
 import org.joda.time.DateTimeField;
-import org.joda.time.DateTimeFieldType;
-import org.joda.time.DateTimeZone;
+import org.joda.time.IllegalFieldValueException;
 import org.joda.time.chrono.IslamicChronology;
-import org.joda.time.chrono.ZonedChronology;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class FieldUtils_ESTestTest53 extends FieldUtils_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
-    @Test(timeout = 4000)
-    public void test52() throws Throwable {
-        DateTimeZone dateTimeZone0 = DateTimeZone.UTC;
-        IslamicChronology islamicChronology0 = IslamicChronology.getInstance(dateTimeZone0);
-        ZonedChronology zonedChronology0 = ZonedChronology.getInstance(islamicChronology0, dateTimeZone0);
-        DateTimeField dateTimeField0 = zonedChronology0.minuteOfHour();
-        // Undeclared exception!
+/**
+ * This test class contains tests for the FieldUtils class.
+ * The original class name was FieldUtils_ESTestTest53.
+ */
+public class FieldUtilsTest {
+
+    /**
+     * Tests that verifyValueBounds throws an IllegalFieldValueException when the
+     * specified bounds are invalid (i.e., lower bound is greater than the upper bound).
+     */
+    @Test
+    public void verifyValueBounds_shouldThrowException_whenBoundsAreInverted() {
+        // Arrange: Set up a DateTimeField and define an invalid range where the
+        // lower bound is greater than the upper bound.
+        // The specific DateTimeField (minuteOfHour) is arbitrary; any field would work
+        // as it's only used to construct the exception message.
+        final DateTimeField minuteOfHourField = IslamicChronology.getInstanceUTC().minuteOfHour();
+
+        final int valueToTest = 1;
+        final int lowerBound = 1;
+        final int upperBound = -1; // Create an invalid range where lower > upper
+
+        // Act & Assert
         try {
-            FieldUtils.verifyValueBounds(dateTimeField0, 1, 1, (-1));
-            fail("Expecting exception: IllegalArgumentException");
-        } catch (IllegalArgumentException e) {
-            //
-            // Value 1 for minuteOfHour must be in the range [1,-1]
-            //
-            verifyException("org.joda.time.field.FieldUtils", e);
+            FieldUtils.verifyValueBounds(minuteOfHourField, valueToTest, lowerBound, upperBound);
+            fail("Expected an IllegalFieldValueException to be thrown due to invalid bounds.");
+        } catch (IllegalFieldValueException e) {
+            // The method correctly throws an exception because the value (1) is greater
+            // than the upper bound (-1), thus falling outside the invalid range [1, -1].
+            final String expectedMessage = "Value 1 for minuteOfHour must be in the range [1,-1]";
+            assertEquals(expectedMessage, e.getMessage());
         }
     }
 }
