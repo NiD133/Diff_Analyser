9320d9b62cc8491f98181e869917a2d47a3bc2fb
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:47:33 +0800

    gemini processedAutoTestCases defaultkeyedvaluedataset_estest21.java

--- defaultkeyedvaluedataset_estest21.java
+++ defaultkeyedvaluedataset_estest21.java
@@ -2,26 +2,32 @@ package org.jfree.data.general;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.math.BigInteger;
-import javax.swing.JLayeredPane;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.util.MockDate;
-import org.jfree.chart.date.SerialDate;
-import org.jfree.chart.date.SpreadsheetDate;
-import org.jfree.data.statistics.SimpleHistogramBin;
-import org.jfree.data.xy.OHLCDataItem;
-import org.junit.runner.RunWith;
 
-public class DefaultKeyedValueDataset_ESTestTest21 extends DefaultKeyedValueDataset_ESTest_scaffolding {
+/**
+ * Tests for the cloning and equality implementation of the {@link DefaultKeyedValueDataset} class.
+ */
+public class DefaultKeyedValueDatasetTest {
 
-    @Test(timeout = 4000)
-    public void test20() throws Throwable {
-        DefaultKeyedValueDataset defaultKeyedValueDataset0 = new DefaultKeyedValueDataset();
-        Object object0 = defaultKeyedValueDataset0.clone();
-        boolean boolean0 = defaultKeyedValueDataset0.equals(object0);
-        assertNotSame(object0, defaultKeyedValueDataset0);
-        assertTrue(boolean0);
+    /**
+     * Verifies that cloning an empty dataset produces a new instance that is
+     * equal to the original, fulfilling the general contract for Object.clone().
+     */
+    @Test
+    public void clone_onEmptyDataset_returnsEqualButNotSameInstance() throws CloneNotSupportedException {
+        // Arrange: Create an empty dataset instance.
+        DefaultKeyedValueDataset originalDataset = new DefaultKeyedValueDataset();
+
+        // Act: Clone the dataset.
+        DefaultKeyedValueDataset clonedDataset = (DefaultKeyedValueDataset) originalDataset.clone();
+
+        // Assert: The cloned object should be a separate instance but logically equal.
+        // 1. Check that they are not the same object in memory.
+        assertNotSame("A cloned dataset should be a new object instance.", originalDataset, clonedDataset);
+
+        // 2. Check that the contents are logically equal.
+        assertEquals("A cloned dataset should be equal to the original.", originalDataset, clonedDataset);
+        
+        // 3. As per the equals/hashCode contract, equal objects must have equal hash codes.
+        assertEquals("Equal datasets must have the same hash code.", originalDataset.hashCode(), clonedDataset.hashCode());
     }
 }
