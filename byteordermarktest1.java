dbc5796f70c26c748e71100107bce724dd748e9d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:26:02 +0800

    Original processedManualTestCases byteordermarktest1.java

---
+++ byteordermarktest1.java
@@ -0,0 +1,32 @@
+package org.apache.commons.io;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.nio.charset.Charset;
+import org.junit.jupiter.api.Test;
+
+public class ByteOrderMarkTestTest1 {
+
+    private static final ByteOrderMark TEST_BOM_1 = new ByteOrderMark("test1", 1);
+
+    private static final ByteOrderMark TEST_BOM_2 = new ByteOrderMark("test2", 1, 2);
+
+    private static final ByteOrderMark TEST_BOM_3 = new ByteOrderMark("test3", 1, 2, 3);
+
+    /**
+     * Tests that {@link ByteOrderMark#getCharsetName()} can be loaded as a {@link java.nio.charset.Charset} as advertised.
+     */
+    @Test
+    void testConstantCharsetNames() {
+        assertNotNull(Charset.forName(ByteOrderMark.UTF_8.getCharsetName()));
+        assertNotNull(Charset.forName(ByteOrderMark.UTF_16BE.getCharsetName()));
+        assertNotNull(Charset.forName(ByteOrderMark.UTF_16LE.getCharsetName()));
+        assertNotNull(Charset.forName(ByteOrderMark.UTF_32BE.getCharsetName()));
+        assertNotNull(Charset.forName(ByteOrderMark.UTF_32LE.getCharsetName()));
+    }
+}
