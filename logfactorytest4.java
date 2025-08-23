9c8aa6bca5f30202143e0d870ed2a278d816eb65
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:30:52 +0800

    gemini processedManualTestCases logfactorytest4.java

--- logfactorytest4.java
+++ logfactorytest4.java
@@ -1,38 +1,38 @@
 package org.apache.ibatis.logging;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import java.io.Reader;
-import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.logging.commons.JakartaCommonsLoggingImpl;
+
 import org.apache.ibatis.logging.jdk14.Jdk14LoggingImpl;
-import org.apache.ibatis.logging.log4j.Log4jImpl;
-import org.apache.ibatis.logging.log4j2.Log4j2Impl;
-import org.apache.ibatis.logging.nologging.NoLoggingImpl;
-import org.apache.ibatis.logging.slf4j.Slf4jImpl;
-import org.apache.ibatis.logging.stdout.StdOutImpl;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 
-public class LogFactoryTestTest4 {
+/**
+ * This test suite verifies the configuration of the LogFactory.
+ * It ensures that when a specific logging implementation is requested,
+ * the factory provides a logger of the correct type.
+ */
+class LogFactoryConfigurationTest {
 
-    @AfterAll
-    static void restore() {
-        LogFactory.useSlf4jLogging();
-    }
+  /**
+   * After all tests, reset the logging implementation to the default (Slf4j).
+   * This prevents side effects in other test suites that might rely on the
+   * default auto-discovery behavior of the LogFactory.
+   */
+  @AfterAll
+  static void resetLoggingToDefault() {
+    LogFactory.useSlf4jLogging();
+  }
 
-    private void logSomething(Log log) {
-        log.warn("Warning message.");
-        log.debug("Debug message.");
-        log.error("Error message.");
-        log.error("Error with Exception.", new Exception("Test exception."));
-    }
+  @Test
+  void shouldSelectJdkLoggingImplementation() {
+    // Arrange: Force the LogFactory to use the JDK logging implementation.
+    LogFactory.useJdkLogging();
 
-    @Test
-    void shouldUseJdKLogging() {
-        LogFactory.useJdkLogging();
-        Log log = LogFactory.getLog(Object.class);
-        logSomething(log);
-        assertEquals(log.getClass().getName(), Jdk14LoggingImpl.class.getName());
-    }
+    // Act: Request a logger instance from the factory.
+    Log log = LogFactory.getLog(LogFactoryConfigurationTest.class);
+
+    // Assert: Verify that the returned logger is of the expected type.
+    assertEquals(Jdk14LoggingImpl.class, log.getClass(),
+        "The LogFactory should have returned an instance of Jdk14LoggingImpl.");
+  }
 }
