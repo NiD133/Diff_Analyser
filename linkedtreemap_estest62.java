646c2adc1d10b0d463f2e58a69dd6c1008c6471e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:00:42 +0800

    Original processedAutoTestCases linkedtreemap_estest62.java

---
+++ linkedtreemap_estest62.java
@@ -0,0 +1,27 @@
+package com.google.gson.internal;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.AbstractMap;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.BiFunction;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class LinkedTreeMap_ESTestTest62 extends LinkedTreeMap_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test61() throws Throwable {
+        LinkedTreeMap<Integer, Integer> linkedTreeMap0 = new LinkedTreeMap<Integer, Integer>();
+        Set<Map.Entry<Integer, Integer>> set0 = (Set<Map.Entry<Integer, Integer>>) linkedTreeMap0.entrySet();
+        linkedTreeMap0.remove((Object) set0);
+        assertTrue(set0.isEmpty());
+        assertEquals(0, linkedTreeMap0.size());
+    }
+}
