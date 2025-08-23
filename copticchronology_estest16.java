ce241cbc9cdb59df0e8c4aca7ea160e1c481ddb3
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:45:56 +0800

    gemini processedAutoTestCases copticchronology_estest16.java

--- copticchronology_estest16.java
+++ copticchronology_estest16.java
@@ -1,20 +1,30 @@
 package org.joda.time.chrono;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.joda.time.Chronology;
-import org.joda.time.DateTimeZone;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class CopticChronology_ESTestTest16 extends CopticChronology_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link CopticChronology} class.
+ */
+public class CopticChronologyTest {
 
-    @Test(timeout = 4000)
-    public void test15() throws Throwable {
-        CopticChronology copticChronology0 = CopticChronology.getInstance();
-        int int0 = copticChronology0.getMaxYear();
-        assertEquals(292272708, int0);
+    /**
+     * Tests that getMaxYear() returns the documented maximum supported year.
+     * The CopticChronology has a hardcoded maximum year to ensure calculations
+     * do not overflow the 64-bit long used for storing milliseconds.
+     */
+    @Test
+    public void getMaxYear_shouldReturnConstantMaximumValue() {
+        // Arrange
+        CopticChronology copticChronology = CopticChronology.getInstance();
+        // This value is defined as a private constant MAX_YEAR in CopticChronology.
+        final int EXPECTED_MAX_YEAR = 292272708;
+
+        // Act
+        int actualMaxYear = copticChronology.getMaxYear();
+
+        // Assert
+        assertEquals("The maximum year should match the constant defined in the class.",
+                     EXPECTED_MAX_YEAR, actualMaxYear);
     }
 }
