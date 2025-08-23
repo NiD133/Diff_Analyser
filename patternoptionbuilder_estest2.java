462a1018fcceb216b804cf4527066e663252e8f7
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:07:32 +0800

    gemini processedAutoTestCases patternoptionbuilder_estest2.java

--- patternoptionbuilder_estest2.java
+++ patternoptionbuilder_estest2.java
@@ -1,25 +1,18 @@
 package org.apache.commons.cli;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class PatternOptionBuilder_ESTestTest2 extends PatternOptionBuilder_ESTest_scaffolding {
+/**
+ * Tests for {@link PatternOptionBuilder}.
+ */
+public class PatternOptionBuilderTest {
 
-    @Test(timeout = 4000)
-    public void test01() throws Throwable {
-        // Undeclared exception!
-        try {
-            PatternOptionBuilder.parsePattern((String) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.commons.cli.PatternOptionBuilder", e);
-        }
+    /**
+     * Tests that a NullPointerException is thrown when a null string is passed
+     * to the parsePattern method, as the pattern must not be null.
+     */
+    @Test(expected = NullPointerException.class)
+    public void parsePatternShouldThrowNullPointerExceptionForNullInput() {
+        PatternOptionBuilder.parsePattern(null);
     }
 }
