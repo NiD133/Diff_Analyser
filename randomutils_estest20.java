e1d3dc2ed62e6f8ec9d622403535cbe6bce040e9
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:12:15 +0800

    Original processedAutoTestCases randomutils_estest20.java

---
+++ randomutils_estest20.java
@@ -0,0 +1,28 @@
+package org.apache.commons.lang3;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.security.SecureRandom;
+import java.util.Random;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class RandomUtils_ESTestTest20 extends RandomUtils_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test19() throws Throwable {
+        RandomUtils randomUtils0 = RandomUtils.insecure();
+        // Undeclared exception!
+        try {
+            randomUtils0.randomFloat(3.3724796E38F, 1.0F);
+            fail("Expecting exception: IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //
+            // Start value must be smaller or equal to end value.
+            //
+            verifyException("org.apache.commons.lang3.Validate", e);
+        }
+    }
+}
