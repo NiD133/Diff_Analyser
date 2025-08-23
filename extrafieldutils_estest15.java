ac105a382ce520340e824d2312cea9673edc1dc8
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:49:55 +0800

    gemini processedAutoTestCases extrafieldutils_estest15.java

--- extrafieldutils_estest15.java
+++ extrafieldutils_estest15.java
@@ -1,26 +1,20 @@
 package org.apache.commons.compress.archivers.zip;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.zip.ZipException;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class ExtraFieldUtils_ESTestTest15 extends ExtraFieldUtils_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link ExtraFieldUtils} class.
+ */
+public class ExtraFieldUtilsTest {
 
-    @Test(timeout = 4000)
-    public void test14() throws Throwable {
-        // Undeclared exception!
-        try {
-            ExtraFieldUtils.parse((byte[]) null, false);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.commons.compress.archivers.zip.ExtraFieldUtils", e);
-        }
+    /**
+     * Verifies that calling the parse method with a null byte array
+     * results in a NullPointerException.
+     */
+    @Test(expected = NullPointerException.class)
+    public void parseWithNullDataShouldThrowNullPointerException() {
+        // The 'local' parameter is set to false, but its value is not
+        // relevant for this specific null-check scenario.
+        ExtraFieldUtils.parse(null, false);
     }
 }
