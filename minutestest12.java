482acb47cb6a6f3496ab6079d2eda71c21735d81
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:31:08 +0800

    Original processedManualTestCases minutestest12.java

---
+++ minutestest12.java
@@ -0,0 +1,38 @@
+package org.joda.time;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class MinutesTestTest12 extends TestCase {
+
+    // (before the late 90's they were all over the place)
+    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
+
+    public static TestSuite suite() {
+        return new TestSuite(TestMinutes.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+    }
+
+    public void testIsLessThan() {
+        assertEquals(false, Minutes.THREE.isLessThan(Minutes.TWO));
+        assertEquals(false, Minutes.THREE.isLessThan(Minutes.THREE));
+        assertEquals(true, Minutes.TWO.isLessThan(Minutes.THREE));
+        assertEquals(false, Minutes.ONE.isLessThan(null));
+        assertEquals(true, Minutes.minutes(-1).isLessThan(null));
+    }
+}
