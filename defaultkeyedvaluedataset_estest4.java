d39e4ce70249fc50229b3bbe79bd9a8b2094e714
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:47:34 +0800

    gemini processedAutoTestCases defaultkeyedvaluedataset_estest4.java

--- defaultkeyedvaluedataset_estest4.java
+++ defaultkeyedvaluedataset_estest4.java
@@ -2,28 +2,27 @@ package org.jfree.data.general;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.math.BigInteger;
-import javax.swing.JLayeredPane;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.util.MockDate;
-import org.jfree.chart.date.SerialDate;
-import org.jfree.chart.date.SpreadsheetDate;
-import org.jfree.data.statistics.SimpleHistogramBin;
-import org.jfree.data.xy.OHLCDataItem;
-import org.junit.runner.RunWith;
 
-public class DefaultKeyedValueDataset_ESTestTest4 extends DefaultKeyedValueDataset_ESTest_scaffolding {
+/**
+ * A test suite for the {@link DefaultKeyedValueDataset} class.
+ */
+public class DefaultKeyedValueDatasetTest {
 
-    @Test(timeout = 4000)
-    public void test03() throws Throwable {
-        SimpleHistogramBin simpleHistogramBin0 = new SimpleHistogramBin((-1201.1136675752093), 1.0, false, false);
-        byte[] byteArray0 = new byte[3];
-        byteArray0[2] = (byte) (-93);
-        BigInteger bigInteger0 = new BigInteger(byteArray0);
-        DefaultKeyedValueDataset defaultKeyedValueDataset0 = new DefaultKeyedValueDataset(simpleHistogramBin0, bigInteger0);
-        Number number0 = defaultKeyedValueDataset0.getValue();
-        assertEquals((byte) (-93), number0.byteValue());
+    /**
+     * Verifies that a value provided to the constructor can be correctly retrieved
+     * using the getValue() method.
+     */
+    @Test
+    public void getValue_shouldReturnTheValueProvidedInTheConstructor() {
+        // Arrange: Define a key and a value for the dataset.
+        Comparable<String> key = "Sample Key";
+        Number expectedValue = -93;
+
+        // Act: Create a new dataset instance with the specified key and value.
+        DefaultKeyedValueDataset dataset = new DefaultKeyedValueDataset(key, expectedValue);
+        Number actualValue = dataset.getValue();
+
+        // Assert: The value retrieved from the dataset should match the initial value.
+        assertEquals(expectedValue, actualValue);
     }
 }
