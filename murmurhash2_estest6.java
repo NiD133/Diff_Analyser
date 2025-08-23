5b26163779b4c9b58237a26676c1064bb9848dd4
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:04:33 +0800

    Original processedAutoTestCases murmurhash2_estest6.java

---
+++ murmurhash2_estest6.java
@@ -0,0 +1,17 @@
+package org.apache.commons.codec.digest;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class MurmurHash2_ESTestTest6 extends MurmurHash2_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test05() throws Throwable {
+        long long0 = MurmurHash2.hash64((byte[]) null, 0);
+        assertEquals((-7207201254813729732L), long0);
+    }
+}
