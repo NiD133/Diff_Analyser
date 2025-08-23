be482b8d4b689a5b8f727e8efc93b85e853c4a0f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:49:05 +0800

    Original processedAutoTestCases elements_estest91.java

---
+++ elements_estest91.java
@@ -0,0 +1,42 @@
+package org.jsoup.select;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.ConcurrentModificationException;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.jsoup.nodes.Comment;
+import org.jsoup.nodes.DataNode;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.FormElement;
+import org.jsoup.nodes.TextNode;
+import org.jsoup.parser.Parser;
+import org.junit.runner.RunWith;
+
+public class Elements_ESTestTest91 extends Elements_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test090() throws Throwable {
+        Elements elements0 = null;
+        try {
+            elements0 = new Elements((Element[]) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("java.util.Objects", e);
+        }
+    }
+}
