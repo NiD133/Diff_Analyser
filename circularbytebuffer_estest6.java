6b73fb687288049f67da98445b655b56b3294033
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:43:29 +0800

    Original processedAutoTestCases circularbytebuffer_estest6.java

---
+++ circularbytebuffer_estest6.java
@@ -0,0 +1,21 @@
+package org.apache.commons.io.input.buffer;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class CircularByteBuffer_ESTestTest6 extends CircularByteBuffer_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test05() throws Throwable {
+        CircularByteBuffer circularByteBuffer0 = new CircularByteBuffer();
+        byte[] byteArray0 = new byte[7];
+        byteArray0[0] = (byte) 127;
+        boolean boolean0 = circularByteBuffer0.peek(byteArray0, (byte) 0, (byte) 25);
+        assertFalse(boolean0);
+        assertEquals(8192, circularByteBuffer0.getSpace());
+    }
+}
