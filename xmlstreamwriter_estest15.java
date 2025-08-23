e361c9767e7596fd2b859250daa3268c45000684
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:27:45 +0800

    Original processedAutoTestCases xmlstreamwriter_estest15.java

---
+++ xmlstreamwriter_estest15.java
@@ -0,0 +1,38 @@
+package org.apache.commons.io.output;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.io.Writer;
+import java.nio.CharBuffer;
+import java.nio.charset.IllegalCharsetNameException;
+import java.nio.charset.UnsupportedCharsetException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.evosuite.runtime.mock.java.io.MockPrintStream;
+import org.junit.runner.RunWith;
+
+public class XmlStreamWriter_ESTestTest15 extends XmlStreamWriter_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test14() throws Throwable {
+        MockFile mockFile0 = new MockFile("", "");
+        XmlStreamWriter xmlStreamWriter0 = null;
+        try {
+            xmlStreamWriter0 = new XmlStreamWriter(mockFile0, "");
+            fail("Expecting exception: FileNotFoundException");
+        } catch (Throwable e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.evosuite.runtime.mock.java.io.MockFileOutputStream", e);
+        }
+    }
+}
