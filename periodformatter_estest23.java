4643f34ffd2a2ac9c844391587fa00a8c34cf207
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:08:29 +0800

    gemini processedAutoTestCases periodformatter_estest23.java

--- periodformatter_estest23.java
+++ periodformatter_estest23.java
@@ -1,53 +1,52 @@
 package org.joda.time.format;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.IOException;
-import java.io.PipedWriter;
-import java.io.StringWriter;
-import java.io.Writer;
-import java.util.LinkedList;
-import java.util.Locale;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.joda.time.Duration;
-import org.joda.time.Hours;
-import org.joda.time.Minutes;
-import org.joda.time.MutablePeriod;
-import org.joda.time.Period;
-import org.joda.time.PeriodType;
-import org.joda.time.ReadWritablePeriod;
-import org.joda.time.ReadablePeriod;
-import org.joda.time.Seconds;
-import org.joda.time.Weeks;
-import org.joda.time.Years;
-import org.junit.runner.RunWith;
 
-public class PeriodFormatter_ESTestTest23 extends PeriodFormatter_ESTest_scaffolding {
+/**
+ * This test case verifies the behavior of the PeriodFormatter when its
+ * underlying parser is configured with a circular dependency.
+ */
+public class PeriodFormatter_ESTestTest23 {
 
-    @Test(timeout = 4000)
-    public void test22() throws Throwable {
-        PeriodFormatterBuilder.Literal periodFormatterBuilder_Literal0 = PeriodFormatterBuilder.Literal.EMPTY;
-        String[] stringArray0 = new String[6];
-        stringArray0[0] = "";
-        stringArray0[1] = "";
-        stringArray0[2] = "";
-        stringArray0[3] = "";
-        stringArray0[4] = "";
-        stringArray0[5] = "";
-        PeriodFormatterBuilder.Separator periodFormatterBuilder_Separator0 = new PeriodFormatterBuilder.Separator("", "", stringArray0, periodFormatterBuilder_Literal0, periodFormatterBuilder_Literal0, true, true);
-        PeriodFormatter periodFormatter0 = new PeriodFormatter(periodFormatterBuilder_Literal0, periodFormatterBuilder_Separator0);
-        PeriodFormat.DynamicWordBased periodFormat_DynamicWordBased0 = new PeriodFormat.DynamicWordBased(periodFormatter0);
-        periodFormatterBuilder_Separator0.finish(periodFormatterBuilder_Literal0, periodFormat_DynamicWordBased0);
-        // Undeclared exception!
-        try {
-            periodFormatter0.parsePeriod("");
-            fail("Expecting exception: StackOverflowError");
-        } catch (StackOverflowError e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-        }
+    /**
+     * Tests that a call to parsePeriod() throws a StackOverflowError when the
+     * formatter is configured with a parser that recursively calls itself.
+     *
+     * <p>This scenario is created by setting up a chain of parsers where the
+     * last one points back to the first, creating an infinite loop:</p>
+     * <ol>
+     *   <li>A {@link PeriodFormatter} is created with a {@code Separator} as its parser.</li>
+     *   <li>A {@code DynamicWordBased} parser is created, which wraps the {@code PeriodFormatter}.</li>
+     *   <li>The {@code Separator}'s internal parser is then set to be the {@code DynamicWordBased} parser.</li>
+     * </ol>
+     * <p>This results in the following recursive parsing chain:
+     * {@code Formatter -> Separator -> DynamicWordBased -> Formatter -> ...}</p>
+     */
+    @Test(expected = StackOverflowError.class)
+    public void parsePeriodWithCircularParserDependencyThrowsStackOverflowError() {
+        // --- Arrange: Create a circular dependency in the parser structure ---
+
+        // A dummy component that does nothing, used as a placeholder printer/parser.
+        PeriodFormatterBuilder.Literal dummyComponent = PeriodFormatterBuilder.Literal.EMPTY;
+
+        // A separator that will be part of the recursive loop.
+        // It is initially configured with the dummy component as its parser.
+        PeriodFormatterBuilder.Separator recursiveSeparator = new PeriodFormatterBuilder.Separator(
+                "", "", null, dummyComponent, dummyComponent, true, true);
+
+        // The main formatter whose parser is the separator.
+        PeriodFormatter formatterWithCircularParser = new PeriodFormatter(dummyComponent, recursiveSeparator);
+
+        // A wrapper parser that contains the main formatter.
+        PeriodParser wrapperParser = new PeriodFormat.DynamicWordBased(formatterWithCircularParser);
+
+        // Complete the circular dependency by setting the separator's internal parser to be the wrapper.
+        // This creates the infinite recursion.
+        recursiveSeparator.finish(dummyComponent, wrapperParser);
+
+        // --- Act & Assert ---
+        // Attempt to parse a string, which will trigger the infinite recursion.
+        // The @Test(expected=...) annotation asserts that a StackOverflowError is thrown.
+        formatterWithCircularParser.parsePeriod("");
     }
 }
