e42a4ca10c42dee4c8954866a6f0f3626b84f63d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:28:45 +0800

    gemini processedAutoTestCases xnode_estest18.java

--- xnode_estest18.java
+++ xnode_estest18.java
@@ -1,31 +1,33 @@
 package org.apache.ibatis.parsing;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.List;
-import java.util.Locale;
+import static org.junit.Assert.assertEquals;
+
 import java.util.Properties;
-import java.util.function.Supplier;
 import javax.imageio.metadata.IIOMetadataNode;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
-import org.w3c.dom.Document;
+import org.junit.Test;
 import org.w3c.dom.Node;
-import org.xml.sax.ext.DefaultHandler2;
 
-public class XNode_ESTestTest18 extends XNode_ESTest_scaffolding {
+/**
+ * Test suite for the default value behavior of XNode's body parsing methods.
+ */
+public class XNodeTest {
 
-    @Test(timeout = 4000)
-    public void test017() throws Throwable {
-        Properties properties0 = new Properties();
-        IIOMetadataNode iIOMetadataNode0 = new IIOMetadataNode();
-        XNode xNode0 = new XNode((XPathParser) null, iIOMetadataNode0, properties0);
-        Integer integer0 = new Integer(0);
-        Integer integer1 = xNode0.getIntBody(integer0);
-        assertEquals(0, (int) integer1);
-    }
+  /**
+   * Verifies that getIntBody(defaultValue) returns the default value
+   * when the underlying XML node has no text content, resulting in a null body.
+   */
+  @Test
+  public void getIntBodyShouldReturnDefaultValueWhenNodeHasNoBody() {
+    // Arrange
+    // An IIOMetadataNode without any children or text content results in a null body for the XNode.
+    Node emptyNode = new IIOMetadataNode();
+    XNode xNode = new XNode(null, emptyNode, new Properties());
+    Integer defaultValue = 0;
+
+    // Act
+    Integer result = xNode.getIntBody(defaultValue);
+
+    // Assert
+    assertEquals("Should return the default value for a node with no body", defaultValue, result);
+  }
 }
