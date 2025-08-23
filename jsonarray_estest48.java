085bdc7c67430b3ee47c60e338bd562f1d527e8e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:56:10 +0800

    Original processedAutoTestCases jsonarray_estest48.java

---
+++ jsonarray_estest48.java
@@ -0,0 +1,30 @@
+package com.google.gson;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Iterator;
+import java.util.List;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class JsonArray_ESTestTest48 extends JsonArray_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test47() throws Throwable {
+        JsonArray jsonArray0 = new JsonArray();
+        // Undeclared exception!
+        try {
+            jsonArray0.addAll((JsonArray) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("com.google.gson.JsonArray", e);
+        }
+    }
+}
