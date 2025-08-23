b0218b82a9ba910bf86085fadba72367489b2217
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:36:52 +0800

    Original processedAutoTestCases abstractpartial_estest57.java

---
+++ abstractpartial_estest57.java
@@ -0,0 +1,49 @@
+package org.joda.time.base;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.Date;
+import java.util.Locale;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.util.MockGregorianCalendar;
+import org.joda.time.Chronology;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeField;
+import org.joda.time.DateTimeFieldType;
+import org.joda.time.DateTimeZone;
+import org.joda.time.DurationFieldType;
+import org.joda.time.Instant;
+import org.joda.time.LocalDate;
+import org.joda.time.LocalDateTime;
+import org.joda.time.LocalTime;
+import org.joda.time.MonthDay;
+import org.joda.time.Partial;
+import org.joda.time.ReadablePartial;
+import org.joda.time.Weeks;
+import org.joda.time.YearMonth;
+import org.joda.time.Years;
+import org.joda.time.chrono.CopticChronology;
+import org.joda.time.chrono.GJChronology;
+import org.joda.time.chrono.GregorianChronology;
+import org.joda.time.chrono.IslamicChronology;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.DateTimeParser;
+import org.joda.time.format.DateTimePrinter;
+import org.junit.runner.RunWith;
+
+public class AbstractPartial_ESTestTest57 extends AbstractPartial_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test56() throws Throwable {
+        GJChronology gJChronology0 = GJChronology.getInstanceUTC();
+        LocalDate localDate0 = new LocalDate((long) 0, (Chronology) gJChronology0);
+        DateTimeZone dateTimeZone0 = gJChronology0.getZone();
+        DateTime dateTime0 = new DateTime(0L, dateTimeZone0);
+        DateTime dateTime1 = localDate0.toDateTime(dateTime0);
+        assertNotSame(dateTime0, dateTime1);
+    }
+}
