033313e8b6cd84e2a3e702ec002493e915751238
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:06:36 +0800

    Original processedAutoTestCases optiongroup_estest16.java

---
+++ optiongroup_estest16.java
@@ -0,0 +1,19 @@
+package org.apache.commons.cli;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.Collection;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class OptionGroup_ESTestTest16 extends OptionGroup_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test15() throws Throwable {
+        OptionGroup optionGroup0 = new OptionGroup();
+        boolean boolean0 = optionGroup0.isRequired();
+        assertFalse(boolean0);
+    }
+}
