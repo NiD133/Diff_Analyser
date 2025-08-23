821937449b00972eeae4b2f02acb784d0bb58b83
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:19:22 +0800

    Original processedAutoTestCases symmetry010chronology_estest1.java

---
+++ symmetry010chronology_estest1.java
@@ -0,0 +1,54 @@
+package org.threeten.extra.chrono;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.time.Clock;
+import java.time.DateTimeException;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.time.Year;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.chrono.ChronoLocalDateTime;
+import java.time.chrono.ChronoZonedDateTime;
+import java.time.chrono.Era;
+import java.time.chrono.IsoEra;
+import java.time.chrono.JapaneseEra;
+import java.time.chrono.ThaiBuddhistEra;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAccessor;
+import java.time.temporal.UnsupportedTemporalTypeException;
+import java.time.temporal.ValueRange;
+import java.util.List;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.time.MockClock;
+import org.evosuite.runtime.mock.java.time.MockInstant;
+import org.evosuite.runtime.mock.java.time.MockLocalDate;
+import org.evosuite.runtime.mock.java.time.MockOffsetDateTime;
+import org.evosuite.runtime.mock.java.time.MockYear;
+import org.junit.runner.RunWith;
+
+public class Symmetry010Chronology_ESTestTest1 extends Symmetry010Chronology_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test00() throws Throwable {
+        Symmetry010Chronology symmetry010Chronology0 = Symmetry010Chronology.INSTANCE;
+        Symmetry010Date symmetry010Date0 = Symmetry010Date.ofYearDay(30, 14);
+        IsoEra isoEra0 = symmetry010Date0.getEra();
+        // Undeclared exception!
+        try {
+            symmetry010Chronology0.date((Era) isoEra0, 364, (-2552), 1769);
+            fail("Expecting exception: DateTimeException");
+        } catch (DateTimeException e) {
+            //
+            // Invalid value for MonthOfYear (valid values 1 - 12): -2552
+            //
+            verifyException("java.time.temporal.ValueRange", e);
+        }
+    }
+}
