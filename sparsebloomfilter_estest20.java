6f983d325356ab443254210c2af7a0f369a2768d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:17:00 +0800

    Original processedAutoTestCases sparsebloomfilter_estest20.java

---
+++ sparsebloomfilter_estest20.java
@@ -0,0 +1,31 @@
+package org.apache.commons.collections4.bloomfilter;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.function.IntPredicate;
+import java.util.function.LongPredicate;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class SparseBloomFilter_ESTestTest20 extends SparseBloomFilter_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test19() throws Throwable {
+        Shape shape0 = Shape.fromKM(18, 18);
+        SparseBloomFilter sparseBloomFilter0 = new SparseBloomFilter(shape0);
+        // Undeclared exception!
+        try {
+            sparseBloomFilter0.merge((BloomFilter<?>) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // other
+            //
+            verifyException("java.util.Objects", e);
+        }
+    }
+}
