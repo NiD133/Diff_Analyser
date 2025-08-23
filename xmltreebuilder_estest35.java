d68761ac6119a402178a71ba9037088fead62f62
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:28:05 +0800

    gemini processedAutoTestCases xmltreebuilder_estest35.java

--- xmltreebuilder_estest35.java
+++ xmltreebuilder_estest35.java
@@ -1,32 +1,26 @@
 package org.jsoup.parser;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.PipedReader;
-import java.io.PipedWriter;
-import java.io.Reader;
-import java.io.StringReader;
-import java.io.UncheckedIOException;
-import java.util.ArrayList;
-import java.util.NoSuchElementException;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.jsoup.nodes.Attributes;
-import org.jsoup.nodes.CDataNode;
-import org.jsoup.nodes.Comment;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.nodes.LeafNode;
-import org.jsoup.select.Elements;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class XmlTreeBuilder_ESTestTest35 extends XmlTreeBuilder_ESTest_scaffolding {
+/**
+ * Tests for the {@link XmlTreeBuilder} class.
+ */
+public class XmlTreeBuilderTest {
 
-    @Test(timeout = 4000)
-    public void test34() throws Throwable {
-        XmlTreeBuilder xmlTreeBuilder0 = new XmlTreeBuilder();
-        String string0 = xmlTreeBuilder0.defaultNamespace();
-        assertEquals("http://www.w3.org/XML/1998/namespace", string0);
+    /**
+     * Verifies that the defaultNamespace() method returns the correct, standard XML namespace URI.
+     */
+    @Test
+    public void defaultNamespaceReturnsCorrectXmlNamespaceUri() {
+        // Arrange
+        XmlTreeBuilder xmlTreeBuilder = new XmlTreeBuilder();
+        String expectedNamespace = "http://www.w3.org/XML/1998/namespace";
+
+        // Act
+        String actualNamespace = xmlTreeBuilder.defaultNamespace();
+
+        // Assert
+        assertEquals(expectedNamespace, actualNamespace);
     }
 }
