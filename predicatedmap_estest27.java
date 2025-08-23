c8e9ce66d0ece64762b1b98113b08262fa4fd906
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:09:28 +0800

    Original processedAutoTestCases predicatedmap_estest27.java

---
+++ predicatedmap_estest27.java
@@ -0,0 +1,45 @@
+package org.apache.commons.collections4.map;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.lang.reflect.Array;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import org.apache.commons.collections4.Predicate;
+import org.apache.commons.collections4.Transformer;
+import org.apache.commons.collections4.functors.AnyPredicate;
+import org.apache.commons.collections4.functors.ComparatorPredicate;
+import org.apache.commons.collections4.functors.ConstantTransformer;
+import org.apache.commons.collections4.functors.EqualPredicate;
+import org.apache.commons.collections4.functors.ExceptionPredicate;
+import org.apache.commons.collections4.functors.IdentityPredicate;
+import org.apache.commons.collections4.functors.NonePredicate;
+import org.apache.commons.collections4.functors.NotNullPredicate;
+import org.apache.commons.collections4.functors.NullPredicate;
+import org.apache.commons.collections4.functors.OnePredicate;
+import org.apache.commons.collections4.functors.OrPredicate;
+import org.apache.commons.collections4.functors.PredicateTransformer;
+import org.apache.commons.collections4.functors.TransformerPredicate;
+import org.apache.commons.collections4.functors.TruePredicate;
+import org.apache.commons.collections4.functors.UniquePredicate;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class PredicatedMap_ESTestTest27 extends PredicatedMap_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test26() throws Throwable {
+        UniquePredicate<Object> uniquePredicate0 = new UniquePredicate<Object>();
+        HashMap<Transformer<Object, Integer>, Object> hashMap0 = new HashMap<Transformer<Object, Integer>, Object>();
+        PredicatedMap<Transformer<Object, Integer>, Object> predicatedMap0 = PredicatedMap.predicatedMap((Map<Transformer<Object, Integer>, Object>) hashMap0, (Predicate<? super Transformer<Object, Integer>>) uniquePredicate0, (Predicate<? super Object>) uniquePredicate0);
+        HashMap hashMap1 = (HashMap) predicatedMap0.checkSetValue(hashMap0);
+        assertTrue(hashMap1.isEmpty());
+    }
+}
