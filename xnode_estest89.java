1c32108eea8fdbb54667fdb938cf3523059c8cce
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:29:17 +0800

    gemini processedAutoTestCases xnode_estest89.java

--- xnode_estest89.java
+++ xnode_estest89.java
@@ -1,31 +1,38 @@
 package org.apache.ibatis.parsing;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.List;
-import java.util.Locale;
+import static org.junit.Assert.assertNull;
+
 import java.util.Properties;
-import java.util.function.Supplier;
 import javax.imageio.metadata.IIOMetadataNode;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
-import org.xml.sax.ext.DefaultHandler2;
 
-public class XNode_ESTestTest89 extends XNode_ESTest_scaffolding {
+/**
+ * Test suite for the {@link XNode} class.
+ */
+public class XNodeTest {
 
-    @Test(timeout = 4000)
-    public void test088() throws Throwable {
-        IIOMetadataNode iIOMetadataNode0 = new IIOMetadataNode();
-        XPathParser xPathParser0 = new XPathParser((Document) null, false);
-        Properties properties0 = new Properties();
-        XNode xNode0 = new XNode(xPathParser0, iIOMetadataNode0, properties0);
-        String string0 = xNode0.getName();
-        assertNull(string0);
+    /**
+     * Verifies that XNode.getName() returns null if the underlying DOM Node's name is null.
+     */
+    @Test
+    public void getNameShouldReturnNullWhenWrappedNodeNameIsNull() {
+        // Arrange
+        // An IIOMetadataNode is used here because its default constructor creates a
+        // DOM Node implementation for which getNodeName() returns null. This allows us
+        // to test how XNode handles such a case.
+        Node nodeWithNullName = new IIOMetadataNode();
+
+        // Create minimal dependencies required by the XNode constructor.
+        XPathParser dummyParser = new XPathParser((Document) null, false);
+        Properties variables = new Properties();
+        XNode xNode = new XNode(dummyParser, nodeWithNullName, variables);
+
+        // Act
+        String name = xNode.getName();
+
+        // Assert
+        assertNull("The name should be null when the underlying node's name is null.", name);
     }
 }
