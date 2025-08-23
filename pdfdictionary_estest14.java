7ef4616dc2ac199bdafcd5f249472f2365feffba
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:07:48 +0800

    Original processedAutoTestCases pdfdictionary_estest14.java

---
+++ pdfdictionary_estest14.java
@@ -0,0 +1,36 @@
+package com.itextpdf.text.pdf;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.collection.PdfCollectionField;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PipedOutputStream;
+import java.util.LinkedHashMap;
+import java.util.Set;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockPrintStream;
+import org.junit.runner.RunWith;
+
+public class PdfDictionary_ESTestTest14 extends PdfDictionary_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test13() throws Throwable {
+        PdfResources pdfResources0 = new PdfResources();
+        PdfWriter pdfWriter0 = new PdfWriter();
+        // Undeclared exception!
+        try {
+            pdfResources0.toPdf(pdfWriter0, (OutputStream) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("com.itextpdf.text.pdf.PdfDictionary", e);
+        }
+    }
+}
