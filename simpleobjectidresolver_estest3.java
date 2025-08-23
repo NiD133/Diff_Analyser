21bbe3697cf487eb8ea5c7e6c51a12109fe2a0ea
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:16:22 +0800

    gemini processedAutoTestCases simpleobjectidresolver_estest3.java

--- simpleobjectidresolver_estest3.java
+++ simpleobjectidresolver_estest3.java
@@ -1,28 +1,37 @@
 package com.fasterxml.jackson.annotation;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class SimpleObjectIdResolver_ESTestTest3 extends SimpleObjectIdResolver_ESTest_scaffolding {
+/**
+ * Tests for {@link SimpleObjectIdResolver}.
+ */
+public class SimpleObjectIdResolverTest {
 
-    @Test(timeout = 4000)
-    public void test2() throws Throwable {
-        SimpleObjectIdResolver simpleObjectIdResolver0 = new SimpleObjectIdResolver();
-        Object object0 = new Object();
-        simpleObjectIdResolver0.bindItem((ObjectIdGenerator.IdKey) null, object0);
-        // Undeclared exception!
-        try {
-            simpleObjectIdResolver0.bindItem((ObjectIdGenerator.IdKey) null, (Object) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("com.fasterxml.jackson.annotation.SimpleObjectIdResolver", e);
-        }
+    /**
+     * This test verifies the behavior when attempting to bind an ID that has already been bound.
+     * Specifically, it tests the edge case where the ID is {@code null}.
+     * <p>
+     * The expected {@link NullPointerException} is a side-effect of the internal error-reporting
+     * logic, which attempts to access properties of the ID object to create a detailed
+     * error message. When the ID is null, this access results in an NPE.
+     */
+    @Test(expected = NullPointerException.class)
+    public void bindItem_whenRebindingExistingNullId_shouldThrowNullPointerException() {
+        // Arrange
+        SimpleObjectIdResolver resolver = new SimpleObjectIdResolver();
+        Object initialObject = new Object();
+        Object secondObject = new Object();
+
+        // Bind the null ID to an initial object. This should succeed.
+        resolver.bindItem(null, initialObject);
+
+        // Act
+        // Now, attempt to bind the same null ID to a different object.
+        // This action is expected to fail.
+        resolver.bindItem(null, secondObject);
+
+        // Assert
+        // The test expects a NullPointerException, which is declared in the @Test annotation.
+        // If no exception is thrown, the test will fail automatically.
     }
 }
