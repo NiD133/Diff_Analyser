147cb3fc96e722cf4fd96036601665437526c089
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:27:06 +0800

    gemini processedAutoTestCases weeks_estest17.java

--- weeks_estest17.java
+++ weeks_estest17.java
@@ -2,17 +2,25 @@ package org.joda.time;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class Weeks_ESTestTest17 extends Weeks_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link Weeks} class.
+ */
+public class WeeksTest {
 
-    @Test(timeout = 4000)
-    public void test16() throws Throwable {
-        Weeks weeks0 = Weeks.ZERO;
-        Weeks weeks1 = weeks0.negated();
-        assertEquals(0, weeks1.getWeeks());
+    /**
+     * Tests that negating a Weeks object representing zero weeks
+     * results in a Weeks object that is also zero.
+     */
+    @Test
+    public void negated_onZero_returnsZero() {
+        // Arrange: Create a Weeks object representing zero.
+        Weeks zeroWeeks = Weeks.ZERO;
+
+        // Act: Negate the zero-week object.
+        Weeks result = zeroWeeks.negated();
+
+        // Assert: The result should still be equivalent to zero weeks.
+        assertEquals(Weeks.ZERO, result);
     }
 }
