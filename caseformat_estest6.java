25b992100f9a42413dfc4aa2626783aea717c14d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:41:43 +0800

    gemini processedAutoTestCases caseformat_estest6.java

--- caseformat_estest6.java
+++ caseformat_estest6.java
@@ -1,16 +1,25 @@
 package com.google.common.base;
 
+import static org.junit.Assert.assertSame;
+
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class CaseFormat_ESTestTest6 extends CaseFormat_ESTest_scaffolding {
+/**
+ * Tests for {@link CaseFormat}.
+ */
+public class CaseFormatTest {
 
-    @Test(timeout = 4000)
-    public void test05() throws Throwable {
-        CaseFormat.valueOf("LOWER_CAMEL");
+    @Test
+    public void valueOf_givenValidName_returnsCorrespondingEnum() {
+        // Arrange
+        String lowerCamelName = "LOWER_CAMEL";
+        CaseFormat expectedFormat = CaseFormat.LOWER_CAMEL;
+
+        // Act
+        CaseFormat actualFormat = CaseFormat.valueOf(lowerCamelName);
+
+        // Assert
+        assertSame("valueOf should return the correct enum constant for a valid name.",
+                expectedFormat, actualFormat);
     }
 }
