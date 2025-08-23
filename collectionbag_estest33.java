ef6bb5166a7206d273a571d2a72dbdf9d5d4b8ff
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:44:13 +0800

    gemini processedAutoTestCases collectionbag_estest33.java

--- collectionbag_estest33.java
+++ collectionbag_estest33.java
@@ -1,47 +1,23 @@
 package org.apache.commons.collections4.bag;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.ConcurrentModificationException;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Stack;
-import java.util.TreeSet;
+import static org.junit.Assert.assertThrows;
+
 import org.apache.commons.collections4.Bag;
-import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.SortedBag;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.FalsePredicate;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.MapTransformer;
-import org.apache.commons.collections4.functors.NullIsExceptionPredicate;
-import org.apache.commons.collections4.functors.TransformerPredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class CollectionBag_ESTestTest33 extends CollectionBag_ESTest_scaffolding {
+/**
+ * Unit tests for {@link CollectionBag}.
+ */
+public class CollectionBagTest {
 
-    @Test(timeout = 4000)
-    public void test32() throws Throwable {
-        CollectionBag<Object> collectionBag0 = null;
-        try {
-            collectionBag0 = new CollectionBag<Object>((Bag<Object>) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // collection
-            //
-            verifyException("java.util.Objects", e);
-        }
+    /**
+     * Tests that the CollectionBag constructor throws a NullPointerException
+     * when the decorated bag is null, as per its contract.
+     */
+    @Test
+    public void constructorShouldThrowNullPointerExceptionWhenBagIsNull() {
+        // The constructor is documented to throw a NullPointerException if the decorated bag is null.
+        // This test confirms that behavior.
+        assertThrows(NullPointerException.class, () -> new CollectionBag<Object>((Bag<Object>) null));
     }
 }
