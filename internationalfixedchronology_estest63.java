c5170919403ad4460e94b1f0000ffb46fddd7193
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:54:36 +0800

    Original processedAutoTestCases internationalfixedchronology_estest63.java

---
+++ internationalfixedchronology_estest63.java
@@ -0,0 +1,47 @@
+package org.threeten.extra.chrono;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.time.Clock;
+import java.time.DateTimeException;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.Period;
+import java.time.Year;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.chrono.ChronoLocalDateTime;
+import java.time.chrono.ChronoZonedDateTime;
+import java.time.chrono.Era;
+import java.time.chrono.IsoEra;
+import java.time.chrono.JapaneseEra;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAccessor;
+import java.time.temporal.TemporalAmount;
+import java.time.temporal.UnsupportedTemporalTypeException;
+import java.time.temporal.ValueRange;
+import java.util.List;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.System;
+import org.evosuite.runtime.mock.java.time.MockClock;
+import org.evosuite.runtime.mock.java.time.MockInstant;
+import org.evosuite.runtime.mock.java.time.MockLocalDateTime;
+import org.evosuite.runtime.mock.java.time.MockYear;
+import org.evosuite.runtime.mock.java.time.MockZonedDateTime;
+import org.junit.runner.RunWith;
+
+public class InternationalFixedChronology_ESTestTest63 extends InternationalFixedChronology_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test62() throws Throwable {
+        InternationalFixedChronology internationalFixedChronology0 = InternationalFixedChronology.INSTANCE;
+        boolean boolean0 = internationalFixedChronology0.isLeapYear(365000000L);
+        assertTrue(boolean0);
+    }
+}
