da35de97e80af2eacca266cfed86da1b0dc4964b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:35:51 +0800

    gemini processedManualTestCases weekstest17.java

--- weekstest17.java
+++ weekstest17.java
@@ -1,42 +1,36 @@
 package org.joda.time;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
-public class WeeksTestTest17 extends TestCase {
+/**
+ * Test cases for the {@link Weeks} class, focusing on conversion methods.
+ */
+public class WeeksTest {
 
-    // (before the late 90's they were all over the place)
-    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
+    private static final int WEEKS_IN_A_DAY = 7;
+    private static final int HOURS_IN_A_DAY = 24;
+    private static final int MINUTES_IN_AN_HOUR = 60;
 
-    public static void main(String[] args) {
-        junit.textui.TestRunner.run(suite());
+    @Test
+    public void toStandardMinutes_shouldConvertWeeksToMinutes() {
+        // Arrange
+        Weeks twoWeeks = Weeks.weeks(2);
+        int expectedMinutesValue = 2 * WEEKS_IN_A_DAY * HOURS_IN_A_DAY * MINUTES_IN_AN_HOUR;
+        Minutes expectedMinutes = Minutes.minutes(expectedMinutesValue);
+
+        // Act
+        Minutes actualMinutes = twoWeeks.toStandardMinutes();
+
+        // Assert
+        assertEquals(expectedMinutes, actualMinutes);
     }
 
-    public static TestSuite suite() {
-        return new TestSuite(TestWeeks.class);
-    }
+    @Test(expected = ArithmeticException.class)
+    public void toStandardMinutes_shouldThrowExceptionWhenResultOverflows() {
+        // Act: This should throw an ArithmeticException due to integer overflow.
+        Weeks.MAX_VALUE.toStandardMinutes();
 
-    @Override
-    protected void setUp() throws Exception {
-    }
-
-    @Override
-    protected void tearDown() throws Exception {
-    }
-
-    public void testToStandardMinutes() {
-        Weeks test = Weeks.weeks(2);
-        Minutes expected = Minutes.minutes(2 * 7 * 24 * 60);
-        assertEquals(expected, test.toStandardMinutes());
-        try {
-            Weeks.MAX_VALUE.toStandardMinutes();
-            fail();
-        } catch (ArithmeticException ex) {
-            // expected
-        }
+        // Assert: The 'expected' parameter of the @Test annotation handles the assertion.
     }
 }
