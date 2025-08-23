a8c26375b851fb46273c98ee409afb4eb37abff5
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:23:34 +0800

    Original processedAutoTestCases transformedsortedmap_estest4.java

---
+++ transformedsortedmap_estest4.java
@@ -0,0 +1,44 @@
+package org.apache.commons.collections4.map;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.lang.reflect.Array;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.NoSuchElementException;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import org.apache.commons.collections4.Closure;
+import org.apache.commons.collections4.Factory;
+import org.apache.commons.collections4.Predicate;
+import org.apache.commons.collections4.Transformer;
+import org.apache.commons.collections4.functors.ChainedTransformer;
+import org.apache.commons.collections4.functors.ConstantFactory;
+import org.apache.commons.collections4.functors.ConstantTransformer;
+import org.apache.commons.collections4.functors.ExceptionTransformer;
+import org.apache.commons.collections4.functors.FactoryTransformer;
+import org.apache.commons.collections4.functors.InvokerTransformer;
+import org.apache.commons.collections4.functors.MapTransformer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class TransformedSortedMap_ESTestTest4 extends TransformedSortedMap_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test03() throws Throwable {
+        TreeMap<Integer, Integer> treeMap0 = new TreeMap<Integer, Integer>();
+        Integer integer0 = new Integer(850);
+        Transformer<Object, Integer> transformer0 = ConstantTransformer.constantTransformer(integer0);
+        TransformedSortedMap<Integer, Integer> transformedSortedMap0 = TransformedSortedMap.transformingSortedMap((SortedMap<Integer, Integer>) treeMap0, (Transformer<? super Integer, ? extends Integer>) null, (Transformer<? super Integer, ? extends Integer>) transformer0);
+        treeMap0.put(integer0, integer0);
+        SortedMap<Integer, Integer> sortedMap0 = transformedSortedMap0.tailMap(integer0);
+        assertFalse(sortedMap0.isEmpty());
+    }
+}
