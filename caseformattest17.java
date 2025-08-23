e4245059802ede33277d51235236245083053810
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:26:21 +0800

    gemini processedManualTestCases caseformattest17.java

--- caseformattest17.java
+++ caseformattest17.java
@@ -1,24 +1,31 @@
 package com.google.common.base;
 
 import static com.google.common.base.CaseFormat.LOWER_CAMEL;
-import static com.google.common.base.CaseFormat.LOWER_HYPHEN;
-import static com.google.common.base.CaseFormat.LOWER_UNDERSCORE;
-import static com.google.common.base.CaseFormat.UPPER_CAMEL;
 import static com.google.common.base.CaseFormat.UPPER_UNDERSCORE;
 import static com.google.common.truth.Truth.assertThat;
-import static com.google.common.truth.Truth.assertWithMessage;
-import com.google.common.annotations.GwtCompatible;
-import com.google.common.annotations.GwtIncompatible;
-import com.google.common.annotations.J2ktIncompatible;
-import com.google.common.testing.NullPointerTester;
-import com.google.common.testing.SerializableTester;
+
 import junit.framework.TestCase;
-import org.jspecify.annotations.NullUnmarked;
 
-public class CaseFormatTestTest17 extends TestCase {
+/**
+ * Tests for conversions from {@link CaseFormat#LOWER_CAMEL} to {@link CaseFormat#UPPER_UNDERSCORE}.
+ */
+public class CaseFormatTest extends TestCase {
 
-    public void testLowerCamelToUpperUnderscore() {
-        assertThat(LOWER_CAMEL.to(UPPER_UNDERSCORE, "foo")).isEqualTo("FOO");
-        assertThat(LOWER_CAMEL.to(UPPER_UNDERSCORE, "fooBar")).isEqualTo("FOO_BAR");
-    }
+  /**
+   * Tests the conversion of a single-word string, which should result in an all-caps version of
+   * the same word.
+   */
+  public void testLowerCamelToUpperUnderscore_convertsSingleWord() {
+    String converted = LOWER_CAMEL.to(UPPER_UNDERSCORE, "foo");
+    assertThat(converted).isEqualTo("FOO");
+  }
+
+  /**
+   * Tests the conversion of a multi-word string, which should result in an underscore-separated,
+   * all-caps string.
+   */
+  public void testLowerCamelToUpperUnderscore_convertsMultipleWords() {
+    String converted = LOWER_CAMEL.to(UPPER_UNDERSCORE, "fooBar");
+    assertThat(converted).isEqualTo("FOO_BAR");
+  }
 }
