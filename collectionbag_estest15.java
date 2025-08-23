a9de244ad954bd6f048c89a3393ae3872e2e6e7c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:44:07 +0800

    gemini processedAutoTestCases collectionbag_estest15.java

--- collectionbag_estest15.java
+++ collectionbag_estest15.java
@@ -1,51 +1,34 @@
 package org.apache.commons.collections4.bag;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.ConcurrentModificationException;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Stack;
-import java.util.TreeSet;
 import org.apache.commons.collections4.Bag;
-import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.SortedBag;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.FalsePredicate;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.MapTransformer;
-import org.apache.commons.collections4.functors.NullIsExceptionPredicate;
-import org.apache.commons.collections4.functors.TransformerPredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class CollectionBag_ESTestTest15 extends CollectionBag_ESTest_scaffolding {
+import java.util.ConcurrentModificationException;
 
-    @Test(timeout = 4000)
-    public void test14() throws Throwable {
-        TreeBag<Integer> treeBag0 = new TreeBag<Integer>();
-        Integer integer0 = new Integer(273);
-        CollectionBag<Integer> collectionBag0 = new CollectionBag<Integer>(treeBag0);
-        collectionBag0.add(integer0, 273);
-        // Undeclared exception!
-        try {
-            collectionBag0.addAll(treeBag0);
-            fail("Expecting exception: ConcurrentModificationException");
-        } catch (ConcurrentModificationException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.commons.collections4.bag.AbstractMapBag$BagIterator", e);
-        }
+/**
+ * Test suite for {@link CollectionBag}.
+ */
+public class CollectionBagTest {
+
+    /**
+     * Tests that attempting to add a bag's contents to itself via {@code addAll}
+     * results in a {@link ConcurrentModificationException}.
+     * <p>
+     * This is expected behavior when a collection is modified while it is being iterated over,
+     * which occurs when a bag is the source and destination for an {@code addAll} operation.
+     */
+    @Test(expected = ConcurrentModificationException.class)
+    public void addAll_whenAddingBagToItself_throwsConcurrentModificationException() {
+        // Arrange
+        // Create a bag, add an element, and wrap it in a CollectionBag decorator.
+        Bag<String> underlyingBag = new TreeBag<>();
+        Bag<String> collectionBag = new CollectionBag<>(underlyingBag);
+        collectionBag.add("An element");
+
+        // Act
+        // Attempt to add all elements from the underlying bag back into the decorated bag.
+        // This will fail because the addAll operation iterates over the underlying bag
+        // while simultaneously trying to modify it.
+        collectionBag.addAll(underlyingBag);
     }
 }
