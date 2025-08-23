8d1d4b39d7d7bf1c7114105893ff13f723608154
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:53:09 +0800

    gemini processedAutoTestCases hashcodeandequalssafeset_estest4.java

--- hashcodeandequalssafeset_estest4.java
+++ hashcodeandequalssafeset_estest4.java
@@ -1,29 +1,23 @@
 package org.mockito.internal.util.collections;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.LinkedHashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Vector;
-import java.util.function.Predicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class HashCodeAndEqualsSafeSet_ESTestTest4 extends HashCodeAndEqualsSafeSet_ESTest_scaffolding {
+/**
+ * Tests for {@link HashCodeAndEqualsSafeSet}.
+ */
+public class HashCodeAndEqualsSafeSetTest {
 
-    @Test(timeout = 4000)
-    public void test03() throws Throwable {
-        HashCodeAndEqualsSafeSet hashCodeAndEqualsSafeSet0 = new HashCodeAndEqualsSafeSet();
-        Object object0 = new Object();
-        hashCodeAndEqualsSafeSet0.add(object0);
-        int int0 = hashCodeAndEqualsSafeSet0.size();
-        assertEquals(1, int0);
+    @Test
+    public void size_shouldReturnOne_afterAddingOneElement() {
+        // Arrange
+        HashCodeAndEqualsSafeSet safeSet = new HashCodeAndEqualsSafeSet();
+        Object element = new Object();
+
+        // Act
+        safeSet.add(element);
+
+        // Assert
+        assertEquals(1, safeSet.size());
     }
 }
