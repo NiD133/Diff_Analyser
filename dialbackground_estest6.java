c92f4d32ef06a0334aca59cedd5ab27e254940a4
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:48:07 +0800

    Original processedAutoTestCases dialbackground_estest6.java

---
+++ dialbackground_estest6.java
@@ -0,0 +1,36 @@
+package org.jfree.chart.plot.dial;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.awt.Color;
+import java.awt.GradientPaint;
+import java.awt.Graphics2D;
+import java.awt.Paint;
+import java.awt.Rectangle;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.jfree.chart.ChartRenderingInfo;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.entity.EntityCollection;
+import org.jfree.chart.plot.FastScatterPlot;
+import org.jfree.chart.plot.pie.PiePlot;
+import org.jfree.chart.util.GradientPaintTransformType;
+import org.jfree.chart.util.GradientPaintTransformer;
+import org.jfree.chart.util.StandardGradientPaintTransformer;
+import org.jfree.data.general.DefaultValueDataset;
+import org.junit.runner.RunWith;
+
+public class DialBackground_ESTestTest6 extends DialBackground_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test05() throws Throwable {
+        DialBackground dialBackground0 = new DialBackground();
+        Color color0 = (Color) PiePlot.DEFAULT_LABEL_BACKGROUND_PAINT;
+        DialBackground dialBackground1 = new DialBackground(color0);
+        boolean boolean0 = dialBackground0.equals(dialBackground1);
+        assertFalse(boolean0);
+    }
+}
