7372ce67867369a3b2e9f6948de06238ffa72936
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:34:09 +0800

    gemini processedManualTestCases stringconvertertest21.java

--- stringconvertertest21.java
+++ stringconvertertest21.java
@@ -1,87 +1,35 @@
 package org.joda.time.convert;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-import java.util.Arrays;
-import java.util.Locale;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
-import org.joda.time.Chronology;
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
-import org.joda.time.MutableInterval;
 import org.joda.time.MutablePeriod;
 import org.joda.time.PeriodType;
-import org.joda.time.TimeOfDay;
-import org.joda.time.chrono.BuddhistChronology;
-import org.joda.time.chrono.ISOChronology;
-import org.joda.time.chrono.JulianChronology;
 
-public class StringConverterTestTest21 extends TestCase {
+/**
+ * Unit tests for {@link StringConverter}.
+ * This class focuses on the setInto(ReadWritablePeriod, ...) method.
+ */
+public class StringConverterTest extends TestCase {
 
-    private static final DateTimeZone ONE_HOUR = DateTimeZone.forOffsetHours(1);
+    /**
+     * Tests that setInto correctly parses a complete ISO 8601 period string
+     * and populates a MutablePeriod instance.
+     */
+    public void testSetInto_populatesMutablePeriod_fromCompleteISOString() {
+        // Arrange
+        // An ISO 8601 format period string with year, week, day, and time components.
+        final String isoPeriodString = "P2Y4W3DT12H24M48S";
+        final MutablePeriod period = new MutablePeriod(PeriodType.yearWeekDayTime());
+        
+        // Act
+        StringConverter.INSTANCE.setInto(period, isoPeriodString, null);
 
-    private static final DateTimeZone SIX = DateTimeZone.forOffsetHours(6);
-
-    private static final DateTimeZone SEVEN = DateTimeZone.forOffsetHours(7);
-
-    private static final DateTimeZone EIGHT = DateTimeZone.forOffsetHours(8);
-
-    private static final DateTimeZone UTC = DateTimeZone.UTC;
-
-    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
-
-    private static final DateTimeZone LONDON = DateTimeZone.forID("Europe/London");
-
-    private static final Chronology ISO_EIGHT = ISOChronology.getInstance(EIGHT);
-
-    private static final Chronology ISO_PARIS = ISOChronology.getInstance(PARIS);
-
-    private static final Chronology ISO_LONDON = ISOChronology.getInstance(LONDON);
-
-    private static Chronology ISO;
-
-    private static Chronology JULIAN;
-
-    private DateTimeZone zone = null;
-
-    private Locale locale = null;
-
-    public static void main(String[] args) {
-        junit.textui.TestRunner.run(suite());
-    }
-
-    public static TestSuite suite() {
-        return new TestSuite(TestStringConverter.class);
-    }
-
-    @Override
-    protected void setUp() throws Exception {
-        zone = DateTimeZone.getDefault();
-        locale = Locale.getDefault();
-        DateTimeZone.setDefault(LONDON);
-        Locale.setDefault(Locale.UK);
-        JULIAN = JulianChronology.getInstance();
-        ISO = ISOChronology.getInstance();
-    }
-
-    @Override
-    protected void tearDown() throws Exception {
-        DateTimeZone.setDefault(zone);
-        Locale.setDefault(locale);
-        zone = null;
-    }
-
-    public void testSetIntoPeriod_Object2() throws Exception {
-        MutablePeriod m = new MutablePeriod(PeriodType.yearWeekDayTime());
-        StringConverter.INSTANCE.setInto(m, "P2Y4W3DT12H24M48S", null);
-        assertEquals(2, m.getYears());
-        assertEquals(4, m.getWeeks());
-        assertEquals(3, m.getDays());
-        assertEquals(12, m.getHours());
-        assertEquals(24, m.getMinutes());
-        assertEquals(48, m.getSeconds());
-        assertEquals(0, m.getMillis());
+        // Assert
+        assertEquals("Years should match", 2, period.getYears());
+        assertEquals("Weeks should match", 4, period.getWeeks());
+        assertEquals("Days should match", 3, period.getDays());
+        assertEquals("Hours should match", 12, period.getHours());
+        assertEquals("Minutes should match", 24, period.getMinutes());
+        assertEquals("Seconds should match", 48, period.getSeconds());
+        assertEquals("Millis should be zero", 0, period.getMillis());
     }
 }
