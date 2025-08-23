c172b109071afa2821398227f324dfd17c7e3fdb
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:23:36 +0800

    gemini processedAutoTestCases transformedsortedmap_estest9.java

--- transformedsortedmap_estest9.java
+++ transformedsortedmap_estest9.java
@@ -1,44 +1,44 @@
 package org.apache.commons.collections4.map;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.NavigableMap;
-import java.util.NoSuchElementException;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+
 import java.util.SortedMap;
 import java.util.TreeMap;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.Factory;
-import org.apache.commons.collections4.Predicate;
 import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.ChainedTransformer;
-import org.apache.commons.collections4.functors.ConstantFactory;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.FactoryTransformer;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.MapTransformer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.apache.commons.collections4.functors.NOPTransformer;
+import org.junit.Test;
 
-public class TransformedSortedMap_ESTestTest9 extends TransformedSortedMap_ESTest_scaffolding {
+/**
+ * Contains tests for the {@link TransformedSortedMap#getSortedMap()} method.
+ */
+public class TransformedSortedMap_ESTestTest9 {
 
-    @Test(timeout = 4000)
-    public void test08() throws Throwable {
-        TreeMap<Closure<Object>, Object> treeMap0 = new TreeMap<Closure<Object>, Object>();
-        Class<Integer>[] classArray0 = (Class<Integer>[]) Array.newInstance(Class.class, 7);
-        InvokerTransformer<Object, Closure<Object>> invokerTransformer0 = new InvokerTransformer<Object, Closure<Object>>("org.apache.commons.collections4.functors.ComparatorPredicate", classArray0, classArray0);
-        TransformedSortedMap<Closure<Object>, Object> transformedSortedMap0 = new TransformedSortedMap<Closure<Object>, Object>(treeMap0, invokerTransformer0, invokerTransformer0);
-        TransformedSortedMap<Closure<Object>, Object> transformedSortedMap1 = TransformedSortedMap.transformingSortedMap((SortedMap<Closure<Object>, Object>) transformedSortedMap0, (Transformer<? super Closure<Object>, ? extends Closure<Object>>) invokerTransformer0, (Transformer<? super Object, ?>) invokerTransformer0);
-        SortedMap<Closure<Object>, Object> sortedMap0 = transformedSortedMap1.getSortedMap();
-        assertEquals(0, sortedMap0.size());
+    /**
+     * Tests that getSortedMap() returns the map instance it directly decorates,
+     * even when that decorated map is another TransformedSortedMap.
+     */
+    @Test
+    public void testGetSortedMapReturnsImmediateDecoratedMap() {
+        // Arrange
+        final SortedMap<String, Integer> originalMap = new TreeMap<>();
+        final Transformer<Object, Object> nopTransformer = NOPTransformer.nopTransformer();
+
+        // Create the first layer of decoration over the original map.
+        final TransformedSortedMap<String, Integer> firstDecorator =
+                new TransformedSortedMap<>(originalMap, nopTransformer, nopTransformer);
+
+        // Create a second layer of decoration over the first decorator.
+        final TransformedSortedMap<String, Integer> secondDecorator =
+                TransformedSortedMap.transformingSortedMap(firstDecorator, nopTransformer, nopTransformer);
+
+        // Act
+        // Retrieve the map that the second decorator is wrapping.
+        final SortedMap<String, Integer> retrievedMap = secondDecorator.getSortedMap();
+
+        // Assert
+        // The method should return the map it directly wraps, which is the first decorator.
+        assertSame("getSortedMap should return the immediate decorated map instance", firstDecorator, retrievedMap);
+        assertEquals("The retrieved map should be empty, same as the original", 0, retrievedMap.size());
     }
 }
