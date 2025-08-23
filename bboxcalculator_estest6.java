f11aa43acff48f573c06660c67befe782d0b99dc
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:39:03 +0800

    Original processedAutoTestCases bboxcalculator_estest6.java

---
+++ bboxcalculator_estest6.java
@@ -0,0 +1,28 @@
+package org.locationtech.spatial4j.shape.impl;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.HashMap;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+import org.locationtech.spatial4j.context.SpatialContext;
+import org.locationtech.spatial4j.context.SpatialContextFactory;
+import org.locationtech.spatial4j.distance.GeodesicSphereDistCalc;
+import org.locationtech.spatial4j.shape.Point;
+import org.locationtech.spatial4j.shape.Rectangle;
+
+public class BBoxCalculator_ESTestTest6 extends BBoxCalculator_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test05() throws Throwable {
+        SpatialContextFactory spatialContextFactory0 = new SpatialContextFactory();
+        SpatialContext spatialContext0 = spatialContextFactory0.newSpatialContext();
+        BBoxCalculator bBoxCalculator0 = new BBoxCalculator(spatialContext0);
+        bBoxCalculator0.expandRange((-388.0), 1.0, (-388.0), (-2148.3));
+        double double0 = bBoxCalculator0.getMinX();
+        assertEquals((-388.0), bBoxCalculator0.getMinY(), 0.01);
+        assertEquals((-388.0), double0, 0.01);
+    }
+}
