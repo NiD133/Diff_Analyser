50c5e2f87cabaa8cdd9df6b432e38ac0ae8dc333
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:32:42 +0800

    Original processedManualTestCases queryparsertest15.java

---
+++ queryparsertest15.java
@@ -0,0 +1,19 @@
+package org.jsoup.select;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.junit.jupiter.api.Test;
+import static org.jsoup.select.EvaluatorDebug.asElement;
+import static org.jsoup.select.EvaluatorDebug.sexpr;
+import static org.jsoup.select.Selector.SelectorParseException;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class QueryParserTestTest15 {
+
+    @Test
+    void trailingParens() {
+        SelectorParseException exception = assertThrows(SelectorParseException.class, () -> QueryParser.parse("div:has(p))"));
+        assertEquals("Could not parse query 'div:has(p))': unexpected token at ')'", exception.getMessage());
+    }
+}
