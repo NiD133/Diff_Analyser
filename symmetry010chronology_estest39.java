6463f65cd22ee4a53be172c0b5f77d44ccb1bb0c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:19:34 +0800

    Original processedAutoTestCases symmetry010chronology_estest39.java

---
+++ symmetry010chronology_estest39.java
@@ -0,0 +1,45 @@
+package org.threeten.extra.chrono;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.time.Clock;
+import java.time.DateTimeException;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.time.Year;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.chrono.ChronoLocalDateTime;
+import java.time.chrono.ChronoZonedDateTime;
+import java.time.chrono.Era;
+import java.time.chrono.IsoEra;
+import java.time.chrono.JapaneseEra;
+import java.time.chrono.ThaiBuddhistEra;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAccessor;
+import java.time.temporal.UnsupportedTemporalTypeException;
+import java.time.temporal.ValueRange;
+import java.util.List;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.time.MockClock;
+import org.evosuite.runtime.mock.java.time.MockInstant;
+import org.evosuite.runtime.mock.java.time.MockLocalDate;
+import org.evosuite.runtime.mock.java.time.MockOffsetDateTime;
+import org.evosuite.runtime.mock.java.time.MockYear;
+import org.junit.runner.RunWith;
+
+public class Symmetry010Chronology_ESTestTest39 extends Symmetry010Chronology_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test38() throws Throwable {
+        Symmetry010Chronology symmetry010Chronology0 = Symmetry010Chronology.INSTANCE;
+        ChronoField chronoField0 = ChronoField.YEAR;
+        ValueRange valueRange0 = symmetry010Chronology0.range(chronoField0);
+        assertNotNull(valueRange0);
+    }
+}
