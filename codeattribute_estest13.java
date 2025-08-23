a2d1f57532d4f91f10322b6cf67446680e4486b6
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:43:47 +0800

    Original processedAutoTestCases codeattribute_estest13.java

---
+++ codeattribute_estest13.java
@@ -0,0 +1,37 @@
+package org.apache.commons.compress.harmony.unpack200.bytecode;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.PipedOutputStream;
+import java.util.LinkedList;
+import java.util.List;
+import org.apache.commons.compress.harmony.unpack200.Segment;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockFileOutputStream;
+import org.junit.runner.RunWith;
+
+public class CodeAttribute_ESTestTest13 extends CodeAttribute_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test12() throws Throwable {
+        byte[] byteArray0 = new byte[9];
+        byteArray0[2] = (byte) (-46);
+        int[] intArray0 = new int[5];
+        OperandManager operandManager0 = new OperandManager(intArray0, intArray0, intArray0, intArray0, intArray0, intArray0, intArray0, intArray0, intArray0, intArray0, intArray0, intArray0, intArray0, intArray0, intArray0, intArray0, intArray0, intArray0, intArray0, intArray0, intArray0);
+        LinkedList<ExceptionTableEntry> linkedList0 = new LinkedList<ExceptionTableEntry>();
+        CodeAttribute codeAttribute0 = null;
+        try {
+            codeAttribute0 = new CodeAttribute(141, (-3388), byteArray0, (Segment) null, operandManager0, linkedList0);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.apache.commons.compress.harmony.unpack200.bytecode.OperandManager", e);
+        }
+    }
+}
