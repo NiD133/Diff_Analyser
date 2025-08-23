d8b5ce3c7fb5d09d655690c94c7749b662810163
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:12:37 +0800

    Original processedAutoTestCases rfc1522codec_estest14.java

---
+++ rfc1522codec_estest14.java
@@ -0,0 +1,30 @@
+package org.apache.commons.codec.net;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.IllegalCharsetNameException;
+import java.nio.charset.UnsupportedCharsetException;
+import org.apache.commons.codec.CodecPolicy;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class RFC1522Codec_ESTestTest14 extends RFC1522Codec_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test13() throws Throwable {
+        BCodec bCodec0 = new BCodec();
+        try {
+            bCodec0.decodeText("=?UTF-8?Q?Q?=?=");
+            fail("Expecting exception: Exception");
+        } catch (Exception e) {
+            //
+            // This codec cannot decode Q encoded content
+            //
+            verifyException("org.apache.commons.codec.net.RFC1522Codec", e);
+        }
+    }
+}
