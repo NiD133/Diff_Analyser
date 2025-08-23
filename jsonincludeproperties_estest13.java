1c454b837ee820b9da949531e1079cb2d073def0
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:56:46 +0800

    Original processedAutoTestCases jsonincludeproperties_estest13.java

---
+++ jsonincludeproperties_estest13.java
@@ -0,0 +1,24 @@
+package com.fasterxml.jackson.annotation;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class JsonIncludeProperties_ESTestTest13 extends JsonIncludeProperties_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test12() throws Throwable {
+        JsonIncludeProperties.Value jsonIncludeProperties_Value0 = JsonIncludeProperties.Value.ALL;
+        JsonIncludeProperties jsonIncludeProperties0 = mock(JsonIncludeProperties.class, CALLS_REAL_METHODS);
+        doReturn((String[]) null).when(jsonIncludeProperties0).value();
+        JsonIncludeProperties.Value jsonIncludeProperties_Value1 = JsonIncludeProperties.Value.from(jsonIncludeProperties0);
+        JsonIncludeProperties.Value jsonIncludeProperties_Value2 = jsonIncludeProperties_Value0.withOverrides(jsonIncludeProperties_Value1);
+        assertSame(jsonIncludeProperties_Value2, jsonIncludeProperties_Value1);
+    }
+}
