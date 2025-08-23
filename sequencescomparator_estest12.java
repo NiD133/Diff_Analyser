76094ce6538396999987d15e32e4131202fa5fae
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:14:35 +0800

    gemini processedAutoTestCases sequencescomparator_estest12.java

--- sequencescomparator_estest12.java
+++ sequencescomparator_estest12.java
@@ -1,33 +1,52 @@
 package org.apache.commons.collections4.sequence;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.ConcurrentModificationException;
 import java.util.LinkedList;
 import java.util.List;
-import org.apache.commons.collections4.Equator;
-import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.functors.DefaultEquator;
-import org.apache.commons.collections4.functors.NotNullPredicate;
-import org.apache.commons.collections4.functors.PredicateTransformer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class SequencesComparator_ESTestTest12 extends SequencesComparator_ESTest_scaffolding {
+/**
+ * Contains tests for the {@link SequencesComparator} class.
+ */
+public class SequencesComparatorTest {
 
-    @Test(timeout = 4000)
-    public void test11() throws Throwable {
-        LinkedList<Object> linkedList0 = new LinkedList<Object>();
-        linkedList0.add((Object) linkedList0);
-        LinkedList<Object> linkedList1 = new LinkedList<Object>(linkedList0);
-        linkedList1.add((Object) linkedList0);
-        linkedList1.add((Object) linkedList0);
-        SequencesComparator<Object> sequencesComparator0 = new SequencesComparator<Object>(linkedList0, linkedList1);
-        linkedList1.add((Object) linkedList0);
-        linkedList1.offer(sequencesComparator0);
-        EditScript<Object> editScript0 = sequencesComparator0.getScript();
-        assertEquals(4, editScript0.getModifications());
+    /**
+     * Tests that the SequencesComparator reflects modifications made to the input lists
+     * after the comparator has been instantiated. This is because the comparator
+     * does not create a defensive copy of the lists and operates on the live references.
+     */
+    @Test
+    public void getScriptShouldReflectModificationsMadeToSequenceAfterConstruction() {
+        // Arrange
+        // Define two simple sequences to be compared.
+        List<String> originalSequence = new LinkedList<>();
+        originalSequence.add("A");
+
+        List<String> modifiedSequence = new LinkedList<>();
+        modifiedSequence.add("A"); // Common element
+        modifiedSequence.add("B");
+        modifiedSequence.add("C");
+
+        // Create the comparator. At this point, it holds direct references to the lists.
+        SequencesComparator<String> comparator = new SequencesComparator<>(originalSequence, modifiedSequence);
+
+        // Act
+        // Modify the second sequence *after* the comparator has been created.
+        // This is the behavior under test.
+        modifiedSequence.add("D");
+        modifiedSequence.add("E");
+
+        // When getScript() is called, the comparator will see the final state of the lists:
+        // originalSequence: ["A"]
+        // modifiedSequence: ["A", "B", "C", "D", "E"]
+        EditScript<String> script = comparator.getScript();
+
+        // Assert
+        // The comparison should result in one "keep" command for the common element "A"
+        // and four "insert" commands for "B", "C", "D", and "E".
+        // The total number of modifications (inserts + deletes) is therefore 4.
+        final int expectedModifications = 4;
+        assertEquals("The number of modifications should reflect changes made to the list after construction",
+                     expectedModifications, script.getModifications());
     }
 }
