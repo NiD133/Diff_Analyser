f2cc4f138f18d65fbd2a7f71680ba9e675c1b363
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:29:26 +0800

    Original processedManualTestCases indexedcollectiontest3.java

---
+++ indexedcollectiontest3.java
@@ -0,0 +1,84 @@
+package org.apache.commons.collections4.collection;
+
+import static java.util.Arrays.asList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import org.apache.commons.collections4.Transformer;
+import org.junit.jupiter.api.Test;
+
+public class IndexedCollectionTestTest3 extends AbstractCollectionTest<String> {
+
+    protected Collection<String> decorateCollection(final Collection<String> collection) {
+        return IndexedCollection.nonUniqueIndexedCollection(collection, new IntegerTransformer());
+    }
+
+    protected IndexedCollection<Integer, String> decorateUniqueCollection(final Collection<String> collection) {
+        return IndexedCollection.uniqueIndexedCollection(collection, new IntegerTransformer());
+    }
+
+    @Override
+    public String[] getFullElements() {
+        return new String[] { "1", "3", "5", "7", "2", "4", "6" };
+    }
+
+    @Override
+    public String[] getOtherElements() {
+        return new String[] { "9", "88", "678", "87", "98", "78", "99" };
+    }
+
+    @Override
+    public Collection<String> makeConfirmedCollection() {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public Collection<String> makeConfirmedFullCollection() {
+        return new ArrayList<>(Arrays.asList(getFullElements()));
+    }
+
+    @Override
+    public Collection<String> makeFullCollection() {
+        return decorateCollection(new ArrayList<>(Arrays.asList(getFullElements())));
+    }
+
+    @Override
+    public Collection<String> makeObject() {
+        return decorateCollection(new ArrayList<>());
+    }
+
+    public Collection<String> makeTestCollection() {
+        return decorateCollection(new ArrayList<>());
+    }
+
+    public Collection<String> makeUniqueTestCollection() {
+        return decorateUniqueCollection(new ArrayList<>());
+    }
+
+    @Override
+    protected boolean skipSerializedCanonicalTests() {
+        // FIXME: support canonical tests
+        return true;
+    }
+
+    private static final class IntegerTransformer implements Transformer<String, Integer>, Serializable {
+
+        private static final long serialVersionUID = 809439581555072949L;
+
+        @Override
+        public Integer transform(final String input) {
+            return Integer.valueOf(input);
+        }
+    }
+
+    @Test
+    void testEnsureDuplicateObjectsCauseException() throws Exception {
+        final Collection<String> coll = makeUniqueTestCollection();
+        coll.add("1");
+        assertThrows(IllegalArgumentException.class, () -> coll.add("1"));
+    }
+}
