ad256d678680b0c0d7d57cd6fbcd11b4072216ac
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:26:55 +0800

    Original processedManualTestCases collectionbagtest2.java

---
+++ collectionbagtest2.java
@@ -0,0 +1,63 @@
+package org.apache.commons.collections4.bag;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
+import org.apache.commons.collections4.Bag;
+import org.apache.commons.collections4.Predicate;
+import org.apache.commons.collections4.collection.AbstractCollectionTest;
+import org.apache.commons.collections4.functors.NonePredicate;
+import org.junit.jupiter.api.Test;
+
+public class CollectionBagTestTest2<T> extends AbstractCollectionTest<T> {
+
+    @Override
+    public String getCompatibilityVersion() {
+        return "4";
+    }
+
+    @Override
+    protected int getIterationBehaviour() {
+        return UNORDERED;
+    }
+
+    /**
+     * Returns an empty List for use in modification testing.
+     *
+     * @return a confirmed empty collection
+     */
+    @Override
+    public Collection<T> makeConfirmedCollection() {
+        return new ArrayList<>();
+    }
+
+    /**
+     * Returns a full Set for use in modification testing.
+     *
+     * @return a confirmed full collection
+     */
+    @Override
+    public Collection<T> makeConfirmedFullCollection() {
+        final Collection<T> set = makeConfirmedCollection();
+        set.addAll(Arrays.asList(getFullElements()));
+        return set;
+    }
+
+    @Override
+    public Bag<T> makeObject() {
+        return CollectionBag.collectionBag(new HashBag<>());
+    }
+
+    @Test
+    void testAdd_Predicate_ComparatorDefault() throws Throwable {
+        final TreeBag<Predicate<Object>> treeBagOfPredicateOfObject = new TreeBag<>();
+        final CollectionBag<Predicate<Object>> collectionBagOfPredicateOfObject = new CollectionBag<>(treeBagOfPredicateOfObject);
+        assertThrows(ClassCastException.class, () -> collectionBagOfPredicateOfObject.add(NonePredicate.nonePredicate(collectionBagOfPredicateOfObject), 24));
+    }
+}
