861deaf767103fa20d30357e29e2db35c86fa181
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:36:37 +0800

    gemini processedAutoTestCases abstractpartial_estest11.java

--- abstractpartial_estest11.java
+++ abstractpartial_estest11.java
@@ -1,46 +1,31 @@
 package org.joda.time.base;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.Date;
-import java.util.Locale;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.evosuite.runtime.mock.java.util.MockGregorianCalendar;
-import org.joda.time.Chronology;
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeField;
 import org.joda.time.DateTimeFieldType;
-import org.joda.time.DateTimeZone;
-import org.joda.time.DurationFieldType;
-import org.joda.time.Instant;
-import org.joda.time.LocalDate;
-import org.joda.time.LocalDateTime;
-import org.joda.time.LocalTime;
 import org.joda.time.MonthDay;
-import org.joda.time.Partial;
-import org.joda.time.ReadablePartial;
-import org.joda.time.Weeks;
-import org.joda.time.YearMonth;
-import org.joda.time.Years;
-import org.joda.time.chrono.CopticChronology;
-import org.joda.time.chrono.GJChronology;
-import org.joda.time.chrono.GregorianChronology;
-import org.joda.time.chrono.IslamicChronology;
-import org.joda.time.format.DateTimeFormatter;
-import org.joda.time.format.DateTimeParser;
-import org.joda.time.format.DateTimePrinter;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Test case for the {@link AbstractPartial#getFieldType(int)} method.
+ * This test uses {@link MonthDay} as a concrete implementation of {@link AbstractPartial}.
+ */
 public class AbstractPartial_ESTestTest11 extends AbstractPartial_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test10() throws Throwable {
-        MonthDay monthDay0 = MonthDay.now();
-        DateTimeFieldType dateTimeFieldType0 = monthDay0.getFieldType(1);
-        assertEquals("dayOfMonth", dateTimeFieldType0.toString());
+    /**
+     * Tests that getFieldType(int) returns the correct field type for a given index.
+     * For a MonthDay partial, index 0 corresponds to 'monthOfYear' and index 1 corresponds to 'dayOfMonth'.
+     */
+    @Test
+    public void getFieldType_returnsDayOfMonthForIndex1() {
+        // Arrange: A MonthDay is a partial with two fields. We create a sample instance.
+        MonthDay monthDay = new MonthDay(6, 23); // June 23
+
+        // Act: Retrieve the field type at the second position (index 1).
+        DateTimeFieldType actualFieldType = monthDay.getFieldType(1);
+
+        // Assert: The retrieved field type should be 'dayOfMonth'.
+        DateTimeFieldType expectedFieldType = DateTimeFieldType.dayOfMonth();
+        assertEquals(expectedFieldType, actualFieldType);
     }
 }
