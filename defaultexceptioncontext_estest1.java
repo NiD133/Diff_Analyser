6aa47b36ac195cb18615660ae78e5e093e708236
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:47:07 +0800

    gemini processedAutoTestCases defaultexceptioncontext_estest1.java

--- defaultexceptioncontext_estest1.java
+++ defaultexceptioncontext_estest1.java
@@ -1,22 +1,27 @@
 package org.apache.commons.lang3.exception;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.List;
-import java.util.Set;
-import org.apache.commons.lang3.tuple.Pair;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
+/**
+ * Tests for {@link DefaultExceptionContext}.
+ * This test focuses on adding and retrieving a context value.
+ */
 public class DefaultExceptionContext_ESTestTest1 extends DefaultExceptionContext_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test0() throws Throwable {
-        DefaultExceptionContext defaultExceptionContext0 = new DefaultExceptionContext();
-        DefaultExceptionContext defaultExceptionContext1 = defaultExceptionContext0.addContextValue("\t[", "\t[");
-        Object object0 = defaultExceptionContext1.getFirstContextValue("\t[");
-        assertEquals("\t[", object0);
+    @Test
+    public void getFirstContextValue_shouldReturnCorrectValue_whenSingleValueIsAddedForLabel() {
+        // Arrange
+        final DefaultExceptionContext context = new DefaultExceptionContext();
+        final String testLabel = "Error Code";
+        final Integer testValue = 404;
+
+        context.addContextValue(testLabel, testValue);
+
+        // Act
+        final Object retrievedValue = context.getFirstContextValue(testLabel);
+
+        // Assert
+        assertEquals("The retrieved value should match the one that was added.", testValue, retrievedValue);
     }
 }
