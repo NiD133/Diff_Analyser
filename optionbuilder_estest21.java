5d4d4557d8dbdb45e4018c11d1ef79b1212a8c40
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:06:14 +0800

    gemini processedAutoTestCases optionbuilder_estest21.java

--- optionbuilder_estest21.java
+++ optionbuilder_estest21.java
@@ -1,19 +1,30 @@
 package org.apache.commons.cli;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class OptionBuilder_ESTestTest21 extends OptionBuilder_ESTest_scaffolding {
+/**
+ * Test case for the OptionBuilder class.
+ * This test focuses on creating an option configured to accept an unlimited number of arguments.
+ */
+public class OptionBuilderTest {
 
-    @Test(timeout = 4000)
-    public void test20() throws Throwable {
+    @Test
+    public void testCreateOptionWithUnlimitedArguments() {
+        // Note: OptionBuilder uses static fields for configuration. This is a design
+        // flaw that can cause tests to interfere with each other. In a full test suite,
+        // a reset mechanism would be needed before each test.
+
+        // Arrange: Configure the builder for an option that can have unlimited arguments.
+        // A long option must be set for the create() method to succeed when no short option is provided.
+        OptionBuilder.withLongOpt("unlimited-args");
         OptionBuilder.hasArgs();
-        OptionBuilder.withLongOpt("");
-        Option option0 = OptionBuilder.create();
-        assertEquals((-2), option0.getArgs());
+
+        // Act: Create the Option instance based on the static configuration.
+        Option option = OptionBuilder.create();
+
+        // Assert: Verify that the created option is correctly configured for unlimited arguments.
+        // The constant Option.UNLIMITED_VALUES is used instead of the magic number -2.
+        assertEquals("The number of arguments should be unlimited.", Option.UNLIMITED_VALUES, option.getArgs());
     }
 }
