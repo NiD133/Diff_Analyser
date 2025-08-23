b440e4bd008d9a0e79a87aaf76aede85af47c0f4
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:55:07 +0800

    gemini processedAutoTestCases islamicchronology_estest15.java

--- islamicchronology_estest15.java
+++ islamicchronology_estest15.java
@@ -1,22 +1,43 @@
 package org.joda.time.chrono;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.TimeZone;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.joda.time.Chronology;
+import static org.junit.Assert.assertEquals;
+
+import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
-import org.joda.time.tz.UTCProvider;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class IslamicChronology_ESTestTest15 extends IslamicChronology_ESTest_scaffolding {
+/**
+ * Tests for {@link IslamicChronology}.
+ */
+public class IslamicChronologyTest {
 
-    @Test(timeout = 4000)
-    public void test14() throws Throwable {
-        IslamicChronology islamicChronology0 = IslamicChronology.getInstanceUTC();
-        long long0 = islamicChronology0.getYearDifference(918518400000L, 604800L);
-        assertEquals(29L, long0);
+    @Test
+    public void getYearDifference_returnsCorrectlyWhenMinuendIsEarlierInItsYear() {
+        // Arrange
+        IslamicChronology chronology = IslamicChronology.getInstanceUTC();
+
+        // Define two dates for the test.
+        // The minuend (the date being subtracted from) is Feb 1, 1999.
+        // In the Islamic calendar, this corresponds to year 1419.
+        DateTime minuendDate = new DateTime(1999, 2, 1, 0, 0, DateTimeZone.UTC);
+
+        // The subtrahend (the date being subtracted) is Jan 8, 1970.
+        // In the Islamic calendar, this corresponds to year 1389.
+        DateTime subtrahendDate = new DateTime(1970, 1, 8, 0, 0, DateTimeZone.UTC);
+
+        // The simple difference in year numbers is 1419 - 1389 = 30.
+        // However, getYearDifference() calculates the number of *full* years that have passed.
+        // It does this by comparing the progress of each date within its respective Islamic year.
+        //
+        // In this specific case, Feb 1, 1999 occurs earlier in its Islamic year (1419)
+        // than Jan 8, 1970 does in its Islamic year (1389).
+        // Therefore, a full 30 years have not yet elapsed between the two instants.
+        long expectedDifference = 29L;
+
+        // Act
+        long actualDifference = chronology.getYearDifference(minuendDate.getMillis(), subtrahendDate.getMillis());
+
+        // Assert
+        assertEquals("The number of full Islamic years should be 29", expectedDifference, actualDifference);
     }
 }
