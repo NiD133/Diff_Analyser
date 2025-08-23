fb49a5a175f957dc4de5dd0346c6231e058b6f55
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:47:14 +0800

    Original processedAutoTestCases defaultflowdataset_estest20.java

---
+++ defaultflowdataset_estest20.java
@@ -0,0 +1,24 @@
+package org.jfree.data.flow;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.time.chrono.HijrahEra;
+import java.util.List;
+import java.util.Set;
+import javax.swing.Icon;
+import javax.swing.JLayeredPane;
+import javax.swing.JRadioButtonMenuItem;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class DefaultFlowDataset_ESTestTest20 extends DefaultFlowDataset_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test19() throws Throwable {
+        DefaultFlowDataset<Integer> defaultFlowDataset0 = new DefaultFlowDataset<Integer>();
+        List<Integer> list0 = defaultFlowDataset0.getSources(0);
+        assertFalse(list0.contains(0));
+    }
+}
