e774e418e590526f4ae5335af946de4cca41a70b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:38:13 +0800

    Original processedAutoTestCases attribute_estest33.java

---
+++ attribute_estest33.java
@@ -0,0 +1,37 @@
+package org.jsoup.nodes;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.ByteArrayOutputStream;
+import java.io.FilterOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PipedWriter;
+import java.io.StringWriter;
+import java.nio.BufferOverflowException;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetEncoder;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockPrintWriter;
+import org.jsoup.internal.QuietAppendable;
+import org.junit.runner.RunWith;
+
+public class Attribute_ESTestTest33 extends Attribute_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test32() throws Throwable {
+        Document.OutputSettings document_OutputSettings0 = new Document.OutputSettings();
+        // Undeclared exception!
+        try {
+            Attribute.html(">Qu{B", "org.jsoup.select.Evaluator$ContainsOwnText", (QuietAppendable) null, document_OutputSettings0);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.jsoup.nodes.Attribute", e);
+        }
+    }
+}
