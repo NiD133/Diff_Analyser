342f21877a55b1f56312a02f62f40a4a2ba46a79
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:30:58 +0800

    gemini processedManualTestCases lzmautilstest4.java

--- lzmautilstest4.java
+++ lzmautilstest4.java
@@ -1,28 +1,44 @@
 package org.apache.commons.compress.compressors.lzma;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import org.junit.jupiter.api.Test;
 
-public class LZMAUtilsTestTest4 {
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-    @SuppressWarnings("deprecation")
-    @Test
-    void testGetUncompressedFilename() {
-        assertEquals("", LZMAUtils.getUncompressedFilename(""));
-        assertEquals("", LZMAUtils.getUncompressedFileName(""));
-        assertEquals(".lzma", LZMAUtils.getUncompressedFilename(".lzma"));
-        assertEquals(".lzma", LZMAUtils.getUncompressedFileName(".lzma"));
-        assertEquals("x", LZMAUtils.getUncompressedFilename("x.lzma"));
-        assertEquals("x", LZMAUtils.getUncompressedFileName("x.lzma"));
-        assertEquals("x", LZMAUtils.getUncompressedFilename("x-lzma"));
-        assertEquals("x", LZMAUtils.getUncompressedFileName("x-lzma"));
-        assertEquals("x.lzma ", LZMAUtils.getUncompressedFilename("x.lzma "));
-        assertEquals("x.lzma ", LZMAUtils.getUncompressedFileName("x.lzma "));
-        assertEquals("x.lzma\n", LZMAUtils.getUncompressedFilename("x.lzma\n"));
-        assertEquals("x.lzma\n", LZMAUtils.getUncompressedFileName("x.lzma\n"));
-        assertEquals("x.lzma.y", LZMAUtils.getUncompressedFilename("x.lzma.y"));
-        assertEquals("x.lzma.y", LZMAUtils.getUncompressedFileName("x.lzma.y"));
+/**
+ * Tests for the filename utility methods in {@link LZMAUtils}.
+ */
+@DisplayName("LZMAUtils Filename Utility Tests")
+class LZMAUtilsFileNameTest {
+
+    /**
+     * Provides test cases for {@link #getUncompressedFileNameBehavesAsExpected(String, String, String)}.
+     * Each argument consists of the input filename, the expected output, and a description of the test case.
+     */
+    static Stream<Arguments> uncompressedFileNameProvider() {
+        return Stream.of(
+            Arguments.of("", "", "An empty filename should remain empty"),
+            Arguments.of(".lzma", ".lzma", "A filename that is only the suffix should not be changed"),
+            Arguments.of("x.lzma", "x", "The '.lzma' suffix should be removed"),
+            Arguments.of("x-lzma", "x", "The '-lzma' suffix should be removed"),
+            Arguments.of("x.lzma ", "x.lzma ", "A suffix with a trailing space should not be removed"),
+            Arguments.of("x.lzma\n", "x.lzma\n", "A suffix with a trailing newline should not be removed"),
+            Arguments.of("x.lzma.y", "x.lzma.y", "A suffix not at the end of the filename should not be removed")
+        );
+    }
+
+    @DisplayName("getUncompressedFileName should correctly handle various filenames")
+    @ParameterizedTest(name = "[{index}] {2}: ''{0}'' -> ''{1}''")
+    @MethodSource("uncompressedFileNameProvider")
+    @SuppressWarnings("deprecation") // Testing a deprecated method is intentional
+    void getUncompressedFileNameBehavesAsExpected(final String input, final String expected, final String description) {
+        // Test the current method
+        assertEquals(expected, LZMAUtils.getUncompressedFileName(input));
+
+        // Also test the deprecated method to ensure consistent behavior
+        assertEquals(expected, LZMAUtils.getUncompressedFilename(input));
     }
 }
