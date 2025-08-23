11ad9e26511f2f5fe2ae2f4d02b814cb7a660368
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:22:26 +0800

    gemini processedAutoTestCases tokenqueue_estest28.java

--- tokenqueue_estest28.java
+++ tokenqueue_estest28.java
@@ -1,27 +1,28 @@
 package org.jsoup.parser;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
+/**
+ * This test verifies the behavior of the TokenQueue class when methods are called after
+ * the queue has been closed.
+ */
 public class TokenQueue_ESTestTest28 extends TokenQueue_ESTest_scaffolding {
 
-    @Test(timeout = 4000)
-    public void test27() throws Throwable {
-        TokenQueue tokenQueue0 = new TokenQueue("fXjR3=h^{j^|Zu");
-        tokenQueue0.close();
-        // Undeclared exception!
-        try {
-            tokenQueue0.matches('A');
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.jsoup.parser.CharacterReader", e);
-        }
+    /**
+     * Verifies that invoking the matches(char) method on a TokenQueue after it has been closed
+     * results in a NullPointerException. The close() method is expected to release internal
+     * resources, rendering subsequent operations invalid.
+     */
+    @Test(expected = NullPointerException.class)
+    public void callingMatchesOnClosedQueueShouldThrowNullPointerException() {
+        // Arrange: Create a TokenQueue and immediately close it.
+        // The initial content is irrelevant as we are testing the closed state.
+        TokenQueue queue = new TokenQueue("some data");
+        queue.close();
+
+        // Act & Assert: Attempt to use the closed queue.
+        // This call is expected to throw a NullPointerException, which is handled
+        // by the @Test(expected=...) annotation.
+        queue.matches('A');
     }
 }
