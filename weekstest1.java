215160dea0e1846e04a9b0414fc867d3c995a2d8
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:35:49 +0800

    Original processedManualTestCases weekstest1.java

---
+++ weekstest1.java
@@ -0,0 +1,40 @@
+package org.joda.time;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class WeeksTestTest1 extends TestCase {
+
+    // (before the late 90's they were all over the place)
+    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
+
+    public static TestSuite suite() {
+        return new TestSuite(TestWeeks.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+    }
+
+    //-----------------------------------------------------------------------
+    public void testConstants() {
+        assertEquals(0, Weeks.ZERO.getWeeks());
+        assertEquals(1, Weeks.ONE.getWeeks());
+        assertEquals(2, Weeks.TWO.getWeeks());
+        assertEquals(3, Weeks.THREE.getWeeks());
+        assertEquals(Integer.MAX_VALUE, Weeks.MAX_VALUE.getWeeks());
+        assertEquals(Integer.MIN_VALUE, Weeks.MIN_VALUE.getWeeks());
+    }
+}
