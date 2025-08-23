f052d1aa8d8d61310a61d86569eb8d6d0dd9abdb
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:44:38 +0800

    Original processedAutoTestCases collectionsortedbag_estest5.java

---
+++ collectionsortedbag_estest5.java
@@ -0,0 +1,50 @@
+package org.apache.commons.collections4.bag;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.lang.reflect.Array;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.ConcurrentModificationException;
+import java.util.LinkedList;
+import java.util.Locale;
+import java.util.PriorityQueue;
+import java.util.Set;
+import org.apache.commons.collections4.Closure;
+import org.apache.commons.collections4.Predicate;
+import org.apache.commons.collections4.SortedBag;
+import org.apache.commons.collections4.Transformer;
+import org.apache.commons.collections4.functors.ComparatorPredicate;
+import org.apache.commons.collections4.functors.ConstantTransformer;
+import org.apache.commons.collections4.functors.EqualPredicate;
+import org.apache.commons.collections4.functors.ExceptionPredicate;
+import org.apache.commons.collections4.functors.ExceptionTransformer;
+import org.apache.commons.collections4.functors.IdentityPredicate;
+import org.apache.commons.collections4.functors.IfClosure;
+import org.apache.commons.collections4.functors.IfTransformer;
+import org.apache.commons.collections4.functors.InstanceofPredicate;
+import org.apache.commons.collections4.functors.InvokerTransformer;
+import org.apache.commons.collections4.functors.NOPClosure;
+import org.apache.commons.collections4.functors.NullPredicate;
+import org.apache.commons.collections4.functors.TransformedPredicate;
+import org.apache.commons.collections4.functors.TransformerPredicate;
+import org.apache.commons.collections4.functors.UniquePredicate;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class CollectionSortedBag_ESTestTest5 extends CollectionSortedBag_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test04() throws Throwable {
+        TreeBag<Locale.FilteringMode> treeBag0 = new TreeBag<Locale.FilteringMode>();
+        Locale.FilteringMode locale_FilteringMode0 = Locale.FilteringMode.MAP_EXTENDED_RANGES;
+        CollectionSortedBag<Locale.FilteringMode> collectionSortedBag0 = new CollectionSortedBag<Locale.FilteringMode>(treeBag0);
+        collectionSortedBag0.add(locale_FilteringMode0, 1407);
+        // Undeclared exception!
+        collectionSortedBag0.retainAll(treeBag0);
+    }
+}
