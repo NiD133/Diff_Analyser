0fc7a19afcb57857647d7ad885deae8c7c4fdd4b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:04:36 +0800

    gemini processedAutoTestCases nodeiterator_estest12.java

--- nodeiterator_estest12.java
+++ nodeiterator_estest12.java
@@ -1,30 +1,27 @@
 package org.jsoup.nodes;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.NoSuchElementException;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.jsoup.parser.Parser;
-import org.junit.runner.RunWith;
 
-public class NodeIterator_ESTestTest12 extends NodeIterator_ESTest_scaffolding {
+/**
+ * Test suite for the {@link NodeIterator} class.
+ * This specific test focuses on the behavior of the restart() method.
+ */
+public class NodeIteratorTest {
 
-    @Test(timeout = 4000)
-    public void test11() throws Throwable {
-        Document document0 = Parser.parse("http://www.w3.org/1999/xhtml", "http://www.w3.org/2000/svg");
-        Class<FormElement> class0 = FormElement.class;
-        NodeIterator<FormElement> nodeIterator0 = new NodeIterator<FormElement>(document0, class0);
-        // Undeclared exception!
-        try {
-            nodeIterator0.restart((Node) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.jsoup.nodes.NodeIterator", e);
-        }
+    /**
+     * Verifies that calling the restart() method with a null node argument
+     * throws a NullPointerException.
+     */
+    @Test(expected = NullPointerException.class)
+    public void restartWithNullNodeThrowsNullPointerException() {
+        // Arrange: Create an initial valid NodeIterator instance.
+        // The specific node and type used for initialization are not important for this test.
+        Document initialNode = new Document("");
+        NodeIterator<Node> iterator = new NodeIterator<>(initialNode, Node.class);
+
+        // Act: Attempt to restart the iterator with a null node.
+        // Assert: The @Test(expected) annotation handles the assertion,
+        // ensuring a NullPointerException is thrown.
+        iterator.restart(null);
     }
 }
