91dfe035a0ef4874b119387938be289658f1780b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:44:19 +0800

    gemini processedAutoTestCases collectionsortedbag_estest1.java

--- collectionsortedbag_estest1.java
+++ collectionsortedbag_estest1.java
@@ -1,53 +1,50 @@
 package org.apache.commons.collections4.bag;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.ConcurrentModificationException;
-import java.util.LinkedList;
-import java.util.Locale;
-import java.util.PriorityQueue;
-import java.util.Set;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.Predicate;
 import org.apache.commons.collections4.SortedBag;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionPredicate;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.IfClosure;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.NOPClosure;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.TransformedPredicate;
-import org.apache.commons.collections4.functors.TransformerPredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class CollectionSortedBag_ESTestTest1 extends CollectionSortedBag_ESTest_scaffolding {
+import java.util.Comparator;
 
-    @Test(timeout = 4000)
-    public void test00() throws Throwable {
-        Comparator<Object> comparator0 = (Comparator<Object>) mock(Comparator.class, new ViolatedAssumptionAnswer());
-        doReturn(0, 0, 0).when(comparator0).compare(any(), any());
-        TreeBag<Object> treeBag0 = new TreeBag<Object>(comparator0);
-        IfClosure<Object> ifClosure0 = new IfClosure<Object>((Predicate<? super Object>) null, (Closure<? super Object>) null);
-        treeBag0.add((Object) ifClosure0);
-        CollectionSortedBag<Object> collectionSortedBag0 = new CollectionSortedBag<Object>(treeBag0);
-        Integer integer0 = new Integer(1);
-        boolean boolean0 = collectionSortedBag0.remove((Object) integer0);
-        assertTrue(boolean0);
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+
+/**
+ * Contains tests for the {@link CollectionSortedBag} decorator.
+ */
+public class CollectionSortedBagTest {
+
+    /**
+     * Tests that {@code remove(Object)} succeeds if the provided object is considered
+     * equal to an existing element by the bag's comparator, even if they are
+     * different object instances.
+     */
+    @Test
+    public void testRemoveSucceedsWhenComparatorFindsAMatch() {
+        // Arrange
+        // 1. Create a comparator that considers any two objects to be equal.
+        @SuppressWarnings("unchecked")
+        final Comparator<Object> allEqualComparator = mock(Comparator.class);
+        doReturn(0).when(allEqualComparator).compare(any(), any());
+
+        // 2. Create a backing TreeBag with this comparator and add an element.
+        final SortedBag<Object> backingBag = new TreeBag<>(allEqualComparator);
+        backingBag.add("first element");
+
+        // 3. Decorate the bag with CollectionSortedBag, the class under test.
+        final SortedBag<Object> collectionSortedBag = new CollectionSortedBag<>(backingBag);
+
+        // 4. Define a different object that the comparator will treat as equal.
+        final Object objectToRemove = "second element";
+
+        // Act
+        // Attempt to remove the second object. Because the comparator returns 0 (equal),
+        // the bag should find and remove the first element.
+        final boolean wasRemoved = collectionSortedBag.remove(objectToRemove);
+
+        // Assert
+        assertTrue("The remove operation should return true as the comparator " +
+                   "considers the objects to be equal.", wasRemoved);
     }
 }
