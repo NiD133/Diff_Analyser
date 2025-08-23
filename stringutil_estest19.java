43ba487280c822883bab05ce8e5c5f16707b4f8e
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:18:45 +0800

    gemini processedAutoTestCases stringutil_estest19.java

--- stringutil_estest19.java
+++ stringutil_estest19.java
@@ -1,28 +1,23 @@
 package org.jsoup.internal;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Collection;
-import java.util.ConcurrentModificationException;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.ListIterator;
-import java.util.stream.Collector;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.evosuite.runtime.mock.java.net.MockURL;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class StringUtil_ESTestTest19 extends StringUtil_ESTest_scaffolding {
+/**
+ * Test suite for {@link StringUtil}.
+ */
+public class StringUtilTest {
 
-    @Test(timeout = 4000)
-    public void test18() throws Throwable {
-        String string0 = StringUtil.padding(0);
-        assertEquals("", string0);
+    @Test
+    public void paddingWithZeroWidthReturnsEmptyString() {
+        // Arrange
+        final int width = 0;
+        final String expectedPadding = "";
+
+        // Act
+        final String actualPadding = StringUtil.padding(width);
+
+        // Assert
+        assertEquals(expectedPadding, actualPadding);
     }
 }
