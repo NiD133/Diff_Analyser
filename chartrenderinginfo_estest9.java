160cefbd5b50a4b7e2575c5f85e959228e30402f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:43:15 +0800

    Original processedAutoTestCases chartrenderinginfo_estest9.java

---
+++ chartrenderinginfo_estest9.java
@@ -0,0 +1,35 @@
+package org.jfree.chart;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.awt.Rectangle;
+import java.awt.geom.Rectangle2D;
+import java.util.Locale;
+import java.util.SimpleTimeZone;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.jfree.chart.axis.DateAxis;
+import org.jfree.chart.entity.EntityCollection;
+import org.jfree.chart.entity.StandardEntityCollection;
+import org.jfree.chart.plot.CombinedRangeCategoryPlot;
+import org.jfree.chart.plot.PlotRenderingInfo;
+import org.jfree.data.xy.XYDatasetTableModel;
+import org.junit.runner.RunWith;
+
+public class ChartRenderingInfo_ESTestTest9 extends ChartRenderingInfo_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test08() throws Throwable {
+        ChartRenderingInfo chartRenderingInfo0 = new ChartRenderingInfo();
+        // Undeclared exception!
+        try {
+            chartRenderingInfo0.setChartArea((Rectangle2D) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+        }
+    }
+}
