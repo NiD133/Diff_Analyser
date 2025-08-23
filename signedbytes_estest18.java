98b7dda17cd3b0e1e74638b4ba0f3b38cc338998
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:15:52 +0800

    gemini processedAutoTestCases signedbytes_estest18.java

--- signedbytes_estest18.java
+++ signedbytes_estest18.java
@@ -1,27 +1,26 @@
 package com.google.common.primitives;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.Comparator;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class SignedBytes_ESTestTest18 extends SignedBytes_ESTest_scaffolding {
+/**
+ * Tests for {@link SignedBytes#sortDescending(byte[], int, int)}.
+ */
+public class SignedBytesTest {
 
-    @Test(timeout = 4000)
-    public void test17() throws Throwable {
-        byte[] byteArray0 = new byte[13];
-        // Undeclared exception!
-        try {
-            SignedBytes.sortDescending(byteArray0, (int) (byte) 127, (int) (byte) 127);
-            fail("Expecting exception: IndexOutOfBoundsException");
-        } catch (IndexOutOfBoundsException e) {
-            //
-            // java.lang.String@0000000001 (java.lang.Integer@0000000002) must not be greater than size (java.lang.Integer@0000000003)
-            //
-            verifyException("com.google.common.base.Preconditions", e);
-        }
+    /**
+     * This test verifies that sortDescending throws an IndexOutOfBoundsException
+     * when the specified 'fromIndex' is outside the bounds of the array.
+     */
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void sortDescending_withRange_fromIndexOutOfBounds_throwsException() {
+        // GIVEN: An array and a 'fromIndex' that is greater than the array's length.
+        byte[] array = new byte[10];
+        int outOfBoundsIndex = array.length + 1;
+
+        // WHEN: sortDescending is called with the out-of-bounds index.
+        // The 'toIndex' must be >= 'fromIndex', so we set it to the same value.
+        SignedBytes.sortDescending(array, outOfBoundsIndex, outOfBoundsIndex);
+
+        // THEN: An IndexOutOfBoundsException is expected, as declared by the @Test annotation.
     }
 }
