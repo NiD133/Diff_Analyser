4e75489310e23204e54cc8fec4f2257a65976631
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:59:53 +0800

    gemini processedAutoTestCases lazilyparsednumber_estest20.java

--- lazilyparsednumber_estest20.java
+++ lazilyparsednumber_estest20.java
@@ -1,19 +1,43 @@
 package com.google.gson.internal;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
-public class LazilyParsedNumber_ESTestTest20 extends LazilyParsedNumber_ESTest_scaffolding {
+/**
+ * Tests for {@link LazilyParsedNumber}.
+ */
+public class LazilyParsedNumberTest {
 
-    @Test(timeout = 4000)
-    public void test19() throws Throwable {
-        LazilyParsedNumber lazilyParsedNumber0 = new LazilyParsedNumber("Deserialization is pnsuported");
-        LazilyParsedNumber lazilyParsedNumber1 = new LazilyParsedNumber("Deserialization is pnsuported");
-        boolean boolean0 = lazilyParsedNumber0.equals(lazilyParsedNumber1);
-        assertTrue(boolean0);
+    /**
+     * Tests that two LazilyParsedNumber instances are considered equal
+     * if they are constructed with the same string value.
+     */
+    @Test
+    public void equals_whenObjectsHaveSameStringValue_returnsTrue() {
+        // Arrange
+        String numericString = "123.45";
+        LazilyParsedNumber number1 = new LazilyParsedNumber(numericString);
+        LazilyParsedNumber number2 = new LazilyParsedNumber(numericString);
+
+        // Act & Assert
+        // The equals() method should return true because the underlying string values are identical.
+        assertEquals(number1, number2);
+        // Also verify the hashCode contract
+        assertEquals(number1.hashCode(), number2.hashCode());
+    }
+
+    /**
+     * Tests that two LazilyParsedNumber instances are not considered equal
+     * if they are constructed with different string values.
+     */
+    @Test
+    public void equals_whenObjectsHaveDifferentStringValue_returnsFalse() {
+        // Arrange
+        LazilyParsedNumber number1 = new LazilyParsedNumber("123.45");
+        LazilyParsedNumber number2 = new LazilyParsedNumber("678.90");
+
+        // Act & Assert
+        assertNotEquals(number1, number2);
     }
 }
