11501311381b52f14c1ba4eb62e1a059d2e40c49
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:56:09 +0800

    gemini processedAutoTestCases jsonarray_estest44.java

--- jsonarray_estest44.java
+++ jsonarray_estest44.java
@@ -1,32 +1,24 @@
 package com.google.gson;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.util.Iterator;
-import java.util.List;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class JsonArray_ESTestTest44 extends JsonArray_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link JsonArray} class, focusing on type conversion methods.
+ */
+public class JsonArrayTest {
 
-    @Test(timeout = 4000)
-    public void test43() throws Throwable {
-        JsonArray jsonArray0 = new JsonArray();
-        Boolean boolean0 = new Boolean(true);
-        jsonArray0.add(boolean0);
-        // Undeclared exception!
-        try {
-            jsonArray0.getAsBigInteger();
-            fail("Expecting exception: NumberFormatException");
-        } catch (NumberFormatException e) {
-            //
-            // For input string: \"true\"
-            //
-            verifyException("java.lang.NumberFormatException", e);
-        }
+    /**
+     * Verifies that calling getAsBigInteger() on a JsonArray containing a single
+     * boolean element results in a NumberFormatException.
+     */
+    @Test(expected = NumberFormatException.class)
+    public void getAsBigInteger_whenArrayContainsSingleBoolean_shouldThrowNumberFormatException() {
+        // Arrange: Create a JsonArray and add a single boolean value.
+        JsonArray jsonArray = new JsonArray();
+        jsonArray.add(true);
+
+        // Act & Assert: Attempting to get the value as a BigInteger should throw
+        // a NumberFormatException because "true" cannot be parsed as a number.
+        jsonArray.getAsBigInteger();
     }
 }
