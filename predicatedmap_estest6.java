5636cab3664c7834fdab8dbc46746f727d682d53
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:09:31 +0800

    gemini processedAutoTestCases predicatedmap_estest6.java

--- predicatedmap_estest6.java
+++ predicatedmap_estest6.java
@@ -1,45 +1,35 @@
 package org.apache.commons.collections4.map;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
 import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionPredicate;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NotNullPredicate;
 import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.OnePredicate;
-import org.apache.commons.collections4.functors.OrPredicate;
-import org.apache.commons.collections4.functors.PredicateTransformer;
-import org.apache.commons.collections4.functors.TransformerPredicate;
-import org.apache.commons.collections4.functors.TruePredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class PredicatedMap_ESTestTest6 extends PredicatedMap_ESTest_scaffolding {
+import java.util.HashMap;
+import java.util.Map;
 
-    @Test(timeout = 4000)
-    public void test05() throws Throwable {
-        HashMap<HashMap<Object, Object>, Integer> hashMap0 = new HashMap<HashMap<Object, Object>, Integer>();
-        UniquePredicate<Object> uniquePredicate0 = new UniquePredicate<Object>();
-        PredicatedMap<HashMap<Object, Object>, Integer> predicatedMap0 = new PredicatedMap<HashMap<Object, Object>, Integer>(hashMap0, uniquePredicate0, uniquePredicate0);
-        Integer integer0 = predicatedMap0.checkSetValue((Integer) null);
-        assertNull(integer0);
+import static org.junit.Assert.assertNull;
+
+/**
+ * Contains tests for the {@link PredicatedMap#checkSetValue(Object)} method.
+ */
+public class PredicatedMapTest {
+
+    /**
+     * Tests that checkSetValue returns the provided value (null in this case)
+     * when the value predicate evaluates to true.
+     */
+    @Test
+    public void checkSetValueShouldReturnNullWhenPredicateAcceptsNull() {
+        // Arrange: Create a PredicatedMap with a predicate that allows null values.
+        // The key predicate is irrelevant for this test and can be null.
+        final Map<String, Integer> emptyMap = new HashMap<>();
+        final Predicate<Integer> acceptsNullPredicate = NullPredicate.nullPredicate();
+        final PredicatedMap<String, Integer> predicatedMap = new PredicatedMap<>(emptyMap, null, acceptsNullPredicate);
+
+        // Act: Check a null value against the predicate.
+        final Integer validatedValue = predicatedMap.checkSetValue(null);
+
+        // Assert: The method should return the original null value, as the predicate passed.
+        assertNull("The returned value should be null because the predicate allows it.", validatedValue);
     }
 }
