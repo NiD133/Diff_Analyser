ab8b56e2a6b38d2327570e7e8e2664ac7b58ba66
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:04:51 +0800

    gemini processedAutoTestCases nullreader_estest22.java

--- nullreader_estest22.java
+++ nullreader_estest22.java
@@ -1,29 +1,29 @@
 package org.apache.commons.io.input;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
 import java.io.EOFException;
 import java.io.IOException;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class NullReader_ESTestTest22 extends NullReader_ESTest_scaffolding {
+/**
+ * Contains tests for the {@link NullReader} class.
+ */
+public class NullReaderTest {
 
-    @Test(timeout = 4000)
-    public void test21() throws Throwable {
-        NullReader nullReader0 = new NullReader((-1935L), true, true);
-        char[] charArray0 = new char[0];
-        nullReader0.read(charArray0, 1095, 1095);
-        try {
-            nullReader0.read(charArray0);
-            fail("Expecting exception: EOFException");
-        } catch (EOFException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.commons.io.input.NullReader", e);
-        }
+    /**
+     * Tests that calling read() on a reader that is already at its end and is
+     * configured to throw an exception on EOF, does indeed throw an EOFException.
+     */
+    @Test(expected = EOFException.class)
+    public void readOnEndedReaderShouldThrowEOFExceptionWhenConfigured() throws IOException {
+        // Arrange: Create a NullReader that is already at its end (due to a negative size)
+        // and is configured to throw an exception upon reaching the end of the file.
+        final long size = -1L;
+        final boolean markSupported = true; // This parameter is not relevant for this test case.
+        final boolean throwEofException = true;
+        final NullReader reader = new NullReader(size, markSupported, throwEofException);
+
+        // Act: Attempt to read from the exhausted reader.
+        // Assert: An EOFException is expected, as specified by the @Test annotation.
+        reader.read();
     }
 }
