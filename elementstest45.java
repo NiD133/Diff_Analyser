6616aed33ac4532a059c45cb1d6849466cde650c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:28:15 +0800

    Original processedManualTestCases elementstest45.java

---
+++ elementstest45.java
@@ -0,0 +1,36 @@
+package org.jsoup.select;
+
+import org.jsoup.Jsoup;
+import org.jsoup.TextUtil;
+import org.jsoup.nodes.Comment;
+import org.jsoup.nodes.DataNode;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.FormElement;
+import org.jsoup.nodes.Node;
+import org.jsoup.nodes.TextNode;
+import org.junit.jupiter.api.Test;
+import java.util.Iterator;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ElementsTestTest45 {
+
+    @Test
+    public void retainAll() {
+        Document doc = Jsoup.parse("<p>One<p>Two<p>Three<p>Four</p><div>Div");
+        Elements ps = doc.select("p");
+        assertEquals(4, ps.size());
+        //Two and Three
+        Elements midPs = doc.select("p:gt(0):lt(3)");
+        assertEquals(2, midPs.size());
+        boolean removed = ps.retainAll(midPs);
+        assertEquals(2, ps.size());
+        assertTrue(removed);
+        assertEquals(2, midPs.size());
+        assertEquals("<p>Two</p>\n<p>Three</p>\n<div>Div</div>", doc.body().html());
+        Elements psAgain = doc.select("p");
+        assertFalse(midPs.retainAll(psAgain));
+        assertEquals("<p>Two</p>\n<p>Three</p>\n<div>Div</div>", doc.body().html());
+    }
+}
