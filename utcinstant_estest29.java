7c346e37e5e8b2ea6617dfb400777894984c4a59
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:25:15 +0800

    Original processedAutoTestCases utcinstant_estest29.java

---
+++ utcinstant_estest29.java
@@ -0,0 +1,32 @@
+package org.threeten.extra.scale;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.nio.CharBuffer;
+import java.time.DateTimeException;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.format.DateTimeParseException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.time.MockInstant;
+import org.junit.runner.RunWith;
+
+public class UtcInstant_ESTestTest29 extends UtcInstant_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test28() throws Throwable {
+        UtcInstant utcInstant0 = UtcInstant.ofModifiedJulianDay(9223372036854775807L, 490L);
+        // Undeclared exception!
+        try {
+            utcInstant0.toTaiInstant();
+            fail("Expecting exception: ArithmeticException");
+        } catch (ArithmeticException e) {
+            //
+            // long overflow
+            //
+            verifyException("java.lang.Math", e);
+        }
+    }
+}
