383efe36671cc8a57d1ce3b8b3f994bcbe193853
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:38:29 +0800

    Original processedAutoTestCases attribute_estest81.java

---
+++ attribute_estest81.java
@@ -0,0 +1,31 @@
+package org.jsoup.nodes;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.ByteArrayOutputStream;
+import java.io.FilterOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PipedWriter;
+import java.io.StringWriter;
+import java.nio.BufferOverflowException;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetEncoder;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockPrintWriter;
+import org.jsoup.internal.QuietAppendable;
+import org.junit.runner.RunWith;
+
+public class Attribute_ESTestTest81 extends Attribute_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test80() throws Throwable {
+        Attribute attribute0 = new Attribute("!m\"", "!m\"");
+        StringWriter stringWriter0 = new StringWriter(3969);
+        Document.OutputSettings document_OutputSettings0 = new Document.OutputSettings();
+        attribute0.html((Appendable) stringWriter0, document_OutputSettings0);
+        assertEquals("!m_=\"!m&quot;\"", stringWriter0.toString());
+    }
+}
