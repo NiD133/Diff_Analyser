1f13c22a5ed1c8fc94e431bb888549d1a43aaebd
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:55:53 +0800

    gemini processedAutoTestCases jndidatasourcefactory_estest1.java

--- jndidatasourcefactory_estest1.java
+++ jndidatasourcefactory_estest1.java
@@ -1,31 +1,27 @@
 package org.apache.ibatis.datasource.jndi;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.Reader;
-import java.io.StringReader;
+
 import java.util.Properties;
-import javax.sql.DataSource;
-import org.apache.ibatis.datasource.unpooled.UnpooledDataSource;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class JndiDataSourceFactory_ESTestTest1 extends JndiDataSourceFactory_ESTest_scaffolding {
+/**
+ * Tests for the {@link JndiDataSourceFactory} class.
+ */
+public class JndiDataSourceFactoryTest {
 
-    @Test(timeout = 4000)
-    public void test0() throws Throwable {
-        JndiDataSourceFactory jndiDataSourceFactory0 = new JndiDataSourceFactory();
-        // Undeclared exception!
-        try {
-            jndiDataSourceFactory0.setProperties((Properties) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.ibatis.datasource.jndi.JndiDataSourceFactory", e);
-        }
+    /**
+     * Verifies that calling setProperties with a null argument
+     * results in a NullPointerException.
+     */
+    @Test(expected = NullPointerException.class)
+    public void shouldThrowNullPointerExceptionWhenPropertiesAreNull() {
+        // Arrange
+        JndiDataSourceFactory dataSourceFactory = new JndiDataSourceFactory();
+        Properties nullProperties = null;
+
+        // Act & Assert
+        // This call is expected to throw a NullPointerException, which is
+        // verified by the @Test(expected=...) annotation.
+        dataSourceFactory.setProperties(nullProperties);
     }
 }
