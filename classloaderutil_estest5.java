366599a98868e5d3e352c39ae04634828fd66147
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:43:31 +0800

    gemini processedAutoTestCases classloaderutil_estest5.java

--- classloaderutil_estest5.java
+++ classloaderutil_estest5.java
@@ -1,20 +1,26 @@
 package org.apache.commons.jxpath.util;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class ClassLoaderUtil_ESTestTest5 extends ClassLoaderUtil_ESTest_scaffolding {
+/**
+ * Contains tests for the {@link ClassLoaderUtil} class.
+ */
+public class ClassLoaderUtilTest {
 
-    @Test(timeout = 4000)
-    public void test4() throws Throwable {
-        try {
-            ClassLoaderUtil.getClass("9[]", false);
-            fail("Expecting exception: ClassNotFoundException");
-        } catch (ClassNotFoundException e) {
-        }
+    /**
+     * Verifies that attempting to load a class with a malformed array name
+     * (e.g., one starting with a digit) correctly throws a ClassNotFoundException.
+     *
+     * @throws ClassNotFoundException expected exception for this test case
+     */
+    @Test(expected = ClassNotFoundException.class)
+    public void getClassWithMalformedArrayNameThrowsClassNotFoundException() throws ClassNotFoundException {
+        // A valid Java class name cannot begin with a digit.
+        // This test ensures that such malformed names are properly handled.
+        String malformedArrayClassName = "9[]";
+        boolean initialize = false;
+
+        // This call is expected to fail with a ClassNotFoundException.
+        ClassLoaderUtil.getClass(malformedArrayClassName, initialize);
     }
 }
