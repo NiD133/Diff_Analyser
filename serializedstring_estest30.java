d496eb3d11ee1110ec0b68c8f8f76c2a4158df73
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:15:08 +0800

    Original processedAutoTestCases serializedstring_estest30.java

---
+++ serializedstring_estest30.java
@@ -0,0 +1,28 @@
+package com.fasterxml.jackson.core.io;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PipedOutputStream;
+import java.nio.ByteBuffer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.evosuite.runtime.mock.java.io.MockFileOutputStream;
+import org.evosuite.runtime.mock.java.io.MockPrintStream;
+import org.junit.runner.RunWith;
+
+public class SerializedString_ESTestTest30 extends SerializedString_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test29() throws Throwable {
+        SerializedString serializedString0 = new SerializedString("8+19.:23Vy=-x");
+        ByteBuffer byteBuffer0 = ByteBuffer.allocateDirect(0);
+        boolean boolean0 = serializedString0.equals(byteBuffer0);
+        assertFalse(boolean0);
+    }
+}
