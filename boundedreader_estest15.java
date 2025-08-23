03935714b59b8bd318fddd8b62d92d39ba104796
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:39:39 +0800

    Original processedAutoTestCases boundedreader_estest15.java

---
+++ boundedreader_estest15.java
@@ -0,0 +1,29 @@
+package org.apache.commons.io.input;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class BoundedReader_ESTestTest15 extends BoundedReader_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test14() throws Throwable {
+        BoundedReader boundedReader0 = new BoundedReader((Reader) null, 4168);
+        // Undeclared exception!
+        try {
+            boundedReader0.mark(4168);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.apache.commons.io.input.BoundedReader", e);
+        }
+    }
+}
