3d35e4d688fe5d7de09f6a9e32e75211370e85ef
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:22:21 +0800

    Original processedAutoTestCases tokenqueue_estest19.java

---
+++ tokenqueue_estest19.java
@@ -0,0 +1,19 @@
+package org.jsoup.parser;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class TokenQueue_ESTestTest19 extends TokenQueue_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test18() throws Throwable {
+        TokenQueue tokenQueue0 = new TokenQueue("Jkl):ip4E/0M");
+        tokenQueue0.consume();
+        String string0 = tokenQueue0.consumeTo("Jkl):ip4E/0M");
+        assertEquals("kl):ip4E/0M", string0);
+    }
+}
