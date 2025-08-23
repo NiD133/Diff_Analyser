19288fb94c0fa608afcd8fc570fd826f37474e96
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:38:37 +0800

    gemini processedAutoTestCases base16_estest15.java

--- base16_estest15.java
+++ base16_estest15.java
@@ -1,29 +1,29 @@
 package org.apache.commons.codec.binary;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.apache.commons.codec.CodecPolicy;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class Base16_ESTestTest15 extends Base16_ESTest_scaffolding {
+/**
+ * Tests for {@link Base16}.
+ */
+public class Base16Test {
 
-    @Test(timeout = 4000)
-    public void test14() throws Throwable {
-        byte[] byteArray0 = new byte[6];
-        Base16 base16_0 = new Base16();
-        BaseNCodec.Context baseNCodec_Context0 = new BaseNCodec.Context();
-        // Undeclared exception!
-        try {
-            base16_0.encode(byteArray0, 76, 76, baseNCodec_Context0);
-            fail("Expecting exception: ArrayIndexOutOfBoundsException");
-        } catch (ArrayIndexOutOfBoundsException e) {
-            //
-            // 76
-            //
-            verifyException("org.apache.commons.codec.binary.Base16", e);
-        }
+    /**
+     * Tests that the encode method throws an ArrayIndexOutOfBoundsException
+     * when the specified offset is greater than the input data's length.
+     */
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void encodeWithOffsetOutOfBoundsShouldThrowException() {
+        // Arrange: Create a Base16 instance and prepare test data.
+        final Base16 base16 = new Base16();
+        final byte[] data = new byte[10];
+        final BaseNCodec.Context context = new BaseNCodec.Context();
+
+        // The offset is intentionally set beyond the bounds of the 'data' array.
+        final int invalidOffset = 11;
+        final int length = 1; // A valid length to isolate the offset error.
+
+        // Act: Call the encode method with the invalid offset.
+        // The @Test(expected=...) annotation will assert that the correct exception is thrown.
+        base16.encode(data, invalidOffset, length, context);
     }
 }
