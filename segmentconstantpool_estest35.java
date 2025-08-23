59d38ca9afd93e6f15be2dc0ba5d33ef97063b8c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:13:44 +0800

    Original processedAutoTestCases segmentconstantpool_estest35.java

---
+++ segmentconstantpool_estest35.java
@@ -0,0 +1,29 @@
+package org.apache.commons.compress.harmony.unpack200;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.IOException;
+import org.apache.commons.compress.harmony.unpack200.bytecode.ClassFileEntry;
+import org.apache.commons.compress.harmony.unpack200.bytecode.ConstantPoolEntry;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class SegmentConstantPool_ESTestTest35 extends SegmentConstantPool_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test34() throws Throwable {
+        SegmentConstantPool segmentConstantPool0 = new SegmentConstantPool((CpBands) null);
+        // Undeclared exception!
+        try {
+            segmentConstantPool0.getValue(9, 7);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.apache.commons.compress.harmony.unpack200.SegmentConstantPool", e);
+        }
+    }
+}
