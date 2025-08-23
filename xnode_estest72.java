1b4c3712e476cf6ca4944f54bfa490df75aacfaf
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:29:10 +0800

    Original processedAutoTestCases xnode_estest72.java

---
+++ xnode_estest72.java
@@ -0,0 +1,37 @@
+package org.apache.ibatis.parsing;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.List;
+import java.util.Locale;
+import java.util.Properties;
+import java.util.function.Supplier;
+import javax.imageio.metadata.IIOMetadataNode;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.xml.sax.ext.DefaultHandler2;
+
+public class XNode_ESTestTest72 extends XNode_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test071() throws Throwable {
+        IIOMetadataNode iIOMetadataNode0 = new IIOMetadataNode();
+        iIOMetadataNode0.setAttribute("2pY", "2pY");
+        XPathParser xPathParser0 = new XPathParser((Document) null, false);
+        Properties properties0 = new Properties();
+        XNode xNode0 = new XNode(xPathParser0, iIOMetadataNode0, properties0);
+        Double double0 = new Double((-1.0));
+        // Undeclared exception!
+        try {
+            xNode0.getDoubleAttribute("2pY", double0);
+            fail("Expecting exception: NumberFormatException");
+        } catch (NumberFormatException e) {
+        }
+    }
+}
