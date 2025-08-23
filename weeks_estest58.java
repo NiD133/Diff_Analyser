3d4728761bacda0d6184e47331ac788062f32252
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:27:24 +0800

    gemini processedAutoTestCases weeks_estest58.java

--- weeks_estest58.java
+++ weeks_estest58.java
@@ -1,26 +1,32 @@
 package org.joda.time;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
-public class Weeks_ESTestTest58 extends Weeks_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link Weeks} class.
+ */
+public class WeeksTest {
 
-    @Test(timeout = 4000)
-    public void test57() throws Throwable {
-        Weeks weeks0 = Weeks.weeks(Integer.MIN_VALUE);
-        // Undeclared exception!
+    /**
+     * Tests that adding a value that causes an integer overflow
+     * correctly throws an ArithmeticException.
+     */
+    @Test
+    public void plus_whenResultingInIntegerOverflow_throwsArithmeticException() {
+        // Arrange: Create a Weeks instance with the minimum possible integer value.
+        Weeks minWeeks = Weeks.weeks(Integer.MIN_VALUE);
+        int valueToAdd = Integer.MIN_VALUE;
+
+        // Act & Assert: Attempt the addition and verify the expected exception.
         try {
-            weeks0.plus(Integer.MIN_VALUE);
-            fail("Expecting exception: ArithmeticException");
+            minWeeks.plus(valueToAdd);
+            fail("Expected an ArithmeticException to be thrown due to integer overflow.");
         } catch (ArithmeticException e) {
-            //
-            // The calculation caused an overflow: -2147483648 + -2147483648
-            //
-            verifyException("org.joda.time.field.FieldUtils", e);
+            // Verify that the exception message clearly indicates an overflow.
+            String expectedMessage = "The calculation caused an overflow: -2147483648 + -2147483648";
+            assertEquals(expectedMessage, e.getMessage());
         }
     }
 }
