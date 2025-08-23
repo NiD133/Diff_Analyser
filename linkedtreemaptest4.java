61b1afe44dfb46dd4699c3d39633405d4cc16922
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:30:42 +0800

    gemini processedManualTestCases linkedtreemaptest4.java

--- linkedtreemaptest4.java
+++ linkedtreemaptest4.java
@@ -1,26 +1,29 @@
 package com.google.gson.internal;
 
-import static com.google.common.truth.Truth.assertThat;
 import static org.junit.Assert.assertThrows;
-import com.google.gson.common.MoreAsserts;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Random;
+
 import org.junit.Test;
 
-public class LinkedTreeMapTestTest4 {
+/**
+ * Tests for {@link LinkedTreeMap}.
+ */
+public class LinkedTreeMapTest {
 
-    @Test
-    @SuppressWarnings("ModifiedButNotUsed")
-    public void testPutNonComparableKeyFails() {
-        LinkedTreeMap<Object, String> map = new LinkedTreeMap<>();
-        assertThrows(ClassCastException.class, () -> map.put(new Object(), "android"));
-    }
+  /**
+   * Verifies that {@code put} throws a {@link ClassCastException} when a non-comparable key is
+   * inserted into a map that uses natural ordering. The default {@link LinkedTreeMap} constructor
+   * creates such a map.
+   */
+  @Test
+  public void put_nonComparableKey_throwsClassCastException() {
+    // Arrange: Create a map that uses natural ordering for its keys.
+    LinkedTreeMap<Object, String> map = new LinkedTreeMap<>();
+    Object nonComparableKey = new Object();
+
+    // Act & Assert: Attempting to put a key that does not implement Comparable should fail.
+    assertThrows(
+        "Putting a non-Comparable key should throw ClassCastException",
+        ClassCastException.class,
+        () -> map.put(nonComparableKey, "any value"));
+  }
 }
