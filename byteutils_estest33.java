83a732a238f17ffd4f86e0e917cd64bd5e8182e6
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:41:05 +0800

    Original processedAutoTestCases byteutils_estest33.java

---
+++ byteutils_estest33.java
@@ -0,0 +1,43 @@
+package org.apache.commons.compress.utils;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInput;
+import java.io.DataInputStream;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.io.PushbackInputStream;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.io.MockFileOutputStream;
+import org.junit.runner.RunWith;
+
+public class ByteUtils_ESTestTest33 extends ByteUtils_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test32() throws Throwable {
+        PipedInputStream pipedInputStream0 = new PipedInputStream();
+        DataInputStream dataInputStream0 = new DataInputStream(pipedInputStream0);
+        try {
+            ByteUtils.fromLittleEndian((DataInput) dataInputStream0, 1);
+            fail("Expecting exception: IOException");
+        } catch (IOException e) {
+            //
+            // Pipe not connected
+            //
+            verifyException("java.io.PipedInputStream", e);
+        }
+    }
+}
