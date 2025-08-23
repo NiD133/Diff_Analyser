13266fcea3f2dff603dee77ce8c3944afd20b65d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:53:55 +0800

    gemini processedAutoTestCases indexedcollection_estest20.java

--- indexedcollection_estest20.java
+++ indexedcollection_estest20.java
@@ -1,63 +1,37 @@
 package org.apache.commons.collections4.collection;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.ConcurrentModificationException;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Set;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.Predicate;
 import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AllPredicate;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ChainedTransformer;
-import org.apache.commons.collections4.functors.CloneTransformer;
-import org.apache.commons.collections4.functors.ClosureTransformer;
-import org.apache.commons.collections4.functors.ConstantFactory;
 import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.DefaultEquator;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.FactoryTransformer;
-import org.apache.commons.collections4.functors.FalsePredicate;
-import org.apache.commons.collections4.functors.ForClosure;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.NOPClosure;
-import org.apache.commons.collections4.functors.NOPTransformer;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NotNullPredicate;
-import org.apache.commons.collections4.functors.NullIsFalsePredicate;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.SwitchTransformer;
-import org.apache.commons.collections4.functors.TransformedPredicate;
-import org.apache.commons.collections4.functors.TransformerClosure;
-import org.apache.commons.collections4.functors.TransformerPredicate;
-import org.apache.commons.collections4.functors.TruePredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class IndexedCollection_ESTestTest20 extends IndexedCollection_ESTest_scaffolding {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
-    @Test(timeout = 4000)
-    public void test19() throws Throwable {
-        Transformer<Object, Object> transformer0 = ConstantTransformer.nullTransformer();
-        LinkedList<Object> linkedList0 = new LinkedList<Object>();
-        linkedList0.add((Object) transformer0);
-        IndexedCollection<Object, Object> indexedCollection0 = IndexedCollection.nonUniqueIndexedCollection((Collection<Object>) linkedList0, transformer0);
-        indexedCollection0.add(linkedList0);
-        HashMap<Integer, Integer> hashMap0 = new HashMap<Integer, Integer>();
-        Set<Integer> set0 = hashMap0.keySet();
-        Transformer<Integer, Integer> transformer1 = ConstantTransformer.nullTransformer();
-        IndexedCollection<Integer, Integer> indexedCollection1 = IndexedCollection.nonUniqueIndexedCollection((Collection<Integer>) set0, transformer1);
-        // Undeclared exception!
-        indexedCollection1.removeAll(linkedList0);
+/**
+ * Contains tests for {@link IndexedCollection} focusing on exception handling with incompatible types.
+ */
+public class IndexedCollectionTest {
+
+    /**
+     * Tests that calling removeAll() with a collection containing elements of an incompatible
+     * type throws a ClassCastException. The method is expected to fail when it internally
+     * tries to cast an incompatible object to the collection's generic type.
+     */
+    @Test(expected = ClassCastException.class)
+    public void removeAllShouldThrowClassCastExceptionForIncompatibleCollectionType() {
+        // Arrange
+        // Create an IndexedCollection designed to hold only Integer objects.
+        Transformer<Integer, Integer> integerKeyTransformer = ConstantTransformer.nullTransformer();
+        IndexedCollection<Integer, Integer> integerCollection =
+                IndexedCollection.nonUniqueIndexedCollection(new ArrayList<>(), integerKeyTransformer);
+
+        // Create a collection containing an element of an incompatible type (a String).
+        Collection<Object> incompatibleTypeCollection = Collections.singletonList("not an integer");
+
+        // Act & Assert
+        // Attempting to remove a collection of Strings from a collection of Integers
+        // should trigger a ClassCastException.
+        integerCollection.removeAll(incompatibleTypeCollection);
     }
 }
