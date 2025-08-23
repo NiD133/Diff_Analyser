536b492a79cab73723219eb0e7b49e06d2d1e3b2
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:25:46 +0800

    Original processedManualTestCases boundediteratortest12.java

---
+++ boundediteratortest12.java
@@ -0,0 +1,53 @@
+package org.apache.commons.collections4.iterators;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class BoundedIteratorTestTest12<E> extends AbstractIteratorTest<E> {
+
+    /**
+     * Test array of size 7
+     */
+    private final String[] testArray = { "a", "b", "c", "d", "e", "f", "g" };
+
+    private List<E> testList;
+
+    @Override
+    public Iterator<E> makeEmptyIterator() {
+        return new BoundedIterator<>(Collections.<E>emptyList().iterator(), 0, 10);
+    }
+
+    @Override
+    public Iterator<E> makeObject() {
+        return new BoundedIterator<>(new ArrayList<>(testList).iterator(), 1, testList.size() - 1);
+    }
+
+    @SuppressWarnings("unchecked")
+    @BeforeEach
+    public void setUp() throws Exception {
+        testList = Arrays.asList((E[]) testArray);
+    }
+
+    /**
+     * Test the {@code remove()} method being called without
+     * {@code next()} being called first.
+     */
+    @Test
+    void testRemoveWithoutCallingNext() {
+        final List<E> testListCopy = new ArrayList<>(testList);
+        final Iterator<E> iter = new BoundedIterator<>(testListCopy.iterator(), 1, 5);
+        final IllegalStateException thrown = assertThrows(IllegalStateException.class, () -> iter.remove());
+        assertEquals("remove() cannot be called before calling next()", thrown.getMessage());
+    }
+}
