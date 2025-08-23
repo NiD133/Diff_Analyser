ad3eae0d5e65c14fd0616293fba39dd7d0b0650c
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:42:17 +0800

    Original processedAutoTestCases charsequencereader_estest32.java

---
+++ charsequencereader_estest32.java
@@ -0,0 +1,23 @@
+package com.google.common.io;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.IOException;
+import java.nio.CharBuffer;
+import java.nio.ReadOnlyBufferException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class CharSequenceReader_ESTestTest32 extends CharSequenceReader_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test31() throws Throwable {
+        char[] charArray0 = new char[2];
+        CharBuffer charBuffer0 = CharBuffer.wrap(charArray0);
+        CharSequenceReader charSequenceReader0 = new CharSequenceReader(charBuffer0);
+        charSequenceReader0.reset();
+        assertTrue(charSequenceReader0.markSupported());
+    }
+}
