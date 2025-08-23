76491a8d18724745b2795cde4678b143b3cb06be
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:44:43 +0800

    Original processedAutoTestCases compactlinkedhashset_estest21.java

---
+++ compactlinkedhashset_estest21.java
@@ -0,0 +1,30 @@
+package com.google.common.collect;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.Collection;
+import java.util.Locale;
+import java.util.Set;
+import java.util.Spliterator;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class CompactLinkedHashSet_ESTestTest21 extends CompactLinkedHashSet_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test20() throws Throwable {
+        CompactLinkedHashSet<Object> compactLinkedHashSet0 = new CompactLinkedHashSet<Object>();
+        // Undeclared exception!
+        try {
+            compactLinkedHashSet0.init((-1191));
+            fail("Expecting exception: IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            //
+            // Expected size must be >= 0
+            //
+            verifyException("com.google.common.base.Preconditions", e);
+        }
+    }
+}
