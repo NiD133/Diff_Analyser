380f832dd145036999fbb87b8f29e415560b17b2
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:25:11 +0800

    Original processedManualTestCases base16test15.java

---
+++ base16test15.java
@@ -0,0 +1,64 @@
+package org.apache.commons.codec.binary;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Random;
+import org.apache.commons.codec.CodecPolicy;
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.EncoderException;
+import org.apache.commons.lang3.ArrayUtils;
+import org.junit.jupiter.api.Test;
+
+public class Base16TestTest15 {
+
+    private static final Charset CHARSET_UTF8 = StandardCharsets.UTF_8;
+
+    private final Random random = new Random();
+
+    /**
+     * @return the random.
+     */
+    public Random getRandom() {
+        return this.random;
+    }
+
+    private void testBase16InBuffer(final int startPasSize, final int endPadSize) {
+        final String content = "Hello World";
+        final String encodedContent;
+        final byte[] bytesUtf8 = StringUtils.getBytesUtf8(content);
+        byte[] buffer = ArrayUtils.addAll(bytesUtf8, new byte[endPadSize]);
+        buffer = ArrayUtils.addAll(new byte[startPasSize], buffer);
+        final byte[] encodedBytes = new Base16().encode(buffer, startPasSize, bytesUtf8.length);
+        encodedContent = StringUtils.newStringUtf8(encodedBytes);
+        assertEquals("48656C6C6F20576F726C64", encodedContent, "encoding hello world");
+    }
+
+    private String toString(final byte[] data) {
+        final StringBuilder buf = new StringBuilder();
+        for (int i = 0; i < data.length; i++) {
+            buf.append(data[i]);
+            if (i != data.length - 1) {
+                buf.append(",");
+            }
+        }
+        return buf.toString();
+    }
+
+    // encode/decode random arrays from size 0 to size 11
+    @Test
+    void testEncodeDecodeSmall() {
+        for (int i = 0; i < 12; i++) {
+            final byte[] data = new byte[i];
+            getRandom().nextBytes(data);
+            final byte[] enc = new Base16().encode(data);
+            final byte[] data2 = new Base16().decode(enc);
+            assertArrayEquals(data, data2, toString(data) + " equals " + toString(data2));
+        }
+    }
+}
