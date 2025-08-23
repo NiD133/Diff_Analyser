6b84f9aa793bfef24aa420696ec0de8b78b51c6f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:14:36 +0800

    gemini processedAutoTestCases sequencescomparator_estest14.java

--- sequencescomparator_estest14.java
+++ sequencescomparator_estest14.java
@@ -1,31 +1,38 @@
 package org.apache.commons.collections4.sequence;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.ConcurrentModificationException;
+import static org.junit.Assert.assertEquals;
+
 import java.util.LinkedList;
 import java.util.List;
-import org.apache.commons.collections4.Equator;
-import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.functors.DefaultEquator;
-import org.apache.commons.collections4.functors.NotNullPredicate;
-import org.apache.commons.collections4.functors.PredicateTransformer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class SequencesComparator_ESTestTest14 extends SequencesComparator_ESTest_scaffolding {
+/**
+ * Contains tests for {@link SequencesComparator}.
+ * This refactored test focuses on the understandability of a specific test case.
+ */
+public class SequencesComparator_ESTestTest14 {
 
-    @Test(timeout = 4000)
-    public void test13() throws Throwable {
-        LinkedList<Object> linkedList0 = new LinkedList<Object>();
-        linkedList0.add((Object) linkedList0);
-        LinkedList<Object> linkedList1 = new LinkedList<Object>(linkedList0);
-        linkedList1.add((Object) linkedList1);
-        linkedList1.add((Object) linkedList0);
-        SequencesComparator<Object> sequencesComparator0 = new SequencesComparator<Object>(linkedList1, linkedList0);
-        EditScript<Object> editScript0 = sequencesComparator0.getScript();
-        assertEquals(2, editScript0.getModifications());
+    /**
+     * Tests that the comparator correctly identifies the number of modifications (insertions)
+     * required to transform a shorter sequence into a longer one.
+     */
+    @Test
+    public void getScriptShouldReturnCorrectModificationCountForTwoInsertions() {
+        // Arrange: Define two sequences. The 'to' sequence is created by
+        // keeping the 'from' sequence and appending two new elements.
+        // from: ["A"]
+        // to:   ["A", "B", "C"]
+        final List<String> fromSequence = new LinkedList<>(List.of("A"));
+        final List<String> toSequence = new LinkedList<>(List.of("A", "B", "C"));
+
+        // Act: Generate the edit script by comparing the two sequences.
+        final SequencesComparator<String> comparator = new SequencesComparator<>(fromSequence, toSequence);
+        final EditScript<String> script = comparator.getScript();
+
+        // Assert: The transformation requires keeping one element ("A") and inserting two ("B", "C").
+        // The number of modifications (inserts + deletes) should therefore be 2.
+        final int expectedModifications = 2;
+        assertEquals("The number of modifications should be 2 for two insertions.",
+                     expectedModifications, script.getModifications());
     }
 }
