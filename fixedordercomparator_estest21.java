5b6bab72fdf091cc1ff673d69631736402b2cc42
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:51:25 +0800

    gemini processedAutoTestCases fixedordercomparator_estest21.java

--- fixedordercomparator_estest21.java
+++ fixedordercomparator_estest21.java
@@ -1,27 +1,23 @@
 package org.apache.commons.collections4.comparators;
 
+import static org.junit.Assert.assertFalse;
+
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.function.Function;
-import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.PredicateTransformer;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class FixedOrderComparator_ESTestTest21 extends FixedOrderComparator_ESTest_scaffolding {
+/**
+ * Unit tests for {@link FixedOrderComparator}.
+ * This class focuses on the behavior of the equals() method.
+ */
+public class FixedOrderComparatorTest {
 
-    @Test(timeout = 4000)
-    public void test20() throws Throwable {
-        FixedOrderComparator<FixedOrderComparator<Object>> fixedOrderComparator0 = new FixedOrderComparator<FixedOrderComparator<Object>>();
-        boolean boolean0 = fixedOrderComparator0.equals((Object) null);
-        assertFalse(boolean0);
+    @Test
+    public void equalsShouldReturnFalseWhenComparedToNull() {
+        // Arrange: Create an empty comparator instance.
+        // The generic type is irrelevant for this test, so we use a simple one like String.
+        final FixedOrderComparator<String> comparator = new FixedOrderComparator<>();
+
+        // Act & Assert: Verify that the comparator is not equal to null,
+        // which is a standard contract for the equals() method.
+        assertFalse(comparator.equals(null));
     }
 }
