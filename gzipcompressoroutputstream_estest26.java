53dd0a2356daa25fb9593a1028eaef95577fb58f
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:52:55 +0800

    Original processedAutoTestCases gzipcompressoroutputstream_estest26.java

---
+++ gzipcompressoroutputstream_estest26.java
@@ -0,0 +1,31 @@
+package org.apache.commons.compress.compressors.gzip;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FilterOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.io.PipedOutputStream;
+import java.io.PrintStream;
+import java.util.Locale;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.evosuite.runtime.mock.java.io.MockFileOutputStream;
+import org.evosuite.runtime.mock.java.io.MockPrintStream;
+import org.junit.runner.RunWith;
+
+public class GzipCompressorOutputStream_ESTestTest26 extends GzipCompressorOutputStream_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test25() throws Throwable {
+        MockFileOutputStream mockFileOutputStream0 = new MockFileOutputStream("Y[6ArI`UH_`7e@4");
+        GzipCompressorOutputStream gzipCompressorOutputStream0 = new GzipCompressorOutputStream(mockFileOutputStream0);
+        gzipCompressorOutputStream0.write((-546));
+        assertFalse(gzipCompressorOutputStream0.isClosed());
+    }
+}
