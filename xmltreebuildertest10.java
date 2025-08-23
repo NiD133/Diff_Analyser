b967f86017834da744e84dc69c2f3be3788c5879
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:36:07 +0800

    Original processedManualTestCases xmltreebuildertest10.java

---
+++ xmltreebuildertest10.java
@@ -0,0 +1,32 @@
+package org.jsoup.parser;
+
+import org.jsoup.Jsoup;
+import org.jsoup.TextUtil;
+import org.jsoup.nodes.*;
+import org.jsoup.select.Elements;
+import org.junit.jupiter.api.Test;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import static org.jsoup.nodes.Document.OutputSettings.Syntax;
+import static org.jsoup.parser.Parser.NamespaceHtml;
+import static org.jsoup.parser.Parser.NamespaceXml;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class XmlTreeBuilderTestTest10 {
+
+    private static void assertXmlNamespace(Element el) {
+        assertEquals(NamespaceXml, el.tag().namespace(), String.format("Element %s not in XML namespace", el.tagName()));
+    }
+
+    @Test
+    public void testDoesHandleEOFInTag() {
+        String html = "<img src=asdf onerror=\"alert(1)\" x=";
+        Document xmlDoc = Jsoup.parse(html, "", Parser.xmlParser());
+        assertEquals("<img src=\"asdf\" onerror=\"alert(1)\" x=\"\"></img>", xmlDoc.html());
+    }
+}
