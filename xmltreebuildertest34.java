3653c4db216ce4223364539c2fe6f74eec55d3c1
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:36:14 +0800

    gemini processedManualTestCases xmltreebuildertest34.java

--- xmltreebuildertest34.java
+++ xmltreebuildertest34.java
@@ -1,49 +1,84 @@
 package org.jsoup.parser;
 
 import org.jsoup.Jsoup;
-import org.jsoup.TextUtil;
-import org.jsoup.nodes.*;
-import org.jsoup.select.Elements;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.DocumentType;
+import org.jsoup.nodes.Node;
+import org.jsoup.nodes.XmlDeclaration;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URISyntaxException;
-import java.nio.charset.StandardCharsets;
-import java.util.List;
-import static org.jsoup.nodes.Document.OutputSettings.Syntax;
-import static org.jsoup.parser.Parser.NamespaceHtml;
-import static org.jsoup.parser.Parser.NamespaceXml;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-public class XmlTreeBuilderTestTest34 {
+/**
+ * Tests the parsing of XML declarations by the XmlTreeBuilder.
+ * This includes the XML prolog, DOCTYPE, and other markup declarations.
+ */
+@DisplayName("XmlParser Declaration Parsing")
+class XmlParserDeclarationsTest {
 
-    private static void assertXmlNamespace(Element el) {
-        assertEquals(NamespaceXml, el.tag().namespace(), String.format("Element %s not in XML namespace", el.tagName()));
+    @Test
+    @DisplayName("should correctly parse an XML declaration prolog")
+    void parsesXmlDeclaration() {
+        // Arrange
+        String xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?>";
+        
+        // Act
+        Document doc = Jsoup.parse(xml, Parser.xmlParser());
+        XmlDeclaration xmlDecl = (XmlDeclaration) doc.childNode(0);
+
+        // Assert
+        assertAll("XML Declaration properties",
+            () -> assertEquals("xml", xmlDecl.name()),
+            () -> assertEquals("1.0", xmlDecl.attr("version")),
+            () -> assertEquals("utf-8", xmlDecl.attr("encoding")),
+            () -> assertEquals("version=\"1.0\" encoding=\"utf-8\"", xmlDecl.getWholeDeclaration()),
+            () -> assertEquals(xml, xmlDecl.outerHtml())
+        );
+        assertEquals(xml, doc.outerHtml());
     }
 
     @Test
-    void declarations() {
-        String xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?><!DOCTYPE html\n" + "  PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\"\n" + "  \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">" + "<!ELEMENT footnote (#PCDATA|a)*>";
+    @DisplayName("should correctly parse a DOCTYPE declaration")
+    void parsesDoctypeDeclaration() {
+        // Arrange
+        String xml = "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">";
+
+        // Act
         Document doc = Jsoup.parse(xml, Parser.xmlParser());
-        XmlDeclaration proc = (XmlDeclaration) doc.childNode(0);
-        DocumentType doctype = (DocumentType) doc.childNode(1);
-        XmlDeclaration decl = (XmlDeclaration) doc.childNode(2);
-        assertEquals("xml", proc.name());
-        assertEquals("1.0", proc.attr("version"));
-        assertEquals("utf-8", proc.attr("encoding"));
-        assertEquals("version=\"1.0\" encoding=\"utf-8\"", proc.getWholeDeclaration());
-        assertEquals("<?xml version=\"1.0\" encoding=\"utf-8\"?>", proc.outerHtml());
-        assertEquals("html", doctype.name());
-        assertEquals("-//W3C//DTD XHTML 1.0 Transitional//EN", doctype.attr("publicId"));
-        assertEquals("http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd", doctype.attr("systemId"));
-        assertEquals("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">", doctype.outerHtml());
-        assertEquals("ELEMENT", decl.name());
-        assertEquals("footnote (#PCDATA|a)*", decl.getWholeDeclaration());
-        assertTrue(decl.hasAttr("footNote"));
-        assertFalse(decl.hasAttr("ELEMENT"));
-        assertEquals("<!ELEMENT footnote (#PCDATA|a)*>", decl.outerHtml());
-        assertEquals("<?xml version=\"1.0\" encoding=\"utf-8\"?>" + "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">" + "<!ELEMENT footnote (#PCDATA|a)*>", doc.outerHtml());
+        DocumentType doctype = (DocumentType) doc.childNode(0);
+
+        // Assert
+        assertAll("DOCTYPE properties",
+            () -> assertEquals("html", doctype.name()),
+            () -> assertEquals("-//W3C//DTD XHTML 1.0 Transitional//EN", doctype.attr("publicId")),
+            () -> assertEquals("http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd", doctype.attr("systemId")),
+            () -> assertEquals(xml, doctype.outerHtml())
+        );
+        assertEquals(xml, doc.outerHtml());
+    }
+
+    @Test
+    @DisplayName("should correctly parse a general markup declaration (e.g., <!ELEMENT>)")
+    void parsesMarkupDeclaration() {
+        // Arrange
+        String xml = "<!ELEMENT footnote (#PCDATA|a)*>";
+
+        // Act
+        Document doc = Jsoup.parse(xml, Parser.xmlParser());
+        Node node = doc.childNode(0);
+
+        // Assert
+        // Jsoup uses the XmlDeclaration node to represent general DTD markup declarations like <!ELEMENT>, <!ATTLIST>, etc.
+        assertInstanceOf(XmlDeclaration.class, node, "General markup declarations should be parsed as XmlDeclaration nodes");
+        
+        XmlDeclaration markupDecl = (XmlDeclaration) node;
+        assertAll("Markup Declaration properties",
+            () -> assertEquals("ELEMENT", markupDecl.name()),
+            () -> assertEquals("footnote (#PCDATA|a)*", markupDecl.getWholeDeclaration()),
+            () -> assertEquals(xml, markupDecl.outerHtml())
+        );
+        assertEquals(xml, doc.outerHtml());
     }
 }
