c3fac7bd8bb8660daf7cdabf1b62428bf42d62be
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:29:53 +0800

    Original processedAutoTestCases xyinterval_estest7.java

---
+++ xyinterval_estest7.java
@@ -0,0 +1,21 @@
+package org.jfree.data.xy;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class XYInterval_ESTestTest7 extends XYInterval_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test06() throws Throwable {
+        XYInterval xYInterval0 = new XYInterval((-1.0), (-1.0), 0.0, (-3333.3187), (-1.0));
+        double double0 = xYInterval0.getYLow();
+        assertEquals((-1.0), xYInterval0.getYHigh(), 0.01);
+        assertEquals((-1.0), xYInterval0.getXHigh(), 0.01);
+        assertEquals((-3333.3187), double0, 0.01);
+        assertEquals((-1.0), xYInterval0.getXLow(), 0.01);
+        assertEquals(0.0, xYInterval0.getY(), 0.01);
+    }
+}
