f1cc3b1a9a73b8c730123d46c91444f41732d5c5
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:44:52 +0800

    gemini processedAutoTestCases comparableobjectitem_estest18.java

--- comparableobjectitem_estest18.java
+++ comparableobjectitem_estest18.java
@@ -2,19 +2,28 @@ package org.jfree.data;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
 
-public class ComparableObjectItem_ESTestTest18 extends ComparableObjectItem_ESTest_scaffolding {
+/**
+ * Contains unit tests for the {@link ComparableObjectItem} class.
+ */
+public class ComparableObjectItemTest {
 
-    @Test(timeout = 4000)
-    public void test17() throws Throwable {
-        ComparableObjectItem comparableObjectItem0 = new ComparableObjectItem("^VA/-a*$;'d", "^VA/-a*$;'d");
-        Object object0 = comparableObjectItem0.clone();
-        assertNotSame(comparableObjectItem0, object0);
+    /**
+     * Verifies that the clone() method creates a new object instance that is
+     * a separate entity from the original but holds equal values.
+     */
+    @Test
+    public void cloneShouldReturnADistinctButEqualObject() throws CloneNotSupportedException {
+        // Arrange: Create an original item.
+        ComparableObjectItem originalItem = new ComparableObjectItem("Key", "Value");
+
+        // Act: Create a clone of the original item.
+        ComparableObjectItem clonedItem = (ComparableObjectItem) originalItem.clone();
+
+        // Assert: The clone should be a different object instance...
+        assertNotSame("The cloned object should be a new instance.", originalItem, clonedItem);
+
+        // ...but it should be equal in content to the original.
+        assertEquals("The cloned object should be equal to the original.", originalItem, clonedItem);
     }
 }
