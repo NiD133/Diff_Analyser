bb4590fb01fd959ce13446e44a3bb466b058fec0
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:03:57 +0800

    Original processedAutoTestCases multifilteredrenderlistener_estest7.java

---
+++ multifilteredrenderlistener_estest7.java
@@ -0,0 +1,31 @@
+package com.itextpdf.text.pdf.parser;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.itextpdf.awt.geom.Rectangle2D;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class MultiFilteredRenderListener_ESTestTest7 extends MultiFilteredRenderListener_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test6() throws Throwable {
+        MultiFilteredRenderListener multiFilteredRenderListener0 = new MultiFilteredRenderListener();
+        RenderFilter[] renderFilterArray0 = new RenderFilter[1];
+        multiFilteredRenderListener0.attachRenderListener((LocationTextExtractionStrategy) null, renderFilterArray0);
+        // Undeclared exception!
+        try {
+            multiFilteredRenderListener0.renderText((TextRenderInfo) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("com.itextpdf.text.pdf.parser.MultiFilteredRenderListener", e);
+        }
+    }
+}
