42788a9d083ba5a17adcda08b548df8bc45938d6
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:04:18 +0800

    gemini processedAutoTestCases multireadonlyseekablebytechannel_estest38.java

--- multireadonlyseekablebytechannel_estest38.java
+++ multireadonlyseekablebytechannel_estest38.java
@@ -1,40 +1,32 @@
 package org.apache.commons.compress.utils;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.File;
+
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.channels.ClosedChannelException;
-import java.nio.channels.FileChannel;
 import java.nio.channels.NonWritableChannelException;
 import java.nio.channels.SeekableByteChannel;
-import java.nio.file.NoSuchFileException;
-import java.nio.file.OpenOption;
-import java.nio.file.Path;
-import java.util.LinkedList;
+import java.util.Collections;
 import java.util.List;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.io.MockFile;
-import org.junit.runner.RunWith;
 
-public class MultiReadOnlySeekableByteChannel_ESTestTest38 extends MultiReadOnlySeekableByteChannel_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link MultiReadOnlySeekableByteChannel} class.
+ */
+public class MultiReadOnlySeekableByteChannelTest {
 
-    @Test(timeout = 4000)
-    public void test37() throws Throwable {
-        LinkedList<SeekableByteChannel> linkedList0 = new LinkedList<SeekableByteChannel>();
-        MultiReadOnlySeekableByteChannel multiReadOnlySeekableByteChannel0 = new MultiReadOnlySeekableByteChannel(linkedList0);
-        // Undeclared exception!
-        try {
-            multiReadOnlySeekableByteChannel0.truncate(0L);
-            fail("Expecting exception: NonWritableChannelException");
-        } catch (NonWritableChannelException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("org.apache.commons.compress.utils.MultiReadOnlySeekableByteChannel", e);
-        }
+    /**
+     * Verifies that attempting to truncate a MultiReadOnlySeekableByteChannel
+     * throws a NonWritableChannelException, enforcing its read-only contract.
+     */
+    @Test(expected = NonWritableChannelException.class)
+    public void truncateShouldThrowExceptionForReadOnlyChannel() throws IOException {
+        // Arrange: Create a MultiReadOnlySeekableByteChannel.
+        // The list of underlying channels can be empty for this test's purpose.
+        final List<SeekableByteChannel> emptyChannelList = Collections.emptyList();
+        final SeekableByteChannel channel = new MultiReadOnlySeekableByteChannel(emptyChannelList);
+
+        // Act: Attempt to call the truncate method, which is a write operation.
+        channel.truncate(0L);
+
+        // Assert: The test expects a NonWritableChannelException, as declared in the @Test annotation.
     }
 }
