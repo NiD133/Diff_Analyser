50b844de4b873c5b848666e04cac429ad9b38065
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:49:19 +0800

    gemini processedAutoTestCases endpointpair_estest8.java

--- endpointpair_estest8.java
+++ endpointpair_estest8.java
@@ -1,27 +1,23 @@
 package com.google.common.graph;
 
+import static org.junit.Assert.assertThrows;
+
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import com.google.common.collect.UnmodifiableIterator;
-import java.util.Locale;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class EndpointPair_ESTestTest8 extends EndpointPair_ESTest_scaffolding {
+/**
+ * Tests for the factory methods of {@link EndpointPair}.
+ */
+public class EndpointPairTest {
 
-    @Test(timeout = 4000)
-    public void test07() throws Throwable {
-        // Undeclared exception!
-        try {
-            EndpointPair.ordered((Comparable<Integer>) null, (Comparable<Integer>) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("com.google.common.base.Preconditions", e);
-        }
+    @Test
+    public void ordered_whenSourceIsNull_throwsNullPointerException() {
+        // The source node for an ordered pair is non-nullable.
+        assertThrows(NullPointerException.class, () -> EndpointPair.ordered(null, "nodeV"));
+    }
+
+    @Test
+    public void ordered_whenTargetIsNull_throwsNullPointerException() {
+        // The target node for an ordered pair is non-nullable.
+        assertThrows(NullPointerException.class, () -> EndpointPair.ordered("nodeU", null));
     }
 }
