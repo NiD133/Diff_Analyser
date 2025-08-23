ab83e5a58d73e6566dfd705a9a9a0a4d9cff0d94
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:00:32 +0800

    gemini processedAutoTestCases linkedtreemap_estest35.java

--- linkedtreemap_estest35.java
+++ linkedtreemap_estest35.java
@@ -2,29 +2,31 @@ package com.google.gson.internal;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
 import java.util.AbstractMap;
-import java.util.Comparator;
 import java.util.Map;
 import java.util.Set;
-import java.util.function.BiFunction;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
 
-public class LinkedTreeMap_ESTestTest35 extends LinkedTreeMap_ESTest_scaffolding {
+/**
+ * This test focuses on the behavior of the EntrySet returned by a LinkedTreeMap,
+ * specifically its contains() method.
+ */
+public class LinkedTreeMapEntrySetTest {
 
-    @Test(timeout = 4000)
-    public void test34() throws Throwable {
-        LinkedTreeMap<Integer, Integer> linkedTreeMap0 = new LinkedTreeMap<Integer, Integer>();
-        Integer integer0 = new Integer((-1));
-        LinkedTreeMap.Node<Object, Object> linkedTreeMap_Node0 = new LinkedTreeMap.Node<Object, Object>(true);
-        LinkedTreeMap.Node<Object, Object> linkedTreeMap_Node1 = new LinkedTreeMap.Node<Object, Object>(true, linkedTreeMap_Node0, integer0, linkedTreeMap_Node0, linkedTreeMap_Node0);
-        LinkedTreeMap.EntrySet linkedTreeMap_EntrySet0 = linkedTreeMap0.new EntrySet();
-        boolean boolean0 = linkedTreeMap_EntrySet0.contains(linkedTreeMap_Node1);
-        assertFalse(boolean0);
-        assertEquals(0, linkedTreeMap_EntrySet0.size());
+    @Test
+    public void contains_onEmptySet_shouldReturnFalse() {
+        // Arrange
+        LinkedTreeMap<Integer, Integer> map = new LinkedTreeMap<>();
+        Set<Map.Entry<Integer, Integer>> entrySet = map.entrySet();
+
+        // Create a sample entry that is not in the map.
+        // Using AbstractMap.SimpleEntry is a standard way to create a Map.Entry for testing.
+        Map.Entry<Integer, Integer> nonExistentEntry = new AbstractMap.SimpleEntry<>(-1, 100);
+
+        // Act
+        boolean result = entrySet.contains(nonExistentEntry);
+
+        // Assert
+        assertFalse("contains() on an empty set should always return false.", result);
+        assertEquals("The size of an empty entry set should be 0.", 0, entrySet.size());
     }
 }
