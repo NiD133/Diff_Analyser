26c78cc2b07ad1f2d9aca3fb4f18f9979eab8af7
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:26:26 +0800

    Original processedManualTestCases caseformattest8.java

---
+++ caseformattest8.java
@@ -0,0 +1,24 @@
+package com.google.common.base;
+
+import static com.google.common.base.CaseFormat.LOWER_CAMEL;
+import static com.google.common.base.CaseFormat.LOWER_HYPHEN;
+import static com.google.common.base.CaseFormat.LOWER_UNDERSCORE;
+import static com.google.common.base.CaseFormat.UPPER_CAMEL;
+import static com.google.common.base.CaseFormat.UPPER_UNDERSCORE;
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.truth.Truth.assertWithMessage;
+import com.google.common.annotations.GwtCompatible;
+import com.google.common.annotations.GwtIncompatible;
+import com.google.common.annotations.J2ktIncompatible;
+import com.google.common.testing.NullPointerTester;
+import com.google.common.testing.SerializableTester;
+import junit.framework.TestCase;
+import org.jspecify.annotations.NullUnmarked;
+
+public class CaseFormatTestTest8 extends TestCase {
+
+    public void testLowerUnderscoreToLowerHyphen() {
+        assertThat(LOWER_UNDERSCORE.to(LOWER_HYPHEN, "foo")).isEqualTo("foo");
+        assertThat(LOWER_UNDERSCORE.to(LOWER_HYPHEN, "foo_bar")).isEqualTo("foo-bar");
+    }
+}
