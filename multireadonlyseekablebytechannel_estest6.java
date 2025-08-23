c87b021f0aea57f6f82400d762b99fdfdd8e37ee
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:04:19 +0800

    gemini processedAutoTestCases multireadonlyseekablebytechannel_estest6.java

--- multireadonlyseekablebytechannel_estest6.java
+++ multireadonlyseekablebytechannel_estest6.java
@@ -1,41 +1,32 @@
 package org.apache.commons.compress.utils;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.File;
+
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.channels.ClosedChannelException;
-import java.nio.channels.FileChannel;
-import java.nio.channels.NonWritableChannelException;
 import java.nio.channels.SeekableByteChannel;
-import java.nio.file.NoSuchFileException;
-import java.nio.file.OpenOption;
-import java.nio.file.Path;
-import java.util.LinkedList;
+import java.util.Collections;
 import java.util.List;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.io.MockFile;
-import org.junit.runner.RunWith;
 
-public class MultiReadOnlySeekableByteChannel_ESTestTest6 extends MultiReadOnlySeekableByteChannel_ESTest_scaffolding {
+/**
+ * Tests for {@link MultiReadOnlySeekableByteChannel}.
+ */
+public class MultiReadOnlySeekableByteChannelTest {
 
-    @Test(timeout = 4000)
-    public void test05() throws Throwable {
-        LinkedList<SeekableByteChannel> linkedList0 = new LinkedList<SeekableByteChannel>();
-        linkedList0.add((SeekableByteChannel) null);
-        MultiReadOnlySeekableByteChannel multiReadOnlySeekableByteChannel0 = new MultiReadOnlySeekableByteChannel(linkedList0);
-        // Undeclared exception!
-        try {
-            multiReadOnlySeekableByteChannel0.size();
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("java.util.stream.MatchOps$1MatchSink", e);
-        }
+    /**
+     * Verifies that the size() method throws a NullPointerException
+     * if the channel is constructed with a list containing a null element.
+     * The size() method needs to access each underlying channel to calculate the total size,
+     * and a null channel will cause a NullPointerException.
+     */
+    @Test(expected = NullPointerException.class)
+    public void sizeShouldThrowNPEWhenChannelListContainsNull() throws IOException {
+        // Arrange: Create a list containing a single null channel.
+        List<SeekableByteChannel> channelsWithNull = Collections.singletonList(null);
+        MultiReadOnlySeekableByteChannel multiChannel = new MultiReadOnlySeekableByteChannel(channelsWithNull);
+
+        // Act: Calling size() on the multi-channel should trigger the exception.
+        multiChannel.size();
+
+        // Assert: The @Test(expected) annotation handles the exception verification.
     }
 }
