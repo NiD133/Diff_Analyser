d16c5346e3fb663ee62cf9e0ea909396a436f3e1
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:01:38 +0800

    gemini processedAutoTestCases loopinglistiterator_estest19.java

--- loopinglistiterator_estest19.java
+++ loopinglistiterator_estest19.java
@@ -1,34 +1,29 @@
 package org.apache.commons.collections4.iterators;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.ConcurrentModificationException;
-import java.util.LinkedList;
+
+import java.util.Collections;
 import java.util.List;
 import java.util.NoSuchElementException;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class LoopingListIterator_ESTestTest19 extends LoopingListIterator_ESTest_scaffolding {
+/**
+ * Contains tests for the {@link LoopingListIterator}.
+ */
+public class LoopingListIteratorTest {
 
-    @Test(timeout = 4000)
-    public void test18() throws Throwable {
-        LinkedList<Object> linkedList0 = new LinkedList<Object>();
-        LoopingListIterator<Object> loopingListIterator0 = new LoopingListIterator<Object>(linkedList0);
-        // Undeclared exception!
-        try {
-            loopingListIterator0.previous();
-            fail("Expecting exception: NoSuchElementException");
-        } catch (NoSuchElementException e) {
-            //
-            // There are no elements for this iterator to loop on
-            //
-            verifyException("org.apache.commons.collections4.iterators.LoopingListIterator", e);
-        }
+    /**
+     * Tests that calling previous() on an iterator created with an empty list
+     * throws a NoSuchElementException, as there is no element to retrieve.
+     */
+    @Test(expected = NoSuchElementException.class)
+    public void previousOnEmptyListShouldThrowNoSuchElementException() {
+        // Arrange: Create a LoopingListIterator with an empty list.
+        final List<Object> emptyList = Collections.emptyList();
+        final LoopingListIterator<Object> iterator = new LoopingListIterator<>(emptyList);
+
+        // Act: Attempt to get the previous element, which should fail.
+        iterator.previous();
+
+        // Assert: The expected exception is verified by the @Test(expected=...) annotation.
     }
 }
