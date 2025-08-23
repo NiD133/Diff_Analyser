b194f80ded8c51f37c553e2c28542df4c1f3e7c3
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:52:51 +0800

    Original processedAutoTestCases gzipcompressoroutputstream_estest14.java

---
+++ gzipcompressoroutputstream_estest14.java
@@ -0,0 +1,39 @@
+package org.apache.commons.compress.compressors.gzip;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FilterOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.io.PipedOutputStream;
+import java.io.PrintStream;
+import java.util.Locale;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.evosuite.runtime.mock.java.io.MockFileOutputStream;
+import org.evosuite.runtime.mock.java.io.MockPrintStream;
+import org.junit.runner.RunWith;
+
+public class GzipCompressorOutputStream_ESTestTest14 extends GzipCompressorOutputStream_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test13() throws Throwable {
+        PipedOutputStream pipedOutputStream0 = new PipedOutputStream();
+        GzipParameters gzipParameters0 = new GzipParameters();
+        GzipCompressorOutputStream gzipCompressorOutputStream0 = null;
+        try {
+            gzipCompressorOutputStream0 = new GzipCompressorOutputStream(pipedOutputStream0, gzipParameters0);
+            fail("Expecting exception: IOException");
+        } catch (Throwable e) {
+            //
+            // Pipe not connected
+            //
+            verifyException("java.io.PipedOutputStream", e);
+        }
+    }
+}
