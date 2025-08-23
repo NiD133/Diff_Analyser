eb04b1e62564f400471dc4f9dfe768d1550baa7b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:25:19 +0800

    Original processedAutoTestCases utcinstant_estest38.java

---
+++ utcinstant_estest38.java
@@ -0,0 +1,31 @@
+package org.threeten.extra.scale;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.nio.CharBuffer;
+import java.time.DateTimeException;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.format.DateTimeParseException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.time.MockInstant;
+import org.junit.runner.RunWith;
+
+public class UtcInstant_ESTestTest38 extends UtcInstant_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test37() throws Throwable {
+        // Undeclared exception!
+        try {
+            UtcInstant.of((Instant) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.evosuite.runtime.mock.java.time.MockInstant", e);
+        }
+    }
+}
