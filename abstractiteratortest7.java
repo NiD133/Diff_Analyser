98fce71b2948fd6d15aa404ab5976601f074b862
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:24:38 +0800

    gemini processedManualTestCases abstractiteratortest7.java

--- abstractiteratortest7.java
+++ abstractiteratortest7.java
@@ -1,31 +1,38 @@
 package com.google.common.collect;
 
 import static com.google.common.collect.ReflectionFreeAssertThrows.assertThrows;
-import static com.google.common.collect.SneakyThrows.sneakyThrow;
-import com.google.common.annotations.GwtCompatible;
-import com.google.common.annotations.GwtIncompatible;
-import com.google.common.annotations.J2ktIncompatible;
-import com.google.common.collect.TestExceptions.SomeCheckedException;
+
 import com.google.common.collect.TestExceptions.SomeUncheckedException;
-import com.google.common.testing.GcFinalization;
-import java.lang.ref.WeakReference;
 import java.util.Iterator;
-import java.util.NoSuchElementException;
 import junit.framework.TestCase;
-import org.jspecify.annotations.NullMarked;
-import org.jspecify.annotations.Nullable;
 
-public class AbstractIteratorTestTest7 extends TestCase {
+/**
+ * Tests for {@link AbstractIterator}.
+ */
+public class AbstractIteratorTest extends TestCase {
 
-    public void testExceptionAfterEndOfData() {
-        Iterator<Integer> iter = new AbstractIterator<Integer>() {
-
-            @Override
-            public Integer computeNext() {
-                endOfData();
-                throw new SomeUncheckedException();
-            }
+  /**
+   * This test verifies the behavior of the iterator when the {@code computeNext()} implementation
+   * first signals the end of data and then immediately throws an exception. The expected behavior
+   * is that the exception is propagated, and the iterator is left in a failed state.
+   */
+  public void testHasNext_whenComputeNextThrowsAfterSignalingEndOfData_propagatesException() {
+    // Arrange: Create an iterator whose computeNext() calls endOfData() and then throws.
+    Iterator<Integer> iterator =
+        new AbstractIterator<Integer>() {
+          @Override
+          protected Integer computeNext() {
+            endOfData();
+            throw new SomeUncheckedException();
+          }
         };
-        assertThrows(SomeUncheckedException.class, iter::hasNext);
-    }
+
+    // Act & Assert: The first call to hasNext() should propagate the exception from computeNext().
+    assertThrows(SomeUncheckedException.class, iterator::hasNext);
+
+    // Assert: The iterator is now in a failed state. Subsequent calls to hasNext() or next()
+    // should throw an IllegalStateException, as documented by AbstractIterator.
+    assertThrows(IllegalStateException.class, iterator::hasNext);
+    assertThrows(IllegalStateException.class, iterator::next);
+  }
 }
