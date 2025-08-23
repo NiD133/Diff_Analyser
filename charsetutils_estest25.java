ac2a6a34bfc77952aa3b5a0708e342f44169e2b5
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:42:47 +0800

    Original processedAutoTestCases charsetutils_estest25.java

---
+++ charsetutils_estest25.java
@@ -0,0 +1,15 @@
+package org.apache.commons.lang3;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class CharSetUtils_ESTestTest25 extends CharSetUtils_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test24() throws Throwable {
+        CharSetUtils charSetUtils0 = new CharSetUtils();
+    }
+}
