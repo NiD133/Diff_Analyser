0dfd1feca9e9fd1a2fb3d23c56d674e2608c3c1a
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:40:26 +0800

    Original processedAutoTestCases bufferedline_estest59.java

---
+++ bufferedline_estest59.java
@@ -0,0 +1,35 @@
+package org.locationtech.spatial4j.shape.impl;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.HashMap;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+import org.locationtech.spatial4j.context.SpatialContext;
+import org.locationtech.spatial4j.context.SpatialContextFactory;
+import org.locationtech.spatial4j.distance.CartesianDistCalc;
+import org.locationtech.spatial4j.shape.Point;
+import org.locationtech.spatial4j.shape.Rectangle;
+import org.locationtech.spatial4j.shape.Shape;
+import org.locationtech.spatial4j.shape.SpatialRelation;
+
+public class BufferedLine_ESTestTest59 extends BufferedLine_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test58() throws Throwable {
+        SpatialContext spatialContext0 = SpatialContext.GEO;
+        PointImpl pointImpl0 = new PointImpl(0.0, 0.0, spatialContext0);
+        BufferedLine bufferedLine0 = new BufferedLine(pointImpl0, pointImpl0, 0.0, spatialContext0);
+        // Undeclared exception!
+        try {
+            bufferedLine0.getBuffered((-3307.844427), spatialContext0);
+            fail("Expecting exception: AssertionError");
+        } catch (AssertionError e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+        }
+    }
+}
