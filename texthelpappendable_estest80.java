59daf5803dd82adf4ef5854ed6bb68894014db91
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:21:16 +0800

    gemini processedAutoTestCases texthelpappendable_estest80.java

--- texthelpappendable_estest80.java
+++ texthelpappendable_estest80.java
@@ -1,45 +1,29 @@
 package org.apache.commons.cli.help;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.IOException;
-import java.io.PipedWriter;
-import java.io.StringWriter;
-import java.nio.BufferOverflowException;
-import java.nio.ByteBuffer;
-import java.nio.CharBuffer;
-import java.nio.ReadOnlyBufferException;
-import java.nio.charset.Charset;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Locale;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.SortedSet;
-import java.util.Stack;
-import java.util.TreeSet;
-import java.util.Vector;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
-public class TextHelpAppendable_ESTestTest80 extends TextHelpAppendable_ESTest_scaffolding {
+/**
+ * Tests for {@link TextHelpAppendable}.
+ */
+public class TextHelpAppendableTest {
 
-    @Test(timeout = 4000)
-    public void test79() throws Throwable {
-        TextHelpAppendable textHelpAppendable0 = TextHelpAppendable.systemOut();
-        assertEquals(74, textHelpAppendable0.getMaxWidth());
-        CharBuffer charBuffer0 = CharBuffer.allocate(7);
-        textHelpAppendable0.appendTitle(charBuffer0);
-        assertEquals(3, textHelpAppendable0.getIndent());
+    /**
+     * Verifies that an instance created with the systemOut() factory method
+     * is initialized with the correct default property values.
+     */
+    @Test
+    public void systemOutInstanceShouldHaveDefaultSettings() {
+        // Arrange
+        TextHelpAppendable helpFormatter = TextHelpAppendable.systemOut();
+
+        // Act
+        // No action is performed, as we are testing the initial state after creation.
+
+        // Assert
+        // Check that the formatter is configured with the expected default constants.
+        assertEquals("Default max width should be set", TextHelpAppendable.DEFAULT_WIDTH, helpFormatter.getMaxWidth());
+        assertEquals("Default indent should be set", TextHelpAppendable.DEFAULT_INDENT, helpFormatter.getIndent());
+        assertEquals("Default left pad should be set", TextHelpAppendable.DEFAULT_LEFT_PAD, helpFormatter.getLeftPad());
     }
 }
