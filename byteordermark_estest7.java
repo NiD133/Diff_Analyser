b66f66df96689bfe801761f12a1b237a94fd5e22
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:40:56 +0800

    gemini processedAutoTestCases byteordermark_estest7.java

--- byteordermark_estest7.java
+++ byteordermark_estest7.java
@@ -1,26 +1,22 @@
 package org.apache.commons.io;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class ByteOrderMark_ESTestTest7 extends ByteOrderMark_ESTest_scaffolding {
+/**
+ * Tests for {@link ByteOrderMark}.
+ */
+public class ByteOrderMarkTest {
 
-    @Test(timeout = 4000)
-    public void test06() throws Throwable {
-        ByteOrderMark byteOrderMark0 = ByteOrderMark.UTF_8;
-        // Undeclared exception!
-        try {
-            byteOrderMark0.get((-5));
-            fail("Expecting exception: ArrayIndexOutOfBoundsException");
-        } catch (ArrayIndexOutOfBoundsException e) {
-            //
-            // -5
-            //
-            verifyException("org.apache.commons.io.ByteOrderMark", e);
-        }
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void getShouldThrowExceptionForNegativeIndex() {
+        // Arrange
+        final ByteOrderMark utf8Bom = ByteOrderMark.UTF_8;
+        final int invalidIndex = -1;
+
+        // Act
+        // This call is expected to throw the exception.
+        utf8Bom.get(invalidIndex);
+
+        // Assert: The exception type is verified by the @Test(expected=...) annotation.
     }
 }
