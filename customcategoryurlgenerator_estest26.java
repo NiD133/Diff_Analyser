c13f538e99cbe5e7ddd4f131685406c95a452ff8
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:46:24 +0800

    Original processedAutoTestCases customcategoryurlgenerator_estest26.java

---
+++ customcategoryurlgenerator_estest26.java
@@ -0,0 +1,29 @@
+package org.jfree.chart.urls;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+import java.util.Vector;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.general.DefaultKeyedValues2DDataset;
+import org.junit.runner.RunWith;
+
+public class CustomCategoryURLGenerator_ESTestTest26 extends CustomCategoryURLGenerator_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test25() throws Throwable {
+        CustomCategoryURLGenerator customCategoryURLGenerator0 = new CustomCategoryURLGenerator();
+        CustomCategoryURLGenerator customCategoryURLGenerator1 = (CustomCategoryURLGenerator) customCategoryURLGenerator0.clone();
+        assertTrue(customCategoryURLGenerator1.equals((Object) customCategoryURLGenerator0));
+        Vector<String> vector0 = new Vector<String>();
+        customCategoryURLGenerator1.addURLSeries(vector0);
+        boolean boolean0 = customCategoryURLGenerator0.equals(customCategoryURLGenerator1);
+        assertFalse(boolean0);
+        assertFalse(customCategoryURLGenerator1.equals((Object) customCategoryURLGenerator0));
+    }
+}
