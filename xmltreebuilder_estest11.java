641aea1a792480cdc06f296a09cac129ef7a15c2
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:27:55 +0800

    gemini processedAutoTestCases xmltreebuilder_estest11.java

--- xmltreebuilder_estest11.java
+++ xmltreebuilder_estest11.java
@@ -1,40 +1,35 @@
 package org.jsoup.parser;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.io.PipedReader;
-import java.io.PipedWriter;
-import java.io.Reader;
-import java.io.StringReader;
-import java.io.UncheckedIOException;
-import java.util.ArrayList;
-import java.util.NoSuchElementException;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.jsoup.nodes.Attributes;
-import org.jsoup.nodes.CDataNode;
-import org.jsoup.nodes.Comment;
-import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.nodes.LeafNode;
-import org.jsoup.select.Elements;
-import org.junit.runner.RunWith;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
-public class XmlTreeBuilder_ESTestTest11 extends XmlTreeBuilder_ESTest_scaffolding {
+/**
+ * Test suite for {@link XmlTreeBuilder}.
+ * This class focuses on specific behaviors of the XML tree builder.
+ */
+public class XmlTreeBuilderTest {
 
-    @Test(timeout = 4000)
-    public void test10() throws Throwable {
-        XmlTreeBuilder xmlTreeBuilder0 = new XmlTreeBuilder();
-        Document document0 = xmlTreeBuilder0.parse(">: ", ">: ");
-        Tokeniser tokeniser0 = new Tokeniser(xmlTreeBuilder0);
-        Parser parser0 = new Parser(xmlTreeBuilder0);
-        StreamParser streamParser0 = new StreamParser(parser0);
-        streamParser0.parseFragment("http://www.w3.org/2000/svg", (Element) document0, "http://www.w3.org/XML/1998/namespace");
-        assertEquals(">: ", document0.location());
-        Token.XmlDecl token_XmlDecl0 = tokeniser0.createXmlDeclPending(true);
-        boolean boolean0 = xmlTreeBuilder0.process(token_XmlDecl0);
-        assertTrue(boolean0);
-        assertEquals("http://www.w3.org/XML/1998/namespace", xmlTreeBuilder0.defaultNamespace());
+    /**
+     * Verifies that the default namespace of the XmlTreeBuilder is set to the base URI
+     * provided when parsing an XML fragment.
+     */
+    @Test
+    public void defaultNamespaceIsSetFromBaseUriDuringFragmentParse() {
+        // Arrange
+        XmlTreeBuilder xmlBuilder = new XmlTreeBuilder();
+        Parser parser = new Parser(xmlBuilder);
+        String xmlFragment = "<item>Data</item>";
+        Element context = new Element("root");
+        String expectedNamespace = "http://www.w3.org/2000/svg";
+
+        // Act
+        // The parseFragment method initializes the tree builder for the fragment parse,
+        // which should set the base URI.
+        parser.parseFragment(xmlFragment, context, expectedNamespace);
+
+        // Assert
+        // The defaultNamespace() method is expected to return the base URI that was set.
+        assertEquals(expectedNamespace, xmlBuilder.defaultNamespace());
     }
 }
