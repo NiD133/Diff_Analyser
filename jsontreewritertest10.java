30ebe3208324ab79c4d9092cdd2fc72cb53cbbe5
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:30:15 +0800

    gemini processedManualTestCases jsontreewritertest10.java

--- jsontreewritertest10.java
+++ jsontreewritertest10.java
@@ -1,28 +1,33 @@
 package com.google.gson.internal.bind;
 
 import static com.google.common.truth.Truth.assertThat;
-import static org.junit.Assert.assertThrows;
+
 import com.google.gson.JsonElement;
-import com.google.gson.JsonNull;
-import com.google.gson.Strictness;
-import com.google.gson.common.MoreAsserts;
-import com.google.gson.stream.JsonWriter;
+import com.google.gson.JsonObject;
 import java.io.IOException;
-import java.io.Writer;
-import java.util.Arrays;
-import java.util.List;
 import org.junit.Test;
 
-public class JsonTreeWriterTestTest10 {
+/**
+ * Tests for {@link JsonTreeWriter}.
+ */
+public class JsonTreeWriterTest {
 
-    @Test
-    public void testSerializeNullsFalse() throws IOException {
-        JsonTreeWriter writer = new JsonTreeWriter();
-        writer.setSerializeNulls(false);
-        writer.beginObject();
-        writer.name("A");
-        writer.nullValue();
-        writer.endObject();
-        assertThat(writer.get().toString()).isEqualTo("{}");
-    }
+  @Test
+  public void writingNullObjectProperty_whenSerializeNullsIsFalse_omitsProperty() throws IOException {
+    // Arrange
+    JsonTreeWriter writer = new JsonTreeWriter();
+    writer.setSerializeNulls(false);
+
+    // Act: Write an object with a single property whose value is null.
+    writer.beginObject();
+    writer.name("a");
+    writer.nullValue();
+    writer.endObject();
+
+    // Assert
+    JsonElement result = writer.get();
+    JsonObject expected = new JsonObject(); // An empty JSON object
+
+    assertThat(result).isEqualTo(expected);
+  }
 }
