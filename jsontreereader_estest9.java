4fff465861d4e9ff9fdb4db5e6407ca5ce3b73ed
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:58:23 +0800

    gemini processedAutoTestCases jsontreereader_estest9.java

--- jsontreereader_estest9.java
+++ jsontreereader_estest9.java
@@ -1,40 +1,36 @@
 package com.google.gson.internal.bind;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonNull;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonPrimitive;
-import com.google.gson.Strictness;
-import com.google.gson.stream.JsonToken;
+import org.junit.Test;
+
 import java.io.IOException;
 import java.util.ConcurrentModificationException;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class JsonTreeReader_ESTestTest9 extends JsonTreeReader_ESTest_scaffolding {
+import static org.junit.Assert.assertThrows;
 
-    @Test(timeout = 4000)
-    public void test008() throws Throwable {
-        JsonObject jsonObject0 = new JsonObject();
-        jsonObject0.addProperty("Ql", "Ql");
-        JsonTreeReader jsonTreeReader0 = new JsonTreeReader(jsonObject0);
-        jsonTreeReader0.beginObject();
-        Boolean boolean0 = Boolean.valueOf(false);
-        jsonObject0.addProperty("JSON forbids NaN and infinities: ", boolean0);
-        // Undeclared exception!
-        try {
-            jsonTreeReader0.skipValue();
-            fail("Expecting exception: ConcurrentModificationException");
-        } catch (ConcurrentModificationException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("com.google.gson.internal.LinkedTreeMap$LinkedTreeMapIterator", e);
-        }
+/**
+ * Tests for {@link JsonTreeReader}.
+ * This class focuses on specific behaviors, such as handling concurrent modifications.
+ */
+public class JsonTreeReaderTest {
+
+    @Test
+    public void readingFromObjectAfterConcurrentModificationShouldThrowException() throws IOException {
+        // Arrange: Create a JsonObject and a JsonTreeReader, and begin reading the object.
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("key1", "value1");
+        JsonTreeReader reader = new JsonTreeReader(jsonObject);
+        
+        // Position the reader to start iterating over the object's properties.
+        // This creates an internal iterator for the JsonObject.
+        reader.beginObject();
+
+        // Act: Modify the underlying JsonObject while the reader is iterating over it.
+        // This action invalidates the reader's internal iterator.
+        jsonObject.addProperty("key2", "value2");
+
+        // Assert: Any subsequent read operation on the reader should fail.
+        // We use skipValue() as a representative read operation.
+        assertThrows(ConcurrentModificationException.class, reader::skipValue);
     }
 }
