4af86babf57694090d51dbdc017f88d677495f99
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:34:08 +0800

    gemini processedManualTestCases stringconvertertest19.java

--- stringconvertertest19.java
+++ stringconvertertest19.java
@@ -1,80 +1,29 @@
 package org.joda.time.convert;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-import java.util.Arrays;
-import java.util.Locale;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-import org.joda.time.Chronology;
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
-import org.joda.time.MutableInterval;
-import org.joda.time.MutablePeriod;
 import org.joda.time.PeriodType;
-import org.joda.time.TimeOfDay;
-import org.joda.time.chrono.BuddhistChronology;
-import org.joda.time.chrono.ISOChronology;
-import org.joda.time.chrono.JulianChronology;
+import org.junit.Test;
 
-public class StringConverterTestTest19 extends TestCase {
+import static org.junit.Assert.assertEquals;
 
-    private static final DateTimeZone ONE_HOUR = DateTimeZone.forOffsetHours(1);
+/**
+ * Unit tests for {@link StringConverter}.
+ * This test focuses on the getPeriodType method.
+ */
+public class StringConverterTest {
 
-    private static final DateTimeZone SIX = DateTimeZone.forOffsetHours(6);
+    private final StringConverter converter = StringConverter.INSTANCE;
 
-    private static final DateTimeZone SEVEN = DateTimeZone.forOffsetHours(7);
+    @Test
+    public void getPeriodType_shouldReturnStandardType_forIsoPeriodString() {
+        // Arrange: An ISO 8601 standard representation of a period.
+        // "P2Y6M9D" means a period of 2 years, 6 months, and 9 days.
+        String isoPeriodString = "P2Y6M9D";
+        PeriodType expectedPeriodType = PeriodType.standard();
 
-    private static final DateTimeZone EIGHT = DateTimeZone.forOffsetHours(8);
+        // Act: Get the period type from the string.
+        PeriodType actualPeriodType = converter.getPeriodType(isoPeriodString);
 
-    private static final DateTimeZone UTC = DateTimeZone.UTC;
-
-    private static final DateTimeZone PARIS = DateTimeZone.forID("Europe/Paris");
-
-    private static final DateTimeZone LONDON = DateTimeZone.forID("Europe/London");
-
-    private static final Chronology ISO_EIGHT = ISOChronology.getInstance(EIGHT);
-
-    private static final Chronology ISO_PARIS = ISOChronology.getInstance(PARIS);
-
-    private static final Chronology ISO_LONDON = ISOChronology.getInstance(LONDON);
-
-    private static Chronology ISO;
-
-    private static Chronology JULIAN;
-
-    private DateTimeZone zone = null;
-
-    private Locale locale = null;
-
-    public static void main(String[] args) {
-        junit.textui.TestRunner.run(suite());
-    }
-
-    public static TestSuite suite() {
-        return new TestSuite(TestStringConverter.class);
-    }
-
-    @Override
-    protected void setUp() throws Exception {
-        zone = DateTimeZone.getDefault();
-        locale = Locale.getDefault();
-        DateTimeZone.setDefault(LONDON);
-        Locale.setDefault(Locale.UK);
-        JULIAN = JulianChronology.getInstance();
-        ISO = ISOChronology.getInstance();
-    }
-
-    @Override
-    protected void tearDown() throws Exception {
-        DateTimeZone.setDefault(zone);
-        Locale.setDefault(locale);
-        zone = null;
-    }
-
-    //-----------------------------------------------------------------------
-    public void testGetPeriodType_Object() throws Exception {
-        assertEquals(PeriodType.standard(), StringConverter.INSTANCE.getPeriodType("P2Y6M9D"));
+        // Assert: The converter should correctly identify the standard period type.
+        assertEquals(expectedPeriodType, actualPeriodType);
     }
 }
