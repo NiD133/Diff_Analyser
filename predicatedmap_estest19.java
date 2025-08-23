6222cd3bfd76e8844d70030032b20e4ac807656d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:09:25 +0800

    Original processedAutoTestCases predicatedmap_estest19.java

---
+++ predicatedmap_estest19.java
@@ -0,0 +1,51 @@
+package org.apache.commons.collections4.map;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.lang.reflect.Array;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import org.apache.commons.collections4.Predicate;
+import org.apache.commons.collections4.Transformer;
+import org.apache.commons.collections4.functors.AnyPredicate;
+import org.apache.commons.collections4.functors.ComparatorPredicate;
+import org.apache.commons.collections4.functors.ConstantTransformer;
+import org.apache.commons.collections4.functors.EqualPredicate;
+import org.apache.commons.collections4.functors.ExceptionPredicate;
+import org.apache.commons.collections4.functors.IdentityPredicate;
+import org.apache.commons.collections4.functors.NonePredicate;
+import org.apache.commons.collections4.functors.NotNullPredicate;
+import org.apache.commons.collections4.functors.NullPredicate;
+import org.apache.commons.collections4.functors.OnePredicate;
+import org.apache.commons.collections4.functors.OrPredicate;
+import org.apache.commons.collections4.functors.PredicateTransformer;
+import org.apache.commons.collections4.functors.TransformerPredicate;
+import org.apache.commons.collections4.functors.TruePredicate;
+import org.apache.commons.collections4.functors.UniquePredicate;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.junit.runner.RunWith;
+
+public class PredicatedMap_ESTestTest19 extends PredicatedMap_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test18() throws Throwable {
+        Predicate<Object> predicate0 = TruePredicate.truePredicate();
+        PredicatedMap<Object, Transformer<Object, Integer>> predicatedMap0 = null;
+        try {
+            predicatedMap0 = new PredicatedMap<Object, Transformer<Object, Integer>>((Map<Object, Transformer<Object, Integer>>) null, predicate0, predicate0);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // map
+            //
+            verifyException("java.util.Objects", e);
+        }
+    }
+}
