3792a7255f499589abb04cb34bdfbfdbf4e22cd0
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:30:09 +0800

    gemini processedManualTestCases jsonparsertest7.java

--- jsonparsertest7.java
+++ jsonparsertest7.java
@@ -1,20 +1,29 @@
 package com.google.gson;
 
-import static com.google.common.truth.Truth.assertThat;
 import static org.junit.Assert.assertThrows;
-import com.google.gson.common.TestTypes.BagOfPrimitives;
-import com.google.gson.internal.Streams;
-import com.google.gson.stream.JsonReader;
-import java.io.CharArrayReader;
-import java.io.CharArrayWriter;
-import java.io.IOException;
-import java.io.StringReader;
+
 import org.junit.Test;
 
-public class JsonParserTestTest7 {
+/**
+ * Tests for {@link JsonParser} focusing on handling of invalid inputs.
+ */
+public class JsonParserTest {
 
-    @Test
-    public void testParseUnquotedMultiWordStringFails() {
-        assertThrows(JsonSyntaxException.class, () -> JsonParser.parseString("Test is a test..blah blah"));
-    }
+  /**
+   * Tests that attempting to parse a string literal that is not enclosed in quotes
+   * and contains multiple words results in a {@link JsonSyntaxException}.
+   * According to the JSON specification, a string value must be enclosed in double quotes.
+   * The parser should correctly identify this as a syntax error.
+   */
+  @Test
+  public void parseString_withUnquotedMultiWordString_throwsJsonSyntaxException() {
+    // Arrange: Define an invalid JSON string which is not quoted.
+    String unquotedMultiWordString = "Test is a test..blah blah";
+
+    // Act & Assert: Verify that parsing the invalid string throws a JsonSyntaxException.
+    assertThrows(
+        JsonSyntaxException.class,
+        () -> JsonParser.parseString(unquotedMultiWordString)
+    );
+  }
 }
