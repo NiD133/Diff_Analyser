33dfbe0d120d54afb2f5480c5560ce5373337fdc
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:24:12 +0800

    Original processedAutoTestCases typeadapter_estest8.java

---
+++ typeadapter_estest8.java
@@ -0,0 +1,29 @@
+package com.google.gson;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.io.Writer;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+
+public class TypeAdapter_ESTestTest8 extends TypeAdapter_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test07() throws Throwable {
+        Gson.FutureTypeAdapter<Object> gson_FutureTypeAdapter0 = new Gson.FutureTypeAdapter<Object>();
+        Gson.FutureTypeAdapter<Object> gson_FutureTypeAdapter1 = new Gson.FutureTypeAdapter<Object>();
+        gson_FutureTypeAdapter1.setDelegate(gson_FutureTypeAdapter0);
+        gson_FutureTypeAdapter0.setDelegate(gson_FutureTypeAdapter1);
+        // Undeclared exception!
+        gson_FutureTypeAdapter0.toJsonTree(gson_FutureTypeAdapter0);
+    }
+}
