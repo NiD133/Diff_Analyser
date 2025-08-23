7224f89157a384aea870db64081a498ca4178d4b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:30:07 +0800

    gemini processedManualTestCases jsonparsertest12.java

--- jsonparsertest12.java
+++ jsonparsertest12.java
@@ -1,34 +1,45 @@
 package com.google.gson;
 
 import static com.google.common.truth.Truth.assertThat;
-import static org.junit.Assert.assertThrows;
+
 import com.google.gson.common.TestTypes.BagOfPrimitives;
-import com.google.gson.internal.Streams;
 import com.google.gson.stream.JsonReader;
-import java.io.CharArrayReader;
-import java.io.CharArrayWriter;
-import java.io.IOException;
+import com.google.gson.stream.Strictness;
 import java.io.StringReader;
 import org.junit.Test;
 
 public class JsonParserTestTest12 {
 
-    @Test
-    public void testReadWriteTwoObjects() throws Exception {
-        Gson gson = new Gson();
-        CharArrayWriter writer = new CharArrayWriter();
-        BagOfPrimitives expectedOne = new BagOfPrimitives(1, 1, true, "one");
-        writer.write(gson.toJson(expectedOne).toCharArray());
-        BagOfPrimitives expectedTwo = new BagOfPrimitives(2, 2, false, "two");
-        writer.write(gson.toJson(expectedTwo).toCharArray());
-        CharArrayReader reader = new CharArrayReader(writer.toCharArray());
-        JsonReader parser = new JsonReader(reader);
-        parser.setStrictness(Strictness.LENIENT);
-        JsonElement element1 = Streams.parse(parser);
-        JsonElement element2 = Streams.parse(parser);
-        BagOfPrimitives actualOne = gson.fromJson(element1, BagOfPrimitives.class);
-        assertThat(actualOne.stringValue).isEqualTo("one");
-        BagOfPrimitives actualTwo = gson.fromJson(element2, BagOfPrimitives.class);
-        assertThat(actualTwo.stringValue).isEqualTo("two");
-    }
+  /**
+   * Tests that {@link JsonParser#parseReader(JsonReader)} can parse a stream
+   * containing multiple top-level JSON objects when the reader is in lenient mode.
+   * Strict JSON format only allows a single top-level value.
+   */
+  @Test
+  public void parseReader_multipleJsonObjectsInStream_inLenientMode() {
+    // Arrange
+    BagOfPrimitives expected1 = new BagOfPrimitives(1, 1, true, "one");
+    BagOfPrimitives expected2 = new BagOfPrimitives(2, 2, false, "two");
+
+    // A stream containing two consecutive JSON objects. This is not valid in strict mode.
+    String jsonStream =
+        "{\"longValue\":1,\"intValue\":1,\"booleanValue\":true,\"stringValue\":\"one\"}"
+      + "{\"longValue\":2,\"intValue\":2,\"booleanValue\":false,\"stringValue\":\"two\"}";
+
+    JsonReader jsonReader = new JsonReader(new StringReader(jsonStream));
+    jsonReader.setStrictness(Strictness.LENIENT);
+
+    Gson gson = new Gson();
+
+    // Act
+    JsonElement element1 = JsonParser.parseReader(jsonReader);
+    JsonElement element2 = JsonParser.parseReader(jsonReader);
+
+    // Assert
+    BagOfPrimitives actual1 = gson.fromJson(element1, BagOfPrimitives.class);
+    BagOfPrimitives actual2 = gson.fromJson(element2, BagOfPrimitives.class);
+
+    assertThat(actual1).isEqualTo(expected1);
+    assertThat(actual2).isEqualTo(expected2);
+  }
 }
