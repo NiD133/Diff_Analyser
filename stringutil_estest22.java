85cfba18f5d1c5d15b134b68cbb4e179763c2f9b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:18:46 +0800

    gemini processedAutoTestCases stringutil_estest22.java

--- stringutil_estest22.java
+++ stringutil_estest22.java
@@ -1,29 +1,29 @@
 package org.jsoup.internal;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Collection;
-import java.util.ConcurrentModificationException;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.ListIterator;
-import java.util.stream.Collector;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.evosuite.runtime.mock.java.net.MockURL;
-import org.junit.runner.RunWith;
 
-public class StringUtil_ESTestTest22 extends StringUtil_ESTest_scaffolding {
+import java.util.Collections;
 
-    @Test(timeout = 4000)
-    public void test21() throws Throwable {
-        LinkedList<StringBuilder> linkedList0 = new LinkedList<StringBuilder>();
-        String string0 = StringUtil.join((Collection<?>) linkedList0, "String must not be empty");
-        assertEquals("", string0);
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Tests for the {@link StringUtil} class.
+ */
+public class StringUtilTest {
+
+    @Test
+    public void joinOnEmptyCollectionReturnsEmptyString() {
+        // Arrange
+        // An empty collection, the type of elements does not matter.
+        Iterable<?> emptyCollection = Collections.emptyList();
+        String separator = ", ";
+
+        // Act
+        // The join method is called with the empty collection.
+        String result = StringUtil.join(emptyCollection, separator);
+
+        // Assert
+        // The result should be an empty string, regardless of the separator.
+        assertEquals("", result);
     }
 }
