61cc405112bb43814458c495ae4c9cb7dec9a3b1
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:41:40 +0800

    gemini processedAutoTestCases caseformat_estest21.java

--- caseformat_estest21.java
+++ caseformat_estest21.java
@@ -1,27 +1,22 @@
 package com.google.common.base;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.junit.runner.RunWith;
 
-public class CaseFormat_ESTestTest21 extends CaseFormat_ESTest_scaffolding {
+/**
+ * Tests for {@link CaseFormat}.
+ */
+public class CaseFormatTest {
 
-    @Test(timeout = 4000)
-    public void test20() throws Throwable {
-        CaseFormat caseFormat0 = CaseFormat.LOWER_HYPHEN;
-        CaseFormat caseFormat1 = CaseFormat.UPPER_UNDERSCORE;
-        // Undeclared exception!
-        try {
-            caseFormat0.convert(caseFormat1, (String) null);
-            fail("Expecting exception: NullPointerException");
-        } catch (NullPointerException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("com.google.common.base.CaseFormat$1", e);
-        }
+    @Test(expected = NullPointerException.class)
+    public void to_whenGivenNullInputString_throwsNullPointerException() {
+        // Arrange
+        CaseFormat sourceFormat = CaseFormat.LOWER_HYPHEN;
+        CaseFormat targetFormat = CaseFormat.UPPER_UNDERSCORE;
+
+        // Act
+        // The 'to' method should throw a NullPointerException when the input string is null.
+        sourceFormat.to(targetFormat, null);
+
+        // Assert (handled by the @Test(expected=...) annotation)
     }
 }
