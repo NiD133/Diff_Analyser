715597ce66190dd1e75d0a4b1965d4023f6cf3fe
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:46:12 +0800

    Original processedAutoTestCases countingpathvisitor_estest34.java

---
+++ countingpathvisitor_estest34.java
@@ -0,0 +1,47 @@
+package org.apache.commons.io.file;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.function.UnaryOperator;
+import org.apache.commons.io.filefilter.CanWriteFileFilter;
+import org.apache.commons.io.filefilter.EmptyFileFilter;
+import org.apache.commons.io.filefilter.FileFileFilter;
+import org.apache.commons.io.filefilter.HiddenFileFilter;
+import org.apache.commons.io.filefilter.IOFileFilter;
+import org.apache.commons.io.filefilter.NotFileFilter;
+import org.apache.commons.io.filefilter.PathEqualsFileFilter;
+import org.apache.commons.io.filefilter.SuffixFileFilter;
+import org.apache.commons.io.function.IOBiFunction;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.evosuite.runtime.mock.java.io.MockIOException;
+import org.junit.runner.RunWith;
+
+public class CountingPathVisitor_ESTestTest34 extends CountingPathVisitor_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test33() throws Throwable {
+        Counters.PathCounters counters_PathCounters0 = CountingPathVisitor.defaultPathCounters();
+        FileFileFilter fileFileFilter0 = (FileFileFilter) FileFileFilter.INSTANCE;
+        CountingPathVisitor countingPathVisitor0 = null;
+        try {
+            countingPathVisitor0 = new CountingPathVisitor(counters_PathCounters0, fileFileFilter0, fileFileFilter0, (IOBiFunction<Path, IOException, FileVisitResult>) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // visitFileFailedFunction
+            //
+            verifyException("java.util.Objects", e);
+        }
+    }
+}
