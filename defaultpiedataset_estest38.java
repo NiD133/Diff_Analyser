f2db9eb524003b6f935deaf9ab2d8b1e410b9870
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:47:46 +0800

    gemini processedAutoTestCases defaultpiedataset_estest38.java

--- defaultpiedataset_estest38.java
+++ defaultpiedataset_estest38.java
@@ -2,31 +2,26 @@ package org.jfree.data.general;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.time.chrono.ChronoLocalDate;
-import java.util.List;
-import java.util.Vector;
-import javax.swing.JLayeredPane;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.jfree.chart.api.SortOrder;
-import org.jfree.chart.api.TableOrder;
-import org.jfree.data.DefaultKeyedValues;
-import org.jfree.data.KeyedValues;
-import org.jfree.data.category.CategoryDataset;
-import org.jfree.data.category.CategoryToPieDataset;
-import org.jfree.data.category.DefaultIntervalCategoryDataset;
-import org.jfree.data.category.SlidingCategoryDataset;
-import org.jfree.data.statistics.DefaultBoxAndWhiskerCategoryDataset;
-import org.junit.runner.RunWith;
 
-public class DefaultPieDataset_ESTestTest38 extends DefaultPieDataset_ESTest_scaffolding {
+/**
+ * Unit tests for the {@link DefaultPieDataset} class.
+ */
+public class DefaultPieDatasetTest {
 
-    @Test(timeout = 4000)
-    public void test37() throws Throwable {
-        DefaultPieDataset<Integer> defaultPieDataset0 = new DefaultPieDataset<Integer>();
-        Integer integer0 = JLayeredPane.DRAG_LAYER;
-        int int0 = defaultPieDataset0.getIndex(integer0);
-        assertEquals((-1), int0);
+    /**
+     * Verifies that getIndex() returns -1 for a key that does not exist in the dataset.
+     * This also implicitly tests the behavior for an empty dataset.
+     */
+    @Test
+    public void getIndex_forNonExistentKey_shouldReturnNegativeOne() {
+        // Arrange: Create an empty pie dataset.
+        DefaultPieDataset<Integer> dataset = new DefaultPieDataset<>();
+        Integer nonExistentKey = 400; // An arbitrary key not present in the dataset.
+
+        // Act: Attempt to get the index of the non-existent key.
+        int actualIndex = dataset.getIndex(nonExistentKey);
+
+        // Assert: The returned index should be -1, indicating the key was not found.
+        assertEquals(-1, actualIndex);
     }
 }
