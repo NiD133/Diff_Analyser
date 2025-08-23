f4ac83ee5550daefcf3245cbf56c9533292b3567
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:36:37 +0800

    gemini processedAutoTestCases abstractpartial_estest10.java

--- abstractpartial_estest10.java
+++ abstractpartial_estest10.java
@@ -1,46 +1,25 @@
 package org.joda.time.base;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.util.Date;
-import java.util.Locale;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.evosuite.runtime.mock.java.util.MockGregorianCalendar;
-import org.joda.time.Chronology;
-import org.joda.time.DateTime;
 import org.joda.time.DateTimeField;
-import org.joda.time.DateTimeFieldType;
-import org.joda.time.DateTimeZone;
-import org.joda.time.DurationFieldType;
-import org.joda.time.Instant;
-import org.joda.time.LocalDate;
-import org.joda.time.LocalDateTime;
-import org.joda.time.LocalTime;
-import org.joda.time.MonthDay;
 import org.joda.time.Partial;
-import org.joda.time.ReadablePartial;
-import org.joda.time.Weeks;
-import org.joda.time.YearMonth;
-import org.joda.time.Years;
-import org.joda.time.chrono.CopticChronology;
-import org.joda.time.chrono.GJChronology;
-import org.joda.time.chrono.GregorianChronology;
-import org.joda.time.chrono.IslamicChronology;
-import org.joda.time.format.DateTimeFormatter;
-import org.joda.time.format.DateTimeParser;
-import org.joda.time.format.DateTimePrinter;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class AbstractPartial_ESTestTest10 extends AbstractPartial_ESTest_scaffolding {
+import static org.junit.Assert.assertEquals;
 
-    @Test(timeout = 4000)
-    public void test09() throws Throwable {
-        Partial partial0 = new Partial();
-        DateTimeField[] dateTimeFieldArray0 = partial0.getFields();
-        assertEquals(0, dateTimeFieldArray0.length);
+/**
+ * Unit test for the getFields() method in AbstractPartial, using Partial as the concrete implementation.
+ */
+public class AbstractPartialTest {
+
+    @Test
+    public void getFields_onEmptyPartial_returnsEmptyArray() {
+        // Arrange: Create an empty Partial instance, which contains no fields.
+        Partial emptyPartial = new Partial();
+
+        // Act: Retrieve the array of DateTimeField objects.
+        DateTimeField[] fields = emptyPartial.getFields();
+
+        // Assert: The returned array should be empty.
+        assertEquals("An empty Partial should have no fields.", 0, fields.length);
     }
 }
