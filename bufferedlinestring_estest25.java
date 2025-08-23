6a1c6c5f8809042ec4cf9712cbb66b6d0c255148
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:40:04 +0800

    Original processedAutoTestCases bufferedlinestring_estest25.java

---
+++ bufferedlinestring_estest25.java
@@ -0,0 +1,36 @@
+package org.locationtech.spatial4j.shape.impl;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.util.HashMap;
+import java.util.LinkedList;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.junit.runner.RunWith;
+import org.locationtech.spatial4j.context.SpatialContext;
+import org.locationtech.spatial4j.context.SpatialContextFactory;
+import org.locationtech.spatial4j.distance.GeodesicSphereDistCalc;
+import org.locationtech.spatial4j.shape.Point;
+import org.locationtech.spatial4j.shape.Rectangle;
+import org.locationtech.spatial4j.shape.Shape;
+import org.locationtech.spatial4j.shape.ShapeCollection;
+import org.locationtech.spatial4j.shape.SpatialRelation;
+
+public class BufferedLineString_ESTestTest25 extends BufferedLineString_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test24() throws Throwable {
+        LinkedList<Point> linkedList0 = new LinkedList<Point>();
+        BufferedLineString bufferedLineString0 = null;
+        try {
+            bufferedLineString0 = new BufferedLineString(linkedList0, (-116.02777), (SpatialContext) null);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.locationtech.spatial4j.shape.impl.BufferedLineString", e);
+        }
+    }
+}
