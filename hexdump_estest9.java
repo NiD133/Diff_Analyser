0db83e4978a5a7af53918b7e9518a75306e56e29
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:53:19 +0800

    Original processedAutoTestCases hexdump_estest9.java

---
+++ hexdump_estest9.java
@@ -0,0 +1,41 @@
+package org.apache.commons.io;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PipedOutputStream;
+import java.io.PipedWriter;
+import java.io.StringWriter;
+import java.nio.BufferOverflowException;
+import java.nio.CharBuffer;
+import java.nio.ReadOnlyBufferException;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.mock.java.io.MockFile;
+import org.evosuite.runtime.mock.java.io.MockFileOutputStream;
+import org.evosuite.runtime.mock.java.io.MockFileWriter;
+import org.evosuite.runtime.mock.java.io.MockPrintStream;
+import org.junit.runner.RunWith;
+
+public class HexDump_ESTestTest9 extends HexDump_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test08() throws Throwable {
+        MockPrintStream mockPrintStream0 = new MockPrintStream("\n");
+        // Undeclared exception!
+        try {
+            HexDump.dump((byte[]) null, (-2841L), (OutputStream) mockPrintStream0, 198);
+            fail("Expecting exception: NullPointerException");
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("org.apache.commons.io.HexDump", e);
+        }
+    }
+}
