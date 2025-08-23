700b4b7c7ff8b3e87da7fd581510262151949dd8
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:23:15 +0800

    gemini processedAutoTestCases transformedmultivaluedmap_estest21.java

--- transformedmultivaluedmap_estest21.java
+++ transformedmultivaluedmap_estest21.java
@@ -1,65 +1,45 @@
 package org.apache.commons.collections4.multimap;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.AbstractMap;
-import java.util.ArrayDeque;
-import java.util.Comparator;
-import java.util.ConcurrentModificationException;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
-import java.util.PriorityQueue;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.Factory;
 import org.apache.commons.collections4.MultiValuedMap;
-import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AllPredicate;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ChainedTransformer;
-import org.apache.commons.collections4.functors.CloneTransformer;
-import org.apache.commons.collections4.functors.ClosureTransformer;
-import org.apache.commons.collections4.functors.ConstantFactory;
-import org.apache.commons.collections4.functors.ConstantTransformer;
-import org.apache.commons.collections4.functors.ExceptionFactory;
-import org.apache.commons.collections4.functors.ExceptionTransformer;
-import org.apache.commons.collections4.functors.FactoryTransformer;
-import org.apache.commons.collections4.functors.IfTransformer;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.MapTransformer;
-import org.apache.commons.collections4.functors.NOPTransformer;
-import org.apache.commons.collections4.functors.NotPredicate;
-import org.apache.commons.collections4.functors.NullIsExceptionPredicate;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.SwitchTransformer;
-import org.apache.commons.collections4.functors.TransformerClosure;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class TransformedMultiValuedMap_ESTestTest21 extends TransformedMultiValuedMap_ESTest_scaffolding {
+import java.util.Collections;
+import java.util.Map;
 
-    @Test(timeout = 4000)
-    public void test20() throws Throwable {
-        LinkedHashSetValuedLinkedHashMap<Integer, Integer> linkedHashSetValuedLinkedHashMap0 = new LinkedHashSetValuedLinkedHashMap<Integer, Integer>((-1));
-        HashMap<Integer, Integer> hashMap0 = new HashMap<Integer, Integer>();
-        Integer integer0 = new Integer((-1));
-        hashMap0.put(integer0, integer0);
-        TransformedMultiValuedMap<Integer, Integer> transformedMultiValuedMap0 = new TransformedMultiValuedMap<Integer, Integer>(linkedHashSetValuedLinkedHashMap0, (Transformer<? super Integer, ? extends Integer>) null, (Transformer<? super Integer, ? extends Integer>) null);
-        // Undeclared exception!
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+/**
+ * Contains tests for {@link TransformedMultiValuedMap}.
+ */
+public class TransformedMultiValuedMapTest {
+
+    /**
+     * Tests that an exception from the decorated map's putAll method
+     * is correctly propagated by the TransformedMultiValuedMap.
+     */
+    @Test
+    public void putAllWithMapShouldPropagateExceptionFromDecoratedMap() {
+        // Arrange: Create a backing map that is guaranteed to fail.
+        // The LinkedHashSetValuedLinkedHashMap uses an internal HashMap. By providing a
+        // negative initial capacity, we ensure its constructor will throw an
+        // IllegalArgumentException when it's eventually initialized during a putAll call.
+        final MultiValuedMap<Integer, Integer> failingBackingMap =
+                new LinkedHashSetValuedLinkedHashMap<>(-1);
+
+        // The transformers are irrelevant for this test, so they are set to null.
+        final TransformedMultiValuedMap<Integer, Integer> transformedMap =
+                new TransformedMultiValuedMap<>(failingBackingMap, null, null);
+
+        final Map<Integer, Integer> mapToAdd = Collections.singletonMap(1, 1);
+
+        // Act & Assert
         try {
-            transformedMultiValuedMap0.putAll((Map<? extends Integer, ? extends Integer>) hashMap0);
-            fail("Expecting exception: IllegalArgumentException");
-        } catch (IllegalArgumentException e) {
-            //
-            // Illegal initial capacity: -1
-            //
-            verifyException("java.util.HashMap", e);
+            transformedMap.putAll(mapToAdd);
+            fail("Expected an IllegalArgumentException to be thrown.");
+        } catch (final IllegalArgumentException e) {
+            // Verify that the expected exception from the underlying map was propagated.
+            assertEquals("Illegal initial capacity: -1", e.getMessage());
         }
     }
 }
