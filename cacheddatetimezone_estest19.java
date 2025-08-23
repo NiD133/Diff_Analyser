1aa803a7c8a81444bd5620d99ca1dfad6de7ef1b
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:41:17 +0800

    Original processedAutoTestCases cacheddatetimezone_estest19.java

---
+++ cacheddatetimezone_estest19.java
@@ -0,0 +1,28 @@
+package org.joda.time.tz;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.joda.time.Chronology;
+import org.joda.time.DateTimeZone;
+import org.joda.time.Instant;
+import org.joda.time.LocalDateTime;
+import org.joda.time.chrono.GregorianChronology;
+import org.junit.runner.RunWith;
+
+public class CachedDateTimeZone_ESTestTest19 extends CachedDateTimeZone_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test18() throws Throwable {
+        DateTimeZone dateTimeZone0 = DateTimeZone.getDefault();
+        // Undeclared exception!
+        try {
+            dateTimeZone0.nextTransition((-1L));
+            //  fail("Expecting exception: IllegalArgumentException");
+            // Unstable assertion
+        } catch (IllegalArgumentException e) {
+        }
+    }
+}
