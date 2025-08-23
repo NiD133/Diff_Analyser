bff5ae042a4bf43c98cd5200195de376a6d00a3d
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:41:45 +0800

    gemini processedAutoTestCases categoryitementity_estest13.java

--- categoryitementity_estest13.java
+++ categoryitementity_estest13.java
@@ -1,39 +1,46 @@
 package org.jfree.chart.entity;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.awt.Polygon;
-import java.awt.Rectangle;
-import java.awt.geom.Rectangle2D;
-import java.time.Clock;
-import java.time.LocalDate;
-import java.time.chrono.ChronoLocalDate;
-import java.time.chrono.JapaneseDate;
-import javax.swing.JLayeredPane;
-import javax.swing.text.DefaultCaret;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.mock.java.time.MockClock;
-import org.evosuite.runtime.mock.java.time.MockLocalDate;
-import org.evosuite.runtime.mock.java.time.chrono.MockJapaneseDate;
 import org.jfree.data.category.CategoryDataset;
 import org.jfree.data.statistics.DefaultBoxAndWhiskerCategoryDataset;
 import org.jfree.data.statistics.DefaultMultiValueCategoryDataset;
-import org.jfree.data.statistics.DefaultStatisticalCategoryDataset;
-import org.junit.runner.RunWith;
+import org.junit.Test;
 
-public class CategoryItemEntity_ESTestTest13 extends CategoryItemEntity_ESTest_scaffolding {
+import java.awt.Rectangle;
+import java.awt.Shape;
 
-    @Test(timeout = 4000)
-    public void test12() throws Throwable {
-        DefaultCaret defaultCaret0 = new DefaultCaret();
-        DefaultMultiValueCategoryDataset<Integer, Integer> defaultMultiValueCategoryDataset0 = new DefaultMultiValueCategoryDataset<Integer, Integer>();
-        CategoryItemEntity<Integer, Integer> categoryItemEntity0 = new CategoryItemEntity<Integer, Integer>(defaultCaret0, (String) null, (String) null, defaultMultiValueCategoryDataset0, (Integer) 0, (Integer) 0);
-        DefaultBoxAndWhiskerCategoryDataset<Integer, Integer> defaultBoxAndWhiskerCategoryDataset0 = new DefaultBoxAndWhiskerCategoryDataset<Integer, Integer>();
-        categoryItemEntity0.setDataset(defaultBoxAndWhiskerCategoryDataset0);
-        CategoryItemEntity<Integer, Integer> categoryItemEntity1 = new CategoryItemEntity<Integer, Integer>(defaultCaret0, (String) null, (String) null, defaultMultiValueCategoryDataset0, (Integer) defaultCaret0.y, (Integer) 0);
-        boolean boolean0 = categoryItemEntity0.equals(categoryItemEntity1);
-        assertFalse(boolean0);
+import static org.junit.Assert.assertFalse;
+
+/**
+ * Contains tests for the equals() method in the {@link CategoryItemEntity} class.
+ */
+public class CategoryItemEntityEqualsTest {
+
+    /**
+     * Verifies that the equals() method returns false for two entities that
+     * are identical in all aspects except for their underlying dataset.
+     */
+    @Test
+    public void equals_shouldReturnFalse_whenDatasetsAreDifferent() {
+        // Arrange
+        Shape commonArea = new Rectangle();
+        Integer commonKey = 0;
+
+        // Create two different types of datasets to ensure they are not equal.
+        CategoryDataset<Integer, Integer> dataset1 = new DefaultMultiValueCategoryDataset<>();
+        CategoryDataset<Integer, Integer> dataset2 = new DefaultBoxAndWhiskerCategoryDataset<>();
+
+        // Create the first entity using the first dataset.
+        CategoryItemEntity<Integer, Integer> entity1 = new CategoryItemEntity<>(
+                commonArea, null, null, dataset1, commonKey, commonKey);
+
+        // Create a second entity that is identical to the first, but uses the second dataset.
+        CategoryItemEntity<Integer, Integer> entity2 = new CategoryItemEntity<>(
+                commonArea, null, null, dataset2, commonKey, commonKey);
+
+        // Act
+        boolean areEqual = entity1.equals(entity2);
+
+        // Assert
+        assertFalse("Entities with different datasets should not be equal", areEqual);
     }
 }
