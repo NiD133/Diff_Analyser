52d5bcf0960237eba15c735210a1ceca75a38204
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 00:51:06 +0800

    gemini processedAutoTestCases filterlistiterator_estest28.java

--- filterlistiterator_estest28.java
+++ filterlistiterator_estest28.java
@@ -1,70 +1,34 @@
 package org.apache.commons.collections4.iterators;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.evosuite.shaded.org.mockito.Mockito.*;
-import static org.evosuite.runtime.EvoAssertions.*;
-import java.lang.reflect.Array;
-import java.util.Collection;
-import java.util.Comparator;
+
 import java.util.ConcurrentModificationException;
 import java.util.LinkedList;
 import java.util.ListIterator;
-import java.util.Map;
-import java.util.NoSuchElementException;
-import java.util.function.Consumer;
-import org.apache.commons.collections4.Closure;
-import org.apache.commons.collections4.Equator;
-import org.apache.commons.collections4.Predicate;
-import org.apache.commons.collections4.Transformer;
-import org.apache.commons.collections4.functors.AnyPredicate;
-import org.apache.commons.collections4.functors.ClosureTransformer;
-import org.apache.commons.collections4.functors.ComparatorPredicate;
-import org.apache.commons.collections4.functors.DefaultEquator;
-import org.apache.commons.collections4.functors.EqualPredicate;
-import org.apache.commons.collections4.functors.ExceptionClosure;
-import org.apache.commons.collections4.functors.ExceptionPredicate;
-import org.apache.commons.collections4.functors.IdentityPredicate;
-import org.apache.commons.collections4.functors.IfClosure;
-import org.apache.commons.collections4.functors.InstanceofPredicate;
-import org.apache.commons.collections4.functors.InvokerTransformer;
-import org.apache.commons.collections4.functors.MapTransformer;
-import org.apache.commons.collections4.functors.NonePredicate;
-import org.apache.commons.collections4.functors.NotNullPredicate;
-import org.apache.commons.collections4.functors.NotPredicate;
-import org.apache.commons.collections4.functors.NullIsExceptionPredicate;
-import org.apache.commons.collections4.functors.NullIsFalsePredicate;
-import org.apache.commons.collections4.functors.NullIsTruePredicate;
-import org.apache.commons.collections4.functors.NullPredicate;
-import org.apache.commons.collections4.functors.OnePredicate;
-import org.apache.commons.collections4.functors.TransformedPredicate;
-import org.apache.commons.collections4.functors.TransformerPredicate;
-import org.apache.commons.collections4.functors.TruePredicate;
-import org.apache.commons.collections4.functors.UniquePredicate;
-import org.apache.commons.collections4.functors.WhileClosure;
-import org.evosuite.runtime.EvoRunner;
-import org.evosuite.runtime.EvoRunnerParameters;
-import org.evosuite.runtime.ViolatedAssumptionAnswer;
-import org.junit.runner.RunWith;
 
-public class FilterListIterator_ESTestTest28 extends FilterListIterator_ESTest_scaffolding {
+/**
+ * Contains tests for the FilterListIterator class, focusing on its behavior
+ * when the underlying collection is modified during iteration.
+ */
+public class FilterListIteratorConcurrencyTest {
 
-    @Test(timeout = 4000)
-    public void test27() throws Throwable {
-        LinkedList<Integer> linkedList0 = new LinkedList<Integer>();
-        ListIterator<Integer> listIterator0 = linkedList0.listIterator(0);
-        Integer integer0 = new Integer(681);
-        linkedList0.offerLast(integer0);
-        FilterListIterator<Object> filterListIterator0 = new FilterListIterator<Object>(listIterator0);
-        // Undeclared exception!
-        try {
-            filterListIterator0.hasNext();
-            fail("Expecting exception: ConcurrentModificationException");
-        } catch (ConcurrentModificationException e) {
-            //
-            // no message in exception (getMessage() returned null)
-            //
-            verifyException("java.util.LinkedList$ListItr", e);
-        }
+    /**
+     * Tests that hasNext() throws a ConcurrentModificationException if the underlying
+     * collection is modified after the FilterListIterator is created. This is the
+     * expected "fail-fast" behavior inherited from the underlying iterator.
+     */
+    @Test(expected = ConcurrentModificationException.class)
+    public void hasNextShouldThrowConcurrentModificationExceptionWhenUnderlyingListIsModified() {
+        // Arrange: Create a list, get an iterator, and wrap it in a FilterListIterator.
+        final LinkedList<Integer> list = new LinkedList<>();
+        final ListIterator<Integer> underlyingIterator = list.listIterator();
+        final FilterListIterator<Integer> filteredIterator = new FilterListIterator<>(underlyingIterator);
+
+        // Act: Modify the underlying list directly. This invalidates the iterator's state.
+        list.add(100);
+
+        // Assert: Calling hasNext() on the now-invalidated iterator is expected to throw
+        // a ConcurrentModificationException.
+        filteredIterator.hasNext();
     }
 }
