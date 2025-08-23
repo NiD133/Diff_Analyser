6df4b90a4e12fa7d806ccc7d3c34ae6652b2a123
NiD133 <k19052355@kcl.ac.uk>
2025-08-24 01:19:17 +0800

    Original processedAutoTestCases stringutil_estest96.java

---
+++ stringutil_estest96.java
@@ -0,0 +1,30 @@
+package org.jsoup.internal;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.evosuite.shaded.org.mockito.Mockito.*;
+import static org.evosuite.runtime.EvoAssertions.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Collection;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.stream.Collector;
+import org.evosuite.runtime.EvoRunner;
+import org.evosuite.runtime.EvoRunnerParameters;
+import org.evosuite.runtime.ViolatedAssumptionAnswer;
+import org.evosuite.runtime.mock.java.net.MockURL;
+import org.junit.runner.RunWith;
+
+public class StringUtil_ESTestTest96 extends StringUtil_ESTest_scaffolding {
+
+    @Test(timeout = 4000)
+    public void test95() throws Throwable {
+        String string0 = StringUtil.padding(8170, (-1));
+        StringUtil.normaliseWhitespace(string0);
+        // Undeclared exception!
+        StringUtil.normaliseWhitespace(string0);
+    }
+}
